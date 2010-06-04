b907c2f419b9220adba08abae1d23c2d95689f1c
Lennart Koopmann <lennart@scopeport.org>
2010-06-05 01:07:30 +0200

    changed wrong notices on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,9 @@ class UsersController < ApplicationController
       # reset session
       self.current_user = @user # !! now logged in
       redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      flash[:notice] = "User has been created."
     else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      flash[:error]  = "Could not create user."
       render :action => 'new'
     end
   end
