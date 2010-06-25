9be158a86acbb9d08caa0e054bee9a57ab0524ba
Lennart Koopmann <lennart@scopeport.org>
2010-06-25 21:01:14 +0200

    Added statistics section

---
+++ app/controllers/statistics_controller.rb
@@ -0,0 +1,4 @@
+class StatisticsController < ApplicationController
+  def index
+  end
+end
--- app/models/systemstatistic.rb
+++ app/models/systemstatistic.rb
@@ -4,4 +4,16 @@ class Systemstatistic
   set_database_name 'graylog2'
 
   key :handled_syslog_events, Float
+
+  def self.get_total_messages
+    Message.count.to_i
+  end
+
+  def self.get_message_count_of_last_minute
+    begin
+      return Systemstatistic.first(:order => "created_at DESC").handled_syslog_events.to_i
+    rescue
+      return 0
+    end
+  end
 end
