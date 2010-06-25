14410d7a1555daf3fb30c5aa571fa18e84c743ca
Lennart Koopmann <lennart@scopeport.org>
2010-06-25 19:37:36 +0200

    fixed not working purging of systemstatistics table on startup

---
+++ src/org/graylog2/MongoBridge.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright 2010 Lennart Koopmann <lennart@scopeport.org>
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * MongoBridge.java: lennart | Apr 13, 2010 9:13:03 PM
+ */
+
+package org.graylog2;
+
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBObjectBuilder;
+
+import graylog2.*;
+
+import java.util.List;
+
+import org.productivity.java.syslog4j.server.SyslogServerEventIF;
+
+public class MongoBridge {
+    // TODO: make configurable
+    public static final int MAX_MESSAGE_SIZE = 500000000;
+    public static final int STANDARD_PORT = 27017;
+    
+    private String username = null;
+    private String password = null;
+    private String hostname = null;
+    private String database = null;
+    private int    port = 27017;
+
+    public MongoBridge(String username, String password, String hostname, String database, int port) throws Exception {
+        this.username = username;
+        this.password = password;
+        this.hostname = hostname;
+        this.database = database;
+        if (port == 0) {
+            this.port = MongoBridge.STANDARD_PORT;
+        } else {
+            this.port = port;
+        }
+    }
+
+    public void dropCollection(String collectionName) throws Exception {
+        MongoConnection.getInstance().getDatabase().getCollection(collectionName).remove(new BasicDBObject());
+    }
+
+    public DBCollection getMessagesColl() {
+        DBCollection coll = null;
+
+        // Create a capped collection if the collection does not yet exist.
+        if(MongoConnection.getInstance().getDatabase().getCollectionNames().contains("messages")) {
+            coll = MongoConnection.getInstance().getDatabase().getCollection("messages");
+        } else {
+            coll = MongoConnection.getInstance().getDatabase().createCollection("messages", BasicDBObjectBuilder.start().add("capped", true).add("size", MongoBridge.MAX_MESSAGE_SIZE).get());
+        }
+
+        coll.ensureIndex(new BasicDBObject("created_at", 1));
+        coll.ensureIndex(new BasicDBObject("host", 1));
+        coll.ensureIndex(new BasicDBObject("facility", 1));
+        coll.ensureIndex(new BasicDBObject("level", 1));
+
+        return coll;
+    }
+
+    public void insert(SyslogServerEventIF event) throws Exception {
+        DBCollection coll = this.getMessagesColl();
+
+        BasicDBObject dbObj = new BasicDBObject();
+        dbObj.put("message", event.getMessage());
+        dbObj.put("host", event.getHost());
+        dbObj.put("facility", event.getFacility());
+        dbObj.put("level", event.getLevel());
+        dbObj.put("created_at", (int) (System.currentTimeMillis()/1000));
+
+        coll.insert(dbObj);
+    }
+    
+    public void insertSystemStatisticValue(String key, long value) throws Exception {       
+        DBCollection coll = null;
+
+        // Create a capped collection if the collection does not yet exist.
+        if(MongoConnection.getInstance().getDatabase().getCollectionNames().contains("systemstatistics")) {
+            coll = MongoConnection.getInstance().getDatabase().getCollection("systemstatistics");
+        } else {
+            coll = MongoConnection.getInstance().getDatabase().createCollection("systemstatistics", BasicDBObjectBuilder.start().add("capped", true).add("size", 5242880).get());
+        }
+        
+        BasicDBObject dbObj = new BasicDBObject();
+        dbObj.put(key, value);
+        dbObj.put("timestamp", System.currentTimeMillis()/1000);
+
+        coll.insert(dbObj);
+    }
+
+    public void distinctHosts() throws Exception {
+        // Fetch all hosts.
+        DBCollection messages = this.getMessagesColl();
+        List<String> hosts = messages.distinct("host");
+
+        DBCollection coll = null;
+
+        // Create a capped collection if the collection does not yet exist.
+        if(MongoConnection.getInstance().getDatabase().getCollectionNames().contains("hosts")) {
+            coll = MongoConnection.getInstance().getDatabase().getCollection("hosts");
+        } else {
+            coll = MongoConnection.getInstance().getDatabase().createCollection("hosts", new BasicDBObject());
+        }
+
+        coll.ensureIndex(new BasicDBObject("name", 1));
+
+        // Truncate host collection.
+        coll.remove(new BasicDBObject());
+        
+        // Go trough every host and insert.
+        for (String host : hosts) {
+            try {
+                // Get message count of this host.
+                BasicDBObject countQuery = new BasicDBObject();
+                countQuery.put("host", host);
+                long messageCount = messages.getCount(countQuery);
+
+                // Build document.
+                BasicDBObject doc = new BasicDBObject();
+                doc.put("host", host);
+                doc.put("message_count", messageCount);
+
+                // Store document.
+                coll.insert(doc);
+            } catch (Exception e) {
+                Log.crit("Could not insert distinct host: " + e.toString());
+                continue;
+            }
+        }
+    }
+
+}
---
+++ src/org/graylog2/MongoConnection.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2010 Lennart Koopmann <lennart@scopeport.org>
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * MongoConnection.java: Lennart Koopmann <lennart@scopeport.org> | Jun 6, 2010 1:36:19 PM
+ */
+
+package graylog2;
+
+import com.mongodb.Mongo;
+import com.mongodb.DB;
+import com.mongodb.MongoException;
+
+public class MongoConnection {
+    private static MongoConnection INSTANCE;
+
+    private static Mongo m = null;
+    private static DB db = null;
+
+    private MongoConnection() {}
+
+    public synchronized static MongoConnection getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new MongoConnection();
+        }
+        return INSTANCE;
+    }
+
+    public void connect(String username, String password, String hostname, String database, int port, String useAuth) throws Exception {
+        try {
+            MongoConnection.m = new Mongo(hostname, port);
+            MongoConnection.db = m.getDB(database);
+
+            // Try to authenticate if configured.
+            if (useAuth.equals("true")) {
+                if(!db.authenticate(username, password.toCharArray())) {
+                    throw new Exception("Could not authenticate to database '" + database + "' with user '" + username + "'.");
+                }
+            }
+        } catch (MongoException.Network e) {
+            throw new Exception("Could not connect to Mongo DB. (" + e.toString() + ")");
+        }
+    }
+
+    public Mongo getConnection() {
+        return MongoConnection.m;
+    }
+
+    public DB getDatabase() {
+        return MongoConnection.db;
+    }
+}
