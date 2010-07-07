9467dc7b27ad4956471c3cf8923a19425d0afddb
Lennart Koopmann <lennart@scopeport.org>
2010-07-07 18:04:05 +0200

    message count interval now user definable

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,9 +2,18 @@ class Setting < ActiveRecord::Base
   TYPE_MESSAGE_LENGTH = 1
   TYPE_MESSAGE_LENGTH_STANDARD = 150
 
+  TYPE_MESSAGE_COUNT_INTERVAL = 2
+  TYPE_MESSAGE_COUNT_INTERVAL_STANDARD = 10
+
   def self.get_message_length current_user
     setting = Setting.find_by_user_id_and_setting_type current_user.id, TYPE_MESSAGE_LENGTH
     return TYPE_MESSAGE_LENGTH_STANDARD if setting.blank?
+    return setting.value
+  end
+
+  def self.get_message_count_interval current_user
+    setting = Setting.find_by_user_id_and_setting_type current_user.id, TYPE_MESSAGE_COUNT_INTERVAL
+    return TYPE_MESSAGE_COUNT_INTERVAL_STANDARD if setting.blank?
     return setting.value
   end
 end
