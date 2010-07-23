0110c434d96342589e723911bcfd62106c556c64
Lennart Koopmann <lennart@scopeport.org>
2010-07-23 23:26:37 +0200

    styled GELF full message view similar messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,4 +19,45 @@ class MessagesController < ApplicationController
     message = Message.find params[:id]
     render :text => CGI.escapeHTML(message.message)
   end
+
+  def getsimilarmessages
+    # Get the message we want to compare.
+    message = Message.find params[:id]
+    message_id = message.id
+    message = message.message
+
+    ## Prepare comparison
+    # Cut until first ':' if there is one and it is in the first half of the message (this is usually the hostname)
+    if message.include?(':') and message.index(':') <= message.length/2
+      message = message[message.index(':')+1..message.length].strip
+    end
+
+    # Get first 30% chars of message. This is what we will do a REGEX search over all messages.
+    get_chars = ((message.length.to_f)/100*30).to_i
+    message = message[0..get_chars]
+
+    ## Get the messages
+    # Blacklist
+    conditions = Hash.new
+    (blacklist = BlacklistedTerm.get_all_as_condition_hash).blank? ? nil : conditions[:message] = blacklist;
+
+    # Add our search condition.
+    conditions[:message] = Hash.new if conditions[:message].blank?
+    conditions[:message]['$in'] = [/#{Regexp.escape(message)}/]
+
+    # Don't search for the message we used to compare.
+    #conditions[:id] = Hash.new
+    #conditions[:id]['$nin'] = [message_id]
+
+    # Get the messages.
+    @messages = Message.all :limit => 50, :order => "_id DESC", :conditions => conditions
+
+    if @messages.blank?
+      render :text => "No similar messages found"
+      return
+    end
+
+    @inline_version = true
+    render :partial => "table"
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -106,6 +106,17 @@ $(document).ready(function(){
         return false;
     });
 
+    // Show similar messages in GELF show view.
+    $('#messages-show-similar-messages-link').bind('click', function() {
+        $('#messages-show-similar-messages-link').hide();
+        $('.loading').show();
+
+        $.post("/messages/getsimilarmessages", {id: $('#message-id').html()}, function(data) {
+            $('#messages-show-similar-messages').html(data);
+        });
+        return false;
+    });
+
     $('#messages-show-message-full').css('width', parseInt($('#content').css('width'))-15);
     $('#messages-show-message-full').css('height', parseInt($('#messages-show-message-full').css('height'))+10);
 });
