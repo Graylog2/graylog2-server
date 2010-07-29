a673dbf1e91bb14fe32207347cdce0c95dc9a96f
Lennart Koopmann <lennart@scopeport.org>
2010-07-29 19:14:12 +0200

    further prepared for release

--- config/environment.rb
+++ config/environment.rb
@@ -17,9 +17,15 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "mongo", :version => '1.0.1'
-  config.gem "bson_ext", :version => '1.0.1'
-  config.gem "mysql", :version => '2.8.1'
+  config.gem 'json', :version => '1.4.3'
+  config.gem 'bson', :version => '1.0.1'
+  config.gem 'mongo', :version => '1.0.1'
+  config.gem 'bson_ext', :version => '1.0.1'
+  config.gem 'mongo_mapper', :version => '0.8.2'
+  config.gem 'jnunemaker-validatable', :version => '1.8.4' # Dependency of mongo_mapper
+  config.gem 'plucky', :version => '0.3.2' # Dependency of mongo_mapper
+  config.gem 'mysql', :version => '2.8.1'
+  config.gem 'rack', :version => '1.1.0'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
