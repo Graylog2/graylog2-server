b29181fcf1d92f5622084e6e95751e9776df8371
Lennart Koopmann <lennart@scopeport.org>
2010-07-30 18:03:25 +0200

    the release build script

---
+++ build_script/build_release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+BUILD_NUMBER=$1
+BUILD_NAME=graylog2-server-$BUILD_NUMBER
+BUILD_DIR=builds/$BUILD_NAME
+BUILD_DATE=`date`
+LOGFILE=`pwd`/logs/$BUILD_NAME
+
+# Check if required version parameter is given
+if [ -z $BUILD_NUMBER ]; then
+  echo "ERROR: Missing parameter. (build number)"
+  exit 1
+fi
+
+# Create directories
+mkdir -p logs
+mkdir -p builds
+mkdir -p $BUILD_DIR
+
+# Create logfile
+touch $LOGFILE
+date >> $LOGFILE
+
+echo "BUILDING $BUILD_NAME"
+
+# Add build date to release.
+echo $BUILD_DATE > $BUILD_DIR/build_date
+
+echo "Copying files ..."
+
+# Copy files.
+cp ../dist ../README $BUILD_DIR -r
+
+# Copy example config file
+cp ../misc/graylog2.conf $BUILD_DIR/graylog2.conf.example
+
+# Copy control script
+cp copy/bin $BUILD_DIR -r
+
+cd builds/
+
+# tar it
+echo "Building Tarball ..."
+tar cfz $BUILD_NAME.tar.gz $BUILD_NAME
+rm -rf ./$BUILD_NAME
+
+echo "DONE! Created Graylog2 release $BUILD_NAME on $BUILD_DATE"
