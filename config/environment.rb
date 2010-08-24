5b2cc3a63f2752fd054ba14e228bd01e72fbf0d3
Lennart Koopmann <lennart@scopeport.org>
2010-08-24 16:57:35 +0200

    Updated required gem versions. Now compatible with MongoDB >= 1.6.0

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,15 @@ require File.join(File.dirname(__FILE__), 'boot')
 require 'mongo_mapper'
 
 Rails::Initializer.run do |config|
-  config.gem 'json', :version => '1.4.3'
-  config.gem 'bson', :version => '1.0.1'
-  config.gem 'mongo', :version => '1.0.1'
-  config.gem 'bson_ext', :version => '1.0.1'
-  config.gem 'mongo_mapper', :version => '0.8.2'
+  config.gem 'json', :version => '1.4.6'
+  config.gem 'bson', :version => '1.0.4'
+  config.gem 'mongo', :version => '1.0.7'
+  config.gem 'bson_ext', :version => '1.0.4'
+  config.gem 'mongo_mapper', :version => '0.8.3'
   config.gem 'jnunemaker-validatable', :version => '1.8.4' # Dependency of mongo_mapper
-  config.gem 'plucky', :version => '0.3.2' # Dependency of mongo_mapper
+  config.gem 'plucky', :version => '0.3.4' # Dependency of mongo_mapper
   config.gem 'mysql', :version => '2.8.1'
-  config.gem 'rack', :version => '1.1.0'
+  config.gem 'rack', :version => '1.2.1'
 
   config.time_zone = 'Berlin'
 end
