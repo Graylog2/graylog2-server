251efdc2944371997004acda382757963bf5f578
Sean Porter <portertech@gmail.com>
2010-10-09 15:16:57 -0700

    move mongomapper config

--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -1,9 +1,8 @@
-db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
+db_config = YAML::load(File.read(RAILS_ROOT + "/config/mongodb.yml"))
 
 begin
-  if db_config['mongodb']
-    config = db_config['mongodb']
-
+  if db_config[Rails.env]
+    config = db_config[Rails.env]
     MongoMapper.connection = Mongo::Connection.new(config['hostname'], config['port'], { :logger => Rails.logger })
     MongoMapper.database = config['database']
     if config['authenticate'] == true
