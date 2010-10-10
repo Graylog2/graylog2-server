1ed8cf1af2a7cb7c0c794f541554b72c2cf1c4a6
Sean Porter <portertech@gmail.com>
2010-10-10 08:55:03 +0800

    rrd config based on env

--- config/initializers/rrd.rb
+++ config/initializers/rrd.rb
@@ -4,17 +4,17 @@ begin
 
   # Is graphing enabled?
   $rrd_graphs_enabled = true
-  if rrd_config["graphs"]["enable_graphs"].blank? or rrd_config["graphs"]["enable_graphs"] == false
+  if rrd_config[Rails.env]["enable_graphs"].blank? or rrd_config[Rails.env]["enable_graphs"] == false
     $rrd_graphs_enabled = false
   end
 
   if $rrd_graphs_enabled
-    $rrd_binary = rrd_config["graphs"]["rrdtool_binary_path"]
+    $rrd_binary = rrd_config[Rails.env]["rrdtool_binary_path"]
     throw "Missing variable 'rrdtool_binary_path'" if $rrd_binary.blank?
     throw "'rrdtool_binary_path' does not exist!" if !File.exist? $rrd_binary
     throw "'rrdtool_binary_path' is not executable!" if !File.executable? $rrd_binary
 
-    $rrd_storage_path = rrd_config["graphs"]["rrd_storage_path"]
+    $rrd_storage_path = rrd_config[Rails.env]["rrd_storage_path"]
     throw "Missing variable 'rrd_storage_path'" if $rrd_storage_path.blank?
     throw "'rrd_storage_path' does not exist!" if !File.directory? $rrd_storage_path
 
