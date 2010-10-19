5e881d548003459a56d31472e53e03b884cb9e6b
Dennis Oelkers <dennis@lauschmusik.de>
2010-10-19 12:27:25 +0200

    Fixed minor typos

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -21,7 +21,7 @@ class Setting < ActiveRecord::Base
   end
 
   def self.get_message_max_count current_user
-    setting = Setting.find_by_user_id_and_setting_type current_user.object_sid, TYPE_MESSAGE_MAX_COUNT
+    setting = Setting.find_by_user_id_and_setting_type current_user.object_id, TYPE_MESSAGE_MAX_COUNT
     return TYPE_MESSAGE_MAX_COUNT_STANDARD if setting.blank?
     return setting.value
   end
