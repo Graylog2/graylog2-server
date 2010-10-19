a9328e8253970d6b71032e9d140a6fd67b6456f2
Sean Porter <portertech@gmail.com>
2010-10-19 01:25:48 -0700

    updated a few gems, basic replica set support

--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -3,7 +3,11 @@ db_config = YAML::load(File.read(RAILS_ROOT + "/config/mongodb.yml"))
 begin
   if db_config[Rails.env]
     config = db_config[Rails.env]
-    MongoMapper.connection = Mongo::Connection.new(config['hostname'], config['port'], { :logger => Rails.logger })
+    if config['hostname'].is_a? Array
+      MongoMapper.connection = Mongo::Connection.multi(config['hostname'], { :logger => Rails.logger })
+    else
+      MongoMapper.connection = Mongo::Connection.new(config['hostname'], config['port'], { :logger => Rails.logger })
+    end
     MongoMapper.database = config['database']
     if config['authenticate'] == true
       MongoMapper.database.authenticate(config['username'], config['password'])
