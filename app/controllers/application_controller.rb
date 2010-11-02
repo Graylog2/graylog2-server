686a6de4535e4ff518190722490ae2d1f5f6f2a8
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-11-02 17:05:43 +0800

    Clean-up exception handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,31 +3,42 @@
 
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
-  
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  
+
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
 
   before_filter :login_required
 
-  def rescue_action e
-    # Connection to MongoDB failed.
-    if e.class == Mongo::ConnectionFailure
-        render :file => "#{RAILS_ROOT}/public/mongo_connectionfailure.html", :status => 500
-        return
+  rescue_from Exception do |e|
+    if consider_all_requests_local || local_request?
+      rescue_action_without_handler e
+    else
+      logger.error "ERROR: #{e.to_s}"
+      render :file  => "#{RAILS_ROOT}/public/500.html", :status => 500
     end
+  end
 
-    # Default 404 error.
-    if e.class == ActionController::RoutingError
-      render :file  => "#{RAILS_ROOT}/public/404.html", :status => 404
-      return
+  rescue_from ActionView::TemplateError do |e|
+    # This rescue_from handling all kind of errors which is raised during template rendering.
+    # So, if environment is development, we should handle it as usual.
+    # If environment is test or production, we should handle it in particular way –
+    # just reraise the exception so that other rescue_from can handle it.
+    if Rails.env.development?
+      rescue_action_without_handler e
+    else
+      rescue_with_handler e.original_exception
     end
+  end
 
-    # Default 500 error.
-    logger.error "ERROR: #{e.to_s}"
-    render :file  => "#{RAILS_ROOT}/public/500.html", :status => 500
+  rescue_from Mongo::ConnectionFailure do |e|
+    render :file => "#{RAILS_ROOT}/public/mongo_connectionfailure.html", :status => 500
+  end
+
+  rescue_from ActionController::RoutingError do |e|
+    render :file  => "#{RAILS_ROOT}/public/404.html", :status => 404
   end
 
   helper_method :has_users
@@ -44,7 +55,7 @@ class ApplicationController < ActionController::Base
     end
     return false
   end
-  
+
   def login_required
     if !logged_in?
       redirect_to :controller => "sessions", :action => "new"
