b128f6ebc0f4b0c0a7eaa5ae4171e828f47a4c56
Sean Porter <portertech@gmail.com>
2010-11-11 01:26:06 -0800

    passwords are now filtered from the log in config/application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
-
   before_filter :login_required
 
   def rescue_action e
