e7675e0cd5e57434628d1bccfd12847db916f030
Lennart Koopmann <lennart@socketfeed.com>
2010-11-12 21:38:54 +0100

    cleanup routes. revert setting from rails 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
-#ActionController::Routing::Routes.draw do |map|
-#  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-#  map.login '/login', :controller => 'sessions', :action => 'new'
-#
-#  map.resource :session
-#
-#  map.root :controller => "messages"
-#  
-#  map.connect ':controller/:action/:id'
-#  map.connect ':controller/:action/:id.:format'
-#end
-
 Graylog2WebInterface::Application.routes.draw do
   match '/logout' => 'sessions#destroy', :as => :logout
   match '/login' => 'sessions#new', :as => :login
