e86e5fe60b84826e8c45939d5bae5d8fb388df4c
Dennis Oelkers <dennis@lauschmusik.de>
2010-11-19 23:21:03 +0100

    - added permission for new alert function

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -2,7 +2,7 @@ authorization do
   role :admin do
     has_permission_on :messages, :to => [:index, :show]
     
-    has_permission_on :streams, :to => [:index, :show, :create, :delete]
+    has_permission_on :streams, :to => [:index, :show, :create, :delete, :alertable]
     has_permission_on :streamrules, :to => [:create, :delete]
     
     has_permission_on :hosts, :to => [:index]
@@ -18,7 +18,7 @@ authorization do
   end
   
   role :reader do
-    has_permission_on :streams, :to => [:index, :show] do
+    has_permission_on :streams, :to => [:index, :show, :alertable] do
       if_attribute :users => contains { user }
     end
     has_permission_on :sessions, :to => [:destroy]
