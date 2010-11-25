6b11606ebe5324f50c3690ce895ae900ab1b3398
Lennart Koopmann <lennart@socketfeed.com>
2010-11-25 16:38:36 +0100

    don't always create a new connection

--- misc/convert_rsyslog_db_to_mongo.rb
+++ misc/convert_rsyslog_db_to_mongo.rb
@@ -7,8 +7,11 @@ require 'time'
 
 
 begin
-  dbh = Mysql.real_connect("localhost", "root", "123123123", "Syslog")
+  dbh = Mysql.real_connect("localhost", "root", "mama", "Syslog")
   res = dbh.query("SELECT Message, ReceivedAt, FromHost, Facility, Priority FROM SystemEvents ORDER BY ReceivedAt ASC");
+  
+  db = Mongo::Connection.new.db("graylog2")
+  coll = db.collection("messages")
 
   while row = res.fetch_row do
     begin
@@ -20,8 +23,6 @@ begin
       level = row[4].to_i
 
       # store in mongo
-      db = Mongo::Connection.new.db("graylog2")
-      coll = db.collection("messages")
       doc = {"message" => message, "date" => date, "created_at" => created_at, "host" => host, "facility" => facility, "level" => level}
       coll.insert(doc)
     rescue Exception => e
