b25267f353faa5a26112fa0496fbc0c5216cd86a
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-12-10 22:39:59 +0300

    Fix test for Ruby 1.9 / MiniTest.

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -31,7 +31,7 @@ class SessionsControllerTest < ActionController::TestCase
   def test_should_not_remember_me
     @request.cookies["auth_token"] = nil
     post :create, :login => 'quentin', :password => 'monkey', :remember_me => "0"
-    assert @response.cookies["auth_token"].blank?, @response.cookies["auth_token"]
+    assert @response.cookies["auth_token"].blank?, "Cookie contains #{@response.cookies["auth_token"]}"
   end
   
   def test_should_delete_token_on_logout
