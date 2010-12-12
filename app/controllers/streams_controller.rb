ae54e940429ec513e8d6288bfaa010615b7d1a40
Lennart Koopmann <lennart@socketfeed.com>
2010-12-12 03:45:57 +0100

    moved rules management to own tab

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -9,7 +9,6 @@ class StreamsController < ApplicationController
   def show
     @stream = Stream.find params[:id]
     @messages = Message.all_of_stream @stream.id, params[:page]
-    @new_rule = Streamrule.new
     @total_count = Message.count_stream @stream.id
 
     # Find out if this stream is alertable.
@@ -17,6 +16,11 @@ class StreamsController < ApplicationController
     
     # Find out if this stream is favorited by the current user.
     FavoritedStream.find_by_stream_id_and_user_id(params[:id], current_user.id).blank? ? @is_favorited = false : @is_favorited = true
+  end
+
+  def rules
+    @stream = Stream.find params[:id]
+    @new_rule = Streamrule.new
   end
 
   def create
@@ -41,30 +45,30 @@ class StreamsController < ApplicationController
     redirect_to :action => "index"
   end
 
-  def get_hosts_statistic
-    throw "Missing stream ID" if params[:id].blank?
-
-    total_message_count = Stream.get_message_count(params[:id]).to_i
-    hosts = Stream.get_distinct_hosts params[:id]
-
-    ready_hosts = Array.new
-    hosts.each do |host|
-      message_count = Stream.get_count_by_host(params[:id], host).to_i
-      # Thanks to Sarah and her wicked percentage calculation skills. (<3)
-      percent = 100-(((total_message_count-message_count)*100)/total_message_count)
-      ready_hosts << { 'name' => host, 'percent' => percent.to_i }
-    end
-
-    # Sort the result.
-    ready_hosts = ready_hosts.sort { |a,b| b['percent'] <=> a['percent'] }
-
-    if hosts.blank?
-      render :text => 'No messages found.'
-      return
-    end
-
-    render :partial => 'statistics', :locals => { :hosts => ready_hosts }
-  end
+#  def get_hosts_statistic
+#    throw "Missing stream ID" if params[:id].blank?
+#
+#    total_message_count = Stream.get_message_count(params[:id]).to_i
+#    hosts = Stream.get_distinct_hosts params[:id]
+#
+#    ready_hosts = Array.new
+#    hosts.each do |host|
+#      message_count = Stream.get_count_by_host(params[:id], host).to_i
+#      # Thanks to Sarah and her wicked percentage calculation skills. (<3)
+#      percent = 100-(((total_message_count-message_count)*100)/total_message_count)
+#      ready_hosts << { 'name' => host, 'percent' => percent.to_i }
+#    end
+#
+#    # Sort the result.
+#    ready_hosts = ready_hosts.sort { |a,b| b['percent'] <=> a['percent'] }
+#
+#    if hosts.blank?
+#      render :text => 'No messages found.'
+#      return
+#    end
+#
+#    render :partial => 'statistics', :locals => { :hosts => ready_hosts }
+#  end
 
   def alertable
     stream = Stream.find params[:id]
