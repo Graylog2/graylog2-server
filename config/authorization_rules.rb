0d4037e2d06b0a3542594d059c7f7f2a215a0031
Dennis Oelkers <dennis@lauschmusik.de>
2010-12-14 16:58:39 +0100

    - update of auth rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -3,13 +3,13 @@ authorization do
     has_permission_on :messages, :to => [:index, :show, :getcompletemessage]
     
     has_permission_on :streams, :to => [:index, :show, :create, :destroy, :alertable, :get_hosts_statistic]
-    has_permission_on :streamrules, :to => [:create, :delete]
+    has_permission_on :streamrules, :to => [:create, :destroy]
     
     has_permission_on :hosts, :to => [:index, :show]
     has_permission_on :hostgroups, :to => [:new, :create, :hosts, :index, :settings, :show, :destroy]
     has_permission_on :hostgroup_hosts, :to => [:create]
     
-    has_permission_on :blacklists, :to => [:index, :create, :delete]
+    has_permission_on :blacklists, :to => [:index, :show, :create, :delete]
     has_permission_on :blacklist_terms, :to => [:create, :delete]
     
     has_permission_on :settings, :to => [:index, :store]
