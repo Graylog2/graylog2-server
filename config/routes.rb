aff4603a3b60423e0a9e97f3eb99115785f2ffe1
Lennart Koopmann <lennart@socketfeed.com>
2010-12-16 19:08:19 +0100

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Graylog2WebInterface::Application.routes.draw do
   match '/logout' => 'sessions#destroy', :as => :logout
   match '/login' => 'sessions#new', :as => :login
+  match '/session' => 'sessions#new'
   resource :session
   match '/' => 'messages#index'
   match '/:controller(/:action(/:id))'
