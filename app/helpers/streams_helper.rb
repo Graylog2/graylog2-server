686fe8a980323ecfd7e51fad1d101be23dd18196
Lennart Koopmann <lennart@socketfeed.com>
2010-12-23 21:12:28 +0100

    fix broken streamrule format

--- app/helpers/streams_helper.rb
+++ app/helpers/streams_helper.rb
@@ -27,7 +27,6 @@ module StreamsHelper
         value = "#{syslog_facility_to_human(rule.value)} (#{h(rule.value.to_i)})"
     end
 
-    #return "<span class=\"black\">#{type}</span>: <i>#{value}</i>"
-    return [type, value]
+    return "<span class=\"black\">#{type}</span>: <i>#{value}</i>"
   end
 end
