877ac583d04a24bc143894766520573ad578c8d4
Lennart Koopmann <lennart@socketfeed.com>
2010-12-25 14:57:10 +0100

    migration for new user models. standard role is now admin if no role is set

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  
+  STANDARD_ROLE = :admin
 
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
@@ -53,7 +53,7 @@ class User < ActiveRecord::Base
   end
   
   def role_symbols
-    [role.to_sym]
+    [(role.blank? ? STANDARD_ROLE : role.to_sym)]
   end
   
   def valid_roles
