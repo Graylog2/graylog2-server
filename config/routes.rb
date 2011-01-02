a05af5fc727474a675575f416ed34df28395025c
Lennart Koopmann <lennart@socketfeed.com>
2011-01-02 15:05:13 +0100

    seems like issue #18 was invalid

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,48 @@
 Graylog2WebInterface::Application.routes.draw do
- #scope Configuration.general_url_prefix do
-  match 'logout' => 'sessions#destroy', :as => :logout
-  match 'login' => 'sessions#new', :as => :login
-  resource :session
-  resources :messages do
-    collection do
-      post :showrange
+  scope Configuration.general_url_prefix do
+    match 'logout' => 'sessions#destroy', :as => :logout
+    match 'login' => 'sessions#new', :as => :login
+    resource :session
+    resources :messages do
+      collection do
+        post :showrange
+      end
     end
-  end
   
-  resources :blacklists do
-    resources :blacklistedterms, :as => "terms"
-  end
-
-  resources :hosts do
-    resources :messages
-  end
-
-  resources :facilities do
-    member do
-      post :changetitle
+    resources :blacklists do
+      resources :blacklistedterms, :as => "terms"
     end
-  end
 
-  resources :messagecomments do
-  end
+    resources :hosts do
+      resources :messages
+    end
 
-  resources :streams do
-    resources :messages
-    resources :streamrules
+    resources :facilities do
+      member do
+        post :changetitle
+      end
+    end
+
+    resources :messagecomments do
+    end
+
+    resources :streams do
+      resources :messages
+      resources :streamrules
     
-    member do
-      post :favorite
-      post :unfavorite
-      post :alertable
-      post :setdescription
-      post :showrange
+      member do
+        post :favorite
+        post :unfavorite
+        post :alertable
+        post :setdescription
+        post :showrange
+      end
     end
-  end
   
-  match '/' => 'messages#index', :as => "root"
-  match '/:controller(/:action(/:id))'
- #end
+    match '/' => 'messages#index', :as => "root"
+    match '/:controller(/:action(/:id))'
+  end
  
- match '/' => 'messages#index'
+  match '/' => 'messages#index'
 end
 
