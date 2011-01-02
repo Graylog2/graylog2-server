363ef5750a25cd9c80407ec5a7da24374fb0a000
Lennart Koopmann <lennart@socketfeed.com>
2011-01-02 17:46:24 +0100

    fix settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,29 +1,24 @@
 class SettingsController < ApplicationController
-  filter_resource_access
+  filter_access_to :index
+  filter_access_to :store
+  
   def index
   end
 
   def store
-    # Find out if this setting is already stored.
-    setting = Setting.find_by_user_id_and_setting_type current_user.id, params[:setting_type]
-    if setting.blank?
-      # This setting does not exist. Create it.
-      setting = Setting.new
-      setting.user_id = current_user.id
-      setting.setting_type = params[:setting_type]
-      setting.value = params[:value].to_i
-      flash[:notice] = "<strong>Setting has been saved</strong><span>Created new setting</span>"
-      redirect_to :action => "index" if setting.save
-      return
+    Setting.delete_all(["user_id = ? and setting_type = ?", current_user.id, params[:setting_type]])
+      
+    setting = Setting.new
+    setting.user_id = current_user.id
+    setting.setting_type = params[:setting_type]
+    setting.value = params[:value].to_i
+    
+    if setting.save
+      flash[:notice] = "Setting has been saved!"
     else
-      # Update the already existing setting.
-      setting.value = params[:value].to_i
-      flash[:notice] = "<strong>Setting has been saved</strong><span>Updated setting</span>"
-      redirect_to :action => "index" if setting.save
-      return
+      flash[:error] = "Could not edit setting."
     end
 
-    flash[:error] = "Could not edit setting."
     redirect_to :action => "index"
   end
 end
