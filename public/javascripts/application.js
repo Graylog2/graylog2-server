2e08cfdfe33f2ee6ce224edf7663093d4bd23dd8
Lennart Koopmann <lennart@socketfeed.com>
2011-01-15 22:23:50 +0100

    removed gritter dependency. added own notification bars that are better to read and look awsm

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,46 +1,9 @@
 $(document).ready(function(){
-    // Show notifications with gritter if present.
-    if ($('#notification-notice').html().length !== 0) {
-        title = $('#notification-notice strong').html();
-        text = $('#notification-notice span').html();
 
-        if (title == null || title.length == 0) {title = "Notice";}
-        if (text == null || text.length == 0) {
-            text = $('#notification-notice').html();
-            
-            // Fallback if we still have nothing at all.
-            if (text == null || text.length == 0) {
-                text = "Success."
-            }
-        }
-
-        $.gritter.add({
-            title: title,
-            text: text,
-            image: '/images/icons/okay.png'
-        });
-    }
-
-    if ($('#notification-error').html().length !== 0) {
-        title = $('#notification-error strong').html();
-        text = $('#notification-error span').html();
-
-        if (title == null || title.length == 0) {title = "Notice";}
-        if (text == null || text.length == 0) {
-            text = $('#notification-error').html();
-
-            // Fallback if we still have nothing at all.
-            if (text == null || text.length == 0) {
-                text = "Success.";
-            }
-        }
-
-        $.gritter.add({
-            title: '<span class="gritter-error-title">' + title + '</span>',
-            text: text,
-            image: '/images/icons/error.png'
-        });
-    }
+    // Hide notifications after some time.
+    setInterval(function() {
+      $(".notification").hide("drop");
+    }, 3500);
 
     // Stream rule form.
     $('#streamrule_rule_type').bind('change', function() {
