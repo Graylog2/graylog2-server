42e7b7671379fb58c71203a30b585104bfbdd358
Lennart Koopmann <lennart@socketfeed.com>
2011-01-31 16:07:06 +0100

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(:version => 20110130212941) do
     t.datetime "updated_at"
   end
 
+  create_table "correlation_rules", :force => true do |t|
+    t.integer  "stream_id"
+    t.integer  "correlation_type"
+    t.string   "value"
+    t.integer  "timeframe"
+    t.boolean  "global",           :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "facilities", :force => true do |t|
     t.integer  "number"
     t.string   "title"
