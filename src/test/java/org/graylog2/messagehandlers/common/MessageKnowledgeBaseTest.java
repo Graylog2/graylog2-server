4b88942e1cf26c63d9a5f65d70da75c9e469b0af
Joshua Spaulding <joshua.spaulding@gmail.com>
2011-02-22 15:09:45 -0600

    Added two more tests for complete test coverage of rule file.

--- src/test/java/org/graylog2/messagehandlers/common/MessageKnowledgeBaseTest.java
+++ src/test/java/org/graylog2/messagehandlers/common/MessageKnowledgeBaseTest.java
@@ -32,4 +32,27 @@ public class MessageKnowledgeBaseTest {
 		this.drools.evaluate(this.message);
 		assertEquals(message.getHost(), "localhost.example.com");
 	}
+	
+	@Test
+	public void testFilteroutRuleFile() throws Exception {
+		GELFMessage message = new GELFMessage();
+		message.setHost("www1");
+		message.setFacility("graylog2-test");
+		message.setVersion("1.0");
+		message.setShortMessage("Test");
+		this.drools.evaluate(message);
+		assertTrue(message.getFilterOut());
+	}
+	
+	@Test
+	public void testRegularExpressionFilteroutRuleFile() throws Exception {
+		GELFMessage message = new GELFMessage();
+		message.setHost("firewall");
+		message.setFacility("graylog2-test");
+		message.setVersion("1.0");
+		message.setShortMessage("Test");
+		message.setFullMessage("Received ICMP Packet from firewall");
+		this.drools.evaluate(message);
+		assertTrue(message.getFilterOut());	
+	}
 }
