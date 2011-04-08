ae89c83e31b19225a5df7234fb2795a98908b057
Lennart Koopmann <lennart@socketfeed.com>
2011-04-08 23:50:51 +0200

    don't verify uniqueness of email address of a user #WEBINTERFACE-27

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User
 
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
   STANDARD_ROLE = :admin
