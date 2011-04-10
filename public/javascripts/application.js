cbcc7209d7606572ad6e766bba07b7fcb3908d2c
Lennart Koopmann <lennart@socketfeed.com>
2011-04-10 14:08:57 +0200

    fix failing ajax requests by always sending the CSRF token with ajax requests. this fixes the sudden logout after ajax requests after rails upgrade \o/ #NO_ISSUE

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,8 @@
+$(document).ajaxSend(function(e, xhr, options) {
+  var token = $("meta[name='csrf-token']").attr("content");
+  xhr.setRequestHeader("X-CSRF-Token", token);
+});
+
 $(document).ready(function(){
 
     // Hide notifications after some time.
