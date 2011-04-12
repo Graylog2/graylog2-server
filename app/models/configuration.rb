a30ea6e3383bee0cab621f076258c064e37ac07b
Luiz Ozaki <_ozaki@hm1397.locaweb.com.br>
2011-04-12 11:42:36 -0300

    Mail alert compat fix (pony)

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -58,9 +58,10 @@ class Configuration
     ret = Hash.new
 
     if config['via'] == 'smtp'
-      ret[:host] = config['host'] unless config['host'].blank?
+      ret[:address] = config['host'] unless config['host'].blank?
       ret[:port] = config['port'] unless config['port'].blank?
       ret[:user] = config['user'] unless config['user'].blank?
+      ret[:enable_starttls_auto] = config['enable_starttls_auto'] unless config['enable_starttls_auto']
       ret[:password] = config['password'] unless config['password'].blank?
       ret[:auth] = config['auth'] unless config['auth'].blank?
       ret[:domain] = config['domain'] unless config['domain'].blank?
