703d9fd4c18f3bf8abd062bd3e442ff55eababac
Lennart Koopmann <lennart@socketfeed.com>
2011-04-14 23:38:58 +0200

    hostgroup quickfilter now pre-selected on results page #WEBINTERFACE-23

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,10 +1,16 @@
 module MessagesHelper
-  def get_quickfilter_selected filters, filter, not_to_i = false
+  def get_quickfilter_selected filters, filter, convert_to = :nothing
     return nil if filters.blank? or filters[filter].blank?
-    if not_to_i
+    
+    begin
+      case convert_to
+        when :integer then return filters[filter].to_i
+        when :bson_id then return BSON::ObjectId(filters[filter])
+        else return filters[filter] # No converting
+      end
+    rescue => e
       return filters[filter]
-    else
-      return filters[filter].to_i
     end
+
   end
 end
