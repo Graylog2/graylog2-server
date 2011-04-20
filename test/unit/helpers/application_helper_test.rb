80ac8238a8781af1119091f6f2fd3c5605bed392
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-04-20 16:19:10 +0400

    Fix test.

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
   context "time_to_formatted_s" do
     should "format time with milliseconds" do
-      assert_equal "2011-04-19 21:25:14.128", time_to_formatted_s(Time.at(1303233914.12842))
+      assert_equal "2011-04-20 11:40:57.146", time_to_formatted_s(Time.zone.at(1303299657.146426))
     end
   end
 end
