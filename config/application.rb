1113345a85d161ddfbd2d0c24dc25af7af83b360
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-04-21 10:56:34 +0400

    Do not change default time zone.

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Graylog2WebInterface
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Berlin'
+    config.time_zone = 'UTC'  # do not change it
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
