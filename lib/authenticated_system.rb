2a061cf8417e6b327ec7acbf23e2a933d51d5e4d
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-04-26 12:21:53 +0400

    Fix deprecation warning.

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -81,7 +81,7 @@ module AuthenticatedSystem
     #
     # We can return to this location by calling #redirect_back_or_default.
     def store_location
-      session[:return_to] = request.request_uri
+      session[:return_to] = request.fullpath
     end
 
     # Redirect to the URI stored by the most recent store_location call or
