1ccfae12efcadf9a5bbae7538129d23ce02b43d4
Lennart Koopmann <lennart@socketfeed.com>
2011-05-17 19:14:25 +0200

    first work on new server_value stuff #SERVER-26

--- src/main/java/org/graylog2/ServerValue.java
+++ src/main/java/org/graylog2/ServerValue.java
@@ -20,7 +20,12 @@
 
 package org.graylog2;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import org.graylog2.database.MongoBridge;
+import org.graylog2.database.MongoConnection;
 
 /**
  * ServerValue.java: Jan 16, 2011 1:35:00 PM
@@ -30,6 +35,45 @@ import org.graylog2.database.MongoBridge;
  * @author: Lennart Koopmann <lennart@socketfeed.com>
  */
 public class ServerValue {
+
+    DBObject mongoInstance = null;
+
+    public ServerValue(String instanceId) {
+        this.removeDeadInstances();
+        
+        this.mongoInstance = findOrCreateMongoInstance(instanceId);
+        System.out.println("GOT: " + this.mongoInstance.toString());
+    }
+
+    private void removeDeadInstances() {
+        System.out.println("PLEASE IMPLEMENT ME - REMOVE DEAD INSTANCES");
+    }
+
+    private DBObject findOrCreateMongoInstance(String instanceId) {
+        DBCollection sv = MongoConnection.getInstance().getDatabase().getCollection("server_values");
+
+        // Doing it this way because java driver sucks in querying for subdoc keys. :(
+        DBCursor cur = sv.find(); // find all
+        while (cur.hasNext()) {
+            DBObject obj = cur.next();
+            if (obj.containsField(instanceId)) {
+                // Found the instance.
+                return obj;
+            }
+        }
+
+        // There is no such instance in the collection yet if we arrive here.
+        DBObject instance = new BasicDBObject();
+        instance.put(instanceId, null);
+        sv.insert(instance);
+
+        return instance;
+    }
+
+
+
+
+
 
     public static void setStartupTime(int timestamp) {
         set("startup_time", timestamp);
