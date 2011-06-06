1887ebff160e393caf023c089dca149fac7ff5d0
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-06-06 17:30:50 +0400

    Fix unit tests, use html_safe.

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
   context "time_to_formatted_s" do
     should "format time with milliseconds" do
-      assert_equal "2011-04-20 11:40:57.146", time_to_formatted_s(Time.zone.at(1303299657.146426))
+      res = time_to_formatted_s(Time.zone.at(1303299657.146426))
+      assert res.html_safe?
+      assert_equal "2011-04-20 11:40:57<span class='time-light'>.146</span>", res
     end
   end
 end
