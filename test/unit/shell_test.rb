9c4a1a1567d16ad0e52e165b9dc9ac95db402557
Lennart Koopmann <lennart@socketfeed.com>
2011-06-20 00:10:49 +0200

    current state - now understands find. includes a failing test #shell

--- test/unit/shell_test.rb
+++ test/unit/shell_test.rb
@@ -191,4 +191,128 @@ class ShellTest < ActiveSupport::TestCase
 
   end
 
+  context "finding" do
+
+    should "find with no options" do
+      3.times { Message.make }
+
+      s = Shell.new('all.find()')
+      result = s.compute
+
+      assert_equal 3, result[:result].count
+    end
+
+    should "find a simple message" do
+      Message.make(:message => "foo")
+      Message.make(:host => "bar.example.org", :message => "bar")
+
+      s = Shell.new('all.find(message = "bar")')
+      result = s.compute
+
+      assert_equal "find", result[:operation]
+      assert_equal "bar.example.org", result[:result][0].host
+      assert_equal "bar", result[:result][0].message
+    end
+
+    should "find multiple messages" do
+      Message.make(:message => "foo")
+      5.times { Message.make(:host => "bar.example.org", :message => "bar") }
+
+      s = Shell.new('all.find(message = "bar")')
+      result = s.compute
+
+      assert_equal "find", result[:operation]
+      assert_equal 5, result[:result].count
+      result[:result].each do |message|
+        assert_equal "bar.example.org", message.host
+        assert_equal "bar", message.message
+      end
+    end
+    
+    should "find multiple messages with combined options and regex" do
+      pattern = /^ba.\.example.(com|org)/
+
+      3.times { Message.make(:host => "foo.example.org", :message => "foo") }
+      5.times { Message.make(:host => "bar.example.org", :message => "bar") }
+      2.times { Message.make(:host => "baz.example.com", :message => "bar") }
+
+      s = Shell.new("all.find(host = /#{pattern.source}/, message = \"bar\")")
+      result = s.compute
+
+      assert_equal "find", result[:operation]
+      assert_equal 7, result[:result].count
+      result[:result].each do |message|
+        assert_match pattern, message.host
+        assert_equal "bar", message.message
+      end
+    end
+
+    should "find with conditional operators" do
+      3.times { Message.make(:host => "example.org", :_http_response_code => 500) }
+      7.times { Message.make(:host => "example.com", :_http_response_code => 500) }
+      8.times { Message.make(:host => "example.com", :_http_response_code => 200) }
+      10.times { Message.make(:host => "example.com", :_http_response_code => 201) }
+      1.times { Message.make(:host => "example.com", :_http_response_code => 300) }
+
+      s = Shell.new('all.find(host != "example.org", _http_response_code < 300, _http_response_code >= 200)')
+      result = s.compute
+      
+      assert_equal "find", result[:operation]
+      assert_equal 18, result[:result].count
+    end
+
+    should "find in a stream" do
+      correct_stream_id = BSON::ObjectId.new
+      wrong_stream_id = BSON::ObjectId.new
+      2.times { Message.make(:streams => correct_stream_id) }
+      3.times { Message.make(:streams => wrong_stream_id) }
+
+      s = Shell.new("stream(#{correct_stream_id}).find()")
+      result = s.compute
+
+      assert_equal "find", result[:operation]
+      assert_equal 2, result[:result].count
+      result[:result].each { |r| assert_equal correct_stream_id, r.streams }
+    end
+
+    should "find in streams" do
+      correct_stream_ids = [ BSON::ObjectId.new, BSON::ObjectId.new ]
+      wrong_stream_id = BSON::ObjectId.new
+      5.times { Message.make(:streams => correct_stream_ids[0]) }
+      2.times { Message.make(:streams => correct_stream_ids[1]) }
+      2.times { Message.make(:streams => correct_stream_ids) }
+      3.times { Message.make(:streams => wrong_stream_id) }
+
+      s = Shell.new("stream(#{correct_stream_ids.join(',')}).find()")
+      result = s.compute
+
+      assert_equal "find", result[:operation]
+      assert_equal 9, result[:result].count
+    end
+    
+    should "find in a stream with options" do
+
+    end
+
+    should "find in streams with options" do
+      correct_stream_ids = [ BSON::ObjectId.new, BSON::ObjectId.new ]
+      wrong_stream_id = BSON::ObjectId.new
+      6.times { Message.make(:host => "baz.example.org", :_foo => 12, :streams => correct_stream_ids[0]) }
+      1.times { Message.make(:host => "bar.example.com", :_foo => 9001, :streams => correct_stream_ids[1]) }
+      4.times { Message.make(:host => "bar.example.org", :_foo => 50, :streams => correct_stream_ids) }
+      2.times { Message.make(:host => "bar.example.org", :_foo => 1, :streams => correct_stream_ids[0]) }
+      2.times { Message.make(:host => "foo.example.org", :_foo => 5, :streams => correct_stream_ids[0]) }
+      6.times { Message.make(:host => "baz.example.org", :_foo => 12, :streams => wrong_stream_id) }
+
+      s = Shell.new("stream(#{correct_stream_ids.join(',')}).find(host = /^ba.\.example.(com|org)/, _foo > 10)")
+      result = s.compute
+
+      puts s.mongo_selector
+
+      assert_equal "find", result[:operation]
+      assert_equal 11, result[:result].count
+    end
+
+  end
+
 end
