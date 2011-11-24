b73b4c73108ed9718f84ff901ff6dfa49ed24cb9
Lennart Koopmann <lennart@socketfeed.com>
2011-11-24 16:27:00 +0100

    HTTP 400 as ES answer for existing index is okay
    strange, but okay. happens in older versions. fixes #SERVER-75

--- src/main/java/org/graylog2/indexer/Indexer.java
+++ src/main/java/org/graylog2/indexer/Indexer.java
@@ -60,11 +60,12 @@ public class Indexer {
         URL url = new URL(Indexer.buildIndexURL());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("HEAD");
-        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+        // Older versions of ElasticSearch return 400 Bad Request in cse of an existing index.
+        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK || conn.getResponseCode() == HttpURLConnection.HTTP_BAD_REQUEST) {
             return true;
         } else {
             if (conn.getResponseCode() != HttpURLConnection.HTTP_NOT_FOUND) {
-                LOG.warn("Indexer response code was not 200 or 404, but " + conn.getResponseCode());
+                LOG.warn("Indexer response code was not (200 or 400) or 404, but " + conn.getResponseCode());
             }
 
             return false;
