1b47820e9daa4ee66304d39f35b65e3b718414ad
Lennart Koopmann <lennart@socketfeed.com>
2011-12-12 01:07:46 +0100

    set raw syslog message as full message
    fixes #SERVER-85

--- src/main/java/org/graylog2/messagehandlers/syslog/SyslogEventHandler.java
+++ src/main/java/org/graylog2/messagehandlers/syslog/SyslogEventHandler.java
@@ -78,6 +78,7 @@ public class SyslogEventHandler implements SyslogServerSessionlessEventHandlerIF
             gelf.setConvertedFromSyslog(true);
             gelf.setVersion("0");
             gelf.setShortMessage(event.getMessage());
+            gelf.setFullMessage(new String(event.getRaw()));
             gelf.setHost(event.getHost());
             gelf.setFacility(Tools.syslogFacilityToReadable(event.getFacility()));
             gelf.setLevel(event.getLevel());
