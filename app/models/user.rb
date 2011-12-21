97332a810a183eb7a03905115cf7771b594d1347
Lennart Koopmann <lennart@socketfeed.com>
2011-12-21 19:03:16 +0100

    fixed broken streams updating with new mongoid
    fixes #WEBINTERFACE-195

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation, :role
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :role, :stream_ids
 
   field :login, :type => String
   field :email, :type => String
