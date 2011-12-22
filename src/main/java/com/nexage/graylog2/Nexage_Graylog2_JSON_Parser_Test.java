e30b1a3fe26b466045e98431e8fec602f034d76c
Bob Webber <webber@panix.com>
2011-12-22 14:05:20 -0500

    Ready to test in Graylog2 context.

--- src/main/java/com/nexage/graylog2/Nexage_Graylog2_JSON_Parser_Test.java
+++ src/main/java/com/nexage/graylog2/Nexage_Graylog2_JSON_Parser_Test.java
@@ -2,11 +2,8 @@ package com.nexage.graylog2;
 
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
 import java.util.ListIterator;
 import org.json.simple.JSONObject;
-// import org.json.simple.JSONAware;
-// import org.json.simple.JSONStreamAware;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -18,7 +15,8 @@ public class Nexage_Graylog2_JSON_Parser_Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="{\"foo\":{\"1\":2},\"bar\":[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]}";
+		// String s="{\"foo\":{\"1\":2},\"bar\":[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]}";
+		String s = "{\"startedOn\":1324390232727,\"endedOn\":1324390232810,\"executionTimeInMillis\":83,\"requestData\":{\"tag\":\"8a809449013131b07f6bbe6bd77402c1\",\"mode\":\"live\",\"h\":480,\"w\":320,\"spcat\":\"IAB9,IAB1\",\"dip\":\"99.255.144.138\",\"dcountry\":\"CAN\",\"dua\":\"Mozilla/5.0 (iPod touch; U; CPU iPhone OS 5_0_1 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16\",\"dmake\":\"Apple\",\"dmodel\":\"iPod Touch\",\"dos\":\"iOS\",\"dosv\":\"3.0\",\"dpid\":\"30f1418d2f02f90c17a1adbd1b8377cb8172741b\",\"prettyLog\":false,\"hasUserData\":false,\"hasDeviceData\":true},\"auctionParticipants\":{\"119\":\"Status : NOBID, Offered Price : 0\",\"288\":\"Status : NOBID, Offered Price : 0\",\"327\":\"Status : NOBID, Offered Price : 0\",\"223\":\"Status : NOBID, Offered Price : 0\",\"236\":\"Status : NOBID, Offered Price : 0\",\"54\":\"Status : NOBID, Offered Price : 0\",\"353\":\"Status : NOBID, Offered Price : 0\",\"67\":\"Status : NOBID, Offered Price : 0\",\"41\":\"Status : NOBID, Offered Price : 0\",\"210\":\"Status : NOBID, Offered Price : 0\",\"379\":\"Status : NOBID, Offered Price : 0\"},\"output\":\"\",\"methodProfileStats\":[]}";
 		System.out.println("=======decode=======");
 		Map<String,Object> sausage;
 		sausage = nxParseJsonString(s);
