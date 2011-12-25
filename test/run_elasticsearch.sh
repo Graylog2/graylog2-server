4e1e64b79bd4868ccbd72ea61e1e9ab36c4a8f1e
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-12-26 00:13:38 +0400

    Uncomment download.

--- test/run_elasticsearch.sh
+++ test/run_elasticsearch.sh
@@ -2,7 +2,7 @@
 
 # based on https://github.com/fabian/Elastica/blob/master/test/bin/run_elasticsearch.sh
 
-#wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.18.5.tar.gz
+wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.18.5.tar.gz
 tar -xzf elasticsearch-0.18.5.tar.gz
 sed 's/# index.number_of_shards: 1/index.number_of_shards: 1/' elasticsearch-0.18.5/config/elasticsearch.yml > elasticsearch-0.18.5/config/elasticsearch.yml
 sed 's/# index.number_of_replicas: 0/index.number_of_replicas: 0/' elasticsearch-0.18.5/config/elasticsearch.yml > elasticsearch-0.18.5/config/elasticsearch.yml
