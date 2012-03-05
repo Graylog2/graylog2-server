9130e1bb8aba20988b8c9dd5ebfe6e867fe86d2c
Lev Lukomsky <lllukom@gmail.com>
2012-03-05 16:18:29 +0200

    Disable verbose logs like: cache: [POST /health/currentthroughput] invalidate, pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ Graylog2WebInterface::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_dispatch.rack_cache = {:metastore => "rails:/", :entitystore => "rails:/", :verbose => false}
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
