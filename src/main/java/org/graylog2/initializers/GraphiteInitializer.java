1a81709915d8fb0fc19e575520bf10edfca136c9
Lennart Koopmann <lennart@socketfeed.com>
2012-05-16 01:28:09 +0200

    send metrics to graphite every 5 seconds

--- src/main/java/org/graylog2/initializers/GraphiteInitializer.java
+++ src/main/java/org/graylog2/initializers/GraphiteInitializer.java
@@ -40,8 +40,8 @@ public class GraphiteInitializer extends SimpleFixedRateScheduleInitializer impl
     public void initialize() {
         // Enable graphite metrics reporter.
         GraphiteReporter.enable(
-                1,
-                TimeUnit.MINUTES,
+                5,
+                TimeUnit.SECONDS,
                 this.graylogServer.getConfiguration().getGraphiteCarbonHost(),
                 this.graylogServer.getConfiguration().getGraphiteCarbonTcpPort(),
                 "graylog2-server"
