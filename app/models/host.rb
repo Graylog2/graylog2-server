e922e03bec1df2b8f8b32141c87dbe9fbce22fc0
Lennart Koopmann <lennart@socketfeed.com>
2012-06-20 18:17:14 +0200

    rescue possible encoding problems of hostnames

--- app/models/host.rb
+++ app/models/host.rb
@@ -9,4 +9,19 @@ class Host
 
   validates_presence_of :host
 
+  def self.all_hostnames
+    r = []
+    all.collect do |host|
+      begin
+        r << CGI::escapeHTML(host.host)
+      rescue
+        # There may be errors from faulty UTF8 characters in the hostname.
+        # (True story: https://groups.google.com/d/topic/graylog2/g9g1bJ-kjgY/discussion)
+        next
+      end
+    end
+
+    return r.sort
+  end
+
 end
