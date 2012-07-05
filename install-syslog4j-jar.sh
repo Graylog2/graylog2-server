5bce534851591acfa461217b4f1ab173d090ba07
Peter Donald <peter@realityforge.org>
2012-07-05 22:45:46 +1000

    Update the before_install script for travis to install the syslog jar into the correct location

--- install-syslog4j-jar.sh
+++ install-syslog4j-jar.sh
@@ -1,5 +1,2 @@
-MVN_REPO="/tmp/graylog2-server-build-${USER}"
-MVN_OPTS=-Dmaven.repo.local=${MVN_REPO}
-
 echo "Installing provided syslog4j jar to local mvn repository for great justice!"
-mvn install:install-file ${MVN_OPTS} -DgroupId=org.syslog4j -DartifactId=syslog4j -Dversion=0.9.46 -Dpackaging=jar -Dfile=lib/syslog4j-0.9.46-bin.jar
+mvn install:install-file -DgroupId=org.syslog4j -DartifactId=syslog4j -Dversion=0.9.46 -Dpackaging=jar -Dfile=lib/syslog4j-0.9.46-bin.jar
