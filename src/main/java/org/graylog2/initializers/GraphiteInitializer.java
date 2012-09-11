d319163bd72c7e6e3371dbf9dc17a3ab9c1fd676
Lennart Koopmann <lennart@socketfeed.com>
2012-09-11 17:12:24 +0200

    allow to set graphite prefix

--- src/main/java/org/graylog2/initializers/GraphiteInitializer.java
+++ src/main/java/org/graylog2/initializers/GraphiteInitializer.java
@@ -42,7 +42,7 @@ public class GraphiteInitializer extends SimpleFixedRateScheduleInitializer impl
                 TimeUnit.SECONDS,
                 this.graylogServer.getConfiguration().getGraphiteCarbonHost(),
                 this.graylogServer.getConfiguration().getGraphiteCarbonTcpPort(),
-                "graylog2-server"
+                this.graylogServer.getConfiguration().getGraphitePrefix()
         );
 
         configureScheduler(
