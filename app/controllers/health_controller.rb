542658f6d48c3330c5f2db34250392516ea6dce7
Lennart Koopmann <lennart@socketfeed.com>
2012-09-15 16:06:10 +0200

    sorting for activity log table #WEBINTERFACE-309

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -5,7 +5,9 @@ class HealthController < ApplicationController
     # Delete outdated server values from instances not running anymore.
     ServerValue.delete_outdated
     
-      @server_activities = ServerActivity.all.desc(:timestamp).page(params[:page])
+    sort = params[:sort].blank? ? :timestamp : params[:sort].to_sym
+    order = params[:order].blank? ? :desc : params[:order].to_sym
+ 	@server_activities = ServerActivity.all.order_by([[sort, order]]).page(params[:page])
   end
 
   def currentthroughput
--- app/helpers/health_helper.rb
+++ app/helpers/health_helper.rb
@@ -1,2 +1,19 @@
 module HealthHelper
+
+	def sorting_link(what, sorting, current_order)
+		current_order = "desc" if current_order.blank?
+
+		if !sorting.blank? and sorting == what.to_s
+			if current_order == "desc"
+				new_order = "asc"
+			else
+				new_order = "desc"
+			end
+		else
+			new_order = "desc"
+		end
+
+		link_to image_tag('icons/table_sorting.gif'), "?sort=#{what}&order=#{new_order}"
+	end
+
 end
