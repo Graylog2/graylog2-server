9c6dc6f5d16da35afc5fd9946004f8d8dfcaa3fe
Lennart Koopmann <lennart@socketfeed.com>
2012-09-16 03:47:57 +0200

    show index and deflector information on health page. fixes #WEBINTERFACE-305

--- app/helpers/health_helper.rb
+++ app/helpers/health_helper.rb
@@ -16,4 +16,11 @@ module HealthHelper
 		link_to image_tag('icons/table_sorting.gif'), "?sort=#{what}&order=#{new_order}"
 	end
 
+	def shard_status(shard)
+		return :started if shard["is_started"]
+		return :unassigned if shard["is_unassigned"]
+		return :relocating if shard["is_relocating"]
+		return :initializing if shard["is_initializing"]
+	end
+
 end
