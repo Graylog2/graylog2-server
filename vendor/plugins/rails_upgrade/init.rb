8dc9daffaa484571a286960504052f3a249dbd3a
Lennart Koopmann <lennart@socketfeed.com>
2012-09-27 00:25:33 +0200

    removed obsolete rails_upgrade plugin

--- vendor/plugins/rails_upgrade/init.rb
+++
@@ -1,2 +0,0 @@
-# Get long stack traces for easier debugging; you'll thank me later.
-Rails.backtrace_cleaner.remove_silencers!
--- vendor/plugins/rails_upgrade/test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-require 'test/unit'
-
-require 'rubygems'
-require 'active_support'
-require 'active_support/test_case'
