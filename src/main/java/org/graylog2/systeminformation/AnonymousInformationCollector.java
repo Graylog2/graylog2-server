4da9276a34c4d8cb72695ccace7604418271b15c
Lennart Koopmann <lennart@socketfeed.com>
2012-10-06 01:11:56 +0200

    gnaah, imports. thx for the hint, @kroepke

--- src/main/java/org/graylog2/systeminformation/AnonymousInformationCollector.java
+++ src/main/java/org/graylog2/systeminformation/AnonymousInformationCollector.java
@@ -19,7 +19,7 @@
  */
 package org.graylog2.systeminformation;
 
-import com.beust.jcommander.internal.Maps;
+import com.google.common.collect.Maps;
 import java.util.Map;
 import org.apache.log4j.Logger;
 import org.graylog2.Core;
