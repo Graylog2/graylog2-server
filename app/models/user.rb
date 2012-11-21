355227bb475e6f57107c0462b01cb429a61f1dc8
Lennart Koopmann <lennart@socketfeed.com>
2012-11-22 00:10:24 +0100

    fixed broken user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,6 @@ class User
   validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
   validates_length_of       :name,     :maximum => 100
 
-  validates_presence_of     :email
-  validates_length_of       :email,    :within => 6..100 #r@a.wk
-
   STANDARD_ROLE = :admin
 
   # HACK HACK HACK -- how to do attr_accessible from here?
