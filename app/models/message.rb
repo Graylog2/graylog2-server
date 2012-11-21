da06157cb24e09b482b83befd97b847d4511ffd8
Lennart Koopmann <lennart@socketfeed.com>
2012-11-21 23:27:49 +0100

    added _score to reserved ES fields
    fixes #WEBINTERFACE-347

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message
 
   LIMIT = 100
   ADDITIONAL_FIELD_SEPARATOR = '_'
-  RESERVED_ADDITIONAL_FIELDS = %w(_type _index _version)
+  RESERVED_ADDITIONAL_FIELDS = %w(_type _index _version _score )
 
   @fields = [ :id, :message, :full_message, :created_at, :facility, :level, :host, :file, :line, :deleted, :streams ]
   @fields.each { |f| attr_accessor(f) }
