99ade712f4c1216b50663c901d8bedbc694131c0
Lennart Koopmann <lennart@socketfeed.com>
2012-11-22 22:53:34 +0100

    make stream tab ordering consistent
    fixes #WEBINTERFACE-359

--- app/helpers/streams_helper.rb
+++ app/helpers/streams_helper.rb
@@ -23,9 +23,9 @@ module StreamsHelper
       tabs << ["Show", stream_path(@stream)] if permitted_to?(:show, @stream)
       tabs << ["Rules", rules_stream_path(@stream)] if permitted_to?(:rules, @stream)
       tabs << ["Analytics", analytics_stream_path(@stream)] if permitted_to?(:analytics, @stream)
+      tabs << ["Settings", settings_stream_path(@stream)] if permitted_to?(:settings, @stream)
       tabs << ["Alarms", alarms_stream_path(@stream)] if permitted_to?(:alarms, @stream)
       tabs << ["Outputs", outputs_stream_path(@stream)] if permitted_to?(:outputs, @stream)
-      tabs << ["Settings", settings_stream_path(@stream)] if permitted_to?(:settings, @stream)
     end
 
     tabs
