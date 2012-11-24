eff162be82d8bc54703b9978cac156004b319049
Sébastien Masset <smasset@server>
2012-11-24 09:44:19 +0100

    Remove throughput related methods

--- src/main/java/org/graylog2/plugin/MessageCounter.java
+++ src/main/java/org/graylog2/plugin/MessageCounter.java
@@ -36,10 +36,6 @@ public interface MessageCounter {
 
     public Map<String, Integer> getHostCounts();
 
-    public int getThroughput();
-
-    public int getHighestThroughput();
-
     public void resetAllCounts();
 
     public void resetHostCounts();
@@ -47,16 +43,10 @@ public interface MessageCounter {
     public void resetStreamCounts();
 
     public void resetTotal();
-    
-    public void resetThroughput();
 
     public void incrementTotal();
 
-    public void incrementThroughput();
-
     public void countUpTotal(final int x);
-
-    public void countUpThroughput(final int x);
 
     public void incrementStream(final ObjectId streamId);
 
