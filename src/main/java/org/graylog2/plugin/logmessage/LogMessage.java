3a8736b5c62312f38fd4272c8a738d59b0efaca0
Lennart Koopmann <lennart@socketfeed.com>
2012-12-08 18:37:51 +0100

    changes to input interface

--- src/main/java/org/graylog2/plugin/logmessage/LogMessage.java
+++ src/main/java/org/graylog2/plugin/logmessage/LogMessage.java
@@ -1,87 +1,246 @@
 /**
- * Copyright (c) 2012 Lennart Koopmann <lennart@socketfeed.com>
+ * Copyright 2012 Lennart Koopmann <lennart@socketfeed.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
+ * This file is part of Graylog2.
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software. 
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
-*/
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 package org.graylog2.plugin.logmessage;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.graylog2.plugin.streams.Stream;
+
 import java.util.List;
 import java.util.Map;
-import org.graylog2.plugin.streams.Stream;
+import java.util.UUID;
+import org.graylog2.plugin.Tools;
 
 /**
  * @author Lennart Koopmann <lennart@socketfeed.com>
  */
-public interface LogMessage {
-
-    public boolean isComplete();
-
-    public String getId();
+public class LogMessage {
     
+    public static final int STANDARD_LEVEL = 1;
+    public static final String STANDARD_FACILITY = "unknown";
+
+    private String id;
+
+    // Standard fields.
+    private String shortMessage;
+    private String fullMessage;
+    private String host;
+    private int level;
+    private String facility;
+    private String file;
+    private int line;
+
+    private Map<String, Object> additionalData = Maps.newHashMap();
+    private List<Stream> streams = Lists.newArrayList();
+
+    private double createdAt = 0;
+    
+    private static final ImmutableSet<String> PROTECTED_KEYS = ImmutableSet.of(
+        "_id",
+        "_ttl",
+        "_source",
+        "_all",
+        "_index",
+        "_type",
+        "_score"
+    );
+
+    public LogMessage() {
+        // the elasticsearch version is the same as the "standard" one, except the encoding is different.
+        // to avoid recomputing it when submitting the message to elasticsearch we always use its method.
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public boolean isComplete() {
+        return (shortMessage != null && !shortMessage.isEmpty() && host != null && !host.isEmpty());
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public Map<String, Object> toElasticSearchObject() {
+        Map<String, Object> obj = Maps.newHashMap();
+        obj.put("message", this.getShortMessage());
+        obj.put("full_message", this.getFullMessage());
+        obj.put("file", this.getFile());
+        obj.put("line", this.getLine());
+        obj.put("host", this.getHost());
+        obj.put("facility", this.getFacility());
+        obj.put("level", this.getLevel());
+
+        // Add additional fields. XXX PERFORMANCE
+        for(Map.Entry<String, Object> entry : this.getAdditionalData().entrySet()) {
+            obj.put(entry.getKey(), entry.getValue());
+        }
+
+        if (this.getCreatedAt() <= 0) {
+            double timestamp = Tools.getUTCTimestampWithMilliseconds();
+            // This should have already been set at receiving, but to make sure...
+            obj.put("created_at", timestamp);
+            obj.put("histogram_time", Tools.buildElasticSearchTimeFormat(timestamp));
+        } else {
+            obj.put("created_at", this.getCreatedAt());
+            obj.put("histogram_time", Tools.buildElasticSearchTimeFormat(this.getCreatedAt()));
+        }
+
+
+        // Manually converting stream ID to string - caused strange problems without it.
+        List<String> streamIds = Lists.newArrayList();
+        for (Stream stream : this.getStreams()) {
+            streamIds.add(stream.getId().toString());
+        }
+        obj.put("streams", streamIds);
+
+        return obj;
+    }
+
     @Override
-    public String toString();
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("level: ").append(level).append(" | ");
+        sb.append("host: ").append(host).append(" | ");
+        sb.append("facility: ").append(facility).append(" | ");
+        sb.append("add.: ").append(additionalData.size()).append(" | ");
+        sb.append("shortMessage: ").append(shortMessage);
 
-    public double getCreatedAt();
+        // Replace all newlines and tabs.
+        String ret = sb.toString().replaceAll("\\n", "").replaceAll("\\t", "");
 
-    public void setCreatedAt(double createdAt);
+        // Cut to 225 chars if the message is too long.
+        if (ret.length() > 225) {
+            ret = ret.substring(0, 225);
+            ret += " (...)";
+        }
 
-    public String getFacility();
+        return ret;
+    }
 
-    public void setFacility(String facility);
+    public double getCreatedAt() {
+        return createdAt;
+    }
 
-    public String getFile();
+    public void setCreatedAt(double createdAt) {
+        this.createdAt = createdAt;
+    }
 
-    public void setFile(String file);
+    public String getFacility() {
+        return facility;
+    }
+ 
+    public void setFacility(String facility) {
+        this.facility = facility;
+    }
 
-    public String getFullMessage();
+    public String getFile() {
+        return file;
+    }
 
-    public void setFullMessage(String fullMessage);
+    public void setFile(String file) {
+        this.file = file;
+    }
 
-    public String getHost();
+    public String getFullMessage() {
+        return fullMessage;
+    }
 
-    public void setHost(String host);
+    public void setFullMessage(String fullMessage) {
+        this.fullMessage = fullMessage;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public String getShortMessage() {
+        return shortMessage;
+    }
+
+    public void setShortMessage(String shortMessage) {
+        this.shortMessage = shortMessage;
+    }
+
+    public void addAdditionalData(String key, Object value) {
+        String pKey = prepareAdditionalDataKey(key);
+        
+        // Don't accept protected keys.
+        if (PROTECTED_KEYS.contains(pKey)) {
+            return;
+        }
+        
+        this.additionalData.put(pKey, value);
+    }      
+
+    public void addAdditionalData(Map<String, String> fields) {
+        for (Map.Entry<String, String> field : fields.entrySet()) {
+            addAdditionalData(field.getKey(), field.getValue());
+        }
+    }
+
+    public void removeAdditionalData(String key) {
+        this.additionalData.remove(key);
+    }
+
+    public Map<String, Object> getAdditionalData() {
+        return this.additionalData;
+    }
+
+    public void setStreams(List<Stream> streams) {
+        this.streams = streams;
+    }
+
+    public List<Stream> getStreams() {
+        return this.streams;
+    }
     
-    public int getLevel();
+    private String prepareAdditionalDataKey(final String _key) {
+        String key = _key.trim();
+        
+        // Add the required underscore if it was not set.
+        if (!key.startsWith("_")) {
+            key = "_" + key;
+        }
 
-    public void setLevel(int level);
+        return key;
+    }
 
-    public int getLine();
-
-    public void setLine(int line);
-
-    public String getShortMessage();
-
-    public void setShortMessage(String shortMessage);
-
-    public void addAdditionalData(String key, Object value);
-
-    public void addAdditionalData(Map<String, String> fields);
-
-    public void removeAdditionalData(String key);
-
-    public Map<String, Object> getAdditionalData();
-
-    public void setStreams(List<Stream> streams);
-
-    public List<Stream> getStreams();
-    
-    public Map<String, Object> toElasticSearchObject();
-    
 }
