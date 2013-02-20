851122dcdd5206034d4d214e125ef38944393f58
Scott Severtson <scott@severtson.us>
2013-02-20 16:30:43 -0500

    Implment HTML email transport with optional links to stream messages Replace Commons Email with direct javax.mail calls, reusing sessions and transports where possible for lower overhead Add support for smtps protocol

---
+++ src/main/java/org/graylog2/alarms/transports/EmailLayout.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2012 Lennart Koopmann <lennart@socketfeed.com>
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.graylog2.alarms.transports;
+
+import java.util.Map;
+
+import org.graylog2.plugin.alarms.Alarm;
+
+public interface EmailLayout {
+
+    void initialize(Map<String, String> pluginConfiguration);
+    String getContentType();
+    String formatMessageBody(Alarm message, long unixTimestamp);
+    String getSubject(Alarm message, long unixTimestamp);
+
+}
