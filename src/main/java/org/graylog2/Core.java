363133c66791716702fe25846a2309e2011214ac
Lennart Koopmann <lennart@socketfeed.com>
2013-05-19 02:09:07 +0200

    bumped version to 0.12.0-rc.1

--- src/main/java/org/graylog2/Core.java
+++ src/main/java/org/graylog2/Core.java
@@ -91,7 +91,7 @@ public class Core implements GraylogServer {
     private static final int SCHEDULED_THREADS_POOL_SIZE = 30;
     private ScheduledExecutorService scheduler;
 
-    public static final String GRAYLOG2_VERSION = "0.12.0-dev";
+    public static final String GRAYLOG2_VERSION = "0.12.0-rc.1";
 
     public static final String MASTER_COUNTER_NAME = "master";
 
