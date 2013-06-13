d0e9ba724e0919e6175306e96278ea64051bb817
Lennart Koopmann <lennart@socketfeed.com>
2013-06-14 00:19:28 +0200

    fix import

--- graylog2-server/src/main/java/org/graylog2/rest/resources/system/jobs/SystemJobResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/system/jobs/SystemJobResource.java
@@ -19,8 +19,8 @@
  */
 package org.graylog2.rest.resources.system.jobs;
 
-import com.beust.jcommander.internal.Lists;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.sun.jersey.api.core.ResourceConfig;
 import org.graylog2.Core;
