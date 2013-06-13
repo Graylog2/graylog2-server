8abc56a660a22aae3c55cf2b881e470166fedc31
Kay Roepke <kroepke@googlemail.com>
2013-06-14 00:04:11 +0200

    fix spaces vs tabs *sigh*

--- app/lib/Api.java
+++ app/lib/Api.java
@@ -19,41 +19,41 @@ public class Api {
 
 	public static final String ERROR_MSG_IO = "Could not connect to graylog2-server. Please make sure that it is running and you configured the correct REST URI.";
 
-    public static <T> T get(URL url, Class<T> responseClass) throws APIException, IOException {
-        try {
-            AsyncHttpClient.BoundRequestBuilder requestBuilder = getClient().prepareGet(url.toString());
-            requestBuilder.addHeader("Accept", "application/json");
-            final Response response = requestBuilder.execute().get();
+	public static <T> T get(URL url, Class<T> responseClass) throws APIException, IOException {
+		try {
+			AsyncHttpClient.BoundRequestBuilder requestBuilder = getClient().prepareGet(url.toString());
+			requestBuilder.addHeader("Accept", "application/json");
+			final Response response = requestBuilder.execute().get();
 
-            if (response.getStatusCode() != 200) {
-                throw new APIException(response.getStatusCode(), "REST call [" + url + "] returned " + response.getStatusText());
-            }
+			if (response.getStatusCode() != 200) {
+				throw new APIException(response.getStatusCode(), "REST call [" + url + "] returned " + response.getStatusText());
+			}
 
-            Gson gson = new Gson();
-            return gson.fromJson(response.getResponseBody("UTF-8"), responseClass);
-        } catch (InterruptedException e) {
-            // TODO
-        } catch (ExecutionException e) {
-            throw new APIException(-1, "REST call [" + url + "] failed: " + e.getMessage());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Malformed URL.", e);
-        }
-        return (T) null;
-    }
+			Gson gson = new Gson();
+			return gson.fromJson(response.getResponseBody("UTF-8"), responseClass);
+		} catch (InterruptedException e) {
+			// TODO
+		} catch (ExecutionException e) {
+			throw new APIException(-1, "REST call [" + url + "] failed: " + e.getMessage());
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("Malformed URL.", e);
+		}
+		return (T) null;
+	}
 
-    public static <T> T get(String target, Class<T> responseClass) throws IOException, APIException {
-        return get(buildTarget(target), responseClass);
-    }
-	
+	public static <T> T get(String target, Class<T> responseClass) throws IOException, APIException {
+		return get(buildTarget(target), responseClass);
+	}
+
 	public static URL buildTarget(String part) throws MalformedURLException {
 		return new URL(Configuration.getServerRestUri() + part);
-    }
-	
+	}
+
 	public static String urlEncode(String x) {
 		if (x == null || x.isEmpty()) {
 			return "";
 		}
-		
+
 		try {
 			return URLEncoder.encode(x, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
@@ -61,10 +61,10 @@ public class Api {
 		}
 	}
 
-    // TODO make cached
-    private static AsyncHttpClient getClient() {
-        AsyncHttpClientConfig.Builder builder = new AsyncHttpClientConfig.Builder();
-        builder.setAllowPoolingConnection(true);
-        return new AsyncHttpClient(builder.build());
-    }
+	// TODO make cached
+	private static AsyncHttpClient getClient() {
+		AsyncHttpClientConfig.Builder builder = new AsyncHttpClientConfig.Builder();
+		builder.setAllowPoolingConnection(true);
+		return new AsyncHttpClient(builder.build());
+	}
 }
