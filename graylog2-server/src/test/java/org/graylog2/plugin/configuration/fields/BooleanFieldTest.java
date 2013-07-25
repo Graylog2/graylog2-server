fa40bf429f70c36cd8d863319fc9ae3f55147c63
Lennart Koopmann <lennart@socketfeed.com>
2013-07-25 13:01:21 +0200

    boolean field tests

---
+++ graylog2-server/src/test/java/org/graylog2/plugin/configuration/fields/BooleanFieldTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.graylog2.plugin.configuration.fields;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+/**
+ * @author Lennart Koopmann <lennart@torch.sh>
+ */
+public class BooleanFieldTest {
+
+    @Test
+    public void testGetFieldType() throws Exception {
+        BooleanField f = new BooleanField("name", false, "description");
+        assertEquals(BooleanField.FIELD_TYPE, f.getFieldType());
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        BooleanField f = new BooleanField("name", false, "description");
+        assertEquals("name", f.getName());
+    }
+
+    @Test
+    public void testGetDescription() throws Exception {
+        BooleanField f = new BooleanField("name", false, "description");
+        assertEquals("description", f.getDescription());
+    }
+
+    @Test
+    public void testGetDefaultValue() throws Exception {
+        BooleanField f = new BooleanField("name", true, "description");
+        assertEquals(true, f.getDefaultValue());
+    }
+
+    @Test
+    public void testIsOptional() throws Exception {
+        BooleanField f = new BooleanField("name", true, "description", ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals(ConfigurationField.Optional.NOT_OPTIONAL, f.isOptional());
+    }
+
+    @Test
+    public void testGetAttributes() throws Exception {
+        // Boolean field has no attributes.
+        BooleanField f = new BooleanField("name", true, "description");
+        assertNotNull(f.getAttributes());
+    }
+
+}
