b8924e92f790156c7b240d70019a8e8f10647865
Lennart Koopmann <lennart@socketfeed.com>
2013-09-02 00:09:59 +0200

    test for DropdownField #210

---
+++ graylog2-server/src/test/java/org/graylog2/plugin/configuration/fields/DropdownFieldTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.graylog2.plugin.configuration.fields;
+
+import com.google.common.collect.Maps;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Lennart Koopmann <lennart@torch.sh>
+ */
+public class DropdownFieldTest {
+
+    @Test
+    public void testGetFieldType() throws Exception {
+        DropdownField f = new DropdownField("test", "Name", "fooval", new HashMap<String, String>(), ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals(DropdownField.FIELD_TYPE, f.getFieldType());
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        DropdownField f = new DropdownField("test", "Name", "fooval", new HashMap<String, String>(), ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals("test", f.getName());
+    }
+
+    @Test
+    public void testGetHumanName() throws Exception {
+        DropdownField f = new DropdownField("test", "Name", "fooval", new HashMap<String, String>(), ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals("Name", f.getHumanName());
+    }
+
+    @Test
+    public void testGetDescription() throws Exception {
+        DropdownField f = new DropdownField("test", "Name", "fooval", new HashMap<String, String>(), "description", ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals("description", f.getDescription());
+    }
+
+    @Test
+    public void testGetDefaultValue() throws Exception {
+        DropdownField f = new DropdownField("test", "Name", "fooval", new HashMap<String, String>(), ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals("fooval", f.getDefaultValue());
+    }
+
+    @Test
+    public void testIsOptional() throws Exception {
+        DropdownField f = new DropdownField("test", "Name", "fooval", new HashMap<String, String>(), ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals(ConfigurationField.Optional.NOT_OPTIONAL, f.isOptional());
+
+        DropdownField f2 = new DropdownField("test", "Name", "fooval", new HashMap<String, String>(), ConfigurationField.Optional.OPTIONAL);
+        assertEquals(ConfigurationField.Optional.OPTIONAL, f2.isOptional());
+    }
+
+    @Test
+    public void testGetValues() throws Exception {
+        Map<String,String> values = Maps.newHashMap();
+        values.put("foo", "bar");
+        values.put("zomg", "baz");
+
+        DropdownField f = new DropdownField("test", "Name", "fooval", values, ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals(values, f.getAdditionalInformation().get("values"));
+    }
+
+}
