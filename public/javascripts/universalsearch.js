ea970eacede04d648cb4757bb0ce84fb05b47102
Lennart Koopmann <lennart@socketfeed.com>
2013-09-07 20:32:58 +0200

    absolute timerange selector #194

--- public/javascripts/universalsearch.js
+++ public/javascripts/universalsearch.js
@@ -9,4 +9,14 @@ $(document).ready(function() {
         $("input,select", selected).removeAttr("disabled");
     });
 
+    $("#universalsearch .timerange-selector-container .absolute .date-select").datepicker({
+        format: "yyyy-mm-dd"
+    }).on("changeDate", function(ev) {
+        var d = new Date();
+        var hours = ('0' + d.getHours()).slice(-2);
+        var minutes = ('0' + d.getMinutes()).slice(-2);
+
+        $(this).val($(this).val() + " " + hours + "-" + minutes + "-00.000");
+    });
+
 });
