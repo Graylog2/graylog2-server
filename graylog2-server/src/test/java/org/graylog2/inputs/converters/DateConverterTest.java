16ff393ac0955b5db59f13847a584af0cff8a8a9
Lennart Koopmann <lennart@socketfeed.com>
2013-09-10 17:42:14 +0200

    fix date converter breaking on UTC systems (like travis)

--- graylog2-server/src/test/java/org/graylog2/inputs/converters/DateConverterTest.java
+++ graylog2-server/src/test/java/org/graylog2/inputs/converters/DateConverterTest.java
@@ -35,7 +35,8 @@ public class DateConverterTest {
 
     @Test
     public void testBasicConvert() throws Exception {
-        assertEquals("2013-08-15T23:15:16.000+02:00", new DateConverter(config("YYYY MMM dd HH:mm:ss")).convert("2013 Aug 15 23:15:16").toString());
+        // .startsWith() because of possibly different timezones per test environment.
+        assert(new DateConverter(config("YYYY MMM dd HH:mm:ss")).convert("2013 Aug 15 23:15:16").toString().startsWith("2013-08-15T23:15:16.000"));
     }
 
     @Test(expected = ConfigurationException.class)
