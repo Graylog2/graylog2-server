3c103f5cf926b5d3b0e2b53eeefa7db4a75b80d0
Lennart Koopmann <lennart@socketfeed.com>
2013-09-10 17:20:18 +0200

    fix broken test case assertion

--- graylog2-server/src/test/java/org/graylog2/inputs/extractors/SplitAndIndexExtractorTest.java
+++ graylog2-server/src/test/java/org/graylog2/inputs/extractors/SplitAndIndexExtractorTest.java
@@ -87,10 +87,10 @@ public class SplitAndIndexExtractorTest {
     public void testBasicExtractionWithCutStrategyCanOverwriteSameField() throws Exception {
         Message msg = new Message("The short message", "TestUnit", Tools.getUTCTimestampWithMilliseconds());
 
-        SplitAndIndexExtractor x = new SplitAndIndexExtractor("foo", "foo", Extractor.CursorStrategy.CUT, "message", "message", config(" ", 0), "foo", noConverters(), Extractor.ConditionType.NONE, null);
+        SplitAndIndexExtractor x = new SplitAndIndexExtractor("foo", "foo", Extractor.CursorStrategy.CUT, "message", "message", config(" ", 1), "foo", noConverters(), Extractor.ConditionType.NONE, null);
         x.runExtractor(new GraylogServerStub(), msg);
 
-        assertEquals("short message", msg.getField("message"));
+        assertEquals("short", msg.getField("message"));
     }
     @Test
     public void testBasicExtractionWithCutStrategyAtEndOfString() throws Exception {
