c73c441fb784f66b8825173792dfc2c5c9035a33
Lennart Koopmann <lennart@socketfeed.com>
2013-09-11 23:16:17 +0200

    try to use right working directory for travis ci

--- travis_sonar.sh
+++ travis_sonar.sh
@@ -1,3 +1,7 @@
+pwd
+cd $TRAVIS_BUILD_DIR
+pwd
+
 mvn sonar:sonar -Dsonar.host.url="http://sonar.torch.sh" \
                 -Dsonar.jdbc.url="jdbc:mysql://sonar.torch.sh:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" \
                 -Dsonar.jdbc.username="$SONAR_MYSQL_USER" \
