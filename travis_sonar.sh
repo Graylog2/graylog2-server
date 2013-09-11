0701b1e37ace978af1c1370fc85b9ad34341a00f
Lennart Koopmann <lennart@socketfeed.com>
2013-09-11 22:58:51 +0200

    thought of another way of using sonar with travis ci

---
+++ travis_sonar.sh
@@ -0,0 +1,4 @@
+mvn sonar:sonar -Dsonar.host.url="http://sonar.torch.sh" \
+                -Dsonar.jdbc.url="jdbc:mysql://sonar.torch.sh:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" \
+                -Dsonar.jdbc.username="$SONAR_MYSQL_USER" \
+                -Dsonar.jdbc.password="$SONAR_MYSQL_PASS"
