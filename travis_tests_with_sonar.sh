a815a0d9dc36d51d98e4a2f5c73abd39818410fe
Lennart Koopmann <lennart@socketfeed.com>
2013-09-12 01:04:08 +0200

    remove sonar from travis

--- travis_tests_with_sonar.sh
+++
@@ -1,4 +0,0 @@
-mvn sonar:sonar -Dsonar.host.url="http://sonar.torch.sh" \
-                -Dsonar.jdbc.url="jdbc:mysql://sonar.torch.sh:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" \
-                -Dsonar.jdbc.username="$SONAR_USER" \
-                -Dsonar.jdbc.password="$SONAR_PASS"
