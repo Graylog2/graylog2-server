c13b7bca3743ad7c97de512b8fbabed45e3b782e
Kay Roepke <kroepke@googlemail.com>
2013-09-21 11:59:32 +0200

    use injected Api instance

--- app/lib/ServerNodesRefreshService.java
+++ app/lib/ServerNodesRefreshService.java
@@ -64,7 +64,7 @@ public class ServerNodesRefreshService {
         public List<Node> call() throws Exception {
             List<Node> newNodes = Lists.newArrayList();
             log.debug("Updating graylog2 server node list from node {}", node);
-            NodeResponse response = ApiClient.get(NodeResponse.class)
+            NodeResponse response = api.get(NodeResponse.class)
                     .path("/cluster/nodes")
                     .node(node)
                     .execute();
