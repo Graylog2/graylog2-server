62cab9f9e7edaedf4654ff7a33623f17e94465fc
Lennart Koopmann <lennart@socketfeed.com>
2013-09-23 23:52:47 +0200

    more documented REST APIs #241

--- graylog2-server/src/main/java/org/graylog2/rest/resources/system/MessagesResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/system/MessagesResource.java
@@ -23,6 +23,9 @@ import com.codahale.metrics.annotation.Timed;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.graylog2.plugin.Tools;
+import org.graylog2.rest.documentation.annotations.Api;
+import org.graylog2.rest.documentation.annotations.ApiOperation;
+import org.graylog2.rest.documentation.annotations.ApiParam;
 import org.graylog2.rest.resources.RestResource;
 import org.graylog2.system.activities.SystemMessage;
 import org.slf4j.Logger;
@@ -39,14 +42,16 @@ import java.util.Map;
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
  */
+@Api(value = "System/Messages", description = "Internal Graylog2 messages")
 @Path("/system/messages")
 public class MessagesResource extends RestResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(MessagesResource.class);
 
     @GET @Timed
+    @ApiOperation(value = "Get internal Graylog2 system messages")
     @Produces(MediaType.APPLICATION_JSON)
-    public String all(@QueryParam("page") int page) {
+    public String all( @ApiParam(title = "page", description = "Page", required = false) @QueryParam("page") int page) {
         List<Map<String, Object>> messages = Lists.newArrayList();
 
         for (SystemMessage sm : SystemMessage.all(core, page(page))) {
