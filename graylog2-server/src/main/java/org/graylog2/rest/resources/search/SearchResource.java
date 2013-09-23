94cf0863a32d7ed3c02f1ec759f4ec51f5df2445
Lennart Koopmann <lennart@socketfeed.com>
2013-09-23 17:23:44 +0200

    lol naming

--- graylog2-server/src/main/java/org/graylog2/rest/resources/search/SearchResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/search/SearchResource.java
@@ -138,7 +138,7 @@ public class SearchResource extends RestResource {
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Invalid timerange parameters provided.")
     })
-    public String searchKeyword(
+    public String searchAbsolute(
             @ApiParam(title = "query", description = "Query (Lucene syntax)", required = true) @QueryParam("query") String query,
             @ApiParam(title = "from", description = "Timerange start. See description for date format", required = true) @QueryParam("from") String from,
             @ApiParam(title = "to", description = "Timerange end. See description for date format", required = true) @QueryParam("to") String to,
@@ -156,7 +156,7 @@ public class SearchResource extends RestResource {
             @ApiResponse(code = 400, message = "Invalid timerange parameters provided.")
     })
     @Produces(MediaType.APPLICATION_JSON)
-    public String termsKeyword(
+    public String termsAbsolute(
             @ApiParam(title = "field", description = "Message field of to return terms of", required = true) @QueryParam("field") String field,
             @ApiParam(title = "query", description = "Query (Lucene syntax)", required = true) @QueryParam("query") String query,
             @ApiParam(title = "size", description = "Maximum number of terms to return", required = false) @QueryParam("size") int size,
@@ -178,7 +178,7 @@ public class SearchResource extends RestResource {
             @ApiResponse(code = 400, message = "Invalid timerange parameters provided."),
             @ApiResponse(code = 400, message = "Field is not of numeric type.")
     })
-    public String statsKeyword(
+    public String statsAbsolute(
             @ApiParam(title = "field", description = "Message field of numeric type to return statistics for", required = true) @QueryParam("field") String field,
             @ApiParam(title = "query", description = "Query (Lucene syntax)", required = true) @QueryParam("query") String query,
             @ApiParam(title = "from", description = "Timerange start. See search method description for date format", required = true) @QueryParam("from") String from,
@@ -192,7 +192,7 @@ public class SearchResource extends RestResource {
 
     @GET @Path("/universal/absolute/histogram") @Timed
     @Produces(MediaType.APPLICATION_JSON)
-    public String histogramKeyword(@QueryParam("query") String query, @QueryParam("interval") String interval, @QueryParam("from") String from, @QueryParam("to") String to) {
+    public String histogramAbsolute(@QueryParam("query") String query, @QueryParam("interval") String interval, @QueryParam("from") String from, @QueryParam("to") String to) {
         interval = interval.toUpperCase();
         checkQueryAndInterval(query, interval);
         validateInterval(interval);
@@ -205,6 +205,7 @@ public class SearchResource extends RestResource {
                 )
         ));
     }
+
 
 
 
