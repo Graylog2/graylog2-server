b56ec3d952d3aab724647983ec95403419160bfb
Lennart Koopmann <lennart@socketfeed.com>
2013-09-25 15:49:43 +0200

    upgraded to Play\! 2.2.0. fixes #212

--- project/Build.scala
+++ project/Build.scala
@@ -8,21 +8,17 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
+    cache,
     javaCore,
     javaJdbc,
     javaEbean,
 
     "com.google.code.gson" % "gson" % "2.2",
     "com.google.guava" % "guava" % "14.0.1",
-
     "com.ning" % "async-http-client" % "1.7.17",
-
     "org.apache.shiro" % "shiro-core" % "1.2.2",
-
     "com.google.inject" % "guice" % "3.0",
     "com.google.inject.extensions" % "guice-assistedinject" % "3.0",
-
     "org.fluentlenium" % "fluentlenium-core" % "0.9.0" % "test",
     "org.fluentlenium" % "fluentlenium-festassert" % "0.9.0" % "test",
     "org.mockito" % "mockito-all" % "1.9.5" % "test",
