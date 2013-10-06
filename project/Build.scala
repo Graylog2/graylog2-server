7854a7b8e1c17aa2a421e20a4101bbbbae65ae96
Kay Roepke <kroepke@googlemail.com>
2013-10-06 13:23:47 +0200

    require more from jersey because fook
    fixed #281

--- project/Build.scala
+++ project/Build.scala
@@ -23,10 +23,13 @@ object ApplicationBuild extends Build {
     "org.fluentlenium" % "fluentlenium-core" % "0.9.0" % "test",
     "org.fluentlenium" % "fluentlenium-festassert" % "0.9.0" % "test",
     "org.mockito" % "mockito-all" % "1.9.5" % "test",
+
+    // TODO this is stupid, just to get that UriBuilder...
     "javax.ws.rs" % "jsr311-api" % "0.11",
-    "com.sun.jersey" % "jersey-grizzly2" % "1.17.1" % "test",
-    "com.sun.jersey" % "jersey-bundle" % "1.17.1" % "test",
     "com.sun.jersey" % "jersey-server" % "1.17.1",
+    "com.sun.jersey" % "jersey-grizzly2" % "1.17.1",
+    "com.sun.jersey" % "jersey-bundle" % "1.17.1",
+
     "org.codehaus.jackson" % "jackson-core-asl" % "1.9.12" % "test"
 )
 
