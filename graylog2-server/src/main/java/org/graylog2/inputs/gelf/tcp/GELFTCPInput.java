67b2777936efd8e1a3f5d034edce09d86c17abaf
Lennart Koopmann <lennart@socketfeed.com>
2013-10-30 15:09:05 +0100

    build proper GELFTCPInput metric names

--- graylog2-server/src/main/java/org/graylog2/inputs/gelf/tcp/GELFTCPInput.java
+++ graylog2-server/src/main/java/org/graylog2/inputs/gelf/tcp/GELFTCPInput.java
@@ -47,12 +47,12 @@ public class GELFTCPInput extends GELFInputBase {
     public void launch() throws MisfireException {
         // Register throughput counter gauges.
         for(Map.Entry<String,Gauge<Long>> gauge : throughputCounter.gauges().entrySet()) {
-            core.metrics().register(MetricRegistry.name(GELFTCPInput.class, gauge.getKey()), gauge.getValue());
+            core.metrics().register(MetricRegistry.name(getUniqueReadableId(), gauge.getKey()), gauge.getValue());
         }
 
         // Register connection counter gauges.
-        core.metrics().register(MetricRegistry.name(GELFTCPInput.class, "open_connections"), connectionCounter.gaugeCurrent());
-        core.metrics().register(MetricRegistry.name(GELFTCPInput.class, "total_connections"), connectionCounter.gaugeTotal());
+        core.metrics().register(MetricRegistry.name(getUniqueReadableId(), "open_connections"), connectionCounter.gaugeCurrent());
+        core.metrics().register(MetricRegistry.name(getUniqueReadableId(), "total_connections"), connectionCounter.gaugeTotal());
 
         final ExecutorService bossThreadPool = Executors.newCachedThreadPool(
                 new ThreadFactoryBuilder()
