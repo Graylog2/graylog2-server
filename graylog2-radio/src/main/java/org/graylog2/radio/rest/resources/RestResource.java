cd42823ab7c4efdbcb30c26d98aaa98b5b241db5
Lennart Koopmann <lennart@socketfeed.com>
2013-11-27 17:19:50 +0100

    metrics for radio

--- graylog2-radio/src/main/java/org/graylog2/radio/rest/resources/RestResource.java
+++ graylog2-radio/src/main/java/org/graylog2/radio/rest/resources/RestResource.java
@@ -19,6 +19,9 @@
  */
 package org.graylog2.radio.rest.resources;
 
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Timer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -32,6 +35,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
@@ -80,6 +84,80 @@ public class RestResource {
         r.put("megabytes", bytes/mb);
 
         return r;
+    }
+
+    protected Map<String, Object> buildTimerMap(Timer t) {
+        Map<String, Object> metrics = Maps.newHashMap();
+
+        if (t == null) {
+            return metrics;
+        }
+
+        TimeUnit timeUnit = TimeUnit.MICROSECONDS;
+
+        Map<String, Object> time = Maps.newHashMap();
+        time.put("max", TimeUnit.MICROSECONDS.convert(t.getSnapshot().getMax(), TimeUnit.NANOSECONDS));
+        time.put("min", TimeUnit.MICROSECONDS.convert(t.getSnapshot().getMin(), TimeUnit.NANOSECONDS));
+        time.put("mean", TimeUnit.MICROSECONDS.convert((long) t.getSnapshot().getMean(), TimeUnit.NANOSECONDS));
+        time.put("95th_percentile", TimeUnit.MICROSECONDS.convert((long) t.getSnapshot().get95thPercentile(), TimeUnit.NANOSECONDS));
+        time.put("98th_percentile", TimeUnit.MICROSECONDS.convert((long) t.getSnapshot().get98thPercentile(), TimeUnit.NANOSECONDS));
+        time.put("99th_percentile", TimeUnit.MICROSECONDS.convert((long) t.getSnapshot().get99thPercentile(), TimeUnit.NANOSECONDS));
+        time.put("std_dev", TimeUnit.MICROSECONDS.convert((long) t.getSnapshot().getStdDev(), TimeUnit.NANOSECONDS));
+
+        Map<String, Object> rate = Maps.newHashMap();
+        rate.put("one_minute", t.getOneMinuteRate());
+        rate.put("five_minute", t.getFiveMinuteRate());
+        rate.put("fifteen_minute", t.getFifteenMinuteRate());
+        rate.put("total", t.getCount());
+        rate.put("mean", t.getMeanRate());
+
+        metrics.put("rate_unit", "events/second");
+        metrics.put("duration_unit", timeUnit.toString().toLowerCase());
+        metrics.put("time", time);
+        metrics.put("rate", rate);
+
+        return metrics;
+    }
+    protected Map<String, Object> buildHistogramMap(Histogram h) {
+        Map<String, Object> metrics = Maps.newHashMap();
+
+        if (h == null) {
+            return metrics;
+        }
+
+        Map<String, Object> time = Maps.newHashMap();
+        time.put("max", h.getSnapshot().getMax());
+        time.put("min", h.getSnapshot().getMin());
+        time.put("mean", (long) h.getSnapshot().getMean());
+        time.put("95th_percentile", (long) h.getSnapshot().get95thPercentile());
+        time.put("98th_percentile", (long) h.getSnapshot().get98thPercentile());
+        time.put("99th_percentile", (long) h.getSnapshot().get99thPercentile());
+        time.put("std_dev", (long) h.getSnapshot().getStdDev());
+
+        metrics.put("time", time);
+        metrics.put("count", h.getCount());
+
+        return metrics;
+    }
+
+    protected Map<String, Object> buildMeterMap(Meter m) {
+        Map<String, Object> metrics = Maps.newHashMap();
+
+        if (m == null) {
+            return metrics;
+        }
+
+        Map<String, Object> rate = Maps.newHashMap();
+        rate.put("one_minute", m.getOneMinuteRate());
+        rate.put("five_minute", m.getFiveMinuteRate());
+        rate.put("fifteen_minute", m.getFifteenMinuteRate());
+        rate.put("total", m.getCount());
+        rate.put("mean", m.getMeanRate());
+
+        metrics.put("rate_unit", "events/second");
+        metrics.put("rate", rate);
+
+        return metrics;
     }
 
 }
