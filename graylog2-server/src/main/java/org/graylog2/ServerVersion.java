290efcebc4935ae7b46420613e92f64e96faaa99
Lennart Koopmann <lennart@socketfeed.com>
2013-12-12 23:53:48 +0100

    bumped version to preview.8

--- graylog2-server/src/main/java/org/graylog2/ServerVersion.java
+++ graylog2-server/src/main/java/org/graylog2/ServerVersion.java
@@ -34,7 +34,8 @@ public class ServerVersion {
     public static final Version v0_20_0_PREVIEW_5 = new Version(0, 20, 0, "preview.5");
     public static final Version v0_20_0_PREVIEW_6 = new Version(0, 20, 0, "preview.6");
     public static final Version v0_20_0_PREVIEW_7 = new Version(0, 20, 0, "preview.7");
+    public static final Version v0_20_0_PREVIEW_8 = new Version(0, 20, 0, "preview.8");
 
-    public static final Version VERSION = v0_20_0_PREVIEW_7;
+    public static final Version VERSION = v0_20_0_PREVIEW_8;
 
 }
