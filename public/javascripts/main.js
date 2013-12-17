2d321b18b2cc3d5f39dc59ee4bd90e457f0aa6b4
Kay Roepke <kroepke@googlemail.com>
2013-12-17 18:00:47 +0100

    allow removing LDAP-based users
    repair confirm box when deleting users
    
    fixes #348

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,29 +4,29 @@ $(document).ready(function() {
 	$(".messages tbody > tr").bind("click", function() {
 		messageId = $(this).attr("data-message-id");
 		index = $(this).attr("data-source-index");
-		
+
 		// Highlight message.
 		$(".messages tbody > tr").removeClass("message-highlighted");
 		$(this).addClass("message-highlighted");
-		
+
 		// Hide original sidebar and show ours again if it was already hidden before.
 		$("#sidebar-original").hide();
 		$("#sidebar-replacement").show();
-		
+
 		// Show loading spinner. Will be replaced onSuccess.
 		spinner = "<h2><i class='icon-refresh icon-spin'></i> &nbsp;Loading message</h2>";
 		$("#sidebar-replacement").html(spinner);
-		
+
 		$.get("/messages/" + index + "/" + messageId + "/partial", function(data) {
 			$("#sidebar-replacement").html(data);
 		})
-		
+
 		.fail(function() { displayFailureInSidebar("Sorry, could not load message."); })
-		
+
 		.complete(function() {
 
             sizeSidebar();
-			
+
 			// Inject terms of a message when modal is requested.
 			$('.terms-msg-modal').on('show', function() {
                 messageId = $(this).attr("data-msg-id");
@@ -52,7 +52,7 @@ $(document).ready(function() {
                     // Mark as already loaded so we don't add the terms again on next open.
                     $(this).attr("data-loaded", "true");
                 }
-				
+
 				// Show as list link.
 				list_link.bind("click", function() {
 					list.addClass("as-list");
@@ -62,17 +62,17 @@ $(document).ready(function() {
 			});
 		})
 	});
-	
+
 	// Date histogram resolution selector.
 	$(".date-histogram-res-selector").each(function() {
 		$(this).attr("href", "/search?" + addParameterToCurrentUrl("interval", $(this).attr("data-resolution")));
 	});
-	
+
 	// Go back in sidebar history / Show original sidebar.
 	$(".sidebar-back").live("click", function() {
 		$("#sidebar-replacement").hide();
 		$("#sidebar-original").show();
-		
+
 		// Remove highlighting.
 		$(".messages tbody > tr").removeClass("message-highlighted");
 
@@ -93,7 +93,7 @@ $(document).ready(function() {
 		hash = $(this).attr("data-field-hash");
 		td = $(".result-td-" + hash);
 		th = $("#result-th-" + hash);
-		
+
 		if ($(this).is(':checked')) {
 			th.show();
 			td.show();
@@ -284,10 +284,6 @@ $(document).ready(function() {
             }
         }, 150);
     }
-
-    $(".delete-user-form").on("submit", function() {
-        return confirm("Really remove user " + $(this).attr("data-username") + "?");
-    });
 
     // Universalsearch validation.
     $("#universalsearch").on("submit", function() {
@@ -535,9 +531,9 @@ $(document).ready(function() {
 		x = "<span class='alert alert-error sidebar-alert'><i class='icon-warning-sign'></i> " + message + "</span>"
 		$("#sidebar-inner").html(x);
 	}
-	
+
 	function addParameterToCurrentUrl(key, value) {
-	    key = escape(key); 
+	    key = escape(key);
 	    value = escape(value);
 
 	    var kvp = document.location.search.substr(1).split('&');
@@ -558,7 +554,7 @@ $(document).ready(function() {
 	    	kvp[kvp.length] = [key,value].join('=');
 	    }
 
-	    return kvp.join('&'); 
+	    return kvp.join('&');
 	}
 
     function sizeSidebar() {
