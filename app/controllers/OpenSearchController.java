dd601ff50df4892eda2cbfbca34a6aff37d222a2
Kay Roepke <kroepke@googlemail.com>
2013-12-17 19:21:34 +0100

    add super-hipster opensearch descriptor + route
    needs favicon and testing.
    chrome refuses to load the descriptor in dev, might be because of localhost/IP
    
    issue #439

---
+++ app/controllers/OpenSearchController.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 TORCH UG
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package controllers;
+
+import play.mvc.Result;
+
+public class OpenSearchController extends BaseController  {
+
+    public static Result index() {
+        final String relative = routes.SearchController.index("{searchTerms}", "relative", 3600, "", "", "", "", 1).absoluteURL(request());
+        final String unescaped = relative.replaceAll("%7B", "{").replaceAll("%7D", "}").replaceAll("&", "&amp;");
+        String content =
+                "<OpenSearchDescription xmlns=\"http://a9.com/-/spec/opensearch/1.1/\"\n" +
+                "                       xmlns:moz=\"http://www.mozilla.org/2006/browser/search/\">\n" +
+                "    <ShortName>Graylog2</ShortName>\n" +
+                "    <Description>Search Graylog2 (last hour)</Description>\n" +
+                "    <InputEncoding>UTF-8</InputEncoding>\n" +
+                "    <Url type=\"text/html\" method=\"get\" template=\""+ unescaped +"\"/>\n" +
+                "    <moz:SearchForm>"+unescaped+"</moz:SearchForm>\n" +
+                "</OpenSearchDescription>\n";
+
+        //                "    <!-- Image width=\"16\" height=\"16\" type=\"image/x-icon\">/favicon.ico</Image -->\n" +
+
+        return ok(content);
+    }
+}
