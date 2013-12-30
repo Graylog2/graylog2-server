bfd6bdd2f59ca86a7ac5c208470f9b7d8f10f020
Lennart Koopmann <lennart@socketfeed.com>
2013-12-30 15:47:42 +0100

    first parts of field chart widgets. #370

---
+++ public/javascripts/dashboards/widgets/field_chart.js
@@ -0,0 +1,18 @@
+function addWidget_field_chart(dashboardId, description, eventElem) {
+    var params = originalUniversalSearchSettings();
+    params.widgetType = "FIELD_CHART";
+    params.field = eventElem.attr("data-field");
+
+    var chart = eventElem.closest(".field-graph-container");
+
+    params.valuetype = chart.attr("data-config-valuetype");
+    params.renderer = chart.attr("data-config-renderer");
+    params.interpolation = chart.attr("data-config-interpolation");
+    params.interval = chart.attr("data-config-interval");
+
+    if (!!eventElem.attr("data-stream-id")) {
+        params.streamId =  eventElem.attr("data-stream-id");
+    }
+
+    addWidget(dashboardId, description, params);
+}
