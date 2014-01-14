168b6172617dcfc6c4cf05082d76ad048e907799
Dennis Oelkers <dennis@torch.sh>
2014-01-14 12:17:58 +0100

    Minor fix in global input launch

--- app/models/Radio.java
+++ app/models/Radio.java
@@ -147,16 +147,16 @@ public class Radio extends ClusterEntity {
 
     public boolean launchExistingInput(String inputId) {
         try {
-            api.get(EmptyResponse.class)
-                    .path("/system/inputs/{0}", inputId)
+            api.get(InputLaunchResponse.class)
+                    .path("/system/inputs/{0}/launch", inputId)
                     .radio(this)
                     .expect(Http.Status.ACCEPTED)
                     .execute();
             return true;
         } catch (APIException e) {
-            log.error("Could not terminate input " + inputId, e);
+            log.error("Could not launch input " + inputId, e);
         } catch (IOException e) {
-            log.error("Could not terminate input " + inputId, e);
+            log.error("Could not launch input " + inputId, e);
         }
 
         return false;
