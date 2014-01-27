578bd281f8cb12f46ca902cd2b87b0838a831a90
Lennart Koopmann <lennart@socketfeed.com>
2014-01-27 15:07:58 +0100

    change syslog processor log levels. fixes #570

--- graylog2-inputs/src/main/java/org/graylog2/inputs/syslog/SyslogProcessor.java
+++ graylog2-inputs/src/main/java/org/graylog2/inputs/syslog/SyslogProcessor.java
@@ -86,7 +86,7 @@ public class SyslogProcessor {
             lm = parse(msg, remoteAddress);
         } catch (Exception e) {
             parsingFailures.mark();
-            LOG.error("Could not parse syslog message. Not further handling.", e);
+            LOG.warn("Could not parse syslog message. Not further handling.", e);
             return;
         }
 
@@ -193,10 +193,10 @@ public class SyslogProcessor {
         // Check if date could be parsed.
         if (msg.getDate() == null) {
             if (config.getBoolean(SyslogInputBase.CK_ALLOW_OVERRIDE_DATE)) {
-                LOG.info("Date could not be parsed. Was set to NOW because {} is true.", SyslogInputBase.CK_ALLOW_OVERRIDE_DATE);
+                LOG.debug("Date could not be parsed. Was set to NOW because {} is true.", SyslogInputBase.CK_ALLOW_OVERRIDE_DATE);
                 return Tools.iso8601();
             } else {
-                LOG.info("Syslog message is missing date or date could not be parsed. (Possibly set {} to true) "
+                LOG.warn("Syslog message is missing date or date could not be parsed. (Possibly set {} to true) "
                         + "Not further handling. Message was: {}", SyslogInputBase.CK_ALLOW_OVERRIDE_DATE, new String(msg.getRaw()));
                 throw new IllegalStateException();
             }
