1be46826e45c7b5feebb555c2e2dce469487be03
Lennart Koopmann <lennart@socketfeed.com>
2014-02-10 17:43:37 +0100

    bump to rc.2

--- app/lib/Version.java
+++ app/lib/Version.java
@@ -41,8 +41,9 @@ public class Version {
     public static final Version v0_20_0_PREVIEW_8 = new Version(0, 20, 0, "preview.8");
     public static final Version v0_20_0_RC_1 = new Version(0, 20, 0, "rc.1");
     public static final Version v0_20_0_RC_1_1 = new Version(0, 20, 0, "rc.1-1");
+    public static final Version v0_20_0_RC_2 = new Version(0, 20, 0, "rc.2");
 
-    public static final Version VERSION = v0_20_0_RC_1_1;
+    public static final Version VERSION = v0_20_0_RC_2;
 
     public final int major;
     public final int minor;
