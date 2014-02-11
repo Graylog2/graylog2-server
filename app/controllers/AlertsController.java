633f105f2d8d277aba9f84a950b162b1c9ef8433
Dennis Oelkers <dennis@torch.sh>
2014-02-11 11:59:22 +0100

    Changing copied error description to be more accurate

--- app/controllers/AlertsController.java
+++ app/controllers/AlertsController.java
@@ -271,7 +271,7 @@ public class AlertsController extends AuthenticatedController {
         } catch (IOException e) {
             return status(504, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
         } catch (APIException e) {
-            String message = "Could not add alert receiver: We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
+            String message = "Could not send dummy alert: We expected HTTP 204, but got a HTTP " + e.getHttpCode() + ".";
             return status(504, views.html.errors.error.render(message, e, request()));
         }
     }
