a3f4dfd3853e0aefb23713ebb544775fc4dfdea9
Lennart Koopmann <lennart@socketfeed.com>
2014-02-24 17:38:27 +0100

    bump to 0.20.1

--- graylog2-server/src/main/java/org/graylog2/ServerVersion.java
+++ graylog2-server/src/main/java/org/graylog2/ServerVersion.java
@@ -42,7 +42,8 @@ public class ServerVersion {
     public static final Version v0_20_0_RC_3 = new Version(0, 20, 0, "rc.3");
 
     public static final Version v0_20_0 = new Version(0, 20, 0);
+    public static final Version v0_20_1 = new Version(0, 20, 1);
 
-    public static final Version VERSION = v0_20_0;
+    public static final Version VERSION = v0_20_1;
 
 }
