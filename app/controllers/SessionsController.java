14c2a57c072c6036ba7c9246efb4186fd8fd2448
Kay Roepke <kroepke@googlemail.com>
2014-02-28 15:06:29 +0100

    redirect(null), I nulled.
    fixes Graylog2/graylog2-server#440

--- app/controllers/SessionsController.java
+++ app/controllers/SessionsController.java
@@ -102,7 +102,7 @@ public class SessionsController extends BaseController {
 
             // if we were redirected from somewhere else because the session had expired, redirect back to that page
             // otherwise use the configured startpage (or skip it if that was requested)
-            if (r.destination != null) {
+            if (r.destination != null && !r.destination.isEmpty()) {
                 return redirect(r.destination);
             }
             // upon redirect, the auth layer will load the user with the given session and log the user in.
