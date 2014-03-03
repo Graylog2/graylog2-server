9e3b6287b9a840b9ca73373ab700007990a3e881
Kay Roepke <kroepke@googlemail.com>
2014-03-03 17:31:39 +0100

    mark open file limit notification as fixed
    (as it gets really annoying when flashing the icon so often)

--- graylog2-server/src/main/java/org/graylog2/periodical/IndexerClusterCheckerThread.java
+++ graylog2-server/src/main/java/org/graylog2/periodical/IndexerClusterCheckerThread.java
@@ -1,7 +1,6 @@
 package org.graylog2.periodical;
 
 import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
-import org.graylog2.Core;
 import org.graylog2.notifications.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +16,7 @@ public class IndexerClusterCheckerThread extends Periodical {
 
     @Override
     public void run() {
+        boolean allHigher = true;
         for (NodeInfo node : core.getIndexer().cluster().getDataNodes()) {
             // Check number of maximum open files.
             if (node.getProcess().getMaxFileDescriptors() < MINIMUM_OPEN_FILES_LIMIT) {
@@ -29,7 +29,11 @@ public class IndexerClusterCheckerThread extends Periodical {
                         .addType(Notification.Type.ES_OPEN_FILES)
                         .addSeverity(Notification.Severity.URGENT)
                         .publishIfFirst();
+                allHigher = false;
             }
+        }
+        if (allHigher) {
+            Notification.build(core).addType(Notification.Type.ES_OPEN_FILES).fixed();
         }
     }
 
