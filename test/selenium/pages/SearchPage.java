7dc095bf82835a232fdc27ad0ac25a42f3512078
Kay Roepke <kroepke@googlemail.com>
2014-03-03 18:51:52 +0100

    fix stub selenium test
    issue #610

--- test/selenium/pages/SearchPage.java
+++ test/selenium/pages/SearchPage.java
@@ -36,10 +36,11 @@ public class SearchPage extends FluentPage {
     private String streamId;
     private String sortField;
     private String sortOrder;
+    private String fields;
 
     @Override
     public String getUrl() {
-        return routes.SearchController.index(queryString, rangeType, relativeTimeSpan, fromTime, toTime, keyword, interval, pageNumber, streamId, sortField, sortOrder).url();
+        return routes.SearchController.index(queryString, rangeType, relativeTimeSpan, fromTime, toTime, keyword, interval, pageNumber, streamId, sortField, sortOrder, fields).url();
     }
 
     @Override
