a79728ef59484f7e646c74009c0dbf106334cd4a
Dennis Oelkers <dennis@torch.sh>
2014-03-25 10:20:32 +0100

    Implementing pattern size-limited cache for RegexMatchers
    Changing control flow for BlacklistFilter, so matching against blacklists does not end when LoadingCache throws exception

--- graylog2-server/src/main/java/org/graylog2/streams/matchers/RegexMatcher.java
+++ graylog2-server/src/main/java/org/graylog2/streams/matchers/RegexMatcher.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2011, 2012, 2013 Lennart Koopmann <lennart@socketfeed.com>
+/*
+ * Copyright 2012-2014 TORCH GmbH
  *
  * This file is part of Graylog2.
  *
@@ -15,25 +15,48 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 package org.graylog2.streams.matchers;
 
-import java.util.regex.Pattern;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.streams.StreamRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Pattern;
 
 /**
  * @author Lennart Koopmann <lennart@socketfeed.com>
  */
 public class RegexMatcher implements StreamRuleMatcher {
+    private static final Logger LOG = LoggerFactory.getLogger(RegexMatcher.class);
+
+    private static final long CACHESIZE = 1000;
+    private static final LoadingCache<String, Pattern> patternCache = CacheBuilder.newBuilder().maximumSize(CACHESIZE).build(new CacheLoader<String, Pattern>() {
+        @Override
+        public Pattern load(String key) throws Exception {
+            return Pattern.compile(key, Pattern.DOTALL);
+        }
+    });
 
     @Override
     public boolean match(Message msg, StreamRule rule) {
         if (msg.getField(rule.getField()) == null)
             return false;
-        return rule.getInverted() ^ Pattern.compile(rule.getValue(), Pattern.DOTALL).matcher(msg.getField(rule.getField()).toString()).find();
+
+        try {
+            Pattern pattern = patternCache.get(rule.getValue());
+            return rule.getInverted() ^ pattern.matcher(msg.getField(rule.getField()).toString()).find();
+        } catch (ExecutionException e) {
+            LOG.error("Unable to get pattern from regex cache: ", e);
+        }
+
+        return false;
     }
 
 }
