38ad99e83537cfe1c6a026c09f92323f985d87bc
Lennart Koopmann <lennart@socketfeed.com>
2014-03-31 22:10:50 +0200

    renamed Kafka radio input class (non-breaking) #385

--- graylog2-inputs/src/main/java/org/graylog2/inputs/radio/RadioInput.java
+++ graylog2-inputs/src/main/java/org/graylog2/inputs/radio/RadioInput.java
@@ -19,102 +19,17 @@
  */
 package org.graylog2.inputs.radio;
 
-import org.graylog2.inputs.kafka.KafkaInput;
-import org.graylog2.plugin.InputHost;
-import org.graylog2.plugin.configuration.Configuration;
-import org.graylog2.plugin.configuration.ConfigurationException;
-import org.graylog2.plugin.configuration.ConfigurationRequest;
-import org.graylog2.plugin.configuration.fields.ConfigurationField;
-import org.graylog2.plugin.configuration.fields.NumberField;
-import org.graylog2.plugin.configuration.fields.TextField;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
  */
-public class RadioInput extends KafkaInput {
+public class RadioInput extends RadioKafkaInput {
 
-    private static final Logger LOG = LoggerFactory.getLogger(KafkaInput.class);
-
-    public static final String NAME = "Graylog2 Radio Input";
-
-    @Override
-    public void checkConfiguration() throws ConfigurationException {
-        configuration.setString(CK_TOPIC_FILTER, "^graylog2-radio-messages$");
-
-        if (!checkConfig(configuration)) {
-            throw new ConfigurationException(configuration.getSource().toString());
-        }
-    }
-
-    @Override
-    public String getName() {
-        return NAME;
-    }
-
-    @Override
-    public String linkToDocs() {
-        return "http://support.torch.sh/help/kb/graylog2-server/using-graylog2-radio-v020x";
-    }
-
-    @Override
-    public boolean isExclusive() {
-        return true;
-    }
-
-    @Override
-    public ConfigurationRequest getRequestedConfiguration() {
-        ConfigurationRequest cr = new ConfigurationRequest();
-
-        cr.addField(new TextField(
-                CK_ZOOKEEPER,
-                "ZooKeeper address",
-                "192.168.1.1:2181",
-                "Host and port of the ZooKeeper that is managing your Kafka cluster.",
-                ConfigurationField.Optional.NOT_OPTIONAL
-        ));
-
-        cr.addField(new NumberField(
-                CK_FETCH_MIN_BYTES,
-                "Fetch minimum bytes",
-                5,
-                "Wait for a message batch to reach at least this size or the configured maximum wait time before fetching.",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-
-        cr.addField(new NumberField(
-                CK_FETCH_WAIT_MAX,
-                "Fetch maximum wait time (ms)",
-                100,
-                "Wait for this time or the configured minimum size of a message batch before fetching.",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-
-        cr.addField(new NumberField(
-                CK_THREADS,
-                "Processor threads",
-                10,
-                "Number of processor threads to spawn.",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-
-        return cr;
-    }
-
-    @Override
-    public Map<String, Object> getAttributes() {
-        return configuration.getSource();
-    }
-
-    @Override
-    protected boolean checkConfig(Configuration config) {
-        return config.intIsSet(CK_FETCH_MIN_BYTES)
-                && config.intIsSet(CK_FETCH_WAIT_MAX)
-                && config.stringIsSet(CK_ZOOKEEPER)
-                && config.intIsSet(CK_THREADS)  && config.getInt(CK_THREADS) > 0;
-    }
+    /*
+     * RadioInput became RadioKafkaInput when RadioAMQPInput was introduced. This
+     * is just a class for convenience of all the users that had already launched
+     * a RadioInput. Inputs are launched by class name for plugin flexibility.
+     *
+     * lol naming.
+     */
 
 }
