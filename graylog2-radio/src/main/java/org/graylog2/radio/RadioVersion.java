686375ea8ce7581785c947af635121d9c35bb0aa
Lennart Koopmann <lennart@socketfeed.com>
2014-05-23 17:37:00 +0200

    bump to v0.20.2

--- graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
+++ graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
@@ -36,6 +36,8 @@ public class RadioVersion {
     public static final Version v0_20_2_SNAPSHOT = new Version(0, 20, 2, "snapshot");
     public static final Version v0_20_2_RC_1 = new Version(0, 20, 2, "rc.1");
 
-    public static final Version VERSION = v0_20_2_RC_1;
+    public static final Version v0_20_2 = new Version(0, 20, 2);
+
+    public static final Version VERSION = v0_20_2;
 
 }
