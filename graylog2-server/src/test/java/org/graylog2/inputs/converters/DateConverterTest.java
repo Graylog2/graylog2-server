30e0e08bc56e5be9fe9862a8a5d059421a1b20d4
Lennart Koopmann <lennart@socketfeed.com>
2014-05-23 13:58:53 +0200

    fix test

--- graylog2-server/src/test/java/org/graylog2/inputs/converters/DateConverterTest.java
+++ graylog2-server/src/test/java/org/graylog2/inputs/converters/DateConverterTest.java
@@ -23,6 +23,7 @@ import org.graylog2.ConfigurationException;
 import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.inputs.Converter;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -43,14 +44,8 @@ public class DateConverterTest {
 
     @Test
     public void testAnotherBasicConvert() throws Exception {
-        // .startsWith() because of possibly different timezones per test environment.
-        System.out.println(new DateConverter(config("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZZ")));
-        System.out.println(new DateConverter(config("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZZ")).convert("2014-05-19T00:30:43.116847+00:00"));
-        System.out.println("LOCAL TIME: " + new DateTime());
-        System.out.println("LOCAL YEAR: " + new DateTime().getYear());
-        System.out.println("JVM: " + Tools.getSystemInformation());
-
-        assert(new DateConverter(config("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZZ")).convert("2014-05-19T00:30:43.116847+00:00").toString().startsWith("2014-05-19T02:30:43.116"));
+        DateTime date = (DateTime) new DateConverter(config("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZZ")).convert("2014-05-19T00:30:43.116847+00:00");
+        assertEquals("2014-05-19T00:30:43.116Z", date.toDateTime(DateTimeZone.UTC).toString());
     }
 
     @Test(expectedExceptions = ConfigurationException.class)
