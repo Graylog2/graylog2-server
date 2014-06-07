eba7e984d0999606f90689ca3b89ee644b1e92a4
Lennart Koopmann <lennart@socketfeed.com>
2014-06-07 17:49:00 +0200

    fix typo

--- graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
@@ -221,7 +221,7 @@ public class Searches {
         stats.order(TermsStatsFacet.ComparatorType.fromString(order.toString().toLowerCase()));
         stats.size(size);
 
-        terms.facetFilter(standardFilters(range, filter));
+        stats.facetFilter(standardFilters(range, filter));
 
         srb.addFacet(stats);
 
