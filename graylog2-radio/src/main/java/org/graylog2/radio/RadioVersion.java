731bcfa6c91b5bc91a0afb968fcc25f5277ab019
Kay Roepke <kroepke@googlemail.com>
2014-06-12 16:23:41 +0200

    bump version to 0.20.3

--- graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
+++ graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
@@ -38,6 +38,8 @@ public class RadioVersion {
 
     public static final Version v0_20_2 = new Version(0, 20, 2);
 
-    public static final Version VERSION = v0_20_2;
+    public static final Version v0_20_3 = new Version(0, 20, 3);
+
+    public static final Version VERSION = v0_20_3;
 
 }
--- graylog2-server/src/main/java/org/graylog2/ServerVersion.java
+++ graylog2-server/src/main/java/org/graylog2/ServerVersion.java
@@ -49,6 +49,8 @@ public class ServerVersion {
 
     public static final Version v0_20_2 = new Version(0, 20, 2);
 
-    public static final Version VERSION = v0_20_2;
+    public static final Version v0_20_3 = new Version(0, 20, 3);
+
+    public static final Version VERSION = v0_20_3;
 
 }
