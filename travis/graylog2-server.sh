c12342f6f1a8d8bd98024f876f9acf174bf14eee
Kay Roepke <kroepke@googlemail.com>
2014-06-13 13:24:37 +0200

    use mvn install to gain access to the correct graylog2-rest-modules jar
    and do it before running play update!

--- travis/graylog2-server.sh
+++ travis/graylog2-server.sh
@@ -9,6 +9,8 @@ pushd graylog2-server
 sh install-syslog4j-jar.sh
 mvn clean package -DskipTests
 
+mvn install
+
 cp $TRAVIS_BUILD_DIR/travis/server.conf graylog2-travis-server.conf
 nohup java -jar graylog2-server/target/graylog2-server.jar -f graylog2-travis-server.conf -l -p graylog2-travis-server.pid &
 
