5b6c5b3d6429952ddab62615fc2f528ef805bd75
Bernd Ahlers <bernd@torch.sh>
2014-06-27 16:37:11 +0200

    Add getHighlightedField and hasHighlightedFields to MessageResult.
    Remove getHighlightRangesAsJson.
    
    Needed for #819.

--- src/main/java/org/graylog2/restclient/models/api/results/MessageResult.java
+++ src/main/java/org/graylog2/restclient/models/api/results/MessageResult.java
@@ -23,7 +23,6 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.gson.Gson;
 import org.graylog2.restclient.models.FieldMapper;
 import org.graylog2.restclient.models.api.responses.HighlightRange;
 import org.joda.time.DateTime;
@@ -185,7 +184,11 @@ public class MessageResult {
         return highlightRanges;
     }
 
-    public String getHighlightRangesAsJson() {
-        return new Gson().toJson(getHighlightRanges());
+    public boolean hasHighlightedFields() {
+        return highlightRanges != null;
+    }
+
+    public HighlightedField getHighlightedField(String field) {
+        return new HighlightedField(this, field);
     }
 }
