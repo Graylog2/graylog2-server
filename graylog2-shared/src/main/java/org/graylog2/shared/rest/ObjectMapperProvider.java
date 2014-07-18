352d54efc966987e56103135f13cb357bccc4f71
Kay Roepke <kroepke@googlemail.com>
2014-07-18 16:57:09 +0200

    add forgotten ObjectMapperProvider for supporting mongojack's _id

---
+++ graylog2-shared/src/main/java/org/graylog2/shared/rest/ObjectMapperProvider.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2013 TORCH GmbH
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.shared.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
+    private final ObjectMapper objectMapper;
+
+    /**
+     * This abomination is necessary because when using MongoJack to read "_id" object ids back from the database
+     * the property name isn't correctly mapped to the POJO using the LowerCaseWithUnderscoresStrategy.
+     *
+     * Apparently no one in the world tried to use a different naming strategy with MongoJack.
+     * (one of my many useless talents is finding corner cases).
+     */
+    public class PreserveLeadingUnderscoreStrategy extends PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy {
+        @Override
+        public String translate(String input) {
+            String translated = super.translate(input);
+            if (input.startsWith("_") && !translated.startsWith("_")) {
+                translated = "_" + translated; // lol underscore
+            }
+            return translated;
+        }
+    }
+    public ObjectMapperProvider() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setPropertyNamingStrategy(new PreserveLeadingUnderscoreStrategy());
+        objectMapper.registerModule(new JodaModule());
+        objectMapper.registerModule(new GuavaModule());
+        final SimpleModule module = new SimpleModule() {
+            {
+                addSerializer(new RangeJsonSerializer());
+            }
+        };
+        objectMapper.registerModule(module);
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return objectMapper;
+    }
+
+}
