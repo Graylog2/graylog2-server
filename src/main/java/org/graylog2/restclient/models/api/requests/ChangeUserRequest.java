7d8785c6ec3f2cde732679b2875af840ea4058fe
Bernd Ahlers <bernd@torch.sh>
2014-07-23 13:43:38 +0200

    Use full_name instead of fullname.
    Related to #485

--- src/main/java/org/graylog2/restclient/models/api/requests/ChangeUserRequest.java
+++ src/main/java/org/graylog2/restclient/models/api/requests/ChangeUserRequest.java
@@ -27,6 +27,7 @@ import java.util.List;
 
 public class ChangeUserRequest extends ApiRequest {
     @Constraints.Required
+    @SerializedName("full_name")
     public String fullname;
     @Constraints.Required
     public String email;
