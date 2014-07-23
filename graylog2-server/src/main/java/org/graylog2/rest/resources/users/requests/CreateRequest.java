ee59cb8f0ddcf51874f88ff28eb723f5de94d36e
Bernd Ahlers <bernd@torch.sh>
2014-07-23 13:42:04 +0200

    Consistently use full_name instead of fullname.
    Fixes #485

--- graylog2-server/src/main/java/org/graylog2/rest/resources/users/requests/CreateRequest.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/users/requests/CreateRequest.java
@@ -31,6 +31,7 @@ public class CreateRequest {
     public String username;
     public String password;
     public String email;
+    @JsonProperty("full_name")
     public String fullname;
     public List<String> permissions;
     public String timezone;
