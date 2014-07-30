bb4bc336a94fd032f416778bb5b8d26c3b19dae9
Kay Roepke <kroepke@googlemail.com>
2014-07-30 18:39:36 +0200

    delay launching persisted inputs until after the node is in RUNNING state
    otherwise this could lead to read many messages before the system is actually capabable of processing data.
    nothing would've gotten lost, however it added unnecessary time to the start up could use lots of memory
    
    fixes #638

--- graylog2-shared/src/main/java/org/graylog2/shared/initializers/InputSetupService.java
+++ graylog2-shared/src/main/java/org/graylog2/shared/initializers/InputSetupService.java
@@ -20,35 +20,74 @@
 package org.graylog2.shared.initializers;
 
 import com.google.common.base.Stopwatch;
-import com.google.common.util.concurrent.AbstractIdleService;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import com.google.common.util.concurrent.AbstractExecutionThreadService;
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.graylog2.plugin.inputs.InputState;
 import org.graylog2.plugin.inputs.MessageInput;
+import org.graylog2.plugin.lifecycles.Lifecycle;
 import org.graylog2.shared.inputs.InputRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author Dennis Oelkers <dennis@torch.sh>
  */
-public class InputSetupService extends AbstractIdleService {
+public class InputSetupService extends AbstractExecutionThreadService {
     private static final Logger LOG = LoggerFactory.getLogger(InputSetupService.class);
     private final InputRegistry inputRegistry;
+    private final EventBus eventBus;
+
+    private CountDownLatch startLatch = new CountDownLatch(1);
+    private CountDownLatch stopLatch = new CountDownLatch(1);
+    private Lifecycle previousLifecycle;
 
     @Inject
-    public InputSetupService(InputRegistry inputRegistry) {
+    public InputSetupService(InputRegistry inputRegistry, EventBus eventBus) {
         this.inputRegistry = inputRegistry;
+        this.eventBus = eventBus;
+        eventBus.register(this);
+    }
+
+    @Subscribe
+    public void lifecycleChanged(Lifecycle lifecycle) {
+        LOG.debug("Lifecycle is now {}", lifecycle);
+        // if we switch to RUNNING from STARTING (or unknown) the server is ready to accept connections on inputs.
+        // we want to postpone opening the inputs earlier, so we don't get swamped with messages before
+        // we can actually process them.
+        if ((lifecycle == Lifecycle.RUNNING) && (previousLifecycle == Lifecycle.STARTING || previousLifecycle == null)) {
+            LOG.info("Triggering launching persisted inputs, node transitioned from {} to {}", previousLifecycle, lifecycle);
+            startLatch.countDown();
+        }
+        previousLifecycle = lifecycle;
     }
 
     @Override
-    protected void startUp() throws Exception {
+    protected void run() throws Exception {
+        // prevent launching persisted inputs too early.
+        LOG.debug("Delaying lauching persisted inputs until the node is in RUNNING state.");
+        Uninterruptibles.awaitUninterruptibly(startLatch);
+
+        LOG.debug("Launching persisted inputs now.");
         inputRegistry.launchAllPersisted();
+
+        // next, simply block until we are asked to shutdown, even though we are consuming a thread this way.
+        Uninterruptibles.awaitUninterruptibly(stopLatch);
+    }
+
+    @Override
+    protected void triggerShutdown() {
+        stopLatch.countDown();
     }
 
     @Override
     protected void shutDown() throws Exception {
+        eventBus.unregister(this);
         for (InputState state : inputRegistry.getRunningInputs()) {
             MessageInput input = state.getMessageInput();
 
