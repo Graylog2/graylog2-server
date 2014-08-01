c6246d9512b1cb4ea501eff261798873adb9b263
Dennis Oelkers <dennis@torch.sh>
2014-08-01 13:16:20 +0200

    Adding tests for OutputRouter.

--- graylog2-server/src/test/java/org/graylog2/outputs/OutputRegistryTest.java
+++ graylog2-server/src/test/java/org/graylog2/outputs/OutputRegistryTest.java
@@ -13,8 +13,7 @@ import org.testng.annotations.Test;
 
 import java.util.Set;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.Mock;
 import static org.testng.Assert.*;
---
+++ graylog2-server/src/test/java/org/graylog2/outputs/OutputRouterTest.java
@@ -0,0 +1,157 @@
+package org.graylog2.outputs;
+
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.outputs.MessageOutput;
+import org.graylog2.plugin.streams.Output;
+import org.graylog2.plugin.streams.Stream;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.Mock;
+import static org.testng.Assert.*;
+
+/**
+ * @author Dennis Oelkers <dennis@torch.sh>
+ */
+@Test
+public class OutputRouterTest {
+    @Mock private MessageOutput defaultMessageOutput;
+    @Mock private OutputRegistry outputRegistry;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    public void testAlwaysIncludeDefaultOutput() throws Exception {
+        final Message message = mock(Message.class);
+        final OutputRouter outputRouter = new OutputRouter(defaultMessageOutput, outputRegistry);
+
+        final Collection<MessageOutput> messageOutputs = outputRouter.getOutputsForMessage(message);
+
+        assertEquals(messageOutputs.size(), 1);
+        assertTrue(messageOutputs.contains(defaultMessageOutput));
+    }
+
+    public void testGetMessageOutputsForEmptyStream() throws Exception {
+        final Stream stream = mock(Stream.class);
+        final OutputRouter outputRouter = new OutputRouter(defaultMessageOutput, outputRegistry);
+
+        final Collection<MessageOutput> messageOutputs = outputRouter.getMessageOutputsForStream(stream);
+
+        assertEquals(messageOutputs.size(), 0);
+    }
+
+    public void testGetMessageOutputsForSingleStream() throws Exception {
+        final Stream stream = mock(Stream.class);
+        final Output output = mock(Output.class);
+        final String outputId = "foobar";
+        final MessageOutput messageOutput = mock(MessageOutput.class);
+        final Set<Output> outputSet = new HashSet<Output>() {{ add(output); }};
+        when(stream.getOutputs()).thenReturn(outputSet);
+        when(output.getId()).thenReturn(outputId);
+        when(outputRegistry.getOutputForId(eq(outputId))).thenReturn(messageOutput);
+        final OutputRouter outputRouter = new OutputRouter(defaultMessageOutput, outputRegistry);
+
+        final Collection<MessageOutput> messageOutputs = outputRouter.getMessageOutputsForStream(stream);
+
+        assertEquals(messageOutputs.size(), 1);
+        assertTrue(messageOutputs.contains(messageOutput));
+    }
+
+    public void testGetMessageOutputsForStreamWithTwoOutputs() throws Exception {
+        final Stream stream = mock(Stream.class);
+        final Output output1 = mock(Output.class);
+        final Output output2 = mock(Output.class);
+        final String output1Id = "foo";
+        final String output2Id = "bar";
+        final MessageOutput messageOutput1 = mock(MessageOutput.class);
+        final MessageOutput messageOutput2 = mock(MessageOutput.class);
+        final Set<Output> outputSet = new HashSet<Output>() {{ add(output1); add(output2); }};
+        when(stream.getOutputs()).thenReturn(outputSet);
+        when(output1.getId()).thenReturn(output1Id);
+        when(output2.getId()).thenReturn(output2Id);
+        when(outputRegistry.getOutputForId(eq(output1Id))).thenReturn(messageOutput1);
+        when(outputRegistry.getOutputForId(eq(output2Id))).thenReturn(messageOutput2);
+        final OutputRouter outputRouter = new OutputRouter(defaultMessageOutput, outputRegistry);
+
+        final Collection<MessageOutput> messageOutputs = outputRouter.getMessageOutputsForStream(stream);
+
+        assertEquals(messageOutputs.size(), 2);
+        assertTrue(messageOutputs.contains(messageOutput1));
+        assertTrue(messageOutputs.contains(messageOutput2));
+    }
+
+    public void testGetOutputFromSingleStreams() throws Exception {
+        final Stream stream = mock(Stream.class);
+        List<Stream> streamList = new ArrayList<Stream>() {{
+            add(stream);
+        }};
+        final Message message = mock(Message.class);
+        when(message.getStreams()).thenReturn(streamList);
+
+        final MessageOutput messageOutput = mock(MessageOutput.class);
+        final Set<MessageOutput> messageOutputList = new HashSet<MessageOutput>() {{ add(messageOutput); }};
+
+        final OutputRouter outputRouter = Mockito.spy(new OutputRouter(defaultMessageOutput, outputRegistry));
+        doReturn(messageOutputList).when(outputRouter).getMessageOutputsForStream(eq(stream));
+
+        // Call to test
+        final Collection<MessageOutput> messageOutputs = outputRouter.getOutputsForMessage(message);
+
+        // Verification
+        assertEquals(messageOutputs.size(), 2);
+        assertTrue(messageOutputs.contains(defaultMessageOutput));
+        assertTrue(messageOutputs.contains(messageOutput));
+    }
+
+    public void testGetOutputsFromTwoStreams() throws Exception {
+        final Stream stream1 = mock(Stream.class);
+        final Stream stream2 = mock(Stream.class);
+        final MessageOutput messageOutput1 = mock(MessageOutput.class);
+        final Set<MessageOutput> messageOutputSet1 = new HashSet<MessageOutput>() {{ add(messageOutput1); }};
+        final MessageOutput messageOutput2 = mock(MessageOutput.class);
+        final Set<MessageOutput> messageOutputSet2 = new HashSet<MessageOutput>() {{ add(messageOutput2); }};
+        final Message message = mock(Message.class);
+        final List<Stream> streamList = new ArrayList<Stream>() {{ add(stream1); add(stream2); }};
+        when(message.getStreams()).thenReturn(streamList);
+
+        OutputRouter outputRouter = Mockito.spy(new OutputRouter(defaultMessageOutput, outputRegistry));
+        doReturn(messageOutputSet1).when(outputRouter).getMessageOutputsForStream(eq(stream1));
+        doReturn(messageOutputSet2).when(outputRouter).getMessageOutputsForStream(eq(stream2));
+
+        final Collection<MessageOutput> result = outputRouter.getOutputsForMessage(message);
+
+        assertEquals(result.size(), 3);
+        assertTrue(result.contains(defaultMessageOutput));
+        assertTrue(result.contains(messageOutput1));
+        assertTrue(result.contains(messageOutput2));
+    }
+
+    @Test
+    public void testGetOutputsWithIdenticalMessageOutputs() throws Exception {
+        final Stream stream1 = mock(Stream.class);
+        final Stream stream2 = mock(Stream.class);
+        final MessageOutput messageOutput = mock(MessageOutput.class);
+        final Set<MessageOutput> messageOutputSet = new HashSet<MessageOutput>() {{ add(messageOutput); }};
+        final Message message = mock(Message.class);
+        final List<Stream> streamList = new ArrayList<Stream>() {{ add(stream1); add(stream2); }};
+        when(message.getStreams()).thenReturn(streamList);
+
+        OutputRouter outputRouter = Mockito.spy(new OutputRouter(defaultMessageOutput, outputRegistry));
+        doReturn(messageOutputSet).when(outputRouter).getMessageOutputsForStream(eq(stream1));
+        doReturn(messageOutputSet).when(outputRouter).getMessageOutputsForStream(eq(stream2));
+
+        final Collection<MessageOutput> result = outputRouter.getOutputsForMessage(message);
+
+        assertEquals(result.size(), 2);
+        assertTrue(result.contains(defaultMessageOutput));
+        assertTrue(result.contains(messageOutput));
+    }
+}
