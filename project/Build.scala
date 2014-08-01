b2113fb616573f1e805eff657858364ed75181d7
Dennis Oelkers <dennis@torch.sh>
2014-08-01 16:20:16 +0200

    Bumping version number.

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ import com.typesafe.sbt.packager.universal.Keys.packageZipTarball
 object ApplicationBuild extends Build {
 
   val appName         = "graylog2-web-interface"
-  val appVersion      = "0.21.0-SNAPSHOT"
+  val appVersion      = "0.21.0-beta1"
 
 
   // use this to potentially add a timestamp to the built package name
