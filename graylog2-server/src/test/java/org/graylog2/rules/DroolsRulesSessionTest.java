780025adbea794355747b80b344cdb8a2a3cee9f
Jochen Schalanda <jochen@torch.sh>
2014-08-25 12:22:56 +0200

    Upgrade to TestNG 6.8.8

--- graylog2-server/src/test/java/org/graylog2/rules/DroolsRulesSessionTest.java
+++ graylog2-server/src/test/java/org/graylog2/rules/DroolsRulesSessionTest.java
@@ -25,11 +25,11 @@ import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static junit.framework.Assert.assertFalse;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class DroolsRulesSessionTest {
