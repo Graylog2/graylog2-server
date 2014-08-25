47a0dd7050cdfa5ef469d8679cdadb803052de87
Jochen Schalanda <jochen@torch.sh>
2014-08-25 10:07:06 +0200

    Clean up IndicesResource

--- graylog2-server/src/main/java/org/graylog2/rest/resources/system/indexer/IndicesResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/system/indexer/IndicesResource.java
@@ -17,9 +17,9 @@
 package org.graylog2.rest.resources.system.indexer;
 
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest;
@@ -27,16 +27,23 @@ import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
 import org.elasticsearch.action.admin.indices.close.CloseIndexRequest;
 import org.elasticsearch.action.admin.indices.open.OpenIndexRequest;
 import org.elasticsearch.action.admin.indices.settings.UpdateSettingsRequest;
-import org.elasticsearch.action.admin.indices.stats.*;
+import org.elasticsearch.action.admin.indices.stats.CommonStats;
+import org.elasticsearch.action.admin.indices.stats.IndexStats;
+import org.elasticsearch.action.admin.indices.stats.IndicesStatsRequest;
+import org.elasticsearch.action.admin.indices.stats.IndicesStatsResponse;
+import org.elasticsearch.action.admin.indices.stats.ShardStats;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.routing.ShardRouting;
-import org.elasticsearch.common.collect.UnmodifiableIterator;
 import org.graylog2.Configuration;
 import org.graylog2.indexer.Deflector;
 import org.graylog2.indexer.Indexer;
 import org.graylog2.indexer.ranges.RebuildIndexRangesJob;
-import org.graylog2.rest.documentation.annotations.*;
+import org.graylog2.rest.documentation.annotations.Api;
+import org.graylog2.rest.documentation.annotations.ApiOperation;
+import org.graylog2.rest.documentation.annotations.ApiParam;
+import org.graylog2.rest.documentation.annotations.ApiResponse;
+import org.graylog2.rest.documentation.annotations.ApiResponses;
 import org.graylog2.rest.resources.RestResource;
 import org.graylog2.security.RestPermissions;
 import org.graylog2.system.jobs.SystemJob;
@@ -46,23 +53,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 @RequiresAuthentication
-@Api(value = "Indexer/Indices", description = "Index informations")
+@Api(value = "Indexer/Indices", description = "Index information")
 @Path("/system/indexer/indices")
 public class IndicesResource extends RestResource {
-
     private static final Logger LOG = LoggerFactory.getLogger(IndicesResource.class);
 
     @Inject
@@ -76,63 +83,63 @@ public class IndicesResource extends RestResource {
     @Inject
     private Configuration configuration;
 
-    @GET @Timed
+    @GET
+    @Timed
     @Path("/{index}")
     @ApiOperation(value = "Get information of an index and its shards.")
     @Produces(MediaType.APPLICATION_JSON)
     public Response single(@ApiParam(title = "index") @PathParam("index") String index) {
         checkPermission(RestPermissions.INDICES_READ, index);
 
-        Map<String, Object> result = Maps.newHashMap();
+        final ImmutableMap.Builder<String, Object> result = ImmutableMap.builder();
 
-        IndexStats indexStats;
         try {
-            IndicesStatsResponse indicesStatsResponse = indexer.getClient().admin().indices().stats(new IndicesStatsRequest().all()).get();
-            indexStats = indicesStatsResponse.getIndex(index);
+            final IndicesStatsResponse indicesStatsResponse = indexer.getClient().admin().indices().stats(new IndicesStatsRequest().all()).get();
+            final IndexStats indexStats = indicesStatsResponse.getIndex(index);
 
             if (indexStats == null) {
-                LOG.error("Index [{}] not found.", index);
-                return Response.status(404).build();
+                LOG.warn("Index [{}] not found.", index);
+                return Response.status(Response.Status.NOT_FOUND).build();
             }
 
-            List<Map<String, Object>> routing = Lists.newArrayList();
+            final ImmutableList.Builder<Map<String, Object>> routing = ImmutableList.builder();
             for (ShardStats shardStats : indexStats.getShards()) {
                 routing.add(shardRouting(shardStats.getShardRouting()));
             }
 
             result.put("primary_shards", indexStats(indexStats.getPrimaries()));
             result.put("all_shards", indexStats(indexStats.getTotal()));
-            result.put("routing", routing);
+            result.put("routing", routing.build());
             result.put("is_reopened", indexer.indices().isReopened(index));
         } catch (Exception e) {
             LOG.error("Could not get indices information.", e);
-            return Response.status(500).build();
+            return Response.serverError().build();
         }
 
-        return Response.ok().entity(json(result)).build();
+        return Response.ok().entity(json(result.build())).build();
     }
 
-    @GET @Timed
+    @GET
+    @Timed
     @Path("/closed")
     @ApiOperation(value = "Get a list of closed indices that can be reopened.")
     @Produces(MediaType.APPLICATION_JSON)
     public Response closed() {
-        Map<String, Object> result = Maps.newHashMap();
+        final ImmutableSet.Builder<String> closedIndicesBuilder = ImmutableSet.builder();
 
-        Set<String> closedIndices = Sets.newHashSet();
         try {
             // Get a list of all indices and select those that are closed. This is only possible via metadata.
-            ClusterStateRequest csr = new ClusterStateRequest()
+            final ClusterStateRequest csr = new ClusterStateRequest()
                     .filterNodes(true)
                     .filterRoutingTable(true)
                     .filterBlocks(true)
                     .filterMetaData(false);
-            ClusterState state = indexer.getClient().admin().cluster().state(csr).actionGet().getState();
+            final ClusterState state = indexer.getClient().admin().cluster().state(csr).actionGet().getState();
 
-            UnmodifiableIterator<IndexMetaData> it = state.getMetaData().getIndices().valuesIt();
+            final Iterator<IndexMetaData> it = state.getMetaData().getIndices().valuesIt();
+            while (it.hasNext()) {
+                final IndexMetaData indexMeta = it.next();
 
-            while(it.hasNext()) {
-                IndexMetaData indexMeta = it.next();
                 // Only search in our indices.
                 if (!indexMeta.getIndex().startsWith(configuration.getElasticSearchIndexPrefix())) {
                     continue;
@@ -140,22 +147,25 @@ public class IndicesResource extends RestResource {
                 if (!isPermitted(RestPermissions.INDICES_READ, indexMeta.getIndex())) {
                     continue;
                 }
-                if(indexMeta.getState().equals(IndexMetaData.State.CLOSE)) {
-                    closedIndices.add(indexMeta.getIndex());
+                if (indexMeta.getState().equals(IndexMetaData.State.CLOSE)) {
+                    closedIndicesBuilder.add(indexMeta.getIndex());
                 }
             }
         } catch (Exception e) {
             LOG.error("Could not get closed indices.", e);
-            return Response.status(500).build();
+            return Response.serverError().build();
         }
 
-        result.put("indices", closedIndices);
-        result.put("total", closedIndices.size());
+        final Set<String> closedIndices = closedIndicesBuilder.build();
+        final Map<String, Object> result = ImmutableMap.of(
+                "indices", closedIndices,
+                "total", closedIndices.size());
 
         return Response.ok().entity(json(result)).build();
     }
 
-    @POST @Timed
+    @POST
+    @Timed
     @Path("/{index}/reopen")
     @ApiOperation(value = "Reopen a closed index. This will also trigger an index ranges rebuild job.")
     @Produces(MediaType.APPLICATION_JSON)
@@ -163,28 +173,27 @@ public class IndicesResource extends RestResource {
         checkPermission(RestPermissions.INDICES_CHANGESTATE, index);
 
         // Mark this index as re-opened. It will never be touched by retention.
-        UpdateSettingsRequest settings = new UpdateSettingsRequest(index);
-        settings.settings(new HashMap() {{
-            put("graylog2_reopened", true);
-        }});
+        final UpdateSettingsRequest settings = new UpdateSettingsRequest(index);
+        settings.settings(ImmutableMap.of("graylog2_reopened", true));
         indexer.getClient().admin().indices().updateSettings(settings).actionGet();
 
         // Open index.
         indexer.getClient().admin().indices().open(new OpenIndexRequest(index)).actionGet();
 
         // Trigger index ranges rebuild job.
-        SystemJob rebuildJob = rebuildIndexRangesJobFactory.create(deflector);
+        final SystemJob rebuildJob = rebuildIndexRangesJobFactory.create(deflector);
         try {
             systemJobManager.submit(rebuildJob);
         } catch (SystemJobConcurrencyException e) {
             LOG.error("Concurrency level of this job reached: " + e.getMessage());
-            throw new WebApplicationException(403);
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
         }
 
         return Response.noContent().build();
     }
 
-    @POST @Timed
+    @POST
+    @Timed
     @Path("/{index}/close")
     @ApiOperation(value = "Close an index. This will also trigger an index ranges rebuild job.")
     @Produces(MediaType.APPLICATION_JSON)
@@ -195,25 +204,26 @@ public class IndicesResource extends RestResource {
         checkPermission(RestPermissions.INDICES_CHANGESTATE, index);
 
         if (deflector.getCurrentActualTargetIndex(indexer).equals(index)) {
-            return Response.status(403).build();
+            return Response.status(Response.Status.FORBIDDEN).build();
         }
 
         // Close index.
         indexer.getClient().admin().indices().close(new CloseIndexRequest(index)).actionGet();
 
         // Trigger index ranges rebuild job.
-        SystemJob rebuildJob = rebuildIndexRangesJobFactory.create(deflector);
+        final SystemJob rebuildJob = rebuildIndexRangesJobFactory.create(deflector);
         try {
             systemJobManager.submit(rebuildJob);
         } catch (SystemJobConcurrencyException e) {
             LOG.error("Concurrency level of this job reached: " + e.getMessage());
-            throw new WebApplicationException(403);
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
         }
 
         return Response.noContent().build();
     }
 
-    @DELETE @Timed
+    @DELETE
+    @Timed
     @Path("/{index}")
     @ApiOperation(value = "Delete an index. This will also trigger an index ranges rebuild job.")
     @Produces(MediaType.APPLICATION_JSON)
@@ -224,26 +234,26 @@ public class IndicesResource extends RestResource {
         checkPermission(RestPermissions.INDICES_DELETE, index);
 
         if (deflector.getCurrentActualTargetIndex(indexer).equals(index)) {
-            return Response.status(403).build();
+            return Response.status(Response.Status.FORBIDDEN).build();
         }
 
         // Delete index.
         indexer.indices().delete(index);
 
         // Trigger index ranges rebuild job.
-        SystemJob rebuildJob = rebuildIndexRangesJobFactory.create(deflector);
+        final SystemJob rebuildJob = rebuildIndexRangesJobFactory.create(deflector);
         try {
             systemJobManager.submit(rebuildJob);
         } catch (SystemJobConcurrencyException e) {
             LOG.error("Concurrency level of this job reached: " + e.getMessage());
-            throw new WebApplicationException(403);
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
         }
 
         return Response.noContent().build();
     }
 
     private Map<String, Object> shardRouting(ShardRouting route) {
-        Map<String, Object> result = Maps.newHashMap();
+        final ImmutableMap.Builder<String, Object> result = ImmutableMap.builder();
 
         result.put("id", route.shardId().getId());
         result.put("state", route.state().name().toLowerCase());
@@ -254,61 +264,61 @@ public class IndicesResource extends RestResource {
         result.put("node_hostname", translateESNodeIdToHostname(route.currentNodeId()));
         result.put("relocating_to", route.relocatingNodeId());
 
-        return result;
+        return result.build();
     }
 
     private Map<String, Object> indexStats(final CommonStats stats) {
-        Map<String, Object> result = Maps.newHashMap();
+        final ImmutableMap.Builder<String, Object> result = ImmutableMap.builder();
 
-        result.put("flush", new HashMap<String, Object>() {{
-            put("total", stats.getFlush().getTotal());
-            put("time_seconds", stats.getFlush().getTotalTime().getSeconds());
-        }});
+        result.put("flush", ImmutableMap.<String, Object>of(
+                "total", stats.getFlush().getTotal(),
+                "time_seconds", stats.getFlush().getTotalTime().getSeconds()
+        ));
 
-        result.put("get", new HashMap<String, Object>() {{
-            put("total", stats.getGet().getCount());
-            put("time_seconds", stats.getGet().getTime().getSeconds());
-        }});
+        result.put("get", ImmutableMap.<String, Object>of(
+                "total", stats.getGet().getCount(),
+                "time_seconds", stats.getGet().getTime().getSeconds()
+        ));
 
-        result.put("index", new HashMap<String, Object>() {{
-            put("total", stats.getIndexing().getTotal().getIndexCount());
-            put("time_seconds", stats.getIndexing().getTotal().getIndexTime().getSeconds());
-        }});
+        result.put("index", ImmutableMap.<String, Object>of(
+                "total", stats.getIndexing().getTotal().getIndexCount(),
+                "time_seconds", stats.getIndexing().getTotal().getIndexTime().getSeconds()
+        ));
 
-        result.put("merge", new HashMap<String, Object>() {{
-            put("total", stats.getMerge().getTotal());
-            put("time_seconds", stats.getMerge().getTotalTime().getSeconds());
-        }});
+        result.put("merge", ImmutableMap.<String, Object>of(
+                "total", stats.getMerge().getTotal(),
+                "time_seconds", stats.getMerge().getTotalTime().getSeconds()
+        ));
 
-        result.put("refresh", new HashMap<String, Object>() {{
-            put("total", stats.getRefresh().getTotal());
-            put("time_seconds", stats.getRefresh().getTotalTime().getSeconds());
-        }});
+        result.put("refresh", ImmutableMap.<String, Object>of(
+                "total", stats.getRefresh().getTotal(),
+                "time_seconds", stats.getRefresh().getTotalTime().getSeconds()
+        ));
 
-        result.put("search_query", new HashMap<String, Object>() {{
-            put("total", stats.getSearch().getTotal().getQueryCount());
-            put("time_seconds", stats.getSearch().getTotal().getQueryTime().getSeconds());
-        }});
+        result.put("search_query", ImmutableMap.<String, Object>of(
+                "total", stats.getSearch().getTotal().getQueryCount(),
+                "time_seconds", stats.getSearch().getTotal().getQueryTime().getSeconds()
+        ));
 
-        result.put("search_fetch", new HashMap<String, Object>() {{
-            put("total", stats.getSearch().getTotal().getFetchCount());
-            put("time_seconds", stats.getSearch().getTotal().getFetchTime().getSeconds());
-        }});
+        result.put("search_fetch", ImmutableMap.<String, Object>of(
+                "total", stats.getSearch().getTotal().getFetchCount(),
+                "time_seconds", stats.getSearch().getTotal().getFetchTime().getSeconds()
+        ));
 
         result.put("open_search_contexts", stats.getSearch().getOpenContexts());
         result.put("store_size_bytes", stats.getStore().getSize().getBytes());
         result.put("segments", stats.getSegments().getCount());
 
-        result.put("documents", new HashMap<String, Object>() {{
-            put("count", stats.getDocs().getCount());
-            put("deleted", stats.getDocs().getDeleted());
-        }});
+        result.put("documents", ImmutableMap.<String, Object>of(
+                "count", stats.getDocs().getCount(),
+                "deleted", stats.getDocs().getDeleted()
+        ));
 
-        return result;
+        return result.build();
     }
 
     private String translateESNodeIdToName(String id) {
-        NodeInfo[] result = indexer.getClient().admin().cluster().nodesInfo(new NodesInfoRequest(id)).actionGet().getNodes();
+        final NodeInfo[] result = indexer.getClient().admin().cluster().nodesInfo(new NodesInfoRequest(id)).actionGet().getNodes();
         if (result == null || result.length == 0) {
             return "unknown";
         }
@@ -317,12 +327,11 @@ public class IndicesResource extends RestResource {
     }
 
     private String translateESNodeIdToHostname(String id) {
-        NodeInfo[] result = indexer.getClient().admin().cluster().nodesInfo(new NodesInfoRequest(id)).actionGet().getNodes();
+        final NodeInfo[] result = indexer.getClient().admin().cluster().nodesInfo(new NodesInfoRequest(id)).actionGet().getNodes();
         if (result == null || result.length == 0) {
             return "unknown";
         }
 
         return result[0].getHostname();
     }
-
 }
