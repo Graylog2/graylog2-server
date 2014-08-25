5d10bc53ee6714c611dbef6b3c242247ef0192f6
Dennis Oelkers <dennis@torch.sh>
2014-08-25 17:50:51 +0200

    Bumping version to next snapshot.

--- src/main/java/org/graylog2/restclient/lib/Version.java
+++ src/main/java/org/graylog2/restclient/lib/Version.java
@@ -62,8 +62,9 @@ public class Version {
     public static final Version v0_21_0_BETA3 = new Version(0, 21, 0, "beta3");
     public static final Version v0_21_0_BETA4_SNAPSHOT = new Version(0, 21, 0, "beta4-snapshot");
     public static final Version v0_21_0_BETA4= new Version(0, 21, 0, "beta4");
+    public static final Version v0_21_0_BETA5_SNAPSHOT = new Version(0, 21, 0, "beta5-snapshot");
 
-    public static final Version VERSION = v0_21_0_BETA4;
+    public static final Version VERSION = v0_21_0_BETA5_SNAPSHOT;
 
     public final int major;
     public final int minor;
