f1f7cbd4d2e7d3214857ae14026f19ee365aa572
Jochen Schalanda <jochen@torch.sh>
2014-09-01 10:23:43 +0200

    Remove obsolete local copy of syslog4j

--- install-syslog4j-jar.sh
+++
@@ -1,2 +0,0 @@
-echo "Installing our provided syslog4j fork .jar to local mvn repository for great justice!"
-mvn install:install-file -DgroupId=org.productivity.java -DartifactId=syslog4j-graylog2 -Dversion=0.9.48-graylog2 -Dpackaging=jar -Dfile=lib/syslog4j-graylog2-0.9.48-graylog2.jar
