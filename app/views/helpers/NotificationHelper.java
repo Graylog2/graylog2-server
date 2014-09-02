533f224863f00cac7d943cd293afa0dcec8856ce
Jochen Schalanda <jochen@torch.sh>
2014-09-02 12:49:51 +0200

    Updated even more documentation links

--- app/views/helpers/NotificationHelper.java
+++ app/views/helpers/NotificationHelper.java
@@ -1,11 +1,8 @@
 package views.helpers;
 
-/**
- * @author Dennis Oelkers <dennis@torch.sh>
- */
 public class NotificationHelper {
     public static String linkToKnowledgeBase(String article, String body) {
-        StringBuilder url = new StringBuilder("http://support.torch.sh/help/kb/graylog2-server/");
+        StringBuilder url = new StringBuilder("http://www.graylog2.org/resources/documentation/");
         url.append(article);
 
         return "<a href='" + url + "' target='_blank'>" + body + "</a>";
