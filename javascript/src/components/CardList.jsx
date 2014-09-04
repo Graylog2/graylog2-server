76994caff2e2691a371a8e166a0f3585c53cb616
Edmundo Alvarez <edmundo@torch.sh>
2014-09-04 17:51:14 +0200

    Add description to quick start card

--- javascript/src/components/CardList.jsx
+++ javascript/src/components/CardList.jsx
@@ -8,11 +8,17 @@ var BootstrapAccordion = require('./BootstrapAccordion');
 
 var CardList = React.createClass({
     render: function () {
+        var quickStartDescription = <p>New to Graylog2? Select one of the preconfigured setups to get you started:</p>;
         var contentInfo = <p>Need help with the search syntax? Take a look at our <a href="http://www.graylog2.org/resources/documentation/general/queries" target="_blank">documentation</a>.</p>;
         return (
             <div className="card-list">
                 <Card title="Quick Start">
-                    <BootstrapAccordion/>
+                    {quickStartDescription}
+                    <div className="row">
+                        <div className="span6">
+                            <BootstrapAccordion/>
+                        </div>
+                    </div>
                 </Card>
                 <Card type="info">
                     {contentInfo}
