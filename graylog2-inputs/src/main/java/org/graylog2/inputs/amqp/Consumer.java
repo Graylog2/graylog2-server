b1c4d01ef08af89d73b1867d8d06c7072ecb4b3b
Jochen Schalanda <jochen@torch.sh>
2014-09-05 14:27:01 +0200

    Revert "Guard MessagePack with a ThreadLocal where necessary"
    This reverts commit d4405a9cce22742a7053b3b5cb45a260a0efb93f.
    
    Unfortunately the ThreadLocal did not solve the problem. As it turns out
    the reason that our usage failed is the quite rapid creation of MessagePack
    instances in the first place and the reuse of memory after GC collection of
    those instances which produce the same hash code as per Object#hashCode();
    also see http://bugs.java.com/bugdatabase/view_bug.do?bug_id=6321873

--- graylog2-inputs/src/main/java/org/graylog2/inputs/amqp/Consumer.java
+++ graylog2-inputs/src/main/java/org/graylog2/inputs/amqp/Consumer.java
@@ -36,15 +36,12 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * @author Lennart Koopmann <lennart@torch.sh>
+ */
 public class Consumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(Consumer.class);
-    private static final ThreadLocal<MessagePack> MSGPACK = new ThreadLocal<MessagePack>() {
-        @Override
-        protected MessagePack initialValue() {
-            return new MessagePack();
-        }
-    };
 
     // Not threadsafe!
 
@@ -102,6 +99,8 @@ public class Consumer {
             connect();
         }
 
+        final MessagePack msgpack = new MessagePack();
+
         for (int i = 0; i < parallelQueues; i++) {
             final String queueName = String.format(queue, i);
             channel.queueDeclare(queueName, true, false, false, null);
@@ -114,7 +113,7 @@ public class Consumer {
                             totalBytesRead.addAndGet(body.length);
                             lastSecBytesReadTmp.addAndGet(body.length);
 
-                            final RadioMessage msg = MSGPACK.get().read(body, RadioMessage.class);
+                            final RadioMessage msg = msgpack.read(body, RadioMessage.class);
 
                             if (!msg.strings.containsKey("message") || !msg.strings.containsKey("source") || msg.timestamp <= 0) {
                                 LOG.error("Incomplete AMQP message. Skipping.");
