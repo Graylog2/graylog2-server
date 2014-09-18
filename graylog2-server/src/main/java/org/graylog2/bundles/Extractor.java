738b9b5c2286c83e2514620895dfd8bbc928d3ce
Jochen Schalanda <jochen@torch.sh>
2014-09-18 16:05:42 +0200

    Remove unused bundle.Extractor#regexConditionPattern
    The field `regexConditionPattern` is actually only used in case of an RegexExtractor
    and is derived from `conditionValue` directly (precomiled Pattern instance).

--- graylog2-server/src/main/java/org/graylog2/bundles/Extractor.java
+++ graylog2-server/src/main/java/org/graylog2/bundles/Extractor.java
@@ -43,8 +43,6 @@ public class Extractor {
     private String conditionValue;
     @JsonProperty
     private int order;
-    @JsonProperty
-    private String regexConditionPattern;
 
     public String getTitle() {
         return title;
@@ -124,13 +122,5 @@ public class Extractor {
 
     public void setOrder(int order) {
         this.order = order;
-    }
-
-    public String getRegexConditionPattern() {
-        return regexConditionPattern;
-    }
-
-    public void setRegexConditionPattern(String regexConditionPattern) {
-        this.regexConditionPattern = regexConditionPattern;
     }
 }
