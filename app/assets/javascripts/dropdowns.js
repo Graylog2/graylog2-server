01092475948fb43584c253277211d823c171dc2a
Edmundo Alvarez <edmundo@torch.sh>
2014-09-20 01:20:25 +0200

    Draw dropdowns in the available space
    Avoiding to overflow the parent container. Related to #906

--- app/assets/javascripts/dropdowns.js
+++ app/assets/javascripts/dropdowns.js
@@ -14,6 +14,26 @@ $(document).ready(function() {
         }
     }
 
+    /*
+     * Check if a dropdown overflows a scrollable container.
+     * container: div with overflow:hidden.
+     * element: element to be drawn.
+     * coordinate: ("top"|"bottom") specifies if it should check overflow at the top or bottom of the container.
+     */
+    function elementOverflowParentHeight(container, element, coordinate) {
+        if (coordinate == "top") {
+            var containerTop = container.offset().top;
+            var elementTop = element.offset().top;
+            return elementTop < containerTop;
+        } else if (coordinate == "bottom") {
+            var containerBottom = container.offset().top + container.outerHeight();
+            var elementBottom = element.offset().top + element.outerHeight();
+            return elementBottom > containerBottom;
+        } else {
+            throw new Error("Undefined coordinate '" + coordinate + "', must be 'top' or 'bottom'");
+        }
+    }
+
     $(".dropdown-toggle").on("click", function() {
         var menu = $(this).siblings("ul.dropdown-menu").first();
 
@@ -39,6 +59,38 @@ $(document).ready(function() {
         } else {
             if ($(this).hasClass("left-submenu")) {
                 $(this).removeClass("left-submenu");
+            }
+        }
+    });
+
+    $(document).on("click", ".message-field-dropdown .key", function() {
+        var dropdownContainer = $(this).parent();
+        var menu = $(this).siblings("ul.dropdown-menu").first();
+        var container = $("#sidebar-replacement .nano .content");
+
+        dropdownContainer.removeClass("dropup");
+
+        if (elementOverflowParentHeight(container, menu, "bottom")) {
+            dropdownContainer.addClass("dropup");
+        }
+    });
+
+    $(document).on("hover", ".message-field-dropdown .dropdown-submenu", function() {
+        var menu = $(this).children("ul.dropdown-menu").first();
+        var container = $("#sidebar-replacement .nano .content");
+
+        // By default, let submenu go down
+        $(this).removeClass("up-submenu");
+        $(this).addClass("down-submenu");
+
+        if (elementOverflowParentHeight(container, menu, "bottom")) {
+            // Move submenu up
+            $(this).removeClass("down-submenu");
+            $(this).addClass("up-submenu");
+
+            if (elementOverflowParentHeight(container, menu, "top")) {
+                $(this).removeClass("up-submenu")
+                $(this).addClass("down-submenu");
             }
         }
     });
