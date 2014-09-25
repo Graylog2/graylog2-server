29017978edccad3173162b7dcbcff6044187cbc6
olli <oliver.zeigermann@gmail.com>
2014-09-25 16:44:31 +0200

    Updated gulp based build process

---
+++ javascript/build.properties.js
@@ -0,0 +1,13 @@
+module.exports = {
+    deployDir: '../app/views/system/users/',
+    deployDirJs: '../app/assets/javascripts/users',
+    targetDir: null,
+    targetDirJs: null,
+
+    assetDir: 'javascripts/users',
+    scriptTemplatePath: 'scripts.hbs',
+    scriptDestinationName: 'scripts.scala.html',
+    entryPoints: ['./src/mount.jsx']
+};
+
+
--- javascript/gulpfile.js
+++ javascript/gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var browserify = require('browserify');
 var reactify = require('reactify');
+var watchify = require('watchify');
 var react = require('gulp-react');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
@@ -15,74 +16,123 @@ var handlebars = require('gulp-compile-handlebars');
 var runSequence = require('run-sequence');
 var jshint = require('gulp-jshint');
 var source = require('vinyl-source-stream');
-
-var deployDir = '../app/views/system/users/';
-var deployDirJs = '../app/assets/javascripts/users';
+var jest = require('gulp-jest');
+var debug = require('gulp-debug');
+var livereload = require('gulp-livereload');
+var config = require('./build.properties');
 
 var handlebarOpts = {
     helpers: {
         assetPath: function (path, context) {
-            return ['javascripts/users', context.data.root[path]].join('/');
+            return [config.assetDir, context.data.root[path]].join('/');
         }
     }
 };
 
-gulp.task('replace-rev', function () {
-    var manifest = JSON.parse(fs.readFileSync('dist/rev-manifest.json', 'utf8'));
+var reactOpts = {
+    harmony: true,
+    stripTypes: true
+};
 
-    return gulp.src('scripts.hbs')
-        .pipe(handlebars(manifest, handlebarOpts))
-        .pipe(rename('scripts.scala.html'))
-        .pipe(gulp.dest(deployDir));
+gulp.task('test', function () {
+    return gulp.src('src', {read: false})
+        //.pipe(debug({verbose: false}))
+        .pipe(jest({
+            scriptPreprocessor: "<rootDir>/jest-preprocessor.js",
+            "rootDir": ".",
+            unmockedModulePathPatterns: [
+                "<rootDir>/node_modules/react/",
+                "<rootDir>/node_modules/react-bootstrap/",
+                "<rootDir>/node_modules/URIjs/",
+                "<rootDir>/node_modules/joi/"
+            ],
+            testPathIgnorePatterns: [
+                "node_modules"
+            ],
+            moduleFileExtensions: [
+                "js",
+                "jsx"
+            ]
+        }));
 });
 
-gulp.task('clean', function() {
+var replaceRev = function (debug, noSync) {
+    var manifest;
+    if (!debug) {
+        manifest = JSON.parse(fs.readFileSync('dist/rev-manifest.json', 'utf8'));
+    } else {
+        manifest = {
+            "app.js":"app.js",
+            livereload: !noSync && '<script src="//localhost:35729/livereload.js"></script>'
+        };
+    }
+
+    return gulp.src(config.scriptTemplatePath)
+        .pipe(handlebars(manifest, handlebarOpts))
+        .pipe(rename(config.scriptDestinationName))
+        .pipe(config.targetDir ? gulp.dest(config.targetDir) : gutil.noop())
+        .pipe(gulp.dest(config.deployDir));
+};
+gulp.task('replace-rev-dev', function() {
+    return replaceRev(true);
+});
+gulp.task('replace-rev-dev-no-sync', function() {
+    return replaceRev(true, true);
+});
+gulp.task('replace-rev-prod', function() {
+    return replaceRev(false);
+});
+
+gulp.task('clean', function () {
     return gulp.src('dist/*')
         .pipe(rm());
 });
 
-gulp.task('clean-target', function(callback) {
-    rimraf(deployDirJs, callback);
+gulp.task('clean-target', function (callback) {
+    rimraf(config.deployDirJs, callback);
 });
 
-gulp.task('prepare-lint', ['clean'], function() {
+gulp.task('prepare-lint', ['clean'], function () {
     return gulp.src('src/**/*.jsx')
-        .pipe(react({
-            harmony: true
-        }))
-        .on('error', function(err) {
+        .pipe(react(reactOpts))
+        .on('error', function (err) {
             gutil.log(err);
             this.emit('end');
         })
         .pipe(gulp.dest('dist/jsx'));
 });
 
-gulp.task('lint', ['prepare-lint'], function() {
-    return gulp.src(['src/**/*.js', 'dist/jsx/**/*.js'])
+var lintSrc = ['src/**/*.js', 'dist/jsx/**/*.js', '!src/**/__tests__/**'];
+
+gulp.task('lint', ['prepare-lint'], function () {
+    return gulp.src(lintSrc)
         .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
 });
 
-gulp.task('lint-strict', ['prepare-lint'], function() {
-    return gulp.src(['src/**/*.js', 'dist/jsx/**/*.js'])
+gulp.task('lint-strict', ['prepare-lint'], function () {
+    return gulp.src(lintSrc)
         .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
         .pipe(jshint.reporter('fail'));
 });
 
-function browserifyIt(debug) {
+function browserifyCall(debug) {
     return browserify({
-        entries: ['./src/mount.jsx'],
+        cache: {}, packageCache: {}, fullPaths: true,
+        entries: config.entryPoints,
         extensions: ['.jsx', '.js'],
         "transform": [
-            ["reactify", {
-                harmony: true
-            }]
+            ["reactify", reactOpts]
         ],
         debug: debug
-    })
+    });
+}
+
+function browserifyIt(debug) {
+    return browserifyCall(debug)
         .bundle()
-        .on('error', function(err) {
+        .on('error', function (err) {
             gutil.log(err);
             this.emit('end');
         })
@@ -90,21 +140,17 @@ function browserifyIt(debug) {
         .pipe(gulp.dest('./dist/'));
 }
 
-gulp.task('debug-js', function () {
-    return browserifyIt(true);
-});
-
-gulp.task('prod-js', function() {
+gulp.task('prod-js', function () {
     return browserifyIt(false);
 });
 
-gulp.task('compress', function() {
+gulp.task('compress', function () {
     return gulp.src('./dist/app.js')
         .pipe(uglify())
         .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('rev', function() {
+gulp.task('rev', function () {
     return gulp.src('./dist/app.js')
         .pipe(rev())
         .pipe(gulp.dest('./dist/'))
@@ -112,34 +158,42 @@ gulp.task('rev', function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('rev-dummy', function() {
-    fs.writeFileSync('dist/rev-manifest.json', JSON.stringify({"app.js": "app.js"}));
-});
-
-gulp.task('deploy-rev', function() {
+gulp.task('deploy-rev', function () {
     return gulp.src(['./dist/app-*.js'])
-        .pipe(gulp.dest(deployDirJs));
+        .pipe(config.targetDirJs ? gulp.dest(config.targetDirJs) : gutil.noop())
+        .pipe(gulp.dest(config.deployDirJs));
 });
 
-gulp.task('deploy', function() {
-    return gulp.src(['./dist/app.js'])
-        .pipe(gulp.dest(deployDirJs));
+gulp.task('deploy-prod', function (callback) {
+    runSequence('test', 'lint-strict', 'clean-target', 'clean', 'prod-js', 'compress', 'rev', 'replace-rev-prod', 'deploy-rev', callback);
 });
 
-gulp.task('deploy-prod', function(callback) {
-    runSequence('lint-strict', 'clean-target', 'clean', 'prod-js', 'compress', 'rev', 'replace-rev', 'deploy-rev', callback);
+gulp.task('prepare-dev', function (callback) {
+    runSequence('lint', 'clean-target', 'clean', 'replace-rev-dev', callback);
 });
 
-gulp.task('deploy-dev', function(callback) {
-    runSequence('lint', 'clean-target', 'clean', 'debug-js', 'rev-dummy', 'replace-rev', 'deploy', callback);
-});
-
-gulp.task('build-test', function(callback) {
+gulp.task('build-test', function (callback) {
     runSequence('clean', 'debug-js', callback);
 });
 
-gulp.task('watch', ['deploy-dev'], function() {
-    gulp.watch('./src/**/*.*', ['deploy-dev']);
-});
+gulp.task('default', ['deploy-prod']);
 
-gulp.task('default', ['watch']);
+gulp.task('watch', function () {
+    var bundler = watchify(browserifyCall(true));
+
+    bundler.on('update', rebundle);
+
+    function rebundle() {
+        gutil.log("Rebundling");
+        return bundler.bundle()
+            // log errors if they happen
+            .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+            .pipe(source('app.js'))
+            .pipe(gulp.dest('./dist'))
+            .pipe(config.targetDirJs ? gulp.dest(config.targetDirJs) : gutil.noop())
+            .pipe(gulp.dest(config.deployDirJs))
+            .pipe(livereload());
+    }
+
+    return rebundle();
+});
