8c5adeb3f337baabad47446cf255fe77d9e5c4f0
olli <oliver.zeigermann@gmail.com>
2014-09-26 11:58:07 +0200

    Fixed react import case (failed on case sensitive file systems)

--- javascript/src/mount.jsx
+++ javascript/src/mount.jsx
@@ -1,6 +1,6 @@
 'use strict';
 
-var React = require('React');
+var React = require('react/addons');
 var UserPreferencesButton = require('./components/UserPreferencesButton');
 var UserPreferencesModal = require('./components/UserPreferencesModal');
 
