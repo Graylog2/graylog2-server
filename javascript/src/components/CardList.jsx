ea1ddde84602b6ef53ec69dc6af269fb820dbcba
Edmundo Alvarez <edmundo@torch.sh>
2014-10-01 18:07:20 +0200

    Add getting started link back to the start page

--- javascript/src/components/CardList.jsx
+++ javascript/src/components/CardList.jsx
@@ -8,12 +8,14 @@ var InformationCard = require('./InformationCard');
 
 var CardList = React.createClass({
     render: function () {
-        var contentInfo = <p>Need help with the search syntax? Take a look at our <a href="http://www.graylog2.org/resources/documentation/general/queries" target="_blank">documentation</a>.</p>;
         return (
             <div className="card-list">
                 <QuickStartCard />
                 <InformationCard>
-                    {contentInfo}
+                    <p>Use our <a href="http://graylog2.org/getting-started" target="_blank">getting started</a> guide to take your first steps with Graylog2.</p>
+                </InformationCard>
+                <InformationCard>
+                    <p>Need help with the search syntax? Take a look at our <a href="http://www.graylog2.org/resources/documentation/general/queries" target="_blank">documentation</a>.</p>
                 </InformationCard>
             </div>
         );
