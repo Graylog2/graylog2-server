9b54f2235f0ff0ae9b002cb15ffa867d2e48be58
Dennis Oelkers <dennis@torch.sh>
2014-10-17 09:50:51 +0200

    Stopping message processing after max radio transport errors.

--- graylog2-radio/src/main/java/org/graylog2/radio/buffers/processors/RadioProcessBufferProcessor.java
+++ graylog2-radio/src/main/java/org/graylog2/radio/buffers/processors/RadioProcessBufferProcessor.java
@@ -81,11 +81,12 @@ public class RadioProcessBufferProcessor extends ProcessBufferProcessor {
         } catch (Exception e) {
             int errors = errorCount.addAndGet(1);
             if (radioTransportMaxErrors > 0 && errors >= radioTransportMaxErrors) {
+                serverStatus.pauseMessageProcessing();
                 serverStatus.overrideLoadBalancerDead();
                 LOG.error("Number of Radio transport errors exceeded threshold ({}), switching to lb:dead.", radioTransportMaxErrors);
             }
             erroredMessages.mark();
-            LOG.error("Caught exception while sending message to Radio transport: ", e);
+            LOG.error("[Error #{}] Caught exception while sending message to Radio transport: ", errors, e);
         }
     }
 }
