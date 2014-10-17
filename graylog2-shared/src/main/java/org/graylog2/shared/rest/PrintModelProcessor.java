b21df9c1e95f8e71605eada681e81e40eb810ca6
Jochen Schalanda <jochen@torch.sh>
2014-10-17 11:34:09 +0200

    Improve readability and completeness of PrintModelProcessor output
    The previous implementation of PrintModelProcessor didn't output all registered
    resources from the Jersey ResourceModel. The output is now complete, sorted and
    printed through the logging subsystem instead of being dumped to stdout.

--- graylog2-shared/src/main/java/org/graylog2/shared/rest/PrintModelProcessor.java
+++ graylog2-shared/src/main/java/org/graylog2/shared/rest/PrintModelProcessor.java
@@ -21,43 +21,99 @@ import org.glassfish.jersey.server.model.ModelProcessor;
 import org.glassfish.jersey.server.model.Resource;
 import org.glassfish.jersey.server.model.ResourceMethod;
 import org.glassfish.jersey.server.model.ResourceModel;
-import org.glassfish.jersey.uri.PathPattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Configuration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 public class PrintModelProcessor implements ModelProcessor {
+    private static final Logger LOG = LoggerFactory.getLogger("REST API");
+
     @Override
     public ResourceModel processResourceModel(ResourceModel resourceModel, Configuration configuration) {
-        System.out.println("Map for resource model <" + resourceModel + ">:");
+        LOG.debug("Map for resource model <" + resourceModel + ">:");
+        final List<Resource> resources = new ArrayList<>();
+
         for (Resource resource : resourceModel.getResources()) {
-            for (ResourceMethod resourceMethod : resource.getAllMethods()) {
-                System.out.println(formatEndpoint(
-                        resourceMethod.getHttpMethod(),
-                        resource.getPathPattern(),
-                        resource.getHandlerClasses()));
-            }
+            resources.add(resource);
+            resources.addAll(findChildResources(resource));
         }
+
+        logResources(resources);
 
         return resourceModel;
     }
 
     @Override
     public ResourceModel processSubResource(ResourceModel subResourceModel, Configuration configuration) {
-        System.out.println("Map for sub-resource model <" + subResourceModel + ">:");
-        for (Resource resource : subResourceModel.getResources()) {
-            for (ResourceMethod resourceMethod : resource.getAllMethods()) {
-                System.out.println(formatEndpoint(
-                        resourceMethod.getHttpMethod(),
-                        resource.getPathPattern(),
-                        resource.getHandlerClasses()));
-            }
-        }
+        LOG.debug("Map for sub-resource model <" + subResourceModel + ">:");
+        logResources(subResourceModel.getResources());
 
         return subResourceModel;
     }
 
-    private String formatEndpoint(final String method, final PathPattern pathPattern, Iterable<Class<?>> handlerClasses) {
-        return String.format("    %-7s %s (%s)", method, pathPattern, Joiner.on(", ").join(handlerClasses));
+    private void logResources(List<Resource> resources) {
+        final List<ResourceDescription> resourceDescriptions = new ArrayList<>();
+        for (Resource resource : resources) {
+            for (ResourceMethod resourceMethod : resource.getAllMethods()) {
+                String path = resource.getPath();
+                Resource parent = resource.getParent();
+                while (parent != null) {
+                    if (!path.startsWith("/")) {
+                        path = "/" + path;
+                    }
+
+                    path = parent.getPath() + path;
+                    parent = parent.getParent();
+                }
+
+                resourceDescriptions.add(new ResourceDescription(resourceMethod.getHttpMethod(), path, resource.getHandlerClasses()));
+            }
+        }
+
+        Collections.sort(resourceDescriptions);
+        for (ResourceDescription resource : resourceDescriptions) {
+            LOG.debug(resource.toString());
+        }
     }
 
+    private List<Resource> findChildResources(Resource parentResource) {
+        final List<Resource> childResources = new ArrayList<>();
+        for (Resource resource : parentResource.getChildResources()) {
+            childResources.add(resource);
+            childResources.addAll(findChildResources(resource));
+        }
+
+        return childResources;
+    }
+
+    private static class ResourceDescription implements Comparable<ResourceDescription> {
+        private String method;
+        private String path;
+        private Set<Class<?>> handlerClasses;
+
+        private ResourceDescription(String method, String path, Set<Class<?>> handlerClasses) {
+            this.method = method;
+            this.path = path;
+            this.handlerClasses = handlerClasses;
+        }
+
+        @Override
+        public int compareTo(final ResourceDescription o) {
+            if (this.path.compareTo(o.path) == 0) {
+                return this.method.compareTo(o.method);
+            } else {
+                return this.path.compareTo(o.path);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return String.format("    %-7s %s (%s)", method, path, Joiner.on(", ").join(handlerClasses));
+        }
+    }
 }
