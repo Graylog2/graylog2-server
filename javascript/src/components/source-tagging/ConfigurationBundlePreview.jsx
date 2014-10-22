49ae2db55623d4912b465cc4e355031c80bbdced
Jochen Schalanda <jochen@torch.sh>
2014-10-22 18:50:44 +0200

    Add button to delete Content Packs, refs #934

--- javascript/src/components/source-tagging/ConfigurationBundlePreview.jsx
+++ javascript/src/components/source-tagging/ConfigurationBundlePreview.jsx
@@ -8,20 +8,25 @@ var Markdown = require('markdown').markdown;
 var ConfigurationBundlePreview = React.createClass({
     render: function () {
         var preview = "Select an element from the left list to see its preview";
-        var action = "";
+        var apply_action = "";
+        var delete_action = "";
 
         if (this.props.sourceTypeDescription) {
             preview = this.props.sourceTypeDescription;
-            action = <form action={"/a/system/contentpacks/" + this.props.sourceTypeId + "/apply"} method="POST">
-                        <input type="submit" value="Apply" className="btn btn-success"/>
-                     </form>;
+            apply_action = <form action={"/a/system/contentpacks/" + this.props.sourceTypeId + "/apply"} method="POST">
+                             <input type="submit" value="Apply" className="btn btn-success"/>
+                           </form>;
+            delete_action = <form action={"/a/system/contentpacks/" + this.props.sourceTypeId + "/delete"} method="POST">
+                              <input type="submit" value="Delete" className="btn btn-danger"/>
+                            </form>;
         }
 
         return (
             <div className="bundle-preview">
                 <h2>Preview:</h2>
                 <p dangerouslySetInnerHTML={{__html: Markdown.toHTML(preview)}}></p>
-                {action}
+                {apply_action}
+                {delete_action}
             </div>
         );
     }
