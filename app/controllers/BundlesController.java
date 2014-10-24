4d7e21c2e1647e0f2585077beb7c8f5e52749aca
Jochen Schalanda <jochen@torch.sh>
2014-10-24 19:33:07 +0200

    Fix bread crumbs for content packs

--- app/controllers/BundlesController.java
+++ app/controllers/BundlesController.java
@@ -65,14 +65,14 @@ public class BundlesController extends AuthenticatedController {
     public Result index() {
         BreadcrumbList bc = new BreadcrumbList();
         bc.addCrumb("System", routes.SystemController.index(0));
-        bc.addCrumb("Bundles", routes.BundlesController.index());
+        bc.addCrumb("Content packs", routes.BundlesController.index());
         return ok(views.html.system.bundles.index.render(currentUser(), bc));
     }
 
     public Result exportForm()  {
         BreadcrumbList bc = new BreadcrumbList();
         bc.addCrumb("System", routes.SystemController.index(0));
-        bc.addCrumb("Bundles", routes.BundlesController.index());
+        bc.addCrumb("Content packs", routes.BundlesController.index());
         bc.addCrumb("Export", routes.BundlesController.exportForm());
 
         final Map<String, List> data = getListData();
@@ -94,7 +94,7 @@ public class BundlesController extends AuthenticatedController {
         if (form.hasErrors()) {
             BreadcrumbList bc = new BreadcrumbList();
             bc.addCrumb("System", routes.SystemController.index(0));
-            bc.addCrumb("Bundles", routes.BundlesController.index());
+            bc.addCrumb("Content packs", routes.BundlesController.index());
             bc.addCrumb("Export", routes.BundlesController.exportForm());
 
             Map<String, List> data = getListData();
