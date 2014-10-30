c764bcfca835002f576ab89b2aa54d721c846880
Jochen Schalanda <jochen@torch.sh>
2014-10-30 14:52:21 +0100

    Use HostAndPort instead of custom splitting for parsing ES hosts
    Free bonus: Elasticsearch version check now also works with IPv6 literals.

--- graylog2-server/src/main/java/org/graylog2/indexer/Indexer.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/Indexer.java
@@ -23,6 +23,7 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.net.HostAndPort;
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.ListenableFuture;
 import com.ning.http.client.Response;
@@ -166,14 +167,12 @@ public class Indexer {
                 // if no Elasticsearch running
                 for (String host : hostList) {
                     // guess that Elasticsearch http is listening on port 9200
-                    final Iterable<String> hostAndPort = Splitter.on(':').limit(2).split(host);
-                    final Iterator<String> it = hostAndPort.iterator();
-                    final String ip = it.next();
-                    LOG.info("Checking Elasticsearch HTTP API at http://{}:9200/", ip);
+                    final HostAndPort hostAndPort = HostAndPort.fromString(host);
+                    LOG.info("Checking Elasticsearch HTTP API at http://{}:9200/", hostAndPort.getHostText());
 
                     try {
                         // Try the HTTP API endpoint
-                        final ListenableFuture<Response> future = httpClient.prepareGet("http://" + ip + ":9200/_nodes").execute();
+                        final ListenableFuture<Response> future = httpClient.prepareGet("http://" + hostAndPort.getHostText() + ":9200/_nodes").execute();
                         final Response response = future.get();
 
                         final JsonNode resultTree = new ObjectMapper().readTree(response.getResponseBody());
@@ -203,7 +202,7 @@ public class Indexer {
                     } catch (InterruptedException ignore) {
                     } catch (ExecutionException e1) {
                         // could not find any server on that address
-                        LOG.error("Could not connect to Elasticsearch at http://" + ip + ":9200/, is it running?", e1.getCause());
+                        LOG.error("Could not connect to Elasticsearch at http://" + hostAndPort.getHostText() + ":9200/, is it running?", e1.getCause());
                     }
                 }
             }
