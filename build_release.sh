bb2e3d57e725ab7cb2c767196a1e5db0f00c20a7
Jochen Schalanda <jochen@torch.sh>
2014-11-04 16:34:32 +0100

    Add processing of JavaScript to build_release.sh

--- build_release.sh
+++ build_release.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 ACTIVATOR_VERISON='1.2.10'
 ACTIVATOR_URL="http://downloads.typesafe.com/typesafe-activator/${ACTIVATOR_VERISON}/typesafe-activator-${ACTIVATOR_VERISON}-minimal.zip"
 
-ACTIVATOR_BIN=$(which activator &>/dev/null)
+ACTIVATOR_BIN=$(which activator &>/dev/null || true)
 
 if [[ -z "${ACTIVATOR_BIN}" ]]; then
   ACTIVATOR_BIN="${ACTIVATOR_PATH}/activator"
@@ -27,6 +27,15 @@ else
   echo "Please do that."
   exit 1
 fi  
+
+# Clean working directory
+"${ACTIVATOR_BIN}" clean
+
+# Prepare JavaScript
+pushd javascript
+npm test
+node_modules/.bin/gulp deploy-prod
+popd
 
 # Build universal .tar.gz
 "${ACTIVATOR_BIN}" universal:package-zip-tarball
