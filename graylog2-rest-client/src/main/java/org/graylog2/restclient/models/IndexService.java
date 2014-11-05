f8b2cf302458fb3f59caeca9b2c06c31612f5dd2
Bernd Ahlers <bernd@torch.sh>
2014-11-05 18:46:37 +0100

    Use correct route to delete index. Fixes #960.
    (cherry picked from commit 1dc7ebfc26b5ef1f88e84702582715eb631202db)

--- graylog2-rest-client/src/main/java/org/graylog2/restclient/models/IndexService.java
+++ graylog2-rest-client/src/main/java/org/graylog2/restclient/models/IndexService.java
@@ -103,7 +103,7 @@ public class IndexService {
 
     // Not part an Index model instance method because opening/closing can be applied to indices without calculated ranges.
     public void delete(String index) throws APIException, IOException {
-        api.path(routes.IndicesResource().single(index))
+        api.path(routes.IndicesResource().delete(index))
                 .timeout(apiTimeout("index_delete", 60, TimeUnit.SECONDS))
                 .expect(204)
                 .execute();
