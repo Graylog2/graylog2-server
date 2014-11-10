b024a63a49006f869d7af610ae0ff5810cdb7c47
Jochen Schalanda <jochen@torch.sh>
2014-11-10 15:12:42 +0100

    Fix ClusterService#allNotifications() in REST client

--- graylog2-rest-client/src/main/java/org/graylog2/restclient/models/ClusterService.java
+++ graylog2-rest-client/src/main/java/org/graylog2/restclient/models/ClusterService.java
@@ -82,7 +82,7 @@ public class ClusterService {
         List<Notification> notifications = Lists.newArrayList();
         for (NotificationSummaryResponse notification : r.notifications) {
             try {
-                notifications.add(new Notification(notification));
+                notifications.add(new Notification(notification, nodeService));
             } catch (IllegalArgumentException e) {
                 LOG.warn("There is a notification type we can't handle: [{}]", notification.type);
             }
