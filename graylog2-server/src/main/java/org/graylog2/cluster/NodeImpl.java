566e475926ec05fc6af7aef8818c3f28b369f6b3
Dennis Oelkers <dennis@torch.sh>
2014-11-17 15:53:37 +0100

    Ignoring internal fields in JSON serialization.

--- graylog2-server/src/main/java/org/graylog2/cluster/NodeImpl.java
+++ graylog2-server/src/main/java/org/graylog2/cluster/NodeImpl.java
@@ -16,6 +16,7 @@
  */
 package org.graylog2.cluster;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.bson.types.ObjectId;
 import org.graylog2.database.CollectionName;
 import org.graylog2.database.PersistedImpl;
@@ -72,11 +73,13 @@ public class NodeImpl extends PersistedImpl implements Node {
     }
 
     @Override
+    @JsonIgnore
     public Map<String, Validator> getValidations() {
         return Collections.emptyMap();
     }
 
     @Override
+    @JsonIgnore
     public Map<String, Validator> getEmbeddedValidations(String key) {
         return Collections.emptyMap();
     }
