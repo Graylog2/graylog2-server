946f0126e0c674e4d714b5f50d42726054d3ec40
Jochen Schalanda <jochen@torch.sh>
2014-11-19 18:41:08 +0100

    Revert "Add argument check for ClusterEntity#normalizeUriPath(String)"
    This reverts commit ea758872864c152385e9a9256b8baee63a4b4127.

--- graylog2-rest-client/src/main/java/org/graylog2/restclient/models/ClusterEntity.java
+++ graylog2-rest-client/src/main/java/org/graylog2/restclient/models/ClusterEntity.java
@@ -20,7 +20,6 @@ import org.graylog2.restclient.lib.APIException;
 import org.graylog2.restclient.lib.ExclusiveInputException;
 import org.graylog2.restclient.models.api.responses.system.InputLaunchResponse;
 import org.graylog2.restclient.models.api.responses.system.InputTypeSummaryResponse;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
@@ -28,60 +27,46 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Strings.isNullOrEmpty;
-
+/**
+ * @author Lennart Koopmann <lennart@torch.sh>
+ */
 public abstract class ClusterEntity {
-    private static final Logger LOG = LoggerFactory.getLogger(ClusterEntity.class);
+
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(ClusterEntity.class);
 
     protected ClusterEntity() { /* what you gonna do */ }
 
     protected URI normalizeUriPath(String address) {
-        checkArgument(isNullOrEmpty(address), "address must not be empty or null!");
-
         final URI uri = URI.create(address);
         return normalizeUriPath(uri);
     }
 
     protected URI normalizeUriPath(URI uri) {
-        if (isNullOrEmpty(uri.getPath())) {
+        if (uri.getPath() == null || uri.getPath().isEmpty()) {
             return uri;
         }
-
         if (uri.getPath().equals("/")) {
             try {
                 return new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), "", uri.getQuery(), uri.getFragment());
             } catch (URISyntaxException e) { // sigh exception.
-                LOG.error("Could not process transportAddress {}, invalid URI syntax", uri.toASCIIString());
+                log.error("Could not process transportAddress {}, invalid URI syntax", uri.toASCIIString());
                 return uri;
             }
         }
-        LOG.info("Could not normalize path on node transport address, it contained some unrecognized path: {}", uri.toASCIIString());
+        log.info("Could not normalize path on node transport address, it contained some unrecognized path: {}", uri.toASCIIString());
         return uri;
     }
 
     public abstract String getShortNodeId();
-
     public abstract String getHostname();
-
     public abstract String getTransportAddress();
-
     public abstract void touch();
-
     public abstract void markFailure();
-
     public abstract boolean terminateInput(String inputId);
-
     public abstract String getNodeId();
-
     public abstract InputLaunchResponse launchInput(String title, String type, Boolean global, Map<String, Object> configuration, User creator, boolean isExclusive) throws ExclusiveInputException;
-
     public abstract InputTypeSummaryResponse getInputTypeInformation(String type) throws IOException, APIException;
-
     public abstract void stopInput(String inputId) throws IOException, APIException;
-
     public abstract void startInput(String inputId) throws IOException, APIException;
-
     public abstract void restartInput(String inputId) throws IOException, APIException;
 }
