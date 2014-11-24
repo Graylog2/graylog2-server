abc608e9f59475d7c781cd3d2cc7d6d7b8a0dded
Jochen Schalanda <jochen@torch.sh>
2014-11-24 17:11:09 +0100

    Ensure WRITE_DATES_AS_TIMESTAMPS is enabled in MessageToJsonSerializer
    The deserialization logic expects the "timestamp" field to be a long containing
    the milliseconds since epoch.
    
    Fixes #781

--- graylog2-server/src/main/java/org/graylog2/utilities/MessageToJsonSerializer.java
+++ graylog2-server/src/main/java/org/graylog2/utilities/MessageToJsonSerializer.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.google.common.base.Optional;
@@ -180,6 +181,7 @@ public class MessageToJsonSerializer {
             }
         };
         this.mapper.registerModule(simpleModule);
+        this.mapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     public byte[] serializeToBytes(Message message) throws JsonProcessingException {
