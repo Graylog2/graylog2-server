2651860878190af8f189ecfcda06dd1fd7dd9b7e
olli <oliver.zeigermann@gmail.com>
2014-11-26 10:42:30 +0100

    Refactoring of sources tab: factor out line chart part 2

---
+++ javascript/src/components/sources/SourceLineChart.jsx
@@ -0,0 +1,128 @@
+/* global activateTimerangeChooser, momentHelper, graphHelper */
+/* jshint -W107 */
+
+'use strict';
+
+var React = require('react');
+
+var $ = require('jquery');
+
+var dc = require('dc');
+var d3 = require('d3');
+
+var UniversalSearch = require('../search/UniversalSearch');
+
+var SourceLineChart = React.createClass({
+    getInitialState() {
+        return {
+            histogramDataAvailable: true,
+            reloadingHistogram: false,
+            lineChartWidth: "100%"
+        };
+    },
+    renderLineChart(dimension, group, syncRangeWithQuery) {
+        var lineChartDomNode = $("#dc-sources-line-chart")[0];
+        var width = $(lineChartDomNode).width();
+        $(lineChartDomNode).on('mouseup', (event) => {
+            $(".timerange-selector-container").effect("bounce", {
+                complete: () => {
+                    // Submit search directly if alt key is pressed.
+                    if (event.altKey) {
+                        UniversalSearch.submit();
+                    }
+                }
+            });
+        });
+        this._lineChart = dc._lineChart(lineChartDomNode);
+        this._lineChart
+            .height(200)
+            .margins({left: 35, right: 20, top: 20, bottom: 20})
+            .dimension(dimension)
+            .group(group)
+            .x(d3.time.scale())
+            .renderHorizontalGridLines(true)
+            // FIXME: causes those nasty exceptions when rendering data (one per x axis tick)
+            .elasticX(true)
+            .elasticY(true)
+            .on("filtered", (chart) => {
+                dc.events.trigger(() => {
+                    var filter = chart.filter();
+                    if (filter) {
+                        var fromDateTime = momentHelper.toUserTimeZone(filter[0]);
+                        var toDateTime = momentHelper.toUserTimeZone(filter[1]);
+
+                        activateTimerangeChooser("absolute", $('.timerange-selector-container .dropdown-menu a[data-selector-name="absolute"]'));
+                        var fromInput = $('#universalsearch .absolute .absolute-from-human');
+                        var toInput = $('#universalsearch .absolute .absolute-to-human');
+
+                        fromInput.val(fromDateTime.format(momentHelper.DATE_FORMAT_TZ));
+                        toInput.val(toDateTime.format(momentHelper.DATE_FORMAT_TZ));
+                    } else {
+                        syncRangeWithQuery();
+                    }
+                });
+            });
+        this._configureWidth(width);
+        this._lineChart.xAxis()
+            .ticks(graphHelper.customTickInterval())
+            .tickFormat(graphHelper.customDateTimeFormat());
+        this._lineChart.yAxis()
+            .ticks(6)
+            .tickFormat(d3.format("s"));
+    },
+    clearFilters() {
+        this._lineChart.filterAll();
+    },
+    getFilters() {
+        return this._lineChart ? this._lineChart.filters() : [];
+    },
+    setFilter(filter) {
+        this._lineChart.filter(filter);
+    },
+    _configureWidth(lineChartWidth) {
+        this._lineChart
+            .width(lineChartWidth);
+        this.setState({lineChartWidth: String(lineChartWidth) + "px"});
+    },
+    updateWidth() {
+        var lineChartDomNode = $("#dc-sources-line-chart");
+        var lineChartWidth = lineChartDomNode.width();
+        this._configureWidth(lineChartWidth);
+    },
+    render() {
+        var loadingSpinnerStyle = {
+            display: this.state.reloadingHistogram ? 'block' : 'none',
+            width: this.state.lineChartWidth
+        };
+        var loadingSpinner = (
+            <div className="sources overlay" style={loadingSpinnerStyle}>
+                <i className="icon-spin icon-refresh spinner"></i>
+            </div>
+        );
+
+        var noDataOverlayStyle = {
+            display: this.state.histogramDataAvailable ? 'none' : 'block',
+            width: this.state.lineChartWidth
+        };
+        var noDataOverlay = (
+            <div className="sources overlay" style={noDataOverlayStyle}>Not enough data</div>
+        );
+        return (
+            <div id="dc-sources-line-chart" className="span12">
+                <h3>
+                    <i className="icon icon-calendar"></i> Messages per {this.props.resolution}&nbsp;
+                    <small>
+                        <a href="javascript:undefined" className="reset" onClick={this.props.resetFilters} title="Reset filter" style={{"display": "none"}}>
+                            <i className="icon icon-retweet"></i>
+                        </a>
+                    </small>
+                </h3>
+                {loadingSpinner}
+                {noDataOverlay}
+            </div>
+        );
+
+    }
+});
+
+module.exports = SourceLineChart;
