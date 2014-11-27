6d1309d46327a6c14c410f093905da828277d3f7
Dennis Oelkers <dennis@torch.sh>
2014-11-27 13:21:40 +0100

    Removing OutputBufferWatermark, querying ringbuffer directly instead.

--- graylog2-server/src/main/java/org/graylog2/buffers/OutputBuffer.java
+++ graylog2-server/src/main/java/org/graylog2/buffers/OutputBuffer.java
@@ -50,8 +50,6 @@ public class OutputBuffer extends Buffer {
 
     private final ExecutorService executor;
 
-    private final OutputBufferWatermark outputBufferWatermark;
-
     private final Configuration configuration;
     private final OutputCache overflowCache;
 
@@ -64,11 +62,9 @@ public class OutputBuffer extends Buffer {
     @Inject
     public OutputBuffer(OutputBufferProcessor.Factory outputBufferProcessorFactory,
                         MetricRegistry metricRegistry,
-                        OutputBufferWatermark outputBufferWatermark,
                         Configuration configuration,
                         OutputCache overflowCache) {
         this.outputBufferProcessorFactory = outputBufferProcessorFactory;
-        this.outputBufferWatermark = outputBufferWatermark;
         this.configuration = configuration;
         this.overflowCache = overflowCache;
         this.executor = executorService(metricRegistry);
@@ -173,7 +169,6 @@ public class OutputBuffer extends Buffer {
 
     @Override
     protected void afterInsert(int n) {
-        outputBufferWatermark.addAndGet(n);
         incomingMessages.mark(n);
     }
 }
