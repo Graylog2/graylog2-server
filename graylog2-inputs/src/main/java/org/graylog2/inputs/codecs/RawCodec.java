e70a48bccf7455fcec294a9dfbdb8ed2c515375b
Dennis Oelkers <dennis@torch.sh>
2014-12-03 14:21:05 +0100

    Fixing name resolution and overriding source again.
    Fixes #790.

--- graylog2-inputs/src/main/java/org/graylog2/inputs/codecs/RawCodec.java
+++ graylog2-inputs/src/main/java/org/graylog2/inputs/codecs/RawCodec.java
@@ -33,6 +33,8 @@
  */
 package org.graylog2.inputs.codecs;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
@@ -41,6 +43,7 @@ import com.google.inject.assistedinject.AssistedInject;
 import org.graylog2.plugin.ConfigClass;
 import org.graylog2.plugin.FactoryClass;
 import org.graylog2.plugin.Message;
+import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.configuration.Configuration;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
 import org.graylog2.plugin.inputs.MessageInput;
@@ -48,44 +51,69 @@ import org.graylog2.plugin.inputs.codecs.Codec;
 import org.graylog2.plugin.inputs.codecs.CodecAggregator;
 import org.graylog2.plugin.inputs.transports.NettyTransport;
 import org.graylog2.plugin.journal.RawMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
 
+import static com.codahale.metrics.MetricRegistry.name;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 public class RawCodec implements Codec {
+    private static final Logger LOG = LoggerFactory.getLogger(RawCodec.class);
+
     private final ObjectMapper objectMapper;
     private final Configuration configuration;
 
+    private final Timer resolveTime;
+    private final Timer decodeTime;
+
     @AssistedInject
-    public RawCodec(ObjectMapper objectMapper, @Assisted Configuration configuration) {
+    public RawCodec(ObjectMapper objectMapper, MetricRegistry metricRegistry, @Assisted Configuration configuration) {
         this.objectMapper = objectMapper;
         this.configuration = configuration;
+        this.resolveTime = metricRegistry.timer(name(RawCodec.class, "resolveTime"));
+        this.decodeTime = metricRegistry.timer(name(RawCodec.class, "decodeTime"));
     }
 
     @Nullable
     @Override
     public Message decode(@Nonnull RawMessage raw) {
-        // TODO fix remote address, use raw message
-        String source;
-        if (raw.getMetaData() != null) {
-            try {
-                final Map<String, Object> map = objectMapper.readValue(raw.getMetaData(),
-                                                        new TypeReference<Map<String, Object>>() {
-                                                        });
-                source = map.containsKey("remote_address") ? (String) map.get("remote_address") : "unknown";
-            } catch (IOException e) {
-                source = "unknown";
-            }
-        } else {
+        try (Timer.Context decode = this.decodeTime.time()) {
+            // TODO fix remote address, use raw message
+            String source;
             if (configuration.stringIsSet(MessageInput.CK_OVERRIDE_SOURCE)) {
                 source = configuration.getString(MessageInput.CK_OVERRIDE_SOURCE);
             } else {
-                source = "unknown";
+                /*if (!isNullOrEmpty(raw.getMetaData())) {
+                    try {
+                        final Map<String, Object> map = objectMapper.readValue(raw.getMetaData(),
+                                new TypeReference<Map<String, Object>>() {
+                                });
+                        source = map.containsKey("remote_address") ? (String) map.get("remote_address") : "unknown";
+                    } catch (IOException e) {
+                        source = "unknown";
+                    }
+                } else {
+                    source = "unknown";
+                }*/
+                final InetSocketAddress remoteAddress = raw.getRemoteAddress();
+                try (Timer.Context context = this.resolveTime.time()) {
+                    source = Tools.rdnsLookup(remoteAddress.getAddress());
+                } catch (UnknownHostException e) {
+                    LOG.warn("Reverse DNS lookup failed. Falling back to parsed hostname.", e);
+                    source = "unknown";
+                }
+
             }
+            return new Message(new String(raw.getPayload(), Charsets.UTF_8), source, raw.getTimestamp());
         }
-        return new Message(new String(raw.getPayload(), Charsets.UTF_8), source, raw.getTimestamp());
     }
 
     @Nullable
