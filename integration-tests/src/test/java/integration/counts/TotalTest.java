da79730f067991cfaa675d9e4927b08f200b867f
Dennis Oelkers <dennis@torch.sh>
2014-12-16 15:14:03 +0100

    Adding simple Test for CountResource.

---
+++ integration-tests/src/test/java/integration/counts/TotalTest.java
@@ -0,0 +1,34 @@
+/**
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package integration.counts;
+
+import integration.BaseRestTest;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+
+public class TotalTest extends BaseRestTest {
+    @Test
+    public void total() {
+        given()
+                .when()
+                    .get("/count/total")
+                .then()
+                    .statusCode(200)
+                    .body(".", containsAllKeys("events"));
+    }
+}
