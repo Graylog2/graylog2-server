b51e6396580acc869d6126cdb63355e3936dfc14
Jochen Schalanda <jochen@torch.sh>
2015-01-14 12:48:23 +0100

    Replace AsyncHttpClient with OkHttp in Ping class

--- graylog2-radio/src/main/java/org/graylog2/radio/cluster/Ping.java
+++ graylog2-radio/src/main/java/org/graylog2/radio/cluster/Ping.java
@@ -17,42 +17,38 @@
 package org.graylog2.radio.cluster;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.inject.Inject;
-import javax.inject.Named;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.Request;
-import com.ning.http.client.Response;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 import org.graylog2.plugin.ServerStatus;
 import org.graylog2.rest.models.radio.requests.PingRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.core.UriBuilder;
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.io.IOException;
 import java.net.URI;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 public class Ping implements Runnable {
-
     /*
      * This is extremely simple. Once we do more than just the ping API calls
      * we should build something proper here.
      */
 
     private static final Logger LOG = LoggerFactory.getLogger(Ping.class);
+    private static final MediaType CONTENT_TYPE = MediaType.parse("application/json");
 
     private final ObjectMapper objectMapper;
-    private final AsyncHttpClient httpClient;
+    private final OkHttpClient httpClient;
     private final String nodeId;
     private final URI serverUri;
     private final URI ourUri;
 
     @Inject
-    public Ping(ObjectMapper objectMapper, AsyncHttpClient httpClient, @Named("rest_transport_uri") URI ourUri, @Named("graylog2_server_uri") URI serverUri, ServerStatus serverStatus) {
+    public Ping(ObjectMapper objectMapper, OkHttpClient httpClient, @Named("rest_transport_uri") URI ourUri, @Named("graylog2_server_uri") URI serverUri, ServerStatus serverStatus) {
         this.objectMapper = objectMapper;
         this.httpClient = httpClient;
         this.nodeId = serverStatus.getNodeId().toString();
@@ -60,22 +56,20 @@ public class Ping implements Runnable {
         this.serverUri = serverUri;
     }
 
-    public void ping() throws IOException, ExecutionException, InterruptedException {
+    public void ping() throws IOException {
         final PingRequest pingRequest = PingRequest.create(ourUri.toString());
 
-        final UriBuilder uriBuilder = UriBuilder.fromUri(serverUri);
-        uriBuilder.path("/system/radios/" + nodeId + "/ping");
+        final URI uri = serverUri.resolve("/system/radios/" + nodeId + "/ping");
+        final Request request = new Request.Builder()
+                .url(uri.toURL())
+                .put(RequestBody.create(CONTENT_TYPE, objectMapper.writeValueAsBytes(pingRequest)))
+                .build();
 
-        final Request request = httpClient.preparePut(uriBuilder.build().toString())
-                .setHeader("Content-Type", "application/json")
-                .setBody(objectMapper.writeValueAsString(pingRequest)).build();
-        Future<Response> f = httpClient.executeRequest(request);
-
-        Response r = f.get();
+        final Response r = httpClient.newCall(request).execute();
 
         // fail on a non-ok status
-        if (r.getStatusCode() > 299) {
-            throw new RuntimeException("Expected ping HTTP response OK but got [" + r.getStatusCode() + "]. Request was " + request.getUrl());
+        if (!r.isSuccessful()) {
+            throw new RuntimeException("Expected successful HTTP response [2xx] but got [" + r.code() + "]. Request was " + request.urlString());
         }
     }
 
@@ -83,7 +77,7 @@ public class Ping implements Runnable {
     public void run() {
         try {
             ping();
-        } catch (IOException | ExecutionException | InterruptedException e) {
+        } catch (IOException e) {
             LOG.error("Pinging the master node failed: ", e);
         }
     }
