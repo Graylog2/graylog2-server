a4a56ad7f7e81c126bb78fc7e639f56897fb35b9
Dennis Oelkers <dennis@torch.sh>
2015-01-22 15:04:22 +0100

    Returning 0 per default for metrics which have not been returned.

--- graylog2-rest-client/src/main/java/org/graylog2/restclient/models/ClusterService.java
+++ graylog2-rest-client/src/main/java/org/graylog2/restclient/models/ClusterService.java
@@ -186,7 +186,10 @@ public class ClusterService {
 
     // TODO duplicated
     private long asLong(String read_bytes, Map<String, Metric> metrics) {
-        return ((Number) ((Gauge) metrics.get(read_bytes)).getValue()).longValue();
+        if (metrics.get(read_bytes) != null)
+            return ((Number) ((Gauge) metrics.get(read_bytes)).getValue()).longValue();
+        else
+            return 0;
     }
 
     // TODO duplicated
