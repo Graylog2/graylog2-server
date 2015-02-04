bc81bf5d65f8c4c65b9f2714c7d7889adb7cefef
Jochen Schalanda <jochen@graylog.com>
2015-02-04 18:32:48 +0100

    Assorted rebranding-related text replacements

--- app/lib/notifications/EmailTransportConfigurationInvalidNotification.java
+++ app/lib/notifications/EmailTransportConfigurationInvalidNotification.java
@@ -29,7 +29,7 @@ public class EmailTransportConfigurationInvalidNotification implements Notificat
     @Override
     public String getDescription() {
         return "The configuration for the email transport subsystem has shown to be missing or invalid. " +
-                "Please check the related section of your Graylog2 server configuration file." +
+                "Please check the related section of your Graylog server configuration file." +
                 "This is the detailed error message: " + notification.getDetail("exception");
     }
 
