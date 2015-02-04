12885e094c5d079374b48a911a49fa2bf0215914
Jochen Schalanda <jochen@graylog.com>
2015-02-04 17:31:13 +0100

    Update license headers in Java source files

--- graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/inputs/annotations/Codec.java
+++ graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/inputs/annotations/Codec.java
@@ -1,6 +1,6 @@
 /**
  * The MIT License
- * Copyright (c) 2012 TORCH GmbH
+ * Copyright (c) 2012 Graylog, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
