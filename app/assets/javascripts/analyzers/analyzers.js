8b827064e0633f28ccc12b2e6239d5a8bad59c8d
Lennart Koopmann <lennart@graylog.com>
2015-03-16 11:34:11 +0100

    mark fields that are open for analysis as open #ux

--- app/assets/javascripts/analyzers/analyzers.js
+++ app/assets/javascripts/analyzers/analyzers.js
@@ -5,6 +5,14 @@ $(document).ready(function() {
 
         $(".analyze-field", $(this).parent()).toggle();
         $(this).toggleClass("open-analyze-field-active");
+
+        if($(this).hasClass("fa-caret-right")) {
+            $(this).removeClass("fa-caret-right");
+            $(this).addClass("fa-caret-down");
+        } else {
+            $(this).removeClass("fa-caret-down");
+            $(this).addClass("fa-caret-right");
+        }
     });
 
 });
