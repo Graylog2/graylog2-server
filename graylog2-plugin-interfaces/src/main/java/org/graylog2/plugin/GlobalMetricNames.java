e075b8d8fc51e2c61bb3095bc6a5012f4918bc5a
Kay Roepke <kroepke@googlemail.com>
2015-03-24 13:48:09 +0100

    use org.graylog2. as metric prefix, otherwise the values won't be displayed in the metrics browser
    #1071

--- graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/GlobalMetricNames.java
+++ graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/GlobalMetricNames.java
@@ -26,10 +26,10 @@ import static com.codahale.metrics.MetricRegistry.name;
 
 public class GlobalMetricNames {
 
-    public static final String INPUT_THROUGHPUT = "org.graylog.throughput.input";
+    public static final String INPUT_THROUGHPUT = "org.graylog2.throughput.input";
     public static final String INPUT_THROUGHPUT_RATE = name(INPUT_THROUGHPUT, "1-sec-rate");
 
-    public static final String OUTPUT_THROUGHPUT = "org.graylog.throughput.output";
+    public static final String OUTPUT_THROUGHPUT = "org.graylog2.throughput.output";
     public static final String OUTPUT_THROUGHPUT_RATE = name(OUTPUT_THROUGHPUT, "1-sec-rate");
 
 }
