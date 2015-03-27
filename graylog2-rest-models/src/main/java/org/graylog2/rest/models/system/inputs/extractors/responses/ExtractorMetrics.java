8c701e7cc3affcad8748a0ce02ad831cc7ff1917
Dennis Oelkers <dennis@torch.sh>
2015-03-27 12:03:22 +0100

    Removing unneeded import.

--- graylog2-rest-models/src/main/java/org/graylog2/rest/models/system/inputs/extractors/responses/ExtractorMetrics.java
+++ graylog2-rest-models/src/main/java/org/graylog2/rest/models/system/inputs/extractors/responses/ExtractorMetrics.java
@@ -22,8 +22,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import org.graylog2.rest.models.metrics.responses.TimerRateMetricsResponse;
 
-import java.util.Map;
-
 @AutoValue
 @JsonAutoDetect
 public abstract class ExtractorMetrics {
