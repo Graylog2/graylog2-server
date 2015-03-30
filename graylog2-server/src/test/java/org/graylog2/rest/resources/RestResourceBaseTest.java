260560ea212772c18f9ade9aaaa4b7d5d2aa7924
Dennis Oelkers <dennis@graylog.com>
2015-03-30 15:12:25 +0200

    Setting up injector in new base class for JAX-RS resource test classes.

--- graylog2-server/src/test/java/org/graylog2/rest/resources/RestResourceBaseTest.java
+++ graylog2-server/src/test/java/org/graylog2/rest/resources/RestResourceBaseTest.java
@@ -1,4 +1,17 @@
 package org.graylog2.rest.resources;
 
+import com.google.inject.Module;
+import org.assertj.core.util.Lists;
+import org.graylog2.shared.bindings.GuiceInjectorHolder;
+import org.junit.Before;
+
+import java.util.List;
+
 public class RestResourceBaseTest {
+    @Before
+    public void setUpInjector() throws Exception {
+        // The list of modules is empty for now so only JIT injection will be used.
+        final List<Module> modules = Lists.emptyList();
+        GuiceInjectorHolder.createInjector(modules);
+    }
 }
