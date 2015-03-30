ba96f373ed4124a7a496a9359b6b45873107e624
Dennis Oelkers <dennis@graylog.com>
2015-03-30 15:56:58 +0200

    Adding more tests to AgentRegistrationResourceTest.

--- graylog2-server/src/test/java/org/graylog2/rest/resources/system/agent/AgentRegistrationResourceTest.java
+++ graylog2-server/src/test/java/org/graylog2/rest/resources/system/agent/AgentRegistrationResourceTest.java
@@ -20,10 +20,50 @@ public class AgentRegistrationResourceTest extends RestResourceBaseTest {
 
     @Test
     public void testRegister() throws Exception {
-        final AgentRegistrationRequest input = AgentRegistrationRequest.create("foobar", "nodeId", AgentNodeDetails.create("DummyOS 1.0"));
+        final AgentRegistrationRequest input = AgentRegistrationRequest.create("agentId", "nodeId", AgentNodeDetails.create("DummyOS 1.0"));
 
         final Response response = this.resource.register(input);
 
         assertThat(response).isSuccess();
+    }
+
+    @Test
+    public void testRegisterInvalidAgentId() throws Exception {
+        final AgentRegistrationRequest invalid = AgentRegistrationRequest.create("", "nodeId", AgentNodeDetails.create("DummyOS 1.0"));
+
+        final Response response = this.resource.register(invalid);
+
+        assertThat(response).isError();
+        assertThat(response).isStatus(Response.Status.BAD_REQUEST);
+    }
+
+    @Test
+    public void testRegisterInvalidNodeId() throws Exception {
+        final AgentRegistrationRequest invalid = AgentRegistrationRequest.create("agentId", "", AgentNodeDetails.create("DummyOS 1.0"));
+
+        final Response response = this.resource.register(invalid);
+
+        assertThat(response).isError();
+        assertThat(response).isStatus(Response.Status.BAD_REQUEST);
+    }
+
+    @Test
+    public void testRegisterMissingNodeDetails() throws Exception {
+        final AgentRegistrationRequest invalid = AgentRegistrationRequest.create("agentId", "nodeId", null);
+
+        final Response response = this.resource.register(invalid);
+
+        assertThat(response).isError();
+        assertThat(response).isStatus(Response.Status.BAD_REQUEST);
+    }
+
+    @Test
+    public void testRegisterMissingOperatingSystem() throws Exception {
+        final AgentRegistrationRequest invalid = AgentRegistrationRequest.create("agentId", "nodeId", AgentNodeDetails.create(""));
+
+        final Response response = this.resource.register(invalid);
+
+        assertThat(response).isError();
+        assertThat(response).isStatus(Response.Status.BAD_REQUEST);
     }
 }
