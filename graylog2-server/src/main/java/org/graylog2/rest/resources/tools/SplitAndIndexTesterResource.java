ce341c0ee05cff82453be42d9de036cd87e83d44
Edmundo Alvarez <edmundo@graylog.com>
2015-03-31 16:27:29 +0200

    Support testing extractors through POST
    Refs Graylog2/graylog2-web-interface#1190

--- graylog2-server/src/main/java/org/graylog2/rest/resources/tools/SplitAndIndexTesterResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/tools/SplitAndIndexTesterResource.java
@@ -19,12 +19,16 @@ package org.graylog2.rest.resources.tools;
 import com.codahale.metrics.annotation.Timed;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.graylog2.inputs.extractors.SplitAndIndexExtractor;
-import org.graylog2.shared.rest.resources.RestResource;
+import org.graylog2.rest.resources.tools.requests.SplitAndIndexTestRequest;
 import org.graylog2.rest.resources.tools.responses.SplitAndIndexTesterResponse;
+import org.graylog2.shared.rest.resources.RestResource;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -39,10 +43,22 @@ public class SplitAndIndexTesterResource extends RestResource {
     public SplitAndIndexTesterResponse splitAndIndexTester(@QueryParam("split_by") @NotNull String splitBy,
                                                            @QueryParam("index") @Min(0) int index,
                                                            @QueryParam("string") @NotNull String string) {
+        return doSplitAndIndexTest(string, splitBy, index);
+    }
+
+    @POST
+    @Timed
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public SplitAndIndexTesterResponse splitAndIndexTest(@Valid @NotNull SplitAndIndexTestRequest splitAndIndexTestRequest) {
+        return doSplitAndIndexTest(splitAndIndexTestRequest.string(),
+                splitAndIndexTestRequest.splitBy(), splitAndIndexTestRequest.index());
+    }
+
+    private SplitAndIndexTesterResponse doSplitAndIndexTest(String string, String splitBy, int index) {
         final String cut = SplitAndIndexExtractor.cut(string, splitBy, index - 1);
         int[] positions = SplitAndIndexExtractor.getCutIndices(string, splitBy, index - 1);
 
         return SplitAndIndexTesterResponse.create((cut != null), cut, positions[0], positions[1]);
     }
-
 }
