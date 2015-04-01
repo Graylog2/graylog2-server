c43e3dadfb1e9b872caac3ea330510dea3d32f9c
Dennis Oelkers <dennis@graylog.com>
2015-04-01 15:17:45 +0200

    Removing unneeded imports and verbs from api operation definitions.

--- graylog2-server/src/main/java/org/graylog2/rest/resources/system/agent/AgentResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/system/agent/AgentResource.java
@@ -6,20 +6,21 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
-import org.graylog2.rest.models.agent.AgentNodeDetails;
+import org.graylog2.agents.Agent;
+import org.graylog2.agents.AgentService;
+import org.graylog2.agents.Agents;
+import org.graylog2.database.NotFoundException;
 import org.graylog2.rest.models.agent.responses.AgentList;
 import org.graylog2.rest.models.agent.responses.AgentSummary;
 import org.graylog2.shared.rest.resources.RestResource;
-import org.joda.time.DateTime;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Collections;
 import java.util.List;
 
 @Api(value = "System/Agents", description = "Management of graylog agents.")
@@ -27,23 +28,36 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class AgentResource extends RestResource {
+    private final AgentService agentService;
+
+    @Inject
+    public AgentResource(AgentService agentService) {
+        this.agentService = agentService;
+    }
+
     @GET
     @Timed
-    @ApiOperation(value = "List - lists all existing agent registrations")
+    @ApiOperation(value = "lists all existing agent registrations")
     public AgentList list() {
-        final List<AgentSummary> agents = Collections.emptyList();
-        return AgentList.create(agents);
+        final List<Agent> agents = agentService.all();
+        final List<AgentSummary> agentSummaries = Agents.toSummaryList(agents);
+        return AgentList.create(agentSummaries);
     }
 
     @GET
     @Timed
     @Path("/{agentId}")
-    @ApiOperation(value = "Get - Returns at most one agent summary for the specified agent id")
+    @ApiOperation(value = "Returns at most one agent summary for the specified agent id")
     @ApiResponses(value = {
             @ApiResponse(code = 404, message = "No agent with the specified id exists")
     })
     public AgentSummary get(@ApiParam(name = "agentId", required = true)
-                            @PathParam("agentId") String agentId) {
-        return AgentSummary.create("foo", "bar", AgentNodeDetails.create("DummyOs"), DateTime.now());
+                            @PathParam("agentId") String agentId) throws NotFoundException {
+        final Agent agent = agentService.findById(agentId);
+
+        if (agent != null)
+            return Agents.toSummary(agent);
+        else
+            throw new NotFoundException("Agent <" + agentId + "> not found!");
     }
 }
