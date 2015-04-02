2680868339d466c9746e8439cf4c245a98854f01
Dennis Oelkers <dennis@graylog.com>
2015-04-02 12:56:46 +0200

    Adding very basic list view of connected agents.

--- app/controllers/AgentsController.java
+++ app/controllers/AgentsController.java
@@ -1,4 +1,30 @@
 package controllers;
 
-public class AgentsController {
+import lib.BreadcrumbList;
+import org.graylog2.rest.models.agent.responses.AgentSummary;
+import org.graylog2.restclient.lib.APIException;
+import org.graylog2.restclient.models.AgentService;
+import play.mvc.Result;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
+
+public class AgentsController extends AuthenticatedController {
+    private final AgentService agentService;
+
+    @Inject
+    public AgentsController(AgentService agentService) {
+        this.agentService = agentService;
+    }
+
+    public Result index() throws APIException, IOException {
+        final List<AgentSummary> agents = agentService.all();
+
+        BreadcrumbList bc = new BreadcrumbList();
+        bc.addCrumb("System", routes.SystemController.index(0));
+        bc.addCrumb("Agents", routes.AgentsController.index());
+
+        return ok(views.html.system.agents.index.render(currentUser(), bc, agents));
+    }
 }
