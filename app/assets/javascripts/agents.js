e1ed768fb7860aa7264fed66e5983aff85bf304d
Dennis Oelkers <dennis@graylog.com>
2015-04-07 17:35:10 +0200

    Adding filtering, reactifying toggling of displaying inactive agents.

--- app/assets/javascripts/agents.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".toggleInactive").toggle(function() {
-        $(this).text("Hide inactive");
-        $("tr.inactive").show();
-    }, function() {
-        $(this).text("Show inactive");
-        $("tr.inactive").hide();
-    })
-});
--- javascript/src/components/agents/AgentList.jsx
+++ javascript/src/components/agents/AgentList.jsx
@@ -10,7 +10,9 @@ var AgentList = React.createClass({
 
     getInitialState() {
         return {
-            agents: []
+            agents: [],
+            filter: "",
+            showInactive: false
         };
     },
     componentDidMount() {
@@ -27,15 +29,21 @@ var AgentList = React.createClass({
 
         setTimeout(this.loadData, this.AGENT_DATA_REFRESH);
     },
+    _getFilteredAgents() {
+        var filter = this.state.filter.toLowerCase().trim();
+        return this.state.agents.filter((agent) => { return !filter || agent.id.toLowerCase().indexOf(filter) !== -1
+            || agent.node_id.toLowerCase().indexOf(filter) !== -1
+            || agent.node_details.operating_system.toLowerCase().indexOf(filter) !== -1; })
+    },
     render() {
         var agentList;
 
         if (this.state.agents.length === 0) {
             agentList = <div><div className="alert alert-info">There are no agents.</div></div>;
         } else {
-            var agents = this.state.agents.map((agent) => {
+            var agents = this._getFilteredAgents().filter((agent) => {return (this.state.showInactive || agent.active)}).map((agent) => {
                 var agentClass = agent.active ? "" : "greyedOut inactive";
-                var style = agent.active ? {} : {display: 'none'};
+                var style = {}; //agent.active ? {} : {display: 'none'};
                 var annotation = agent.active ? "" : "(inactive)";
                 return (
                     <tr className={agentClass} style={style}>
@@ -59,26 +67,42 @@ var AgentList = React.createClass({
             });
 
             agentList = (
-                <table className="table table-striped users-list">
-                    <thead>
-                    <tr>
-                        <th className="name">
-                            Agent ID
-                        </th>
-                        <th>Node Id</th>
-                        <th>Operating System</th>
-                        <th>Last Seen</th>
-                        <th className="actions">Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {agents}
-                    </tbody>
-                </table>
+                <div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <form className="form-inline agents-filter-form">
+                                <label htmlFor="agentsfilter">Filter agents:</label>
+                                <input type="text" name="filter" id="agentsfilter" value={this.state.filter} onChange={(event) => {this.setState({filter: event.target.value});}} />
+                            </form>
+
+                            <a onClick={this.toggleShowInactive}>Toggle</a> displaying inactive agents
+
+                            <table className="table table-striped users-list">
+                                <thead>
+                                <tr>
+                                    <th className="name">
+                                        Agent ID
+                                    </th>
+                                    <th>Node Id</th>
+                                    <th>Operating System</th>
+                                    <th>Last Seen</th>
+                                    <th className="actions">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    {agents}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             );
         }
 
         return agentList;
+    },
+    toggleShowInactive() {
+        this.setState({showInactive: !this.state.showInactive});
     }
 });
 
