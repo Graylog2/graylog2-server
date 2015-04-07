1bb8ee8c1bd9d83ca71c0af828d6edce1df3d100
Edmundo Alvarez <edmundo@graylog.com>
2015-04-07 18:43:57 +0200

    Render quickvalues table using dc

--- javascript/src/components/visualizations/QuickValuesVisualization.jsx
+++ javascript/src/components/visualizations/QuickValuesVisualization.jsx
@@ -1,22 +1,18 @@
 'use strict';
 
 var React = require('react');
-var numeral = require('numeral');
 
-var TableRowWrapper = React.createClass({
-    render() {
-        return (
-            <tr>
-                <td>{this.props.term}</td>
-                <td>{this.props.percentage}</td>
-                <td>{this.props.count}</td>
-            </tr>
-        );
-    }
-});
+var numeral = require('numeral');
+var crossfilter = require('crossfilter');
+var dc = require('dc');
+var d3 = require('d3');
 
 var QuickValuesVisualization = React.createClass({
+    NUMBER_OF_TOP_VALUES: 5,
     getInitialState() {
+        this.quickValuesData = crossfilter();
+        this.dimension = this.quickValuesData.dimension((d) => d.count);
+
         return {
             total: undefined,
             others: undefined,
@@ -24,14 +20,33 @@ var QuickValuesVisualization = React.createClass({
             terms: {}
         };
     },
-    componentWillReceiveProps(newProps) {
-        var quickValues = newProps.data;
-        this.setState({
-            total: quickValues.total,
-            others: quickValues.other,
-            missing: quickValues.missing,
-            terms: quickValues.terms
-        });
+    componentDidMount() {
+        var tableDomNode = this.getDOMNode().getElementsByTagName("table")[0];
+
+        this.dataTable = dc.dataTable(tableDomNode);
+        this.dataTable
+            .dimension(this.dimension)
+            .group((d) => {
+                var topValues = this.dimension.top(this.NUMBER_OF_TOP_VALUES);
+                var dInTopValues = topValues.some((value) => d.term.localeCompare(value.term) === 0);
+                return dInTopValues ? "Top values" : "Others";
+            })
+            .size(50)
+            .columns([
+                (d) => d.term,
+                (d) => {
+                    var total = this.state.total - this.state.missing;
+                    return this._formatPercentage(d.count / total);
+                },
+                (d) => this._formatCount(d.count)
+            ])
+            .sortBy((d) => d.count)
+            .order(d3.descending)
+            .on('renderlet', (table) => {
+                table.selectAll(".dc-table-group").classed("info", true);
+            });
+
+        this.dataTable.render();
     },
     _formatPercentage(percentage) {
         try {
@@ -43,49 +58,46 @@ var QuickValuesVisualization = React.createClass({
     _formatCount(count) {
         try {
             return numeral(count).format("0,0");
-        } catch(e) {
+        } catch (e) {
             return count;
         }
     },
-    _getTableData() {
-        var rows = [];
-        var values = Object.keys(this.state.terms);
+    componentWillReceiveProps(newProps) {
+        var quickValues = newProps.data;
 
-        if (values.length === 0) {
-            return [];
-        }
+        var terms = Object.keys(quickValues.terms);
 
-        values.sort((a, b) => Number(this.state.terms[b]) - Number(this.state.terms[a]));
-
-        values.forEach((term) => {
-            var count = this.state.terms[term];
-            var percentage = count / this.state.total;
-            rows.push(
-                <TableRowWrapper key={term}
-                                 term={term}
-                                 percentage={this._formatPercentage(percentage)}
-                                 count={this._formatCount(count)}/>
-            );
+        var formattedTerms = terms.map((term) => {
+            var count = quickValues.terms[term];
+            return {
+                term: term,
+                count: count
+            };
         });
 
-        return rows;
+        this.setState({
+            total: quickValues.total,
+            others: quickValues.other,
+            missing: quickValues.missing,
+            terms: formattedTerms
+        }, this.drawData);
+    },
+    drawData() {
+        this.quickValuesData.remove();
+        this.quickValuesData.add(this.state.terms);
+        this.dataTable.redraw();
     },
     render() {
-        var tableData = this._getTableData();
         return (
-            <div className="quickvalues-visualization">
-                <table className="table table-condensed table-hover table-striped">
+            <div id={"visualization-" + this.props.id} className="quickvalues-visualization">
+                <table className="table table-condensed table-striped table-hover">
                     <thead>
-                        <tr>
-                            <th style={{width: "225px"}}>Value</th>
-                            <th style={{width: "50px"}}>%</th>
-                            <th>Count</th>
-                        </tr>
+                    <tr>
+                        <th style={{width: "225px"}}>Value</th>
+                        <th style={{width: "50px"}}>%</th>
+                        <th>Count</th>
+                    </tr>
                     </thead>
-
-                    <tbody>
-                        {tableData}
-                    </tbody>
                 </table>
             </div>
         );
