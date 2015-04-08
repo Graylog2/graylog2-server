6dfcdce5e2b5fd8e9109f5983084fff15eb5239a
Edmundo Alvarez <edmundo@graylog.com>
2015-04-08 13:37:16 +0200

    Extract tooltipRenderlet into D3Utils

--- javascript/src/util/D3Utils.js
+++ javascript/src/util/D3Utils.js
@@ -7,6 +7,14 @@ var D3Utils = {
         return d3.scale.ordinal().range(['#16ACE3', '#6DCFF6', '#F7941E', '#FBB040', '#BE1E2D',
                                          '#FF3B00', '#333333', '#9E1F63', '#8DC63F', '#BFD730',
                                          '#D7DF23', '#E3E5E5', '#F1F2F2']);
+    },
+    // Add a data element to the given D3 selection to show a bootstrap tooltip
+    tooltipRenderlet(graph, selector, callback) {
+        graph.on('renderlet', (chart) => {
+            d3.select(chart.root()[0][0]).selectAll(selector)
+                .attr('rel', 'tooltip')
+                .attr('data-original-title', callback);
+        });
     }
 };
 
