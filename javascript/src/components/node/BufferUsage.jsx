c3b5549715a5802d6caf7d065cd508d07b1dbf03
Kay Roepke <kroepke@googlemail.com>
2015-04-09 11:55:04 +0200

    update buffer utilization automatically
    extract common component to render the buffer util boxes using react
    
    #1203

---
+++ javascript/src/components/node/BufferUsage.jsx
@@ -0,0 +1,81 @@
+/* global jsRoutes */
+
+'use strict';
+
+var React = require('react');
+var ProgressBar = require('react-bootstrap').ProgressBar;
+var Button = require('react-bootstrap').Button;
+//noinspection JSUnusedGlobalSymbols
+var MetricsStore = require('../../stores/metrics/MetricsStore');
+var numeral = require('numeral');
+
+var metricsStore = MetricsStore.instance;
+
+var BufferUsage = React.createClass({
+    getInitialState() {
+        return ({
+            usagePercentage: 0,
+            usage: 0,
+            size: 0,
+            hasError: false,
+            initialized: true
+        });
+    },
+    componentWillMount() {
+        var metricNames = [
+            this._metricPrefix() + 'usage',
+            this._metricPrefix() + 'size'
+        ];
+        metricsStore.listen({
+            nodeId: this.props.nodeId,
+            metricNames: metricNames,
+            callback: (update, hasError) => {
+                if (hasError) {
+                    this.setState({hasError: hasError});
+                }
+                // update is [{nodeId, values: [{name, value: {metric}}]} ...]
+                // metric can be various different things, depending on metric {type: "GAUGE"|"COUNTER"|"METER"|"TIMER"}
+
+                var usage = 0;
+                var size = 0;
+                // we will only get one result, because we ask for only one node
+                update[0].values.forEach((namedMetric) => {
+                    if (namedMetric.name === this._metricPrefix() + 'usage') {
+                        usage = namedMetric.metric.value;
+                    }
+                    if (namedMetric.name === this._metricPrefix() + 'size') {
+                        size = namedMetric.metric.value;
+                    }
+                });
+                var usagePercentage = size === 0 ? 0 : (usage / size) * 100;
+                this.setState({
+                    initialized: true,
+                    usagePercentage: usagePercentage,
+                    usage: usage,
+                    size: size,
+                    hasError: hasError
+                });
+            }
+        });
+    },
+    render() {
+        var percentLabel = numeral(this.state.usagePercentage / 100).format('0.0 %');
+        return (
+            <div>
+                <Button href={jsRoutes.controllers.MetricsController.ofNode(this.props.nodeId, this._metricPrefix()).url} bsSize="xsmall" className="pull-right">Metrics</Button>
+                <h3>{this.props.title}</h3>
+                <div className="node-buffer-usage">
+                    <ProgressBar now={numeral(this.state.usagePercentage).format('0.0')}
+                                 bsStyle='warning'
+                                 label={percentLabel} />
+                </div>
+                <span><strong>{this.state.usage} messages</strong> in {this.props.title.toLowerCase()}, {percentLabel} utilized.</span>
+            </div>);
+    },
+    _metricPrefix() {
+        return 'org.graylog2.buffers.' + this.props.bufferType + '.';
+    }
+
+});
+
+module.exports = BufferUsage;
--- javascript/src/components/node/mount.jsx
+++ javascript/src/components/node/mount.jsx
@@ -2,6 +2,7 @@
 
 var React = require('react/addons');
 var JvmHeapUsage = require('./JvmHeapUsage');
+var BufferUsage = require('./BufferUsage');
 
 var heapUsage = document.getElementsByClassName('react-jvm-heap');
 if (heapUsage) {
@@ -9,5 +10,16 @@ if (heapUsage) {
         var elem = heapUsage[i];
         var id = elem.getAttribute('data-node-id');
         React.render(<JvmHeapUsage nodeId={id}/>, elem);
+    }
+}
+
+var buffers = document.getElementsByClassName('react-buffer-usage');
+if (buffers) {
+    for (var i = 0; i < buffers.length; i++) {
+        var elem = buffers[i];
+        var id = elem.getAttribute('data-node-id');
+        var title = elem.getAttribute('data-title');
+        var bufferType = elem.getAttribute('data-buffer-type');
+        React.render(<BufferUsage nodeId={id} title={title} bufferType={bufferType}/>, elem);
     }
 }
