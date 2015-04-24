98942aee4ef07269e759243c512ffbc70b8aebff
Dennis Oelkers <dennis@torch.sh>
2015-04-24 12:02:56 +0200

    Adding version classifier.

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ import com.typesafe.sbt.less.Import.LessKeys
 
 object ApplicationBuild extends Build {
   val appName         = "graylog-web-interface"
-  val appVersion      = "1.1.0-SNAPSHOT"
+  val appVersion      = "1.1.0-editable-alarmcallbacks-SNAPSHOT"
   val appDependencies = Seq(
     cache,
     javaCore,
