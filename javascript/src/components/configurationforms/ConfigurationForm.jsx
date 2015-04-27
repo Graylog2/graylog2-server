721e2c499b45e85acba2ceb1e72a542f832c7a55
Dennis Oelkers <dennis@torch.sh>
2015-04-27 10:44:58 +0200

    Separating props and state in ConfigurationForm.

--- javascript/src/components/configurationforms/ConfigurationForm.jsx
+++ javascript/src/components/configurationforms/ConfigurationForm.jsx
@@ -14,20 +14,15 @@ var ConfigurationForm = React.createClass({
     getDefaultProps() {
         return {
             values: {},
-            includeTitleField: true
+            includeTitleField: true,
+            titleValue: ""
         };
     },
     _copyStateFromProps(props) {
         return {
             configFields: $.extend({}, props.configFields),
-            title: props.title,
-            typeName: props.typeName,
-            formId: props.formId,
-            submitAction: props.submitAction,
-            helpBlock: props.helpBlock,
             values: $.extend({}, props.values),
-            titleValue: props.titleValue,
-            includeTitleField: props.includeTitleField
+            titleValue: this.props.titleValue
         };
     },
     getInitialState() {
@@ -40,10 +35,10 @@ var ConfigurationForm = React.createClass({
 		this.setState(this._copyStateFromProps(props));
 	},
     render() {
-        var typeName = this.state.typeName;
+        var typeName = this.props.typeName;
         var configFields = $.map(this.state.configFields, this._renderConfigField);
-        var title = this.state.title;
-        var helpBlock = this.state.helpBlock;
+        var title = this.props.title;
+        var helpBlock = this.props.helpBlock;
         var header = (
             <h2 className="modal-title">
                 <i className="fa fa-signin"></i>
@@ -52,7 +47,7 @@ var ConfigurationForm = React.createClass({
         );
 
         var titleField = {is_optional: false, attributes: [], human_name: "Title", description: helpBlock};
-        var titleElement = (this.state.includeTitleField ? <TextField key={typeName + "-title"} typeName={typeName} title="title"
+        var titleElement = (this.props.includeTitleField ? <TextField key={typeName + "-title"} typeName={typeName} title="title"
                                       field={titleField} value={this.state.titleValue} onChange={this._handleTitleChange}/> : "");
 
         var body = (
@@ -73,7 +68,7 @@ var ConfigurationForm = React.createClass({
     _save(evt) {
         var values = this.state.values;
         var data = {title: this.state.titleValue,
-            type: this.state.typeName,
+            type: this.props.typeName,
             configuration: {}};
         $.map(this.state.configFields, function(field, name) {
             data.configuration[name] = (values[name] || field.default_value);
@@ -100,15 +95,20 @@ var ConfigurationForm = React.createClass({
     },
     _renderConfigField(configField, key) {
         var value = this.state.values[key];
+        var typeName = this.props.typeName;
         switch(configField.type) {
             case "text":
-                return (<TextField key={this.state.typeName + "-" + key} typeName={this.state.typeName} title={key} field={configField} value={value} onChange={this._handleChange}/>);
+                return (<TextField key={typeName + "-" + key} typeName={typeName} title={key} field={configField}
+                                   value={value} onChange={this._handleChange}/>);
             case "number":
-                return (<NumberField key={this.state.typeName + "-" + key} typeName={this.state.typeName} title={key} field={configField} value={value} onChange={this._handleChange}/>);
+                return (<NumberField key={typeName + "-" + key} typeName={typeName} title={key} field={configField}
+                                     value={value} onChange={this._handleChange}/>);
             case "boolean":
-                return (<BooleanField key={this.state.typeName + "-" + key} typeName={this.state.typeName} title={key} field={configField} value={value} onChange={this._handleChange}/>);
+                return (<BooleanField key={typeName + "-" + key} typeName={typeName} title={key} field={configField}
+                                      value={value} onChange={this._handleChange}/>);
             case "dropdown":
-                return (<DropdownField key={this.state.typeName + "-" + key} typeName={this.state.typeName} title={key} field={configField} value={value} onChange={this._handleChange}/>);
+                return (<DropdownField key={typeName + "-" + key} typeName={typeName} title={key} field={configField}
+                                       value={value} onChange={this._handleChange}/>);
         }
     }
 });
