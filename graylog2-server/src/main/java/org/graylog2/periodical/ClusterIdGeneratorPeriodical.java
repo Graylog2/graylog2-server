3b0ca53ba62720395fde0cdb744e09245d0974d4
Jochen Schalanda <jochen@graylog.com>
2015-04-28 15:03:02 +0200

    Add ClusterId and ClusterIdGeneratorPeriodical

---
+++ graylog2-server/src/main/java/org/graylog2/periodical/ClusterIdGeneratorPeriodical.java
@@ -0,0 +1,87 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.periodical;
+
+import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.graylog2.plugin.cluster.ClusterId;
+import org.graylog2.plugin.periodical.Periodical;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+public class ClusterIdGeneratorPeriodical extends Periodical {
+    private static final Logger LOG = LoggerFactory.getLogger(ClusterIdGeneratorPeriodical.class);
+
+    private final ClusterConfigService clusterConfigService;
+
+    @Inject
+    public ClusterIdGeneratorPeriodical(ClusterConfigService clusterConfigService) {
+        this.clusterConfigService = clusterConfigService;
+    }
+
+    @Override
+    public boolean runsForever() {
+        return true;
+    }
+
+    @Override
+    public boolean stopOnGracefulShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean masterOnly() {
+        return true;
+    }
+
+    @Override
+    public boolean startOnThisNode() {
+        return true;
+    }
+
+    @Override
+    public boolean isDaemon() {
+        return true;
+    }
+
+    @Override
+    public int getInitialDelaySeconds() {
+        return 0;
+    }
+
+    @Override
+    public int getPeriodSeconds() {
+        return 0;
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return LOG;
+    }
+
+    @Override
+    public void doRun() {
+        if(clusterConfigService.get(ClusterId.class) == null) {
+            ClusterId clusterId = ClusterId.create(UUID.randomUUID().toString());
+            clusterConfigService.write(clusterId);
+
+            LOG.debug("Generated cluster ID {}", clusterId.clusterId());
+        }
+    }
+}
