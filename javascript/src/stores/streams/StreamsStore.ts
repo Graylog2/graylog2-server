d02ba57488e1b0dd0a165cf03bd760e37395dcdf
Dennis Oelkers <dennis@torch.sh>
2015-04-28 13:51:38 +0200

    Adding StreamsStore component for js streams access.

--- javascript/src/stores/streams/StreamsStore.ts
+++ javascript/src/stores/streams/StreamsStore.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 declare var jsRoutes: any;
+declare var $: any;
 
 import UserNotification = require("../../util/UserNotification");
 import URLUtils = require("../../util/URLUtils");
@@ -24,6 +25,60 @@ var StreamsStore = {
         $.ajax({
             type: "DELETE",
             url: url
+        }).done(callback).fail(failCallback);
+    },
+    resume(streamId: string, callback: (() => void)) {
+        var failCallback = (jqXHR, textStatus, errorThrown) => {
+            UserNotification.error("Resuming Stream failed with status: " + errorThrown,
+                "Could not resume Stream!");
+        };
+
+        var url = jsRoutes.controllers.api.StreamsApiController.resume(streamId).url;
+        $.ajax({
+            type: "POST",
+            url: url
+        }).done(callback).fail(failCallback);
+    },
+    save(stream: any, callback: ((streamId: string) => void)) {
+        var failCallback = (jqXHR, textStatus, errorThrown) => {
+            UserNotification.error("Saving Stream failed with status: " + errorThrown,
+                "Could not save Stream!");
+        };
+
+        var url = jsRoutes.controllers.api.StreamsApiController.create().url;
+        $.ajax({
+            type: "POST",
+            url: url,
+            contentType: "application/json",
+            data: JSON.stringify(stream)
+        }).done(callback).fail(failCallback);
+    },
+    update(streamId: string, data: any, callback: (() => void)) {
+        var failCallback = (jqXHR, textStatus, errorThrown) => {
+            UserNotification.error("Updating Stream failed with status: " + errorThrown,
+                "Could not update Stream!");
+        };
+
+        var url = jsRoutes.controllers.api.StreamsApiController.update(streamId).url;
+        $.ajax({
+            type: "PUT",
+            url: url,
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        }).done(callback).fail(failCallback);
+    },
+    cloneStream(streamId: string, data: any, callback: (() => void)) {
+        var failCallback = (jqXHR, textStatus, errorThrown) => {
+            UserNotification.error("Cloning Stream failed with status: " + errorThrown,
+                "Could not clone Stream!");
+        };
+
+        var url = jsRoutes.controllers.api.StreamsApiController.cloneStream(streamId).url;
+        $.ajax({
+            type: "POST",
+            url: url,
+            contentType: "application/json",
+            data: JSON.stringify(data)
         }).done(callback).fail(failCallback);
     }
 };
