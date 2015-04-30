c5dd68db7c94024bb125ec67aad809745487a07d
Dennis Oelkers <dennis@torch.sh>
2015-04-30 14:47:46 +0200

    Fixing bug where opening the edit dialog needed a second click.
    Instead of generating the population form on demand (when clicking)
    we are now rendering it straight away. It was done on demand when we
    needed to do a request to fetch the configuration details. Now that
    these are passed in for all types, we can just render the form.
    
    (cherry picked from commit 3192679b633c8fd8275fe05ea99f36596e3b5a96)

--- javascript/src/components/alarmcallbacks/EditAlarmCallbackButton.jsx
+++ javascript/src/components/alarmcallbacks/EditAlarmCallbackButton.jsx
@@ -4,39 +4,26 @@ var React = require('react/addons');
 var ConfigurationForm = require('../configurationforms/ConfigurationForm');
 
 var EditAlarmCallbackButton = React.createClass({
-    getInitialState() {
-        return {
-            typeDefinition: undefined,
-            typeName: undefined
-        };
-    },
     _handleClick() {
-        var alarmCallback = this.props.alarmCallback;
-        var definition = this.props.types[alarmCallback.type];
-        if (definition) {
-            this.setState({typeDefinition: definition.requested_configuration});
-            this.refs.configurationForm.open();
-        }
+        this.refs.configurationForm.open();
     },
     _handleSubmit(data) {
         this.props.onUpdate(this.props.alarmCallback, data);
     },
     render() {
-        var typeDefinition = this.state.typeDefinition;
         var alarmCallback = this.props.alarmCallback;
-        var configurationForm = (typeDefinition ?
-            <ConfigurationForm ref="configurationForm" key={"configuration-form-alarm-callback-"+alarmCallback.id} configFields={this.state.typeDefinition}
-                               title={"Editing Alarm Callback "}
-                               typeName={alarmCallback.type} includeTitleField={false}
-                               submitAction={this._handleSubmit} values={alarmCallback.configuration} />
-            : ""
-        );
+        var definition = this.props.types[alarmCallback.type];
+
         return (
             <span>
                 <button className="btn btn-success btn-xs" onClick={this._handleClick}>
                     <i className="fa fa-edit"></i>  Edit
                 </button>
-                {configurationForm}
+                <ConfigurationForm ref="configurationForm" key={"configuration-form-alarm-callback-"+alarmCallback.id}
+                                   configFields={definition.requested_configuration}
+                                   title={"Editing Alarm Callback "}
+                                   typeName={alarmCallback.type} includeTitleField={false}
+                                   submitAction={this._handleSubmit} values={alarmCallback.configuration} />
             </span>
         );
     }
