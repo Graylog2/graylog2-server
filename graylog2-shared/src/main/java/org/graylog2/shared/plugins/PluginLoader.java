4b168d8bbdab70bfa81a1a7efa9e31078d4f517c
Jochen Schalanda <jochen@graylog.com>
2015-04-30 15:57:37 +0200

    Use correct Preconditions class in PluginLoader

--- graylog2-shared/src/main/java/org/graylog2/shared/plugins/PluginLoader.java
+++ graylog2-shared/src/main/java/org/graylog2/shared/plugins/PluginLoader.java
@@ -38,7 +38,7 @@ import java.util.Objects;
 import java.util.ServiceLoader;
 import java.util.Set;
 
-import static com.google.inject.internal.util.$Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class PluginLoader {
     private static final Logger LOG = LoggerFactory.getLogger(PluginLoader.class);
