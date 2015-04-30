f1db4938a1950700223c7e8ce10c43632ca62ea1
Dennis Oelkers <dennis@graylog.com>
2015-04-30 09:38:16 +0200

    Allowing toggling between relative and absolute time display.

--- javascript/src/components/agents/AgentRow.jsx
+++ javascript/src/components/agents/AgentRow.jsx
@@ -1,8 +1,14 @@
 'use strict';
 
 var React = require('react/addons');
+var moment = require('moment');
 
 var AgentRow = React.createClass({
+    getInitialState() {
+        return {
+            showRelativeTime: true
+        };
+    },
     _getOsGlyph(operatingSystem) {
         var glyphClass = "fa-question-circle";
         var os = operatingSystem.trim().toLowerCase();
@@ -18,12 +24,16 @@ var AgentRow = React.createClass({
 
         return (<i className={"fa " + glyphClass}></i>);
     },
+    _toggleRelativeTime() {
+        this.setState({showRelativeTime: !this.state.showRelativeTime});
+    },
     render() {
         var agent = this.props.agent;
         var agentClass = agent.active ? "" : "greyed-out inactive";
         var style = {};
         var annotation = agent.active ? "" : "(inactive)";
         var osGlyph = this._getOsGlyph(agent.node_details.operating_system);
+        var formattedTime = (this.state.showRelativeTime ? moment(agent.last_seen).fromNow() :  momentHelper.toUserTimeZone(moment(agent.last_seen)).format());
         return (
             <tr className={agentClass} style={style}>
                 <td className="limited">
@@ -41,7 +51,7 @@ var AgentRow = React.createClass({
                     {agent.node_details.operating_system}
                 </td>
                 <td className="limited">
-                    {moment(agent.last_seen).fromNow()}
+                    <time dateTime={agent.last_seen} onClick={this._toggleRelativeTime}>{formattedTime}</time>
                 </td>
             </tr>
         );
