43205365370c602bf47acdc8d4a97cd06dbd4d2b
Jochen Schalanda <jochen@graylog.com>
2015-04-30 17:18:58 +0200

    Configure Guice injector to use Stage.PRODUCTION
    We actually *do* want to fail fast on injection errors, even if the bindings
    haven't been created with `asEagerSingleton()`, see https://github.com/google/guice/wiki/Scopes#eager-singletons

--- graylog2-server/src/main/java/org/graylog2/ESTimestampFixup.java
+++ graylog2-server/src/main/java/org/graylog2/ESTimestampFixup.java
@@ -33,6 +33,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Scopes;
+import com.google.inject.Stage;
 import org.apache.log4j.Level;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
@@ -169,7 +170,7 @@ public class ESTimestampFixup {
         final Configuration configuration = readConfiguration(jadConfig, commandLineOptions);
 
         final GuiceInstantiationService instantiationService = new GuiceInstantiationService();
-        final Injector injector = Guice.createInjector(new Bindings(configuration));
+        final Injector injector = Guice.createInjector(Stage.PRODUCTION, new Bindings(configuration));
 
         instantiationService.setInjector(injector);
 
