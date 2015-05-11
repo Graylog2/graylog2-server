2a4f0e0ce02f9606d95e83c0384584e905894209
Jochen Schalanda <jochen@graylog.com>
2015-05-11 11:52:45 +0200

    Increase default RecvBufferSize for UDP transports to 64K
    While the receive buffer size for UDP-based transports could be configured for each transport,
    the effective size for the receive buffer was fixed to 8192 bytes by using a FixedReceiveBufferSizePredictor
    with a hard-coded value.
    
    By changing the default receive buffer size for UDP-based transports to 65535 bytes and using the
    configured receive buffer size as value for the FixedReceiveBufferSizePredictor, the maximum UDP packet
    size that can be processed by UDP-based transports is now finally configurable.
    
    As the practical maximum for a UDP packet is 65507 bytes, the default of 65535 bytes for the receive buffer
    should be fine (at the cost of increased memory allocations).
    
    Fixes #1143

--- graylog2-inputs/src/main/java/org/graylog2/inputs/transports/UdpTransport.java
+++ graylog2-inputs/src/main/java/org/graylog2/inputs/transports/UdpTransport.java
@@ -14,33 +14,18 @@
  * You should have received a copy of the GNU General Public License
  * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- *
- * This file is part of Graylog.
- *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.inputs.transports;
 
 import com.codahale.metrics.InstrumentedExecutorService;
+import com.google.common.primitives.Ints;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
-import org.graylog2.plugin.inputs.annotations.ConfigClass;
-import org.graylog2.plugin.inputs.annotations.FactoryClass;
 import org.graylog2.plugin.LocalMetricRegistry;
 import org.graylog2.plugin.configuration.Configuration;
+import org.graylog2.plugin.configuration.ConfigurationRequest;
+import org.graylog2.plugin.inputs.annotations.ConfigClass;
+import org.graylog2.plugin.inputs.annotations.FactoryClass;
 import org.graylog2.plugin.inputs.transports.NettyTransport;
 import org.graylog2.plugin.inputs.transports.Transport;
 import org.graylog2.plugin.inputs.util.ThroughputCounter;
@@ -77,11 +62,11 @@ public class UdpTransport extends NettyTransport {
 
     @Override
     public Bootstrap getBootstrap() {
-        final ConnectionlessBootstrap bootstrap =
-                new ConnectionlessBootstrap(new NioDatagramChannelFactory(workerExecutor));
+        final ConnectionlessBootstrap bootstrap = new ConnectionlessBootstrap(new NioDatagramChannelFactory(workerExecutor));
 
-        bootstrap.setOption("receiveBufferSizePredictorFactory", new FixedReceiveBufferSizePredictorFactory(8192));
-        bootstrap.setOption("receiveBufferSize", getRecvBufferSize());
+        final int recvBufferSize = Ints.saturatedCast(getRecvBufferSize());
+        bootstrap.setOption("receiveBufferSizePredictorFactory", new FixedReceiveBufferSizePredictorFactory(recvBufferSize));
+        bootstrap.setOption("receiveBufferSize", recvBufferSize);
 
         return bootstrap;
     }
@@ -95,8 +80,15 @@ public class UdpTransport extends NettyTransport {
         Config getConfig();
     }
 
-    // only here for consistency
     @ConfigClass
     public static class Config extends NettyTransport.Config {
+        @Override
+        public ConfigurationRequest getRequestedConfiguration() {
+            final ConfigurationRequest r = super.getRequestedConfiguration();
+
+            r.addField(ConfigurationRequest.Templates.recvBufferSize(CK_RECV_BUFFER_SIZE, 65535));
+
+            return r;
+        }
     }
 }
---
+++ graylog2-inputs/src/test/java/org/graylog2/inputs/transports/UdpTransportTest.java
@@ -0,0 +1,211 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.inputs.transports;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.Callables;
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.graylog2.plugin.LocalMetricRegistry;
+import org.graylog2.plugin.configuration.Configuration;
+import org.graylog2.plugin.configuration.ConfigurationRequest;
+import org.graylog2.plugin.inputs.MessageInput;
+import org.graylog2.plugin.inputs.MisfireException;
+import org.graylog2.plugin.inputs.transports.NettyTransport;
+import org.graylog2.plugin.inputs.util.ThroughputCounter;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.ChannelHandler;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.FixedReceiveBufferSizePredictorFactory;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.ReceiveBufferSizePredictorFactory;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.jboss.netty.channel.UpstreamMessageEvent;
+import org.jboss.netty.util.HashedWheelTimer;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UdpTransportTest {
+    private static final String BIND_ADDRESS = "127.0.0.1";
+    private static final int PORT = 5555;
+    private static final int RECV_BUFFER_SIZE = 1024;
+    private static final ImmutableMap<String, Object> CONFIG_SOURCE = ImmutableMap.<String, Object>of(
+            NettyTransport.CK_BIND_ADDRESS, BIND_ADDRESS,
+            NettyTransport.CK_PORT, PORT,
+            NettyTransport.CK_RECV_BUFFER_SIZE, RECV_BUFFER_SIZE);
+    private static final Configuration CONFIGURATION = new Configuration(CONFIG_SOURCE);
+
+    private UdpTransport udpTransport;
+    private ThroughputCounter throughputCounter;
+    private LocalMetricRegistry localMetricRegistry;
+
+    @Before
+    public void setUp() throws Exception {
+        throughputCounter = new ThroughputCounter(new HashedWheelTimer());
+        localMetricRegistry = new LocalMetricRegistry();
+        udpTransport = new UdpTransport(CONFIGURATION, throughputCounter, localMetricRegistry);
+    }
+
+    @Test
+    public void transportReceivesDataSmallerThanRecvBufferSize() throws Exception {
+        final CountingChannelUpstreamHandler handler = new CountingChannelUpstreamHandler();
+        final UdpTransport transport = launchTransportForBootStrapTest(handler);
+
+        sendUdpDatagram(BIND_ADDRESS, PORT, 100);
+        await().atMost(5, TimeUnit.SECONDS).until(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return !handler.getBytesWritten().isEmpty();
+            }
+        });
+        transport.stop();
+
+        assertThat(handler.getBytesWritten()).containsOnly(100);
+    }
+
+    @Test
+    public void transportReceivesDataExactlyRecvBufferSize() throws Exception {
+        final CountingChannelUpstreamHandler handler = new CountingChannelUpstreamHandler();
+        final UdpTransport transport = launchTransportForBootStrapTest(handler);
+
+        // This will be variable depending on the version of the IP protocol and the UDP packet size.
+        final int udpOverhead = 16;
+        final int maxPacketSize = RECV_BUFFER_SIZE - udpOverhead;
+
+        sendUdpDatagram(BIND_ADDRESS, PORT, maxPacketSize);
+        await().atMost(5, TimeUnit.SECONDS).until(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return !handler.getBytesWritten().isEmpty();
+            }
+        });
+        transport.stop();
+
+        assertThat(handler.getBytesWritten()).containsOnly(maxPacketSize);
+    }
+
+    @Test
+    public void transportDiscardsDataLargerRecvBufferSize() throws Exception {
+        final CountingChannelUpstreamHandler handler = new CountingChannelUpstreamHandler();
+        final UdpTransport transport = launchTransportForBootStrapTest(handler);
+
+        sendUdpDatagram(BIND_ADDRESS, PORT, 2 * RECV_BUFFER_SIZE);
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        transport.stop();
+
+        assertThat(handler.getBytesWritten()).isEmpty();
+    }
+
+    private UdpTransport launchTransportForBootStrapTest(final ChannelHandler channelHandler) throws MisfireException {
+        final UdpTransport transport = new UdpTransport(CONFIGURATION, throughputCounter, new LocalMetricRegistry()) {
+            @Override
+            protected LinkedHashMap<String, Callable<? extends ChannelHandler>> getBaseChannelHandlers(MessageInput input) {
+                final LinkedHashMap<String, Callable<? extends ChannelHandler>> handlers = new LinkedHashMap<>();
+                handlers.put("counter", Callables.returning(channelHandler));
+                handlers.putAll(super.getFinalChannelHandlers(input));
+                return handlers;
+            }
+        };
+
+        final MessageInput messageInput = mock(MessageInput.class);
+        when(messageInput.getId()).thenReturn("TEST");
+        when(messageInput.getName()).thenReturn("TEST");
+
+        transport.launch(messageInput);
+
+        return transport;
+    }
+
+    @Test
+    public void receiveBufferSizeIsDefaultSize() throws Exception {
+        assertThat(udpTransport.getBootstrap().getOption("receiveBufferSize")).isEqualTo(RECV_BUFFER_SIZE);
+    }
+
+    @Test
+    public void receiveBufferSizePredictorIsUsingDefaultSize() throws Exception {
+        ReceiveBufferSizePredictorFactory receiveBufferSizePredictorFactory =
+                (ReceiveBufferSizePredictorFactory) udpTransport.getBootstrap().getOption("receiveBufferSizePredictorFactory");
+        assertThat(receiveBufferSizePredictorFactory).isInstanceOf(FixedReceiveBufferSizePredictorFactory.class);
+        assertThat(receiveBufferSizePredictorFactory.getPredictor().nextReceiveBufferSize()).isEqualTo(RECV_BUFFER_SIZE);
+    }
+
+    @Test
+    public void getMetricSetReturnsLocalMetricRegistry() {
+        assertThat(udpTransport.getMetricSet()).isSameAs(localMetricRegistry);
+    }
+
+    @Test
+    public void testDefaultReceiveBufferSize() throws Exception {
+        final UdpTransport.Config config = new UdpTransport.Config();
+        final ConfigurationRequest requestedConfiguration = config.getRequestedConfiguration();
+
+        assertThat(requestedConfiguration.getField(NettyTransport.CK_RECV_BUFFER_SIZE).getDefaultValue()).isEqualTo(65535);
+    }
+
+    private void sendUdpDatagram(String hostname, int port, int size) throws IOException {
+        final InetAddress address = InetAddress.getByName(hostname);
+        final byte[] data = new byte[size];
+        final DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+
+        DatagramSocket toSocket = null;
+        try {
+            toSocket = new DatagramSocket();
+            toSocket.send(packet);
+        } finally {
+            if (toSocket != null) {
+                toSocket.close();
+            }
+        }
+    }
+
+    public static class CountingChannelUpstreamHandler extends SimpleChannelUpstreamHandler {
+        private final List<Integer> bytesWritten = new ArrayList<>();
+
+        @Override
+        public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+            if (e instanceof UpstreamMessageEvent) {
+                ChannelBuffer buffer = (ChannelBuffer) e.getMessage();
+                try {
+                    synchronized (bytesWritten) {
+                        bytesWritten.add(buffer.readableBytes());
+                    }
+                } finally {
+                    super.messageReceived(ctx, e);
+                }
+            }
+        }
+
+        public List<Integer> getBytesWritten() {
+            return bytesWritten;
+        }
+    }
+}
