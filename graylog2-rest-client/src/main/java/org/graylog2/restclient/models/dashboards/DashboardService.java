e379c5ebcbb475a14c1798f0abe189683f6e1789
Edmundo Alvarez <edmundo@graylog.com>
2015-05-18 17:22:28 +0200

    Return dashboard ID after creation on rest client

--- graylog2-rest-client/src/main/java/org/graylog2/restclient/models/dashboards/DashboardService.java
+++ graylog2-rest-client/src/main/java/org/graylog2/restclient/models/dashboards/DashboardService.java
@@ -17,7 +17,7 @@
 package org.graylog2.restclient.models.dashboards;
 
 import com.google.common.collect.Lists;
-import javax.inject.Inject;
+import org.graylog2.rest.models.dashboards.responses.CreateDashboardResponse;
 import org.graylog2.restclient.lib.APIException;
 import org.graylog2.restclient.lib.ApiClient;
 import org.graylog2.restclient.models.api.requests.dashboards.CreateDashboardRequest;
@@ -26,12 +26,10 @@ import org.graylog2.restclient.models.api.responses.dashboards.GetDashboardsResp
 import org.graylog2.restroutes.generated.routes;
 import play.mvc.Http;
 
+import javax.inject.Inject;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 public class DashboardService {
 
     private final ApiClient api;
@@ -68,12 +66,14 @@ public class DashboardService {
         return dashboards;
     }
 
-    public void create(CreateDashboardRequest request) throws APIException, IOException {
-        api.path(routes.DashboardsResource().create())
+    public String create(CreateDashboardRequest request) throws APIException, IOException {
+        CreateDashboardResponse response = api.path(routes.DashboardsResource().create(), CreateDashboardResponse.class)
                 .onlyMasterNode()
                 .body(request)
                 .expect(Http.Status.CREATED)
                 .execute();
+
+        return response.dashboardId();
     }
 
     public void delete(String id) throws APIException, IOException {
