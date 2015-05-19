1ecf80c5dc809222793aa784625c0d0c0293fb5b
Kay Roepke <kroepke@googlemail.com>
2015-05-19 16:50:01 +0200

    auto-reload quickvalues and show spinner when load is pending
     - the placement of the spinner needs some improvement, it's not consistent across components
    
    fixes #1298

--- javascript/src/components/search/FieldQuickValues.jsx
+++ javascript/src/components/search/FieldQuickValues.jsx
@@ -6,6 +6,7 @@ var Button = require('react-bootstrap').Button;
 var QuickValuesVisualization = require('../visualizations/QuickValuesVisualization');
 var AddToDashboardMenu = require('../dashboard/AddToDashboardMenu');
 var Widget = require('../widgets/Widget');
+var Spinner = require('../common/Spinner');
 
 var FieldQuickValuesStore = require('../../stores/field-analyzers/FieldQuickValuesStore');
 
@@ -13,21 +14,46 @@ var FieldQuickValues = React.createClass({
     getInitialState() {
         return {
             field: undefined,
+            autoReload: true,
             data: []
         };
+    },
+
+    componentDidMount() {
+        this.updateIntervalId = window.setInterval(() => this._loadQuickValuesData(), 3000);
+    },
+    componentWillUnmount() {
+        window.clearInterval(this.updateIntervalId);
+    },
+    _toggleAutoReload() {
+        var shouldAutoReload = !this.state.autoReload;
+        this.setState({autoReload: shouldAutoReload});
     },
     addFieldQuickValues(field) {
         this.setState({field: field}, this._loadQuickValuesData);
     },
     _loadQuickValuesData() {
-        var promise = FieldQuickValuesStore.getQuickValues(this.state.field);
-        promise.done((data) => this.setState({data: data}));
+        if (this.state.field !== undefined && this.state.autoReload) {
+            this.setState({loadPending: true});
+            var promise = FieldQuickValuesStore.getQuickValues(this.state.field);
+            promise.done((data) => this.setState({data: data, loadPending: false}));
+        }
     },
     _resetStatus() {
         this.setState(this.getInitialState());
     },
     render() {
         var content;
+
+        var inner;
+        if (this.state.data.length === 0) {
+            inner = <Spinner />;
+        } else {
+            inner = <QuickValuesVisualization id={this.state.field}
+                                      horizontal={true}
+                                      config={{show_pie_chart: true, show_data_table: true}}
+                                      data={this.state.data}/>;
+        }
 
         if (this.state.field !== undefined) {
             content = (
@@ -39,21 +65,16 @@ var FieldQuickValues = React.createClass({
                                             bsStyle='default'
                                             pullRight={true}
                                             permissions={this.props.permissions}>
+                            <Button bsSize='small' onClick={() => this._toggleAutoReload()}>{this.state.autoReload ? "Stop reloading" : "Reload automatically"} </Button>
                             <Button bsSize='small' onClick={() => this._resetStatus()}>Dismiss</Button>
                         </AddToDashboardMenu>
                     </div>
-                    <h1>Field Statistics</h1>
+                    <h1>Field Statistics {this.state.loadPending && <i className="fa fa-spin fa-spinner"></i>}</h1>
 
-                    <div>
-                        <QuickValuesVisualization id={this.state.field}
-                                                  horizontal={true}
-                                                  config={{show_pie_chart: true, show_data_table: true}}
-                                                  data={this.state.data}/>
-                    </div>
+                    <div>{inner}</div>
                 </div>
             );
         }
-
         return <div id="field-quick-values">{content}</div>;
     }
 });
