2be77b1b1a16628ed6d632191a451ae0298da5be
Dennis Oelkers <dennis@graylog.com>
2015-05-27 14:22:59 +0200

    Allowing to change number of alerts per page.

--- javascript/src/components/alerts/AlertsComponent.jsx
+++ javascript/src/components/alerts/AlertsComponent.jsx
@@ -7,8 +7,10 @@ var Col = require('react-bootstrap').Col;
 var Spinner = require('../common/Spinner');
 var AlertsTable = require('./AlertsTable');
 var Paginator = require('../common/Paginator');
+var Input = require('react-bootstrap').Input;
 
 var AlertsComponent = React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
     getInitialState() {
         return {
             skip: 0,
@@ -18,15 +20,33 @@ var AlertsComponent = React.createClass({
     },
     _onSelected(pageNo) {
         this.setState({currentPage: pageNo});
-        this.loadData(pageNo);
+        this.loadData(pageNo, this.state.limit);
     },
     componentDidMount() {
-        this.loadData(this.state.currentPage);
+        this.loadData(this.state.currentPage, this.state.limit);
     },
-    loadData(pageNo) {
-        AlertsStore.list(this.props.streamId, (pageNo-1)*this.state.limit, this.state.limit).done((alerts) => {
+    loadData(pageNo, limit) {
+        AlertsStore.list(this.props.streamId, (pageNo-1)*limit, limit).done((alerts) => {
             this.setState({alerts: alerts});
         });
+    },
+    _onChangePageSize(e) {
+        var pageSize = parseInt(e.target.value);
+        var currentPage = Math.floor((this.state.currentPage-1)*this.state.limit/pageSize)+1;
+        this.setState({
+            limit: pageSize,
+            currentPage: currentPage
+        });
+        this.loadData(currentPage, pageSize);
+    },
+    _showPerPageSelect() {
+        return (
+            <Input type='select' onChange={this._onChangePageSize}>
+                <option value={10}>10</option>
+                <option value={50}>50</option>
+                <option value={100}>100</option>
+            </Input>
+        );
     },
     render() {
         if (this.state.alerts) {
@@ -37,9 +57,11 @@ var AlertsComponent = React.createClass({
                             Triggered alerts
 
                             <small>{this.state.alerts.total} alerts total</small>
+                            <div style={{float: "right"}}>
+                                {this._showPerPageSelect()}
+                            </div>
                         </h2>
                         {' '}
-
                         <AlertsTable alerts={this.state.alerts.alerts} />
                         <Paginator pages={Math.ceil(this.state.alerts.total/this.state.limit)} size={this.state.limit}
                                    currentPage={this.state.currentPage} onSelected={this._onSelected}/>
