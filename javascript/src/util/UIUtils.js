71559b803ef73be193c48df71d45ca145fcc6eb4
Edmundo Alvarez <edmundo@graylog.com>
2015-05-28 19:38:38 +0200

    Scroll to the element that displayed the indicator
    We need to use a different hint element to avoid conflicts with main.js
    
    Refs #1284

--- javascript/src/util/UIUtils.js
+++ javascript/src/util/UIUtils.js
@@ -3,16 +3,26 @@
 var $ = require('jquery');
 
 var UIUtils = {
-    NAVBAR_HEIGHT: 50,
+    NAVBAR_HEIGHT: 55,
     scrollToHint(element) {
         if (!this.isElementVisible(element)) {
-            $("#scroll-to-search-hint").fadeIn("fast").delay(1500).fadeOut("fast");
+            var $scrollHint = $("#scroll-to-hint");
+            $scrollHint
+                .fadeIn("fast")
+                .delay(1500)
+                .fadeOut("fast")
+                .on('click', (event) => {
+                    event.preventDefault();
+                    var top = window.pageYOffset - this.NAVBAR_HEIGHT + element.getBoundingClientRect().top;
+                    $("html, body").animate({ scrollTop: top }, 'fast');
+                    $scrollHint.off('click');
+                });
         }
     },
     isElementVisible(element) {
         var rect = element.getBoundingClientRect();
 
-        return rect.top > this.NAVBAR_HEIGHT && rect.bottom > this.NAVBAR_HEIGHT;
+        return rect.top > 0 && rect.bottom > 0;
     }
 };
 
