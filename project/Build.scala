0f8f585fdefe226d5c844055656bb26d8b3edb6b
Dennis Oelkers <dennis@graylog.com>
2015-05-29 13:11:32 +0200

    Adding classifier to version string.

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ import com.typesafe.sbt.less.Import.LessKeys
 
 object ApplicationBuild extends Build {
   val appName         = "graylog-web-interface"
-  val appVersion      = "1.2.0-SNAPSHOT"
+  val appVersion      = "1.2.0-feature-alarmcallback-history-SNAPSHOT"
   val appDependencies = Seq(
     cache,
     javaCore,
