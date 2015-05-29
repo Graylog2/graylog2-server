3cde0f0f90341c21562d893992181650049985b8
Bernd Ahlers <bernd@graylog.com>
2015-05-29 19:00:45 +0200

    Unbreak server startup with collector thresholds set.
    Use the correct validator for the config options.
    
    Fixes #1194.

--- graylog2-server/src/main/java/org/graylog2/Configuration.java
+++ graylog2-server/src/main/java/org/graylog2/Configuration.java
@@ -125,10 +125,10 @@ public class Configuration extends BaseConfiguration {
     @Parameter(value = "default_message_output_class")
     private String defaultMessageOutputClass = "";
 
-    @Parameter(value = "collector_expiration_threshold", validator = PositiveIntegerValidator.class)
+    @Parameter(value = "collector_expiration_threshold", validator = PositiveDurationValidator.class)
     private Duration collectorExpirationThreshold = Duration.days(14);
 
-    @Parameter(value = "collector_inactive_threshold", validator = PositiveIntegerValidator.class)
+    @Parameter(value = "collector_inactive_threshold", validator = PositiveDurationValidator.class)
     private Duration collectorInactiveThreshold = Duration.minutes(1);
 
     @Parameter(value = "dashboard_widget_default_cache_time", validator = PositiveDurationValidator.class)
