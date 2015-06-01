884b574661f082f33c11b53153cd32ccd2570d3e
Kay Roepke <kroepke@googlemail.com>
2015-06-01 14:24:25 +0200

    fix duplicate metrics reporting if two components subscribed to the same metric on the same page
    if one global subscription and one per-node subscription was present on the same page, metrics were reported twice
    
    fixes Graylog2/graylog2-server#1199

--- app/models/sockjs/MetricValuesUpdate.java
+++ app/models/sockjs/MetricValuesUpdate.java
@@ -1,16 +1,22 @@
 package models.sockjs;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.google.common.collect.Sets;
 import org.graylog2.restclient.lib.metrics.Metric;
 
-import java.util.Collection;
+import java.util.Set;
 
 @JsonAutoDetect
 public class MetricValuesUpdate {
 
-    public String nodeId;
+    public final String nodeId;
 
-    public Collection<NamedMetric> values;
+    public final Set<NamedMetric> values;
+
+    public MetricValuesUpdate(String nodeId) {
+        this.nodeId = nodeId;
+        this.values = Sets.newHashSet();
+    }
 
     public static class NamedMetric {
         public String name;
@@ -19,6 +25,21 @@ public class MetricValuesUpdate {
         public NamedMetric(String name, Metric value) {
             this.name = name;
             metric = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            NamedMetric that = (NamedMetric) o;
+
+            return name.equals(that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
         }
     }
 }
