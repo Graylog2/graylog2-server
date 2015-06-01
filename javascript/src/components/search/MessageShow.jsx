6cdfafac60504ade03c9b7e92f29d339a10af027
Edmundo Alvarez <edmundo@graylog.com>
2015-06-01 15:37:36 +0200

    Fix margin problems with message details page

--- javascript/src/components/search/MessageShow.jsx
+++ javascript/src/components/search/MessageShow.jsx
@@ -11,17 +11,15 @@ var MessageShow = React.createClass({
     },
     render() {
         return (
-            <div className="col-md-12" id="main-content">
-                <div className="row content">
-                    <div className="col-md-12">
-                        <MessageDetail {...this.props} message={this.props.message}
-                                                       inputs={this.props.inputs}
-                                                       streams={this.props.streams}
-                                                       nodes={this.props.nodes}
-                                                       possiblyHighlight={this.possiblyHighlight}
-                                                       showTimestamp={true}
-                                                       allStreams={Immutable.List()}/>
-                    </div>
+            <div className="row content">
+                <div className="col-md-12">
+                    <MessageDetail {...this.props} message={this.props.message}
+                                                   inputs={this.props.inputs}
+                                                   streams={this.props.streams}
+                                                   nodes={this.props.nodes}
+                                                   possiblyHighlight={this.possiblyHighlight}
+                                                   showTimestamp={true}
+                                                   allStreams={Immutable.List()}/>
                 </div>
             </div>
         );
