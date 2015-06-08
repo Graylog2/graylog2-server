bda08b31ff35fbc6762b9a7980ec630bc87c8096
Dennis Oelkers <dennis@graylog.com>
2015-06-08 15:03:32 +0200

    Checking for absence of type def to unbreak alarm callback listing.
    Disabling edit button and show alert if type definition for alarm
    callback is not present (mostly because of unloaded plugin providing
    the alarm callback).
    
    Fixes #1450

--- javascript/src/components/alarmcallbacks/EditAlarmCallbackButton.jsx
+++ javascript/src/components/alarmcallbacks/EditAlarmCallbackButton.jsx
@@ -2,6 +2,7 @@
 
 var React = require('react/addons');
 var ConfigurationForm = require('../configurationforms/ConfigurationForm');
+var Button = require('react-bootstrap').Button;
 
 var EditAlarmCallbackButton = React.createClass({
     _handleClick() {
@@ -10,20 +11,26 @@ var EditAlarmCallbackButton = React.createClass({
     _handleSubmit(data) {
         this.props.onUpdate(this.props.alarmCallback, data);
     },
+    getDefaultProps() {
+        return {
+            disabled: false
+        };
+    },
     render() {
         var alarmCallback = this.props.alarmCallback;
         var definition = this.props.types[alarmCallback.type];
+        var configurationForm = (definition ? <ConfigurationForm ref="configurationForm" key={"configuration-form-alarm-callback-"+alarmCallback.id}
+                                                                 configFields={definition.requested_configuration}
+                                                                 title={"Editing Alarm Callback "}
+                                                                 typeName={alarmCallback.type} includeTitleField={false}
+                                                                 submitAction={this._handleSubmit} values={alarmCallback.configuration} /> : null);
 
         return (
             <span>
-                <button className="btn btn-success" onClick={this._handleClick}>
+                <Button bsStyle="success" disabled={this.props.disabled} onClick={this._handleClick}>
                     Edit callback
-                </button>
-                <ConfigurationForm ref="configurationForm" key={"configuration-form-alarm-callback-"+alarmCallback.id}
-                                   configFields={definition.requested_configuration}
-                                   title={"Editing Alarm Callback "}
-                                   typeName={alarmCallback.type} includeTitleField={false}
-                                   submitAction={this._handleSubmit} values={alarmCallback.configuration} />
+                </Button>
+                {configurationForm}
             </span>
         );
     }
