fc2946cb8ba9c0b241863610101730205925a912
Dennis Oelkers <dennis@graylog.com>
2015-06-12 14:52:34 +0200

    Adding correct min/max attributes to number fields in config forms.
    Fixes #1473

--- javascript/src/components/configurationforms/NumberField.jsx
+++ javascript/src/components/configurationforms/NumberField.jsx
@@ -16,15 +16,20 @@ var NumberField = React.createClass({
         this.setState(props);
     },
     mapValidationAttribute(attribute) {
-        switch(attribute) {
-            case "ONLY_NEGATIVE": return "negative_number";
-            case "ONLY_POSITIVE": return "positive_number";
-            case "IS_PORT_NUMBER": return "port_number";
-            default: return attribute.toLowerCase();
+        switch(attribute.toLocaleUpperCase()) {
+            case "ONLY_NEGATIVE": return {min: Number.MIN_SAFE_INTEGER, max: -1};
+            case "ONLY_POSITIVE": return {min: 0, max: Number.MAX_SAFE_INTEGER};
+            case "IS_PORT_NUMBER": return {min: 0, max: 65535};
+            default: return {};
         }
     },
     validationSpec(field) {
-        return field.attributes.map(this.mapValidationAttribute).join(" ");
+        var validationAttributes = field.attributes.map(this.mapValidationAttribute);
+        if (validationAttributes.length > 0) {
+            return validationAttributes.reduce((x, y) => { return x.extend(y); });
+        } else {
+            return {};
+        }
     },
     handleChange(evt) {
         this.props.onChange(this.state.title, evt.target.value);
@@ -44,8 +49,9 @@ var NumberField = React.createClass({
                     {FieldHelpers.optionalMarker(field)}
                 </label>
                 <input id={field.title} type="number" required={isRequired} onChange={this.handleChange} value={this.state.value}
-                       min={Number.MIN_VALUE.toFixed()} max={Number.MAX_VALUE.toFixed()} defaultValue={defaultValue}
-                    className="input-xlarge validatable form-control" data-validate={validationSpecs} />
+                       defaultValue={defaultValue} className="input-xlarge validatable form-control"
+                       {...validationSpecs}/>
+
                 <p className="help-block">{field.description}</p>
             </div>
         );
