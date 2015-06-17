fef9659bd2a54cd7a114c297c223dbc493fba2de
Edmundo Alvarez <edmundo@graylog.com>
2015-06-17 18:04:25 +0200

    Remove unused controllers and play templates

--- app/controllers/MessagesController.java
+++ app/controllers/MessagesController.java
@@ -18,81 +18,28 @@
  */
 package controllers;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.graylog2.restclient.lib.APIException;
 import org.graylog2.restclient.lib.ApiClient;
-import org.graylog2.restclient.models.Input;
 import org.graylog2.restclient.models.MessagesService;
-import org.graylog2.restclient.models.Node;
-import org.graylog2.restclient.models.NodeService;
-import org.graylog2.restclient.models.Radio;
-import org.graylog2.restclient.models.Stream;
-import org.graylog2.restclient.models.StreamService;
 import org.graylog2.restclient.models.api.results.MessageAnalyzeResult;
 import org.graylog2.restclient.models.api.results.MessageResult;
-import play.Logger;
 import play.libs.Json;
 import play.mvc.Result;
 
 import javax.inject.Inject;
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
-
-import static lib.security.RestPermissions.INPUTS_READ;
-import static lib.security.RestPermissions.STREAMS_READ;
-import static views.helpers.Permissions.isPermitted;
 
 public class MessagesController extends AuthenticatedController {
 
-    private final NodeService nodeService;
-    private final StreamService streamService;
     private final MessagesService messagesService;
 
     @Inject
-    public MessagesController(NodeService nodeService, StreamService streamService, MessagesService messagesService) {
-        this.nodeService = nodeService;
-        this.streamService = streamService;
+    public MessagesController(MessagesService messagesService) {
         this.messagesService = messagesService;
     }
 
-    public Result partial(String index, String id) {
-        try {
-            MessageResult message = messagesService.getMessage(index, id);
-            Node sourceNode = getSourceNode(message);
-            Radio sourceRadio = getSourceRadio(message);
-            List<Stream> messageInStreams = Lists.newArrayList();
-
-            for (String streamId : message.getStreamIds()) {
-                if (isPermitted(STREAMS_READ, streamId)) {
-                    try {
-                        messageInStreams.add(streamService.get(streamId));
-                    } catch (APIException e) {
-                        //  We get a 404 if the stream no longer exists.
-                        Logger.debug("Skipping stream of message", e);
-                    }
-                }
-            }
-
-            return ok(views.html.messages.show_as_partial.render(
-                            message,
-                            messageInStreams,
-                            getSourceInput(sourceNode, message),
-                            sourceNode,
-                            sourceRadio,
-                            getSourceInput(sourceRadio, message),
-                            streamService.all())
-            );
-        } catch (IOException e) {
-            return status(500, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
-        } catch (APIException e) {
-            String message = "Could not get message. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
-            return status(500, views.html.errors.error.render(message, e, request()));
-        }
-    }
-
-    // TODO move this to an API controller.
     public Result single(String index, String id) {
         try {
 
@@ -131,52 +78,4 @@ public class MessagesController extends AuthenticatedController {
             return status(500, views.html.errors.error.render(message, e, request()));
         }
     }
-
-    private Node getSourceNode(MessageResult m) {
-        try {
-            return nodeService.loadNode(m.getSourceNodeId());
-        } catch (Exception e) {
-            Logger.warn("Could not derive source node from message <" + m.getId() + ">.", e);
-        }
-
-        return null;
-    }
-
-
-    private Radio getSourceRadio(MessageResult m) {
-        if (m.viaRadio()) {
-            try {
-                return nodeService.loadRadio(m.getSourceRadioId());
-            } catch (Exception e) {
-                Logger.warn("Could not derive source radio from message <" + m.getId() + ">.", e);
-            }
-        }
-
-        return null;
-    }
-
-    private static Input getSourceInput(Node node, MessageResult m) {
-        if (node != null && isPermitted(INPUTS_READ, m.getSourceInputId())) {
-            try {
-                return node.getInput(m.getSourceInputId());
-            } catch (Exception e) {
-                Logger.warn("Could not derive source input from message <" + m.getId() + ">.", e);
-            }
-        }
-
-        return null;
-    }
-
-    private static Input getSourceInput(Radio radio, MessageResult m) {
-        if (radio != null) {
-            try {
-                return radio.getInput(m.getSourceRadioInputId());
-            } catch (Exception e) {
-                Logger.warn("Could not derive source radio input from message <" + m.getId() + ">.", e);
-            }
-        }
-
-        return null;
-    }
-
 }
--- app/controllers/StreamRulesController.java
+++ app/controllers/StreamRulesController.java
@@ -16,15 +16,11 @@ import javax.inject.Inject;
 import java.io.IOException;
 
 public class StreamRulesController extends AuthenticatedController {
-    private static final Form<CreateStreamRuleRequest> createStreamRuleForm = Form.form(CreateStreamRuleRequest.class);
-
     private final StreamService streamService;
-    private final StreamRuleService streamRuleService;
 
     @Inject
-    public StreamRulesController(StreamService streamService, StreamRuleService streamRuleService) {
+    public StreamRulesController(StreamService streamService) {
         this.streamService = streamService;
-        this.streamRuleService = streamRuleService;
     }
 
     public Result index(String streamId) {
@@ -38,63 +34,6 @@ public class StreamRulesController extends AuthenticatedController {
             return status(504, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
         }
 
-        return ok(views.html.streamrules.index.render(currentUser(), stream, stream.getStreamRules(), standardBreadcrumbs(stream)));
-    }
-
-    public Result create(String streamId) {
-        Form<CreateStreamRuleRequest> form = createStreamRuleForm.bindFromRequest();
-        CreateStreamRuleResponse response = null;
-
-        try {
-            CreateStreamRuleRequest csrr = form.get();
-            response = streamRuleService.create(streamId, csrr);
-            StreamRule streamRule = streamRuleService.get(streamId, response.streamrule_id);
-            return created(views.html.partials.streamrules.list_item.render(streamRule));
-        } catch (APIException e) {
-            String message = "Could not create stream rule. We expected HTTP 201, but got a HTTP " + e.getHttpCode() + ".";
-            return status(504, message);
-        } catch (IOException e) {
-            return status(504, e.toString());
-        }
-
-    }
-
-    public Result update(String streamId, String streamRuleId) {
-        Form<CreateStreamRuleRequest> form = createStreamRuleForm.bindFromRequest();
-        CreateStreamRuleResponse response = null;
-
-        try {
-            CreateStreamRuleRequest csrr = form.get();
-            response = streamRuleService.update(streamId, streamRuleId, csrr);
-            StreamRule streamRule = streamRuleService.get(streamId, response.streamrule_id);
-            return created(views.html.partials.streamrules.list_item.render(streamRule));
-        } catch (APIException e) {
-            String message = "Could not create stream rule. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
-            return status(504, message);
-        } catch (IOException e) {
-            return status(504, e.toString());
-        }
-    }
-
-    public Result delete(String streamId, String streamRuleId) {
-        try {
-            streamRuleService.delete(streamId, streamRuleId);
-        } catch (APIException e) {
-            String message = "Could not delete stream rule. We expect HTTP 204, but got a HTTP " + e.getHttpCode() + ".";
-            return status(504, views.html.errors.error.render(message, e, request()));
-        } catch (IOException e) {
-            return status(504, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
-        }
-
-        return ok();
-    }
-
-    private static BreadcrumbList standardBreadcrumbs(Stream stream) {
-        BreadcrumbList bc = new BreadcrumbList();
-        bc.addCrumb("Streams", routes.StreamsController.index());
-        bc.addCrumb("Stream: " + stream.getTitle(), null);
-        bc.addCrumb("Rules", null);
-
-        return bc;
+        return ok(views.html.streamrules.index.render(currentUser(), stream, stream.getStreamRules()));
     }
 }
