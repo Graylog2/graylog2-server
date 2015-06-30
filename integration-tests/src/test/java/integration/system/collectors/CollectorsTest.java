4852f53aa630cb6b0247d0c8bbecd6747126c208
Dennis Oelkers <dennis@graylog.com>
2015-06-30 12:00:24 +0200

    Using proper annotation instead of commenting out the test one.

--- integration-tests/src/test/java/integration/system/collectors/CollectorsTest.java
+++ integration-tests/src/test/java/integration/system/collectors/CollectorsTest.java
@@ -20,6 +20,7 @@ import integration.BaseRestTest;
 import integration.RequiresAuthentication;
 import integration.RequiresVersion;
 import org.joda.time.DateTime;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -66,8 +67,8 @@ public class CollectorsTest extends BaseRestTest {
                     .assertThat().body("collectors", notNullValue());
     }
 
-    // TODO: reenable after #1273 has been fixed.
-    //@Test
+    @Ignore("TODO: reenable after #1273 has been fixed.")
+    @Test
     @RequiresAuthentication
     public void testGetCollector() throws Exception {
         given().when()
