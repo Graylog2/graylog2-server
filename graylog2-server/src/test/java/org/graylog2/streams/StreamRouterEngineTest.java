0f6962288494b4b53c7f5d9d492c1d748d2fc976
Dennis Oelkers <dennis@graylog.com>
2015-07-09 15:43:15 +0200

    Fixing error in test case.

--- graylog2-server/src/test/java/org/graylog2/streams/StreamRouterEngineTest.java
+++ graylog2-server/src/test/java/org/graylog2/streams/StreamRouterEngineTest.java
@@ -517,7 +517,7 @@ public class StreamRouterEngineTest {
         when(message.getField(eq(dummyField))).thenReturn(dummyValue);
         when(message.getFieldNames()).thenReturn(Sets.newHashSet(dummyField));
 
-        final StreamRouterEngine engine = newEngine(Lists.newArrayList(stream1));
+        final StreamRouterEngine engine = newEngine(Lists.newArrayList(stream1, stream2));
 
         final List<Stream> result = engine.match(message);
 
