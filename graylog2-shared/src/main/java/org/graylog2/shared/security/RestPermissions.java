139f16107939b6faae3ec4ff35ab9014b74414f4
Jochen Schalanda <jochen@graylog.com>
2015-07-09 16:19:20 +0200

    Clean up RestPermissions

--- graylog2-shared/src/main/java/org/graylog2/shared/security/RestPermissions.java
+++ graylog2-shared/src/main/java/org/graylog2/shared/security/RestPermissions.java
@@ -18,20 +18,27 @@ package org.graylog2.shared.security;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ListMultimap;
-import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 public class RestPermissions {
     private static final Logger LOG = LoggerFactory.getLogger(RestPermissions.class);
 
-    // These should all be in the form of "group:action", because allPermissions() below depends on it.
-    // Should this ever change, you need to adapt the code below, too.
+    /**
+     *These should all be in the form of "group:action", because {@link #allPermissions()} below depends on it.
+     * Should this ever change, you need to adapt the code below, too.
+     */
     public static final String USERS_CREATE = "users:create";
     public static final String USERS_EDIT = "users:edit";
     public static final String USERS_LIST = "users:list";
@@ -114,37 +121,60 @@ public class RestPermissions {
     public static final String JOURNAL_EDIT = "journal:edit";
     public static final String COLLECTORS_READ = "collectors:read";
 
-    private static Map<String, Collection<String>> allPermissions;
-
-    public static Set<String> adminPermissions = Sets.newHashSet("*");
-
     // Standard set of permissions of readers.
-    public static Set<String> readerBasePermissions = Sets.newHashSet(
-            BUFFERS_READ,
-            FIELDNAMES_READ,
-            INDEXERCLUSTER_READ,
-            INPUTS_READ,
-            JOURNAL_READ,
-            JVMSTATS_READ,
-            MESSAGECOUNT_READ,
-            MESSAGES_READ,
-            METRICS_READ,
-            SYSTEM_READ,
-            THROUGHPUT_READ,
-            SAVEDSEARCHES_CREATE,
-            SAVEDSEARCHES_EDIT,
-            SAVEDSEARCHES_READ
-    );
+    private static final Set<String> READER_BASE_PERMISSIONS = ImmutableSet.<String>builder().add(
+                    BUFFERS_READ,
+                    FIELDNAMES_READ,
+                    INDEXERCLUSTER_READ,
+                    INPUTS_READ,
+                    JOURNAL_READ,
+                    JVMSTATS_READ,
+                    MESSAGECOUNT_READ,
+                    MESSAGES_READ,
+                    METRICS_READ,
+                    SYSTEM_READ,
+                    THROUGHPUT_READ,
+                    SAVEDSEARCHES_CREATE,
+                    SAVEDSEARCHES_EDIT,
+                    SAVEDSEARCHES_READ
+    ).build();
+
+    private static final Map<String, Collection<String>> ALL_PERMISSIONS;
+
+    static {
+        final Field[] declaredFields = RestPermissions.class.getDeclaredFields();
+        final ListMultimap<String, String> all = ArrayListMultimap.create();
+        for (Field declaredField : declaredFields) {
+            if (!Modifier.isStatic(declaredField.getModifiers())) {
+                continue;
+            }
+            if (!String.class.isAssignableFrom(declaredField.getType())) {
+                continue;
+            }
+            declaredField.setAccessible(true);
+            try {
+                final String permission = (String) declaredField.get(RestPermissions.class);
+                final Iterator<String> split = Splitter.on(':').limit(2).split(permission).iterator();
+                final String group = split.next();
+                final String action = split.next();
+                all.put(group, action);
+            } catch (IllegalAccessException ignored) {
+            }
+        }
+        ALL_PERMISSIONS = all.asMap();
+    }
 
     public static Set<String> readerPermissions(String username) {
-        final HashSet<String> perms = Sets.newHashSet(readerBasePermissions);
-        if (username == null || username.isEmpty()) {
+        final ImmutableSet.Builder<String> perms = ImmutableSet.<String>builder().addAll(READER_BASE_PERMISSIONS);
+        if (isNullOrEmpty(username)) {
             LOG.error("Username cannot be empty or null for creating reader permissions");
             throw new IllegalArgumentException("Username was null or empty when getting reader permissions.");
         }
+
         perms.add(perInstance(USERS_EDIT, username));
         perms.add(perInstance(USERS_PASSWORDCHANGE, username));
-        return perms;
+
+        return perms.build();
     }
 
     public static String perInstance(String permission, String instance) {
@@ -152,29 +182,7 @@ public class RestPermissions {
         return permission + ":" + instance;
     }
 
-    public static synchronized Map<String, Collection<String>> allPermissions() {
-        if (allPermissions == null) {
-            final Field[] declaredFields = RestPermissions.class.getDeclaredFields();
-            ListMultimap<String, String> all = ArrayListMultimap.create();
-            for (Field declaredField : declaredFields) {
-                if (! Modifier.isStatic(declaredField.getModifiers())) {
-                    continue;
-                }
-                if (! String.class.isAssignableFrom(declaredField.getType())) {
-                    continue;
-                }
-                declaredField.setAccessible(true);
-                try {
-                    final String permission = (String) declaredField.get(RestPermissions.class);
-                    final Iterator<String> split = Splitter.on(':').limit(2).split(permission).iterator();
-                    final String group = split.next();
-                    final String action = split.next();
-                    all.put(group, action);
-                } catch (IllegalAccessException ignored) {
-                }
-            }
-            allPermissions = all.asMap();
-        }
-        return allPermissions;
+    public static Map<String, Collection<String>> allPermissions() {
+        return ALL_PERMISSIONS;
     }
 }
