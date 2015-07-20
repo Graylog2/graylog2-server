f00e58d2ef76fc413eb046832faa88157154b847
Dennis Oelkers <dennis@graylog.com>
2015-07-20 13:51:09 +0200

    Correcting strange merging(?) error.

--- javascript/src/components/alarmcallbacks/AlarmCallback.jsx
+++ javascript/src/components/alarmcallbacks/AlarmCallback.jsx
@@ -15,37 +15,36 @@ var AlarmCallback = React.createClass({
     _typeNotAvailable() {
         return (this.props.types[this.props.alarmCallback.type] == undefined);
     },
-    render() {
-        var alarmCallback = this.props.alarmCallback;
-        var humanReadableType = (this._typeNotAvailable() ? <i>Unknown alarm callback ({alarmCallback.type})</i> : this.props.types[alarmCallback.type].name);
-        var editAlarmCallbackButton = (this.isPermitted(this.props.permissions, ["streams:edit:"+this.props.streamId]) ?
-            <EditAlarmCallbackButton disabled={this._typeNotAvailable()} alarmCallback={alarmCallback} types={this.props.types}
-                                     streamId={this.props.streamId} onUpdate={this.props.updateAlarmCallback} /> : null);
-        var deleteAlarmCallbackButton = (this.isPermitted(this.props.permissions, ["streams:edit:"+this.props.streamId]) ?
-            <DeleteAlarmCallbackButton alarmCallback={alarmCallback} onClick={this.props.deleteAlarmCallback} /> : null);
-        var alert = (this._typeNotAvailable() ? <Alert bsStyle="danger">
-                The plugin required for this alarm callback is not loaded. Editing it is not possible. Please load the plugin or delete the alarm callback.
-            </Alert> : null);
-        var configurationWell = (this._typeNotAvailable() ? null : <ConfigurationWell configuration={alarmCallback.configuration}
-                                                                                      typeDefinition={this.props.types[alarmCallback.type]} />);
-        return (
-            <div className="col-md-3" style={{textAlign: "right"}}>
-                {' '}
-                {editAlarmCallbackButton}
-                {' '}
-                {deleteAlarmCallbackButton}
-            </div>
-        );
-    },
     getDefaultProps() {
         return {
             hideButtons: false
         };
     },
+    _formatActionButtons() {
+        var alarmCallback = this.props.alarmCallback;
+        var editAlarmCallbackButton = (this.isPermitted(this.props.permissions, ["streams:edit:"+this.props.streamId]) ?
+            <EditAlarmCallbackButton disabled={this._typeNotAvailable()} alarmCallback={alarmCallback} types={this.props.types}
+                                     streamId={this.props.streamId} onUpdate={this.props.updateAlarmCallback} /> : null);
+        var deleteAlarmCallbackButton = (this.isPermitted(this.props.permissions, ["streams:edit:"+this.props.streamId]) ?
+            <DeleteAlarmCallbackButton alarmCallback={alarmCallback} onClick={this.props.deleteAlarmCallback} /> : null);
+        return (
+            <span>
+                {' '}
+                {editAlarmCallbackButton}
+                {' '}
+                {deleteAlarmCallbackButton}
+            </span>
+        );
+    },
     render() {
         var alarmCallback = this.props.alarmCallback;
         var humanReadableType = this.props.types[alarmCallback.type].name;
         var actionButtons = (this.props.hideButtons ? null : this._formatActionButtons());
+        var alert = (this._typeNotAvailable() ? <Alert bsStyle="danger">
+            The plugin required for this alarm callback is not loaded. Editing it is not possible. Please load the plugin or delete the alarm callback.
+        </Alert> : null);
+        var configurationWell = (this._typeNotAvailable() ? null : <ConfigurationWell configuration={alarmCallback.configuration}
+                                                                                      typeDefinition={this.props.types[alarmCallback.type]} />);
         return (
             <div className="alert-callback" data-destination-id={alarmCallback.id}>
                 <Row style={{marginBottom: 0}}>
@@ -60,10 +59,7 @@ var AlarmCallback = React.createClass({
                     </Col>
 
                     <Col md={3} style={{textAlign: "right"}}>
-                        {' '}
-                        {editAlarmCallbackButton}
-                        {' '}
-                        {deleteAlarmCallbackButton}
+                        {actionButtons}
                     </Col>
                 </Row>
 
