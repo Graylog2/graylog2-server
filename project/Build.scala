784fcbe7ef2d3d3afc67a6e9cd08ce9b0ec6d5d1
Dennis Oelkers <dennis@graylog.com>
2015-07-23 12:09:47 +0200

    Revert "Adding classifier to version string."
    This reverts commit 78e2f9eae9ca852fcf56335086f66a7fbdfdf895.

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ import com.typesafe.sbt.less.Import.LessKeys
 
 object ApplicationBuild extends Build {
   val appName         = "graylog-web-interface"
-  val appVersion      = "1.2.0-feature-alarmcallback-history-SNAPSHOT"
+  val appVersion      = "1.2.0-SNAPSHOT"
   val appDependencies = Seq(
     cache,
     javaCore,
