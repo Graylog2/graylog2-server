ad59cf8f1ba9e40938be61d4c6a73160c26586ca
Jochen Schalanda <jochen@graylog.com>
2015-07-24 12:29:09 +0200

    Add configuration option for time zone to DateConverter
    Fixes #1320

--- graylog2-server/src/main/java/org/graylog2/inputs/converters/DateConverter.java
+++ graylog2-server/src/main/java/org/graylog2/inputs/converters/DateConverter.java
@@ -20,17 +20,26 @@ import org.graylog2.ConfigurationException;
 import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.inputs.Converter;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.YearMonth;
+import org.joda.time.Years;
 import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.util.Map;
 
-public class DateConverter extends Converter {
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.base.Strings.emptyToNull;
+import static com.google.common.base.Strings.isNullOrEmpty;
 
+public class DateConverter extends Converter {
     private static final Logger LOG = LoggerFactory.getLogger(DateConverter.class);
 
     private final String dateFormat;
+    private final DateTimeZone timeZone;
 
     public DateConverter(Map<String, Object> config) throws ConfigurationException {
         super(Type.DATE, config);
@@ -39,20 +48,37 @@ public class DateConverter extends Converter {
             throw new ConfigurationException("Missing config [date_format].");
         }
 
-        dateFormat = ((String) config.get("date_format")).trim();
+        this.dateFormat = ((String) config.get("date_format")).trim();
+        this.timeZone = buildTimeZone(config.get("time_zone"));
+    }
+
+    private static DateTimeZone buildTimeZone(Object timeZoneId) {
+        if (timeZoneId instanceof String) {
+            try {
+                final String timeZoneString = (String) timeZoneId;
+                final String zoneId = firstNonNull(emptyToNull(timeZoneString.trim()), "Etc/UTC");
+                return DateTimeZone.forID(zoneId);
+            } catch (IllegalArgumentException e) {
+                return DateTimeZone.forID("Etc/UTC");
+            }
+        } else {
+            return DateTimeZone.forID("Etc/UTC");
+        }
     }
 
     @Override
-    public Object convert(String value) {
-        if (value == null || value.isEmpty()) {
+    @Nullable
+    public Object convert(@Nullable String value) {
+        if (isNullOrEmpty(value)) {
             return null;
         }
 
-        final DateTime localNow = Tools.iso8601();
-
-        LOG.debug("Trying to parse date <{}> with pattern <{}>.", value, dateFormat);
-
-        return DateTime.parse(value, DateTimeFormat.forPattern(dateFormat).withDefaultYear(localNow.getYear()));
+        LOG.debug("Trying to parse date <{}> with pattern <{}> and timezone <{}>.", value, dateFormat, timeZone);
+        final DateTimeFormatter formatter = DateTimeFormat
+                .forPattern(dateFormat)
+                .withDefaultYear(YearMonth.now(timeZone).getYear())
+                .withZone(timeZone);
+        return DateTime.parse(value, formatter);
     }
 
     @Override
--- graylog2-server/src/test/java/org/graylog2/inputs/converters/DateConverterTest.java
+++ graylog2-server/src/test/java/org/graylog2/inputs/converters/DateConverterTest.java
@@ -16,47 +16,100 @@
  */
 package org.graylog2.inputs.converters;
 
+import org.assertj.jodatime.api.Assertions;
 import org.graylog2.ConfigurationException;
+import org.graylog2.plugin.inputs.Converter;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.YearMonth;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 public class DateConverterTest {
-
     @Test
     public void testBasicConvert() throws Exception {
         // .startsWith() because of possibly different timezones per test environment.
-        assert(new DateConverter(config("YYYY MMM dd HH:mm:ss")).convert("2013 Aug 15 23:15:16").toString().startsWith("2013-08-15T23:15:16.000"));
+        final DateConverter converter = new DateConverter(config("YYYY MMM dd HH:mm:ss", null));
+        final Object result = converter.convert("2013 Aug 15 23:15:16");
+        assertThat(result).isNotNull();
+        assertThat(String.valueOf(result)).startsWith("2013-08-15T23:15:16.000");
     }
 
     @Test
     public void testAnotherBasicConvert() throws Exception {
-        DateTime date = (DateTime) new DateConverter(config("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZZ")).convert("2014-05-19T00:30:43.116847+00:00");
-        assertEquals("2014-05-19T00:30:43.116Z", date.toDateTime(DateTimeZone.UTC).toString());
+        final DateConverter converter = new DateConverter(config("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZZ", "Etc/UTC"));
+        final DateTime date = (DateTime) converter.convert("2014-05-19T00:30:43.116+00:00");
+        assertThat(date).isNotNull();
+        Assertions.assertThat(date).isEqualTo(new DateTime(2014, 5, 19, 0, 30, 43, 116, DateTimeZone.UTC));
+
+    }
+
+    @Test
+    public void testConvertWithoutYear() throws Exception {
+        final int year = YearMonth.now(DateTimeZone.UTC).getYear();
+        final DateConverter converter = new DateConverter(config("dd-MM HH:mm:ss", "Etc/UTC"));
+        final DateTime date = (DateTime) converter.convert("19-05 10:20:30");
+        assertThat(date).isNotNull();
+        Assertions.assertThat(date).isEqualTo(new DateTime(year, 5, 19, 10, 20, 30, DateTimeZone.UTC));
     }
 
     @Test(expected = ConfigurationException.class)
-    public void testWithEmptyConfig() throws Exception {
-        assertEquals(null, new DateConverter(config("")).convert("foo"));
+    public void testWithEmptyDateFormat() throws Exception {
+        assertThat(new DateConverter(config("", null)).convert("foo")).isNull();
     }
 
     @Test(expected = ConfigurationException.class)
-    public void testWithNullConfig() throws Exception {
-        assertEquals(null, new DateConverter(config(null)).convert("foo"));
+    public void testWithNullDateFormat() throws Exception {
+        assertThat(new DateConverter(config(null, null)).convert("foo")).isNull();
     }
 
-    private Map<String, Object> config(final String dateFormat) {
-        return new HashMap<String, Object>() {{
-            put("date_format", dateFormat);
-        }};
+    @Test
+    public void convertObeysTimeZone() throws Exception {
+        final DateTimeZone timeZone = DateTimeZone.forOffsetHours(12);
+        final Converter c = new DateConverter(config("YYYY-MM-dd HH:mm:ss", timeZone.toString()));
+
+        final DateTime dateOnly = (DateTime) c.convert("2014-03-12 10:00:00");
+        assertThat(dateOnly.getZone()).isEqualTo(timeZone);
+
+        Assertions.assertThat(dateOnly)
+                .isEqualTo(new DateTime(2014, 3, 12, 10, 0, 0, timeZone))
+                .isBefore(new DateTime(2014, 3, 13, 10, 0, 0, timeZone));
+
+        final DateTime dateTime = (DateTime) c.convert("2014-03-12 12:34:00");
+        assertThat(dateTime.getZone()).isEqualTo(timeZone);
+        Assertions.assertThat(dateTime)
+                .isEqualTo(new DateTime(2014, 3, 12, 12, 34, 0, timeZone));
     }
 
+    @Test
+    public void convertUsesEtcUTCIfTimeZoneSettingIsEmpty() throws Exception {
+        final Converter c = new DateConverter(config("YYYY-MM-dd HH:mm:ss", ""));
+        final DateTime dateOnly = (DateTime) c.convert("2014-03-12 10:00:00");
+        assertThat(dateOnly.getZone()).isEqualTo(DateTimeZone.forID("Etc/UTC"));
+    }
+
+    @Test
+    public void convertUsesEtcUTCIfTimeZoneSettingIsBlank() throws Exception {
+        final Converter c = new DateConverter(config("YYYY-MM-dd HH:mm:ss", " "));
+        final DateTime dateOnly = (DateTime) c.convert("2014-03-12 10:00:00");
+        assertThat(dateOnly.getZone()).isEqualTo(DateTimeZone.forID("Etc/UTC"));
+    }
+
+    @Test
+    public void convertUsesEtcUTCIfTimeZoneSettingIsInvalid() throws Exception {
+        final Converter c = new DateConverter(config("YYYY-MM-dd HH:mm:ss", "TEST"));
+        final DateTime dateOnly = (DateTime) c.convert("2014-03-12 10:00:00");
+        assertThat(dateOnly.getZone()).isEqualTo(DateTimeZone.forID("Etc/UTC"));
+    }
+
+    private Map<String, Object> config(final String dateFormat, final String timeZone) {
+        final Map<String, Object> config = new HashMap<>();
+        config.put("date_format", dateFormat);
+        config.put("time_zone", timeZone);
+        return config;
+    }
 }
