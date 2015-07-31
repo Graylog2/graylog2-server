9ddd2e850db766e3764741bb7d3ba9f66966356a
Dennis Oelkers <dennis@graylog.com>
2015-07-31 13:53:16 +0200

    Returning 400 for bad requests. Using this in DashboardsResource.

---
+++ graylog2-shared/src/main/java/org/graylog2/shared/rest/exceptionmappers/BadRequestExceptionMapper.java
@@ -0,0 +1,41 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.shared.rest.exceptionmappers;
+
+import org.glassfish.jersey.spi.ExtendedExceptionMapper;
+import org.graylog2.plugin.rest.ApiError;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class BadRequestExceptionMapper implements ExtendedExceptionMapper<BadRequestException> {
+    @Override
+    public boolean isMappable(BadRequestException exception) {
+        return true;
+    }
+
+    @Override
+    public Response toResponse(BadRequestException e) {
+        final ApiError apiError = new ApiError(e.getMessage());
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity(apiError)
+                .build();
+    }
+}
