464e3eb92e7e1516a701da396fa8c9fb65eeaade
Jochen Schalanda <jochen@graylog.com>
2015-08-04 12:46:31 +0200

    Add getter with default values to Configuration

--- graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/configuration/Configuration.java
+++ graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/configuration/Configuration.java
@@ -29,85 +29,75 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 public class Configuration {
     private static final Logger LOG = LoggerFactory.getLogger(Configuration.class);
-    private static final ObjectMapper objectMapper;
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
+            .enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
     public static final Configuration EMPTY_CONFIGURATION = new Configuration(null);
-
-    static {
-        objectMapper = new ObjectMapper();
-        objectMapper
-                .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
-                .enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-    }
 
     @JsonProperty
     private final Map<String, Object> source;
-
     @JsonIgnore
-    private final Map<String, String> strings;
+    private final Map<String, String> strings = Maps.newHashMap();
     @JsonIgnore
-    private final Map<String, Integer> ints;
+    private final Map<String, Integer> ints = Maps.newHashMap();
     @JsonIgnore
-    private final Map<String, Boolean> bools;
-    @JsonIgnore
-    private String serializedSource = null;
+    private final Map<String, Boolean> bools = Maps.newHashMap();
 
     @JsonCreator
-    public Configuration(@JsonProperty("source") Map<String, Object> m) {
-        this.source = m;
+    public Configuration(@JsonProperty("source") @Nullable Map<String, Object> m) {
+        this.source = firstNonNull(m, Collections.<String, Object>emptyMap());
 
-        if (m != null && !m.isEmpty()) {
-            try {
-                this.serializedSource = objectMapper.writeValueAsString(this);
-            } catch (JsonProcessingException e) {
-                LOG.error("Serializing configuration failed.", e);
+        for (Map.Entry<String, Object> e : this.source.entrySet()) {
+            final String key = e.getKey();
+            final Object value = e.getValue();
+
+            if (value == null) {
+                LOG.debug("NULL value in configuration key <{}>", key);
+                continue;
             }
-        }
 
-        strings = Maps.newHashMap();
-        ints = Maps.newHashMap();
-        bools = Maps.newHashMap();
-
-        if (m != null) {
-            for(Map.Entry<String, Object> e : m.entrySet()) {
-                try {
-                    if (e.getValue() == null) {
-                        LOG.debug("NULL value in configuration key <{}>", e.getKey());
-                        continue;
-                    }
-
-                    if (e.getValue() instanceof String) {
-                        strings.put(e.getKey(), (String) e.getValue());
-                    } else if (e.getValue() instanceof Integer) {
-                        ints.put(e.getKey(), (Integer) e.getValue());
-                    } else if (e.getValue() instanceof Long) {
-                        ints.put(e.getKey(), ((Long) e.getValue()).intValue()); // We only support integers but MongoDB likes to return longs.
-                    } else if (e.getValue() instanceof Double) {
-                        ints.put(e.getKey(), ((Double) e.getValue()).intValue()); // same as for longs lol
-                    } else if (e.getValue() instanceof Boolean) {
-                        bools.put(e.getKey(), (Boolean) e.getValue());
-                    } else {
-                        LOG.error("Cannot handle type [{}] of plugin configuration key <{}>.", e.getValue().getClass().getCanonicalName(), e.getKey());
-                    }
-
-                } catch(Exception ex) {
-                    LOG.warn("Could not read input configuration key <" + e.getKey() + ">. Skipping.", ex);
+            try {
+                if (value instanceof String) {
+                    strings.put(key, (String) value);
+                } else if (value instanceof Integer) {
+                    ints.put(key, (Integer) value);
+                } else if (value instanceof Long) {
+                    ints.put(key, ((Long) value).intValue()); // We only support integers but MongoDB likes to return longs.
+                } else if (value instanceof Double) {
+                    ints.put(key, ((Double) value).intValue()); // same as for longs lol
+                } else if (value instanceof Boolean) {
+                    bools.put(key, (Boolean) value);
+                } else {
+                    LOG.error("Cannot handle type [{}] of plugin configuration key <{}>.", value.getClass().getCanonicalName(), key);
                 }
+
+            } catch (Exception ex) {
+                LOG.warn("Could not read input configuration key <" + key + ">. Skipping.", ex);
             }
         }
     }
 
+    @Nullable
     public String getString(String key) {
         return strings.get(key);
+    }
+
+    public String getString(String key, String defaultValue) {
+        return firstNonNull(strings.get(key), defaultValue);
     }
 
     public void setString(String key, String value) {
@@ -118,13 +108,18 @@ public class Configuration {
         return ints.get(key);
     }
 
-    public boolean getBoolean(String key) {
-        if (!bools.containsKey(key)) {
-            return false;
-        }
-
-        return bools.get(key);
+    public int getInt(String key, int defaultValue) {
+        return firstNonNull(ints.get(key), defaultValue);
     }
+
+    public boolean getBoolean(String key) {
+        return getBoolean(key, false);
+    }
+
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return firstNonNull(bools.get(key), defaultValue);
+    }
+
     public boolean booleanIsSet(String key) {
         return bools.containsKey(key);
     }
@@ -133,26 +128,34 @@ public class Configuration {
         bools.put(key, value);
     }
 
+    @Nullable
     public Map<String, Object> getSource() {
         return source;
     }
 
     public boolean stringIsSet(String key) {
-        return strings.get(key) != null && !strings.get(key).isEmpty();
+        return !isNullOrEmpty(strings.get(key));
     }
 
     public boolean intIsSet(String key) {
-        return ints.get(key) != null;
+        return ints.containsKey(key);
     }
 
+    @Nullable
     public String serializeToJson() {
-        return serializedSource;
+        try {
+            return source.isEmpty() ? null : objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            LOG.error("Serializing configuration failed.", e);
+            return null;
+        }
     }
 
     public static Configuration deserializeFromJson(String json) {
-        if (Strings.isNullOrEmpty(json)) {
+        if (isNullOrEmpty(json)) {
             return EMPTY_CONFIGURATION;
         }
+
         try {
             return objectMapper.readValue(json, Configuration.class);
         } catch (IOException e) {
