58adc31b68ec5cb1b7cbf8ff822c65419d7eef90
Dennis Oelkers <dennis@graylog.com>
2015-08-06 17:09:44 +0200

    Adding minification, deduplication and id optimization for prod builds.

--- javascript/webpack.config.js
+++ javascript/webpack.config.js
@@ -1,12 +1,23 @@
 // webpack.config.js
 var webpackConfig = require("./webpack.shared.config");
 var Html = require('html-webpack-plugin');
+var webpack = require("webpack");
 
 webpackConfig.output.filename = "app.[hash].js";
 
-webpackConfig.plugins.push(new Html({
-        filename: '../../../app/views/build/scripts.scala.html',
-        template: 'templates/scripts.scala.html.template'
-    }));
+webpackConfig.plugins.push(
+  new Html({
+    filename: '../../../app/views/build/scripts.scala.html',
+    template: 'templates/scripts.scala.html.template'
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin()
+  );
 
 module.exports = webpackConfig;
