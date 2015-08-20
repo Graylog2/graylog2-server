d01f912d6d016b9a86b340bcac5933be7b202301
Kay Roepke <kroepke@googlemail.com>
2015-08-20 18:37:24 +0200

    enable multi-select and toggling for streams and dashboards

--- javascript/src/components/users/PermissionSelector.jsx
+++ javascript/src/components/users/PermissionSelector.jsx
@@ -9,44 +9,63 @@ import DataTable from '../common/DataTable';
 const PermissionSelector = React.createClass({
 
   _toggleStreamReadPermissions(stream) {
-    this._toggleReadPermissions('streams', stream.id);
+    this._toggleReadPermissions('streams', Immutable.Set.of(stream.id));
   },
   _toggleStreamEditPermissions(stream) {
-    this._toggleEditPermissions('streams', stream.id);
+    this._toggleEditPermissions('streams', Immutable.Set.of(stream.id));
   },
 
   _toggleDashboardReadPermissions(dashboard) {
-    this._toggleReadPermissions('dashboards', dashboard.id);
+    this._toggleReadPermissions('dashboards', Immutable.Set.of(dashboard.id));
   },
   _toggleDashboardEditPermissions(dashboard) {
-    this._toggleEditPermissions('dashboards', dashboard.id);
+    this._toggleEditPermissions('dashboards', Immutable.Set.of(dashboard.id));
   },
 
-  _toggleReadPermissions(target, id) {
-    const readTarget = target + ':read:' + id;
-    const editTarget = target + ':edit:' + id;
-
+  _toggleReadPermissions(target, idList) {
     let added = Immutable.Set();
     let deleted = Immutable.Set();
-    if (this.props.permissions.contains(readTarget)) {
-      deleted = deleted.add(readTarget).add(editTarget);
-    } else {
-      added = added.add(readTarget);
-    }
+
+    idList.forEach((id) => {
+      const readTarget = target + ':read:' + id;
+      const editTarget = target + ':edit:' + id;
+
+      if (this.props.permissions.contains(readTarget)) {
+        deleted = deleted.add(readTarget).add(editTarget);
+      } else {
+        added = added.add(readTarget);
+      }
+    }, this);
     this.props.onChange(added, deleted);
   },
-  _toggleEditPermissions(target, id) {
-    const readTarget = target + ':read:' + id;
-    const editTarget = target + ':edit:' + id;
-
+  _toggleEditPermissions(target, idList) {
     let added = Immutable.Set();
     let deleted = Immutable.Set();
-    if (this.props.permissions.contains(editTarget)) {
-      deleted = deleted.add(editTarget);
-    } else {
-      added = added.add(readTarget).add(editTarget);
-    }
+
+    idList.forEach((id) => {
+      const readTarget = target + ':read:' + id;
+      const editTarget = target + ':edit:' + id;
+
+      if (this.props.permissions.contains(editTarget)) {
+        deleted = deleted.add(editTarget);
+      } else {
+        added = added.add(readTarget).add(editTarget);
+      }
+    }, this);
     this.props.onChange(added, deleted);
+  },
+
+  _toggleAllStreamsRead(streamIds) {
+    this._toggleReadPermissions('streams', streamIds);
+  },
+  _toggleAllStreamsEdit(streamIds) {
+    this._toggleEditPermissions('streams', streamIds);
+  },
+  _toggleAllDashboardsRead(dashboardIds) {
+    this._toggleReadPermissions('dashboards', dashboardIds);
+  },
+  _toggleAllDashboardsEdit(dashboardIds) {
+    this._toggleEditPermissions('dashboards', dashboardIds);
   },
 
   render() {
@@ -60,6 +79,17 @@ const PermissionSelector = React.createClass({
                 active={isEdit}>Allow editing</Button>
       </ButtonGroup>);
     };
+
+    const multiStreamButtons = (streamIds) => {
+      return (
+        <div className="pull-right" style={{marginTop: 10, marginBottom: 10}}>
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllStreamsRead(streamIds)}>Toggle read permissions</Button>
+          &nbsp;
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllStreamsEdit(streamIds)}>Toggle edit permissions</Button>
+        </div>
+      );
+    };
+
     const dashboardItemButtons = (dashboard) => {
       const isRead = this.props.permissions.contains(`dashboards:read:${dashboard.id}`);
       const isEdit = this.props.permissions.contains(`dashboards:edit:${dashboard.id}`);
@@ -70,6 +100,17 @@ const PermissionSelector = React.createClass({
                 active={isEdit}>Allow editing</Button>
       </ButtonGroup>);
     };
+
+    const multiDashboardButtons = (dashboardIds) => {
+      return (
+        <div className="pull-right" style={{marginTop: 10, marginBottom: 10}}>
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllDashboardsRead(dashboardIds)}>Toggle read permissions</Button>
+          &nbsp;
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllDashboardsEdit(dashboardIds)}>Toggle edit permissions</Button>
+        </div>
+      );
+    };
+
     return (
       <div>
         <TabbedArea defaultActiveKey={1} animation={false}>
@@ -79,6 +120,7 @@ const PermissionSelector = React.createClass({
               filterLabel="Filter Streams"
               filterKeys={['title']}
               itemActionsFactory={streamItemButtons}
+              headerActionsFactory={multiStreamButtons}
             />
           </TabPane>
           <TabPane eventKey={2} tab="Dashboards">
@@ -87,6 +129,7 @@ const PermissionSelector = React.createClass({
               filterLabel="Filter Dashboards"
               filterKeys={['title']}
               itemActionsFactory={dashboardItemButtons}
+              headerActionsFactory={multiDashboardButtons}
             />
           </TabPane>
         </TabbedArea>
