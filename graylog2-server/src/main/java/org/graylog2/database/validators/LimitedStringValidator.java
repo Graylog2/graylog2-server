422d7c9b6384976d91a41c6693b6dd9b3aac1a1d
Jochen Schalanda <jochen@graylog.com>
2015-08-26 12:39:40 +0200

    Fix LimitedStringValidator and add tests

--- graylog2-server/src/main/java/org/graylog2/database/validators/LimitedStringValidator.java
+++ graylog2-server/src/main/java/org/graylog2/database/validators/LimitedStringValidator.java
@@ -18,33 +18,37 @@ package org.graylog2.database.validators;
 
 import org.graylog2.plugin.database.validators.ValidationResult;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class LimitedStringValidator extends FilledStringValidator {
-    private int minLength;
-    private int maxLength;
+    private final int minLength;
+    private final int maxLength;
 
     public LimitedStringValidator(int minLength, int maxLength) {
+        checkArgument(minLength > 0, "minLength must be greater than 0");
+        checkArgument(maxLength > 0, "maxLength must be greater than 0");
+        checkArgument(minLength <= maxLength, "maxLength must be greater than or equal to minLength");
+
         this.minLength = minLength;
         this.maxLength = maxLength;
     }
 
     /**
-     * Validates: applies the validation from FilledStringValidator and also check that value's length
+     * Validates: applies the validation from {@link FilledStringValidator} and also check that value's length
      * is between the minimum and maximum length passed to the constructor.
-     *
      *
      * @param value The object to check
      * @return validation result
      */
     @Override
     public ValidationResult validate(Object value) {
-        ValidationResult superResult = super.validate(value);
-        if (superResult instanceof ValidationResult.ValidationPassed) {
-            String sValue = (String)value;
-            if (sValue.length() >= minLength && sValue.length() <= maxLength)
-                new ValidationResult.ValidationPassed();
-            else
-                new ValidationResult.ValidationFailed("Value is not between " + minLength + " and " + maxLength + " in length!");
+        ValidationResult result = super.validate(value);
+        if (result instanceof ValidationResult.ValidationPassed) {
+            final String sValue = (String)value;
+            if (sValue.length() < minLength || sValue.length() > maxLength) {
+                result = new ValidationResult.ValidationFailed("Value is not between " + minLength + " and " + maxLength + " in length!");
+            }
         }
-        return superResult;
+        return result;
     }
 }
---
+++ graylog2-server/src/test/java/org/graylog2/database/validators/LimitedStringValidatorTest.java
@@ -0,0 +1,97 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.database.validators;
+
+import org.graylog2.plugin.database.validators.ValidationResult;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LimitedStringValidatorTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeMinLength() {
+        new LimitedStringValidator(-1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeMaxLength() {
+        new LimitedStringValidator(1, -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testZeroMinLength() {
+        new LimitedStringValidator(0, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testZeroMaxLength() {
+        new LimitedStringValidator(1, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMinLengthGreaterThanMaxLength() {
+        new LimitedStringValidator(10, 1);
+    }
+
+    @Test
+    public void testValidateNullValue() {
+        assertThat(new LimitedStringValidator(1, 1).validate(null))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateEmptyValue() {
+        assertThat(new LimitedStringValidator(1, 1).validate(""))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateLongString() {
+        assertThat(new LimitedStringValidator(1, 1).validate("12"))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateShortString() {
+        assertThat(new LimitedStringValidator(2, 2).validate("1"))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateNoString() {
+        assertThat(new LimitedStringValidator(1, 1).validate(123))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateValidString() {
+        assertThat(new LimitedStringValidator(1, 5).validate("test"))
+                .isInstanceOf(ValidationResult.ValidationPassed.class);
+    }
+
+    @Test
+    public void testValidateMinLengthString() {
+        assertThat(new LimitedStringValidator(1, 5).validate("1"))
+                .isInstanceOf(ValidationResult.ValidationPassed.class);
+    }
+
+    @Test
+    public void testValidateMaxLengthString() {
+        assertThat(new LimitedStringValidator(1, 5).validate("12345"))
+                .isInstanceOf(ValidationResult.ValidationPassed.class);
+    }
+}
