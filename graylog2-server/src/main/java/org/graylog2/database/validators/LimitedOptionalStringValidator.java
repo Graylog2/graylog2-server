2f0adf269a1204a931fdebcb7b2dd0d493a357c7
Jochen Schalanda <jochen@graylog.com>
2015-08-26 12:46:19 +0200

    Fix LimitedOptionalStringValidator and add tests

--- graylog2-server/src/main/java/org/graylog2/database/validators/LimitedOptionalStringValidator.java
+++ graylog2-server/src/main/java/org/graylog2/database/validators/LimitedOptionalStringValidator.java
@@ -18,26 +18,27 @@ package org.graylog2.database.validators;
 
 import org.graylog2.plugin.database.validators.ValidationResult;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class LimitedOptionalStringValidator extends OptionalStringValidator {
-    private int maxLength;
+    private final int maxLength;
 
     public LimitedOptionalStringValidator(int maxLength) {
+        checkArgument(maxLength > 0, "maxLength must be greater than 0");
         this.maxLength = maxLength;
     }
 
     @Override
     public ValidationResult validate(Object value) {
-        final ValidationResult superResult = super.validate(value);
+        ValidationResult result = super.validate(value);
 
-        if (superResult instanceof ValidationResult.ValidationPassed) {
+        if (result instanceof ValidationResult.ValidationPassed) {
             final String sValue = (String) value;
-            if (sValue == null || sValue.length() <= maxLength) {
-                new ValidationResult.ValidationPassed();
-            } else {
-                new ValidationResult.ValidationFailed("Value is longer than " + maxLength + " characters!");
+            if (sValue != null && sValue.length() > maxLength) {
+                result = new ValidationResult.ValidationFailed("Value is longer than " + maxLength + " characters!");
             }
         }
 
-        return superResult;
+        return result;
     }
 }
---
+++ graylog2-server/src/test/java/org/graylog2/database/validators/LimitedOptionalStringValidatorTest.java
@@ -0,0 +1,64 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.database.validators;
+
+import org.graylog2.plugin.database.validators.ValidationResult;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LimitedOptionalStringValidatorTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeMaxLength() {
+        new LimitedOptionalStringValidator(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testZeroMaxLength() {
+        new LimitedOptionalStringValidator(0);
+    }
+
+    @Test
+    public void testValidateNullValue() {
+        assertThat(new LimitedOptionalStringValidator(1).validate(null))
+                .isInstanceOf(ValidationResult.ValidationPassed.class);
+    }
+
+    @Test
+    public void testValidateEmptyValue() {
+        assertThat(new LimitedOptionalStringValidator(1).validate(""))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateLongString() {
+        assertThat(new LimitedOptionalStringValidator(1).validate("12"))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateNoString() {
+        assertThat(new LimitedOptionalStringValidator(1).validate(123))
+                .isInstanceOf(ValidationResult.ValidationFailed.class);
+    }
+
+    @Test
+    public void testValidateMaxLengthString() {
+        assertThat(new LimitedOptionalStringValidator(5).validate("12345"))
+                .isInstanceOf(ValidationResult.ValidationPassed.class);
+    }
+}
