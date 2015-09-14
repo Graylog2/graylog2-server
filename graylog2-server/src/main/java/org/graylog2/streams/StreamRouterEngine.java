67d2ad2933c6a80fc2b0b0e2b0da7720e78a44e6
Edmundo Alvarez <edmundo@graylog.com>
2015-09-14 14:16:45 +0200

    Fix isMatched results for OR streams
    When testing a stream using an OR operator, check if any of the values
    for the rules is true instead of false.
    
    Fixes #1417

--- graylog2-server/src/main/java/org/graylog2/streams/StreamRouterEngine.java
+++ graylog2-server/src/main/java/org/graylog2/streams/StreamRouterEngine.java
@@ -319,7 +319,7 @@ public class StreamRouterEngine {
         public boolean isMatched() {
             switch (matchingType) {
                 case OR:
-                    return matches.values().contains(false);
+                    return matches.values().contains(true);
                 case AND:
                 default:
                     return matches.size() > 0 && !matches.values().contains(false);
