a5748649d61bfa8347e8adf1c5bc07aefa888e52
Bernd Ahlers <bernd@graylog.com>
2015-09-14 15:54:24 +0200

    [human-release-plugin] prepare for next devlopment iteration

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ import com.typesafe.sbt.less.Import.LessKeys
 
 object ApplicationBuild extends Build {
   val appName         = "graylog-web-interface"
-  val appVersion      = "1.2.0"
+  val appVersion      = "1.2.1-SNAPSHOT"
   val appDependencies = Seq(
     cache,
     javaCore,
