9a317171f8466c78c0c1456938695e8b6075eaf5
Dennis Oelkers <dennis@graylog.com>
2015-09-15 11:20:35 +0200

    Adding annotation for default password hashing algorithm.

---
+++ graylog2-server/src/main/java/org/graylog2/users/DefaultPasswordAlgorithm.java
@@ -0,0 +1,13 @@
+package org.graylog2.users;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@Qualifier
+public @interface DefaultPasswordAlgorithm {
+}
