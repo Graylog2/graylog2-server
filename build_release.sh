eac413dd4fcf77077cca050af3934885b8503276
Edmundo Alvarez <edmundo@graylog.com>
2015-09-28 17:45:23 +0200

    Use npm 3 on build_release script

--- build_release.sh
+++ build_release.sh
@@ -30,12 +30,12 @@ pushd javascript
 
 # Install same npm version as we use in travis
 rm -rf ./node_modules
-npm install --no-spin npm@latest-2
+npm install npm@latest --no-spin --progress=false
 
 echo -n "Using npm "
 ./node_modules/.bin/npm --version
 
-./node_modules/.bin/npm install --no-spin
+./node_modules/.bin/npm install --progress=false
 ./node_modules/.bin/npm test
 ./node_modules/.bin/npm run build
 popd
