bc0bbf60196de8fb9f6fbb17e0f1508943bcd924
Dennis Oelkers <dennis@graylog.com>
2015-09-29 14:55:47 +0200

    Rendering initial component straightaway.

--- javascript/src/mount.jsx
+++ javascript/src/mount.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import $ from 'jquery';
 import AppFacade from 'routing/AppFacade';
 
-$(document).ready(() => {
-  // must come first because it registers a global (ahem) singleton.
-  require('./stores/metrics/mount');
-  require('./stores/nodes/mount');
+// must come first because it registers a global (ahem) singleton.
+require('./stores/metrics/mount');
+require('./stores/nodes/mount');
 
-  React.render(<AppFacade />, document.body);
-});
-
+React.render(<AppFacade />, document.body);
