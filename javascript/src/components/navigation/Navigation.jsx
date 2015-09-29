f87033f9dbc40425b2fb667b041d19da98b9d04a
Dennis Oelkers <dennis@graylog.com>
2015-09-29 14:56:09 +0200

    Replacing static links with react-router helpers.

--- javascript/src/components/navigation/Navigation.jsx
+++ javascript/src/components/navigation/Navigation.jsx
@@ -10,11 +10,11 @@ import Routes from 'routing/Routes';
 
 const Navigation = React.createClass({
   render() {
-    const logoUrl = jsRoutes.controllers.Assets.at('images/toplogo.png').url;
-    const homeUrl = jsRoutes.controllers.StartpageController.redirect().url;
-    const brand = (<a href={homeUrl}>
-      <img src={logoUrl}/>
-    </a>);
+    const logoUrl = require('images/toplogo.png');
+    const brand = (
+      <LinkContainer to={Routes.HOME}>
+        <img src={logoUrl}/>
+      </LinkContainer>);
     return (
       <Navbar inverse fluid fixedTop toggleNavKey={0}>
         <NavBrand>{brand}</NavBrand>
@@ -27,38 +27,39 @@ const Navigation = React.createClass({
               <NavItem>Streams</NavItem>
             </LinkContainer>
 
-            <NavItem href={jsRoutes.controllers.DashboardsController.index().url}
-                     active={this._isActive('/dashboards')}>Dashboards</NavItem>
+            <LinkContainer to={Routes.DASHBOARDS}>
+              <NavItem >Dashboards</NavItem>
+            </LinkContainer>
 
             {this._isPermitted(['SOURCES_READ']) &&
-              <NavItem href={jsRoutes.controllers.SourcesController.list().url}
-                       active={this._isActive("/sources")}>Sources</NavItem>
+              <LinkContainer to={Routes.SOURCES}>
+                <NavItem>Sources</NavItem>
+              </LinkContainer>
             }
-            <DropdownButton title={this._systemTitle()} active={this._isActive("/system")}>
-              <MenuItem href={jsRoutes.controllers.SystemController.index(0).url}>Overview</MenuItem>
-              <MenuItem href={jsRoutes.controllers.NodesController.nodes().url}>Nodes</MenuItem>
-              { this._isPermitted(['INPUTS_READ']) && <MenuItem href={jsRoutes.controllers.InputsController.index().url}>Inputs</MenuItem> }
-              { this._isPermitted(['OUTPUTS_READ']) && <MenuItem href={jsRoutes.controllers.OutputsController.index().url}>Outputs</MenuItem> }
-              { this._isPermitted(['COLLECTORS_READ']) && <MenuItem href={jsRoutes.controllers.CollectorsController.index().url}>Collectors</MenuItem> }
-              { this._isPermitted(['INDICES_READ']) && <MenuItem href={jsRoutes.controllers.IndicesController.index().url}>Indices</MenuItem> }
-              { this._isPermitted(['LOGGERS_READ']) && <MenuItem href={jsRoutes.controllers.LoggingController.index().url}>Logging</MenuItem> }
-              { this._isPermitted(['USERS_EDIT']) && <MenuItem href={jsRoutes.controllers.UsersController.index().url}>Users</MenuItem> }
-              { this._isPermitted(['ROLES_EDIT']) && <MenuItem href={jsRoutes.controllers.UsersController.rolesPage().url}>Roles</MenuItem> }
-              { this._isPermitted(['DASHBOARDS_CREATE', 'INPUTS_CREATE', 'STREAMS_CREATE']) && <MenuItem href={jsRoutes.controllers.BundlesController.index().url}>Content Packs</MenuItem> }
-              { this._isPermitted(['INPUTS_EDIT']) && <MenuItem href={jsRoutes.controllers.GrokPatternsController.index().url}>Grok Patterns</MenuItem> }
+            <DropdownButton>
+              <LinkContainer to={Routes.SYSTEM.OVERVIEW}>
+                <MenuItem>Overview</MenuItem>
+              </LinkContainer>
+              <LinkContainer to={Routes.SYSTEM.NODES}>
+                <MenuItem>Nodes</MenuItem>
+              </LinkContainer>
             </DropdownButton>
           </Nav>
 
           <Nav navbar>
-            <NavItem href={jsRoutes.controllers.SystemController.index(0).url} className="notification-badge-link">
-              <span className="badge" style={{backgroundColor: '#ff3b00'}} id="notification-badge"></span>
-            </NavItem>
+            <LinkContainer to={Routes.SYSTEM.OVERVIEW}>
+              <NavItem className="notification-badge-link">
+                <span className="badge" style={{backgroundColor: '#ff3b00'}} id="notification-badge"></span>
+              </NavItem>
+            </LinkContainer>
           </Nav>
 
           <Nav navbar right>
-            <NavItem href={jsRoutes.controllers.NodesController.nodes().url}>
-              <GlobalThroughput />
-            </NavItem>
+            <LinkContainer to={Routes.SYSTEM.NODES}>
+              <NavItem>
+                <GlobalThroughput />
+              </NavItem>
+            </LinkContainer>
             <UserMenu fullName={this.props.fullName} loginName={this.props.loginName}/>
           </Nav>
         </CollapsibleNav>
--- javascript/src/components/navigation/UserMenu.jsx
+++ javascript/src/components/navigation/UserMenu.jsx
@@ -5,6 +5,7 @@ import { MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import SessionActions from 'actions/sessions/SessionActions';
 import SessionStore from 'stores/sessions/SessionStore';
+import Routes from 'routing/Routes';
 
 const UserMenu = React.createClass({
   onLogoutClicked() {
@@ -13,7 +14,9 @@ const UserMenu = React.createClass({
   render() {
     return (
       <NavDropdown navItem title={this.props.fullName} id="user-menu-dropdown">
-        <MenuItem href={jsRoutes.controllers.UsersController.editUserForm(this.props.loginName).url}>Edit profile</MenuItem>
+        <LinkContainer to={Routes.USER_EDIT}>
+          <MenuItem>Edit profile</MenuItem>
+        </LinkContainer>
         <MenuItem divider />
         <MenuItem onClick={this.onLogoutClicked}><i className="fa fa-sign-out"></i> Log out</MenuItem>
       </NavDropdown>
