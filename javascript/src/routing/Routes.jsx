de9f3518149c6f702bdcbbcd2adbb7336a71d9b3
Dennis Oelkers <dennis@graylog.com>
2015-09-29 14:56:31 +0200

    Adding prospective routes.

--- javascript/src/routing/Routes.jsx
+++ javascript/src/routing/Routes.jsx
@@ -1,5 +1,13 @@
 const Routes = {
+  HOME: '/',
   STREAMS: 'streams',
+  SOURCES: 'sources',
+  DASHBOARDS: 'dashboards',
+  SYSTEM: {
+    OVERVIEW: 'system/overview',
+    NODES: 'system/nodes',
+  },
+  USER_EDIT: 'user/edit',
 };
 
 export default Routes;
