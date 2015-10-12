26c6d01f4c16a9ba7ed12d52f58bead1aa0dd7e4
Jochen Schalanda <jochen@graylog.com>
2015-10-12 11:21:49 +0200

    Bump version to 2.0.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ import com.typesafe.sbt.less.Import.LessKeys
 
 object ApplicationBuild extends Build {
   val appName         = "graylog-web-interface"
-  val appVersion      = "1.3.0-SNAPSHOT"
+  val appVersion      = "2.0.0-SNAPSHOT"
   val appDependencies = Seq(
     cache,
     javaCore,
