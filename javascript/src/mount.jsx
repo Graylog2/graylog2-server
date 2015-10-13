e73949b3515a05e1fb9fba445a267444839a241a
Dennis Oelkers <dennis@graylog.com>
2015-10-13 16:35:17 +0200

    Disable MetricsStore initialisation for now.

--- javascript/src/mount.jsx
+++ javascript/src/mount.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AppFacade from 'routing/AppFacade';
 
 // must come first because it registers a global (ahem) singleton.
-require('./stores/metrics/mount');
+//require('./stores/metrics/mount');
 require('./stores/nodes/mount');
 
 React.render(<AppFacade />, document.body);
