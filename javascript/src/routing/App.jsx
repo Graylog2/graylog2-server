acb7fb55352d6c97681f45b013282d46d3a8bbcc
Dennis Oelkers <dennis@graylog.com>
2015-10-13 15:07:24 +0200

    Add stylesheets and needed styling elements in main app template.

--- javascript/src/routing/App.jsx
+++ javascript/src/routing/App.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import Navigation from 'components/navigation/Navigation';
+import { Row, Col } from 'react-bootstrap';
+
+import 'javascripts/shims/styles/shim.css';
+import 'stylesheets/bootstrap.min.css';
+import 'stylesheets/font-awesome.min.css';
+import 'stylesheets/newfonts.less';
+import 'stylesheets/bootstrap-submenus.less';
+import 'stylesheets/rickshaw.min.css';
+import 'stylesheets/toastr.min.css';
+import 'stylesheets/datepicker.less';
+import 'stylesheets/chosen.bootstrap.min.css';
+import 'stylesheets/chosen-bootstrap.less';
+import 'stylesheets/jquery.gridster.min.css';
+import 'stylesheets/jquery.dynatable.css';
+import 'stylesheets/typeahead.less';
+import 'stylesheets/graylog2.less';
+import 'c3/c3.css';
+import 'dc/dc.css';
 
 const App = React.createClass({
   render() {
     return (
       <div>
         <Navigation requestPath="/" fullName="John Doe" loginName="johndoe"/>
-        {this.props.children}
+        <div className="container-fluid">
+          <Row id="main-row">
+            <Col md={12} id="main-content">
+              {this.props.children}
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   },
