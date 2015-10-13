6ad99fa419a94a33f6d86396ed84a5dffea4923c
Dennis Oelkers <dennis@graylog.com>
2015-10-13 16:50:35 +0200

    Update travis config and build script, remove shrinkwrap file.

--- build_release.sh
+++ build_release.sh
@@ -1,27 +1,4 @@
 #!/bin/bash -e
-SBT_VERSION='0.13.9'
-SBT_URL="https://dl.bintray.com/sbt/native-packages/sbt/${SBT_VERSION}/sbt-${SBT_VERSION}.tgz"
-
-SBT_BIN=$(which sbt)
-
-if [[ -z "${SBT_BIN}" ]]; then
-  SBT_BIN="${SBT_PATH}/bin/sbt"
-
-  if [[ ! -x "${SBT_BIN}" ]]; then
-    echo "ERROR: Couldn't find SBT in \$PATH or \$SBT_PATH."
-    echo
-    echo "Please download and install SBT before running this script:"
-    echo
-    echo "  $ wget ${SBT_URL}"
-    echo "  $ brew install sbt ## Homebrew (Third-party package)"
-    echo "  $ port install sbt ## Macports (Third-party package)"
-    echo
-    exit 1
-  fi
-fi
-
-# Clean working directory
-"${SBT_BIN}" clean
 
 ########################
 ## Prepare JavaScript ##
@@ -30,49 +7,14 @@ pushd javascript
 
 # Install same npm version as we use in travis
 rm -rf ./node_modules
-rm -rf ./node_globals
-
-# Install npm globally using the prefix argument, avoiding using some shared location to put the files,
-# and also avoiding pollution in the `node_modules` folder.
-npm install -g npm@latest --prefix=./node_globals --no-spin --progress=false
+npm install --no-spin npm@latest-2
 
 echo -n "Using npm "
-./node_globals/bin/npm --version
+./node_modules/.bin/npm --version
 
-./node_globals/bin/npm install --progress=false
-./node_globals/bin/npm test
-./node_globals/bin/npm run build
-popd
-
-
-############################
-## Build Play application ##
-############################
-"${SBT_BIN}" update
-"${SBT_BIN}" compile
-"${SBT_BIN}" test
-
-# Build universal .tar.gz
-"${SBT_BIN}" universal:package-zip-tarball
-
-date
-echo "Your package(s) are ready in 'target/universal':"
-echo
-ls -lt ./target/universal/graylog-web-interface-*.tgz
-
-echo
-echo '# Calculating artifact checksums'
-pushd ./target/universal
-for ARTIFACT in graylog-web-interface-*.tgz
-do
-  echo
-  echo -n "MD5:    "
-  md5sum "${ARTIFACT}" | tee "${ARTIFACT}.md5.txt"
-  echo -n "SHA1:   "
-  sha1sum "${ARTIFACT}" | tee "${ARTIFACT}.sha1.txt"
-  echo -n "SHA256: "
-  sha256sum "${ARTIFACT}" | tee "${ARTIFACT}.sha256.txt"
-done
+./node_modules/.bin/npm install --no-spin
+./node_modules/.bin/npm test
+./node_modules/.bin/npm run build
 popd
 
 echo
