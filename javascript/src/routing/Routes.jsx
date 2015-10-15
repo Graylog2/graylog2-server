fc05d08798ea36db6e778dd089c36a29c7f387fc
Dennis Oelkers <dennis@graylog.com>
2015-10-15 10:37:20 +0200

    Correct application routes.

--- javascript/src/routing/Routes.jsx
+++ javascript/src/routing/Routes.jsx
@@ -1,18 +1,18 @@
 const Routes = {
   HOME: '/',
-  STREAMS: 'streams',
-  SOURCES: 'sources',
-  DASHBOARDS: 'dashboards',
+  STREAMS: '/streams',
+  SOURCES: '/sources',
+  DASHBOARDS: '/dashboards',
   SYSTEM: {
-    OVERVIEW: 'system/overview',
-    NODES: 'system/nodes',
+    OVERVIEW: '/system/overview',
+    NODES: '/system/nodes',
   },
-  USER_EDIT: 'user/edit',
-  stream_edit: (streamId) => 'streams/' + streamId + '/edit',
-  stream_outputs: (streamId) => 'streams/' + streamId + '/outputs',
-  stream_alerts: (streamId) => 'streams/' + streamId + '/alerts',
-  stream_search: (streamId, query, type, range) => 'streams/' + streamId + '/search' + '?query=' + query + "&type=" + range + '&range=' + range,
-  startpage_set: (type, id) => 'startpage/set/' + type + '/' + id,
+  USER_EDIT: '/user/edit',
+  stream_edit: (streamId) => '/streams/' + streamId + '/edit',
+  stream_outputs: (streamId) => '/streams/' + streamId + '/outputs',
+  stream_alerts: (streamId) => '/streams/' + streamId + '/alerts',
+  stream_search: (streamId, query, type, range) => '/streams/' + streamId + '/search' + '?query=' + query + "&type=" + range + '&range=' + range,
+  startpage_set: (type, id) => '/startpage/set/' + type + '/' + id,
 };
 
 export default Routes;
