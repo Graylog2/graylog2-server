5b61982454dfd80551133b1997073a353e6dc822
Dennis Oelkers <dennis@graylog.com>
2015-10-15 11:39:26 +0200

    Reformat InputDropdown, changing some field names of response.

--- javascript/src/components/inputs/InputDropdown.jsx
+++ javascript/src/components/inputs/InputDropdown.jsx
@@ -1,45 +1,43 @@
-'use strict';
+import React from 'react';
+import LinkedStateMixin from 'react-addons-linked-state-mixin';
+import { Input } from 'react-bootstrap';
+import Immutable from 'immutable';
 
-var React = require('react');
-var LinkedStateMixin = require('react-addons-linked-state-mixin');
-var Input = require('react-bootstrap').Input;
-var Spinner = require('../common/Spinner');
-var Immutable = require('immutable');
+import Spinner from 'components/common/Spinner';
 
-var InputDropdown = React.createClass({
-    mixins: [LinkedStateMixin],
-    PLACEHOLDER: "placeholder",
-    getInitialState() {
-        return {
-            selectedInput: this.PLACEHOLDER
-        };
-    },
-    _formatInput(input) {
-        return <option key={input.id} value={input.id}>{input.title} ({input.type})</option>;
-    },
-    _sortByTitle(input1, input2) {
-        return input1.title.localeCompare(input2.title);
-    },
-    _onClick() {
-        this.props.onClick(this.state.selectedInput);
-    },
-    render() {
-        if (this.props.inputs) {
-            var inputs = Immutable.List(this.props.inputs.sort(this._sortByTitle).values()).map(this._formatInput);
-            return (
-                <div>
-                        <Input type='select' style={{float: "left", width: "400px", marginRight: "10px"}} valueLink={this.linkState('selectedInput')} placeholder={this.PLACEHOLDER}>
-                            <option value={this.PLACEHOLDER}>Select an input</option>
-                            {inputs}
-                        </Input>
-
-                        <a className="btn btn-info" disabled={this.state.selectedInput === this.PLACEHOLDER} onClick={this._onClick}>{this.props.title}</a>
-                </div>
-            );
-        } else {
-            return <Spinner />;
-        }
+const InputDropdown = React.createClass({
+  mixins: [LinkedStateMixin],
+  PLACEHOLDER: "placeholder",
+  getInitialState() {
+    return {
+      selectedInput: this.PLACEHOLDER
+    };
+  },
+  _formatInput(input) {
+    return <option key={input.input_id} value={input.input_id}>{input.title} ({input.type})</option>;
+  },
+  _sortByTitle(input1, input2) {
+    return input1.title.localeCompare(input2.title);
+  },
+  _onClick() {
+    this.props.onClick(this.state.selectedInput);
+  },
+  render() {
+    if (!this.props.inputs) {
+      return <Spinner />;
     }
+    const inputs = Immutable.List(this.props.inputs.sort(this._sortByTitle).values()).map(this._formatInput);
+    return (
+      <div>
+        <Input type='select' style={{float: "left", width: "400px", marginRight: "10px"}} valueLink={this.linkState('selectedInput')} placeholder={this.PLACEHOLDER}>
+          <option value={this.PLACEHOLDER}>Select an input</option>
+          {inputs}
+        </Input>
+
+        <a className="btn btn-info" disabled={this.state.selectedInput === this.PLACEHOLDER} onClick={this._onClick}>{this.props.title}</a>
+      </div>
+    );
+  }
 });
 
-module.exports = InputDropdown;
+export default InputDropdown;
