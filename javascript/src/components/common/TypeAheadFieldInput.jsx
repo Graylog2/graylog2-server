9130d483c1d8e71a2339a96a652bc5d9b026724d
Dennis Oelkers <dennis@graylog.com>
2015-10-15 13:04:31 +0200

    Reformatting TypeAheadFieldInput, using explicit imports.

--- javascript/src/components/common/TypeAheadFieldInput.jsx
+++ javascript/src/components/common/TypeAheadFieldInput.jsx
@@ -1,71 +1,70 @@
-/* global jsRoutes, substringMatcher */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Immutable from 'immutable';
+import { Input } from 'react-bootstrap';
+import $ from 'jquery';
+import UniversalSearch from 'logic/search/UniversalSearch';
 
-'use strict';
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Immutable = require('immutable');
-var Input = require('react-bootstrap').Input;
-var $ = require('jquery');
+import jsRoutes from 'routing/jsRoutes';
+import URLUtils from 'util/URLUtils';
+import fetch from 'logic/rest/FetchProvider';
 
 var TypeAheadFieldInput = React.createClass({
-    componentDidMount() {
-        if (this.refs.fieldInput) {
-            var fieldInput = $(this.refs.fieldInput.getInputDOMNode());
-            $.ajax({
-                url: jsRoutes.controllers.api.SystemApiController.fields().url,
-                success: (data) => {
-                    fieldInput.typeahead({
-                            hint: true,
-                            highlight: true,
-                            minLength: 1
-                        },
-                        {
-                            name: 'fields',
-                            displayKey: 'value',
-                            source: substringMatcher(data.fields, 'value', 6)
-                        });
-
-                    if (this.props.autoFocus) {
-                        fieldInput.focus();
-                        fieldInput.typeahead('close');
-                    }
-                }
+  componentDidMount() {
+    if (this.refs.fieldInput) {
+      const fieldInput = $(this.refs.fieldInput.getInputDOMNode());
+      fetch('GET', URLUtils.qualifyUrl(jsRoutes.controllers.api.SystemApiController.fields().url))
+        .then((data) => {
+          fieldInput.typeahead({
+              hint: true,
+              highlight: true,
+              minLength: 1
+            },
+            {
+              name: 'fields',
+              displayKey: 'value',
+              source: UniversalSearch.substringMatcher(data.fields, 'value', 6)
             });
 
-            var fieldFormGroup = ReactDOM.findDOMNode(this.refs.fieldInput);
-            $(fieldFormGroup).on('typeahead:change typeahead:selected', (event) => {
-                if (this.props.valueLink) {
-                    this.props.valueLink.requestChange(event.target.value);
-                }
-            });
+          if (this.props.autoFocus) {
+            fieldInput.focus();
+            fieldInput.typeahead('close');
+          }
+        });
+
+      const fieldFormGroup = ReactDOM.findDOMNode(this.refs.fieldInput);
+      $(fieldFormGroup).on('typeahead:change typeahead:selected', (event) => {
+        if (this.props.valueLink) {
+          this.props.valueLink.requestChange(event.target.value);
         }
-    },
-    componentWillUnmount() {
-        if (this.refs.fieldInput) {
-            var fieldInput = $(this.refs.fieldInput.getInputDOMNode());
-            fieldInput.typeahead('destroy');
-            var fieldFormGroup = ReactDOM.findDOMNode(this.refs.fieldInput);
-            $(fieldFormGroup).off('typeahead:change typeahead:selected');
-        }
-    },
-
-    _getFilteredProps() {
-        var props = Immutable.fromJS(this.props);
-
-        if (props.has('valueLink')) {
-            props = props.delete('valueLink');
-        }
-
-        return props.toJS();
-    },
-
-    render() {
-        return <Input ref="fieldInput"
-                      wrapperClassName="typeahead-wrapper"
-                      defaultValue={this.props.valueLink.value}
-                      {...this._getFilteredProps()}/>;
+      });
     }
+  },
+  componentWillUnmount() {
+    if (this.refs.fieldInput) {
+      const fieldInput = $(this.refs.fieldInput.getInputDOMNode());
+      fieldInput.typeahead('destroy');
+      const fieldFormGroup = ReactDOM.findDOMNode(this.refs.fieldInput);
+      $(fieldFormGroup).off('typeahead:change typeahead:selected');
+    }
+  },
+
+  _getFilteredProps() {
+    var props = Immutable.fromJS(this.props);
+
+    if (props.has('valueLink')) {
+      props = props.delete('valueLink');
+    }
+
+    return props.toJS();
+  },
+
+  render() {
+    return <Input ref="fieldInput"
+                  wrapperClassName="typeahead-wrapper"
+                  defaultValue={this.props.valueLink.value}
+      {...this._getFilteredProps()}/>;
+  }
 });
 
-module.exports = TypeAheadFieldInput;
+export default TypeAheadFieldInput;
