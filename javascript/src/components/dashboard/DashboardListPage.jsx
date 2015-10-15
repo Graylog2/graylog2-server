fff4eb4d4b20d75bacddb0ed466bbb856a5ce351
Dennis Oelkers <dennis@graylog.com>
2015-10-15 18:15:36 +0200

    Using PageHeader to clean up DashboardListPage.

--- javascript/src/components/dashboard/DashboardListPage.jsx
+++ javascript/src/components/dashboard/DashboardListPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Immutable from 'immutable';
+import { Row, Col } from 'react-bootstrap';
 
+import DashboardStore from 'stores/dashboard/DashboardStore';
+
+import DocsHelper from 'util/DocsHelper';
+import PermissionsMixin from 'util/PermissionsMixin';
+
+import DocumentationLink from 'components/support/DocumentationLink';
+import Spinner from 'components/common/Spinner';
+import PageHeader from 'components/common/PageHeader';
 import DashboardList from './DashboardList';
 import EditDashboardModalTrigger from './EditDashboardModalTrigger';
-import DashboardStore from '../../stores/dashboard/DashboardStore';
-
-import Spinner from '../common/Spinner';
-import SupportLink from '../support/SupportLink';
-import DocsHelper from '../../util/DocsHelper';
-import DocumentationLink from '../support/DocumentationLink';
-import PermissionsMixin from '../../util/PermissionsMixin';
 
 const DashboardListPage = React.createClass({
   propTypes: {
@@ -27,38 +29,32 @@ const DashboardListPage = React.createClass({
     DashboardStore.addOnDashboardsChangedCallback(this._onDashboardsChange);
     DashboardStore.updateDashboards();
   },
-  render() {
-    let createDashboardButton;
-
-    if (this.isPermitted(this.props.permissions, ['dashboards:create'])) {
-      createDashboardButton = (
-        <div style={{marginTop: 20}}>
-          <EditDashboardModalTrigger action="create" buttonClass="btn-success btn-lg"/>
-        </div>
-      );
+  _onDashboardsChange(dashboards) {
+    if (dashboards) {
+      this.setState({dashboards: dashboards, filteredDashboards: dashboards, dashboardsLoaded: true});
+    } else {
+      this.setState({dashboardsLoaded: false});
     }
+  },
+  render() {
+    const createDashboardButton = this.isPermitted(this.props.permissions, ['dashboards:create']) ?
+      <EditDashboardModalTrigger action="create" buttonClass="btn-success btn-lg"/> : null;
 
     const pageHeader = (
-      <div className="row content content-head">
-        <div className="col-md-10">
-          <h1>Dashboards</h1>
-
-          <p className="description">
+      <PageHeader title="Dashboards">
+        <span>
             Use dashboards to create specific views on your messages. Create a new dashboard here and add
             any graph or chart you create in other parts of Graylog with one click.
-          </p>
+          </span>
 
-          <SupportLink>
+          <span>
             Take a look at the
             {' '}<DocumentationLink page={DocsHelper.PAGES.DASHBOARDS} text="dashboard tutorial"/>{' '}
             for lots of other useful tips.
-          </SupportLink>
-        </div>
+          </span>
 
-        <div className="col-md-2">
-          {createDashboardButton}
-        </div>
-      </div>
+        {createDashboardButton}
+      </PageHeader>
     );
 
     if (!this.state.dashboardsLoaded) {
@@ -66,9 +62,9 @@ const DashboardListPage = React.createClass({
         <div>
           {pageHeader}
 
-          <div className="row content">
+          <Row className="content">
             <div style={{marginLeft: 10}}><Spinner/></div>
-          </div>
+          </Row>
         </div>
       );
     }
@@ -89,20 +85,13 @@ const DashboardListPage = React.createClass({
       <div>
         {pageHeader}
 
-        <div className="row content">
-          <div className="col-md-12">
+        <Row className="content">
+          <Col md={12}>
             {dashboardList}
-          </div>
-        </div>
+          </Col>
+        </Row>
       </div>
     );
-  },
-  _onDashboardsChange(dashboards) {
-    if (dashboards) {
-      this.setState({dashboards: dashboards, filteredDashboards: dashboards, dashboardsLoaded: true});
-    } else {
-      this.setState({dashboardsLoaded: false});
-    }
   },
 });
 
