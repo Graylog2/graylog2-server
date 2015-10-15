905c68cdd3a8f665e2c98dd700e36e9f54fb63e8
Dennis Oelkers <dennis@graylog.com>
2015-10-15 09:50:20 +0200

    Add dev entry points instead of replacing them for webpack hot reloading

--- javascript/webpack.hmr.config.js
+++ javascript/webpack.hmr.config.js
@@ -2,12 +2,9 @@
 var webpackConfig = require('./webpack.shared.config');
 var webpack = require('webpack');
 
-webpackConfig.entry = {
-  hotDevServer: 'webpack/hot/dev-server',
-  onlyDevServer: 'webpack/hot/only-dev-server',
-  app: './src/mount.jsx',
-  config: './src/config.js'
-};
+webpackConfig.entry.hotDevServer = 'webpack/hot/dev-server';
+webpackConfig.entry.onlyDevServer = 'webpack/hot/only-dev-server';
+
 webpackConfig.output.publicPath = 'http://localhost:8080/';
 webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 webpackConfig.plugins.push(new webpack.NoErrorsPlugin());
