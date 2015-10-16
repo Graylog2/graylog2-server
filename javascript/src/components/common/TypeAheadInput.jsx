52aa21fd5fab460eeb8f9c7b0abe4f5216d30d71
Dennis Oelkers <dennis@graylog.com>
2015-10-16 12:12:45 +0200

    Fixing linter errors.

--- javascript/src/components/common/TypeAheadInput.jsx
+++ javascript/src/components/common/TypeAheadInput.jsx
@@ -7,7 +7,28 @@ require('!script!../../../public/javascripts/jquery-2.1.1.min.js');
 require('!script!../../../public/javascripts/typeahead.jquery.min.js');
 
 const TypeAheadInput = React.createClass({
+  propTypes: {
+    label: React.PropTypes.string.isRequired,
+    onKeyPress: React.PropTypes.func.isRequired,
+  },
 
+  componentDidMount() {
+    this._updateTypeahead(this.props);
+  },
+  componentWillReceiveProps(newProps) {
+    this._destroyTypeahead();
+    this._updateTypeahead(newProps);
+  },
+  componentWillUnmount() {
+    this._destroyTypeahead();
+  },
+
+  getValue() {
+    return $(this.fieldInput).typeahead('val');
+  },
+  clear() {
+    $(this.fieldInput).typeahead('val', '');
+  },
   _destroyTypeahead() {
     $(this.fieldInput).typeahead('destroy');
     $(this.fieldFormGroup).off('typeahead:select typeahead:autocomplete');
@@ -16,22 +37,22 @@ const TypeAheadInput = React.createClass({
     this.fieldInput = this.refs.fieldInput.getInputDOMNode();
     this.fieldFormGroup = ReactDOM.findDOMNode(this.refs.fieldInput);
 
-    var $fieldInput = $(this.fieldInput);
+    const $fieldInput = $(this.fieldInput);
 
     // props.suggestions:
     // [ "some string", "otherstring" ]
     $fieldInput.typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-      },
+      hint: true,
+      highlight: true,
+      minLength: 1,
+    },
       {
         name: 'dataset-name',
         displayKey: props.displayKey,
         source: substringMatcher(props.suggestions, props.displayKey, 6),
         templates: {
-          suggestion: (value) => `<div><strong>${ props.suggestionText }</strong> ${ value.value }</div>`
-        }
+          suggestion: (value) => `<div><strong>${ props.suggestionText }</strong> ${ value.value }</div>`,
+        },
       });
 
     if (typeof props.onTypeaheadLoaded === 'function') {
@@ -43,29 +64,12 @@ const TypeAheadInput = React.createClass({
       props.onSuggestionSelected(event, suggestion);
     });
   },
-  componentDidMount() {
-    this._updateTypeahead(this.props);
-  },
-  componentWillUnmount() {
-    this._destroyTypeahead();
-  },
-  componentWillReceiveProps(newProps) {
-    this._destroyTypeahead();
-    this._updateTypeahead(newProps);
-  },
-
-  getValue() {
-    return $(this.fieldInput).typeahead('val');
-  },
-  clear() {
-    $(this.fieldInput).typeahead('val', '');
-  },
   render() {
-    return <Input type="text" ref="fieldInput"
-                  wrapperClassName="typeahead-wrapper"
-                  label={this.props.label}
-                  onKeyPress={this.props.onKeyPress}/>;
-  }
+    return (<Input type="text" ref="fieldInput"
+                   wrapperClassName="typeahead-wrapper"
+                   label={this.props.label}
+                   onKeyPress={this.props.onKeyPress}/>);
+  },
 });
 
-module.exports = TypeAheadInput;
+export default TypeAheadInput;
