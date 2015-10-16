ae07f8e5ae02ce17345618e26d908e7d6fb0ec83
Edmundo Alvarez <edmundo@graylog.com>
2015-10-16 15:03:02 +0200

    Reformat ChosenSelectInput

--- javascript/src/components/common/ChosenSelectInput.jsx
+++ javascript/src/components/common/ChosenSelectInput.jsx
@@ -1,49 +1,55 @@
-'use strict';
+/* global $ */
 
-var $ = require('jquery');
+import React, {PropTypes} from 'react';
+import {Input} from 'react-bootstrap';
 
-var React = require('react');
-var Input = require('react-bootstrap').Input;
+require('!script!../../../public/javascripts/jquery-2.1.1.min.js');
+require('!script!../../../public/javascripts/chosen.jquery.min.js');
 
-var ChosenSelectInput = React.createClass({
-    componentDidMount() {
-        var $selectDOMNode = $(this.refs.select.getInputDOMNode());
+const ChosenSelectInput = React.createClass({
+  propTypes: {
+    children: PropTypes.any,
+    onChange: PropTypes.func,
+    dataPlaceholder: PropTypes.string,
+  },
+  componentDidMount() {
+    const $selectDOMNode = $(this.refs.select.getInputDOMNode());
 
-        // We only want to apply the classes the parent component gave us, not those from react-bootstrap
-        $selectDOMNode.on('chosen:ready', (event, params) => {
-            params.chosen.container.addClass('input-sm');
-        });
+    // We only want to apply the classes the parent component gave us, not those from react-bootstrap
+    $selectDOMNode.on('chosen:ready', (event, params) => {
+      params.chosen.container.addClass('input-sm');
+    });
 
-        $selectDOMNode.chosen({
-            disable_search_threshold: 3,
-            search_contains: true,
-            inherit_select_classes: false,
-            display_disabled_options: false,
-            placeholder_text_single: this.props.dataPlaceholder
-        }).change((event, selection) => {
-            this.selectedOption = selection.selected;
-            if (typeof this.props.onChange === 'function') {
-                this.props.onChange(selection.selected);
-            }
-        });
-    },
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.children !== prevProps.children) {
-            $(this.refs.select.getInputDOMNode()).trigger('chosen:updated');
-        }
-    },
-    getValue() {
-        return this.selectedOption;
-    },
-    render() {
-        // Chosen needs the empty option to render the placeholder
-        return (
-            <Input ref='select' type='select' placeholder='placeholder' {...this.props}>
-                <option value='placeholder'></option>
-                {this.props.children}
-            </Input>
-        );
+    $selectDOMNode.chosen({
+      disable_search_threshold: 3,
+      search_contains: true,
+      inherit_select_classes: false,
+      display_disabled_options: false,
+      placeholder_text_single: this.props.dataPlaceholder,
+    }).change((event, selection) => {
+      this.selectedOption = selection.selected;
+      if (typeof this.props.onChange === 'function') {
+        this.props.onChange(selection.selected);
+      }
+    });
+  },
+  componentDidUpdate(prevProps) {
+    if (this.props.children !== prevProps.children) {
+      $(this.refs.select.getInputDOMNode()).trigger('chosen:updated');
     }
+  },
+  getValue() {
+    return this.selectedOption;
+  },
+  render() {
+    // Chosen needs the empty option to render the placeholder
+    return (
+      <Input ref="select" type="select" placeholder="placeholder" {...this.props}>
+        <option value="placeholder"></option>
+        {this.props.children}
+      </Input>
+    );
+  },
 });
 
-module.exports = ChosenSelectInput;
+export default ChosenSelectInput;
