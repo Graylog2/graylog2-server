a1d4205cf3da88c7af39a345a02aca65d97b1909
Dennis Oelkers <dennis@graylog.com>
2015-10-16 12:02:14 +0200

    Reformat and add pages to DocsHelper.

--- javascript/src/util/DocsHelper.ts
+++ javascript/src/util/DocsHelper.ts
@@ -1,29 +1,45 @@
 'use strict';
 
-import Version = require("./Version");
+import Version = require('./Version');
 
 interface Pages {
-    [page: string]: string;
+  [page: string]: string;
 }
 
 class DocsHelper {
-    PAGES: Pages = {
-        WELCOME: "", // Welcome page to the documentation
-        DASHBOARDS: "dashboards.html",
-        EXTERNAL_DASHBOARDS: "external_dashboards.html",
-        SEARCH_QUERY_LANGUAGE: "queries.html",
-        STREAMS: "streams.html"
-    };
-    DOCS_URL: string = "http://docs.graylog.org/en/";
+  PAGES: Pages = {
+    ALERTS: 'streams.html#alerts',
+    CLUSTER_STATUS_EXPLAINED: 'configuring_es.html#cluster-status-explained',
+    COLLECTOR: 'collector.html',
+    CONFIGURING_ES: 'configuring_es.html',
+    DASHBOARDS: 'dashboards.html',
+    ES_CLUSTER_STATUS_RED: 'configuring_es.html#cluster-status-explained',
+    ES_CLUSTER_UNAVAILABLE: 'configuring_es.html#configuration',
+    ES_OPEN_FILE_LIMITS: 'configuring_es.html#open-file-limits',
+    EXTERNAL_DASHBOARDS: 'external_dashboards.html',
+    EXTRACTORS: 'extractors.html',
+    FLEXIBLE_DATE_CONVERTER: 'extractors.html#the-flexible-date-converter',
+    INDEXER_FAILURES: 'indexer_failures.html',
+    INDEX_MODEL: 'index_model.html',
+    LOAD_BALANCERS: 'load_balancers.html',
+    RADIO_ARCHITECTURE: 'architecture.html#highly-available-setup-with-graylog-radio',
+    SEARCH_QUERY_LANGUAGE: 'queries.html',
+    STANDARD_DATE_CONVERTER: 'extractors.html#the-standard-date-converter',
+    STREAMS: 'streams.html',
+    STREAM_PROCESSING_RUNTIME_LIMITS: 'streams.html#stream-processing-runtime-limits',
+    USERS_ROLES: 'users_roles.html',
+    WELCOME: '', // Welcome page to the documentation
+  };
+  DOCS_URL: string = 'http://docs.graylog.org/en/';
 
-    toString(path: string): string {
-        var baseUrl = this.DOCS_URL + Version.getMajorAndMinorVersion();
-        return path === "" ? baseUrl : baseUrl + "/pages/" + path;
-    }
+  toString(path: string): string {
+    var baseUrl = this.DOCS_URL + Version.getMajorAndMinorVersion();
+    return path === '' ? baseUrl : baseUrl + '/pages/' + path;
+  }
 
-    toLink(path: string, title: string): string {
-        return "<a href=\"" + this.toString(path) + "\" target=\"_blank\">" + title + "</a>";
-    }
+  toLink(path: string, title: string): string {
+    return '<a href=\'' + this.toString(path) + '\' target=\'_blank\'>' + title + '</a>';
+  }
 }
 
 var docsHelper = new DocsHelper();
