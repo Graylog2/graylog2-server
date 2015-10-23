903e3cbed743f46ac1dca7e06ff43540fed55dac
Dennis Oelkers <dennis@graylog.com>
2015-10-23 09:30:09 +0200

    Make sizes and styles configurable for PageHeader component.

--- javascript/src/components/common/PageHeader.jsx
+++ javascript/src/components/common/PageHeader.jsx
@@ -7,13 +7,23 @@ const PageHeader = React.createClass({
   propTypes: {
     title: React.PropTypes.string.isRequired,
     children: React.PropTypes.oneOfType([React.PropTypes.array, React.PropTypes.node]),
+    titleSize: React.PropTypes.number,
+    buttonSize: React.PropTypes.number,
+    buttonStyle: React.PropTypes.object,
+  },
+  getDefaultProps() {
+    return {
+      titleSize: 10,
+      buttonSize: 2,
+      buttonStyle: {textAlign: 'center', marginTop: '35px'},
+    };
   },
   render() {
     const children = (this.props.children.length !== undefined ? this.props.children : [this.props.children]);
     return (
       <span>
         <Row className="content content-head">
-          <Col md={10}>
+          <Col md={this.props.titleSize}>
             <h1>
               {this.props.title}
             </h1>
@@ -28,7 +38,7 @@ const PageHeader = React.createClass({
             }
           </Col>
           {children[2] &&
-          <Col md={2} style={{textAlign: 'center', marginTop: '35px'}}>
+          <Col md={this.props.buttonSize} style={this.props.buttonStyle}>
             {children[2]}
           </Col>
           }
