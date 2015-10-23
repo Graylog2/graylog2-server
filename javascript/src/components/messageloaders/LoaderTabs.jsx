cb1b9f94b5e3180dd11a70f5e508ce5719b94f97
Dennis Oelkers <dennis@graylog.com>
2015-10-23 09:10:51 +0200

    Close Col tag in LoaderTabs.

--- javascript/src/components/messageloaders/LoaderTabs.jsx
+++ javascript/src/components/messageloaders/LoaderTabs.jsx
@@ -57,7 +57,7 @@ const LoaderTabs = React.createClass({
                        disableTestAgainstStream
                        disableFieldActions={!this.props.customFieldActions}
                        customFieldActions={this.props.customFieldActions}/>
-        </div>
+        </Col>
       );
     }
 
