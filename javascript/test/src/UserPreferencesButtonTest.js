d324b5b2a78452b5fc9729ef6ec0a27b80ecaeb1
Dennis Oelkers <dennis@graylog.com>
2015-10-26 16:41:42 +0100

    Reformatting and adapting test.

--- javascript/test/src/UserPreferencesButtonTest.js
+++ javascript/test/src/UserPreferencesButtonTest.js
@@ -1,35 +1,28 @@
-'use strict';
+import PreferencesStore from 'stores/users/PreferencesStore';
+import UserPreferencesButton from 'components/users/UserPreferencesButton';
+import UserPreferencesModal from 'components/users/UserPreferencesModal';
+import React from 'react/addons';
+import $ from 'jquery';
 
-var PreferencesStore = require('../../src/stores/users/PreferencesStore');
-var UserPreferencesButton = require('../../src/components/users/UserPreferencesButton');
-var UserPreferencesModal = require('../../src/components/users/UserPreferencesModal');
-var React = require('react/addons');
-var ReactTestUtils = React.addons.TestUtils;
-var $ = require('jquery');
+const ReactTestUtils = React.addons.TestUtils;
 
 describe('UserPreferencesButton', function () {
     it('should load user data when user clicks edit button', function () {
-        spyOn(PreferencesStore, "loadUserPreferences").and.callFake(function (userName, callback) {
+        spyOn(PreferencesStore, 'loadUserPreferences').and.callFake(function (userName, callback) {
             callback([]);
         });
 
         // we can not mock this, as the plugin is never loaded (we just have jquery)
         $.fn.modal = function () {
-        }
+        };
 
-        var userName = "Full";
-        var modal = ReactTestUtils.renderIntoDocument(
-            <UserPreferencesModal userName={userName} />
-        );
-        spyOn(modal, "openModal").and.callThrough();
-
+        var userName = 'Full';
         var instance = ReactTestUtils.renderIntoDocument(
-            <UserPreferencesButton modal={modal}/>
+            <UserPreferencesButton userName={userName}/>
         );
-        var input = ReactTestUtils.findRenderedDOMComponentWithTag(instance, "button");
+        var input = ReactTestUtils.findRenderedDOMComponentWithTag(instance, 'button');
 
         ReactTestUtils.Simulate.click(input.getDOMNode());
-        expect(modal.openModal).toHaveBeenCalled();
         expect(PreferencesStore.loadUserPreferences).toHaveBeenCalledWith(userName, jasmine.any(Function));
     });
 });
