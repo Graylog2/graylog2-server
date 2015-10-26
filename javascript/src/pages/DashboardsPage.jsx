e9cd6de98e93cc8f340546b95718a56c9829a866
Edmundo Alvarez <edmundo@graylog.com>
2015-10-26 11:16:13 +0100

    Add semicolon

--- javascript/src/pages/DashboardsPage.jsx
+++ javascript/src/pages/DashboardsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Reflux from 'reflux';
 import DashboardListPage from 'components/dashboard/DashboardListPage';
-import CurrentUserStore from 'stores/users/CurrentUserStore'
+import CurrentUserStore from 'stores/users/CurrentUserStore';
 
 const DashboardsPage = React.createClass({
   mixins: [Reflux.connect(CurrentUserStore)],
