d17c27894667652f6692c23d2085da69c4dcd555
Dennis Oelkers <dennis@graylog.com>
2015-10-27 10:31:03 +0100

    Add TimezoneStore, display timezones in TimezoneSelect.

--- javascript/src/components/users/TimezoneSelect.jsx
+++ javascript/src/components/users/TimezoneSelect.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import Reflux from 'reflux';
+
+import TimezonesStore from 'stores/system/TimezonesStore';
 
 import Select from 'components/common/Select';
+import Spinner from 'components/common/Spinner';
 
 const TimezoneSelect = React.createClass({
+  mixins: [Reflux.connect(TimezonesStore)],
   getValue() {
     return this.refs.timezone.getValue();
   },
-  options: [
-    { value: 'Europe', label: 'Europe' },
-    { value: 'America', label: 'America' },
-  ],
+  _formatTimezones(timezones) {
+    /*return Object.keys(timezones).sort().map((continent) => {
+      return (
+        <optgroup key={'timezone-group-' + continent} label={continent}>
+          {timezones[continent].sort().map((timezone) => <option key={'timezone-' + continent + '-' + timezone} value={continent + '/' + timezone}>{timezone}</option>)}
+        </optgroup>
+      );
+    });*/
+    return [].concat.apply([], Object.keys(timezones).sort().map((continent) => {
+      return [{label: continent, disabled: true, value: continent}]
+        .concat(timezones[continent]
+          .sort()
+          .map((timezone) => { return {value: continent + '/' + timezone, label: timezone.replace("_", " ")}; })
+        );
+    }));
+  },
+  _renderOption(option) {
+    if (!option.disabled) {
+      return <span key={option.value}>&nbsp; {option.label}</span>;
+    }
+    return <span key={option.value}>{option.label}</span>;
+  },
   render() {
+    if (!this.state.timezones) {
+      return <Spinner />;
+    }
+
     return (
-      <Select ref="timezone" {...this.props} placeholder="Pick your time zone" options={this.options} />
+      <Select ref="timezone" {...this.props}
+              placeholder="Pick your time zone"
+              options={this._formatTimezones(this.state.timezones)}
+              optionRenderer={this._renderOption}/>
     );
   },
 });
