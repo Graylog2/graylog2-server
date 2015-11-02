3a3e9f50219f6ba569c03f2273fe78a2db53b909
Dennis Oelkers <dennis@graylog.com>
2015-11-02 12:18:51 +0100

    Add components/common package index for easier import of components.

---
+++ javascript/src/components/common/index.jsx
@@ -0,0 +1,28 @@
+import ChosenSelectInput from './ChosenSelectInput';
+import DataFilter from './DataFilter';
+import DataTable from './DataTable';
+import EntityList from './EntityList';
+import EntityListItem from './EntityListItem';
+import MultiSelect from './MultiSelect';
+import PageHeader from './PageHeader';
+import Select from './Select';
+import Spinner from './Spinner';
+import TableList from './TableList';
+import TypeAheadDataFilter from './TypeAheadDataFilter';
+import TypeAheadFieldInput from './TypeAheadFieldInput';
+import TypeAheadInput from './TypeAheadInput';
+
+export {
+  ChosenSelectInput,
+  DataFilter,
+  DataTable,
+  EntityList,
+  EntityListItem,
+  MultiSelect,
+  PageHeader,
+  Spinner,
+  TableList,
+  TypeAheadDataFilter,
+  TypeAheadFieldInput,
+  TypeAheadInput
+};
