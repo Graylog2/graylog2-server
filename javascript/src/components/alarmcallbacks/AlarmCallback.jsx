15c0e746c3ef4ae9ce0197d7b9bf94bad08b5500
Dennis Oelkers <dennis@graylog.com>
2015-11-05 10:56:10 +0100

    Reformat and fix lint errors for AlarmCallback.

--- javascript/src/components/alarmcallbacks/AlarmCallback.jsx
+++ javascript/src/components/alarmcallbacks/AlarmCallback.jsx
@@ -1,106 +1,113 @@
-'use strict';
+import React from 'react';
+import { Row, Col, Alert, Button } from 'react-bootstrap';
 
-var React = require('react');
-var PermissionsMixin = require('../../util/PermissionsMixin');
-var DeleteAlarmCallbackButton = require('./DeleteAlarmCallbackButton');
-var ConfigurationWell = require('../configurationforms/ConfigurationWell');
-var EditAlarmCallbackButton = require('./EditAlarmCallbackButton');
-var Row = require('react-bootstrap').Row;
-var Col = require('react-bootstrap').Col;
-var Alert = require('react-bootstrap').Alert;
-var Button = require('react-bootstrap').Button;
+import PermissionsMixin from '../../util/PermissionsMixin';
+import DeleteAlarmCallbackButton from 'components/alarmcallbacks/DeleteAlarmCallbackButton';
+import ConfigurationWell from 'components/configurationforms/ConfigurationWell';
+import EditAlarmCallbackButton from 'components/alarmcallbacks/EditAlarmCallbackButton';
 
-var AlarmCallback = React.createClass({
-    mixins: [PermissionsMixin],
-    getInitialState() {
-        return {
-            showConfiguration: false
-        };
-    },
-    /* jshint -W116 */
-    _typeNotAvailable() {
-        return (this.props.types[this.props.alarmCallback.type] == undefined);
-    },
-    getDefaultProps() {
-        return {
-            hideButtons: false
-        };
-    },
-    _toggleConfiguration() {
-        this.setState({showConfiguration: !this.state.showConfiguration});
-    },
-    _formatActionButtons() {
-        var alarmCallback = this.props.alarmCallback;
-        var editAlarmCallbackButton = (this.isPermitted(this.props.permissions, ["streams:edit:"+this.props.streamId]) ?
-            <EditAlarmCallbackButton disabled={this._typeNotAvailable()} alarmCallback={alarmCallback} types={this.props.types}
-                                     streamId={this.props.streamId} onUpdate={this.props.updateAlarmCallback} /> : null);
-        var deleteAlarmCallbackButton = (this.isPermitted(this.props.permissions, ["streams:edit:"+this.props.streamId]) ?
-            <DeleteAlarmCallbackButton alarmCallback={alarmCallback} onClick={this.props.deleteAlarmCallback} /> : null);
-        return (
-            <span>
+const AlarmCallback = React.createClass({
+  propTypes: {
+    alarmCallback: React.PropTypes.object.isRequired,
+    concise: React.PropTypes.bool.isRequired,
+    deleteAlarmCallback: React.PropTypes.func.isRequired,
+    permissions: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+    streamId: React.PropTypes.string.isRequired,
+    subtitle: React.PropTypes.string.isRequired,
+    titleAnnotation: React.PropTypes.string.isRequired,
+    types: React.PropTypes.object.isRequired,
+    updateAlarmCallback: React.PropTypes.func.isRequired,
+  },
+  mixins: [PermissionsMixin],
+  getDefaultProps() {
+    return {
+      hideButtons: false,
+    };
+  },
+  getInitialState() {
+    return {
+      showConfiguration: false,
+    };
+  },
+  /* jshint -W116 */
+  _typeNotAvailable() {
+    return (this.props.types[this.props.alarmCallback.type] === undefined);
+  },
+  _toggleConfiguration() {
+    this.setState({showConfiguration: !this.state.showConfiguration});
+  },
+  _formatActionButtons() {
+    const alarmCallback = this.props.alarmCallback;
+    const editAlarmCallbackButton = (this.isPermitted(this.props.permissions, ['streams:edit:' + this.props.streamId]) ?
+      <EditAlarmCallbackButton disabled={this._typeNotAvailable()} alarmCallback={alarmCallback} types={this.props.types}
+                               streamId={this.props.streamId} onUpdate={this.props.updateAlarmCallback} /> : null);
+    const deleteAlarmCallbackButton = (this.isPermitted(this.props.permissions, ['streams:edit:' + this.props.streamId]) ?
+      <DeleteAlarmCallbackButton alarmCallback={alarmCallback} onClick={this.props.deleteAlarmCallback} /> : null);
+    return (
+      <span>
                 {' '}
-                {editAlarmCallbackButton}
-                {' '}
-                {deleteAlarmCallbackButton}
+        {editAlarmCallbackButton}
+        {' '}
+        {deleteAlarmCallbackButton}
             </span>
-        );
-    },
-    _renderToggleConfigurationLink() {
-        if (!this.props.concise) {
-            return;
-        }
-        var toggleConfigurationText = (this.state.showConfiguration ? "Hide configuration" : "Show configuration");
-        return <Button onClick={this._toggleConfiguration} bsSize="xsmall" bsStyle="link">{toggleConfigurationText}</Button>;
-    },
-    _renderConfiguration(alarmCallback) {
-        if (this.props.concise && !this.state.showConfiguration) return;
-
-        var alert = (this._typeNotAvailable() ? <Alert bsStyle="danger">
-            The plugin required for this alarm callback is not loaded. Editing it is not possible. Please load the plugin or delete the alarm callback.
-        </Alert> : null);
-        var configurationWell = (this._typeNotAvailable() ? null :
-            <ConfigurationWell configuration={alarmCallback.configuration}
-                               typeDefinition={this.props.types[alarmCallback.type]} />);
-        return (
-            <Row style={{marginBottom: 0}}>
-                <Col md={12}>
-                    {alert}
-                    {configurationWell}
-                </Col>
-            </Row>
-        );
-    },
-    render() {
-        var alarmCallback = this.props.alarmCallback;
-        var humanReadableType = (this._typeNotAvailable() ? <i>Type not available ({alarmCallback.type})</i> : this.props.types[alarmCallback.type].name);
-        return (
-            <div className="alert-callback" data-destination-id={alarmCallback.id}>
-                <Row style={{marginBottom: 0}}>
-                    <Col md={9}>
-                        <h3>
-                            {' '}
-                            <span>{humanReadableType}</span> <small>{this.props.titleAnnotation}</small>
-                            <span> {this._renderToggleConfigurationLink()} </span>
-                            <small>{this.props.subtitle}</small>
-                        </h3>
-
-                        {this.props.concise ? null : "Executed once per triggered alert condition."}
-                    </Col>
-
-
-                    {!this.props.concise &&
-                    <Col md={3} style={{textAlign: "right"}}>
-                        {this._formatActionButtons()}
-                    </Col>
-                    }
-                </Row>
-
-                {this._renderConfiguration(alarmCallback)}
-
-                <hr />
-            </div>
-        );
+    );
+  },
+  _renderToggleConfigurationLink() {
+    if (!this.props.concise) {
+      return null;
     }
+    const toggleConfigurationText = (this.state.showConfiguration ? 'Hide configuration' : 'Show configuration');
+    return <Button onClick={this._toggleConfiguration} bsSize="xsmall" bsStyle="link">{toggleConfigurationText}</Button>;
+  },
+  _renderConfiguration(alarmCallback) {
+    if (this.props.concise && !this.state.showConfiguration) return null;
+
+    const alert = (this._typeNotAvailable() ? <Alert bsStyle="danger">
+      The plugin required for this alarm callback is not loaded. Editing it is not possible. Please load the plugin or delete the alarm callback.
+    </Alert> : null);
+    const configurationWell = (this._typeNotAvailable() ? null :
+      <ConfigurationWell configuration={alarmCallback.configuration}
+                         typeDefinition={this.props.types[alarmCallback.type]} />);
+    return (
+      <Row style={{marginBottom: 0}}>
+        <Col md={12}>
+          {alert}
+          {configurationWell}
+        </Col>
+      </Row>
+    );
+  },
+  render() {
+    const alarmCallback = this.props.alarmCallback;
+    const humanReadableType = (this._typeNotAvailable() ? <i>Type not available ({alarmCallback.type})</i> : this.props.types[alarmCallback.type].name);
+    return (
+      <div className="alert-callback" data-destination-id={alarmCallback.id}>
+        <Row style={{marginBottom: 0}}>
+          <Col md={9}>
+            <h3>
+              {' '}
+              <span>{humanReadableType}</span> <small>{this.props.titleAnnotation}</small>
+              <span> {this._renderToggleConfigurationLink()} </span>
+              <small>{this.props.subtitle}</small>
+            </h3>
+
+            {this.props.concise ? null : 'Executed once per triggered alert condition.'}
+          </Col>
+
+
+          {!this.props.concise &&
+          <Col md={3} style={{textAlign: 'right'}}>
+            {this._formatActionButtons()}
+          </Col>
+            }
+        </Row>
+
+        {this._renderConfiguration(alarmCallback)}
+
+        <hr />
+      </div>
+    );
+  },
 });
 
-module.exports = AlarmCallback;
+export default AlarmCallback;
