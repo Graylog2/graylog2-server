896e5b256bbe41247c7e653d540e6e3f1eed9a76
Dennis Oelkers <dennis@graylog.com>
2015-11-05 11:44:39 +0100

    Remove unused function from AlarmCallbackList.

--- javascript/src/components/alarmcallbacks/AlarmCallbackList.jsx
+++ javascript/src/components/alarmcallbacks/AlarmCallbackList.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
+import { Alert } from 'react-bootstrap';
 
 import { AlarmCallback } from 'components/alarmcallbacks';
-import { Spinner } from 'components/common';
 
 const AlarmCallbackList = React.createClass({
   propTypes: {
     alarmCallbacks: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-    availableAlarmCallbacks: React.PropTypes.object.isRequired,
     types: React.PropTypes.object.isRequired,
     streamId: React.PropTypes.string.isRequired,
     onUpdate: React.PropTypes.func.isRequired,
     onDelete: React.PropTypes.func.isRequired,
-  },
-  _humanReadableType(alarmCallback) {
-    if (!this.props.availableAlarmCallbacks) {
-      return <Spinner />;
-    }
-
-    const available = this.props.availableAlarmCallbacks[alarmCallback.type];
-
-    if (available) {
-      return available.name;
-    }
-
-    return 'Unknown callback type';
   },
   render() {
     const alarmCallbacks = this.props.alarmCallbacks.map((alarmCallback) => {
@@ -40,9 +26,9 @@ const AlarmCallbackList = React.createClass({
     }
 
     return (
-      <div className="alert alert-info no-alarm-callbacks">
+      <Alert bsStule="info" className="no-alarm-callbacks">
         No configured alarm callbacks.
-      </div>
+      </Alert>
     );
   },
 });
