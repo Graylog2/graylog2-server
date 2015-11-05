b7701c8483b67eb5a728f9ef452ba9b397efba90
Edmundo Alvarez <edmundo@graylog.com>
2015-11-05 18:33:36 +0100

    Add support for fetching a single extractor
    On ExtractorsStore

--- javascript/src/actions/extractors/ExtractorsActions.js
+++ javascript/src/actions/extractors/ExtractorsActions.js
@@ -2,6 +2,7 @@ import Reflux from 'reflux';
 
 const ExtractorsActions = Reflux.createActions({
   list: {asyncResult: true},
+  get: {asyncResult: true},
 });
 
 export default ExtractorsActions;
--- javascript/src/stores/extractors/ExtractorsStore.js
+++ javascript/src/stores/extractors/ExtractorsStore.js
@@ -6,32 +6,23 @@ import ExtractorsActions from 'actions/extractors/ExtractorsActions';
 const ExtractorsStore = Reflux.createStore({
   listenables: [ExtractorsActions],
   sourceUrl: '/system/inputs/',
-  extractors: undefined,
-
-  init() {
-    this._propagateState();
-  },
-
-  getInitialState() {
-    return this.getExtractorsInfo();
-  },
-
-  getExtractorsInfo() {
-    return {extractors: this.extractors};
-  },
-
-  _propagateState() {
-    this.trigger(this.getExtractorsInfo());
-  },
 
   list(inputId) {
     const promise = fetch('GET', URLUtils.qualifyUrl(URLUtils.concatURLPath(this.sourceUrl, inputId, 'extractors')))
       .then(response => {
-        this.extractors = response.extractors;
-        this._propagateState();
+        this.trigger({extractors: response.extractors});
       });
 
     ExtractorsActions.list.promise(promise);
+  },
+
+  get(inputId, extractorId) {
+    const promise = fetch('GET', URLUtils.qualifyUrl(URLUtils.concatURLPath(this.sourceUrl, inputId, 'extractors', extractorId)))
+      .then(response => {
+        this.trigger({extractor: response});
+      });
+
+    ExtractorsActions.get.promise(promise);
   },
 });
 
