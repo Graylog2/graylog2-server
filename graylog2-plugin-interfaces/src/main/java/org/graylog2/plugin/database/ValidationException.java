85f2d17f6bf4efcb3f079f6d6a9b1b196e1caaa3
Jochen Schalanda <jochen@graylog.com>
2015-11-09 15:04:33 +0100

    Set Throwable#detailMessage in ValidationException
    Refs #1543

--- graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/database/ValidationException.java
+++ graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/database/ValidationException.java
@@ -20,28 +20,12 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-/**
- * This file is part of Graylog.
- *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.plugin.database;
 
+import com.google.common.collect.ImmutableMap;
 import org.graylog2.plugin.database.validators.ValidationResult;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -49,7 +33,7 @@ public class ValidationException extends Exception {
     private final Map<String, List<ValidationResult>> errors;
 
     public ValidationException(Map<String, List<ValidationResult>> errors) {
-        this.errors = errors;
+        this.errors = ImmutableMap.copyOf(errors);
     }
 
     public ValidationException(final String message) {
@@ -57,8 +41,8 @@ public class ValidationException extends Exception {
     }
 
     public ValidationException(final String field, final String message) {
-        this.errors = new HashMap<>();
-        this.errors.put(field, Collections.<ValidationResult>singletonList(new ValidationResult.ValidationFailed(message)));
+        super(message);
+        this.errors = ImmutableMap.of(field, Collections.<ValidationResult>singletonList(new ValidationResult.ValidationFailed(message)));
     }
 
     public Map<String, List<ValidationResult>> getErrors() {
