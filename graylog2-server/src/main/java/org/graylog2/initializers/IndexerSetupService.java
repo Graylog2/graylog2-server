efdc1c8c89efe76a650fbaa9fd870126cb391feb
Jochen Schalanda <jochen@graylog.com>
2015-11-10 12:39:28 +0100

    Fix Elasticsearch version check in IndexerSetupService
    (cherry picked from commit 78dd925df3d925c0a7f8ea77697ee8f9d99846b2)

--- graylog2-server/src/main/java/org/graylog2/initializers/IndexerSetupService.java
+++ graylog2-server/src/main/java/org/graylog2/initializers/IndexerSetupService.java
@@ -62,7 +62,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class IndexerSetupService extends AbstractIdleService {
     private static final Logger LOG = LoggerFactory.getLogger(IndexerSetupService.class);
     private static final Version MINIMUM_ES_VERSION = Version.V_1_3_4;
-    private static final Version MAXIMUM_ES_VERSION = Version.fromString("1.6.99");
+    private static final Version MAXIMUM_ES_VERSION = Version.fromString("1.7.99");
 
     private final Node node;
     private final ElasticsearchConfiguration configuration;
@@ -196,7 +196,7 @@ public class IndexerSetupService extends AbstractIdleService {
     }
 
     private void checkClusterVersion(Version clusterVersion) {
-        if (!clusterVersion.onOrAfter(MINIMUM_ES_VERSION) && !clusterVersion.onOrBefore(MAXIMUM_ES_VERSION)) {
+        if (!clusterVersion.onOrAfter(MINIMUM_ES_VERSION) || !clusterVersion.onOrBefore(MAXIMUM_ES_VERSION)) {
             LOG.error("Elasticsearch node is of the wrong version {}, it must be between {} and {}! "
                             + "Please make sure you are running the correct version of Elasticsearch.",
                     clusterVersion, MINIMUM_ES_VERSION, MAXIMUM_ES_VERSION);
