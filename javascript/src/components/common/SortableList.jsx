263843fe5f43f926ce8f8e1d79d95d79de724bd6
Edmundo Alvarez <edmundo@graylog.com>
2015-11-16 21:05:23 +0100

    Add sort extractors functionality
    - Add modal displaying a list of extractors to be sorted
    - Add components to display a sortable list (using react-dnd underneath)
    - When pressing "save" the list is stored in the server

---
+++ javascript/src/components/common/SortableList.jsx
@@ -0,0 +1,45 @@
+import React, {PropTypes} from 'react';
+import {ListGroup} from 'react-bootstrap';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import SortableListItem from './SortableListItem';
+
+const SortableList = React.createClass({
+  propTypes: {
+    items: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onMoveItem: PropTypes.func,
+  },
+  getInitialState() {
+    return {
+      items: this.props.items,
+    };
+  },
+  _moveItem(dragIndex, hoverIndex) {
+    const sortedItems = this.state.items;
+    const tempItem = sortedItems[dragIndex];
+    sortedItems[dragIndex] = sortedItems[hoverIndex];
+    sortedItems[hoverIndex] = tempItem;
+    this.setState({items: sortedItems});
+    if (typeof this.props.onMoveItem === 'function') {
+      this.props.onMoveItem(sortedItems);
+    }
+  },
+  render() {
+    const formattedItems = this.state.items.map((item, idx) => {
+      return (
+        <SortableListItem key={`sortable-list-item-${item.id}`} index={idx} id={item.id} text={item.title}
+                          moveItem={this._moveItem}/>
+      );
+    });
+
+    return (
+      <ListGroup className="sortable-list">
+        {formattedItems}
+      </ListGroup>
+    );
+  },
+});
+
+
+export default DragDropContext(HTML5Backend)(SortableList);
--- javascript/src/components/extractors/ExtractorsList.jsx
+++ javascript/src/components/extractors/ExtractorsList.jsx
@@ -1,12 +1,13 @@
 import React, {PropTypes} from 'react';
 import Reflux from 'reflux';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Button} from 'react-bootstrap';
 import naturalSort from 'javascript-natural-sort';
 
 import Spinner from 'components/common/Spinner';
 import AddExtractorWizard from 'components/extractors/AddExtractorWizard';
 import EntityList from 'components/common/EntityList';
 import ExtractorsListItem from './ExtractorsListItem';
+import ExtractorsSortModal from './ExtractorSortModal';
 
 import ExtractorsActions from 'actions/extractors/ExtractorsActions';
 import ExtractorsStore from 'stores/extractors/ExtractorsStore';
@@ -21,14 +22,29 @@ const ExtractorsList = React.createClass({
     ExtractorsActions.list.triggerPromise(this.props.input.input_id);
   },
   _formatExtractor(extractor) {
-    return <ExtractorsListItem key={extractor.id} extractor={extractor} inputId={this.props.input.input_id} nodeId={this.props.node.node_id}/>;
+    return (
+      <ExtractorsListItem key={extractor.id} extractor={extractor} inputId={this.props.input.input_id}
+                          nodeId={this.props.node.node_id}/>
+    );
   },
   _isLoading() {
     return !this.state.extractors;
   },
+  _openSortModal() {
+    this.refs.sortModal.open();
+  },
   render() {
     if (this._isLoading()) {
       return <Spinner/>;
+    }
+
+    let sortExtractorsButton;
+    if (this.state.extractors.length > 1) {
+      sortExtractorsButton = (
+        <Button bsSize="xsmall" bsStyle="primary" className="pull-right" onClick={this._openSortModal}>
+          Sort extractors
+        </Button>
+      );
     }
 
     const formattedExtractors = this.state.extractors
@@ -40,11 +56,19 @@ const ExtractorsList = React.createClass({
         <AddExtractorWizard inputId={this.props.input.input_id}/>
         <Row className="content extractor-list">
           <Col md={12}>
-            <h2>Configured extractors</h2>
+            <Row className="row-sm">
+              <Col md={8}>
+                <h2>Configured extractors</h2>
+              </Col>
+              <Col md={4}>
+                {sortExtractorsButton}
+              </Col>
+            </Row>
             <EntityList bsNoItemsStyle="info" noItemsText="This input has no configured extractors."
                         items={formattedExtractors}/>
           </Col>
         </Row>
+        <ExtractorsSortModal ref="sortModal" input={this.props.input} extractors={this.state.extractors}/>
       </div>
     );
   },
