8676f9f97f4310551fa5cd5796f9d2e725c3f6f1
Edmundo Alvarez <edmundo@graylog.com>
2015-11-17 14:30:12 +0100

    Add method to Version.ts to get full version

--- javascript/src/util/Version.ts
+++ javascript/src/util/Version.ts
@@ -4,17 +4,22 @@
 var pjson = require('../../package.json');
 
 class Version {
+    full: string
     major: string;
     minor: string;
     constructor() {
-        var fullVersion = pjson.version;
-        var splitVersion = fullVersion.split(".");
+        this.full = pjson.version;
+        var splitVersion = this.full.split(".");
         this.major = splitVersion[0];
         this.minor = splitVersion[1];
     }
 
     getMajorAndMinorVersion() {
         return this.major + "." + this.minor;
+    }
+
+    getFullVersion() {
+        return this.full;
     }
 }
 
