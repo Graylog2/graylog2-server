ccad50d54f3b88fb0cf31a88b00aae845ac76c4b
Dennis Oelkers <dennis@graylog.com>
2015-11-20 14:48:59 +0100

    Reformat and correct FIeldHelpers component.

--- javascript/src/components/configurationforms/FieldHelpers.jsx
+++ javascript/src/components/configurationforms/FieldHelpers.jsx
@@ -1,14 +1,12 @@
-'use strict';
+import React from 'react';
 
-var React = require('react');
-
-var FieldHelpers = {
-    hasAttribute: (ary, attribute) => {
-        return ary.indexOf(attribute) > -1;
-    },
-    optionalMarker: (field) => {
-        return field.is_optional ? <span className="configuration-field-optional">(optional)</span> : "";
-    }
+const FieldHelpers = {
+  hasAttribute: (ary, attribute) => {
+    return ary.indexOf(attribute) > -1;
+  },
+  optionalMarker: (field) => {
+    return field.is_optional ? <span className="configuration-field-optional">(optional)</span> : null;
+  },
 };
 
-module.exports = FieldHelpers;
+export default FieldHelpers;
