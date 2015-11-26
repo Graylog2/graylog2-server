7051974c1a7cd459f99d111f973534bca846144b
Dennis Oelkers <dennis@graylog.com>
2015-11-26 12:43:47 +0100

    Reenable global throughput in navbar.

--- javascript/src/components/throughput/GlobalThroughput.jsx
+++ javascript/src/components/throughput/GlobalThroughput.jsx
@@ -1,75 +1,30 @@
 import React from 'react';
-//noinspection JSUnusedGlobalSymbols
-import MetricsStore from 'stores/metrics/MetricsStore';
+import Reflux from 'reflux';
 import numeral from 'numeral';
 
-var GlobalThroughput = React.createClass({
-  getInitialState() {
-    return {
-      nodeCount: 0,
-      totalIn: 0,
-      totalOut: 0,
-      hasError: false
-    };
-  },
+import GlobalThroughputStore from 'stores/metrics/GlobalThroughputStore';
+
+import MetricsActions from 'actions/metrics/MetricsActions';
+
+import { Spinner } from 'components/common';
+
+const GlobalThroughput = React.createClass({
+  mixins: [Reflux.connect(GlobalThroughputStore)],
   componentDidMount() {
-    const metricsStore = MetricsStore.instance;
-    if (metricsStore) {
-      metricsStore.listen({
-        nodeId: null, // across all nodes
-        metricNames: ['org.graylog2.throughput.input.1-sec-rate', 'org.graylog2.throughput.output.1-sec-rate'],
-        callback: (update, hasError) => {
-          if (hasError) {
-            this.setState({hasError: hasError});
-            return;
-          }
-          // update is [{nodeId, values: [{name, value: {metric}}]} ...]
-          // metric can be various different things, depending on metric {type: 'GAUGE'|'COUNTER'|'METER'|'TIMER'}
-          const nodeCount = update.length;
-
-          let throughIn = 0;
-          let throughOut = 0;
-          // not using filter.map.reduce because that's even worse to read than this code...
-          update.forEach((perNode) => {
-            perNode.values.forEach((namedMetric) => {
-              if (namedMetric.name === 'org.graylog2.throughput.input.1-sec-rate') {
-                throughIn += namedMetric.metric.value;
-              } else if (namedMetric.name === 'org.graylog2.throughput.output.1-sec-rate') {
-                throughOut += namedMetric.metric.value;
-              }
-            });
-          });
-          this.setState({nodeCount: nodeCount, totalIn: throughIn, totalOut: throughOut, hasError: hasError});
-        }
-      });
-    } else {
-      this.setState({hasError: 'MetricsStore instance is unavailable!'});
-    }
+    setInterval(MetricsActions.list, 2000);
   },
-
+  metrics: ['org.graylog2.throughput.input.1-sec-rate', 'org.graylog2.throughput.output.1-sec-rate'],
   render() {
-    if (this.state.hasError) {
-      return (
-        <span>
-          <strong className="total-throughput">Throughput unavailable</strong>
-        </span>
-      );
-    }
-
-    if (this.state.nodeCount === 0) {
-      return (
-        <span>
-          <strong className="total-throughput"><i className="fa fa-spin fa-spinner"></i> Loading throughput...</strong>
-        </span>
-      );
+    if (!this.state.throughput) {
+      return <Spinner />;
     }
     return (
       <span>
-        In <strong className="total-throughput">{numeral(this.state.totalIn).format('0,0')}</strong> / Out <strong className="total-throughput">{numeral(this.state.totalOut).format('0,0')}</strong> msg/s
+        In <strong className="total-throughput">{numeral(this.state.throughput.input).format('0,0')}</strong>{' '}
+        / Out <strong className="total-throughput">{numeral(this.state.throughput.output).format('0,0')}</strong> msg/s
       </span>
-
     );
-  }
+  },
 });
 
 module.exports = GlobalThroughput;
