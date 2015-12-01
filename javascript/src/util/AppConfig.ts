4fbd50b02e782a1f832d707c06f9666a7be4276f
Dennis Oelkers <dennis@graylog.com>
2015-12-01 17:41:09 +0100

    Remove unused accessors of unused config fields.

--- javascript/src/util/AppConfig.ts
+++ javascript/src/util/AppConfig.ts
@@ -12,22 +12,6 @@ class AppConfig {
   gl2AppPathPrefix() : string {
     return this.appConfig['gl2AppPathPrefix'];
   }
-
-  gl2UserTimeZone() : string {
-    return this.appConfig['gl2UserTimeZone'];
-  }
-
-  gl2UserTimeZoneOffset() : number {
-    return this.appConfig['gl2UserTimeZoneOffset'];
-  }
-
-  gl2UserSessionId() : string {
-    return this.appConfig['gl2UserSessionId'];
-  }
-
-  sockJsWebSocketsEnabled() : boolean {
-    return this.appConfig['sockJsWebSocketsEnabled'];
-  }
 }
 
 export = AppConfig;
