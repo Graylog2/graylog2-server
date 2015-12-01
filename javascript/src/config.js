9488117105a295109410cd34b6201460be9ea93a
Dennis Oelkers <dennis@graylog.com>
2015-12-01 17:40:49 +0100

    Remove unused config fields.

--- javascript/src/config.js
+++ javascript/src/config.js
@@ -1,10 +1,6 @@
 const appConfig = {
   gl2ServerUrl: 'http://localhost:12900',
   gl2AppPathPrefix: '',
-  gl2UserTimeZone: 'UTC',
-  gl2UserTimeZoneOffset: 0,
-  gl2UserSessionId: 'd7ead1b8cafbe9204e75a0b628b42b5af1ae14f00a93e713bef414a4dd71a95506dca05db88e99b74b6fc2d040ef0a4c',
-  sockJsWebSocketsEnabled: false,
 };
 
 window.appConfig = appConfig;
