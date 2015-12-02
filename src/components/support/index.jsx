9d12829d3ab02fda1592a71fd3f8f46ad68d9961
Dennis Oelkers <dennis@graylog.com>
2015-12-02 16:54:25 +0100

    Fix support package index.

--- src/components/support/index.jsx
+++ src/components/support/index.jsx
@@ -1,3 +1,3 @@
-export DocumentationLink from './DocumentationLink';
-export SmallSupportLink from './SmallSupportLink';
-export SupportLink from './SupportLink';
+export { default as DocumentationLink } from './DocumentationLink';
+export { default as SmallSupportLink } from './SmallSupportLink';
+export { default as SupportLink } from './SupportLink';
