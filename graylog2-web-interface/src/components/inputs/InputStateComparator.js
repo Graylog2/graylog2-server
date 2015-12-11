a4b8d38295e54e2cd9cf91965060f75e763e8f20
Dennis Oelkers <dennis@graylog.com>
2015-12-11 13:16:23 +0100

    Fix syntax for comparator class.

--- graylog2-web-interface/src/components/inputs/InputStateComparator.js
+++ graylog2-web-interface/src/components/inputs/InputStateComparator.js
@@ -1,15 +1,17 @@
 class InputStateComparator {
-  mapping = {
-    'CREATED': 0,
-    'INITIALIZED': 1,
-    'INVALID_CONFIGURATION': 2,
-    'STARTING': 3,
-    'RUNNING' : 4,
-    'FAILED' : 2,
-    'STOPPING' : 1,
-    'STOPPED' : 0,
-    'TERMINATED' : 0,
-  };
+  constructor() {
+    this.mapping = {
+      'CREATED': 0,
+      'INITIALIZED': 1,
+      'INVALID_CONFIGURATION': 2,
+      'STARTING': 3,
+      'RUNNING': 4,
+      'FAILED': 2,
+      'STOPPING': 1,
+      'STOPPED': 0,
+      'TERMINATED': 0,
+    };
+  }
 
   compare(state1, state2) {
     return this.mapping(state1) - this.mapping(state2);
