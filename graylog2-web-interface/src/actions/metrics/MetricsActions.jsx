60aeac76c1018e7e4fdc6924a8c876f8e5510e23
Dennis Oelkers <dennis@graylog.com>
2015-12-17 15:23:03 +0100

    Sort actions alphabetically and add clear action.

--- graylog2-web-interface/src/actions/metrics/MetricsActions.jsx
+++ graylog2-web-interface/src/actions/metrics/MetricsActions.jsx
@@ -1,13 +1,14 @@
 import Reflux from 'reflux';
 
 const MetricsActions = Reflux.createActions({
-  'list': { asyncResult: true },
-  'names': { asyncResult: true },
   'add': { asyncResult: true },
   'addGlobal': { asyncResult: true },
+  'clear': { asyncResult: true },
+  'filter': { asyncResult: true },
+  'list': { asyncResult: true },
+  'names': { asyncResult: true },
   'remove': { asyncResult: true },
   'removeGlobal': { asyncResult: true },
-  'filter': { asyncResult: true },
 });
 
 export default MetricsActions;
