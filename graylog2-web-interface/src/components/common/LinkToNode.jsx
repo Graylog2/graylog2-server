f18e84ade9bdb0dcec860b1b06636419e8976c7c
Edmundo Alvarez <edmundo@graylog.com>
2016-01-13 11:47:27 +0100

    Show tooltip on icon to master node in cluster

--- graylog2-web-interface/src/components/common/LinkToNode.jsx
+++ graylog2-web-interface/src/components/common/LinkToNode.jsx
@@ -20,9 +20,10 @@ const LinkToNode = React.createClass({
 
     if (node) {
       const iconClass = node.is_master ? 'fa fa-star master-node' : 'fa fa-code-fork';
+      const iconTitle = node.is_master ? 'This is the master node in the cluster' : '';
       return (
         <Link to={Routes.SYSTEM.NODES.SHOW(this.props.nodeId)}>
-          <i className={iconClass} />
+          <i className={iconClass} title={iconTitle} />
           {' '}
           {node.short_node_id} / {node.hostname}
         </Link>
