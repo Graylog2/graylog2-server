ac64d797682b84f6eda68111245f94c58ed52f05
Edmundo Alvarez <edmundo@graylog.com>
2016-01-20 15:41:37 +0100

    Shim promises if browser doesn't support them
    Reflux requires a global `Promise` object in order to return a promise
    when calling to `triggerPromise`. As some browsers still haven't got
    native promise support, we shim them to bluebird, which is the promise
    library we use by default.

--- graylog2-web-interface/src/index.jsx
+++ graylog2-web-interface/src/index.jsx
@@ -1,3 +1,7 @@
+// This needs to be imported before Reflux (or components importing it), in order to polyfill
+// the Promise object if it is not available natively.
+import {} from 'util/PromiseShim';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppFacade from 'routing/AppFacade';
