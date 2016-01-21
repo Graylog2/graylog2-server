77bbb8b09f78c7781e1e0b41d0e005ea7844c086
Edmundo Alvarez <edmundo@graylog.com>
2016-01-21 18:06:42 +0100

    Rewrite NumberUtils in JS

---
+++ graylog2-web-interface/src/util/NumberUtils.js
@@ -0,0 +1,45 @@
+import numeral from 'numeral';
+
+const NumberUtils = {
+  normalizeNumber(number) {
+    switch (number) {
+      case 'NaN':
+        return NaN;
+      case 'Infinity':
+        return Number.MAX_VALUE;
+      case '-Infinity':
+        return Number.MIN_VALUE;
+      default:
+        return number;
+    }
+  },
+  normalizeGraphNumber(number) {
+    switch (number) {
+      case 'NaN':
+      case 'Infinity':
+      case '-Infinity':
+        return 0;
+      default:
+        return number;
+    }
+  },
+  formatNumber(number) {
+    try {
+      return numeral(this.normalizeNumber(number)).format('0,0.[00]');
+    } catch (e) {
+      return number;
+    }
+  },
+  formatPercentage(percentage) {
+    try {
+      return numeral(this.normalizeNumber(percentage)).format('0.00%');
+    } catch (e) {
+      return percentage;
+    }
+  },
+  isNumber(possibleNumber) {
+    return possibleNumber !== '' && !isNaN(possibleNumber);
+  },
+};
+
+export default NumberUtils;
