209a344b72c435ee31080ef57d8f0c44f2795dcb
Edmundo Alvarez <edmundo@graylog.com>
2016-01-21 17:58:33 +0100

    Rewrite DocsHelper in JS

--- graylog2-web-interface/src/util/DocsHelper.js
+++ graylog2-web-interface/src/util/DocsHelper.js
@@ -1,13 +1,7 @@
-'use strict';
-
-import Version = require('./Version');
-
-interface Pages {
-  [page: string]: string;
-}
+import Version from './Version';
 
 class DocsHelper {
-  PAGES: Pages = {
+  PAGES = {
     ALERTS: 'streams.html#alerts',
     CLUSTER_STATUS_EXPLAINED: 'configuring_es.html#cluster-status-explained',
     COLLECTOR: 'collector.html',
@@ -32,22 +26,22 @@ class DocsHelper {
     USERS_ROLES: 'users_and_roles.html',
     WELCOME: '', // Welcome page to the documentation
   };
-  DOCS_URL: string = 'http://docs.graylog.org/en/';
+  DOCS_URL = 'http://docs.graylog.org/en/';
 
-  toString(path: string): string {
-    var baseUrl = this.DOCS_URL + Version.getMajorAndMinorVersion();
-    return path === '' ? baseUrl : baseUrl + '/pages/' + path;
+  toString(path) {
+    const baseUrl = this.DOCS_URL + Version.getMajorAndMinorVersion();
+    return path === '' ? baseUrl : `${baseUrl}/pages/${path}`;
   }
 
-  toLink(path: string, title: string): string {
-    return '<a href=\'' + this.toString(path) + '\' target=\'_blank\'>' + title + '</a>';
+  toLink(path, title) {
+    return `<a href="${this.toString(path)}" target="_blank">${title}</a>`;
   }
 
-  versionedDocsHomePage(): string {
+  versionedDocsHomePage() {
     return this.toString('');
   }
 }
 
-var docsHelper = new DocsHelper();
+const docsHelper = new DocsHelper();
 
-export = docsHelper;
+export default docsHelper;
