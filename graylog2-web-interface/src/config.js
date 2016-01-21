774ec3109992874f3dda30511b9fcf6b29f54b57
Dennis Oelkers <dennis@graylog.com>
2016-01-21 13:58:59 +0100

    Assign appConfig directly to window key to avoid var keyword.

--- graylog2-web-interface/src/config.js
+++ graylog2-web-interface/src/config.js
@@ -1,9 +1,5 @@
-// noinspection Eslint
-var appConfig = {
+window.appConfig = {
   gl2ServerUrl: 'http://localhost:12900',
   gl2AppPathPrefix: '',
   rootTimeZone: 'Europe/Berlin',
 };
-
-window.appConfig = appConfig;
-
