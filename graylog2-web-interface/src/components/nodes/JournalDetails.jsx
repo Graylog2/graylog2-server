10e861871bf86d73915322f80b745bcb2ce6ac1e
Edmundo Alvarez <edmundo@graylog.com>
2016-01-22 19:16:59 +0100

    Add journal details back to node overview page

--- graylog2-web-interface/src/components/nodes/JournalDetails.jsx
+++ graylog2-web-interface/src/components/nodes/JournalDetails.jsx
@@ -1,151 +1,129 @@
 import React, {PropTypes} from 'react';
+import Reflux from 'reflux';
 import { ProgressBar, Row, Col } from 'react-bootstrap';
 import numeral from 'numeral';
 import moment from 'moment';
 import {} from 'moment-duration-format';
 
-import MetricsStore from 'stores/metrics/MetricsStore';
-import { Timestamp } from 'components/common';
-import jsRoutes from 'routing/jsRoutes';
+import MetricsExtractor from 'logic/metrics/MetricsExtractor';
+import MetricsActions from 'actions/metrics/MetricsActions';
 
-const metricsStore = MetricsStore.instance;
+import MetricsStore from 'stores/metrics/MetricsStore';
+import JournalStore from 'stores/journal/JournalStore';
+
+import { Spinner, Timestamp } from 'components/common';
+
+import NumberUtils from 'util/NumberUtils';
+import jsRoutes from 'routing/jsRoutes';
 
 const JournalDetails = React.createClass({
   propTypes: {
     nodeId: PropTypes.string.isRequired,
-    enabled: PropTypes.boolean,
-    directory: PropTypes.string.isRequired,
-    maxSize: PropTypes.number.isRequired,
-    maxAge: PropTypes.string.isRequired,
-    flushInterval: PropTypes.number.isRequired,
-    flushAge: PropTypes.string.isRequired,
   },
+  mixins: [Reflux.connect(MetricsStore)],
+
   getInitialState() {
-    return ({
-      initialized: false,
-      hasError: false,
-      append: 0,
-      read: 0,
-      segments: 0,
-      entriesUncommitted: 0,
-      size: 0,
-      sizeLimit: 0,
-      utilizationRatio: 0,
-      oldestSegment: moment(),
+    return {
+      journalInformation: undefined,
+    };
+  },
+
+  componentDidMount() {
+    JournalStore.get(this.props.nodeId).then(journalInformation => {
+      this.setState({journalInformation: journalInformation}, this._listenToMetrics);
     });
   },
 
-  componentWillMount() {
-    // only listen for updates if the journal is actually turned on
-    if (this.props.enabled) {
-      const metricNames = [
-        'org.graylog2.journal.append.1-sec-rate',
-        'org.graylog2.journal.read.1-sec-rate',
-        'org.graylog2.journal.segments',
-        'org.graylog2.journal.entries-uncommitted',
-        'org.graylog2.journal.size',
-        'org.graylog2.journal.size-limit',
-        'org.graylog2.journal.utilization-ratio',
-        'org.graylog2.journal.oldest-segment',
-      ];
-      metricsStore.listen({
-        nodeId: this.props.nodeId,
-        metricNames: metricNames,
-        callback: (update, hasError) => {
-          if (hasError) {
-            this.setState({hasError: hasError});
-            return;
-          }
-          // update is [{nodeId, values: [{name, value: {metric}}]} ...]
-          // metric can be various different things, depending on metric {type: "GAUGE"|"COUNTER"|"METER"|"TIMER"}
-
-          const newState = {
-            initialized: true,
-            hasError: hasError,
-          };
-          // we will only get one result, because we ask for only one node
-          // get the base name from the metric name, and put the gauge metrics into our new state.
-          update[0].values.forEach((namedMetric) => {
-            const baseName = namedMetric.name.replace('org.graylog2.journal.', '').replace('.1-sec-rate', '');
-            const camelCase = this.camelCase(baseName);
-            newState[camelCase] = namedMetric.metric.value;
-          });
-          this.setState(newState);
-        },
-      });
+  componentWillUnmount() {
+    if (this.metricNames) {
+      Object.keys(this.metricNames).forEach(metricShortName => MetricsActions.remove(this.props.nodeId, this.metricNames[metricShortName]));
     }
   },
 
-  camelCase(input) {
-    return input.toLowerCase().replace(/-(.)/g, (match, group1) => group1.toUpperCase());
+  _listenToMetrics() {
+    // only listen for updates if the journal is actually turned on
+    if (this.state.journalInformation.enabled) {
+      this.metricNames = {
+        append: 'org.graylog2.journal.append.1-sec-rate',
+        read: 'org.graylog2.journal.read.1-sec-rate',
+        segments: 'org.graylog2.journal.segments',
+        entriesUncommitted: 'org.graylog2.journal.entries-uncommitted',
+        utilizationRatio: 'org.graylog2.journal.utilization-ratio',
+        oldestSegment: 'org.graylog2.journal.oldest-segment',
+      };
+      Object.keys(this.metricNames).forEach(metricShortName => MetricsActions.add(this.props.nodeId, this.metricNames[metricShortName]));
+    }
+  },
+
+  _isLoading() {
+    return !(this.state.metrics && this.state.journalInformation);
   },
 
   render() {
-    let content = null;
-    if (this.props.enabled) {
-      const oldestSegment = moment(this.state.oldestSegment);
-      let overcommittedWarning = null;
-      if (this.state.initialized && (this.state.utilizationRatio >= 1)) {
-        overcommittedWarning = (
-          <span>
-            <strong>Warning!</strong> The journal utilization is exceeding the maximum size defined.
-            <a href={jsRoutes.controllers.SystemController.index().url}> Click here</a> for more information.<br/>
-          </span>
-        );
-      }
-
-      content = (
-        <div>
-          <p className="description">Incoming messages are written to the disk journal to ensure they are kept safe in
-            case of a server failure. The journal also helps keeping Graylog working if any of the outputs is too slow
-            to keep up with the message rate
-            or whenever there is a peak in incoming messages. It makes sure that Graylog does not buffer all of those
-            messages in main memory and avoids overly long garbage collection pauses that way.
-          </p>
-          <Row className="row-sm">
-            <Col md={6}>
-              <h3>Configuration</h3>
-              <dl className="system-journal">
-                <dt>Path:</dt>
-                <dd>{this.props.directory}</dd>
-                <dt>Earliest entry:</dt>
-                <dd><Timestamp dateTime={oldestSegment} relative/></dd>
-                <dt>Maximum size:</dt>
-                <dd>{numeral(this.props.maxSize).format('0,0 b')}</dd>
-                <dt>Maximum age:</dt>
-                <dd>{moment.duration(this.props.maxAge).format('d [days] h [hours] m [minutes]')}</dd>
-                <dt>Flush policy:</dt>
-                <dd>
-                  Every {numeral(this.props.flushInterval).format('0,0')} messages
-                  {' '}or {moment.duration(this.props.flushAge).format('h [hours] m [minutes] s [seconds]')}
-                </dd>
-              </dl>
-            </Col>
-            <Col md={6}>
-              <h3>Utilization</h3>
-
-              <ProgressBar now={this.state.utilizationRatio * 100}
-                           label={numeral(this.state.utilizationRatio).format('0.0 %')}/>
-
-              {overcommittedWarning}
-
-              <strong>{numeral(this.state.entriesUncommitted).format('0,0')} unprocessed messages</strong>
-              {' '}are currently in the journal, in {this.state.segments} segments.<br/>
-              <strong>{numeral(this.state.append).format('0,0')} messages</strong>
-              {' '}have been appended in the last second,{' '}
-              <strong>{numeral(this.state.read).format('0,0')} messages</strong> have been read in the last second.
-            </Col>
-          </Row>
-        </div>
-      );
-    } else {
-      content = (<span>The disk journal is disabled for this node.</span>);
+    if (this._isLoading()) {
+      return <Spinner text="Loading journal metrics..."/>;
     }
 
-    return (<div>
-      <h2>Disk Journal</h2>
-      {content}
-    </div>);
+    const nodeId = this.props.nodeId;
+    const nodeMetrics = this.state.metrics[nodeId];
+    const journalInformation = this.state.journalInformation;
+    const metrics = MetricsExtractor.getValuesForNode(nodeMetrics, this.metricNames);
+
+    if (Object.keys(metrics).length === 0) {
+      return <span>Journal metrics unavailable.</span>;
+    }
+
+    if (!journalInformation.enabled) {
+      return <span>The disk journal is disabled for this node.</span>;
+    }
+
+    const oldestSegment = moment(metrics.oldestSegment);
+    let overcommittedWarning;
+    if (metrics.utilizationRatio >= 1) {
+      overcommittedWarning = (
+        <span>
+          <strong>Warning!</strong> The journal utilization is exceeding the maximum size defined.
+          <a href={jsRoutes.controllers.SystemController.index().url}> Click here</a> for more information.<br/>
+        </span>
+      );
+    }
+
+    return (
+      <Row className="row-sm">
+        <Col md={6}>
+          <h3>Configuration</h3>
+          <dl className="system-journal">
+            <dt>Path:</dt>
+            <dd>{journalInformation.journal_config.directory}</dd>
+            <dt>Earliest entry:</dt>
+            <dd><Timestamp dateTime={oldestSegment} relative/></dd>
+            <dt>Maximum size:</dt>
+            <dd>{numeral(journalInformation.journal_config.max_size).format('0,0 b')}</dd>
+            <dt>Maximum age:</dt>
+            <dd>{moment.duration(journalInformation.journal_config.max_age).format('d [days] h [hours] m [minutes]')}</dd>
+            <dt>Flush policy:</dt>
+            <dd>
+              Every {numeral(journalInformation.journal_config.flush_interval).format('0,0')} messages
+              {' '}or {moment.duration(journalInformation.journal_config.flush_age).format('h [hours] m [minutes] s [seconds]')}
+            </dd>
+          </dl>
+        </Col>
+        <Col md={6} className="journal-details-usage">
+          <h3>Utilization</h3>
+
+          <ProgressBar now={metrics.utilizationRatio * 100}
+                       label={NumberUtils.formatPercentage(metrics.utilizationRatio)}/>
+
+          {overcommittedWarning}
+
+          <strong>{numeral(metrics.entriesUncommitted).format('0,0')} unprocessed messages</strong>
+          {' '}are currently in the journal, in {metrics.segments} segments.<br/>
+          <strong>{numeral(metrics.append).format('0,0')} messages</strong>
+          {' '}have been appended in the last second,{' '}
+          <strong>{numeral(metrics.read).format('0,0')} messages</strong> have been read in the last second.
+        </Col>
+      </Row>
+    );
   },
 });
 
