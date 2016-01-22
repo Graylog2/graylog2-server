814f5a82974f819ae506278176c0fe82bb7a3193
Edmundo Alvarez <edmundo@graylog.com>
2016-01-22 15:30:48 +0100

    Add buffer usage information to node overview

--- graylog2-web-interface/src/components/nodes/NodeOverview.jsx
+++ graylog2-web-interface/src/components/nodes/NodeOverview.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import { Row, Col } from 'react-bootstrap';
 
+import BufferUsage from './BufferUsage';
 import SystemOverviewDetails from './SystemOverviewDetails';
 import JvmHeapUsage from './JvmHeapUsage';
 
@@ -25,6 +26,27 @@ const NodeOverview = React.createClass({
           <Col md={12}>
             <h2 style={{marginBottom: 5}}>Memory/Heap usage</h2>
             <JvmHeapUsage nodeId={node.node_id}/>
+          </Col>
+        </Row>
+
+        <Row className="content">
+          <Col md={12}>
+            <h2>Buffers</h2>
+            <p className="description">
+              Buffers are built to cache small amounts of messages for a very short time
+              (usually milliseconds) on their way through the different processors.
+            </p>
+            <Row>
+              <Col md={4}>
+                <BufferUsage nodeId={node.node_id} title="Input buffer" bufferType="input"/>
+              </Col>
+              <Col md={4}>
+                <BufferUsage nodeId={node.node_id} title="Process buffer" bufferType="process"/>
+              </Col>
+              <Col md={4}>
+                <BufferUsage nodeId={node.node_id} title="Output buffer" bufferType="output"/>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </div>
