18eb23b9570a80377cc288f12a176bc64ed43f0a
Dennis Oelkers <dennis@graylog.com>
2016-01-26 10:39:31 +0100

    Fix karma runs for separate vendor/shared bundles.

--- graylog2-web-interface/karma.dev.conf.js
+++ graylog2-web-interface/karma.dev.conf.js
@@ -1,41 +1,13 @@
-var webpackSharedConfig = require("./webpack.config");
+var karmaSharedConfig = require('./karma.shared');
+var merge = require('webpack-merge');
 
 webpackSharedConfig.externals = [];
 
 module.exports = function (config) {
+  karmaSharedConfig(config);
   config.set({
-
-    basePath: '',
-
-    frameworks: ['jasmine'],
-
-    files: [
-      'test/shim/server-side-global-vars.js',
-      'test/src/*.js',
-      'test/src/**/*.js',
-    ],
-
-    preprocessors: {
-      'test/src/*.js': ['webpack'],
-      'test/src/**/*.js': ['webpack'],
-    },
-
-    reporters: ['progress'],
-
-    port: 9876,
-
-    colors: true,
-
-    logLevel: config.LOG_INFO,
-
     autoWatch: true,
-
     browsers: ['Chrome'],
-
-    captureTimeout: 60000,
-
     singleRun: false,
-
-    webpack: webpackSharedConfig
   });
 };
