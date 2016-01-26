43bc971f1028403af9145139fd418f877e051861
Kay Roepke <kroepke@googlemail.com>
2016-01-26 12:03:41 +0100

    check for presence, not absence

--- src/main/java/org/graylog/plugins/pipelineprocessor/functions/SetField.java
+++ src/main/java/org/graylog/plugins/pipelineprocessor/functions/SetField.java
@@ -38,7 +38,7 @@ public class SetField implements Function<Void> {
         final Optional<Object> field = args.evaluated(FIELD, context, Object.class);
         final Optional<Object> value = args.evaluated(VALUE, context, Object.class);
 
-        if (!field.isPresent() && !field.get().toString().isEmpty()) {
+        if (field.isPresent() && !field.get().toString().isEmpty()) {
             context.currentMessage().addField(field.get().toString(), value.get());
         }
         return null;
