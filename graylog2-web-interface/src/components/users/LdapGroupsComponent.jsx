525abc92fb36da458dc98a39d809e62cd81339e9
Edmundo Alvarez <edmundo@graylog.com>
2016-01-30 19:41:44 +0100

    Reformat LdapGroupsComponent

--- graylog2-web-interface/src/components/users/LdapGroupsComponent.jsx
+++ graylog2-web-interface/src/components/users/LdapGroupsComponent.jsx
@@ -1,21 +1,19 @@
-const React = require('react');
-const Immutable = require('immutable');
+import React from 'react';
+import Immutable from 'immutable';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Row, Col, Input, Panel, Button } from 'react-bootstrap';
+import naturalSort from 'javascript-natural-sort';
 
-const Row = require('react-bootstrap').Row;
-const Col = require('react-bootstrap').Col;
-const Input = require('react-bootstrap').Input;
-const Panel = require('react-bootstrap').Panel;
-const Button = require('react-bootstrap').Button;
+import { Spinner } from 'components/common';
 
-const Spinner = require('../common/Spinner');
+import Routes from 'routing/Routes';
 
-const RolesStore = require('../../stores/users/RolesStore').RolesStore;
-const LdapGroupsStore = require('../../stores/users/LdapGroupsStore').LdapGroupsStore;
+import RolesStore from 'stores/users/RolesStore';
+import LdapGroupsStore from 'stores/users/LdapGroupsStore';
 
 const LdapGroupsComponent = React.createClass({
   getInitialState() {
     return {
-      loaded: 0,
       groups: Immutable.Set.of(),
       roles: Immutable.Set.of(),
       mapping: Immutable.Map(),
@@ -24,70 +22,92 @@ const LdapGroupsComponent = React.createClass({
   },
 
   componentDidMount() {
-    LdapGroupsStore.loadMapping().done(mapping => this.setState({mapping: Immutable.Map(mapping), loaded: this.state.loaded + 1}));
+    LdapGroupsStore.loadMapping().then(mapping => this.setState({mapping: Immutable.Map(mapping)}));
     LdapGroupsStore.loadGroups()
-      .done(groups => this.setState({groups: Immutable.Set(groups), loaded: this.state.loaded + 1}))
-      .fail((jqXHR, textStatus, errorThrown) => this.setState({groupsErrorMessage: errorThrown}));
-    RolesStore.loadRoles().done(roles => this.setState({roles: Immutable.Set(roles), loaded: this.state.loaded + 1}));
+      .then(
+        groups => this.setState({groups: Immutable.Set(groups)}),
+        error => this.setState({groupsErrorMessage: error})
+      );
+    RolesStore.loadRoles().then(roles => this.setState({roles: Immutable.Set(roles)}));
+  },
+
+  _updateMapping(event) {
+    const role = event.target.value;
+    const group = event.target.getAttribute('data-group');
+    if (role === '') {
+      this.setState({mapping: this.state.mapping.delete(group)});
+    } else {
+      this.setState({mapping: this.state.mapping.set(group, role)});
+    }
+  },
+
+  _saveMapping() {
+    LdapGroupsStore.saveMapping(this.state.mapping.toJS());
+  },
+
+  _isLoading() {
+    return !(this.state.mapping && this.state.groups && this.state.roles);
   },
 
   render() {
-    const options = this.state.roles.sort().map(role => {
-      return (<option key={role.name} value={role.name}>{role.name}</option>);
-    });
-    const content = this.state.groups.sortBy(group => group.toLocaleUpperCase()).map(group => {
-      return (<li key={group}>
-        <Input label={group} data-group={group} type="select" value={this.state.mapping.get(group, '')} onChange={this._updateMapping} labelClassName="col-sm-2" wrapperClassName="col-sm-5">
-          <option value="">None</option>
-          {options}
-        </Input>
-      </li>);
-    });
-    if (this.state.groupsErrorMessage !== null) {
-      return (
-        <div>
-          <Panel header="Error: Unable to load LDAP groups" bsStyle="danger">The error message was:<br/>{this.state.groupsErrorMessage}</Panel>
-        </div>
-      );
-    }
-    if (this.state.loaded < 3) {
+    if (this._isLoading()) {
       return <Spinner />;
     }
+
+    if (this.state.groupsErrorMessage !== null) {
+      return (
+        <Panel header="Error: Unable to load LDAP groups" bsStyle="danger">
+          The error message was:<br/>{this.state.groupsErrorMessage}
+        </Panel>
+      );
+    }
+
+    naturalSort.insensitive = true; // sigh
+
+    const options = this.state.roles.sort(naturalSort).map(role => {
+      return <option key={role.name} value={role.name}>{role.name}</option>;
+    });
+
+    const content = this.state.groups.sort(naturalSort).map(group => {
+      return (
+        <li key={group}>
+          <Input label={group} data-group={group} type="select" value={this.state.mapping.get(group, '')}
+                 onChange={this._updateMapping} labelClassName="col-sm-2" wrapperClassName="col-sm-5">
+            <option value="">None</option>
+            {options}
+          </Input>
+        </li>
+      );
+    });
+
+    naturalSort.insensitive = false; // sigh 2
+
     if (content.size === 0) {
-      return (<Row>
-        <Col md={12} mdPush={1}>
-          <Panel bsStyle="info">No LDAP/Active Directory groups found. Please verify that your <a href="">LDAP group mapping</a> settings are correct.</Panel>
-        </Col>
-      </Row>);
+      return (
+        <p>
+          No LDAP/Active Directory groups found. Please verify that your{' '}
+          <LinkContainer to={Routes.SYSTEM.LDAP.SETTINGS}><a>LDAP group mapping</a></LinkContainer>{' '}
+          settings are correct.
+        </p>
+      );
     } else {
       return (
-        <form className='form-horizontal'>
+        <form className="form-horizontal">
           <Row>
             <Col md={12}>
               <ul style={{padding: 0}}>{content}</ul>
             </Col>
             <Col md={10} mdPush={2}>
               <Button onClick={this._saveMapping} bsStyle="success">Save</Button>&nbsp;
-              <Button href={jsRoutes.controllers.UsersController.index().url}>Cancel</Button>
+              <LinkContainer to={Routes.SYSTEM.USERS.LIST}>
+                <Button>Cancel</Button>
+              </LinkContainer>
             </Col>
           </Row>
         </form>
       );
     }
   },
-
-  _updateMapping(e) {
-    const role = e.target.value;
-    const group = e.target.getAttribute('data-group');
-    if (role === "") {
-      this.setState({mapping: this.state.mapping.delete(group)});
-    } else {
-      this.setState({mapping: this.state.mapping.set(group, role)});
-    }
-  },
-  _saveMapping() {
-    LdapGroupsStore.saveMapping(this.state.mapping.toJS());
-  },
 });
 
-module.exports = LdapGroupsComponent;
+export default LdapGroupsComponent;
