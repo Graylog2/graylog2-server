67d70d89f3f7cbeba83fcae8e16ff8119d4acf59
Dennis Oelkers <dennis@graylog.com>
2016-02-02 13:42:39 +0100

    Remove unused legacy JS from the legacy directory.

--- graylog2-web-interface/src/legacy/universalsearch.js
+++
@@ -1,9 +0,0 @@
-function activateTimerangeChooserV2(rangeType, rangeParams) {
-    "use strict";
-    $(document).trigger('change-timerange.graylog.search', {rangeType: rangeType, rangeParams: rangeParams});
-}
-
-function submitSearch() {
-    "use strict";
-    $(document).trigger('execute.graylog.search');
-}
