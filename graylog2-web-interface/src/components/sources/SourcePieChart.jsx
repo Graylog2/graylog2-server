a341bf632242fa89d10c5725adf923787938ecb9
Edmundo Alvarez <edmundo@graylog.com>
2016-02-04 17:59:00 +0100

    Reformat source tab components

--- graylog2-web-interface/src/components/sources/SourcePieChart.jsx
+++ graylog2-web-interface/src/components/sources/SourcePieChart.jsx
@@ -1,65 +1,74 @@
-/* jshint -W107 */
-'use strict';
+import $ from 'jquery';
+import dc from 'dc';
+import numeral from 'numeral';
+import React, {PropTypes} from 'react';
 
-var $ = require('jquery');
+import SourceTitle from './SourceTitle';
+import D3Utils from 'util/D3Utils';
 
-var dc = require('dc');
-var numeral = require('numeral');
+const SourcePieChart = React.createClass({
+  propTypes: {
+    resetFilters: PropTypes.func.isRequired,
+    numberOfTopValues: PropTypes.number.isRequired,
+  },
 
-var React = require('react');
+  getFilters() {
+    return this._pieChart ? this._pieChart.filters() : [];
+  },
 
-var SourceTitle = require('./SourceTitle');
-var D3Utils = require('../../util/D3Utils');
+  setFilter(filter) {
+    this._pieChart.filter(filter);
+  },
 
-var SourcePieChart = React.createClass({
-    renderPieChart(dimension, group, onDataFiltered) {
-        var pieChartDomNode = $("#dc-sources-pie-chart")[0];
-        var pieChartWidth = $(pieChartDomNode).width();
-        this._pieChart = dc.pieChart(pieChartDomNode);
-        this._pieChart
-            .renderLabel(false)
-            .dimension(dimension)
-            .group(group)
-            .colors(D3Utils.glColourPalette())
-            .slicesCap(this.props.numberOfTopValues)
-            .title((d) => { return d.key + ": " + numeral(d.value).format("0,0"); })
-            .on('renderlet', (chart) => {
-                chart.selectAll(".pie-slice").on("click", () => {
-                    onDataFiltered();
-                });
-            });
-        this._configureWidth(pieChartWidth);
-    },
-    redraw() {
-        this._pieChart.redraw();
-    },
-    clearFilters() {
-        this._pieChart.filterAll();
-    },
-    getFilters() {
-        return this._pieChart ? this._pieChart.filters() : [];
-    },
-    setFilter(filter) {
-        this._pieChart.filter(filter);
-    },
-    _configureWidth(pieChartWidth) {
-        this._pieChart.width(pieChartWidth)
-            .height(pieChartWidth)
-            .radius(pieChartWidth / 2 - 10)
-            .innerRadius(pieChartWidth / 5);
-    },
-    updateWidth() {
-        var pieChartDomNode = $("#dc-sources-pie-chart").parent();
-        var pieChartWidth = pieChartDomNode.width();
-        this._configureWidth(pieChartWidth);
-    },
-    render() {
-        return (
-            <div id="dc-sources-pie-chart" ref="sourcePieChart">
-                <SourceTitle className="reset" resetFilters={this.props.resetFilters}>Messages per source</SourceTitle>
-            </div>
-        );
-    }
+  clearFilters() {
+    this._pieChart.filterAll();
+  },
+
+  redraw() {
+    this._pieChart.redraw();
+  },
+
+  _configureWidth(pieChartWidth) {
+    this._pieChart.width(pieChartWidth)
+      .height(pieChartWidth)
+      .radius(pieChartWidth / 2 - 10)
+      .innerRadius(pieChartWidth / 5);
+  },
+
+  updateWidth() {
+    const $pieChartDomNode = $('#dc-sources-pie-chart').parent();
+    const pieChartWidth = $pieChartDomNode.width();
+    this._configureWidth(pieChartWidth);
+  },
+
+  renderPieChart(dimension, group, onDataFiltered) {
+    const pieChartDomNode = $('#dc-sources-pie-chart')[0];
+    const pieChartWidth = $(pieChartDomNode).width();
+    this._pieChart = dc.pieChart(pieChartDomNode);
+    this._pieChart
+      .renderLabel(false)
+      .dimension(dimension)
+      .group(group)
+      .colors(D3Utils.glColourPalette())
+      .slicesCap(this.props.numberOfTopValues)
+      .title((d) => {
+        return d.key + ': ' + numeral(d.value).format('0,0');
+      })
+      .on('renderlet', (chart) => {
+        chart.selectAll('.pie-slice').on('click', () => {
+          onDataFiltered();
+        });
+      });
+    this._configureWidth(pieChartWidth);
+  },
+
+  render() {
+    return (
+      <div id="dc-sources-pie-chart" ref="sourcePieChart">
+        <SourceTitle className="reset" resetFilters={this.props.resetFilters}>Messages per source</SourceTitle>
+      </div>
+    );
+  },
 });
 
-module.exports = SourcePieChart;
+export default SourcePieChart;
