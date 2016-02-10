fb1f34c5bc22bef01ea838e69ae2852291f3b5f7
Dennis Oelkers <dennis@graylog.com>
2016-02-10 17:50:35 +0100

    Removing slider, replacing with dropdown containing interval presets.

--- graylog2-web-interface/src/components/search/RefreshControls.jsx
+++ graylog2-web-interface/src/components/search/RefreshControls.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import Reflux from 'reflux';
-import { Button, ButtonGroup, DropdownButton } from 'react-bootstrap';
+import moment from 'moment';
+import { Button, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
+
+import { Pluralize } from 'components/common';
 
 import RefreshStore from 'stores/tools/RefreshStore';
-
 import RefreshActions from 'actions/tools/RefreshActions';
 
 const RefreshControls = React.createClass({
   mixins: [Reflux.connect(RefreshStore, 'refresh')],
+  INTERVAL_OPTIONS: {
+    '1 Second': 1,
+    '2 Seconds': 2,
+    '5 Seconds': 5,
+    '10 Seconds': 10,
+    '30 Seconds': 30,
+    '1 Minute': 60,
+    '5 Minutes': 300,
+  },
   render() {
+    const intervalOptions = Object.keys(this.INTERVAL_OPTIONS).map((key) => {
+      const interval = this.INTERVAL_OPTIONS[key] * 1000;
+      return <MenuItem key={'RefreshControls-' + key} onClick={() => RefreshActions.changeInterval(interval)}>{key}</MenuItem>;
+    });
+    const intervalDuration = moment.duration(this.state.refresh.interval);
+    const naturalInterval = intervalDuration.asSeconds() < 60 ?
+      <span>{intervalDuration.asSeconds()} <Pluralize singular="second" plural="seconds" value={intervalDuration.asSeconds()} /></span> :
+      <span>{intervalDuration.asMinutes()} <Pluralize singular="minute" plural="minutes" value={intervalDuration.asMinutes()} /></span>;
+    const buttonLabel = <span>Update every {naturalInterval}</span>;
     return (
       <ButtonGroup>
         <Button bsSize="small" onClick={() => this.state.refresh.enabled ? RefreshActions.disable() : RefreshActions.enable()}>
           {this.state.refresh.enabled ? <i className="fa fa-pause"/> : <i className="fa fa-play"/>}
         </Button>
 
-        <DropdownButton bsSize="small" title={this.state.refresh.enabled ? 'Update every ' + (this.state.refresh.interval / 1000) + ' seconds' : 'Not updating'} id="refresh-options-dropdown">
-          <div className="form-control" style={{'height': '100%', 'width': '150%', display: 'flex'}}>
-            1s{' '}
-            <input id="mySlider"
-                   type="range"
-                   value={this.state.refresh.interval}
-                   min={1000}
-                   max={60 * 1000}
-                   onChange={(evt) => RefreshActions.changeInterval(evt.target.value)}
-                   step={1000} />
-            {' '}60s
-          </div>
+        <DropdownButton bsSize="small" title={this.state.refresh.enabled ? buttonLabel : 'Not updating'} id="refresh-options-dropdown">
+          {intervalOptions}
         </DropdownButton>
       </ButtonGroup>
     );
