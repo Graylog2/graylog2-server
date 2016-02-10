7e4b2893fcac6ab326c2d9553076a037183b9403
Edmundo Alvarez <edmundo@graylog.com>
2016-02-10 13:00:54 +0100

    Format MessageTablePaginator

---
+++ graylog2-web-interface/src/components/common/Page.jsx
@@ -0,0 +1,32 @@
+import React, {PropTypes} from 'react';
+import { PageItem } from 'react-bootstrap';
+
+const Page = React.createClass({
+  propTypes: {
+    href: PropTypes.string,
+    page: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]).isRequired,
+    onPageChanged: PropTypes.func.isRequired,
+    isDisabled: PropTypes.bool,
+    isActive: PropTypes.bool,
+  },
+  render() {
+    let className = '';
+    if (this.props.isActive) {
+      className = 'active';
+    }
+
+    return (
+      <PageItem href={this.props.href}
+                className={className}
+                disabled={this.props.isDisabled}
+                onSelect={() => this.props.onPageChanged(this.props.page)}>
+        {this.props.page}
+      </PageItem>
+    );
+  },
+});
+
+export default Page;
