0ebbfe5ac89d6bbc0beaf28bfc2b65ea47c2398e
Edmundo Alvarez <edmundo@graylog.com>
2016-02-11 15:17:35 +0100

    Reformat EditOutputButton

--- graylog2-web-interface/src/components/outputs/EditOutputButton.jsx
+++ graylog2-web-interface/src/components/outputs/EditOutputButton.jsx
@@ -1,45 +1,58 @@
-'use strict';
+import React, {PropTypes} from 'react';
+import { Button } from 'react-bootstrap';
+import { ConfigurationForm } from 'components/configurationforms';
 
-var React = require('react');
-var ConfigurationForm = require('../configurationforms/ConfigurationForm');
-var Button = require('react-bootstrap').Button;
+const EditOutputButton = React.createClass({
+  propTypes: {
+    output: PropTypes.object,
+    disabled: PropTypes.bool,
+    getTypeDefinition: PropTypes.func.isRequired,
+    onUpdate: PropTypes.func,
+  },
+  getInitialState() {
+    return {
+      typeDefinition: undefined,
+      typeName: undefined,
+      configurationForm: '',
+    };
+  },
 
-var EditOutputButton = React.createClass({
-    getInitialState() {
-        return {
-            typeDefinition: undefined,
-            typeName: undefined,
-            configurationForm: ""
-        };
-    },
-    handleClick() {
-        this.props.getTypeDefinition(this.props.output.type, (definition) => {
-            this.setState({typeDefinition: definition.requested_configuration});
-            this.refs.configurationForm.open();
-        });
-    },
-    _handleSubmit(data) {
-        this.props.onUpdate(this.props.output, data);
-    },
-    render() {
-        var typeDefinition = this.state.typeDefinition;
-        var output = this.props.output;
-        var configurationForm = (typeDefinition ?
-            <ConfigurationForm ref="configurationForm" key={"configuration-form-output-"+output.id} configFields={this.state.typeDefinition}
-                               title={"Editing Output " + output.title}
-                               typeName={output.type}
-                               helpBlock={"Select a name of your new output that describes it."}
-                               submitAction={this._handleSubmit} values={output.configuration} titleValue={output.title}/>
-            : ""
-        );
-        return (
-            <span>
-                <Button disabled={this.props.disabled} bsStyle="info" onClick={this.handleClick.bind(null, output)}>
-                    Edit
-                </Button>
-                {configurationForm}
-            </span>
-        );
+  handleClick() {
+    this.props.getTypeDefinition(this.props.output.type, (definition) => {
+      this.setState({typeDefinition: definition.requested_configuration});
+      this.refs.configurationForm.open();
+    });
+  },
+
+  _handleSubmit(data) {
+    this.props.onUpdate(this.props.output, data);
+  },
+
+  render() {
+    const typeDefinition = this.state.typeDefinition;
+    const output = this.props.output;
+    let configurationForm;
+
+    if (typeDefinition) {
+      configurationForm = (
+        <ConfigurationForm ref="configurationForm" key={`configuration-form-output-${output.id}`}
+                           configFields={this.state.typeDefinition}
+                           title={`Editing Output ${output.title}`}
+                           typeName={output.type}
+                           helpBlock={"Select a name of your new output that describes it."}
+                           submitAction={this._handleSubmit} values={output.configuration} titleValue={output.title}/>
+      );
     }
+
+    return (
+      <span>
+        <Button disabled={this.props.disabled} bsStyle="info" onClick={this.handleClick.bind(null, output)}>
+          Edit
+        </Button>
+        {configurationForm}
+      </span>
+    );
+  },
 });
-module.exports = EditOutputButton;
+
+export default EditOutputButton;
