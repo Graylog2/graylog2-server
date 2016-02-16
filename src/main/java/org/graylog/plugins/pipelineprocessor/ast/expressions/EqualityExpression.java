c1104df9a42d81bfdde9b1a33ec869385551d256
Kay Roepke <kroepke@googlemail.com>
2016-02-16 18:14:24 +0100

    EqualityExpression special cases DateTime objects
    Joda's DateTime insists on times not being equal if their timezones are different but both refer to the same instant. Need to use isEqual instead :grumpy:

--- src/main/java/org/graylog/plugins/pipelineprocessor/ast/expressions/EqualityExpression.java
+++ src/main/java/org/graylog/plugins/pipelineprocessor/ast/expressions/EqualityExpression.java
@@ -17,8 +17,13 @@
 package org.graylog.plugins.pipelineprocessor.ast.expressions;
 
 import org.graylog.plugins.pipelineprocessor.EvaluationContext;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EqualityExpression extends BinaryExpression implements LogicalExpression {
+    private static final Logger log = LoggerFactory.getLogger(EqualityExpression.class);
+
     private final boolean checkEquality;
 
     public EqualityExpression(Expression left, Expression right, boolean checkEquality) {
@@ -41,14 +46,34 @@ public class EqualityExpression extends BinaryExpression implements LogicalExpre
         final Object left = this.left.evaluate(context);
         final Object right = this.right.evaluate(context);
         if (left == null) {
-            // TODO log error
+            log.warn("left expression evaluated to null, returning false: {}", this.left);
             return false;
         }
-        final boolean equals = left.equals(right);
+        final boolean equals;
+        // sigh: DateTime::equals takes the chronology into account, so identical instants expressed in different timezones are not equal
+        if (left instanceof DateTime && right instanceof DateTime) {
+            equals = ((DateTime) left).isEqual((DateTime) right);
+        } else {
+            equals = left.equals(right);
+        }
+
+        if (log.isTraceEnabled()) {
+            traceEquality(left, right, equals, checkEquality);
+        }
         if (checkEquality) {
             return equals;
         }
         return !equals;
+    }
+
+    private void traceEquality(Object left,
+                               Object right,
+                               boolean equals,
+                               boolean checkEquality) {
+        log.trace(checkEquality
+                          ? "[{}] {} == {} : {} == {}"
+                          : "[{}] {} != {} : {} != {}",
+                  checkEquality == equals, this.left, this.right, left, right);
     }
 
     @Override
