18e3474810da19e94944ec90dd0af94fbafc0d35
Edmundo Alvarez <edmundo@edmundoa.com>
2016-02-22 18:19:14 +0100

    Add plugin to web interface

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -1,10 +1,19 @@
 import packageJson from '../../package.json';
 import {} from '../../config.js';
 import { PluginManifest, PluginStore } from 'graylog-web-plugin/plugin';
+import MapVisualization from 'components/MapVisualization';
 import MapPage from 'pages/MapPage';
 
 PluginStore.register(new PluginManifest(packageJson, {
-  routes: [
-    { path: '/maps', component: MapPage },
+  routes: {
+    path: '/maps', component: MapPage,
+  },
+  widgets: [
+    {
+      type: 'org.graylog.plugins.map.widget.strategy.MapWidgetStrategy',
+      defaultHeight: 2,
+      defaultWidth: 2,
+      visualization: MapVisualization,
+    },
   ],
 }));
