5fc1b4487c02cf156fd0dd0aeef5fea19b62da6d
Edmundo Alvarez <edmundo@edmundoa.com>
2016-02-22 18:26:50 +0100

    Fix routes registration

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -5,9 +5,9 @@ import MapVisualization from 'components/MapVisualization';
 import MapPage from 'pages/MapPage';
 
 PluginStore.register(new PluginManifest(packageJson, {
-  routes: {
-    path: '/maps', component: MapPage,
-  },
+  routes: [
+    { path: '/maps', component: MapPage },
+  ],
   widgets: [
     {
       type: 'org.graylog.plugins.map.widget.strategy.MapWidgetStrategy',
