f63065ad999604d8e03843af56988554e78d4c8a
Bernd Ahlers <bernd@graylog.com>
2016-02-24 10:18:32 +0100

    Dynamic marker radius based on occurrences
    Also increase marker radius on higher zoom levels.

--- src/web/components/MapVisualization.jsx
+++ src/web/components/MapVisualization.jsx
@@ -35,10 +35,11 @@ const MapVisualization = React.createClass({
   DEFAULT_URL: `https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=${window.mapConfig.mapboxAccessToken}`,
   DEFAULT_ATTRIBUTION: `<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>`,
   // Coordinates are given as "lat,long"
-  _formatMarker(coordinates, occurrences) {
+  _formatMarker(coordinates, occurrences, min, max, increment) {
     const formattedCoordinates = coordinates.split(',').map(component => Number(component));
+    const radius = this._getBucket(occurrences, 10, min, max, increment);
     return (
-      <CircleMarker key={coordinates} center={formattedCoordinates} radius={5} color="#AF2228" fillColor="#D3242B" opacity={0.8}>
+      <CircleMarker key={coordinates} center={formattedCoordinates} radius={radius} color="#AF2228" fillColor="#D3242B" weight={2} opacity={0.8}>
         <Popup>
             <dl>
               <dt>Coordinates:</dt>
@@ -53,14 +54,27 @@ const MapVisualization = React.createClass({
   _onZoomChange(event) {
     this.setState({zoomLevel: event.target.getZoom()});
   },
+  _getBucket(value, bucketCount, minValue, maxValue, increment) {
+    // Calculate bucket size based on min/max value and the number of buckets.
+    const bucketSize = (maxValue - minValue) / bucketCount;
+    // Select bucket for the given value.
+    const bucket = value < maxValue ? Math.ceil((value - minValue) / bucketSize) : bucketCount;
+
+    return bucket + increment;
+  },
   render() {
     if (!this.state.isComponentMounted) {
       return <Spinner/>;
     }
 
     const data = this.props.data;
+    const occurrences = Object.keys(data).map((k) => data[k]);
+    const minOccurrences = occurrences.reduce((prev, cur) => Math.min(prev, cur), Infinity);
+    const maxOccurrences = occurrences.reduce((prev, cur) => Math.max(prev, cur), -Infinity);
+    const increment = this._getBucket(this.state.zoomLevel, 10, 1, 10, 1);
+
     const coordinates = Object.keys(data);
-    const markers = coordinates.map(aCoordinates => this._formatMarker(aCoordinates, data[aCoordinates]));
+    const markers = coordinates.map(aCoordinates => this._formatMarker(aCoordinates, data[aCoordinates], minOccurrences, maxOccurrences, increment));
     const leafletUrl = this.props.url || this.DEFAULT_URL;
     const leafletAttribution = this.props.attribution || this.DEFAULT_ATTRIBUTION;
 
