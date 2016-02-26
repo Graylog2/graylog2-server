39e013cc333e4e2533602621d853ee1fbd68000d
Bernd Ahlers <bernd@graylog.com>
2016-02-26 15:16:56 +0100

    Bump Graylog server dependency to 2.0.0-alpha.4
    Set version of this plugin to alpha.4.

--- src/main/java/org/graylog/plugins/map/MapWidgetMetaData.java
+++ src/main/java/org/graylog/plugins/map/MapWidgetMetaData.java
@@ -47,7 +47,7 @@ public class MapWidgetMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 0, 0);
+        return new Version(1, 0, 0, "alpha.4");
     }
 
     @Override
