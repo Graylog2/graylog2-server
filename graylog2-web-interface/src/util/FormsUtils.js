06cccbea867dab438a7cc592824f5b6698f8d65d
Edmundo Alvarez <edmundo@graylog.com>
2016-03-02 19:35:55 +0100

    Handle radio buttons on FormsUtils

--- graylog2-web-interface/src/util/FormsUtils.js
+++ graylog2-web-interface/src/util/FormsUtils.js
@@ -3,6 +3,9 @@ import NumberUtils from 'util/NumberUtils';
 const FormUtils = {
   getValueFromInput(input) {
     switch (input.type) {
+      case 'radio':
+        const value = input.value;
+        return (value === 'true' || value === 'false' ? value === 'true' : value);
       case 'checkbox':
         return input.checked;
       case 'number':
