85593397511ac8f6d45d3bc4f77d44a36f14b311
Edmundo Alvarez <edmundo@graylog.com>
2016-03-03 11:42:32 +0100

    Allow to remove options from selectable list

--- graylog2-web-interface/src/components/common/SelectableList.jsx
+++ graylog2-web-interface/src/components/common/SelectableList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 import { Select } from 'components/common';
 
@@ -27,13 +28,32 @@ const SelectableList = React.createClass({
     }
   },
 
+  _onRemoveOption(optionIndex) {
+    return () => {
+      const newSelectedOptions = this.state.selectedOptions.filter((_, idx) => idx !== optionIndex);
+      this.setState({selectedOptions: newSelectedOptions});
+      if (typeof this.props.onChange === 'function') {
+        this.props.onChange(newSelectedOptions);
+      }
+    };
+  },
+
   render() {
-    const formattedOptions = this.state.selectedOptions.map((option, idx) => <li key={`${option}-${idx}`}>{option}</li>);
+    const formattedOptions = this.state.selectedOptions.map((option, idx) => {
+      return (
+        <ListGroupItem key={`${option}-${idx}`}>
+          <div className="pull-right">
+            <Button bsStyle="primary" bsSize="xsmall" onClick={this._onRemoveOption(idx)}>Remove</Button>
+          </div>
+          {option}
+        </ListGroupItem>
+      );
+    });
     return (
       <div>
         <Select options={this.props.options} onValueChange={this._onAddOption}/>
         {formattedOptions.length > 0 &&
-        <ul>{formattedOptions}</ul>
+        <ListGroup style={{marginTop: 10}}>{formattedOptions}</ListGroup>
         }
       </div>
     );
