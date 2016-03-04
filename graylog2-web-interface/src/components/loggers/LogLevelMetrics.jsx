67ff6893715f455d64bcc1561ab0b3d6f3bf1173
Dennis Oelkers <dennis@graylog.com>
2016-03-04 11:55:54 +0100

    Move spinner into log level metrics detail components.

--- graylog2-web-interface/src/components/loggers/LogLevelMetrics.jsx
+++ graylog2-web-interface/src/components/loggers/LogLevelMetrics.jsx
@@ -27,22 +27,27 @@ const LogLevelMetrics = React.createClass({
   render() {
     const { loglevel, nodeId } = this.props;
     const { metrics } = this.state;
+    let metricsDetails;
     if (!metrics || !metrics[nodeId] || !metrics[nodeId][this._metricName()]) {
-      return <Spinner />;
+      metricsDetails =  <Spinner />;
+    } else {
+      const metric = metrics[nodeId][this._metricName()].metric;
+      metricsDetails = (
+        <dl className="loglevel-metrics-list">
+          <dt>Total written:</dt>
+          <dd><span className="loglevel-metric-total">{metric.rate.total}</span></dd>
+          <dt>Mean rate:</dt>
+          <dd><span className="loglevel-metric-mean">{numeral(metric.rate.mean).format('0.00')}</span> / second</dd>
+          <dt>1 min rate:</dt>
+          <dd><span className="loglevel-metric-1min">{numeral(metric.rate.one_minute).format('0.00')}</span> / second</dd>
+        </dl>
+      );
     }
-    const metric = metrics[nodeId][this._metricName()].metric;
     return (
       <div className="loglevel-metrics-row">
         <Col md={4}>
           <h3 className="u-light">Level: {String(loglevel).capitalize().toString()}</h3>
-          <dl className="loglevel-metrics-list">
-            <dt>Total written:</dt>
-            <dd><span className="loglevel-metric-total">{metric.rate.total}</span></dd>
-            <dt>Mean rate:</dt>
-            <dd><span className="loglevel-metric-mean">{numeral(metric.rate.mean).format('0.00')}</span> / second</dd>
-            <dt>1 min rate:</dt>
-            <dd><span className="loglevel-metric-1min">{numeral(metric.rate.one_minute).format('0.00')}</span> / second</dd>
-          </dl>
+          {metricsDetails}
         </Col>
       </div>
     );
