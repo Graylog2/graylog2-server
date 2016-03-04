ca95aae0c056d8d71190e9d63acbba4e4d379184
Dennis Oelkers <dennis@graylog.com>
2016-03-04 14:42:53 +0100

    Fixing some linter hints.

--- graylog2-web-interface/src/util/ExtractorUtils.jsx
+++ graylog2-web-interface/src/util/ExtractorUtils.jsx
@@ -39,53 +39,53 @@ const ExtractorUtils = {
 
   getReadableExtractorTypeName(extractorType) {
     switch (extractorType) {
-    case ExtractorTypes.COPY_INPUT:
-      return 'Copy input';
-    case ExtractorTypes.GROK:
-      return 'Grok pattern';
-    case ExtractorTypes.JSON:
-      return 'JSON';
-    case ExtractorTypes.REGEX:
-      return 'Regular expression';
-    case ExtractorTypes.REGEX_REPLACE:
-      return 'Replace with regular expression';
-    case ExtractorTypes.SPLIT_AND_INDEX:
-      return 'Split & Index';
-    case ExtractorTypes.SUBSTRING:
-      return 'Substring';
-    default:
-      return extractorType;
+      case ExtractorTypes.COPY_INPUT:
+        return 'Copy input';
+      case ExtractorTypes.GROK:
+        return 'Grok pattern';
+      case ExtractorTypes.JSON:
+        return 'JSON';
+      case ExtractorTypes.REGEX:
+        return 'Regular expression';
+      case ExtractorTypes.REGEX_REPLACE:
+        return 'Replace with regular expression';
+      case ExtractorTypes.SPLIT_AND_INDEX:
+        return 'Split & Index';
+      case ExtractorTypes.SUBSTRING:
+        return 'Substring';
+      default:
+        return extractorType;
     }
   },
 
   getReadableConverterTypeName(converterType) {
     switch (converterType) {
-    case this.ConverterTypes.NUMERIC:
-      return 'Numeric';
-    case this.ConverterTypes.DATE:
-      return 'Date';
-    case this.ConverterTypes.FLEXDATE:
-      return 'Flexible Date';
-    case this.ConverterTypes.HASH:
-      return 'Hash';
-    case this.ConverterTypes.LOWERCASE:
-      return 'Lowercase';
-    case this.ConverterTypes.UPPERCASE:
-      return 'Uppercase';
-    case this.ConverterTypes.TOKENIZER:
-      return 'Key = Value Pairs To Fields';
-    case this.ConverterTypes.CSV:
-      return 'CSV To Fields';
-    case this.ConverterTypes.SPLIT_AND_COUNT:
-      return 'Split & Count';
-    case this.ConverterTypes.IP_ANONYMIZER:
-      return 'Anonymize IPv4 Addresses';
-    case this.ConverterTypes.SYSLOG_PRI_LEVEL:
-      return 'Syslog Level From PRI';
-    case this.ConverterTypes.SYSLOG_PRI_FACILITY:
-      return 'Syslog Facility From PRI';
-    default:
-      return converterType;
+      case this.ConverterTypes.NUMERIC:
+        return 'Numeric';
+      case this.ConverterTypes.DATE:
+        return 'Date';
+      case this.ConverterTypes.FLEXDATE:
+        return 'Flexible Date';
+      case this.ConverterTypes.HASH:
+        return 'Hash';
+      case this.ConverterTypes.LOWERCASE:
+        return 'Lowercase';
+      case this.ConverterTypes.UPPERCASE:
+        return 'Uppercase';
+      case this.ConverterTypes.TOKENIZER:
+        return 'Key = Value Pairs To Fields';
+      case this.ConverterTypes.CSV:
+        return 'CSV To Fields';
+      case this.ConverterTypes.SPLIT_AND_COUNT:
+        return 'Split & Count';
+      case this.ConverterTypes.IP_ANONYMIZER:
+        return 'Anonymize IPv4 Addresses';
+      case this.ConverterTypes.SYSLOG_PRI_LEVEL:
+        return 'Syslog Level From PRI';
+      case this.ConverterTypes.SYSLOG_PRI_FACILITY:
+        return 'Syslog Facility From PRI';
+      default:
+        return converterType;
     }
   },
 
