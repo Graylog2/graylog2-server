4c9861a0cd4850bc0b290bf1491f6db2388ad446
Edmundo Alvarez <edmundo@graylog.com>
2016-03-04 18:05:12 +0100

    Update license headers

--- benchmarks/src/main/java/org/graylog/benchmarks/pipeline/DeclaringClassBenchmark.java
+++ benchmarks/src/main/java/org/graylog/benchmarks/pipeline/DeclaringClassBenchmark.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Graylog.
+ * This file is part of Graylog Pipeline Processor.
  *
- * Graylog is free software: you can redistribute it and/or modify
+ * Graylog Pipeline Processor is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog is distributed in the hope that it will be useful,
+ * Graylog Pipeline Processor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog Pipeline Processor.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog.benchmarks.pipeline;
 
