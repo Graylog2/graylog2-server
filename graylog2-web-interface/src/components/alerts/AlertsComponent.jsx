667f8e8aee42b040a347261573ab925d656110c2
Edmundo Alvarez <edmundo@graylog.com>
2016-03-10 16:38:37 +0100

    Reformat AlertsComponent

--- graylog2-web-interface/src/components/alerts/AlertsComponent.jsx
+++ graylog2-web-interface/src/components/alerts/AlertsComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Input, Pagination } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import LinkedStateMixin from 'react-addons-linked-state-mixin';
 
 import AlertsStore from 'stores/alerts/AlertsStore';
@@ -7,21 +7,30 @@ import { PaginatedList, Spinner } from 'components/common';
 import AlertsTable from 'components/alerts/AlertsTable';
 
 const AlertsComponent = React.createClass({
-  mixins: [LinkedStateMixin],
-  componentDidMount() {
-    this.loadData(1, 10);
+  propTypes: {
+    streamId: React.PropTypes.string.isRequired,
   },
+
+  mixins: [LinkedStateMixin],
+
   getInitialState() {
     return {};
   },
+
+  componentDidMount() {
+    this.loadData(1, 10);
+  },
+
   loadData(pageNo, limit) {
-    AlertsStore.list(this.props.streamId, (pageNo-1)*limit, limit).done((alerts) => {
-      this.setState({alerts: alerts});
+    AlertsStore.list(this.props.streamId, (pageNo - 1) * limit, limit).done((alerts) => {
+      this.setState({ alerts });
     });
   },
+
   _onChangePaginatedList(page, size) {
     this.loadData(page, size);
   },
+
   render() {
     if (!this.state.alerts) {
       return (
@@ -33,7 +42,10 @@ const AlertsComponent = React.createClass({
       );
     }
 
-    const triggeredAlertsText = this.state.alerts.total > 0 ? <span>&nbsp; <small>{this.state.alerts.total} alerts total</small></span> : null;
+    let triggeredAlertsText;
+    if (this.state.alerts.total > 0) {
+      triggeredAlertsText = <span>&nbsp;<small>{this.state.alerts.total} alerts total</small></span>;
+    }
 
     return (
       <Row className="content triggered-alerts">
