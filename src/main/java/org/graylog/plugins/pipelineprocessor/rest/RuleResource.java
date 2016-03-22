dd87cd25fae1ec69e4ef02e8406ec193d217b6e9
Kay Roepke <kroepke@googlemail.com>
2016-03-22 10:59:52 +0100

    update to swagger 1.5 imports
    depends on graylog2-server/swagger-1.5.x

--- src/main/java/org/graylog/plugins/pipelineprocessor/rest/RuleResource.java
+++ src/main/java/org/graylog/plugins/pipelineprocessor/rest/RuleResource.java
@@ -17,9 +17,9 @@
 package org.graylog.plugins.pipelineprocessor.rest;
 
 import com.google.common.eventbus.EventBus;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.graylog.plugins.pipelineprocessor.ast.Rule;
 import org.graylog.plugins.pipelineprocessor.db.RuleDao;
 import org.graylog.plugins.pipelineprocessor.db.RuleService;
