659681dc73bc3c2e338b0d7a6faf6a03b5ee5f0b
Jochen Schalanda <jochen@graylog.com>
2016-03-24 17:17:10 +0100

    Remove unused command line parameters "-r" and "-t"

--- graylog2-server/src/main/java/org/graylog2/commands/Server.java
+++ graylog2-server/src/main/java/org/graylog2/commands/Server.java
@@ -83,25 +83,11 @@ public class Server extends ServerBootstrap {
         super("server", configuration);
     }
 
-    @Option(name = {"-t", "--configtest"}, description = "Validate Graylog configuration and exit")
-    private boolean configTest = false;
-
     @Option(name = {"-l", "--local"}, description = "Run Graylog in local mode. Only interesting for Graylog developers.")
     private boolean local = false;
 
-    @Option(name = {"-r", "--no-retention"}, description = "Do not automatically remove messages from index that are older than the retention time")
-    private boolean noRetention = false;
-
-    public boolean isConfigTest() {
-        return configTest;
-    }
-
     public boolean isLocal() {
         return local;
-    }
-
-    public boolean performRetention() {
-        return !noRetention;
     }
 
     @Override
