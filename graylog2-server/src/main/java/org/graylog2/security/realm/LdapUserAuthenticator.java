Graylog2/graylog2-server

commit 9b746fa442d758a0b15d8c991172f030d74a60b4

Prevent NPE in UserImpl#getPermissions() if field is null (#2046)
* Prevent NPE in UserImpl#getPermissions() if field is null
* log other exceptions as errors as well to help tracking down problems

Fixes #2045

graylog2-server/src/main/java/org/graylog2/security/realm/LdapUserAuthenticator.java
@@ -125,6 +125,8 @@ public class LdapUserAuthenticator extends AuthenticatingRealm {
         } catch (CursorException e) {
             LOG.error("Unable to read LDAP entry", e);
             return null;
+        } catch (Exception e) {
+            LOG.error("Error during LDAP user account sync. Cannot log in user {}", principal, e);
         } finally {
             if (connection != null) {
                 try {

graylog2-server/src/main/java/org/graylog2/database/PersistedImpl.java
@@ -24,6 +24,7 @@ import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -41,22 +42,31 @@ public abstract class PersistedImpl implements Persisted {
     protected final ObjectId id;
     private final AtomicReference<String> hexId = new AtomicReference<>(null);
 
-    protected PersistedImpl(final Map<String, Object> fields) {
+    protected PersistedImpl(@Nullable final Map<String, Object> fields) {
         this(new ObjectId(), fields);
     }
 
-    protected PersistedImpl(final ObjectId id, final Map<String, Object> fields) {
+    protected PersistedImpl(final ObjectId id, @Nullable final Map<String, Object> fields) {
         this.id = id;
-        this.fields = fields;
 
         if (null != this.id) {
             hexId.set(this.id.toHexString());
         }
 
-        // Transform all java.util.Date's to JodaTime because MongoDB gives back java.util.Date's. #lol
-        for (Map.Entry<String, Object> field : fields.entrySet()) {
-            if (field.getValue() instanceof Date) {
-                fields.put(field.getKey(), new DateTime(field.getValue(), DateTimeZone.UTC));
+        if(fields == null) {
+            this.fields = new HashMap<>();
+        } else {
+            this.fields = new HashMap<>(fields.size());
+
+            // Transform all java.util.Date's to JodaTime because MongoDB gives back java.util.Date's. #lol
+            for (Map.Entry<String, Object> field : fields.entrySet()) {
+                final String key = field.getKey();
+                final Object value = field.getValue();
+                if (value instanceof Date) {
+                    this.fields.put(key, new DateTime(value, DateTimeZone.UTC));
+                } else {
+                    this.fields.put(key, value);
+                }
             }
         }
     }

graylog2-server/src/main/java/org/graylog2/shared/users/UserService.java
@@ -21,11 +21,13 @@ import org.graylog2.plugin.database.users.User;
 import org.graylog2.shared.security.ldap.LdapEntry;
 import org.graylog2.shared.security.ldap.LdapSettings;
 
+import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 public interface UserService extends PersistedService {
+    @Nullable
     User load(String username);
 
     int delete(String username);
@@ -34,6 +36,7 @@ public interface UserService extends PersistedService {
 
     List<User> loadAll();
 
+    @Nullable
     User syncFromLdapEntry(LdapEntry userEntry, LdapSettings ldapSettings, String username);
 
     void updateFromLdap(User user, LdapEntry userEntry, LdapSettings ldapSettings, String username);

graylog2-server/src/main/java/org/graylog2/users/UserImpl.java
@@ -19,9 +19,6 @@ package org.graylog2.users;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import org.bson.types.ObjectId;
@@ -44,7 +41,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -83,7 +83,6 @@ public class UserImpl extends PersistedImpl implements User {
     public static final String EXTERNAL_USER = "external_user";
     public static final String SESSION_TIMEOUT = "session_timeout_ms";
     public static final String STARTPAGE = "startpage";
-    public static final String HASH_ALGORITHM = "SHA-1";
     public static final String ROLES = "roles";
 
     public static final int MAX_USERNAME_LENGTH = 100;
@@ -161,11 +160,14 @@ public class UserImpl extends PersistedImpl implements User {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<String> getPermissions() {
-        final Set<String> permissionSet = Sets.newHashSet((List<String>) fields.get(PERMISSIONS));
-        permissionSet.addAll(permissions.userSelfEditPermissions(getName()));
-        return Lists.newArrayList(permissionSet);
+        final Set<String> permissionSet = new HashSet<>(this.permissions.userSelfEditPermissions(getName()));
+        @SuppressWarnings("unchecked")
+        final List<String> permissions = (List<String>) fields.get(PERMISSIONS);
+        if (permissions != null) {
+            permissionSet.addAll(permissions);
+        }
+        return new ArrayList<>(permissionSet);
     }
 
     @Override
@@ -190,7 +192,7 @@ public class UserImpl extends PersistedImpl implements User {
 
     @Override
     public Map<String, String> getStartpage() {
-        final Map<String, String> startpage = Maps.newHashMap();
+        final Map<String, String> startpage = new HashMap<>();
 
         if (fields.containsKey(STARTPAGE)) {
             @SuppressWarnings("unchecked")
@@ -302,17 +304,17 @@ public class UserImpl extends PersistedImpl implements User {
     @Override
     public Set<String> getRoleIds() {
         final List<ObjectId> roles = firstNonNull((List<ObjectId>) fields.get(ROLES), Collections.<ObjectId>emptyList());
-        return Sets.newHashSet(Collections2.transform(roles, new ObjectIdStringFunction()));
+        return new HashSet<>(Collections2.transform(roles, new ObjectIdStringFunction()));
     }
 
     @Override
     public void setRoleIds(Set<String> roles) {
-        fields.put(ROLES, Lists.newArrayList(Collections2.transform(roles, new StringObjectIdFunction())));
+        fields.put(ROLES, new ArrayList<>(Collections2.transform(roles, new StringObjectIdFunction())));
     }
 
     @Override
     public void setStartpage(final String type, final String id) {
-        final Map<String, String> startpage = Maps.newHashMap();
+        final Map<String, String> startpage = new HashMap<>();
 
         if (type != null && id != null) {
             startpage.put("type", type);

graylog2-server/src/main/java/org/graylog2/users/UserServiceImpl.java
@@ -44,6 +44,7 @@ import org.graylog2.shared.users.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.util.Collection;
 import java.util.Collections;
@@ -84,6 +85,7 @@ public class UserServiceImpl extends PersistedServiceImpl implements UserService
     }
 
     @Override
+    @Nullable
     public User load(final String username) {
         LOG.debug("Loading user {}", username);
 
@@ -145,6 +147,7 @@ public class UserServiceImpl extends PersistedServiceImpl implements UserService
     }
 
     @Override
+    @Nullable
     public User syncFromLdapEntry(LdapEntry userEntry, LdapSettings ldapSettings, String username) {
         UserImpl user = (UserImpl) load(username);
 
@@ -192,13 +195,6 @@ public class UserServiceImpl extends PersistedServiceImpl implements UserService
             ((UserImpl) user).setHashedPassword("User synced from LDAP.");
         }
 
-        if (user.getPermissions() == null) {
-            user.setPermissions(Lists.newArrayList(permissions.userSelfEditPermissions(username)));
-        } else {
-            user.setPermissions(Lists.newArrayList(Sets.union(permissions.userSelfEditPermissions(username),
-                                                              Sets.newHashSet(user.getPermissions()))));
-        }
-
         // map ldap groups to user roles, if the mapping is present
         final Set<String> translatedRoleIds = Sets.newHashSet(Sets.union(Sets.newHashSet(ldapSettings.getDefaultGroupId()),
                                                                          ldapSettings.getAdditionalDefaultGroupIds()));
@@ -233,7 +229,7 @@ public class UserServiceImpl extends PersistedServiceImpl implements UserService
             translatedRoleIds.addAll(user.getRoleIds());
         }
         user.setRoleIds(translatedRoleIds);
-
+        user.setPermissions(Collections.emptyList());
     }
 
     @Override

graylog2-server/src/test/java/org/graylog2/users/UserImplTest.java
@@ -1,0 +1,63 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.users;
+
+import com.google.common.collect.ImmutableMap;
+import org.graylog2.security.PasswordAlgorithmFactory;
+import org.graylog2.shared.security.Permissions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserImplTest {
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private PasswordAlgorithmFactory passwordAlgorithmFactory;
+
+    private UserImpl user;
+
+    @Test
+    public void getPermissionsWorksWithEmptyPermissions() throws Exception {
+        final Permissions permissions = new Permissions(Collections.emptySet());
+        final Map<String, Object> fields = Collections.singletonMap(UserImpl.USERNAME, "foobar");
+        user = new UserImpl(passwordAlgorithmFactory, permissions, fields);
+        assertThat(user.getPermissions()).containsAll(permissions.userSelfEditPermissions("foobar"));
+    }
+
+    @Test
+    public void getPermissionsReturnsListOfPermissions() throws Exception {
+        final Permissions permissions = new Permissions(Collections.emptySet());
+        final List<String> customPermissions = Collections.singletonList("subject:action");
+        final Map<String, Object> fields = ImmutableMap.of(
+            UserImpl.USERNAME, "foobar",
+            UserImpl.PERMISSIONS, customPermissions);
+        user = new UserImpl(passwordAlgorithmFactory, permissions, fields);
+        assertThat(user.getPermissions())
+            .containsAll(permissions.userSelfEditPermissions("foobar"))
+            .contains("subject:action");
+    }
+}

graylog2-server/src/test/java/org/graylog2/users/UserServiceImplTest.java
@@ -18,7 +18,6 @@ package org.graylog2.users;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
@@ -36,7 +35,10 @@ import org.graylog2.security.PasswordAlgorithmFactory;
 import org.graylog2.security.hashing.SHA1HashPasswordAlgorithm;
 import org.graylog2.shared.security.Permissions;
 import org.graylog2.shared.security.RestPermissions;
+import org.graylog2.shared.security.ldap.LdapEntry;
+import org.graylog2.shared.security.ldap.LdapSettings;
 import org.graylog2.shared.users.Role;
+import org.graylog2.shared.users.UserService;
 import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -52,6 +54,7 @@ import java.util.Map;
 
 import static com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb.InMemoryMongoRuleBuilder.newInMemoryMongoDbRule;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -63,20 +66,20 @@ public class UserServiceImplTest {
 
     private MongoConnection mongoConnection;
     private Configuration configuration;
+    private UserImpl.Factory userFactory;
     private UserServiceImpl userService;
+    private Permissions permissions;
     @Mock
     private RoleService roleService;
     @Mock
     private InMemoryRolePermissionResolver permissionsResolver;
-    @Mock
-    private Permissions permissions;
 
     @Before
     public void setUp() throws Exception {
         this.mongoConnection = mongoRule.getMongoConnection();
         this.configuration = new Configuration();
-        final UserImpl.Factory userFactory = new UserImplFactory(configuration);
-        final Permissions permissions = new Permissions(ImmutableSet.of(new RestPermissions()));
+        this.userFactory = new UserImplFactory(configuration);
+        this.permissions = new Permissions(ImmutableSet.of(new RestPermissions()));
         this.userService = new UserServiceImpl(mongoConnection, configuration, roleService, userFactory, permissions, permissionsResolver);
 
         when(roleService.getAdminRoleObjectId()).thenReturn("deadbeef");
@@ -86,6 +89,7 @@ public class UserServiceImplTest {
     @UsingDataSet(loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void testLoad() throws Exception {
         final User user = userService.load("user1");
+        assertThat(user).isNotNull();
         assertThat(user.getName()).isEqualTo("user1");
         assertThat(user.getEmail()).isEqualTo("user1@example.com");
     }
@@ -201,17 +205,40 @@ public class UserServiceImplTest {
 
     @Test
     public void testGetPermissionsForUser() throws Exception {
+        final InMemoryRolePermissionResolver permissionResolver = mock(InMemoryRolePermissionResolver.class);
+        final UserService userService = new UserServiceImpl(mongoConnection, configuration, roleService, userFactory, permissions, permissionResolver);
+
         final UserImplFactory factory = new UserImplFactory(new Configuration());
         final UserImpl user = factory.create(new HashMap<>());
+        user.setName("user");
         final Role role = createRole("Foo");
 
-        user.setRoleIds(Sets.newHashSet(role.getId()));
-        user.setPermissions(Lists.newArrayList("hello:world"));
+        user.setRoleIds(Collections.singleton(role.getId()));
+        user.setPermissions(Collections.singletonList("hello:world"));
 
+        when(permissionResolver.resolveStringPermission(role.getId())).thenReturn(Collections.singleton("foo:bar"));
 
-        when(permissionsResolver.resolveStringPermission(role.getId())).thenReturn(Sets.newHashSet("foo:bar"));
-        when(permissions.userSelfEditPermissions(user.getName())).thenReturn(Sets.newHashSet("foo:baz"));
+        assertThat(userService.getPermissionsForUser(user)).containsOnly("users:passwordchange:user", "users:edit:user", "foo:bar", "hello:world");
+    }
 
-        assertThat(userService.getPermissionsForUser(user)).containsOnly("foo:bar", "hello:world", "foo:baz");
+    @Test
+    @UsingDataSet(loadStrategy = LoadStrategyEnum.DELETE_ALL)
+    public void testSyncFromLdapEntry() {
+        final LdapEntry userEntry = new LdapEntry();
+        final LdapSettings ldapSettings = mock(LdapSettings.class);
+        when(ldapSettings.getDisplayNameAttribute()).thenReturn("displayName");
+        when(ldapSettings.getDefaultGroupId()).thenReturn("54e3deadbeefdeadbeef0001");
+        when(ldapSettings.getAdditionalDefaultGroupIds()).thenReturn(Collections.emptySet());
+
+        final User ldapUser = userService.syncFromLdapEntry(userEntry, ldapSettings, "user");
+
+        assertThat(ldapUser).isNotNull();
+        assertThat(ldapUser.isExternalUser()).isTrue();
+        assertThat(ldapUser.getName()).isEqualTo("user");
+        assertThat(ldapUser.getEmail()).isEqualTo("user@localhost");
+        assertThat(ldapUser.getHashedPassword()).isEqualTo("User synced from LDAP.");
+        assertThat(ldapUser.getTimeZone()).isEqualTo(DateTimeZone.UTC);
+        assertThat(ldapUser.getRoleIds()).containsOnly("54e3deadbeefdeadbeef0001");
+        assertThat(ldapUser.getPermissions()).isNotEmpty();
     }
 }

commit 9c21c825c395106aac3c11f8b028a5c5fc1f9392

Ensure login failure by default
Always return `null` by default in LdapUserAuthenticator#doGetAuthenticationInfo()
to make it harder to accidentally login the user even if the password is
wrong or some other error occurs.

Fixes a security issue where all passwords have been accepted when
using LDAP. This issue has been introduced in commit 9b746fa one day ago
and has never been released outside of snapshots.

Fixes #2069

graylog2-server/src/main/java/org/graylog2/security/realm/LdapUserAuthenticator.java
@@ -119,12 +119,12 @@ public class LdapUserAuthenticator extends AuthenticatingRealm {
                 LOG.error("Unable to sync LDAP user {} (DN {})", userEntry.getBindPrincipal(), userEntry.getDn());
                 return null;
             }
+
+            return new SimpleAccount(principal, null, "ldap realm");
         } catch (LdapException e) {
             LOG.error("LDAP error", e);
-            return null;
         } catch (CursorException e) {
             LOG.error("Unable to read LDAP entry", e);
-            return null;
         } catch (Exception e) {
             LOG.error("Error during LDAP user account sync. Cannot log in user {}", principal, e);
         } finally {
@@ -136,7 +136,9 @@ public class LdapUserAuthenticator extends AuthenticatingRealm {
                 }
             }
         }
-        return new SimpleAccount(principal, null, "ldap realm");
+
+        // Return null by default to ensure a login failure if anything goes wrong.
+        return null;
     }
 
     public boolean isEnabled() {

graylog2-server/src/test/java/org/graylog2/security/realm/LdapUserAuthenticatorTest.java
@@ -1,0 +1,144 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.security.realm;
+
+import org.apache.directory.ldap.client.api.LdapConnectionConfig;
+import org.apache.directory.server.annotations.CreateLdapServer;
+import org.apache.directory.server.annotations.CreateTransport;
+import org.apache.directory.server.core.annotations.ApplyLdifFiles;
+import org.apache.directory.server.core.annotations.ContextEntry;
+import org.apache.directory.server.core.annotations.CreateDS;
+import org.apache.directory.server.core.annotations.CreateIndex;
+import org.apache.directory.server.core.annotations.CreatePartition;
+import org.apache.directory.server.core.annotations.LoadSchema;
+import org.apache.directory.server.core.integ.AbstractLdapTestUnit;
+import org.apache.directory.server.core.integ.FrameworkRunner;
+import org.apache.directory.server.core.partition.impl.avl.AvlPartition;
+import org.apache.directory.server.ldap.LdapServer;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.graylog2.Configuration;
+import org.graylog2.plugin.database.users.User;
+import org.graylog2.security.ldap.LdapConnector;
+import org.graylog2.security.ldap.LdapSettingsImpl;
+import org.graylog2.security.ldap.LdapSettingsService;
+import org.graylog2.shared.security.ldap.LdapEntry;
+import org.graylog2.shared.security.ldap.LdapSettings;
+import org.graylog2.shared.users.UserService;
+import org.graylog2.users.RoleService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(FrameworkRunner.class)
+@CreateLdapServer(transports = {
+        @CreateTransport(protocol = "LDAP")
+})
+@CreateDS(
+        name = "LdapConnectorTest",
+        partitions = {
+                @CreatePartition(
+                        name = "example.com",
+                        type = AvlPartition.class,
+                        suffix = "dc=example,dc=com",
+                        contextEntry = @ContextEntry(
+                                entryLdif = "dn: dc=example,dc=com\n" +
+                                        "dc: example\n" +
+                                        "objectClass: top\n" +
+                                        "objectClass: domain\n\n"
+
+                        ),
+                        indexes = {
+                                @CreateIndex(attribute = "objectClass"),
+                                @CreateIndex(attribute = "dc"),
+                                @CreateIndex(attribute = "ou")
+                        }
+
+                )
+        },
+        loadedSchemas = {
+                @LoadSchema(name = "nis", enabled = true)
+        }
+)
+@ApplyLdifFiles("org/graylog2/security/ldap/base.ldif")
+public class LdapUserAuthenticatorTest extends AbstractLdapTestUnit {
+    private static final String ADMIN_DN = "uid=admin,ou=system";
+    private static final String ADMIN_PASSWORD = "secret";
+
+    private static final AuthenticationToken VALID_TOKEN = new UsernamePasswordToken("john", "test");
+    private static final AuthenticationToken INVALID_TOKEN = new UsernamePasswordToken("john", "__invalid__");
+    private static final String PASSWORD_SECRET = "r8Om85b0zgHmiGsK86T3ZFlmSIdMd3hcKmOa4T60MSPEobfRCTLNOK4T91GdHbGx";
+
+    private LdapConnector ldapConnector;
+    private LdapServer server;
+    private LdapSettingsService ldapSettingsService;
+    private LdapSettings ldapSettings;
+    private Configuration configuration;
+    private UserService userService;
+
+    @Before
+    public void setUp() throws Exception {
+        server = getLdapServer();
+        final LdapConnectionConfig ldapConfig = new LdapConnectionConfig();
+
+        ldapConfig.setLdapHost("localHost");
+        ldapConfig.setLdapPort(server.getPort());
+        ldapConfig.setName(ADMIN_DN);
+        ldapConfig.setCredentials(ADMIN_PASSWORD);
+
+        configuration = mock(Configuration.class);
+        when(configuration.getPasswordSecret()).thenReturn(PASSWORD_SECRET);
+
+        ldapConnector = new LdapConnector(10000);
+        ldapSettingsService = mock(LdapSettingsService.class);
+        userService = mock(UserService.class);
+
+        ldapSettings = new LdapSettingsImpl(configuration, mock(RoleService.class));
+        ldapSettings.setEnabled(true);
+        ldapSettings.setUri(URI.create("ldap://localhost:" + server.getPort()));
+        ldapSettings.setUseStartTls(false);
+        ldapSettings.setSystemUsername(ADMIN_DN);
+        ldapSettings.setSystemPassword(ADMIN_PASSWORD);
+        ldapSettings.setSearchBase("ou=users,dc=example,dc=com");
+        ldapSettings.setSearchPattern("(&(objectClass=posixAccount)(uid={0}))");
+        ldapSettings.setDisplayNameAttribute("cn");
+        ldapSettings.setActiveDirectory(false);
+        ldapSettings.setGroupSearchBase("ou=groups,dc=example,dc=com");
+        ldapSettings.setGroupIdAttribute("cn");
+        ldapSettings.setGroupSearchPattern("(|(objectClass=groupOfNames)(objectClass=posixGroup))");
+    }
+
+    @Test
+    public void testDoGetAuthenticationInfo() throws Exception {
+        final LdapUserAuthenticator authenticator = new LdapUserAuthenticator(ldapConnector, ldapSettingsService, userService);
+
+        when(ldapSettingsService.load())
+                .thenReturn(ldapSettings);
+        when(userService.syncFromLdapEntry(any(LdapEntry.class), any(LdapSettings.class), any(String.class)))
+                .thenReturn(mock(User.class));
+
+        assertThat(authenticator.doGetAuthenticationInfo(VALID_TOKEN)).isNotNull();
+        assertThat(authenticator.doGetAuthenticationInfo(INVALID_TOKEN)).isNull();
+    }
+}