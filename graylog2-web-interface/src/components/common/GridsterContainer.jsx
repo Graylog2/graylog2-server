9dc3a3799363776c1b1cff69338aca596be66375
Edmundo Alvarez <edmundo@graylog.com>
2016-04-15 17:20:32 +0200

    Use a constant layout for dashboards
    So far we tried to make the dashboard layout to be responsive with the
    screen. This was never really working, as widgets were dancing around
    inconsistently. Trying to manage gridster from react made this even
    worse, as now it is harder to reset gridster or to make sense when the
    DOM is changing.
    
    Instead of trying to unsuccessfully do that, these changes are going in
    the opposite direction: Users can unlock the dashboard and arrange
    widgets as desired, and that arrangement will be keep in the future,
    regardless on the screen size. Users can still scroll through the widgets
    or re-arrange them if they want to, but we won't try to be smart about
    it.

--- graylog2-web-interface/src/components/common/GridsterContainer.jsx
+++ graylog2-web-interface/src/components/common/GridsterContainer.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
 import { GridsterWidget } from 'components/common';
@@ -12,21 +12,40 @@ const GridsterContainer = React.createClass({
     children: PropTypes.node.isRequired,
     onPositionsChange: PropTypes.func.isRequired,
   },
+
   getInitialState() {
     return { grid: undefined };
   },
+
   componentDidMount() {
+    this._initializeDashboard();
+  },
+
+  WIDGET_WIDTH: 410,
+  WIDGET_HEIGHT: 200,
+
+  _initializeDashboard() {
     const rootNode = ReactDOM.findDOMNode(this.refs.gridster);
     const grid = this._initGridster(rootNode);
 
     this._lockGrid(grid);
 
-    this.setState({grid: grid});
+    this.setState({ grid: grid });
   },
+
   _initGridster(rootNode) {
+    const minColumns = Object.keys(this.props.positions).reduce((max, current) => {
+      const position = this.props.positions[current];
+      // Column may be set to 0 as default, but the minimum effective column is 1
+      const effectiveColumn = Math.max(position.col, 1);
+      return Math.max(max, effectiveColumn + position.width - 1);
+    }, 0);
+
     return $(rootNode).gridster({
       widget_margins: [10, 10],
-      widget_base_dimensions: [410, 200],
+      widget_base_dimensions: [this.WIDGET_WIDTH, this.WIDGET_HEIGHT],
+      autogrow_cols: true,
+      min_cols: minColumns,
       resize: {
         enabled: true,
         stop: this._onPositionsChange,
@@ -63,14 +82,14 @@ const GridsterContainer = React.createClass({
   },
   _onPositionsChange() {
     const positions = this.state.grid.serialize().map((position) => {
-      return {id: position.id, col: position.col, row: position.row, width: position.size_x, height: position.size_y};
+      return { id: position.id, col: position.col, row: position.row, width: position.size_x, height: position.size_y };
     });
     this.props.onPositionsChange(positions);
   },
 
   render() {
     const children = (this.state.grid && React.Children.map(this.props.children, (child) => {
-      const position = this.props.positions[child.props.id] || {row: 0, col: 0, width: 1, height: 1};
+      const position = this.props.positions[child.props.id];
 
       return (
         <GridsterWidget grid={this.state.grid} position={position}>
