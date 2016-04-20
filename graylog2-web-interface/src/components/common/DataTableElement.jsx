e1bab12c211b53a916aa9fd2b48ef6cdb678ba7e
Kay Roepke <kroepke@googlemail.com>
2016-04-20 14:44:17 +0200

    when a rule could not be resolved, fall back to the rule reference name without a link
    DataTable and DataTableElement pass the current iteration index into the header and row formatter functions now (as the second parameter)
    This allows formatters to pick render the number or to pick an alternative from a secondary source
    
    fixes Graylog2/graylog-plugin-pipeline-processor#11

--- graylog2-web-interface/src/components/common/DataTableElement.jsx
+++ graylog2-web-interface/src/components/common/DataTableElement.jsx
@@ -4,9 +4,10 @@ const DataTableElement = React.createClass({
   propTypes: {
     element: React.PropTypes.any,
     formatter: React.PropTypes.func.isRequired,
+    index: React.PropTypes.number,
   },
   render() {
-    return this.props.formatter(this.props.element);
+    return this.props.formatter(this.props.element, this.props.index);
   },
 });
 
