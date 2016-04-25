fb4e966ad7050922153406121a76a7576fcc83e9
Edmundo Alvarez <edmundo@graylog.com>
2016-04-25 19:45:46 +0200

    Use same name as java metadata

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig;
 const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
 
-module.exports = new PluginWebpackConfig('org.graylog.plugins.enterprise_integration.EnterpriseIntegration', loadBuildConfig(path.resolve(__dirname, './build.config')), {
+module.exports = new PluginWebpackConfig('org.graylog.plugins.enterprise_integration.EnterpriseIntegrationPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
   loaders: [
     { test: /\.ts$/, loader: 'babel-loader!ts-loader', exclude: /node_modules|\.node_cache/ },
   ],
