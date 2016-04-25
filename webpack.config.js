9c9eb06289d0292135e1caadddbaf5f58c6e3aff
Edmundo Alvarez <edmundo@graylog.com>
2016-04-25 19:35:20 +0200

    Allow loading typescript files

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,10 @@ const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
 
 module.exports = new PluginWebpackConfig('org.graylog.plugins.enterprise_integration.EnterpriseIntegration', loadBuildConfig(path.resolve(__dirname, './build.config')), {
-  // Here goes your additional webpack configuration.
+  loaders: [
+    { test: /\.ts$/, loader: 'babel-loader!ts-loader', exclude: /node_modules|\.node_cache/ },
+  ],
+  resolve: {
+    extensions: ['.ts'],
+  },
 });
