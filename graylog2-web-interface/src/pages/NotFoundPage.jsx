0ea2700900821d4d98c451850de48bc80314e46d
Edmundo Alvarez <github@edmundoa.com>
2016-04-26 12:03:04 +0200

    Add 404 page (#2139) (cherry picked from commit 2d993acf6f32b3ec3ef5c545130033dc9a62110e)

---
+++ graylog2-web-interface/src/pages/NotFoundPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Col, Jumbotron, Row } from 'react-bootstrap';
+
+import style from '!style/useable!css!./NotFoundPage.css';
+
+const NotFoundPage = React.createClass({
+  componentDidMount() {
+    style.use();
+  },
+
+  componentWillUnmount() {
+    style.unuse();
+  },
+
+  render() {
+    return (
+      <Row className="jumbotron-container">
+        <Col mdOffset={2} md={8}>
+          <Jumbotron>
+            <h1>404 - Page not found</h1>
+            <p>The party gorilla was just here, but had another party to rock.</p>
+            <p>Oh, party gorilla! How we miss you! Will we ever see you again?</p>
+          </Jumbotron>
+        </Col>
+      </Row>
+    );
+  },
+});
+
+export default NotFoundPage;
