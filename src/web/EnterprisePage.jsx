70c6f957e71884c2a8d49be00a4333004bc2f0ae
Bernd Ahlers <bernd@graylog.com>
2016-05-10 15:56:04 +0200

    Show cluster id and node count and include in enterprise link
    (cherry picked from commit 0272f8f1bc8921b22d1b765c363a45e1738d6a41)

--- src/web/EnterprisePage.jsx
+++ src/web/EnterprisePage.jsx
@@ -1,23 +1,74 @@
 import React from 'react';
-import { PageHeader } from 'components/common';
+import Reflux from 'reflux';
+import { Row, Col } from 'react-bootstrap';
+import { PageHeader, Spinner } from 'components/common';
 import PluginList from './PluginList';
 
+import StoreProvider from 'injection/StoreProvider';
+import ActionsProvider from 'injection/ActionsProvider';
+
+const NodesStore = StoreProvider.getStore('Nodes');
+const NodesActions = ActionsProvider.getActions('Nodes');
+
 const EnterprisePage = React.createClass({
+  mixins: [Reflux.connect(NodesStore)],
+
+  componentDidMount() {
+    NodesActions.list();
+  },
+
+  _clusterId() {
+    if (this.state.nodes) {
+      return Object.keys(this.state.nodes).map(id => this.state.nodes[id]).map(node => node.cluster_id)[0].toUpperCase();
+    }
+    return null;
+  },
+
+  _numberOfNodes() {
+    if (this.state.nodes) {
+      return Object.keys(this.state.nodes).length;
+    }
+
+    return null;
+  },
+
   render() {
+    let orderLink = 'https://www.graylog.org/enterprise';
+    let numberOfNodes = <Spinner />;
+    let clusterId = <Spinner />;
+
+    if (this.state.nodes) {
+      orderLink = `https://www.graylog.org/enterprise?cid=${this._clusterId()}&nodes=${this._numberOfNodes()}`;
+      numberOfNodes = this._numberOfNodes();
+      clusterId = this._clusterId();
+    }
+
     return (
       <div>
         <PageHeader title="Graylog Enterprise">
           {null}
-          
+
           <span>
             Graylog Enterprise adds commercial functionality to the Open Source Graylog core. You can learn more
-            about Graylog Enterprise and order a license on the <a href="https://www.graylog.org/enterprise" target="_blank">product page</a>.
+            about Graylog Enterprise and order a license on the <a href={orderLink} target="_blank">product page</a>.
           </span>
-          
+
           <span>
-            <a className="btn btn-lg btn-success" href="https://www.graylog.org/enterprise" target="_blank">Order a license</a>
+            <a className="btn btn-lg btn-success" href={orderLink} target="_blank">Order a license</a>
           </span>
         </PageHeader>
+
+        <Row className="content">
+          <Col md={12}>
+            <h2 style={{ marginBottom: 10 }}>Cluster Details</h2>
+            <dl style={{ marginBottom: 0 }}>
+              <dt>Cluster ID</dt>
+              <dd>{clusterId}</dd>
+              <dt>Number of nodes</dt>
+              <dd>{numberOfNodes}</dd>
+            </dl>
+          </Col>
+        </Row>
 
         <PluginList/>
       </div>
