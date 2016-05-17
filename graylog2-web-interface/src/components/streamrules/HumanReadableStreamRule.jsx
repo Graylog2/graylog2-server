00ff8782a969f353c89cd81874978ca6bb3d534a
Dennis Oelkers <dennis@graylog.com>
2016-05-17 12:46:29 +0200

    Fixing linter hints for HumanReadableStreamRule.

--- graylog2-web-interface/src/components/streamrules/HumanReadableStreamRule.jsx
+++ graylog2-web-interface/src/components/streamrules/HumanReadableStreamRule.jsx
@@ -1,45 +1,45 @@
-'use strict';
+import React from 'react';
 
-var React = require('react');
-
-var HumanReadableStreamRule = React.createClass({
-    FIELD_PRESENCE_RULE_TYPE: 5,
-    _getTypeForInteger(type, streamRuleTypes) {
-        if (streamRuleTypes) {
-            return streamRuleTypes.filter((streamRuleType) => {
-                return String(streamRuleType.id) === String(type);
-            })[0];
-        } else {
-            return undefined;
-        }
-    },
-    _formatRuleValue(streamRule) {
-        if (String(streamRule.type) !== String(this.FIELD_PRESENCE_RULE_TYPE)) {
-            if (streamRule.value) {
-                return streamRule.value;
-            } else {
-                return "<empty>";
-            }
-        } else {
-            return null;
-        }
-    },
-    _formatRuleField(streamRule) {
-        if (streamRule.field) {
-            return streamRule.field;
-        } else {
-            return "<empty>";
-        }
-    },
-    render() {
-        var streamRule = this.props.streamRule;
-        var streamRuleType = this._getTypeForInteger(streamRule.type, this.props.streamRuleTypes);
-        var negation = (streamRule.inverted ? "not " : null);
-        var longDesc = (streamRuleType ? streamRuleType.long_desc : null);
-        return (
-            <span><em>{this._formatRuleField(streamRule)}</em> must {negation}{longDesc} <em>{this._formatRuleValue(streamRule)}</em></span>
-        );
+const HumanReadableStreamRule = React.createClass({
+  propTypes: {
+    streamRule: React.PropTypes.object.isRequired,
+    streamRuleTypes: React.PropTypes.array.isRequired,
+  },
+  EMPTY_TAG: '<empty>',
+  FIELD_PRESENCE_RULE_TYPE: 5,
+  _getTypeForInteger(type, streamRuleTypes) {
+    if (streamRuleTypes) {
+      return streamRuleTypes.filter((streamRuleType) => {
+        return String(streamRuleType.id) === String(type);
+      })[0];
     }
+    return undefined;
+  },
+  _formatRuleValue(streamRule) {
+    if (String(streamRule.type) !== String(this.FIELD_PRESENCE_RULE_TYPE)) {
+      if (streamRule.value) {
+        return streamRule.value;
+      }
+      return this.EMPTY_TAG;
+    }
+
+    return null;
+  },
+  _formatRuleField(streamRule) {
+    if (streamRule.field) {
+      return streamRule.field;
+    }
+    return this.EMPTY_TAG;
+  },
+  render() {
+    const streamRule = this.props.streamRule;
+    const streamRuleType = this._getTypeForInteger(streamRule.type, this.props.streamRuleTypes);
+    const negation = (streamRule.inverted ? 'not ' : null);
+    const longDesc = (streamRuleType ? streamRuleType.long_desc : null);
+    return (
+      <span><em>{this._formatRuleField(streamRule)}</em> must {negation}{longDesc} <em>{this._formatRuleValue(streamRule)}</em></span>
+    );
+  },
 });
 
-module.exports = HumanReadableStreamRule;
+export default HumanReadableStreamRule;
