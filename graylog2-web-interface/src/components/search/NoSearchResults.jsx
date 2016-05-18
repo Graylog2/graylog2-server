ac2b4efc8d71ad6e79d17520e0f07f9ad003e6f8
Edmundo Alvarez <edmundo@graylog.com>
2016-05-18 15:38:03 +0200

    Improve text in no results page

--- graylog2-web-interface/src/components/search/NoSearchResults.jsx
+++ graylog2-web-interface/src/components/search/NoSearchResults.jsx
@@ -36,12 +36,15 @@ const NoSearchResults = React.createClass({
             </h1>
 
             <p className="description">
-              Your search returned no results.&nbsp;
-              <a href="#" onClick={this._showQueryModal}>Show the Elasticsearch query.</a>
-              <ShowQueryModal key="debugQuery" ref="showQueryModal" builtQuery={this.props.builtQuery}/>
-              <strong>&nbsp;Take a look at the&nbsp;<DocumentationLink page={DocsHelper.PAGES.SEARCH_QUERY_LANGUAGE}
-                                                                       text="documentation"/>
-                &nbsp;if you need help with the search syntax.</strong>
+              Your search returned no results, try changing the used time range or the search query.{' '}
+              Do you want more details? <a href="#" onClick={this._showQueryModal}>Show the Elasticsearch query</a>.
+              <ShowQueryModal key="debugQuery" ref="showQueryModal" builtQuery={this.props.builtQuery} />
+              <br />
+              <strong>
+                Take a look at the{' '}
+                <DocumentationLink page={DocsHelper.PAGES.SEARCH_QUERY_LANGUAGE} text="documentation" />{' '}
+                if you need help with the search syntax or the time range selector.
+              </strong>
             </p>
           </Col>
         </Row>
@@ -49,18 +52,24 @@ const NoSearchResults = React.createClass({
           <Col md={12}>
             <div className="support-sources">
               <h2>Need help?</h2>
-              Do not hesitate to consult the Graylog community if your questions are not answered in the&nbsp;
-              <DocumentationLink page={DocsHelper.PAGES.WELCOME} text="documentation"/>.
+              <p>
+                Do not hesitate to consult the Graylog community if your questions are not answered in the{' '}
+                <DocumentationLink page={DocsHelper.PAGES.WELCOME} text="documentation" />.
+              </p>
 
               <ul>
-                <li><i className="fa fa-group" /> <a href="https://www.graylog.org/community-support/"
-                                                     target="_blank">Community support</a></li>
-                <li><i className="fa fa-github-alt" /> <a
-                  href="https://github.com/Graylog2/graylog2-server/issues" target="_blank">Issue tracker</a>
+                <li>
+                  <i className="fa fa-group"/>&nbsp;
+                  <a href="https://www.graylog.org/community-support/" target="_blank">Community support</a>
                 </li>
-                <li><i className="fa fa-heart" /> <a href="https://www.graylog.org/professional-support" target="_blank">
-                  Professional support
-                </a></li>
+                <li>
+                  <i className="fa fa-github-alt"/>&nbsp;
+                  <a href="https://github.com/Graylog2/graylog2-server/issues" target="_blank">Issue tracker</a>
+                </li>
+                <li>
+                  <i className="fa fa-heart"/>&nbsp;
+                  <a href="https://www.graylog.org/professional-support" target="_blank">Professional support</a>
+                </li>
               </ul>
             </div>
 
