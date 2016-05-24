d2fcddba1d97e8158c21b7bb6ba28e10e8a84c2b
Edmundo Alvarez <github@edmundoa.com>
2016-05-24 11:30:02 +0200

    Fix issues with old dashboards (#2262)
    * Always render quickvalues data table by default
    
    Quickvalues widgets created before introducing pie charts may not render
    neither a data table, nor a pie chart. In this commit we ensure we
    always render the data table by default.
    
    Refs #2163
    
    * Fix typo
    
    * Be more picky getting widgets dimensions
    
    Dashboards created in old Graylog versions may have a positions object
    that does not contain widget width or height. Instead of relying on what
    that positions object had, this commit ensures each component of the
    widget position is initialized with the persisted value or its default.
    
    Refs #2163

--- graylog2-web-interface/src/pages/ShowDashboardPage.jsx
+++ graylog2-web-interface/src/pages/ShowDashboardPage.jsx
@@ -79,7 +79,7 @@ const ShowDashboardPage = React.createClass({
       dimensions.height = widgetPlugin.defaultHeight;
       dimensions.width = widgetPlugin.defaultWidth;
     } else {
-      dimensions.heigh = this.DEFAULT_HEIGHT;
+      dimensions.height = this.DEFAULT_HEIGHT;
       dimensions.width = this.DEFAULT_WIDTH;
     }
 
@@ -95,7 +95,14 @@ const ShowDashboardPage = React.createClass({
 
     const positions = {};
     dashboard.widgets.forEach(widget => {
-      positions[widget.id] = dashboard.positions[widget.id] || this._defaultWidgetDimensions(widget);
+      const persistedDimensions = dashboard.positions[widget.id] || {};
+      const defaultDimensions = this._defaultWidgetDimensions(widget);
+      positions[widget.id] = {
+        col: (persistedDimensions.col === undefined ? defaultDimensions.col : persistedDimensions.col),
+        row: (persistedDimensions.row === undefined ? defaultDimensions.row : persistedDimensions.row),
+        height: (persistedDimensions.height === undefined ? defaultDimensions.height : persistedDimensions.height),
+        width: (persistedDimensions.width === undefined ? defaultDimensions.width : persistedDimensions.width),
+      };
     });
 
     const widgets = dashboard.widgets.sort((widget1, widget2) => {
