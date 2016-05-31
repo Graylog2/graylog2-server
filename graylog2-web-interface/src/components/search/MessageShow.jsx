7d3e4ea98cd82da2923d8a5c14a2f36fe999ea88
Edmundo Alvarez <edmundo@graylog.com>
2016-05-31 17:19:34 +0200

    Ensure props passed to MessageShow use Immutable.js
    As we don't use Immutable.js in new components, convert received props
    to not break old components used by MessageShow.

--- graylog2-web-interface/src/components/search/MessageShow.jsx
+++ graylog2-web-interface/src/components/search/MessageShow.jsx
@@ -1,13 +1,30 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import Immutable from 'immutable';
 import MessageDetail from './MessageDetail';
 
 const MessageShow = React.createClass({
   propTypes: {
-    message: PropTypes.object,
-    inputs: PropTypes.object,
-    streams: PropTypes.object,
-    nodes: PropTypes.object,
+    message: React.PropTypes.object,
+    inputs: React.PropTypes.object,
+    streams: React.PropTypes.object,
+    nodes: React.PropTypes.object,
   },
+
+  getInitialState() {
+    return this._getImmutableProps(this.props);
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(this._getImmutableProps(nextProps));
+  },
+
+  _getImmutableProps(props) {
+    return {
+      streams: props.streams ? Immutable.Map(props.streams) : props.streams,
+      nodes: props.nodes ? Immutable.Map(props.nodes) : props.nodes,
+    };
+  },
+
   possiblyHighlight(fieldName) {
     // No highlighting for the message details view.
     return this.props.message.fields[fieldName];
@@ -18,8 +35,8 @@ const MessageShow = React.createClass({
         <div className="col-md-12">
           <MessageDetail {...this.props} message={this.props.message}
                                          inputs={this.props.inputs}
-                                         streams={this.props.streams}
-                                         nodes={this.props.nodes}
+                                         streams={this.state.streams}
+                                         nodes={this.state.nodes}
                                          possiblyHighlight={this.possiblyHighlight}
                                          showTimestamp/>
         </div>
