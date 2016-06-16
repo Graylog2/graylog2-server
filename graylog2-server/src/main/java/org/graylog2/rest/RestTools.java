11aedb8911bcbee5da087830e7f843dd5cdcf609
Jochen Schalanda <jochen@graylog.com>
2016-06-16 13:15:19 +0200

    Fix base URI for API documentation
    Refs #2360

--- graylog2-server/src/main/java/org/graylog2/rest/RestTools.java
+++ graylog2-server/src/main/java/org/graylog2/rest/RestTools.java
@@ -16,13 +16,20 @@
  */
 package org.graylog2.rest;
 
+import com.google.common.base.Strings;
 import org.graylog2.shared.security.ShiroPrincipal;
 import org.graylog2.shared.security.ShiroSecurityContext;
 
 import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.SecurityContext;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 public class RestTools {
     @Nullable
@@ -43,5 +50,33 @@ public class RestTools {
         final ShiroPrincipal shiroPrincipal = (ShiroPrincipal) userPrincipal;
 
         return shiroPrincipal.getName();
+    }
+
+    public static String buildEndpointUri(@NotNull HttpHeaders httpHeaders, @NotNull URI defaultEndpointUri) {
+        Optional<String> endpointUri = Optional.empty();
+        final List<String> headers = httpHeaders.getRequestHeader("X-Graylog-Server-URL");
+        if (headers != null && !headers.isEmpty()) {
+            endpointUri = headers.stream().filter(s -> {
+                try {
+                    if (Strings.isNullOrEmpty(s)) {
+                        return false;
+                    }
+                    final URI uri = new URI(s);
+                    if (!uri.isAbsolute()) {
+                        return true;
+                    }
+                    switch (uri.getScheme()) {
+                        case "http":
+                        case "https":
+                            return true;
+                    }
+                    return false;
+                } catch (URISyntaxException e) {
+                    return false;
+                }
+            }).findFirst();
+        }
+
+        return endpointUri.orElse(defaultEndpointUri.toString());
     }
 }
