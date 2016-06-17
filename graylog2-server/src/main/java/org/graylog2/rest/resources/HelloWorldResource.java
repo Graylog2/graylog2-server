e19fac36419c3b52ebf5c7b663eb4a091ea9cb51
Jochen Schalanda <jochen@schalanda.name>
2016-06-17 14:52:40 +0200

    Add handler for / in the Graylog REST API (#2377)
    * Add handler for / in the Graylog REST API
    
    * Remove @JsonProperty annotation from HelloWorldResponse#create()

---
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/HelloWorldResource.java
@@ -0,0 +1,63 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.rest.resources;
+
+import com.codahale.metrics.annotation.Timed;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.graylog2.plugin.Version;
+import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.graylog2.plugin.cluster.ClusterId;
+import org.graylog2.plugin.system.NodeId;
+import org.graylog2.rest.models.HelloWorldResponse;
+import org.graylog2.shared.rest.resources.RestResource;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import static java.util.Objects.requireNonNull;
+
+@Api(value = "Hello World", description = "A friendly hello world message")
+@Path("/")
+public class HelloWorldResource extends RestResource {
+    private final NodeId nodeId;
+    private final ClusterConfigService clusterConfigService;
+
+    @Inject
+    public HelloWorldResource(NodeId nodeId, ClusterConfigService clusterConfigService) {
+        this.nodeId = requireNonNull(nodeId);
+        this.clusterConfigService = requireNonNull(clusterConfigService);
+    }
+
+    @GET
+    @Timed
+    @ApiOperation(value = "A few details about the Graylog node.")
+    @Produces(MediaType.APPLICATION_JSON)
+    public HelloWorldResponse helloWorld() {
+        final ClusterId clusterId = clusterConfigService.getOrDefault(ClusterId.class, ClusterId.create("UNKNOWN"));
+        return HelloWorldResponse.create(
+            clusterId.clusterId(),
+            nodeId.toString(),
+            Version.CURRENT_CLASSPATH.toString(),
+            "Manage your logs in the dark and have lasers going and make it look like you're from space!"
+        );
+    }
+}
