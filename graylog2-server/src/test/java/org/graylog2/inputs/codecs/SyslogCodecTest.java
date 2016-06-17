e3249f8f3b297ecdf40783e9d7e7008dcc930deb
Jochen Schalanda <jochen@graylog.com>
2016-06-17 13:17:02 +0200

    Add test cases from RFC 3164 and RFC 5242 to SyslogCodecTest
    Refs #2374

--- graylog2-server/src/test/java/org/graylog2/inputs/codecs/SyslogCodecTest.java
+++ graylog2-server/src/test/java/org/graylog2/inputs/codecs/SyslogCodecTest.java
@@ -18,6 +18,7 @@ package org.graylog2.inputs.codecs;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
+import com.google.common.collect.ImmutableMap;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.configuration.Configuration;
@@ -32,7 +33,9 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.net.InetSocketAddress;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -204,6 +207,114 @@ public class SyslogCodecTest {
         assertEquals(message.getField("level"), 5);
         assertEquals(message.getField("facility"), "syslogd");
         assertEquals(message.getField("full_message"), UNSTRUCTURED);
+    }
+
+
+    @Test
+    public void rfc3164_section5_4_messages() {
+        // See https://tools.ietf.org/html/rfc3164#section-5.4
+        final Map<String, Map<String, Object>> rfc3164messages = ImmutableMap.of(
+            "<34>Oct 11 22:14:15 mymachine su: 'su root' failed for lonvick on /dev/pts/8",
+            ImmutableMap.of(
+                "timestamp", new DateTime(YEAR + "-10-11T22:14:15"),
+                "source", "mymachine",
+                "level", 2,
+                "facility", "security/authorization",
+                "message", "mymachine su: 'su root' failed for lonvick on /dev/pts/8"),
+
+            "<13>Feb  5 17:32:18 10.0.0.99 Use the BFG!",
+            ImmutableMap.of(
+                "timestamp", new DateTime(YEAR + "-02-05T17:32:18"),
+                "source", "10.0.0.99",
+                "level", 5,
+                "facility", "user-level",
+                "message", "10.0.0.99 Use the BFG!")
+
+            /* FAILING
+            "<165>Aug 24 05:34:00 CST 1987 mymachine myproc[10]: %% It's time to make the do-nuts.  %%  Ingredients: Mix=OK, Jelly=OK # Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: Conveyer1=OK, Conveyer2=OK # %%",
+            ImmutableMap.of(
+                "timestamp", new DateTime("1987-08-24T05:34:00", DateTimeZone.forID("CST6CDT")),
+                "source", "mymachine",
+                "level", 5,
+                "facility", "local4"
+            )
+            */
+
+            /* FAILING
+            "<0>1990 Oct 22 10:52:01 TZ-6 scapegoat.dmz.example.org 10.1.2.3 sched[0]: That's All Folks!",
+            ImmutableMap.of(
+                "timestamp", new DateTime("1990-10-22T10:52:01", DateTimeZone.forID("Etc/GMT-6")),
+                "source", "scapegoat.dmz.example.org",
+                "level", 0,
+                "facility", "kernel"
+            )
+            */
+        );
+
+        for (Map.Entry<String, Map<String, Object>> entry : rfc3164messages.entrySet()) {
+            final Message message = codec.decode(buildRawMessage(entry.getKey()));
+            assertThat(message).isNotNull();
+            assertThat(message.getFields()).containsAllEntriesOf(entry.getValue());
+        }
+    }
+
+    @Test
+    public void rfc5424_section6_5_messages() {
+        // See https://tools.ietf.org/html/rfc5424#section-6.5
+        final Map<String, Map<String, Object>> rfc3164messages = ImmutableMap.of(
+            "<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8",
+            ImmutableMap.<String, Object>builder()
+                .put("timestamp", new DateTime("2003-10-11T22:14:15.003", DateTimeZone.UTC).withZone(DateTimeZone.getDefault()))
+                .put("source", "mymachine.example.com")
+                .put("level", 2)
+                .put("facility", "security/authorization")
+                .put("application_name", "su")
+                .put("message", "ID47 - BOM'su root' failed for lonvick on /dev/pts/8")
+                .build(),
+
+            "<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts.",
+            ImmutableMap.<String, Object>builder()
+                .put("timestamp", new DateTime("2003-08-24T05:14:15.000", DateTimeZone.forOffsetHours(-7)).withZone(DateTimeZone.getDefault()))
+                .put("source", "192.0.2.1")
+                .put("level", 5)
+                .put("facility", "local4")
+                .put("application_name", "myproc")
+                .put("process_id", "8710")
+                .put("message", "%% It's time to make the do-nuts.")
+                .build(),
+
+            "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] An application event log entry...",
+            ImmutableMap.<String, Object>builder()
+                .put("timestamp", new DateTime("2003-10-11T22:14:15.003", DateTimeZone.UTC).withZone(DateTimeZone.getDefault()))
+                .put("source", "mymachine.example.com")
+                .put("level", 5)
+                .put("facility", "local4")
+                .put("application_name", "evntslog")
+                .put("message", "An application event log entry...")
+                .put("iut", "3")
+                .put("eventID", "1011")
+                .put("eventSource", "Application")
+                .build(),
+
+            "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][examplePriority@32473 class=\"high\"]",
+            ImmutableMap.<String, Object>builder()
+                .put("timestamp", new DateTime("2003-10-11T22:14:15.003", DateTimeZone.UTC).withZone(DateTimeZone.getDefault()))
+                .put("source", "mymachine.example.com")
+                .put("level", 5)
+                .put("facility", "local4")
+                .put("application_name", "evntslog")
+                .put("iut", "3")
+                .put("eventID", "1011")
+                .put("eventSource", "Application")
+                .build()
+
+        );
+
+        for (Map.Entry<String, Map<String, Object>> entry : rfc3164messages.entrySet()) {
+            final Message message = codec.decode(buildRawMessage(entry.getKey()));
+            assertThat(message).isNotNull();
+            assertThat(message.getFields()).containsAllEntriesOf(entry.getValue());
+        }
     }
 
     private RawMessage buildRawMessage(String message) {
