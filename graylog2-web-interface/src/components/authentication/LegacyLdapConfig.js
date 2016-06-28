3c54284c15ba3e236e77275e9b2dc11cfa2380fc
Kay Roepke <kroepke@googlemail.com>
2016-06-28 18:09:24 +0200

    move LDAP configuration to its own page

--- graylog2-web-interface/src/components/authentication/LegacyLdapConfig.js
+++ graylog2-web-interface/src/components/authentication/LegacyLdapConfig.js
@@ -1,11 +1,37 @@
 import React, { PropTypes } from 'react';
+import { PageHeader } from 'components/common';
+import { Button } from 'react-bootstrap';
+
+import LdapComponent from 'components/ldap/LdapComponent';
+import LdapGroupsComponent from 'components/ldap/LdapGroupsComponent';
 
 const LegacyLdapConfig = React.createClass({
   propTypes: {
     config: PropTypes.object,
   },
+  getInitialState() {
+    return {
+      showSettings: true,
+    };
+  },
+
+  _toggleButton() {
+    this.setState({ showSettings: !this.state.showSettings });
+  },
   render() {
-    return (<span>Configure LDAP/AD integration here.</span>);
+    const toggleButtonText = this.state.showSettings ? 'Ldap Group Mapping' : 'Ldap Settings';
+    const activeComponent = this.state.showSettings ? <LdapComponent /> : <LdapGroupsComponent />
+
+    return (<span>
+      <PageHeader title="LDAP Settings" subpage>
+        <span>This page is the only resource you need to set up the Graylog LDAP integration. You can test the connection to your LDAP server and even try to log in with an LDAP account of your choice right away.</span>
+        {null}
+        <span>
+          <Button bsStyle="info" onClick={this._toggleButton}>{toggleButtonText}</Button>
+        </span>
+      </PageHeader>
+      {activeComponent}
+    </span>);
   },
 });
 
