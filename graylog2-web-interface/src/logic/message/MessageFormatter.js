3619e99dc5a7b5e7e30a786071dba56645527b9f
Edmundo Alvarez <edmundo@graylog.com>
2016-07-01 17:14:48 +0200

    Add highlight ranges into formatted message
    The key was removed by mistake, and is used in the search results.

--- graylog2-web-interface/src/logic/message/MessageFormatter.js
+++ graylog2-web-interface/src/logic/message/MessageFormatter.js
@@ -4,15 +4,15 @@ import MessageFieldsFilter from 'logic/message/MessageFieldsFilter';
 const MessageFormatter = {
   formatMessageSummary(messageSummary) {
     const message = messageSummary.message;
-    return this.formatMessage(message._id, messageSummary.index, message, message);
+    return this.formatMessage(message._id, messageSummary.index, message, message, messageSummary.highlight_ranges);
   },
 
   formatResultMessage(resultMessage) {
     const message = resultMessage.message;
-    return this.formatMessage(message.id, resultMessage.index, message, message.fields);
+    return this.formatMessage(message.id, resultMessage.index, message, message.fields, resultMessage.highlight_ranges);
   },
 
-  formatMessage(id, index, message, fields) {
+  formatMessage(id, index, message, fields, highlightRanges) {
     const filteredFields = MessageFieldsFilter.filterFields(fields);
     const newMessage = {
       id: id,
@@ -24,6 +24,7 @@ const MessageFormatter = {
       source_node_id: fields.gl2_source_node,
       source_input_id: fields.gl2_source_input,
       stream_ids: message.streams,
+      highlight_ranges: highlightRanges,
     };
     return newMessage;
   },
