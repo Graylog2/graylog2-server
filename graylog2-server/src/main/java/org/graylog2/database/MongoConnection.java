e181a9abdebc580d16a77b2092aeffb47809e445
Jochen Schalanda <jochen@graylog.com>
2016-07-01 16:04:08 +0200

    Deprecate MongoConnection#getDatabase()

--- graylog2-server/src/main/java/org/graylog2/database/MongoConnection.java
+++ graylog2-server/src/main/java/org/graylog2/database/MongoConnection.java
@@ -23,7 +23,19 @@ import com.mongodb.client.MongoDatabase;
 public interface MongoConnection {
     Mongo connect();
 
+    /**
+     * Get instance of the configured MongoDB database.
+     *
+     * @return The configured MongoDB database.
+     * @deprecated Use {@link #getMongoDatabase()}.
+     */
+    @Deprecated
     DB getDatabase();
 
+    /**
+     * Get instance of the configured MongoDB database.
+     *
+     * @return The configured MongoDB database.
+     */
     MongoDatabase getMongoDatabase();
 }
