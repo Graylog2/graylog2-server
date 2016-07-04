453c39178caa39bc3ec7f97c8d71e0b6805469dc
Edmundo Alvarez <edmundo@graylog.com>
2016-07-04 14:59:47 +0200

    Report syntax error if search query contains slash
    When the search query contains an unescaped slash, calling to
    `generateParseException()` throws a NullPointerException. We solve it by
    catching the exception and let the normal program flow to continue, which
    at that point will return the syntax error.
    
    Fixes #2372

--- graylog2-server/src/main/java/org/graylog2/rest/resources/search/SearchResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/search/SearchResource.java
@@ -244,9 +244,15 @@ public abstract class SearchResource extends RestResource {
             try {
                 queryParser.parse(query);
             } catch (ParseException parseException) {
-                // FIXME I have no idea why this is necessary but without that call currentToken will be null.
-                final ParseException exception = queryParser.generateParseException();
-                Token currentToken = exception.currentToken;
+                Token currentToken = null;
+                try {
+                    // FIXME I have no idea why this is necessary but without that call currentToken will be null.
+                    final ParseException exception = queryParser.generateParseException();
+                    currentToken = exception.currentToken;
+                } catch (NullPointerException npe) {
+                    LOG.warn("Exception thrown while generating parse exception.", npe);
+                }
+
                 if (currentToken == null) {
                     LOG.warn("No position/token available for ParseException.", parseException);
                     errorMessage = QueryParseError.create(
