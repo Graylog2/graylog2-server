449ad54800d0b6367894a2ef7de7db5d34f0b3f0
Dennis Oelkers <dennis@graylog.com>
2016-07-06 12:26:04 +0200

    Removing useless `is_exclusive` property from decorator type info dto.

--- graylog2-server/src/main/java/org/graylog2/decorators/DecoratorTypeInfo.java
+++ graylog2-server/src/main/java/org/graylog2/decorators/DecoratorTypeInfo.java
@@ -29,7 +29,6 @@ public abstract class DecoratorTypeInfo {
     private static final String FIELD_TYPE = "type";
     private static final String FIELD_NAME = "name";
     private static final String FIELD_HUMAN_NAME = "human_name";
-    private static final String FIELD_IS_EXCLUSIVE = "is_exclusive";
     private static final String FIELD_REQUESTED_CONFIGURATION = "requested_configuration";
     private static final String FIELD_LINK_TO_DOCS = "link_to_docs";
 
@@ -39,8 +38,6 @@ public abstract class DecoratorTypeInfo {
     public abstract String name();
     @JsonProperty(FIELD_HUMAN_NAME)
     public abstract String humanName();
-    @JsonProperty(FIELD_IS_EXCLUSIVE)
-    public abstract boolean isExclusive();
     @JsonProperty(FIELD_REQUESTED_CONFIGURATION)
     public abstract ConfigurationRequest requestedConfiguration();
     @JsonProperty(FIELD_LINK_TO_DOCS)
@@ -50,13 +47,12 @@ public abstract class DecoratorTypeInfo {
     public static DecoratorTypeInfo create(@JsonProperty(FIELD_TYPE) String type,
                                            @JsonProperty(FIELD_NAME) String name,
                                            @JsonProperty(FIELD_HUMAN_NAME) String humanName,
-                                           @JsonProperty(FIELD_IS_EXCLUSIVE) boolean isExclusive,
                                            @JsonProperty(FIELD_REQUESTED_CONFIGURATION) ConfigurationRequest requestedConfiguration,
                                            @JsonProperty(FIELD_LINK_TO_DOCS) String linkToDocs) {
-        return new AutoValue_DecoratorTypeInfo(type, name, humanName, isExclusive, requestedConfiguration, linkToDocs);
+        return new AutoValue_DecoratorTypeInfo(type, name, humanName, requestedConfiguration, linkToDocs);
     }
 
     public static DecoratorTypeInfo create(String type, MessageDecorator.Descriptor descriptor, ConfigurationRequest requestedConfiguration) {
-        return create(type, descriptor.getName(), descriptor.getHumanName(), descriptor.isExclusive(), requestedConfiguration, descriptor.getLinkToDocs());
+        return create(type, descriptor.getName(), descriptor.getHumanName(), requestedConfiguration, descriptor.getLinkToDocs());
     }
 }
