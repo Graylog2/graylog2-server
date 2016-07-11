a4a7d1033d21f94e687075ee5b68883d3e426a20
Jochen Schalanda <jochen@graylog.com>
2016-07-11 20:55:56 +0200

    Add equals(), hashCode(), and toString() to Activity

--- graylog2-server/src/main/java/org/graylog2/shared/system/activities/Activity.java
+++ graylog2-server/src/main/java/org/graylog2/shared/system/activities/Activity.java
@@ -16,25 +16,27 @@
  */
 package org.graylog2.shared.system.activities;
 
-/**
- * @author Lennart Koopmann <lennart@socketfeed.com>
- */
-public class Activity {
+import com.google.common.base.MoreObjects;
 
-    String message;
-    Class<?> caller;
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
+public final class Activity {
+    private final Class<?> caller;
+    private String message;
 
     public Activity(Class<?> caller) {
-        this.caller = caller;
+        this.caller = requireNonNull(caller);
     }
 
     public Activity(String content, Class<?> caller) {
-        this.message = content;
-        this.caller = caller;
+        this.message = requireNonNull(content);
+        this.caller = requireNonNull(caller);
     }
 
     public void setMessage(String message) {
-        this.message = message;
+        this.message = requireNonNull(message);
     }
 
     public String getMessage() {
@@ -45,4 +47,28 @@ public class Activity {
         return caller;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Activity activity = (Activity) o;
+        return Objects.equals(caller, activity.caller) && Objects.equals(message, activity.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(caller, message);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("caller", caller)
+            .add("message", message)
+            .toString();
+    }
 }
