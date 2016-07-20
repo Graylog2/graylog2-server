702543621dcfe78dde6a8638a98ac36d110e6931
Jochen Schalanda <jochen@graylog.com>
2016-07-20 17:37:24 +0200

    Increase interval of ClusterEventCleanupPeriodical to 1 day

--- graylog2-server/src/main/java/org/graylog2/events/ClusterEventCleanupPeriodical.java
+++ graylog2-server/src/main/java/org/graylog2/events/ClusterEventCleanupPeriodical.java
@@ -40,7 +40,7 @@ public class ClusterEventCleanupPeriodical extends Periodical {
     private static final String COLLECTION_NAME = ClusterEventPeriodical.COLLECTION_NAME;
 
     @VisibleForTesting
-    static final long DEFAULT_MAX_EVENT_AGE = TimeUnit.HOURS.toMillis(1L);
+    static final long DEFAULT_MAX_EVENT_AGE = TimeUnit.DAYS.toMillis(1L);
 
     private final JacksonDBCollection<ClusterEvent, String> dbCollection;
     private final long maxEventAge;
@@ -89,7 +89,7 @@ public class ClusterEventCleanupPeriodical extends Periodical {
 
     @Override
     public int getPeriodSeconds() {
-        return Ints.saturatedCast(TimeUnit.MINUTES.toSeconds(5L));
+        return Ints.saturatedCast(TimeUnit.DAYS.toSeconds(1L));
     }
 
     @Override
