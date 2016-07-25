88fb7cce05836a8bf124813df46edc142ed96c5e
Bernd Ahlers <bernd@graylog.com>
2016-07-25 17:57:16 +0200

    Move properties file into a subdirectory to avoid clashes
    The path to the properties file is stored in the
    "Graylog-Plugin-Properties-Path" attribute of the JAR manifest.

--- graylog2-server/src/main/java/org/graylog2/shared/plugins/PluginProperties.java
+++ graylog2-server/src/main/java/org/graylog2/shared/plugins/PluginProperties.java
@@ -19,15 +19,19 @@ package org.graylog2.shared.plugins;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.InputStream;
+import java.io.IOException;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
 import static java.util.Objects.requireNonNull;
 
 public class PluginProperties {
     private static final Logger LOG = LoggerFactory.getLogger(PluginProperties.class);
 
+    private static final String MANIFEST_ATTRIBUTE = "Graylog-Plugin-Properties-Path";
     private static final String GRAYLOG_PLUGIN_PROPERTIES = "graylog-plugin.properties";
 
     private static final String PROPERTY_ISOLATED = "isolated";
@@ -39,20 +43,51 @@ public class PluginProperties {
         this.isolated = Boolean.parseBoolean((String) properties.getOrDefault(PROPERTY_ISOLATED, PROPERTY_ISOLATED_DEFAULT));
     }
 
+    /**
+     * Loads the Graylog plugin properties file from the given JAR file.
+     *
+     * The path to the properties file resource inside the JAR file is stored in the "Graylog-Plugin-Properties-Path"
+     * attribute of the JAR manifest. (Example: {@code org.graylog.plugins.graylog-plugin-map-widget})
+     *
+     * If the plugin properties file does not exist or cannot be found (like in older plugins) a default
+     * {@link PluginProperties} object will be returned.
+     *
+     * @param filename path to the JAR file
+     * @return the plugin properties object
+     */
     public static PluginProperties fromJarFile(final String filename) {
-        // Try to load the plugin properties from the JAR file. This fails for plugins which do not have the
-        // properties file.
         final Properties properties = new Properties();
         try {
-            LOG.debug("Loading {} from {}", GRAYLOG_PLUGIN_PROPERTIES, filename);
             final JarFile jarFile = new JarFile(requireNonNull(filename));
-            final InputStream inputStream = jarFile.getInputStream(jarFile.getEntry(GRAYLOG_PLUGIN_PROPERTIES));
-            properties.load(inputStream);
+            final Optional<String> propertiesPath = getPropertiesPath(jarFile);
+
+            if (propertiesPath.isPresent()) {
+                LOG.debug("Loading <{}> from <{}>", propertiesPath.get(), filename);
+                final ZipEntry entry = jarFile.getEntry(propertiesPath.get());
+
+                if (entry != null) {
+                    properties.load(jarFile.getInputStream(entry));
+                } else {
+                    LOG.debug("Plugin properties <{}> are missing in <{}>", propertiesPath.get(), filename);
+                }
+            }
         } catch (Exception e) {
-            LOG.debug("Unable to load {} from plugin {}", GRAYLOG_PLUGIN_PROPERTIES, filename, e);
+            LOG.debug("Unable to load properties from plugin <{}>", filename, e);
         }
 
         return new PluginProperties(properties);
+    }
+
+    private static Optional<String> getPropertiesPath(final JarFile jarFile) throws IOException {
+        // Lookup the resource path in the JAR manifest.
+        final String value = jarFile.getManifest().getMainAttributes().getValue(MANIFEST_ATTRIBUTE);
+
+        if (isNullOrEmpty(value)) {
+            LOG.debug("No value found for attribute <{}> in JAR manifest of file <{}>", MANIFEST_ATTRIBUTE, jarFile.getName());
+            return Optional.empty();
+        } else {
+            return Optional.of(value + "/" + GRAYLOG_PLUGIN_PROPERTIES);
+        }
     }
 
     public boolean isIsolated() {
