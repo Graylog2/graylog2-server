69ea824e58e1978c9cf838b0b60528d9bfe8efd3
Kay Roepke <kroepke@googlemail.com>
2016-07-25 15:46:45 +0200

    improve styling and wording a bit for authentication providers (#2522)

--- graylog2-web-interface/src/components/authentication/RootUserConfig.js
+++ graylog2-web-interface/src/components/authentication/RootUserConfig.js
@@ -1,11 +1,17 @@
 import React, { PropTypes } from 'react';
+import { PageHeader } from 'components/common';
 
 const RootUserConfig = React.createClass({
   propTypes: {
     config: PropTypes.object,
   },
   render() {
-    return (<span>Configure the hardcoded root user authentication here (probably nothing to configure for now).</span>);
+    return (<span>
+      <PageHeader title="Admin User Authenticator" subpage>
+        <span>This authenticator grants access to the admin user specified in the configuration file.</span>
+      </PageHeader>
+      <span>Currently the admin user authenticator cannot be configured outside of the configuration file. It can also not be disabled at the moment.</span>
+    </span>);
   },
 });
 
