9d443286a64bcef6df065363124c7b410253771b
Kay Roepke <kroepke@googlemail.com>
2016-07-25 14:01:13 +0200

    improve styling and wording a bit for authentication providers

--- graylog2-web-interface/src/components/authentication/MongoDbSessionConfig.js
+++ graylog2-web-interface/src/components/authentication/MongoDbSessionConfig.js
@@ -1,11 +1,17 @@
 import React, { PropTypes } from 'react';
+import { PageHeader } from 'components/common';
 
 const MongoDbSessionConfig = React.createClass({
   propTypes: {
     config: PropTypes.object,
   },
   render() {
-    return (<span>Configure session handling (stored in MongoDB) here.</span>);
+    return (<span>
+      <PageHeader title="Session Authenticator" subpage>
+        <span>This authenticator uses the session supplied from the web interface to grant access to logged in users, it usually runs first.</span>
+      </PageHeader>
+      <span>Since sessions are necessary to let the web interface function it cannot be disabled.</span>
+    </span>);
   },
 });
 
