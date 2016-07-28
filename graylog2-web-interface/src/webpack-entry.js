dbf09b783787de95e82f7300880d41fe783a56af
Edmundo Alvarez <edmundo@graylog.com>
2016-07-28 12:32:16 +0200

    Fix loading of /system pages when no prefix is set

--- graylog2-web-interface/src/webpack-entry.js
+++ graylog2-web-interface/src/webpack-entry.js
@@ -1,4 +1,5 @@
 import URI from 'urijs';
 import AppConfig from 'util/AppConfig';
 
-__webpack_public_path__ = URI.joinPaths(AppConfig.gl2AppPathPrefix(), '/').path();
+// If app prefix was not set, we need to tell webpack to load chunks from root instead of the relative URL path
+__webpack_public_path__ = URI.joinPaths(AppConfig.gl2AppPathPrefix(), '/').path() || '/';
