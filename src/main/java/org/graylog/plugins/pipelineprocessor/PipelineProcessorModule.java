c7ca2f7a1ab63b6b5f91922c00688da44ab35cf0
Bernd Ahlers <bernd@tuneafish.de>
2016-08-03 10:50:49 +0200

    Remove upper case decorator (#73)
    There is no real use case for this besides being a good test case for
    the decorator system development.
    
    Fixes Graylog2/graylog2-server#2588

--- src/main/java/org/graylog/plugins/pipelineprocessor/PipelineProcessorModule.java
+++ src/main/java/org/graylog/plugins/pipelineprocessor/PipelineProcessorModule.java
@@ -50,8 +50,5 @@ public class PipelineProcessorModule extends PluginModule {
         installSearchResponseDecorator(searchResponseDecoratorBinder(),
                 PipelineProcessorMessageDecorator.class,
                 PipelineProcessorMessageDecorator.Factory.class);
-        installSearchResponseDecorator(searchResponseDecoratorBinder(),
-                UpperCaseDecorator.class,
-                UpperCaseDecorator.Factory.class);
     }
 }
