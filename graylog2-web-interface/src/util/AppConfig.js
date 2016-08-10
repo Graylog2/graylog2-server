fec17924f04ecd0bf53a9ecc8115e9e0ab9aa69f
Bernd Ahlers <bernd@graylog.com>
2016-08-10 14:16:56 +0200

    Serve assets from "/" when running in development mode
    - Introduce `AppConfig.gl2DevMode()`
    - Use the function in Navigation.jsx instead of checking for the global
      variable directly

--- graylog2-web-interface/src/util/AppConfig.js
+++ graylog2-web-interface/src/util/AppConfig.js
@@ -7,6 +7,12 @@ const AppConfig = {
     return window.appConfig.gl2AppPathPrefix;
   },
 
+  gl2DevMode() {
+    // The DEVELOPMENT variable will be set by webpack via the DefinePlugin.
+    // eslint-disable-next-line no-undef
+    return typeof(DEVELOPMENT) !== 'undefined' && DEVELOPMENT;
+  },
+
   rootTimeZone() {
     return window.appConfig.rootTimeZone;
   },
--- graylog2-web-interface/src/webpack-entry.js
+++ graylog2-web-interface/src/webpack-entry.js
@@ -1,5 +1,9 @@
 import URI from 'urijs';
 import AppConfig from 'util/AppConfig';
 
+// The webpack-dev-server serves the assets from "/"
+const assetPrefix = AppConfig.gl2DevMode() ? '/' : '/assets/';
+
 // If app prefix was not set, we need to tell webpack to load chunks from root instead of the relative URL path
-__webpack_public_path__ = URI.joinPaths(AppConfig.gl2AppPathPrefix(), '/assets/').path() || '/assets/';
+// eslint-disable-next-line camelcase, no-undef
+__webpack_public_path__ = URI.joinPaths(AppConfig.gl2AppPathPrefix(), assetPrefix).path() || assetPrefix;
