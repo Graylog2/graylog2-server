f21efb91e7859c6291c64f0e92a6f27e4780a026
Edmundo Alvarez <edmundo@graylog.com>
2016-08-11 22:51:13 +0200

    Resolve entity IDs from decorator type definitions
    Attempt to resolve IDs from the decorator type definitions if possible.
    This allow us to display entity names (like we do in the configuration
    forms) instead of simply display an ID.

--- graylog2-web-interface/src/components/search/Decorator.jsx
+++ graylog2-web-interface/src/components/search/Decorator.jsx
@@ -40,11 +40,29 @@ const Decorator = React.createClass({
       name: 'Unknown decorator type',
     };
   },
+  // Attempts to resolve ID values in the decorator configuration against the type definition.
+  // This allows users to see actual names for entities in drop-downs, instead of their IDs.
+  _resolveConfigurationIds(config) {
+    const typeConfig = this.props.typeDefinition.requested_configuration;
+    const resolvedConfig = {};
+    const configKeys = Object.keys(config);
+
+    configKeys.forEach(key => {
+      const configValues = (typeConfig[key] ? typeConfig[key].additional_info.values : undefined);
+      if (typeof configValues === 'object' && !Array.isArray(configValues)) {
+        const originalValue = config[key];
+        resolvedConfig[key] = configValues[originalValue] ? configValues[originalValue] : originalValue;
+      }
+    });
+
+    return Object.assign({}, config, resolvedConfig);
+  },
   render() {
     if (!this.state.types) {
       return <Spinner />;
     }
     const decorator = this.props.decorator;
+    const config = this._resolveConfigurationIds(decorator.config);
     const decoratorType = this.state.types[decorator.type] || this._decoratorTypeNotPresent();
     return (
       <span className={DecoratorStyles.fullWidth}>
@@ -58,7 +76,7 @@ const Decorator = React.createClass({
         </div>
         <ConfigurationWell key={`configuration-well-decorator-${decorator._id}`}
                            id={decorator._id}
-                           configuration={decorator.config}
+                           configuration={config}
                            typeDefinition={this.props.typeDefinition}/>
         <ConfigurationForm ref="editForm"
                            key="configuration-form-decorator"
