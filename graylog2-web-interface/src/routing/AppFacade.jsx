48e091c356521b06dcaa944ce9ddfb4a6ef7e0ee
Dennis Oelkers <dennis@graylog.com>
2016-08-17 11:57:44 +0200

    Checking for presence of currentUser too, because it can be undefined.

--- graylog2-web-interface/src/routing/AppFacade.jsx
+++ graylog2-web-interface/src/routing/AppFacade.jsx
@@ -7,6 +7,7 @@ import ServerUnavailablePage from 'pages/ServerUnavailablePage';
 import StoreProvider from 'injection/StoreProvider';
 const SessionStore = StoreProvider.getStore('Session');
 const ServerAvailabilityStore = StoreProvider.getStore('ServerAvailability');
+const CurrentUserStore = StoreProvider.getStore('CurrentUser');
 
 import 'javascripts/shims/styles/shim.css';
 import 'stylesheets/bootstrap.min.css';
@@ -18,7 +19,7 @@ import 'stylesheets/rickshaw.min.css';
 import 'stylesheets/graylog2.less';
 
 const AppFacade = React.createClass({
-  mixins: [Reflux.connect(SessionStore), Reflux.connect(ServerAvailabilityStore)],
+  mixins: [Reflux.connect(SessionStore), Reflux.connect(ServerAvailabilityStore), Reflux.connect(CurrentUserStore)],
 
   componentDidMount() {
     this.interval = setInterval(ServerAvailabilityStore.ping, 20000);
@@ -34,7 +35,7 @@ const AppFacade = React.createClass({
     if (!this.state.server.up) {
       return <ServerUnavailablePage server={this.state.server} />;
     }
-    if (!this.state.sessionId) {
+    if (!this.state.sessionId || !this.state.currentUser) {
       return <LoginPage />;
     }
     return <LoggedInPage />;
