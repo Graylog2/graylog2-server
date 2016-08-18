c7da50f3768f75acc988ad6bfa4efce4a2ff5b37
Edmundo Alvarez <edmundo@graylog.com>
2016-08-18 11:37:04 +0200

    Bumping versions to 2.1.0-beta.4 / 1.1.0-beta.4

--- src/main/java/org/graylog/plugins/enterprise_integration/EnterpriseIntegrationMetaData.java
+++ src/main/java/org/graylog/plugins/enterprise_integration/EnterpriseIntegrationMetaData.java
@@ -50,7 +50,7 @@ public class EnterpriseIntegrationMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 1, 0, "beta.3");
+        return new Version(1, 1, 0, "beta.4");
     }
 
     @Override
