91ed3e1ed12a73e82a0391e256ddb7450428c8d6
Dennis Oelkers <dennis@graylog.com>
2016-09-05 14:43:47 +0200

    Removing unused import.

--- graylog2-server/src/main/java/org/graylog2/shared/rest/resources/ProxiedResource.java
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/resources/ProxiedResource.java
@@ -35,7 +35,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.function.Function;
 import java.util.stream.Collectors;
