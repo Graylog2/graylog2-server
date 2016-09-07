3cfc43f2ad44cbbab59d33890875d341b28d13d8
Jochen Schalanda <jochen@graylog.com>
2016-09-07 14:28:12 +0200

    Try to simplify ProxiedResource#getForAllNodes()

--- graylog2-server/src/main/java/org/graylog2/shared/rest/resources/ProxiedResource.java
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/resources/ProxiedResource.java
@@ -63,46 +63,42 @@ public abstract class ProxiedResource extends RestResource {
         }
     }
 
-    protected <RemoteInterfaceType, RemoteCallResponseType> Map<String, Optional<RemoteCallResponseType>> getForAllNodes(Function<RemoteInterfaceType,Call<RemoteCallResponseType>> fn, Function<String, Optional<RemoteInterfaceType>> interfaceProvider) {
+    protected <RemoteInterfaceType, RemoteCallResponseType> Map<String, Optional<RemoteCallResponseType>> getForAllNodes(Function<RemoteInterfaceType, Call<RemoteCallResponseType>> fn, Function<String, Optional<RemoteInterfaceType>> interfaceProvider) {
         return getForAllNodes(fn, interfaceProvider, Function.identity());
     }
 
     protected <RemoteInterfaceType, FinalResponseType, RemoteCallResponseType> Map<String, Optional<FinalResponseType>> getForAllNodes(Function<RemoteInterfaceType, Call<RemoteCallResponseType>> fn, Function<String, Optional<RemoteInterfaceType>> interfaceProvider, Function<RemoteCallResponseType, FinalResponseType> transformer) {
-        final Map<String, Future<Optional<FinalResponseType>>> futures = this.nodeService.allActive()
-            .entrySet()
-            .stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
-                final Optional<RemoteInterfaceType> remoteInterface = interfaceProvider.apply(entry.getKey());
-                if (!remoteInterface.isPresent()) {
-                    return CompletableFuture.completedFuture(Optional.empty());
-                }
-                return this.executor.submit(() -> {
-                    final Call<RemoteCallResponseType> call = fn.apply(remoteInterface.get());
-                    try {
-                        final Response<RemoteCallResponseType> response = call.execute();
-                        if (response.isSuccessful()) {
-                            return Optional.of(transformer.apply(response.body()));
-                        } else {
-                            LOG.warn("Unable to call " + call.request().url().toString() + " on node <" + entry.getKey() + ">, result: " + response.message());
-                            return Optional.empty();
-                        }
-                    } catch (IOException e) {
-                        LOG.warn("Unable to call " + call.request().url().toString() + " on node <" + entry.getKey() + ">, caught exception: {} ({})", e.getMessage(), e.getClass());
-                        return Optional.empty();
-                    }
-                });
-            }));
+        final Map<String, Future<Optional<FinalResponseType>>> futures = this.nodeService.allActive().keySet().stream()
+                .collect(Collectors.toMap(Function.identity(), node -> interfaceProvider.apply(node)
+                        .map(r -> executor.submit(() -> {
+                            final Call<RemoteCallResponseType> call = fn.apply(r);
+                            try {
+                                final Response<RemoteCallResponseType> response = call.execute();
+                                if (response.isSuccessful()) {
+                                    return Optional.of(transformer.apply(response.body()));
+                                } else {
+                                    LOG.warn("Unable to call {} on node <{}>, result: {}", call.request().url(), node, response.message());
+                                    return Optional.<FinalResponseType>empty();
+                                }
+                            } catch (IOException e) {
+                                LOG.warn("Unable to call {} on node <{}>", call.request().url(), node, e);
+                                return Optional.<FinalResponseType>empty();
+                            }
+                        }))
+                        .orElse(CompletableFuture.completedFuture(Optional.empty()))
+                ));
 
         return futures
-            .entrySet()
-            .stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
-                try {
-                    return entry.getValue().get();
-                } catch (InterruptedException | ExecutionException e) {
-                    return Optional.empty();
-                }
-            }));
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+                    try {
+                        return entry.getValue().get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        LOG.debug("Couldn't retrieve future", e);
+                        return Optional.empty();
+                    }
+                }));
     }
 
     protected <RemoteInterfaceType> Function<String, Optional<RemoteInterfaceType>> createRemoteInterfaceProvider(Class<RemoteInterfaceType> interfaceClass) {
