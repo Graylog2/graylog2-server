42c031dbc2b2703e5e614a6e35040f78dc0bb7ff
Edmundo Alvarez <edmundo@graylog.com>
2016-09-09 13:15:31 +0200

    Display loading page while loading user information
    Refs #2770

---
+++ graylog2-web-interface/src/pages/LoadingPage.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+import { Spinner } from 'components/common';
+
+import disconnectedStyle from '!style/useable!css!less!stylesheets/disconnected.less';
+import authStyle from '!style/useable!css!less!stylesheets/auth.less';
+
+const LoadingPage = React.createClass({
+  propTypes: {
+    text: React.PropTypes.string,
+  },
+
+  getDefaultProps() {
+    return {
+      text: 'Loading, please wait...',
+    };
+  },
+
+  componentDidMount() {
+    disconnectedStyle.use();
+    authStyle.use();
+  },
+
+  componentWillUnmount() {
+    disconnectedStyle.unuse();
+    authStyle.unuse();
+  },
+
+  render() {
+    return (
+      <div>
+        <div className="container" id="login-box">
+          <Row>
+            <Col md={4} mdOffset={4} className="well" id="login-box-content">
+              <legend><i className="fa fa-group"/> Welcome to Graylog</legend>
+              <p className="loading-text">
+                <Spinner text={this.props.text}/>
+              </p>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  },
+});
+
+export default LoadingPage;
