617b227ecd2c85097bcea942b18cb96f34d24064
Bernd Ahlers <bernd@tuneafish.de>
2016-09-21 11:12:24 +0200

    Make ValidatorProvider a Singleton
    The Validator instance creation is quite expensive. Making this a
    Singleton reduced the CPU load by 50% and reduced the GC load from 5 GCs
    per second to 2 GCs per second when running a load test of the collector
    registration endpoint.
    
    Closes #3019
    
    (cherry picked from commit b82ca3e4a1b3ddfbca11359419cfca5df0b029c3)

--- graylog2-server/src/main/java/org/graylog2/shared/bindings/ValidatorModule.java
+++ graylog2-server/src/main/java/org/graylog2/shared/bindings/ValidatorModule.java
@@ -17,13 +17,19 @@
 package org.graylog2.shared.bindings;
 
 import com.google.inject.AbstractModule;
-import org.graylog2.shared.bindings.providers.ValidatorProvider;
 
+import javax.validation.Validation;
 import javax.validation.Validator;
 
 public class ValidatorModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(Validator.class).toProvider(ValidatorProvider.class);
+        // Validator instances are thread-safe and can be reused.
+        // See: http://hibernate.org/validator/documentation/getting-started/
+        //
+        // The Validator instance creation is quite expensive.
+        // Making this a Singleton reduced the CPU load by 50% and reduced the GC load from 5 GCs per second to 2 GCs
+        // per second when running a load test of the collector registration endpoint.
+        bind(Validator.class).toInstance(Validation.buildDefaultValidatorFactory().getValidator());
     }
 }
