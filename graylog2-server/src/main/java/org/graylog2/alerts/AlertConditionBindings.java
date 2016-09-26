207e97f403f14f075e3f0089e9dcfcc7d3d7c77b
Dennis Oelkers <dennis@graylog.com>
2016-09-26 15:09:55 +0200

    Adding helper method to add alert conditions to PluginModule & using it.

--- graylog2-server/src/main/java/org/graylog2/alerts/AlertConditionBindings.java
+++ graylog2-server/src/main/java/org/graylog2/alerts/AlertConditionBindings.java
@@ -16,27 +16,21 @@
  */
 package org.graylog2.alerts;
 
-import com.google.inject.multibindings.MapBinder;
 import org.graylog2.alerts.types.FieldContentValueAlertCondition;
 import org.graylog2.alerts.types.FieldValueAlertCondition;
 import org.graylog2.alerts.types.MessageCountAlertCondition;
-import org.graylog2.plugin.alarms.AlertCondition;
-import org.graylog2.plugin.inject.Graylog2Module;
+import org.graylog2.plugin.PluginModule;
 
-public class AlertConditionBindings extends Graylog2Module {
+public class AlertConditionBindings extends PluginModule {
     @Override
     protected void configure() {
-        MapBinder<String, AlertCondition.Factory> alertConditionBinder = alertConditionBinder();
-        installAlertConditionWithCustomName(alertConditionBinder,
-            AbstractAlertCondition.Type.FIELD_CONTENT_VALUE.toString(),
+        addAlertCondition(AbstractAlertCondition.Type.FIELD_CONTENT_VALUE.toString(),
             FieldContentValueAlertCondition.class,
             FieldContentValueAlertCondition.Factory.class);
-        installAlertConditionWithCustomName(alertConditionBinder,
-            AbstractAlertCondition.Type.FIELD_VALUE.toString(),
+        addAlertCondition(AbstractAlertCondition.Type.FIELD_VALUE.toString(),
             FieldValueAlertCondition.class,
             FieldValueAlertCondition.Factory.class);
-        installAlertConditionWithCustomName(alertConditionBinder,
-            AbstractAlertCondition.Type.MESSAGE_COUNT.toString(),
+        addAlertCondition(AbstractAlertCondition.Type.MESSAGE_COUNT.toString(),
             MessageCountAlertCondition.class,
             MessageCountAlertCondition.Factory.class);
     }
