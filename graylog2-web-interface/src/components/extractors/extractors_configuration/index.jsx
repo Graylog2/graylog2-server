bae655edbeb0d711676b6c27b2e8491fbd9ea14e
Dennis Oelkers <dennis@graylog.com>
2016-09-29 18:28:06 +0200

    Fixing reexports. (#2906)

--- graylog2-web-interface/src/components/extractors/extractors_configuration/index.jsx
+++ graylog2-web-interface/src/components/extractors/extractors_configuration/index.jsx
@@ -1,7 +1,7 @@
-export CopyInputExtractorConfiguration from './CopyInputExtractorConfiguration';
-export GrokExtractorConfiguration from './GrokExtractorConfiguration';
-export JSONExtractorConfiguration from './JSONExtractorConfiguration';
-export RegexExtractorConfiguration from './RegexExtractorConfiguration';
-export RegexReplaceExtractorConfiguration from './RegexReplaceExtractorConfiguration';
-export SplitAndIndexExtractorConfiguration from './SplitAndIndexExtractorConfiguration';
-export SubstringExtractorConfiguration from './SubstringExtractorConfiguration';
+export { default as CopyInputExtractorConfiguration } from './CopyInputExtractorConfiguration';
+export { default as GrokExtractorConfiguration } from './GrokExtractorConfiguration';
+export { default as JSONExtractorConfiguration } from './JSONExtractorConfiguration';
+export { default as RegexExtractorConfiguration } from './RegexExtractorConfiguration';
+export { default as RegexReplaceExtractorConfiguration } from './RegexReplaceExtractorConfiguration';
+export { default as SplitAndIndexExtractorConfiguration } from './SplitAndIndexExtractorConfiguration';
+export { default as SubstringExtractorConfiguration } from './SubstringExtractorConfiguration';
