5815a057a9b0b31954ae377a453c19644e5a6de0
Dennis Oelkers <dennis@graylog.com>
2016-10-06 16:34:36 +0200

    Simplifying glob, correcting web module lookup for plugins in sub dir.

--- graylog2-web-interface/webpack.combined.config.js
+++ graylog2-web-interface/webpack.combined.config.js
@@ -8,20 +8,18 @@ const MANIFESTS_PATH = path.resolve(ROOT_PATH, 'manifests');
 const VENDOR_MANIFEST_PATH = path.resolve(MANIFESTS_PATH, 'vendor-manifest.json');
 const VENDOR_MANIFEST = require(VENDOR_MANIFEST_PATH);
 
-// Regular plugins
-const pluginPattern1 = '../../graylog-plugin-*/webpack.config.js';
-// Plugins where the web part is in a subdir
-const pluginPattern2 = '../../graylog-plugin-*/*/webpack.config.js';
+const pluginPrefix = '../../graylog-plugin-*/**/';
+const pluginConfigPattern = pluginPrefix + 'webpack.config.js';
 
-const pluginConfigs = process.env.disable_plugins == 'true' ? [] : glob.sync(pluginPattern1).concat(glob.sync(pluginPattern2));
+const pluginConfigs = process.env.disable_plugins == 'true' ? [] : glob.sync(pluginConfigPattern);
 
 process.env.web_src_path = path.resolve(__dirname);
 
 const webpackConfig = require(path.resolve(__dirname, './webpack.config.js'));
 
-pluginConfigs.forEach(function(plugin) {
-  const pluginName = plugin.split('/')[2];
-  const pluginDir = path.resolve(__dirname, '../..', pluginName, 'src/web');
+pluginConfigs.forEach(function(pluginConfig) {
+  const pluginName = pluginConfig.split('/')[2];
+  const pluginDir = path.resolve(pluginConfig, '../src/web');
   webpackConfig.entry[pluginName] = pluginDir;
   webpackConfig.resolve.modulesDirectories.unshift(pluginDir);
   webpackConfig.plugins.unshift(new webpack.DllReferencePlugin({ manifest: VENDOR_MANIFEST, context: path.resolve(pluginDir, '../..') }));
