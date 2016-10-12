46d95d69464a7487a0589895a57b8d18e9ea38c0
Marius Sturm <marius@graylog.com>
2016-10-12 17:51:23 +0200

    bump to version 2.2.0-alpha.1

--- src/main/java/org/graylog/plugins/enterprise_integration/EnterpriseIntegrationMetaData.java
+++ src/main/java/org/graylog/plugins/enterprise_integration/EnterpriseIntegrationMetaData.java
@@ -50,7 +50,7 @@ public class EnterpriseIntegrationMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 1, 0, "rc.1");
+        return new Version(2, 2, 0, "alpha.1");
     }
 
     @Override
