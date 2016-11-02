8c4673c83f2a75896426e0eb04fbc790bf1bfe4f
Kay Roepke <kroepke@googlemail.com>
2016-11-02 16:45:13 +0100

    ignore varref arguments when precomputing constant args
    during direct tree interpretation the variables are not yet defined, so we need to skip them
    code generation doesn't care and won't ever take this code path

--- plugin/src/main/java/org/graylog/plugins/pipelineprocessor/ast/functions/FunctionArgs.java
+++ plugin/src/main/java/org/graylog/plugins/pipelineprocessor/ast/functions/FunctionArgs.java
@@ -19,6 +19,7 @@ package org.graylog.plugins.pipelineprocessor.ast.functions;
 import com.google.common.collect.Maps;
 
 import org.graylog.plugins.pipelineprocessor.ast.expressions.Expression;
+import org.graylog.plugins.pipelineprocessor.ast.expressions.VarRefExpression;
 
 import java.util.Collections;
 import java.util.Map;
@@ -54,6 +55,7 @@ public class FunctionArgs {
     public Map<String, Expression> getConstantArgs() {
         return args.entrySet().stream()
                 .filter(e -> e.getValue().isConstant())
+                .filter(e -> !(e.getValue() instanceof VarRefExpression)) // do not eagerly touch variables
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
