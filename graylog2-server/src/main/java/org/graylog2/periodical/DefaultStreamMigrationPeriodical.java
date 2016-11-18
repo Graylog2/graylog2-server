6b340ce48d94fc6a57ce553a1fc53cf7543262d8
Bernd Ahlers <bernd@graylog.com>
2016-11-18 18:33:29 +0100

    Merge migration periodicals for default stream and index sets
    This makes it easier to execute the steps in order.

--- graylog2-server/src/main/java/org/graylog2/periodical/DefaultStreamMigrationPeriodical.java
+++ graylog2-server/src/main/java/org/graylog2/periodical/DefaultStreamMigrationPeriodical.java
@@ -16,15 +16,28 @@
  */
 package org.graylog2.periodical;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.google.auto.value.AutoValue;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import org.bson.types.ObjectId;
+import org.graylog2.configuration.ElasticsearchConfiguration;
 import org.graylog2.database.NotFoundException;
 import org.graylog2.events.ClusterEventBus;
+import org.graylog2.indexer.indexset.IndexSetConfig;
+import org.graylog2.indexer.indexset.IndexSetService;
+import org.graylog2.indexer.management.IndexManagementConfig;
 import org.graylog2.plugin.cluster.ClusterConfigService;
 import org.graylog2.plugin.database.ValidationException;
+import org.graylog2.plugin.indexer.retention.RetentionStrategy;
+import org.graylog2.plugin.indexer.retention.RetentionStrategyConfig;
+import org.graylog2.plugin.indexer.rotation.RotationStrategy;
+import org.graylog2.plugin.indexer.rotation.RotationStrategyConfig;
 import org.graylog2.plugin.periodical.Periodical;
 import org.graylog2.plugin.streams.Stream;
 import org.graylog2.streams.StreamImpl;
+import org.graylog2.streams.StreamRuleService;
 import org.graylog2.streams.StreamService;
 import org.graylog2.streams.events.StreamsChangedEvent;
 import org.joda.time.DateTime;
@@ -33,8 +46,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.Map;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.base.Preconditions.checkState;
 
 /**
  * Periodical creating the default stream if it doesn't exist.
@@ -43,20 +62,48 @@ public class DefaultStreamMigrationPeriodical extends Periodical {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultStreamMigrationPeriodical.class);
 
     private final StreamService streamService;
+    private final StreamRuleService streamRuleService;
+    private final ElasticsearchConfiguration elasticsearchConfiguration;
+    private final Map<String, Provider<RotationStrategy>> rotationStrategies;
+    private final Map<String, Provider<RetentionStrategy>> retentionStrategies;
+    private final IndexSetService indexSetService;
     private final ClusterEventBus clusterEventBus;
     private final ClusterConfigService clusterConfigService;
 
     @Inject
     public DefaultStreamMigrationPeriodical(final StreamService streamService,
+                                            final StreamRuleService streamRuleService,
+                                            final ElasticsearchConfiguration elasticsearchConfiguration,
+                                            final Map<String, Provider<RotationStrategy>> rotationStrategies,
+                                            final Map<String, Provider<RetentionStrategy>> retentionStrategies,
+                                            final IndexSetService indexSetService,
                                             final ClusterEventBus clusterEventBus,
                                             final ClusterConfigService clusterConfigService) {
         this.streamService = streamService;
+        this.streamRuleService = streamRuleService;
+        this.elasticsearchConfiguration = elasticsearchConfiguration;
+        this.rotationStrategies = rotationStrategies;
+        this.retentionStrategies = retentionStrategies;
+        this.indexSetService = indexSetService;
         this.clusterEventBus = clusterEventBus;
         this.clusterConfigService = clusterConfigService;
     }
 
     @Override
     public void doRun() {
+        try {
+            streamService.load(Stream.DEFAULT_STREAM_ID);
+        } catch (NotFoundException ignored) {
+            doRunDefaultStreamMigration();
+        }
+
+        if (clusterConfigService.get(IndexSetMigrated.class) == null) {
+            doRunIndexSetMigration();
+        }
+    }
+
+    @VisibleForTesting
+    void doRunDefaultStreamMigration() {
         final ObjectId id = new ObjectId(Stream.DEFAULT_STREAM_ID);
         final Map<String, Object> fields = ImmutableMap.<String, Object>builder()
                 .put(StreamImpl.FIELD_TITLE, "All messages")
@@ -79,6 +126,53 @@ public class DefaultStreamMigrationPeriodical extends Periodical {
         }
     }
 
+    @VisibleForTesting
+    void doRunIndexSetMigration() {
+        final IndexManagementConfig indexManagementConfig = clusterConfigService.get(IndexManagementConfig.class);
+        checkState(indexManagementConfig != null, "Couldn't find index management configuration");
+
+        final RotationStrategyConfig rotationStrategyConfig = getRotationStrategyConfig(indexManagementConfig);
+        final RetentionStrategyConfig retentionStrategyConfig = getRetentionStrategyConfig(indexManagementConfig);
+
+        final IndexSetConfig indexSetConfig = IndexSetConfig.builder()
+                .title("Default index set")
+                .description("The Graylog default index set")
+                .indexPrefix(elasticsearchConfiguration.getIndexPrefix())
+                .shards(elasticsearchConfiguration.getShards())
+                .replicas(elasticsearchConfiguration.getReplicas())
+                .rotationStrategy(rotationStrategyConfig)
+                .retentionStrategy(retentionStrategyConfig)
+                .creationDate(ZonedDateTime.now(ZoneOffset.UTC))
+                .build();
+
+        final IndexSetConfig savedConfig = indexSetService.save(indexSetConfig);
+        clusterConfigService.write(IndexSetMigrated.create());
+
+        LOG.info("Successfully migrated index settings (database ID <{}>)", savedConfig.id());
+    }
+
+    private RetentionStrategyConfig getRetentionStrategyConfig(IndexManagementConfig indexManagementConfig) {
+        final String strategyName = indexManagementConfig.retentionStrategy();
+        final Provider<RetentionStrategy> provider = retentionStrategies.get(strategyName);
+        checkState(provider != null, "Couldn't retrieve retention strategy provider for <" + strategyName + ">");
+        final RetentionStrategy strategy = provider.get();
+        @SuppressWarnings("unchecked")
+        final Class<RetentionStrategyConfig> configClass = (Class<RetentionStrategyConfig>) strategy.configurationClass();
+        final RetentionStrategyConfig config = clusterConfigService.get(configClass);
+        return firstNonNull(config, strategy.defaultConfiguration());
+    }
+
+    private RotationStrategyConfig getRotationStrategyConfig(IndexManagementConfig indexManagementConfig) {
+        final String strategyName = indexManagementConfig.rotationStrategy();
+        final Provider<RotationStrategy> provider = rotationStrategies.get(strategyName);
+        checkState(provider != null, "Couldn't retrieve rotation strategy provider for <" + strategyName + ">");
+        final RotationStrategy strategy = provider.get();
+        @SuppressWarnings("unchecked")
+        final Class<RotationStrategyConfig> configClass = (Class<RotationStrategyConfig>) strategy.configurationClass();
+        final RotationStrategyConfig config = clusterConfigService.get(configClass);
+        return firstNonNull(config, strategy.defaultConfiguration());
+    }
+
     @Override
     public boolean runsForever() {
         return true;
@@ -96,12 +190,7 @@ public class DefaultStreamMigrationPeriodical extends Periodical {
 
     @Override
     public boolean startOnThisNode() {
-        try {
-            return streamService.load(Stream.DEFAULT_STREAM_ID) == null;
-        } catch (NotFoundException ignored) {
-            // if the stream cannot be found, recreate it
-            return true;
-        }
+        return true;
     }
 
     @Override
@@ -122,5 +211,14 @@ public class DefaultStreamMigrationPeriodical extends Periodical {
     @Override
     protected Logger getLogger() {
         return LOG;
+    }
+
+    @JsonAutoDetect
+    @AutoValue
+    static abstract class IndexSetMigrated {
+        @JsonCreator
+        public static IndexSetMigrated create() {
+            return new AutoValue_DefaultStreamMigrationPeriodical_IndexSetMigrated();
+        }
     }
 }
--- graylog2-server/src/test/java/org/graylog2/periodical/DefaultStreamMigrationPeriodicalTest.java
+++ graylog2-server/src/test/java/org/graylog2/periodical/DefaultStreamMigrationPeriodicalTest.java
@@ -16,25 +16,44 @@
  */
 package org.graylog2.periodical;
 
+import org.graylog2.configuration.ElasticsearchConfiguration;
 import org.graylog2.database.NotFoundException;
 import org.graylog2.events.ClusterEventBus;
+import org.graylog2.indexer.IndexSet;
+import org.graylog2.indexer.indexset.IndexSetConfig;
+import org.graylog2.indexer.indexset.IndexSetService;
+import org.graylog2.indexer.management.IndexManagementConfig;
 import org.graylog2.plugin.cluster.ClusterConfigService;
 import org.graylog2.plugin.database.ValidationException;
+import org.graylog2.plugin.indexer.retention.RetentionStrategy;
+import org.graylog2.plugin.indexer.retention.RetentionStrategyConfig;
+import org.graylog2.plugin.indexer.rotation.RotationStrategy;
+import org.graylog2.plugin.indexer.rotation.RotationStrategyConfig;
 import org.graylog2.plugin.streams.Stream;
 import org.graylog2.streams.StreamImpl;
+import org.graylog2.streams.StreamRuleService;
 import org.graylog2.streams.StreamService;
 import org.graylog2.streams.events.StreamsChangedEvent;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
@@ -47,20 +66,39 @@ public class DefaultStreamMigrationPeriodicalTest {
     @Mock
     private StreamService streamService;
     @Mock
+    private StreamRuleService streamRuleService;
+    @Mock
     private ClusterEventBus clusterEventBus;
     @Mock
     private ClusterConfigService clusterConfigService;
 
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    @Mock
+    private IndexSetService indexSetService;
+
+    private final ElasticsearchConfiguration elasticsearchConfiguration = new ElasticsearchConfiguration();
+    private RotationStrategy rotationStrategy = new StubRotationStrategy();
+    private RetentionStrategy retentionStrategy = new StubRetentionStrategy();
+
     @Before
     public void setUpService() throws Exception {
-        periodical = new DefaultStreamMigrationPeriodical(streamService, clusterEventBus, clusterConfigService);
+        periodical = new DefaultStreamMigrationPeriodical(streamService,
+                streamRuleService,
+                elasticsearchConfiguration,
+                Collections.singletonMap("test", () -> rotationStrategy),
+                Collections.singletonMap("test", () -> retentionStrategy),
+                indexSetService,
+                clusterEventBus,
+                clusterConfigService);
     }
 
     @Test
     public void doRunCreatesStreamAndStreamRule() throws Exception {
         final ArgumentCaptor<Stream> streamArgumentCaptor = ArgumentCaptor.forClass(Stream.class);
 
-        periodical.doRun();
+        periodical.doRunDefaultStreamMigration();
 
         verify(streamService).save(streamArgumentCaptor.capture());
 
@@ -73,7 +111,7 @@ public class DefaultStreamMigrationPeriodicalTest {
     @Test
     public void doRunPostsStreamsChangedEvent() throws Exception {
         final ArgumentCaptor<StreamsChangedEvent> argumentCaptor = ArgumentCaptor.forClass(StreamsChangedEvent.class);
-        periodical.doRun();
+        periodical.doRunDefaultStreamMigration();
         verify(clusterEventBus).post(argumentCaptor.capture());
 
         final StreamsChangedEvent streamsChangedEvent = argumentCaptor.getValue();
@@ -84,14 +122,16 @@ public class DefaultStreamMigrationPeriodicalTest {
     public void doRunDoesNotCreateStreamRuleIfStreamCreationFails() throws Exception {
         when(streamService.save(any(Stream.class))).thenThrow(ValidationException.class);
 
-        periodical.doRun();
+        periodical.doRunDefaultStreamMigration();
+
+        verifyNoMoreInteractions(streamRuleService);
     }
 
     @Test
     public void doRunDoesNotPostStreamsChangedEventIfStreamCreationFails() throws Exception {
         when(streamService.save(any(Stream.class))).thenThrow(ValidationException.class);
 
-        periodical.doRun();
+        periodical.doRunDefaultStreamMigration();
 
         verifyNoMoreInteractions(clusterEventBus);
     }
@@ -100,10 +140,109 @@ public class DefaultStreamMigrationPeriodicalTest {
     public void doRunDoesNotSaveDefaultStreamCreatedClusterConfigIfStreamCreationFails() throws Exception {
         when(streamService.save(any(Stream.class))).thenThrow(ValidationException.class);
 
-        periodical.doRun();
+        periodical.doRunDefaultStreamMigration();
 
         verifyNoMoreInteractions(clusterConfigService);
     }
+
+    @Test
+    public void doRunCreatesDefaultIndexSet() throws Exception {
+        final StubRotationStrategyConfig rotationStrategyConfig = new StubRotationStrategyConfig();
+        final StubRetentionStrategyConfig retentionStrategyConfig = new StubRetentionStrategyConfig();
+        final IndexSetConfig savedIndexSetConfig = IndexSetConfig.builder()
+                .id("id")
+                .title("title")
+                .indexPrefix("prefix")
+                .shards(1)
+                .replicas(0)
+                .rotationStrategy(rotationStrategyConfig)
+                .retentionStrategy(retentionStrategyConfig)
+                .creationDate(ZonedDateTime.of(2016, 10, 12, 0, 0, 0, 0, ZoneOffset.UTC))
+                .build();
+        when(clusterConfigService.get(IndexManagementConfig.class)).thenReturn(IndexManagementConfig.create("test", "test"));
+        when(clusterConfigService.get(StubRotationStrategyConfig.class)).thenReturn(rotationStrategyConfig);
+        when(clusterConfigService.get(StubRetentionStrategyConfig.class)).thenReturn(retentionStrategyConfig);
+        when(indexSetService.save(any(IndexSetConfig.class))).thenReturn(savedIndexSetConfig);
+
+        final ArgumentCaptor<IndexSetConfig> indexSetConfigCaptor = ArgumentCaptor.forClass(IndexSetConfig.class);
+
+        periodical.doRunIndexSetMigration();
+
+        verify(indexSetService).save(indexSetConfigCaptor.capture());
+        verify(clusterConfigService).write(DefaultStreamMigrationPeriodical.IndexSetMigrated.create());
+
+        final IndexSetConfig capturedIndexSetConfig = indexSetConfigCaptor.getValue();
+        assertThat(capturedIndexSetConfig.id()).isNull();
+        assertThat(capturedIndexSetConfig.title()).isEqualTo("Default index set");
+        assertThat(capturedIndexSetConfig.description()).isEqualTo("The Graylog default index set");
+        assertThat(capturedIndexSetConfig.indexPrefix()).isEqualTo(elasticsearchConfiguration.getIndexPrefix());
+        assertThat(capturedIndexSetConfig.shards()).isEqualTo(elasticsearchConfiguration.getShards());
+        assertThat(capturedIndexSetConfig.replicas()).isEqualTo(elasticsearchConfiguration.getReplicas());
+        assertThat(capturedIndexSetConfig.rotationStrategy()).isInstanceOf(StubRotationStrategyConfig.class);
+        assertThat(capturedIndexSetConfig.retentionStrategy()).isInstanceOf(StubRetentionStrategyConfig.class);
+    }
+
+    @Test
+    public void doRunCreatesDefaultIndexSetWithDefaultRotationAndRetentionStrategyConfig() throws Exception {
+        final StubRotationStrategyConfig rotationStrategyConfig = new StubRotationStrategyConfig();
+        final StubRetentionStrategyConfig retentionStrategyConfig = new StubRetentionStrategyConfig();
+        final IndexSetConfig savedIndexSetConfig = IndexSetConfig.builder()
+                .id("id")
+                .title("title")
+                .indexPrefix("prefix")
+                .shards(1)
+                .replicas(0)
+                .rotationStrategy(rotationStrategyConfig)
+                .retentionStrategy(retentionStrategyConfig)
+                .creationDate(ZonedDateTime.of(2016, 10, 12, 0, 0, 0, 0, ZoneOffset.UTC))
+                .build();
+        when(clusterConfigService.get(IndexManagementConfig.class)).thenReturn(IndexManagementConfig.create("test", "test"));
+        when(clusterConfigService.get(StubRotationStrategyConfig.class)).thenReturn(null);
+        when(clusterConfigService.get(StubRetentionStrategyConfig.class)).thenReturn(null);
+        when(indexSetService.save(any(IndexSetConfig.class))).thenReturn(savedIndexSetConfig);
+
+        final ArgumentCaptor<IndexSetConfig> indexSetConfigCaptor = ArgumentCaptor.forClass(IndexSetConfig.class);
+
+        periodical.doRunIndexSetMigration();
+
+        verify(indexSetService).save(indexSetConfigCaptor.capture());
+        verify(clusterConfigService).write(DefaultStreamMigrationPeriodical.IndexSetMigrated.create());
+
+        final IndexSetConfig capturedIndexSetConfig = indexSetConfigCaptor.getValue();
+        assertThat(capturedIndexSetConfig.rotationStrategy()).isInstanceOf(StubRotationStrategyConfig.class);
+        assertThat(capturedIndexSetConfig.retentionStrategy()).isInstanceOf(StubRetentionStrategyConfig.class);
+    }
+
+    @Test
+    public void doRunThrowsIllegalStateExceptionIfIndexManagementConfigIsMissing() throws Exception {
+        when(clusterConfigService.get(IndexManagementConfig.class)).thenReturn(null);
+
+        expectedException.expect(IllegalStateException.class);
+        expectedException.expectMessage("Couldn't find index management configuration");
+
+        periodical.doRunIndexSetMigration();
+    }
+
+    @Test
+    public void doRunThrowsIllegalStateExceptionIfRotationStrategyIsMissing() throws Exception {
+        when(clusterConfigService.get(IndexManagementConfig.class)).thenReturn(IndexManagementConfig.create("foobar", "test"));
+
+        expectedException.expect(IllegalStateException.class);
+        expectedException.expectMessage("Couldn't retrieve rotation strategy provider for <foobar>");
+
+        periodical.doRunIndexSetMigration();
+    }
+
+    @Test
+    public void doRunThrowsIllegalStateExceptionIfRetentionStrategyIsMissing() throws Exception {
+        when(clusterConfigService.get(IndexManagementConfig.class)).thenReturn(IndexManagementConfig.create("test", "foobar"));
+
+        expectedException.expect(IllegalStateException.class);
+        expectedException.expectMessage("Couldn't retrieve retention strategy provider for <foobar>");
+
+        periodical.doRunIndexSetMigration();
+    }
+
 
     @Test
     public void runsForeverReturnsTrue() throws Exception {
@@ -121,15 +260,60 @@ public class DefaultStreamMigrationPeriodicalTest {
     }
 
     @Test
-    public void startOnThisNodeReturnsFalseIfDefaultStreamHasBeenCreatedBefore() throws Exception {
+    public void testDoRunIfDefaultStreamHasBeenCreatedBefore() throws Exception {
+        final DefaultStreamMigrationPeriodical periodicalSpy = spy(periodical);
+
         final Stream stream = mock(Stream.class);
         when(streamService.load(Stream.DEFAULT_STREAM_ID)).thenReturn(stream);
-        assertThat(periodical.startOnThisNode()).isFalse();
+        doNothing().when(periodicalSpy).doRunDefaultStreamMigration();
+        doNothing().when(periodicalSpy).doRunIndexSetMigration();
+
+        periodicalSpy.doRun();
+
+        verify(periodicalSpy, never()).doRunDefaultStreamMigration();
     }
 
     @Test
-    public void startOnThisNodeReturnsTrueIfStreamDoesNotExist() throws Exception {
+    public void testDoRunIfDefaultStreamDoesNotExist() throws Exception {
+        final DefaultStreamMigrationPeriodical periodicalSpy = spy(periodical);
+
         when(streamService.load("000000000000000000000001")).thenThrow(NotFoundException.class);
+        doNothing().when(periodicalSpy).doRunDefaultStreamMigration();
+        doNothing().when(periodicalSpy).doRunIndexSetMigration();
+
+        periodicalSpy.doRun();
+
+        verify(periodicalSpy, times(1)).doRunDefaultStreamMigration();
+    }
+
+    @Test
+    public void testDoRunIfIndexSetHasBeenCreated() throws Exception {
+        final DefaultStreamMigrationPeriodical periodicalSpy = spy(periodical);
+
+        when(clusterConfigService.get(DefaultStreamMigrationPeriodical.IndexSetMigrated.class)).thenReturn(DefaultStreamMigrationPeriodical.IndexSetMigrated.create());
+        doNothing().when(periodicalSpy).doRunDefaultStreamMigration();
+        doNothing().when(periodicalSpy).doRunIndexSetMigration();
+
+        periodicalSpy.doRun();
+
+        verify(periodicalSpy, never()).doRunIndexSetMigration();
+    }
+
+    @Test
+    public void testDoRunIfIndexSetHasNotBeenCreated() throws Exception {
+        final DefaultStreamMigrationPeriodical periodicalSpy = spy(periodical);
+
+        when(clusterConfigService.get(DefaultStreamMigrationPeriodical.IndexSetMigrated.class)).thenReturn(null);
+        doNothing().when(periodicalSpy).doRunDefaultStreamMigration();
+        doNothing().when(periodicalSpy).doRunIndexSetMigration();
+
+        periodicalSpy.doRun();
+
+        verify(periodicalSpy, times(1)).doRunIndexSetMigration();
+    }
+
+    @Test
+    public void startOnThisNodeReturnsTrue() throws Exception {
         assertThat(periodical.startOnThisNode()).isTrue();
     }
 
@@ -151,5 +335,51 @@ public class DefaultStreamMigrationPeriodicalTest {
     @Test
     public void getLoggerReturnsClassLogger() throws Exception {
         assertThat(periodical.getLogger().getName()).isEqualTo(DefaultStreamMigrationPeriodical.class.getCanonicalName());
+    }
+
+    private static class StubRotationStrategy implements RotationStrategy {
+        @Override
+        public void rotate(IndexSet indexSet) {
+        }
+
+        @Override
+        public Class<? extends RotationStrategyConfig> configurationClass() {
+            return StubRotationStrategyConfig.class;
+        }
+
+        @Override
+        public RotationStrategyConfig defaultConfiguration() {
+            return new StubRotationStrategyConfig();
+        }
+    }
+
+    private static class StubRotationStrategyConfig implements RotationStrategyConfig {
+        @Override
+        public String type() {
+            return StubRotationStrategy.class.getCanonicalName();
+        }
+    }
+
+    private static class StubRetentionStrategy implements RetentionStrategy {
+        @Override
+        public void retain(IndexSet indexSet) {
+        }
+
+        @Override
+        public Class<? extends RetentionStrategyConfig> configurationClass() {
+            return StubRetentionStrategyConfig.class;
+        }
+
+        @Override
+        public RetentionStrategyConfig defaultConfiguration() {
+            return new StubRetentionStrategyConfig();
+        }
+    }
+
+    private static class StubRetentionStrategyConfig implements RetentionStrategyConfig {
+        @Override
+        public String type() {
+            return StubRetentionStrategy.class.getCanonicalName();
+        }
     }
 }
