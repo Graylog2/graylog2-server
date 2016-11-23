e501b8af94fa7b6f62ee2c65bae039f4c006c68c
Bernd Ahlers <bernd@graylog.com>
2016-11-23 12:04:35 +0100

    Fix eslint errors in StreamForm component and convert to ES6 style

--- graylog2-web-interface/src/components/streams/StreamForm.jsx
+++ graylog2-web-interface/src/components/streams/StreamForm.jsx
@@ -1,65 +1,78 @@
-'use strict';
+import React from 'react';
+import LinkedStateMixin from 'react-addons-linked-state-mixin';
+import BootstrapModalForm from 'components/bootstrap/BootstrapModalForm';
+import { Input } from 'react-bootstrap';
 
-var React = require('react');
-var LinkedStateMixin = require('react-addons-linked-state-mixin');
-var BootstrapModalForm = require('../bootstrap/BootstrapModalForm');
-var Input = require('react-bootstrap').Input;
+const StreamForm = React.createClass({
+  propTypes: {
+    onSubmit: React.PropTypes.func.isRequired,
+    stream: React.PropTypes.object.isRequired,
+    title: React.PropTypes.string.isRequired,
+  },
 
-var StreamForm = React.createClass({
-    propTypes: {
-      onSubmit: React.PropTypes.func.isRequired,
-    },
-    mixins: [LinkedStateMixin],
-    getInitialState() {
-        return this._getValuesFromProps(this.props);
-    },
-    getDefaultProps() {
-        return {
-            stream: {
-                title: "",
-                description: "",
-                remove_matches_from_default_stream: false
-            }
-        };
-    },
-    _resetValues() {
-        this.setState(this._getValuesFromProps(this.props));
-    },
-    _getValuesFromProps(props) {
-        return {
-            title: props.stream.title,
-            description: props.stream.description,
-            remove_matches_from_default_stream: props.stream.remove_matches_from_default_stream
-        };
-    },
-    _onSubmit(evt) {
-        this.props.onSubmit(this.props.stream.id,
-            {
-                title: this.state.title,
-                description: this.state.description,
-                remove_matches_from_default_stream: this.state.remove_matches_from_default_stream,
-            });
-        this.refs.modal.close();
-    },
-    open() {
-        this._resetValues();
-        this.refs.modal.open();
-    },
-    close() {
-        this.refs.modal.close();
-    },
-    render() {
-        return (
-            <BootstrapModalForm ref="modal"
-                                title={this.props.title}
-                                onSubmitForm={this._onSubmit}
-                                submitButtonText="Save">
-                <Input type='text' required={true} label='Title' placeholder='A descriptive name of the new stream' valueLink={this.linkState('title')} autoFocus />
-                <Input type='text' required={true} label='Description' placeholder='What kind of messages are routed into this stream?' valueLink={this.linkState('description')}/>
-                <Input type="checkbox" label="Remove matches from 'All messages' stream" checkedLink={this.linkState('remove_matches_from_default_stream')}/>
-            </BootstrapModalForm>
-        );
-    }
+  mixins: [LinkedStateMixin],
+
+  getDefaultProps() {
+    return {
+      stream: {
+        title: '',
+        description: '',
+        remove_matches_from_default_stream: false,
+      },
+    };
+  },
+
+  getInitialState() {
+    return this._getValuesFromProps(this.props);
+  },
+
+  _resetValues() {
+    this.setState(this._getValuesFromProps(this.props));
+  },
+
+  _getValuesFromProps(props) {
+    return {
+      title: props.stream.title,
+      description: props.stream.description,
+      remove_matches_from_default_stream: props.stream.remove_matches_from_default_stream,
+    };
+  },
+
+  _onSubmit() {
+    this.props.onSubmit(this.props.stream.id,
+      {
+        title: this.state.title,
+        description: this.state.description,
+        remove_matches_from_default_stream: this.state.remove_matches_from_default_stream,
+      });
+    this.refs.modal.close();
+  },
+
+  open() {
+    this._resetValues();
+    this.refs.modal.open();
+  },
+
+  close() {
+    this.refs.modal.close();
+  },
+
+  render() {
+    return (
+      <BootstrapModalForm ref="modal"
+                          title={this.props.title}
+                          onSubmitForm={this._onSubmit}
+                          submitButtonText="Save">
+        <Input type="text" required label="Title" placeholder="A descriptive name of the new stream"
+               valueLink={this.linkState('title')} autoFocus/>
+        <Input type="text" required label="Description"
+               placeholder="What kind of messages are routed into this stream?"
+               valueLink={this.linkState('description')}/>
+        <Input type="checkbox" label="Remove matches from 'All messages' stream"
+               checkedLink={this.linkState('remove_matches_from_default_stream')}/>
+      </BootstrapModalForm>
+    );
+  },
 });
 
-module.exports = StreamForm;
+export default StreamForm;
