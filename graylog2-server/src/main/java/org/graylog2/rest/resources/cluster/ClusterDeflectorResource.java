a03ff11f801c31927e630622087d9945d6fad924
Bernd Ahlers <bernd@graylog.com>
2016-11-30 16:51:18 +0100

    Add missing /cluster/deflector/{indexSet}/cycle endpoint
    Fixes manual index rotation for an index set.

--- graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterDeflectorResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterDeflectorResource.java
@@ -19,6 +19,7 @@ package org.graylog2.rest.resources.cluster;
 import com.codahale.metrics.annotation.Timed;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.graylog2.audit.jersey.NoAuditEvent;
 import org.graylog2.cluster.Node;
@@ -32,6 +33,7 @@ import javax.inject.Named;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.ServiceUnavailableException;
 import javax.ws.rs.core.Context;
@@ -61,15 +63,31 @@ public class ClusterDeflectorResource extends ProxiedResource {
     @Path("/cycle")
     @NoAuditEvent("this is a proxy resource, the event will be triggered on the individual nodes")
     public void cycle() throws IOException {
-        final Optional<Node> master = nodeService.allActive().values().stream().filter(Node::isMaster).findFirst();
-        if (!master.isPresent()) {
-            throw new ServiceUnavailableException("No master present.");
-        }
-        final Function<String, Optional<RemoteDeflectorResource>> remoteInterfaceProvider = createRemoteInterfaceProvider(RemoteDeflectorResource.class);
-        final Optional<RemoteDeflectorResource> deflectorResource = remoteInterfaceProvider.apply(master.get().getNodeId());
+        getDeflectorResource().cycle().execute();
+    }
 
-        deflectorResource
-            .orElseThrow(() -> new InternalServerErrorException("Unable to get remote deflector resource."))
-            .cycle().execute();
+    @POST
+    @Timed
+    @ApiOperation(value = "Finds master node and triggers deflector cycle")
+    @Path("/{indexSetId}/cycle")
+    @NoAuditEvent("this is a proxy resource, the event will be triggered on the individual nodes")
+    public void cycle(@ApiParam(name = "indexSetId") @PathParam("indexSetId") String indexSetId) throws IOException {
+        getDeflectorResource().cycleIndexSet(indexSetId).execute();
+    }
+
+    private RemoteDeflectorResource getDeflectorResource() {
+        final Node master = findMasterNode();
+        final Function<String, Optional<RemoteDeflectorResource>> remoteInterfaceProvider = createRemoteInterfaceProvider(RemoteDeflectorResource.class);
+        final Optional<RemoteDeflectorResource> deflectorResource = remoteInterfaceProvider.apply(master.getNodeId());
+
+        return deflectorResource
+                .orElseThrow(() -> new InternalServerErrorException("Unable to get remote deflector resource."));
+    }
+
+    private Node findMasterNode() {
+        return nodeService.allActive().values().stream()
+                .filter(Node::isMaster)
+                .findFirst()
+                .orElseThrow(() -> new ServiceUnavailableException("No master present."));
     }
 }
--- graylog2-server/src/main/java/org/graylog2/shared/rest/resources/system/RemoteDeflectorResource.java
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/resources/system/RemoteDeflectorResource.java
@@ -18,8 +18,12 @@ package org.graylog2.shared.rest.resources.system;
 
 import retrofit2.Call;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface RemoteDeflectorResource {
     @POST("system/deflector/cycle")
     Call<Void> cycle();
+
+    @POST("system/deflector/{indexSetId}/cycle")
+    Call<Void> cycleIndexSet(@Path("indexSetId") String indexSetId);
 }
