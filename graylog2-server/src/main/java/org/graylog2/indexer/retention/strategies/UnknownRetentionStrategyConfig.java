c395cdfa337b4f4d5a9a3d9e2b4cd9be9d46e792
Kay Roepke <kroepke@googlemail.com>
2016-12-01 14:04:28 +0100

    handle unavailable retention strategy configs gracefully
    this can happen when the a plugin-provided strategy isn't available

---
+++ graylog2-server/src/main/java/org/graylog2/indexer/retention/strategies/UnknownRetentionStrategyConfig.java
@@ -0,0 +1,47 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.indexer.retention.strategies;
+
+import com.google.auto.value.AutoValue;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import org.graylog2.plugin.indexer.retention.RetentionStrategyConfig;
+
+/**
+ * This is being used as the fallback {@link RetentionStrategyConfig} if the requested class is not
+ * available (usually because it was contributed by a plugin which is not loaded).
+ * <p>
+ * By itself it does nothing useful except accepting all properties but not exposing them.
+ */
+@JsonAutoDetect
+@AutoValue
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract class UnknownRetentionStrategyConfig implements RetentionStrategyConfig {
+
+    @JsonCreator
+    public static UnknownRetentionStrategyConfig create() {
+        return new AutoValue_UnknownRetentionStrategyConfig(UnknownRetentionStrategyConfig.class.getCanonicalName());
+    }
+
+
+    public static UnknownRetentionStrategyConfig createDefault() {
+        return create();
+    }
+}
