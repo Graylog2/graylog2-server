08687a1629490e06f659ec0e3e1c9948412777d1
Edmundo Alvarez <edmundo@graylog.com>
2016-12-14 14:50:46 +0100

    Remove old components
    These components were replaced with those under `alertnotifications`.

--- graylog2-web-interface/src/components/alarmcallbacks/index.jsx
+++ graylog2-web-interface/src/components/alarmcallbacks/index.jsx
@@ -1,8 +1,2 @@
-export { default as AlarmCallback } from './AlarmCallback';
-export { default as AlarmCallbackComponent } from './AlarmCallbackComponent';
 export { default as AlarmCallbackHistory } from './AlarmCallbackHistory';
 export { default as AlarmCallbackHistoryOverview } from './AlarmCallbackHistoryOverview';
-export { default as AlarmCallbackList } from './AlarmCallbackList';
-export { default as CreateAlarmCallbackButton } from './CreateAlarmCallbackButton';
-export { default as DeleteAlarmCallbackButton } from './DeleteAlarmCallbackButton';
-export { default as EditAlarmCallbackButton } from './EditAlarmCallbackButton';
