736c2e20b63fe5b7d798f6b934779359db676413
Edmundo Alvarez <edmundo@graylog.com>
2016-12-14 14:47:37 +0100

    Use new store to get available callback types
    Previously, we fetched the available callback types per stream, using
    `AlarmCallbacksActions` and `AlarmCallbacksStore`, but now we only need
    to get them globally, and we can do that by using
    `AlertNotificationsActions` and `AlertNotificationsStore`.

--- graylog2-web-interface/src/components/alarmcallbacks/AlarmCallbackHistoryOverview.jsx
+++ graylog2-web-interface/src/components/alarmcallbacks/AlarmCallbackHistoryOverview.jsx
@@ -4,7 +4,7 @@ import { Row, Col } from 'react-bootstrap';
 
 import CombinedProvider from 'injection/CombinedProvider';
 const { AlarmCallbackHistoryStore } = CombinedProvider.get('AlarmCallbackHistory');
-const { AlarmCallbacksStore } = CombinedProvider.get('AlarmCallbacks');
+const { AlertNotificationsStore } = CombinedProvider.get('AlertNotifications');
 
 import { EntityList, Spinner } from 'components/common';
 import { AlarmCallbackHistory } from 'components/alarmcallbacks';
@@ -15,15 +15,15 @@ const AlarmCallbackHistoryOverview = React.createClass({
     streamId: React.PropTypes.string.isRequired,
   },
 
-  mixins: [Reflux.connect(AlarmCallbackHistoryStore), Reflux.connect(AlarmCallbacksStore)],
+  mixins: [Reflux.connect(AlarmCallbackHistoryStore), Reflux.connect(AlertNotificationsStore)],
 
   _formatHistory(history) {
     return (
-      <AlarmCallbackHistory key={history.id} alarmCallbackHistory={history} types={this.state.availableAlarmCallbacks}/>
+      <AlarmCallbackHistory key={history.id} alarmCallbackHistory={history} types={this.state.availableNotifications}/>
     );
   },
   _isLoading() {
-    return !(this.state.histories && this.state.availableAlarmCallbacks);
+    return !(this.state.histories && this.state.availableNotifications);
   },
   render() {
     if (this._isLoading()) {
