8a17bdd245a1073ab1df94862bf253cb97ccf550
Bernd Ahlers <bernd@graylog.com>
2016-12-16 17:47:35 +0100

    Let IllegalStateException bubble up instead of returning a 404

--- graylog2-server/src/main/java/org/graylog2/rest/resources/system/DeflectorResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/system/DeflectorResource.java
@@ -39,7 +39,6 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -70,12 +69,8 @@ public class DeflectorResource extends RestResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Deprecated
     public DeflectorSummary deprecatedDeflector() throws TooManyAliasesException {
-        try {
-            final IndexSet indexSet = indexSetRegistry.getDefault();
-            return DeflectorSummary.create(indexSet.isUp(), indexSet.getCurrentActualTargetIndex());
-        } catch (IllegalStateException e) {
-            throw new NotFoundException("Default index set not found");
-        }
+        final IndexSet indexSet = indexSetRegistry.getDefault();
+        return DeflectorSummary.create(indexSet.isUp(), indexSet.getCurrentActualTargetIndex());
     }
 
     @GET
@@ -99,19 +94,15 @@ public class DeflectorResource extends RestResource {
     @AuditEvent(type = AuditEventTypes.ES_WRITE_INDEX_UPDATE_JOB_START)
     @Deprecated
     public void deprecatedCycle() {
-        try {
-            final IndexSet indexSet = indexSetRegistry.getDefault();
+        final IndexSet indexSet = indexSetRegistry.getDefault();
 
-            checkCycle(indexSet);
+        checkCycle(indexSet);
 
-            final String msg = "Cycling deflector for default index set <" + indexSet.getConfig().id() + ">. Reason: REST request.";
-            LOG.info(msg);
-            activityWriter.write(new Activity(msg, DeflectorResource.class));
+        final String msg = "Cycling deflector for default index set <" + indexSet.getConfig().id() + ">. Reason: REST request.";
+        LOG.info(msg);
+        activityWriter.write(new Activity(msg, DeflectorResource.class));
 
-            indexSet.cycle();
-        } catch (IllegalStateException e) {
-            throw new NotFoundException("Default index set not found");
-        }
+        indexSet.cycle();
     }
 
     @POST
