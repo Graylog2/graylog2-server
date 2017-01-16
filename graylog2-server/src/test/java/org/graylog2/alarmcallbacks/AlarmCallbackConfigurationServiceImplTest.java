76331bfad3a315a99de9f9cf7f84911ebe1b9042
Dennis Oelkers <dennis@graylog.com>
2017-01-16 14:40:28 +0100

    Renaming test suite for AlarmCallbackConfigurationServiceImpl.

--- graylog2-server/src/test/java/org/graylog2/alarmcallbacks/AlarmCallbackConfigurationServiceImplTest.java
+++ graylog2-server/src/test/java/org/graylog2/alarmcallbacks/AlarmCallbackConfigurationServiceImplTest.java
@@ -35,7 +35,7 @@ import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class AlarmCallbackConfigurationServiceMJImplTest extends MongoDBServiceTest {
+public class AlarmCallbackConfigurationServiceImplTest extends MongoDBServiceTest {
     private AlarmCallbackConfigurationService alarmCallbackConfigurationService;
 
     @Before
