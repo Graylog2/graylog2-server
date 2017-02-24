81cd471f7c1a4b3db7fe7836d967e44800dba185
Dennis Oelkers <dennis@graylog.com>
2017-02-24 12:09:27 +0100

    Adapting webpack config to webpack 2. (#34)

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
 
 module.exports = new PluginWebpackConfig('org.graylog.plugins.map.MapWidgetPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
-  loaders: [
-    { test: /\.ts$/, loader: 'babel-loader!ts-loader', exclude: /node_modules|\.node_cache/ },
-  ],
+  module: {
+    rules: [
+      { test: /\.ts$/, use: ['babel-loader', 'ts-loader'], exclude: /node_modules|\.node_cache/ },
+    ],
+  },
   resolve: {
     extensions: ['.ts'],
   },
