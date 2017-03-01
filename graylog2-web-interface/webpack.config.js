322c2a07ed51af4dede2f25a170ea9cdeca02f86
Dennis Oelkers <dennis@graylog.com>
2017-03-01 16:59:04 +0100

    Removing HMR plugin, it is now being included automatically for --hot.

--- graylog2-web-interface/webpack.config.js
+++ graylog2-web-interface/webpack.config.js
@@ -114,7 +114,6 @@ if (TARGET === 'start') {
       hotUpdateMainFilename: 'hot-update.json',
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
       new webpack.NamedModulesPlugin(),
       new webpack.DefinePlugin({DEVELOPMENT: true}),
     ],
