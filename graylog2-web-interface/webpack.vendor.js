8b4ee9ec94cde5822cb3cc1a51551145bbbb1cd5
Dennis Oelkers <dennis@graylog.com>
2017-03-01 12:21:09 +0100

    Adding build mode for vendor bundle, cleaning build dir before.

--- graylog2-web-interface/webpack.vendor.js
+++ graylog2-web-interface/webpack.vendor.js
@@ -4,12 +4,16 @@ const path = require('path');
 const Clean = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const merge = require('webpack-merge');
 
 const ROOT_PATH = path.resolve(__dirname);
 const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 const MANIFESTS_PATH = path.resolve(ROOT_PATH, 'manifests');
 
 const vendorModules = require('./vendor.modules');
+
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
 
 console.error('Building vendor bundle.');
 
@@ -24,7 +28,7 @@ const webpackConfig = {
     library: '__[name]',
   },
   plugins: [
-    new Clean([path.resolve(BUILD_PATH, 'vendor.*.js')]),
+    new Clean([path.resolve(BUILD_PATH)]),
     new webpack.DllPlugin({
       path: path.resolve(MANIFESTS_PATH, '[name]-manifest.json'),
       name: '__[name]',
@@ -35,4 +39,24 @@ const webpackConfig = {
   recordsPath: path.resolve(ROOT_PATH, 'webpack/vendor-module-ids.json'),
 };
 
-module.exports = webpackConfig;
+if (TARGET === 'build') {
+  module.exports = merge(webpackConfig, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        sourceMap: true,
+        compress: {
+          warnings: false,
+        },
+        mangle: {
+          except: ['$super', '$', 'exports', 'require'],
+        },
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      }),
+    ],
+  });
+} else {
+  module.exports = webpackConfig;
+}
