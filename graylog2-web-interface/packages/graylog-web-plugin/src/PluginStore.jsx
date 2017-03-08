31e523bdf6ecbc2ee14ac1c92363c23b5c54e554
Dennis Oelkers <dennis@graylog.com>
2017-03-08 13:59:09 +0100

    Provide method to unregister a plugin's manifest from the PluginStore.

--- graylog2-web-interface/packages/graylog-web-plugin/src/PluginStore.jsx
+++ graylog2-web-interface/packages/graylog-web-plugin/src/PluginStore.jsx
@@ -6,6 +6,18 @@ class PluginStore {
     window.plugins.push(plugin);
   }
 
+  static unregister(plugin) {
+    if (!window.plugins) {
+      return;
+    }
+
+    window.plugins.forEach((item, idx) => {
+      if (item.metadata && plugin.metadata && item.metadata.name === plugin.metadata.name) {
+        window.plugins.splice(idx, 1);
+      }
+    });
+  }
+
   static get() {
     if (!window.plugins) {
       window.plugins = [];
