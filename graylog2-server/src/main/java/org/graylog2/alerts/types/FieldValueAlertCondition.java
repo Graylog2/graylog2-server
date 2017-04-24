63862f928b8e431d43061d0953d3ad9ae20bb712
Jochen Schalanda <jochen@graylog.com>
2017-04-24 11:00:16 +0200

    Remove unused exception classes

--- graylog2-server/src/main/java/org/graylog2/alerts/types/FieldValueAlertCondition.java
+++ graylog2-server/src/main/java/org/graylog2/alerts/types/FieldValueAlertCondition.java
@@ -20,7 +20,6 @@ import com.google.common.collect.Lists;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import org.graylog2.alerts.AbstractAlertCondition;
-import org.graylog2.indexer.InvalidRangeFormatException;
 import org.graylog2.indexer.results.FieldStatsResult;
 import org.graylog2.indexer.results.ResultMessage;
 import org.graylog2.indexer.searches.Searches;
@@ -247,10 +246,6 @@ public class FieldValueAlertCondition extends AbstractAlertCondition {
         } catch (InvalidRangeParametersException e) {
             // cannot happen lol
             LOG.error("Invalid timerange.", e);
-            return null;
-        } catch (InvalidRangeFormatException e) {
-            // lol same here
-            LOG.error("Invalid timerange format.", e);
             return null;
         } catch (Searches.FieldTypeException e) {
             LOG.debug("Field [{}] seems not to have a numerical type or doesn't even exist at all. Returning not triggered.", field, e);
