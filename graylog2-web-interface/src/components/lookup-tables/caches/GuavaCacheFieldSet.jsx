49ee665dca5d1d34421112014923eb0a6b530658
kroepke <kroepke@googlemail.com>
2017-04-24 18:16:11 +0200

    expose simple guava cache configuration

--- graylog2-web-interface/src/components/lookup-tables/caches/GuavaCacheFieldSet.jsx
+++ graylog2-web-interface/src/components/lookup-tables/caches/GuavaCacheFieldSet.jsx
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 
+import { Input } from 'components/bootstrap';
+
 const NullCacheFieldSet = React.createClass({
   propTypes: {
     config: PropTypes.object.isRequired,
@@ -7,7 +9,21 @@ const NullCacheFieldSet = React.createClass({
   },
 
   render() {
-    return null;
+    const config = this.props.config;
+
+    return (<fieldset>
+      <Input type="text"
+             id="max_size"
+             name="max_size"
+             label="Maximum entries"
+             autoFocus
+             required
+             onChange={this.props.onChange}
+             help="The limit of the number of entries the cache keeps in memory."
+             value={config.max_size}
+             labelClassName="col-sm-3"
+             wrapperClassName="col-sm-9" />
+    </fieldset>);
   },
 });
 
