36504c25853c6b4309683bebcec1fe94f2309f63
Dennis Oelkers <dennis@graylog.com>
2017-04-25 16:12:10 +0200

    Combine vendor and app webpack builds. (#3576)
    * Creating multi compiler build, combining vendor and app bundles.
    
    * Adapting syntax to webpack 2.
    
    * Employing plugin to make sure module manifest is written for dev too.
    
    * Replacing names of webpack config files.
    
    * Supplying manifest as a path, so it is resolved at the right time.
    
    * Retrieving vendor asset filename from vendor manifest.
    
    * Refqctoring plugin lookup and config merging.
    
    * Adding build mode for vendor bundle, cleaning build dir before.
    
    * Removing HMR plugin, it is now being included automatically for --hot.
    
    * Using chunkhashed filename for production builds only.
    
    * Explicitly using hot reloading for start, adding history api fallback.
    
    * Do not include plugins in production build.
    
    * Revert "Do not include plugins in production build."
    
    This reverts commit cd67aeb321c3e28c4bad2cfb43529911d79071d4.
    
    Commit included unnecessarily modified vendor module ids.
    
    * Do not include plugins in production build. (Take 2)
    
    * Replacing html with assets plugin to prevent entries in module ids file.
    
    * Running clean task only for production build of vendor bundle.
    
    * Passing in path to vendor module in template via loader options.
    
    * Replacing yucky JS template with standard EJS template again.
    
    * Load vendor files first.

--- graylog2-web-interface/webpack.vendor.js
+++ graylog2-web-interface/webpack.vendor.js
@@ -2,6 +2,8 @@
 const webpack = require('webpack');
 const path = require('path');
 const Clean = require('clean-webpack-plugin');
+const AssetsPlugin = require('assets-webpack-plugin')
+const merge = require('webpack-merge');
 
 const ROOT_PATH = path.resolve(__dirname);
 const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
@@ -9,9 +11,13 @@ const MANIFESTS_PATH = path.resolve(ROOT_PATH, 'manifests');
 
 const vendorModules = require('./vendor.modules');
 
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
 console.error('Building vendor bundle.');
 
 const webpackConfig = {
+  name: 'vendor',
   entry: {
     vendor: vendorModules,
   },
@@ -21,13 +27,61 @@ const webpackConfig = {
     library: '__[name]',
   },
   plugins: [
-    new Clean([path.resolve(BUILD_PATH, 'vendor.*.js')]),
     new webpack.DllPlugin({
       path: path.resolve(MANIFESTS_PATH, '[name]-manifest.json'),
       name: '__[name]',
     }),
+    new AssetsPlugin({ filename: 'vendor-module.json', path: BUILD_PATH, processOutput: function (assets) {
+      const jsfiles = [];
+      const cssfiles = [];
+      const chunks = {};
+      Object.keys(assets).forEach((chunk) => {
+        if (assets[chunk].js) {
+          jsfiles.push(assets[chunk].js);
+        }
+        if (assets[chunk].css) {
+          jsfiles.push(assets[chunk].css);
+        }
+        chunks[chunk] = {
+          size: 0,
+          entry: assets[chunk].js,
+          css: assets[chunk].css || []
+        };
+      });
+      return JSON.stringify({
+        files: {
+          js: jsfiles,
+          css: cssfiles,
+          chunks: chunks
+        }
+      });
+    } })
   ],
   recordsPath: path.resolve(ROOT_PATH, 'webpack/vendor-module-ids.json'),
 };
 
-module.exports = webpackConfig;
+if (TARGET === 'build') {
+  module.exports = merge(webpackConfig, {
+    plugins: [
+      new Clean([path.resolve(BUILD_PATH)]),
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        sourceMap: true,
+        compress: {
+          warnings: false,
+        },
+        mangle: {
+          except: ['$super', '$', 'exports', 'require'],
+        },
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      }),
+    ],
+    output: {
+      filename: '[name].[chunkhash].js',
+    },
+  });
+} else {
+  module.exports = webpackConfig;
+}
