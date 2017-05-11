dff15a495c90a91260964cdb15337b536072a8e7
Jochen Schalanda <jochen@graylog.com>
2017-05-11 18:51:39 +0200

    Extend UI for Date converter to include locale

---
+++ graylog2-web-interface/src/components/common/LocaleSelect.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Reflux from 'reflux';
+
+import Select from "components/common/Select";
+
+import StoreProvider from "injection/StoreProvider";
+const SystemStore = StoreProvider.getStore('System');
+
+const LocaleSelect = React.createClass({
+  mixins: [Reflux.connect(SystemStore)],
+  propTypes: {
+    onChange: React.PropTypes.func,
+  },
+  getValue() {
+    return this.refs.locale.getValue();
+  },
+  _formatLocales(locales) {
+    return Object.values(locales).map(locale => {
+      return {value: locale['language_tag'], label: locale['display_name']}
+    }).sort(function(a, b) {
+        const nameA = a.label.toUpperCase();
+        const nameB = b.label.toUpperCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+
+        return 0;
+    });
+  },
+  _renderOption(option) {
+      return <span key={option.value} title="{option.value} [{option.value}]">{option.label} [{option.value}]</span>;
+  },
+  render() {
+    if (!this.state.locales) {
+      return <Spinner />;
+    }
+
+    const locales = this._formatLocales(this.state.locales);
+    return (
+      <Select ref="locale" {...this.props}
+              placeholder="Pick a locale"
+              options={locales}
+              optionRenderer={this._renderOption} />
+    );
+  },
+});
+
+export default LocaleSelect;
--- graylog2-web-interface/src/stores/system/SystemStore.js
+++ graylog2-web-interface/src/stores/system/SystemStore.js
@@ -6,14 +6,19 @@ import fetch from 'logic/rest/FetchProvider';
 
 const SystemStore = Reflux.createStore({
   system: undefined,
+  locales: undefined,
   init() {
     this.info().then((response) => {
       this.trigger({ system: response });
       this.system = response;
     });
+    this.systemLocales().then((response) => {
+      this.trigger({ locales: response });
+      this.locales = response.locales;
+    });
   },
   getInitialState() {
-    return { system: this.system };
+    return { system: this.system, locales: this.locales };
   },
   info() {
     const url = URLUtils.qualifyUrl(ApiRoutes.SystemApiController.info().url);
@@ -22,6 +27,11 @@ const SystemStore = Reflux.createStore({
   },
   jvm() {
     const url = URLUtils.qualifyUrl(ApiRoutes.SystemApiController.jvm().url);
+
+    return fetch('GET', url);
+  },
+  systemLocales() {
+    const url = URLUtils.qualifyUrl(ApiRoutes.SystemApiController.locales().url);
 
     return fetch('GET', url);
   },
