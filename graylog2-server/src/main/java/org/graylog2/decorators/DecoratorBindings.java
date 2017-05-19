bfa084c9219acba487ea254e39f4ebe85325a8c5
Bernd Ahlers <bernd@graylog.com>
2017-05-19 16:51:24 +0200

    Add Lookup Table search result decorator
    Closes #3844

--- graylog2-server/src/main/java/org/graylog2/decorators/DecoratorBindings.java
+++ graylog2-server/src/main/java/org/graylog2/decorators/DecoratorBindings.java
@@ -31,5 +31,9 @@ public class DecoratorBindings extends Graylog2Module {
         installSearchResponseDecorator(searchResponseDecoratorBinder,
                                        FormatStringDecorator.class,
                                        FormatStringDecorator.Factory.class);
+
+        installSearchResponseDecorator(searchResponseDecoratorBinder,
+                                       LookupTableDecorator.class,
+                                       LookupTableDecorator.Factory.class);
     }
 }
