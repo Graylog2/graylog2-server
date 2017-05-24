74f3826e93fa1b67ecb757c33472ffd4b360609c
Jochen Schalanda <jochen@graylog.com>
2017-05-24 14:15:48 +0200

    Correctly use filter parameter in Searches#count()

--- graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
@@ -201,7 +201,7 @@ public class Searches {
 
     public CountResult count(String query, TimeRange range, String filter) {
         // limit, offset, and highlight are *NOT* supported by count queries
-        final SearchSourceBuilder searchSourceBuilder = standardSearchRequest(query, -1, -1, range, null, null, false);
+        final SearchSourceBuilder searchSourceBuilder = standardSearchRequest(query, -1, -1, range, filter, null, false);
         final Set<String> affectedIndices = determineAffectedIndices(range, filter);
         if (affectedIndices.isEmpty()) {
             return CountResult.empty();
