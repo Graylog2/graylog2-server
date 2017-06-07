cf350841e289ce1f169f9473da6dd3c1d07777dc
Dennis Oelkers <dennis@graylog.com>
2017-06-07 16:32:29 +0200

    Making annotation runtime retained and suitable for bindings.

--- graylog2-server/src/main/java/org/graylog2/bindings/annotations/ElasticsearchVersion.java
+++ graylog2-server/src/main/java/org/graylog2/bindings/annotations/ElasticsearchVersion.java
@@ -1,4 +1,11 @@
 package org.graylog2.bindings.annotations;
 
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@BindingAnnotation
 public @interface ElasticsearchVersion {
 }
