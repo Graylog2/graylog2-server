3be3ea7651d0f211744106aaca86a6ed937f8316
Bernd Ahlers <bernd@graylog.com>
2017-07-17 09:05:04 +0200

    Switch to graylog-plugin.properties and use graylog plugin parent

--- src/main/java/org/graylog/plugins/netflow/NetFlowPluginMetaData.java
+++ src/main/java/org/graylog/plugins/netflow/NetFlowPluginMetaData.java
@@ -25,6 +25,8 @@ import java.util.Collections;
 import java.util.Set;
 
 public class NetFlowPluginMetaData implements PluginMetaData {
+    private static final String PLUGIN_PROPERTIES = "org.graylog.plugins.graylog-plugin-netflow/graylog-plugin.properties";
+
     @Override
     public String getUniqueId() {
         return "org.graylog.plugins.netflow.NetFlowPlugin";
@@ -47,7 +49,7 @@ public class NetFlowPluginMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(0, 1, 0);
+        return Version.fromPluginProperties(this.getClass(), PLUGIN_PROPERTIES, "version", Version.from(0, 0, 0, "unknown"));
     }
 
     @Override
@@ -57,7 +59,7 @@ public class NetFlowPluginMetaData implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(2, 0, 0);
+        return Version.fromPluginProperties(this.getClass(), PLUGIN_PROPERTIES, "graylog.version", Version.CURRENT_CLASSPATH);
     }
 
     @Override
