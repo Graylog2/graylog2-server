b6e147fd3e29928c5d9e43f1dd1ad89cfb606b03
Dennis Oelkers <dennis@graylog.com>
2017-08-16 16:39:05 +0200

    Adding user specified HTTP headers to HTTP JSON data adapter.
    This allows the user to specify and manage HTTP headers for data
    adapters. These are required for all kinds of purposes, especially for
    access tokens required to access certain external services.

--- graylog2-web-interface/src/components/lookup-tables/adapters/HTTPJSONPathAdapterSummary.jsx
+++ graylog2-web-interface/src/components/lookup-tables/adapters/HTTPJSONPathAdapterSummary.jsx
@@ -1,25 +1,24 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const HTTPJSONPathAdapterSummary = React.createClass({
-  propTypes: {
-    dataAdapter: PropTypes.object.isRequired,
-  },
+const HTTPJSONPathAdapterSummary = ({ dataAdapter }) => {
+  const { config } = dataAdapter;
+  return (<dl>
+    <dt>Lookup URL</dt>
+    <dd>{config.url}</dd>
+    <dt>Single value JSONPath</dt>
+    <dd><code>{config.single_value_jsonpath}</code></dd>
+    <dt>Multi value JSONPath</dt>
+    <dd><code>{config.multi_value_jsonpath}</code></dd>
+    <dt>HTTP User-Agent</dt>
+    <dd>{config.user_agent}</dd>
+    <dt>HTTP Headers</dt>
+    <dd><pre>{config.headers ? Object.keys(config.headers).map(key => <p key={key} style={{ 'margin-bottom': 0 }}>{key}: {config.headers[key]}</p>) : <i>Empty</i>}</pre></dd>
+  </dl>);
+};
 
-  render() {
-    const config = this.props.dataAdapter.config;
-    return (<dl>
-      <dt>Lookup URL</dt>
-      <dd>{config.url}</dd>
-      <dt>Single value JSONPath</dt>
-      <dd><code>{config.single_value_jsonpath}</code></dd>
-      <dt>Multi value JSONPath</dt>
-      <dd><code>{config.multi_value_jsonpath}</code></dd>
-      <dt>HTTP User-Agent</dt>
-      <dd>{config.user_agent}</dd>
-    </dl>);
-  },
-});
-
+HTTPJSONPathAdapterSummary.propTypes = {
+  dataAdapter: PropTypes.object.isRequired,
+};
 
 export default HTTPJSONPathAdapterSummary;
