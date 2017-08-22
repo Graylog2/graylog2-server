9c844a9ad460f10ab5de52eee3123d1ace3a8dfa
Dennis Oelkers <dennis@graylog.com>
2017-08-22 17:03:01 +0200

    Add back search term highlighting (#4103)
    During the switch from an ES node client to an HTTP client, code relevant for parsing the highlighting information contained in the search response was removed.
    
    Fixes #4101

--- graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
@@ -39,7 +39,6 @@ import org.elasticsearch.common.Strings;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.filter.FilterAggregationBuilder;
 import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramBuilder;
@@ -278,7 +277,7 @@ public class Searches {
 
         final io.searchbox.core.SearchResult searchResult = checkForFailedShards(JestUtils.execute(jestClient, searchBuilder.build(), () -> "Unable to perform search query."));
         final List<ResultMessage> hits = searchResult.getHits(Map.class, false).stream()
-            .map(hit -> ResultMessage.parseFromSource(hit.id, hit.index, (Map<String, Object>) hit.source))
+            .map(hit -> ResultMessage.parseFromSource(hit.id, hit.index, (Map<String, Object>) hit.source, hit.highlight))
             .collect(Collectors.toList());
         final long tookMs = tookMsFromSearchResult(searchResult);
         recordEsMetrics(tookMs, config.range());
---
+++ graylog2-server/src/test/java/org/graylog2/indexer/results/HighlightParserTest.java
@@ -0,0 +1,103 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.indexer.results;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Range;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HighlightParserTest {
+    @Test
+    public void simpleHighlightAtStartOfString() throws Exception {
+        final Map<String, List<String>> highlights = ImmutableMap.of(
+                "message", ImmutableList.of("<em>last</em> message repeated 2 times")
+        );
+
+        final Multimap<String, Range<Integer>> result = HighlightParser.extractHighlightRanges(highlights);
+
+        assertThat(result).isNotNull();
+        assertThat(result.get("message")).isNotNull()
+                .isNotEmpty()
+                .containsExactly(Range.closed(0, 4));
+    }
+
+    @Test
+    public void multipleHighlights() throws Exception {
+        final Map<String, List<String>> highlights = ImmutableMap.of(
+                "message", ImmutableList.of("/<em>usr</em>/sbin/cron[22390]: (root) CMD (/<em>usr</em>/libexec/atrun)"),
+                "full_message", ImmutableList.of("<78>Aug 22 10:40:00 /<em>usr</em>/sbin/cron[22390]: (root) CMD (/<em>usr</em>/libexec/atrun)")
+        );
+
+        final Multimap<String, Range<Integer>> result = HighlightParser.extractHighlightRanges(highlights);
+
+        assertThat(result).isNotNull();
+        assertThat(result.get("message")).isNotNull()
+                .isNotEmpty()
+                .containsExactly(
+                        Range.closed(1, 4),
+                        Range.closed(36, 39)
+                );
+        assertThat(result.get("full_message")).isNotNull()
+                .isNotEmpty()
+                .containsExactly(
+                        Range.closed(21, 24),
+                        Range.closed(56, 59)
+                );
+    }
+
+    @Test
+    public void brokenHighlight() throws Exception {
+        final Map<String, List<String>> highlights = ImmutableMap.of(
+                "message", ImmutableList.of("/<em>usr</em>/sbin</em>/cron[22390]<em>: (root) CMD (/<em>usr</em>/libexec/atrun)")
+        );
+
+        final Multimap<String, Range<Integer>> result = HighlightParser.extractHighlightRanges(highlights);
+
+        assertThat(result).isNotNull();
+        assertThat(result.get("message")).isNotNull()
+                .isNotEmpty()
+                .containsExactly(
+                        Range.closed(1, 4),
+                        Range.closed(26, 48)
+                );
+    }
+
+    @Test
+    public void emptyHighlights() throws Exception {
+        final Map<String, List<String>> highlights = ImmutableMap.of();
+
+        final Multimap<String, Range<Integer>> result = HighlightParser.extractHighlightRanges(highlights);
+
+        assertThat(result).isNotNull();
+        assertThat(result.entries()).isEmpty();
+    }
+
+    @Test
+    public void nullHighlights() throws Exception {
+        final Multimap<String, Range<Integer>> result = HighlightParser.extractHighlightRanges(null);
+
+        assertThat(result).isNotNull();
+        assertThat(result.entries()).isEmpty();
+    }
+}
