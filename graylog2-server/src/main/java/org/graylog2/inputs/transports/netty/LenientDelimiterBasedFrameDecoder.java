6589b213aacca0a2b4676bb39ec5fa26138360ec
Jochen Schalanda <jochen@graylog.com>
2017-08-23 17:09:32 +0200

    Retain original copyright notices
    [ci skip]

--- graylog2-server/src/main/java/org/graylog2/inputs/transports/netty/LenientDelimiterBasedFrameDecoder.java
+++ graylog2-server/src/main/java/org/graylog2/inputs/transports/netty/LenientDelimiterBasedFrameDecoder.java
@@ -14,6 +14,21 @@
  * You should have received a copy of the GNU General Public License
  * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package org.graylog2.inputs.transports.netty;
 
 import org.jboss.netty.buffer.ChannelBuffer;
