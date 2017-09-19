8859a0c377dc5194b4475036a253f70400ba9a73
Dennis Oelkers <dennis@graylog.com>
2017-09-19 16:04:13 +0200

    Adding Jest tests for search page component delegation.

--- graylog2-web-interface/src/pages/DelegatedSearchPage.jsx
+++ graylog2-web-interface/src/pages/DelegatedSearchPage.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import SearchPage from './SearchPage';
 import { PluginStore } from 'graylog-web-plugin/plugin';
+import SearchPage from './SearchPage';
 
-const DelegatedSearchPage = (props) => {
-  const components = PluginStore.exports("pages")
+export default (props) => {
+  const components = PluginStore.exports('pages')
     .map(c => c.search || {})
     .map(c => c.component)
     .filter(c => c) || [];
   const Component = components[0] || SearchPage;
   return <Component {...props} />;
 };
-
-export default DelegatedSearchPage;
