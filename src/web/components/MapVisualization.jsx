0d7d029707b79597a329eb77341ba74b357956a6
Edmundo Alvarez <github@edmundoa.com>
2017-09-22 14:54:08 +0200

    Map rendering and style improvements (#53)
    * Bump dependencies
    
    * Adapt to changes in react-leaflet
    
    * Adapt widget to latest changes in core
    
    * Support media print
    
    * Make map rendering more reliable
    
    Sometimes resizing the map container ends in markers out of their
    correct coordinates or map tiles not being loaded. This commits adds a
    workaround to avoid it, sending a `resize` event and invalidating the
    previous size.
    
    * Fix some linting issues
    
    * Make onRenderComplete callback more reliable
    
    - Check if tiles are fully loaded
    - Disable all animations if map is not interactive, so transitions do
      not get in the way
    
    * Avoid map being split when priting
    
    * Improve handling of container updates
    
    - No need to trigger a resize on componentDidMount
    - First trigger resize event, then invalidate size
    
    * Add prop to lock/unlock map controls
    
    As leaflet is not particularly made to toggle options, we disable
    controls by using a good-old div overlay.
    
    * Fix props formatting

--- src/web/components/MapVisualization.jsx
+++ src/web/components/MapVisualization.jsx
@@ -1,71 +1,86 @@
+/* eslint-env browser */
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Map, TileLayer, CircleMarker, Popup } from 'react-leaflet';
-import { Spinner } from 'components/common';
+
+import {} from 'leaflet/dist/leaflet.css';
+import style from './MapVisualization.css';
 
 const MapVisualization = React.createClass({
   propTypes: {
     id: PropTypes.string.isRequired,
     data: PropTypes.object,
-    config: PropTypes.object.isRequired,
-    height: PropTypes.number,
-    width: PropTypes.number,
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
     url: PropTypes.string,
     attribution: PropTypes.string,
+    interactive: PropTypes.bool,
+    onRenderComplete: PropTypes.func,
+    locked: PropTypes.bool, // Disables zoom and dragging
   },
   getDefaultProps() {
     return {
       data: {},
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
+      interactive: true,
+      onRenderComplete: () => {},
+      locked: PropTypes.bool,
     };
   },
+
   getInitialState() {
     return {
       zoomLevel: 1,
-      isComponentMounted: false, // Leaflet operates directly with the DOM, so we need to wait until it is ready :grumpy:
     };
   },
-  componentDidMount() {
-    this.leafletStyle.use();
-    this.style.use();
-    this._onComponentMount();
+
+  componentDidUpdate(prevProps) {
+    if (this.props.height !== prevProps.height || this.props.width !== prevProps.width) {
+      this._forceMapUpdate();
+    }
   },
 
-  componentWillUnmount() {
-    this.leafletStyle.unuse();
-    this.style.unuse();
-  },
-
-  _onComponentMount() {
-    this.setState({isComponentMounted: true});
-  },
-
-  leafletStyle: require('!style/useable!css!leaflet/dist/leaflet.css'),
-  style: require('!style/useable!css!./MapVisualization.css'),
-
+  _map: undefined,
+  _isMapReady: false,
+  _areTilesReady: false,
   position: [0, 0],
-  DEFAULT_URL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  DEFAULT_ATTRIBUTION: `&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors`,
   MARKER_RADIUS_SIZES: 10,
   MARKER_RADIUS_INCREMENT_SIZES: 10,
+
+  // Workaround to avoid wrong placed markers or empty tiles if the map container size changed.
+  _forceMapUpdate() {
+    if (this._map) {
+      window.dispatchEvent(new Event('resize'));
+      this._map.leafletElement.invalidateSize(this.props.interactive);
+    }
+  },
+
   // Coordinates are given as "lat,long"
   _formatMarker(coordinates, occurrences, min, max, increment) {
     const formattedCoordinates = coordinates.split(',').map(component => Number(component));
     const radius = this._getBucket(occurrences, this.MARKER_RADIUS_SIZES, min, max, increment);
     return (
-      <CircleMarker key={coordinates} center={formattedCoordinates} radius={radius} color="#AF2228" fillColor="#D3242B" weight={2} opacity={0.8}>
+      <CircleMarker key={coordinates}
+                    center={formattedCoordinates}
+                    radius={radius}
+                    color="#AF2228"
+                    fillColor="#D3242B"
+                    weight={2}
+                    opacity={0.8}>
         <Popup>
-            <dl>
-              <dt>Coordinates:</dt>
-              <dd>{coordinates}</dd>
-              <dt>Number of occurrences:</dt>
-              <dd>{occurrences}</dd>
-            </dl>
+          <dl>
+            <dt>Coordinates:</dt>
+            <dd>{coordinates}</dd>
+            <dt>Number of occurrences:</dt>
+            <dd>{occurrences}</dd>
+          </dl>
         </Popup>
       </CircleMarker>
     );
   },
   _onZoomChange(event) {
-    this.setState({zoomLevel: event.target.getZoom()});
+    this.setState({ zoomLevel: event.target.getZoom() });
   },
   _getBucket(value, bucketCount, minValue, maxValue, increment) {
     // Calculate bucket size based on min/max value and the number of buckets.
@@ -75,29 +90,55 @@ const MapVisualization = React.createClass({
 
     return bucket + increment;
   },
-  render() {
-    if (!this.state.isComponentMounted) {
-      return <Spinner/>;
-    }
 
-    const data = this.props.data.terms;
-    const occurrences = Object.keys(data).map((k) => data[k]);
+  _handleRenderComplete() {
+    if (this._areTilesReady && this._isMapReady) {
+      this.props.onRenderComplete();
+    }
+  },
+
+  _handleMapReady() {
+    this._isMapReady = true;
+    this._handleRenderComplete();
+  },
+
+  _handleTilesReady() {
+    this._areTilesReady = true;
+    this._handleRenderComplete();
+  },
+
+  render() {
+    const { data, id, height, width, url, attribution, interactive, locked } = this.props;
+
+    const terms = data.terms;
+    const occurrences = Object.keys(terms).map(k => terms[k]);
     const minOccurrences = occurrences.reduce((prev, cur) => Math.min(prev, cur), Infinity);
     const maxOccurrences = occurrences.reduce((prev, cur) => Math.max(prev, cur), -Infinity);
     const increment = this._getBucket(this.state.zoomLevel, this.MARKER_RADIUS_INCREMENT_SIZES, 1, 10, 1);
 
-    const coordinates = Object.keys(data);
-    const markers = coordinates.map(aCoordinates => this._formatMarker(aCoordinates, data[aCoordinates], minOccurrences, maxOccurrences, increment));
-    const leafletUrl = this.props.url || this.DEFAULT_URL;
-    const leafletAttribution = this.props.attribution || this.DEFAULT_ATTRIBUTION;
+    const coordinates = Object.keys(terms);
+    const markers = coordinates.map(aCoordinates => this._formatMarker(aCoordinates, terms[aCoordinates], minOccurrences, maxOccurrences, increment));
 
     return (
-      <Map center={this.position} zoom={this.state.zoomLevel} onZoomend={this._onZoomChange} style={{height: this.props.height, width: this.props.width}} scrollWheelZoom={false}>
-        <TileLayer url={leafletUrl}
-                   maxZoom={19}
-                   attribution={leafletAttribution}/>
-        {markers}
-      </Map>
+      <div className={locked ? style.mapLocked : ''}>
+        {locked && <div className={style.overlay} style={{ height: height, width: width }} />}
+        <Map ref={(c) => { this._map = c; }}
+             id={`visualization-${id}`}
+             center={this.position}
+             zoom={this.state.zoomLevel}
+             onZoomend={this._onZoomChange}
+             className={style.map}
+             style={{ height: height, width: width }}
+             scrollWheelZoom={false}
+             animate={interactive}
+             zoomAnimation={interactive}
+             fadeAnimation={interactive}
+             markerZoomAnimation={interactive}
+             whenReady={this._handleMapReady}>
+          <TileLayer url={url} maxZoom={19} attribution={attribution} onLoad={this._handleTilesReady} />
+          {markers}
+        </Map>
+      </div>
     );
   },
 });
