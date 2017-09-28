9fb305dad718ad173962505cccebabacd681b135
Dennis Oelkers <dennis@graylog.com>
2017-09-28 14:05:36 +0200

    Create wrapper class transforming props for HistogramVisualization.

---
+++ enterprise/src/web/enterprise/components/widgets/Histogram.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import HistogramVisualization from 'components/visualizations/HistogramVisualization';
+
+export default function Histogram(props) {
+  return <HistogramVisualization {...props}
+                                 computationTimeRange={props.data.queried_timerange}
+                                 config={Object.assign(props.config, {
+                                   interval: props.data.interval,
+                                 })}
+                                 data={props.data.results} />;
+};
+
+Histogram.propTypes = {
+  data: PropTypes.shape({
+    config: PropTypes.shape({
+      timerange: PropTypes.object.isRequired,
+    }).isRequired,
+    interval: PropTypes.string.isRequired,
+    queried_timerange: PropTypes.object.isRequired,
+    results: PropTypes.object.isRequired,
+  }).isRequired,
+};
