12f67f3785539ae9a51160ef89cbd65d48cefc6c
Dennis Oelkers <dennis@graylog.com>
2017-10-02 15:16:08 +0200

    Always close response body in HTTPFileRetriever (#4193)
    Refs Graylog2/graylog-plugin-threatintel#59

--- graylog2-server/src/main/java/org/graylog2/lookup/adapters/dsvhttp/HTTPFileRetriever.java
+++ graylog2-server/src/main/java/org/graylog2/lookup/adapters/dsvhttp/HTTPFileRetriever.java
@@ -55,21 +55,21 @@ public class HTTPFileRetriever {
         }
         final Call request = client.newCall(requestBuilder.build());
 
-        final Response response = request.execute();
+        try (final Response response = request.execute()) {
+            if (response.isSuccessful()) {
+                final String lastModifiedHeader = response.header("Last-Modified", DateTime.now(DateTimeZone.UTC).toString());
+                final Map<String, String> newLastModified = new HashMap<>(this.lastLastModified.get());
+                newLastModified.put(url, lastModifiedHeader);
+                this.lastLastModified.set(ImmutableMap.copyOf(newLastModified));
 
-        if (response.isSuccessful()) {
-            final String lastModifiedHeader = response.header("Last-Modified", DateTime.now(DateTimeZone.UTC).toString());
-            final Map<String, String> newLastModified = new HashMap<>(this.lastLastModified.get());
-            newLastModified.put(url, lastModifiedHeader);
-            this.lastLastModified.set(ImmutableMap.copyOf(newLastModified));
-
-            if (response.body() != null) {
-                final String body = response.body().string();
-                return body == null ? Optional.empty() : Optional.of(body);
-            }
-        } else {
-            if (response.code() != 304) {
-                throw new IOException("Request failed: " + response.message());
+                if (response.body() != null) {
+                    final String body = response.body().string();
+                    return Optional.ofNullable(body);
+                }
+            } else {
+                if (response.code() != 304) {
+                    throw new IOException("Request failed: " + response.message());
+                }
             }
         }
 
