Graylog2/graylog2-server

commit ac4c9b040148eac3dd9709c2b593579a1f48c743

Improving widget/grid stability and prop consistency (#4160)
* Fix linting issues in ReactGridContainer

* Add prop to set row height in ReactGridContainer

* Add extra flag to resize in ReactGridContainer

Now it is possible to restrict widget resize, even if ReactGridContainer is
not locked.

* Set default positioning values

This makes ReactGridContainer work even if not all position properties
are set.

* Improve numeric visualization styling

- Use flexbox to make styling a bit simpler
- Move styling away from global stylesheet

* Defining propTypes, passing through disableSurroundingSearch prop.

* Passing page size and current page.

* Allowing to pass sort order, title & limit to quick values vis.

* Using count dimension to fix limiting in QuickValuesVisualization.

* Using actual limit parameter instead of (removed) constant.

* Making axis labels configurable for histogram.

* Using updated computation time range when normalizing data.

* Allowing to pass custom title to stacked chart series.

* Allow disabling circles on stacked graphs.

* Use new props when formatting data for stacked graphs.

* Check if data points are not empty before deducing custom tick values.

* Using next props to rerender graph upon props update.

* Add an optional threshold line in the GraphVisualization component

Also namespace the renderlet calback in D3Utils.tooltipRenderlet() to
avoid conflicts with other callbacks.

* Fix threshold updating in GraphVisualization

* Update threshold dot title on update in GraphVisualization

Also rename a variable and remove static attributes from updating.

* Use a dashed line for the threshold

That way it doesn't look like another regular value.

* Removing unused import.

* Removing unused constant.

* Escaping threshold tooltip.

* Separating pie chart slice and data table row limits.

graylog2-web-interface/src/components/search/MessageTablePaginator.jsx
@@ -6,7 +6,6 @@ import { Page } from 'components/common';
 import EventHandlersThrottler from 'util/EventHandlersThrottler';
 
 import StoreProvider from 'injection/StoreProvider';
-const SearchStore = StoreProvider.getStore('Search');
 const UniversalSearchStore = StoreProvider.getStore('UniversalSearch');
 
 import $ from 'jquery';
@@ -20,12 +19,16 @@ const MessageTablePaginator = React.createClass({
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.element),
       PropTypes.element,
-    ]),
+    ]).isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    pageSize: PropTypes.number,
     position: PropTypes.string,
   },
+
   getInitialState() {
     return {
       paginationWidth: 0,
+      pageSize: UniversalSearchStore.DEFAULT_LIMIT,
     };
   },
 
@@ -59,7 +62,7 @@ const MessageTablePaginator = React.createClass({
     }
   },
   _numberOfPages() {
-    return Math.ceil(this.props.resultCount / UniversalSearchStore.DEFAULT_LIMIT);
+    return Math.ceil(this.props.resultCount / this.props.pageSize);
   },
   _minPage() {
     const currentTenMin = Math.floor(this.props.currentPage / 10) * 10;
@@ -83,7 +86,7 @@ const MessageTablePaginator = React.createClass({
       newPage = Number(page);
     }
 
-    SearchStore.page = newPage;
+    this.props.onPageChange(newPage);
   },
   render() {
     const pages = [];

graylog2-web-interface/public/stylesheets/graylog2.less
@@ -1079,59 +1079,11 @@ td.limited {
   display: none;
 }
 
-.dashboard .widget .search_result_count .value {
-  line-height: 70px !important;
-  text-align: center;
-  font-size: 70px;
-  position: relative;
-  top: 20px;
-}
-
-.dashboard .widget .search_result_count .trend-indicators {
-  font-size: 25px;
-  height: 70px;
-  position: absolute;
-  top: 35px;
-  left: 345px;
-  width: 25px;
-}
-
-.dashboard .widget .search_result_count .trend-indicators .trend-icon {
-  height: 25px;
-  margin-bottom: -18px;
-}
-
-.dashboard .widget .search_result_count .trend-indicators .trend-icons-higher {
-  margin-bottom: 5px;
-}
-
-.dashboard .widget .search_result_count .icon {
-  color: #ebebeb;
-}
-
 .dashboard .widget .loading-failed {
   color: #ff4646 !important;
 }
 
 .dashboard .widget .controls {
-  display: none;
-}
-
-.dashboard .widget .edit-description {
-  display: none;
-  color: #0085A7 !important;
-  font-size: 12px;
-}
-
-.dashboard .widget .edit-description .icon {
-  color: #0085A7 !important;
-}
-
-.dashboard .widget .description-form {
-  display: none;
-}
-
-.dashboard .widget .info .cache-info {
   display: none;
 }
 
@@ -1186,44 +1138,6 @@ td.limited {
 
 .dashboard .widget svg {
   overflow: hidden;
-}
-
-.dashboard .widget .number {
-  line-height: 70px;
-}
-
-.dashboard .widget .number .value {
-  line-height: 100px;
-  text-align: center;
-  font-size: 70px;
-  position: relative;
-}
-
-.dashboard .widget .number .trend-indicators {
-  font-size: 25px;
-  height: 70px;
-  position: absolute;
-  top: 30px;
-  right: 20px;
-  width: 25px;
-}
-
-.dashboard .widget .number .trend-indicators .trend-icon {
-  color: #E3E5E5;
-  height: 25px;
-  margin-bottom: -18px;
-}
-
-.dashboard .widget .number .trend-indicators .trend-icon.trend-good {
-  color: #8DC63F;
-}
-
-.dashboard .widget .number .trend-indicators .trend-icon.trend-bad {
-  color: #BE1E2D;
-}
-
-.dashboard .widget .number .trend-indicators .trend-icons-higher {
-  margin-bottom: 5px;
 }
 
 .dashboard .widget .quickvalues-visualization {

graylog2-web-interface/src/components/common/ReactGridContainer.jsx
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
-const WidthAdjustedReactGridLayout = WidthProvider(Responsive);
 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import style from './ReactGridContainer.css';
 
+const WidthAdjustedReactGridLayout = WidthProvider(Responsive);
+
 const COLUMN_WIDTH = 350;
+const ROW_HEIGHT = 200;
 
 const COLUMNS = {
   xxl: 6,
@@ -33,17 +35,21 @@ const ReactGridContainer = React.createClass({
     children: PropTypes.node.isRequired,
     onPositionsChange: PropTypes.func.isRequired,
     locked: PropTypes.bool,
+    isResizable: React.PropTypes.bool,
+    rowHeight: React.PropTypes.number,
   },
 
   getDefaultProps() {
     return {
       locked: false,
+      isResizable: true,
+      rowHeight: ROW_HEIGHT,
     };
   },
 
   _onLayoutChange(newLayout) {
     const newPositions = [];
-    newLayout.forEach(widget => {
+    newLayout.forEach((widget) => {
       newPositions.push({
         id: widget.i,
         col: widget.x + 1,
@@ -57,30 +63,31 @@ const ReactGridContainer = React.createClass({
   },
 
   render() {
-    const layout = Object.keys(this.props.positions).map(id => {
-      const position = this.props.positions[id];
+    const { children, locked, isResizable, positions, rowHeight } = this.props;
+    const layout = Object.keys(positions).map((id) => {
+      const { col, row, height, width } = positions[id];
       return {
         i: id,
-        x: Math.max(position.col - 1, 0),
-        y: (position.row <= 0 ? Infinity : position.row - 1),
-        h: position.height,
-        w: position.width,
+        x: col ? Math.max(col - 1, 0) : 0,
+        y: (row === undefined || row <= 0 ? Infinity : row - 1),
+        h: height || 1,
+        w: width || 1,
       };
     });
 
     // We need to use a className and draggableHandle to avoid re-rendering all graphs on lock/unlock. See:
     // https://github.com/STRML/react-grid-layout/issues/371
     return (
-      <WidthAdjustedReactGridLayout className={`${style.reactGridLayout} ${this.props.locked ? 'locked' : 'unlocked'}`}
+      <WidthAdjustedReactGridLayout className={`${style.reactGridLayout} ${locked || !isResizable ? 'locked' : 'unlocked'}`}
                                     layouts={{ xxl: layout, xl: layout, lg: layout, md: layout, sm: layout, xs: layout }}
                                     breakpoints={BREAKPOINTS}
                                     cols={COLUMNS}
-                                    rowHeight={200}
+                                    rowHeight={rowHeight}
                                     margin={[10, 10]}
                                     onDragStop={this._onLayoutChange}
                                     onResizeStop={this._onLayoutChange}
-                                    draggableHandle={this.props.locked ? '.no-handle' : ''}>
-        {this.props.children}
+                                    draggableHandle={locked ? '.no-handle' : ''}>
+        {children}
       </WidthAdjustedReactGridLayout>
     );
   },

graylog2-web-interface/src/components/search/MessageTableEntry.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import MessageDetail from './MessageDetail';
 import Immutable from 'immutable';
+
+import MessageDetail from './MessageDetail';
 import { Timestamp } from 'components/common';
 import StringUtils from 'util/StringUtils';
 
 const MessageTableEntry = React.createClass({
+  propTypes: {
+    allStreams: React.PropTypes.instanceOf(Immutable.List).isRequired,
+    allStreamsLoaded: React.PropTypes.bool.isRequired,
+    disableSurroundingSearch: React.PropTypes.bool,
+    expandAllRenderAsync: React.PropTypes.bool.isRequired,
+    expanded: React.PropTypes.bool.isRequired,
+    highlight: React.PropTypes.bool,
+    highlightMessage: React.PropTypes.string,
+    inputs: React.PropTypes.instanceOf(Immutable.Map).isRequired,
+    message: React.PropTypes.shape({
+      fields: React.PropTypes.object.isRequired,
+      highlight_ranges: React.PropTypes.object,
+      id: React.PropTypes.string.isRequired,
+      index: React.PropTypes.string.isRequired,
+    }).isRequired,
+    nodes: React.PropTypes.instanceOf(Immutable.Map).isRequired,
+    searchConfig: React.PropTypes.object,
+    selectedFields: React.PropTypes.instanceOf(Immutable.OrderedSet),
+    showMessageRow: React.PropTypes.bool,
+    streams: React.PropTypes.instanceOf(Immutable.Map).isRequired,
+    toggleDetail: React.PropTypes.func.isRequired,
+  },
+  getDefaultProps() {
+    return {
+      disableSurroundingSearch: false,
+      highlight: false,
+      highlightMessage: undefined,
+      searchConfig: undefined,
+      selectedFields: Immutable.OrderedSet(),
+      showMessageRow: false,
+    };
+  },
+
   shouldComponentUpdate(newProps) {
     if (this.props.highlight !== newProps.highlight) {
       return true;
@@ -95,10 +129,16 @@ const MessageTableEntry = React.createClass({
         {this.props.expanded &&
         <tr className="message-detail-row" style={{ display: 'table-row' }}>
           <td colSpan={colSpanFixup}>
-            <MessageDetail message={this.props.message} inputs={this.props.inputs} streams={this.props.streams}
-                         allStreams={this.props.allStreams} allStreamsLoaded={this.props.allStreamsLoaded}
-                         nodes={this.props.nodes} possiblyHighlight={this.possiblyHighlight}
-                         expandAllRenderAsync={this.props.expandAllRenderAsync} searchConfig={this.props.searchConfig} />
+            <MessageDetail message={this.props.message}
+                           inputs={this.props.inputs}
+                           streams={this.props.streams}
+                           allStreams={this.props.allStreams}
+                           allStreamsLoaded={this.props.allStreamsLoaded}
+                           nodes={this.props.nodes}
+                           possiblyHighlight={this.possiblyHighlight}
+                           disableSurroundingSearch={this.props.disableSurroundingSearch}
+                           expandAllRenderAsync={this.props.expandAllRenderAsync}
+                           searchConfig={this.props.searchConfig}/>
           </td>
         </tr>
         }

graylog2-web-interface/src/components/search/ResultTable.jsx
@@ -14,17 +14,26 @@ import { MessageTableEntry, MessageTablePaginator } from 'components/search';
 
 const ResultTable = React.createClass({
   propTypes: {
+    disableSurroundingSearch: React.PropTypes.bool,
     highlight: PropTypes.bool.isRequired,
     inputs: PropTypes.object.isRequired,
     messages: PropTypes.array.isRequired,
     nodes: PropTypes.object.isRequired,
+    onPageChange: React.PropTypes.func,
     page: PropTypes.number.isRequired,
+    pageSize: React.PropTypes.number,
     resultCount: PropTypes.number.isRequired,
     selectedFields: PropTypes.object.isRequired,
     sortField: PropTypes.string.isRequired,
     sortOrder: PropTypes.string.isRequired,
     streams: PropTypes.object.isRequired,
     searchConfig: PropTypes.object.isRequired,
+  },
+  getDefaultProps() {
+    return {
+      disableSurroundingSearch: false,
+      onPageChange: (page) => { SearchStore.page = page; },
+    };
   },
   getInitialState() {
     return {
@@ -139,7 +148,10 @@ const ResultTable = React.createClass({
                   disabled={this.state.expandedMessages.size === 0}><i className="fa fa-compress" /></Button>
         </ButtonGroup>
 
-        <MessageTablePaginator position="top" currentPage={Number(this.props.page)}
+        <MessageTablePaginator currentPage={Number(this.props.page)}
+                               onPageChange={this.props.onPageChange}
+                               pageSize={this.props.pageSize}
+                               position="top"
                                resultCount={this.props.resultCount} />
 
         <div className="search-results-table">
@@ -162,6 +174,7 @@ const ResultTable = React.createClass({
                 {this.props.messages.map((message) => {
                   return (
                     <MessageTableEntry key={`${message.index}-${message.id}`}
+                                       disableSurroundingSearch={this.props.disableSurroundingSearch}
                                        message={message}
                                        showMessageRow={this.props.selectedFields.contains('message')}
                                        selectedFields={selectedColumns}
@@ -183,7 +196,10 @@ const ResultTable = React.createClass({
           </div>
         </div>
 
-        <MessageTablePaginator position="bottom" currentPage={Number(this.props.page)}
+        <MessageTablePaginator currentPage={Number(this.props.page)}
+                               onPageChange={this.props.onPageChange}
+                               pageSize={this.props.pageSize}
+                               position="bottom"
                                resultCount={this.props.resultCount}>
           <ButtonGroup bsSize="small" className="pull-right" style={{ position: 'absolute', marginTop: 20, right: 10 }}>
             <Button title="Expand all messages" onClick={this.expandAll}><i className="fa fa-expand" /></Button>

graylog2-web-interface/src/components/visualizations/GraphVisualization.jsx
@@ -6,6 +6,7 @@ import crossfilter from 'crossfilter';
 import dc from 'dc';
 import d3 from 'd3';
 import deepEqual from 'deep-equal';
+import _ from 'lodash';
 
 import DateTime from 'logic/datetimes/DateTime';
 import HistogramFormatter from 'logic/graphs/HistogramFormatter';
@@ -64,7 +65,10 @@ const GraphVisualization = React.createClass({
     id: PropTypes.string.isRequired,
     data: PropTypes.object.isRequired,
     config: PropTypes.object.isRequired,
-    computationTimeRange: PropTypes.object,
+    computationTimeRange: PropTypes.shape({
+      from: PropTypes.string.isRequired,
+      to: PropTypes.string.isRequired,
+    }).isRequired,
     height: PropTypes.number,
     width: PropTypes.number,
   },
@@ -92,7 +96,10 @@ const GraphVisualization = React.createClass({
   },
   componentDidMount() {
     this.renderGraph();
-    this._updateData(this.props.data, this.props.config);
+    this._updateData(this.props.data, this.props.config, this.props.computationTimeRange);
+  },
+  componentDidUpdate() {
+    this.drawData();
   },
   componentWillReceiveProps(nextProps) {
     if (deepEqual(this.props, nextProps)) {
@@ -102,14 +109,14 @@ const GraphVisualization = React.createClass({
     if (nextProps.height !== this.props.height || nextProps.width !== this.props.width) {
       this._resizeVisualization(nextProps.width, nextProps.height);
     }
-    this._updateData(nextProps.data, nextProps.config);
+    this._updateData(nextProps.data, nextProps.config, nextProps.computationTimeRange);
   },
-  _updateData(data, config) {
+  _updateData(data, config, computationTimeRange) {
     const isSearchAll = (config.timerange.type === 'relative' && config.timerange.range === 0);
-    const dataPoints = HistogramFormatter.format(data, this.props.computationTimeRange,
+    const dataPoints = HistogramFormatter.format(data, computationTimeRange,
       config.interval, this.props.width, isSearchAll, config.valuetype);
 
-    this.setState({ dataPoints: this._normalizeData(dataPoints) }, this.drawData);
+    this.setState({ dataPoints: this._normalizeData(dataPoints) });
   },
   _normalizeData(data) {
     if (data === null || data === undefined || !Array.isArray(data)) {
@@ -154,6 +161,87 @@ const GraphVisualization = React.createClass({
     } else {
       this.graph.redraw();
     }
+    if (this.props.config.threshold) {
+      this.renderThreshold();
+    }
+  },
+  // Draws a horizontal threshold line in the graph
+  renderThreshold() {
+    const threshold = this.props.config.threshold;
+    const thresholdColor = this.props.config.threshold_color || '#f00';
+    const thresholdTooltip = this.props.config.threshold_tooltip || `threshold: ${threshold}`;
+    const thresholdDotHidden = 1e-6;
+    const thresholdDotVisible = 0.8;
+    const thresholdDotRadius = 4;
+
+    this.graph.on('renderlet.threshold', (chart) => {
+      const lineData = [
+        {
+          x: chart.x().range()[0],
+          y: chart.y()(threshold),
+        },
+        {
+          x: chart.x().range()[1],
+          y: chart.y()(threshold),
+        },
+      ];
+
+      const line = d3.svg.line()
+        .x(d => d.x)
+        .y(d => d.y)
+        .interpolate('linear');
+
+      const chartBody = chart.select('g.chart-body');
+      const paths = chartBody.selectAll('path.threshold').data([lineData]);
+      paths // Modify the existing path
+        .attr('stroke', thresholdColor)
+        .attr('d', line);
+      paths.enter() // This will only do something if there isn't a path yet
+        .append('path')
+        .attr('class', 'threshold')
+        .attr('stroke', thresholdColor)
+        .attr('stroke-width', 1)
+        .attr('stroke-dasharray', ('2', '2'))
+        .attr('d', line);
+      paths.exit().remove(); // Remove any outdated paths
+
+      // Collect all x-axis values
+      const xValues = chart.data().reduce((list, value) => {
+        value.values.forEach(v => list.push(v.x));
+        return list;
+      }, []);
+
+      // Put circles on the threshold line to make it easier to show the tooltip
+      const dots = chartBody.selectAll('circle.threshold').data(xValues);
+      dots.enter()
+        .append('circle')
+        .attr('class', 'threshold')
+        .attr('r', thresholdDotRadius)
+        .attr('rel', 'tooltip') // To make the tooltip helper pick it up
+        .attr('data-original-title', () => {
+          return `<div class="datapoint-info">${_.escape(thresholdTooltip)}</div>`;
+        })
+        .style('stroke-opacity', thresholdDotHidden)
+        .style('fill-opacity', thresholdDotHidden)
+        .on('mousemove', function show() {
+          d3.select(this)
+            .style('stroke-opacity', thresholdDotVisible)
+            .style('fill-opacity', thresholdDotVisible);
+        })
+        .on('mouseout', function hide() {
+          d3.select(this)
+            .style('stroke-opacity', thresholdDotHidden)
+            .style('fill-opacity', thresholdDotHidden);
+        });
+      dots
+        .attr('cx', d => dc.utils.safeNumber(chart.x()(d)))
+        .attr('cy', () => dc.utils.safeNumber(chart.y()(threshold)))
+        .attr('data-original-title', () => {
+          return `<div class="datapoint-info">${thresholdTooltip}</div>`;
+        })
+        .attr('fill', thresholdColor);
+      dots.exit().remove();
+    });
   },
   renderGraph() {
     const graphDomNode = ReactDOM.findDOMNode(this);

graylog2-web-interface/src/components/visualizations/HistogramVisualization.jsx
@@ -80,6 +80,8 @@ const HistogramVisualization = React.createClass({
   },
   renderHistogram() {
     const histogramDomNode = ReactDOM.findDOMNode(this);
+    const xAxisLabel = this.props.config.xAxis || 'Time';
+    const yAxisLabel = this.props.config.yAxis || 'Messages';
 
     this.histogram = dc.barChart(histogramDomNode);
     this.histogram
@@ -94,8 +96,8 @@ const HistogramVisualization = React.createClass({
       .centerBar(true)
       .renderHorizontalGridLines(true)
       .brushOn(false)
-      .xAxisLabel('Time')
-      .yAxisLabel('Messages')
+      .xAxisLabel(xAxisLabel)
+      .yAxisLabel(yAxisLabel)
       .renderTitle(false)
       .colors(D3Utils.glColourPalette())
       .on('renderlet', () => {

graylog2-web-interface/src/components/visualizations/NumericVisualization.css
@@ -1,0 +1,40 @@
+:local(.number) {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+
+:local(.aside) {
+    flex: 1;
+    align-self: flex-start;
+}
+
+:local(.value) {
+    flex: 2;
+    line-height: 100px; /* Ensure changing the value font-size doesn't change the element's height */
+    text-align: center;
+}
+
+:local(.trendIndicators) {
+    font-size: 24px;
+    margin-top: 12px; /* font-size / 2 */
+    margin-left: 10px;
+}
+
+:local(.trendIndicators) > *:first-child {
+    margin-bottom: 5px;
+}
+
+:local(.trendIcon) {
+    color: #E3E5E5;
+    height: 25px;
+    margin-bottom: -18px;
+}
+
+:local(.trendGood) {
+    color: #8DC63F;
+}
+
+:local(.trendBad) {
+    color: #BE1E2D;
+}

commit 8daa83ed55bc7a4625920df938d2bc8a53b638b8

Correctly initialize the "pageSize" prop in MessageTablePaginator
Fixes an issue when no "pageSize" prop is provided. This broke in #4160.

graylog2-web-interface/src/components/search/MessageTablePaginator.jsx
@@ -19,16 +19,21 @@ const MessageTablePaginator = React.createClass({
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.element),
       PropTypes.element,
-    ]).isRequired,
+    ]),
     onPageChange: PropTypes.func.isRequired,
     pageSize: PropTypes.number,
     position: PropTypes.string,
   },
 
+  getDefaultProps() {
+    return {
+      pageSize: UniversalSearchStore.DEFAULT_LIMIT,
+    };
+  },
+
   getInitialState() {
     return {
       paginationWidth: 0,
-      pageSize: UniversalSearchStore.DEFAULT_LIMIT,
     };
   },
 