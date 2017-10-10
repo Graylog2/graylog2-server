e10f3b8a1521388b9a2de70fc5c2de4b1905bfc2
Jochen Schalanda <jochen@graylog.com>
2017-10-10 13:23:51 +0200

    Make UserPermissionsCleanupListener easier to debug

--- graylog2-server/src/main/java/org/graylog2/users/UserPermissionsCleanupListener.java
+++ graylog2-server/src/main/java/org/graylog2/users/UserPermissionsCleanupListener.java
@@ -16,19 +16,19 @@
  */
 package org.graylog2.users;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import org.graylog2.dashboards.events.DashboardDeletedEvent;
 import org.graylog2.plugin.database.ValidationException;
+import org.graylog2.plugin.database.users.User;
 import org.graylog2.shared.users.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 public class UserPermissionsCleanupListener {
     private static final Logger LOG = LoggerFactory.getLogger(UserPermissionsCleanupListener.class);
@@ -44,28 +44,23 @@ public class UserPermissionsCleanupListener {
 
     @Subscribe
     public void cleanupPermissionsOnDashboardRemoval(DashboardDeletedEvent event) {
-        final Set<String> permissions = ImmutableSet.of(
+        final List<String> dashboardPermissions = Arrays.asList(
                 "dashboards:read:" + event.dashboardId(),
-                "dashboards:edit:" + event.dashboardId()
-        );
-        this.userService.loadAll().forEach(user -> {
-            List<String> userPermissions = new ArrayList<>(user.getPermissions());
-            boolean modifiedUser = false;
-            for (String permission : permissions) {
-                if (userPermissions.contains(permission)) {
-                    userPermissions.remove(permission);
-                    modifiedUser = true;
-                }
-            }
+                "dashboards:edit:" + event.dashboardId());
+        final List<User> users = userService.loadAll();
+        for (User user : users) {
+            final List<String> userPermissions = new ArrayList<>(user.getPermissions());
+            boolean modifiedUser = userPermissions.removeAll(dashboardPermissions);
 
             if (modifiedUser) {
                 user.setPermissions(userPermissions);
                 try {
-                    userService.save(user);
+                    final String userId = userService.save(user);
+                    LOG.debug("Successfully updated permissions of user <{}>: {}", userId, userPermissions);
                 } catch (ValidationException e) {
                     LOG.warn("Unable to save user while removing permissions of deleted dashboard: ", e);
                 }
             }
-        });
+        }
     }
 }
