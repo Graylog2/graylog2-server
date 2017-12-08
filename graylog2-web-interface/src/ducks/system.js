f63db3044d1b7c95619ca00ac1f3f6ac735f5c68
Edmundo Alvarez <edmundo@graylog.com>
2017-12-08 20:22:43 +0100

    Add our first reducer and its actions
    For this we use the pattern called 'ducks', avoiding to spread many
    files over the project:
    https://github.com/erikras/ducks-modular-redux

---
+++ graylog2-web-interface/src/ducks/system.js
@@ -0,0 +1,62 @@
+import URLUtils from 'util/URLUtils';
+import ApiRoutes from 'routing/ApiRoutes';
+import fetch from 'logic/rest/FetchProvider';
+
+const SYSTEM_INFO_REQUEST = 'graylog/system/SYSTEM_INFO_REQUEST';
+const SYSTEM_INFO_SUCCESS = 'graylog/system/SYSTEM_INFO_SUCCESS';
+const SYSTEM_INFO_FAILURE = 'graylog/system/SYSTEM_INFO_FAILURE';
+
+const requestSystemInfo = () => ({
+  type: SYSTEM_INFO_REQUEST,
+});
+
+const receiveSystemInfo = response => ({
+  type: SYSTEM_INFO_SUCCESS,
+  response: response,
+});
+
+const failedSystemInfo = error => ({
+  type: SYSTEM_INFO_FAILURE,
+  error: error,
+});
+
+export const loadSystemInfo = () => {
+  return (dispatch) => {
+    dispatch(requestSystemInfo());
+    const url = URLUtils.qualifyUrl(ApiRoutes.SystemApiController.info().url);
+    return fetch('GET', url)
+      .then(
+        response => dispatch(receiveSystemInfo(response)),
+        error => dispatch(failedSystemInfo(error)),
+      );
+  };
+};
+
+const initialState = {
+  frontend: {
+    isLoading: true,
+    error: undefined,
+  },
+  entities: {
+    systemInfo: undefined,
+  },
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SYSTEM_INFO_REQUEST:
+      return Object.assign({}, state, { frontend: { isLoading: true } });
+    case SYSTEM_INFO_SUCCESS:
+      return Object.assign({}, state, {
+        frontend: { isLoading: false },
+        entities: { systemInfo: action.response },
+      });
+    case SYSTEM_INFO_FAILURE:
+      return Object.assign({}, state, {
+        frontend: { isLoading: false, error: action.error.message },
+      });
+    default:
+      return state;
+  }
+};
+
