5b2f4f404cd446d169fcd8d7ff3edd6607434424
Dennis Oelkers <dennis@graylog.com>
2018-01-04 15:56:52 +0100

    Checking user privileges before performing Input State operations. (#4454)
    * Checking user privileges before performing Input State operations.
    
    Before this change, any user was able to start/stop inputs without the
    possession of the required privileges. The `InputStatesResource` class
    did not perform any permission checks for operations.
    
    After this change, a user who starts/stops an input requires the newly
    introduced `inputs:changestate` permission (consistent with permission
    handling of indices/processing/streams) in order to perform these
    state changes.
    
    Fixes #4439.
    
    * Adding new permission to set containing all permissions.
    
    (cherry picked from commit 6680fa6d4eff533f261b5a25d008a0347dda782d)

--- graylog2-server/src/main/java/org/graylog2/shared/security/RestPermissions.java
+++ graylog2-server/src/main/java/org/graylog2/shared/security/RestPermissions.java
@@ -67,6 +67,7 @@ public class RestPermissions implements PluginPermissions {
     public static final String INDICES_DELETE = "indices:delete";
     public static final String INDICES_FAILURES = "indices:failures";
     public static final String INDICES_READ = "indices:read";
+    public static final String INPUTS_CHANGESTATE = "inputs:changestate";
     public static final String INPUTS_CREATE = "inputs:create";
     public static final String INPUTS_EDIT = "inputs:edit";
     public static final String INPUTS_READ = "inputs:read";
@@ -172,6 +173,7 @@ public class RestPermissions implements PluginPermissions {
         .add(create(INDICES_DELETE, ""))
         .add(create(INDICES_FAILURES, ""))
         .add(create(INDICES_READ, ""))
+        .add(create(INPUTS_CHANGESTATE, ""))
         .add(create(INPUTS_CREATE, ""))
         .add(create(INPUTS_EDIT, ""))
         .add(create(INPUTS_READ, ""))
