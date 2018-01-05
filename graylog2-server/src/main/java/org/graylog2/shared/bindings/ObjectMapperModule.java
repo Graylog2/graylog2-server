fdcc0a37e21e02b8f6e68e078bde3c69275a3056
Kay Roepke <kroepke@googlemail.com>
2018-01-05 16:58:24 +0100

    the objectmapper module depends on the jackson subtypes binder
    inherit from the abstract Graylog2Module so we can create the empty binder
    during configure

--- graylog2-server/src/main/java/org/graylog2/shared/bindings/ObjectMapperModule.java
+++ graylog2-server/src/main/java/org/graylog2/shared/bindings/ObjectMapperModule.java
@@ -18,13 +18,13 @@ package org.graylog2.shared.bindings;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.inject.AbstractModule;
+import org.graylog2.plugin.inject.Graylog2Module;
 import org.graylog2.shared.bindings.providers.ObjectMapperProvider;
 import org.graylog2.shared.plugins.GraylogClassLoader;
 
 import static java.util.Objects.requireNonNull;
 
-public class ObjectMapperModule extends AbstractModule {
+public class ObjectMapperModule extends Graylog2Module {
     private final ClassLoader classLoader;
 
     @VisibleForTesting
@@ -38,6 +38,7 @@ public class ObjectMapperModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        jacksonSubTypesBinder();
         bind(ClassLoader.class).annotatedWith(GraylogClassLoader.class).toInstance(classLoader);
         bind(ObjectMapper.class).toProvider(ObjectMapperProvider.class).asEagerSingleton();
     }
