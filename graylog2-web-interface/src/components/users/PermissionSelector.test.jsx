85d5638b777a7315acc86b9077e068c7939b1102
Konrad Merz <konrad@graylog.com>
2018-01-09 17:01:07 +0100

    Change Permission Buttons from Toggle to Set and Clear
    After selecting two streams (or dashboards) in the Edit Role
    view, two buttons appeared in the header of the table.
    "Toggle read permissions" and "Toggle edit permissions"
    which would invert the permissions of a selected streams.
    
    The user would rather set or clear the permissions of the
    selected streams.
    
    So I changed the label of the buttons from "Toggle" to
    "Set" and "Clear" depending on which state the streams
    permissions are. If all permissions are "Allow" the button
    will read "Clear" and remove the permissions. If one or
    more will have no "Allow" permission, the button will read
    "Set" and will set the permission to "Allow" for all selected
    streams.
    
    Also add test for PermissionSelector Component and did
    some small ESLint fixes.
    
    Fixes #3210

---
+++ graylog2-web-interface/src/components/users/PermissionSelector.test.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Immutable from 'immutable';
+import { mount } from 'enzyme';
+
+import PermissionSelector from 'components/users/PermissionSelector';
+
+/* https://github.com/facebook/react/issues/7371
+ *
+ * findDomNode with refs is not supported by the react-test-renderer.
+ * So we need to mock the findDOMNode function for TableList respectievly
+ * for its child component TypeAheadDataFilter.
+ */
+jest.mock('react-dom', () => ({
+  findDOMNode: () => ({}),
+}));
+
+describe('<PermissionSelector />', () => {
+  const streams = Immutable.List([
+    { id: '01', description: 'stream1' },
+    { id: '02', description: 'stream2' },
+  ]);
+  const dashboards = Immutable.List([
+    { id: '01', description: 'dashboard1' },
+    { id: '02', description: 'dashboard2' },
+  ]);
+
+  it('should render with empty permissions', () => {
+    const wrapper = renderer.create(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set([])}
+      dashboards={dashboards}
+      onChange={() => {}}
+    />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with set permissions', () => {
+    const permissions = Immutable.Set([
+      'streams:read:01',
+      'streams:edit:02',
+      'dashboards:read:02',
+    ]);
+    const wrapper = renderer.create(<PermissionSelector
+      streams={streams}
+      permissions={permissions}
+      dashboards={dashboards}
+      onChange={() => {}}
+    />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should allow reading when clicked on "Allow reading"', () => {
+    const onButtonClick = jest.fn((added) => {
+      expect(added).toEqual(Immutable.Set(['streams:read:01']));
+    });
+    const wrapper = mount(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set([])}
+      dashboards={dashboards}
+      onChange={onButtonClick}
+    />);
+    wrapper.find('button[children="Allow reading"]').at(0).simulate('click');
+    expect(onButtonClick.mock.calls.length).toBe(1);
+  });
+
+  it('should allow reading and editing when clicked on "Allow editing"', () => {
+    const onButtonClick = jest.fn((added) => {
+      expect(added).toEqual(Immutable.Set(['streams:read:01', 'streams:edit:01']));
+    });
+    const wrapper = mount(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set([])}
+      dashboards={dashboards}
+      onChange={onButtonClick}
+    />);
+    wrapper.find('button[children="Allow editing"]').at(0).simulate('click');
+    expect(onButtonClick.mock.calls.length).toBe(1);
+  });
+
+  it('should not allow reading when "Allow reading" was unselected', () => {
+    const onButtonClick = jest.fn((_, deleted) => {
+      expect(deleted).toEqual(Immutable.Set(['streams:read:01', 'streams:edit:01']));
+    });
+    const wrapper = mount(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set(['streams:read:01'])}
+      dashboards={dashboards}
+      onChange={onButtonClick}
+    />);
+    wrapper.find('button[children="Allow reading"]').at(0).simulate('click');
+    expect(onButtonClick.mock.calls.length).toBe(1);
+  });
+
+  it('should bulk set "Allow reading" for selected streams', () => {
+    const onButtonClick = jest.fn((added, deleted) => {
+      expect(added).toEqual(Immutable.Set(['streams:read:01', 'streams:read:02']));
+      expect(deleted).toEqual(Immutable.Set([]));
+    });
+    const wrapper = mount(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set(['streams:read:01'])}
+      dashboards={dashboards}
+      onChange={onButtonClick}
+    />);
+    wrapper.find('input[label="Select all"]').at(0).simulate('change', { target: { checked: true } });
+    expect(wrapper.find('button').at(2).text()).toEqual('Set read permissions');
+    wrapper.find('button').at(2).simulate('click');
+    expect(onButtonClick.mock.calls.length).toBe(1);
+  });
+
+  it('should bulk set "Allow editing" and "Allow reading" for selected streams', () => {
+    const onButtonClick = jest.fn((added, deleted) => {
+      expect(added).toEqual(Immutable.Set(['streams:read:01', 'streams:edit:01', 'streams:read:02', 'streams:edit:02']));
+      expect(deleted).toEqual(Immutable.Set([]));
+    });
+    const wrapper = mount(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set(['streams:read:01', 'stream:edit:01'])}
+      dashboards={dashboards}
+      onChange={onButtonClick}
+    />);
+    wrapper.find('input[label="Select all"]').at(0).simulate('change', { target: { checked: true } });
+    expect(wrapper.find('button').at(3).text()).toEqual('Set edit permissions');
+    wrapper.find('button').at(3).simulate('click');
+    expect(onButtonClick.mock.calls.length).toBe(1);
+  });
+
+  it('should bulk clear "Allow reading" for selected streams', () => {
+    const onButtonClick = jest.fn((added, deleted) => {
+      expect(added).toEqual(Immutable.Set([]));
+      expect(deleted).toEqual(Immutable.Set(['streams:read:01', 'streams:edit:01', 'streams:read:02', 'streams:edit:02']));
+    });
+    const wrapper = mount(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set(['streams:read:01', 'streams:read:02'])}
+      dashboards={dashboards}
+      onChange={onButtonClick}
+    />);
+    wrapper.find('input[label="Select all"]').at(0).simulate('change', { target: { checked: true } });
+    expect(wrapper.find('button').at(2).text()).toEqual('Clear read permissions');
+    wrapper.find('button').at(2).simulate('click');
+    expect(onButtonClick.mock.calls.length).toBe(1);
+  });
+
+  it('should bulk set "Allow editing" for selected streams', () => {
+    const onButtonClick = jest.fn((added, deleted) => {
+      expect(added).toEqual(Immutable.Set(['streams:read:01', 'streams:edit:01',
+        'streams:read:02', 'streams:edit:02']));
+      expect(deleted).toEqual(Immutable.Set([]));
+    });
+    const wrapper = mount(<PermissionSelector
+      streams={streams}
+      permissions={Immutable.Set(['streams:read:01', 'streams:edit:01'])}
+      dashboards={dashboards}
+      onChange={onButtonClick}
+    />);
+    wrapper.find('input[label="Select all"]').at(0).simulate('change', { target: { checked: true } });
+    expect(wrapper.find('button').at(3).text()).toEqual('Set edit permissions');
+    wrapper.find('button').at(3).simulate('click');
+    expect(onButtonClick.mock.calls.length).toBe(1);
+  });
+});
