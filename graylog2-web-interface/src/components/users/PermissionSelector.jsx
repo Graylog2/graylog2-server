08a596293b5b2a42a0f77026756ca738e284771c
Konrad Merz <kmerz@hulud.net>
2018-01-15 17:25:39 +0100

    Change Permission Buttons from Toggle to Set and Clear (#4467)
    After selecting two streams (or dashboards) in the Edit Role
    view, two buttons appeared in the header of the table.
    "Toggle read permissions" and "Toggle edit permissions"
    which would invert the permissions of a selected streams.
    
    The user would rather set or clear the permissions of the
    selected streams.
    
    So I changed the label of the buttons from "Toggle" to
    "Set" and "Clear" depending on which state the streams
    permissions are. If all permissions are "Allow" the button
    will read "Clear" and remove the permissions. If one or
    more will have no "Allow" permission, the button will read
    "Set" and will set the permission to "Allow" for all selected
    streams.
    
    Also add test for PermissionSelector Component and did
    some small ESLint fixes.
    
    Fixes #3210

--- graylog2-web-interface/src/components/users/PermissionSelector.jsx
+++ graylog2-web-interface/src/components/users/PermissionSelector.jsx
@@ -19,19 +19,27 @@ const PermissionSelector = React.createClass({
       const isRead = this.props.permissions.contains(`streams:read:${stream.id}`);
       const isEdit = this.props.permissions.contains(`streams:edit:${stream.id}`);
       return (<ButtonGroup bsSize="small">
-        <Button bsStyle={isRead ? 'info' : 'default'} onClick={() => this._toggleStreamReadPermissions(stream)}
+        <Button bsStyle={isRead ? 'info' : 'default'}
+                onClick={() => this._toggleStreamReadPermissions(stream)}
                 active={isRead}>Allow reading</Button>
-        <Button bsStyle={isEdit ? 'info' : 'default'} onClick={() => this._toggleStreamEditPermissions(stream)}
+        <Button bsStyle={isEdit ? 'info' : 'default'}
+                onClick={() => this._toggleStreamEditPermissions(stream)}
                 active={isEdit}>Allow editing</Button>
       </ButtonGroup>);
     };
 
     const multiStreamButtons = (streamIds) => {
+      const selectedStreams = this.props.streams.filter(stream => streamIds.contains(stream.id));
+      const allRead = selectedStreams.every(stream => this.props.permissions.contains(`streams:read:${stream.id}`));
+      const allEdit = selectedStreams.every(stream => this.props.permissions.contains(`streams:edit:${stream.id}`));
+      const readActionLabel = allRead ? 'Clear' : 'Set';
+      const editActionLabel = allEdit ? 'Clear' : 'Set';
+
       return (
         <div className="pull-right" style={{ marginTop: 10, marginBottom: 10 }}>
-          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllStreamsRead(streamIds)}>Toggle read permissions</Button>
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllStreamsRead(streamIds, allRead)}>{readActionLabel} read permissions</Button>
           &nbsp;
-          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllStreamsEdit(streamIds)}>Toggle edit permissions</Button>
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllStreamsEdit(streamIds, allEdit)}>{editActionLabel} edit permissions</Button>
         </div>
       );
     };
@@ -40,19 +48,26 @@ const PermissionSelector = React.createClass({
       const isRead = this.props.permissions.contains(`dashboards:read:${dashboard.id}`);
       const isEdit = this.props.permissions.contains(`dashboards:edit:${dashboard.id}`);
       return (<ButtonGroup bsSize="small">
-        <Button bsStyle={isRead ? 'info' : 'default'} onClick={() => this._toggleDashboardReadPermissions(dashboard)}
+        <Button bsStyle={isRead ? 'info' : 'default'}
+                onClick={() => this._toggleDashboardReadPermissions(dashboard)}
                 active={isRead}>Allow reading</Button>
-        <Button bsStyle={isEdit ? 'info' : 'default'} onClick={() => this._toggleDashboardEditPermissions(dashboard)}
+        <Button bsStyle={isEdit ? 'info' : 'default'}
+                onClick={() => this._toggleDashboardEditPermissions(dashboard)}
                 active={isEdit}>Allow editing</Button>
       </ButtonGroup>);
     };
-
     const multiDashboardButtons = (dashboardIds) => {
+      const selectedDashboards = this.props.dashboards.filter(dashboard => dashboardIds.contains(dashboard.id));
+      const allRead = selectedDashboards.every(dashboard => this.props.permissions.contains(`dashboards:read:${dashboard.id}`));
+      const allEdit = selectedDashboards.every(dashboard => this.props.permissions.contains(`dashboards:edit:${dashboard.id}`));
+      const readActionLabel = allRead ? 'Clear' : 'Set';
+      const editActionLabel = allEdit ? 'Clear' : 'Set';
+
       return (
         <div className="pull-right" style={{ marginTop: 10, marginBottom: 10 }}>
-          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllDashboardsRead(dashboardIds)}>Toggle read permissions</Button>
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllDashboardsRead(dashboardIds, allRead)}>{readActionLabel} read permissions</Button>
           &nbsp;
-          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllDashboardsEdit(dashboardIds)}>Toggle edit permissions</Button>
+          <Button bsSize="xsmall" bsStyle="info" onClick={() => this._toggleAllDashboardsEdit(dashboardIds, allEdit)}>{editActionLabel} edit permissions</Button>
         </div>
       );
     };
@@ -110,23 +125,23 @@ const PermissionSelector = React.createClass({
    * onClick actions for bulk edits
    */
 
-  _toggleAllStreamsRead(streamIds) {
-    this._toggleReadPermissions('streams', streamIds);
+  _toggleAllStreamsRead(streamIds, clearPermissions) {
+    this._toggleReadPermissions('streams', streamIds, clearPermissions);
   },
 
-  _toggleAllStreamsEdit(streamIds) {
-    this._toggleEditPermissions('streams', streamIds);
+  _toggleAllStreamsEdit(streamIds, clearPermissions) {
+    this._toggleEditPermissions('streams', streamIds, clearPermissions);
   },
 
-  _toggleAllDashboardsRead(dashboardIds) {
-    this._toggleReadPermissions('dashboards', dashboardIds);
+  _toggleAllDashboardsRead(dashboardIds, clearPermissions) {
+    this._toggleReadPermissions('dashboards', dashboardIds, clearPermissions);
   },
 
-  _toggleAllDashboardsEdit(dashboardIds) {
-    this._toggleEditPermissions('dashboards', dashboardIds);
+  _toggleAllDashboardsEdit(dashboardIds, clearPermissions) {
+    this._toggleEditPermissions('dashboards', dashboardIds, clearPermissions);
   },
 
-  _toggleReadPermissions(target, idList) {
+  _toggleReadPermissions(target, idList, clearPermissions = true) {
     let added = Immutable.Set.of();
     let deleted = Immutable.Set.of();
 
@@ -134,7 +149,7 @@ const PermissionSelector = React.createClass({
       const readTarget = `${target}:read:${id}`;
       const editTarget = `${target}:edit:${id}`;
 
-      if (this.props.permissions.contains(readTarget)) {
+      if (this.props.permissions.contains(readTarget) && clearPermissions) {
         deleted = deleted.add(readTarget).add(editTarget);
       } else {
         added = added.add(readTarget);
@@ -142,7 +157,7 @@ const PermissionSelector = React.createClass({
     }, this);
     this.props.onChange(added, deleted);
   },
-  _toggleEditPermissions(target, idList) {
+  _toggleEditPermissions(target, idList, clearPermissions = true) {
     let added = Immutable.Set.of();
     let deleted = Immutable.Set.of();
 
@@ -150,7 +165,7 @@ const PermissionSelector = React.createClass({
       const readTarget = `${target}:read:${id}`;
       const editTarget = `${target}:edit:${id}`;
 
-      if (this.props.permissions.contains(editTarget)) {
+      if (this.props.permissions.contains(editTarget) && clearPermissions) {
         deleted = deleted.add(editTarget);
       } else {
         added = added.add(readTarget).add(editTarget);
