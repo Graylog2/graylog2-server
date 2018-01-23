d4be744385fccff5892a87784f2a7f9486362c17
Edmundo Alvarez <edmundo@graylog.com>
2018-01-23 17:51:29 +0100

    Replace deprecated shorthand functions

--- graylog2-web-interface/src/stores/tools/FocusStore.jsx
+++ graylog2-web-interface/src/stores/tools/FocusStore.jsx
@@ -4,8 +4,8 @@ import $ from 'jquery';
 const FocusStore = Reflux.createStore({
   focus: true,
   init() {
-    $(window).blur(() => { this.trigger({ focus: false }); this.focus = false; });
-    $(window).focus(() => { this.trigger({ focus: true }); this.focus = true; });
+    $(window).on('blur', () => { this.trigger({ focus: false }); this.focus = false; });
+    $(window).on('focus', () => { this.trigger({ focus: true }); this.focus = true; });
   },
   getInitialState() {
     return { focus: this.focus };
