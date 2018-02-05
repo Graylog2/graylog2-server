163dee60b3a1366325a77ec2e3c986216262574e
Edmundo Alvarez <edmundo@graylog.com>
2018-02-05 12:28:13 +0100

    Show header actions also if 1 element is selected
    This is not different than the case when user selects two or more elements
    and it makes easier to figure out what the UI does.

--- graylog2-web-interface/src/components/common/TableList.jsx
+++ graylog2-web-interface/src/components/common/TableList.jsx
@@ -80,7 +80,7 @@ const TableList = React.createClass({
   _headerItem() {
     let bulkHeaderActions;
 
-    if (this.state.selected.count() > 1) {
+    if (this.state.selected.count() > 0) {
       bulkHeaderActions = this.props.headerActionsFactory(this.state.selected);
     }
 
