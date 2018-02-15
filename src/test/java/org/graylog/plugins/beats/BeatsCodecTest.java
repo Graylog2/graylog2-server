1b3c51f2446e406eac1adc37be0ad5c122674e19
Jochen Schalanda <jochen@schalanda.name>
2018-02-15 13:12:55 +0100

    Better support for generic beats and additional metadata (#29)
    * Better support for generic beats and additional metadata
    
    This commit makes `BeatsCodec` handle Beats events more generically.
    
    Instead of handling Filebeat, Packetbeat, Metricbeat, etc. individually,
    the codec now tries to discover the name of the beat and prefix all fields
    found in the Beats event with that name, e. g. "type" in a Filebeat event
    will become "filebeat_type" in the resulting message, and "process_id" in
    a Winlogbeat event will become "winlogbeat_process_id" in the resulting
    message.
    
    The "facility" field will always be "beats" and the "beats_type" field
    will contain the name of the Beat, e. g. "filebeat" for Filebeat and
    "beat" for any other generic beat which doesn't have the respective
    information in "@metadata".
    
    These changes are breaking backward compatibility with messages
    generated by previous versions of this plugin, so extractors and
    pipeline rules will have to be adapted to the new message schemas.
    
    Closes #25
    
    * Add option for adding Beats type as prefix for each key
    
    * Add support for boolean value nodes
    
    * Expand objects nested in arrays

--- src/test/java/org/graylog/plugins/beats/BeatsCodecTest.java
+++ src/test/java/org/graylog/plugins/beats/BeatsCodecTest.java
@@ -27,10 +27,12 @@ import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,14 +41,13 @@ public class BeatsCodecTest {
     @Rule
     public final MockitoRule mockitoRule = MockitoJUnit.rule();
 
-    @Mock
+    private final ObjectMapper objectMapper = new ObjectMapperProvider().get();
     private Configuration configuration;
-    private ObjectMapper objectMapper;
     private BeatsCodec codec;
 
     @Before
     public void setUp() throws Exception {
-        objectMapper = new ObjectMapperProvider().get();
+        configuration = new Configuration(Collections.singletonMap("beats_prefix", true));
         codec = new BeatsCodec(configuration, objectMapper);
     }
 
@@ -56,58 +57,76 @@ public class BeatsCodecTest {
     }
 
     @Test
+    public void decodeMessagesHandlesFilebeatMessagesWithoutPrefix() throws Exception {
+        configuration = new Configuration(Collections.singletonMap("beats_prefix", false));
+        codec = new BeatsCodec(configuration, objectMapper);
+
+        final Message message = codec.decode(messageFromJson("filebeat.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("TEST");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("filebeat");
+        assertThat(message.getField("source")).isEqualTo("/tmp/test.log");
+        assertThat(message.getField("input_type")).isEqualTo("log");
+        assertThat(message.getField("count")).isEqualTo(1);
+        assertThat(message.getField("offset")).isEqualTo(0);
+        @SuppressWarnings("unchecked") final List<String> tags = (List<String>) message.getField("tags");
+        assertThat(tags).containsOnly("foobar", "test");
+    }
+
+    @Test
     public void decodeMessagesHandlesFilebeatMessages() throws Exception {
-        final byte[] json = Resources.toByteArray(Resources.getResource("filebeat.json"));
-        final RawMessage rawMessage = new RawMessage(json);
-        final Message message = codec.decode(rawMessage);
+        final Message message = codec.decode(messageFromJson("filebeat.json"));
         assertThat(message).isNotNull();
         assertThat(message.getMessage()).isEqualTo("TEST");
         assertThat(message.getSource()).isEqualTo("example.local");
         assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
-        assertThat(message.getField("facility")).isEqualTo("filebeat");
-        assertThat(message.getField("file")).isEqualTo("/tmp/test.log");
-        assertThat(message.getField("type")).isEqualTo("log");
-        assertThat(message.getField("count")).isEqualTo(1);
-        assertThat(message.getField("offset")).isEqualTo(0);
-        @SuppressWarnings("unchecked")
-        final List<String> tags = (List<String>) message.getField("tags");
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("filebeat");
+        assertThat(message.getField("filebeat_source")).isEqualTo("/tmp/test.log");
+        assertThat(message.getField("filebeat_input_type")).isEqualTo("log");
+        assertThat(message.getField("filebeat_count")).isEqualTo(1);
+        assertThat(message.getField("filebeat_offset")).isEqualTo(0);
+        @SuppressWarnings("unchecked") final List<String> tags = (List<String>) message.getField("filebeat_tags");
         assertThat(tags).containsOnly("foobar", "test");
     }
 
     @Test
     public void decodeMessagesHandlesPacketbeatMessages() throws Exception {
-        final byte[] json = Resources.toByteArray(Resources.getResource("packetbeat-dns.json"));
-        final RawMessage rawMessage = new RawMessage(json);
-        final Message message = codec.decode(rawMessage);
+        final Message message = codec.decode(messageFromJson("packetbeat-dns.json"));
         assertThat(message).isNotNull();
         assertThat(message.getSource()).isEqualTo("example.local");
         assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
-        assertThat(message.getField("facility")).isEqualTo("packetbeat");
-        assertThat(message.getField("type")).isEqualTo("dns");
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("packetbeat");
+        assertThat(message.getField("packetbeat_type")).isEqualTo("dns");
+        assertThat(message.getField("packetbeat_status")).isEqualTo("OK");
+        assertThat(message.getField("packetbeat_method")).isEqualTo("QUERY");
+        assertThat(message.getField("packetbeat_dns_answers_0_type")).isEqualTo("A");
+        assertThat(message.getField("packetbeat_dns_flags_recursion_allowed")).isEqualTo(true);
     }
 
     @Test
     public void decodeMessagesHandlesTopbeatMessages() throws Exception {
-        final byte[] json = Resources.toByteArray(Resources.getResource("topbeat-system.json"));
-        final RawMessage rawMessage = new RawMessage(json);
-        final Message message = codec.decode(rawMessage);
+        final Message message = codec.decode(messageFromJson("topbeat-system.json"));
         assertThat(message).isNotNull();
         assertThat(message.getSource()).isEqualTo("example.local");
         assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
-        assertThat(message.getField("facility")).isEqualTo("topbeat");
-        assertThat(message.getField("type")).isEqualTo("system");
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("topbeat");
+        assertThat(message.getField("topbeat_type")).isEqualTo("system");
     }
 
     @Test
     public void decodeMessagesHandlesWinlogbeatMessages() throws Exception {
-        final byte[] json = Resources.toByteArray(Resources.getResource("winlogbeat.json"));
-        final RawMessage rawMessage = new RawMessage(json);
-        final Message message = codec.decode(rawMessage);
+        final Message message = codec.decode(messageFromJson("winlogbeat.json"));
         assertThat(message).isNotNull();
         assertThat(message.getSource()).isEqualTo("example.local");
         assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 11, 24, 12, 13, DateTimeZone.UTC));
-        assertThat(message.getField("facility")).isEqualTo("winlogbeat");
-        assertThat(message.getField("type")).isEqualTo("wineventlog");
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("winlogbeat");
+        assertThat(message.getField("winlogbeat_type")).isEqualTo("wineventlog");
         assertThat(message.getField("winlogbeat_level")).isEqualTo("Information");
         assertThat(message.getField("winlogbeat_event_id")).isEqualTo(5024);
         assertThat(message.getField("winlogbeat_process_id")).isEqualTo(500);
@@ -116,27 +135,25 @@ public class BeatsCodecTest {
 
     @Test
     public void decodeMessagesHandleGenericBeatMessages() throws Exception {
-        final byte[] json = Resources.toByteArray(Resources.getResource("generic.json"));
-        final RawMessage rawMessage = new RawMessage(json);
-        final Message message = codec.decode(rawMessage);
+        final Message message = codec.decode(messageFromJson("generic.json"));
         assertThat(message).isNotNull();
         assertThat(message.getSource()).isEqualTo("unknown");
         assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
-        assertThat(message.getField("facility")).isEqualTo("genericbeat");
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
         assertThat(message.getField("beat_foo")).isEqualTo("bar");
     }
 
     @Test
     public void decodeMessagesHandleGenericBeatMessagesWithFields() throws Exception {
-        final byte[] json = Resources.toByteArray(Resources.getResource("generic-with-fields.json"));
-        final RawMessage rawMessage = new RawMessage(json);
-        final Message message = codec.decode(rawMessage);
+        final Message message = codec.decode(messageFromJson("generic-with-fields.json"));
         assertThat(message).isNotNull();
         assertThat(message.getSource()).isEqualTo("unknown");
         assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
-        assertThat(message.getField("facility")).isEqualTo("genericbeat");
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
         assertThat(message.getField("beat_foo")).isEqualTo("bar");
-        assertThat(message.getField("foo_field")).isEqualTo("bar");
+        assertThat(message.getField("beat_fields_foo_field")).isEqualTo("bar");
     }
 
     @Test
@@ -161,13 +178,129 @@ public class BeatsCodecTest {
         };
 
         for (String testFile : testFiles) {
-            final byte[] json = Resources.toByteArray(Resources.getResource(testFile));
-            final RawMessage rawMessage = new RawMessage(json);
-            final Message message = codec.decode(rawMessage);
+            final Message message = codec.decode(messageFromJson(testFile));
             assertThat(message).isNotNull();
             assertThat(message.getSource()).isEqualTo("example.local");
             assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 12, 14, 12, 0, DateTimeZone.UTC));
-            assertThat(message.getField("facility")).isEqualTo("metricbeat");
+            assertThat(message.getField("facility")).isEqualTo("beats");
+            assertThat(message.getField("beats_type")).isEqualTo("metricbeat");
         }
+    }
+
+    @Test
+    public void decodeMessagesHandlesGenericBeatWithDocker() throws Exception {
+        final Message message = codec.decode(messageFromJson("generic-with-docker.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("-");
+        assertThat(message.getSource()).isEqualTo("unknown");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
+        assertThat(message.getField("beat_foo")).isEqualTo("bar");
+        assertThat(message.getField("beat_docker_id")).isEqualTo("123");
+        assertThat(message.getField("beat_docker_name")).isEqualTo("container-1");
+        assertThat(message.getField("beat_docker_labels_docker-kubernetes-pod")).isEqualTo("hello");
+    }
+
+    @Test
+    public void decodeMessagesHandlesGenericBeatWithKubernetes() throws Exception {
+        final Message message = codec.decode(messageFromJson("generic-with-kubernetes.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("-");
+        assertThat(message.getSource()).isEqualTo("unknown");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
+        assertThat(message.getField("beat_foo")).isEqualTo("bar");
+        assertThat(message.getField("beat_kubernetes_pod_name")).isEqualTo("testpod");
+        assertThat(message.getField("beat_kubernetes_namespace")).isEqualTo("testns");
+        assertThat(message.getField("beat_kubernetes_labels_labelkey")).isEqualTo("labelvalue");
+    }
+
+    @Test
+    public void decodeMessagesHandlesGenericBeatWithCloudAlibaba() throws Exception {
+        final Message message = codec.decode(messageFromJson("generic-with-cloud-alibaba.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("-");
+        assertThat(message.getSource()).isEqualTo("unknown");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
+        assertThat(message.getField("beat_foo")).isEqualTo("bar");
+        assertThat(message.getField("beat_meta_cloud_provider")).isEqualTo("ecs");
+        assertThat(message.getField("beat_meta_cloud_instance_id")).isEqualTo("i-wz9g2hqiikg0aliyun2b");
+        assertThat(message.getField("beat_meta_cloud_availability_zone")).isEqualTo("cn-shenzhen");
+        assertThat(message.getField("beat_meta_cloud_region")).isEqualTo("cn-shenzhen-a");
+    }
+
+    @Test
+    public void decodeMessagesHandlesGenericBeatWithCloudDigitalOcean() throws Exception {
+        final Message message = codec.decode(messageFromJson("generic-with-cloud-digital-ocean.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("-");
+        assertThat(message.getSource()).isEqualTo("unknown");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
+        assertThat(message.getField("beat_foo")).isEqualTo("bar");
+        assertThat(message.getField("beat_meta_cloud_provider")).isEqualTo("digitalocean");
+        assertThat(message.getField("beat_meta_cloud_instance_id")).isEqualTo("1234567");
+        assertThat(message.getField("beat_meta_cloud_region")).isEqualTo("nyc2");
+    }
+
+    @Test
+    public void decodeMessagesHandlesGenericBeatWithCloudEC2() throws Exception {
+        final Message message = codec.decode(messageFromJson("generic-with-cloud-ec2.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("-");
+        assertThat(message.getSource()).isEqualTo("unknown");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
+        assertThat(message.getField("beat_foo")).isEqualTo("bar");
+        assertThat(message.getField("beat_meta_cloud_provider")).isEqualTo("ec2");
+        assertThat(message.getField("beat_meta_cloud_machine_type")).isEqualTo("t2.medium");
+        assertThat(message.getField("beat_meta_cloud_instance_id")).isEqualTo("i-4e123456");
+        assertThat(message.getField("beat_meta_cloud_region")).isEqualTo("us-east-1");
+        assertThat(message.getField("beat_meta_cloud_availability_zone")).isEqualTo("us-east-1c");
+    }
+
+    @Test
+    public void decodeMessagesHandlesGenericBeatWithCloudGCE() throws Exception {
+        final Message message = codec.decode(messageFromJson("generic-with-cloud-gce.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("-");
+        assertThat(message.getSource()).isEqualTo("unknown");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
+        assertThat(message.getField("beat_foo")).isEqualTo("bar");
+        assertThat(message.getField("beat_meta_cloud_provider")).isEqualTo("gce");
+        assertThat(message.getField("beat_meta_cloud_machine_type")).isEqualTo("projects/1234567890/machineTypes/f1-micro");
+        assertThat(message.getField("beat_meta_cloud_instance_id")).isEqualTo("1234556778987654321");
+        assertThat(message.getField("beat_meta_cloud_project_id")).isEqualTo("my-dev");
+        assertThat(message.getField("beat_meta_cloud_availability_zone")).isEqualTo("projects/1234567890/zones/us-east1-b");
+    }
+
+    @Test
+    public void decodeMessagesHandlesGenericBeatWithCloudTencent() throws Exception {
+        final Message message = codec.decode(messageFromJson("generic-with-cloud-tencent.json"));
+        assertThat(message).isNotNull();
+        assertThat(message.getMessage()).isEqualTo("-");
+        assertThat(message.getSource()).isEqualTo("unknown");
+        assertThat(message.getTimestamp()).isEqualTo(new DateTime(2016, 4, 1, 0, 0, DateTimeZone.UTC));
+        assertThat(message.getField("facility")).isEqualTo("beats");
+        assertThat(message.getField("beats_type")).isEqualTo("beat");
+        assertThat(message.getField("beat_foo")).isEqualTo("bar");
+        assertThat(message.getField("beat_meta_cloud_provider")).isEqualTo("qcloud");
+        assertThat(message.getField("beat_meta_cloud_instance_id")).isEqualTo("ins-qcloudv5");
+        assertThat(message.getField("beat_meta_cloud_region")).isEqualTo("china-south-gz");
+        assertThat(message.getField("beat_meta_cloud_availability_zone")).isEqualTo("gz-azone2");
+    }
+
+    private RawMessage messageFromJson(String resourceName) throws IOException {
+        final URL resource = Resources.getResource(resourceName);
+        final byte[] json = Resources.toByteArray(resource);
+        return new RawMessage(json);
     }
 }
