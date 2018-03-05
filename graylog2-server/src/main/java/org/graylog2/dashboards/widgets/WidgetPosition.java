a7900cc378064f8e816eec15f1e6b68d4dc6d579
Konrad Merz <konrad@graylog.com>
2018-03-05 15:06:32 +0100

    Remove unused imports from WidgetPosition

--- graylog2-server/src/main/java/org/graylog2/dashboards/widgets/WidgetPosition.java
+++ graylog2-server/src/main/java/org/graylog2/dashboards/widgets/WidgetPosition.java
@@ -16,12 +16,7 @@
  */
 package org.graylog2.dashboards.widgets;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
-import org.graylog.autovalue.WithBeanGetter;
 
 @AutoValue
 public abstract class WidgetPosition {
