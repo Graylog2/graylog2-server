092683c6af1445cc7a7c36b8ed08342bac2eda83
Konrad Merz <konrad@graylog.com>
2018-03-12 16:33:41 +0100

    Add new content-packs overview
    For now I use a static list of content-packs to be shown
    in the overview. This will obviously change later on during development.
    
    I used a ControlledList to create the new ContentPacksList as I needed
    it. This list has filter and pagination implemented.

---
+++ graylog2-web-interface/src/components/content-packs/ContentPacksList.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import 'helpers/mocking/react-dom_mock';
+
+import ContentPacksList from 'components/content-packs/ContentPacksList';
+
+describe('<ContentPacksList />', () => {
+  const contentPacks = [
+    { id: '1', title: 'UFW Grok Patterns', summary: 'Grok Patterns to extract informations from UFW logfiles', version: '1.0', states: ['installed', 'edited'] },
+    { id: '2', title: 'Rails Log Patterns', summary: 'Patterns to retreive rails production logs', version: '2.1', states: [] },
+    { id: '3', title: 'Backup Content Pack', summary: '', version: '3.0', states: ['installed'] },
+    { id: '4', title: 'SSH Archive', summary: 'A crypted backup over ssh.', version: '3.4', states: ['error'] },
+    { id: '5', title: 'FTP Backup', summary: 'Fast but insecure backup', version: '1.0', states: ['installed', 'updatable'] },
+    { id: '6', title: 'UFW Grok Patterns', summary: 'Grok Patterns to extract informations from UFW logfiles', version: '1.0', states: ['installed', 'edited'] },
+    { id: '7', title: 'Rails Log Patterns', summary: 'Patterns to retreive rails production logs', version: '2.1', states: [] },
+    { id: '8', title: 'Backup Content Pack', summary: '', version: '3.0', states: ['installed'] },
+    { id: '9', title: 'SSH Archive', summary: 'A crypted backup over ssh.', version: '3.4', states: ['error'] },
+    { id: '10', title: 'FTP Backup', summary: 'Fast but insecure backup', version: '1.0', states: ['installed', 'updatable'] },
+    { id: '11', title: 'UFW Grok Patterns', summary: 'Grok Patterns to extract informations from UFW logfiles', version: '1.0', states: ['installed', 'edited'] },
+    { id: '12', title: 'Rails Log Patterns', summary: 'Patterns to retreive rails production logs', version: '2.1', states: [] },
+    { id: '13', title: 'Backup Content Pack', summary: '', version: '3.0', states: ['installed'] },
+    { id: '14', title: 'SSH Archive', summary: 'A crypted backup over ssh.', version: '3.4', states: ['error'] },
+    { id: '15', title: 'FTP Backup', summary: 'Fast but insecure backup', version: '1.0', states: ['installed', 'updatable'] },
+  ];
+
+  it('should render with empty content-packs', () => {
+    const wrapper = renderer.create(<ContentPacksList contentPacks={[]} />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with content-packs', () => {
+    const wrapper = renderer.create(<ContentPacksList contentPacks={contentPacks} />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+});
