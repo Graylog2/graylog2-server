ecbefe369e52747ea4731a01ce619c1bde912444
Dennis Oelkers <dennis@graylog.com>
2018-03-13 16:03:08 +0100

    Fixing linter hints.

--- graylog2-web-interface/test/setup-jest.jsx
+++ graylog2-web-interface/test/setup-jest.jsx
@@ -1,10 +1,8 @@
 import jQuery from 'jquery';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 
 global.$ = jQuery;
 global.jQuery = jQuery;
 
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
-
 configure({ adapter: new Adapter() });
-
