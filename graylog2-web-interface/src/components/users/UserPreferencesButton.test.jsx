d1c8d3b6bb60114f25ac53cecc2244137e8fb4ba
Dennis Oelkers <dennis@graylog.com>
2018-03-13 16:03:58 +0100

    Fixing linter hints.

--- graylog2-web-interface/src/components/users/UserPreferencesButton.test.jsx
+++ graylog2-web-interface/src/components/users/UserPreferencesButton.test.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 import { CombinedProviderMock, StoreMock } from 'helpers/mocking';
 
 const e = () => {
+  // eslint-disable-next-line global-require
   const enzyme = require('enzyme');
+  // eslint-disable-next-line global-require
   const Adapter = require('enzyme-adapter-react-15');
   enzyme.configure({ adapter: new Adapter() });
   return enzyme;
 };
 
-describe('UserPreferencesButton', function () {
+describe('UserPreferencesButton', () => {
   beforeEach(() => {
     jest.resetModules();
   });
 
-  it('should load user data when user clicks edit button', function () {
+  it('should load user data when user clicks edit button', () => {
     const PreferencesStore = StoreMock('get', 'listen', 'loadUserPreferences');
     const combinedProviderMock = new CombinedProviderMock({
       Preferences: { PreferencesStore },
@@ -22,6 +24,7 @@ describe('UserPreferencesButton', function () {
 
     jest.doMock('injection/CombinedProvider', () => combinedProviderMock);
 
+    // eslint-disable-next-line global-require
     const UserPreferencesButton = require('components/users/UserPreferencesButton');
     const userName = 'Full';
     const instance = e().mount(<UserPreferencesButton userName={userName} />);
