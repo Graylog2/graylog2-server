04bb262b43c376d319d18f5c582c9dddc3863326
Konrad Merz <konrad@graylog.com>
2018-03-14 10:28:55 +0100

    Content Pack Constraints view

---
+++ graylog2-web-interface/src/components/content-packs/ContentPackConstraints.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { DataTable } from 'components/common';
+import { Badge } from 'react-bootstrap';
+import ContentPackConstraintsStyle from './ContentPackConstraints.css';
+
+class ContentPackConstraints extends React.Component {
+  static propTypes = {
+    constraints: PropTypes.object.isRequired,
+  };
+
+  static rowFormatter(item) {
+    const fullfilledIcon = item.fullfilled ? <i className="fa fa-check" /> : <i className="fa fa-times" />;
+    const fullfilledBg = item.fullfilled ? 'success' : 'failure';
+    return (
+      <tr key={item.id}>
+        <td>{item.type}</td>
+        <td>{item.name}</td>
+        <td>{item.version}</td>
+        <td><Badge className={fullfilledBg}>{fullfilledIcon}</Badge></td>
+      </tr>
+    );
+  }
+
+  render() {
+    const headers = ['Name', 'Type', 'Version', 'Fullfilled'];
+    return (
+      <DataTable
+        id="content-packs-constraints"
+        headers={headers}
+        headerCellFormatter={header => <th>{header}</th>}
+        sortByKey="type"
+        dataRowFormatter={ContentPackConstraints.rowFormatter}
+        rows={this.props.constraints}
+        filterKeys={['name']}
+      />);
+  }
+}
+
+export default ContentPackConstraints;
