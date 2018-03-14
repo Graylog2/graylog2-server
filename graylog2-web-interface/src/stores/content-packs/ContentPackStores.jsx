9b7e5477ad32e5a944e4d030bbf65b4cd915e38e
Konrad Merz <konrad@graylog.com>
2018-03-14 16:13:22 +0100

    Add Component to select a revision of a content pack

--- graylog2-web-interface/src/stores/content-packs/ContentPackStores.jsx
+++ graylog2-web-interface/src/stores/content-packs/ContentPackStores.jsx
@@ -33,14 +33,26 @@ This content pack provides several useful dashboards for auditing Active Directo
 *   Logon Summary - Failed Authentication Attempts, Interactive Logins
 `;
 
-    this.contentPack = { id: '1', title: 'UFW Grok Patterns', summary: 'Grok Patterns to extract informations from UFW logfiles',
-      version: '1.0', states: ['installed', 'edited'], vendor: 'graylog.org <info@graylog.org>',
-      description: this.readme,
-      url: "https://github.com/graylog2/graylog2-server",
-      constraints: [
-        {type: "Server", name: "graylog", version: "3.0", fullfilled: true},
-      ],
+    this.contentPack = {
+      '1.0': { id: '1', title: 'UFW Grok Patterns', summary: 'Grok Patterns to extract informations from UFW logfiles',
+        version: '1.0', states: ['installed', 'edited'], vendor: 'graylog.org <info@graylog.org>',
+        description: this.readme,
+        url: "https://github.com/graylog2/graylog2-server",
+        constraints: [
+          {type: "Server", name: "graylog", version: "3.0", fullfilled: true},
+        ],
+      },
+      '2.0': { id: '1', title: 'UFW Grok Patterns', summary: 'Grok Patterns to extract informations from UFW logfiles',
+        version: '2.0', states: ['installed', 'edited'], vendor: 'graylog.org <info@graylog.org>',
+        description: this.readme,
+        url: "https://github.com/graylog2/graylog2-server",
+        constraints: [
+          {type: "Server", name: "graylog", version: "3.0", fullfilled: true},
+          {type: "Plugin", name: "IntelThreadPlugin", version: "1.0", fullfilled: false},
+        ],
+      },
     };
+
   },
 
   get(contentPackId) {
