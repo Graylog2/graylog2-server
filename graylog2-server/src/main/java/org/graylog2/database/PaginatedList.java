8570cb12c6540a9d85a9a23156f2580af36a6779
Bernd Ahlers <bernd@graylog.com>
2018-03-19 19:07:12 +0100

    Convert PaginationInfo to AutoValue

--- graylog2-server/src/main/java/org/graylog2/database/PaginatedList.java
+++ graylog2-server/src/main/java/org/graylog2/database/PaginatedList.java
@@ -18,6 +18,7 @@ package org.graylog2.database;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ForwardingList;
 import com.google.common.collect.ImmutableMap;
@@ -33,9 +34,9 @@ public class PaginatedList<E> extends ForwardingList<E> {
 
     private final PaginationInfo paginationInfo;
 
-    public PaginatedList(@Nonnull List<E> delegate, int globalTotal, int page, int perPage) {
+    public PaginatedList(@Nonnull List<E> delegate, int total, int page, int perPage) {
         this.delegate = delegate;
-        this.paginationInfo = new PaginationInfo(globalTotal, page, perPage);
+        this.paginationInfo = PaginationInfo.create(total, delegate.size(), page, perPage);
     }
 
     @Override
@@ -78,69 +79,31 @@ public class PaginatedList<E> extends ForwardingList<E> {
     }
 
     @JsonAutoDetect
-    public class PaginationInfo {
-        private final int globalTotal;
-        private final int page;
-        private final int perPage;
-
-        public PaginationInfo(int globalTotal, int page, int perPage) {
-            this.globalTotal = globalTotal;
-            this.page = page;
-            this.perPage = perPage;
-        }
+    @AutoValue
+    public static abstract class PaginationInfo {
+        @JsonProperty("total")
+        public abstract int total();
 
         @JsonProperty("count")
-        public int getCount() {
-            return delegate().size();
-        }
-
-        @JsonProperty("total")
-        public int getGlobalTotal() {
-            return globalTotal;
-        }
+        public abstract int count();
 
         @JsonProperty("page")
-        public int getPage() {
-            return page;
-        }
+        public abstract int page();
 
         @JsonProperty("per_page")
-        public int getPerPage() {
-            return perPage;
+        public abstract int perPage();
+
+        public static PaginationInfo create(int total, int count, int page, int perPage) {
+            return new AutoValue_PaginatedList_PaginationInfo(total, count, page, perPage);
         }
 
         public ImmutableMap<String, Object> asMap() {
             return ImmutableMap.of(
-                    "total", globalTotal,
-                    "page", page,
-                    "per_page", perPage,
-                    "count", getCount()
+                    "total", total(),
+                    "page", page(),
+                    "per_page", perPage(),
+                    "count", count()
             );
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof PaginatedList.PaginationInfo)) return false;
-            @SuppressWarnings("unchecked")
-            PaginationInfo that = (PaginationInfo) o;
-            return globalTotal == that.globalTotal &&
-                    page == that.page &&
-                    perPage == that.perPage;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(globalTotal, page, perPage);
-        }
-
-        @Override
-        public String toString() {
-            return MoreObjects.toStringHelper(this)
-                    .add("globalTotal", globalTotal)
-                    .add("page", page)
-                    .add("perPage", perPage)
-                    .toString();
         }
     }
 }
--- graylog2-server/src/test/java/org/graylog2/database/PaginatedDbServiceTest.java
+++ graylog2-server/src/test/java/org/graylog2/database/PaginatedDbServiceTest.java
@@ -136,32 +136,32 @@ public class PaginatedDbServiceTest {
 
         final PaginatedList<TestDTO> page1 = dbService.findPaginatedWithQueryAndSort(DBQuery.empty(), DBSort.asc("title"), 1, 2);
 
-        assertThat(page1.pagination().getCount()).isEqualTo(2);
-        assertThat(page1.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page1.pagination().count()).isEqualTo(2);
+        assertThat(page1.pagination().total()).isEqualTo(5);
         assertThat(page1.delegate())
                 .extracting("title")
                 .containsExactly("hello1", "hello2");
 
         final PaginatedList<TestDTO> page2 = dbService.findPaginatedWithQueryAndSort(DBQuery.empty(), DBSort.asc("title"), 2, 2);
 
-        assertThat(page2.pagination().getCount()).isEqualTo(2);
-        assertThat(page2.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page2.pagination().count()).isEqualTo(2);
+        assertThat(page2.pagination().total()).isEqualTo(5);
         assertThat(page2.delegate())
                 .extracting("title")
                 .containsExactly("hello3", "hello4");
 
         final PaginatedList<TestDTO> page3 = dbService.findPaginatedWithQueryAndSort(DBQuery.empty(), DBSort.asc("title"), 3, 2);
 
-        assertThat(page3.pagination().getCount()).isEqualTo(1);
-        assertThat(page3.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page3.pagination().count()).isEqualTo(1);
+        assertThat(page3.pagination().total()).isEqualTo(5);
         assertThat(page3.delegate())
                 .extracting("title")
                 .containsExactly("hello5");
 
         final PaginatedList<TestDTO> page1reverse = dbService.findPaginatedWithQueryAndSort(DBQuery.empty(), DBSort.desc("title"), 1, 2);
 
-        assertThat(page1reverse.pagination().getCount()).isEqualTo(2);
-        assertThat(page1reverse.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page1reverse.pagination().count()).isEqualTo(2);
+        assertThat(page1reverse.pagination().total()).isEqualTo(5);
         assertThat(page1reverse.delegate())
                 .extracting("title")
                 .containsExactly("hello5", "hello4");
@@ -181,40 +181,40 @@ public class PaginatedDbServiceTest {
 
         final PaginatedList<TestDTO> page1 = dbService.findPaginatedWithQueryFilterAndSort(DBQuery.empty(), filter, DBSort.asc("title"), 1, 2);
 
-        assertThat(page1.pagination().getCount()).isEqualTo(2);
-        assertThat(page1.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page1.pagination().count()).isEqualTo(2);
+        assertThat(page1.pagination().total()).isEqualTo(5);
         assertThat(page1.delegate())
                 .extracting("title")
                 .containsExactly("hello2", "hello3");
 
         final PaginatedList<TestDTO> page2 = dbService.findPaginatedWithQueryFilterAndSort(DBQuery.empty(), filter, DBSort.asc("title"), 2, 2);
 
-        assertThat(page2.pagination().getCount()).isEqualTo(2);
-        assertThat(page2.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page2.pagination().count()).isEqualTo(2);
+        assertThat(page2.pagination().total()).isEqualTo(5);
         assertThat(page2.delegate())
                 .extracting("title")
                 .containsExactly("hello4", "hello5");
 
         final PaginatedList<TestDTO> page3 = dbService.findPaginatedWithQueryFilterAndSort(DBQuery.empty(), filter, DBSort.asc("title"), 3, 2);
 
-        assertThat(page3.pagination().getCount()).isEqualTo(1);
-        assertThat(page3.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page3.pagination().count()).isEqualTo(1);
+        assertThat(page3.pagination().total()).isEqualTo(5);
         assertThat(page3.delegate())
                 .extracting("title")
                 .containsExactly("hello6");
 
         final PaginatedList<TestDTO> page4 = dbService.findPaginatedWithQueryFilterAndSort(DBQuery.empty(), filter, DBSort.asc("title"), 2, 4);
 
-        assertThat(page4.pagination().getCount()).isEqualTo(1);
-        assertThat(page4.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page4.pagination().count()).isEqualTo(1);
+        assertThat(page4.pagination().total()).isEqualTo(5);
         assertThat(page4.delegate())
                 .extracting("title")
                 .containsExactly("hello6");
 
         final PaginatedList<TestDTO> page1reverse = dbService.findPaginatedWithQueryFilterAndSort(DBQuery.empty(), filter, DBSort.desc("title"), 1, 2);
 
-        assertThat(page1reverse.pagination().getCount()).isEqualTo(2);
-        assertThat(page1reverse.pagination().getGlobalTotal()).isEqualTo(5);
+        assertThat(page1reverse.pagination().count()).isEqualTo(2);
+        assertThat(page1reverse.pagination().total()).isEqualTo(5);
         assertThat(page1reverse.delegate())
                 .extracting("title")
                 .containsExactly("hello6", "hello5");
