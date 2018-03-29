ed39b2849fbcd2ae427b09a8c60375dfa1ead97f
Bernd Ahlers <bernd@graylog.com>
2018-03-29 20:50:24 +0200

    Add WidgetConfigModal

--- enterprise/src/web/enterprise/components/widgets/index.jsx
+++ enterprise/src/web/enterprise/components/widgets/index.jsx
@@ -1,3 +1,5 @@
 export { default as Histogram } from './Histogram';
 export { default as MessageList } from './MessageList';
 export { default as SearchSidebarWidget } from './SearchSidebarWidget';
+export { default as WidgetConfigModal } from './WidgetConfigModal';
+export { default as WidgetHeader } from './WidgetHeader';
