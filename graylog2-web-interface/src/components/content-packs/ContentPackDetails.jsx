20ea108294782789576c8cd4de1c5325c595195d
Konrad Merz <konrad@graylog.com>
2018-04-09 15:36:54 +0200

    Use ContentPackDetails as Preview for Content Packs creation
    For that we need to be able to show a content pack without values,
    and there for fix some styling.
    Also add parameters to the list of deatails.

--- graylog2-web-interface/src/components/content-packs/ContentPackDetails.jsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackDetails.jsx
@@ -14,26 +14,34 @@ class ContentPackDetails extends React.Component {
   };
 
   render() {
-    const markdownDescription = markdown.toHTML(this.props.contentPack.description);
+    const markdownDescription = markdown.toHTML(this.props.contentPack.description || '');
 
     return (
       <div id="content-pack-details">
-        <h2>Details</h2><small>Version: {this.props.contentPack.version}</small>
-        <dl className="deflist">
-          <dt>Name:</dt> <dd>{this.props.contentPack.name}</dd>
-          <dt>Summary:</dt> <dd>{this.props.contentPack.summary}</dd>
-          <dt>Vendor:</dt> <dd>{this.props.contentPack.vendor}</dd>
-          <dt>URL:</dt> <dd><a href={this.props.contentPack.url}>{this.props.contentPack.url}</a></dd>
-          <dt>ID:</dt> <dd><code>{this.props.contentPack.id}</code></dd>
-        </dl>
-        <ContentPackStatus states={this.props.contentPack.states} />
+        <h2>Details</h2><small>Version: {this.props.contentPack.rev}</small>
+        <div>
+          <dl className="deflist">
+            <dt>Name:</dt> <dd>{this.props.contentPack.name}&nbsp;</dd>
+            <dt>Summary:</dt> <dd>{this.props.contentPack.summary}&nbsp;</dd>
+            <dt>Vendor:</dt> <dd>{this.props.contentPack.vendor}&nbsp;</dd>
+            <dt>URL:</dt> <dd><a href={this.props.contentPack.url}>{this.props.contentPack.url}</a>&nbsp;</dd>
+            { this.props.contentPack.id && (<span><dt>ID:</dt> <dd><code>{this.props.contentPack.id}</code></dd></span>) }
+            <dt>Parameters:</dt><dd>{this.props.contentPack.parameters.length}</dd>
+          </dl>
+        </div>
+        <br />
+        { this.props.contentPack.status && <ContentPackStatus states={this.props.contentPack.states} /> }
         <br />
         <br />
-        <h2>Constrains</h2>
-        <br />
-        <ContentPackConstraints constraints={this.props.contentPack.constraints} />
-        <br />
-        <h2>Description</h2>
+        { this.props.contentPack.constraints &&
+        <div>
+          <h3>Constrains</h3>
+          <br />
+          <ContentPackConstraints constraints={this.props.contentPack.constraints} />
+          <br />
+        </div>
+        }
+        <h3>Description</h3>
         <br />
         <div dangerouslySetInnerHTML={{ __html: markdownDescription }} />
       </div>
