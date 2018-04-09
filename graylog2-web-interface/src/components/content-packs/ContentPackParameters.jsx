4619c53665b9c80e7bf821a1a769c1cfdd0e0bc4
Konrad Merz <konrad@graylog.com>
2018-04-09 15:28:14 +0200

    Add parameter page content
    This is the content of the content packs wizard parameter page.
    You can now add, list and delete a parameter and it will be stored
    via onStateChange

---
+++ graylog2-web-interface/src/components/content-packs/ContentPackParameters.jsx
@@ -0,0 +1,185 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { Row, Col, Button } from 'react-bootstrap';
+import { Input } from 'components/bootstrap';
+import DataTable from 'components/common/DataTable';
+import FormsUtils from 'util/FormsUtils';
+import ObjectUtils from 'util/ObjectUtils';
+
+class ContentPackParameters extends React.Component {
+  static propTypes = {
+    contentPack: PropTypes.object.isRequired,
+    onStateChange: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onStateChange: () => {},
+  };
+
+  static emptyParameter = {
+    name: '',
+    title: '',
+    description: '',
+    value_type: 'string',
+    default_value: '',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      newParameter: ObjectUtils.clone(ContentPackParameters.emptyParameter),
+    };
+
+    this._addNewParameter = this._addNewParameter.bind(this);
+    this._bindValue = this._bindValue.bind(this);
+  }
+
+  _validateParameter() {
+    const param = this.state.newParameter;
+    if (!param.name || !param.title || !param.description) {
+      return false;
+    }
+    return true;
+  }
+
+  _updateField(name, value) {
+    const updatedParameter = ObjectUtils.clone(this.state.newParameter);
+    updatedParameter[name] = value;
+    this.setState({ newParameter: updatedParameter });
+  }
+
+  _bindValue(event) {
+    this._updateField(event.target.name, FormsUtils.getValueFromInput(event.target));
+  }
+
+  _addNewParameter(e) {
+    e.preventDefault();
+    if (!this._validateParameter()) {
+      return;
+    }
+    const newContentPack = ObjectUtils.clone(this.props.contentPack);
+    newContentPack.parameters.push(this.state.newParameter);
+    this.props.onStateChange(newContentPack);
+    this.setState({ newParameter: ObjectUtils.clone(ContentPackParameters.emptyParameter) });
+  }
+
+  _deleteParameter = (parameter) => {
+    const newContentPack = ObjectUtils.clone(this.props.contentPack);
+    const indexToDelete = newContentPack.parameters.map(p => p.name).indexOf(parameter.name);
+    if (indexToDelete < 0) {
+      return;
+    }
+    newContentPack.parameters.splice(indexToDelete, 1);
+    this.props.onStateChange(newContentPack);
+  };
+
+
+  _parameterRowFormater = (parameter) => {
+    return (
+      <tr key={parameter.name}>
+        <td>{parameter.title}</td>
+        <td>{parameter.name}</td>
+        <td>{parameter.description}</td>
+        <td>{parameter.value_type}</td>
+        <td>{parameter.default_value}</td>
+        <td><Button bsStyle="primary" onClick={() => { this._deleteParameter(parameter); }}>Delete</Button></td>
+      </tr>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col lg={8}>
+            <h2>Create parameters</h2>
+            <br />
+            <form className="form-horizontal content-selection-form" id="content-selection-form" onSubmit={this._addNewParameter}>
+              <fieldset>
+                <Input name="title"
+                       id="title"
+                       type="text"
+                       maxLength={250}
+                       value={this.state.newParameter.title}
+                       onChange={this._bindValue}
+                       labelClassName="col-sm-3"
+                       wrapperClassName="col-sm-9"
+                       label="Title"
+                       help="Give a descriptive title for this content pack."
+                       required />
+                <Input name="name"
+                       id="name"
+                       type="text"
+                       maxLength={250}
+                       value={this.state.newParameter.name}
+                       onChange={this._bindValue}
+                       labelClassName="col-sm-3"
+                       wrapperClassName="col-sm-9"
+                       label="Name"
+                       help="This is used as the parameter reference and must not contain a space."
+                       required />
+                <Input name="description"
+                       id="description"
+                       type="text"
+                       maxLength={250}
+                       value={this.state.newParameter.description}
+                       onChange={this._bindValue}
+                       labelClassName="col-sm-3"
+                       wrapperClassName="col-sm-9"
+                       label="Description"
+                       help="Give a description explaining what will be done with this parameter."
+                       required />
+                <Input name="value_type"
+                       id="value_type"
+                       type="select"
+                       maxLength={250}
+                       value={this.state.newParameter.value_type}
+                       onChange={this._bindValue}
+                       labelClassName="col-sm-3"
+                       wrapperClassName="col-sm-9"
+                       label="Value Type"
+                       help="Give the type of the parameter."
+                       required>
+                  <option value="string">String</option>
+                  <option value="integer">Integer</option>
+                  <option value="ipv4Address">IPv4 Address</option>
+                  <option value="ipv6Address">IPv6 Address</option>
+                  <option value="port">port</option>
+                </Input>
+                <Input name="default_value"
+                       id="default_value"
+                       type="text"
+                       maxLength={250}
+                       value={this.state.newParameter.default_value}
+                       onChange={this._bindValue}
+                       labelClassName="col-sm-3"
+                       wrapperClassName="col-sm-9"
+                       label="Default value"
+                       help="Give a default value if the parameter is not optional." />
+                <Button bsStyle="info" type="submit">Add Parameter</Button>
+              </fieldset>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Parameters list</h2>
+            <br />
+            <DataTable
+              id="parameter-list"
+              headers={['Title', 'Name', 'Description', 'Value Type', 'Default Value']}
+              headerCellFormatter={header => <th>{header}</th>}
+              sortByKey="title"
+              filterKeys={[]}
+              rows={this.props.contentPack.parameters}
+              dataRowFormatter={this._parameterRowFormater}
+            />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ContentPackParameters;
---
+++ graylog2-web-interface/src/components/content-packs/ContentPackParameters.test.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import 'helpers/mocking/react-dom_mock';
+
+import ContentPackParameters from 'components/content-packs/ContentPackParameters';
+
+describe('<ContentPackParameters />', () => {
+  it('should render with empty parameters', () => {
+    const contentPack = {
+      parameters: [],
+    };
+    const wrapper = renderer.create(<ContentPackParameters contentPack={contentPack} />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render a parameter', () => {
+    const contentPack = {
+      parameters: [{
+        name: 'A parameter name',
+        title: 'A parameter title',
+        description: 'A parameter descriptions',
+        value_type: 'string',
+        default_value: 'test',
+      }],
+    };
+    const wrapper = renderer.create(<ContentPackParameters contentPack={contentPack} />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should delete a parameter', () => {
+    const changeFn = jest.fn((newContentPack) => {
+      expect(newContentPack.parameters).toEqual([]);
+    });
+
+    const contentPack = {
+      parameters: [{
+        name: 'A parameter name',
+        title: 'A parameter title',
+        description: 'A parameter descriptions',
+        value_type: 'string',
+        default_value: 'test',
+      }],
+    };
+    const wrapper = mount(<ContentPackParameters contentPack={contentPack} onStateChange={changeFn} />);
+    wrapper.find('button[children="Delete"]').simulate('click');
+    expect(changeFn.mock.calls.length).toBe(1);
+  });
+
+  it('should create a parameter', () => {
+    const changeFn = jest.fn((newContentPack) => {
+      expect(newContentPack.parameters).toEqual([{
+        name: 'name',
+        title: 'title',
+        description: 'descr',
+        value_type: 'string',
+        default_value: 'test',
+      }]);
+    });
+
+    const contentPack = {
+      parameters: [],
+    };
+    const wrapper = mount(<ContentPackParameters contentPack={contentPack} onStateChange={changeFn} />);
+    wrapper.find('input#name').simulate('change', { target: { name: 'name', value: 'name' } });
+    wrapper.find('input#title').simulate('change', { target: { name: 'title', value: 'title' } });
+    wrapper.find('input#description').simulate('change', { target: { name: 'description', value: 'descr' } });
+    wrapper.find('input#default_value').simulate('change', { target: { name: 'default_value', value: 'test' } });
+    wrapper.find('form').at(0).simulate('submit');
+    expect(changeFn.mock.calls.length).toBe(1);
+  });
+
+  it('should not create a parameter if name is missing', () => {
+    const changeFn = jest.fn();
+
+    const contentPack = {
+      parameters: [],
+    };
+    const wrapper = mount(<ContentPackParameters contentPack={contentPack} onStateChange={changeFn} />);
+    wrapper.find('input#title').simulate('change', { target: { name: 'title', value: 'title' } });
+    wrapper.find('input#description').simulate('change', { target: { name: 'description', value: 'descr' } });
+    wrapper.find('input#default_value').simulate('change', { target: { name: 'default_value', value: 'test' } });
+    wrapper.find('form').at(0).simulate('submit');
+    expect(changeFn.mock.calls.length).toBe(0);
+  });
+});
