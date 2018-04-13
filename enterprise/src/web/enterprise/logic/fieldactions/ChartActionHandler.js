de0e053d4219bc759e2887c407f43f738355953b
Dennis Oelkers <dennis@graylog.com>
2018-04-13 17:04:40 +0200

    Generating pivot properly for chart action.

--- enterprise/src/web/enterprise/logic/fieldactions/ChartActionHandler.js
+++ enterprise/src/web/enterprise/logic/fieldactions/ChartActionHandler.js
@@ -1,13 +1,14 @@
 import uuid from 'uuid/v4';
 
 import WidgetActions from 'enterprise/actions/WidgetActions';
+import { pivotForField } from '../searchtypes/aggregation/PivotGenerator';
 
 export default function (viewId, queryId, field) {
   const widget = {
     id: uuid(),
     type: 'AGGREGATION',
     config: {
-      rowPivots: ['timestamp'],
+      rowPivots: [pivotForField('timestamp')],
       columnPivots: [],
       series: [`sum(${field})`],
       sort: [],
