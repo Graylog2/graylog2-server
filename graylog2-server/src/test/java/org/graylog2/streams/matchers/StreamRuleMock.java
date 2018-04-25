2d599289a7f6143ac5229687f2a4a732738c5ad8
Jochen Schalanda <jochen@graylog.com>
2018-04-25 13:12:16 +0200

    Properly set description in StreamRuleMock

--- graylog2-server/src/test/java/org/graylog2/streams/matchers/StreamRuleMock.java
+++ graylog2-server/src/test/java/org/graylog2/streams/matchers/StreamRuleMock.java
@@ -45,6 +45,7 @@ public class StreamRuleMock implements StreamRule {
         this.inverted = (Boolean) rule.get(StreamRuleImpl.FIELD_INVERTED);
         this.contentPack = (String) rule.get(StreamRuleImpl.FIELD_CONTENT_PACK);
         this.streamId = (String) rule.get(StreamRuleImpl.FIELD_STREAM_ID);
+        this.description = (String) rule.get(StreamRuleImpl.FIELD_DESCRIPTION);
     }
 
     @Override
