82c93e63159be622f11bc4e91e67b2b1581db905
Dennis Oelkers <dennis@graylog.com>
2018-05-02 10:36:50 +0200

    Not enforcing focus for widget edit modal.

--- enterprise/src/web/enterprise/components/widgets/EditWidgetFrame.jsx
+++ enterprise/src/web/enterprise/components/widgets/EditWidgetFrame.jsx
@@ -7,7 +7,7 @@ import styles from '!style?insertAt=bottom!css!./EditWidgetFrame.css';
 const EditWidgetDialog = ({ children, ...rest }) => <Modal.Dialog {...rest} dialogClassName={styles.editWidgetDialog}>{children}</Modal.Dialog>;
 
 const EditWidgetFrame = ({ children }) => (
-  <Modal show animation={false} dialogComponentClass={EditWidgetDialog}>
+  <Modal show animation={false} dialogComponentClass={EditWidgetDialog} enforceFocus={false}>
     <div style={{ height: 'calc(100% - 20px)' }}>
       {children}
     </div>
