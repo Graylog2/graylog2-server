ec7b2e48123e50118259c4ca56146335ee7eb76b
Konrad Merz <kmerz@hulud.net>
2018-05-08 09:58:53 +0200

    Rule Helper Filter (#4768)
    * Rule Helper Filter
    
    The Rule Helper displays a lot of documentation for pipeline rules.
    Since it is tiresome to go through 10 pages of pipeline functions
    I add a filter to make it easier to search for a specific pipeline
    rule function.
    
    - Also: Boy Scout Rule: Remove eslint errors.
    
    * Move FilterInput into a reusable component
    
    * Fix newly introduced linter warning!

---
+++ graylog2-web-interface/src/components/common/FilterInput.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import 'helpers/mocking/react-dom_mock';
+
+import FilterInput from 'components/common/FilterInput';
+
+describe('<FilterInput />', () => {
+  it('should render a filter input', () => {
+    const wrapper = renderer.create(<FilterInput onChange={() => {}} />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render a filter input with custom label', () => {
+    const wrapper = renderer.create(<FilterInput
+      onChange={() => {}}
+      filterLabel="Search"
+      labelClassName="col-sm-3"
+      wrapperClassName="col-xs-1"
+    />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should take filter input', () => {
+    const changeFn = jest.fn((value) => {
+      expect(value).toEqual('kaladin');
+    });
+    const wrapper = mount(<FilterInput onChange={changeFn} />);
+
+    wrapper.find('input').simulate('change', { target: { value: 'kaladin' } });
+    expect(changeFn.mock.calls.length).toBe(1);
+  });
+});
