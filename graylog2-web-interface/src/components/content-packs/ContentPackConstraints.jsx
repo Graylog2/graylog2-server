e68b17a58eea62874953d2b35254c50871158000
Konrad Merz <konrad@graylog.com>
2018-05-17 15:35:08 +0200

    Add constraints to the content pack and preview page

--- graylog2-web-interface/src/components/content-packs/ContentPackConstraints.jsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackConstraints.jsx
@@ -8,37 +8,44 @@ import ContentPackConstraintsStyle from './ContentPackConstraints.css';
 class ContentPackConstraints extends React.Component {
   static propTypes = {
     constraints: PropTypes.array,
+    isFullFilled: PropTypes.bool,
   };
 
   static defaultProps = {
     constraints: [],
+    isFullFilled: false,
   };
 
-  static rowFormatter(item) {
-    const fullfilledIcon = item.fullfilled ? <i className="fa fa-check" /> : <i className="fa fa-times" />;
-    const fullfilledBg = item.fullfilled ? 'success' : 'failure';
+  _rowFormatter = (item) => {
+    const fullfilledIcon = item.fullfilled || this.props.isFullFilled ? <i className="fa fa-check" /> : <i className="fa fa-times" />;
+    const fullfilledBg = item.fullfilled || this.props.isFullFilled ? 'success' : 'failure';
     return (
       <tr key={item.id}>
         <td>{item.type}</td>
-        <td>{item.name}</td>
         <td>{item.version}</td>
         <td><Badge className={fullfilledBg}>{fullfilledIcon}</Badge></td>
       </tr>
     );
-  }
+  };
 
   render() {
-    const headers = ['Name', 'Type', 'Version', 'Fullfilled'];
+    const headers = ['Type', 'Version', 'Fullfilled'];
     return (
-      <DataTable
-        id="content-packs-constraints"
-        headers={headers}
-        headerCellFormatter={header => <th>{header}</th>}
-        sortByKey="type"
-        dataRowFormatter={ContentPackConstraints.rowFormatter}
-        rows={this.props.constraints}
-        filterKeys={[]}
-      />);
+      <div>
+        <h2>Constraints</h2>
+        <br />
+        <br />
+        <DataTable
+          id="content-packs-constraints"
+          headers={headers}
+          headerCellFormatter={header => <th>{header}</th>}
+          sortByKey="type"
+          dataRowFormatter={this._rowFormatter}
+          rows={this.props.constraints}
+          filterKeys={[]}
+        />
+      </div>
+    );
   }
 }
 
