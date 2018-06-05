f07df7c071d7455e1e265dc30f76c4d6f6300081
Konrad Merz <konrad@graylog.com>
2018-06-05 14:55:00 +0200

    Add search form to entity list and parameters list

--- graylog2-web-interface/src/components/content-packs/ContentPackEntitiesList.test.jsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackEntitiesList.test.jsx
@@ -1,43 +1,69 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import 'helpers/mocking/react-dom_mock';
 
 import ContentPackEntitiesList from 'components/content-packs/ContentPackEntitiesList';
 
 describe('<ContentPackEntitiesList />', () => {
+  const entity1 = {
+    id: '111-beef',
+    type: 'Input',
+    v: '1.0',
+    data: {
+      name: { type: 'string', value: 'Input' },
+      title: { type: 'string', value: 'A good input' },
+      configuration: {
+        listen_address: { type: 'string', value: '1.2.3.4' },
+        port: { type: 'integer', value: '23' },
+      },
+    },
+  };
+  const entity2 = {
+    id: '121-beef',
+    type: 'Input',
+    v: '1.0',
+    data: {
+      name: { type: 'string', value: 'BadInput' },
+      title: { type: 'string', value: 'A bad input' },
+      configuration: {
+        listen_address: { type: 'string', value: '1.2.3.4' },
+        port: { type: 'integer', value: '22' },
+      },
+    },
+  };
+  const contentPack = {
+    entities: [entity1, entity2],
+    parameters: [{
+      name: 'A parameter name',
+      title: 'A parameter title',
+      description: 'A parameter descriptions',
+      type: 'string',
+      default_value: 'test',
+    }],
+  };
+
   it('should render with empty entities', () => {
-    const contentPack = { entities: [] };
-    const wrapper = renderer.create(<ContentPackEntitiesList contentPack={contentPack} readOnly />);
+    const emptyContentPack = { entities: [] };
+    const wrapper = renderer.create(<ContentPackEntitiesList contentPack={emptyContentPack} readOnly />);
     expect(wrapper.toJSON()).toMatchSnapshot();
   });
 
   it('should render with entities and parameters without readOnly', () => {
-    const entity = {
-      id: '111-beef',
-      v: '1.0',
-      data: {
-        name: { type: 'string', value: 'Input' },
-        title: { type: 'string', value: 'A good input' },
-        configuration: {
-          listen_address: { type: 'string', value: '1.2.3.4' },
-          port: { type: 'integer', value: '23' },
-        },
-      },
-    };
-    const contentPack = {
-      entities: [entity],
-      parameters: [{
-        name: 'A parameter name',
-        title: 'A parameter title',
-        description: 'A parameter descriptions',
-        type: 'string',
-        default_value: 'test',
-      }],
-    };
     const appliedParameter = { '111-beef': [{ configKey: 'title', paramName: 'A parameter name' }] };
 
     const wrapper = renderer.create(<ContentPackEntitiesList contentPack={contentPack}
                                                              appliedParameter={appliedParameter} />);
     expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should filter entities', () => {
+    const wrapper = mount(<ContentPackEntitiesList contentPack={contentPack} />);
+    expect(wrapper.find("td[children='A good input']").exists()).toBe(true);
+    wrapper.find('input').simulate('change', { target: { value: 'Bad' } });
+    wrapper.find('form').simulate('submit');
+    expect(wrapper.find("td[children='A good input']").exists()).toBe(false);
+    wrapper.find("button[children='Reset']").simulate('click');
+    expect(wrapper.find("td[children='A good input']").exists()).toBe(true);
   });
 });
