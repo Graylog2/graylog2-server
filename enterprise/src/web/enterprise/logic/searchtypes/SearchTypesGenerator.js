f3e944a6c3e4702720716ab3278775632610de7f
Dennis Oelkers <dennis@graylog.com>
2018-06-27 14:32:39 +0200

    Use backend support for widget filters. (Graylog2/graylog-plugin-enterprise#280)
    * Add proper babel presets and module mapping for Jest.
    
    This change adds the following for Jest tests:
    
      * `stage-0` and `flow` presets to babel config
      * a module mapping so imports starting with `enterprise` are properly
    resolved
    
    * Use backend support for widget filters.
    
    This change extracts `filter` definitions (added by the widget filter
    control in the UI) and adds them to the generated search types
    definition when a new search is generated. This enables filtering of
    widget data for widgets other than the message list.
    
    Requires Graylog2/graylog-plugin-enterprise#279 to be merged before.
    
    Fixes Graylog2/graylog-plugin-enterprise#170.

--- enterprise/src/web/enterprise/logic/searchtypes/SearchTypesGenerator.js
+++ enterprise/src/web/enterprise/logic/searchtypes/SearchTypesGenerator.js
@@ -4,20 +4,25 @@ import uuid from 'uuid/v4';
 import { widgetDefinition } from '../Widget';
 import { searchTypeDefinition } from '../SearchType';
 
+const filterForWidget = widget => (widget.filter ? { filter: { type: 'query_string', query: widget.filter } } : {});
+
 export default (widgets) => {
   let widgetMapping = Immutable.Map();
-  const searchTypes = widgets.map(widget => widgetDefinition(widget.type).searchTypes(widget.config).map(searchType => Object.assign(searchType, { widgetId: widget.id })))
+  const searchTypes = widgets.map(widget => widgetDefinition(widget.type).searchTypes(widget.config).map(searchType => Object.assign(searchType, { widgetId: widget.id }, filterForWidget(widget))))
     .reduce((acc, cur) => acc.merge(cur), Immutable.Set())
     .map((searchType) => {
       const searchTypeId = uuid();
       widgetMapping = widgetMapping.update(searchType.widgetId, new Immutable.Set(), widgetSearchTypes => widgetSearchTypes.add(searchTypeId));
       const typeDefinition = searchTypeDefinition(searchType.type);
       if (!typeDefinition || !typeDefinition.defaults) {
+        // eslint-disable-next-line no-console
         console.warn(`Unable to find type definition or defaults for search type ${searchType.type} - skipping!`);
       }
       const defaults = typeDefinition ? typeDefinition.defaults : {};
+      const filter = { filter: searchType.filter } || {};
       return new Immutable.Map(defaults)
         .merge(searchType.config)
+        .merge(filter)
         .merge(
           {
             id: searchTypeId,
