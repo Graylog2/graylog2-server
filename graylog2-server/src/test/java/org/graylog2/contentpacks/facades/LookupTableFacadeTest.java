9bb1d9394483f2f9461fa9910724f018e066f42c
Jochen Schalanda <jochen@graylog.com>
2018-07-17 18:00:33 +0200

    Add more tests for LookupTableFacade

--- graylog2-server/src/test/java/org/graylog2/contentpacks/facades/LookupTableFacadeTest.java
+++ graylog2-server/src/test/java/org/graylog2/contentpacks/facades/LookupTableFacadeTest.java
@@ -16,7 +16,10 @@
  */
 package org.graylog2.contentpacks.facades;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.graph.Graph;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb;
@@ -28,7 +31,11 @@ import org.graylog2.contentpacks.model.entities.EntityDescriptor;
 import org.graylog2.contentpacks.model.entities.EntityExcerpt;
 import org.graylog2.contentpacks.model.entities.EntityV1;
 import org.graylog2.contentpacks.model.entities.EntityWithConstraints;
+import org.graylog2.contentpacks.model.entities.LookupCacheEntity;
+import org.graylog2.contentpacks.model.entities.LookupDataAdapterEntity;
 import org.graylog2.contentpacks.model.entities.LookupTableEntity;
+import org.graylog2.contentpacks.model.entities.NativeEntity;
+import org.graylog2.contentpacks.model.entities.references.ReferenceMapUtils;
 import org.graylog2.contentpacks.model.entities.references.ValueReference;
 import org.graylog2.database.MongoConnectionRule;
 import org.graylog2.events.ClusterEventBus;
@@ -42,6 +49,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.Executors;
@@ -58,13 +67,14 @@ public class LookupTableFacadeTest {
 
     private final ObjectMapper objectMapper = new ObjectMapperProvider().get();
 
+    private DBLookupTableService lookupTableService;
     private LookupTableFacade facade;
 
     @Before
     @SuppressForbidden("Using Executors.newSingleThreadExecutor() is okay in tests")
     public void setUp() throws Exception {
         final ClusterEventBus clusterEventBus = new ClusterEventBus("cluster-event-bus", Executors.newSingleThreadExecutor());
-        final DBLookupTableService lookupTableService = new DBLookupTableService(
+        lookupTableService = new DBLookupTableService(
                 mongoRule.getMongoConnection(),
                 new MongoJackObjectMapperProvider(objectMapper),
                 clusterEventBus);
@@ -73,7 +83,7 @@ public class LookupTableFacadeTest {
     }
 
     @Test
-    public void encode() {
+    public void exportEntity() {
         final LookupTableDto lookupTableDto = LookupTableDto.builder()
                 .id("1234567890")
                 .name("lookup-table-name")
@@ -104,6 +114,157 @@ public class LookupTableFacadeTest {
         assertThat(lookupTableEntity.defaultSingleValueType()).isEqualTo(ValueReference.of(LookupDefaultValue.Type.STRING));
         assertThat(lookupTableEntity.defaultMultiValue()).isEqualTo(ValueReference.of("default-multi"));
         assertThat(lookupTableEntity.defaultMultiValueType()).isEqualTo(ValueReference.of(LookupDefaultValue.Type.STRING));
+    }
+
+    @Test
+    @UsingDataSet(locations = "/org/graylog2/contentpacks/lut_tables.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void exportNativeEntity() {
+        final EntityDescriptor descriptor = EntityDescriptor.create(ModelId.of("5adf24dd4b900a0fdb4e530d"), ModelTypes.LOOKUP_TABLE);
+        final EntityWithConstraints entityWithConstraints = facade.exportEntity(descriptor).orElseThrow(AssertionError::new);
+        final Entity entity = entityWithConstraints.entity();
+
+        assertThat(entity).isInstanceOf(EntityV1.class);
+        assertThat(entity.id()).isEqualTo(ModelId.of("5adf24dd4b900a0fdb4e530d"));
+        assertThat(entity.type()).isEqualTo(ModelTypes.LOOKUP_TABLE);
+
+        final EntityV1 entityV1 = (EntityV1) entity;
+        final LookupTableEntity lookupTableEntity = objectMapper.convertValue(entityV1.data(), LookupTableEntity.class);
+        assertThat(lookupTableEntity.name()).isEqualTo(ValueReference.of("http-dsv-no-cache"));
+        assertThat(lookupTableEntity.title()).isEqualTo(ValueReference.of("HTTP DSV without Cache"));
+        assertThat(lookupTableEntity.description()).isEqualTo(ValueReference.of("HTTP DSV without Cache"));
+        assertThat(lookupTableEntity.dataAdapterName()).isEqualTo(ValueReference.of("5adf24a04b900a0fdb4e52c8"));
+        assertThat(lookupTableEntity.cacheName()).isEqualTo(ValueReference.of("5adf24b24b900a0fdb4e52dd"));
+        assertThat(lookupTableEntity.defaultSingleValue()).isEqualTo(ValueReference.of("Default single value"));
+        assertThat(lookupTableEntity.defaultSingleValueType()).isEqualTo(ValueReference.of(LookupDefaultValue.Type.STRING));
+        assertThat(lookupTableEntity.defaultMultiValue()).isEqualTo(ValueReference.of("Default multi value"));
+        assertThat(lookupTableEntity.defaultMultiValueType()).isEqualTo(ValueReference.of(LookupDefaultValue.Type.OBJECT));
+    }
+
+    @Test
+    @UsingDataSet(locations = "/org/graylog2/contentpacks/lut_tables.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void findExisting() {
+        final Entity entity = EntityV1.builder()
+                .id(ModelId.of("1"))
+                .type(ModelTypes.LOOKUP_TABLE)
+                .data(objectMapper.convertValue(LookupTableEntity.create(
+                        ValueReference.of("http-dsv-no-cache"),
+                        ValueReference.of("HTTP DSV without Cache"),
+                        ValueReference.of("HTTP DSV without Cache"),
+                        ValueReference.of("cache-id"),
+                        ValueReference.of("data-adapter-id"),
+                        ValueReference.of("Default single value"),
+                        ValueReference.of(LookupDefaultValue.Type.STRING),
+                        ValueReference.of("Default multi value"),
+                        ValueReference.of(LookupDefaultValue.Type.OBJECT)), JsonNode.class))
+                .build();
+        final NativeEntity<LookupTableDto> existingEntity = facade.findExisting(entity, Collections.emptyMap()).orElseThrow(AssertionError::new);
+
+        assertThat(existingEntity.descriptor().id()).isEqualTo(ModelId.of("5adf24dd4b900a0fdb4e530d"));
+        assertThat(existingEntity.descriptor().type()).isEqualTo(ModelTypes.LOOKUP_TABLE);
+        assertThat(existingEntity.entity().name()).isEqualTo("http-dsv-no-cache");
+        assertThat(existingEntity.entity().title()).isEqualTo("HTTP DSV without Cache");
+        assertThat(existingEntity.entity().description()).isEqualTo("HTTP DSV without Cache");
+        assertThat(existingEntity.entity().dataAdapterId()).isEqualTo("5adf24a04b900a0fdb4e52c8");
+        assertThat(existingEntity.entity().cacheId()).isEqualTo("5adf24b24b900a0fdb4e52dd");
+        assertThat(existingEntity.entity().defaultSingleValue()).isEqualTo("Default single value");
+        assertThat(existingEntity.entity().defaultSingleValueType()).isEqualTo(LookupDefaultValue.Type.STRING);
+        assertThat(existingEntity.entity().defaultMultiValue()).isEqualTo("Default multi value");
+        assertThat(existingEntity.entity().defaultMultiValueType()).isEqualTo(LookupDefaultValue.Type.OBJECT);
+    }
+
+    @Test
+    @UsingDataSet(locations = {"/org/graylog2/contentpacks/lut_caches.json", "/org/graylog2/contentpacks/lut_data_adapters.json", "/org/graylog2/contentpacks/lut_tables.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void resolveEntity() {
+        final Entity entity = EntityV1.builder()
+                .id(ModelId.of("5adf24dd4b900a0fdb4e530d"))
+                .type(ModelTypes.LOOKUP_TABLE)
+                .data(objectMapper.convertValue(LookupTableEntity.create(
+                        ValueReference.of("http-dsv-no-cache"),
+                        ValueReference.of("HTTP DSV without Cache"),
+                        ValueReference.of("HTTP DSV without Cache"),
+                        ValueReference.of("5adf24b24b900a0fdb4e52dd"),
+                        ValueReference.of("5adf24a04b900a0fdb4e52c8"),
+                        ValueReference.of("Default single value"),
+                        ValueReference.of(LookupDefaultValue.Type.STRING),
+                        ValueReference.of("Default multi value"),
+                        ValueReference.of(LookupDefaultValue.Type.OBJECT)), JsonNode.class))
+                .build();
+        final Entity cacheEntity = EntityV1.builder()
+                .id(ModelId.of("5adf24b24b900a0fdb4e52dd"))
+                .type(ModelTypes.LOOKUP_CACHE)
+                .data(objectMapper.convertValue(LookupCacheEntity.create(
+                        ValueReference.of("no-op-cache"),
+                        ValueReference.of("No-op cache"),
+                        ValueReference.of("No-op cache"),
+                        ReferenceMapUtils.toReferenceMap(ImmutableMap.of("type", "none"))
+                ), JsonNode.class))
+                .build();
+        final Entity dataAdapterEntity = EntityV1.builder()
+                .id(ModelId.of("5adf24a04b900a0fdb4e52c8"))
+                .type(ModelTypes.LOOKUP_ADAPTER)
+                .data(objectMapper.convertValue(LookupDataAdapterEntity.create(
+                        ValueReference.of("http-dsv"),
+                        ValueReference.of("HTTP DSV"),
+                        ValueReference.of("HTTP DSV"),
+                        ReferenceMapUtils.toReferenceMap(Collections.emptyMap())
+                ), JsonNode.class))
+                .build();
+        final Map<EntityDescriptor, Entity> entities = ImmutableMap.of(
+                cacheEntity.toEntityDescriptor(), cacheEntity,
+                dataAdapterEntity.toEntityDescriptor(), dataAdapterEntity);
+        final Graph<Entity> graph = facade.resolve(entity, Collections.emptyMap(), entities);
+
+        assertThat(graph.nodes())
+                .hasSize(3)
+                .containsOnly(entity, cacheEntity, dataAdapterEntity);
+    }
+
+    @Test
+    @UsingDataSet(locations = {"/org/graylog2/contentpacks/lut_caches.json", "/org/graylog2/contentpacks/lut_data_adapters.json", "/org/graylog2/contentpacks/lut_tables.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void resolveEntityDescriptor() {
+        final EntityDescriptor descriptor = EntityDescriptor.create(ModelId.of("5adf24dd4b900a0fdb4e530d"), ModelTypes.LOOKUP_TABLE);
+
+        final Graph<EntityDescriptor> graph = facade.resolve(descriptor);
+        assertThat(graph.nodes())
+                .hasSize(3)
+                .containsOnly(
+                        descriptor,
+                        EntityDescriptor.create(ModelId.of("5adf24a04b900a0fdb4e52c8"), ModelTypes.LOOKUP_ADAPTER),
+                        EntityDescriptor.create(ModelId.of("5adf24b24b900a0fdb4e52dd"), ModelTypes.LOOKUP_CACHE));
+    }
+
+    @Test
+    @UsingDataSet(locations = "/org/graylog2/contentpacks/lut_tables.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void findExistingWithNoExistingEntity() {
+        final Entity entity = EntityV1.builder()
+                .id(ModelId.of("1"))
+                .type(ModelTypes.LOOKUP_TABLE)
+                .data(objectMapper.convertValue(LookupTableEntity.create(
+                        ValueReference.of("some-name"),
+                        ValueReference.of("Title"),
+                        ValueReference.of("Description"),
+                        ValueReference.of("cache-id"),
+                        ValueReference.of("data-adapter-id"),
+                        ValueReference.of("Default single value"),
+                        ValueReference.of(LookupDefaultValue.Type.STRING),
+                        ValueReference.of("Default multi value"),
+                        ValueReference.of(LookupDefaultValue.Type.OBJECT)), JsonNode.class))
+                .build();
+        final Optional<NativeEntity<LookupTableDto>> existingEntity = facade.findExisting(entity, Collections.emptyMap());
+
+        assertThat(existingEntity).isEmpty();
+    }
+
+    @Test
+    @UsingDataSet(locations = "/org/graylog2/contentpacks/lut_tables.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void delete() {
+        final Optional<LookupTableDto> lookupTableDto = lookupTableService.get("5adf24dd4b900a0fdb4e530d");
+
+        assertThat(lookupTableService.findAll()).hasSize(1);
+        lookupTableDto.ifPresent(facade::delete);
+
+        assertThat(lookupTableService.findAll()).isEmpty();
+        assertThat(lookupTableService.get("5adf24dd4b900a0fdb4e530d")).isEmpty();
     }
 
     @Test
