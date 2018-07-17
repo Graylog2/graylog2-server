b28bfad061972fdcbe052ba7f9fb845e686c1962
Dennis Oelkers <dennis@graylog.com>
2018-07-17 13:30:33 +0200

    Refresh/clear Streams Store when loading search page/logging out. (Graylog2/graylog-plugin-enterprise#320)
    This PR does two things:
    
      * Refresh stream store when entering search page so newly created
    streams show up/deleted streams vanish from the list.
      * Clear the internal state of the store when a user logs out to
    prevent race conditions where users logging in can see streams from the
    last user.
    
    Fixes Graylog2/graylog-plugin-enterprise#302.

--- enterprise/src/web/enterprise/stores/StreamsStore.js
+++ enterprise/src/web/enterprise/stores/StreamsStore.js
@@ -3,21 +3,33 @@ import Reflux from 'reflux';
 import CombinedProvider from 'injection/CombinedProvider';
 
 const OriginalStreamsStore = CombinedProvider.get('Streams').StreamsStore;
+const { SessionActions } = CombinedProvider.get('Session');
+
+export const StreamsActions = Reflux.createActions(['refresh']);
 
 /* As the current typescript implementation of the `StreamsStore` is not holding a state, using it requires to query the
    streams list for every component using it over and over again. This simple Reflux store is supposed to query the
    `StreamsStore` once and hold the result for future subscribers.
    */
 export const StreamsStore = Reflux.createStore({
+  listenables: [StreamsActions],
   streams: [],
   init() {
+    this.refresh();
+    SessionActions.logout.completed.listen(() => this.clear());
+  },
+  getInitialState() {
+    return this._state();
+  },
+  refresh() {
     OriginalStreamsStore.listStreams().then((result) => {
       this.streams = result;
       this._trigger();
     });
   },
-  getInitialState() {
-    return this._state();
+  clear() {
+    this.streams = [];
+    this._trigger();
   },
   _state() {
     return {
