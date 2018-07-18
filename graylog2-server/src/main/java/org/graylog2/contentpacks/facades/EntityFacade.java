47a95f25e3c30988fcfbdb8a94304687a8532c30
Jochen Schalanda <jochen@graylog.com>
2018-07-18 18:09:40 +0200

    Add Javadoc to EntityFacade interface

--- graylog2-server/src/main/java/org/graylog2/contentpacks/facades/EntityFacade.java
+++ graylog2-server/src/main/java/org/graylog2/contentpacks/facades/EntityFacade.java
@@ -32,31 +32,99 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface EntityFacade<T> {
+    /**
+     * Create an exportable model of a native entity of type {@code T} including optional constraints.
+     *
+     * @param nativeEntity the native entity to export
+     * @return an exportable (serializable) model of the entity including optional constraints.
+     * @see EntityWithConstraints
+     */
     EntityWithConstraints exportNativeEntity(T nativeEntity);
 
+    /**
+     * Create an exportable model of a native entity referenced by an {@link EntityDescriptor}
+     * including optional constraints.
+     *
+     * @param entityDescriptor the descriptor of the native entity to export
+     * @return an exportable (serializable) model of the entity including optional constraints,
+     * or {@link Optional#empty()} if the entity couldn't be found.
+     */
+    Optional<EntityWithConstraints> exportEntity(EntityDescriptor entityDescriptor);
+
+    /**
+     * Create a native entity of type {@code T} from an entity model.
+     *
+     * @param entity         the entity model from which a native entity should be created
+     * @param parameters     user-provided parameters to resolve parameters in the entity model
+     * @param nativeEntities existing native entities to reference during the creation of the native entity
+     * @param username       the name of the user creating the entity
+     * @return the created native entity wrapped in {@link NativeEntity<T>}
+     * @see Entity
+     * @see NativeEntity
+     */
     NativeEntity<T> createNativeEntity(Entity entity,
                                        Map<String, ValueReference> parameters,
                                        Map<EntityDescriptor, Object> nativeEntities,
                                        String username);
 
+    /**
+     * Find an existing instance of the native entity described by the entity model.
+     *
+     * @param entity     the entity model from which a native entity should be created
+     * @param parameters user-provided parameters to resolve parameters in the entity model
+     * @return the existing native entity in the database wrapped in {@link NativeEntity<T>},
+     * or {@link Optional#empty()} if the entity couldn't be found.
+     * @see Entity
+     * @see NativeEntity
+     */
     default Optional<NativeEntity<T>> findExisting(Entity entity, Map<String, ValueReference> parameters) {
         return Optional.empty();
     }
 
+    /**
+     * Delete the given native entity.
+     *
+     * @param nativeEntity The native entity to delete
+     */
     void delete(T nativeEntity);
 
+    /**
+     * Create an excerpt (id, type, title) of a native entity for display purposes.
+     *
+     * @param nativeEntity The native entity to create an excerpt of
+     * @return The entity excerpt of the native entity
+     * @see EntityExcerpt
+     */
     EntityExcerpt createExcerpt(T nativeEntity);
 
+    /**
+     * Create entity excerpts of all native entities of type {@code T}.
+     *
+     * @return A collection of entity excerpts of all native entities of type {@code T}
+     * @see EntityExcerpt
+     */
     Set<EntityExcerpt> listEntityExcerpts();
 
-    Optional<EntityWithConstraints> exportEntity(EntityDescriptor entityDescriptor);
-
+    /**
+     * Create the dependency graph of a native entity described by the given entity descriptor.
+     *
+     * @param entityDescriptor the descriptor of the native entity to resolve dependencies for
+     * @return A directed graph of the native entity with entity descriptors as nodes.
+     * @see Graph
+     */
     default Graph<EntityDescriptor> resolveNativeEntity(EntityDescriptor entityDescriptor) {
         final MutableGraph<EntityDescriptor> mutableGraph = GraphBuilder.directed().build();
         mutableGraph.addNode(entityDescriptor);
         return ImmutableGraph.copyOf(mutableGraph);
     }
 
+    /**
+     * Create the dependency graph of an entity described by the given entity model during content pack installation.
+     *
+     * @param entity the entity model to resolve dependencies for
+     * @return A directed graph of the native entity with entity models as nodes.
+     * @see Graph
+     */
     default Graph<Entity> resolveForInstallation(Entity entity,
                                                  Map<String, ValueReference> parameters,
                                                  Map<EntityDescriptor, Entity> entities) {
