a50c1280b307c07dd8b7f6af5d0764537ffdc1bc
Jochen Schalanda <jochen@graylog.com>
2018-07-18 15:35:11 +0200

    Revert "Remove deprecated methods from Version class"
    This reverts commit 46fffe593ff46a0a8f07cf44eeed2dd677cd4e0c.

--- graylog2-server/src/test/java/org/graylog2/plugin/VersionTest.java
+++ graylog2-server/src/test/java/org/graylog2/plugin/VersionTest.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.assertTrue;
 
 public class VersionTest {
     @Test
-    public void testGetName() {
+    public void testGetName() throws Exception {
         assertEquals("0.20.0", Version.from(0, 20, 0).toString());
         assertEquals("1.0.0", Version.from(1, 0, 0).toString());
         assertEquals("1.2.3", Version.from(1, 2, 3).toString());
@@ -34,7 +34,7 @@ public class VersionTest {
     }
 
     @Test
-    public void testEquals() {
+    public void testEquals() throws Exception {
         assertTrue(Version.from(0, 20, 0).equals(Version.from(0, 20, 0)));
         assertTrue(Version.from(0, 20, 0, "preview.1").equals(Version.from(0, 20, 0, "preview.1")));
         assertTrue(Version.from(1, 2, 3).equals(Version.from(1, 2, 3)));
@@ -48,7 +48,41 @@ public class VersionTest {
     }
 
     @Test
-    public void testSameOrHigher() {
+    @SuppressWarnings("deprecation")
+    public void testGreaterMinor() throws Exception {
+        Version v = Version.from(0, 20, 0);
+
+        assertTrue(v.greaterMinor(Version.from(0, 19, 0)));
+        assertTrue(v.greaterMinor(Version.from(0, 18, 2)));
+        assertTrue(v.greaterMinor(Version.from(0, 19, 9001)));
+
+        assertFalse(v.greaterMinor(Version.from(0, 20, 0)));
+        assertFalse(v.greaterMinor(Version.from(1, 0, 0)));
+        assertFalse(v.greaterMinor(Version.from(1, 0, 9001)));
+        assertFalse(v.greaterMinor(Version.from(1, 20, 0)));
+        assertFalse(v.greaterMinor(Version.from(1, 1, 0)));
+        assertFalse(v.greaterMinor(Version.from(3, 2, 1)));
+
+        assertTrue(v.greaterMinor(Version.from(0, 19, 0, "rc.1")));
+
+        v = Version.from(1, 5, 0);
+
+        assertTrue(v.greaterMinor(Version.from(0, 19, 0)));
+        assertTrue(v.greaterMinor(Version.from(1, 0, 0)));
+        assertTrue(v.greaterMinor(Version.from(0, 19, 9001)));
+
+        assertFalse(v.greaterMinor(Version.from(1, 6, 0)));
+        assertFalse(v.greaterMinor(Version.from(3, 0, 0)));
+        assertFalse(v.greaterMinor(Version.from(1, 5, 9001)));
+        assertFalse(v.greaterMinor(Version.from(1, 20, 0)));
+        assertFalse(v.greaterMinor(Version.from(1, 20, 5)));
+        assertFalse(v.greaterMinor(Version.from(3, 2, 1)));
+
+        assertTrue(v.greaterMinor(Version.from(0, 19, 0, "rc.1")));
+    }
+
+    @Test
+    public void testSameOrHigher() throws Exception {
         Version v = Version.from(0, 20, 2);
 
         assertTrue(v.sameOrHigher(Version.from(0, 19, 0)));
