584beafa9e0056af9905364999bb787c56917ff7
Kay Roepke <kroepke@googlemail.com>
2018-08-17 18:16:58 +0200

    index mapping tweaks for es 6
    add request logging to elasticsearch client for easier debugging

--- graylog2-server/src/main/java/org/graylog2/indexer/IndexMapping6.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/IndexMapping6.java
@@ -10,15 +10,17 @@ import java.util.Map;
 public class IndexMapping6 extends IndexMapping {
 
     @Override
-    public Map<String, Object> messageTemplate(final String template, final String analyzer, final int order) {
-        final Map<String, Object> analyzerKeyword = ImmutableMap.of("analyzer_keyword", ImmutableMap.of(
-            "tokenizer", "keyword",
-            "filter", "lowercase"));
+    public Map<String, Object> messageTemplate(final String template, final String analyzer,
+        final int order) {
+        final Map<String, Object> analyzerKeyword = ImmutableMap
+            .of("analyzer_keyword", ImmutableMap.of(
+                "tokenizer", "keyword",
+                "filter", "lowercase"));
         final Map<String, Object> analysis = ImmutableMap.of("analyzer", analyzerKeyword);
         final Map<String, Object> settings = ImmutableMap.of("analysis", analysis);
         // mapping types are deprecated, 7.0 and later will only allow `_doc` here, so we start using that
         // see https://www.elastic.co/guide/en/elasticsearch/reference/6.3/removal-of-types.html#_schedule_for_removal_of_mapping_types
-        final Map<String, Object> mappings = ImmutableMap.of("_doc", messageMapping(analyzer));
+        final Map<String, Object> mappings = ImmutableMap.of(IndexMapping.TYPE_MESSAGE, messageMapping(analyzer));
 
         return ImmutableMap.of(
             "template", template,
@@ -29,29 +31,42 @@ public class IndexMapping6 extends IndexMapping {
     }
 
     protected List<Map<String, Map<String, Object>>> dynamicTemplate() {
-        final Map<String, Object> defaultInternal = of(
-            "match", "gl2_*",
-            "mapping", notAnalyzedString());
-        final Map<String, Map<String, Object>> templateInternal = of("internal_fields", defaultInternal);
+        final Map<String, Map<String, Object>> templateInternal =
+            of("internal_fields", of(
+                "match", "gl2_*",
+                "mapping", notAnalyzedString()));
 
-        final Map<String, Object> defaultAll = of(
-            // Match all
-            "match", "*",
-            // Analyze nothing by default
-            "mapping", of("index", true),
-            "type", "keyword");
-        final Map<String, Map<String, Object>> templateAll = of("store_generic", defaultAll);
+        final Map<String, Object> dynamicStrings = of(
+            "match_mapping_type", "string",
+            "mapping", notAnalyzedString()
+        );
+        final Map<String, Map<String, Object>> templateAll = of("store_generic", dynamicStrings);
 
         return ImmutableList.of(templateInternal, templateAll);
     }
 
     @Override
     protected Map<String, Map<String, Object>> fieldProperties(String analyzer) {
-        return null;
+        return of(
+            "message", analyzedString(analyzer, false),
+            "full_message", analyzedString(analyzer, false),
+            // http://joda-time.sourceforge.net/api-release/org/joda/time/format/DateTimeFormat.html
+            // http://www.elasticsearch.org/guide/reference/mapping/date-format.html
+            "timestamp", typeTimeWithMillis(),
+            // to support wildcard searches in source we need to lowercase the content (wildcard search lowercases search term)
+            "source", analyzedString("analyzer_keyword", true),
+            "streams", notAnalyzedString());
+    }
+
+    private Map<String, Object> analyzedString(String analyzer, boolean fieldData) {
+        return of(
+            "type", "text",
+            "analyzer", analyzer,
+            "fielddata", fieldData);
     }
 
     @Override
     protected Map<String, Object> notAnalyzedString() {
-        return null;
+        return of("type", "keyword");
     }
 }
