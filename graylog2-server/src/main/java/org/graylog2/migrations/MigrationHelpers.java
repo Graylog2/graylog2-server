bd434f6e98fb78742a6e0d0938670eff8b73f002
Dennis Oelkers <dennis@graylog.com>
2018-08-20 11:47:22 +0200

    Consolidate migration helpers to ensure existence of users and roles. (#5003)
    * Consolidate migration helpers to ensure existence of users and roles.
    
    In existing migrations, a pattern of checking for the existence of users
    and roles, creating them if nonexistent was repeatedly used by copying
    the identical code. This change is consolidating these helper functions
    to make them usable for all migrations.
    
    * Fixing typo and quotations.
    
    * Adding license header.

---
+++ graylog2-server/src/main/java/org/graylog2/migrations/MigrationHelpers.java
@@ -0,0 +1,126 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.migrations;
+
+import com.mongodb.DuplicateKeyException;
+import org.graylog2.database.NotFoundException;
+import org.graylog2.plugin.database.ValidationException;
+import org.graylog2.plugin.database.users.User;
+import org.graylog2.shared.users.Role;
+import org.graylog2.shared.users.UserService;
+import org.graylog2.users.RoleImpl;
+import org.graylog2.users.RoleService;
+import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+public class MigrationHelpers {
+    private static final Logger LOG = LoggerFactory.getLogger(MigrationHelpers.class);
+    private final RoleService roleService;
+    private final UserService userService;
+
+    @Inject
+    public MigrationHelpers(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @Nullable
+    public String ensureBuiltinRole(String roleName, String description, Set<String> expectedPermissions) {
+        Role previousRole = null;
+        try {
+            previousRole = roleService.load(roleName);
+            if (!previousRole.isReadOnly() || !expectedPermissions.equals(previousRole.getPermissions())) {
+                final String msg = "Invalid role '" + roleName + "', fixing it.";
+                LOG.error(msg);
+                throw new IllegalArgumentException(msg); // jump to fix code
+            }
+        } catch (NotFoundException | IllegalArgumentException | NoSuchElementException ignored) {
+            LOG.info("{} role is missing or invalid, re-adding it as a built-in role.", roleName);
+            final RoleImpl fixedRole = new RoleImpl();
+            // copy the mongodb id over, in order to update the role instead of readding it
+            if (previousRole != null) {
+                fixedRole._id = previousRole.getId();
+            }
+            fixedRole.setReadOnly(true);
+            fixedRole.setName(roleName);
+            fixedRole.setDescription(description);
+            fixedRole.setPermissions(expectedPermissions);
+
+            try {
+                final Role savedRole = roleService.save(fixedRole);
+                return savedRole.getId();
+            } catch (DuplicateKeyException | ValidationException e) {
+                LOG.error("Unable to save fixed '" + roleName + "' role, please restart Graylog to fix this.", e);
+            }
+        }
+
+        if (previousRole == null) {
+            LOG.error("Unable to access fixed '" + roleName + "' role, please restart Graylog to fix this.");
+            return null;
+        }
+
+        return previousRole.getId();
+    }
+
+    @Nullable
+    public String ensureUser(String userName, String password, String fullName, String email, Set<String> expectedRoles) {
+        User previousUser = null;
+        try {
+            previousUser = userService.load(userName);
+            if (previousUser == null || !expectedRoles.equals(previousUser.getRoleIds())) {
+                final String msg = "Invalid user '" + userName + "', fixing it.";
+                LOG.error(msg);
+                throw new IllegalArgumentException(msg);
+            }
+        } catch (IllegalArgumentException ignored) {
+            LOG.info("{} user is missing or invalid, re-adding it as a built-in user.", userName);
+            final User fixedUser;
+            if (previousUser != null) {
+                fixedUser = previousUser;
+                fixedUser.setRoleIds(expectedRoles);
+            } else {
+                fixedUser = userService.create();
+                fixedUser.setName(userName);
+                fixedUser.setFullName(fullName);
+                fixedUser.setPassword(password);
+                fixedUser.setEmail(email);
+                fixedUser.setPermissions(Collections.emptyList());
+                fixedUser.setRoleIds(expectedRoles);
+                fixedUser.setTimeZone(DateTimeZone.UTC);
+            }
+            try {
+                return userService.save(fixedUser);
+            } catch (ValidationException e) {
+                LOG.error("Unable to save fixed '" + userName + "' user, please restart Graylog to fix this.", e);
+            }
+        }
+
+        if (previousUser == null) {
+            LOG.error("Unable to access fixed '" + userName + "' user, please restart Graylog to fix this.");
+            return null;
+        }
+
+        return previousUser.getId();
+    }
+}
