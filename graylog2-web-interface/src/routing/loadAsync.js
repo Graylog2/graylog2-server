9875873052e2492379f4736a6cb48ef65f50cfe6
Dennis Oelkers <dennis@graylog.com>
2018-08-30 15:04:20 +0200

    Including simple error component.

--- graylog2-web-interface/src/routing/loadAsync.js
+++ graylog2-web-interface/src/routing/loadAsync.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import loadable from 'loadable-components';
 
-export default f => loadable(() => f().then(c => c.default));
+const ErrorComponent = ({ error }) => <div>Loading component failed: {error.message}</div>;
+ErrorComponent.propTypes = {
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }).isRequired,
+};
+
+export default f => loadable(() => f().then(c => c.default), { ErrorComponent });
