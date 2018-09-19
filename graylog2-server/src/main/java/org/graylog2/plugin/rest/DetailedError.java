edf84c7ea199aa1458090c5c1676120697a83536
Dan Torrey <dan@graylog.com>
2018-09-19 03:57:40 -0500

    Remove "AutoValue_" prefix from API error JSON (#5118)
    * Fix issue #4938: Remove "AutoValue_" prefix from API error JSON
    
    * Fix comment for consistency with other usages @JsonTypeName

--- graylog2-server/src/main/java/org/graylog2/plugin/rest/DetailedError.java
+++ graylog2-server/src/main/java/org/graylog2/plugin/rest/DetailedError.java
@@ -17,9 +17,11 @@
 package org.graylog2.plugin.rest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import java.util.Collection;
 
+@JsonTypeName("DetailedError") // Explicitly indicates the class type to avoid AutoValue_ at the beginning
 public interface DetailedError extends GenericError {
     @JsonProperty
     Collection<String> details();
