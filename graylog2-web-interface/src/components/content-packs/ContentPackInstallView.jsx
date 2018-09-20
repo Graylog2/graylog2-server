c99ed457bf2f15b96ea73e86619cd3569edbb312
Konrad Merz <konrad@graylog.com>
2018-09-20 15:28:37 +0200

    View entites in installation details

--- graylog2-web-interface/src/components/content-packs/ContentPackInstallView.jsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackInstallView.jsx
@@ -2,28 +2,42 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import { Row, Col } from 'react-bootstrap';
-import Timestamp from 'components/common/Timestamp';
+import { DataTable, Timestamp } from 'components/common';
 import DateTime from 'logic/datetimes/DateTime';
 
 import 'components/content-packs/ContentPackDetails.css';
 
 const ContentPackInstallView = (props) => {
+  const rowFormatter = entity => (<tr><td>{entity.title}</td><td>{entity.type.name}</td></tr>);
   const { comment } = props.install;
   const createdAt = props.install.created_at;
   const createdBy = props.install.created_by;
+  const headers = ['Title', 'Type'];
   return (<div>
     <Row>
-      <Col smOffset={1}>
-        <div>
-          <dl className="deflist">
-            <dt>Comment:</dt>
-            <dd>{comment}</dd>
-            <dt>Installed by:</dt>
-            <dd>{createdBy}&nbsp;</dd>
-            <dt>Installed at:</dt>
-            <dd><Timestamp dateTime={createdAt} format={DateTime.Formats.COMPLETE} tz="browser" /></dd>
-          </dl>
-        </div>
+      <Col smOffset={1} sm={10}>
+        <h3>General information</h3>
+        <dl className="deflist">
+          <dt>Comment:</dt>
+          <dd>{comment}</dd>
+          <dt>Installed by:</dt>
+          <dd>{createdBy}&nbsp;</dd>
+          <dt>Installed at:</dt>
+          <dd><Timestamp dateTime={createdAt} format={DateTime.Formats.COMPLETE} tz="browser" /></dd>
+        </dl>
+      </Col>
+    </Row>
+    <Row>
+      <Col smOffset={1} sm={10}>
+        <h3>Installed Entities</h3>
+        <DataTable
+          id="installed-entities"
+          headers={headers}
+          sortByKey="title"
+          dataRowFormatter={rowFormatter}
+          rows={props.install.entities}
+          filterKeys={[]}
+        />
       </Col>
     </Row>
   </div>);
