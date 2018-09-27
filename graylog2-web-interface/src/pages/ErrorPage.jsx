e5e60b7e7b5cd7f6d91f7af092c246705452c9ff
Dennis Oelkers <dennis@graylog.com>
2018-09-27 16:50:07 +0200

    Wrapping error page content in correct class to fix footer.

--- graylog2-web-interface/src/pages/ErrorPage.jsx
+++ graylog2-web-interface/src/pages/ErrorPage.jsx
@@ -29,34 +29,36 @@ class ErrorPage extends React.Component {
     const { error, info } = this.props;
 
     return (
-      <DocumentTitle title="Something went wrong.">
-        <Row className="jumbotron-container">
-          <Col mdOffset={2} md={8}>
-            <Jumbotron>
-              <h1>Something went wrong.</h1>
-              <p>It seems like the page you navigated to contained an error.</p>
-              <p>You can use the navigation to reach other parts of the product, refresh the page or submit an error report.</p>
-              <p>The details of the error are below:</p>
-              <dl className={errorPageStyles.errorMessage}>
-                <dt>Error:</dt>
-                <dd>
-                  <pre>{error.message}</pre>
-                </dd>
+      <div className="container-fluid">
+        <DocumentTitle title="Something went wrong.">
+          <Row className="jumbotron-container">
+            <Col mdOffset={2} md={8}>
+              <Jumbotron>
+                <h1>Something went wrong.</h1>
+                <p>It seems like the page you navigated to contained an error.</p>
+                <p>You can use the navigation to reach other parts of the product, refresh the page or submit an error report.</p>
+                <p>The details of the error are below:</p>
+                <dl className={errorPageStyles.errorMessage}>
+                  <dt>Error:</dt>
+                  <dd>
+                    <pre>{error.message}</pre>
+                  </dd>
 
-                <dt>Stack:</dt>
-                <dd>
-                  <pre>{error.stack}</pre>
-                </dd>
+                  <dt>Stack:</dt>
+                  <dd>
+                    <pre>{error.stack}</pre>
+                  </dd>
 
-                <dt>Component Tree:</dt>
-                <dd>
-                  <pre>{info.componentStack}</pre>
-                </dd>
-              </dl>
-            </Jumbotron>
-          </Col>
-        </Row>
-      </DocumentTitle>
+                  <dt>Component Tree:</dt>
+                  <dd>
+                    <pre>{info.componentStack}</pre>
+                  </dd>
+                </dl>
+              </Jumbotron>
+            </Col>
+          </Row>
+        </DocumentTitle>
+      </div>
     );
   }
 }
