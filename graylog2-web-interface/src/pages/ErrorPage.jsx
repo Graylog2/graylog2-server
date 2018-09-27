c4ce3d3de30aa96a98b740dcdd816a94576c274b
Dennis Oelkers <dennis@graylog.com>
2018-09-27 17:06:58 +0200

    Include measures to reach out for help in error page.

--- graylog2-web-interface/src/pages/ErrorPage.jsx
+++ graylog2-web-interface/src/pages/ErrorPage.jsx
@@ -6,6 +6,7 @@ import { DocumentTitle } from 'components/common';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import style from '!style/useable!css!./NotFoundPage.css';
 import errorPageStyles from './ErrorPage.css';
+import SupportSources from '../components/support/SupportSources';
 
 class ErrorPage extends React.Component {
   static propTypes = {
@@ -37,23 +38,27 @@ class ErrorPage extends React.Component {
                 <h1>Something went wrong.</h1>
                 <p>It seems like the page you navigated to contained an error.</p>
                 <p>You can use the navigation to reach other parts of the product, refresh the page or submit an error report.</p>
-                <p>The details of the error are below:</p>
-                <dl className={errorPageStyles.errorMessage}>
-                  <dt>Error:</dt>
-                  <dd>
-                    <pre>{error.message}</pre>
-                  </dd>
+                <div className={errorPageStyles.errorMessage}>
+                  <div className="content" style={{ padding: '2em' }}>
+                    <SupportSources />
+                  </div>
+                  <dl>
+                    <dt>Error:</dt>
+                    <dd className={errorPageStyles.greyBackground}>
+                      <pre className="content">{error.message}</pre>
+                    </dd>
 
-                  <dt>Stack:</dt>
-                  <dd>
-                    <pre>{error.stack}</pre>
-                  </dd>
+                    <dt>Stack:</dt>
+                    <dd className={errorPageStyles.greyBackground}>
+                      <pre className="content">{error.stack}</pre>
+                    </dd>
 
-                  <dt>Component Tree:</dt>
-                  <dd>
-                    <pre>{info.componentStack}</pre>
-                  </dd>
-                </dl>
+                    <dt>Component Tree:</dt>
+                    <dd className={errorPageStyles.greyBackground}>
+                      <pre className="content">{info.componentStack}</pre>
+                    </dd>
+                  </dl>
+                </div>
               </Jumbotron>
             </Col>
           </Row>
