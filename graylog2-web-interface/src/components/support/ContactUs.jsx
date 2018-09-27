17a1929f12999cc33e72a51b5c66be74c471ed04
Dennis Oelkers <dennis@graylog.com>
2018-09-27 17:34:56 +0200

    Turning ContactUs into functional component.

--- graylog2-web-interface/src/components/support/ContactUs.jsx
+++ graylog2-web-interface/src/components/support/ContactUs.jsx
@@ -3,16 +3,12 @@ import { Col, Row } from 'react-bootstrap';
 
 import SupportSources from './SupportSources';
 
-class ContactUs extends React.Component {
-  render() {
-    return (
-      <Row className="content">
-        <Col md={12}>
-          <SupportSources />
-        </Col>
-      </Row>
-    );
-  }
-}
+const ContactUs = () => (
+  <Row className="content">
+    <Col md={12}>
+      <SupportSources />
+    </Col>
+  </Row>
+);
 
 export default ContactUs;
