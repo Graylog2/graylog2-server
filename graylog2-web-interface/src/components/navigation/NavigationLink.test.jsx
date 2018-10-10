8780e37548d9c8b9dca8f9a70b840127cedada49
Dennis Oelkers <dennis@graylog.com>
2018-10-10 15:38:04 +0200

    Fixing NavigationLink tests.

--- graylog2-web-interface/src/components/navigation/NavigationLink.test.jsx
+++ graylog2-web-interface/src/components/navigation/NavigationLink.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import NavigationLink from './NavigationLink';
 
 jest.mock('util/URLUtils', () => ({ appPrefixed: path => `/prefix${path}` }));
@@ -7,12 +7,11 @@ jest.mock('util/URLUtils', () => ({ appPrefixed: path => `/prefix${path}` }));
 describe('NavigationLink', () => {
   it('renders with simple props', () => {
     const wrapper = mount(<NavigationLink description="Hello there!" path="/hello" />);
-    expect(wrapper.find('LinkContainer')).toHaveProp('path', '/hello');
     expect(wrapper.find('LinkContainer')).toHaveProp('to', '/prefix/hello');
     expect(wrapper.find('MenuItem')).toHaveText('Hello there!');
   });
   it('passes props to LinkContainer', () => {
-    const wrapper = mount(<NavigationLink description="Hello there!" path="/hello" someProp={42} />);
+    const wrapper = shallow(<NavigationLink description="Hello there!" path="/hello" someProp={42} />);
     expect(wrapper.find('LinkContainer')).toHaveProp('someProp', 42);
   });
 });
