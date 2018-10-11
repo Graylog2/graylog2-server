dc8a6d039dffd9ea3653c03188400aafc2cffaaf
Marco Pfatschbacher <marco@graylog.com>
2018-10-11 15:24:00 +0200

    Rename "Beats Legacy" input to "Beats (deprecated)"
    The former name suggested that it could be used to parse
    an old version of the Beats protocol.
    The new name is hopefully less confusing.

--- graylog2-server/src/main/java/org/graylog/plugins/beats/BeatsCodec.java
+++ graylog2-server/src/main/java/org/graylog/plugins/beats/BeatsCodec.java
@@ -40,7 +40,7 @@ import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
-@Codec(name = "beats-legacy", displayName = "Beats Legacy")
+@Codec(name = "beats-deprecated", displayName = "Beats (deprecated)")
 public class BeatsCodec extends AbstractCodec {
     private static final Logger LOG = LoggerFactory.getLogger(BeatsCodec.class);
     private static final String MAP_KEY_SEPARATOR = "_";
