77daea0a2004570acae078edb4fceaea1b995696
Bernd Ahlers <bernd@users.noreply.github.com>
2018-10-30 14:50:04 +0100

    Update netty to 4.1.31 (#5250)
    * Update netty to 4.1.31
    
    * Make ConnectionCounterTest more resilient
    
    This adds some latches to the server pipeline which allow us to wait
    until the server has actually processed our message before we check the
    connection counters.
    
    * Fix forbidden-apis error

--- graylog2-server/src/test/java/org/graylog2/plugin/inputs/util/ConnectionCounterTest.java
+++ graylog2-server/src/test/java/org/graylog2/plugin/inputs/util/ConnectionCounterTest.java
@@ -22,6 +22,9 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -32,6 +35,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -39,18 +44,46 @@ import java.util.concurrent.atomic.AtomicLong;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConnectionCounterTest {
+    private static final int LATCH_TIMEOUT = 30;
+
     private ConnectionCounter connectionCounter;
     private NioEventLoopGroup eventLoopGroup;
     private Channel serverChannel;
+    private CountDownLatch readCompleteLatch;
+    private CountDownLatch disconnectedLatch;
 
     @Before
     public void setUp() throws Exception {
+        readCompleteLatch = new CountDownLatch(1);
+        disconnectedLatch = new CountDownLatch(1);
         connectionCounter = new ConnectionCounter(new AtomicInteger(), new AtomicLong());
         eventLoopGroup = new NioEventLoopGroup(1);
         final ServerBootstrap serverBootstrap = new ServerBootstrap()
                 .group(eventLoopGroup)
                 .channel(NioServerSocketChannel.class)
-                .childHandler(connectionCounter);
+                .childHandler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addFirst("connection-counter", connectionCounter);
+
+                        // This adds some latches to allow us to wait until the server processed our connection.
+                        // Without this, this test was failing from time to time because we checked the connection
+                        // counter values before the connection was actually processed.
+                        ch.pipeline().addLast("latches", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+                                disconnectedLatch.countDown();
+                                super.channelUnregistered(ctx);
+                            }
+
+                            @Override
+                            public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+                                readCompleteLatch.countDown();
+                                super.channelReadComplete(ctx);
+                            }
+                        });
+                    }
+                });
 
         serverBootstrap.bind(InetAddress.getLocalHost(), 0)
                 .addListener((ChannelFutureListener) future -> serverChannel = future.channel())
@@ -81,7 +114,11 @@ public class ConnectionCounterTest {
             final Channel clientChannel = connectFuture.channel();
 
             assertThat(clientChannel.isWritable()).isTrue();
-            clientChannel.writeAndFlush(Unpooled.EMPTY_BUFFER).syncUninterruptibly();
+            // We have to send a message here to make sure that channelReadComplete gets called in the handler
+            clientChannel.writeAndFlush(Unpooled.wrappedBuffer("canary".getBytes(StandardCharsets.UTF_8))).syncUninterruptibly();
+
+            // Wait until the server read the message
+            readCompleteLatch.await(LATCH_TIMEOUT, TimeUnit.SECONDS);
 
             // One client active
             assertThat(connectionCounter.getTotalConnections()).isEqualTo(1L);
@@ -92,6 +129,9 @@ public class ConnectionCounterTest {
             clientEventLoopGroup.shutdownGracefully();
             clientEventLoopGroup.awaitTermination(1, TimeUnit.SECONDS);
         }
+
+        // Wait until the client has disconnected
+        disconnectedLatch.await(LATCH_TIMEOUT, TimeUnit.SECONDS);
 
         // No client, but 1 connection so far
         assertThat(connectionCounter.getTotalConnections()).isEqualTo(1L);
