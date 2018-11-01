72b5d1ed40c0f93fafaab9b331023c41c50cc6a0
Bernd Ahlers <bernd@graylog.com>
2018-11-01 08:21:31 +0100

    Some fixes for the grok pattern content pack migration
    - Do not run migration if the content pack migration is already done
    - Fix indentation
    - Install content pack after inserting it into the database
    - Rename JSON property for the content pack ID in the migration marker
    - Change wording in the content pack name, summary and description
    - Fix server-version to not include "<4.0.0"

--- graylog2-server/src/main/java/org/graylog2/migrations/V20180924111644_AddDefaultGrokPatterns.java
+++ graylog2-server/src/main/java/org/graylog2/migrations/V20180924111644_AddDefaultGrokPatterns.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auto.value.AutoValue;
 import org.graylog.autovalue.WithBeanGetter;
 import org.graylog2.contentpacks.ContentPackPersistenceService;
+import org.graylog2.contentpacks.ContentPackService;
 import org.graylog2.contentpacks.model.ContentPack;
 import org.graylog2.plugin.cluster.ClusterConfigService;
 import org.slf4j.Logger;
@@ -32,19 +33,22 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.net.URL;
 import java.time.ZonedDateTime;
-import java.util.List;
+import java.util.Collections;
 
 public class V20180924111644_AddDefaultGrokPatterns extends Migration {
     private static final Logger LOG = LoggerFactory.getLogger(V20180924111644_AddDefaultGrokPatterns.class);
 
+    private final ContentPackService contentPackService;
     private final ObjectMapper objectMapper;
     private final ClusterConfigService configService;
     private final ContentPackPersistenceService contentPackPersistenceService;
 
     @Inject
     public V20180924111644_AddDefaultGrokPatterns(final ContentPackPersistenceService contentPackPersistenceService,
+                                                  final ContentPackService contentPackService,
                                                   final ObjectMapper objectMapper,
                                                   final ClusterConfigService clusterConfigService) {
+        this.contentPackService = contentPackService;
         this.objectMapper = objectMapper;
         this.contentPackPersistenceService = contentPackPersistenceService;
         this.configService = clusterConfigService;
@@ -59,31 +63,34 @@ public class V20180924111644_AddDefaultGrokPatterns extends Migration {
     public void upgrade() {
         if (configService.get(MigrationCompleted.class) != null) {
             LOG.debug("Migration already completed.");
+            return;
         }
 
         try {
-           final URL contentPackURL = V20180924111644_AddDefaultGrokPatterns.class
-                   .getResource("V20180924111644_AddDefaultGrokPatterns_Default_Grok_Patterns.json");
-           final ContentPack contentPack = this.objectMapper.readValue(contentPackURL, ContentPack.class);
-           ContentPack pack = this.contentPackPersistenceService.insert(contentPack)
-                   .orElseThrow(() -> new Error("Content pack " + contentPack.id() + " with this revision " + contentPack.revision() + " already found!"));
+            final URL contentPackURL = V20180924111644_AddDefaultGrokPatterns.class
+                    .getResource("V20180924111644_AddDefaultGrokPatterns_Default_Grok_Patterns.json");
+            final ContentPack contentPack = this.objectMapper.readValue(contentPackURL, ContentPack.class);
+            final ContentPack pack = this.contentPackPersistenceService.insert(contentPack)
+                    .orElseThrow(() -> new Error("Content pack " + contentPack.id() + " with this revision " + contentPack.revision() + " already found!"));
+
+            contentPackService.installContentPack(pack, Collections.emptyMap(), "Add default grok patterns", "admin");
+
             configService.write(MigrationCompleted.create(pack.id().toString()));
         } catch (IOException e) {
             LOG.error("Unable to import content pack for default grok patterns: {}", e);
         }
     }
 
-
     @JsonAutoDetect
     @AutoValue
     @WithBeanGetter
     public static abstract class MigrationCompleted {
-        @JsonProperty("content_bundle_id")
-        public abstract String contentBundleId();
+        @JsonProperty("content_pack_id")
+        public abstract String contentPackId();
 
         @JsonCreator
-        public static MigrationCompleted create(@JsonProperty("content_bundle_id") final String contentBundleId) {
-            return new AutoValue_V20180924111644_AddDefaultGrokPatterns_MigrationCompleted(contentBundleId);
+        public static MigrationCompleted create(@JsonProperty("content_pack_id") final String contentPackId) {
+            return new AutoValue_V20180924111644_AddDefaultGrokPatterns_MigrationCompleted(contentPackId);
         }
     }
 }
