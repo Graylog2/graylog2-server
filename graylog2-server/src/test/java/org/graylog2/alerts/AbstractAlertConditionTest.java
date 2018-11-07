0533cfd57b2efe2425563074d7e47ffa8468d25c
Bernd Ahlers <bernd@users.noreply.github.com>
2018-11-07 12:17:02 +0100

    Make search query in alert conditions configurable in the UI (#5212)
    * Make search query in alert conditions configurable in the UI
    
    This exposes a new query config field in the alert condition
    configuration. The value defaults to "*".
    
    The query is used to build a filter string for the alert condition
    search. We add the query string to the query filter for performance
    reasons.
    
    Fixes #3966
    
    * Trim streamId and query strings
    
    * Use query config in field content value condition as well
    
    * Add HTTP API endpoints to test alert conditions
    
    There are two test endpoints, one to test existing alert conditions and
    one to test new alert conditions which have not been stored in the
    database yet.
    
    * Add missing NoAuditEvent annotations to test resources
    
    * Fix failing test because of missing stream id
    
    * Use AND operator and parentheses to combine streams and custom query
    
    Otherwise the generated query will be wrong.

--- graylog2-server/src/test/java/org/graylog2/alerts/AbstractAlertConditionTest.java
+++ graylog2-server/src/test/java/org/graylog2/alerts/AbstractAlertConditionTest.java
@@ -23,6 +23,8 @@ import org.junit.Test;
 
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 
 public class AbstractAlertConditionTest extends AlertConditionTest {
@@ -36,6 +38,33 @@ public class AbstractAlertConditionTest extends AlertConditionTest {
         assertEquals(3, alertConditionWithStringDouble.getGrace());
         final AlertCondition alertConditionWithStringInteger = getDummyAlertCondition(ImmutableMap.of("grace", "3"));
         assertEquals(3, alertConditionWithStringInteger.getGrace());
+    }
+
+    @Test
+    public void testQueryFilterBuilder() {
+        final AbstractAlertCondition condition = (AbstractAlertCondition) getDummyAlertCondition(ImmutableMap.of());
+
+        assertThatThrownBy(() -> condition.buildQueryFilter(null, null))
+                .hasMessageContaining("streamId")
+                .hasMessageContaining("be null");
+        assertThatThrownBy(() -> condition.buildQueryFilter("", null))
+                .hasMessageContaining("streamId")
+                .hasMessageContaining("be empty");
+
+        assertThat(condition.buildQueryFilter("  abc123 ", null))
+                .isEqualTo("streams:abc123");
+        assertThat(condition.buildQueryFilter("abc123", ""))
+                .isEqualTo("streams:abc123");
+        assertThat(condition.buildQueryFilter("abc123", "*"))
+                .isEqualTo("streams:abc123");
+        assertThat(condition.buildQueryFilter("abc123", " *  "))
+                .isEqualTo("streams:abc123");
+        assertThat(condition.buildQueryFilter("abc123", " hello:world foo:\"bar baz\"   "))
+                .isEqualTo("streams:abc123 AND (hello:world foo:\"bar baz\")");
+        assertThat(condition.buildQueryFilter("abc123", "hello:world AND foo:\"bar baz\""))
+                .isEqualTo("streams:abc123 AND (hello:world AND foo:\"bar baz\")");
+        assertThat(condition.buildQueryFilter("abc123", "hello:world AND (foo:\"bar baz\" OR foo:yolo)"))
+                .isEqualTo("streams:abc123 AND (hello:world AND (foo:\"bar baz\" OR foo:yolo))");
     }
 
     private AlertCondition getDummyAlertCondition(Map<String, Object> parameters) {
