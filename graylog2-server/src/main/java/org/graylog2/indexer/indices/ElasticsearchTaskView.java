f80156e3155b1b4d2569b6599f298e7b2783f55d
Bernd Ahlers <bernd@graylog.com>
2018-11-20 22:26:55 +0100

    Fix indentation

--- graylog2-server/src/main/java/org/graylog2/indexer/indices/ElasticsearchTaskView.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/indices/ElasticsearchTaskView.java
@@ -28,8 +28,6 @@ import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestResult;
-import java.util.Optional;
-import javax.annotation.Nullable;
 import org.graylog2.indexer.cluster.jest.JestUtils;
 import org.graylog2.indexer.cluster.jest.tasks.CancelTask.Builder;
 import org.graylog2.indexer.cluster.jest.tasks.GetTasks;
@@ -38,6 +36,9 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
 
 /**
  * Represents an async task we have started, or are tracking, in Elasticsearch. Use this class to
@@ -57,175 +58,176 @@ import org.slf4j.LoggerFactory;
  */
 public class ElasticsearchTaskView {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ElasticsearchTaskView.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ElasticsearchTaskView.class);
 
-  private final JestClient jestClient;
-  private final ObjectMapper objectMapper;
-  private final JsonNode rawJson;
-  private final StaticData staticData;
-  private final boolean completed;
-  private final JsonNode response;
+    private final JestClient jestClient;
+    private final ObjectMapper objectMapper;
+    private final JsonNode rawJson;
+    private final StaticData staticData;
+    private final boolean completed;
+    private final JsonNode response;
 
-  @AssistedInject
-  ElasticsearchTaskView(JestClient jestClient, ObjectMapper objectMapper,
-      @Assisted JsonNode rawJson) {
-    this.jestClient = jestClient;
-    this.objectMapper = objectMapper;
-    this.rawJson = rawJson;
+    @AssistedInject
+    ElasticsearchTaskView(JestClient jestClient, ObjectMapper objectMapper,
+                          @Assisted JsonNode rawJson) {
+        this.jestClient = jestClient;
+        this.objectMapper = objectMapper;
+        this.rawJson = rawJson;
 
-    StaticData staticData1 = null;
-    boolean completed1 = false;
-    JsonNode response = MissingNode.getInstance();
-    try {
-      staticData1 = objectMapper.treeToValue(rawJson.path("task"), StaticData.class);
-      completed1 = rawJson.path("completed").asBoolean();
-      response = rawJson.path("response");
-    } catch (JsonProcessingException e) {
-      LOG.error("Unable to parse task data", ExceptionUtils.getRootCause(e));
+        StaticData staticData1 = null;
+        boolean completed1 = false;
+        JsonNode response = MissingNode.getInstance();
+        try {
+            staticData1 = objectMapper.treeToValue(rawJson.path("task"), StaticData.class);
+            completed1 = rawJson.path("completed").asBoolean();
+            response = rawJson.path("response");
+        } catch (JsonProcessingException e) {
+            LOG.error("Unable to parse task data", ExceptionUtils.getRootCause(e));
+        }
+        this.staticData = staticData1;
+        this.completed = completed1;
+        this.response = response;
     }
-    this.staticData = staticData1;
-    this.completed = completed1;
-    this.response = response;
-  }
 
-  public long id() {
-    return staticData.id();
-  }
-
-  /**
-   * Elasticsearch uses this id for uniquely identifying tasks across the cluster.
-   * @return the clusterwide unique task id
-   */
-  public String taskId() {
-    return nodeId() + ":" + id();
-  }
-
-  public String action() {
-    return staticData.action();
-  }
-
-  public boolean cancellable() {
-    return staticData.cancellable();
-  }
-
-  public String description() {
-    return staticData.description();
-  }
-
-  public String nodeId() {
-    return staticData.nodeId();
-  }
-
-  public Optional<String> parentTaskId() {
-    return staticData.parentTaskId();
-  }
-
-  public String type() {
-    return staticData.type();
-  }
-
-  public DateTime startTime() {
-    return staticData.startTime();
-  }
-
-  public boolean isCompleted () {
-    return completed;
-  }
-
-  public JsonNode responseAsJson() {
-    return response;
-  }
-
-  public <T> Optional<T> responseAs(Class<T> responseKlass) {
-    try {
-      return Optional.of(objectMapper.treeToValue(response, responseKlass));
-    } catch (JsonProcessingException e) {
-      LOG.error("Expected response type {}, cannot decode task response.", responseKlass, ExceptionUtils.getRootCause(e));
-      return Optional.empty();
+    public long id() {
+        return staticData.id();
     }
-  }
-
-  public boolean cancel() {
-    if (!cancellable()) {
-      LOG.error("The task " + taskId() + "/" + action() + " is not cancellable.");
-      return false;
-    }
-    final JestResult cancelResponse = JestUtils.execute(jestClient,
-        new Builder(taskId()).build(),
-        () -> "Unable to cancel task " + id());
-    return cancelResponse.isSucceeded();
-  }
-
-  /**
-   * Blocks until this task has been completed.
-   *
-   * @return true if the task completed, false otherwise (which likely throw an exception)
-   */
-  public boolean waitForCompletion() {
-    if (isCompleted()) {
-      return true;
-    }
-    final JestResult completion = JestUtils
-        .execute(jestClient, new GetTasks.Builder(taskId()).waitForCompletion(true).build(),
-            () -> "Unable to wait for task " + taskId() + " to complete.");
-    return completion.isSucceeded();
-  }
-
-  public interface Factory {
 
     /**
-     * Create a new task from the given data
+     * Elasticsearch uses this id for uniquely identifying tasks across the cluster.
      *
-     * @param responseJsonNode the json returned from elasticsearch, contains static data, response
-     * and completion information
-     * @return the current view of the task
+     * @return the clusterwide unique task id
      */
-    ElasticsearchTaskView create(JsonNode responseJsonNode);
-  }
-
-  // we are not interested in all properties just yet
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  @AutoValue
-  public abstract static class StaticData {
-
-    @JsonCreator
-    public static StaticData create(
-        @JsonProperty("id") long id,
-        @JsonProperty("action") String action,
-        @JsonProperty("cancellable") boolean cancellable,
-        @JsonProperty("description") String description,
-        @JsonProperty("node") String nodeId,
-        @JsonProperty("parent_task_id") @Nullable String parentTaskId,
-        @JsonProperty("type") String type,
-        @JsonProperty("start_time_in_millis") long startTime) {
-      return new AutoValue_ElasticsearchTaskView_StaticData(id, action, cancellable, description,
-          nodeId, Optional.ofNullable(parentTaskId), type,
-          new DateTime(startTime, DateTimeZone.UTC));
+    public String taskId() {
+        return nodeId() + ":" + id();
     }
 
-    abstract long id();
+    public String action() {
+        return staticData.action();
+    }
 
-    abstract String action();
+    public boolean cancellable() {
+        return staticData.cancellable();
+    }
 
-    abstract boolean cancellable();
+    public String description() {
+        return staticData.description();
+    }
 
-    abstract String description();
+    public String nodeId() {
+        return staticData.nodeId();
+    }
 
-    abstract String nodeId();
+    public Optional<String> parentTaskId() {
+        return staticData.parentTaskId();
+    }
 
-    abstract Optional<String> parentTaskId();
+    public String type() {
+        return staticData.type();
+    }
 
-    abstract String type();
+    public DateTime startTime() {
+        return staticData.startTime();
+    }
 
-    abstract DateTime startTime();
-  }
+    public boolean isCompleted() {
+        return completed;
+    }
 
-  @AutoValue
-  public abstract static class Status {
+    public JsonNode responseAsJson() {
+        return response;
+    }
 
-    public abstract boolean completed();
+    public <T> Optional<T> responseAs(Class<T> responseKlass) {
+        try {
+            return Optional.of(objectMapper.treeToValue(response, responseKlass));
+        } catch (JsonProcessingException e) {
+            LOG.error("Expected response type {}, cannot decode task response.", responseKlass, ExceptionUtils.getRootCause(e));
+            return Optional.empty();
+        }
+    }
 
-    public abstract long elapsedNanos();
+    public boolean cancel() {
+        if (!cancellable()) {
+            LOG.error("The task " + taskId() + "/" + action() + " is not cancellable.");
+            return false;
+        }
+        final JestResult cancelResponse = JestUtils.execute(jestClient,
+                new Builder(taskId()).build(),
+                () -> "Unable to cancel task " + id());
+        return cancelResponse.isSucceeded();
+    }
 
-  }
+    /**
+     * Blocks until this task has been completed.
+     *
+     * @return true if the task completed, false otherwise (which likely throw an exception)
+     */
+    public boolean waitForCompletion() {
+        if (isCompleted()) {
+            return true;
+        }
+        final JestResult completion = JestUtils
+                .execute(jestClient, new GetTasks.Builder(taskId()).waitForCompletion(true).build(),
+                        () -> "Unable to wait for task " + taskId() + " to complete.");
+        return completion.isSucceeded();
+    }
+
+    public interface Factory {
+
+        /**
+         * Create a new task from the given data
+         *
+         * @param responseJsonNode the json returned from elasticsearch, contains static data, response
+         *                         and completion information
+         * @return the current view of the task
+         */
+        ElasticsearchTaskView create(JsonNode responseJsonNode);
+    }
+
+    // we are not interested in all properties just yet
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @AutoValue
+    public abstract static class StaticData {
+
+        @JsonCreator
+        public static StaticData create(
+                @JsonProperty("id") long id,
+                @JsonProperty("action") String action,
+                @JsonProperty("cancellable") boolean cancellable,
+                @JsonProperty("description") String description,
+                @JsonProperty("node") String nodeId,
+                @JsonProperty("parent_task_id") @Nullable String parentTaskId,
+                @JsonProperty("type") String type,
+                @JsonProperty("start_time_in_millis") long startTime) {
+            return new AutoValue_ElasticsearchTaskView_StaticData(id, action, cancellable, description,
+                    nodeId, Optional.ofNullable(parentTaskId), type,
+                    new DateTime(startTime, DateTimeZone.UTC));
+        }
+
+        abstract long id();
+
+        abstract String action();
+
+        abstract boolean cancellable();
+
+        abstract String description();
+
+        abstract String nodeId();
+
+        abstract Optional<String> parentTaskId();
+
+        abstract String type();
+
+        abstract DateTime startTime();
+    }
+
+    @AutoValue
+    public abstract static class Status {
+
+        public abstract boolean completed();
+
+        public abstract long elapsedNanos();
+
+    }
 }
