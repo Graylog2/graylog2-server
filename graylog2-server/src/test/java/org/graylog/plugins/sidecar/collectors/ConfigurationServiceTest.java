e5c304899055c7149f8c309e47a430c485ce7f16
Marco Pfatschbacher <marco@graylog.com>
2018-11-21 16:40:45 +0100

    Add test for configuration rendering with variables

--- graylog2-server/src/test/java/org/graylog/plugins/sidecar/collectors/ConfigurationServiceTest.java
+++ graylog2-server/src/test/java/org/graylog/plugins/sidecar/collectors/ConfigurationServiceTest.java
@@ -22,6 +22,7 @@ import com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb;
 import com.lordofthejars.nosqlunit.mongodb.MongoFlexibleComparisonStrategy;
 import org.graylog.plugins.sidecar.database.MongoConnectionRule;
 import org.graylog.plugins.sidecar.rest.models.Configuration;
+import org.graylog.plugins.sidecar.rest.models.ConfigurationVariable;
 import org.graylog.plugins.sidecar.rest.models.NodeDetails;
 import org.graylog.plugins.sidecar.rest.models.Sidecar;
 import org.graylog.plugins.sidecar.services.ConfigurationService;
@@ -59,6 +60,7 @@ public class ConfigurationServiceTest {
     public MongoConnectionRule mongoRule = MongoConnectionRule.build("test");
 
     private ConfigurationService configurationService;
+    private ConfigurationVariableService configurationVariableService;
     private Configuration configuration;
 
 
@@ -76,7 +78,7 @@ public class ConfigurationServiceTest {
         when(sidecar.nodeName()).thenReturn("mockymock");
         when(sidecar.nodeDetails()).thenReturn(nodeDetails);
 
-        ConfigurationVariableService configurationVariableService = new ConfigurationVariableService(mongoRule.getMongoConnection(), mongoJackObjectMapperProvider);
+        this.configurationVariableService = new ConfigurationVariableService(mongoRule.getMongoConnection(), mongoJackObjectMapperProvider);
         this.configurationService = new ConfigurationService(mongoRule.getMongoConnection(), mongoJackObjectMapperProvider, configurationVariableService);
     }
 
@@ -102,4 +104,18 @@ public class ConfigurationServiceTest {
         assertEquals(configWithNewline, result);
     }
 
+    @Test
+    public void testTemplateRenderWithConfigurationVariables() throws Exception {
+        final String TEMPLATE = "foo bar\n myVariable: ${myVariable}\n";
+        final String TEMPLATE_RENDERED = "foo bar\n myVariable: content of myVariable\n";
+        configuration = buildTestConfig(TEMPLATE);
+        this.configurationService.save(configuration);
+
+        ConfigurationVariable myVariable = ConfigurationVariable.create("myVariable", "desc", "content of myVariable");
+        this.configurationVariableService.save(myVariable);
+
+        Configuration result = this.configurationService.renderConfigurationForCollector(sidecar, configuration);
+
+        assertEquals(TEMPLATE_RENDERED, result.template());
+    }
 }
