d42a2793b27b3c4895ce426806731b9c39c2903a
Marco Pfatschbacher <marco@graylog.com>
2018-11-26 10:35:01 +0100

    Simplify debounced validation
    Remove unnecessary lambdas.

--- graylog2-web-interface/src/components/sidecars/configuration-forms/EditConfigurationVariableModal.jsx
+++ graylog2-web-interface/src/components/sidecars/configuration-forms/EditConfigurationVariableModal.jsx
@@ -63,9 +63,7 @@ class EditConfigurationVariableModal extends React.Component {
     });
   };
 
-  _debouncedValidate = lodash.debounce(() => {
-    this._validate();
-  }, 200);
+  _debouncedValidate = lodash.debounce(this._validate, 200);
 
   _save = () => {
     const configuration = this.state;
@@ -76,7 +74,7 @@ class EditConfigurationVariableModal extends React.Component {
   };
 
   _changeName = (event) => {
-    this.setState({ name: event.target.value }, () => this._debouncedValidate());
+    this.setState({ name: event.target.value }, this._debouncedValidate);
   };
 
   _changeDescription = (event) => {
