89c4449e75f1ca2ba0cc48b3e2864f36f7373366
Bernd Ahlers <bernd@graylog.com>
2018-12-06 19:44:16 +0100

    Improve performance of RequestResponseLogger
    Don't do anything if TRACE logging is not enabled.

--- graylog2-server/src/main/java/org/graylog2/indexer/cluster/jest/RequestResponseLogger.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/cluster/jest/RequestResponseLogger.java
@@ -49,22 +49,25 @@ public class RequestResponseLogger implements HttpResponseInterceptor {
 
     @Override
     public void process(HttpResponse response, HttpContext context) throws HttpException, IOException {
-        final StatusLine statusLine = response.getStatusLine();
-        final HttpHost targetHost = (HttpHost) context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
-        final HttpRequest httpRequest = (HttpRequest) context
-                .getAttribute(HttpCoreContext.HTTP_REQUEST);
-        final RequestLine request = httpRequest.getRequestLine();
-        URI uri = null;
-        try {
-            uri = new URIBuilder(targetHost.toURI()).setPath(request.getUri()).build();
-        } catch (URISyntaxException ignore) {
-            // we only update the path of a valid URI, that should never fail
+        // Avoid work if TRACE is not enabled for this class
+        if (logger.isTraceEnabled()) {
+            final StatusLine statusLine = response.getStatusLine();
+            final HttpHost targetHost = (HttpHost) context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
+            final HttpRequest httpRequest = (HttpRequest) context
+                    .getAttribute(HttpCoreContext.HTTP_REQUEST);
+            final RequestLine request = httpRequest.getRequestLine();
+            URI uri = null;
+            try {
+                uri = new URIBuilder(targetHost.toURI()).setPath(request.getUri()).build();
+            } catch (URISyntaxException ignore) {
+                // we only update the path of a valid URI, that should never fail
+            }
+            logger.trace("[{} {}]: {} {}",
+                    statusLine.getStatusCode(),
+                    statusLine.getReasonPhrase(),
+                    request.getMethod(),
+                    uri
+            );
         }
-        logger.trace("[{} {}]: {} {}",
-                statusLine.getStatusCode(),
-                statusLine.getReasonPhrase(),
-                request.getMethod(),
-                uri
-        );
     }
 }
