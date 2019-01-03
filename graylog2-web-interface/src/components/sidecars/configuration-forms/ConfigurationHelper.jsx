345ae84a6f0ae0232e7ad33320fde7370c5f0ec3
Marco Pfatschbacher <marco@graylog.com>
2019-01-03 17:46:19 +0100

    Add NXlog collectors and add new documentation reference (#5409)
    * Add nxlog default collectors and templates for linux and windows
    
    Fixes #5291
    
    * Replace inline filebeat documentation with external links
    
    We don't want to maintain the documentation of various collectors.
    Link to the official documentation instead.
    
    Fixes #5162
    
    * Fix review comments
    
    * Name nxlog without -os postfix

--- graylog2-web-interface/src/components/sidecars/configuration-forms/ConfigurationHelper.jsx
+++ graylog2-web-interface/src/components/sidecars/configuration-forms/ConfigurationHelper.jsx
@@ -1,12 +1,7 @@
 import PropTypes from 'prop-types';
-import lodash from 'lodash';
 import React from 'react';
-import { Col, MenuItem, Nav, Navbar, NavDropdown, Panel, Row, Tab, Tabs } from 'react-bootstrap';
+import { Col, Panel, Row, Tab, Tabs } from 'react-bootstrap';
 
-import DocumentationLink from 'components/support/DocumentationLink';
-import DocsHelper from 'util/DocsHelper';
-
-import FilebeatHelper from './FilebeatHelper';
 import TemplatesHelper from './TemplatesHelper';
 import ConfigurationVariablesHelper from './ConfigurationVariablesHelper';
 import ConfigurationHelperStyle from './ConfigurationHelper.css';
@@ -16,48 +11,9 @@ class ConfigurationHelper extends React.Component {
     onVariableRename: PropTypes.func.isRequired,
   };
 
-  state = {
-    section: undefined,
-    paragraph: undefined,
-  };
-
-  _onSelect = (event) => {
-    const newState = event.split('.');
-    this.setState({ section: newState[0], paragraph: newState[1] });
-  };
-
   _getId = (idName, index) => {
     const idIndex = index !== undefined ? `. ${index}` : '';
     return idName + idIndex;
-  };
-
-  _getEventKey = (a, b) => {
-    return (`${a}.${b}`);
-  };
-
-  navDropDowns = (content) => {
-    const dropDowns = [];
-    Object.keys(content).forEach((section) => {
-      if (!Object.prototype.hasOwnProperty.call(content, section)) {
-        return;
-      }
-
-      const paragraphs = content[section];
-      const menuItems = [];
-
-      for (let i = 0; i < paragraphs.length; i += 1) {
-        menuItems.push(
-          <MenuItem key={this._getId(section, i)} eventKey={this._getEventKey(section, paragraphs[i])}>{lodash.capitalize(paragraphs[i])}</MenuItem>,
-        );
-      }
-      dropDowns.push(
-        <NavDropdown key={this._getId(section)} title={lodash.capitalize(section)} id="basic-nav-dropdown">
-          {menuItems}
-        </NavDropdown>,
-      );
-    });
-
-    return dropDowns;
   };
 
   render() {
@@ -88,22 +44,16 @@ class ConfigurationHelper extends React.Component {
                 <Row className="row-sm">
                   <Col md={12}>
                     <p className={ConfigurationHelperStyle.marginQuickReferenceText}>
-                      Read the <DocumentationLink page={DocsHelper.PAGES.PIPELINE_RULES}
-                                                  text="full documentation" />{' '}
-                      to gain a better understanding of how the Filebeat collector work.
+                      We provide collector configuration templates to get you started.<br />
+                      For further information please refer to the official documentation of your collector.
+                      <ul className={ConfigurationHelperStyle.ulStyle}>
+                        <li><a href="https://www.elastic.co/guide/en/beats/filebeat/current/index.html" target="_blank" rel="noopener noreferrer">Filebeat Reference</a> </li>
+                        <li><a href="https://www.elastic.co/guide/en/beats/winlogbeat/current/index.html" target="_blank" rel="noopener noreferrer">Winlogbeat Reference</a> </li>
+                        <li><a href="https://nxlog.co/docs/nxlog-ce/nxlog-reference-manual.html" target="_blank" rel="noopener noreferrer">NXLog Reference Manual</a> </li>
+                      </ul>
                     </p>
                   </Col>
                 </Row>
-                <Navbar collapseOnSelect>
-                  <Navbar.Collapse>
-                    <Nav onSelect={this._onSelect}>
-                      {this.navDropDowns(FilebeatHelper.toc)}
-                    </Nav>
-                  </Navbar.Collapse>
-                </Navbar>
-                <Panel>
-                  <FilebeatHelper section={this.state.section} paragraph={this.state.paragraph} />
-                </Panel>
               </Tab>
             </Tabs>
           </Col>
