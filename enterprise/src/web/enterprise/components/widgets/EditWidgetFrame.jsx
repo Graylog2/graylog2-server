7261ae940be0b5f1b2ab27712b5e64ebf64adbc9
Dennis Oelkers <dennis@graylog.com>
2019-01-14 16:16:27 +0100

    Limit scope of overriding CSS definitions to extended search. (Graylog2/graylog-plugin-enterprise#640)
    * Limit scope of overriding CSS definitions to extended search.
    
    We are overriding some definitions for the bootstrap modals in parts of
    the extended search. This was leaking to other parts of the product
    accidentally. This change is now limiting those overrides to the
    extended search and they are unloaded when it is left.
    
    Fixes Graylog2/graylog-plugin-enterprise#564.
    
    * Adding flow types and correcting linter rule that is disabled.

--- enterprise/src/web/enterprise/components/widgets/EditWidgetFrame.jsx
+++ enterprise/src/web/enterprise/components/widgets/EditWidgetFrame.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+// @flow strict
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'react-bootstrap';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import styles from '!style?insertAt=bottom!css!./EditWidgetFrame.css';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import globalStyles from '!style/useable!css!./EditWidgetFrame.global.css';
 
 const EditWidgetDialog = ({ children, ...rest }) => <Modal.Dialog {...rest} dialogClassName={styles.editWidgetDialog}>{children}</Modal.Dialog>;
 
@@ -13,16 +16,31 @@ EditWidgetDialog.propTypes = {
 
 const catchEvent = e => e.stopPropagation();
 
-const EditWidgetFrame = ({ children }) => (
-  <Modal show animation={false} dialogComponentClass={EditWidgetDialog} enforceFocus={false}>
-    <div style={{ height: 'calc(100% - 20px)' }} onMouseDown={catchEvent}>
-      {children}
-    </div>
-  </Modal>
-);
-
-EditWidgetFrame.propTypes = {
-  children: PropTypes.node.isRequired,
+type Props = {
+  children: React.Node,
 };
 
-export default EditWidgetFrame;
+export default class EditWidgetFrame extends React.Component<Props> {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  componentWillMount() {
+    globalStyles.use();
+  }
+
+  componentWillUnmount() {
+    globalStyles.unuse();
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <Modal show animation={false} dialogComponentClass={EditWidgetDialog} enforceFocus={false}>
+        <div role="presentation" style={{ height: 'calc(100% - 20px)' }} onMouseDown={catchEvent}>
+          {children}
+        </div>
+      </Modal>
+    );
+  }
+}
