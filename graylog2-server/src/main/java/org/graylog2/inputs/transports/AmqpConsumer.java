f74a68f268ddfe462730b162097337d931e9704a
Dennis Oelkers <dennis@graylog.com>
2019-01-16 17:22:47 +0100

    Removing own AMQP reconnecting in favor of the library's. (#5526)
    Before this change, the `AMQPConsumer`, triggered by a connection
    shutdown callback, tried to reconnect to the broker, supposedly because
    the library did not support auto recovery at the time of implementation.
    
    Current version of the rabbit AMQP client library do support it and in
    the version used it is enabled per default. This lead to creating
    another connection needlessly, wasting resources and leaving one of the
    connections in a state that it is operating on a closed channel,
    therefore throwing an exception and NACKing the first incoming
    exception, ceasing operation afterwards.
    
    This change is doing these things to improve the situation:
    
      * Enable auto recovery explicitly in case the default changes in the
    future
      * Removing custom reconnection code, leaving only a short logging call
    
    Fixes #4474.

--- graylog2-server/src/main/java/org/graylog2/inputs/transports/AmqpConsumer.java
+++ graylog2-server/src/main/java/org/graylog2/inputs/transports/AmqpConsumer.java
@@ -16,15 +16,12 @@
  */
 package org.graylog2.inputs.transports;
 
-import com.google.common.util.concurrent.Uninterruptibles;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.ShutdownListener;
-import com.rabbitmq.client.ShutdownSignalException;
 import org.graylog2.plugin.inputs.MessageInput;
 import org.graylog2.plugin.journal.RawMessage;
 import org.slf4j.Logger;
@@ -95,12 +92,7 @@ public class AmqpConsumer {
         this.requeueInvalid = requeueInvalid;
         this.amqpTransport = amqpTransport;
 
-        scheduler.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                lastSecBytesRead.set(lastSecBytesReadTmp.getAndSet(0));
-            }
-        }, 1, 1, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(() -> lastSecBytesRead.set(lastSecBytesReadTmp.getAndSet(0)), 1, 1, TimeUnit.SECONDS);
     }
 
     public void run() throws IOException {
@@ -156,6 +148,8 @@ public class AmqpConsumer {
         factory.setPort(port);
         factory.setVirtualHost(virtualHost);
         factory.setRequestedHeartbeat(heartbeatTimeout);
+        // explicitly setting this, to ensure it is true even if the default changes.
+        factory.setAutomaticRecoveryEnabled(true);
 
         if (tls) {
             try {
@@ -187,35 +181,15 @@ public class AmqpConsumer {
         if (null != channel && prefetchCount > 0) {
             channel.basicQos(prefetchCount);
 
-            LOG.info("AMQP prefetch count overriden to <{}>.", prefetchCount);
+            LOG.debug("AMQP prefetch count overriden to <{}>.", prefetchCount);
         }
 
-        connection.addShutdownListener(new ShutdownListener() {
-            @Override
-            public void shutdownCompleted(ShutdownSignalException cause) {
-                if (cause.isInitiatedByApplication()) {
-                    LOG.info("Not reconnecting connection, we disconnected explicitly.");
-                    return;
-                }
-                while (true) {
-                    try {
-                        LOG.error("AMQP connection lost! Trying reconnect in 1 second.");
-
-                        Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
-
-                        connect();
-
-                        LOG.info("Connected! Re-starting consumer.");
-
-                        run();
-
-                        LOG.info("Consumer running.");
-                        break;
-                    } catch (IOException e) {
-                        LOG.error("Could not re-connect to AMQP broker.", e);
-                    }
-                }
+        connection.addShutdownListener(cause -> {
+            if (cause.isInitiatedByApplication()) {
+                LOG.info("Shutting down AMPQ consumer.");
+                return;
             }
+            LOG.warn("AMQP connection lost! Reconnecting ...");
         });
     }
 
