83629b74dc32f7ac5a837afc31818b24dd8aee5d
Konrad Merz <konrad@graylog.com>
2019-01-18 13:42:13 +0100

    Use default_value of parameter for entity title
    Prior to this change, a entity with a parameter as title
    did display the parameter name as a title.
    
    This change tries to get the default value of the parameter
    to dispaly as title. If no default value is given
    the fallback is still the name of the parameter.
    
    Fixes #5332

--- graylog2-web-interface/src/pages/CreateContentPackPage.jsx
+++ graylog2-web-interface/src/pages/CreateContentPackPage.jsx
@@ -79,9 +79,9 @@ const CreateContentPackPage = createReactClass({
     CatalogActions.getSelectedEntities(selectedEntities).then((result) => {
       const newContentPack = this.state.contentPack.toBuilder()
         /* Mark entities from server */
-        .entities(result.entities.map(e => Entity.fromJSON(e, true)))
+        .entities(result.entities.map(e => Entity.fromJSON(e, true, this.contentPack.parameters)))
         .build();
-      const fetchedEntities = result.entities.map(e => Entity.fromJSON(e));
+      const fetchedEntities = result.entities.map(e => Entity.fromJSON(e, false, this.contentPack.parameters));
       this.setState({ contentPack: newContentPack, fetchedEntities });
     });
   },
