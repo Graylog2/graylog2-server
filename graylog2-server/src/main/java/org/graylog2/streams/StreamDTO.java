dac4f2b9de02661592b212526fad73d558e280d1
Konrad Merz <konrad@graylog.com>
2019-02-12 13:58:01 +0100

    Add rules to StreamDTO
    Prior to this change, the StreamDTO could not hold the rules.
    
    This change adds a Builder class to add the rules to the StreamDTO
    later on.

--- graylog2-server/src/main/java/org/graylog2/streams/StreamDTO.java
+++ graylog2-server/src/main/java/org/graylog2/streams/StreamDTO.java
@@ -21,12 +21,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import org.graylog.autovalue.WithBeanGetter;
+import org.graylog2.plugin.streams.StreamRule;
 import org.graylog2.rest.models.alarmcallbacks.requests.AlertReceivers;
 import org.graylog2.rest.models.streams.alerts.AlertConditionSummary;
 import org.graylog2.rest.models.system.outputs.responses.OutputSummary;
 
 import javax.annotation.Nullable;
 import java.util.Collection;
+import java.util.Collections;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 
@@ -53,6 +55,10 @@ public abstract class StreamDTO {
 
     @JsonProperty("created_at")
     public abstract String createdAt();
+
+    @JsonProperty("rules")
+    @Nullable
+    public abstract Collection<StreamRule> rules();
 
     @JsonProperty("disabled")
     public abstract boolean disabled();
@@ -83,21 +89,24 @@ public abstract class StreamDTO {
     @JsonProperty("index_set_id")
     public abstract String indexSetId();
 
+    public abstract Builder toBuilder();
+
     @JsonCreator
     public static StreamDTO create(@JsonProperty("_id") String id,
-                                        @JsonProperty("creator_user_id") String creatorUserId,
-                                        @JsonProperty("outputs") @Nullable Collection<OutputSummary> outputs,
-                                        @JsonProperty("matching_type") String matchingType,
-                                        @JsonProperty("description") @Nullable String description,
-                                        @JsonProperty("created_at") String createdAt,
-                                        @JsonProperty("disabled") boolean disabled,
-                                        @JsonProperty("alert_conditions") @Nullable Collection<AlertConditionSummary> alertConditions,
-                                        @JsonProperty("alert_receivers") @Nullable AlertReceivers alertReceivers,
-                                        @JsonProperty("title") String title,
-                                        @JsonProperty("content_pack") @Nullable String contentPack,
-                                        @JsonProperty("is_default_stream") @Nullable Boolean isDefault,
-                                        @JsonProperty("remove_matches_from_default_stream") @Nullable Boolean removeMatchesFromDefaultStream,
-                                        @JsonProperty("index_set_id") String indexSetId) {
+                                   @JsonProperty("creator_user_id") String creatorUserId,
+                                   @JsonProperty("outputs") @Nullable Collection<OutputSummary> outputs,
+                                   @JsonProperty("matching_type") String matchingType,
+                                   @JsonProperty("description") @Nullable String description,
+                                   @JsonProperty("created_at") String createdAt,
+                                   @JsonProperty("disabled") boolean disabled,
+                                   @JsonProperty("rules") @Nullable Collection<StreamRule> rules,
+                                   @JsonProperty("alert_conditions") @Nullable Collection<AlertConditionSummary> alertConditions,
+                                   @JsonProperty("alert_receivers") @Nullable AlertReceivers alertReceivers,
+                                   @JsonProperty("title") String title,
+                                   @JsonProperty("content_pack") @Nullable String contentPack,
+                                   @JsonProperty("is_default_stream") @Nullable Boolean isDefault,
+                                   @JsonProperty("remove_matches_from_default_stream") @Nullable Boolean removeMatchesFromDefaultStream,
+                                   @JsonProperty("index_set_id") String indexSetId) {
         return new AutoValue_StreamDTO(
                 id,
                 creatorUserId,
@@ -105,6 +114,7 @@ public abstract class StreamDTO {
                 matchingType,
                 description,
                 createdAt,
+                rules,
                 disabled,
                 alertConditions,
                 alertReceivers,
@@ -113,5 +123,60 @@ public abstract class StreamDTO {
                 firstNonNull(isDefault, false),
                 firstNonNull(removeMatchesFromDefaultStream, false),
                 indexSetId);
+    }
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+        @JsonCreator
+        public static Builder create() {
+            return new AutoValue_StreamDTO.Builder();
+        }
+
+        @JsonProperty("id")
+        public abstract Builder id(String id);
+
+        @JsonProperty("creator_user_id")
+        public abstract Builder creatorUserId(String creatorUserId);
+
+        @JsonProperty("outputs")
+        public abstract Builder outputs(Collection<OutputSummary> outputs);
+
+        @JsonProperty("matching_type")
+        public abstract Builder matchingType(String matchingType);
+
+        @JsonProperty("description")
+        public abstract Builder description(String description);
+
+        @JsonProperty("created_at")
+        public abstract Builder createdAt(String createdAt);
+
+        @JsonProperty("content_pack")
+        public abstract Builder contentPack(String contentPack);
+
+        @JsonProperty("disabled")
+        public abstract Builder disabled(boolean disabled);
+
+        @JsonProperty("alert_conditions")
+        public abstract Builder alertConditions(Collection<AlertConditionSummary> alertConditions);
+
+        @JsonProperty("rules")
+        public abstract Builder rules(Collection<StreamRule> rules);
+
+        @JsonProperty("alert_receivers")
+        public abstract Builder alertReceivers(AlertReceivers receivers);
+
+        @JsonProperty("title")
+        public abstract Builder title(String title);
+
+        @JsonProperty("is_default_stream")
+        public abstract Builder isDefault(Boolean isDefault);
+
+        @JsonProperty("remove_matches_from_default_stream")
+        public abstract Builder removeMatchesFromDefaultStream(Boolean removeMatchesFromDefaultStream);
+
+        @JsonProperty("index_set_id")
+        public abstract Builder indexSetId(String indexSetId);
+
+        public abstract StreamDTO build();
     }
 }
