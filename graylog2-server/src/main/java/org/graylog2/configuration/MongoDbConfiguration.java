8aa834097ea4c5ed4c71e237c6d309484e2b63fd
Marius Sturm <marius@graylog.com>
2019-03-01 18:35:44 +0100

    Escape + signs in mongo user/password strings

--- graylog2-server/src/main/java/org/graylog2/configuration/MongoDbConfiguration.java
+++ graylog2-server/src/main/java/org/graylog2/configuration/MongoDbConfiguration.java
@@ -21,6 +21,7 @@ import com.github.joschi.jadconfig.ValidationException;
 import com.github.joschi.jadconfig.ValidatorMethod;
 import com.github.joschi.jadconfig.validators.PositiveIntegerValidator;
 import com.github.joschi.jadconfig.validators.StringNotBlankValidator;
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 
@@ -44,7 +45,7 @@ public class MongoDbConfiguration {
     }
 
     public String getUri() {
-        return uri;
+        return escapeUserHostInformation(uri);
     }
 
     public MongoClientURI getMongoClientURI() {
@@ -52,7 +53,7 @@ public class MongoDbConfiguration {
                 .connectionsPerHost(getMaxConnections())
                 .threadsAllowedToBlockForConnectionMultiplier(getThreadsAllowedToBlockMultiplier());
 
-        return new MongoClientURI(uri, mongoClientOptionsBuilder);
+        return new MongoClientURI(escapeUserHostInformation(uri), mongoClientOptionsBuilder);
     }
 
     @ValidatorMethod
@@ -60,5 +61,48 @@ public class MongoDbConfiguration {
         if(getMongoClientURI() == null) {
             throw new ValidationException("mongodb_uri is not a valid MongoDB connection string");
         }
+    }
+
+    private String escapeUserHostInformation(String uri) {
+        // Validate connection string by MongoDriver
+        String connectionString = new ConnectionString(uri).getConnectionString();
+        final String MONGODB_PREFIX = "mongodb://";
+        final String MONGODB_SRV_PREFIX = "mongodb+srv://";
+
+        String unprocessedConnectionString;
+        String escapedConnectionString;
+        if (connectionString.startsWith(MONGODB_PREFIX)) {
+            unprocessedConnectionString = connectionString.substring(MONGODB_PREFIX.length());
+            escapedConnectionString = MONGODB_PREFIX;
+        } else {
+            unprocessedConnectionString = connectionString.substring(MONGODB_SRV_PREFIX.length());
+            escapedConnectionString = MONGODB_SRV_PREFIX;
+        }
+
+        // Split out the user and host information
+        String userAndHostInformation;
+        int idx = unprocessedConnectionString.indexOf("/");
+        if (idx == -1) {
+            userAndHostInformation = unprocessedConnectionString;
+            unprocessedConnectionString = "";
+        } else {
+            userAndHostInformation = unprocessedConnectionString.substring(0, idx);
+            unprocessedConnectionString = unprocessedConnectionString.substring(idx + 1);
+        }
+
+        // Split the user and host information
+        String escapedUserInfo;
+        String hostIdentifier;
+        idx = userAndHostInformation.lastIndexOf("@");
+        if (idx > 0) {
+            escapedUserInfo = userAndHostInformation.substring(0, idx).replace("+", "%2B");
+            hostIdentifier = userAndHostInformation.substring(idx + 1);
+            escapedConnectionString += escapedUserInfo + "@" + hostIdentifier;
+        } else {
+            escapedConnectionString += userAndHostInformation;
+        }
+        escapedConnectionString += "/" + unprocessedConnectionString;
+
+        return escapedConnectionString;
     }
 }
