de608f228e4d397d0ed2ca7ca2769e7840068201
Konrad Merz <konrad@graylog.com>
2019-03-07 11:47:43 +0100

    Add more dashboards to the test
    Also: Fix annotations made by @dennisoelkers

--- graylog2-server/src/test/java/org/graylog2/migrations/V20180214093600_AdjustDashboardPositionToNewResolutionTest.java
+++ graylog2-server/src/test/java/org/graylog2/migrations/V20180214093600_AdjustDashboardPositionToNewResolutionTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * This file is part of Graylog.
  *
  * Graylog is free software: you can redistribute it and/or modify
@@ -16,7 +16,6 @@
  */
 package org.graylog2.migrations;
 
-import com.google.common.collect.ImmutableList;
 import org.graylog2.dashboards.Dashboard;
 import org.graylog2.dashboards.DashboardService;
 import org.graylog2.dashboards.widgets.WidgetPosition;
@@ -25,7 +24,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -38,9 +36,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class V20180214093600_AdjustDashboardPositionToNewResolutionTest {
     @Rule
@@ -78,7 +74,7 @@ public class V20180214093600_AdjustDashboardPositionToNewResolutionTest {
         final Dashboard dashboard = mock(Dashboard.class);
         when(dashboard.getId()).thenReturn("uuu-iii-ddd");
         when(dashboard.getPositions()).thenReturn(Collections.emptyList());
-        when(this.dashboardService.all()).thenReturn(ImmutableList.of(dashboard));
+        when(this.dashboardService.all()).thenReturn(Collections.singletonList(dashboard));
 
         this.adjustDashboardResolutionMigration.upgrade();
 
@@ -87,20 +83,54 @@ public class V20180214093600_AdjustDashboardPositionToNewResolutionTest {
 
     @Test
     public void doMigrateOneDashboardsPositions() throws Exception {
-        final List<WidgetPosition> oldPositions = new ArrayList<>(1);
-        oldPositions.add(WidgetPosition.builder().id("my-position-id").width(5).height(4).col(2).row(2).build());
+        /* Dashboard 1 */
+        final List<WidgetPosition> oldPositions1 = new ArrayList<>(1);
+        oldPositions1.add(WidgetPosition.builder().id("my-position-id").width(5).height(4).col(2).row(2).build());
 
-        final List<WidgetPosition> newPositions = new ArrayList<>(1);
-        newPositions.add(WidgetPosition.builder().id("my-position-id").width(10).height(8).col(3).row(3).build());
+        final List<WidgetPosition> newPositions1 = new ArrayList<>(1);
+        newPositions1.add(WidgetPosition.builder().id("my-position-id").width(10).height(8).col(3).row(3).build());
 
-        final Dashboard dashboard = mock(Dashboard.class, Mockito.RETURNS_DEEP_STUBS);
-        when(dashboard.getPositions()).thenReturn(oldPositions);
-        when(dashboard.getId()).thenReturn("uuu-iii-ddd");
-        when(this.dashboardService.all()).thenReturn(ImmutableList.of(dashboard));
+        final Dashboard dashboard1 = mock(Dashboard.class);
+        when(dashboard1.getPositions()).thenReturn(oldPositions1);
+        when(dashboard1.getId()).thenReturn("uuu-iii-ddd");
+
+        /* Dashboard 2 */
+        final List<WidgetPosition> oldPositions2 = new ArrayList<>(1);
+        oldPositions2.add(WidgetPosition.builder().id("your-position-id").width(1).height(1).col(1).row(1).build());
+
+        final List<WidgetPosition> newPositions2 = new ArrayList<>(1);
+        newPositions2.add(WidgetPosition.builder().id("your-position-id").width(2).height(2).col(1).row(1).build());
+
+        final Dashboard dashboard2 = mock(Dashboard.class);
+        when(dashboard2.getPositions()).thenReturn(oldPositions2);
+        when(dashboard2.getId()).thenReturn("uuu-iii-eee");
+
+        /* Dashboard 3 */
+        final List<WidgetPosition> oldPositions3 = new ArrayList<>(1);
+        oldPositions3.add(WidgetPosition.builder().id("his-position-id").width(1).height(1).col(1).row(1).build());
+        oldPositions3.add(WidgetPosition.builder().id("her-position-id").width(2).height(2).col(2).row(2).build());
+
+        final List<WidgetPosition> newPositions3 = new ArrayList<>(1);
+        newPositions3.add(WidgetPosition.builder().id("his-position-id").width(2).height(2).col(1).row(1).build());
+        newPositions3.add(WidgetPosition.builder().id("her-position-id").width(4).height(4).col(3).row(3).build());
+
+        final Dashboard dashboard3 = mock(Dashboard.class);
+        when(dashboard3.getPositions()).thenReturn(oldPositions3);
+        when(dashboard3.getId()).thenReturn("uuu-iii-eee");
+
+        List<Dashboard> dashboards = new ArrayList<>(2);
+        dashboards.add(dashboard1);
+        dashboards.add(dashboard2);
+        dashboards.add(dashboard3);
+        when(this.dashboardService.all()).thenReturn(dashboards);
 
         this.adjustDashboardResolutionMigration.upgrade();
 
-        verify(dashboard).setPositions(newPositions);
-        verify(this.dashboardService, times(1)).save(dashboard);
+        verify(dashboard1).setPositions(newPositions1);
+        verify(this.dashboardService, times(1)).save(dashboard1);
+        verify(dashboard2).setPositions(newPositions2);
+        verify(this.dashboardService, times(1)).save(dashboard2);
+        verify(dashboard3).setPositions(newPositions3);
+        verify(this.dashboardService, times(1)).save(dashboard3);
     }
 }
