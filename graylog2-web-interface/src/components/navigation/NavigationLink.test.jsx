a03f02cd02c5a995ed5955e1e72f0eb0d433a3a1
Dennis Oelkers <dennis@graylog.com>
2019-03-14 13:23:49 +0100

    Prevent double prefixing paths for navigation items in system menu. (#5765)
    This PR is backporting #5703 to `3.0`.
    
    In #5193, the newly created `NavigationLink` component performs
    prefixing the path passed to it with the application prefix. This is
    happening although in `Routes.jsx` all routes are already being prefixed
    before being exported.
    
    This change is removing the prefixing in `NavigationLink`.
    
    Fixes #5702.
    
    (cherry picked from commit bcf53ec214ea302f8ec3ea0815a0a3c979bbaf13)

--- graylog2-web-interface/src/components/navigation/NavigationLink.test.jsx
+++ graylog2-web-interface/src/components/navigation/NavigationLink.test.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import NavigationLink from './NavigationLink';
+import URLUtils from '../../util/URLUtils';
 
-jest.mock('util/URLUtils', () => ({ appPrefixed: path => `/prefix${path}` }));
+jest.mock('util/URLUtils', () => ({ appPrefixed: jest.fn(path => path) }));
 
 describe('NavigationLink', () => {
   it('renders with simple props', () => {
     const wrapper = mount(<NavigationLink description="Hello there!" path="/hello" />);
-    expect(wrapper.find('LinkContainer')).toHaveProp('to', '/prefix/hello');
+    expect(wrapper.find('LinkContainer')).toHaveProp('to', '/hello');
     expect(wrapper.find('MenuItem')).toHaveText('Hello there!');
   });
   it('passes props to LinkContainer', () => {
     const wrapper = shallow(<NavigationLink description="Hello there!" path="/hello" someProp={42} />);
     expect(wrapper.find('LinkContainer')).toHaveProp('someProp', 42);
+  });
+  it('does not prefix URL with app prefix', () => {
+    URLUtils.appPrefixed.mockImplementation(path => `/someprefix${path}`);
+    const wrapper = shallow(<NavigationLink description="Hello there!" path="/hello" someProp={42} />);
+    const linkContainer = wrapper.find('LinkContainer');
+    expect(linkContainer.props().to).not.toContain('/someprefix');
+    expect(URLUtils.appPrefixed).not.toHaveBeenCalled();
   });
 });
