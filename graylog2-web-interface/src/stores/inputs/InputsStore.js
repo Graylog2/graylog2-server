e934f3c54b63d8f29acaef912935bae37ad932b4
Dennis Oelkers <dennis@graylog.com>
2019-03-21 16:22:13 +0100

    Allowing to retrieve current state from `InputsStore`. (#5791)
    * Allowing to retrieve current state from `InputsStore`.
    
    Before this change, the `InputsStore` did not contain a
    `getInitialState` function, allowing consumers (especially `connect`) to
    retrieve the current state without triggering an update.
    
    This is required by `connect` to get its initial seed. Also it
    alleviates the need to ensure that the consumer is listening to the
    store _before_ a `list` action is triggered.
    
    * Fixing linter hints.

--- graylog2-web-interface/src/stores/inputs/InputsStore.js
+++ graylog2-web-interface/src/stores/inputs/InputsStore.js
@@ -5,9 +5,9 @@ import fetch from 'logic/rest/FetchProvider';
 import UserNotification from 'util/UserNotification';
 
 import StoreProvider from 'injection/StoreProvider';
-const InputStaticFieldsStore = StoreProvider.getStore('InputStaticFields');
-
 import ActionsProvider from 'injection/ActionsProvider';
+
+const InputStaticFieldsStore = StoreProvider.getStore('InputStaticFields');
 const InputsActions = ActionsProvider.getActions('Inputs');
 
 const InputsStore = Reflux.createStore({
@@ -17,8 +17,16 @@ const InputsStore = Reflux.createStore({
   input: undefined,
 
   init() {
-    this.trigger({ inputs: this.inputs, input: this.input });
+    this.trigger(this._state());
     this.listenTo(InputStaticFieldsStore, this.list);
+  },
+
+  getInitialState() {
+    return this._state();
+  },
+
+  _state() {
+    return { inputs: this.inputs, input: this.input };
   },
 
   list() {
@@ -27,7 +35,7 @@ const InputsStore = Reflux.createStore({
       .then(
         (response) => {
           this.inputs = response.inputs;
-          this.trigger({ inputs: this.inputs });
+          this.trigger(this._state());
 
           return this.inputs;
         },
@@ -50,16 +58,16 @@ const InputsStore = Reflux.createStore({
       .then(
         (response) => {
           this.input = response;
-          this.trigger({ input: this.input });
+          this.trigger(this._state());
 
           return this.input;
         },
         (error) => {
           if (showError) {
             UserNotification.error(`Fetching input ${inputId} failed with status: ${error}`,
-                                   'Could not retrieve input');
+              'Could not retrieve input');
           } else {
-            this.trigger({ input: {} });
+            this.trigger(this._state());
           }
         });
 
