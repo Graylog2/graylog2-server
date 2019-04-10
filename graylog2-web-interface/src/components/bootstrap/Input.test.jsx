4a31372ea6b183be289fdd2bd2e578383f6487b7
Kyle Knight <kyle@graylog.com>
2019-04-10 11:08:28 -0500

    Update test and snapshot

--- graylog2-web-interface/src/components/bootstrap/Input.test.jsx
+++ graylog2-web-interface/src/components/bootstrap/Input.test.jsx
@@ -6,13 +6,17 @@ import Input from './Input';
 
 describe('Input', () => {
   it('renders a button after the input if buttonAfter is passed', () => {
-    const wrapper = mount(<Input id="inputWithButton" type="text" buttonAfter={<Button />} />);
+    const wrapper = mount(
+      <Input id="inputWithButton" type="text" buttonAfter={<Button />} />,
+    );
     expect(wrapper.find('button')).toExist();
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders a addon after the input if addonAfter is passed', () => {
-    const wrapper = mount(<Input id="inputWithAddon" type="text" addonAfter=".00" />);
+    const wrapper = mount(
+      <Input id="inputWithAddon" type="text" addonAfter=".00" />,
+    );
     const addon = wrapper.find('span.input-group-addon');
     expect(addon).toExist();
     expect(addon).toHaveText('.00');
@@ -20,8 +24,28 @@ describe('Input', () => {
   });
 
   it('renders a checkbox addon after the input if addonAfter is passed', () => {
-    const wrapper = mount(<Input id="inputWithCheckboxAddon" type="text" addonAfter={<input id="addonCheckbox" type="checkbox" aria-label="..." />} />);
+    const wrapper = mount(
+      <Input id="inputWithCheckboxAddon"
+             type="text"
+             addonAfter={
+               <input id="addonCheckbox" type="checkbox" aria-label="..." />
+        } />,
+    );
     expect(wrapper.find('input#addonCheckbox')).toExist();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders input w/ `name` attribute w/o setting prop', () => {
+    const wrapper = mount(<Input id="inputWithoutNameProp" type="text" />);
+    expect(wrapper.find('input[name="inputWithoutNameProp"]')).toExist();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders input w/ `name` attribute w/ setting prop', () => {
+    const wrapper = mount(
+      <Input id="inputWithoutNameProp" name="inputWithNameProp" type="text" />,
+    );
+    expect(wrapper.find('input[name="inputWithNameProp"]')).toExist();
     expect(wrapper).toMatchSnapshot();
   });
 });
