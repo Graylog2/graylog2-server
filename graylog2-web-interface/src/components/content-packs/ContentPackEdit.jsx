20b121a3624c01639d75068ece1cd9470cbb42cf
Konrad Merz <konrad@graylog.com>
2019-05-13 11:42:56 +0200

    Fix linter warnings

--- graylog2-web-interface/src/components/content-packs/ContentPackEdit.jsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackEdit.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import { AutoAffix } from 'react-overlays';
-import { Spinner, Wizard, ScrollButton } from 'components/common';
+import { ScrollButton, Spinner, Wizard } from 'components/common';
 import ValueReferenceData from 'util/ValueReferenceData';
 
 import ContentPackSelection from 'components/content-packs/ContentPackSelection';
@@ -43,50 +43,16 @@ class ContentPackEdit extends React.Component {
     };
   }
 
-  _disableParameters() {
-    const content = this.props.contentPack;
-    const { selectedEntities } = this.props;
-    const selection = Object.keys(selectedEntities)
-      .reduce((acc, key) => { return acc + selectedEntities[key].length; }, 0) > 0;
-    return !(content.name && content.summary && content.vendor && selection);
-  }
-
-  _disablePreview() {
-    return this.state.selectedStep === 'selection' || !this.state.selectedStep;
-  }
-
-  _prepareForPreview() {
-    const newEntities = this.props.fetchedEntities.map((entity) => {
-      const parameters = this.props.appliedParameter[entity.id] || [];
-      const newEntityBuilder = entity.toBuilder();
-      const entityData = new ValueReferenceData(entity.data);
-      const configPaths = entityData.getPaths();
-
-      Object.keys(configPaths).forEach((path) => {
-        const index = parameters.findIndex((paramMap) => { return paramMap.configKey === path; });
-        if (index >= 0) {
-          configPaths[path].setParameter(parameters[index].paramName);
-        }
-      });
-      newEntityBuilder.data(entityData.getData()).parameters(this.props.contentPack.parameters);
-      return newEntityBuilder.build();
-    });
-    const newContentPack = this.props.contentPack.toBuilder()
-      .entities(newEntities)
-      .build();
-
-    this.props.onStateChange({ contentPack: newContentPack });
-  }
-
   _stepChanged = (selectedStep) => {
     switch (selectedStep) {
       case 'parameters': {
-        const newContentPack = this.props.contentPack.toBuilder()
-          .entities(this.props.fetchedEntities || [])
+        const { onStateChange, selectedEntities, fetchedEntities, onGetEntities, contentPack } = this.props;
+        const newContentPack = contentPack.toBuilder()
+          .entities(fetchedEntities || [])
           .build();
-        this.props.onStateChange({ contentPack: newContentPack });
-        if (Object.keys(this.props.selectedEntities).length > 0) {
-          this.props.onGetEntities(this.props.selectedEntities);
+        onStateChange({ contentPack: newContentPack });
+        if (Object.keys(selectedEntities).length > 0) {
+          onGetEntities(selectedEntities);
         }
         break;
       }
@@ -101,26 +67,66 @@ class ContentPackEdit extends React.Component {
     this.setState({ selectedStep: selectedStep });
   };
 
+  _disableParameters() {
+    const { contentPack, selectedEntities } = this.props;
+    const content = contentPack;
+    const selection = Object.keys(selectedEntities)
+      .reduce((acc, key) => { return acc + selectedEntities[key].length; }, 0) > 0;
+    return !(content.name && content.summary && content.vendor && selection);
+  }
+
+  _disablePreview() {
+    const { selectedStep } = this.state;
+    return selectedStep === 'selection' || !selectedStep;
+  }
+
+  _prepareForPreview() {
+    const { onStateChange, fetchedEntities, contentPack, appliedParameter } = this.props;
+    const newEntities = fetchedEntities.map((entity) => {
+      const parameters = appliedParameter[entity.id] || [];
+      const newEntityBuilder = entity.toBuilder();
+      const entityData = new ValueReferenceData(entity.data);
+      const configPaths = entityData.getPaths();
+
+      Object.keys(configPaths).forEach((path) => {
+        const index = parameters.findIndex((paramMap) => { return paramMap.configKey === path; });
+        if (index >= 0) {
+          configPaths[path].setParameter(parameters[index].paramName);
+        }
+      });
+      newEntityBuilder.data(entityData.getData()).parameters(contentPack.parameters);
+      return newEntityBuilder.build();
+    });
+    const newContentPack = contentPack.toBuilder()
+      .entities(newEntities)
+      .build();
+
+    onStateChange({ contentPack: newContentPack });
+  }
+
   render() {
-    if (!this.props.contentPack) {
+    const { edit, entityIndex, appliedParameter, selectedEntities, contentPack, onSave, onStateChange } = this.props;
+    const { selectedStep } = this.state;
+
+    if (!contentPack) {
       return (<Spinner />);
     }
 
     const selectionComponent = (
-      <ContentPackSelection contentPack={this.props.contentPack}
-                            selectedEntities={this.props.selectedEntities}
-                            edit={this.props.edit}
-                            onStateChange={this.props.onStateChange}
-                            entities={this.props.entityIndex} />
+      <ContentPackSelection contentPack={contentPack}
+                            selectedEntities={selectedEntities}
+                            edit={edit}
+                            onStateChange={onStateChange}
+                            entities={entityIndex} />
     );
     const parameterComponent = (
-      <ContentPackParameters contentPack={this.props.contentPack}
-                             onStateChange={this.props.onStateChange}
-                             appliedParameter={this.props.appliedParameter} />
+      <ContentPackParameters contentPack={contentPack}
+                             onStateChange={onStateChange}
+                             appliedParameter={appliedParameter} />
     );
     const previewComponent = (
-      <ContentPackPreview contentPack={this.props.contentPack}
-                          onSave={this.props.onSave} />
+      <ContentPackPreview contentPack={contentPack}
+                          onSave={onSave} />
     );
     const steps = [
       { key: 'selection', title: 'Content Selection', component: selectionComponent },
@@ -131,10 +137,10 @@ class ContentPackEdit extends React.Component {
     return (
       <div>
         <Wizard steps={steps} onStepChange={this._stepChanged} affixed>
-          {this.state.selectedStep !== 'preview' ? (
+          {selectedStep !== 'preview' ? (
             <AutoAffix viewportOffsetTop={65}>
               <div>
-                <ContentPackDetails contentPack={this.props.contentPack} />
+                <ContentPackDetails contentPack={contentPack} />
               </div>
             </AutoAffix>
           ) : undefined}
