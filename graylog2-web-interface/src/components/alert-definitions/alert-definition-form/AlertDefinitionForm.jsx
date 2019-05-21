71fa82be8331678c8db413df526ec8e55ab2090e
Edmundo Alvarez <edmundo@graylog.com>
2019-05-21 12:21:41 +0200

    Clean up AlertDefinitionForm

--- graylog2-web-interface/src/components/alert-definitions/alert-definition-form/AlertDefinitionForm.jsx
+++ graylog2-web-interface/src/components/alert-definitions/alert-definition-form/AlertDefinitionForm.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import lodash from 'lodash';
-
-import AlertDefinitionPriorityEnum from 'logic/alerts/AlertDefinitionPriorityEnum';
 
 import { Wizard } from 'components/common';
 
 import styles from './AlertDefinitionForm.css';
-
-const priorityOptions = lodash.map(AlertDefinitionPriorityEnum.properties, (value, key) => ({ value: key, label: lodash.upperFirst(value.name) }));
 
 class AlertDefinitionForm extends React.Component {
   static propTypes = {
@@ -24,52 +19,14 @@ class AlertDefinitionForm extends React.Component {
     streams: [],
   };
 
-  state = {
-    completedSteps: [],
-    selectedStep: undefined,
-    isFormVisible: false,
-    formParameters: {},
-  };
-
-  handleSubmit = (alert) => {
-    alert.pralertDefault();
-    this.props.onSubmit();
-  };
-
-  handleChange = (alert) => {
-    const key = alert.target.name;
-    const nextValue = alert.target.value;
-    this.props.onChange(key, nextValue);
-  };
-
-  handlePriorityChange = (nextPriority) => {
-    this.props.onChange('priority', lodash.toNumber(nextPriority));
-  };
-
-  changeSelectedStep = (step) => {
-    this.setState({ selectedStep: step });
-  };
-
-  showForm = (parameters = {}) => {
-    this.setState({ isFormVisible: true, formParameters: parameters });
-  };
-
-  hideForm = () => {
-    this.setState({ isFormVisible: false, formParameters: {} });
-  };
-
-  handleSubmitStepForm = (step) => {
-    this.setState({ completedSteps: lodash.concat(this.state.completedSteps, step) });
-    this.hideForm();
-  };
-
-  handleCancelStepForm = () => {
-    this.hideForm();
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit();
   };
 
   render() {
     const { action, alertDefinition, onCancel, onChange } = this.props;
-    const { selectedStep, completedSteps, isFormVisible, formParameters } = this.state;
 
     const steps = [
       { key: 'alert-details', title: 'Alert Details', component: <div>TBD</div> },
