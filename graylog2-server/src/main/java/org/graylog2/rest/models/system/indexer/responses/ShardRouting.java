3c51d16029212fde182817ab1c9e4043f11baf5d
Dan Torrey <dan@graylog.com>
2019-05-27 02:55:20 -0500

    Remove unused sun.security.provider.SHA import (#5968)

--- graylog2-server/src/main/java/org/graylog2/rest/models/system/indexer/responses/ShardRouting.java
+++ graylog2-server/src/main/java/org/graylog2/rest/models/system/indexer/responses/ShardRouting.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import org.graylog.autovalue.WithBeanGetter;
-import sun.security.provider.SHA;
 
 import javax.annotation.Nullable;
 
