a7ca0a6a8552f9d743cf2c73b463e2ab511c8ec8
Dennis Oelkers <dennis@graylog.com>
2019-06-12 11:34:58 +0200

    Fixing leftover imports.

--- graylog2-web-interface/src/views/components/searchbar/queryinput/ace.js
+++ graylog2-web-interface/src/views/components/searchbar/queryinput/ace.js
@@ -1,6 +1,6 @@
 import AceEditor from 'react-ace-builds';
-import 'enterprise/components/searchbar/queryinput/webpack-resolver';
-import 'enterprise/components/searchbar/queryinput/ace-queryinput';
+import 'views/components/searchbar/queryinput/webpack-resolver';
+import 'views/components/searchbar/queryinput/ace-queryinput';
 import 'ace-builds/src-min-noconflict/ext-language_tools';
 
 export default AceEditor;
