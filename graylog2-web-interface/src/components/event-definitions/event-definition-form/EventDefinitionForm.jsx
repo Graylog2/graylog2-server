9e260e3114fe1d4f3d17c67620e9142ec343e48b
Edmundo Alvarez <edmundo@graylog.com>
2019-06-13 17:42:38 +0200

    Add submit/cancel buttons on summary step

--- graylog2-web-interface/src/components/event-definitions/event-definition-form/EventDefinitionForm.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-form/EventDefinitionForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import lodash from 'lodash';
+import { Button, ButtonToolbar, Col, Row } from 'react-bootstrap';
 
 import { Wizard } from 'components/common';
 import EventDetailsForm from './EventDetailsForm';
@@ -9,6 +11,8 @@ import NotificationsForm from './NotificationsForm';
 import EventDefinitionSummary from './EventDefinitionSummary';
 
 import styles from './EventDefinitionForm.css';
+
+const STEP_KEYS = ['event-details', 'filter-aggregation', 'fields', 'notifications', 'summary'];
 
 class EventDefinitionForm extends React.Component {
   static propTypes = {
@@ -25,50 +29,83 @@ class EventDefinitionForm extends React.Component {
     streams: [],
   };
 
+  state = {
+    activeStep: STEP_KEYS[0],
+  };
+
+  // TODO: Add validation when step changes
+  handleStepChange = (nextStep) => {
+    this.setState({ activeStep: nextStep });
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
     const { onSubmit } = this.props;
     onSubmit();
   };
 
+  renderButtons = (activeStep) => {
+    if (activeStep === lodash.last(STEP_KEYS)) {
+      return (
+        <Row>
+          <Col md={2} mdOffset={7}>
+            <ButtonToolbar>
+              <Button bsStyle="primary">Done</Button>
+              <Button>Cancel</Button>
+            </ButtonToolbar>
+          </Col>
+        </Row>
+      );
+    }
+    return null;
+  };
+
   render() {
     const { action, eventDefinition, streams, onCancel, onChange } = this.props;
+    const { activeStep } = this.state;
 
     const steps = [
       {
-        key: 'event-details',
+        key: STEP_KEYS[0],
         title: 'Event Details',
         component: <EventDetailsForm eventDefinition={eventDefinition} onChange={onChange} />,
       },
       {
-        key: 'filter-aggregation',
+        key: STEP_KEYS[1],
         title: 'Filter & Aggregation',
         component: <FilterAggregationForm eventDefinition={eventDefinition} streams={streams} onChange={onChange} />,
       },
       {
-        key: 'fields',
+        key: STEP_KEYS[2],
         title: 'Fields',
         component: <FieldsForm eventDefinition={eventDefinition} onChange={onChange} />,
       },
       {
-        key: 'notifications',
+        key: STEP_KEYS[3],
         title: 'Notifications',
         component: <NotificationsForm eventDefinition={eventDefinition} onChange={onChange} />,
       },
       {
-        key: 'summary',
+        key: STEP_KEYS[4],
         title: 'Summary',
         component: <EventDefinitionSummary eventDefinition={eventDefinition} />,
       },
     ];
 
     return (
-      <Wizard steps={steps}
-              horizontal
-              justified
-              navigationClassName={styles.steps}
-              containerClassName=""
-              hidePreviousNextButtons />
+      <Row>
+        <Col md={12}>
+          <Wizard steps={steps}
+                  activeStep={activeStep}
+                  onStepChange={this.handleStepChange}
+                  horizontal
+                  justified
+                  navigationClassName={styles.steps}
+                  containerClassName=""
+                  hidePreviousNextButtons />
+          {this.renderButtons(activeStep)}
+        </Col>
+      </Row>
     );
   }
 }
