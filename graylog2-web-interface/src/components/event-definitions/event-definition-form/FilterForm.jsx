28775a1f1d1664a2e9ac29921947324971292486
Edmundo Alvarez <edmundo@graylog.com>
2019-06-21 13:45:36 +0200

    Add input to set filter timerange

--- graylog2-web-interface/src/components/event-definitions/event-definition-form/FilterForm.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-form/FilterForm.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import lodash from 'lodash';
 import { ControlLabel, FormGroup, HelpBlock } from 'react-bootstrap';
+import moment from 'moment';
 
-import { MultiSelect } from 'components/common';
+import { MultiSelect, TimeUnitInput } from 'components/common';
 import { Input } from 'components/bootstrap';
 
 import { naturalSortIgnoreCase } from 'util/SortUtils';
 import FormsUtils from 'util/FormsUtils';
 
 import commonStyles from '../common/commonStyles.css';
+
+const TIME_UNITS = ['HOURS', 'MINUTES', 'SECONDS'];
+
+const durationFromMs = (durationMs) => {
+  const duration = moment.duration(durationMs);
+  const timeUnit = TIME_UNITS.find(unit => lodash.isInteger(duration.as(unit))) || lodash.last(TIME_UNITS);
+  const durationInUnit = duration.as(timeUnit);
+  return {
+    timeUnit: timeUnit,
+    duration: durationInUnit,
+  };
+};
 
 class FilterForm extends React.Component {
   static propTypes = {
@@ -33,11 +46,20 @@ class FilterForm extends React.Component {
     onChange('config', config);
   };
 
+  handleSearchWithinChange = (nextValue, nextUnit) => {
+    const { eventDefinition, onChange } = this.props;
+    const config = lodash.cloneDeep(eventDefinition.config);
+    config.search_within_ms = moment.duration(nextValue, nextUnit).asMilliseconds();
+    onChange('config', config);
+  };
+
   render() {
     const { eventDefinition, streams } = this.props;
     const formattedStreams = streams
       .map(stream => ({ label: stream.title, value: stream.id }))
       .sort((s1, s2) => naturalSortIgnoreCase(s1.label, s2.label));
+
+    const searchWithin = durationFromMs(eventDefinition.config.search_within_ms);
 
     return (
       <fieldset>
@@ -58,6 +80,18 @@ class FilterForm extends React.Component {
                        options={formattedStreams}
                        value={lodash.defaultTo(eventDefinition.config.streams, []).join(',')} />
           <HelpBlock>Select streams the search should include. Searches in all streams if empty.</HelpBlock>
+        </FormGroup>
+
+        <FormGroup controlId="search-within">
+          <TimeUnitInput label="Search within the last"
+                         update={this.handleSearchWithinChange}
+                         value={searchWithin.duration}
+                         unit={searchWithin.timeUnit}
+                         units={TIME_UNITS}
+                         required />
+        </FormGroup>
+
+        <FormGroup controlId="execute-every">
         </FormGroup>
       </fieldset>
     );
