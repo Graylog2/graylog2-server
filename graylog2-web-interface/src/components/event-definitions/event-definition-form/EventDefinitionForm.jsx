27878bd105f12ba77555a39f095869eccc6f77b9
Edmundo Alvarez <edmundo@graylog.com>
2019-06-21 17:01:46 +0200

    Load right condition form on demand

--- graylog2-web-interface/src/components/event-definitions/event-definition-form/EventDefinitionForm.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-form/EventDefinitionForm.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import lodash from 'lodash';
 import { Button, ButtonToolbar, Col, Row } from 'react-bootstrap';
+import { PluginStore } from 'graylog-web-plugin/plugin';
 
 import { Wizard } from 'components/common';
 import EventDetailsForm from './EventDetailsForm';
-import FilterAggregationForm from './event-definition-types/FilterAggregationForm';
 import FieldsForm from './FieldsForm';
 import NotificationsForm from './NotificationsForm';
 import EventDefinitionSummary from './EventDefinitionSummary';
 
 import styles from './EventDefinitionForm.css';
 
-const STEP_KEYS = ['event-details', 'filter-aggregation', 'fields', 'notifications', 'summary'];
+const STEP_KEYS = ['event-details', 'condition', 'fields', 'notifications', 'summary'];
 
 class EventDefinitionForm extends React.Component {
   static propTypes = {
@@ -48,6 +48,17 @@ class EventDefinitionForm extends React.Component {
     onSubmit();
   };
 
+  areStepsDisabled = (eventDefinition) => {
+    return eventDefinition.config.type === undefined;
+  };
+
+  getConditionPlugin = (type) => {
+    if (type === undefined) {
+      return {};
+    }
+    return PluginStore.exports('eventDefinitionTypes').find(edt => edt.type === type);
+  };
+
   renderButtons = (activeStep) => {
     if (activeStep === lodash.last(STEP_KEYS)) {
       const { onCancel } = this.props;
@@ -76,6 +87,15 @@ class EventDefinitionForm extends React.Component {
       onChange,
     };
 
+    const eventDefinitionType = this.getConditionPlugin(eventDefinition.config.type);
+    const eventDefinitionTypeComponent = eventDefinitionType.formComponent
+      ? React.createElement(eventDefinitionType.formComponent, {
+        ...defaultStepProps,
+        allFieldTypes: allFieldTypes,
+        streams: streams,
+      })
+      : null;
+
     const steps = [
       {
         key: STEP_KEYS[0],
@@ -84,23 +104,27 @@ class EventDefinitionForm extends React.Component {
       },
       {
         key: STEP_KEYS[1],
-        title: 'Filter & Aggregation',
-        component: <FilterAggregationForm {...defaultStepProps} allFieldTypes={allFieldTypes} streams={streams} />,
+        title: lodash.defaultTo(eventDefinitionType.displayName, 'Condition'),
+        component: eventDefinitionTypeComponent,
+        disabled: this.areStepsDisabled(eventDefinition),
       },
       {
         key: STEP_KEYS[2],
         title: 'Fields',
         component: <FieldsForm {...defaultStepProps} />,
+        disabled: this.areStepsDisabled(eventDefinition),
       },
       {
         key: STEP_KEYS[3],
         title: 'Notifications',
         component: <NotificationsForm {...defaultStepProps} />,
+        disabled: this.areStepsDisabled(eventDefinition),
       },
       {
         key: STEP_KEYS[4],
         title: 'Summary',
         component: <EventDefinitionSummary action={action} eventDefinition={eventDefinition} />,
+        disabled: this.areStepsDisabled(eventDefinition),
       },
     ];
 
