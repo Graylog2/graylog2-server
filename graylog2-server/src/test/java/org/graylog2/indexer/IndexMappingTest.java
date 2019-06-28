d4f2a9c398a69b0cf684779b3fb57c68542324c0
Dennis Oelkers <dennis@graylog.com>
2019-06-28 15:30:57 +0200

    Adding tests for index mapping types.

---
+++ graylog2-server/src/test/java/org/graylog2/indexer/IndexMappingTest.java
@@ -0,0 +1,164 @@
+package org.graylog2.indexer;
+
+import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.joschi.jadconfig.util.Duration;
+import com.google.common.collect.ImmutableList;
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import org.elasticsearch.index.query.QueryStringQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.graylog2.audit.AuditEventSender;
+import org.graylog2.bindings.providers.JestClientProvider;
+import org.graylog2.indexer.cluster.Node;
+import org.graylog2.indexer.indexset.IndexSetConfig;
+import org.graylog2.indexer.indices.Indices;
+import org.graylog2.indexer.messages.Messages;
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.system.NodeId;
+import org.graylog2.shared.bindings.providers.ObjectMapperProvider;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.CLUSTER_NAME;
+import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.TRANSPORT_TCP_PORT;
+
+@RunWith(Parameterized.class)
+public class IndexMappingTest {
+    private static final String indexName = "testmessages";
+    private static final String currentIndex = "testmessages_0";
+    private static final ObjectMapper objectMapper = new ObjectMapperProvider().get();
+    private final EmbeddedElastic embeddedElastic;
+    private final JestClient jestClient;
+
+    private final IndexSet indexSet;
+    private Indices indices;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> provideData() {
+        return Arrays.asList(new Object[][]{{"5.3.0"}, {"6.2.0"}});
+    }
+
+    public IndexMappingTest(String esVersion) throws IOException, InterruptedException {
+        embeddedElastic = EmbeddedElastic.builder()
+                .withElasticVersion(esVersion)
+                .withSetting(TRANSPORT_TCP_PORT, 0)
+                .withSetting(CLUSTER_NAME, "test")
+                .withEsJavaOpts("-Xms128m -Xmx512m")
+                //.withIndex(indexName, indexSettings)
+                .withStartTimeout(1, MINUTES)
+                .build()
+                .start();
+        jestClient = new JestClientProvider(
+                Collections.singletonList(URI.create("http://localhost:" + embeddedElastic.getHttpPort())),
+                Duration.seconds(30),
+                Duration.seconds(30),
+                Duration.seconds(30),
+                10,
+                10,
+                5,
+                false,
+                null,
+                Duration.seconds(30),
+                false,
+                objectMapper
+        ).get();
+
+        this.indexSet = mock(IndexSet.class);
+        final IndexSetConfig indexSetConfig = mock(IndexSetConfig.class);
+        when(indexSet.getConfig()).thenReturn(indexSetConfig);
+        when(indexSet.getWriteIndexAlias()).thenReturn(currentIndex);
+        when(indexSet.getIndexWildcard()).thenReturn(indexName + "_*");
+        when(indexSetConfig.indexAnalyzer()).thenReturn("standard");
+        when(indexSetConfig.indexTemplateName()).thenReturn(indexName);
+        when(indexSetConfig.indexWildcard()).thenReturn(indexName + "_*");
+        when(indexSetConfig.replicas()).thenReturn(1);
+        when(indexSetConfig.shards()).thenReturn(1);
+    }
+
+    private Messages messages;
+
+    @Before
+    public void setUpClasses() {
+        final IndexMappingFactory indexMappingFactory = new IndexMappingFactory(new Node(jestClient));
+        final NodeId nodeId = mock(NodeId.class);
+        when(nodeId.toString()).thenReturn("deadbeef");
+        final AuditEventSender auditEventSender = mock(AuditEventSender.class);
+        indices = new Indices(jestClient, objectMapper, indexMappingFactory, null, nodeId, auditEventSender, null);
+
+        final MetricRegistry metricRegistry = new MetricRegistry();
+        messages = new Messages(metricRegistry, jestClient);
+        indices.create(currentIndex, indexSet);
+    }
+
+    @Test
+    public void ensureBooleanIndexing() throws IOException {
+        indexMessage(Collections.singletonMap("gl2_boolean", true));
+
+        final SearchResult result = searchFor("gl2_number:true");
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(result.getTotal()).isEqualTo(1);
+    }
+
+    @Test
+    public void ensureNumberIndexing() throws IOException {
+        indexMessage(Collections.singletonMap("gl2_number", 42));
+
+        final SearchResult result = searchFor("gl2_number >= 42");
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(result.getTotal()).isEqualTo(1);
+    }
+
+    @Test
+    public void ensureArrayIndexing() throws IOException {
+        indexMessage(Collections.singletonMap("gl2_array", ImmutableList.of(23, "fourtytwo")));
+
+        final SearchResult result = searchFor("gl2_array:23");
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(result.getTotal()).isEqualTo(1);
+    }
+
+    private void indexMessage(Map<String, Object> additionalFields) {
+        final Message message = new Message("foo", "bar", DateTime.now());
+        additionalFields.forEach(message::addField);
+        final List<Map.Entry<IndexSet, Message>> messageList = Collections.singletonList(new AbstractMap.SimpleEntry<>(indexSet, message));
+        final List<String> messageIds = messages.bulkIndex(messageList);
+
+        assertThat(messageIds).isEmpty();
+    }
+
+    private String queryFor(String query) {
+        return new SearchSourceBuilder()
+                .query(new QueryStringQueryBuilder(query))
+                .toString();
+    }
+
+    private SearchResult searchFor(String query) throws IOException {
+        final Search search = new Search.Builder(queryFor(query))
+                .addIndex(currentIndex)
+                .addType(IndexMapping.TYPE_MESSAGE)
+                .build();
+        return jestClient.execute(search);
+    }
+}
