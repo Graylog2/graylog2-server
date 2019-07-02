c4abc96a9a17a59430733a2e91fa7d99b2963f36
Bernd Ahlers <bernd@graylog.com>
2019-07-02 15:58:15 +0200

    Track ingest receive time in DirectMessageHandler as well

--- graylog2-server/src/main/java/org/graylog2/shared/buffers/DirectMessageHandler.java
+++ graylog2-server/src/main/java/org/graylog2/shared/buffers/DirectMessageHandler.java
@@ -17,21 +17,30 @@
 package org.graylog2.shared.buffers;
 
 import com.lmax.disruptor.WorkHandler;
+import org.graylog2.plugin.journal.RawMessage;
+import org.graylog2.system.processing.ProcessingStatusRecorder;
 
 import javax.inject.Inject;
 
 class DirectMessageHandler implements WorkHandler<RawMessageEvent> {
 
     private final ProcessBuffer processBuffer;
+    private final ProcessingStatusRecorder processingStatusRecorder;
 
     @Inject
-    public DirectMessageHandler(ProcessBuffer processBuffer) {
+    public DirectMessageHandler(ProcessBuffer processBuffer,
+                                ProcessingStatusRecorder processingStatusRecorder) {
         this.processBuffer = processBuffer;
+        this.processingStatusRecorder = processingStatusRecorder;
     }
 
     @Override
     public void onEvent(RawMessageEvent event) throws Exception {
-        processBuffer.insertBlocking(event.getRawMessage());
+        final RawMessage rawMessage = event.getRawMessage();
+        processBuffer.insertBlocking(rawMessage);
+        if (rawMessage != null) {
+            processingStatusRecorder.updateIngestReceiveTime(rawMessage.getTimestamp());
+        }
         // clear out for gc and to avoid promoting the raw message event to a tenured gen
         event.setRawMessage(null);
     }
