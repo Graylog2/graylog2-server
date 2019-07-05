9c85ca32905d8cafeea66f34cbac55c64c7796ec
Dennis Oelkers <dennis@graylog.com>
2019-07-05 10:22:23 +0200

    Only extract node information for proper response.

--- graylog2-web-interface/src/stores/nodes/NodesStore.js
+++ graylog2-web-interface/src/stores/nodes/NodesStore.js
@@ -41,12 +41,14 @@ const NodesStore = Reflux.createStore({
     const promise = this.promises.list || fetchPeriodically('GET', URLUtils.qualifyUrl(ApiRoutes.ClusterApiResource.list().url))
       .then((response) => {
         this.nodes = {};
-        response.nodes.forEach((node) => {
-          this.nodes[node.node_id] = node;
-        });
-        this.clusterId = this._clusterId();
-        this.nodeCount = this._nodeCount();
-        this._propagateState();
+        if (response.nodes) {
+          response.nodes.forEach((node) => {
+            this.nodes[node.node_id] = node;
+          });
+          this.clusterId = this._clusterId();
+          this.nodeCount = this._nodeCount();
+          this._propagateState();
+        }
         return response;
       })
       .finally(() => delete this.promises.list);
