0fb54ffb1f679b792adc5bfdb3d29491c9bdeeee
Bernd Ahlers <bernd@users.noreply.github.com>
2019-07-10 23:19:20 +0200

    Generate a value for the "gl2_message_id" field for each new message (#6074)
    See Message.FIELD_GL2_MESSAGE_ID documentation and the referenced issue
    for more details.
    
    Closes #5994

--- graylog2-server/src/main/java/org/graylog2/shared/buffers/processors/ProcessBufferProcessor.java
+++ graylog2-server/src/main/java/org/graylog2/shared/buffers/processors/ProcessBufferProcessor.java
@@ -23,6 +23,7 @@ import com.google.inject.Provider;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import com.lmax.disruptor.WorkHandler;
+import de.huxhorn.sulky.ulid.ULID;
 import org.graylog2.buffers.OutputBuffer;
 import org.graylog2.messageprocessors.OrderedMessageProcessors;
 import org.graylog2.plugin.Message;
@@ -52,6 +53,7 @@ public class ProcessBufferProcessor implements WorkHandler<MessageEvent> {
 
     private final OutputBuffer outputBuffer;
     private final ProcessingStatusRecorder processingStatusRecorder;
+    private final ULID ulid;
     private final DecodingProcessor decodingProcessor;
     private final Provider<Stream> defaultStreamProvider;
 
@@ -60,11 +62,13 @@ public class ProcessBufferProcessor implements WorkHandler<MessageEvent> {
                                   OrderedMessageProcessors orderedMessageProcessors,
                                   OutputBuffer outputBuffer,
                                   ProcessingStatusRecorder processingStatusRecorder,
+                                  ULID ulid,
                                   @Assisted DecodingProcessor decodingProcessor,
                                   @DefaultStream Provider<Stream> defaultStreamProvider) {
         this.orderedMessageProcessors = orderedMessageProcessors;
         this.outputBuffer = outputBuffer;
         this.processingStatusRecorder = processingStatusRecorder;
+        this.ulid = ulid;
         this.decodingProcessor = decodingProcessor;
         this.defaultStreamProvider = defaultStreamProvider;
 
@@ -122,6 +126,10 @@ public class ProcessBufferProcessor implements WorkHandler<MessageEvent> {
             messages = messageProcessor.process(messages);
         }
         for (Message message : messages) {
+            // Set the message ID once all message processors have finished
+            // See documentation of Message.FIELD_GL2_MESSAGE_ID for details
+            message.addField(Message.FIELD_GL2_MESSAGE_ID, ulid.nextULID());
+
             // The processing time should only be set once all message processors have finished
             message.setProcessingTime(Tools.nowUTC());
             processingStatusRecorder.updatePostProcessingReceiveTime(message.getReceiveTime());
