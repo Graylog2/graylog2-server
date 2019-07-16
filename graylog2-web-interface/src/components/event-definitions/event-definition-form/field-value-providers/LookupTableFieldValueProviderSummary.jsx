24e99b289043d4005e550fcddc0291f6b22e0ca3
Edmundo Alvarez <edmundo@graylog.com>
2019-07-16 18:13:30 +0200

    Add Lookup Table Provider summary

--- graylog2-web-interface/src/components/event-definitions/event-definition-form/field-value-providers/LookupTableFieldValueProviderSummary.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-form/field-value-providers/LookupTableFieldValueProviderSummary.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import CommonFieldValueProviderSummary from './CommonFieldValueProviderSummary';
+
 class LookupTableFieldValueProviderSummary extends React.Component {
-  static propTypes = {};
+  static propTypes = {
+    fieldName: PropTypes.string.isRequired,
+    config: PropTypes.object.isRequired,
+    keys: PropTypes.array.isRequired,
+  };
 
   render() {
-    return <div></div>;
+    const { config } = this.props;
+    const provider = config.providers[0];
+
+    return (
+      <CommonFieldValueProviderSummary {...this.props}>
+        <React.Fragment>
+          <tr>
+            <td>Value source</td>
+            <td>Lookup Table</td>
+          </tr>
+          <tr>
+            <td>Take Lookup Table Key from</td>
+            <td>{provider.key_context === 'source' ? 'Source log message' : 'Generated Event'}</td>
+          </tr>
+          <tr>
+            <td>Lookup Table Key Field</td>
+            <td>{provider.key_field}</td>
+          </tr>
+          <tr>
+            <td>Lookup Table</td>
+            <td>{provider.table_name}</td>
+          </tr>
+        </React.Fragment>
+      </CommonFieldValueProviderSummary>
+    );
   }
 }
 
