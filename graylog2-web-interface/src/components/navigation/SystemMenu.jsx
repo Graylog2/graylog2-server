95d179c382cf765f50a90e9c81e3f489d061175c
Dennis Oelkers <dennis@graylog.com>
2019-07-17 16:32:18 +0200

    Prefixing navigation item paths for system menu as well.

--- graylog2-web-interface/src/components/navigation/SystemMenu.jsx
+++ graylog2-web-interface/src/components/navigation/SystemMenu.jsx
@@ -72,11 +72,12 @@ const SystemMenu = ({ location }) => {
   const pluginSystemNavigations = PluginStore.exports('systemnavigation')
     .sort((route1, route2) => naturalSort(route1.description.toLowerCase(), route2.description.toLowerCase()))
     .map(({ description, path, permissions }) => {
-      const link = <NavigationLink description={description} path={path} />;
+      const prefixedPath = URLUtils.appPrefixed(path);
+      const link = <NavigationLink description={description} path={prefixedPath} />;
       if (permissions) {
         return <IfPermitted key={description} permissions={permissions}>{link}</IfPermitted>;
       }
-      return <NavigationLink key={description} path={path} description={description} />;
+      return <NavigationLink key={description} path={prefixedPath} description={description} />;
     });
 
   return (
