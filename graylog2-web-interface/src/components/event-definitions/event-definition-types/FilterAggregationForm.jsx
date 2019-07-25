bc0753fcbb1afd4a46101e93631faa65cb03bf57
Edmundo Alvarez <edmundo@graylog.com>
2019-07-25 14:50:57 +0200

    Remove data source in Filter & Aggregation

--- graylog2-web-interface/src/components/event-definitions/event-definition-types/FilterAggregationForm.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-types/FilterAggregationForm.jsx
@@ -8,16 +8,9 @@ import FilterForm from './FilterForm';
 import FilterPreviewContainer from './FilterPreviewContainer';
 import AggregationForm from './AggregationForm';
 
-import commonStyles from '../common/commonStyles.css';
-
 const conditionTypes = {
   AGGREGATION: 0,
   FILTER: 1,
-};
-
-const dataSources = {
-  LOG_MESSAGES: 0,
-  EVENTS: 1,
 };
 
 const initialFilterConfig = {
@@ -51,14 +44,12 @@ class FilterAggregationForm extends React.Component {
   constructor(props) {
     super(props);
 
-    const defaultDataSource = props.action === 'edit' ? dataSources.LOG_MESSAGES : undefined;
     // eslint-disable-next-line camelcase
     const { group_by, series } = props.eventDefinition.config;
     const defaultConditionType = (lodash.isEmpty(group_by) && lodash.isEmpty(series)
       ? conditionTypes.FILTER : conditionTypes.AGGREGATION);
 
     this.state = {
-      dataSource: defaultDataSource,
       conditionType: defaultConditionType,
     };
   }
@@ -104,89 +95,49 @@ class FilterAggregationForm extends React.Component {
     this.propagateChange(name, FormsUtils.getValueFromInput(event.target));
   };
 
-  renderDataSourceForm = (dataSource) => {
-    const { allFieldTypes, entityTypes, eventDefinition, streams } = this.props;
-    const { conditionType } = this.state;
-
-    if (dataSource === dataSources.LOG_MESSAGES) {
-      return (
-        <React.Fragment>
-          <Row>
-            <Col md={7} lg={6}>
-              <FilterForm eventDefinition={eventDefinition} streams={streams} onChange={this.propagateChange} />
-
-              <FormGroup>
-                <ControlLabel>Create Events for Definition if...</ControlLabel>
-                <Radio id="filter-type"
-                       name="conditionType"
-                       value={conditionTypes.FILTER}
-                       checked={conditionType === conditionTypes.FILTER}
-                       onChange={this.handleTypeChange}>
-                  Filter has results
-                </Radio>
-                <Radio id="aggregation-type"
-                       name="conditionType"
-                       value={conditionTypes.AGGREGATION}
-                       checked={conditionType === conditionTypes.AGGREGATION}
-                       onChange={this.handleTypeChange}>
-                  Aggregation of results reaches a threshold
-                </Radio>
-              </FormGroup>
-            </Col>
-            <Col md={5} lgOffset={1}>
-              <FilterPreviewContainer eventDefinition={eventDefinition} />
-            </Col>
-          </Row>
-          {conditionType === conditionTypes.AGGREGATION && (
-            <Row>
-              <Col md={12}>
-                <AggregationForm eventDefinition={eventDefinition}
-                                 allFieldTypes={allFieldTypes}
-                                 aggregationFunctions={entityTypes.aggregation_functions}
-                                 onChange={this.propagateChange} />
-              </Col>
-            </Row>
-          )}
-        </React.Fragment>
-      );
-    }
-
-    return (<div>TBD</div>);
-  };
-
   render() {
-    const { dataSource } = this.state;
+    const { conditionType } = this.state;
+    const { allFieldTypes, entityTypes, eventDefinition, streams } = this.props;
 
     return (
-      <Row>
-        <Col md={12}>
-          <h2 className={commonStyles.title}>Select Data Source</h2>
-          <fieldset>
-            <FormGroup>
-              <Radio id="data-source-log-messages"
-                     name="dataSource"
-                     value={dataSources.LOG_MESSAGES}
-                     checked={dataSource === dataSources.LOG_MESSAGES}
-                     onChange={this.handleTypeChange}>
-                Log Messages
-              </Radio>
-              <HelpBlock>Select this option to create an Alert from Log Messages.</HelpBlock>
-              <Radio id="data-source-events"
-                     name="dataSource"
-                     value={dataSources.EVENTS}
-                     checked={dataSource === dataSources.EVENTS}
-                     onChange={this.handleTypeChange}>
-                Events
-              </Radio>
-              <HelpBlock>
-                Select this option to create an Alert from Events created by other Event Definitions.
-              </HelpBlock>
-            </FormGroup>
-          </fieldset>
+      <React.Fragment>
+        <Row>
+          <Col md={7} lg={6}>
+            <FilterForm eventDefinition={eventDefinition} streams={streams} onChange={this.propagateChange} />
 
-          {dataSource !== undefined && this.renderDataSourceForm(dataSource)}
-        </Col>
-      </Row>
+            <FormGroup>
+              <ControlLabel>Create Events for Definition if...</ControlLabel>
+              <Radio id="filter-type"
+                     name="conditionType"
+                     value={conditionTypes.FILTER}
+                     checked={conditionType === conditionTypes.FILTER}
+                     onChange={this.handleTypeChange}>
+                Filter has results
+              </Radio>
+              <Radio id="aggregation-type"
+                     name="conditionType"
+                     value={conditionTypes.AGGREGATION}
+                     checked={conditionType === conditionTypes.AGGREGATION}
+                     onChange={this.handleTypeChange}>
+                Aggregation of results reaches a threshold
+              </Radio>
+            </FormGroup>
+          </Col>
+          <Col md={5} lgOffset={1}>
+            <FilterPreviewContainer eventDefinition={eventDefinition} />
+          </Col>
+        </Row>
+        {conditionType === conditionTypes.AGGREGATION && (
+          <Row>
+            <Col md={12}>
+              <AggregationForm eventDefinition={eventDefinition}
+                               allFieldTypes={allFieldTypes}
+                               aggregationFunctions={entityTypes.aggregation_functions}
+                               onChange={this.propagateChange} />
+            </Col>
+          </Row>
+        )}
+      </React.Fragment>
     );
   }
 }
