0f35c621d4a71dbf81e76a56886864f6cad62445
Edmundo Alvarez <edmundo@graylog.com>
2019-07-26 16:12:14 +0200

    Enable/disable message backlog size with checkbox

--- graylog2-web-interface/src/components/event-definitions/event-definition-form/NotificationSettingsForm.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-form/NotificationSettingsForm.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormGroup, HelpBlock } from 'react-bootstrap';
+import { ControlLabel, FormControl, FormGroup, HelpBlock, InputGroup } from 'react-bootstrap';
 import lodash from 'lodash';
 import moment from 'moment';
 
-import { Input } from 'components/bootstrap';
 import { TimeUnitInput } from 'components/common';
 import { extractDurationAndUnit } from 'components/common/TimeUnitInput';
+import FormsUtils from 'util/FormsUtils';
 
 import commonStyles from '../common/commonStyles.css';
 
@@ -18,17 +18,29 @@ class NotificationSettingsForm extends React.Component {
     onSettingsChange: PropTypes.func.isRequired,
   };
 
-  state = {
-    lastEnabledGracePeriod: undefined,
-    lastBacklogSize: undefined,
+  constructor(props) {
+    super(props);
+
+    const { backlog_size: backlogSize } = props.eventDefinition.notification_settings;
+    const effectiveBacklogSize = lodash.defaultTo(backlogSize, 0);
+
+    this.state = {
+      lastEnabledGracePeriod: undefined,
+      isBacklogSizeEnabled: effectiveBacklogSize > 0,
+      backlogSize: effectiveBacklogSize,
+    };
+  }
+
+  propagateChanges = (key, value) => {
+    const { eventDefinition, onSettingsChange } = this.props;
+    const nextNotificationSettings = lodash.cloneDeep(eventDefinition.notification_settings);
+    nextNotificationSettings[key] = value;
+    onSettingsChange('notification_settings', nextNotificationSettings);
   };
 
   handleGracePeriodChange = (nextValue, nextUnit, enabled) => {
-    const { eventDefinition, onSettingsChange } = this.props;
     const durationInMs = enabled ? moment.duration(nextValue, nextUnit).asMilliseconds() : 0;
-    const nextNotificationSettings = lodash.cloneDeep(eventDefinition.notification_settings);
-    nextNotificationSettings.grace_period_ms = durationInMs;
-    onSettingsChange('notification_settings', nextNotificationSettings);
+    this.propagateChanges('grace_period_ms', durationInMs);
 
     // Update last enabled grace period in state to display, to display the previous value when input is disabled.
     const stateUpdate = {};
@@ -43,17 +55,22 @@ class NotificationSettingsForm extends React.Component {
     this.setState(stateUpdate);
   };
 
-  handleBacklogSizeChange = (e) => {
-    const { eventDefinition, onSettingsChange } = this.props;
-    const nextNotificationSettings = lodash.cloneDeep(eventDefinition.notification_settings);
-    nextNotificationSettings.backlog_size = e.target.value;
-    onSettingsChange('notification_settings', nextNotificationSettings);
-    this.setState({ lastBacklogSize: e.target.value });
+  handleChange = (event) => {
+    const { name } = event.target;
+    const value = FormsUtils.getValueFromInput(event.target);
+    this.setState({ [lodash.camelCase(name)]: value });
+    this.propagateChanges(name, value);
+  };
+
+  toggleBacklogSize = () => {
+    const { isBacklogSizeEnabled, backlogSize } = this.state;
+    this.setState({ isBacklogSizeEnabled: !isBacklogSizeEnabled });
+    this.propagateChanges('backlog_size', (isBacklogSizeEnabled ? 0 : backlogSize));
   };
 
   render() {
     const { eventDefinition } = this.props;
-    const { lastEnabledGracePeriod, lastBacklogSize } = this.state;
+    const { lastEnabledGracePeriod, isBacklogSizeEnabled, backlogSize } = this.state;
 
     if (eventDefinition.notifications.length === 0) {
       return null;
@@ -63,7 +80,6 @@ class NotificationSettingsForm extends React.Component {
     const gracePeriod = (lastEnabledGracePeriod === undefined
       ? extractDurationAndUnit(eventDefinition.notification_settings.grace_period_ms, TIME_UNITS)
       : lastEnabledGracePeriod);
-    const backlogSize = (lastBacklogSize === undefined) ? eventDefinition.notification_settings.backlog_size : lastBacklogSize;
 
     return (
       <React.Fragment>
@@ -81,15 +97,25 @@ class NotificationSettingsForm extends React.Component {
               Events with keys have different grace periods for each key.
             </HelpBlock>
           </FormGroup>
-          <Input type="number"
-                 id="backlogsize"
-                 name="backlog_size"
-                 label="Message Backlog"
-                 required
-                 onChange={this.handleBacklogSizeChange}
-                 help="The maximum number of backlog messages to be included in notifications. Use 0 to disable."
-                 value={backlogSize}
-                  />
+
+          <FormGroup>
+            <ControlLabel>Message Backlog</ControlLabel>
+            <InputGroup>
+              <InputGroup.Addon>
+                <input id="toggle_backlog_size"
+                       type="checkbox"
+                       checked={isBacklogSizeEnabled}
+                       onChange={this.toggleBacklogSize} />
+              </InputGroup.Addon>
+              <FormControl type="number"
+                           id="backlog_size"
+                           name="backlog_size"
+                           onChange={this.handleChange}
+                           value={backlogSize}
+                           disabled={!isBacklogSizeEnabled} />
+            </InputGroup>
+            <HelpBlock>Number of messages to be included in Notifications.</HelpBlock>
+          </FormGroup>
         </fieldset>
       </React.Fragment>
     );
