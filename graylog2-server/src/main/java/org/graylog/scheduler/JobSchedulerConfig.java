32a4cafad8a119fb1e76458c235435ace6a0b67f
Bernd Ahlers <bernd@graylog.com>
2019-07-31 20:49:28 +0200

    Change JobSchedulerConfig interface
    - Rename "canRun" to "canStart"
    - Add "canExecute"
    
    Also don't run scheduler loop if "canExecute" returns false. In that
    case sleep for a while and retry.
    
    Refs Graylog2/graylog-plugin-enterprise#1130

--- graylog2-server/src/main/java/org/graylog/scheduler/JobSchedulerConfig.java
+++ graylog2-server/src/main/java/org/graylog/scheduler/JobSchedulerConfig.java
@@ -21,11 +21,19 @@ package org.graylog.scheduler;
  */
 public interface JobSchedulerConfig {
     /**
-     * Determines if the scheduler is can start.
+     * Determines if the scheduler can start.
      *
      * @return true if the scheduler can be started, false otherwise
      */
-    boolean canRun();
+    boolean canStart();
+
+    /**
+     * Determines if the scheduler can execute the next loop iteration.
+     * This method will be called at the beginning of each scheduler loop iteration so it should be fast!
+     *
+     * @return true if scheduler can execute next loop iteration
+     */
+    boolean canExecute();
 
     /**
      * The number of worker threads to start.
