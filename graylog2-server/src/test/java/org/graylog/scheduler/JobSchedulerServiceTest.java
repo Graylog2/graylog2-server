5b4f0961f58e1abc61e69ab5ebafe26cb8f4ca04
Bernd Ahlers <bernd@graylog.com>
2019-08-02 15:46:49 +0200

    Fix test

--- graylog2-server/src/test/java/org/graylog/scheduler/JobSchedulerServiceTest.java
+++ graylog2-server/src/test/java/org/graylog/scheduler/JobSchedulerServiceTest.java
@@ -23,7 +23,6 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
@@ -33,27 +32,20 @@ import static org.mockito.Mockito.when;
 public class JobSchedulerServiceTest {
     @Test
     public void interruptibleSleeper() throws Exception {
-        assertThatCode(() -> new InterruptibleSleeper(new Semaphore(0)))
-            .isInstanceOf(IllegalArgumentException.class);
-        assertThatCode(() -> new InterruptibleSleeper(new Semaphore(2)))
-            .isInstanceOf(IllegalArgumentException.class);
-
         final Semaphore semaphore = spy(new Semaphore(1));
         final InterruptibleSleeper sleeper = new InterruptibleSleeper(semaphore);
 
         when(semaphore.tryAcquire(1, TimeUnit.SECONDS)).thenReturn(false);
         assertThat(sleeper.sleep(1, TimeUnit.SECONDS)).isTrue();
-        verify(semaphore, times(1)).tryAcquire();
+        verify(semaphore, times(1)).drainPermits();
         verify(semaphore, times(1)).tryAcquire(1, TimeUnit.SECONDS);
-        verify(semaphore, times(1)).release();
 
         reset(semaphore);
 
         when(semaphore.tryAcquire(1, TimeUnit.SECONDS)).thenReturn(true);
         assertThat(sleeper.sleep(1, TimeUnit.SECONDS)).isFalse();
-        verify(semaphore, times(1)).tryAcquire();
+        verify(semaphore, times(1)).drainPermits();
         verify(semaphore, times(1)).tryAcquire(1, TimeUnit.SECONDS);
-        verify(semaphore, times(1)).release();
 
         reset(semaphore);
 
