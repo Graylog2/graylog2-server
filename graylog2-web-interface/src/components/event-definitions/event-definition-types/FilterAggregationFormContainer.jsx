43b14fa815c02e86d0f2e91941c98cb2f146cfe5
Edmundo Alvarez <edmundo@graylog.com>
2019-08-08 15:46:10 +0200

    Load Streams in Filter & Aggregation summary

--- graylog2-web-interface/src/components/event-definitions/event-definition-types/FilterAggregationFormContainer.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-types/FilterAggregationFormContainer.jsx
@@ -4,12 +4,10 @@ import PropTypes from 'prop-types';
 import { Spinner } from 'components/common';
 
 import connect from 'stores/connect';
-import CombinedProvider from 'injection/CombinedProvider';
 import { FieldTypesStore } from 'views/stores/FieldTypesStore';
 
 import FilterAggregationForm from './FilterAggregationForm';
-
-const { StreamsStore } = CombinedProvider.get('Streams');
+import withStreams from './withStreams';
 
 // We currently don't support creating Events from these Streams, since they also contain Events
 // and it's not possible to access custom Fields defined in them.
@@ -27,34 +25,18 @@ class FilterAggregationFormContainer extends React.Component {
     onChange: PropTypes.func.isRequired,
   };
 
-  state = {
-    availableStreams: undefined,
-  };
-
-  componentDidMount() {
-    StreamsStore.load((streams) => {
-      const filteredStreams = streams.filter(s => !HIDDEN_STREAMS.includes(s.id));
-      this.setState({ availableStreams: filteredStreams });
-    });
-  }
-
   render() {
     const { fieldTypes, ...otherProps } = this.props;
-    const { availableStreams } = this.state;
-    const isLoading = typeof fieldTypes.all !== 'object' || !availableStreams;
+    const isLoading = typeof fieldTypes.all !== 'object';
 
     if (isLoading) {
       return <Spinner text="Loading Filter & Aggregation Information..." />;
     }
 
-    return (
-      <FilterAggregationForm allFieldTypes={fieldTypes.all.toJS()}
-                             streams={availableStreams}
-                             {...otherProps} />
-    );
+    return <FilterAggregationForm allFieldTypes={fieldTypes.all.toJS()} {...otherProps} />;
   }
 }
 
-export default connect(FilterAggregationFormContainer, {
+export default connect(withStreams(FilterAggregationFormContainer, HIDDEN_STREAMS), {
   fieldTypes: FieldTypesStore,
 });
