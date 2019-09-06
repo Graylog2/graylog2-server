9f7d88b3cb9805a990e081696cae172e189eae6a
Dennis Oelkers <dennis@graylog.com>
2019-09-06 16:57:22 +0200

    Using current view type to generate new query.

--- graylog2-web-interface/src/views/logic/valueactions/UseInNewQueryHandler.js
+++ graylog2-web-interface/src/views/logic/valueactions/UseInNewQueryHandler.js
@@ -7,12 +7,14 @@ import ViewState from 'views/logic/views/ViewState';
 import { escape } from 'views/logic/queries/QueryHelper';
 import type { ValueActionHandlerWithContext, ValueActionHideCondition } from './ValueActionHandler';
 import View from '../views/View';
+import type { ActionContexts } from '../ActionContext';
+import FieldType from '../fieldtypes/FieldType';
 
-const UseInNewQueryHandler: ValueActionHandlerWithContext = (queryId: string, field: string, value: string) => {
+const UseInNewQueryHandler: ValueActionHandlerWithContext = (queryId: string, field: string, value: string, type: FieldType, { view }: ActionContexts) => {
   const query: Query = QueryGenerator().toBuilder()
     .query({ type: 'elasticsearch', query_string: `${field}:${escape(value)}` })
     .build();
-  const state: ViewState = ViewStateGenerator(View.Type.Dashboard);
+  const state: ViewState = ViewStateGenerator(view.type);
   return QueriesActions.create(query, state);
 };
 
