bd902c61be3e7eb6255950e0f84e8ff2936ac62f
Konrad Merz <konrad@graylog.com>
2019-09-06 10:47:00 +0200

    Add IfSearch component (#6380)
    * Add IfSearch component
    
    Add a component which only renders its children when the
    context provides a ViewType of search
    
    Also add unit test for IfDashboard
    
    * Add missing snapshot
    
    * Fix test description
    
    * Remove obsolete snapshot

---
+++ graylog2-web-interface/src/views/components/dashboard/IfDashboard.test.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'helpers/mocking/react-dom_mock';
+
+import View from 'views/logic/views/View';
+import ViewTypeContext from 'views/components/contexts/ViewTypeContext';
+
+import IfDashboard from './IfDashboard';
+
+describe('IfDashboard', () => {
+  it('should render children with dashboard context', () => {
+    const wrapper = renderer.create(
+      <ViewTypeContext.Provider value={View.Type.Dashboard}>
+        <span>I must not fear.</span>
+        <IfDashboard>
+          <span>Fear is the mind-killer.</span>
+        </IfDashboard>
+      </ViewTypeContext.Provider>,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should not render children without dashboard context', () => {
+    const wrapper = renderer.create(
+      <ViewTypeContext.Provider value={View.Type.Search}>
+        <span>I must not fear.</span>
+        <IfDashboard>
+          <span>Fear is the mind-killer.</span>
+        </IfDashboard>
+      </ViewTypeContext.Provider>,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+
+  it('should render children without context since Dashboard is the default', () => {
+    const wrapper = renderer.create(
+      <div>
+        <span>I must not fear.</span>
+        <IfDashboard>
+          <span>Fear is the mind-killer.</span>
+        </IfDashboard>
+      </div>,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
