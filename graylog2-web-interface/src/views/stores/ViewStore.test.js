69ad07532b027dc30221076865b65805284b1bec
Dennis Oelkers <dennis@graylog.com>
2019-09-09 12:03:20 +0200

    Adapting and modernizing ViewStore test.

--- graylog2-web-interface/src/views/stores/ViewStore.test.js
+++ graylog2-web-interface/src/views/stores/ViewStore.test.js
@@ -1,3 +1,4 @@
+// @flow strict
 import * as Immutable from 'immutable';
 
 import ViewState from 'views/logic/views/ViewState';
@@ -5,6 +6,7 @@ import SearchActions from 'views/actions/SearchActions';
 import Search from 'views/logic/search/Search';
 import Query from 'views/logic/queries/Query';
 import View from 'views/logic/views/View';
+import mockAction from 'helpers/mocking/MockAction';
 import AggregationWidget from '../logic/aggregationbuilder/AggregationWidget';
 import { ViewActions, ViewStore } from './ViewStore';
 import { ViewManagementActions } from './ViewManagementStore';
@@ -20,41 +22,25 @@ jest.mock('views/logic/Widget', () => ({
 jest.mock('logic/rest/FetchProvider', () => jest.fn(() => Promise.resolve()));
 
 describe('ViewStore', () => {
-  it('.load should select first query if activeQuery is not set', () => {
-    const view = {
-      search: {
-        queries: Immutable.List([{ id: 'firstQueryId' }]),
-      },
-    };
-    return ViewActions.load(view).then((state) => {
-      expect(state.activeQuery).toBe('firstQueryId');
-    });
-  });
-  it('.load should select activeQuery if it is set and present in view', () => {
-    ViewActions.selectQuery('secondQueryId');
-    const view = {
-      search: {
-        queries: Immutable.List([{ id: 'firstQueryId' }, { id: 'secondQueryId' }]),
-      },
-    };
-    return ViewActions.load(view).then((state) => {
-      expect(state.activeQuery).toBe('secondQueryId');
-    });
-  });
-  it('.load should select first query if activeQuery is set but not present in view', () => {
-    ViewActions.selectQuery('nonExistingQueryId');
-    const view = {
-      search: {
-        queries: Immutable.List([{ id: 'firstQueryId' }, { id: 'secondQueryId' }]),
-      },
-    };
-    return ViewActions.load(view).then((state) => {
-      expect(state.activeQuery).toBe('firstQueryId');
-    });
-  });
+  const dummySearch = Search.create()
+    .toBuilder()
+    .queries([
+      Query.builder().id('firstQueryId').build(),
+      Query.builder().id('secondQueryId').build(),
+    ])
+    .build();
+  const dummyView = View.builder().search(dummySearch).build();
+  it('.load should select first query if activeQuery is not set', () => ViewActions.load(dummyView)
+    .then(state => expect(state.activeQuery).toBe('firstQueryId')));
+  it('.load should select activeQuery if it is set and present in view', () => ViewActions.selectQuery('secondQueryId')
+    .then(() => ViewActions.load(dummyView))
+    .then(state => expect(state.activeQuery).toBe('secondQueryId')));
+  it('.load should select first query if activeQuery is set but not present in view', () => ViewActions.selectQuery('nonExistingQueryId')
+    .then(() => ViewActions.load(dummyView))
+    .then(state => expect(state.activeQuery).toBe('firstQueryId')));
   describe('maintains dirty flag:', () => {
     beforeEach(() => {
-      SearchActions.create = jest.fn(s => Promise.resolve({ search: s }));
+      SearchActions.create = mockAction(jest.fn(s => Promise.resolve({ search: s })));
     });
     it('resets dirty flag when an existing view is updated', () => {
       const search = Search.create();
@@ -72,7 +58,7 @@ describe('ViewStore', () => {
         });
     });
     it('sets dirty flag to false when creating a view', () => {
-      return ViewActions.create()
+      return ViewActions.create(View.Type.Dashboard)
         .then(({ dirty }) => expect(dirty).toBeFalsy());
     });
   });
@@ -81,7 +67,7 @@ describe('ViewStore', () => {
     let search: Search;
     let view: View;
     beforeEach(() => {
-      SearchActions.create = jest.fn(s => Promise.resolve({ search: s }));
+      SearchActions.create = mockAction(jest.fn(s => Promise.resolve({ search: s })));
 
       search = Search.create()
         .toBuilder()
@@ -92,16 +78,11 @@ describe('ViewStore', () => {
         .state(Immutable.fromJS({ firstQueryId: ViewState.create() }))
         .search(search)
         .build();
-      return ViewStore.load(view);
+      return ViewActions.load(view);
     });
-    it('should create search when view is created', (done) => {
-      ViewActions.create()
-        .then(() => {
-          expect(SearchActions.create).toHaveBeenCalled();
-          done();
-        });
-    });
-    it('should create search when state is updated', (done) => {
+    it('should create search when view is created', () => ViewActions.create(View.Type.Dashboard)
+      .then(() => expect(SearchActions.create).toHaveBeenCalled()));
+    it('should create search when state is updated', () => {
       const newState = Immutable.fromJS({
         firstQueryId: ViewState.create()
           .toBuilder()
@@ -109,26 +90,15 @@ describe('ViewStore', () => {
           .build(),
       });
 
-      ViewActions.state(newState)
-        .then(() => {
-          expect(SearchActions.create).toHaveBeenCalled();
-          done();
-        });
+      return ViewActions.state(newState)
+        .then(() => expect(SearchActions.create).toHaveBeenCalled());
     });
-    it('should not recreate search when state is updated to identical state', (done) => {
-      ViewActions.state(view.state)
-        .then(() => {
-          expect(SearchActions.create).not.toHaveBeenCalled();
-          done();
-        });
-    });
-    it('should create search when search is replaced', (done) => {
+    it('should not recreate search when state is updated to identical state', () => ViewActions.state(view.state)
+      .then(() => expect(SearchActions.create).not.toHaveBeenCalled()));
+    it('should create search when search is replaced', () => {
       const newSearch = search.toBuilder().newId().build();
-      ViewActions.search(newSearch)
-        .then(() => {
-          expect(SearchActions.create).toHaveBeenCalled();
-          done();
-        });
+      return ViewActions.search(newSearch)
+        .then(() => expect(SearchActions.create).toHaveBeenCalled());
     });
   });
 });
