c6601f1cd9171eb7ce119259f2e98e8fbc3667a0
Dennis Oelkers <dennis@graylog.com>
2019-09-10 12:25:22 +0200

    Adjusting inputs to show "No Override" options.

---
+++ graylog2-web-interface/src/views/components/searchbar/DisabledTimeRangeSelector.jsx
@@ -0,0 +1,10 @@
+// @flow strict
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+const DisabledTimeRangeSelector = () => {
+};
+
+DisabledTimeRangeSelector.propTypes = {};
+
+export default DisabledTimeRangeSelector;
