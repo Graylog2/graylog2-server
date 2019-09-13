8069858db40c152b4527d8a6183a3ab5b2b75570
Dennis Oelkers <dennis@graylog.com>
2019-09-13 09:48:32 +0200

    Updating widget query/timerange when changing in edit mode.

--- graylog2-web-interface/src/views/components/WidgetQueryControls.jsx
+++ graylog2-web-interface/src/views/components/WidgetQueryControls.jsx
@@ -1,6 +1,7 @@
 // @flow strict
 import * as React from 'react';
 import * as Immutable from 'immutable';
+import moment from 'moment';
 
 import connect from 'stores/connect';
 import { Col, Row } from 'components/graylog/index';
@@ -8,10 +9,11 @@ import DocumentationLink from 'components/support/DocumentationLink';
 import DocsHelper from 'util/DocsHelper';
 
 import Widget from 'views/logic/widgets/Widget';
-import { QueriesActions } from 'views/actions/QueriesActions';
 import { QueryFiltersActions } from 'views/stores/QueryFiltersStore';
 import { StreamsStore } from 'views/stores/StreamsStore';
 import { SearchConfigStore } from 'views/stores/SearchConfigStore';
+import { WidgetActions } from 'views/stores/WidgetStore';
+import type { TimeRange, TimeRangeTypes } from 'views/logic/queries/Query';
 import TimeRangeTypeSelector from './searchbar/TimeRangeTypeSelector';
 import TimeRangeInput from './searchbar/TimeRangeInput';
 import StreamsFilter from './searchbar/StreamsFilter';
@@ -24,24 +26,57 @@ type Props = {
   widget: Widget,
 };
 
+const _updateQuery = (id: string, queryString: string) => WidgetActions.query(id, { type: 'elasticsearch', query_string: queryString });
+const _updateRangeType = (oldTimerange: TimeRange, id: string, newRangeType: TimeRangeTypes) => {
+  const { type } = oldTimerange || {};
+  if (type === newRangeType) {
+    return Promise.resolve();
+  }
+  let newTimerange: TimeRange;
+  // eslint-disable-next-line default-case
+  switch (newRangeType) {
+    case 'absolute':
+      newTimerange = {
+        type: newRangeType,
+        from: moment().subtract(oldTimerange ? oldTimerange.range : 300, 'seconds').toISOString(),
+        to: moment().toISOString(),
+      };
+      break;
+    case 'relative':
+      newTimerange = {
+        type: newRangeType,
+        range: 300,
+      };
+      break;
+    case 'keyword':
+      newTimerange = {
+        type: newRangeType,
+        keyword: 'Last five Minutes',
+      };
+      break;
+  }
+  return WidgetActions.timerange(id, newTimerange);
+};
+
+type Delta = {| range: number |} | {| from: string |} | {| to: string |} | {| keyword: string |};
+
+const _updateRangeParams = (currentTimerange: TimeRange, id: string, delta: Delta) => WidgetActions.timerange(id, { ...currentTimerange, ...delta });
+
 const WidgetQueryControls = ({ availableStreams, config, widget }: Props) => {
   const { query, timerange } = widget;
   const disableSearch = false;
-  const rangeType = timerange ? timerange.type : 'relative';
-  const rangeParams = Immutable.Map(timerange || { range: 300 });
-  const streams = widget.filter
-    .filter(f => f.get('type') === 'stream')
-    .map(f => f.get('id'))
-    .toJS();
+  const { type: rangeType = 'relative', ...rest } = timerange || {};
+  const rangeParams = Immutable.Map(rest || { range: 300 });
+  const streams = [];
   const { id } = widget;
   const performSearch = () => {};
   return (
     <React.Fragment>
       <Row className="no-bm extended-search-query-metadata">
         <Col md={4}>
-          <TimeRangeTypeSelector onSelect={newRangeType => QueriesActions.rangeType(id, newRangeType).then(performSearch)}
+          <TimeRangeTypeSelector onSelect={newRangeType => _updateRangeType(timerange, id, newRangeType).then(performSearch)}
                                  value={rangeType} />
-          <TimeRangeInput onChange={(key, value) => QueriesActions.rangeParams(id, key, value).then(performSearch)}
+          <TimeRangeInput onChange={(key, value) => _updateRangeParams(timerange, id, { [key]: value }).then(performSearch)}
                           rangeType={rangeType}
                           rangeParams={rangeParams}
                           config={config} />
@@ -65,7 +100,7 @@ const WidgetQueryControls = ({ availableStreams, config, widget }: Props) => {
 
           <QueryInput value={query ? query.query_string : undefined}
                       placeholder={'Type your search query here and press enter. E.g.: ("not found" AND http) OR http_response_code:[400 TO 404]'}
-                      onChange={value => QueriesActions.query(id, value).then(performSearch).then(() => value)}
+                      onChange={value => _updateQuery(id, value).then(performSearch).then(() => value)}
                       onExecute={performSearch} />
         </Col>
       </Row>
