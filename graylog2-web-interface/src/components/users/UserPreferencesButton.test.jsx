73afa7f836c221a0d28f8206ed0ac356e28c61dd
Kyle Knight <kyle@graylog.com>
2019-09-19 07:40:08 -0500

    UserPreferencesButton test

--- graylog2-web-interface/src/components/users/UserPreferencesButton.test.jsx
+++ graylog2-web-interface/src/components/users/UserPreferencesButton.test.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-// import { shallow } from 'enzyme';
 
-import { CombinedProviderMock, StoreMock } from 'helpers/mocking';
+import { CombinedProviderMock as MockCombinedProvider, StoreMock as MockStore } from 'helpers/mocking';
 
 import UserPreferencesButton from 'components/users/UserPreferencesButton';
+
+import StoreProvider from 'injection/StoreProvider';
+
+jest.mock('injection/CombinedProvider', () => {
+  const mockPreferencesStore = MockStore('get', 'listen', 'loadUserPreferences');
+  const combinedProviderMock = new MockCombinedProvider({
+    Preferences: { PreferencesStore: mockPreferencesStore },
+  });
+
+  return combinedProviderMock;
+});
+
+const PreferencesStore = StoreProvider.getStore('Preferences');
 
 describe('UserPreferencesButton', () => {
   beforeEach(() => {
@@ -12,13 +24,6 @@ describe('UserPreferencesButton', () => {
   });
 
   it('should load user data when user clicks edit button', () => {
-    const PreferencesStore = StoreMock('get', 'listen', 'loadUserPreferences');
-    const combinedProviderMock = new CombinedProviderMock({
-      Preferences: { PreferencesStore },
-    });
-
-    jest.doMock('injection/CombinedProvider', () => combinedProviderMock);
-
     const instance = render(<UserPreferencesButton userName="Full" />);
     const button = instance.getByTestId('user-preferences-button');
 
