af8a261f7125c64cf57a3a7f198bcd5672f47be6
Kyle Knight <kyle@graylog.com>
2019-09-20 12:53:21 -0500

    General Scratchpad Creation

--- graylog2-web-interface/src/routing/AppWithGlobalNotifications.jsx
+++ graylog2-web-interface/src/routing/AppWithGlobalNotifications.jsx
@@ -1,24 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Scratchpad from '../views/components/common/Scratchpad';
 
 import AppGlobalNotifications from './AppGlobalNotifications';
 
-class AppWithGlobalNotifications extends React.Component {
-  static propTypes = {
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.element),
-      PropTypes.element,
-    ]).isRequired,
-  };
+const GridContent = styled.div`
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 30px;
+  grid-template-areas: "content scratchpad";
+`;
 
-  render() {
-    return (
-      <div>
-        <AppGlobalNotifications />
-        {this.props.children}
-      </div>
-    );
-  }
-}
+const ContentArea = styled.div`
+  grid-area: content;
+`;
+
+const ScratchpadWrapper = styled.div`
+    grid-area: scratchpad;
+    position: sticky;
+    top: 50px;
+    right: 0;
+    z-index: 2;
+    height: calc(100vh - 50px);
+    background-color: #393939;
+  `;
+
+const AppWithGlobalNotifications = ({ children }) => {
+  return (
+    <div>
+      <AppGlobalNotifications />
+      <GridContent>
+        <ContentArea>
+          {children}
+        </ContentArea>
+
+        <ScratchpadWrapper>
+          <Scratchpad />
+        </ScratchpadWrapper>
+      </GridContent>
+    </div>
+  );
+};
+
+AppWithGlobalNotifications.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]).isRequired,
+};
 
 export default AppWithGlobalNotifications;
