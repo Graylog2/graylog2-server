50a3f78de82bec5855669675a8fdcd53aef0656e
Kyle Knight <kyle@graylog.com>
2019-10-04 08:14:08 -0500

    Migrating App.jsx to functional component

--- graylog2-web-interface/src/routing/App.jsx
+++ graylog2-web-interface/src/routing/App.jsx
@@ -1,54 +1,60 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import createReactClass from 'create-react-class';
-import Reflux from 'reflux';
+
 import Navigation from 'components/navigation/Navigation';
 import Spinner from 'components/common/Spinner';
 import Footer from 'components/layout/Footer';
+import connect from 'stores/connect';
+import StoreProvider from 'injection/StoreProvider';
+
+import AppErrorBoundary from './AppErrorBoundary';
 
 import 'stylesheets/jquery.dynatable.css';
 import 'stylesheets/typeahead.less';
 import 'c3/c3.css';
 import 'dc/dc.css';
 
-import StoreProvider from 'injection/StoreProvider';
-import AppErrorBoundary from './AppErrorBoundary';
-
 const CurrentUserStore = StoreProvider.getStore('CurrentUser');
 
-const App = createReactClass({
-  displayName: 'App',
+const App = ({ children, currentUser: { currentUser }, location }) => {
+  if (!currentUser) {
+    return <Spinner />;
+  }
 
-  propTypes: {
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.element),
-      PropTypes.element,
-    ]).isRequired,
-    location: PropTypes.object.isRequired,
-  },
-
-  mixins: [Reflux.connect(CurrentUserStore)],
-
-  render() {
-    if (!this.state.currentUser) {
-      return <Spinner />;
-    }
-    return (
-      <div>
-        <Navigation requestPath={this.props.location.pathname}
-                    fullName={this.state.currentUser.full_name}
-                    loginName={this.state.currentUser.username}
-                    permissions={this.state.currentUser.permissions} />
-        <div id="scroll-to-hint" style={{ display: 'none' }} className="alpha80">
-          <i className="fa fa-arrow-up" />
-        </div>
-        <AppErrorBoundary>
-          {this.props.children}
-        </AppErrorBoundary>
-        <Footer />
+  return (
+    <div>
+      <Navigation requestPath={location.pathname}
+                  fullName={currentUser.full_name}
+                  loginName={currentUser.username}
+                  permissions={currentUser.permissions} />
+      <div id="scroll-to-hint" style={{ display: 'none' }} className="alpha80">
+        <i className="fa fa-arrow-up" />
       </div>
-    );
-  },
-});
+      <AppErrorBoundary>
+        {children}
+      </AppErrorBoundary>
+      <Footer />
+    </div>
+  );
+};
 
-export default App;
+App.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]).isRequired,
+  currentUser: PropTypes.shape({
+    currentUser: PropTypes.shape({
+      full_name: PropTypes.string,
+      username: PropTypes.string,
+      permissions: PropTypes.array,
+    }),
+  }),
+  location: PropTypes.object.isRequired,
+};
+
+App.defaultProps = {
+  currentUser: undefined,
+};
+
+export default connect(App, { currentUser: CurrentUserStore });
