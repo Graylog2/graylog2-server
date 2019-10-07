6ad2f7210726c0afe116cf1ee6e054c85c65ff67
Kyle Knight <kyle@graylog.com>
2019-10-07 07:30:35 -0500

    Themed SplitButton (#6442)
    * Themable Buttons
    
    * ThemeProvider
    
    Tracking webpack build issue
    
    Update vendor manifest
    
    Organize
    
    btn theming
    
    Themed Buttons
    
    * todo
    
    * Themed DropdownButton
    
    buttonStyles cleanup
    
    * remove paran wrap
    
    * Themed SplitButton
    
    * update snapshots

--- graylog2-web-interface/src/components/graylog/Button.jsx
+++ graylog2-web-interface/src/components/graylog/Button.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Button as BootstrapButton } from 'react-bootstrap';
 import styled from 'styled-components';
 
-import { useTheme } from 'theme/GraylogThemeContext';
-import buttonStyles from './styles/buttonStyles';
+import buttonStyles from './styles/button';
+import { propTypes, defaultProps } from './props/button';
 
 const Button = ({ active, bsStyle, ...props }) => {
-  const { colors, utility } = useTheme();
-
   const StyledButton = styled(BootstrapButton)`
-    ${buttonStyles({ colors, active, utility })};
+    ${buttonStyles({ active })};
   `;
 
   return (
-    <StyledButton active={active} bsStyle={bsStyle} {...props} />
+    <StyledButton bsStyle={bsStyle} {...props} />
   );
 };
 
-Button.propTypes = {
-  /* NOTE: need props so we can set default styles */
-  active: PropTypes.bool,
-  bsStyle: PropTypes.oneOf(['success', 'warning', 'danger', 'info', 'default', 'primary', 'link']),
-};
+Button.propTypes = propTypes;
 
-Button.defaultProps = {
-  active: false,
-  bsStyle: 'default',
-};
+Button.defaultProps = defaultProps;
 
 export default Button;
---
+++ graylog2-web-interface/src/components/graylog/SplitButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SplitButton as BootstrapSplitButton } from 'react-bootstrap';
+import styled from 'styled-components';
+
+import buttonStyles from './styles/button';
+import { propTypes, defaultProps } from './props/button';
+
+const SplitButton = ({ active, bsStyle, ...props }) => {
+  const StyledSplitButton = styled(BootstrapSplitButton)`
+    ${buttonStyles({ active })};
+
+    ~ .btn.dropdown-toggle {
+      ${buttonStyles({ active, specific: false })};
+    }
+  `;
+
+  return (
+    <StyledSplitButton bsStyle={bsStyle} {...props} />
+  );
+};
+
+SplitButton.propTypes = propTypes;
+
+SplitButton.defaultProps = defaultProps;
+
+export default SplitButton;
