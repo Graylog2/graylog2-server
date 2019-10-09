5d6134d29933fa41c3d95398c1a716958edbde41
Kyle Knight <kyle@graylog.com>
2019-10-09 07:47:22 -0500

    revert conditional

--- graylog2-web-interface/src/routing/App.jsx
+++ graylog2-web-interface/src/routing/App.jsx
@@ -17,7 +17,7 @@ import 'dc/dc.css';
 const CurrentUserStore = StoreProvider.getStore('CurrentUser');
 
 const App = ({ children, currentUser, location }) => {
-  if (Object.keys(currentUser).length === 0) {
+  if (!currentUser) {
     return <Spinner />;
   }
 
@@ -52,7 +52,7 @@ App.propTypes = {
 };
 
 App.defaultProps = {
-  currentUser: {},
+  currentUser: undefined,
 };
 
 export default connect(App, { currentUser: CurrentUserStore }, ({ currentUser: { currentUser } = {} }) => ({ currentUser }));
