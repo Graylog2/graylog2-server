a79ebe76419608783771224509054d1f447daf98
Kyle Knight <kyle@graylog.com>
2019-10-09 13:16:46 -0500

    remove commented code

--- graylog2-web-interface/src/components/graylog/Button.jsx
+++ graylog2-web-interface/src/components/graylog/Button.jsx
@@ -21,28 +21,3 @@ Button.propTypes = propTypes;
 Button.defaultProps = defaultProps;
 
 export default Button;
-
-
-// import React from 'react';
-// import { Button as BootstrapButton } from 'react-bootstrap';
-// import styled from 'styled-components';
-
-// import buttonStyles from './styles/button';
-// import { propTypes, defaultProps } from './props/button';
-
-// const Button = ({ active, bsStyle, innerRef, ...props }) => {
-//   const ref = React.useRef(innerRef);
-//   const StyledButton = styled(btnProps => <BootstrapButton {...btnProps} ref={ref} />)`
-//     ${buttonStyles({ active })};
-//   `;
-
-//   return (
-//     <StyledButton bsStyle={bsStyle} {...props} />
-//   );
-// };
-
-// Button.propTypes = propTypes;
-
-// Button.defaultProps = defaultProps;
-
-// export default Button;
