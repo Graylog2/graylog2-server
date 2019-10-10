3940bd25b2982342b1e32b0133c8c31ebf77420f
Kyle Knight <kyle@graylog.com>
2019-10-10 14:57:06 -0500

    Rewrite FieldList component

--- graylog2-web-interface/src/views/components/sidebar/FieldList.jsx
+++ graylog2-web-interface/src/views/components/sidebar/FieldList.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import Reflux from 'reflux';
-import createReactClass from 'create-react-class';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { is } from 'immutable';
-import { isEqual } from 'lodash';
 import { FixedSizeList as List } from 'react-window';
 
+import connect from 'stores/connect';
 import { Button } from 'components/graylog';
 import Field from 'views/components/Field';
 import FieldTypeIcon from 'views/components/sidebar/FieldTypeIcon';
@@ -16,62 +13,87 @@ import styles from './FieldList.css';
 
 const isReservedField = fieldName => MessageFieldsFilter.FILTERED_FIELDS.includes(fieldName);
 
-const FieldList = createReactClass({
-  propTypes: {
-    allFields: PropTypes.object.isRequired,
-    listHeight: PropTypes.number,
-    fields: PropTypes.object.isRequired,
-  },
+export const FieldItem = ({ fields, fieldType: { name, type }, selectedQuery, selectedView, style }) => {
+  const disabled = !fields.find(f => f.name === name);
 
-  mixins: [Reflux.connect(ViewMetadataStore, 'viewMetadata')],
+  return (
+    <li key={`field-${name}`} className={styles.fieldListItem} style={style}>
+      <FieldTypeIcon type={type} />
+      {' '}
+      <Field queryId={selectedQuery}
+             viewId={selectedView}
+             disabled={disabled}
+             name={name}
+             type={type}
+             interactive>
+        {name}
+      </Field>
+    </li>
+  );
+};
 
-  getDefaultProps() {
-    return {
-      listHeight: 50,
-    };
-  },
+FieldItem.propTypes = {
+  fields: PropTypes.object.isRequired,
+  fieldType: PropTypes.object.isRequired,
+  selectedQuery: PropTypes.string.isRequired,
+  selectedView: PropTypes.string,
+  style: PropTypes.any.isRequired,
+};
 
-  getInitialState() {
-    return {
-      filter: undefined,
-      showFieldsBy: 'current',
-    };
-  },
+FieldItem.defaultProps = {
+  selectedView: undefined,
+};
 
-  shouldComponentUpdate(nextProps, nextState) {
-    const { allFields, fields, listHeight } = this.props;
-    if (!isEqual(nextProps.listHeight, listHeight)) {
-      return true;
-    }
+export const FieldsByLink = ({ mode, text, title, onChange, showFieldsBy }) => {
+  const isCurrentShowFieldsBy = showFieldsBy === mode;
 
-    if (!isEqual(this.state, nextState)) {
-      return true;
-    }
-    return !is(nextProps.allFields, allFields) || !is(nextProps.fields, fields);
-  },
+  return (
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events
+    <a onClick={() => onChange(mode)}
+       role="button"
+       tabIndex={0}
+       title={title}
+       style={{ fontWeight: isCurrentShowFieldsBy ? 'bold' : 'normal' }}>
+      {text}
+    </a>
+  );
+};
 
-  _renderField({ fields, fieldType, selectedQuery, selectedView, style }) {
-    const { name, type } = fieldType;
-    const disabled = !fields.find(f => f.name === name);
+FieldsByLink.propTypes = {
+  mode: PropTypes.string.isRequired,
+  showFieldsBy: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
-    return (
-      <li key={`field-${name}`} className={styles.fieldListItem} style={style}>
-        <FieldTypeIcon type={type} />
-        {' '}
-        <Field queryId={selectedQuery}
-               viewId={selectedView}
-               disabled={disabled}
-               name={name}
-               type={type}
-               interactive>
-          {name}
-        </Field>
-      </li>
-    );
-  },
 
-  _fieldsToShow(fields, allFields, showFieldsBy = 'all') {
+// eslint-disable-next-line react/prop-types
+const FieldListRow = ({ fieldList, fields, selectedQuery, selectedView }) => ({ index, style }) => {
+  return (
+    <FieldItem fieldType={fieldList.get(index)}
+               selectedQuery={selectedQuery}
+               selectedView={selectedView}
+               fields={fields}
+               style={style} />
+  );
+};
+
+FieldListRow.propTypes = {
+  fieldList: PropTypes.object.isRequired,
+  selectedQuery: PropTypes.string.isRequired,
+  selectedView: PropTypes.string,
+  fields: PropTypes.object.isRequired,
+};
+
+FieldListRow.defaultProps = {
+  selectedView: undefined,
+};
+
+export const FieldListWrap = ({ fields, allFields, showFieldsBy, viewMetadata, filter }) => {
+  const fieldsToShow = () => {
     const isNotReservedField = f => !isReservedField(f.name);
+
     switch (showFieldsBy) {
       case 'all':
         return allFields.filter(isNotReservedField);
@@ -81,104 +103,122 @@ const FieldList = createReactClass({
       default:
         return fields.filter(isNotReservedField);
     }
-  },
+  };
 
-  _renderFieldList({ fields, allFields, showFieldsBy }) {
-    const {
-      filter,
-      viewMetadata: {
-        id: selectedView,
-        activeQuery: selectedQuery,
-      },
-    } = this.state;
-    const { listHeight } = this.props;
+  const {
+    id: selectedView,
+    activeQuery: selectedQuery,
+  } = viewMetadata;
 
-    if (!fields) {
-      return <span>No field information available.</span>;
-    }
-    const fieldFilter = filter ? (field => field.name.toLocaleUpperCase().includes(filter.toLocaleUpperCase())) : () => true;
-    const fieldsToShow = this._fieldsToShow(fields, allFields, showFieldsBy);
-    const fieldList = fieldsToShow
-      .filter(fieldFilter)
-      .sortBy(field => field.name.toLocaleUpperCase());
+  if (!fields) {
+    return <span>No field information available.</span>;
+  }
+  const fieldFilter = filter ? (field => field.name.toLocaleUpperCase().includes(filter.toLocaleUpperCase())) : () => true;
+  const fieldList = fieldsToShow().filter(fieldFilter).sortBy(field => field.name.toLocaleUpperCase());
 
-    if (fieldList.isEmpty()) {
-      return <i>No fields to show. Try changing your filter term or select a different field set above.</i>;
-    }
-    const Row = ({ index, style }) => this._renderField({ fieldType: fieldList.get(index), selectedQuery, selectedView, fields, style });
+  if (fieldList.isEmpty()) {
+    return <i>No fields to show. Try changing your filter term or select a different field set above.</i>;
+  }
 
-    return (
-      <div ref={(elem) => { this.fieldList = elem; }}>
-        <List height={listHeight}
-              itemCount={fieldList.size}
-              itemSize={17}>
-          {Row}
-        </List>
-      </div>
-    );
-  },
-  handleSearch(e) {
-    const filter = e.target.value;
-    this.setState({ filter });
-  },
-  handleSearchReset() {
-    this.setState({ filter: undefined });
-  },
-  changeShowFieldsBy(mode) {
-    this.setState({ showFieldsBy: mode });
-  },
-  isCurrentShowFieldsBy(mode) {
-    const { showFieldsBy } = this.state;
+  return (
+    <div>
+      <List height={50}
+            itemCount={fieldList.size}
+            itemSize={17}>
+        {FieldListRow({ fieldList, fields, selectedQuery, selectedView })}
+      </List>
+    </div>
+  );
+};
 
-    return showFieldsBy === mode;
-  },
-  showFieldsByLink(mode, text, title) {
-    return (
-      // eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events
-      <a onClick={() => this.changeShowFieldsBy(mode)}
-         role="button"
-         tabIndex={0}
-         title={title}
-         style={{ fontWeight: this.isCurrentShowFieldsBy(mode) ? 'bold' : 'normal' }}>
-        {text}
-      </a>
-    );
-  },
-  render() {
-    const { allFields, fields } = this.props;
-    const { filter, showFieldsBy } = this.state;
+FieldListWrap.propTypes = {
+  fields: PropTypes.object.isRequired,
+  allFields: PropTypes.object.isRequired,
+  showFieldsBy: PropTypes.string,
+  viewMetadata: PropTypes.object.isRequired,
+  filter: PropTypes.string,
+};
 
-    return (
-      <div style={{ whiteSpace: 'break-spaces' }}>
-        <form className={`form-inline ${styles.filterContainer}`} onSubmit={e => e.preventDefault()}>
-          <div className={`form-group has-feedback ${styles.filterInputContainer}`}>
-            <input id="common-search-form-query-input"
-                   className="query form-control"
-                   style={{ width: '100%' }}
-                   onChange={this.handleSearch}
-                   value={filter || ''}
-                   placeholder="Filter fields"
-                   type="text"
-                   autoComplete="off"
-                   spellCheck="false" />
-          </div>
-          <div className="form-group">
-            <Button type="reset" className="reset-button" onClick={this.handleSearchReset}>
-              Reset
-            </Button>
-          </div>
-        </form>
-        <div style={{ marginTop: '5px', marginBottom: '0px' }}>
-          List fields of{' '}
-          {this.showFieldsByLink('current', 'current streams', 'This shows fields which are (prospectively) included in the streams you have selected.')},{' '}
-          {this.showFieldsByLink('all', 'all', 'This shows all fields, but no reserved (gl2_*) fields.')} or{' '}
-          {this.showFieldsByLink('allreserved', 'all including reserved', 'This shows all fields, including reserved (gl2_*) fields.')} fields.
+FieldListWrap.defaultProps = {
+  filter: undefined,
+  showFieldsBy: 'all',
+};
+
+const FieldList = ({ fields, allFields, viewMetadata }) => {
+  console.log('viewMetadata', viewMetadata);
+  const [filter, setFilter] = useState(undefined);
+  const [showFieldsBy, setShowFieldsBy] = useState('current');
+  const filterFieldInputRef = useRef();
+
+  const handleSearch = () => {
+    setFilter(filterFieldInputRef.current.value);
+  };
+
+  const handleSearchReset = () => {
+    setFilter(undefined);
+  };
+
+  const changeShowFieldsBy = (mode) => {
+    setShowFieldsBy(mode);
+  };
+
+  return (
+    <div style={{ whiteSpace: 'break-spaces' }}>
+      <form className={`form-inline ${styles.filterContainer}`} onSubmit={e => e.preventDefault()}>
+        <div className={`form-group has-feedback ${styles.filterInputContainer}`}>
+          <input id="common-search-form-query-input"
+                 className="query form-control"
+                 ref={filterFieldInputRef}
+                 style={{ width: '100%' }}
+                 onChange={handleSearch}
+                 value={filter || ''}
+                 placeholder="Filter fields"
+                 type="text"
+                 autoComplete="off"
+                 spellCheck="false" />
         </div>
-        <hr />
-        {this._renderFieldList({ fields, allFields, showFieldsBy })}
-      </div>
-    );
-  },
-});
+        <div className="form-group">
+          <Button type="reset" className="reset-button" onClick={handleSearchReset}>
+            Reset
+          </Button>
+        </div>
+      </form>
 
-export default FieldList;
+      <div style={{ marginTop: '5px', marginBottom: '0px' }}>
+        List fields of{' '}
+        <FieldsByLink mode="current"
+                      text="current streams"
+                      title="This shows fields which are (prospectively) included in the streams you have selected."
+                      onChange={changeShowFieldsBy}
+                      showFieldsBy={showFieldsBy} />,{' '}
+
+        <FieldsByLink mode="all"
+                      text="all"
+                      title="This shows all fields, but no reserved (gl2_*) fields."
+                      onChange={changeShowFieldsBy}
+                      showFieldsBy={showFieldsBy} /> or{' '}
+
+        <FieldsByLink mode="allreserved"
+                      text="all including reserved"
+                      title="This shows all fields, including reserved (gl2_*) fields."
+                      onChange={changeShowFieldsBy}
+                      showFieldsBy={showFieldsBy} />
+      </div>
+
+      <hr />
+
+      <FieldListWrap fields={fields} allFields={allFields} showFieldsBy={showFieldsBy} viewMetadata={viewMetadata} filter={filter} />
+    </div>
+  );
+};
+
+FieldList.propTypes = {
+  allFields: PropTypes.object.isRequired,
+  fields: PropTypes.object.isRequired,
+  viewMetadata: PropTypes.shape({
+    id: PropTypes.number,
+    activeQuery: PropTypes.string,
+  }).isRequired,
+};
+
+export default connect(FieldList, { viewMetadata: ViewMetadataStore }, ({ viewMetadata: { viewMetadata } = {} }) => ({ viewMetadata }));
