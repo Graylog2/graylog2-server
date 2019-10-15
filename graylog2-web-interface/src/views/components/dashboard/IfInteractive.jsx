cb87463d6884d2fe32fdd9fa20f57c6f1b7ed3c4
Dennis Oelkers <dennis@graylog.com>
2019-10-15 15:47:24 +0200

    Adding context indicating if being used interactively or passively.

---
+++ graylog2-web-interface/src/views/components/dashboard/IfInteractive.jsx
@@ -0,0 +1,10 @@
+// @flow strict
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+const IfInteractive = () => {
+};
+
+IfInteractive.propTypes = {};
+
+export default IfInteractive;
