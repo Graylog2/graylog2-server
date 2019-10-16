7900b2f9d72c79ec7e4f8886d80479e25416a296
Konrad Merz <konrad@graylog.com>
2019-10-16 14:38:19 +0200

    Refactor with tipps from @dennisoelkers

--- graylog2-web-interface/src/views/logic/views/ViewTransformer.js
+++ graylog2-web-interface/src/views/logic/views/ViewTransformer.js
@@ -1,22 +1,25 @@
 // @flow strict
 import { List, Map } from 'immutable';
 import Widget from 'views/logic/widgets/Widget';
+import type { QueryId } from 'views/logic/queries/Query';
+import Query from 'views/logic/queries/Query';
 
 import type { ViewStateMap } from './View';
 import View from './View';
 import ViewState from './ViewState';
 
 const ViewTransformer = (searchView: View): View => {
+  const queryMap: Map<QueryId, Query> = Map(searchView.search.queries.map(q => [q.id, q]));
   const newViewStateMap: ViewStateMap = (searchView.state || Map.of()).map((viewState: ViewState, queryId: string) => {
-    const { timerange, query, filter = List.of() } = searchView.search.queries.find(q => q.id === queryId);
+    const { timerange, query, filter = List.of() } = queryMap.get(queryId);
+
+    const streams = filter.get('filters', List.of())
+      .filter(value => Map.isMap(value) && value.get('type') === 'stream')
+      .map(value => value.get('id'))
+      .toList()
+      .toArray();
 
     const widgets: List<Widget> = viewState.widgets.map((widget: Widget) => {
-      const streams = filter.get('filters', List.of())
-        .filter(value => Map.isMap(value) && value.get('type') === 'stream')
-        .map(value => value.get('id'))
-        .toList()
-        .toArray();
-
       return widget.toBuilder()
         .timerange(timerange)
         .query(query)
