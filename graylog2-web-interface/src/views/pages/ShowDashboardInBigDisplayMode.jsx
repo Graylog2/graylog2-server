4531f445f0a1013a647be53f7f3954ffa20b0a5b
Dennis Oelkers <dennis@graylog.com>
2019-10-16 13:51:31 +0200

    Use components to prepare big display mode view.

--- graylog2-web-interface/src/views/pages/ShowDashboardInBigDisplayMode.jsx
+++ graylog2-web-interface/src/views/pages/ShowDashboardInBigDisplayMode.jsx
@@ -1,6 +1,18 @@
 // @flow strict
 import * as React from 'react';
 import { withRouter } from 'react-router';
+import styled from 'styled-components';
+
+import connect from 'stores/connect';
+import InteractiveContext from 'views/components/contexts/InteractiveContext';
+import { ViewStore } from 'views/stores/ViewStore';
+import { ViewMetadataStore } from 'views/stores/ViewMetadataStore';
+import { CurrentQueryStore } from 'views/stores/CurrentQueryStore';
+import BigDisplayModeHeader from 'views/components/dashboard/BigDisplayModeHeader';
+import CycleQueryTab from 'views/components/dashboard/bigdisplay/CycleQueryTab';
+import type { QueryId } from 'views/logic/queries/Query';
+import View from 'views/logic/views/View';
+import ShowViewPage from './ShowViewPage';
 
 export type BigDisplayModeQuery = {|
   tabs?: Array<number>,
@@ -18,6 +30,12 @@ type Props = {
   location: {
     query: UntypedBigDisplayModeQuery,
   },
+  params: any,
+  route: any,
+  view: {
+    view: ?View,
+    activeQuery: ?QueryId,
+  },
 };
 
 const castQueryWithDefaults = ({ tabs, interval, refresh }: UntypedBigDisplayModeQuery): BigDisplayModeQuery => ({
@@ -26,8 +44,23 @@ const castQueryWithDefaults = ({ tabs, interval, refresh }: UntypedBigDisplayMod
   refresh: refresh !== undefined ? Number.parseInt(refresh, 10) : undefined,
 });
 
-const ShowDashboardInBigDisplayMode = ({ location: { query } }: Props) => (
-  <div>{JSON.stringify(castQueryWithDefaults(query))}</div>
-);
+const BodyPositioningWrapper = styled.div`
+  margin-top: -45px;
+  padding: 10px;
+`;
 
-export default withRouter(ShowDashboardInBigDisplayMode);
+const ShowDashboardInBigDisplayMode = ({ location, params, route, view: { view, activeQuery } }: Props) => {
+  const { query } = location;
+  const configuration = castQueryWithDefaults(query);
+  return (
+    <InteractiveContext.Provider value={false}>
+      <BodyPositioningWrapper>
+        {view && activeQuery ? <CycleQueryTab interval={configuration.interval || 30} view={view} activeQuery={activeQuery} tabs={configuration.tabs} /> : null}
+        <BigDisplayModeHeader />
+        <ShowViewPage location={location} params={params} route={route} />
+      </BodyPositioningWrapper>
+    </InteractiveContext.Provider>
+  );
+};
+
+export default withRouter(connect(ShowDashboardInBigDisplayMode, { view: ViewStore, viewMetadata: ViewMetadataStore, query: CurrentQueryStore }));
