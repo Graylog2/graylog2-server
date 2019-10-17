a4b450fc7795780fb2879137f14ffb52ba59724d
Alex Konn <alexander@graylog.com>
2019-10-17 18:39:02 +0200

    Create Unit Tests for Adding Default Streams to Search

--- graylog2-server/src/test/java/org/graylog/plugins/views/search/rest/SearchResourceStreamPermissionsTest.java
+++ graylog2-server/src/test/java/org/graylog/plugins/views/search/rest/SearchResourceStreamPermissionsTest.java
@@ -17,12 +17,10 @@
 package org.graylog.plugins.views.search.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import org.apache.shiro.subject.Subject;
 import org.graylog.plugins.views.search.Query;
 import org.graylog.plugins.views.search.Search;
-import org.graylog.plugins.views.search.SearchJob;
 import org.graylog.plugins.views.search.authorization.SearchAuthorizer;
 import org.graylog.plugins.views.search.db.SearchDbService;
 import org.graylog.plugins.views.search.db.SearchJobService;
@@ -30,33 +28,26 @@ import org.graylog.plugins.views.search.engine.BackendQuery;
 import org.graylog.plugins.views.search.engine.QueryEngine;
 import org.graylog2.plugin.database.users.User;
 import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
-import org.graylog2.plugin.streams.Stream;
 import org.graylog2.shared.bindings.GuiceInjectorHolder;
 import org.graylog2.shared.bindings.providers.ObjectMapperProvider;
-import org.graylog2.shared.security.RestPermissions;
 import org.graylog2.streams.StreamService;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import javax.annotation.Nullable;
 import javax.ws.rs.ForbiddenException;
-import javax.ws.rs.core.Response;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -116,51 +107,6 @@ public class SearchResourceStreamPermissionsTest {
         GuiceInjectorHolder.createInjector(Collections.emptyList());
 
         this.searchResource = new SearchTestResource(subject, queryEngine, searchDbService, searchJobService, objectMapperProvider.get(), streamService);
-    }
-
-    private List<Stream> allStreamsOfUser() {
-        final Stream stream1 = mock(Stream.class);
-        when(stream1.getId()).thenReturn("stream1id");
-        when(subject.isPermitted(RestPermissions.STREAMS_READ + ":stream1id")).thenReturn(true);
-        final Stream stream2 = mock(Stream.class);
-        when(stream2.getId()).thenReturn("stream2id");
-        when(subject.isPermitted(RestPermissions.STREAMS_READ + ":stream2id")).thenReturn(true);
-        final Stream stream3 = mock(Stream.class);
-        when(stream3.getId()).thenReturn("stream3id");
-        when(subject.isPermitted(RestPermissions.STREAMS_READ + ":stream3id")).thenReturn(true);
-
-        return ImmutableList.of(stream1, stream2, stream3);
-    }
-
-    @Test
-    public void executingSearchWithoutStreamsUsesAllStreamsOfUser() {
-        final String queryId = "someQuery";
-        when(query.usedStreamIds()).thenReturn(ImmutableSet.of());
-        when(query.toBuilder()).thenReturn(Query.builder().id(queryId).query(new BackendQuery.Fallback()).timerange(mock(RelativeRange.class)));
-        when(query.id()).thenReturn(queryId);
-
-        final String searchId = "searchId";
-        final Search search = Search.Builder.create().id(searchId).queries(ImmutableSet.of(query)).build();
-        when(searchDbService.getForUser(eq(searchId), any(), any())).thenReturn(Optional.of(search));
-        final SearchJob searchJob = mock(SearchJob.class);
-        when(searchJobService.create(any(Search.class), any(String.class))).thenReturn(searchJob);
-        when(queryEngine.execute(searchJob)).thenReturn(searchJob);
-        when(searchJob.getId()).thenReturn("searchJobId");
-
-        final List<Stream> userStreams = allStreamsOfUser();
-        when(streamService.loadAll()).thenReturn(userStreams);
-
-        final Response response = searchResource.executeQuery(searchId, Collections.emptyMap());
-
-        assertThat(response.getStatusInfo().getFamily()).isEqualTo(Response.Status.Family.SUCCESSFUL);
-
-        final ArgumentCaptor<Search> searchCaptor = ArgumentCaptor.forClass(Search.class);
-        verify(searchJobService, times(1)).create(searchCaptor.capture(), any());
-        final Search modifiedSearch = searchCaptor.getValue();
-
-        final Optional<Query> modifiedQuery = modifiedSearch.getQuery(queryId);
-        assertThat(modifiedQuery).isPresent();
-        assertThat(modifiedQuery.get().usedStreamIds()).containsExactlyInAnyOrder("stream1id", "stream2id", "stream3id");
     }
 
     @Test
