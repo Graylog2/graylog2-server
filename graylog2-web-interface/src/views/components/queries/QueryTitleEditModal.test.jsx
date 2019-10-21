3bc503555379762dadf5f7786759d9892128e36f
linuspahl <lpahl@protonmail.com>
2019-10-21 12:34:28 +0200

    Add missing test cases for QueryTitleEditModal

--- graylog2-web-interface/src/views/components/queries/QueryTitleEditModal.test.jsx
+++ graylog2-web-interface/src/views/components/queries/QueryTitleEditModal.test.jsx
@@ -1,6 +1,6 @@
 // @flow strict
 import * as React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup, fireEvent, wait } from '@testing-library/react';
 
 import QueryTitleEditModal from './QueryTitleEditModal';
 
@@ -9,6 +9,7 @@ jest.mock('views/stores/ViewStore', () => ({ ViewActions: {} }));
 
 describe('QueryTitleEditModal', () => {
   afterEach(cleanup);
+  const modalHeadline = 'Edit query title';
   const openModal = (modalRef, currentTitle = 'CurrentTitle') => {
     if (modalRef) {
       modalRef.open(currentTitle);
@@ -22,10 +23,10 @@ describe('QueryTitleEditModal', () => {
                            onTitleChange={() => Promise.resolve()} />,
     );
     // Modal should not be visible initially
-    expect(queryByText('Edit query title')).toBeNull();
+    expect(queryByText(modalHeadline)).toBeNull();
     openModal(modalRef);
     // Modal should be visible
-    expect(queryByText('Edit query title')).not.toBeNull();
+    expect(queryByText(modalHeadline)).not.toBeNull();
   });
 
   it('has correct initial input value', () => {
@@ -38,10 +39,10 @@ describe('QueryTitleEditModal', () => {
     expect(getByDisplayValue('CurrentTitle')).not.toBeNull();
   });
 
-  it('updates query title and closes', () => {
+  it('updates query title and closes', async () => {
     let modalRef;
     const onTitleChangeFn = jest.fn();
-    const { getByDisplayValue, getByText } = render(
+    const { getByDisplayValue, getByText, queryByText } = render(
       <QueryTitleEditModal ref={(ref) => { modalRef = ref; }}
                            onTitleChange={onTitleChangeFn} />,
     );
@@ -52,26 +53,27 @@ describe('QueryTitleEditModal', () => {
     fireEvent.click(saveButton);
     expect(onTitleChangeFn).toHaveBeenCalledTimes(1);
     expect(onTitleChangeFn).toHaveBeenCalledWith('NewTitle');
-    // TODO: Fix this test case, right now the modal is still visible
-    // // Modal should not be visible anymore
-    // expect(queryByText('Edit query title')).toBeNull();
+    // Modal should not be visible anymore
+    await wait(() => {
+      expect(queryByText(modalHeadline)).toBeNull();
+    });
   });
 
-  // TODO: Fix this test case
-  //   it('closes on click on cancel', async () => {
-  //     let modalRef;
-  //     const onTitleChangeFn = jest.fn();
-  //     const { getByText, queryByText } = render(
-  //       <QueryTitleEditModal ref={(ref) => { modalRef = ref; }}
-  //                            onTitleChange={onTitleChangeFn} />,
-  //     );
-  //     openModal(modalRef);
-  //     // Modal should be visible
-  //     expect(queryByText('Edit query title')).not.toBeNull();
-  //     // Modal should not be visible after click on cancel
-  //     const canselButton = getByText('Cancel');
-  //     fireEvent.click(canselButton);
-  //     await wait();
-  //     expect(queryByText('Edit query title')).toBeNull();
-  //   });
+  it('closes on click on cancel', async () => {
+    let modalRef;
+    const onTitleChangeFn = jest.fn();
+    const { getByText, queryByText } = render(
+      <QueryTitleEditModal ref={(ref) => { modalRef = ref; }}
+                           onTitleChange={onTitleChangeFn} />,
+    );
+    openModal(modalRef);
+    // Modal should be visible
+    expect(queryByText(modalHeadline)).not.toBeNull();
+    // Modal should not be visible after click on cancel
+    const canselButton = getByText('Cancel');
+    fireEvent.click(canselButton);
+    await wait(() => {
+      expect(queryByText(modalHeadline)).toBeNull();
+    });
+  });
 });
