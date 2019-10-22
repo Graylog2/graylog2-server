7d45037bb5dfe8d4b493f836812947d0af9ea0d6
Kay Roepke <kroepke@googlemail.com>
2019-10-22 16:56:02 +0200

    Add metric_counter pipeline function

---
+++ graylog2-server/src/main/java/org/graylog/plugins/pipelineprocessor/functions/debug/MetricCounter.java
@@ -0,0 +1,50 @@
+package org.graylog.plugins.pipelineprocessor.functions.debug;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.MetricRegistry;
+import com.google.common.collect.ImmutableList;
+import org.graylog.plugins.pipelineprocessor.EvaluationContext;
+import org.graylog.plugins.pipelineprocessor.ast.functions.AbstractFunction;
+import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionArgs;
+import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionDescriptor;
+import org.graylog.plugins.pipelineprocessor.ast.functions.ParameterDescriptor;
+
+import javax.inject.Inject;
+
+public class MetricCounter extends AbstractFunction<Void> {
+
+    public static final String NAME = "metric_counter";
+    private final ParameterDescriptor<String, Counter> nameParam;
+    private final ParameterDescriptor<Long, Long> valueParam;
+
+    @Inject
+    public MetricCounter(MetricRegistry metricRegistry) {
+        nameParam = ParameterDescriptor.string("name", Counter.class)
+                .description("The counter metric name, will always be prefixed with 'org.graylog.rulemetrics.'")
+                .transform(name -> metricRegistry.counter(MetricRegistry.name("org.graylog.rulemetrics", name)))
+                .build();
+        valueParam = ParameterDescriptor.integer("value")
+                .description("Value to increment the counter by")
+                .optional()
+                .build();
+    }
+
+    @Override
+    public Void evaluate(FunctionArgs args, EvaluationContext context) {
+        final Counter counter = nameParam.required(args, context);
+        final Long aLong = valueParam.optional(args, context).orElse(1L);
+        //noinspection ConstantConditions
+        counter.inc(aLong);
+        return null;
+    }
+
+    @Override
+    public FunctionDescriptor<Void> descriptor() {
+        return FunctionDescriptor.<Void>builder()
+                .name(NAME)
+                .returnType(Void.class)
+                .description("")
+                .params(ImmutableList.of(nameParam, valueParam))
+                .build();
+    }
+}
