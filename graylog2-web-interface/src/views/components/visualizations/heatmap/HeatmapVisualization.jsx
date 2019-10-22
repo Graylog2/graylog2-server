4313a5c7e6e5c58397e0eb6d2ffbc2152a52abb2
linuspahl <lpahl@protonmail.com>
2019-10-22 13:53:50 +0200

    Remove not needed logic for heatmap component

--- graylog2-web-interface/src/views/components/visualizations/heatmap/HeatmapVisualization.jsx
+++ graylog2-web-interface/src/views/components/visualizations/heatmap/HeatmapVisualization.jsx
@@ -9,26 +9,15 @@ import type { ChartDefinition } from '../ChartData';
 import { chartData } from '../ChartData';
 import XYPlot from '../XYPlot';
 
-const chartGenerator = (type, name, labels, values): ChartDefinition => ({ type, name, x: labels, y: values, line: { shape: 'linear' } });
+const chartGenerator = (type, name, labels, values): ChartDefinition => ({ type, name, x: labels, y: values });
 
-const getChartColor = (fullData, name) => {
-  const data = fullData.find(d => (d.name === name));
-  if (data && data.line && data.line.color) {
-    const { line: { color } } = data;
-    return color;
-  }
-  return undefined;
+const HeatmapVisualization: VisualizationComponent = ({ config, data, effectiveTimerange }: VisualizationComponentProps) => {
+  return (
+    <XYPlot config={config}
+            effectiveTimerange={effectiveTimerange}
+            chartData={chartData(config, data, 'heatmap', chartGenerator)} />
+  );
 };
-
-const setChartColor = (chart, colors) => ({ line: { color: colors[chart.name] } });
-
-const HeatmapVisualization: VisualizationComponent = ({ config, data, effectiveTimerange }: VisualizationComponentProps) => (
-  <XYPlot config={config}
-          effectiveTimerange={effectiveTimerange}
-          getChartColor={getChartColor}
-          setChartColor={setChartColor}
-          chartData={chartData(config, data, 'heatmap', chartGenerator)} />
-);
 
 HeatmapVisualization.propTypes = {
   config: AggregationType.isRequired,
