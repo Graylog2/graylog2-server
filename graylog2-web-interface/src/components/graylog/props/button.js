508bda7d1680f5be069f2bd25990dede1566e1b6
Kyle Knight <kyle@graylog.com>
2019-10-23 14:40:23 -0500

    Updating props and snapshots

--- graylog2-web-interface/src/components/graylog/props/button.js
+++ graylog2-web-interface/src/components/graylog/props/button.js
@@ -1,13 +1,10 @@
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  /* NOTE: need props so we can set default styles */
-  active: PropTypes.bool,
   bsStyle: PropTypes.oneOf(['success', 'warning', 'danger', 'info', 'default', 'primary', 'link']),
 };
 
 const defaultProps = {
-  active: false,
   bsStyle: 'default',
 };
 
