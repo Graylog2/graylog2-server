1de85936e495d7088300913cb6a215bd6a1f1c11
Dennis Oelkers <dennis@graylog.com>
2019-10-24 13:46:33 +0200

    Improving `UserPreferencesButton` tests usage of mocks and snapshots. (#6568)
    * Improving `UserPreferencesButton` tests usage of mocks and snapshots.
    
    Before this change, the `UserPreferencesButton` test was snapshotting
    the instance returned from testing-library directly, including a lot of
    internal structure in the code which is likely to change and therefore
    break the test in future versions of the library. This change is fixing
    this. Additionally, it removes a lot of unnecessary boiler plate.
    
    * Updating snapshot.

--- graylog2-web-interface/src/components/users/UserPreferencesButton.test.jsx
+++ graylog2-web-interface/src/components/users/UserPreferencesButton.test.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
-import { CombinedProviderMock as MockCombinedProvider, StoreMock as MockStore } from 'helpers/mocking';
-
+import { StoreMock as MockStore } from 'helpers/mocking';
 import UserPreferencesButton from 'components/users/UserPreferencesButton';
+import PreferencesStore from 'stores/users/PreferencesStore';
 
-import StoreProvider from 'injection/StoreProvider';
-
-jest.mock('injection/CombinedProvider', () => {
-  const mockPreferencesStore = MockStore('get', 'listen', 'loadUserPreferences');
-  const combinedProviderMock = new MockCombinedProvider({
-    Preferences: { PreferencesStore: mockPreferencesStore },
-  });
-
-  return combinedProviderMock;
-});
-
-const PreferencesStore = StoreProvider.getStore('Preferences');
+jest.mock('stores/users/PreferencesStore', () => MockStore('get', 'listen', 'loadUserPreferences'));
 
 describe('UserPreferencesButton', () => {
-  beforeEach(() => {
-    jest.resetModules();
-  });
-
   it('should load user data when user clicks edit button', () => {
     const instance = render(<UserPreferencesButton userName="Full" />);
     const button = instance.getByTestId('user-preferences-button');
 
-    expect(instance).toMatchSnapshot();
+    expect(instance.container).toMatchSnapshot();
     expect(button).toBeDefined();
 
     fireEvent.click(button);
