157c9d1b1f177f7993d8ef36212428dc60b9dc38
Bernd Ahlers <bernd@users.noreply.github.com>
2019-10-28 17:59:46 +0100

    Implement pipeline rule timers that can be enabled/disabled at runtime (#6293)
    * Implement pipeline rule timers that can be enabled/disabled at runtime
    
    Not having timers for pipeline rules makes it really hard to debug
    performance problems. This change adds a new interpreter listener that
    tracks rule evaluation ("when" statement) and execution ("then"
    statement) timers.
    
    Since tracking timers for lots of rules can be impact processing
    performance, the timers are disabled by default. Users can toggle the
    rule timers on the rules management page.
    
    Example metrics:
    
    org.graylog.plugins.pipelineprocessor.ast.Rule.5d5161cb2c9287782b2826d5.trace.evaluate.duration
    org.graylog.plugins.pipelineprocessor.ast.Rule.5d5161cb2c9287782b2826d5.trace.execute.duration
    
    Closes #5494
    
    * Add audit event for updating rule metrics config
    
    * Switch alert style to "warning" and improve text
    
    * Listen to RulesStore instead of manually maintaining state
    
    * Refactor UI to open metrics config to be a single button
    
    Also name and color the button differently if rule metrics are enabled.
    
    * Also create spearate timers for each pipline stage
    
    * Add missing semicolon in RuleMetricsConfig component

---
+++ graylog2-server/src/main/java/org/graylog/plugins/pipelineprocessor/db/RuleMetricsConfigService.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog.plugins.pipelineprocessor.db;
+
+import org.graylog.plugins.pipelineprocessor.events.RuleMetricsConfigChangedEvent;
+import org.graylog2.events.ClusterEventBus;
+import org.graylog2.plugin.cluster.ClusterConfigService;
+
+import javax.inject.Inject;
+
+public class RuleMetricsConfigService {
+    private final ClusterConfigService clusterConfigService;
+    private final ClusterEventBus clusterEventBus;
+
+    @Inject
+    public RuleMetricsConfigService(ClusterConfigService clusterConfigService,
+                                    ClusterEventBus clusterEventBus) {
+        this.clusterConfigService = clusterConfigService;
+        this.clusterEventBus = clusterEventBus;
+    }
+
+    public RuleMetricsConfigDto save(RuleMetricsConfigDto config) {
+        clusterConfigService.write(config);
+        clusterEventBus.post(RuleMetricsConfigChangedEvent.create(config.metricsEnabled()));
+        return get();
+    }
+
+    public RuleMetricsConfigDto get() {
+        return clusterConfigService.getOrDefault(RuleMetricsConfigDto.class, RuleMetricsConfigDto.createDefault());
+    }
+}
---
+++ graylog2-web-interface/src/components/rules/RuleMetricsConfigContainer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import connect from 'stores/connect';
+import CombinedProvider from 'injection/CombinedProvider';
+import RuleMetricsConfig from './RuleMetricsConfig';
+
+const { RulesStore, RulesActions } = CombinedProvider.get('Rules');
+
+class RuleMetricsConfigContainer extends React.Component {
+  static propTypes = {
+    metricsConfig: PropTypes.object,
+    onClose: PropTypes.func,
+  };
+
+  static defaultProps = {
+    metricsConfig: undefined,
+    onClose: () => {},
+  };
+
+  componentDidMount() {
+    RulesActions.loadMetricsConfig();
+  }
+
+  handleChange = (nextConfig) => {
+    return RulesActions.updateMetricsConfig(nextConfig);
+  };
+
+  render() {
+    const { metricsConfig, onClose } = this.props;
+
+    if (!metricsConfig) {
+      return null;
+    }
+
+    return (
+      <RuleMetricsConfig ref={(component) => { this.configComponent = component; }}
+                         config={metricsConfig}
+                         onChange={this.handleChange}
+                         onClose={onClose} />
+    );
+  }
+}
+
+export default connect(RuleMetricsConfigContainer, { rules: RulesStore }, ({ rules }) => {
+  return { metricsConfig: rules ? rules.metricsConfig : rules };
+});
