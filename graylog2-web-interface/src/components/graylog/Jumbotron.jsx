39137d51ee2c01644fe99bbdc67c9180a0db2b76
Kyle Knight <kyle@graylog.com>
2019-10-28 13:12:33 -0500

    Jumbotron Component

---
+++ graylog2-web-interface/src/components/graylog/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React, { forwardRef, useCallback } from 'react';
+import styled from 'styled-components';
+// eslint-disable-next-line no-restricted-imports
+import { Jumbotron as BootstrapJumbotron } from 'react-bootstrap';
+
+import { useTheme } from 'theme/GraylogThemeContext';
+
+const Jumbotron = forwardRef((props, ref) => {
+  const { colors } = useTheme();
+  const StyledJumbotron = useCallback(styled(BootstrapJumbotron)`
+    color: ${colors.primary.tre};
+  `, []);
+
+  return (
+    <StyledJumbotron ref={ref} {...props} />
+  );
+});
+
+export default Jumbotron;
