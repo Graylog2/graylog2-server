2276c836cb1bb89070f3fdc519023a08dabd4313
Dennis Oelkers <dennis@graylog.com>
2019-10-28 09:58:41 +0100

    Provide optional render completion callback to views widgets. (#6652)
    * Exporting plain `singleton` as well, adding typing
    
    * Adding react context for render completion callback.
    
    * Triggering render completion callback in widgets.
    
    * Exporting `InteractiveContext` as singleton.

--- graylog2-web-interface/src/views/components/aggregationbuilder/EmptyAggregationContent.jsx
+++ graylog2-web-interface/src/views/components/aggregationbuilder/EmptyAggregationContent.jsx
@@ -1,13 +1,22 @@
 // @flow strict
-import * as React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Jumbotron, Button } from 'components/graylog';
+import RenderCompletionCallback from 'views/components/widgets/RenderCompletionCallback';
+import InteractiveContext from '../contexts/InteractiveContext';
 
 type Props = {|
   toggleEdit: () => void,
   editing: boolean,
-|}
+|};
 
 const EmptyAggregationContent = ({ toggleEdit, editing = false }: Props) => {
+  const onRenderComplete = useContext(RenderCompletionCallback);
+  useEffect(() => {
+    if (onRenderComplete) {
+      onRenderComplete();
+    }
+  }, [onRenderComplete]);
+  const interactive = useContext(InteractiveContext);
   const text = editing
     ? (
       <p>You are now editing the widget.<br />
@@ -15,7 +24,7 @@ const EmptyAggregationContent = ({ toggleEdit, editing = false }: Props) => {
         To finish, click &quot;Save&quot; to save, &quot;Cancel&quot; to abandon changes.
       </p>
     )
-    : (<p>Please <Button bsStyle="info" onClick={toggleEdit}>Edit</Button> the widget to see results here.</p>);
+    : (<p>Please {interactive ? <Button bsStyle="info" onClick={toggleEdit}>Edit</Button> : 'edit'} the widget to see results here.</p>);
   return (
     <Jumbotron>
       <h2>Empty Aggregation</h2>
