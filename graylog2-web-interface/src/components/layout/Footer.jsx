8aaf672ec8037d82a3c7f26ec1460c16d8394997
Dennis Oelkers <dennis@graylog.com>
2019-10-29 15:49:41 +0100

    Turning component into functional one.

--- graylog2-web-interface/src/components/layout/Footer.jsx
+++ graylog2-web-interface/src/components/layout/Footer.jsx
@@ -1,49 +1,59 @@
-import React from 'react';
-import createReactClass from 'create-react-class';
-import Reflux from 'reflux';
-import Version from 'util/Version';
+// @flow strict
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
+import Version from 'util/Version';
+import connect from 'stores/connect';
 import StoreProvider from 'injection/StoreProvider';
 
 const SystemStore = StoreProvider.getStore('System');
 
-const Footer = createReactClass({
-  displayName: 'Footer',
-  mixins: [Reflux.connect(SystemStore)],
-  mounted: false,
+type Props = {
+  system?: {
+    version: string,
+    hostname: string
+  },
+};
 
-  componentDidMount() {
-    this.mounted = true;
+const Footer = ({ system }: Props) => {
+  const [jvm, setJvm] = useState();
+  useEffect(() => {
+    let mounted = true;
     SystemStore.jvm().then((jvmInfo) => {
-      if (this.mounted) {
-        this.setState({ jvm: jvmInfo });
+      if (mounted) {
+        setJvm(jvmInfo);
       }
     });
-  },
 
-  componentWillUnmount() {
-    this.mounted = false;
-  },
+    return () => {
+      mounted = false;
+    };
+  }, []);
 
-  _isLoading() {
-    return !(this.state.system && this.state.jvm);
-  },
-
-  render() {
-    if (this._isLoading()) {
-      return (
-        <div id="footer">
-          Graylog {Version.getFullVersion()}
-        </div>
-      );
-    }
-
+  if (!(system && jvm)) {
     return (
       <div id="footer">
-        Graylog {this.state.system.version} on {this.state.system.hostname} ({this.state.jvm.info})
+        Graylog {Version.getFullVersion()}
       </div>
     );
-  },
-});
+  }
 
-export default Footer;
+  return (
+    <div id="footer">
+      Graylog {system.version} on {system.hostname} ({jvm.info})
+    </div>
+  );
+};
+
+Footer.propTypes = {
+  system: PropTypes.shape({
+    version: PropTypes.string,
+    hostname: PropTypes.string,
+  }),
+};
+
+Footer.defaultProps = {
+  system: undefined,
+};
+
+export default connect(Footer, { system: SystemStore });
