c7736c9e331b86e45cf8f3ac07a40a1e8e0eac26
Alex Konn <45592524+alex-konn@users.noreply.github.com>
2019-10-30 16:36:03 +0100

    Filter searches for only permitted ones when GET /views/search is requested (#6687)
    * Extract domain logic from SearchDbService into new class SearchDomain
    
    * added new stateless class SearchDomain which is supposed to hold
      domain logic for the search package, which does not belong into
      entities like Search itself
    * this should help with encapsulating all domain logic in the top-level
      package instead of spreading it out between the jersey resource, db
      service and whatnot
    * easier to unit test than jersey resources and db services
    * moved decisions about whether a user may see a search from
      SearchDbService into this new class as preparation for implementing
      similar logic when a list of all searches is loaded
    * added 2 new exceptions to avoid reusing those from jersey
      (ForbiddenException, NotFoundException) or our database package
      (another NotFoundException)
    
    * Add exception mappers
    
    * mappers for PermissionException and EntityNotFoundException
    * created a new package for exception mappers under search/rest
    
    * Filter searches for user to only permitted ones
    
    * Add license headers
    
    * Throw exception for not existing search id in SearchResource again
    
    * made SearchDomain return an Optional<Search> instead of Search
    * added NotFoundException to SearchResource again
    * removed EntityNotFoundException again, as this was its only usage
    
    * Load multiple ViewSharings at once in ViewPermissions
    
    ...to reduce the number of MongoDB lookups

---
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/SearchDomain.java
@@ -0,0 +1,69 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog.plugins.views.search;
+
+import org.graylog.plugins.views.search.db.SearchDbService;
+import org.graylog.plugins.views.search.errors.PermissionException;
+import org.graylog2.plugin.database.users.User;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class SearchDomain {
+    private final SearchDbService dbService;
+    private final ViewPermissions viewPermissions;
+
+    @Inject
+    public SearchDomain(SearchDbService dbService, ViewPermissions viewPermissions) {
+        this.dbService = dbService;
+        this.viewPermissions = viewPermissions;
+    }
+
+    public Optional<Search> getForUser(String id, User user, Predicate<String> viewReadPermission) {
+        final Optional<Search> search = dbService.get(id);
+
+        search.ifPresent(s -> checkPermission(user, viewReadPermission, s));
+
+        return search;
+    }
+
+    private void checkPermission(User user, Predicate<String> viewReadPermission, Search s) {
+        if (!hasReadPermissionFor(user, viewReadPermission, s))
+            throw new PermissionException("User " + user + " does not have permission to load search " + s.id());
+    }
+
+    public List<Search> getAllForUser(User user, Predicate<String> viewReadPermission) {
+        return dbService.streamAll()
+                .filter(s -> hasReadPermissionFor(user, viewReadPermission, s))
+                .collect(Collectors.toList());
+    }
+
+    private boolean hasReadPermissionFor(User user, Predicate<String> viewReadPermission, Search search) {
+        return isOwned(search, user) || hasPermissionFromViews(search, user, viewReadPermission);
+    }
+
+    private boolean hasPermissionFromViews(Search search, User user, Predicate<String> hasViewReadPermission) {
+        return viewPermissions.isSearchPermitted(search.id(), user, hasViewReadPermission);
+    }
+
+    private boolean isOwned(Search search, User user) {
+        return search.owner().map(o -> o.equals(user.getName())).orElse(false);
+    }
+}
