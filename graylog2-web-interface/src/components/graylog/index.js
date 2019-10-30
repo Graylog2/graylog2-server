0831fbba24b550a4a9c3c1bff557e5ba868c6069
Kyle Knight <kyle@graylog.com>
2019-10-30 04:50:42 -0500

    Adding Themeable Badge Component (#6653)
    note
    
    snapshots

--- graylog2-web-interface/src/components/graylog/index.js
+++ graylog2-web-interface/src/components/graylog/index.js
@@ -1,10 +1,12 @@
 import Alert from './Alert';
+import Badge from './Badge';
 import Button from './Button';
 import DropdownButton from './DropdownButton';
 import SplitButton from './SplitButton';
 
 export {
   Alert,
+  Badge,
   Button,
   DropdownButton,
   SplitButton,
