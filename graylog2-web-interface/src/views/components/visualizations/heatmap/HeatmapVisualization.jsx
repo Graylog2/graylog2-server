ad663043dd912b841da2f58634f6442255a3c049
Linus Pahl <lpahl@protonmail.com>
2019-10-30 15:26:51 +0100

    Show x and y axis name in heatmap hover info box

--- graylog2-web-interface/src/views/components/visualizations/heatmap/HeatmapVisualization.jsx
+++ graylog2-web-interface/src/views/components/visualizations/heatmap/HeatmapVisualization.jsx
@@ -1,7 +1,7 @@
 // @flow strict
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { values, find, isEmpty } from 'lodash';
+import { values, merge, fill, find, isEmpty, get } from 'lodash';
 
 import { AggregationType } from 'views/components/aggregationbuilder/AggregationBuilderPropTypes';
 import type { VisualizationComponent, VisualizationComponentProps } from 'views/components/aggregationbuilder/AggregationBuilder';
@@ -10,10 +10,23 @@ import type { ChartDefinition, ExtractedSeries } from '../ChartData';
 import GenericPlot from '../GenericPlot';
 import { chartData } from '../ChartData';
 
-const _generateSeries = (type, name, x, y, z): ChartDefinition => ({ type, name, x: y, y: x, z, transpose: true });
+const _generateSeries = (type, name, x, y, z): ChartDefinition => ({
+  type,
+  name,
+  test: 'wtwe',
+  x: y,
+  y: x,
+  z,
+  transpose: true,
+  hovertemplate: '%{xaxis.title.text}: %{x}<br>%{yaxis.title.text}: %{y}<br>value: %{z}<extra></extra>',
+});
 
 const _formatSeries = ({ valuesBySeries, xLabels }: {valuesBySeries: Object, xLabels: Array<any>}): ExtractedSeries => {
-  const z = values(valuesBySeries);
+  // When using the hovertemplate, the value z can't be undefined. Plotly would throw errors when hovering over a field.
+  const z = values(valuesBySeries).map((series) => {
+    const newSeries = fill(Array(xLabels.length), null);
+    return merge(newSeries, series);
+  });
   const yLabels = Object.keys(valuesBySeries);
   return [[
     'Heatmap Chart',
@@ -23,7 +36,9 @@ const _formatSeries = ({ valuesBySeries, xLabels }: {valuesBySeries: Object, xLa
   ]];
 };
 
-const _chartLayout = (heatmapData) => {
+const _chartLayout = (heatmapData, config) => {
+  const xAxisTitle = get(config, '_value.columnPivots[0].field');
+  const yAxisTitle = get(config, '_value.rowPivots[0].field');
   const axisConfig = {
     type: undefined,
     fixedrange: true,
@@ -33,8 +48,18 @@ const _chartLayout = (heatmapData) => {
     axisConfig.type = 'category';
   }
   return {
-    yaxis: axisConfig,
-    xaxis: axisConfig,
+    yaxis: {
+      ...axisConfig,
+      title: {
+        text: yAxisTitle,
+      },
+    },
+    xaxis: {
+      ...axisConfig,
+      title: {
+        text: xAxisTitle,
+      },
+    },
     margin: {
       b: 80,
       l: 80,
@@ -46,7 +71,7 @@ const _leafSourceMatcher = (source: string) => source.endsWith('leaf') && source
 
 const HeatmapVisualization: VisualizationComponent = ({ config, data }: VisualizationComponentProps) => {
   const heatmapData = chartData(config, data, 'heatmap', _generateSeries, _formatSeries, _leafSourceMatcher);
-  const layout = _chartLayout(heatmapData);
+  const layout = _chartLayout(heatmapData, config);
   return (
     <GenericPlot chartData={heatmapData} layout={layout} />
   );
