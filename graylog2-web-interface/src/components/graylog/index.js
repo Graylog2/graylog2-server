488b906bfe21ddce0dcd2c7174854036b595eba0
Kyle Knight <kyle@graylog.com>
2019-10-31 07:43:12 -0500

    Themed ControlLabel component (#6658)
    * Themed ControlLabel component
    
    ControlLabel
    
    snapshots
    
    Add Dropdown back to import
    
    Remove dupe badge import
    
    * Themed Badge Component #6653
    
    * Creating non-forwarded component

--- graylog2-web-interface/src/components/graylog/index.js
+++ graylog2-web-interface/src/components/graylog/index.js
@@ -1,6 +1,7 @@
 import Alert from './Alert';
 import Badge from './Badge';
 import Button from './Button';
+import ControlLabel from './ControlLabel';
 import DropdownButton from './DropdownButton';
 import SplitButton from './SplitButton';
 
@@ -8,6 +9,7 @@ export {
   Alert,
   Badge,
   Button,
+  ControlLabel,
   DropdownButton,
   SplitButton,
 };
