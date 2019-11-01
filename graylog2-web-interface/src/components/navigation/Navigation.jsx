0734b8d0401d63200d61162488ffeb417693184c
Dennis Oelkers <dennis@graylog.com>
2019-11-01 17:42:32 +0100

    Disable `Views` navigation menu if feature flag for new search is active (#6654)
    If the `search_3_2` feature flag is present, this PR disables the
    `Views` navigation menu as it is now replaced by the `Search` and
    `Dashboard` functionality.

--- graylog2-web-interface/src/components/navigation/Navigation.jsx
+++ graylog2-web-interface/src/components/navigation/Navigation.jsx
@@ -60,6 +60,8 @@ const formatPluginRoute = (pluginRoute, permissions, location) => {
   return formatSinglePluginRoute(pluginRoute);
 };
 
+const enableNewSearch = AppConfig.isFeatureEnabled('search_3_2');
+
 const Navigation = ({ permissions, fullName, location, loginName }) => {
   const pluginNavigations = PluginStore.exports('navigation')
     .sort((route1, route2) => naturalSort(route1.description.toLowerCase(), route2.description.toLowerCase()))
@@ -88,10 +90,12 @@ const Navigation = ({ permissions, fullName, location, loginName }) => {
             </LinkContainer>
           </IfPermitted>
 
+          {!enableNewSearch && (
           <NavDropdown title="Views" id="views-dropdown">
             <NavigationLink path={Routes.EXTENDEDSEARCH} description="Create new" />
             <NavigationLink path={Routes.VIEWS.LIST} description="Load existing" />
           </NavDropdown>
+          )}
 
           <LinkContainer to={Routes.STREAMS}>
             <NavItem>Streams</NavItem>
