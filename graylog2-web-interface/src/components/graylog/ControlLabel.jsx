e5a2bfa4f8b3ff5ff4f079f0bb6cf4e893f54666
Kyle Knight <kyle@graylog.com>
2019-11-05 11:12:32 -0600

    update snaps

--- graylog2-web-interface/src/components/graylog/ControlLabel.jsx
+++ graylog2-web-interface/src/components/graylog/ControlLabel.jsx
@@ -1,26 +1,14 @@
-import React, { forwardRef } from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 // eslint-disable-next-line no-restricted-imports
 import { ControlLabel as BootstrapControlLabel } from 'react-bootstrap';
 
 import teinte from 'theme/teinte';
 
-const ControlLabelRaw = styled(BootstrapControlLabel)`
+const ControlLabel = styled(BootstrapControlLabel)`
   color: ${teinte.primary.tre};
   font-weight: bold;
   margin-bottom: 5px;
   display: inline-block;
 `;
 
-const ControlLabel = forwardRef((props, ref) => <ControlLabelRaw ref={ref} {...props} />);
-
-const propTypes = {
-  children: PropTypes.any.isRequired,
-};
-
-ControlLabel.propTypes = propTypes;
-ControlLabelRaw.propTypes = propTypes;
-
 export default ControlLabel;
-export { ControlLabelRaw };
