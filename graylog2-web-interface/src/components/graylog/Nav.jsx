75b4eee76dd2609e86b4f2a83d63b919b585978d
Kyle Knight <kyle@graylog.com>
2019-11-07 13:20:35 -0600

    Themed Nav

--- graylog2-web-interface/src/components/graylog/Nav.jsx
+++ graylog2-web-interface/src/components/graylog/Nav.jsx
@@ -1,59 +1,93 @@
 // eslint-disable-next-line no-restricted-imports
 import { Nav as BootstrapNav } from 'react-bootstrap';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import teinte from 'theme/teinte';
-import { colorLevel } from 'theme/util';
-import navTabsStyles from './styles/nav-tabs';
 
-const Nav = styled(BootstrapNav)(() => {
-  const borderColor = colorLevel(teinte.tertiary.due, -3);
-
-  return css`
-    &.nav {
-      > li {
-        > a {
-          &:hover,
-          &:focus {
-            background-color: ${colorLevel(teinte.secondary.due, -3)};
-          }
-        }
-
-        &.disabled > a {
-          color: ${teinte.secondary.tre};
-
-          &:hover,
-          &:focus {
-            color: ${teinte.secondary.tre};
-          }
+const Nav = styled(BootstrapNav)`
+  &.nav {
+    > li {
+      > a {
+        &:hover,
+        &:focus {
+          background-color: ${teinte.secondary.due};
         }
       }
 
-      .open > a {
+      &.disabled > a {
+        color: ${teinte.secondary.tre};
+
+        &:hover,
+        &:focus {
+          color: ${teinte.secondary.tre};
+        }
+      }
+    }
+
+    .open > a {
+      &,
+      &:hover,
+      &:focus {
+        background-color: ${teinte.secondary.due};
+        border-color: ${teinte.tertiary.quattro};
+      }
+    }
+  }
+
+  &.nav-tabs {
+    border-bottom-color: ${teinte.tertiary.quattro};
+
+    > li {
+      > a {
+        &:hover {
+          border-color: ${teinte.secondary.due} ${teinte.secondary.due} ${teinte.tertiary.quattro};
+        }
+      }
+
+      &.active > a {
         &,
         &:hover,
         &:focus {
-          background-color: ${colorLevel(teinte.secondary.due, -3)};
-          border-color: ${borderColor};
+          color: ${teinte.primary.tre};
+          background-color: ${teinte.primary.due};
+          border-color: ${teinte.tertiary.quattro};
+          border-bottom-color: transparent;
         }
       }
     }
+  }
 
-    &.nav-pills {
-      > li {
-        &.active > a {
-          &,
-          &:hover,
-          &:focus {
-            color: ${teinte.primary.due};
-            background-color: ${borderColor};
-          }
+  &.nav-pills {
+    > li {
+      &.active > a {
+        &,
+        &:hover,
+        &:focus {
+          color: ${teinte.primary.due};
+          background-color: ${teinte.tertiary.quattro};
         }
       }
     }
+  }
 
-    &${navTabsStyles()}
-  `;
-});
+  &.nav-tabs.nav-justified {
+    > .active > a,
+    > .active > a:hover,
+    > .active > a:focus {
+      border-color: ${teinte.tertiary.quattro};
+    }
+
+    @media (min-width: 768px) {
+      > li > a {
+        border-bottom-color: ${teinte.tertiary.quattro};
+      }
+      > .active > a,
+      > .active > a:hover,
+      > .active > a:focus {
+        border-bottom-color: ${teinte.primary.due};
+      }
+    }
+  }
+`;
 
 export default Nav;
