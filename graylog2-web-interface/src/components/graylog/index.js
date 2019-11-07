7acfa51133941c55516069e454adad17b7d8f7ce
Kyle Knight <kyle@graylog.com>
2019-11-07 13:20:35 -0600

    Themed Nav & Navbar components #6745

--- graylog2-web-interface/src/components/graylog/index.js
+++ graylog2-web-interface/src/components/graylog/index.js
@@ -11,7 +11,9 @@ import Label from './Label';
 import ListGroupItem from './ListGroupItem';
 import Modal from './Modal';
 import Nav from './Nav';
+
 import Navbar from './Navbar';
+
 import NavDropdown from './NavDropdown';
 import Pagination from './Pagination';
 import Panel from './Panel';
