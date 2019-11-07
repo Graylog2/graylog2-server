073a8092ff82ff18ec599d1c82515c0cfacc43d9
Konrad Merz <konrad@graylog.com>
2019-11-07 14:19:39 +0100

    Convert test fully to react-testing-library to fix it

--- graylog2-web-interface/src/views/components/widgets/Widget.test.jsx
+++ graylog2-web-interface/src/views/components/widgets/Widget.test.jsx
@@ -1,6 +1,5 @@
 // @flow strict
 import React from 'react';
-import { mount } from 'enzyme';
 import { render, wait, fireEvent, cleanup } from '@testing-library/react';
 import { browserHistory } from 'react-router';
 import { Map } from 'immutable';
@@ -25,13 +24,13 @@ import asMock from 'helpers/mocking/AsMock';
 import ViewState from 'views/logic/views/ViewState';
 
 import Widget from './Widget';
-import LoadingWidget from './LoadingWidget';
-import ErrorWidget from './ErrorWidget';
 
 jest.mock('views/actions/SearchActions', () => ({
   create: mockAction(jest.fn()),
   get: mockAction(jest.fn()),
 }));
+
+jest.mock('views/components/search/IfSearch', () => jest.fn(({ children }) => children));
 
 jest.mock('views/stores/ViewManagementStore', () => ({
   ViewManagementActions: {
@@ -51,7 +50,11 @@ jest.mock('graylog-web-plugin/plugin', () => ({
         type: 'dummy',
         displayName: 'Some Dummy Visualization',
         visualizationComponent: 'dummy-visualization',
-        editComponent: 'edit-dummy-visualization',
+        // eslint-disable-next-line react/prop-types
+        editComponent: ({ onChange }) => {
+          // eslint-disable-next-line react/button-has-type
+          return <button onClick={() => onChange({ foo: 23 })}>Click me</button>;
+        },
       },
     ]),
   },
@@ -65,6 +68,12 @@ jest.mock('views/stores/TitlesStore');
 jest.mock('./WidgetColorContext', () => ({ children }) => children);
 
 describe('<Widget />', () => {
+  afterEach(() => {
+    cleanup();
+    jest.clearAllMocks();
+    jest.resetModules();
+  });
+
   const widget = { config: {}, id: 'widgetId', type: 'dummy' };
 
   const viewState = ViewState.builder().build();
@@ -119,38 +128,39 @@ describe('<Widget />', () => {
   });
 
   it('should render loading widget for widget without data', () => {
-    const wrapper = mount(<DummyWidget />);
-    expect(wrapper.find(LoadingWidget)).toHaveLength(1);
+    const { queryAllByTestId } = render(<DummyWidget />);
+    expect(queryAllByTestId('loading-widget')).toHaveLength(1);
   });
   it('should render error widget for widget with one error', () => {
-    const wrapper = mount(<DummyWidget errors={[{ description: 'The widget has failed: the dungeon collapsed, you die!' }]} />);
-    const errorWidgets = wrapper.find(ErrorWidget);
+    const { queryAllByText } = render(<DummyWidget errors={[{ description: 'The widget has failed: the dungeon collapsed, you die!' }]} />);
+    const errorWidgets = queryAllByText('The widget has failed: the dungeon collapsed, you die!');
     expect(errorWidgets).toHaveLength(1);
-    expect(errorWidgets).toIncludeText('The widget has failed: the dungeon collapsed, you die!');
   });
   it('should render error widget including all error messages for widget with multiple errors', () => {
-    const wrapper = mount((
+    const { queryAllByText } = render((
       <DummyWidget errors={[
         { description: 'Something is wrong' },
         { description: 'Very wrong' },
       ]} />
     ));
-    const errorWidgets = wrapper.find(ErrorWidget);
-    expect(errorWidgets).toHaveLength(1);
-    expect(errorWidgets).toIncludeText('Something is wrong');
-    expect(errorWidgets).toIncludeText('Very wrong');
+
+    const errorWidgets1 = queryAllByText('Something is wrong');
+    expect(errorWidgets1).toHaveLength(1);
+    const errorWidgets2 = queryAllByText('Very wrong');
+    expect(errorWidgets2).toHaveLength(1);
   });
   it('should render correct widget visualization for widget with data', () => {
-    const wrapper = mount(<DummyWidget data={[]} />);
-    expect(wrapper.find(LoadingWidget)).toHaveLength(0);
-    expect(wrapper.find('dummy-visualization')).toHaveLength(1);
+    const { queryAllByTestId, queryAllByTitle, debug } = render(<DummyWidget data={[]} />);
+    expect(queryAllByTestId('loading-widget')).toHaveLength(0);
+    expect(queryAllByTitle('Widget Title')).toHaveLength(2);
   });
 
   it('copies title when duplicating widget', (done) => {
-    const wrapper = mount(<DummyWidget title="Dummy Widget" />);
+    const { getByTestId, getByText } = render(<DummyWidget title="Dummy Widget" />);
 
-    wrapper.find('ActionToggle').simulate('click');
-    const duplicate = wrapper.find('a[children="Duplicate"]');
+    const actionToggle = getByTestId('widgetActionDropDown');
+    fireEvent.click(actionToggle);
+    const duplicateBtn = getByText('Duplicate');
     WidgetActions.duplicate = mockAction(jest.fn(() => Promise.resolve(WidgetModel.builder().id('duplicatedWidgetId').build())));
     TitlesActions.set = mockAction(jest.fn((type, id, title) => {
       expect(type).toEqual(TitleTypes.Widget);
@@ -160,9 +170,49 @@ describe('<Widget />', () => {
       return Promise.resolve();
     }));
 
-    duplicate.simulate('click');
-
+    fireEvent.click(duplicateBtn);
     expect(WidgetActions.duplicate).toHaveBeenCalled();
+  });
+  it('adds cancel action to widget in edit mode', () => {
+    const { queryAllByText } = render(<DummyWidget editing />);
+    const cancel = queryAllByText('Cancel');
+    expect(cancel).toHaveLength(1);
+  });
+  it('does not trigger action when clicking cancel after no changes were made', () => {
+    const { getByText } = render(<DummyWidget editing />);
+
+    WidgetActions.updateConfig = mockAction(jest.fn());
+
+    const cancelBtn = getByText('Cancel');
+    fireEvent.click(cancelBtn);
+    expect(WidgetActions.updateConfig).not.toHaveBeenCalled();
+  });
+  it('restores original state of widget config when clicking cancel after changes were made', () => {
+    const widgetWithConfig = { config: { foo: 42 }, id: 'widgetId', type: 'dummy' };
+    const { getByText } = render(<DummyWidget editing widget={widgetWithConfig} />);
+
+    WidgetActions.updateConfig = mockAction(jest.fn());
+    const onChangeBtn = getByText('Click me');
+    fireEvent.click(onChangeBtn);
+    expect(WidgetActions.updateConfig).toHaveBeenCalledWith('widgetId', { foo: 23 });
+
+    const cancelButton = getByText('Cancel');
+    fireEvent.click(cancelButton);
+
+    expect(WidgetActions.updateConfig).toHaveBeenCalledWith('widgetId', { foo: 42 });
+  });
+  it('does not restores original state of widget config when clicking "Finish Editing"', () => {
+    const widgetWithConfig = { config: { foo: 42 }, id: 'widgetId', type: 'dummy' };
+    const { getByText } = render(<DummyWidget editing widget={widgetWithConfig} />);
+
+    const onChangeBtn = getByText('Click me');
+    fireEvent.click(onChangeBtn);
+    expect(WidgetActions.updateConfig).toHaveBeenCalledWith('widgetId', { foo: 23 });
+
+    const saveButton = getByText('Save');
+    fireEvent.click(saveButton);
+
+    expect(WidgetActions.updateConfig).not.toHaveBeenCalledWith('widgetId', { foo: 42 });
   });
 
   describe('copy widget to dashboard', () => {
@@ -215,52 +265,5 @@ describe('<Widget />', () => {
       renderAndClick();
       await wait(() => expect(browserHistory.push).toHaveBeenCalledTimes(1));
     });
-  });
-  it('adds cancel action to widget in edit mode', () => {
-    const wrapper = mount(<DummyWidget editing />);
-    const cancel = wrapper.find('Button[children="Cancel"]');
-    expect(cancel).toExist();
-  });
-  it('does not trigger action when clicking cancel after no changes were made', () => {
-    const wrapper = mount(<DummyWidget editing />);
-
-    WidgetActions.updateConfig = mockAction(jest.fn());
-
-    const cancelButton = wrapper.find('Button[children="Cancel"]').at(0);
-    cancelButton.simulate('click');
-
-    expect(WidgetActions.updateConfig).not.toHaveBeenCalled();
-  });
-  it('restores original state of widget config when clicking cancel after changes were made', () => {
-    const widgetWithConfig = { config: { foo: 42 }, id: 'widgetId', type: 'dummy' };
-    const wrapper = mount(<DummyWidget editing widget={widgetWithConfig} />);
-
-    const editComponent = wrapper.find('edit-dummy-visualization').at(0);
-    const widgetConfigChange = editComponent.props().onChange;
-
-    WidgetActions.updateConfig = mockAction(jest.fn());
-    widgetConfigChange({ foo: 23 });
-    expect(WidgetActions.updateConfig).toHaveBeenCalledWith('widgetId', { foo: 23 });
-
-    const cancelButton = wrapper.find('button[children="Cancel"]');
-    cancelButton.simulate('click');
-
-    expect(WidgetActions.updateConfig).toHaveBeenCalledWith('widgetId', { foo: 42 });
-  });
-  it('does not restores original state of widget config when clicking "Finish Editing"', () => {
-    const widgetWithConfig = { config: { foo: 42 }, id: 'widgetId', type: 'dummy' };
-    const wrapper = mount(<DummyWidget editing widget={widgetWithConfig} />);
-
-    const editComponent = wrapper.find('edit-dummy-visualization').at(0);
-    const widgetConfigChange = editComponent.props().onChange;
-
-    WidgetActions.updateConfig = mockAction(jest.fn());
-    widgetConfigChange({ foo: 23 });
-    expect(WidgetActions.updateConfig).toHaveBeenCalledWith('widgetId', { foo: 23 });
-
-    const cancelButton = wrapper.find('button[children="Save"]');
-    cancelButton.simulate('click');
-
-    expect(WidgetActions.updateConfig).not.toHaveBeenCalledWith('widgetId', { foo: 42 });
   });
 });
