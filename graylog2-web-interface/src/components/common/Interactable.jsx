c8474ad2967ce8561afcf321f95d87032f5aef8a
Kyle Knight <kyle@graylog.com>
2019-11-08 14:43:05 -0600

    Migrate to react-rnd

--- graylog2-web-interface/src/components/common/Interactable.jsx
+++ graylog2-web-interface/src/components/common/Interactable.jsx
@@ -1,38 +1,157 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box } from 'react-mops';
+import { Rnd } from 'react-rnd';
 import styled from 'styled-components';
+import { lighten } from 'polished';
+
+import { Button } from 'components/graylog';
+import { Icon } from 'components/common';
+import teinte from 'theme/teinte';
 
 import 'react-mops/dist/esm/index.css';
 
-const InvisibleMarker = styled.span`
-  position: absolute;
-  pointer-events: none;
+const DEFAULT_SIZE = { width: 450, height: 300 };
+const halfWidth = Math.ceil((document.body.offsetWidth / 2) - (DEFAULT_SIZE.width / 2));
+const halfHeight = Math.ceil((document.body.offsetHeight / 2) - (DEFAULT_SIZE.height / 2));
+const stayOnScreenHeight = halfHeight < 0 ? 55 : halfHeight;
+const DEFAULT_POSITION = {
+  x: halfWidth,
+  y: stayOnScreenHeight,
+};
+
+const InteractableWrapper = styled.div`
+  position: fixed;
   top: 0;
+  right: 0;
+  bottom: 0;
   left: 0;
-  height: 0;
-  width: 0;
+  z-index: 9999;
+  pointer-events: none;
 `;
 
-const Interactable = ({ children, size, ...props }) => {
+const StyledRnd = styled(Rnd)`
+  box-shadow: 0 0 9px rgba(31, 31, 31, .25),
+              0 0 6px rgba(31, 31, 31, .25),
+              0 0 3px rgba(31, 31, 31, .25);
+  background-color: ${lighten(0.15, teinte.primary.tre)};
+  border-radius: 3px;
+  flex-direction: column;
+  display: flex !important;
+  pointer-events: auto;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  padding: 0 15px;
+`;
+
+const Header = styled.header`
+  margin: 0 0 9px;
+  padding: 6px 12px;
+  display: flex;
+  align-items: center;
+  background-color: ${lighten(0.25, teinte.primary.tre)};
+  border-bottom: 1px solid ${teinte.primary.tre};
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+`;
+
+const Title = styled.h3`
+  color: ${teinte.primary.due};
+  flex: 1;
+  cursor: move;
+`;
+
+const Interactable = ({
+  children,
+  className,
+  onClose,
+  onDrag,
+  onResize,
+  position,
+  size,
+  title,
+}) => {
+  const dragHandleRef = React.useRef(null);
+  const [dragHandleClassName, setDragHandleClassName] = React.useState(null);
+  const [dragPosition, setDragPosition] = React.useState(position);
+  const [resizeSize, setResizeSize] = React.useState(size);
+
+  React.useEffect(() => {
+    setDragHandleClassName(dragHandleRef.current.classList[0]);
+  }, []);
+
   return (
-    <Box size={size}
-         marker={InvisibleMarker}
-         fullHandles={false}
-         hideGuides={() => true}
-         drawBox={false}
-         {...props}>
-      {children}
-    </Box>
+    <InteractableWrapper>
+      <StyledRnd default={{ ...position, ...size }}
+                 minHeight={250}
+                 minWidth={250}
+                 maxHeight={document.body.offsetHeight}
+                 maxWidth={document.body.offsetWidth}
+                 dragHandleClassName={dragHandleClassName}
+                 onDragStop={(event, newPosition) => {
+                   const setPosition = {
+                     x: newPosition.x,
+                     y: newPosition.y,
+                   };
+                   setDragPosition(setPosition);
+                   onDrag(setPosition);
+                 }}
+                 onResizeStop={(event, direction, ref) => {
+                   const newSize = {
+                     width: ref.style.width,
+                     height: ref.style.height,
+                   };
+                   setResizeSize(newSize);
+                   onResize(newSize);
+                 }}
+                 position={dragPosition}
+                 size={resizeSize}
+                 className={className}>
+        <Header>
+          <Title ref={dragHandleRef}>{title}</Title>
+
+          {onClose
+          && (
+            <Button bsStyle="default" onClick={onClose} bsSize="sm">
+              <Icon name="times" size="lg" />
+            </Button>
+          )}
+        </Header>
+
+        <Content>
+          {children}
+        </Content>
+      </StyledRnd>
+    </InteractableWrapper>
   );
 };
 
 Interactable.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
+  onClose: PropTypes.func,
+  onDrag: PropTypes.func,
+  onResize: PropTypes.func,
+  position: PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+  }),
   size: PropTypes.shape({
-    height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
-  }).isRequired,
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  }),
+  title: PropTypes.string,
+};
+
+Interactable.defaultProps = {
+  className: undefined,
+  onClose: undefined,
+  onDrag: () => {},
+  onResize: () => {},
+  position: DEFAULT_POSITION,
+  size: DEFAULT_SIZE,
+  title: '',
 };
 
 export default Interactable;
--- graylog2-web-interface/src/components/common/Scratchpad.jsx
+++ graylog2-web-interface/src/components/common/Scratchpad.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import ReactDOM from 'react-dom';
 
+import teinte from 'theme/teinte';
 import { Alert } from 'components/graylog';
 import { Interactable } from 'components/common';
 import isLocalStorageReady from 'util/isLocalStorageReady';
@@ -9,53 +9,15 @@ import { ScratchpadContext } from 'routing/context/ScratchpadProvider';
 
 const LOCALSTORAGE_ITEM = 'gl-scratchpad';
 const DEFAULT_SCRATCHDATA = '';
-const DEFAULT_SIZE = { width: 450, height: 300 };
-const DEFAULT_POSITION = { x: window.document.body.offsetWidth - 250, y: 225 };
-
-const ScratchpadWrapper = styled.div`
-  position: fixed;
-  pointer-events: none;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 9999;
-`;
-
-const StyledInteractable = styled(Interactable)`
-  box-shadow: 0 0 9px rgba(31, 31, 31, .25),
-              0 0 6px rgba(31, 31, 31, .25),
-              0 0 3px rgba(31, 31, 31, .25);
-  background-color: #393939;
-  border-radius: 3px;
-`;
 
 const ContentArea = styled.div`
-  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   height: 100%;
-  padding: 0 15px;
-`;
-
-const ToggleButton = styled.button`
-  background: transparent;
-  border: 0;
-  padding: 0;
-  color: #F1F2F2;
-`;
-
-const Title = styled.h3`
-  color: #fff;
-  margin: 9px 0;
-
-  > ${ToggleButton} {
-    float: right;
-  }
 `;
 
 const Description = styled.p`
-  color: #fff;
+  color: ${teinte.primary.due};
 `;
 
 const Textarea = styled.textarea`
@@ -75,8 +37,8 @@ const Scratchpad = () => {
   const { isScratchpadVisible, setScratchpadVisibility } = useContext(ScratchpadContext);
   const [scratchData, setScratchData] = useState((storage && storage.value) || DEFAULT_SCRATCHDATA);
   const [localStorageReady] = useState(isLocalStorageReady());
-  const [size, setSize] = useState((storage && storage.size) || DEFAULT_SIZE);
-  const [position, setPosition] = useState((storage && storage.position) || DEFAULT_POSITION);
+  const [size, setSize] = useState((storage && storage.size) || undefined);
+  const [position, setPosition] = useState((storage && storage.position) || undefined);
 
   const writeData = (newData) => {
     if (localStorageReady) {
@@ -91,10 +53,14 @@ const Scratchpad = () => {
     writeData({ value: textareaRef.current.value });
   };
 
-  const handleInteractable = ({ size: newSize, position: newPosition }) => {
-    setSize(newSize);
+  const handleDrag = (newPosition) => {
     setPosition(newPosition);
-    writeData({ size: newSize, position: newPosition });
+    writeData({ position: newPosition });
+  };
+
+  const handleSize = (newSize) => {
+    setSize(newSize);
+    writeData({ size: newSize });
   };
 
   useEffect(() => {
@@ -105,31 +71,22 @@ const Scratchpad = () => {
 
   if (!isScratchpadVisible) return null;
 
-  return ReactDOM.createPortal(
-    (
-      <ScratchpadWrapper>
-        <StyledInteractable isDraggable
-                            isResizable
-                            size={size}
-                            minHeight={250}
-                            minWidth={250}
-                            position={position}
-                            onResizeEnd={handleInteractable}
-                            onDragEnd={handleInteractable}>
+  return (
+    <Interactable title="Scratchpad"
+                  onClose={() => setScratchpadVisibility(false)}
+                  onDrag={handleDrag}
+                  onResize={handleSize}
+                  size={size}
+                  position={position}>
+      <ContentArea>
+        <Description>Accusamus atque iste natus officiis laudantium mollitia numquam voluptatibus voluptates! Eligendi, totam dignissimos ipsum obcaecati corrupti qui omnis quibusdam fuga consequatur suscipit!</Description>
 
-          <ContentArea>
-            <Title>Scratchpad <ToggleButton type="button" onClick={() => setScratchpadVisibility(false)}><i className="fa fa-times" /></ToggleButton></Title>
-            <Description>Accusamus atque iste natus officiis laudantium mollitia numquam voluptatibus voluptates! Eligendi, totam dignissimos ipsum obcaecati corrupti qui omnis quibusdam fuga consequatur suscipit!</Description>
+        {!localStorageReady && (<StyledAlert bsStyle="warning">Your browser does not appear to support localStorage, so your Scratchpad may not properly restore between page changes and refreshes.</StyledAlert>)}
 
-            {!localStorageReady && (<StyledAlert bsStyle="warning">Your browser does not appear to support localStorage, so your Scratchpad may not properly restore between page changes and refreshes.</StyledAlert>)}
+        <Textarea ref={textareaRef} onChange={handleChange} value={scratchData} />
+      </ContentArea>
 
-            <Textarea ref={textareaRef} onChange={handleChange} value={scratchData} />
-          </ContentArea>
-
-        </StyledInteractable>
-      </ScratchpadWrapper>
-    ),
-    document.body,
+    </Interactable>
   );
 };
 
