1dbbd97f892ae8afbdbfab141314ef7c862219c3
Ousmane Samba <ousmane@graylog.com>
2019-11-08 15:09:16 +0000

    Fix typo in filter rule function

--- graylog2-web-interface/src/components/pipelines/StageForm.jsx
+++ graylog2-web-interface/src/components/pipelines/StageForm.jsx
@@ -87,14 +87,14 @@ const StageForm = createReactClass({
   _getFormattedOptions(rules) {
     const { stage } = this.state;
     const chosenRules = stage.rules;
-    return rules ? rules.filter(rule => this._filterChoosedRules(rule, chosenRules)).map(this._formatRuleOption) : [];
+    return rules ? rules.filter(rule => this._filterChosendRules(rule, chosenRules)).map(this._formatRuleOption) : [];
   },
 
   _formatRuleOption(rule) {
     return { value: rule.title, label: rule.title };
   },
 
-  _filterChoosedRules(rule, chosenRules) {
+  _filterChosendRules(rule, chosenRules) {
     return !chosenRules.includes(rule.title);
   },
 
