255a8615e2d852ad8f646d04d772ef624a48d436
Kyle Knight <kyle@graylog.com>
2019-11-12 07:22:16 -0600

    cleanup

--- graylog2-web-interface/src/components/graylog/Tooltip.jsx
+++ graylog2-web-interface/src/components/graylog/Tooltip.jsx
@@ -7,11 +7,9 @@ import teinte from 'theme/teinte';
 const Tooltip = styled(BootstrapTooltip)`
   &.top .tooltip-arrow {
     bottom: 0;
-    border-top-color: ${teinte.primary.tre};
   }
-  &.top-left .tooltip-arrow {
-    border-top-color: ${teinte.primary.tre};
-  }
+  &.top .tooltip-arrow,
+  &.top-left .tooltip-arrow,
   &.top-right .tooltip-arrow {
     border-top-color: ${teinte.primary.tre};
   }
@@ -21,12 +19,8 @@ const Tooltip = styled(BootstrapTooltip)`
   &.left .tooltip-arrow {
     border-left-color: ${teinte.primary.tre};
   }
-  &.bottom .tooltip-arrow {
-    border-bottom-color: ${teinte.primary.tre};
-  }
-  &.bottom-left .tooltip-arrow {
-    border-bottom-color: ${teinte.primary.tre};
-  }
+  &.bottom .tooltip-arrow,
+  &.bottom-left .tooltip-arrow,
   &.bottom-right .tooltip-arrow {
     border-bottom-color: ${teinte.primary.tre};
   }
