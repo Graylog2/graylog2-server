6f0f6d593f2d262535ac5eedcd475ec9c2e729c7
Ousmane Samba <ousmane@graylog.com>
2019-11-13 16:49:05 +0000

    Remove unused import

--- graylog2-web-interface/src/pages/LUTCachesPage.jsx
+++ graylog2-web-interface/src/pages/LUTCachesPage.jsx
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import createReactClass from 'create-react-class';
-import Reflux from 'reflux';
 import { LinkContainer } from 'react-router-bootstrap';
 import connect from 'stores/connect';
 import { ButtonToolbar, Col, Row, Button } from 'components/graylog';
