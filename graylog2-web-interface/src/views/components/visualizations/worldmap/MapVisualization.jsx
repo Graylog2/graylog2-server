df50fb7af8dde96fc67703c4d8068b34bb073a14
Linus Pahl <lpahl@protonmail.com>
2019-11-13 14:47:38 +0100

    Convert MapVisualization from createReactClass to ES6 class

--- graylog2-web-interface/src/views/components/visualizations/worldmap/MapVisualization.jsx
+++ graylog2-web-interface/src/views/components/visualizations/worldmap/MapVisualization.jsx
@@ -1,7 +1,6 @@
 /* eslint-env browser */
 import PropTypes from 'prop-types';
 import React from 'react';
-import createReactClass from 'create-react-class';
 import { CircleMarker, Map, Popup, TileLayer } from 'react-leaflet';
 import chroma from 'chroma-js';
 import { flatten } from 'lodash';
@@ -12,10 +11,14 @@ const DEFAULT_VIEWPORT = {
   zoom: 1,
 };
 
-const MapVisualization = createReactClass({
-  displayName: 'MapVisualization',
+class MapVisualization extends React.Component {
+  _map = undefined
 
-  propTypes: {
+  _isMapReady = false
+
+  _areTilesReady = false
+
+  static propTypes = {
     id: PropTypes.string.isRequired,
     data: PropTypes.arrayOf(PropTypes.object),
     height: PropTypes.number.isRequired,
@@ -32,48 +35,42 @@ const MapVisualization = createReactClass({
       center: PropTypes.arrayOf(PropTypes.number),
       zoom: PropTypes.number,
     }),
-  },
+  }
 
-  getDefaultProps() {
-    return {
-      data: {},
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
-      interactive: true,
-      onRenderComplete: () => {},
-      locked: false,
-      viewport: DEFAULT_VIEWPORT,
-      markerRadiusSize: 10,
-      markerRadiusIncrementSize: 10,
-    };
-  },
+  static defaultProps = {
+    data: {},
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
+    interactive: true,
+    onRenderComplete: () => {},
+    locked: false,
+    viewport: DEFAULT_VIEWPORT,
+    markerRadiusSize: 10,
+    markerRadiusIncrementSize: 10,
+  }
 
   componentDidMount() {
     this._forceMapUpdate();
-  },
+  }
 
   componentDidUpdate(prevProps) {
     const { height, width } = this.props;
     if (height !== prevProps.height || width !== prevProps.width) {
       this._forceMapUpdate();
     }
-  },
-
-  _map: undefined,
-  _isMapReady: false,
-  _areTilesReady: false,
+  }
 
   // Workaround to avoid wrong placed markers or empty tiles if the map container size changed.
-  _forceMapUpdate() {
+  _forceMapUpdate = () => {
     if (this._map) {
       window.dispatchEvent(new Event('resize'));
       const { interactive } = this.props;
       this._map.leafletElement.invalidateSize(interactive);
     }
-  },
+  }
 
   // Coordinates are given as "lat,long"
-  _formatMarker(coordinates, value, min, max, radiusSize, increment, color, name, keys) {
+  _formatMarker = (coordinates, value, min, max, radiusSize, increment, color, name, keys) => {
     // eslint-disable-next-line no-restricted-globals
     const formattedCoordinates = coordinates.split(',').map(component => Number(component)).filter(n => !isNaN(n));
     if (formattedCoordinates.length !== 2) {
@@ -108,33 +105,33 @@ const MapVisualization = createReactClass({
         </Popup>
       </CircleMarker>
     );
-  },
+  }
 
-  _getBucket(value, bucketCount, minValue, maxValue, increment) {
+  _getBucket = (value, bucketCount, minValue, maxValue, increment) => {
     // Calculate bucket size based on min/max value and the number of buckets.
     const bucketSize = (maxValue - minValue) / bucketCount;
     // Select bucket for the given value.
     const bucket = value < maxValue ? Math.ceil((value - minValue) / bucketSize) : bucketCount;
 
     return bucket + increment;
-  },
+  }
 
-  _handleRenderComplete() {
+  _handleRenderComplete = () => {
     if (this._areTilesReady && this._isMapReady) {
       const { onRenderComplete } = this.props;
       onRenderComplete();
     }
-  },
+  }
 
-  _handleMapReady() {
+  _handleMapReady = () => {
     this._isMapReady = true;
     this._handleRenderComplete();
-  },
+  }
 
-  _handleTilesReady() {
+  _handleTilesReady = () => {
     this._areTilesReady = true;
     this._handleRenderComplete();
-  },
+  }
 
   render() {
     const { data, id, height, width, url, attribution, interactive, locked, viewport, onChange, markerRadiusSize, markerRadiusIncrementSize } = this.props;
@@ -172,7 +169,8 @@ const MapVisualization = createReactClass({
         </Map>
       </div>
     );
-  },
-});
+  }
+}
+
 
 export default MapVisualization;
