4b86a7bd22b01f55de2fe1399ee1f48b474b6d4b
Dennis Oelkers <dennis@graylog.com>
2019-11-13 11:12:07 +0100

    Improve the `NewDashboardPage` test w/ testing-library and proper mocks.
    Before this change, the `NewDashboardPage` triggered a couple of
    asynchronous errors due implicit triggering and initialization of store
    actions, leading to asynchronous updates. By using `testing-library` and
    proper mocking, this change prevents any updates taking place after
    tests are finished.

--- graylog2-web-interface/src/views/pages/NewDashboardPage.test.jsx
+++ graylog2-web-interface/src/views/pages/NewDashboardPage.test.jsx
@@ -1,44 +1,59 @@
 // @flow strict
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { render, cleanup, waitForElement } from '@testing-library/react';
 
 import mockAction from 'helpers/mocking/MockAction';
-import mockComponent from 'helpers/mocking/MockComponent';
 import { ViewActions } from 'views/stores/ViewStore';
 import Search from 'views/logic/search/Search';
 import View from 'views/logic/views/View';
 import NewDashboardPage from './NewDashboardPage';
 
-jest.mock('./ExtendedSearchPage', () => mockComponent('ExtendedSearchPage'));
+jest.mock('./ExtendedSearchPage', () => () => <div>Extended search page</div>);
+jest.mock('views/stores/ViewStore', () => ({ ViewActions: {} }));
+jest.mock('views/logic/views/ViewLoader', () => ({
+  processHooks: jest.fn((promise, loadHooks, executeHooks, query, onSuccess) => Promise.resolve().then(onSuccess)),
+}));
 
 describe('NewDashboardPage', () => {
-  it('should render minimal', () => {
-    const wrapper = mount(<NewDashboardPage route={{}} location={{}} />);
-    expect(wrapper).toMatchSnapshot();
+  afterEach(cleanup);
+  it('should render minimal', async () => {
+    ViewActions.create = mockAction(jest.fn(() => Promise.resolve()));
+
+    const { getByText } = render(<NewDashboardPage route={{}} location={{}} />);
+
+    await waitForElement(() => getByText('Extended search page'));
   });
 
-  it('should render transform search view to dashboard view', () => {
+  it('should render transform search view to dashboard view', async () => {
     const view = View.create().toBuilder().type(View.Type.Search).search(Search.builder().build())
       .createdAt(new Date('2019-10-16T14:38:44.681Z'))
       .build();
     ViewActions.load = mockAction(jest.fn(() => new Promise(() => {})));
-    const wrapper = mount(<NewDashboardPage route={{}}
-                                            location={{ state: { view } }} />);
-    expect(wrapper).toMatchSnapshot();
+
+    const { getByText } = render((
+      <NewDashboardPage route={{}}
+                        location={{ state: { view } }} />
+    ));
+
+    await waitForElement(() => getByText('Extended search page'));
     expect(ViewActions.load).toHaveBeenCalledTimes(1);
     /* $FlowFixMe ViewActions.load was overridden to test the call */
     expect(ViewActions.load.mock.calls[0][0].type).toStrictEqual(View.Type.Dashboard);
   });
 
-  it('should not render transform search view to dashboard view if view search is in JSON format', () => {
+  it('should not render transform search view to dashboard view if view search is in JSON format', async () => {
     const view = View.create().toBuilder().type(View.Type.Search).search(Search.builder().build())
       .createdAt(new Date('2019-10-16T14:38:44.681Z'))
       .build()
       .toJSON();
     ViewActions.load = mockAction(jest.fn(() => new Promise(() => {})));
-    const wrapper = mount(<NewDashboardPage route={{}}
-                                            location={{ state: { view } }} />);
-    expect(wrapper).toMatchSnapshot();
+
+    const { getByText } = render((
+      <NewDashboardPage route={{}}
+                        location={{ state: { view } }} />
+    ));
+
+    await waitForElement(() => getByText('Extended search page'));
     expect(ViewActions.load).toHaveBeenCalledTimes(0);
   });
 });
