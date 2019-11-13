2f601be2964e575f2baa3ad55a5b1ad297f30dd3
Kyle Knight <kyle@graylog.com>
2019-11-13 15:04:10 -0600

    validation styles

--- graylog2-web-interface/src/components/graylog/InputGroup.jsx
+++ graylog2-web-interface/src/components/graylog/InputGroup.jsx
@@ -6,11 +6,17 @@ import { lighten } from 'polished';
 import teinte from 'theme/teinte';
 
 const InputGroup = styled(BootstrapInputGroup)`
+<<<<<<< HEAD
   .input-group-addon {
     color: ${lighten(0.30, teinte.primary.tre)};
     background-color: ${teinte.primary.due};
     border-color: ${teinte.secondary.tre};
   }
+=======
+  color: ${lighten(0.30, teinte.primary.tre)};
+  background-color: ${teinte.secondary.due};
+  border-color: ${teinte.secondary.tre};
+>>>>>>> dea3e3d75... validation styles
 `;
 
 export default InputGroup;
