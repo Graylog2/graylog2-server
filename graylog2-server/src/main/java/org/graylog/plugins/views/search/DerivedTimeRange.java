01f224f171990603e738f9403cc8556d565b2c3b
Dennis Oelkers <dennis@graylog.com>
2019-11-14 13:07:14 +0100

    Adding `DerivedTimeRange` class to wrap time ranges for search types.
    This class is supposed to wrap `TimeRange` instances in some contexts
    (currently search types) where the `TimeRange` could also be an
    implementation of the `OffsetRange` time range, which requires
    additional dependencies (`Query`/`SearchType`) to calculate the
    effective time range.
    
    The class is wrapping the actual `TimeRange` and the only way to
    retrieve it from an instance is to call `effectiveTimeRange` and pass in
    the required dependencies for its calculation.
    
    Whenever the `OffsetRange` is supposed to be used, it must be wrapped in
    a `DerivedTimeRange` instance, as it only contains the parameters to
    reference and offset an existing time range, but is not able to
    calculate it on its own and therefore throws an ISE when
    `getFrom`/`getTo` is called.

---
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/DerivedTimeRange.java
@@ -0,0 +1,30 @@
+package org.graylog.plugins.views.search;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.auto.value.AutoValue;
+import org.graylog2.plugin.indexer.searches.timeranges.AbsoluteRange;
+import org.graylog2.plugin.indexer.searches.timeranges.KeywordRange;
+import org.graylog2.plugin.indexer.searches.timeranges.OffsetRange;
+import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
+import org.graylog2.plugin.indexer.searches.timeranges.TimeRange;
+
+@AutoValue
+public abstract class DerivedTimeRange {
+    @JsonValue
+    abstract TimeRange value();
+
+    public TimeRange effectiveTimeRange(Query query, SearchType searchType) {
+        switch (value().type()) {
+            case AbsoluteRange.ABSOLUTE:
+            case KeywordRange.KEYWORD:
+            case RelativeRange.RELATIVE: return value();
+            case OffsetRange.OFFSET: return ((OffsetRange) value()).deriveTimeRange(query, searchType);
+            default:
+                throw new IllegalArgumentException("Invalid TimeRange: " + value());
+        }
+    }
+
+    public static DerivedTimeRange of(TimeRange timeRange) {
+        return new AutoValue_DerivedTimeRange(timeRange);
+    }
+}
