0d46a7b343d3464e311c8404bd182362b452e681
Ousmane Samba <ousmane@graylog.com>
2019-11-14 13:18:54 +0000

    Putting back console.error
    This change will fix the falling test.
    * disabled eslint for the conole line

--- graylog2-web-interface/src/components/lookup-tables/DataAdapterCreate.jsx
+++ graylog2-web-interface/src/components/lookup-tables/DataAdapterCreate.jsx
@@ -57,6 +57,8 @@ class DataAdapterCreate extends React.Component {
     const sortedAdapters = Object.keys(types).map((key) => {
       const typeItem = types[key];
       if (adapterPlugins[typeItem.type] === undefined) {
+        // eslint-disable-next-line no-console
+        console.error(`Plugin component for data adapter type ${type.type} is missing - invalid or missing plugin?`);
         return { value: typeItem.type, disabled: true, label: `${typeItem.type} - missing or invalid plugin` };
       }
       return { value: typeItem.type, label: adapterPlugins[typeItem.type].displayName };
