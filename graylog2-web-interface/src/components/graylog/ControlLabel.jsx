8c16074d4c6e01c7aedebeeec55c3d3a2894a70f
Kyle Knight <kyle@graylog.com>
2019-11-20 10:16:20 +0100

    ControlLabel color

--- graylog2-web-interface/src/components/graylog/ControlLabel.jsx
+++ graylog2-web-interface/src/components/graylog/ControlLabel.jsx
@@ -2,10 +2,8 @@ import styled from 'styled-components';
 // eslint-disable-next-line no-restricted-imports
 import { ControlLabel as BootstrapControlLabel } from 'react-bootstrap';
 
-import { color } from 'theme';
-
-const ControlLabel = styled(BootstrapControlLabel)`
-  color: ${color.global.textDefault};
-`;
+const ControlLabel = styled(BootstrapControlLabel)(props => `
+  color: ${props.theme.color.global.textDefault};
+`);
 
 export default ControlLabel;
