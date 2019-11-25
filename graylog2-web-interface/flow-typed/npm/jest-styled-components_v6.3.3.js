31ee7c080d9b1271db2f0e542544cb5679a173d8
Dennis Oelkers <dennis@graylog.com>
2019-11-25 13:11:42 +0100

    Adding flow types for `jest-styled-components`

---
+++ graylog2-web-interface/flow-typed/npm/jest-styled-components_v6.3.3.js
@@ -0,0 +1,30 @@
+/**
+ * Flowtype definitions for jest-styled-components
+ */
+
+/* eslint-disable no-undef */
+declare interface jest$AsymmetricMatcher {
+  $$typeof: Symbol;
+  sample?: string | RegExp | { [key: string]: any } | Array<any> | Function;
+}
+
+declare type jest$Value =
+  | string
+  | number
+  | RegExp
+  | jest$AsymmetricMatcher
+  | void;
+
+declare interface jest$Options {
+  media?: string;
+  modifier?: string;
+  supports?: string;
+}
+
+declare interface jest$Matchers<R, T> {
+  toHaveStyleRule(
+    property: string,
+    value?: jest$Value,
+    options?: jest$Options
+  ): R;
+}
