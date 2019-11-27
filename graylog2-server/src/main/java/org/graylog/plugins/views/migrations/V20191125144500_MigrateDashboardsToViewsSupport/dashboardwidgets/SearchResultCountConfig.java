5104023ad179d66610ed304cac65f7062a1c763b
Dennis Oelkers <dennis@graylog.com>
2019-11-27 12:46:33 +0100

    Implementing migration strategy for search result count widgets.

--- graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/dashboardwidgets/SearchResultCountConfig.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/dashboardwidgets/SearchResultCountConfig.java
@@ -4,13 +4,54 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
+import org.graylog.plugins.views.migrations.V20191125144500_MigrateDashboardsToViewsSupport.ElasticsearchQueryString;
 import org.graylog.plugins.views.migrations.V20191125144500_MigrateDashboardsToViewsSupport.TimeRange;
+import org.graylog.plugins.views.migrations.V20191125144500_MigrateDashboardsToViewsSupport.ViewWidget;
+import org.graylog.plugins.views.migrations.V20191125144500_MigrateDashboardsToViewsSupport.viewwidgets.AggregationConfig;
+import org.graylog.plugins.views.migrations.V20191125144500_MigrateDashboardsToViewsSupport.viewwidgets.NumberVisualizationConfig;
+import org.graylog.plugins.views.migrations.V20191125144500_MigrateDashboardsToViewsSupport.viewwidgets.Series;
+
+import java.util.Collections;
+import java.util.Set;
 
 @AutoValue
 @JsonAutoDetect
 public abstract class SearchResultCountConfig implements WidgetConfig {
     public abstract Boolean lowerIsBetter();
+
     public abstract Boolean trend();
+
+    private Series series() {
+        return Series.createFromString("count()").build();
+    }
+
+    private String visualization() {
+        return "numeric";
+    }
+
+    @Override
+    public Set<ViewWidget> toViewWidgets() {
+        return Collections.singleton(
+                ViewWidget.builder()
+                        .query(ElasticsearchQueryString.create(query()))
+                        .timerange(timerange())
+                        .config(
+                                AggregationConfig.builder()
+                                        .series(Collections.singletonList(series()))
+                                        .visualization(visualization())
+                                        .visualizationConfig(
+                                                NumberVisualizationConfig.builder()
+                                                        .trend(true)
+                                                        .trendPreference(lowerIsBetter()
+                                                                ? NumberVisualizationConfig.TrendPreference.LOWER
+                                                                : NumberVisualizationConfig.TrendPreference.HIGHER)
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build()
+        );
+    }
 
     @JsonCreator
     static SearchResultCountConfig create(
