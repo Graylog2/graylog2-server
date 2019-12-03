26da2ae5760a5805497b35cdb4a11016bdc416fd
Kyle Knight <kyle@graylog.com>
2019-12-03 14:50:17 -0600

    Enzyme tests passing

--- graylog2-web-interface/src/theme/enzymeWithTheme.jsx
+++ graylog2-web-interface/src/theme/enzymeWithTheme.jsx
@@ -1,6 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
 import { mount, shallow } from 'enzyme';
 
-import GraylogThemeProvider from 'theme/GraylogThemeProvider';
+const GraylogThemeProvider = ({ children }) => {
+  return (
+    <ThemeProvider theme={{ mode: 'teinte' }}>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+GraylogThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
 
 export const shallowWithTheme = (Component, options = {}) => shallow(Component, {
   wrappingComponent: GraylogThemeProvider,
