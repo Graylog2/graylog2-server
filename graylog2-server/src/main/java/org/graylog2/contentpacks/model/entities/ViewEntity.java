cf73ba27e5e1e1bdcfd78653291530cbe79e4018
Konrad Merz <konrad@graylog.com>
2019-12-05 16:45:49 +0100

    Fix deserialization of ViewEntity

--- graylog2-server/src/main/java/org/graylog2/contentpacks/model/entities/ViewEntity.java
+++ graylog2-server/src/main/java/org/graylog2/contentpacks/model/entities/ViewEntity.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import java.util.Set;
 
 @AutoValue
-@JsonDeserialize
+@JsonDeserialize(builder = ViewEntity.Builder.class)
 @WithBeanGetter
 public abstract class ViewEntity {
     public enum Type {
