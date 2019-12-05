4077e1b7a2072368dd24f7cc0d4da378d8595d37
Dennis Oelkers <dennis@graylog.com>
2019-12-05 14:20:05 +0100

    Removing unneeded content pack field again.

--- graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/V20191125144500_MigrateDashboardsToViews.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/V20191125144500_MigrateDashboardsToViews.java
@@ -148,7 +148,6 @@ public class V20191125144500_MigrateDashboardsToViews extends Migration {
                 "This dashboard was migrated automatically.",
                 dashboard.description(),
                 newSearch.id(),
-                dashboard.contentPack(),
                 Collections.singletonMap(newQuery.id(), newViewState),
                 Optional.ofNullable(dashboard.creatorUserId()),
                 createdAt
