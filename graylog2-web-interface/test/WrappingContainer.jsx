182cabdaa2ef3ae035c4eff0d87cadfab1cafd19
Kyle Knight <kyle@graylog.com>
2019-12-07 08:08:42 -0600

    lint issues

--- graylog2-web-interface/test/WrappingContainer.jsx
+++ graylog2-web-interface/test/WrappingContainer.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ThemeProvider } from 'styled-components';
 
-/**
- * NOTE: `ThemeProvider` will be removed in future iterations and the actual global provider will be used
- */
+import GraylogThemeProvider from 'theme/GraylogThemeProvider';
+import GlobalThemeStyles from 'theme/GlobalThemeStyles';
 
 const WrappingContainer = ({ children }) => {
   return (
-    <ThemeProvider theme={{ mode: 'teinte' }}>
-      {children}
-    </ThemeProvider>
+    <GraylogThemeProvider>
+      <>
+        <GlobalThemeStyles />
+        {children}
+      </>
+    </GraylogThemeProvider>
   );
 };
 
