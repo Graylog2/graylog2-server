5e36758e85304f69f811af8fcaf220936a948531
Kyle Knight <kyle@graylog.com>
2019-12-10 13:13:20 -0600

    Everything but Collapsible Panel working

--- graylog2-web-interface/src/components/graylog/Panel.jsx
+++ graylog2-web-interface/src/components/graylog/Panel.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 // eslint-disable-next-line no-restricted-imports
 import { Panel as BootstrapPanel } from 'react-bootstrap';
@@ -6,53 +8,64 @@ import { adjustHue, darken } from 'polished';
 import { util } from 'theme';
 import bsStyleThemeVariant from './variants/bsStyle';
 
-const panelVariantStyles = (hex) => {
-  const backgroundColor = util.colorLevel(hex, -9);
-  const borderColor = darken(0.05, adjustHue(-10, hex));
+const backgroundColor = hex => util.colorLevel(hex, -9);
+const borderColor = hex => darken(0.05, adjustHue(-10, hex));
 
-  return css`
-    border-color: ${borderColor};
+const panelVariantStyles = hex => css`
+  border-color: ${borderColor(hex)};
+`;
 
-    & > .panel-heading {
-      color: ${util.colorLevel(backgroundColor, 9)};
-      background-color: ${backgroundColor};
-      border-color: ${borderColor};
+const headingVariantStyles = hex => css`
+  && {
+    color: ${util.colorLevel(backgroundColor(hex), 9)};
+    background-color: ${backgroundColor(hex)};
+    border-color: ${borderColor(hex)};
 
-      + .panel-collapse > .panel-body {
-        border-top-color: ${borderColor};
-      }
-
-      .badge {
-        color: ${backgroundColor};
-        background-color: ${hex};
-      }
+    + .panel-collapse > .panel-body {
+      border-top-color: ${borderColor};
     }
 
-    & > .panel-footer {
-      + .panel-collapse > .panel-body {
-        border-bottom-color: ${borderColor};
-      }
+    .badge {
+      color: ${backgroundColor};
+      background-color: ${hex};
     }
-  `;
-};
+  }
+`;
+
+const footerVariantStyles = hex => css`
+  && {
+    color: ${util.colorLevel(backgroundColor(hex), 9)};
+    background-color: ${backgroundColor(hex)};
+    border-color: ${borderColor(hex)};
+
+    + .panel-collapse > .panel-body {
+      border-bottom-color: ${borderColor};
+    }
+  }
+`;
+
+const PanelHeading = styled(BootstrapPanel.Heading)`
+  ${bsStyleThemeVariant(headingVariantStyles)};
+`;
+
+const PanelFooter = styled(BootstrapPanel.Footer)(({ theme }) => css`
+  background-color: ${theme.color.secondary.tre};
+  border-top-color: ${theme.color.secondary.due};
+  ${bsStyleThemeVariant(footerVariantStyles)};
+`);
 
 const StyledPanel = styled(BootstrapPanel)(({ theme }) => css`
   background-color: ${theme.color.primary.due};
 
-  .panel-footer {
-    background-color: ${theme.color.secondary.tre};
-    border-top-color: ${theme.color.secondary.due};
-  }
-
   .panel-group {
-    .panel-heading {
+    ${PanelHeading} {
       + .panel-collapse > .panel-body,
       + .panel-collapse > .list-group {
         border-top-color: ${theme.color.secondary.due};
       }
     }
 
-    .panel-footer {
+    ${PanelFooter} {
       + .panel-collapse .panel-body {
         border-bottom-color: ${theme.color.secondary.due};
       }
@@ -62,5 +75,72 @@ const StyledPanel = styled(BootstrapPanel)(({ theme }) => css`
   ${bsStyleThemeVariant(panelVariantStyles)}
 `);
 
+const CollapsibleBody = ({ children }) => {
+  return (
+    <BootstrapPanel.Collapse>
+      <BootstrapPanel.Body>
+        {children}
+      </BootstrapPanel.Body>
+    </BootstrapPanel.Collapse>
+  );
+};
+
+const Panel = ({ header, footer, children, collapsible, expanded, title, ...props }) => {
+  if (header || footer || title || collapsible) {
+    /* eslint-disable-next-line no-console */
+    console.warn('Panel: ', 'You have used a deprecated `Panel` prop, please check the documentation to use the latest props.');
+
+    return (
+      /* NOTE: this exists as a deprecated render for older Panel instances */
+      <BootstrapPanel {...props} expanded={expanded}>
+        {header && (
+          <BootstrapPanel.Heading>{header}</BootstrapPanel.Heading>
+        )}
+        {collapsible
+          ? <CollapsibleBody>{children}</CollapsibleBody>
+          : <BootstrapPanel.Body>{children}</BootstrapPanel.Body>}
+        {footer && (
+          <BootstrapPanel.Footer>{footer}</BootstrapPanel.Footer>
+        )}
+      </BootstrapPanel>
+    );
+  }
+
+  return <StyledPanel {...props} expanded={expanded}>{children}</StyledPanel>;
+};
+
+CollapsibleBody.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+Panel.propTypes = {
+  children: PropTypes.any.isRequired,
+  /** @deprecated No longer used, replace with `<Panel.Collapse />` &  `expandable`. */
+  collapsible: PropTypes.bool,
+  /** Must be used in conjunction with `<Panel.Collapse />` */
+  expanded: PropTypes.bool,
+  /** @deprecated No longer used, replace with `<Panel.Footer />`. */
+  footer: PropTypes.string,
+  /** @deprecated No longer used, replace with `<Panel.Heading />`. */
+  header: PropTypes.string,
+  /** @deprecated No longer used, replace with `<Panel.Title />`. */
+  title: PropTypes.string,
+};
+
+Panel.defaultProps = {
+  collapsible: false,
+  expanded: false,
+  footer: undefined,
+  header: undefined,
+  title: undefined,
+};
+
+Panel.Body = BootstrapPanel.Body;
+Panel.Collapse = BootstrapPanel.Collapse;
+Panel.Footer = PanelFooter;
+Panel.Heading = PanelHeading;
+Panel.Title = BootstrapPanel.Title;
+Panel.Toggle = BootstrapPanel.Toggle;
+
 /** @component */
-export default StyledPanel;
+export default Panel;
