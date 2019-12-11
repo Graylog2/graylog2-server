62df922deb17607ad962d7b5509b81832d8e7e23
Dennis Oelkers <dennis@graylog.com>
2019-12-11 15:35:29 +0100

    Forwarding ref in `InlineForm`.

--- graylog2-web-interface/src/views/components/messagelist/decorators/InlineForm.jsx
+++ graylog2-web-interface/src/views/components/messagelist/decorators/InlineForm.jsx
@@ -11,37 +11,38 @@ type Props = {
   onSubmitForm: any => void,
 };
 
-const InlineForm = (submitTitle: string = 'Create'): React.ComponentType<Props> => ({ children, disabled, onCancel, onSubmitForm }) => {
-  const onSubmit = (event) => {
-    event.stopPropagation();
-    const { target: formDOMNode } = event;
+const InlineForm = (submitTitle: string = 'Create'): React.ComponentType<Props> => React.forwardRef(
+  ({ children, disabled, onCancel, onSubmitForm }: Props, ref) => {
+    const onSubmit = (event) => {
+      event.stopPropagation();
+      const { target: formDOMNode } = event;
 
-    if ((typeof formDOMNode.checkValidity === 'function' && !formDOMNode.checkValidity())) {
-      event.preventDefault();
-      return;
-    }
+      if ((typeof formDOMNode.checkValidity === 'function' && !formDOMNode.checkValidity())) {
+        event.preventDefault();
+        return;
+      }
 
-    // Check custom validation for plugin fields
-    if (!validate(formDOMNode)) {
-      event.preventDefault();
-      return;
-    }
+      // Check custom validation for plugin fields
+      if (!validate(formDOMNode)) {
+        event.preventDefault();
+        return;
+      }
 
-    // If function is not given, let the browser continue propagating the submit event
-    if (typeof onSubmitForm === 'function') {
-      event.preventDefault();
-      onSubmitForm(event);
-    }
-  };
-  return (
-    <form onSubmit={onSubmit}>
-      {children}
-      <Button type="submit" bsStyle="success" disabled={disabled}>{submitTitle}</Button>{' '}
-      <Button type="button" disabled={disabled} onClick={onCancel}>Cancel</Button>
-    </form>
-  );
-};
+      // If function is not given, let the browser continue propagating the submit event
+      if (typeof onSubmitForm === 'function') {
+        event.preventDefault();
+        onSubmitForm(event);
+      }
+    };
+    return (
+      <form onSubmit={onSubmit} ref={ref}>
+        {children}
+        <Button type="submit" bsStyle="success" disabled={disabled}>{submitTitle}</Button>{' '}
+        <Button type="button" disabled={disabled} onClick={onCancel}>Cancel</Button>
+      </form>
+    );
+  },
+);
 
-InlineForm.propTypes = {};
 
 export default InlineForm;
