37f1927434b6c6fe85b0fb46d25ca4ef69d94a37
Dennis Oelkers <dennis@graylog.com>
2019-12-11 16:41:05 +0100

    Making presence of decoration stats a bit more lenient.

--- graylog2-web-interface/src/views/components/messagelist/Types.js
+++ graylog2-web-interface/src/views/components/messagelist/Types.js
@@ -3,7 +3,7 @@ export type Message = {|
   id: string,
   index: string,
   fields: { [string]: any },
-  decoration_stats: {
+  decoration_stats?: {
     added_fields: { [string]: any },
     changed_fields: { [string]: any },
     removed_fields: { [string]: any },
