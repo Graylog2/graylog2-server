d7cb790fb97d93b90daedd0bafe54412813ca704
Kyle Knight <kyle@graylog.com>
2019-12-12 11:42:13 -0600

    removing dupe pagination styles

--- graylog2-web-interface/src/theme/GlobalThemeStyles.jsx
+++ graylog2-web-interface/src/theme/GlobalThemeStyles.jsx
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
-import { paginationStyles } from 'components/graylog/Pagination.jsx';
 import { progressBarStyles } from 'components/graylog/ProgressBar.jsx';
 import globalStyles from './styles/globalStyles';
 
 const GlobalThemeStyles = createGlobalStyle`
   ${globalStyles};
-  ${paginationStyles};
   ${progressBarStyles};
 `;
 
