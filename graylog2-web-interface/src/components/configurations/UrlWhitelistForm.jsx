7bc9f47c184b135b1e41ec582e7719f9fa694baa
Ousmane Samba <ousmane@graylog.com>
2019-12-13 07:20:13 +0000

    Add regex validate to toolStore and refactore validation

--- graylog2-web-interface/src/components/configurations/UrlWhitelistForm.jsx
+++ graylog2-web-interface/src/components/configurations/UrlWhitelistForm.jsx
@@ -10,6 +10,9 @@ import ObjectUtils from 'util/ObjectUtils';
 import { get } from 'lodash';
 import FormUtils from 'util/FormsUtils';
 import type { Url, Config } from 'stores/configurations/ConfigurationsStore';
+import StoreProvider from 'injection/StoreProvider';
+
+const ToolsStore = StoreProvider.getStore('Tools');
 
 
 type Props = {
@@ -21,13 +24,16 @@ const UrlWhitelistForm = ({ urls, update, disabled }: Props) => {
   const literal = 'literal';
   const regex = 'regex';
   const options = [{ value: literal, label: 'Literal' }, { value: regex, label: 'Regex' }];
-  const inputs = {};
+  // eslint-disable-next-line prefer-const
+  let inputs = {};
   const [state, setState] = useState<Config>({ entries: urls, disabled });
   const [validationState, setValidationState] = useState({ errors: [] });
+
   const onAdd = (event: Event) => {
     event.preventDefault();
     setState({ ...state, entries: [...state.entries, { id: uuid(), title: '', value: '', type: literal }] });
   };
+
   const onRemove = (event: MouseEvent, idx: number) => {
     event.preventDefault();
     // eslint-disable-next-line prefer-const
@@ -35,6 +41,7 @@ const UrlWhitelistForm = ({ urls, update, disabled }: Props) => {
     stateUpdate.entries.splice(idx, 1);
     setState(stateUpdate);
   };
+
   const validURL = (str: string) => {
     let isValid = true;
     try {
@@ -45,66 +52,52 @@ const UrlWhitelistForm = ({ urls, update, disabled }: Props) => {
     }
     return isValid;
   };
-  const validRegex = (str: string) => {
+
+  const validate = (name: string, idx: number, type: string, value: string) => {
+    const validationUpdate = ObjectUtils.clone(validationState);
+    switch (name) {
+      case 'title':
+        validationUpdate.errors[idx].name = value.trim().length <= 0 ? { valid: false } : { valid: true };
+        break;
+      case 'value':
+        if (type === literal) {
+          validationUpdate.errors[idx].value = validURL(value) ? { valid: true } : { valid: false };
+        } else {
+          const promise = ToolsStore.testRegexValidity(value);
+          promise.then((result) => {
+            validationUpdate.errors[idx].value = result.is_valid ? { valid: true } : { valid: true };
+          });
+        }
+        break;
+      default:
+        break;
+    }
+    setValidationState(validationUpdate);
+    console.log(validationState);
+  };
+  const isFormValid = () => {
     let isValid = true;
-    try {
-      // eslint-disable-next-line no-unused-vars
-      const expr = new RegExp(str);
-    } catch (e) {
+    if (validationState.errors.find((el => el.title.valid === false || el.value.valid === false))) {
       isValid = false;
     }
     return isValid;
   };
-
-  const isValid = (input: string, value: string, type: string) => {
-    switch (input) {
-      case 'title':
-        return value.trim().length > 0;
-      case 'value':
-        if (type === literal) {
-          return validURL(value);
-        }
-        return validRegex(value);
-      default:
-        return true;
-    }
-  };
-  const validate = (input: string, value: string, type: string) => {
-    if (isValid(input, value, type)) {
-      return { valid: true };
-    }
-    return { valid: false };
+  const onInputChange = (event: SyntheticInputEvent<EventTarget>, idx: number, type: string) => {
+    console.log(inputs[event.target.name + idx]);
+    validate(event.target.name, idx, type, FormUtils.getValueFromInput(event.target));
+    // const stateUpdate = ObjectUtils.clone(state);
+    // stateUpdate.entries[idx][event.target.name] = FormUtils.getValueFromInput(event.target);
+    // setState(stateUpdate);
   };
 
-  const validateForm = () => {
-    let isFormValid = true;
-    const validationUpdate = ObjectUtils.clone(validationState);
-    state.entries.forEach((url, idx) => {
-      validationUpdate.errors[idx] = {
-        title: validate('title', url.title, url.type),
-        value: validate('value', url.value, url.type),
-      };
-    });
-    if (validationUpdate.errors.find((el => el.title.valid === false || el.value.valid === false))) {
-      isFormValid = false;
-    }
-    setValidationState(validationUpdate);
-    return isFormValid;
-  };
-  const onInputChange = (event: SyntheticInputEvent<EventTarget>, idx: number) => {
-    const stateUpdate = ObjectUtils.clone(state);
-    stateUpdate.entries[idx][event.target.name] = FormUtils.getValueFromInput(event.target);
-    setState(stateUpdate);
-  };
-
-  const onUpdateType = (idx, value: string) => {
+  const onUpdateType = (idx: number, value: string) => {
     const stateUpdate = ObjectUtils.clone(state);
     stateUpdate.entries[idx] = { ...state.entries[idx], value };
     setState(stateUpdate);
   };
 
   useEffect(() => {
-    const valid = validateForm();
+    const valid = true;
     update(state, valid);
   }, [state]);
 
@@ -134,20 +127,19 @@ const UrlWhitelistForm = ({ urls, update, disabled }: Props) => {
                 <td>{idx + 1}</td>
                 <td>
                   <Input type="text"
-                         ref={(elem) => { inputs[`titleRef${idx}`] = elem; }}
+                         ref={(elem) => { inputs[`title${idx}`] = elem; }}
                          name="title"
                          bsStyle={validationState.errors[idx] && validationState.errors[idx].title && !validationState.errors[idx].title.valid ? 'error' : null}
-                         help={get(validationState.errors[idx], 'title.valid', 'dsdsd')}
-                         onChange={event => onInputChange(event, idx)}
+                         onChange={event => onInputChange(event, idx, url.type)}
                          defaultValue={url.title}
                          required />
                 </td>
                 <td>
                   <Input type="text"
-                         ref={(elem) => { inputs[`urlRref${idx}`] = elem; }}
+                         ref={(elem) => { inputs[`value${idx}`] = elem; }}
                          name="value"
                          bsStyle={validationState.errors[idx] && validationState.errors[idx].value && !validationState.errors[idx].value.valid ? 'error' : null}
-                         onChange={event => onInputChange(event, idx)}
+                         onChange={event => onInputChange(event, idx, url.type)}
                          defaultValue={url.value}
                          required />
                 </td>
