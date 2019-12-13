9591619bb9ad857bbf1a297196730fd0a8ec99c9
Dennis Oelkers <dennis@graylog.com>
2019-12-13 15:21:57 +0100

    Turning ES5 component into functional one.

--- graylog2-web-interface/src/pages/ShowMessagePage.jsx
+++ graylog2-web-interface/src/pages/ShowMessagePage.jsx
@@ -1,7 +1,6 @@
 // @flow strict
 import PropTypes from 'prop-types';
-import React from 'react';
-import createReactClass from 'create-react-class';
+import React, { useEffect, useMemo, useState } from 'react';
 import * as Immutable from 'immutable';
 
 import ActionsProvider from 'injection/ActionsProvider';
@@ -21,82 +20,63 @@ const StreamsStore = StoreProvider.getStore('Streams');
 
 const ConnectedMessageDetail = connect(MessageDetail, { nodes: NodesStore }, ({ nodes }) => ({ nodes: Immutable.Map(nodes.nodes) }));
 
-const ShowMessagePage = createReactClass({
-  displayName: 'ShowMessagePage',
-
-  propTypes: {
-    params: PropTypes.shape({
-      index: PropTypes.string.isRequired,
-      messageId: PropTypes.string.isRequired,
-    }).isRequired,
-    searchConfig: PropTypes.object.isRequired,
-  },
-
-  getInitialState() {
-    return {
-      streams: Immutable.Map(),
-      message: undefined,
-      inputs: Immutable.Map(),
-    };
-  },
-
-  componentDidMount() {
-    const { params: { index, messageId } } = this.props;
+const ShowMessagePage = ({ params: { index, messageId }, params, searchConfig, nodes }) => {
+  const [message, setMessage] = useState();
+  const [inputs, setInputs] = useState();
+  const [streams, setStreams] = useState();
+  useEffect(() => { NodesActions.list(); }, []);
+  useEffect(() => {
     MessagesActions.loadMessage(index, messageId)
-      .then((message) => {
-        this.setState({ message });
-        return message.source_input_id ? InputsActions.get(message.source_input_id) : Promise.resolve();
+      .then((_message) => {
+        setMessage(_message);
+        return _message.source_input_id ? InputsActions.get(_message.source_input_id) : Promise.resolve();
       })
-      .then(this._formatInput);
-    StreamsStore.listStreams().then((streams) => {
-      const streamsMap = {};
-      if (streams) {
-        streams.forEach((stream) => {
-          streamsMap[stream.id] = stream;
-        });
-        this.setState({ streams: Immutable.Map(streamsMap) });
+      .then((input) => {
+        if (input) {
+          const newInputs = Immutable.Map({ [input.id]: input });
+          setInputs(newInputs);
+        }
+      });
+  }, [index, messageId]);
+  useEffect(() => {
+    StreamsStore.listStreams().then((newStreams) => {
+      if (newStreams) {
+        const streamsMap = newStreams.reduce((prev, stream) => ({ ...prev, [stream.id]: stream }), {});
+        setStreams(Immutable.Map(streamsMap));
       }
     });
-    NodesActions.list();
-  },
+  }, []);
+  const isLoaded = useMemo(() => (message && streams && inputs), [message, streams, inputs]);
 
-  _formatInput(input) {
-    if (input) {
-      const inputs = Immutable.Map({ [input.id]: input });
-      this.setState({ inputs });
-    }
-  },
+  if (isLoaded) {
+    return (
+      <DocumentTitle title={`Message ${params.messageId} on ${params.index}`}>
+        <Row className="content">
+          <Col md={12}>
+            <InteractiveContext.Provider value={false}>
+              <ConnectedMessageDetail fields={Immutable.Map()}
+                                      streams={streams}
+                                      disableSurroundingSearch
+                                      disableMessageActions
+                                      disableFieldActions
+                                      inputs={inputs}
+                                      message={message}
+                                      searchConfig={searchConfig} />
+            </InteractiveContext.Provider>
+          </Col>
+        </Row>
+      </DocumentTitle>
+    );
+  }
+  return <Spinner />;
+};
 
-  _isLoaded() {
-    const { message, streams, inputs } = this.state;
-    return message && streams && inputs;
-  },
+ShowMessagePage.propTypes = {
+  params: PropTypes.shape({
+    index: PropTypes.string.isRequired,
+    messageId: PropTypes.string.isRequired,
+  }).isRequired,
+    searchConfig: PropTypes.object.isRequired,
+};
 
-  render() {
-    if (this._isLoaded()) {
-      const { params, searchConfig } = this.props;
-      const { streams, inputs, message } = this.state;
-      return (
-        <DocumentTitle title={`Message ${params.messageId} on ${params.index}`}>
-          <Row className="content">
-            <Col md={12}>
-              <InteractiveContext.Provider value={false}>
-                <ConnectedMessageDetail fields={Immutable.Map()}
-                                        streams={streams}
-                                        disableSurroundingSearch
-                                        disableMessageActions
-                                        disableFieldActions
-                                        inputs={inputs}
-                                        message={message}
-                                        searchConfig={searchConfig} />
-              </InteractiveContext.Provider>
-            </Col>
-          </Row>
-        </DocumentTitle>
-      );
-    }
-    return <Spinner />;
-  },
-});
-
-export default ShowMessagePage;
+export default connect(ShowMessagePage, { nodes: NodesStore });
