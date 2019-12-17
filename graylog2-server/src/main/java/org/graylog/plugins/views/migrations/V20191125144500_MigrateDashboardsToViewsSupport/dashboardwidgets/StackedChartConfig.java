d56adf0d3952f9e9f77096b9ec2d69c977ce4890
Dennis Oelkers <dennis@graylog.com>
2019-12-17 14:28:25 +0100

    Adding link to upgrade notes in log warning.

--- graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/dashboardwidgets/StackedChartConfig.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/dashboardwidgets/StackedChartConfig.java
@@ -62,7 +62,9 @@ public abstract class StackedChartConfig extends WidgetConfigBase implements Wid
 
         if (seriesByQuery.size() > 1) {
             // TODO: Insert URL to changelog/upgrade notes with helpful advice
-            LOG.warn("Migrating dashboards to views: Encountered a stacked chart widget containing multiple distinct queries, splitting up into separate widgets");
+            LOG.warn("Migrating dashboards to views: Encountered a stacked chart widget containing multiple distinct " +
+                    "queries, splitting up into separate widgets. Read more about it here: " +
+                    "https://github.com/Graylog2/graylog2-server/blob/master/UPGRADING.rst#upgrading-to-graylog-32x");
         }
 
         return seriesByQuery.entrySet().stream()
