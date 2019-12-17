6558dd28957ac4182a4a9b8f877f067b31264ba0
Linus Pahl <lpahl@protonmail.com>
2019-12-17 15:43:49 +0100

    Disable eslint import/no-cycle for views pages index

--- graylog2-web-interface/src/views/pages/index.js
+++ graylog2-web-interface/src/views/pages/index.js
@@ -2,11 +2,13 @@ import loadAsync from 'routing/loadAsync';
 
 const DashboardsPage = loadAsync(() => import(/* webpackChunkname: "DashboardsPage" */ './DashboardsPage'));
 const ExtendedSearchPage = loadAsync(() => import(/* webpackChunkName: "ExtendedSearchPage" */ './ExtendedSearchPage'));
+const ViewManagementPage = loadAsync(() => import(/* webpackChunkName: "ViewManagementPage" */ './ViewManagementPage'));
+/* eslint-disable import/no-cycle */
 const NewSearchPage = loadAsync(() => import(/* webpackChunkName: "NewSearchPage" */ './NewSearchPage'));
 const StreamSearchPage = loadAsync(() => import(/* webpackChunkName: "StreamSearchPage" */ './StreamSearchPage'));
 const NewDashboardPage = loadAsync(() => import(/* webpackChunkName: "NewDashboardPage" */ './NewDashboardPage'));
 const ShowViewPage = loadAsync(() => import(/* webpackChunkName: "ShowViewPage" */ './ShowViewPage'));
-const ViewManagementPage = loadAsync(() => import(/* webpackChunkName: "ViewManagementPage" */ './ViewManagementPage'));
+/* eslint-enable import/no-cycle */
 
 export {
   DashboardsPage,
