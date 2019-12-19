32e5619b1a3599a9402e288249886ec01f4256ee
Dennis Oelkers <dennis@graylog.com>
2019-12-19 09:38:31 +0100

    Wrapping alert in container, adding flow types.

--- graylog2-web-interface/src/views/components/messagelist/decorators/DecoratorList.jsx
+++ graylog2-web-interface/src/views/components/messagelist/decorators/DecoratorList.jsx
@@ -1,13 +1,27 @@
+// @flow strict
 import PropTypes from 'prop-types';
-import React from 'react';
+import * as React from 'react';
+import styled from 'styled-components';
 
 import { Alert } from 'components/graylog';
 import { Icon, SortableList } from 'components/common';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import DecoratorStyles from '!style!css!./decoratorStyles.css';
+import type { Decorator } from './Types';
 
-class DecoratorList extends React.Component {
+const AlertContainer: React.ComponentType<{}> = styled.div`
+  margin-bottom: 20px;
+`;
+
+type ReorderedItems = Array<{ id: string }>;
+type Props = {
+  decorators: Array<Decorator>,
+  disableDragging?: boolean,
+  onReorder: (ReorderedItems) => mixed,
+};
+
+class DecoratorList extends React.Component<Props> {
   static propTypes = {
     decorators: PropTypes.arrayOf(PropTypes.object).isRequired,
     disableDragging: PropTypes.bool,
@@ -19,18 +33,20 @@ class DecoratorList extends React.Component {
     onReorder: () => {},
   };
 
-  _onReorderWrapper = (...args) => {
+  _onReorderWrapper = (orderedItems: ReorderedItems) => {
     const { onReorder } = this.props;
-    onReorder(...args);
+    onReorder(orderedItems);
   };
 
   render() {
     const { decorators, disableDragging } = this.props;
     if (!decorators || decorators.length === 0) {
       return (
-        <Alert bsStyle="info" className={DecoratorStyles.noDecoratorsAlert}>
-          <Icon name="info-circle" />&nbsp;No decorators configured.
-        </Alert>
+        <AlertContainer>
+          <Alert bsStyle="info" className={DecoratorStyles.noDecoratorsAlert}>
+            <Icon name="info-circle" />&nbsp;No decorators configured.
+          </Alert>
+        </AlertContainer>
       );
     }
     return (
