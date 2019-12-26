4b2c3dec23be10bc457f140c910c812010ffa9af
Kyle Knight <kyle@graylog.com>
2019-12-26 11:58:49 -0600

    Moving global styles to styled-component css

--- graylog2-web-interface/test/WrappingContainer.jsx
+++ graylog2-web-interface/test/WrappingContainer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 
+import GlobalThemeStyles from 'theme/GlobalThemeStyles';
+
 /**
  * NOTE: `ThemeProvider` will be removed in future iterations and the actual global provider will be used
  */
@@ -9,7 +11,10 @@ import { ThemeProvider } from 'styled-components';
 const WrappingContainer = ({ children }) => {
   return (
     <ThemeProvider theme={{ mode: 'teinte' }}>
-      {children}
+      <>
+        <GlobalThemeStyles />
+        {children}
+      </>
     </ThemeProvider>
   );
 };
