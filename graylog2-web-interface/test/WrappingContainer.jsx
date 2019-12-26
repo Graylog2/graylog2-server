183e1afc91c76d9961eaf7b93746812179676b98
Kyle Knight <kyle@graylog.com>
2019-12-26 12:30:31 -0600

    Revert test wrap

--- graylog2-web-interface/test/WrappingContainer.jsx
+++ graylog2-web-interface/test/WrappingContainer.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 
-import GlobalThemeStyles from 'theme/GlobalThemeStyles';
-
 /**
  * NOTE: `ThemeProvider` will be removed in future iterations and the actual global provider will be used
  */
@@ -11,10 +9,7 @@ import GlobalThemeStyles from 'theme/GlobalThemeStyles';
 const WrappingContainer = ({ children }) => {
   return (
     <ThemeProvider theme={{ mode: 'teinte' }}>
-      <>
-        <GlobalThemeStyles />
-        {children}
-      </>
+      {children}
     </ThemeProvider>
   );
 };
