468b2d53dfffa9d1728c89426e5a19db8f65fb6b
Kyle Knight <kyle@graylog.com>
2020-01-06 11:31:31 -0600

    react-bootstrap-0.33.1

--- graylog2-web-interface/src/components/graylog/Panel.jsx
+++ graylog2-web-interface/src/components/graylog/Panel.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 // eslint-disable-next-line no-restricted-imports
 import { Panel as BootstrapPanel } from 'react-bootstrap';
@@ -6,43 +8,105 @@ import { adjustHue, darken } from 'polished';
 import { teinte, util } from 'theme';
 import bsStyleThemeVariant from './variants/bsStyle';
 
-const panelVariantStyles = (hex) => {
-  const backgroundColor = util.colorLevel(hex, -9);
-  const borderColor = darken(0.05, adjustHue(-10, hex));
+const backgroundColor = hex => util.colorLevel(hex, -9);
+const borderColor = hex => darken(0.05, adjustHue(-10, hex));
 
-  return css`
-    border-color: ${borderColor};
+const PanelHeading = styled(BootstrapPanel.Heading)``;
 
-    & > .panel-heading {
-      color: ${util.colorLevel(backgroundColor, 9)};
-      background-color: ${backgroundColor};
-      border-color: ${borderColor};
+const PanelFooter = styled(BootstrapPanel.Footer)`
+  background-color: ${teinte.secondary.tre};
+  border-top-color: ${teinte.secondary.due};
+`;
 
-      + .panel-collapse > .panel-body {
-        border-top-color: ${borderColor};
-      }
-      .badge {
-        color: ${backgroundColor};
-        background-color: ${hex};
-      }
+const panelVariantStyles = hex => css`
+  border-color: ${borderColor(hex)};
+
+  & > ${PanelHeading} {
+    color: ${util.colorLevel(backgroundColor(hex), 9)};
+    background-color: ${backgroundColor(hex)};
+    border-color: ${borderColor(hex)};
+
+    > .panel-title,
+    > .panel-title > * {
+      font-size: 16px;
     }
-    & > .panel-footer {
-      + .panel-collapse > .panel-body {
-        border-bottom-color: ${borderColor};
-      }
+
+    + .panel-collapse > .panel-body {
+      border-top-color: ${borderColor(hex)};
     }
-  `;
-};
+
+    .badge {
+      color: ${backgroundColor(hex)};
+      background-color: ${hex};
+    }
+  }
+
+  & > ${PanelFooter} {
+    + .panel-collapse > .panel-body {
+      border-bottom-color: ${borderColor(hex)};
+    }
+  }
+`;
 
 const StyledPanel = styled(BootstrapPanel)`
   background-color: ${teinte.primary.due};
 
+  .panel-group {
+    ${PanelHeading} {
+      + .panel-collapse > .panel-body,
+      + .panel-collapse > .list-group {
+        border-top-color: ${teinte.secondary.due};
+      }
+    }
+
+    ${PanelFooter} {
+      + .panel-collapse .panel-body {
+        border-bottom-color: ${teinte.secondary.due};
+      }
+    }
+  }
+
+  ${bsStyleThemeVariant(panelVariantStyles)};
+`;
+
+const deprecatedVariantStyles = hex => css`
+  /** NOTE: Deprecated & should be removed in 4.0 */
+  border-color: ${borderColor(hex)};
+
+  & > .panel-heading {
+    color: ${util.colorLevel(backgroundColor(hex), 9)};
+    background-color: ${backgroundColor(hex)};
+    border-color: ${borderColor(hex)};
+
+    > .panel-title,
+    > .panel-title > * {
+      font-size: 16px;
+    }
+
+    + .panel-collapse > .panel-body {
+      border-top-color: ${borderColor(hex)};
+    }
+    .badge {
+      color: ${backgroundColor(hex)};
+      background-color: ${hex};
+    }
+  }
+
+  & > .panel-footer {
+    + .panel-collapse > .panel-body {
+      border-bottom-color: ${borderColor(hex)};
+    }
+  }
+`;
+
+const DeprecatedStyledPanel = styled(BootstrapPanel)`
+  /** NOTE: Deprecated & should be removed in 4.0 */
+  background-color: ${teinte.primary.due};
 
   .panel-footer {
     background-color: ${teinte.secondary.tre};
     border-top-color: ${teinte.secondary.due};
   }
-
   .panel-group {
     .panel-heading {
       + .panel-collapse > .panel-body,
@@ -50,7 +114,6 @@ const StyledPanel = styled(BootstrapPanel)`
         border-top-color: ${teinte.secondary.due};
       }
     }
-
     .panel-footer {
       + .panel-collapse .panel-body {
         border-bottom-color: ${teinte.secondary.due};
@@ -58,7 +121,121 @@ const StyledPanel = styled(BootstrapPanel)`
     }
   }
 
-  ${bsStyleThemeVariant(panelVariantStyles)}
+  ${bsStyleThemeVariant(deprecatedVariantStyles)}
 `;
 
-export default StyledPanel;
+const Panel = ({
+  title,
+  children,
+  collapsible,
+  defaultExpanded,
+  expanded,
+  footer,
+  header,
+  onToggle,
+  ...props
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  React.useEffect(() => {
+    setIsExpanded((defaultExpanded && expanded)
+      || (!defaultExpanded && expanded)
+      || (defaultExpanded && isExpanded === expanded));
+  }, [expanded]);
+
+  const handleToggle = (nextIsExpanded) => {
+    setIsExpanded(nextIsExpanded);
+    onToggle(nextIsExpanded);
+  };
+
+  const hasDeprecatedChildren = typeof children === 'string' || (Array.isArray(children) && typeof children[0] === 'string');
+
+  if (header || footer || title || collapsible || hasDeprecatedChildren) {
+    /** NOTE: Deprecated & should be removed in 4.0 */
+    useEffect(() => {
+      /* eslint-disable-next-line no-console */
+      console.warn('Graylog Notice: ', 'You have used a deprecated `Panel` prop, please check the documentation to use the latest `Panel`.');
+    }, []);
+
+    return (
+      /* NOTE: this exists as a deprecated render for older Panel instances */
+      <DeprecatedStyledPanel expanded={isExpanded}
+                             onToggle={handleToggle}
+                             {...props}>
+        {(header || title) && (
+          <DeprecatedStyledPanel.Heading>
+            <Panel.Title toggle={collapsible}>
+              {header || title}
+            </Panel.Title>
+          </DeprecatedStyledPanel.Heading>
+        )}
+        <DeprecatedStyledPanel.Body collapsible={collapsible}>
+          {children}
+        </DeprecatedStyledPanel.Body>
+        {footer && (
+          <DeprecatedStyledPanel.Footer>{footer}</DeprecatedStyledPanel.Footer>
+        )}
+      </DeprecatedStyledPanel>
+    );
+  }
+
+  return (
+    <StyledPanel expanded={isExpanded}
+                 onToggle={handleToggle}
+                 defaultExpanded={defaultExpanded}
+                 {...props}>
+      {children}
+    </StyledPanel>
+  );
+};
+
+Panel.propTypes = {
+  children: PropTypes.any.isRequired,
+  /** @deprecated No longer used, replace with `<Panel.Collapse />` &  `expanded`. */
+  collapsible: PropTypes.bool,
+  /**
+   * Default of `expanded` prop
+   *
+   * @controllable onToggle
+   */
+  defaultExpanded: PropTypes.bool,
+  /**
+   * Controls the collapsed/expanded state ofthe Panel. Requires
+   * a `Panel.Collapse` or `<Panel.Body collapsible>` child component
+   * in order to actually animate out or in.
+   *
+   * @controllable onToggle
+   */
+  expanded: PropTypes.bool,
+  /**
+   * A callback fired when the collapse state changes.
+   *
+   * @controllable expanded
+   */
+  onToggle: PropTypes.func,
+  /** @deprecated No longer used, replace with `<Panel.Footer />`. */
+  footer: PropTypes.string,
+  /** @deprecated No longer used, replace with `<Panel.Heading />`. */
+  header: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** @deprecated No longer used, replace with `<Panel.Title />`. */
+  title: PropTypes.string,
+};
+
+Panel.defaultProps = {
+  collapsible: false,
+  defaultExpanded: null,
+  expanded: false,
+  footer: undefined,
+  header: undefined,
+  onToggle: () => {},
+  title: undefined,
+};
+
+Panel.Body = BootstrapPanel.Body;
+Panel.Collapse = BootstrapPanel.Collapse;
+Panel.Footer = PanelFooter;
+Panel.Heading = PanelHeading;
+Panel.Title = BootstrapPanel.Title;
+Panel.Toggle = BootstrapPanel.Toggle;
+
+export default Panel;
--- graylog2-web-interface/src/views/components/widgets/CopyToDashboardForm.test.jsx
+++ graylog2-web-interface/src/views/components/widgets/CopyToDashboardForm.test.jsx
@@ -74,16 +74,19 @@ describe('CopyToDashboardForm', () => {
     expect(onSubmit).toHaveBeenCalledWith('widget-id', 'view-1');
   });
 
-  it('should handle search of dashboards', () => {
+  it('should query for all dashboards & specific dashboards', () => {
     DashboardsActions.search = mockAction(jest.fn(() => Promise.resolve()));
-    const { getByPlaceholderText, getByText } = render(<CopyToDashboardForm dashboards={dashboardState}
-                                                                            widgetId="widget-id"
-                                                                            onCancel={() => {}}
-                                                                            onSubmit={() => {}} />);
+    const { getByPlaceholderText, getByText } = render(
+      <CopyToDashboardForm dashboards={dashboardState}
+                           widgetId="widget-id"
+                           onCancel={() => {}}
+                           onSubmit={() => {}} />,
+    );
+    expect(DashboardsActions.search).toHaveBeenCalledTimes(1);
     const searchInput = getByPlaceholderText('Enter search query...');
     fireEvent.change(searchInput, { target: { value: 'view 1' } });
     const searchButton = getByText('Search');
     fireEvent.click(searchButton);
-    expect(DashboardsActions.search).toHaveBeenCalledTimes(1);
+    expect(DashboardsActions.search).toHaveBeenCalledTimes(2);
   });
 });
