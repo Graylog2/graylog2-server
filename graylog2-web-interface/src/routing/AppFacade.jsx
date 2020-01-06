8c61a5611ea51bd239b22e6193c86ba7f056d088
Kyle Knight <kyle@graylog.com>
2020-01-06 11:31:31 -0600

    react-bootstrap-0.33.1

--- graylog2-web-interface/src/routing/AppFacade.jsx
+++ graylog2-web-interface/src/routing/AppFacade.jsx
@@ -6,6 +6,7 @@ import ServerUnavailablePage from 'pages/ServerUnavailablePage';
 import StoreProvider from 'injection/StoreProvider';
 import connect from 'stores/connect';
 import GraylogThemeProvider from 'theme/GraylogThemeProvider';
+import GlobalThemeStyles from 'theme/GlobalThemeStyles';
 
 import 'bootstrap/less/bootstrap.less';
 import 'opensans-npm-webfont';
@@ -48,7 +49,10 @@ export const AppFacade = ({ currentUser, server, sessionId }) => {
 
   return (
     <GraylogThemeProvider>
-      {Page}
+      <>
+        <GlobalThemeStyles />
+        {Page}
+      </>
     </GraylogThemeProvider>
   );
 };
