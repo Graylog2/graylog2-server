33c4d96e0bdf2ce8e208762a264b67684edef332
Kyle Knight <kyle@graylog.com>
2020-01-06 12:30:12 -0600

    theme_documentation

---
+++ graylog2-web-interface/src/theme/docs/ThemeProvider.md
@@ -0,0 +1,3 @@
+`ThemeProvider` is created in `theme/GraylogThemeContext.jsx` and is used in `routing/AppFacade.jsx` to wrap the entire application.
+
+This will allow developers to access all Theme variables at any time. The Theme object contains multiple child-objects that we can use throughout the Graylog application.
--- graylog2-web-interface/src/theme/util/readableColor.js
+++ graylog2-web-interface/src/theme/util/readableColor.js
@@ -4,8 +4,7 @@ import teinte from '../teinte'; // TODO: replace this with whatever is coming fr
 
 export default function readableColor(color, darkColor = teinte.primary.tre, lightColor = teinte.primary.due) {
   /**
-   * Recreating `color-level` from Bootstrap's SCSS functions
-   * https://github.com/twbs/bootstrap/blob/08ba61e276a6393e8e2b97d56d2feb70a24fe22c/scss/_functions.scss#L97
+   * Generating a readable color following W3C specs using [polished](https://polished.js.org/docs/#readablecolor)
    *
    * @param {string} color - any string that represents a color (ex: "#f00" or "rgb(255, 0, 0)")
    * @param {string} darkColor - defaults to theme's darkest gray
