1f6dedd93d53d2d57c113a9bd6d850695c140421
Claudia Ortiz <claudia@graylog.com>
2020-01-06 11:40:59 -0600

    Add listIsSet and listfield check

--- graylog2-server/src/main/java/org/graylog2/plugin/configuration/ConfigurationRequest.java
+++ graylog2-server/src/main/java/org/graylog2/plugin/configuration/ConfigurationRequest.java
@@ -115,6 +115,12 @@ public class ConfigurationRequest {
                         }
                         break;
                     case TextField.FIELD_TYPE:
+                        break;
+                    case ListField.FIELD_TYPE:
+                        if (!configuration.listIsSet(fieldName)) {
+                            throw new ConfigurationException("Mandatory configuration field \"" + fieldName + "\" is missing or has the wrong data type");
+                        }
+                        break;
                     case DropdownField.FIELD_TYPE:
                         if (!configuration.stringIsSet(fieldName)) {
                             throw new ConfigurationException("Mandatory configuration field \"" + fieldName + "\" is missing or has the wrong data type");
@@ -152,7 +158,11 @@ public class ConfigurationRequest {
                     }
                     break;
                 case TextField.FIELD_TYPE:
+                    break;
                 case ListField.FIELD_TYPE:
+                    if(config.listIsSet(name)){
+                        values.put(name,config.getList(name));
+                    }
                     break;
                 case DropdownField.FIELD_TYPE:
                     if (config.stringIsSet(name)) {
