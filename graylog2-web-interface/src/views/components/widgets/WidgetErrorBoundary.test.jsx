63e26b6ce462cfb0a062152ec2a9c9b931d92a4b
Linus Pahl <lpahl@protonmail.com>
2020-01-08 13:35:58 +0100

    Fix typo

--- graylog2-web-interface/src/views/components/widgets/WidgetErrorBoundary.test.jsx
+++ graylog2-web-interface/src/views/components/widgets/WidgetErrorBoundary.test.jsx
@@ -34,7 +34,7 @@ describe('WidgetErrorBoundary', () => {
     asMock(console.error).mockRestore();
   });
 
-  it('passes own props to child component', () => {
+  it('passes own props to its children', () => {
     const Component = props => <div data-testid="child-component-test-id" {...props} />;
     const { getByTestId } = render((
       <WidgetErrorBoundary extraProp="The extra prop">
