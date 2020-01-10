b3010c553a472a23b4522c26fff96f1bba6577da
Dennis Oelkers <dennis@graylog.com>
2020-01-10 16:04:56 +0100

    Zoom global override when zooming in timeline chart on dashboard.
    Prior to this change, zooming (dragging a range) on a timeline chart on
    a dashboard did nothing, because it set the query's time range which is
    not considered on dashboards.
    
    This change is now conditionally setting the global override's time
    range if on a dashboard and the query's time range if not. It does this
    by checking the view's type in the zoom handler.
    
    Ideally we should instead provide a `DrilldownContext` for all actions,
    that provides commonly used actions/states (setting/getting current time
    range, query, stream selection), so actions do not determine on their
    own which one to pick.

--- graylog2-web-interface/src/views/components/visualizations/OnZoom.js
+++ graylog2-web-interface/src/views/components/visualizations/OnZoom.js
@@ -1,12 +1,16 @@
 // @flow strict
 import moment from 'moment-timezone';
-import { QueriesActions } from 'views/stores/QueriesStore';
 import CombinedProvider from 'injection/CombinedProvider';
+
+import { QueriesActions } from 'views/stores/QueriesStore';
 import Query from 'views/logic/queries/Query';
+import type { ViewType } from 'views/logic/views/View';
+import View from 'views/logic/views/View';
+import { GlobalOverrideActions } from 'views/stores/GlobalOverrideStore';
 
 const { CurrentUserStore } = CombinedProvider.get('CurrentUser');
 
-const onZoom = (currentQuery: Query, from: string, to: string) => {
+const onZoom = (currentQuery: Query, from: string, to: string, viewType: ?ViewType) => {
   const { timezone } = CurrentUserStore.get();
 
   const newTimerange = {
@@ -15,7 +19,11 @@ const onZoom = (currentQuery: Query, from: string, to: string) => {
     to: moment.tz(to, timezone).toISOString(),
   };
 
-  QueriesActions.timerange(currentQuery.id, newTimerange);
+  const action = viewType === View.Type.Dashboard
+    ? GlobalOverrideActions.timerange
+    : timerange => QueriesActions.timerange(currentQuery.id, timerange);
+
+  action(newTimerange);
   return false;
 };
 
