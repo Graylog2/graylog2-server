b144aaf6dcff865744473a5d0a777f949a59b457
Linus Pahl <46300478+linuspahl@users.noreply.github.com>
2020-01-20 10:51:23 +0100

    Implement overlay for select menu (#7159)
    * Implement react overlay for select menu
    
    * Remove not needed Select menu overlay props
    
    * Add needed showUpdatePosition for Select menu overview
    
    * Use unit when defining Select menu width
    
    * Break long option names inside Select menu
    
    * Implement useMemo for Select menu style creation

--- graylog2-web-interface/src/views/components/Select.jsx
+++ graylog2-web-interface/src/views/components/Select.jsx
@@ -1,7 +1,10 @@
 // @flow strict
-import * as React from 'react';
+import React, { useRef, useMemo } from 'react';
+import type { Node, ElementType } from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import ReactSelect, { components as Components } from 'react-select';
+import { Overlay } from 'react-overlays';
 
 const MultiValueRemove = (props) => {
   return (
@@ -11,10 +14,20 @@ const MultiValueRemove = (props) => {
   );
 };
 
-const valueContainer = base => ({
-  ...base,
-  minWidth: '6.5vw',
-});
+const OverlayInner = ({ children, style }: {children: Node, style?: Object}) => React.Children.map(children,
+  child => React.cloneElement(child, { style: Object.assign({}, style, child.props.style) }));
+
+const menu = selectRef => (base) => {
+  const defaultMinWidth = 200;
+  const containerWidth = get(selectRef, 'current.select.controlRef.offsetWidth') || 0;
+  const width = containerWidth > defaultMinWidth ? containerWidth : defaultMinWidth;
+  return {
+    ...base,
+    position: 'relative',
+    width: `${width}px`,
+  };
+};
+
 
 const multiValue = base => ({
   ...base,
@@ -40,31 +53,65 @@ const multiValueRemove = base => ({
   },
 });
 
+const option = base => ({
+  ...base,
+  wordWrap: 'break-word',
+});
+
+const valueContainer = base => ({
+  ...base,
+  minWidth: '6.5vw',
+});
+
 type Props = {
-  components: { [string]: React.ElementType },
+  components: { [string]: ElementType },
   styles: { [string]: any }
 };
-
 
 const ValueWithTitle = (props: {data: { label: string }}) => {
   const { data: { label } } = props;
   return <Components.MultiValue {...props} innerProps={{ title: label }} />;
 };
 
+const MenuOverlay = selectRef => (props) => {
+  const listStyle = {
+    zIndex: 1050,
+    position: 'absolute',
+  };
+  return (
+    <Overlay placement="bottom"
+             shouldUpdatePosition
+             show
+             target={selectRef.current}>
+      <OverlayInner>
+        <div style={listStyle}>
+          <Components.Menu {...props} />
+        </div>
+      </OverlayInner>
+    </Overlay>
+  );
+};
+
 const Select = ({ components, styles, ...rest }: Props) => {
+  const selectRef = useRef(null);
+  const Menu = useMemo(() => MenuOverlay(selectRef), [selectRef]);
+  const menuStyle = useMemo(() => menu(selectRef), [selectRef]);
   const _components = {
+    Menu,
     MultiValueRemove,
     MultiValue: components.MultiValue || ValueWithTitle,
     ...components,
   };
   const _styles = {
+    menu: menuStyle,
     multiValue,
     multiValueLabel,
     multiValueRemove,
+    option,
     valueContainer,
     ...styles,
   };
-  return <ReactSelect {...rest} components={_components} styles={_styles} tabSelectsValue={false} />;
+  return <ReactSelect {...rest} components={_components} styles={_styles} tabSelectsValue={false} ref={selectRef} />;
 };
 
 Select.propTypes = {
