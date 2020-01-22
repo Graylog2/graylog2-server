d0785e75397d97f7be0e17e465480166c8b5db2d
Linus Pahl <lpahl@protonmail.com>
2020-01-22 13:20:15 +0100

    Implement BootstrapModalForm for ViewPropertiesModal
    - add html validation which prevents the user from submitting invalid values

--- graylog2-web-interface/src/views/components/views/ViewPropertiesModal.jsx
+++ graylog2-web-interface/src/views/components/views/ViewPropertiesModal.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { isEqual } from 'lodash';
-
-import { Modal, Button } from 'components/graylog';
 import FormsUtils from 'util/FormsUtils';
+
+import BootstrapModalForm from 'components/bootstrap/BootstrapModalForm';
 import Input from 'components/bootstrap/Input';
 
 export default class ViewPropertiesModal extends React.Component {
   static propTypes = {
     view: PropTypes.object.isRequired,
-    show: PropTypes.bool,
     title: PropTypes.string.isRequired,
     onSave: PropTypes.func.isRequired,
     onClose: PropTypes.func,
@@ -17,30 +16,32 @@ export default class ViewPropertiesModal extends React.Component {
 
   static defaultProps = {
     onClose: () => {},
-    show: false,
   };
 
   constructor(props) {
     super(props);
     this.state = {
       view: props.view,
-      show: props.show,
       title: props.title,
     };
   }
 
   componentWillReceiveProps(nextProps) {
-    const { show, title } = this.props;
+    const { title } = this.props;
     const { view } = this.state;
-    if (show !== nextProps.show || title !== nextProps.title || !isEqual(view, nextProps.view)) {
-      this.setState({ view: nextProps.view, title: nextProps.title, show: nextProps.show });
+    if (title !== nextProps.title || !isEqual(view, nextProps.view)) {
+      this.setState({ view: nextProps.view, title: nextProps.title });
     }
   }
 
   // eslint-disable-next-line consistent-return
   _onChange = (event) => {
     const { name } = event.target;
-    const value = FormsUtils.getValueFromInput(event.target);
+    let value = FormsUtils.getValueFromInput(event.target);
+    const trimmedValue = value.trim();
+    if (trimmedValue === '') {
+      value = trimmedValue;
+    }
 
     switch (name) {
       case 'title': return this.setState(state => ({ view: state.view.toBuilder().title(value).build() }));
@@ -63,40 +64,37 @@ export default class ViewPropertiesModal extends React.Component {
   };
 
   render() {
-    const { show, view, title } = this.state;
+    const { view: { title = '', summary = '', description = '' }, title: modalTitle } = this.state;
     return (
-      <Modal show={show} bsSize="large" onHide={this.handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Input id="title"
-                 type="text"
-                 name="title"
-                 label="Title"
-                 help="The title of the dashboard."
-                 onChange={this._onChange}
-                 value={view.title} />
-          <Input id="summary"
-                 type="text"
-                 name="summary"
-                 label="Summary"
-                 help="A helpful summary of the dashboard."
-                 onChange={this._onChange}
-                 value={view.summary} />
-          <Input id="description"
-                 type="textarea"
-                 name="description"
-                 label="Description"
-                 help="A longer, helpful description of the dashboard and its functionality."
-                 onChange={this._onChange}
-                 value={view.description} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this._onSave} bsStyle="success">Save</Button>
-          <Button onClick={this._onClose}>Cancel</Button>
-        </Modal.Footer>
-      </Modal>
+      <BootstrapModalForm title={modalTitle}
+                          onSubmitForm={this._onSave}
+                          onModalClose={this._onClose}
+                          submitButtonText="Save"
+                          show
+                          bsSize="large">
+        <Input id="title"
+               type="text"
+               name="title"
+               label="Title"
+               help="The title of the dashboard."
+               required
+               onChange={this._onChange}
+               value={title} />
+        <Input id="summary"
+               type="text"
+               name="summary"
+               label="Summary"
+               help="A helpful summary of the dashboard."
+               onChange={this._onChange}
+               value={summary} />
+        <Input id="description"
+               type="textarea"
+               name="description"
+               label="Description"
+               help="A longer, helpful description of the dashboard and its functionality."
+               onChange={this._onChange}
+               value={description} />
+      </BootstrapModalForm>
     );
   }
 }
