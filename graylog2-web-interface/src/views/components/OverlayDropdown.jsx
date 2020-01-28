3a7e8e867b09cd6dad8aa2b6ff0de54030ab8944
Kyle Knight <kyle@graylog.com>
2020-01-28 15:34:34 -0600

    Getting width of dropdown to reposition

--- graylog2-web-interface/src/views/components/OverlayDropdown.jsx
+++ graylog2-web-interface/src/views/components/OverlayDropdown.jsx
@@ -1,77 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { debounce } from 'lodash';
 
-import { Overlay } from 'react-overlays';
+import { Overlay, Transition } from 'react-overlays';
 
 import styles from './OverlayDropdown.css';
+
+const StyledList = styled.ul(({ show, minWidth }) => `
+  padding-left: 5px;
+  padding-right: 5px;
+  color: #666666;
+  z-index: 1050;
+  min-width: ${minWidth};
+  display: ${show ? 'block' : 'none'};
+`);
+
+const oppositePlacement = {
+  left: 'right',
+  right: 'left',
+};
 
 const FilterProps = ({ children, style }) => React.Children.map(children,
   child => React.cloneElement(child, { style: Object.assign({}, style, child.props.style) }));
 
 const OverlayDropdown = ({ children, menuContainer, onToggle, placement, show, toggle, minDropdownWidth }) => {
   const [currentPlacement, setCurrentPlacement] = useState(placement);
-  const [bodyWidth, setBodyWidth] = useState(document.body.clientWidth);
-  const StyledList = styled.ul`
-    padding-left: 5px;
-    padding-right: 5px;
-    color: #666666;
-    z-index: 1050;
-    min-width: ${minDropdownWidth};
-    display: ${show ? 'block' : 'none'};
-  `;
-
   const toggleTarget = React.createRef();
-  const oppositePlacement = {
-    left: 'right',
-    right: 'left',
-  };
 
-  const handleToggle = (event) => {
-    if (event.clientX + minDropdownWidth >= bodyWidth) {
+  const handleOverlayEntering = (dropdownElem) => {
+    const dropdownLeft = dropdownElem.offsetLeft;
+    const dropdownWidth = dropdownElem.offsetWidth;
+
+    if (dropdownLeft + dropdownWidth >= document.body.clientWidth) {
       setCurrentPlacement(oppositePlacement[currentPlacement]);
-      onToggle(event);
-    } else {
-      setCurrentPlacement(placement);
-      onToggle(event);
     }
   };
 
-  const handleBrowserResize = debounce(() => {
-    setBodyWidth(document.body.clientWidth);
-  }, 400);
-
-  useEffect(() => {
-    window.addEventListener('resize', handleBrowserResize, false);
-
-    return () => {
-      window.removeEventListener('resize', handleBrowserResize);
-    };
-  }, []);
-
   return (
     <>
-      <span onClick={handleToggle}
+      <span onClick={onToggle}
             ref={toggleTarget}
             role="presentation"
             className={styles.dropdowntoggle}>
         {toggle}<span className="caret" />
       </span>
-      <Overlay show={show}
-               container={menuContainer}
-               containerPadding={10}
-               placement={currentPlacement}
-               shouldUpdatePosition
-               rootClose
-               onHide={handleToggle}
-               target={() => toggleTarget.current}>
-        <FilterProps>
-          <StyledList className="dropdown-menu">
-            {children}
-          </StyledList>
-        </FilterProps>
-      </Overlay>
+      {show && (
+        <Overlay show={show}
+                 container={menuContainer}
+                 containerPadding={10}
+                 placement={currentPlacement}
+                 shouldUpdatePosition
+                 rootClose
+                 onHide={onToggle}
+                 target={() => toggleTarget.current}
+                 transition={Transition}
+                 onEntering={handleOverlayEntering}>
+          <FilterProps>
+            <StyledList className="dropdown-menu"
+                        minWidth={minDropdownWidth}
+                        show={show}>
+              {children}
+            </StyledList>
+          </FilterProps>
+        </Overlay>
+      )}
+
     </>
   );
 };
@@ -83,7 +76,7 @@ OverlayDropdown.propTypes = {
   placement: PropTypes.string,
   show: PropTypes.bool.isRequired,
   toggle: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
-  minDropdownWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.string]),
+  minDropdownWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
 
 OverlayDropdown.defaultProps = {
