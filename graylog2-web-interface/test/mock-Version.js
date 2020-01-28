7caa2b2d14d0d1973681a6ac80eac8ea5280c151
Linus Pahl <lpahl@protonmail.com>
2020-01-28 17:19:46 +0100

    Define version mock content which is more obvious a mock

--- graylog2-web-interface/test/mock-Version.js
+++ graylog2-web-interface/test/mock-Version.js
@@ -1,8 +1,8 @@
 // We are mocking the application version, to avoid failing snapshot tests after a version change.
 
 const mockDefaultExport = {
-  getMajorAndMinorVersion: jest.fn(() => '1.0'),
-  getFullVersion: jest.fn(() => '1.0.0-SNAPSHOT'),
+  getMajorAndMinorVersion: jest.fn(() => 'MAJOR_AND_MINOR_VERSION_MOCK'),
+  getFullVersion: jest.fn(() => 'FULL_VERSION_MOCK'),
 };
 
 jest.mock('util/Version', () => mockDefaultExport);
