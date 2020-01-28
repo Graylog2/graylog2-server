6543da1b6cf704001461e7cf5dafdd9034044916
Konrad Merz <konrad@graylog.com>
2020-01-28 09:33:45 +0100

    Migrate existing data structures for color charts
    Prior to this change, we changed the data structure of color charts
    from a generic Map to auto value classes. This change prevents
    existing installations to run.
    
    This change will migrate the existing datastructures in the mongodb
    to the new auto value classes.

---
+++ graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20190127111728_MigrateWidgetFormatSettings.java
@@ -0,0 +1,124 @@
+package org.graylog.plugins.views.migrations;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+import com.google.inject.Inject;
+import com.google.protobuf.MapEntry;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import org.graylog.autovalue.WithBeanGetter;
+import org.graylog2.database.MongoConnection;
+import org.graylog2.migrations.Migration;
+import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.slf4j.Logger;
+import org.bson.Document;
+import org.slf4j.LoggerFactory;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class V20190127111728_MigrateWidgetFormatSettings extends Migration {
+    private static final Logger LOG = LoggerFactory.getLogger(V20190127111728_MigrateWidgetFormatSettings.class);
+
+    private final ClusterConfigService clusterConfigService;
+    private final MongoCollection<Document> viewsCollection;
+
+    @Inject
+    public V20190127111728_MigrateWidgetFormatSettings(MongoConnection mongoConnection,
+                                                       ClusterConfigService clusterConfigService) {
+        this.clusterConfigService = clusterConfigService;
+        this.viewsCollection = mongoConnection.getMongoDatabase().getCollection("views");
+    }
+
+    @Override
+    public ZonedDateTime createdAt() {
+        return ZonedDateTime.parse("2019-01-27T11:17:28Z");
+    }
+
+    @Override
+    public void upgrade() {
+        if (clusterConfigService.get(MigrationCompleted.class) != null) {
+            LOG.debug("Migration already completed.");
+            return;
+        }
+
+        final Set<String> viewIds = new HashSet<>();
+        final FindIterable<Document> documents = viewsCollection.find();
+        boolean viewMigrated;
+        for (final Document view : documents) {
+            viewMigrated = false;
+            final Document states = view.get("state", Document.class);
+            for(Map.Entry<String, Object> obj : states.entrySet()) {
+                final Document state = (Document) obj.getValue();
+                if (state.get("widgets") instanceof List) {
+                    @SuppressWarnings("unchecked") final List<Document> widgets = (List) state.get("widgets");
+                    for (final Document widget : widgets) {
+                        final String type = widget.getString("type");
+                        if (type.equals("aggregation")) {
+                            final Document config = widget.get("config", Document.class);
+                            final Document formatSettings = config.get("formatting_settings", Document.class);
+                            if (formatSettings == null) {
+                                continue;
+                            }
+                            final Object charColorsObj = formatSettings.get("chart_colors");
+                            if (charColorsObj == null) {
+                                continue;
+                            }
+                            viewMigrated = true;
+                            @SuppressWarnings({"unchecked", "rawtypes"}) final Map<String, String> chartColors =
+                                    (Map) charColorsObj;
+                            List<Document> chartColorSettings = chartColors.entrySet().stream().map(entry -> {
+                                final Document chartColorFieldSetting = new Document();
+                                chartColorFieldSetting.put("field_name", entry.getKey());
+                                chartColorFieldSetting.put("chart_color", entry.getValue());
+                                return chartColorFieldSetting;
+                            }).collect(Collectors.toList());
+                            formatSettings.put("chart_colors", chartColorSettings);
+                            config.put("formatting_settings", formatSettings);
+                            widget.put("config", config);
+                        }
+                    }
+                    if (viewMigrated) {
+                        state.put("widgets", widgets);
+                    }
+                }
+            }
+
+            if (viewMigrated) {
+                viewsCollection.updateOne(new BasicDBObject("_id", view.getObjectId("_id")), new Document("$set", view));
+                final String viewId = view.getObjectId("_id").toString();
+                viewIds.add(viewId);
+            }
+        }
+        LOG.info("Migration completed. {} views where migrated.", viewIds.size());
+        clusterConfigService.write(V20190127111728_MigrateWidgetFormatSettings.MigrationCompleted.create(
+                viewIds.size(), viewIds));
+    }
+
+    @JsonAutoDetect
+    @AutoValue
+    @WithBeanGetter
+    public static abstract class MigrationCompleted {
+        @JsonProperty("modified_views_count")
+        public abstract long modifiedViewsCount();
+
+        @JsonProperty("modified_view_ids")
+        public abstract Set<String> modifiedViewIds();
+
+        @JsonCreator
+        public static V20190127111728_MigrateWidgetFormatSettings.MigrationCompleted create(
+                @JsonProperty("modified_views_count") final long modifiedViews,
+                @JsonProperty("modified_view_ids") final Set<String> modifiedViewIds) {
+            return new AutoValue_V20190127111728_MigrateWidgetFormatSettings_MigrationCompleted(
+                    modifiedViews, modifiedViewIds);
+        }
+    }
+}
---
+++ graylog2-server/src/test/java/org/graylog/plugins/views/migrations/V20190127111728_MigrateWidgetFormatSettingsTest.java
@@ -0,0 +1,69 @@
+package org.graylog.plugins.views.migrations;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.graylog.testing.mongodb.MongoDBFixtures;
+import org.graylog.testing.mongodb.MongoDBInstance;
+import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class V20190127111728_MigrateWidgetFormatSettingsTest {
+    @Rule
+    public final MongoDBInstance mongoDB = MongoDBInstance.createForClass();
+    @Rule
+    public final MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    private V20190127111728_MigrateWidgetFormatSettings migration;
+
+    @Mock
+    private ClusterConfigService clusterConfigService;
+
+    @Before
+    public void setUp() {
+        migration = new V20190127111728_MigrateWidgetFormatSettings(mongoDB.mongoConnection(), clusterConfigService);
+    }
+
+    @Test
+    @MongoDBFixtures("V20190127111728_MigrateWidgetFormatSettings.json")
+    public void testMigration() {
+        final BasicDBObject dbQuery1 = new BasicDBObject();
+        dbQuery1.put("_id", new ObjectId("5e2ee372b22d7970576b2eb3"));
+        final MongoCollection<Document> collection = mongoDB.mongoConnection()
+                .getMongoDatabase()
+                .getCollection("views");
+        migration.upgrade();
+        final FindIterable<Document> views = collection.find(dbQuery1);
+        final Document view1 = views.first();
+
+        @SuppressWarnings("unchecked")
+        final List<Document> widgets1 = (List) view1.get("state", Document.class).get("2c67cc0f-c62e-47c1-8b70-e3198925e6bc", Document.class).get("widgets");
+        assertThat(widgets1.size()).isEqualTo(2);
+        Set<Document> aggregationWidgets =widgets1.stream().filter(w -> w.getString("type")
+                .equals("aggregation")).collect(Collectors.toSet());
+        assertThat(aggregationWidgets.size()).isEqualTo(1);
+        final Document aggregationWidget = aggregationWidgets.iterator().next();
+        final Document config = aggregationWidget.get("config", Document.class);
+        final Document formattingSettings = config.get("formatting_settings", Document.class);
+
+        @SuppressWarnings("unchecked")
+        final List<Document> chartColors = (List) formattingSettings.get("chart_colors", List.class);
+        assertThat(chartColors.size()).isEqualTo(1);
+        final Document chartColor = chartColors.get(0);
+        assertThat(chartColor.getString("field_name")).isEqualTo("count()");
+        assertThat(chartColor.getString("chart_color")).isEqualTo("#e91e63");
+    }
+}
