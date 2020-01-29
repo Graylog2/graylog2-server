f09d3d6b8391a63afb1676a4b96fce2296eab659
Dennis Oelkers <dennis@graylog.com>
2020-01-29 16:43:29 +0100

    Removing `SearchBar`.

--- graylog2-web-interface/src/components/search/index.jsx
+++ graylog2-web-interface/src/components/search/index.jsx
@@ -3,5 +3,4 @@ export { default as MessageFieldExtractorActions } from './MessageFieldExtractor
 export { default as MessageFields } from './MessageFields';
 export { default as MessageShow } from './MessageShow';
 export { default as MessageTableEntry } from './MessageTableEntry';
-export { default as SearchBar } from './SearchBar';
 export { default as SurroundingSearchButton } from './SurroundingSearchButton';
