1dee6e0ce1107f06706dbc123f9dd20a4d1c195d
Dennis Oelkers <dennis@graylog.com>
2020-01-29 16:38:30 +0100

    Removing `MessageFieldDescription`.

--- graylog2-web-interface/src/components/search/index.jsx
+++ graylog2-web-interface/src/components/search/index.jsx
@@ -1,5 +1,4 @@
 export { default as MessageDetail } from './MessageDetail';
-export { default as MessageFieldDescription } from './MessageFieldDescription';
 export { default as MessageFieldExtractorActions } from './MessageFieldExtractorActions';
 export { default as MessageField } from './MessageField';
 export { default as MessageFields } from './MessageFields';
