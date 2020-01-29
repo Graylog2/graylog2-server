c559f8611947df5907c5c40e13846cef34926d40
Dennis Oelkers <dennis@graylog.com>
2020-01-29 17:47:35 +0100

    Removing `DashboardListPage` component.

--- graylog2-web-interface/src/components/dashboard/index.jsx
+++ graylog2-web-interface/src/components/dashboard/index.jsx
@@ -1,5 +1,4 @@
 export { default as Dashboard } from './Dashboard';
 export { default as DashboardList } from './DashboardList';
-export { default as DashboardListPage } from './DashboardListPage';
 export { default as EditDashboardModal } from './EditDashboardModal';
 export { default as EditDashboardModalTrigger } from './EditDashboardModalTrigger';
