74c490c3dfb8391951184e80ab4741b538e55d24
Dennis Oelkers <dennis@graylog.com>
2020-01-29 16:39:56 +0100

    Removing `MessageTablePaginator`.

--- graylog2-web-interface/src/components/search/index.jsx
+++ graylog2-web-interface/src/components/search/index.jsx
@@ -3,7 +3,6 @@ export { default as MessageFieldExtractorActions } from './MessageFieldExtractor
 export { default as MessageFields } from './MessageFields';
 export { default as MessageShow } from './MessageShow';
 export { default as MessageTableEntry } from './MessageTableEntry';
-export { default as MessageTablePaginator } from './MessageTablePaginator';
 export { default as ResultTable } from './ResultTable';
 export { default as SavedSearchControls } from './SavedSearchControls';
 export { default as SearchBar } from './SearchBar';
