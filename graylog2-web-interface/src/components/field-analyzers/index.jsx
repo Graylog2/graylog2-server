c87ffcdec7d5f3dc9ba8ec00c8098dfc34866688
Dennis Oelkers <dennis@graylog.com>
2020-01-29 17:31:00 +0100

    Removing `FieldAnalyzerMapComponent` component.

--- graylog2-web-interface/src/components/field-analyzers/index.jsx
+++ graylog2-web-interface/src/components/field-analyzers/index.jsx
@@ -1,13 +1,6 @@
 import { PluginManifest, PluginStore } from 'graylog-web-plugin/plugin';
-import FieldAnalyzerMapComponent from 'components/maps/field-analyzers/FieldAnalyzerMapComponent';
 const pluginManifest = new PluginManifest({}, {
   fieldAnalyzers: [
-    {
-      refId: 'fieldAnalyzerMapComponent',
-      displayName: 'World Map',
-      component: FieldAnalyzerMapComponent,
-      displayPriority: 100,
-    },
   ],
 });
 
