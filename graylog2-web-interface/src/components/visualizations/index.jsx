cecc0d3b701d5282a926786612359ba4f35b872d
Dennis Oelkers <dennis@graylog.com>
2020-01-30 09:50:15 +0100

    Removing components from package index.

--- graylog2-web-interface/src/components/visualizations/index.jsx
+++ graylog2-web-interface/src/components/visualizations/index.jsx
@@ -1,7 +1,1 @@
-export { default as GraphVisualization } from './GraphVisualization';
-export { default as HistogramVisualization } from './HistogramVisualization';
-export { default as NumericVisualization } from './NumericVisualization';
-export { default as QuickValuesVisualization } from './QuickValuesVisualization';
-export { default as QuickValuesHistogramVisualization } from './QuickValuesHistogramVisualization';
-export { default as StackedGraphVisualization } from './StackedGraphVisualization';
 export { default as RenderCompletionObserver } from './RenderCompletionObserver';
