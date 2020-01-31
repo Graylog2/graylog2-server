6d16f3518471f21f65bde9543f46ca6654c51df2
Kyle Knight <kyle@graylog.com>
2020-01-31 10:32:49 -0600

    pr ui feedback

--- graylog2-web-interface/src/components/streams/StreamMetaData.jsx
+++ graylog2-web-interface/src/components/streams/StreamMetaData.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import CollapsibleStreamRuleList from 'components/streamrules/CollapsibleStreamRuleList';
 import { Pluralize } from 'components/common';
@@ -7,50 +8,49 @@ import { Button } from 'components/graylog';
 
 import StreamThroughput from './StreamThroughput';
 
+const StreamMetaDataWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const StreamMetaData = ({ isDefaultStream, stream, streamRuleTypes, permissions }) => {
+  let verbalMatchingType;
   const [expanded, setExpanded] = useState(false);
   const toggleText = expanded ? 'Hide' : 'Show';
 
-  const _formatNumberOfStreamRules = () => {
-    if (stream.is_default) {
-      return 'The default stream contains all messages.';
-    }
-    if (stream.rules.length === 0) {
-      return 'No configured rules.';
-    }
+  if (stream.is_default) {
+    return 'The default stream contains all messages.';
+  }
+  if (stream.rules.length === 0) {
+    return 'No configured rules.';
+  }
 
-    let verbalMatchingType;
-    switch (stream.matching_type) {
-      case 'OR': verbalMatchingType = 'at least one'; break;
-      default:
-      case 'AND': verbalMatchingType = 'all'; break;
-    }
-
-    return (
-      <span>
-        Must match {verbalMatchingType} of the {stream.rules.length} configured stream{' '}
-        <Pluralize value={stream.rules.length} plural="rules" singular="rule" />.
-      </span>
-    );
-  };
+  switch (stream.matching_type) {
+    case 'OR': verbalMatchingType = 'at least one'; break;
+    default:
+    case 'AND': verbalMatchingType = 'all'; break;
+  }
 
   const _onHandleToggle = () => {
     setExpanded(!expanded);
   };
 
   return (
-    <div className="stream-metadata">
-      <StreamThroughput streamId={stream.id} />. {_formatNumberOfStreamRules()}
+    <StreamMetaDataWrapper>
+      <StreamThroughput streamId={stream.id} />.
+
+      <span>
+        &nbsp;Must match {verbalMatchingType} of the {stream.rules.length} configured stream&nbsp;
+        <Pluralize value={stream.rules.length} plural="rules" singular="rule" />.
+      </span>
 
       {!isDefaultStream && (
         <>
-          <span className="stream-rules-link">
-            <Button bsStyle="link"
-                    bsSize="xsmall"
-                    onClick={_onHandleToggle}>
-              {toggleText} stream rules
-            </Button>
-          </span>
+          <Button bsStyle="link"
+                  bsSize="xsmall"
+                  onClick={_onHandleToggle}>
+            {toggleText} stream rules
+          </Button>
 
           <CollapsibleStreamRuleList key={`streamRules-${stream.id}`}
                                      stream={stream}
@@ -59,7 +59,7 @@ const StreamMetaData = ({ isDefaultStream, stream, streamRuleTypes, permissions 
                                      expanded={expanded} />
         </>
       )}
-    </div>
+    </StreamMetaDataWrapper>
   );
 };
 
