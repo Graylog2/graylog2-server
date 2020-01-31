1b09af70cba501d26c92007c499712fabe6d01be
Kyle Knight <kyle@graylog.com>
2020-01-31 08:48:11 -0600

    Update AppFacade to functional component (#7068)
    * Update AppFacade to functional component
    
    * pr feedback

--- graylog2-web-interface/src/routing/AppFacade.jsx
+++ graylog2-web-interface/src/routing/AppFacade.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import createReactClass from 'create-react-class';
-import Reflux from 'reflux';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import loadAsync from 'routing/loadAsync';
+import ServerUnavailablePage from 'pages/ServerUnavailablePage';
+import StoreProvider from 'injection/StoreProvider';
+import connect from 'stores/connect';
+import GraylogThemeProvider from 'theme/GraylogThemeContext';
 
 import 'bootstrap/less/bootstrap.less';
 import 'opensans-npm-webfont';
@@ -10,11 +13,6 @@ import 'stylesheets/bootstrap-submenus.less';
 import 'toastr/toastr.less';
 import 'rickshaw/rickshaw.css';
 import 'stylesheets/graylog2.less';
-
-import ServerUnavailablePage from 'pages/ServerUnavailablePage';
-import StoreProvider from 'injection/StoreProvider';
-
-import GraylogThemeProvider from '../theme/GraylogThemeContext';
 
 const SessionStore = StoreProvider.getStore('Session');
 const ServerAvailabilityStore = StoreProvider.getStore('ServerAvailability');
@@ -24,38 +22,54 @@ const LoginPage = loadAsync(() => import(/* webpackChunkName: "LoginPage" */ 'pa
 const LoadingPage = loadAsync(() => import(/* webpackChunkName: "LoadingPage" */ 'pages/LoadingPage'));
 const LoggedInPage = loadAsync(() => import(/* webpackChunkName: "LoggedInPage" */ 'pages/LoggedInPage'));
 
-const AppFacade = createReactClass({
-  displayName: 'AppFacade',
-  mixins: [Reflux.connect(SessionStore), Reflux.connect(ServerAvailabilityStore), Reflux.connect(CurrentUserStore)],
+const SERVER_PING_TIMEOUT = 20000;
 
-  componentDidMount() {
-    this.interval = setInterval(ServerAvailabilityStore.ping, 20000);
-  },
+export const AppFacade = ({ currentUser, server, sessionId }) => {
+  let Page;
 
-  componentWillUnmount() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  },
+  useEffect(() => {
+    const interval = setInterval(ServerAvailabilityStore.ping, SERVER_PING_TIMEOUT);
 
-  render() {
-    const { currentUser, server, sessionId } = this.state;
-    let Page = <LoggedInPage />;
+    return () => clearInterval(interval);
+  }, []);
 
-    if (!server.up) {
-      Page = <ServerUnavailablePage server={server} />;
-    } else if (!sessionId) {
-      Page = <LoginPage />;
-    } else if (!currentUser) {
-      Page = <LoadingPage text="We are preparing Graylog for you..." />;
-    }
+  if (!server.up) {
+    Page = <ServerUnavailablePage server={server} />;
+  } else if (!sessionId) {
+    Page = <LoginPage />;
+  } else if (!currentUser) {
+    Page = <LoadingPage text="We are preparing Graylog for you..." />;
+  } else {
+    Page = <LoggedInPage />;
+  }
 
-    return (
-      <GraylogThemeProvider>
-        {Page}
-      </GraylogThemeProvider>
-    );
-  },
-});
+  return (
+    <GraylogThemeProvider>
+      {Page}
+    </GraylogThemeProvider>
+  );
+};
 
-export default AppFacade;
+AppFacade.propTypes = {
+  currentUser: PropTypes.object,
+  server: PropTypes.shape({
+    up: PropTypes.bool,
+  }),
+  sessionId: PropTypes.string,
+};
+
+AppFacade.defaultProps = {
+  currentUser: undefined,
+  server: undefined,
+  sessionId: undefined,
+};
+
+export default connect(AppFacade, {
+  currentUser: CurrentUserStore,
+  server: ServerAvailabilityStore,
+  sessionId: SessionStore,
+}, ({
+  currentUser: { currentUser } = {},
+  server: { server } = {},
+  sessionId: { sessionId } = '',
+}) => ({ currentUser, server, sessionId }));
