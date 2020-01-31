68df8e87991d924114a204554e939ea117cf89d5
Dennis Oelkers <dennis@graylog.com>
2020-01-31 11:47:40 +0100

    Removing `SearchUtils` module.

--- graylog2-web-interface/src/util/SearchUtils.js
+++
@@ -1,9 +0,0 @@
-const SearchUtils = {
-  INTERVALS: ['minute', 'hour', 'day', 'week', 'month', 'quarter', 'year'],
-
-  histogramIntervals() {
-    return this.INTERVALS;
-  },
-};
-
-export default SearchUtils;
