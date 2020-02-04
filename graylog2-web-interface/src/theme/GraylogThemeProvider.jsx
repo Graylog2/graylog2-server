94c5022a36f32bf71c15f3f00f1dd92f4fe07e7b
Kyle Knight <kyle@graylog.com>
2020-02-04 13:17:28 -0600

    swap global styles

--- graylog2-web-interface/src/theme/GraylogThemeProvider.jsx
+++ graylog2-web-interface/src/theme/GraylogThemeProvider.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 
 import { teinte } from 'theme';
+import GlobalThemeStyles from 'theme/GlobalThemeStyles';
 
 /* NOTE: mode can be `teinte` or `noire` and will eventually need to come from User Preferences */
 const THEME_MODE = 'teinte';
@@ -10,7 +11,10 @@ const THEME_MODE = 'teinte';
 const GraylogThemeProvider = ({ children }) => {
   return (
     <ThemeProvider theme={{ mode: THEME_MODE, color: teinte }}>
-      {children}
+      <>
+        <GlobalThemeStyles />
+        {children}
+      </>
     </ThemeProvider>
   );
 };
