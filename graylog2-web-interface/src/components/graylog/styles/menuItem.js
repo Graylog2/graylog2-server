05102ed92a66c59bbb98dbfe205836e3f85122a4
Kyle Knight <kyle@graylog.com>
2020-02-06 15:45:02 -0600

    cleanup - one err left

--- graylog2-web-interface/src/components/graylog/styles/menuItem.js
+++ graylog2-web-interface/src/components/graylog/styles/menuItem.js
@@ -13,26 +13,26 @@ const menuItemStyles = css`
         color: ${util.contrastingColor(teinte.secondary.due)};
         background-color: ${teinte.secondary.due};
       }
-    }
 
-    > .active > a {
-      background-color: ${teinte.tertiary.due};
-      color: ${util.contrastingColor(teinte.tertiary.due)};
+      > .active > a {
+        background-color: ${teinte.tertiary.due};
+        color: ${util.contrastingColor(teinte.tertiary.due)};
 
-      :hover,
-      :focus {
-        background-color: ${teinte.tertiary.uno};
-        color: ${util.contrastingColor(teinte.tertiary.uno)};
+        :hover,
+        :focus {
+          background-color: ${teinte.tertiary.uno};
+          color: ${util.contrastingColor(teinte.tertiary.uno)};
+        }
       }
-    }
 
-    > .disabled > a {
-      color: ${util.contrastingColor(teinte.primary.tre, 'AA')};
+      > .disabled > a {
+        color: ${util.contrastingColor(teinte.primary.tre, 'AA')};
 
-      :hover,
-      :focus {
-        background-color: ${teinte.secondary.due};
-        color: ${util.contrastingColor(teinte.primary.tre, 'AAA')};
+        :hover,
+        :focus {
+          background-color: ${teinte.secondary.due};
+          color: ${util.contrastingColor(teinte.primary.tre, 'AAA')};
+        }
       }
     }
   }
