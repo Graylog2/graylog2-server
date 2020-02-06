a1df3ab1059a8b53459aac9cfabcf250e43dfa69
Othello Maurer <othello@graylog.com>
2020-02-06 18:03:35 +0100

    Compile regex on object creation to catch illegal expressions early (#7388)
    We don't want to rely entirely on the frontend to make sure that only
    valid expressions are entered. Previously we would accept invalid
    expressions and end up with a broken whitelist because the pattern would
     be compiled on demand.

--- graylog2-server/src/main/java/org/graylog2/system/urlwhitelist/RegexWhitelistEntry.java
+++ graylog2-server/src/main/java/org/graylog2/system/urlwhitelist/RegexWhitelistEntry.java
@@ -20,28 +20,37 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
-import com.google.auto.value.extension.memoized.Memoized;
 import org.graylog.autovalue.WithBeanGetter;
 
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 @AutoValue
 @WithBeanGetter
 @JsonAutoDetect
 public abstract class RegexWhitelistEntry implements WhitelistEntry {
-    @Memoized
-    protected Pattern cachedPattern() {
-        return Pattern.compile(value(), Pattern.DOTALL);
-    }
+    private Pattern pattern;
 
     @JsonCreator
     public static RegexWhitelistEntry create(@JsonProperty("id") String id, @JsonProperty("title") String title,
             @JsonProperty("value") String value) {
-        return new AutoValue_RegexWhitelistEntry(id, Type.REGEX, title, value);
+
+        // compile the pattern early so that we can catch illegal expressions asap
+        final Pattern pattern;
+        try {
+            pattern = Pattern.compile(value, Pattern.DOTALL);
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException(
+                    "Cannot create whitelist entry for invalid regular expression '" + value + "': " + e.getMessage(),
+                    e);
+        }
+        final RegexWhitelistEntry whitelistEntry = new AutoValue_RegexWhitelistEntry(id, Type.REGEX, title, value);
+        whitelistEntry.pattern = pattern;
+        return whitelistEntry;
     }
 
     @Override
     public boolean isWhitelisted(String url) {
-        return cachedPattern().matcher(url).find();
+        return pattern.matcher(url).find();
     }
 }
