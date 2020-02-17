58875f74c6813a030fafd502ff98e866a58c1f4a
Kyle Knight <kyle@graylog.com>
2020-02-17 15:03:17 -0600

    Cleaning up snapshots and faster renderings

--- graylog2-web-interface/src/components/graylog/HelpBlock.jsx
+++ graylog2-web-interface/src/components/graylog/HelpBlock.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-restricted-imports
 import { HelpBlock as BootstrapHelpBlock } from 'react-bootstrap';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { lighten } from 'polished';
 
 import teinte from 'theme/teinte';
