398474b530bd94fb46d8d5a5b6ab35b748b260fc
Dennis Oelkers <dennis@graylog.com>
2020-02-17 15:46:15 +0100

    Adding check if route is defined.

--- graylog2-web-interface/src/views/bindings.routes.test.jsx
+++ graylog2-web-interface/src/views/bindings.routes.test.jsx
@@ -10,6 +10,9 @@ jest.mock('util/AppConfig', () => ({
 describe('bindings.routes', () => {
   it('Stream search route must be unqualified', () => {
     const streamSearchPageRoute = bindings.routes.find(({ component }) => (component === StreamSearchPage));
+    if (!streamSearchPageRoute) {
+      throw new Error('Stream search page route was not registered.');
+    }
     expect(streamSearchPageRoute.path).toEqual('/streams/:streamId/search');
   });
 });
