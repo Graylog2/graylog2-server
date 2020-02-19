4a8189fc87a2903f61a9e33c529fc2ba523a3755
Edmundo Alvarez <edmundo@graylog.com>
2020-02-19 15:28:31 +0100

    Update `LdapGroupsComponent`
    - Fix isLoading method
    - Fix linter errors
    - Fix sorting
    - Use combined provider

--- graylog2-web-interface/src/components/ldap/LdapGroupsComponent.jsx
+++ graylog2-web-interface/src/components/ldap/LdapGroupsComponent.jsx
@@ -1,19 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Immutable from 'immutable';
-import { Row, Col, Panel, Button } from 'components/graylog';
-import naturalSort from 'javascript-natural-sort';
+import { Button, Col, Panel, Row } from 'components/graylog';
 
 import { Input } from 'components/bootstrap';
 import { Spinner } from 'components/common';
+import { naturalSortIgnoreCase } from 'util/SortUtils';
 
-import ActionsProvider from 'injection/ActionsProvider';
+import CombinedProvider from 'injection/CombinedProvider';
 
-import StoreProvider from 'injection/StoreProvider';
-
-const LdapGroupsActions = ActionsProvider.getActions('LdapGroups');
-const RolesStore = StoreProvider.getStore('Roles');
-const LdapGroupsStore = StoreProvider.getStore('LdapGroups');
+const { LdapGroupsActions } = CombinedProvider.get('LdapGroups');
+const { RolesStore } = CombinedProvider.get('Roles');
 
 class LdapGroupsComponent extends React.Component {
   static propTypes = {
@@ -22,15 +19,15 @@ class LdapGroupsComponent extends React.Component {
   };
 
   state = {
-    groups: Immutable.Set.of(),
-    roles: Immutable.Set.of(),
-    mapping: Immutable.Map(),
+    groups: undefined,
+    roles: undefined,
+    mapping: undefined,
     groupsErrorMessage: null,
   };
 
   componentDidMount() {
-    LdapGroupsActions.loadMapping.triggerPromise().then(mapping => this.setState({ mapping: Immutable.Map(mapping) }));
-    LdapGroupsActions.loadGroups.triggerPromise()
+    LdapGroupsActions.loadMapping().then(mapping => this.setState({ mapping: Immutable.Map(mapping) }));
+    LdapGroupsActions.loadGroups()
       .then(
         groups => this.setState({ groups: Immutable.Set(groups) }),
         (error) => {
@@ -45,54 +42,57 @@ class LdapGroupsComponent extends React.Component {
   _updateMapping = (event) => {
     const role = event.target.value;
     const group = event.target.getAttribute('data-group');
+    const { mapping } = this.state;
     if (role === '') {
-      this.setState({ mapping: this.state.mapping.delete(group) });
+      this.setState({ mapping: mapping.delete(group) });
     } else {
-      this.setState({ mapping: this.state.mapping.set(group, role) });
+      this.setState({ mapping: mapping.set(group, role) });
     }
   };
 
   _saveMapping = (event) => {
     event.preventDefault();
-    LdapGroupsActions.saveMapping(this.state.mapping.toJS());
+    const { mapping } = this.state;
+    LdapGroupsActions.saveMapping(mapping.toJS());
   };
 
-  _onShowConfig = (event) => {
-    event.preventDefault();
-    this.props.onShowConfig();
+  _onShowConfig = () => {
+    const { onShowConfig } = this.props;
+    onShowConfig();
   };
 
   _isLoading = () => {
-    return !(this.state.mapping && this.state.groups && this.state.roles);
+    const { groups, mapping, roles } = this.state;
+    return !(mapping && groups && roles);
   };
 
   render() {
     if (this._isLoading()) {
       return <Spinner />;
     }
+    const { groups, groupsErrorMessage, mapping, roles } = this.state;
+    const { onCancel } = this.props;
 
-    if (this.state.groupsErrorMessage) {
+    if (groupsErrorMessage) {
       return (
         <Panel header="Error: Unable to load LDAP groups" bsStyle="danger">
-          The error message was:<br />{this.state.groupsErrorMessage.message}
+          The error message was:<br />{groupsErrorMessage.message}
         </Panel>
       );
     }
 
-    naturalSort.insensitive = true; // sigh
-
-    const options = this.state.roles.sort(naturalSort).map((role) => {
+    const options = roles.sort((r1, r2) => naturalSortIgnoreCase(r1.name, r2.name)).map((role) => {
       return <option key={role.name} value={role.name}>{role.name}</option>;
     });
 
-    const content = this.state.groups.sort(naturalSort).map((group) => {
+    const content = groups.sort(naturalSortIgnoreCase).map((group) => {
       return (
         <li key={group}>
           <Input id={`${group}-select`}
                  label={group}
                  data-group={group}
                  type="select"
-                 value={this.state.mapping.get(group, '')}
+                 value={mapping.get(group, '')}
                  onChange={this._updateMapping}
                  labelClassName="col-sm-2"
                  wrapperClassName="col-sm-5">
@@ -103,15 +103,12 @@ class LdapGroupsComponent extends React.Component {
       );
     });
 
-    naturalSort.insensitive = false; // sigh 2
-
     if (content.size === 0) {
       return (
-        <p>
-          No LDAP/Active Directory groups found. Please verify that your{' '}
-          <a href="#" onClick={this._onShowConfig}>LDAP group mapping</a>{' '}
-          settings are correct.
-        </p>
+        <Panel header="No LDAP/Active Directory groups found" bsStyle="info">
+          <p>Please verify that your LDAP group mapping settings are correct.</p>
+          <Button bsSize="sm" onClick={this._onShowConfig}>Open LDAP settings</Button>
+        </Panel>
       );
     }
     return (
@@ -122,7 +119,7 @@ class LdapGroupsComponent extends React.Component {
           </Col>
           <Col md={10} mdPush={2}>
             <Button type="submit" bsStyle="primary" className="save-button-margin">Save</Button>
-            <Button onClick={this.props.onCancel}>Cancel</Button>
+            <Button onClick={onCancel}>Cancel</Button>
           </Col>
         </Row>
       </form>
