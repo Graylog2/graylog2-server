f164ac45f1909d871bba6274e8893cf64609d9a2
Edmundo Alvarez <edmundo@graylog.com>
2020-02-20 09:14:14 +0100

    Clean up styleguide sections (#7499)
    * Remove inputs section from documentation
    
    This folder contains Graylog inputs components, not form inputs.
    
    * Remove now almost empty visualizations section
    
    Since 3.2 there is only a component in that folder.

--- graylog2-web-interface/styleguide.config.js
+++ graylog2-web-interface/styleguide.config.js
@@ -65,14 +65,6 @@ module.exports = {
           name: 'Configuration Forms',
           components: 'src/components/configurationforms/[A-Z]*.jsx',
         },
-        {
-          name: 'Inputs',
-          components: 'src/components/inputs/[A-Z]*.jsx',
-        },
-        {
-          name: 'Visualizations',
-          components: 'src/components/visualizations/[A-Z]*.jsx',
-        },
       ],
     },
     {
