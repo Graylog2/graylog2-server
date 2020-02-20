12f365a2f4794bc0a4de62db0d7a27b7bbefa98c
Dennis Oelkers <dennis@graylog.com>
2020-02-20 16:38:47 +0100

    Widening accepted types for `fieldTypeFor`.

--- graylog2-web-interface/src/views/logic/fieldtypes/FieldTypeFor.js
+++ graylog2-web-interface/src/views/logic/fieldtypes/FieldTypeFor.js
@@ -4,8 +4,9 @@ import type { FieldTypeMappingsList } from 'views/stores/FieldTypesStore';
 
 import inferTypeForSeries from './InferTypeForSeries';
 import FieldType from './FieldType';
+import FieldTypeMapping from './FieldTypeMapping';
 
-const fieldTypeFor = (field: string, types: FieldTypeMappingsList): FieldType => {
+const fieldTypeFor = (field: string, types: (FieldTypeMappingsList | Array<FieldTypeMapping>)): FieldType => {
   if (isFunction(field)) {
     const { type } = inferTypeForSeries(Series.forFunction(field), types);
     return type;
