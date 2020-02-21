e00793822a92d760d84fc984330cab674b959fd8
Dennis Oelkers <dennis@graylog.com>
2020-02-21 15:15:56 +0100

    Adding test cases.

---
+++ graylog2-web-interface/test/console-warnings-fail-tests.test.js
@@ -0,0 +1,33 @@
+// @flow strict
+/* eslint-disable no-console */
+// eslint-disable-next-line no-unused-vars
+const unused = require('./console-warnings-fail-tests');
+
+describe('console-warnings-fail-tests', () => {
+  describe('console.error', () => {
+    it('throws error if used', () => {
+      expect(() => { console.error('hello there!'); }).toThrowError(new Error('hello there!'));
+    });
+    it('does not throw error if containing react deprecation notice', () => {
+      expect(() => {
+        console.error('Warning: componentWillReceiveProps has been renamed, and is not recommended for use. See https://fb.me/react-unsafe-component-lifecycles for details.');
+      }).not.toThrow();
+    });
+    it('does not throw error if called without arguments', () => {
+      expect(() => { console.error(); }).not.toThrow();
+    });
+  });
+  describe('console.warn', () => {
+    it('throws error if used', () => {
+      expect(() => { console.warn('hello there!'); }).toThrowError(new Error('hello there!'));
+    });
+    it('does not throw error if containing react deprecation notice', () => {
+      expect(() => {
+        console.warn('Warning: componentWillReceiveProps has been renamed, and is not recommended for use. See https://fb.me/react-unsafe-component-lifecycles for details.');
+      }).not.toThrow();
+    });
+    it('does not throw error if called without arguments', () => {
+      expect(() => { console.warn(); }).not.toThrow();
+    });
+  });
+});
