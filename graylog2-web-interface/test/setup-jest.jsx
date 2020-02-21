c6e9ea45f70b37eecfd988c5a474ce456817b16f
Dennis Oelkers <dennis@graylog.com>
2020-02-21 14:57:59 +0100

    Improve logging for tests, include future react deprecation warning.
    In preparation of updating to React 16.12.0, the `console.error/warn`
    handlers for our tests are improved in this PR to make it easier to add
    additional patterns for deprecation warnings that should not be handled
    as test failures.

--- graylog2-web-interface/test/setup-jest.jsx
+++ graylog2-web-interface/test/setup-jest.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable no-console */
 import jQuery from 'jquery';
 import { configure } from 'wrappedEnzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import registerBuiltinStores from 'injection/registerBuiltinStores';
-import { format } from 'util';
 
 global.$ = jQuery;
 global.jQuery = jQuery;
@@ -11,15 +9,3 @@ global.jQuery = jQuery;
 registerBuiltinStores();
 
 configure({ adapter: new Adapter() });
-
-const consoleWarn = console.warn;
-
-console.warn = jest.fn((...args) => {
-  if (!args[0] || !args[0].includes('react-async-component-lifecycle-hooks')) {
-    consoleWarn(format(...args));
-  }
-});
-
-console.error = jest.fn((...args) => {
-  throw new Error(format(...args));
-});
