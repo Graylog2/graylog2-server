201efacb3b588d4a9d0c2cb4adb4e1c2b36088a2
Bernd Ahlers <bernd@graylog.com>
2020-02-22 12:16:00 +0100

    Don't expose KafkaJournalConfiguration via the HTTP API
    This fixes the "/api/cluster/<node-id>/journal" API endpoint to not
    throw an error anymore.
    
    In #7359 we introduced the PathConfiguration#getNativeLibDir() method
    and this broke deserialization of the JournalSummaryResponse because
    "#getNativeLibDir" was automatically serialized but couldn't be
    deserialized.
    
    This change introduces a new KafkaJournalConfigurationSummary value that
    we can serialize and deserialize in the API. Using this also avoids
    exposing new fields from Pathconfiguration or KafkaJournalConfiguration
    automatically.
    This also removes the constructor and jackson annotations in
    KafkaJournalConfiguration beacuse they are not needed anymore.
    
    Fixes #7521

--- graylog2-server/src/main/java/org/graylog2/plugin/KafkaJournalConfiguration.java
+++ graylog2-server/src/main/java/org/graylog2/plugin/KafkaJournalConfiguration.java
@@ -16,68 +16,36 @@
  */
 package org.graylog2.plugin;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.joschi.jadconfig.Parameter;
 import com.github.joschi.jadconfig.util.Size;
 import org.graylog2.configuration.PathConfiguration;
 import org.joda.time.Duration;
 
-import javax.validation.constraints.NotNull;
 import java.nio.file.Path;
-import java.util.Objects;
 
 public class KafkaJournalConfiguration extends PathConfiguration {
 
     public KafkaJournalConfiguration() { }
 
-    @JsonCreator
-    public KafkaJournalConfiguration(@NotNull @JsonProperty("directory") Path messageJournalDir,
-                                     @JsonProperty("segment_size") long messageJournalSegmentSize,
-                                     @JsonProperty("segment_age") Duration messageJournalSegmentAge,
-                                     @JsonProperty("max_size") long messageJournalMaxSize,
-                                     @JsonProperty("max_age") Duration messageJournalMaxAge,
-                                     @JsonProperty("flush_interval") long messageJournalFlushInterval,
-                                     @JsonProperty("flush_age") Duration messageJournalFlushAge) {
-        this.messageJournalDir = Objects.requireNonNull(messageJournalDir);
-        this.messageJournalSegmentSize = Size.bytes(messageJournalSegmentSize);
-        this.messageJournalSegmentAge = messageJournalSegmentAge;
-        this.messageJournalMaxSize = Size.bytes(messageJournalMaxSize);
-        this.messageJournalMaxAge = messageJournalMaxAge;
-        this.messageJournalFlushInterval = messageJournalFlushInterval;
-        this.messageJournalFlushAge = messageJournalFlushAge;
-    }
-
     @Parameter(value = "message_journal_dir", required = true)
-    @JsonProperty("directory")
     private Path messageJournalDir = DEFAULT_DATA_DIR.resolve("journal");
 
     @Parameter("message_journal_segment_size")
-    @JsonProperty("segment_size")
     private Size messageJournalSegmentSize = Size.megabytes(100L);
 
     @Parameter("message_journal_segment_age")
-    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
-    @JsonProperty("segment_age")
     private Duration messageJournalSegmentAge = Duration.standardHours(1L);
 
     @Parameter("message_journal_max_size")
-    @JsonProperty("max_size")
     private Size messageJournalMaxSize = Size.gigabytes(5L);
 
     @Parameter("message_journal_max_age")
-    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
-    @JsonProperty("max_age")
     private Duration messageJournalMaxAge = Duration.standardHours(12L);
 
     @Parameter("message_journal_flush_interval")
-    @JsonProperty("flush_interval")
     private long messageJournalFlushInterval = 1_000_000L;
 
     @Parameter("message_journal_flush_age")
-    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
-    @JsonProperty("flush_age")
     private Duration messageJournalFlushAge = Duration.standardMinutes(1L);
 
     public Path getMessageJournalDir() {
