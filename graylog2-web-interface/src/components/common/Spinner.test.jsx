bcaebb9f2375ac73c175fbc69999843d69f7c6a8
Linus Pahl <lpahl@protonmail.com>
2020-02-24 16:00:14 +0100

    Transform Spinner test to @testing-library/react test

--- graylog2-web-interface/src/components/common/Spinner.test.jsx
+++ graylog2-web-interface/src/components/common/Spinner.test.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { mount } from 'wrappedEnzyme';
+import { render, cleanup } from 'wrappedTestingLibrary';
 
 import Spinner from 'components/common/Spinner';
 
 describe('<Spinner />', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   it('should render without props', () => {
-    const wrapper = mount(<Spinner />);
-    expect(wrapper).toMatchSnapshot();
+    const { getByText } = render(<Spinner />);
+    expect(getByText('Loading...')).not.toBeNull();
   });
 
   it('should render with a different text string', () => {
     const text = 'Hello world!';
-    const wrapper = mount(<Spinner text={text} />);
-    expect(wrapper).toMatchSnapshot();
+    const { getByText } = render(<Spinner text={text} />);
+    expect(getByText(text)).not.toBeNull();
   });
 });
