732791e7ad8db4692abeea5d6822f00f2bd90c95
Kyle Knight <kyle@graylog.com>
2020-02-26 14:59:10 -0600

    LoginBox

--- graylog2-web-interface/src/theme/teinte.js
+++ graylog2-web-interface/src/theme/teinte.js
@@ -1,21 +1,21 @@
 const teinte = {
   primary: {
-    uno: '#FF3633',
-    due: '#FFF',
-    tre: '#1F1F1F',
+    uno: '#ff3633',
+    due: '#fff',
+    tre: '#1f1f1f',
   },
   secondary: {
-    uno: '#FF3B00', /* TODO: Replace with #AD0707 */
-    due: '#F1F2F2', /* TODO: Replace with #A6AFBD */
-    tre: '#DCE1E5',
+    uno: '#ff3b00', /* TODO: replace with #ad0707 */
+    due: '#f1f2f2', /* TODO: replace with #a6afbd */
+    tre: '#dce1e5',
   },
   tertiary: {
-    uno: '#16ACE3', /* TODO: Replace with #0063BE */
-    due: '#6DC6E7',
-    tre: '#8DC63F', /* TODO: Replace with #00AE42 */
-    quattro: '#9E1F63', /* TODO: Replace with #702785 */
-    cinque: '#FF6418',
-    sei: '#FFD200',
+    uno: '#16ace3', /* TODO: replace with #0063be */
+    due: '#6dc6e7',
+    tre: '#8dc63f', /* TODO: replace with #00ae42 */
+    quattro: '#9e1f63', /* TODO: replace with #702785 */
+    cinque: '#ff6418',
+    sei: '#ffd200',
   },
 };
 
