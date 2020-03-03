f0b59192f22a0e416986f4a3f66410e3d6550dbd
Dennis Oelkers <dennis@graylog.com>
2020-03-03 11:16:34 +0100

    Include streams in URL generated by surrounding search button.

--- graylog2-web-interface/src/components/search/SurroundingSearchButton.jsx
+++ graylog2-web-interface/src/components/search/SurroundingSearchButton.jsx
@@ -1,13 +1,18 @@
 // @flow strict
-import PropTypes from 'prop-types';
 import * as React from 'react';
+import PropTypes from 'prop-types';
+import * as Immutable from 'immutable';
 import Qs from 'qs';
 import moment from 'moment';
+import naturalSort from 'javascript-natural-sort';
 
 import Routes from 'routing/Routes';
+import connect from 'stores/connect';
 import { DropdownButton, MenuItem } from 'components/graylog';
-import naturalSort from 'javascript-natural-sort';
 import { addToQuery, escape } from 'views/logic/queries/QueryHelper';
+import { QueryFiltersStore } from 'views/stores/QueryFiltersStore';
+import type { FilterType } from 'views/logic/queries/Query';
+import { filtersToStreamSet } from 'views/logic/queries/Query';
 import type { SearchesConfig } from './SearchConfig';
 
 const buildTimeRangeOptions = ({ surrounding_timerange_options: surroundingTimerangeOptions = {} }) => Object.entries(surroundingTimerangeOptions)
@@ -19,10 +24,10 @@ const buildFilterFields = (messageFields, searchConfig) => {
   return surroundingFilterFields.reduce((prev, cur) => ({ ...prev, [cur]: messageFields[cur] }), {});
 };
 
-const buildSearchLink = (id, fromTime, toTime, fields, filter) => {
-  const query = Object.keys(filter)
-    .filter(key => (filter[key] !== null && filter[key] !== undefined))
-    .map(key => `${key}:"${escape(filter[key])}"`)
+const buildSearchLink = (id, fromTime, toTime, fields, filterFields, streams) => {
+  const query = Object.keys(filterFields)
+    .filter(key => (filterFields[key] !== null && filterFields[key] !== undefined))
+    .map(key => `${key}:"${escape(filterFields[key])}"`)
     .reduce((prev, cur) => addToQuery(prev, cur), '');
 
   const params = {
@@ -33,16 +38,19 @@ const buildSearchLink = (id, fromTime, toTime, fields, filter) => {
     highlightMessage: id,
     fields,
   };
+  const paramsWithStreams = streams && streams.length > 0
+    ? { ...params, streams: streams.join(',') }
+    : params;
 
-  return `${Routes.SEARCH}?${Qs.stringify(params)}`;
+  return `${Routes.SEARCH}?${Qs.stringify(paramsWithStreams)}`;
 };
 
-const searchLink = (range, timestamp, id, messageFields, searchConfig) => {
+const searchLink = (range, timestamp, id, messageFields, searchConfig, streams) => {
   const fromTime = moment(timestamp).subtract(Number(range), 'seconds').toISOString();
   const toTime = moment(timestamp).add(Number(range), 'seconds').toISOString();
   const filterFields = buildFilterFields(messageFields, searchConfig);
 
-  return buildSearchLink(id, fromTime, toTime, [], filterFields);
+  return buildSearchLink(id, fromTime, toTime, [], filterFields, streams);
 };
 
 type Props = {
@@ -50,14 +58,16 @@ type Props = {
   timestamp: string,
   id: string,
   messageFields: { [string]: mixed },
+  queryFilters: ?FilterType,
 };
 
-const SurroundingSearchButton = ({ searchConfig, timestamp, id, messageFields }: Props) => {
+const SurroundingSearchButton = ({ searchConfig, timestamp, id, messageFields, queryFilters }: Props) => {
   const timeRangeOptions = buildTimeRangeOptions(searchConfig);
+  const streams = filtersToStreamSet(queryFilters).toJS();
   const menuItems = Object.keys(timeRangeOptions)
     .sort((a, b) => naturalSort(a, b))
     .map(range => (
-      <MenuItem key={range} href={searchLink(range, timestamp, id, messageFields, searchConfig)} target="_blank" rel="noopener noreferrer">
+      <MenuItem key={range} href={searchLink(range, timestamp, id, messageFields, searchConfig, streams)} target="_blank" rel="noopener noreferrer">
         {timeRangeOptions[range]}
       </MenuItem>
     ));
@@ -76,4 +86,8 @@ SurroundingSearchButton.propTypes = {
   messageFields: PropTypes.object.isRequired,
 };
 
-export default SurroundingSearchButton;
+export default connect(
+  SurroundingSearchButton,
+  { queryFilters: QueryFiltersStore },
+  ({ queryFilters = Immutable.Map() }) => ({ queryFilters: queryFilters.first() }),
+);
--- graylog2-web-interface/test/helpers/mocking/index.js
+++ graylog2-web-interface/test/helpers/mocking/index.js
@@ -1,3 +1,4 @@
 export { default as CombinedProviderMock } from './CombinedProviderMock';
 export { default as StoreMock } from './StoreMock';
 export { default as StoreProviderMock } from './StoreProviderMock';
+export { default as asMock } from './AsMock';
