424a01157c3a4231462abe2262021439253a2edc
Dennis Oelkers <dennis@graylog.com>
2020-03-03 10:56:36 +0100

    Adding mock, simplifying fixture creation.

--- graylog2-web-interface/src/views/hooks/SyncWithQueryParameters.test.jsx
+++ graylog2-web-interface/src/views/hooks/SyncWithQueryParameters.test.jsx
@@ -8,12 +8,36 @@ import { syncWithQueryParameters } from './SyncWithQueryParameters';
 import Query, { createElasticsearchQueryString } from '../logic/queries/Query';
 import Search from '../logic/search/Search';
 
+jest.mock('views/stores/QueryFiltersStore', () => ({
+  QueryFiltersActions: {
+    streams: {
+      completed: {
+        listen: jest.fn(),
+      },
+    },
+  },
+}));
+
 jest.mock('views/stores/ViewStore', () => ({
   ViewStore: {
     getInitialState: jest.fn(),
   },
 }));
 jest.mock('util/History', () => ({ push: jest.fn(), replace: jest.fn() }));
+
+const createSearch = (timerange, queryString = 'foo:42') => Search.builder()
+  .queries([
+    Query.builder()
+      .timerange(timerange)
+      .query(createElasticsearchQueryString(queryString))
+      .build(),
+  ])
+  .build();
+
+const createView = (search, type = View.Type.Search) => View.builder()
+  .type(type)
+  .search(search)
+  .build();
 
 describe('SyncWithQueryParameters', () => {
   it('does not do anything if no view is loaded', () => {
@@ -28,19 +52,7 @@ describe('SyncWithQueryParameters', () => {
     expect(history.push).not.toHaveBeenCalled();
   });
   describe('if current view is search, adds state to history', () => {
-    const view = View.builder()
-      .type(View.Type.Search)
-      .search(
-        Search.builder()
-          .queries([
-            Query.builder()
-              .timerange({ type: 'relative', range: 600 })
-              .query(createElasticsearchQueryString('foo:42'))
-              .build(),
-          ])
-          .build(),
-      )
-      .build();
+    const view = createView(createSearch({ type: 'relative', range: 600 }));
     it('with current time range and query', () => {
       asMock(ViewStore.getInitialState).mockReturnValueOnce({ view });
 
@@ -56,23 +68,11 @@ describe('SyncWithQueryParameters', () => {
       expect(history.push).toHaveBeenCalledWith('/search?somevalue=23&somethingelse=foo&q=foo%3A42&rangetype=relative&relative=600');
     });
     it('if time range is absolute', () => {
-      const viewWithAbsoluteTimerange = View.builder()
-        .type(View.Type.Search)
-        .search(
-          Search.builder()
-            .queries([
-              Query.builder()
-                .timerange({
-                  type: 'absolute',
-                  from: '2019-01-12T13:42:23.000Z',
-                  to: '2020-01-12T13:42:23.000Z',
-                })
-                .query(createElasticsearchQueryString('foo:42'))
-                .build(),
-            ])
-            .build(),
-        )
-        .build();
+      const viewWithAbsoluteTimerange = createView(createSearch({
+        type: 'absolute',
+        from: '2019-01-12T13:42:23.000Z',
+        to: '2020-01-12T13:42:23.000Z',
+      }));
       asMock(ViewStore.getInitialState).mockReturnValueOnce({ view: viewWithAbsoluteTimerange });
 
       syncWithQueryParameters('/search');
@@ -81,22 +81,10 @@ describe('SyncWithQueryParameters', () => {
         .toHaveBeenCalledWith('/search?q=foo%3A42&rangetype=absolute&from=2019-01-12T13%3A42%3A23.000Z&to=2020-01-12T13%3A42%3A23.000Z');
     });
     it('if time range is keyword time range', () => {
-      const viewWithAbsoluteTimerange = View.builder()
-        .type(View.Type.Search)
-        .search(
-          Search.builder()
-            .queries([
-              Query.builder()
-                .timerange({
-                  type: 'keyword',
-                  keyword: 'Last five minutes',
-                })
-                .query(createElasticsearchQueryString('foo:42'))
-                .build(),
-            ])
-            .build(),
-        )
-        .build();
+      const viewWithAbsoluteTimerange = createView(createSearch({
+        type: 'keyword',
+        keyword: 'Last five minutes',
+      }));
       asMock(ViewStore.getInitialState).mockReturnValueOnce({ view: viewWithAbsoluteTimerange });
 
       syncWithQueryParameters('/search');
