418baee7b34422ed3e935ad0b8a05afbef621195
Konrad Merz <konrad@graylog.com>
2020-03-05 16:11:03 +0100

    Fix linter

--- graylog2-web-interface/src/components/configurations/CustomizationConfig.jsx
+++ graylog2-web-interface/src/components/configurations/CustomizationConfig.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
-import {util} from 'theme';
+import { util } from 'theme';
 
 import { Badge, Button } from 'components/graylog';
 import { Icon } from 'components/common';
