a184cfeddf857c14ea2ebef6414c94aa63d2ab76
Linus Pahl <lpahl@protonmail.com>
2020-03-05 11:35:30 +0100

    Refactor FieldSortSelect code style

--- graylog2-web-interface/src/views/components/FieldSortSelect.jsx
+++ graylog2-web-interface/src/views/components/FieldSortSelect.jsx
@@ -4,45 +4,57 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 import * as Immutable from 'immutable';
 
-import { SortList } from 'views/components/aggregationbuilder/AggregationBuilderPropTypes';
 import CustomPropTypes from 'views/components/CustomPropTypes';
 
-import SortConfig from 'views/logic/aggregationbuilder/SortConfig';
-import FieldTypeMapping from 'views/logic/fieldtypes/FieldTypeMapping';
-import Direction from 'views/logic/aggregationbuilder/Direction';
 import { defaultCompare } from 'views/logic/DefaultCompare';
+import Direction from 'views/logic/aggregationbuilder/Direction';
+import FieldTypeMapping from 'views/logic/fieldtypes/FieldTypeMapping';
+import SortConfig from 'views/logic/aggregationbuilder/SortConfig';
 
 type Props = {
-  fields: Immutable.Set<FieldTypeMapping>,
+  fields: Immutable.List<FieldTypeMapping>,
   onChange: (Array<*>) => any,
   sort: Array<SortConfig>,
 };
 
-const currentValue = (sort, options) => {
-  const currentOption = sort && sort.length > 0 && options.find(option => option.label === sort[0].field);
-  if (currentOption === undefined) {
+type Option = {
+  label: string,
+  value: number
+}
+
+const findOptionByLabel = (options: Immutable.List<Option>, label: string) => options.find(option => option.label === label);
+
+const findOptionByValue = (options: Immutable.List<Option>, value: number) => options.find(option => option.value === value);
+
+const currentValue = (sort: Array<SortConfig>, options: Immutable.List<Option>) => sort && sort.length > 0 && findOptionByLabel(options, sort[0].field);
+
+const sortedOptions = (fields: Immutable.List<FieldTypeMapping>) => {
+  return fields.sort(
+    (field1, field2) => defaultCompare(field1.name, field2.name),
+  ).map(
+    (field, idx) => ({ label: field.name, value: idx }),
+  );
+};
+
+const onOptionChange = (options: Immutable.List<Option>, onChange, newValue, reason) => {
+  if (reason.action === 'clear') {
+    return onChange([]);
+  }
+  const { value } = newValue;
+  const option = findOptionByValue(options, value);
+
+  if (!option) {
     return undefined;
   }
-  return currentOption;
+  const sortConfig = new SortConfig('pivot', option.label, Direction.Ascending);
+  return onChange([sortConfig]);
 };
 
 const FieldSortSelect = ({ fields, onChange, sort }: Props) => {
-  const options = fields.sort((field1, field2) => defaultCompare(field1.name, field2.name)).map((field, idx) => ({ label: field.name, value: idx }));
-  const _onChange = (newValue, reason) => {
-    if (reason.action === 'clear') {
-      return onChange([]);
-    }
-    const { value } = newValue;
-    const option = options.find(pt => pt.value === value);
-    if (!option) {
-      return undefined;
-    }
-    const sortConfig = new SortConfig('pivot', option.label, Direction.Ascending);
-    return onChange([sortConfig]);
-  };
+  const options: Immutable.List<Option> = sortedOptions(fields);
   return (
     <Select placeholder="None: click to add fields"
-            onChange={_onChange}
+            onChange={(newValue, reason) => onOptionChange(options, onChange, newValue, reason)}
             options={options}
             isClearable
             value={currentValue(sort, options)} />
@@ -52,7 +64,7 @@ const FieldSortSelect = ({ fields, onChange, sort }: Props) => {
 FieldSortSelect.propTypes = {
   onChange: PropTypes.func.isRequired,
   fields: CustomPropTypes.FieldListType.isRequired,
-  sort: SortList.isRequired,
+  sort: PropTypes.array.isRequired,
 };
 
 export default FieldSortSelect;
