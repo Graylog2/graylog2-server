2dd23dfb8bd940f07f507bbe5d9aa283d11e5bac
Kyle Knight <kyle@graylog.com>
2020-03-06 06:25:32 -0600

    flow-check

--- graylog2-web-interface/src/theme/index.js
+++ graylog2-web-interface/src/theme/index.js
@@ -15,14 +15,61 @@ const theme = {
   },
 };
 
-const themeModes = Object.keys(colors);
+const themeModes: Array<string> = Object.keys(colors);
 
 export type ThemeInterface = {
   color: {
-    primary: { [string]: string },
-    secondary: { [string]: string },
-    tertiary: { [string]: string },
-   },
+    brand: {
+      primary: string,
+      secondary: string,
+      tertiary: string,
+    },
+    global: {
+      textDefault: string,
+      textAlt: string,
+      background: string,
+      contentBackground: string,
+      link: string,
+      linkHover: string,
+    },
+    gray: {
+      "0": string,
+      "10": string,
+      "20": string,
+      "30": string,
+      "40": string,
+      "50": string,
+      "60": string,
+      "70": string,
+      "80": string,
+      "90": string,
+      "100": string,
+    },
+    variant: {
+      danger: string,
+      default: string,
+      info: string,
+      primary: string,
+      success: string,
+      warning: string,
+      light: {
+        danger: string,
+        default: string,
+        info: string,
+        primary: string,
+        success: string,
+        warning: string,
+      },
+      dark: {
+        danger: string,
+        default: string,
+        info: string,
+        primary: string,
+        success: string,
+        warning: string,
+      },
+    },
+  },
 };
 
 export default theme;
