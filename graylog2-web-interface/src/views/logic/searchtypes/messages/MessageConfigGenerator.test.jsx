c4687cae29c5dc6390aa2f6c470518457faa0221
Linus Pahl <lpahl@protonmail.com>
2020-03-06 11:52:46 +0100

    Fix MessageConfigGenerator test

--- graylog2-web-interface/src/views/logic/searchtypes/messages/MessageConfigGenerator.test.jsx
+++ graylog2-web-interface/src/views/logic/searchtypes/messages/MessageConfigGenerator.test.jsx
@@ -1,10 +1,16 @@
 // @flow strict
 import MessagesWidget from 'views/logic/widgets/MessagesWidget';
 import MessagesWidgetConfig from 'views/logic/widgets/MessagesWidgetConfig';
+import SortConfig from 'views/logic/aggregationbuilder/SortConfig';
+import Direction from 'views/logic/aggregationbuilder/Direction';
+
 import MessageConfigGenerator from './MessageConfigGenerator';
+import MessageSortConifg from './MessageSortConifg';
 
 describe('MessageConfigGenerator', () => {
-  it('generates basic search type from message widget', () => {
+  const defaultSort = [new MessageSortConifg(SortConfig.PIVOT_TYPE, 'timestamp', Direction.Descending)];
+
+  it('generates basic search type from message widget with a default sort', () => {
     // $FlowFixMe: We need to force this being a `MessagesWidget`
     const widget: MessagesWidget = MessagesWidget.builder()
       .config(
@@ -15,7 +21,7 @@ describe('MessageConfigGenerator', () => {
 
     const result = MessageConfigGenerator(widget);
 
-    expect(result).toEqual([{ decorators: [], type: 'messages' }]);
+    expect(result).toEqual([{ decorators: [], sort: defaultSort, type: 'messages' }]);
   });
   it('adds decorators to search type', () => {
     const decorators = [
@@ -37,6 +43,7 @@ describe('MessageConfigGenerator', () => {
         { id: 'decorator1', type: 'something', config: {}, stream: null, order: 0 },
         { id: 'decorator2', type: 'something else', config: {}, stream: null, order: 1 },
       ],
+      sort: defaultSort,
       type: 'messages',
     }]);
   });
