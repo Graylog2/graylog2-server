42400ec6b488295f4a02716d93b295901ee90186
Dennis Oelkers <dennis@graylog.com>
2020-03-13 10:53:46 +0100

    Ensure that all mocked stores have `getInitialState`.

--- graylog2-web-interface/test/helpers/mocking/StoreMock.js
+++ graylog2-web-interface/test/helpers/mocking/StoreMock.js
@@ -1,6 +1,7 @@
 export default function (...args) {
   const store = {};
-  Array.from(args).forEach((method) => {
+  const methodsToMock = ['listen', 'getInitialState', ...args];
+  Array.from(methodsToMock).forEach((method) => {
     if (Array.isArray(method)) {
       // eslint-disable-next-line prefer-destructuring
       store[method[0]] = method[1];
