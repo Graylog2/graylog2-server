a45a508b229e5c66117e293450badc079f0abb86
Kyle Knight <kyle@graylog.com>
2020-03-19 08:16:16 -0500

    Remove connect

--- graylog2-web-interface/src/pages/LoginPage.jsx
+++ graylog2-web-interface/src/pages/LoginPage.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'reflux';
+import React, { useState } from 'react';
 import { PluginStore } from 'graylog-web-plugin/plugin';
 
 import { DocumentTitle, Icon } from 'components/common';
@@ -9,44 +8,24 @@ import LoginBox from 'components/login/LoginBox';
 
 import AuthThemeStyles from 'theme/styles/authStyles';
 
-import CombinedProvider from 'injection/CombinedProvider';
-import LoadingPage from './LoadingPage';
-
-const { SessionStore, SessionActions } = CombinedProvider.get('Session');
-
 const LoginPage = () => {
-  const [didValidateSession, setDidValidateSession] = useState(false);
-  const [lastError, setLastError] = useState(false);
+  const [lastError, setLastError] = useState(undefined);
 
-  useEffect(() => {
-    const sessionPromise = SessionActions.validate().then((response) => {
-      setDidValidateSession(true);
-      return response;
-    });
-
-    return () => {
-      sessionPromise.cancel();
-    };
-  }, []);
-
-  const handleErrorChange = (nextError) => {
-    setLastError(nextError);
+  const _resetLastError = () => {
+    setLastError(undefined);
   };
 
-  const resetLastError = () => {
-    handleErrorChange(undefined);
-  };
-
-  const formatLastError = (error) => {
-    if (error) {
+  const _formatLastError = () => {
+    if (lastError) {
       return (
         <div className="form-group">
           <Alert bsStyle="danger">
-            <button type="button" className="close" onClick={resetLastError}>&times;</button>{error}
+            <button type="button" className="close" onClick={_resetLastError}>&times;</button>{lastError}
           </Alert>
         </div>
       );
     }
+
     return null;
   };
 
@@ -55,34 +34,23 @@ const LoginPage = () => {
 
     if (loginComponent.length === 1) {
       return React.createElement(loginComponent[0].formComponent, {
-        onErrorChange: handleErrorChange,
+        onErrorChange: setLastError,
       });
     }
 
-    return <LoginForm onErrorChange={handleErrorChange} />;
+    return <LoginForm onErrorChange={setLastError} />;
   };
 
-  if (!didValidateSession) {
-    return (
-      <LoadingPage />
-    );
-  }
-
-  const alert = formatLastError(lastError);
   return (
     <DocumentTitle title="Sign in">
       <AuthThemeStyles />
       <LoginBox>
         <legend><Icon name="group" /> Welcome to Graylog</legend>
-        {alert}
+        {_formatLastError()}
         {renderLoginForm()}
       </LoginBox>
     </DocumentTitle>
   );
 };
 
-export default connect(LoginPage, {
-  sessionId: SessionStore,
-}, ({
-  sessionId: { sessionId } = '',
-}) => ({ sessionId }));
+export default LoginPage;
