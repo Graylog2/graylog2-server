f62b1e54ea3aaa9e23fd8ddd10b32d0423124b6a
Dennis Oelkers <dennis@graylog.com>
2020-03-25 15:21:47 +0100

    Wiring `WidgetQueryControls` again.

--- graylog2-web-interface/src/views/components/WidgetQueryControls.jsx
+++ graylog2-web-interface/src/views/components/WidgetQueryControls.jsx
@@ -1,10 +1,11 @@
 // @flow strict
 import * as React from 'react';
-import * as Immutable from 'immutable';
-import moment from 'moment';
+import { useCallback } from 'react';
 import styled from 'styled-components';
+import { Field, Form, Formik } from 'formik';
 
 import connect from 'stores/connect';
+import DateTime from 'logic/datetimes/DateTime';
 import { Col, Row } from 'components/graylog';
 import { Icon } from 'components/common';
 import DocumentationLink from 'components/support/DocumentationLink';
@@ -15,7 +16,7 @@ import Widget from 'views/logic/widgets/Widget';
 import { StreamsStore } from 'views/stores/StreamsStore';
 import { SearchConfigStore } from 'views/stores/SearchConfigStore';
 import { WidgetActions } from 'views/stores/WidgetStore';
-import type { TimeRange, TimeRangeTypes } from 'views/logic/queries/Query';
+import type { TimeRange } from 'views/logic/queries/Query';
 import { GlobalOverrideActions, GlobalOverrideStore } from 'views/stores/GlobalOverrideStore';
 import GlobalOverride from 'views/logic/search/GlobalOverride';
 import SearchActions from 'views/actions/SearchActions';
@@ -24,6 +25,8 @@ import TimeRangeInput from './searchbar/TimeRangeInput';
 import StreamsFilter from './searchbar/StreamsFilter';
 import SearchButton from './searchbar/SearchButton';
 import QueryInput from './searchbar/AsyncQueryInput';
+import { DEFAULT_TIMERANGE } from '../Constants';
+import { createElasticsearchQueryString } from 'views/logic/queries/Query';
 
 type Props = {
   availableStreams: Array<any>,
@@ -31,45 +34,6 @@ type Props = {
   globalOverride: ?GlobalOverride,
   widget: Widget,
 };
-
-const _updateQuery = (id: string, queryString: string) => WidgetActions.query(id, { type: 'elasticsearch', query_string: queryString });
-const _updateRangeType = (oldTimerange: ?TimeRange, id: string, newRangeType: TimeRangeTypes) => {
-  const { type } = oldTimerange || {};
-  if (type === newRangeType) {
-    return Promise.resolve();
-  }
-  let newTimerange: TimeRange = { type: 'relative', range: 300 };
-  // eslint-disable-next-line default-case
-  switch (newRangeType) {
-    case 'absolute':
-      newTimerange = {
-        type: newRangeType,
-        from: moment().subtract(oldTimerange && oldTimerange.type === 'relative' ? oldTimerange.range : 300, 'seconds').toISOString(),
-        to: moment().toISOString(),
-      };
-      break;
-    case 'relative':
-      newTimerange = {
-        type: newRangeType,
-        range: 300,
-      };
-      break;
-    case 'keyword':
-      newTimerange = {
-        type: newRangeType,
-        keyword: 'Last five Minutes',
-      };
-      break;
-  }
-  return WidgetActions.timerange(id, newTimerange);
-};
-
-type Delta = {| range: number |} | {| from: string |} | {| to: string |} | {| keyword: string |};
-
-// $FlowFixMe: Resulting time range could actually be inconsistent/incomplete at this point. Need to fix and improve.
-const _updateRangeParams = (rangeType: string, currentTimerange: ?TimeRange, id: string, delta: Delta) => WidgetActions.timerange(id, { ...currentTimerange, type: rangeType, ...delta });
-
-const _updateStreams = (id: string, streams: Array<string>) => WidgetActions.streams(id, streams);
 
 const BlurredWrapper = styled.div`
   filter: blur(4px);
@@ -105,54 +69,93 @@ const ResetOverrideHint = () => (
   </CenteredBox>
 );
 
+const onSubmittingTimerange = (timerange: TimeRange): TimeRange => {
+  switch (timerange.type) {
+    case 'absolute':
+      return {
+        type: timerange.type,
+        from: DateTime.parseFromString(timerange.from).toISOString(),
+        to: DateTime.parseFromString(timerange.to).toISOString(),
+      };
+    case 'relative':
+    case 'keyword':
+      return timerange;
+    default: throw new Error(`Invalid time range type: ${timerange.type}`);
+  }
+};
+
+const onSubmit = (values, widget: Widget) => {
+  const { timerange, streams, query } = values;
+  const newTimerange = onSubmittingTimerange(timerange);
+  const newWidget = widget.toBuilder()
+    .timerange(newTimerange)
+    .query(query)
+    .streams(streams)
+    .build();
+
+  return WidgetActions.update(widget.id, newWidget);
+};
+
 const WidgetQueryControls = ({ availableStreams, config, globalOverride = {}, widget }: Props) => {
-  const { query, timerange, streams } = widget;
+  const { streams } = widget;
+  const query = widget.query || createElasticsearchQueryString('');
+  const timerange = widget.timerange || DEFAULT_TIMERANGE;
+  const { query_string: queryString } = query || {};
   const disableSearch = false;
-  const { type: rangeType = 'relative', ...rest } = timerange || {};
-  const rangeParams = Immutable.Map(rest || { range: 300 });
-  const { id } = widget;
-  const performSearch = () => { };
-  const isGloballyOverridden: boolean = globalOverride !== undefined && globalOverride !== null
+  const isGloballyOverridden: boolean = globalOverride !== undefined
+    && globalOverride !== null
     && (globalOverride.query !== undefined || globalOverride.timerange !== undefined);
   const Wrapper = isGloballyOverridden ? BlurredWrapper : React.Fragment;
+  const _onSubmit = useCallback(values => onSubmit(values, widget), [widget]);
   return (
     <>
       {isGloballyOverridden && <ResetOverrideHint />}
       <Wrapper>
-        <Row className="no-bm extended-search-query-metadata">
-          <Col md={4}>
-            <TimeRangeTypeSelector onSelect={(newRangeType) => _updateRangeType(timerange, id, newRangeType)}
-                                   disabled={isGloballyOverridden}
-                                   value={rangeType} />
-            <TimeRangeInput disabled={isGloballyOverridden}
-                            type={rangeType}
-                            config={config} />
-          </Col>
+        <Formik initialValues={{ timerange, streams, queryString }}
+                onSubmit={_onSubmit}>
+          {({ isSubmitting, isValid, handleSubmit }) => (
+            <Form>
+              <Row className="no-bm extended-search-query-metadata">
+                <Col md={4}>
+                  <TimeRangeTypeSelector disabled={isGloballyOverridden} />
+                  <TimeRangeInput disabled={isGloballyOverridden} config={config} />
+                </Col>
 
-          <Col md={8}>
-            <StreamsFilter value={streams}
-                           disabled={isGloballyOverridden}
-                           streams={availableStreams}
-                           onChange={(value) => _updateStreams(id, value)} />
-          </Col>
-        </Row>
+                <Col md={8}>
+                  <Field name="streams">
+                    {({ field: { name, value, onChange } }) => (
+                      <StreamsFilter value={value}
+                                     disabled={isGloballyOverridden}
+                                     streams={availableStreams}
+                                     onChange={newStreams => onChange({ target: { value: newStreams, name } })} />
+                    )}
+                  </Field>
+                </Col>
+              </Row>
 
-        <Row className="no-bm">
-          <Col md={12}>
-            <div className="pull-right search-help">
-              <DocumentationLink page={DocsHelper.PAGES.SEARCH_QUERY_LANGUAGE}
-                                 title="Search query syntax documentation"
-                                 text={<Icon name="lightbulb-o" />} />
-            </div>
-            <SearchButton disabled={disableSearch || isGloballyOverridden} />
+              <Row className="no-bm">
+                <Col md={12}>
+                  <div className="pull-right search-help">
+                    <DocumentationLink page={DocsHelper.PAGES.SEARCH_QUERY_LANGUAGE}
+                                       title="Search query syntax documentation"
+                                       text={<Icon name="lightbulb-o" />} />
+                  </div>
+                  <SearchButton disabled={disableSearch || isGloballyOverridden || isSubmitting || !isValid} />
 
-            <QueryInput value={query ? query.query_string : undefined}
-                        disabled={isGloballyOverridden}
-                        placeholder={'Type your search query here and press enter. E.g.: ("not found" AND http) OR http_response_code:[400 TO 404]'}
-                        onChange={(value) => _updateQuery(id, value).then(() => value)}
-                        onExecute={performSearch} />
-          </Col>
-        </Row>
+                  <Field name="queryString">
+                    {({ field: { name, value, onChange } }) => (
+                      <QueryInput value={value}
+                                  disabled={isGloballyOverridden}
+                                  placeholder={'Type your search query here and press enter. E.g.: ("not found" AND http) OR http_response_code:[400 TO 404]'}
+                                  onChange={newQuery => onChange({ target: { value: newQuery, name } })}
+                                  onExecute={handleSubmit} />
+                    )}
+                  </Field>
+                </Col>
+              </Row>
+            </Form>
+          )}
+        </Formik>
       </Wrapper>
     </>
   );
