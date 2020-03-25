7febb42a5c77e1d6ebeb20d9843e8622d6cd611f
Dennis Oelkers <dennis@graylog.com>
2020-03-25 13:21:16 +0100

    Correcting return type for `onSubmit`.

--- graylog2-web-interface/flow-typed/npm/formik_v2.x.x.js
+++ graylog2-web-interface/flow-typed/npm/formik_v2.x.x.js
@@ -70,7 +70,7 @@ declare module 'formik/@flow-typed' {
 
   declare export type FormikConfig<Values> = {|
     ...FormikSharedConfig,
-    onSubmit: (values: Values, formikHelpers: FormikHelpers<Values>) => void,
+    onSubmit: (values: Values, formikHelpers: FormikHelpers<Values>) => void | Promise<any>,
 
     component?: React$ComponentType<FormikProps<Values>> | React$Node,
     render?: (props: FormikProps<Values>) => React$Node,
