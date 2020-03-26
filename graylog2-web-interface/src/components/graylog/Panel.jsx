a8e0b0e35ce5450193fd6d50895734c872faf5e4
Kyle Knight <kyle@graylog.com>
2020-03-26 06:47:32 -0500

    Wrap Deprecation Notice Utility

--- graylog2-web-interface/src/components/graylog/Panel.jsx
+++ graylog2-web-interface/src/components/graylog/Panel.jsx
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components';
 // eslint-disable-next-line no-restricted-imports
 import { Panel as BootstrapPanel } from 'react-bootstrap';
 
-import { DEPRECATION_NOTICE } from 'util/constants';
+import deprecationNotice from 'util/deprecationNotice';
 import { util } from 'theme';
 import bsStyleThemeVariant from './variants/bsStyle';
 
@@ -162,7 +162,7 @@ const Panel = ({
     /** NOTE: Deprecated & should be removed in 4.0 */
     useEffect(() => {
       /* eslint-disable-next-line no-console */
-      console.warn(DEPRECATION_NOTICE, 'You have used a deprecated `Panel` prop, please check the documentation to use the latest `Panel`.');
+      deprecationNotice('You have used a deprecated `Panel` prop, please check the documentation to use the latest `Panel`.');
     }, []);
 
     return (
