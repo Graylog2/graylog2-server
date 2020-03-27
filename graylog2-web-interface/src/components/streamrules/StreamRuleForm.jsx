171d64934e077567855403198873e074e97ce45e
Kyle Knight <kyle@graylog.com>
2020-03-27 08:24:58 -0500

    Adding support for Github icons

--- graylog2-web-interface/src/components/streamrules/StreamRuleForm.jsx
+++ graylog2-web-interface/src/components/streamrules/StreamRuleForm.jsx
@@ -115,7 +115,7 @@ class StreamRuleForm extends React.Component {
               The server will try to convert to strings or numbers based on the matcher type as well as it can.
 
               <br /><br />
-              <Icon name="github" />
+              <Icon name={{ prefix: 'fab', iconName: 'github' }} />
               <a href={`https://github.com/Graylog2/graylog2-server/tree/${Version.getMajorAndMinorVersion()}/graylog2-server/src/main/java/org/graylog2/streams/matchers`}
                  target="_blank"
                  rel="noopener noreferrer"> Take a look at the matcher code on GitHub
