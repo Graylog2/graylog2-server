d585a52c1014727f7606ec8e7c04b266a299eb03
Kyle Knight <kyle@graylog.com>
2020-03-31 14:49:17 -0500

    ControlledTableListHeader

--- graylog2-web-interface/src/components/common/ControlledTableListHeader.jsx
+++ graylog2-web-interface/src/components/common/ControlledTableListHeader.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+// eslint-disable-next-line no-restricted-imports
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { ListGroupItem } from 'components/graylog';
 
-import style from './ControlledTableListHeader.css';
+const StyledListGroupItem = styled(ListGroupItem)(({ theme }) => `
+  &.list-group-item {
+    background-color: ${theme.color.global.contentBackground};
+    font-size: 14px;
+    padding: 0 15px;
+  }
+
+  & .form-group {
+      margin: 0;
+  }
+`);
+
+const HeaderWrapper = styled.div`
+  margin: 10px 0;
+  min-height: 20px;
+`;
 
 const ControlledTableListHeader = createReactClass({
   propTypes: {
@@ -19,14 +36,14 @@ const ControlledTableListHeader = createReactClass({
 
   // We wrap string children to ensure they are displayed properly in the header
   wrapStringChildren(text) {
-    return <div className={style.headerWrapper}>{text}</div>;
+    return <HeaderWrapper>{text}</HeaderWrapper>;
   },
 
   render() {
     const { children } = this.props;
 
     const header = typeof children === 'string' ? this.wrapStringChildren(children) : children;
-    return <ListGroupItem className={style.listGroupHeader}>{header}</ListGroupItem>;
+    return <StyledListGroupItem>{header}</StyledListGroupItem>;
   },
 });
 
