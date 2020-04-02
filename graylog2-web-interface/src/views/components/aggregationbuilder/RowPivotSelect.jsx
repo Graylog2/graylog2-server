7ccbf3619d03bcc2f5c6279a7f0d9fb98b776fde
Dennis Oelkers <dennis@graylog.com>
2020-04-02 15:56:19 +0200

    Making ignoring case and not stripping diacritics the default.

--- graylog2-web-interface/src/views/components/aggregationbuilder/RowPivotSelect.jsx
+++ graylog2-web-interface/src/views/components/aggregationbuilder/RowPivotSelect.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createFilter } from 'react-select/lib/filters';
 
 import { FieldList, PivotList } from './AggregationBuilderPropTypes';
 import PivotSelect from './PivotSelect';
@@ -9,7 +8,6 @@ const RowPivotSelect = ({ fields, onChange, rowPivots }) => (
   <PivotSelect placeholder="None: click to add fields"
                onChange={onChange}
                options={fields}
-               filterOption={createFilter({ ignoreCase: true, ignoreAccents: false })}
                value={rowPivots} />
 );
 
