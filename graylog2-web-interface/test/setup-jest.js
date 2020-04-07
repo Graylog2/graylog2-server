69b6fba4d925300f5bb209b72caa1f9688906ade
Dennis Oelkers <dennis@graylog.com>
2020-04-07 11:01:29 +0200

    Reactivate turning console.error/warn calls to test failures.

--- graylog2-web-interface/test/setup-jest.js
+++ graylog2-web-interface/test/setup-jest.js
@@ -1,5 +1,6 @@
 import jQuery from 'jquery';
 import registerBuiltinStores from 'injection/registerBuiltinStores';
+import './console-warnings-fail-tests';
 
 global.$ = jQuery;
 global.jQuery = jQuery;
