7a7dc452f486ed5696fbf087c50dd6454f715313
Dennis Oelkers <dennis@graylog.com>
2020-04-07 13:14:15 +0200

    Including setup file in package json, fixing tests.

--- graylog2-web-interface/test/setup-jest.js
+++ graylog2-web-interface/test/setup-jest.js
@@ -1,6 +1,5 @@
 import jQuery from 'jquery';
 import registerBuiltinStores from 'injection/registerBuiltinStores';
-import './console-warnings-fail-tests';
 
 global.$ = jQuery;
 global.jQuery = jQuery;
