eddd6cb9fad8cce4cf9fadbfe75496c3b2ff2e7e
Kyle Knight <kyle@graylog.com>
2020-04-08 18:52:15 -0500

    Update tests from updated output

--- graylog2-web-interface/src/theme/utils/contrastingColor.test.js
+++ graylog2-web-interface/src/theme/utils/contrastingColor.test.js
@@ -7,21 +7,21 @@ describe('contrastingColor', () => {
     const color3 = contrastingColor({ color: '#f0f' });
     const color4 = contrastingColor({ color: '#000', level: 'AA' });
 
-    expect(color1).toBe('rgb(151,151,151)');
+    expect(color1).toBe('rgb(161,161,161)');
     expect(color2).toBe('rgb(81,81,81)');
-    expect(color3).toBe('rgb(255,249,255)');
-    expect(color4).toBe('rgb(128,128,128)');
+    expect(color3).toBe('rgb(255,255,255)');
+    expect(color4).toBe('rgb(140,140,140)');
   });
 
   it('should accept other color strings', () => {
     const color1 = contrastingColor({ color: 'rgb(0, 0, 0)' });
 
-    expect(color1).toBe('rgb(151,151,151)');
+    expect(color1).toBe('rgb(161,161,161)');
   });
 
   it('should accept transparent color strings', () => {
     const color1 = contrastingColor({ color: 'rgba(0, 0, 0, 0.5)' });
 
-    expect(color1).toBe('rgba(151,151,151,0.675)');
+    expect(color1).toBe('rgba(161,161,161,0.7)');
   });
 });
