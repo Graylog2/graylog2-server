7b66a13d05b5701ffad28953716f6858213fe8cc
Linus Pahl <lpahl@protonmail.com>
2020-04-08 23:56:28 +0200

    Implement CurrentUserContext for SearchResultOverview

--- graylog2-web-interface/src/views/components/sidebar/SearchResultOverview.jsx
+++ graylog2-web-interface/src/views/components/sidebar/SearchResultOverview.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import numeral from 'numeral';
-import { get } from 'lodash';
 
 import { Timestamp } from 'components/common';
-import DateTime from 'logic/datetimes/DateTime';
-import CurrentUserStore from 'stores/users/CurrentUserStore';
-import connect from 'stores/connect';
+import CurrentUserContext from 'components/contexts/CurrentUserContext';
 
-const UserTimestamp = connect(Timestamp, { currentUser: CurrentUserStore }, ({ currentUser }) => ({ tz: get(currentUser, 'currentUser.timezone', 'UTC') }));
+import DateTime from 'logic/datetimes/DateTime';
 
 const SearchResultOverview = ({ results }) => {
-  const { timestamp } = results;
+  const { timestamp } = useContext(CurrentUserContext);
   return (
     <span>
-      Query executed in {numeral(results.duration).format('0,0')}ms at <UserTimestamp dateTime={timestamp} format={DateTime.Formats.DATETIME} />.
+      Query executed in {numeral(results.duration).format('0,0')}ms at <Timestamp dateTime={timestamp} format={DateTime.Formats.DATETIME} />.
     </span>
   );
 };
