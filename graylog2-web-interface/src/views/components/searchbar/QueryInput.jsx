c41641a22ca61d87a3293299a78794cdfa4be2f0
Dennis Oelkers <dennis@graylog.com>
2020-04-09 09:33:52 +0200

    Refactoring `QueryInput` to functional component.

--- graylog2-web-interface/src/views/components/searchbar/QueryInput.jsx
+++ graylog2-web-interface/src/views/components/searchbar/QueryInput.jsx
@@ -1,127 +1,104 @@
 // @flow strict
+import * as React from 'react';
+import { useCallback, useMemo } from 'react';
 import withPluginEntities from 'views/logic/withPluginEntities';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import type { AutoCompleter, Editor } from './ace-types';
 
 import AceEditor from './queryinput/ace';
-import type { Completer } from './SearchBarAutocompletions';
 import SearchBarAutoCompletions from './SearchBarAutocompletions';
+import type { Completer } from './SearchBarAutocompletions';
 
 type Props = {|
   disabled: boolean,
   value: string,
   completers: Array<Completer>,
-  completerClass?: Class<AutoCompleter>,
+  completerFactory: (Array<Completer>) => AutoCompleter,
   onBlur?: (string) => void,
   onChange: (string) => Promise<string>,
   onExecute: (string) => void,
   placeholder: string,
 |};
 
-type State = {
-};
+const defaultCompleterFactory = (completers) => new SearchBarAutoCompletions(completers);
 
-class QueryInput extends Component<Props, State> {
-  completer: AutoCompleter;
-
-  editor: {
-    editor: Editor,
-  } | typeof undefined;
-
-  static defaultProps = {
-    disabled: false,
-    onBlur: () => {},
-    completerClass: SearchBarAutoCompletions,
-    value: '',
-    placeholder: '',
-  };
-
-  constructor(props: Props) {
-    super(props);
-    this.editor = undefined;
-    const CompleterClass = props.completerClass;
-    const { completers = [] } = props;
-    // $FlowFixMe: tailored for this specific one for now
-    this.completer = new CompleterClass(completers);
-  }
-
-  componentDidMount() {
-    const editor = this.editor && this.editor.editor;
-    if (editor) {
-      editor.commands.addCommand({
-        name: 'Execute',
-        bindKey: { win: 'Enter', mac: 'Enter' },
-        exec: this._onExecute,
-      });
-
-      editor.commands.removeCommands(['indent', 'outdent']);
-
-      editor.setFontSize(16);
-
-      editor.completers = [this.completer];
-    }
-  }
-
-  _onExecute = (editor: Editor) => {
-    const { onExecute, value } = this.props;
+const QueryInput = ({ disabled, onBlur, onChange, onExecute, placeholder, value, completers, completerFactory = defaultCompleterFactory }: Props) => {
+  const completer = useMemo(() => completerFactory(completers), [completerFactory, completers]);
+  const _onExecute = (editor: Editor) => {
     if (editor.completer && editor.completer.popup) {
       editor.completer.popup.hide();
     }
     onExecute(value);
   };
 
-  _bindEditor(editor: { editor: Editor }) {
+  const editorRef = useCallback((node) => {
+    const editor = node && node.editor;
     if (editor) {
-      this.editor = editor;
-    }
-  }
+      editor.commands.addCommand({
+        name: 'Execute',
+        bindKey: { win: 'Enter', mac: 'Enter' },
+        exec: _onExecute,
+      });
 
-  render() {
-    const { disabled, onBlur, onChange, onExecute, placeholder, value } = this.props;
-    return (
-      <div className="query" style={{ display: 'flex' }} data-testid="query-input">
-        <AceEditor mode="lucene"
-                   disabled={disabled}
-                   ref={(editor) => editor && this._bindEditor(editor)}
-                   readOnly={disabled}
-                   theme="ace-queryinput"
-                   onBlur={onBlur}
-                   onChange={onChange}
-                   value={value}
-                   name="QueryEditor"
-                   showGutter={false}
-                   showPrintMargin={false}
-                   highlightActiveLine={false}
-                   minLines={1}
-                   maxLines={1}
-                   enableBasicAutocompletion
-                   enableLiveAutocompletion
-                   editorProps={{
-                     $blockScrolling: Infinity,
-                     selectionStyle: 'line',
-                   }}
-                   fontSize={13}
-                   style={{
-                     marginTop: '9px',
-                     height: '34px',
-                     width: '100%',
-                   }}
-                   placeholder={placeholder} />
-      </div>
-    );
-  }
-}
+      editor.commands.removeCommands(['indent', 'outdent']);
+
+      editor.setFontSize(16);
+
+      editor.completers = [completer];
+    }
+  }, [completer]);
+
+  return (
+    <div className="query" style={{ display: 'flex' }} data-testid="query-input">
+      <AceEditor mode="lucene"
+                 disabled={disabled}
+                 ref={editorRef}
+                 readOnly={disabled}
+                 theme="ace-queryinput"
+                 onBlur={onBlur}
+                 onChange={onChange}
+                 value={value}
+                 name="QueryEditor"
+                 showGutter={false}
+                 showPrintMargin={false}
+                 highlightActiveLine={false}
+                 minLines={1}
+                 maxLines={1}
+                 enableBasicAutocompletion
+                 enableLiveAutocompletion
+                 editorProps={{
+                   $blockScrolling: Infinity,
+                   selectionStyle: 'line',
+                 }}
+                 fontSize={13}
+                 style={{
+                   marginTop: '9px',
+                   height: '34px',
+                   width: '100%',
+                 }}
+                 placeholder={placeholder} />
+    </div>
+  );
+};
 
 QueryInput.propTypes = {
-  completers: PropTypes.array.isRequired,
-  completerClass: PropTypes.any,
+  completers: PropTypes.array,
+  completerFactory: PropTypes.func,
   disabled: PropTypes.bool,
   onBlur: PropTypes.func,
   onChange: PropTypes.func.isRequired,
   onExecute: PropTypes.func.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string,
+};
+
+QueryInput.defaultProps = {
+  disabled: false,
+  onBlur: () => {},
+  completers: [],
+  completerFactory: defaultCompleterFactory,
+  value: '',
+  placeholder: '',
 };
 
 export default withPluginEntities(QueryInput, { completers: 'views.completers' });
