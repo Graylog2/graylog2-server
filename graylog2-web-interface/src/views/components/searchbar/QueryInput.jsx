ef47a4feca86cbf05db8dcb920c30d4eed5fd26a
Dennis Oelkers <dennis@graylog.com>
2020-04-09 09:06:21 +0200

    Replacing homebrewn placeholder with ace feature.

--- graylog2-web-interface/src/views/components/searchbar/QueryInput.jsx
+++ graylog2-web-interface/src/views/components/searchbar/QueryInput.jsx
@@ -8,15 +8,6 @@ import AceEditor from './queryinput/ace';
 import type { Completer } from './SearchBarAutocompletions';
 import SearchBarAutoCompletions from './SearchBarAutocompletions';
 
-const _placeholderNode = (placeholder) => {
-  const node = document.createElement('div');
-  node.textContent = placeholder;
-  node.className = 'ace_invisible ace_emptyMessage';
-  node.style.padding = '0 9px';
-  node.style.color = '#aaa';
-  return node;
-};
-
 type Props = {|
   disabled: boolean,
   value: string,
@@ -29,13 +20,9 @@ type Props = {|
 |};
 
 type State = {
-  value: string,
-  lastValue: string,
 };
 
 class QueryInput extends Component<Props, State> {
-  isFocussed: boolean;
-
   completer: AutoCompleter;
 
   editor: {
@@ -52,10 +39,6 @@ class QueryInput extends Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
-    this.state = {
-      value: props.value,
-      lastValue: props.value,
-    };
     this.editor = undefined;
     const CompleterClass = props.completerClass;
     const { completers = [] } = props;
@@ -77,107 +60,15 @@ class QueryInput extends Component<Props, State> {
       editor.setFontSize(16);
 
       editor.completers = [this.completer];
-
-      const { value } = this.props;
-      if (!value && !this._placeholderExists(editor)) {
-        this._addPlaceholder(editor);
-      }
     }
   }
-
-  componentWillReceiveProps(nextProps: Props) {
-    const { value } = this.state;
-    if (nextProps.value !== value) {
-      this.setState({ value: nextProps.value, lastValue: nextProps.value });
-    }
-    if (this.editor) {
-      const { editor } = this.editor;
-      if (nextProps.value && this._placeholderExists(editor)) {
-        this._removePlaceholder(editor);
-      }
-
-      if (!nextProps.value && !this.isFocussed && !this._placeholderExists(editor)) {
-        this._addPlaceholder(editor);
-      }
-    }
-  }
-
-  _addPlaceholder = (editor: Editor) => {
-    if (!editor.renderer.emptyMessageNode) {
-      const { placeholder } = this.props;
-      const node = _placeholderNode(placeholder);
-      // eslint-disable-next-line no-param-reassign
-      editor.renderer.emptyMessageNode = node;
-      editor.renderer.scroller.appendChild(node);
-    }
-  };
-
-  _removePlaceholder = (editor: Editor) => {
-    if (editor.renderer.emptyMessageNode) {
-      editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode);
-      // eslint-disable-next-line no-param-reassign
-      editor.renderer.emptyMessageNode = null;
-    }
-  };
-
-  _placeholderExists = (editor: Editor) => {
-    const { emptyMessageNode } = editor.renderer;
-    return emptyMessageNode !== undefined && emptyMessageNode !== null;
-  };
-
-  _addPlaceholderIfEmptyQuery = () => {
-    const editor = this.editor && this.editor.editor;
-    if (editor) {
-      const shouldShow = !editor.session.getValue().length;
-      if (shouldShow && !this._placeholderExists(editor)) {
-        this._addPlaceholder(editor);
-      }
-    }
-  };
-
-  _removePlaceholderOnFocus = () => {
-    const editor = this.editor && this.editor.editor;
-    if (editor && this._placeholderExists(editor)) {
-      this._removePlaceholder(editor);
-    }
-  };
-
-  _onChange = (newValue: string) => {
-    return new Promise((resolve) => this.setState({ value: newValue }, resolve));
-  };
-
-  _onBlur = () => {
-    this.isFocussed = false;
-    this._addPlaceholderIfEmptyQuery();
-
-    const { onBlur, onChange } = this.props;
-    const { value, lastValue } = this.state;
-    const promise = (value !== lastValue)
-      ? new Promise((resolve) => {
-        this.setState({ lastValue: value }, () => onChange(value).then(resolve));
-      })
-      : Promise.resolve(value);
-
-    return promise.then(onBlur);
-  };
-
-  _onFocus = () => {
-    const { disabled } = this.props;
-    if (!disabled) {
-      this.isFocussed = true;
-      this._removePlaceholderOnFocus();
-    }
-  };
 
   _onExecute = (editor: Editor) => {
-    const { onChange, onExecute } = this.props;
+    const { onExecute, value } = this.props;
     if (editor.completer && editor.completer.popup) {
       editor.completer.popup.hide();
     }
-    const { value } = this.state;
-    new Promise((resolve) => this.setState({ lastValue: value }, resolve))
-      .then(() => onChange(value))
-      .then(onExecute);
+    onExecute(value);
   };
 
   _bindEditor(editor: { editor: Editor }) {
@@ -187,17 +78,16 @@ class QueryInput extends Component<Props, State> {
   }
 
   render() {
-    const { disabled, onBlur, onChange, onExecute, placeholder, value: propsValue, ...rest } = this.props;
-    const { value } = this.state;
+    const { disabled, onBlur, onChange, onExecute, placeholder, value } = this.props;
     return (
       <div className="query" style={{ display: 'flex' }} data-testid="query-input">
         <AceEditor mode="lucene"
+                   disabled={disabled}
                    ref={(editor) => editor && this._bindEditor(editor)}
                    readOnly={disabled}
                    theme="ace-queryinput"
-                   onBlur={this._onBlur}
-                   onChange={this._onChange}
-                   onFocus={this._onFocus}
+                   onBlur={onBlur}
+                   onChange={onChange}
                    value={value}
                    name="QueryEditor"
                    showGutter={false}
@@ -217,7 +107,7 @@ class QueryInput extends Component<Props, State> {
                      height: '34px',
                      width: '100%',
                    }}
-                   {...rest} />
+                   placeholder={placeholder} />
       </div>
     );
   }
