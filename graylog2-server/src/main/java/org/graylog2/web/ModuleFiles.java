31e849f17f7fbe25c520a993da697025b2c5c151
Dennis Oelkers <dennis@graylog.com>
2020-04-10 15:30:40 +0200

    Removing `chunks` field and sorting based on filename.

--- graylog2-server/src/main/java/org/graylog2/web/ModuleFiles.java
+++ graylog2-server/src/main/java/org/graylog2/web/ModuleFiles.java
@@ -18,20 +18,18 @@ package org.graylog2.web;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import org.graylog.autovalue.WithBeanGetter;
 
 import java.util.List;
-import java.util.Map;
 
 @AutoValue
 @WithBeanGetter
 @JsonAutoDetect
+@JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class ModuleFiles {
-    @JsonProperty("chunks")
-    public abstract Map<String, ChunkDescription> chunks();
-
     @JsonProperty("js")
     public abstract List<String> jsFiles();
 
@@ -39,9 +37,8 @@ public abstract class ModuleFiles {
     public abstract List<String> cssFiles();
 
     @JsonCreator
-    public static ModuleFiles create(@JsonProperty("chunks") Map<String, ChunkDescription> chunks,
-                                     @JsonProperty("js") List<String> jsFiles,
+    public static ModuleFiles create(@JsonProperty("js") List<String> jsFiles,
                                      @JsonProperty("css") List<String> cssFiles) {
-        return new AutoValue_ModuleFiles(chunks, jsFiles, cssFiles);
+        return new AutoValue_ModuleFiles(jsFiles, cssFiles);
     }
 }
