a33cb671d93ec7a8724d645f5fb36666964b6a6b
Linus Pahl <lpahl@protonmail.com>
2020-04-14 12:32:27 +0200

    Show loading spinner after starting CSV export

--- graylog2-web-interface/src/views/components/searchbar/csvexport/CSVExportModal.jsx
+++ graylog2-web-interface/src/views/components/searchbar/csvexport/CSVExportModal.jsx
@@ -8,8 +8,8 @@ import connect from 'stores/connect';
 import { FieldTypesStore } from 'views/stores/FieldTypesStore';
 import { defaultSort } from 'views/logic/widgets/MessagesWidgetConfig';
 import FieldTypeMapping from 'views/logic/fieldtypes/FieldTypeMapping';
-import MessagesWidget from 'views/logic/widgets/MessagesWidget';
 import SortConfig from 'views/logic/aggregationbuilder/SortConfig';
+import MessagesWidget from 'views/logic/widgets/MessagesWidget';
 import View from 'views/logic/views/View';
 import Widget from 'views/logic/widgets/Widget';
 
