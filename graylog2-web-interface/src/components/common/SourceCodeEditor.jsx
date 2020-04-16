2654e387f548917bbacfee84012ff4cde7500ef7
Marco Pfatschbacher <marco@graylog.com>
2020-04-16 17:29:20 +0200

    Add comment

--- graylog2-web-interface/src/components/common/SourceCodeEditor.jsx
+++ graylog2-web-interface/src/components/common/SourceCodeEditor.jsx
@@ -223,6 +223,7 @@ class SourceCodeEditor extends React.Component {
             <AceEditor ref={(c) => { this.reactAce = c; }}
                        annotations={annotations}
                        editorProps={{ $blockScrolling: 'Infinity' }}
+                       // Convert Windows line breaks to Unix. See issue #7889
                        setOptions={{ newLineMode: 'unix' }}
                        focus={focus}
                        fontSize={fontSize}
