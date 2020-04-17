5b2094af17e303d3d12cbc15c3af1d03bb7b76a6
Dennis Oelkers <dennis@graylog.com>
2020-04-17 12:32:05 +0200

    Typing map result explicitly.

--- graylog2-web-interface/src/views/components/searchbar/TimeRangeTypeMenuItems.jsx
+++ graylog2-web-interface/src/views/components/searchbar/TimeRangeTypeMenuItems.jsx
@@ -4,7 +4,9 @@ import * as React from 'react';
 import { MenuItem } from 'components/graylog';
 import { availableTimeRangeTypes } from 'views/Constants';
 
-const timeRangeTypeMenuItems = (currentType: string) => availableTimeRangeTypes.map(({ type, name }) => (
+type ResultType = React.Element<MenuItem>;
+
+const timeRangeTypeMenuItems = (currentType: string) => availableTimeRangeTypes.map<ResultType>(({ type, name }) => (
   <MenuItem key={`time-range-type-selector-${type}`}
             eventKey={type}
             active={currentType === type}>
