d271c4b6d3b8f51e858e97a71ac122614ec1d44c
Kyle Knight <kyle@graylog.com>
2020-04-17 13:08:27 -0500

    TimerangeSelector

---
+++ graylog2-web-interface/src/views/components/searchbar/TimerangeSelector.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TimerangeWrap = styled.div(({ theme }) => `
+  .form-group {
+    margin-bottom: 0;
+  }
+
+  .relative {
+    color: ${theme.color.gray[40]};
+  }
+
+  .absolute {
+    font-size: 12px;
+  }
+`);
+
+const TimerangeSelector = ({ className, children, ...restProps }) => {
+  return (
+    <TimerangeWrap className={className} {...restProps}>
+      {children}
+    </TimerangeWrap>
+  );
+};
+
+TimerangeSelector.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+TimerangeSelector.defaultProps = {
+  className: undefined,
+};
+
+export default TimerangeSelector;
