1c7d101769242a7cf75a94b4a81e07fe4d8a97f1
Dennis Oelkers <dennis@graylog.com>
2020-04-17 12:15:16 +0200

    Typing wrapped testing library.

--- graylog2-web-interface/test/wrappedTestingLibrary.jsx
+++ graylog2-web-interface/test/wrappedTestingLibrary.jsx
@@ -1,13 +1,23 @@
-import { render } from '@testing-library/react';
+// @flow strict
+import { type Element } from 'react';
+import { render, type RenderOptionsWithoutCustomQueries } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 
 import WrappingContainer from './WrappingContainer';
 
-export const renderWithWrapper = (Component, options = {}) => render(Component, {
+export const renderWithWrapper = (Component: Element<any>, options: ?RenderOptionsWithoutCustomQueries) => render(Component, {
   wrapper: WrappingContainer,
   ...options,
 });
+
+export function asElement<T>(elem: HTMLElement, elementType: Class<T>): T {
+  if (elem instanceof elementType) {
+    return (elem: T);
+  }
+
+  throw new Error(`Unable to cast ${elem?.constructor?.name ?? 'unknown'} to ${String(elementType)}!`);
+}
 
 export * from '@testing-library/react';
 export {
