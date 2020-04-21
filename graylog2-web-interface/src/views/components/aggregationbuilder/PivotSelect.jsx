0a7b005fcc07db6b32b6aeba05fbfa4e627ce0df
Linus Pahl <lpahl@protonmail.com>
2020-04-21 09:11:07 +0200

    Implement FieldTypesContext for PivotSelect component

--- graylog2-web-interface/src/views/components/aggregationbuilder/PivotSelect.jsx
+++ graylog2-web-interface/src/views/components/aggregationbuilder/PivotSelect.jsx
@@ -1,32 +1,29 @@
 // @flow strict
-import * as React from 'react';
+import React, { useContext } from 'react';
 import * as Immutable from 'immutable';
 import PropTypes from 'prop-types';
-
-import connect from 'stores/connect';
 
 import pivotForField from 'views/logic/searchtypes/aggregation/PivotGenerator';
 import Pivot from 'views/logic/aggregationbuilder/Pivot';
 import FieldType from 'views/logic/fieldtypes/FieldType';
-import { FieldTypesStore } from 'views/stores/FieldTypesStore';
+import FieldTypesContext from 'views/components/contexts/FieldTypesContext';
 import FieldTypeMapping from 'views/logic/fieldtypes/FieldTypeMapping';
 
 import ConfigurableElement from './ConfigurableElement';
 import SortableSelect from './SortableSelect';
 import PivotConfiguration from './PivotConfiguration';
-import CustomPropTypes from '../CustomPropTypes';
 
 const _onChange = (fields, newValue, onChange, fieldTypes) => {
   const newFields = newValue.map((v) => v.value);
 
   return onChange(newFields.map((field) => {
     const existingField = fields.find((f) => f.field === field);
-    const mapping = fieldTypes.find((fieldType) => fieldType.name === field) || new FieldTypeMapping(field, FieldType.Unknown);
+    const mapping = fieldTypes?.find((fieldType) => fieldType.name === field) || new FieldTypeMapping(field, FieldType.Unknown);
     return existingField || pivotForField(field, mapping.type);
   }));
 };
 
-const configFor = ({ value }, values) => (value === '' ? {} : values.find(({ field }) => field === value).config);
+const configFor = ({ value }, values) => (value === '' ? {} : values.find(({ field }) => field === value)?.config);
 
 const newPivotConfigChange = (values, value, newPivotConfig, onChange) => {
   const newValues = values.map((pivot) => {
@@ -38,14 +35,20 @@ const newPivotConfigChange = (values, value, newPivotConfig, onChange) => {
   return onChange(newValues);
 };
 
-const PivotSelect = ({ onChange, fields, value, ...props }) => {
+type Props = {
+  onChange: Array<Pivot> => void,
+  value: Array<Pivot>,
+};
+
+const PivotSelect = ({ onChange, value, ...props }: Props) => {
+  const fieldTypes = useContext(FieldTypesContext);
   // eslint-disable-next-line react/prop-types
   const ValueComponent = ({ children, innerProps, ...rest }) => {
     const element = rest.data;
-    const fieldTypes = fields && fields.all
-      ? fields.all.filter((v) => v.name === element.label)
+    const fields = fieldTypes?.all
+      ? fieldTypes.all.filter((v) => v.name === element.label)
       : Immutable.List();
-    const fieldType = fieldTypes.isEmpty() ? FieldType.Unknown : fieldTypes.first().type;
+    const fieldType = fields.isEmpty() ? FieldType.Unknown : fields.first().type;
     // eslint-disable-next-line react/prop-types
     const { className } = innerProps;
     return (
@@ -60,15 +63,12 @@ const PivotSelect = ({ onChange, fields, value, ...props }) => {
     );
   };
 
-  return <SortableSelect {...props} onChange={(newValue) => _onChange(value, newValue, onChange, fields.all)} value={value} valueComponent={ValueComponent} />;
+  return <SortableSelect {...props} onChange={(newValue) => _onChange(value, newValue, onChange, fieldTypes?.all)} value={value} valueComponent={ValueComponent} />;
 };
 
 PivotSelect.propTypes = {
   onChange: PropTypes.func.isRequired,
-  fields: PropTypes.shape({
-    all: CustomPropTypes.FieldListType,
-  }).isRequired,
   value: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default connect(PivotSelect, { fields: FieldTypesStore });
+export default PivotSelect;
