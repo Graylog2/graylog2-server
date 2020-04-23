4f431011b0f7bd910c8dbf3fb997a513f0b2df39
Konrad Merz <konrad@graylog.com>
2020-04-23 16:52:55 +0200

    Fix linter warnings

--- graylog2-web-interface/src/components/navigation/Navigation.jsx
+++ graylog2-web-interface/src/components/navigation/Navigation.jsx
@@ -10,7 +10,7 @@ import connect from 'stores/connect';
 import StoreProvider from 'injection/StoreProvider';
 import { isPermitted } from 'util/PermissionsMixin';
 import Routes from 'routing/Routes';
-import URLUtils from 'util/URLUtils';
+import { appPrefixed } from 'util/URLUtils';
 import GlobalThroughput from 'components/throughput/GlobalThroughput';
 import { IfPermitted } from 'components/common';
 
@@ -28,11 +28,11 @@ import StyledNavbar from './Navigation.styles';
 const CurrentUserStore = StoreProvider.getStore('CurrentUser');
 
 const _isActive = (requestPath, prefix) => {
-  return requestPath.indexOf(URLUtils.appPrefixed(prefix)) === 0;
+  return requestPath.indexOf(appPrefixed(prefix)) === 0;
 };
 
 const formatSinglePluginRoute = ({ description, path, permissions }, topLevel = false) => {
-  const link = <NavigationLink key={description} description={description} path={URLUtils.appPrefixed(path)} topLevel={topLevel} />;
+  const link = <NavigationLink key={description} description={description} path={appPrefixed(path)} topLevel={topLevel} />;
 
   if (permissions) {
     return <IfPermitted key={description} permissions={permissions}>{link}</IfPermitted>;
@@ -84,7 +84,7 @@ const Navigation = ({ permissions, fullName, location, loginName }) => {
         </Navbar.Brand>
         <Navbar.Toggle />
         <HeaderBadge smallScreen />
-        {pluginItems.map((Item) => <Item smallScreen />)}
+        {pluginItems.map(({ key, compontent: Item }) => <Item key={key} smallScreen />)}
       </Navbar.Header>
 
       <Navbar.Collapse>
@@ -117,7 +117,7 @@ const Navigation = ({ permissions, fullName, location, loginName }) => {
         <Nav navbar pullRight className="header-meta-nav">
           <InactiveNavItem className="dev-badge-wrap">
             <HeaderBadge />
-            {pluginItems.map((Item) => <Item />)}
+            {pluginItems.map(({ key, component: Item }) => <Item key={key} />)}
           </InactiveNavItem>
 
           <LinkContainer to={Routes.SYSTEM.NODES.LIST}>
