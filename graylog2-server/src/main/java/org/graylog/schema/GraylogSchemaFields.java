4c2ebfb6ba98111d482a923f89479f1209bb7adc
Rob Curtis <rob.curtis@graylog.com>
2020-04-27 15:38:14 -0700

    Move to org.graylog.schema

--- graylog2-server/src/main/java/org/graylog/schema/GraylogSchemaFields.java
+++ graylog2-server/src/main/java/org/graylog/schema/GraylogSchemaFields.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.graylog2.inputs;
+package org.graylog.schema;
 
 /**
  * Field names used in the standard Graylog Schema.
