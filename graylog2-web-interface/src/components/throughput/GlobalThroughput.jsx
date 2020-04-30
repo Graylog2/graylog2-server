4c6dd96adbfe099dff05eba6a531a5f7db096070
Kyle Knight <kyle@graylog.com>
2020-04-30 08:28:42 -0500

    Rewrite GlobalThroughput

--- graylog2-web-interface/src/components/throughput/GlobalThroughput.jsx
+++ graylog2-web-interface/src/components/throughput/GlobalThroughput.jsx
@@ -1,53 +1,52 @@
-import React from 'react';
-import createReactClass from 'create-react-class';
-import Reflux from 'reflux';
+import React, { useEffect, useState } from 'react';
 import numeral from 'numeral';
-import { NavItem } from 'components/graylog';
 
+import { useStore } from 'stores/connect';
 import StoreProvider from 'injection/StoreProvider';
 
+import { NavItem } from 'components/graylog';
 import { Spinner } from 'components/common';
 
 import styles from './GlobalThroughput.css';
 
 const GlobalThroughputStore = StoreProvider.getStore('GlobalThroughput');
 
-const GlobalThroughput = createReactClass({
-  displayName: 'GlobalThroughput',
-  mixins: [Reflux.connect(GlobalThroughputStore)],
-
-  getInitialState() {
-    return { throughput: { loading: true } };
-  },
-
-  render() {
-    const { throughput } = this.state;
-    let output = <Spinner text="" />;
-
-    if (!throughput.loading) {
-      const inputNumeral = numeral(throughput.input).format('0,0');
-      const outputNumeral = numeral(throughput.output).format('0,0');
-
-      output = (
-        <strong className={styles['total-throughput__content']}
-                aria-label={`In ${inputNumeral} / Out ${outputNumeral} msg/s`}>
-          <span className={`${styles['total-throughput__data']} ${styles['total-throughput__data--in']}`}>
-            <span>{inputNumeral}</span> <i>in</i>
-          </span>
-          <span className={styles['total-throughput__data']}>
-            <span>{outputNumeral}</span> <i>out</i>
-          </span>
-        </strong>
-      );
-    }
+const GlobalThroughput = (props) => {
+  // const [throughput, setThroughput] = useState({ throughput: { loading: true } });
+  const throughput = useStore(GlobalThroughputStore);
 
 
-    return (
-      <NavItem className={styles['total-throughput']} {...this.props}>
-        {output}
-      </NavItem>
+  // useEffect(() => {
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+
+  let output = <Spinner text="" />;
+
+  if (!throughput.loading) {
+    const inputNumeral = numeral(throughput.input).format('0,0');
+    const outputNumeral = numeral(throughput.output).format('0,0');
+
+    output = (
+      <strong className={styles['total-throughput__content']}
+              aria-label={`In ${inputNumeral} / Out ${outputNumeral} msg/s`}>
+        <span className={`${styles['total-throughput__data']} ${styles['total-throughput__data--in']}`}>
+          <span>{inputNumeral}</span> <i>in</i>
+        </span>
+        <span className={styles['total-throughput__data']}>
+          <span>{outputNumeral}</span> <i>out</i>
+        </span>
+      </strong>
     );
-  },
-});
+  }
+
+  return (
+    <NavItem className={styles['total-throughput']} {...props}>
+      {output}
+    </NavItem>
+  );
+};
+
 
 export default GlobalThroughput;
