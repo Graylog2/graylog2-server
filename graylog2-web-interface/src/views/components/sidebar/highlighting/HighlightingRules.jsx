6736097c291cc72baac5c6481d4200d234ef06b9
Linus Pahl <linus@graylog.com>
2020-04-30 17:43:32 +0200

    Implement styled components for sidebar highlighting rules * Fix styling for fields with long name * Rename ColorBox to ColorPreview * Adjust HighlightingRules test and remove screenshots

--- graylog2-web-interface/src/views/components/sidebar/highlighting/HighlightingRules.jsx
+++ graylog2-web-interface/src/views/components/sidebar/highlighting/HighlightingRules.jsx
@@ -1,6 +1,7 @@
 // @flow strict
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import connect from 'stores/connect';
 
@@ -8,9 +9,12 @@ import { DEFAULT_HIGHLIGHT_COLOR } from 'views/Constants';
 import { HighlightingRulesStore } from 'views/stores/HighlightingRulesStore';
 import Rule from 'views/logic/views/formatting/highlighting/HighlightingRule';
 
-import HighlightingRule from './HighlightingRule';
-import ColorBox from './ColorBox';
-import styles from './HighlightingRules.css';
+import HighlightingRule, { HighlightingRuleGrid } from './HighlightingRule';
+import ColorPreview from './ColorPreview';
+
+const Headline = styled.h4`
+  margin-bottom: 10px;
+`;
 
 type Props = {
   rules: Array<Rule>,
@@ -19,11 +23,11 @@ type Props = {
 const HighlightingRules = ({ rules = [] }: Props) => {
   return (
     <>
-      <h4 className={styles.headline}>Highlighting</h4>
-      <div id="search-term-color" className={styles.highlightingRuleBlock}>
-        <ColorBox color={DEFAULT_HIGHLIGHT_COLOR} />
-        <span>Search terms</span>
-      </div>
+      <Headline>Highlighting</Headline>
+      <HighlightingRuleGrid>
+        <ColorPreview color={DEFAULT_HIGHLIGHT_COLOR} />
+        <div>Search terms</div>
+      </HighlightingRuleGrid>
 
       {rules.map((rule) => <HighlightingRule key={`${rule.field}-${rule.value}-${rule.color}`} rule={rule} />)}
     </>
