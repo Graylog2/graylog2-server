75e8eba512000c10ec4f0950746492bf69a9ebbb
Marco Pfatschbacher <marco@graylog.com>
2020-05-11 16:03:43 +0200

    Sanitize Hyperlink String Decorator links
    (cherry picked from commit b60de306f71250535ad6dcb65a9c13984427374b)

--- graylog2-server/src/main/java/org/graylog2/decorators/LinkFieldDecorator.java
+++ graylog2-server/src/main/java/org/graylog2/decorators/LinkFieldDecorator.java
@@ -16,14 +16,11 @@
  */
 package org.graylog2.decorators;
 
-import com.floreysoft.jmte.Engine;
-import com.floreysoft.jmte.template.Template;
-import com.floreysoft.jmte.template.VariableDescription;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.assistedinject.Assisted;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
-import org.graylog2.plugin.configuration.fields.BooleanField;
 import org.graylog2.plugin.configuration.fields.TextField;
 import org.graylog2.plugin.decorators.SearchResponseDecorator;
 import org.graylog2.rest.models.messages.responses.ResultMessageSummary;
@@ -39,7 +36,7 @@ import static java.util.Objects.requireNonNull;
 
 public class LinkFieldDecorator implements SearchResponseDecorator {
 
-    private static final String CK_LINK_FIELD = "link_field";
+    public static final String CK_LINK_FIELD = "link_field";
 
     private final String linkField;
 
@@ -92,14 +89,36 @@ public class LinkFieldDecorator implements SearchResponseDecorator {
                     }
                     final Message message = new Message(ImmutableMap.copyOf(summary.message()));
                     final String href = (String) summary.message().get(linkField);
-                    final Map<String, String> decoratedField = new HashMap<>();
-                    decoratedField.put("type", "a");
-                    decoratedField.put("href", href);
-                    message.addField(linkField, decoratedField);
+                    if (isValidUrl(href)) {
+                        final Map<String, String> decoratedField = new HashMap<>();
+                        decoratedField.put("type", "a");
+                        decoratedField.put("href", href);
+                        message.addField(linkField, decoratedField);
+                    } else {
+                        message.addField(linkField, href);
+                    }
                     return summary.toBuilder().message(message.getFields()).build();
                 })
                 .collect(Collectors.toList());
 
         return searchResponse.toBuilder().messages(summaries).build();
+    }
+
+    /**
+     * @param url a String URL.
+     * @return true if the URL is valid and false if the URL is invalid.
+     *
+     * All URLS that do not start with the protocol "http" or "https" protocol scheme are considered invalid.
+     * All other non-URL text strings will be considered invalid. This includes inline javascript expressions such as:
+     *  - javascript:...
+     *  - alert()
+     *  - or any other javascript expressions.
+     */
+    private boolean isValidUrl(String url) {
+        String[] schemes = {"http", "https"};
+        // UrlValidator.ALLOW_LOCAL_URLS allows local links to be permitted such as http://my-local-server
+        // Some users may reference such local URLs, and there should be no issue with doing so.
+        UrlValidator urlValidator = new UrlValidator(schemes, UrlValidator.ALLOW_LOCAL_URLS + UrlValidator.ALLOW_2_SLASHES);
+        return urlValidator.isValid(url);
     }
 }
