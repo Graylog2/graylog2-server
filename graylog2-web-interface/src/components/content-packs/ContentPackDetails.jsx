050943fd5322fa2ce26b1398023456db1dd69329
Edmundo Alvarez <edmundo@graylog.com>
2020-05-12 13:46:08 +0200

    Sanitize Content Pack URLs (#8072)
    * Fix linter errors
    
    * Validate URL in Content Pack form
    
    Given URL must have an http or https protocol and be valid.
    
    * Render anchors for HTTP URLs exclusively
    
    If a URL does not uses HTTP or HTTPS protocols, render it as plain text.
    
    * Improve markdown sanitation
    
    Replace deprecated `markdown` library with `marked` and use `DOMPurify`
    to ensure the rendered contents are safe.
    
    * Fix another linter error
    
    * Update snapshots

--- graylog2-web-interface/src/components/content-packs/ContentPackDetails.jsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackDetails.jsx
@@ -1,23 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-
-import { markdown } from 'markdown';
-import { Row, Col, Well } from 'components/graylog';
+import { Col, Row, Well } from 'components/graylog';
+import marked from 'marked';
+import DOMPurify from 'dompurify';
 
 import ContentPackStatus from 'components/content-packs/ContentPackStatus';
 import ContentPackConstraints from 'components/content-packs/ContentPackConstraints';
 import ContentPackEntitiesList from 'components/content-packs/ContentPackEntitiesList';
 import ContentPackParameterList from 'components/content-packs/ContentPackParameterList';
 import 'components/content-packs/ContentPackDetails.css';
+import { hasAcceptedProtocol } from 'util/URLUtils';
 
 const ContentPackDetails = (props) => {
-  const markdownDescription = markdown.toHTML(props.contentPack.description || '');
-  const { contentPack } = props;
-  const { constraints } = props;
+  const { contentPack, offset, verbose, constraints, showConstraints } = props;
+  const markdownDescription = DOMPurify.sanitize(marked(contentPack.description || ''));
+  let contentPackAnchor = contentPack.url;
+  try {
+    if (hasAcceptedProtocol(contentPack.url)) {
+      contentPackAnchor = <a href={contentPack.url}>{contentPack.url}</a>;
+    }
+  } catch (e) {
+    // Do nothing
+  }
 
   return (
     <Row>
-      <Col smOffset={props.offset} sm={9}>
+      <Col smOffset={offset} sm={9}>
         <div id="content-pack-details">
           <h2>Details</h2>
           <br />
@@ -27,10 +35,10 @@ const ContentPackDetails = (props) => {
               <dt>Name:</dt> <dd>{contentPack.name}&nbsp;</dd>
               <dt>Summary:</dt> <dd>{contentPack.summary}&nbsp;</dd>
               <dt>Vendor:</dt> <dd>{contentPack.vendor}&nbsp;</dd>
-              <dt>URL:</dt> <dd><a href={contentPack.url}>{contentPack.url}</a>&nbsp;</dd>
+              <dt>URL:</dt> <dd>{contentPackAnchor}&nbsp;</dd>
               { contentPack.id && (<span><dt>ID:</dt> <dd><code>{contentPack.id}</code></dd></span>) }
-              { contentPack.parameters && !props.verbose && (<span><dt>Parameters:</dt> <dd>{contentPack.parameters.length}</dd></span>) }
-              { contentPack.entities && !props.verbose && (<span><dt>Entities:</dt> <dd>{contentPack.entities.length}</dd></span>) }
+              { contentPack.parameters && !verbose && (<span><dt>Parameters:</dt> <dd>{contentPack.parameters.length}</dd></span>) }
+              { contentPack.entities && !verbose && (<span><dt>Entities:</dt> <dd>{contentPack.entities.length}</dd></span>) }
             </dl>
           </div>
           { contentPack.description
@@ -48,17 +56,17 @@ const ContentPackDetails = (props) => {
           { contentPack.status && <ContentPackStatus states={contentPack.states} /> }
           <br />
           <br />
-          { contentPack.constraints && props.showConstraints
+          { contentPack.constraints && showConstraints
           && (
           <div>
             <ContentPackConstraints constraints={constraints} />
             <br />
           </div>
           )}
-          { contentPack.entities && contentPack.entities.length > 0 && props.verbose
-          && <ContentPackEntitiesList contentPack={props.contentPack} readOnly />}
-          { contentPack.parameters && contentPack.parameters.length > 0 && props.verbose
-          && <ContentPackParameterList contentPack={props.contentPack} readOnly />}
+          { contentPack.entities && contentPack.entities.length > 0 && verbose
+          && <ContentPackEntitiesList contentPack={contentPack} readOnly />}
+          { contentPack.parameters && contentPack.parameters.length > 0 && verbose
+          && <ContentPackParameterList contentPack={contentPack} readOnly />}
         </div>
       </Col>
     </Row>
