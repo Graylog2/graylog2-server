1cccbcddeee9fd01e7c31963b8db3b43ed17ba12
Dennis Oelkers <dennis@graylog.com>
2020-05-14 11:39:43 +0200

    Return colors from themed scale.

--- graylog2-web-interface/src/views/components/contexts/GlobalColorProvider.jsx
+++ graylog2-web-interface/src/views/components/contexts/GlobalColorProvider.jsx
@@ -1,16 +1,28 @@
 // @flow strict
 import * as React from 'react';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import chroma from 'chroma-js';
+import { withTheme } from 'styled-components';
+import { type ThemeInterface } from 'theme';
+
 import ViewColorContext from './ViewColorContext';
 
 const pickColor = (assignedColors: { [string]: string }) => chroma.random().toString();
 
 type Props = {
   children: React.Node,
+  theme: ThemeInterface,
 };
-const GlobalColorProvider = ({ children }: Props) => {
+
+const scaleFromTheme = (theme) => {
+  return ['light', 'dark']
+    .flatMap((lightness) => ['info', 'primary', 'success', 'warning', 'danger']
+      .map((variant) => theme.color.variant[lightness][variant]));
+};
+
+const GlobalColorProvider = ({ children, theme }: Props) => {
   const [assignedColors, setAssignedColors] = useState({});
+  const scale = useMemo(() => scaleFromTheme(theme), [theme]);
   const setColor = useCallback((name, color) => {
     const newAssignedColors = { ...assignedColors, [name]: color };
     setAssignedColors(newAssignedColors);
@@ -19,7 +31,7 @@ const GlobalColorProvider = ({ children }: Props) => {
     if (assignedColors[name]) {
       return assignedColors[name];
     }
-    const color = pickColor(assignedColors);
+    const color = scale[Object.entries(assignedColors).length % scale.length];
 
     setColor(name, color);
     return color;
@@ -31,4 +43,4 @@ const GlobalColorProvider = ({ children }: Props) => {
   );
 };
 
-export default GlobalColorProvider;
+export default withTheme(GlobalColorProvider);
