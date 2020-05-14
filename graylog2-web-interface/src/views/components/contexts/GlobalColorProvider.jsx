801ab1574e54c94e69c193d46366dbe63e46a0dc
Dennis Oelkers <dennis@graylog.com>
2020-05-14 11:50:09 +0200

    Include default variants.

--- graylog2-web-interface/src/views/components/contexts/GlobalColorProvider.jsx
+++ graylog2-web-interface/src/views/components/contexts/GlobalColorProvider.jsx
@@ -1,13 +1,10 @@
 // @flow strict
 import * as React from 'react';
 import { useCallback, useMemo, useState } from 'react';
-import chroma from 'chroma-js';
 import { withTheme } from 'styled-components';
 import { type ThemeInterface } from 'theme';
 
 import ViewColorContext from './ViewColorContext';
-
-const pickColor = (assignedColors: { [string]: string }) => chroma.random().toString();
 
 type Props = {
   children: React.Node,
@@ -15,9 +12,9 @@ type Props = {
 };
 
 const scaleFromTheme = (theme) => {
-  return ['light', 'dark']
+  return ['', 'light', 'dark']
     .flatMap((lightness) => ['info', 'primary', 'success', 'warning', 'danger']
-      .map((variant) => theme.color.variant[lightness][variant]));
+      .map((variant) => (lightness !== '' ? theme.color.variant[lightness][variant] : theme.color.variant[variant])));
 };
 
 const GlobalColorProvider = ({ children, theme }: Props) => {
