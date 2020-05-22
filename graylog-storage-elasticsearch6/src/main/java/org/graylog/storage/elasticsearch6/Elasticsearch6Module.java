292c4fafd8999f4a38aebb14636530700439c2a7
Dennis Oelkers <dennis@graylog.com>
2020-05-22 10:10:53 +0200

    Making Elasticsearch6 support a plugin.

---
+++ graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/Elasticsearch6Module.java
@@ -0,0 +1,9 @@
+package org.graylog.storage.elasticsearch6;
+
+import org.graylog2.plugin.PluginModule;
+
+public class Elasticsearch6Module extends PluginModule {
+    @Override
+    protected void configure() {
+    }
+}
