612fdb3fa6bd046951c9410bcfc768d656027200
Dennis Oelkers <dennis@graylog.com>
2020-05-25 18:10:11 +0200

    Removing DTOs left over from map plugin.

--- graylog2-server/src/main/java/org/graylog/plugins/map/MapWidgetModule.java
+++ graylog2-server/src/main/java/org/graylog/plugins/map/MapWidgetModule.java
@@ -24,8 +24,6 @@ public class MapWidgetModule extends PluginModule {
     @Override
     protected void configure() {
         addMessageProcessor(GeoIpProcessor.class, GeoIpProcessor.Descriptor.class);
-        registerRestControllerPackage(getClass().getPackage().getName());
-
         installLookupDataAdapter(MaxmindDataAdapter.NAME,
                 MaxmindDataAdapter.class,
                 MaxmindDataAdapter.Factory.class,
