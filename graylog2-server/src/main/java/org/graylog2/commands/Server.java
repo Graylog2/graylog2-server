cc2db1e729c03bdd52002230a122a96e26061def
Dennis Oelkers <dennis@graylog.com>
2020-05-25 17:28:07 +0200

    Removing Widget Strategy interface and binding helpers.

--- graylog2-server/src/main/java/org/graylog2/commands/Server.java
+++ graylog2-server/src/main/java/org/graylog2/commands/Server.java
@@ -52,7 +52,6 @@ import org.graylog2.bindings.PasswordAlgorithmBindings;
 import org.graylog2.bindings.PeriodicalBindings;
 import org.graylog2.bindings.PersistenceServicesBindings;
 import org.graylog2.bindings.ServerBindings;
-import org.graylog2.bindings.WidgetStrategyBindings;
 import org.graylog2.bootstrap.Main;
 import org.graylog2.bootstrap.ServerBootstrap;
 import org.graylog2.cluster.NodeService;
@@ -147,7 +146,6 @@ public class Server extends ServerBootstrap {
                 new ObjectMapperModule(chainingClassLoader),
                 new RestApiBindings(),
                 new PasswordAlgorithmBindings(),
-                new WidgetStrategyBindings(),
                 new DecoratorBindings(),
                 new AuditBindings(),
                 new AlertConditionBindings(),
