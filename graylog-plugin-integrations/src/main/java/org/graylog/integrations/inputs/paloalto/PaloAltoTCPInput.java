3502bb2a714ba25d853140f2602ee5cc4d89f463
CHunter <nekto@must-die.ru>
2020-05-26 19:57:11 +0300

    Update PaloAltoTCPInput.java (fix 7345 issue) (Graylog2/graylog-plugin-integrations#486)

--- graylog-plugin-integrations/src/main/java/org/graylog/integrations/inputs/paloalto/PaloAltoTCPInput.java
+++ graylog-plugin-integrations/src/main/java/org/graylog/integrations/inputs/paloalto/PaloAltoTCPInput.java
@@ -38,7 +38,7 @@ import static org.graylog.integrations.inputs.paloalto.PaloAltoCodec.CK_TRAFFIC_
 
 public class PaloAltoTCPInput extends MessageInput {
 
-    public static final String NAME = "Palo Alto Networks Input (TCP)";
+    public static final String NAME = "Palo Alto Networks (TCP)";
 
     private static final Logger LOG = LoggerFactory.getLogger(PaloAltoTCPInput.class);
 
