9b1ca51106af112527b9a8f19c34c3d8560d2d6e
Linus Pahl <linus@graylog.com>
2020-06-03 16:42:52 +0200

    Implement HighlightingRulesContext for CustomHighlighting

--- graylog2-web-interface/src/views/components/messagelist/CustomHighlighting.test.jsx
+++ graylog2-web-interface/src/views/components/messagelist/CustomHighlighting.test.jsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { mount } from 'wrappedEnzyme';
 
+import HighlightingRuleContext from 'views/components/contexts/HighlightingRulesContext';
 import DecoratorContext from 'views/components/messagelist/decoration/DecoratorContext';
 import HighlightingRule from 'views/logic/views/formatting/highlighting/HighlightingRule';
 import FieldType from 'views/logic/fieldtypes/FieldType';
@@ -17,29 +18,28 @@ const renderDecorators = (decorators, field, value) => decorators.map((Decorator
 describe('CustomHighlighting', () => {
   const field = 'foo';
   const value = 42;
-  it('renders value as is when no rules exist', () => {
-    const wrapper = mount((
-      <CustomHighlighting field={field} value={value} highlightingRules={{}}>
+  const SimpleCustomHighlighting = ({ highlightingRules }: {highlightingRules: Array<HighlightingRule>}) => (
+    <HighlightingRuleContext.Provider value={highlightingRules}>
+      <CustomHighlighting field={field} value={value}>
         <DecoratorContext.Consumer>
           {(decorators) => renderDecorators(decorators, field, value)}
         </DecoratorContext.Consumer>
       </CustomHighlighting>
-    ));
+    </HighlightingRuleContext.Provider>
+  );
+
+  it('renders value as is when no rules exist', () => {
+    const wrapper = mount(<SimpleCustomHighlighting highlightingRules={[]} />);
     expect(wrapper.find('PossiblyHighlight')).toMatchSnapshot();
   });
   it('renders value as is when no rule for this field exists', () => {
     const rule = HighlightingRule.builder()
-      .field(field)
+      .field('bar')
       .value(String(value))
       .color('#bc98fd')
       .build();
-    const wrapper = mount((
-      <CustomHighlighting field={field} value={value} highlightingRules={{ bar: [rule] }}>
-        <DecoratorContext.Consumer>
-          {(decorators) => renderDecorators(decorators, field, value)}
-        </DecoratorContext.Consumer>
-      </CustomHighlighting>
-    ));
+    const wrapper = mount(<SimpleCustomHighlighting highlightingRules={[rule]} />);
+
     expect(wrapper.find('PossiblyHighlight')).toMatchSnapshot();
   });
   it('renders highlighted value if rule for value exists', () => {
@@ -48,13 +48,7 @@ describe('CustomHighlighting', () => {
       .value(String(value))
       .color('#bc98fd')
       .build();
-    const wrapper = mount((
-      <CustomHighlighting field={field} value={value} highlightingRules={{ [field]: [rule] }}>
-        <DecoratorContext.Consumer>
-          {(decorators) => renderDecorators(decorators, field, value)}
-        </DecoratorContext.Consumer>
-      </CustomHighlighting>
-    ));
+    const wrapper = mount(<SimpleCustomHighlighting highlightingRules={[rule]} />);
     expect(wrapper.find('PossiblyHighlight')).toMatchSnapshot();
   });
   it('does not render highlight if rule value only matches substring', () => {
@@ -63,13 +57,7 @@ describe('CustomHighlighting', () => {
       .value('2')
       .color('#bc98fd')
       .build();
-    const wrapper = mount((
-      <CustomHighlighting field={field} value={value} highlightingRules={{ [field]: [rule] }}>
-        <DecoratorContext.Consumer>
-          {(decorators) => renderDecorators(decorators, field, value)}
-        </DecoratorContext.Consumer>
-      </CustomHighlighting>
-    ));
+    const wrapper = mount(<SimpleCustomHighlighting highlightingRules={[rule]} />);
     expect(wrapper.find('PossiblyHighlight')).toMatchSnapshot();
   });
   it('does not render highlight if rule value does not match', () => {
@@ -78,13 +66,7 @@ describe('CustomHighlighting', () => {
       .value('23')
       .color('#bc98fd')
       .build();
-    const wrapper = mount((
-      <CustomHighlighting field={field} value={value} highlightingRules={{ [field]: [rule] }}>
-        <DecoratorContext.Consumer>
-          {(decorators) => renderDecorators(decorators, field, value)}
-        </DecoratorContext.Consumer>
-      </CustomHighlighting>
-    ));
+    const wrapper = mount(<SimpleCustomHighlighting highlightingRules={[rule]} />);
     expect(wrapper.find('PossiblyHighlight')).toMatchSnapshot();
   });
 });
