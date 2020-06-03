64d87810f2d69dfffb62099ade94dfa134af2336
Linus Pahl <linus@graylog.com>
2020-06-03 17:07:55 +0200

    Implement HighlightingRulesContext for HighlightingRules

--- graylog2-web-interface/src/views/components/sidebar/highlighting/HighlightingRules.test.jsx
+++ graylog2-web-interface/src/views/components/sidebar/highlighting/HighlightingRules.test.jsx
@@ -2,14 +2,23 @@
 import * as React from 'react';
 import { mount } from 'wrappedEnzyme';
 
+import HighlightingRuleContext from 'views/components/contexts/HighlightingRulesContext';
 import HighlightingRule from 'views/logic/views/formatting/highlighting/HighlightingRule';
 import HighlightingRules from './HighlightingRules';
 
 jest.mock('stores/connect', () => (x) => x);
 
 describe('HighlightingRules', () => {
-  it('includes search term legend even if rules are empty', () => {
+  it('renders search term legend even when HighlightingRulesContext.Provider is not defined', () => {
     const wrapper = mount(<HighlightingRules />);
+    expect(wrapper.text()).toMatch(/Search terms/);
+  });
+  it('renders search term legend even when rules are empty', () => {
+    const wrapper = mount(
+      <HighlightingRuleContext.Provider value={undefined}>
+        <HighlightingRules />
+      </HighlightingRuleContext.Provider>,
+    );
     expect(wrapper.text()).toMatch(/Search terms/);
   });
   it('renders element for each HighlightingRule', () => {
@@ -17,7 +26,11 @@ describe('HighlightingRules', () => {
       HighlightingRule.create('foo', 'bar', undefined, '#f4f141'),
       HighlightingRule.create('response_time', '250', undefined, '#f44242'),
     ];
-    const wrapper = mount(<HighlightingRules rules={rules} />);
+    const wrapper = mount(
+      <HighlightingRuleContext.Provider value={rules}>
+        <HighlightingRules />
+      </HighlightingRuleContext.Provider>,
+    );
     expect(wrapper.text()).toMatch(/for foo = "bar"/);
     expect(wrapper.text()).toMatch(/for response_time = "250"/);
   });
