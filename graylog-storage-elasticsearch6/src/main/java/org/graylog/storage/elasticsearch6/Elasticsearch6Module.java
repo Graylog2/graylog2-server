12e9c825c0a981ba77041fba5314239d8040e988
Dennis Oelkers <dennis@graylog.com>
2020-06-03 15:58:10 +0200

    Binding implementation for `MessagesAdapter`.

--- graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/Elasticsearch6Module.java
+++ graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/Elasticsearch6Module.java
@@ -2,6 +2,7 @@ package org.graylog.storage.elasticsearch6;
 
 import org.graylog.events.search.MoreSearchAdapter;
 import org.graylog2.indexer.searches.SearchesAdapter;
+import org.graylog2.indexer.messages.MessagesAdapter;
 import org.graylog2.plugin.PluginModule;
 
 public class Elasticsearch6Module extends PluginModule {
@@ -9,5 +10,6 @@ public class Elasticsearch6Module extends PluginModule {
     protected void configure() {
         bind(SearchesAdapter.class).to(SearchesAdapterES6.class);
         bind(MoreSearchAdapter.class).to(MoreSearchAdapterES6.class);
+        bind(MessagesAdapter.class).to(MessagesAdapterES6.class);
     }
 }
