24828ee8e1159a02d73f3a0cb4ad2f49b6aa53e0
Alex Konn <alexander@graylog.com>
2020-06-04 13:49:06 +0200

    Fix unconditional warning (#8277)
    * Fix unconditional warning
    
    This was an oversight in pr #8245, which led to tons of misleading
    warnings.
    
    * fix formatting glitch

--- graylog2-server/src/main/java/org/graylog2/indexer/messages/Messages.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/messages/Messages.java
@@ -167,7 +167,7 @@ public class Messages {
         int chunkSize = messageList.size();
         int offset = 0;
         List<BulkResult.BulkResultItem> failedItems = new ArrayList<>();
-        for (; ; ) {
+        for (;;) {
             try {
                 List<BulkResult.BulkResultItem> failures = bulkIndexChunked(messageList, isSystemTraffic, offset, chunkSize);
                 failedItems.addAll(failures);
@@ -264,7 +264,9 @@ public class Messages {
         final Set<BulkResult.BulkResultItem> otherFailures = new HashSet<>(Sets.difference(new HashSet<>(allFailedItems), indexBlocks));
         List<Map.Entry<IndexSet, Message>> blockedMessages = messagesForResultItems(chunk, indexBlocks);
 
-        LOG.warn("Retrying {} messages, because their indices are blocked with status [read-only / allow delete]", indexBlocks.size());
+        if (!indexBlocks.isEmpty()) {
+            LOG.warn("Retrying {} messages, because their indices are blocked with status [read-only / allow delete]", indexBlocks.size());
+        }
 
         long attempt = 1;
 
