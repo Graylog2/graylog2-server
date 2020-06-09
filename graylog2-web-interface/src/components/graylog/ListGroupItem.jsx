29582debd9cbd98e77c6d31e150db63ab28e979b
Kyle Knight <kyle@graylog.com>
2020-06-09 12:57:05 -0500

    ListGroupItem revamp

--- graylog2-web-interface/src/components/graylog/ListGroupItem.jsx
+++ graylog2-web-interface/src/components/graylog/ListGroupItem.jsx
@@ -4,14 +4,17 @@ import styled, { css } from 'styled-components';
 // eslint-disable-next-line no-restricted-imports
 import { ListGroupItem as BootstrapListGroupItem } from 'react-bootstrap';
 
-import bsStyleThemeVariant from './variants/bsStyle';
 
-const listGroupItemStyles = (hex, variant) => css(({ theme }) => {
-  const backgroundColor = theme.utils.colorLevel(theme.colors.variant.light[variant], -5);
+const variantStyles = css(({ bsStyle, theme }) => {
+  if (!bsStyle) {
+    return undefined;
+  }
+
+  const backgroundColor = theme.colors.variant.lighter[bsStyle];
   const textColor = theme.utils.readableColor(backgroundColor);
 
   return css`
-    &.list-group-item-${variant} {
+    &.list-group-item-${bsStyle} {
       color: ${textColor};
       background-color: ${backgroundColor};
 
@@ -27,15 +30,15 @@ const listGroupItemStyles = (hex, variant) => css(({ theme }) => {
         &:hover,
         &:focus {
           color: ${textColor};
-          background-color: ${theme.colors.variant.light[variant]};
+          background-color: ${theme.colors.variant.light[bsStyle]};
         }
 
         &.active,
         &.active:hover,
         &.active:focus {
-          color: ${theme.utils.readableColor(theme.colors.variant.light[variant])};
-          background-color: ${theme.colors.variant.light[variant]};
-          border-color: ${theme.colors.variant.light[variant]};
+          color: ${theme.utils.readableColor(theme.colors.variant.light[bsStyle])};
+          background-color: ${theme.colors.variant.light[bsStyle]};
+          border-color: ${theme.colors.variant.light[bsStyle]};
         }
       }
     }
@@ -112,7 +115,7 @@ const StyledListGroupItem = styled(BootstrapListGroupItem)(({ theme }) => css`
     }
   }
 
-  ${bsStyleThemeVariant(listGroupItemStyles)}
+  ${variantStyles};
 `);
 
 const ListGroupItem = forwardRef(({ bsStyle, ...props }, ref) => {
