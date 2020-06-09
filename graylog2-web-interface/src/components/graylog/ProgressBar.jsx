227c82bc64fb267cdb7a718f7e9cae641ab8110a
Kyle Knight <kyle@graylog.com>
2020-06-09 13:41:12 -0500

    ProgressBar revamp

--- graylog2-web-interface/src/components/graylog/ProgressBar.jsx
+++ graylog2-web-interface/src/components/graylog/ProgressBar.jsx
@@ -7,8 +7,6 @@ import chroma from 'chroma-js';
 
 import { type ThemeInterface } from 'theme';
 
-import bsStyleThemeVariant from './variants/bsStyle';
-
 type ProgressBarProps = {
   bars: Array<{
     animated: boolean,
@@ -48,9 +46,9 @@ const animatedStripes = keyframes`
   }
 `;
 
-const progressBarVariants = (color) => css(({ theme }) => `
-  background-color: ${color};
-  color: ${theme.utils.readableColor(color)};
+const progressBarVariants = css(({ bsStyle, theme }) => `
+  background-color: ${theme.colors.variant[bsStyle]};
+  color: ${theme.utils.readableColor(theme.colors.variant[bsStyle])};
 `);
 
 const ProgressWrap: StyledComponent<{}, ThemeInterface, HTMLDivElement> = styled.div(({ theme }) => css`
@@ -93,7 +91,7 @@ const Bar: StyledComponent<BarProps, ThemeInterface, HTMLDivElement> = styled.di
     ${animated && css`
       animation: ${animatedStripes} 2s linear infinite;
     `}
-    ${bsStyleThemeVariant(progressBarVariants)}
+    ${progressBarVariants}
   `;
 });
 
