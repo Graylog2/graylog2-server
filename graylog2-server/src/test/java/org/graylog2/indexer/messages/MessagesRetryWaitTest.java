08cde9210334238cf7dd194f39c329740c047627
Bernd Ahlers <bernd@graylog.com>
2020-06-09 19:18:25 +0200

    Switch MessagesRetryWaitTest to JUnit 4
    We don't have JUnit 5 in 3.2.

--- graylog2-server/src/test/java/org/graylog2/indexer/messages/MessagesRetryWaitTest.java
+++ graylog2-server/src/test/java/org/graylog2/indexer/messages/MessagesRetryWaitTest.java
@@ -17,34 +17,29 @@
 package org.graylog2.indexer.messages;
 
 import com.github.rholder.retry.WaitStrategy;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class MessagesRetryWaitTest {
     @Test
-    void secondsBasedRetryWaitsForSecondsStartingWith1() {
+    public void secondsBasedRetryWaitsForSecondsStartingWith1() {
         WaitStrategy waitStrategy = Messages.exponentialWaitSeconds;
-        assertAll(
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(1))).isEqualTo(1000),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(2))).isEqualTo(2000),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(3))).isEqualTo(4000),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(4))).isEqualTo(8000),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(5))).isEqualTo(16000)
-        );
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(1))).isEqualTo(1000);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(2))).isEqualTo(2000);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(3))).isEqualTo(4000);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(4))).isEqualTo(8000);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(5))).isEqualTo(16000);
     }
 
     // This test was added to document how the retry strategy actually behaves since this is hard to deduct from the code
     @Test
-    void millisecondsBasedRetryWaitsForMillisecondsStartingWith2() {
+    public void millisecondsBasedRetryWaitsForMillisecondsStartingWith2() {
         WaitStrategy waitStrategy = Messages.exponentialWaitMilliseconds;
-        assertAll(
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(1))).isEqualTo(2),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(2))).isEqualTo(4),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(3))).isEqualTo(8),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(4))).isEqualTo(16),
-                () -> assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(5))).isEqualTo(32)
-        );
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(1))).isEqualTo(2);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(2))).isEqualTo(4);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(3))).isEqualTo(8);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(4))).isEqualTo(16);
+        assertThat(waitStrategy.computeSleepTime(new IndexBlockRetryAttempt(5))).isEqualTo(32);
     }
 }
