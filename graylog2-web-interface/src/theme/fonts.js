2368ee804bd51006dfd88b4433bcf7d82ea1363e
Kyle Knight <kyle@graylog.com>
2020-06-10 10:12:29 -0500

    theme font size config

--- graylog2-web-interface/src/theme/fonts.js
+++ graylog2-web-interface/src/theme/fonts.js
@@ -13,6 +13,14 @@ export type Fonts = {
   size: {
     root: string,
     body: string,
+    large: string,
+    small: string,
+    h1: string,
+    h2: string,
+    h3: string,
+    h4: string,
+    h5: string,
+    h6: string,
   },
 };
 
@@ -21,17 +29,18 @@ const family = {
   monospace: '"Roboto Mono", Menlo, Monaco, Consolas, "Courier New", monospace',
 };
 
+/* Scaled 1.20 Minor Third - https://type-scale.com/ */
 const size = {
-  root: '16px',
-  body: '0.8em',
-  bodyLarge: '1em',
-  bodySmall: '0.65em',
-  h1: '2em',
-  h2: '1.8em',
-  h3: '1.5em',
-  h4: '1.25em',
-  h5: '1em',
-  h6: '0.8em',
+  root: '87.5%', /* 14px */
+  body: '1em',
+  large: '1.2em',
+  small: '0.833em',
+  h1: '2.488em',
+  h2: '2.074em',
+  h3: '1.728em',
+  h4: '1.44em',
+  h5: '1.2em',
+  h6: '1em',
 };
 
 const fonts: Fonts = {
