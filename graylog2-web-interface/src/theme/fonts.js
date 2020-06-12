67a96e3d8b9f565f64f77a9d222bd09b32ac8072
Kyle Knight <kyle@graylog.com>
2020-06-12 11:56:46 -0500

    em -> rem

--- graylog2-web-interface/src/theme/fonts.js
+++ graylog2-web-interface/src/theme/fonts.js
@@ -34,17 +34,17 @@ const family = {
 /* Scaled 1.125 Major Second - https://type-scale.com/ */
 const size = {
   root: '87.5%', /* 14px */
-  body: '1em',
-  huge: '2.027em',
-  large: '1.125em',
-  small: '0.889em',
-  tiny: '0.79em',
-  h1: '1.802em',
-  h2: '1.602em',
-  h3: '1.424em',
-  h4: '1.266em',
-  h5: '1.125em',
-  h6: '1em',
+  body: '1rem',
+  huge: '2.027rem',
+  large: '1.125rem',
+  small: '0.889rem',
+  tiny: '0.79rem',
+  h1: '1.802rem',
+  h2: '1.602rem',
+  h3: '1.424rem',
+  h4: '1.266rem',
+  h5: '1.125rem',
+  h6: '1rem',
 };
 
 const fonts: Fonts = {
