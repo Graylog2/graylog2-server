5c0f68967ad1b8fd291cdcb24a887d6a0632d25f
Rob Curtis <rob.curtis@graylog.com>
2020-06-16 10:41:01 -0700

    Latest updates to Graylog Schema

--- graylog2-server/src/main/java/org/graylog/schema/VendorFields.java
+++ graylog2-server/src/main/java/org/graylog/schema/VendorFields.java
@@ -18,12 +18,13 @@ package org.graylog.schema;
 
 public class VendorFields {
     public static final String VENDOR_ALERT_SEVERITY = "vendor_alert_severity";
+    public static final String VENDOR_ALERT_SEVERITY_LEVEL = "vendor_alert_severity_level";
     public static final String VENDOR_EVENT_ACTION = "vendor_event_action";
     public static final String VENDOR_EVENT_DESCRIPTION = "vendor_event_description";
     public static final String VENDOR_EVENT_OUTCOME = "vendor_event_outcome";
     public static final String VENDOR_EVENT_OUTCOME_REASON = "vendor_event_outcome_reason";
-    public static final String VENDOR_EVENT_SECURITY_LEVEL = "vendor_event_severity_level";
     public static final String VENDOR_EVENT_SEVERITY = "vendor_event_severity";
+    public static final String VENDOR_EVENT_SEVERITY_LEVEL = "vendor_event_severity_level";
     public static final String VENDOR_PRIVATE_IP = "vendor_private_ip";
     public static final String VENDOR_PRIVATE_IPV6 = "vendor_private_ipv6";
     public static final String VENDOR_PUBLIC_IP = "vendor_public_ip";
