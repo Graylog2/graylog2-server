cf431e84a984e9b120038802a6662076fdb09782
Linus Pahl <linus@graylog.com>
2020-06-18 11:50:25 +0200

    Display required format in absolute timerange selection error message

--- graylog2-web-interface/src/views/components/searchbar/AbsoluteTimeRangeSelector.jsx
+++ graylog2-web-interface/src/views/components/searchbar/AbsoluteTimeRangeSelector.jsx
@@ -38,7 +38,7 @@ const _isValidDateString = (dateString: string) => {
 
   return DateTime.isValidDateString(dateString)
     ? undefined
-    : `Invalid date: ${dateString}`;
+    : `Format must be: ${DateTime.Formats.DATETIME}`;
 };
 
 const AbsoluteTimeRangeSelector = ({ disabled }: Props) => {
