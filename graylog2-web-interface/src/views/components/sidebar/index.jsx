249546bdd9469ae43ae23b56a0a18c0b8a923032
Linus Pahl <linus@graylog.com>
2020-06-19 09:42:23 +0200

    Fix sidebar export name in sidebar index file

--- graylog2-web-interface/src/views/components/sidebar/index.jsx
+++ graylog2-web-interface/src/views/components/sidebar/index.jsx
@@ -1,6 +1,5 @@
 export { default as AddWidgetButton } from './create/AddWidgetButton';
 export { default as SearchResultOverview } from './description/SearchResultOverview';
-export { default as Sidebar } from './Sidebar';
 export { default as FieldsOverview } from './fields/FieldsOverview';
 // eslint-disable-next-line import/no-cycle
-export { default as SideBar } from './SideBar';
+export { default as Sidebar } from './Sidebar';
