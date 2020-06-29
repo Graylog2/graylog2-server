45c00057b13cd41ed19fc8c72bcadd30f22a679f
Dennis Oelkers <dennis@graylog.com>
2020-06-29 16:46:58 +0200

    Adding module to bind `CountsAdapterES7`.

---
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/Elasticsearch7Module.java
@@ -0,0 +1,18 @@
+package org.graylog.storage.elasticsearch7;
+
+import com.google.inject.binder.LinkedBindingBuilder;
+import org.graylog2.indexer.counts.CountsAdapter;
+import org.graylog2.plugin.VersionAwareModule;
+
+import static org.graylog.storage.elasticsearch7.Elasticsearch7Plugin.SUPPORTED_ES_VERSION;
+
+public class Elasticsearch7Module extends VersionAwareModule {
+    @Override
+    protected void configure() {
+        bindForSupportedVersion(CountsAdapter.class).to(CountsAdapterES7.class);
+    }
+
+    private <T> LinkedBindingBuilder<T> bindForSupportedVersion(Class<T> interfaceClass) {
+        return bindForVersion(SUPPORTED_ES_VERSION, interfaceClass);
+    }
+}
