e528e3b121446d9db00fb5a64f770647ae786394
Dennis Oelkers <dennis@graylog.com>
2020-06-30 10:31:17 +0200

    Starting and binding adapter.

---
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/ClusterAdapterES7.java
@@ -0,0 +1,6 @@
+package org.graylog.storage.elasticsearch7;
+
+import org.graylog2.indexer.cluster.ClusterAdapter;
+
+public class ClusterAdapterES7 implements ClusterAdapter {
+}
--- graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/Elasticsearch7Module.java
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/Elasticsearch7Module.java
@@ -1,6 +1,7 @@
 package org.graylog.storage.elasticsearch7;
 
 import com.google.inject.binder.LinkedBindingBuilder;
+import org.graylog2.indexer.cluster.ClusterAdapter;
 import org.graylog2.indexer.counts.CountsAdapter;
 import org.graylog2.indexer.messages.MessagesAdapter;
 import org.graylog2.plugin.VersionAwareModule;
@@ -11,6 +12,7 @@ public class Elasticsearch7Module extends VersionAwareModule {
     @Override
     protected void configure() {
         bindForSupportedVersion(CountsAdapter.class).to(CountsAdapterES7.class);
+        bindForSupportedVersion(ClusterAdapter.class).to(ClusterAdapterES7.class);
         bindForSupportedVersion(MessagesAdapter.class).to(MessagesAdapterES7.class);
     }
 
