e82f6af290fa947a4b22c9ff97d128c1abf31ebb
Rob Curtis <rob.curtis@graylog.com>
2020-07-02 09:53:35 -0400

    Add new Vendor fields to the Graylog Schema
    This is required to keep the Graylog Schema in code in sync with the documented Graylog Schema and to support new fields used in the Okta plugin.

--- graylog2-server/src/main/java/org/graylog/schema/VendorFields.java
+++ graylog2-server/src/main/java/org/graylog/schema/VendorFields.java
@@ -18,6 +18,12 @@ package org.graylog.schema;
 
 public class VendorFields {
     public static final String VENDOR_ALERT_SEVERITY = "vendor_alert_severity";
+<<<<<<< Updated upstream
+=======
+    public static final String VENDOR_ALERT_SEVERITY_LEVEL = "vendor_alert_severity_level";
+    public static final String VENDOR_AUTHENTICATION_PROVIDER = "vendor_authentication_provider";
+    public static final String VENDOR_CREDENTIAL_TYPE = "vendor_credential_type";
+>>>>>>> Stashed changes
     public static final String VENDOR_EVENT_ACTION = "vendor_event_action";
     public static final String VENDOR_EVENT_DESCRIPTION = "vendor_event_description";
     public static final String VENDOR_EVENT_OUTCOME = "vendor_event_outcome";
