42ac1fcdb5ecdb05d21d73723d8fb988d10265bf
Dennis Oelkers <dennis@graylog.com>
2020-07-07 16:55:13 +0200

    Adjusting pattern, adding test case.

--- graylog-storage-elasticsearch7/src/test/java/org/graylog/storage/elasticsearch7/ParsedElasticsearchExceptionTest.java
+++ graylog-storage-elasticsearch7/src/test/java/org/graylog/storage/elasticsearch7/ParsedElasticsearchExceptionTest.java
@@ -20,4 +20,18 @@ class ParsedElasticsearchExceptionTest {
                     "id '2f1b81f1-c050-11ea-ad64-d2850321fca4'. Preview of field's value: 'fourty-two'");
         });
     }
+
+    @Test
+    void parsingIndexReadonlyException() {
+        final String exception = "Elasticsearch exception: ElasticsearchException[Elasticsearch exception " +
+                "[type=cluster_block_exception, reason=index [messages_it_deflector] blocked by: [TOO_MANY_REQUESTS/12/index " +
+                "read-only / allow delete (api)];]]";
+
+        final ParsedElasticsearchException parsed = ParsedElasticsearchException.from(exception);
+
+        assertThat(parsed).satisfies(p -> {
+            assertThat(p.type()).isEqualTo("cluster_block_exception");
+            assertThat(p.reason()).isEqualTo("index [messages_it_deflector] blocked by: [TOO_MANY_REQUESTS/12/index read-only / allow delete (api)");
+        });
+    }
 }
