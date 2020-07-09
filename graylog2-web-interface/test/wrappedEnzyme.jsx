62ad10bd62dc40a3e95b1328e8a64f16012b2fa1
Kyle Knight <kyle@graylog.com>
2020-07-09 15:54:26 -0500

    move library

--- graylog2-web-interface/test/wrappedEnzyme.jsx
+++ graylog2-web-interface/test/wrappedEnzyme.jsx
@@ -1,6 +1,7 @@
 // @flow strict
 import { configure, mount, shallow, type ReactWrapper, type ShallowWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-styled-components';
 
 import WrappingContainer from './WrappingContainer';
 
