1a2f6511afdb32309521d2531af23ce62c423e36
Kyle Knight <kyle@graylog.com>
2020-07-10 13:00:41 -0500

    flow typed

--- graylog2-web-interface/src/components/graylog/Row.jsx
+++ graylog2-web-interface/src/components/graylog/Row.jsx
@@ -1,15 +1,23 @@
+// @flow strict
 // eslint-disable-next-line no-restricted-imports
 import { Row as BootstrapRow } from 'react-bootstrap';
 import styled, { css } from 'styled-components';
+import type { StyledComponent } from 'styled-components';
 
-export const RowContentStyles = css(({ theme }) => css`
+import type { ThemeInterface } from 'theme';
+
+type RowContentType = {
+  theme: ThemeInterface,
+};
+
+export const RowContentStyles = css(({ theme }: RowContentType) => css`
   background-color: ${theme.colors.global.contentBackground};
   border: 1px solid ${theme.colors.gray[80]};
   margin-bottom: 9px;
   border-radius: 4px;
 `);
 
-const Row = styled(BootstrapRow)`
+const Row: StyledComponent<{children: any}, void, *> = styled(BootstrapRow)`
   &.content {
     ${RowContentStyles}
   }
