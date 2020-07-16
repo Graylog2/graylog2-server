bda76970e1692e2c5a05e4f27751626f552159bf
Konrad Merz <konrad@graylog.com>
2020-07-16 08:47:49 +0200

    Fix flow error

--- graylog2-web-interface/src/actions/roles/AuthzRolesActions.js
+++ graylog2-web-interface/src/actions/roles/AuthzRolesActions.js
@@ -1,7 +1,7 @@
 // @flow strict
 import Reflux from 'reflux';
 
-import type { PaginatedList } from 'stores/roles/AuthzRolesStore';
+import type { PaginatedListType } from 'stores/roles/AuthzRolesStore';
 import { singletonActions } from 'views/logic/singleton';
 import type { RefluxActions } from 'stores/StoreTypes';
 
@@ -9,7 +9,7 @@ type AuthzRolesActionsType = RefluxActions<{
   loadForUser: (username: string,
                 page: number,
                 perPage: number,
-                query: string) => Promise<PaginatedList>,
+                query: string) => Promise<PaginatedListType>,
 }>;
 
 const AuthzRolesActions: AuthzRolesActionsType = singletonActions(
