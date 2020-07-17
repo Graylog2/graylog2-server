d870a5ae7fc05493e3aef8038ed7d1a89a4fdabf
Konrad Merz <konrad@graylog.com>
2020-07-17 16:10:16 +0200

    Roles in user details (#8553)
    * Add new Role value class
    
    * Add AuthzRolesStore
    
    * Add basic RolesSection
    
    * Refine display of Roles for user
    
    * Fix linter warnings
    
    * Fix flow error
    
    * Create PaginatedItemOverview to make it reusable
    
    * Remove now unused RoleItem
    
    * Fix annotations from @linuspahl
    
    * Fix linter warning
    
    * Fix flow errors

---
+++ graylog2-web-interface/src/actions/roles/AuthzRolesActions.js
@@ -0,0 +1,22 @@
+// @flow strict
+import Reflux from 'reflux';
+
+import type { PaginatedListType } from 'stores/roles/AuthzRolesStore';
+import { singletonActions } from 'views/logic/singleton';
+import type { RefluxActions } from 'stores/StoreTypes';
+
+type AuthzRolesActionsType = RefluxActions<{
+  loadForUser: (username: string,
+                page: number,
+                perPage: number,
+                query: string) => Promise<PaginatedListType>,
+}>;
+
+const AuthzRolesActions: AuthzRolesActionsType = singletonActions(
+  'AuthzRoles',
+  () => Reflux.createActions({
+    loadForUser: { asyncResult: true },
+  }),
+);
+
+export default AuthzRolesActions;
