5e1abb1566086629382b114836ed1592cf06ace7
Linus Pahl <linus@graylog.com>
2020-07-17 13:04:46 +0200

    Move SearchPageLayout sidebar pinning logic form SearchPageLayoutProvider to SearchPageLayoutState

--- graylog2-web-interface/src/views/components/contexts/SearchPageLayoutProvider.jsx
+++ graylog2-web-interface/src/views/components/contexts/SearchPageLayoutProvider.jsx
@@ -1,88 +1,22 @@
 // @flow strict
 import * as React from 'react';
-import { useContext } from 'react';
 import PropTypes from 'prop-types';
-
-import View, { type ViewType } from 'views/logic/views/View';
-import ViewTypeContext from 'views/components/contexts/ViewTypeContext';
-import UserPreferencesContext from 'contexts/UserPreferencesContext';
-import CurrentUserContext from 'contexts/CurrentUserContext';
-import CombinedProvider from 'injection/CombinedProvider';
-import Store from 'logic/local-storage/Store';
 
 import SearchPageLayoutContext from './SearchPageLayoutContext';
 import SearchPageLayoutState from './SearchPageLayoutState';
-
-const { PreferencesActions } = CombinedProvider.get('Preferences');
 
 type Props = {
   children: React.Node,
 };
 
-const getPinningPreferenceKey = (viewType: ?ViewType): string => {
-  const preferenceKeyMapping = {
-    [View.Type.Dashboard]: 'dashboardSidebarIsPinned',
-    [View.Type.Search]: 'searchSidebarIsPinned',
-  };
-
-  const preferenceKey = preferenceKeyMapping[viewType];
-
-  if (!preferenceKey) {
-    throw new Error(`User sidebar pinning preference key is missing for view type ${viewType}`);
-  }
-
-  return preferenceKey;
-};
-
-const getDefaultIsPinned = (currentUser, userPreferences, viewType) => {
-  const fallbackDefault = false;
-  const sidebarPinningPrefKey = getPinningPreferenceKey(viewType);
-
-  // eslint-disable-next-line camelcase
-  if (currentUser?.read_only) {
-    return Store.get(sidebarPinningPrefKey) ?? fallbackDefault;
-  }
-
-  return userPreferences[sidebarPinningPrefKey] ?? fallbackDefault;
-};
-
-const createUserPreferencesArray = (userPreferences) => {
-  return Object.entries(userPreferences).map(([name, value]) => ({
-    name,
-    value,
-  }));
-};
-
-const _toggleSidebarPinning = (currentUser, userPreferences, viewType, setLayoutState, isPinned) => {
-  const sidebarPinningPrefKey = getPinningPreferenceKey(viewType);
-  const newPinningState = !isPinned;
-  setLayoutState('sidebarIsPinned', newPinningState);
-
-  // eslint-disable-next-line camelcase
-  if (currentUser?.read_only) {
-    Store.set(sidebarPinningPrefKey, !isPinned);
-  } else {
-    const newUserPreferences = {
-      ...userPreferences,
-      [sidebarPinningPrefKey]: newPinningState,
-    };
-    PreferencesActions.saveUserPreferences(currentUser?.username, createUserPreferencesArray(newUserPreferences), undefined, false);
-  }
-};
-
 const SearchPageLayoutProvider = ({ children }: Props) => {
-  const currentUser = useContext(CurrentUserContext);
-  const userPreferences = useContext(UserPreferencesContext);
-  const viewType = useContext(ViewTypeContext);
-  const defaultIsPinned = getDefaultIsPinned(currentUser, userPreferences, viewType);
-
   return (
     <SearchPageLayoutState>
       {({ getLayoutState, setLayoutState }) => {
         const config = {
-          sidebar: { isPinned: getLayoutState('sidebarIsPinned', defaultIsPinned) },
+          sidebar: { isPinned: getLayoutState('sidebarIsPinned') },
         };
-        const actions = { toggleSidebarPinning: () => _toggleSidebarPinning(currentUser, userPreferences, viewType, setLayoutState, config.sidebar.isPinned) };
+        const actions = { toggleSidebarPinning: () => setLayoutState('sidebarIsPinned', !config.sidebar.isPinned) };
 
         return (
           <SearchPageLayoutContext.Provider value={{ config, actions }}>
--- graylog2-web-interface/src/views/components/contexts/SearchPageLayoutState.jsx
+++ graylog2-web-interface/src/views/components/contexts/SearchPageLayoutState.jsx
@@ -1,14 +1,83 @@
 // @flow strict
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+
+import View, { type ViewType } from 'views/logic/views/View';
+import ViewTypeContext from 'views/components/contexts/ViewTypeContext';
+import UserPreferencesContext from 'contexts/UserPreferencesContext';
+import CurrentUserContext from 'contexts/CurrentUserContext';
+import CombinedProvider from 'injection/CombinedProvider';
+import Store from 'logic/local-storage/Store';
+
+const { PreferencesActions } = CombinedProvider.get('Preferences');
+
+const _getPinningPreferenceKey = (viewType: ?ViewType): string => {
+  const preferenceKeyMapping = {
+    [View.Type.Dashboard]: 'dashboardSidebarIsPinned',
+    [View.Type.Search]: 'searchSidebarIsPinned',
+  };
+
+  const preferenceKey = preferenceKeyMapping[viewType];
+
+  if (!preferenceKey) {
+    throw new Error(`User sidebar pinning preference key is missing for view type ${viewType}`);
+  }
+
+  return preferenceKey;
+};
+
+const _defaultSidebarPinning = (currentUser, userPreferences, viewType) => {
+  const fallbackDefault = false;
+  const sidebarPinningPrefKey = _getPinningPreferenceKey(viewType);
+
+  // eslint-disable-next-line camelcase
+  if (currentUser?.read_only) {
+    return Store.get(sidebarPinningPrefKey) ?? fallbackDefault;
+  }
+
+  return userPreferences[sidebarPinningPrefKey] ?? fallbackDefault;
+};
+
+const _createUserPreferencesArray = (userPreferences) => {
+  return Object.entries(userPreferences).map(([name, value]) => ({
+    name,
+    value,
+  }));
+};
+
+const _updateUserSidebarPinning = (currentUser, userPreferences, viewType, newIsPinned) => {
+  const sidebarPinningPrefKey = _getPinningPreferenceKey(viewType);
+
+  // eslint-disable-next-line camelcase
+  if (currentUser?.read_only) {
+    Store.set(sidebarPinningPrefKey, newIsPinned);
+  } else {
+    const newUserPreferences = {
+      ...userPreferences,
+      [sidebarPinningPrefKey]: newIsPinned,
+    };
+    PreferencesActions.saveUserPreferences(currentUser?.username, _createUserPreferencesArray(newUserPreferences), undefined, false);
+  }
+};
 
 const SearchPageLayoutState = ({ children }) => {
-  const [state, setState] = useState({});
+  const currentUser = useContext(CurrentUserContext);
+  const userPreferences = useContext(UserPreferencesContext);
+  const viewType = useContext(ViewTypeContext);
+  const [state, setState] = useState({
+    sidebarIsPinned: _defaultSidebarPinning(currentUser, userPreferences, viewType),
+  });
 
-  const getLayoutState = (stateKey, defaultValue) => {
-    return state[stateKey] ?? defaultValue;
+  const _onSidebarPinningChange = (newIsPinned) => _updateUserSidebarPinning(currentUser, userPreferences, viewType, newIsPinned);
+
+  const getLayoutState = (stateKey) => {
+    return state[stateKey];
   };
 
   const setLayoutState = (stateKey, value) => {
+    if (stateKey === 'sidebarIsPinned') {
+      _onSidebarPinningChange(value);
+    }
+
     setState({ ...state, [stateKey]: value });
   };
 
