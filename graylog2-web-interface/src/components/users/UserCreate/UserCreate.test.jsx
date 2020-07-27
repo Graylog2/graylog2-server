8763becba0c7224d84d07a15aaf0263e8dbe1305
Linus Pahl <linus@graylog.com>
2020-07-27 14:52:06 +0200

    Add more tests for user create

--- graylog2-web-interface/src/components/users/UserCreate/UserCreate.test.jsx
+++ graylog2-web-interface/src/components/users/UserCreate/UserCreate.test.jsx
@@ -1,30 +1,39 @@
 // @flow strict
 import React from 'react';
+import * as Immutable from 'immutable';
 import { render, fireEvent, waitFor } from 'wrappedTestingLibrary';
+import selectEvent from 'react-select-event';
+import { act } from 'react-dom/test-utils';
+import { alice } from 'fixtures/users';
 
 import { UsersActions } from 'stores/users/UsersStore';
 
 import UserCreate from './UserCreate';
 
+const existingUser = alice;
+const mockExistingUsers = Immutable.List([alice]);
+
 jest.mock('stores/users/UsersStore', () => ({
   UsersStore: {
-    getInitialState: jest.fn(() => ({ list: undefined })),
+    getInitialState: jest.fn(() => ({ list: mockExistingUsers })),
     listen: jest.fn(),
   },
   UsersActions: {
     create: jest.fn(() => Promise.resolve()),
-    loadUsers: jest.fn(),
+    loadUsers: jest.fn(() => Promise.resolve()),
   },
 }));
 
 describe('<UserCreate />', () => {
   it('should create user', async () => {
-    const { getByLabelText, getByPlaceholderText, getByText } = render(<UserCreate />);
+    const { getByLabelText, getByPlaceholderText, getByText, getByTestId } = render(<UserCreate />);
 
     const usernameInput = getByLabelText('Username');
     const fullNameInput = getByLabelText('Full Name');
     const emailInput = getByLabelText('E-Mail Address');
-
+    const timeoutAmountInput = getByPlaceholderText('Timeout amount');
+    const timeoutUnitSelect = getByTestId('timeout-unit-select');
+    const timezoneSelect = getByLabelText('Time Zone');
     const passwordInput = getByPlaceholderText('Password');
     const passwordRepeatInput = getByPlaceholderText('Repeat password');
     const submitButton = getByText('Create User');
@@ -32,6 +41,11 @@ describe('<UserCreate />', () => {
     fireEvent.change(usernameInput, { target: { value: 'The username' } });
     fireEvent.change(fullNameInput, { target: { value: 'The full name' } });
     fireEvent.change(emailInput, { target: { value: 'username@example.org' } });
+    fireEvent.change(timeoutAmountInput, { target: { value: '40' } });
+    await selectEvent.openMenu(timeoutUnitSelect);
+    await act(async () => { await selectEvent.select(timeoutUnitSelect, 'Hours'); });
+    await selectEvent.openMenu(timezoneSelect);
+    await act(async () => { await selectEvent.select(timezoneSelect, 'Berlin'); });
     fireEvent.change(passwordInput, { target: { value: 'thepassword' } });
     fireEvent.change(passwordRepeatInput, { target: { value: 'thepassword' } });
 
@@ -40,10 +54,33 @@ describe('<UserCreate />', () => {
     await waitFor(() => expect(UsersActions.create).toHaveBeenCalledWith({
       username: 'The username',
       full_name: 'The full name',
+      timezone: 'Europe/Berlin',
       email: 'username@example.org',
       permissions: [],
-      session_timeout_ms: 3600000,
+      session_timeout_ms: 144000000,
       password: 'thepassword',
     }));
+  });
+
+  it('should display warning if username is alreafy taken', async () => {
+    const { getByLabelText, getByText } = render(<UserCreate />);
+
+    const usernameInput = getByLabelText('Username');
+
+    fireEvent.change(usernameInput, { target: { value: existingUser.username } });
+
+    await waitFor(() => expect(getByText('Username is already taken')).not.toBeNull());
+  });
+
+  it('should display warning, if password repeat does not match password', async () => {
+    const { getByPlaceholderText, getByText } = render(<UserCreate />);
+
+    const passwordInput = getByPlaceholderText('Password');
+    const passwordRepeatInput = getByPlaceholderText('Repeat password');
+
+    fireEvent.change(passwordInput, { target: { value: 'thepassword' } });
+    fireEvent.change(passwordRepeatInput, { target: { value: 'notthepassword' } });
+
+    await waitFor(() => expect(getByText('Passwords do not match')).not.toBeNull());
   });
 });
