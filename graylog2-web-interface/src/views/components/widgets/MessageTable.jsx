59e6ed6d281703a31c65bcf3114da675fecb22a3
Konrad Merz <konrad@graylog.com>
2020-07-27 11:15:08 +0200

    Fix `Field` component usage for `MessageTable` head (3.3) (#8575)
    * Revert Field component changes
    
    * Fix field usage for MessageTable head
    
    * Test if message table displays field names in non interactive mode
    
    Co-authored-by: Linus Pahl <linus@graylog.com>

--- graylog2-web-interface/src/views/components/widgets/MessageTable.jsx
+++ graylog2-web-interface/src/views/components/widgets/MessageTable.jsx
@@ -248,7 +248,9 @@ class MessageTable extends React.Component<Props, State> {
                       style={this._columnStyle(selectedFieldName)}>
                     <Field type={this._fieldTypeFor(selectedFieldName, fields)}
                            name={selectedFieldName}
-                           queryId={activeQueryId} />
+                           queryId={activeQueryId}>
+                      {selectedFieldName}
+                    </Field>
                     {editing && (
                       <FieldSortIcon fieldName={selectedFieldName}
                                      onSortChange={onSortChange}
