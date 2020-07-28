c886cac86f81eb44ed17825f5518c200e7fe7ecc
Rob Curtis <6466251+waab76@users.noreply.github.com>
2020-07-28 18:29:41 -0500

    Latest updates to Graylog Schema (#8358) (#8642)

--- graylog2-server/src/main/java/org/graylog/schema/SourceFields.java
+++ graylog2-server/src/main/java/org/graylog/schema/SourceFields.java
@@ -19,16 +19,14 @@ package org.graylog.schema;
 public class SourceFields {
     private static final String SOURCE_PREFIX = "source_";
 
-    public static final String SOURCE_BYTES = "source_bytes";
+    public static final String SOURCE_BYTES_SENT = "source_bytes_sent";
     public static final String SOURCE_HOSTNAME = "source_hostname";
     public static final String SOURCE_IP = "source_ip";
     public static final String SOURCE_IPV6 = "source_ipv6";
     public static final String SOURCE_NAT_IP = "source_nat_ip";
     public static final String SOURCE_NAT_PORT = "source_nat_port";
-    public static final String SOURCE_PACKETS = "source_packets";
+    public static final String SOURCE_PACKETS_SENT = "source_packets_sent";
     public static final String SOURCE_PORT = "source_port";
-    public static final String SOURCE_USER = "source_user";
-    public static final String SOURCE_USER_EMAIL = "source_user_email";
     public static final String SOURCE_VSYS_UUID = "source_vsys_uuid";
     public static final String SOURCE_ZONE = "source_zone";
 
@@ -47,9 +45,21 @@ public class SourceFields {
     public static final String SOURCE_AS_ORGANIZATION_NAME = SOURCE_PREFIX + AutonomousSystemFields.AS_ORGANIZATION_NAME;
 
     // Geo Fields
+    public static final String SOURCE_GEO_CITY_ISO_CODE = SOURCE_PREFIX + GeoFields.GEO_CITY_ISO_CODE;
     public static final String SOURCE_GEO_CITY_NAME = SOURCE_PREFIX + GeoFields.GEO_CITY_NAME;
     public static final String SOURCE_GEO_STATE_NAME = SOURCE_PREFIX + GeoFields.GEO_STATE_NAME;
-    public static final String SOURCE_GEO_ISO_CODE = SOURCE_PREFIX + GeoFields.GEO_ISO_CODE;
+    public static final String SOURCE_GEO_COUNTRY_ISO_CODE = SOURCE_PREFIX + GeoFields.GEO_COUNTRY_ISO_CODE;
     public static final String SOURCE_GEO_COUNTRY_NAME = SOURCE_PREFIX + GeoFields.GEO_COUNTRY_NAME;
     public static final String SOURCE_GEO_COORDINATES = SOURCE_PREFIX + GeoFields.GEO_COORDINATES;
+
+    // To be removed
+    @Deprecated
+    public static final String SOURCE_PACKETS = "source_packets_sent";
+    @Deprecated
+    public static final String SOURCE_USER = "source_user";
+    @Deprecated
+    public static final String SOURCE_USER_EMAIL = "source_user_email";
+    @Deprecated
+    public static final String SOURCE_GEO_ISO_CODE = SOURCE_PREFIX + GeoFields.GEO_ISO_CODE;
+
 }
