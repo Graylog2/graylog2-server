03c3922e2983a5a81dd1a99c6f7ece881a0349d2
Linus Pahl <linus@graylog.com>
2020-08-05 17:12:15 +0200

    Reimplement UsersOverview admin tests

--- graylog2-web-interface/src/components/users/UsersOverview/UsersOverview.test.jsx
+++ graylog2-web-interface/src/components/users/UsersOverview/UsersOverview.test.jsx
@@ -1,15 +1,17 @@
 // @flow strict
 import * as React from 'react';
 import * as Immutable from 'immutable';
-import { render, act, waitFor } from 'wrappedTestingLibrary';
-import { alice, bob, adminUser } from 'fixtures/users';
+import { render, act, waitFor, fireEvent } from 'wrappedTestingLibrary';
+import { alice, bob, adminUser, admin } from 'fixtures/users';
+import asMock from 'helpers/mocking/AsMock';
 
+import CurrentUserContext from 'contexts/CurrentUserContext';
 import { UsersActions } from 'stores/users/UsersStore';
 
 import UsersOverview from './UsersOverview';
 
 const mockUsers = Immutable.List([alice, bob, adminUser]);
-const mockSearchPaginatedPromise = Promise.resolve({
+const searchPaginatedResponse = {
   list: mockUsers,
   pagination: {
     page: 1,
@@ -17,7 +19,8 @@ const mockSearchPaginatedPromise = Promise.resolve({
     total: mockUsers.size,
   },
   adminUser: undefined,
-});
+};
+const mockSearchPaginatedPromise = Promise.resolve(searchPaginatedResponse);
 
 jest.mock('stores/users/UsersStore', () => ({
   UsersStore: {
@@ -74,67 +77,71 @@ describe('UsersOverview', () => {
     `('$username', displaysUserAttributes);
   });
 
-  // describe('admin should', () => {
-  //   const modifiableUser = alice.toBuilder().readOnly(false).build();
-  //   const modifiableUsersList = Immutable.List([modifiableUser]);
-  //   const readOnlyUser = alice.toBuilder().readOnly(true).build();
-  //   const readOnlyUsersList = Immutable.List([readOnlyUser]);
-  //   let oldConfirm;
+  describe('admin should', () => {
+    const modifiableUser = alice.toBuilder().readOnly(false).build();
+    const modifiableUsersList = Immutable.List([modifiableUser]);
+    const readOnlyUser = alice.toBuilder().readOnly(true).build();
+    const readOnlyUsersList = Immutable.List([readOnlyUser]);
+    let oldConfirm;
 
-  //   const UsersOverviewAsAdmin = () => (
-  //     <CurrentUserContext.Provider value={admin}>
-  //       <UsersOverview />
-  //     </CurrentUserContext.Provider>
-  //   );
+    const UsersOverviewAsAdmin = () => (
+      <CurrentUserContext.Provider value={admin}>
+        <UsersOverview />
+      </CurrentUserContext.Provider>
+    );
 
-  //   beforeEach(() => {
-  //     oldConfirm = window.confirm;
-  //     window.confirm = jest.fn(() => true);
-  //   });
+    beforeEach(() => {
+      oldConfirm = window.confirm;
+      window.confirm = jest.fn(() => true);
+    });
 
-  //   afterEach(() => {
-  //     window.confirm = oldConfirm;
-  //   });
+    afterEach(() => {
+      window.confirm = oldConfirm;
+    });
 
-  //   it('be able to delete a modifiable user', () => {
-  //     asMock(UsersStore.getInitialState).mockReturnValueOnce({ list: modifiableUsersList });
-  //     const { getByTitle } = render(<UsersOverviewAsAdmin />);
+    it('be able to delete a modifiable user', async () => {
+      const searchPaginatedPromise = Promise.resolve({ ...searchPaginatedResponse, list: modifiableUsersList });
+      asMock(UsersActions.searchPaginated).mockReturnValueOnce(searchPaginatedPromise);
+      const { getByTitle } = render(<UsersOverviewAsAdmin />);
+      await act(() => searchPaginatedPromise);
 
-  //     fireEvent.click(getByTitle(`Delete user ${modifiableUser.username}`));
+      fireEvent.click(getByTitle(`Delete user ${modifiableUser.username}`));
 
-  //     expect(window.confirm).toHaveBeenCalledTimes(1);
-  //     expect(window.confirm).toHaveBeenCalledWith(`Do you really want to delete user ${modifiableUser.username}?`);
-  //     expect(UsersActions.deleteUser).toHaveBeenCalledTimes(1);
-  //     expect(UsersActions.deleteUser).toHaveBeenCalledWith(alice.username);
-  //   });
+      expect(window.confirm).toHaveBeenCalledTimes(1);
+      expect(window.confirm).toHaveBeenCalledWith(`Do you really want to delete user ${modifiableUser.username}?`);
+      expect(UsersActions.deleteUser).toHaveBeenCalledTimes(1);
+      expect(UsersActions.deleteUser).toHaveBeenCalledWith(alice.username);
+    });
 
-  //   it('not be able to delete a "read only" user', () => {
-  //     asMock(UsersStore.getInitialState).mockReturnValueOnce({ list: readOnlyUsersList });
-  //     const { queryByTitle } = render(<UsersOverviewAsAdmin />);
+    it('not be able to delete a "read only" user', async () => {
+      asMock(UsersActions.searchPaginated).mockReturnValueOnce(Promise.resolve({ ...searchPaginatedResponse, list: readOnlyUsersList }));
+      const { queryByTitle } = render(<UsersOverviewAsAdmin />);
 
-  //     expect(queryByTitle(`Delete user ${readOnlyUser.username}`)).toBeNull();
-  //   });
+      await waitFor(() => expect(queryByTitle(`Delete user ${readOnlyUser.username}`)).toBeNull());
+    });
 
-  //   it('see edit and edit tokens link for a modifiable user ', () => {
-  //     asMock(UsersStore.getInitialState).mockReturnValueOnce({ list: modifiableUsersList });
-  //     const { queryByTitle } = render(<UsersOverviewAsAdmin />);
+    it('see edit and edit tokens link for a modifiable user ', async () => {
+      asMock(UsersActions.searchPaginated).mockReturnValueOnce(Promise.resolve({ ...searchPaginatedResponse, list: modifiableUsersList }));
+      const { queryByTitle } = render(<UsersOverviewAsAdmin />);
 
-  //     expect(queryByTitle(`Edit user ${modifiableUser.username}`)).not.toBeNull();
-  //     expect(queryByTitle(`Edit tokens of user ${modifiableUser.username}`)).not.toBeNull();
-  //   });
+      await waitFor(() => {
+        expect(queryByTitle(`Edit user ${modifiableUser.username}`)).not.toBeNull();
+        expect(queryByTitle(`Edit tokens of user ${modifiableUser.username}`)).not.toBeNull();
+      });
+    });
 
-  //   it('not see edit link for a "read only" user', () => {
-  //     asMock(UsersStore.getInitialState).mockReturnValueOnce({ list: readOnlyUsersList });
-  //     const { queryByTitle } = render(<UsersOverviewAsAdmin />);
+    it('not see edit link for a "read only" user', async () => {
+      asMock(UsersActions.searchPaginated).mockReturnValueOnce(Promise.resolve({ ...searchPaginatedResponse, list: readOnlyUsersList }));
+      const { queryByTitle } = render(<UsersOverviewAsAdmin />);
 
-  //     expect(queryByTitle(`Edit user ${readOnlyUser.username}`)).toBeNull();
-  //   });
+      await waitFor(() => expect(queryByTitle(`Edit user ${readOnlyUser.username}`)).toBeNull());
+    });
 
-  //   it('see edit tokens link for a "read only" user', () => {
-  //     asMock(UsersStore.getInitialState).mockReturnValueOnce({ list: readOnlyUsersList });
-  //     const { queryByTitle } = render(<UsersOverviewAsAdmin />);
+    it('see edit tokens link for a "read only" user', async () => {
+      asMock(UsersActions.searchPaginated).mockReturnValueOnce(Promise.resolve({ ...searchPaginatedResponse, list: readOnlyUsersList }));
+      const { queryByTitle } = render(<UsersOverviewAsAdmin />);
 
-  //     expect(queryByTitle(`Edit tokens for user ${readOnlyUser.username}`)).toBeNull();
-  //   });
-  // });
+      await waitFor(() => expect(queryByTitle(`Edit tokens for user ${readOnlyUser.username}`)).toBeNull());
+    });
+  });
 });
