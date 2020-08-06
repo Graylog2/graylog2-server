f69568c2be08fa76383c70afab1a5e81ad58d4d6
Linus Pahl <linus@graylog.com>
2020-08-06 12:18:10 +0200

    Extract RolesQueryHelper

--- graylog2-web-interface/src/components/roles/RolesOverview/RolesFilter.jsx
+++ graylog2-web-interface/src/components/roles/RolesOverview/RolesFilter.jsx
@@ -3,8 +3,9 @@ import * as React from 'react';
 import styled, { type StyledComponent } from 'styled-components';
 
 import type { ThemeInterface } from 'theme';
-import { SearchForm, Icon } from 'components/common';
-import { OverlayTrigger, Popover, Table, Button } from 'components/graylog';
+import { SearchForm } from 'components/common';
+
+import RolesQueryHelper from '../RolesQueryHelper';
 
 type Props = {
   onSearch: (query: string) => Promise<void>,
@@ -15,41 +16,6 @@ const Container: StyledComponent<{}, ThemeInterface, HTMLDivElement> = styled.di
   margin-bottom: 10px;
 `;
 
-const rolesQueryHelperPopover = (
-  <Popover id="role-search-query-help" title="Search Syntax Help">
-    <p><strong>Available search fields</strong></p>
-    <Table condensed>
-      <thead>
-        <tr>
-          <th>Field</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>name</td>
-          <td>The name of a role</td>
-        </tr>
-        <tr>
-          <td>description</td>
-          <td>The description of a role</td>
-        </tr>
-      </tbody>
-    </Table>
-    <p><strong>Examples</strong></p>
-    <p>
-      Find role with a name containing manager:<br />
-      <kbd>name:manager</kbd><br />
-    </p>
-  </Popover>
-);
-
-const rolesQueryHelper = (
-  <OverlayTrigger trigger="click" rootClose placement="right" overlay={rolesQueryHelperPopover}>
-    <Button bsStyle="link"><Icon name="question-circle" /></Button>
-  </OverlayTrigger>
-);
-
 const RolesFilter = ({ onSearch, onReset }: Props) => {
   const _handleSearch = (newQuery, resetLoading) => onSearch(newQuery).then(resetLoading);
 
@@ -58,7 +24,7 @@ const RolesFilter = ({ onSearch, onReset }: Props) => {
       <SearchForm onSearch={_handleSearch}
                   onReset={onReset}
                   useLoadingState
-                  queryHelpComponent={rolesQueryHelper}
+                  queryHelpComponent={<RolesQueryHelper />}
                   topMargin={0} />
     </Container>
   );
