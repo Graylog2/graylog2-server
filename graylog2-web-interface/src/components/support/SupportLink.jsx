cfdbc276a4a62acb08c6366c34ec0865cff2321e
Kyle Knight <kyle@graylog.com>
2020-08-07 12:08:34 -0500

    SupportLink IconStack colors & flow-typing

--- graylog2-web-interface/src/components/support/SupportLink.jsx
+++ graylog2-web-interface/src/components/support/SupportLink.jsx
@@ -1,20 +1,37 @@
+// @flow strict
 import PropTypes from 'prop-types';
-import React from 'react';
-import styled, { css } from 'styled-components';
+import * as React from 'react';
+import styled, { css, type StyledComponent } from 'styled-components';
 
 import Icon from 'components/common/Icon';
+import type { ThemeInterface } from 'theme/types';
 
-const IconStack = styled.span(({ small, theme }) => css`
+type Props = {
+  children: React.Node,
+  small: boolean,
+};
+
+const IconStack: StyledComponent<{small: boolean}, ThemeInterface, HTMLDivElement> = styled.div(({ small, theme }) => css`
+  position: relative;
+  min-width: 2.5em;
   font-size: ${small ? theme.fonts.size.body : theme.fonts.size.large};
+  
+  .fa-stack-1x {
+    color: ${theme.colors.global.textAlt};
+  }
+  
+  .fa-stack-2x {
+    color: ${theme.colors.global.textDefault};
+  }
 `);
 
-const SupportLink = ({ small, children }) => {
+const SupportLink = ({ small, children }: Props) => {
   return (
     <table className="description-tooltips" style={{ marginBottom: '10px' }}>
       <tbody>
         <tr>
           <td style={{ width: '40px' }}>
-            <IconStack className={`fa-stack ${!small && 'fa-lg'}`} small={small}>
+            <IconStack className={`fa-stack ${small ? '' : 'fa-lg'}`} small={small}>
               <Icon name="circle" className="fa-stack-2x" />
               <Icon name="lightbulb" className="fa-stack-1x" inverse />
             </IconStack>
