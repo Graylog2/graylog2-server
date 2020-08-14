d0b33e14afd587f9311498ab8def3aec9988589c
Bernd Ahlers <bernd@graylog.com>
2020-08-14 17:02:11 +0200

    Implement view sharing migration

--- graylog2-server/src/main/java/org/graylog/security/migrations/V20200811143600_ViewSharingToGrantsMigration.java
+++ graylog2-server/src/main/java/org/graylog/security/migrations/V20200811143600_ViewSharingToGrantsMigration.java
@@ -17,26 +17,63 @@
 package org.graylog.security.migrations;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.graylog.grn.GRN;
+import org.graylog.grn.GRNRegistry;
+import org.graylog.grn.GRNType;
+import org.graylog.grn.GRNTypes;
+import org.graylog.plugins.views.search.views.ViewDTO;
 import org.graylog.plugins.views.search.views.ViewService;
+import org.graylog.security.Capability;
 import org.graylog.security.DBGrantService;
 import org.graylog2.database.MongoConnection;
+import org.graylog2.database.NotFoundException;
 import org.graylog2.migrations.Migration;
+import org.graylog2.plugin.database.users.User;
+import org.graylog2.shared.users.Role;
+import org.graylog2.shared.users.UserService;
+import org.graylog2.users.RoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import java.time.ZonedDateTime;
-import java.util.function.Consumer;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class V20200811143600_ViewSharingToGrantsMigration extends Migration {
+    private static final Logger LOG = LoggerFactory.getLogger(V20200811143600_ViewSharingToGrantsMigration.class);
+
+    // View sharing only allowed to give other users read permissions on searches and dashboards
+    private static final Capability CAPABILITY = Capability.VIEW;
+
     private final DBGrantService grantService;
     private final MongoCollection<Document> collection;
+    private final GRNRegistry grnRegistry;
+    private final UserService userService;
+    private final RoleService roleService;
+    private final String rootUsername;
     private final ViewService viewService;
 
     @Inject
     public V20200811143600_ViewSharingToGrantsMigration(MongoConnection mongoConnection,
                                                         DBGrantService grantService,
+                                                        GRNRegistry grnRegistry,
+                                                        UserService userService,
+                                                        RoleService roleService,
+                                                        @Named("root_username") String rootUsername,
                                                         ViewService viewService) {
+
         this.grantService = grantService;
+        this.grnRegistry = grnRegistry;
+        this.userService = userService;
+        this.roleService = roleService;
+        this.rootUsername = rootUsername;
         this.viewService = viewService;
         this.collection = mongoConnection.getMongoDatabase().getCollection("view_sharings", Document.class);
     }
@@ -48,8 +85,101 @@ public class V20200811143600_ViewSharingToGrantsMigration extends Migration {
 
     @Override
     public void upgrade() {
-        collection.find().forEach((Consumer<? super Document>) document -> {
+        for (final Document document : collection.find()) {
+            LOG.debug("Migrate view sharing: {}", document);
 
-        });
+            final ObjectId sharingId = document.getObjectId("_id");
+            final String sharingType = document.get("type", String.class);
+            final String viewId = document.get("view_id", String.class);
+
+            try {
+                switch (sharingType) {
+                    case "users":
+                        //noinspection unchecked
+                        migrateUsers(viewId, (Collection<String>) document.get("users", Collection.class));
+                        break;
+                    case "roles":
+                        //noinspection unchecked
+                        migrateRoles(viewId, (Collection<String>) document.get("roles", Collection.class));
+                        break;
+                    case "all_of_instance":
+                        migrateAllOfInstance(viewId);
+                        break;
+                    default:
+                        LOG.warn("Skipping unknown view sharing type: {}", sharingType);
+                        continue; // Continue here so we don't delete the sharing document
+                }
+
+                // The view sharing document should be removed after successful migration
+                deleteViewSharing(sharingId);
+            } catch (Exception e) {
+                LOG.error("Couldn't migrate view sharing: {}", document, e);
+            }
+        }
+    }
+
+    private void migrateUsers(String viewId, Collection<String> users) {
+        LOG.debug("Migrate users for view {}: {}", viewId, users);
+
+        final GRN target = getTarget(viewId);
+
+        for (final String user : users) {
+            ensureGrant(user, target);
+        }
+    }
+
+    private void migrateRoles(String viewId, Collection<String> roleNames) {
+        LOG.debug("Migrate roles for view {}: {}", viewId, roleNames);
+
+        final GRN target = getTarget(viewId);
+
+        final Set<Role> roles = roleNames.stream()
+                .map(roleName -> {
+                    try {
+                        return Optional.of(roleService.load(roleName));
+                    } catch (NotFoundException e) {
+                        return Optional.<Role>empty();
+                    }
+                })
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet());
+
+        for (final Role role : roles) {
+            for (final User user : userService.loadAllForRole(role)) {
+                ensureGrant(user.getName(), target);
+            }
+        }
+    }
+
+    private void migrateAllOfInstance(String viewId) {
+        LOG.debug("Migrate all-of-instance for view {}", viewId);
+
+        final GRN target = getTarget(viewId);
+
+        for (final User user : userService.loadAll()) {
+            ensureGrant(user.getName(), target);
+        }
+    }
+
+    private void ensureGrant(String username, GRN target) {
+        // TODO: Needs to use the user ID once we don't use user names in references anymore!
+        final GRN grantee = GRNTypes.USER.toGRN(username);
+
+        if (!grantService.hasGrantFor(grantee, CAPABILITY, target)) {
+            grantService.create(grantee, CAPABILITY, target, rootUsername);
+        }
+    }
+
+    private GRN getTarget(String viewId) {
+        final ViewDTO view = viewService.get(viewId)
+                .orElseThrow(() -> new IllegalArgumentException("View <" + viewId + "> doesn't exist"));
+        final GRNType grnType = ViewDTO.Type.DASHBOARD.equals(view.type()) ? GRNTypes.DASHBOARD : GRNTypes.SEARCH;
+
+        return grnType.toGRN(viewId);
+    }
+
+    private void deleteViewSharing(ObjectId id) {
+        collection.deleteOne(Filters.eq("_id", id));
     }
 }
--- graylog2-server/src/test/java/org/graylog/security/migrations/V20200811143600_ViewSharingToGrantsMigrationTest.java
+++ graylog2-server/src/test/java/org/graylog/security/migrations/V20200811143600_ViewSharingToGrantsMigrationTest.java
@@ -16,9 +16,18 @@
  */
 package org.graylog.security.migrations;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.graylog.grn.GRN;
 import org.graylog.grn.GRNRegistry;
+import org.graylog.grn.GRNTypes;
 import org.graylog.plugins.views.search.views.ViewRequirements;
 import org.graylog.plugins.views.search.views.ViewService;
+import org.graylog.security.Capability;
 import org.graylog.security.DBGrantService;
 import org.graylog.security.entities.EntityOwnershipService;
 import org.graylog.testing.GRNExtension;
@@ -28,14 +37,27 @@ import org.graylog.testing.mongodb.MongoDBTestService;
 import org.graylog.testing.mongodb.MongoJackExtension;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.MongoConnection;
+import org.graylog2.database.NotFoundException;
 import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.graylog2.plugin.database.users.User;
+import org.graylog2.shared.users.Role;
+import org.graylog2.shared.users.UserService;
+import org.graylog2.users.RoleImpl;
+import org.graylog2.users.RoleService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MongoDBExtension.class)
 @ExtendWith(MongoJackExtension.class)
@@ -44,23 +66,137 @@ import java.util.Collections;
 @MongoDBFixtures("view-sharings.json")
 class V20200811143600_ViewSharingToGrantsMigrationTest {
     private V20200811143600_ViewSharingToGrantsMigration migration;
+    private DBGrantService grantService;
+    private UserService userService;
+    private RoleService roleService;
+    private MongoCollection<Document> dbCollection;
 
     @BeforeEach
     void setUp(MongoDBTestService mongodb,
                MongoJackObjectMapperProvider objectMapperProvider,
                GRNRegistry grnRegistry,
-               @Mock ClusterConfigService clusterConfigService) {
+               @Mock ClusterConfigService clusterConfigService,
+               @Mock UserService userService,
+               @Mock RoleService roleService) {
 
-        final DBGrantService grantService = new DBGrantService(mongodb.mongoConnection(), objectMapperProvider, grnRegistry);
+        this.dbCollection = mongodb.mongoCollection("view_sharings");
+        this.userService = userService;
+        this.roleService = roleService;
+        this.grantService = new DBGrantService(mongodb.mongoConnection(), objectMapperProvider, grnRegistry);
+
         final EntityOwnershipService entityOwnershipService = new EntityOwnershipService(grantService, grnRegistry);
         final TestViewService viewService = new TestViewService(mongodb.mongoConnection(), objectMapperProvider, clusterConfigService, entityOwnershipService);
 
-        this.migration = new V20200811143600_ViewSharingToGrantsMigration(mongodb.mongoConnection(), grantService, viewService);
+        this.migration = new V20200811143600_ViewSharingToGrantsMigration(mongodb.mongoConnection(), grantService, grnRegistry, userService, roleService, "admin", viewService);
+    }
+
+    private void assertDeletedViewSharing(String id) {
+        assertThat(dbCollection.countDocuments(Filters.eq("_id", new ObjectId(id))))
+                .isEqualTo(0);
     }
 
     @Test
-    void upgrade() {
+    @DisplayName("migrate user shares")
+    void migrateUserShares() throws Exception {
+        final GRN jane = GRNTypes.USER.toGRN("jane");
+        final GRN john = GRNTypes.USER.toGRN("john");
+        final GRN search = GRNTypes.SEARCH.toGRN("54e3deadbeefdeadbeef0001");
+
+        when(roleService.load(anyString())).thenThrow(new NotFoundException());
+
+        assertThat(grantService.hasGrantFor(jane, Capability.VIEW, search)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.VIEW, search)).isFalse();
+
         migration.upgrade();
+
+        assertThat(grantService.hasGrantFor(jane, Capability.VIEW, search)).isTrue();
+        assertThat(grantService.hasGrantFor(john, Capability.VIEW, search)).isTrue();
+
+        assertThat(grantService.hasGrantFor(jane, Capability.OWN, search)).isFalse();
+        assertThat(grantService.hasGrantFor(jane, Capability.MANAGE, search)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.OWN, search)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.MANAGE, search)).isFalse();
+
+        assertDeletedViewSharing("54e3deadbeefdeadbeef0001");
+    }
+
+    @Test
+    @DisplayName("migrate role shares")
+    void migrateRoleShares() throws Exception {
+        final User userJane = createUser("jane");
+        final User userJohn = createUser("john");
+        final Role role1 = createRole("role1");
+        final Role role2 = createRole("role2");
+
+        when(userService.loadAllForRole(role1)).thenReturn(ImmutableSet.of(userJane, userJohn));
+        when(userService.loadAllForRole(role2)).thenReturn(Collections.emptySet());
+        when(roleService.load(role1.getName())).thenReturn(role1);
+        when(roleService.load(role2.getName())).thenReturn(role2);
+
+        final GRN jane = GRNTypes.USER.toGRN(userJane.getName());
+        final GRN john = GRNTypes.USER.toGRN(userJohn.getName());
+        final GRN dashboard1 = GRNTypes.DASHBOARD.toGRN("54e3deadbeefdeadbeef0002");
+
+        assertThat(grantService.hasGrantFor(jane, Capability.VIEW, dashboard1)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.VIEW, dashboard1)).isFalse();
+
+        migration.upgrade();
+
+        assertThat(grantService.hasGrantFor(jane, Capability.VIEW, dashboard1)).isTrue();
+        assertThat(grantService.hasGrantFor(john, Capability.VIEW, dashboard1)).isTrue();
+
+        assertThat(grantService.hasGrantFor(jane, Capability.OWN, dashboard1)).isFalse();
+        assertThat(grantService.hasGrantFor(jane, Capability.MANAGE, dashboard1)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.OWN, dashboard1)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.MANAGE, dashboard1)).isFalse();
+
+        assertDeletedViewSharing("54e3deadbeefdeadbeef0002");
+    }
+
+    @Test
+    @DisplayName("migrate all-of-instance shares")
+    void migrateAllOfInstanceShares() throws Exception {
+        final User userJane = createUser("jane");
+        final User userJohn = createUser("john");
+
+        when(roleService.load(anyString())).thenThrow(new NotFoundException());
+        when(userService.loadAll()).thenReturn(ImmutableList.of(userJane, userJohn));
+
+        final GRN jane = GRNTypes.USER.toGRN(userJane.getName());
+        final GRN john = GRNTypes.USER.toGRN(userJohn.getName());
+        final GRN dashboard2 = GRNTypes.DASHBOARD.toGRN("54e3deadbeefdeadbeef0003");
+
+        assertThat(grantService.hasGrantFor(jane, Capability.VIEW, dashboard2)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.VIEW, dashboard2)).isFalse();
+
+        migration.upgrade();
+
+        assertThat(grantService.hasGrantFor(jane, Capability.VIEW, dashboard2)).isTrue();
+        assertThat(grantService.hasGrantFor(john, Capability.VIEW, dashboard2)).isTrue();
+
+        assertThat(grantService.hasGrantFor(jane, Capability.OWN, dashboard2)).isFalse();
+        assertThat(grantService.hasGrantFor(jane, Capability.MANAGE, dashboard2)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.OWN, dashboard2)).isFalse();
+        assertThat(grantService.hasGrantFor(john, Capability.MANAGE, dashboard2)).isFalse();
+
+        assertDeletedViewSharing("54e3deadbeefdeadbeef0003");
+    }
+
+    private User createUser(String name) {
+        final User user = mock(User.class);
+        when(user.getName()).thenReturn(name);
+        return user;
+    }
+
+    private Role createRole(String name) {
+        final RoleImpl role = new RoleImpl();
+
+        role._id = new ObjectId().toHexString();
+        role.setName(name);
+        role.setNameLower(name.toLowerCase(Locale.US));
+        role.setDescription("This is role: " + name);
+
+        return role;
     }
 
     public static class TestViewService extends ViewService {
