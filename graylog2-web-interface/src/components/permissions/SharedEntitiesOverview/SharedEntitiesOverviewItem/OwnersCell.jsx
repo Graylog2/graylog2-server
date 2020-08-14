e348230c10e587ecbbf36c6c2e4e8ca93e5afd94
Linus Pahl <linus@graylog.com>
2020-08-14 09:27:25 +0200

    Add ifPermitted for owners of a shared entity

--- graylog2-web-interface/src/components/permissions/SharedEntitiesOverview/SharedEntitiesOverviewItem/OwnersCell.jsx
+++ graylog2-web-interface/src/components/permissions/SharedEntitiesOverview/SharedEntitiesOverviewItem/OwnersCell.jsx
@@ -1,7 +1,10 @@
 // @flow strict
 import * as React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router';
 
+import { isPermitted } from 'util/PermissionsMixin';
+import CurrentUserContext from 'contexts/CurrentUserContext';
 import Routes from 'routing/Routes';
 import type { GranteesList } from 'logic/permissions/EntityShareState';
 import { getIdFromGRN } from 'logic/permissions/GRN';
@@ -10,33 +13,43 @@ type Props = {
   owners: GranteesList,
 };
 
-const _getOwnerLink = ({ type, id }) => {
+const TitleWithLink = ({ to, title }: { to: string, title: string }) => <Link to={to}>{title}</Link>;
+
+const _getOwnerTitle = ({ type, id, title }, userPermissions) => {
   const ownerId = getIdFromGRN(id, type);
 
   switch (type) {
     case 'user':
-      return Routes.SYSTEM.USERS.show(ownerId);
+      if (!isPermitted(userPermissions, 'users:list')) return title;
+
+      return <TitleWithLink to={Routes.SYSTEM.USERS.show(ownerId)} title={title} />;
     case 'team':
-      return Routes.pluginRoute('SYSTEM_TEAMS_TEAMID')(ownerId);
+      if (!isPermitted(userPermissions, 'teams:list')) return title;
+
+      return <TitleWithLink to={Routes.pluginRoute('SYSTEM_TEAMS_TEAMID')(ownerId)} title={title} />;
     default:
       throw new Error(`Owner of entity has not supported type: ${type}`);
   }
 };
 
-const OwnersCell = ({ owners }: Props) => (
-  <td className="limited">
-    {owners.map((owner, index) => {
-      const link = _getOwnerLink(owner);
-      const isLast = index >= owners.size - 1;
+const OwnersCell = ({ owners }: Props) => {
+  const currentUser = useContext(CurrentUserContext);
 
-      return (
-        <React.Fragment key={owner.id}>
-          <Link to={link}>{owner.title}</Link>
-          {!isLast && ', '}
-        </React.Fragment>
-      );
-    })}
-  </td>
-);
+  return (
+    <td className="limited">
+      {owners.map((owner, index) => {
+        const title = _getOwnerTitle(owner, currentUser?.permissions);
+        const isLast = index >= owners.size - 1;
+
+        return (
+          <React.Fragment key={owner.id}>
+            {title}
+            {!isLast && ', '}
+          </React.Fragment>
+        );
+      })}
+    </td>
+  );
+};
 
 export default OwnersCell;
