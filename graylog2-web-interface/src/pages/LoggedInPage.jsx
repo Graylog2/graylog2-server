38278aec8ed834aa5c6acf3171518636dd3fd984
Kyle Knight <kyle@graylog.com>
2020-08-28 09:33:02 -0500

    Reset background on logged in pages

--- graylog2-web-interface/src/pages/LoggedInPage.jsx
+++ graylog2-web-interface/src/pages/LoggedInPage.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import CurrentUserPreferencesProvider from 'contexts/CurrentUserPreferencesProvider';
 import AppRouter from 'routing/AppRouter';
 import CurrentUserProvider from 'contexts/CurrentUserProvider';
+import { GlobalStylesContext } from 'contexts/GlobalStylesProvider';
 
-const LoggedInPage = () => (
-  <CurrentUserProvider>
-    <CurrentUserPreferencesProvider>
-      <AppRouter />
-    </CurrentUserPreferencesProvider>
-  </CurrentUserProvider>
-);
+const LoggedInPage = () => {
+  const { addGlobalStyles } = useContext(GlobalStylesContext);
+
+  useEffect(() => {
+    addGlobalStyles(null);
+  }, []);
+
+  return (
+    <CurrentUserProvider>
+      <CurrentUserPreferencesProvider>
+        <AppRouter />
+      </CurrentUserPreferencesProvider>
+    </CurrentUserProvider>
+  );
+};
 
 export default LoggedInPage;
