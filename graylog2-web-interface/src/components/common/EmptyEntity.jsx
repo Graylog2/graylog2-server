e888a6a88dbffcc30d6ea67d922a469e0ffe254b
Linus Pahl <linus@graylog.com>
2020-09-02 17:46:46 +0200

    Add flow type definitions for Wizard

--- graylog2-web-interface/src/components/common/EmptyEntity.jsx
+++ graylog2-web-interface/src/components/common/EmptyEntity.jsx
@@ -40,8 +40,8 @@ const EmptyEntity = ({ children, title }: Props) => (
 EmptyEntity.propTypes = {
   /** Text or node to be rendered as title. */
   title: PropTypes.oneOfType([
-    PropTypes.node,
     PropTypes.string,
+    PropTypes.node,
   ]),
   /**
    * Any other content the component should display below the title. This may include a description and button
