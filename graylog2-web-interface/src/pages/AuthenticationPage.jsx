0c3560e764e8671736bf4759804add12b51f0311
Linus Pahl <linus@graylog.com>
2020-09-02 11:17:56 +0200

    Refactor AuthenticationPage and add a getting started description

--- graylog2-web-interface/src/pages/AuthenticationPage.jsx
+++ graylog2-web-interface/src/pages/AuthenticationPage.jsx
@@ -1,49 +1,57 @@
+// @flow strict
+import * as React from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
-// eslint-disable-next-line no-restricted-imports
-import createReactClass from 'create-react-class';
 
 import { Row, Col } from 'components/graylog';
 import DocsHelper from 'util/DocsHelper';
-import PermissionsMixin from 'util/PermissionsMixin';
 import {} from 'components/authentication'; // Make sure to load all auth config plugins!
 import PageHeader from 'components/common/PageHeader';
 import DocumentationLink from 'components/support/DocumentationLink';
 import AuthenticationComponent from 'components/authentication/AuthenticationComponent';
+import { EmptyEntity } from 'components/common';
 
-const AuthenticationPage = createReactClass({
-  displayName: 'AuthenticationPage',
-
-  propTypes: {
-    children: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    params: PropTypes.object.isRequired,
+type Props = {
+  location: {
+    pathname: string,
   },
+  params: {
+    name: string,
+  },
+};
 
-  mixins: [PermissionsMixin],
-
-  render() {
-    const { location, params, children } = this.props;
-
-    return (
-      <span>
-        <PageHeader title="Authentication Management">
-          <span>Configure Graylog&apos;s authentication providers of this Graylog cluster.</span>
-          <span>Read more authentication in the <DocumentationLink page={DocsHelper.PAGES.USERS_ROLES}
-                                                                   text="documentation" />.
-          </span>
-        </PageHeader>
-
-        <Row className="content">
-          <Col md={12}>
-            <AuthenticationComponent location={location} params={params}>
-              {children}
-            </AuthenticationComponent>
-          </Col>
-        </Row>
+const AuthenticationPage = ({ location, params }: Props) => (
+  <>
+    <PageHeader title="Authentication Management">
+      <span>Configure Graylog&apos;s authentication providers of this Graylog cluster.</span>
+      <span>Read more authentication in the <DocumentationLink page={DocsHelper.PAGES.USERS_ROLES}
+                                                               text="documentation" />.
       </span>
-    );
-  },
-});
+    </PageHeader>
+
+    <Row className="content">
+      <Col xs={8} xsOffset={2} md={6} mdOffset={3}>
+        <EmptyEntity>
+          <p>
+            Beside the builtin authentication mechanisms like its internal user database, or LDAP/Active Directory,
+            authentication provider can also be extended by plugins to support other authentication mechanisms, for example Single Sign-On or Two Factor Authentication.
+            Select an authentication provider to configure a new one.
+          </p>
+        </EmptyEntity>
+      </Col>
+    </Row>
+
+    {/* Old authentication management which can be removed soon */}
+    <Row className="content">
+      <Col md={12}>
+        <AuthenticationComponent location={location} params={params} />
+      </Col>
+    </Row>
+  </>
+);
+
+AuthenticationPage.propTypes = {
+  location: PropTypes.object.isRequired,
+  params: PropTypes.object.isRequired,
+};
 
 export default AuthenticationPage;
