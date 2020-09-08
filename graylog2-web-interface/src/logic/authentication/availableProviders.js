0a88d196df77f6fd30fe33b5898814c42224e9f4
Linus Pahl <linus@graylog.com>
2020-09-08 11:40:32 +0200

    Improve availableProviders codestyle

--- graylog2-web-interface/src/logic/authentication/availableProviders.js
+++ graylog2-web-interface/src/logic/authentication/availableProviders.js
@@ -2,8 +2,14 @@
 import Routes from 'routing/Routes';
 
 export const availableProviders = {
-  ldap: { name: 'LDAP', route: Routes.SYSTEM.AUTHENTICATION.PROVIDERS.CREATE_LDAP },
-  activeDirectory: { name: 'Active Directory', route: Routes.SYSTEM.AUTHENTICATION.PROVIDERS.CREATE_AD },
+  ldap: {
+    name: 'LDAP',
+    route: Routes.SYSTEM.AUTHENTICATION.PROVIDERS.CREATE_LDAP,
+  },
+  activeDirectory: {
+    name: 'Active Directory',
+    route: Routes.SYSTEM.AUTHENTICATION.PROVIDERS.CREATE_AD,
+  },
 };
 
 export const availableProvidersOptions: Array<{value: string, label: string}> = Object.keys(availableProviders)
