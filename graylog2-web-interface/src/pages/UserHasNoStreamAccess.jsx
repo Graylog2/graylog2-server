3259fb30955b4ed2ae1e4b80ce5327407a9ee70f
Dennis Oelkers <dennis@graylog.com>
2020-09-14 17:18:53 +0200

    Fixing typo.

--- graylog2-web-interface/src/pages/UserHasNoStreamAccess.jsx
+++ graylog2-web-interface/src/pages/UserHasNoStreamAccess.jsx
@@ -5,7 +5,7 @@ import ErrorPage from 'components/errors/ErrorPage';
 
 const UserHasNoStreamAccess = () => {
   return (
-    <ErrorPage title="No stream persmissions."
+    <ErrorPage title="No stream permissions."
                description={'We cannot start a search right now, because you are not allowed to access any stream. '
                  + 'If you feel this is an error, please contact your administrator.'} />
   );
