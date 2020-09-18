8edb6e6be43d1715f4979e504287971576018432
Bernd Ahlers <bernd@graylog.com>
2020-09-18 19:59:36 +0200

    Extract TrustManagerProvider into its own file so it can be reused

--- graylog2-server/src/main/java/org/graylog2/shared/security/SecurityBindings.java
+++ graylog2-server/src/main/java/org/graylog2/shared/security/SecurityBindings.java
@@ -23,8 +23,8 @@ import org.graylog2.plugin.PluginModule;
 import org.graylog2.rest.models.system.sessions.responses.DefaultSessionResponseFactory;
 import org.graylog2.rest.models.system.sessions.responses.SessionResponseFactory;
 import org.graylog2.security.DefaultX509TrustManager;
+import org.graylog2.security.TrustManagerProvider;
 import org.graylog2.security.encryption.EncryptedValueService;
-import org.graylog2.security.ldap.LdapConnector;
 
 import javax.net.ssl.TrustManager;
 
@@ -38,7 +38,7 @@ public class SecurityBindings extends PluginModule {
 
         install(new FactoryModuleBuilder()
                 .implement(TrustManager.class, DefaultX509TrustManager.class)
-                .build(LdapConnector.TrustManagerProvider.class));
+                .build(TrustManagerProvider.class));
 
         OptionalBinder.newOptionalBinder(binder(), ActorAwareAuthenticationTokenFactory.class)
                 .setDefault().to(ActorAwareUsernamePasswordTokenFactory.class);
