5e0e87a833cf6dde232dc8fe5326ad26b1655e7b
Dennis Oelkers <dennis@graylog.com>
2020-09-24 13:12:57 +0200

    Implement `withParams` using `useParams`.

--- graylog2-web-interface/src/routing/withParams.jsx
+++ graylog2-web-interface/src/routing/withParams.jsx
@@ -1,5 +1,17 @@
 // @flow strict
+import * as React from 'react';
+import { useParams } from 'react-router-dom';
 
-const withParams = <T>(Component: T): T => Component;
+type InjectedProps = {| params: { [string]: string } |};
+
+function withParams<Config>(
+  Component: React.AbstractComponent<{| ...Config, ...InjectedProps |}>,
+): React.AbstractComponent<Config> {
+  return (props: Config) => {
+    const params = useParams();
+
+    return <Component {...props} params={params} />;
+  };
+}
 
 export default withParams;
