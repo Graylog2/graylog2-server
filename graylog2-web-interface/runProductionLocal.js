8f9aec39df35d1523c78e65af168ddf3532ca610
Gaya Kessler <gaya@theclevernode.com>
2020-09-28 10:33:01 +0200

    add param for providing api url

--- graylog2-web-interface/runProductionLocal.js
+++ graylog2-web-interface/runProductionLocal.js
@@ -6,7 +6,8 @@ const productionHelpers = require('./productionHelpers');
 process.setMaxListeners(0);
 
 const buildDir = process.argv[2] || 'target/web/build';
-const plugins = process.argv.slice(3);
+const apiUrl = process.argv[3] || 'http://localhost:9000/api';
+const plugins = process.argv.slice(4);
 
 const pluginModules = plugins.map((plugin) => JSON.parse(fs.readFileSync(plugin)));
 const assets = productionHelpers.collectAssets(pluginModules);
@@ -14,7 +15,7 @@ const pluginMounts = productionHelpers.collectMounts(plugins);
 
 const { url } = productionHelpers.bootstrapExpress(
   buildDir,
-  productionHelpers.config('http://localhost:9000/api'),
+  productionHelpers.config(apiUrl),
   pluginMounts,
   productionHelpers.generateIndexHtml(assets),
   '8080',
