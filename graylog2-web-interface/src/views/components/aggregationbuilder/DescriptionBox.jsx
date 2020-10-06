63fb8f3d578ee21296af0ba465583d1b8d20ba31
Linus Pahl <linus@graylog.com>
2020-10-06 14:46:08 +0200

    Add button type for columns config button in widget edit modal to prevent widget edit form submit

--- graylog2-web-interface/src/views/components/aggregationbuilder/DescriptionBox.jsx
+++ graylog2-web-interface/src/views/components/aggregationbuilder/DescriptionBox.jsx
@@ -71,6 +71,7 @@ class DescriptionBox extends React.Component {
     if (configurableOptions) {
       return (
         <ConfigButton ref={(node) => { this.target = node; }}
+                      type="button"
                       onClick={this.onToggleConfig}>
           <Icon name="wrench" />
         </ConfigButton>
