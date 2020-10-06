ed8ee3b9d9688d1046ceee888a01374c349f1c89
Linus Pahl <linus@graylog.com>
2020-10-06 09:54:16 +0200

    Typo fix

--- graylog2-web-interface/src/components/authentication/BackendCreate/GettingStarted.jsx
+++ graylog2-web-interface/src/components/authentication/BackendCreate/GettingStarted.jsx
@@ -15,7 +15,7 @@ const GettingStarted = ({ title }: Props) => (
     <Col md={6} mdOffset={3}>
       <EmptyEntity title={title}>
         <p>
-          Beside the builtin authentication mechanisms like its internal user database or LDAP/Active Directory,
+          Beside the built-in authentication mechanisms like its internal user database or LDAP/Active Directory,
           authentication services can also be extended by plugins to support other authentication mechanisms.
           Select an authentication services to setup a new one.
         </p>
