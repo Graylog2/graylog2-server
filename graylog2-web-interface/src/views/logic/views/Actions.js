1813e2d249fa8e282a6e5f0d8fa147219a09c09d
Dennis Oelkers <dennis@graylog.com>
2020-10-07 10:30:06 +0200

    Small cleanup.

--- graylog2-web-interface/src/views/logic/views/Actions.js
+++ graylog2-web-interface/src/views/logic/views/Actions.js
@@ -2,14 +2,8 @@
 import history from 'util/History';
 import Routes from 'routing/Routes';
 
-export const loadNewView = () => {
-  return history.push(`${Routes.SEARCH}/new`);
-};
+export const loadNewView = () => history.push(`${Routes.SEARCH}/new`);
 
-export const loadNewViewForStream = (streamId: string) => {
-  return history.push(`${Routes.stream_search(streamId)}/new`);
-};
+export const loadNewViewForStream = (streamId: string) => history.push(`${Routes.stream_search(streamId)}/new`);
 
-export const loadView = (viewId: string) => {
-  return history.push(`${Routes.SEARCH}/${viewId}`);
-};
+export const loadView = (viewId: string) => history.push(`${Routes.SEARCH}/${viewId}`);
