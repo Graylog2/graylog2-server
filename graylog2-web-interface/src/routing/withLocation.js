8df478f2806b3262e61985971c19a9683546ee5f
Dennis Oelkers <dennis@graylog.com>
2020-10-08 11:03:15 +0200

    Adding parsed query parameters in `withLocation`.

--- graylog2-web-interface/src/routing/withLocation.js
+++ graylog2-web-interface/src/routing/withLocation.js
@@ -1,5 +1,9 @@
 // @flow strict
-import { withRouter } from 'react-router';
+import * as React from 'react';
+import { useMemo } from 'react';
+import { useLocation } from 'react-router';
+
+import useQuery from './useQuery';
 
 export type Location = {
   query: { [string]: ?string },
@@ -11,6 +15,12 @@ type LocationContext = {
   location: Location,
 };
 
-const withLocation = <Props: {...}, ComponentType: React$ComponentType<Props>>(Component: ComponentType): React$ComponentType<$Diff<React$ElementConfig<ComponentType>, LocationContext>> => withRouter(Component);
+const withLocation = <Props: {...}, ComponentType: React$ComponentType<Props>>(Component: ComponentType): React$ComponentType<$Diff<React$ElementConfig<ComponentType>, LocationContext>> => (props) => {
+  const location = useLocation();
+  const query = useQuery();
+  const locationWithQuery = useMemo(() => ({ ...location, query }), [location, query]);
+
+  return <Component {...props} location={locationWithQuery} />;
+};
 
 export default withLocation;
