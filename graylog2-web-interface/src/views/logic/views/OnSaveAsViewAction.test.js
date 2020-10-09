894f82386d0d4335dabfdba4f02d4d6e9eda176b
Dennis Oelkers <dennis@graylog.com>
2020-10-09 10:32:11 +0200

    Making test simpler.

--- graylog2-web-interface/src/views/logic/views/OnSaveAsViewAction.test.js
+++ graylog2-web-interface/src/views/logic/views/OnSaveAsViewAction.test.js
@@ -1,98 +1,78 @@
+// @flow strict
+import { ViewActions } from 'views/stores/ViewStore';
+import { ViewManagementActions } from 'views/stores/ViewManagementStore';
+import UserNotification from 'util/UserNotification';
+
 import View from './View';
+import OnSaveAsViewAction from './OnSaveAsViewAction';
+import { loadDashboard } from './Actions';
 
-// eslint-disable-next-line global-require
-const loadSUT = () => require('./OnSaveAsViewAction').default;
-
-const mockActions = () => {
-  const ViewActions = { load: jest.fn(() => Promise.resolve({ view: { id: 'deadbeef' } })).mockName('load') };
-  const ViewManagementActions = {
+jest.mock('views/stores/ViewManagementStore', () => ({
+  ViewManagementActions: {
     create: jest.fn((v) => Promise.resolve(v)).mockName('create'),
-  };
+  },
+}));
 
-  const history = {
-    push: jest.fn(),
-  };
+jest.mock('views/stores/ViewStore', () => ({
+  ViewActions: {
+    load: jest.fn(() => Promise.resolve({ view: { id: 'deadbeef' } })).mockName('load'),
+  },
+}));
 
-  jest.doMock('views/stores/ViewManagementStore', () => ({ ViewManagementActions }));
-  jest.doMock('views/stores/ViewStore', () => ({ ViewActions }));
-  jest.doMock('util/History', () => history);
-
-  return { ViewActions, ViewManagementActions, history };
-};
+jest.mock('util/UserNotification');
+jest.mock('views/logic/views/Actions');
 
 describe('OnSaveAsViewAction', () => {
   afterEach(() => {
-    jest.resetModules();
+    jest.clearAllMocks();
   });
 
-  it('saves a given new view', () => {
-    const { ViewManagementActions } = mockActions();
-    const onSaveAsView = loadSUT();
+  it('saves a given new view', async () => {
     const view = View.create();
 
-    return onSaveAsView(view).then(() => {
-      expect(ViewManagementActions.create).toHaveBeenCalledTimes(1);
-      expect(ViewManagementActions.create.mock.calls[0][0]).toEqual(view);
-    });
+    await OnSaveAsViewAction(view);
+
+    expect(ViewManagementActions.create).toHaveBeenCalled();
+    expect(ViewManagementActions.create).toHaveBeenCalledWith(view);
   });
 
-  it('loads saved view', () => {
-    const { ViewActions } = mockActions();
-    const onSaveAsView = loadSUT();
+  it('loads saved view', async () => {
     const view = View.create();
 
-    return onSaveAsView(view).then(() => {
-      expect(ViewActions.load).toHaveBeenCalledTimes(1);
-      expect(ViewActions.load.mock.calls[0][0]).toEqual(view);
-    });
+    await OnSaveAsViewAction(view);
+
+    expect(ViewActions.load).toHaveBeenCalled();
+    expect(ViewActions.load).toHaveBeenCalledWith(view);
   });
 
-  it('redirects to saved view', () => {
-    const { history } = mockActions();
-    const onSaveAsView = loadSUT();
+  it('redirects to saved view', async () => {
     const view = View.create();
 
-    return onSaveAsView(view).then(() => {
-      expect(history.push).toHaveBeenCalledWith('/dashboards/deadbeef');
-    });
+    await OnSaveAsViewAction(view);
+
+    expect(loadDashboard).toHaveBeenCalledWith('deadbeef');
   });
 
-  it('shows notification upon success', () => {
-    mockActions();
-    const UserNotification = { success: jest.fn().mockName('success') };
-
-    jest.doMock('util/UserNotification', () => UserNotification);
-    const onSaveAsView = loadSUT();
+  it('shows notification upon success', async () => {
     const view = View.create().toBuilder().title('Test View').build();
 
-    return onSaveAsView(view).then(() => {
-      expect(UserNotification.success).toHaveBeenCalledTimes(1);
-      expect(UserNotification.success.mock.calls[0][0]).toEqual(`Saving view "${view.title}" was successful!`);
-      expect(UserNotification.success.mock.calls[0][1]).toEqual('Success!');
-    });
+    await OnSaveAsViewAction(view);
+
+    expect(UserNotification.success).toHaveBeenCalled();
+    expect(UserNotification.success).toHaveBeenCalledWith(`Saving view "${view.title}" was successful!`, 'Success!');
   });
 
-  it('does not do anything if saving fails', () => {
-    const { ViewManagementActions, ViewActions, history } = mockActions();
+  it('does not do anything if saving fails', async () => {
+    ViewManagementActions.create.mockImplementation(() => Promise.reject(new Error('Something bad happened!')));
 
-    ViewManagementActions.create = jest.fn(() => Promise.reject(new Error('Something bad happened!')));
-    const UserNotification = {
-      success: jest.fn().mockName('success'),
-      error: jest.fn().mockName('error'),
-    };
-
-    jest.doMock('util/UserNotification', () => UserNotification);
-
-    const onSaveAsView = loadSUT();
     const view = View.create();
 
-    return onSaveAsView(view).then(() => {
-      expect(ViewActions.load).not.toHaveBeenCalled();
-      expect(history.push).not.toHaveBeenCalled();
-      expect(UserNotification.success).not.toHaveBeenCalled();
-      expect(UserNotification.error).toHaveBeenCalledTimes(1);
-      expect(UserNotification.error.mock.calls[0][0]).toEqual('Saving view failed: Error: Something bad happened!');
-      expect(UserNotification.error.mock.calls[0][1]).toEqual('Error!');
-    });
+    await OnSaveAsViewAction(view);
+
+    expect(ViewActions.load).not.toHaveBeenCalled();
+    expect(loadDashboard).not.toHaveBeenCalled();
+    expect(UserNotification.success).not.toHaveBeenCalled();
+    expect(UserNotification.error).toHaveBeenCalledTimes(1);
+    expect(UserNotification.error).toHaveBeenCalledWith('Saving view failed: Error: Something bad happened!', 'Error!');
   });
 });
