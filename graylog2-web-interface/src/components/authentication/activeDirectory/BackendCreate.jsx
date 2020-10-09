a357779dc050995083700e7fd623c579945c272d
Linus Pahl <linus@graylog.com>
2020-10-09 10:19:43 +0200

    Create group sync backend on active directory backend create

--- graylog2-web-interface/src/components/authentication/activeDirectory/BackendCreate.jsx
+++ graylog2-web-interface/src/components/authentication/activeDirectory/BackendCreate.jsx
@@ -1,11 +1,15 @@
 // @flow strict
 import * as React from 'react';
+import { PluginStore } from 'graylog-web-plugin/plugin';
 
 import { DocumentTitle } from 'components/common';
+import type { LdapCreate } from 'logic/authentication/ldap/types';
 import BackendWizard from 'components/authentication/BackendWizard';
 import AuthenticationDomain from 'domainActions/authentication/AuthenticationDomain';
 
 import WizardPageHeader from './WizardPageHeader';
+
+import type { WizardFormValues } from '../BackendWizard/contexts/BackendWizardContext';
 
 export const HELP = {
   systemUserDn: (
@@ -55,13 +59,28 @@ export const AUTH_BACKEND_META = {
   urlScheme: 'ldap',
 };
 
-const BackendCreate = () => (
-  <DocumentTitle title="Create Active Directory Authentication Services">
-    <WizardPageHeader />
-    <BackendWizard authBackendMeta={AUTH_BACKEND_META}
-                   help={HELP}
-                   onSubmit={AuthenticationDomain.create} />
-  </DocumentTitle>
-);
+const BackendCreate = () => {
+  const authGroupSyncPlugins = PluginStore.exports('authentication.enterprise.ldap.groupSync');
+  const groupSyncActions = authGroupSyncPlugins?.[0]?.actions;
+
+  const _handleSubmit = (payload: LdapCreate, formValues: WizardFormValues) => {
+    return AuthenticationDomain.create(payload).then((result) => {
+      if (result && formValues.synchronizeGroups && groupSyncActions?.handleUpdate) {
+        return groupSyncActions.handleUpdate(formValues, result.backend.id, AUTH_BACKEND_META.serviceType);
+      }
+
+      return result;
+    });
+  };
+
+  return (
+    <DocumentTitle title="Create Active Directory Authentication Services">
+      <WizardPageHeader />
+      <BackendWizard authBackendMeta={AUTH_BACKEND_META}
+                     help={HELP}
+                     onSubmit={_handleSubmit} />
+    </DocumentTitle>
+  );
+};
 
 export default BackendCreate;
