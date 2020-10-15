348aac729dcf9a8e9b1b0814ef3cc2c745dd6b58
Linus Pahl <linus@graylog.com>
2020-10-15 10:39:56 +0200

    Display Alert if UsersOverview table is empty

--- graylog2-web-interface/src/components/users/UsersOverview/UsersFilter.jsx
+++ graylog2-web-interface/src/components/users/UsersOverview/UsersFilter.jsx
@@ -1,8 +1,6 @@
 // @flow strict
 import * as React from 'react';
-import styled, { type StyledComponent } from 'styled-components';
 
-import type { ThemeInterface } from 'theme';
 import { SearchForm } from 'components/common';
 import type { PaginatedUsers } from 'actions/users/UsersActions';
 
@@ -13,22 +11,18 @@ type Props = {
   onReset: () => Promise<?PaginatedUsers>,
 };
 
-const Container: StyledComponent<{}, ThemeInterface, HTMLDivElement> = styled.div`
-  margin-bottom: 10px;
-`;
-
 const UsersFilter = ({ onSearch, onReset }: Props) => {
   const _handleSearch = (newQuery, resetLoading) => onSearch(newQuery).then(resetLoading);
   const queryHelper = <UserQueryHelper />;
 
   return (
-    <Container>
-      <SearchForm onSearch={_handleSearch}
-                  onReset={onReset}
-                  useLoadingState
-                  queryHelpComponent={queryHelper}
-                  topMargin={0} />
-    </Container>
+    <SearchForm onSearch={_handleSearch}
+                wrapperClass="has-bm"
+                onReset={onReset}
+                useLoadingState
+                queryHelpComponent={queryHelper}
+                topMargin={0} />
+
   );
 };
 
