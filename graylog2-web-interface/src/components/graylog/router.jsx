3e1bc309a4361824a2ac83c6855cbe7ccf2bd492
Dennis Oelkers <dennis@graylog.com>
2020-10-16 10:41:28 +0200

    Reimplementing `LinkContainer`, compatible to previous one.

--- graylog2-web-interface/src/components/graylog/router.jsx
+++ graylog2-web-interface/src/components/graylog/router.jsx
@@ -1,6 +1,32 @@
 // @flow strict
-import { LinkContainer } from 'react-router-bootstrap';
+import * as React from 'react';
+import { useCallback } from 'react';
 import { Link } from 'react-router-dom';
+
+import history from 'util/History';
+
+type Props = {
+  children: React.Node,
+  onClick: () => mixed,
+  to: string,
+};
+
+const LinkContainer = ({ children, onClick, to }: Props) => {
+  const { props: { onClick: childrenOnClick } } = children;
+  const _onClick = useCallback(() => {
+    if (childrenOnClick) {
+      childrenOnClick();
+    }
+
+    if (onClick) {
+      onClick();
+    }
+
+    history.push(to);
+  }, [childrenOnClick, onClick, to]);
+
+  return React.cloneElement(React.Children.only(children), { onClick: _onClick });
+};
 
 export {
   Link,
