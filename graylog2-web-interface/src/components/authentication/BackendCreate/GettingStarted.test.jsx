184ece3cbe6e4fba7e60f1297c43f23466a7ca60
Linus Pahl <linus@graylog.com>
2020-10-23 15:13:48 +0200

    Fix authentication service GettingStarted test

--- graylog2-web-interface/src/components/authentication/BackendCreate/GettingStarted.test.jsx
+++ graylog2-web-interface/src/components/authentication/BackendCreate/GettingStarted.test.jsx
@@ -8,7 +8,7 @@ describe('GettingStarted', () => {
   it('should display description and select', () => {
     const { queryByText, queryByLabelText } = render(<GettingStarted />);
 
-    expect(queryByText(/Select an authentication services to setup a new one./)).not.toBeNull();
+    expect(queryByText(/Select an authentication service to setup a new one./)).not.toBeNull();
     expect(queryByLabelText('Select a service')).not.toBeNull();
   });
 });
