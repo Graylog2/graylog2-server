094fe9de885882f75fa066989344388d411da7a4
Kyle Knight <kyle@graylog.com>
2020-10-26 08:18:49 -0500

    DateTimePicker Live Preview (#9125)
    * Migrating original range options to Popover
    
    * Converting existing DateTimePicker to Popover
    
    * Styling TimeRangeDisplay
    
    * Flow Tests
    
    * Some Cleanup
    
    * Linting
    
    * Rendering Live Preview
    
    * merge/rebase touchups

---
+++ graylog2-web-interface/src/views/components/searchbar/date-time-picker/TimeRangeLivePreview.jsx
@@ -0,0 +1,77 @@
+// @flow strict
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import styled, { css, type StyledComponent } from 'styled-components';
+
+import { type ThemeInterface } from 'theme';
+import { Icon } from 'components/common';
+
+import { EMPTY_OUTPUT, dateOutput } from '../TimeRangeDisplay';
+
+type Props = {
+  timerange: any,
+};
+
+const PreviewWrapper: StyledComponent<{}, void, HTMLDivElement> = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  float: right;
+  transform: translateY(-3px);
+`;
+
+const FromWrapper: StyledComponent<{}, void, HTMLSpanElement> = styled.span`
+  text-align: right;
+`;
+
+const UntilWrapper: StyledComponent<{}, void, HTMLSpanElement> = styled.span`
+  text-align: left;
+`;
+
+const Title: StyledComponent<{}, ThemeInterface, HTMLSpanElement> = styled.span(({ theme }) => css`
+  font-size: ${theme.fonts.size.large};
+  color: ${theme.colors.variant.dark.info};
+  display: block;
+  font-style: italic;
+`);
+
+const Date: StyledComponent<{}, ThemeInterface, HTMLSpanElement> = styled.span(({ theme }) => css`
+  font-size: ${theme.fonts.size.body};
+  color: ${theme.colors.variant.primary};
+  display: block;
+  font-weight: bold;
+`);
+
+const MiddleIcon: StyledComponent<{}, ThemeInterface, HTMLSpanElement> = styled.span(({ theme }) => css`
+  font-size: ${theme.fonts.size.large};
+  color: ${theme.colors.variant.default};
+  padding: 0 15px;
+`);
+
+const TimeRangeLivePreview = ({ timerange }: Props) => {
+  const [{ from, until }, setTimeOutput] = useState(EMPTY_OUTPUT);
+
+  useEffect(() => {
+    setTimeOutput(dateOutput(timerange));
+  }, [timerange]);
+
+  return (
+    <PreviewWrapper>
+      <FromWrapper>
+        <Title>From</Title>
+        <Date>{from}</Date>
+      </FromWrapper>
+
+      <MiddleIcon>
+        <Icon name="arrow-right" />
+      </MiddleIcon>
+
+      <UntilWrapper>
+        <Title>Until</Title>
+        <Date>{until}</Date>
+      </UntilWrapper>
+    </PreviewWrapper>
+  );
+};
+
+export default TimeRangeLivePreview;
