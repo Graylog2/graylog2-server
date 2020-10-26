af5950c85fb1be7945f72d298a561267eb35a693
Linus Pahl <linus@graylog.com>
2020-10-26 17:44:12 +0100

    Provide active className for Buttons wrapped with LinkContainer if target equals currrent pathname

--- graylog2-web-interface/src/components/graylog/router.jsx
+++ graylog2-web-interface/src/components/graylog/router.jsx
@@ -4,18 +4,37 @@ import { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import type { LocationShape } from 'react-router';
 
+import withLocation, { type Location } from 'routing/withLocation';
 import history from 'util/History';
 
 export type HistoryElement = LocationShape;
+
+const _setActiveClassName = (location, to, currentClassName, displayName) => {
+  const props = {};
+  const isActive = to === location.pathname;
+
+  if (displayName === 'Button' && isActive) {
+    let className = 'active';
+
+    if (currentClassName) {
+      className = `${className} ${currentClassName}`;
+    }
+
+    props.className = className;
+  }
+
+  return props;
+};
 
 type Props = {
   children: React.Node,
   onClick?: () => mixed,
   to: string | HistoryElement,
+  location: Location,
 };
 
-const LinkContainer = ({ children, onClick, to, ...rest }: Props) => {
-  const { props: { onClick: childrenOnClick } } = React.Children.only(children);
+const LinkContainer = withLocation(({ children, onClick, to, location, ...rest }: Props) => {
+  const { props: { onClick: childrenOnClick, className }, type: { displayName } } = React.Children.only(children);
   const _onClick = useCallback(() => {
     if (childrenOnClick) {
       childrenOnClick();
@@ -27,9 +46,10 @@ const LinkContainer = ({ children, onClick, to, ...rest }: Props) => {
 
     history.push(to);
   }, [childrenOnClick, onClick, to]);
+  const childrenClassName = _setActiveClassName(location, to, className, displayName);
 
-  return React.cloneElement(React.Children.only(children), { ...rest, onClick: _onClick });
-};
+  return React.cloneElement(React.Children.only(children), { ...rest, ...childrenClassName, onClick: _onClick });
+});
 
 export {
   Link,
