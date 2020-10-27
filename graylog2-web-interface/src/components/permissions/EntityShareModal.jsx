5d5dcd0bd873a8387c74725808831e0bf6e19f4c
Linus Pahl <linus@graylog.com>
2020-10-27 17:41:43 +0100

    Remove icon from entity share modal title

--- graylog2-web-interface/src/components/permissions/EntityShareModal.jsx
+++ graylog2-web-interface/src/components/permissions/EntityShareModal.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import EntityShareDomain from 'domainActions/permissions/EntityShareDomain';
 import { createGRN } from 'logic/permissions/GRN';
 import { useStore } from 'stores/connect';
-import { Spinner, Icon } from 'components/common';
+import { Spinner } from 'components/common';
 import { EntityShareStore } from 'stores/permissions/EntityShareStore';
 import { type EntitySharePayload } from 'actions/permissions/EntityShareActions';
 import SharedEntity from 'logic/permissions/SharedEntity';
@@ -70,7 +70,7 @@ const EntityShareModal = ({ description, entityId, entityType, entityTitle, enti
                            onConfirm={_handleSave}
                            onModalClose={onClose}
                            showModal
-                           title={<><Icon name="user-plus" /> Sharing {entityTypeTitle ?? entityType}: <i>{entityTitle}</i></>}>
+                           title={<>Sharing {entityTypeTitle ?? entityType}: <i>{entityTitle}</i></>}>
       <>
         {(entityShareState && entityShareState.entity === entityGRN) ? (
           <EntityShareSettings description={description}
