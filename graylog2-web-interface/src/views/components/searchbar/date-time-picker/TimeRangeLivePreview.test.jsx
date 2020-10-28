ed3626dfedbafe153fa4db26a19a72a9263f2722
Kyle Knight <kyle@graylog.com>
2020-10-28 07:46:48 -0500

    More tests & linting

--- graylog2-web-interface/src/views/components/searchbar/date-time-picker/TimeRangeLivePreview.test.jsx
+++ graylog2-web-interface/src/views/components/searchbar/date-time-picker/TimeRangeLivePreview.test.jsx
@@ -2,26 +2,24 @@
 import React from 'react';
 import { render, screen } from 'wrappedTestingLibrary';
 
-import TimeRangeLivePreview from './TimeRangeLivePreview';
+import {
+  type RelativeTimeRange,
+  type AbsoluteTimeRange,
+  type KeywordTimeRange,
+} from 'views/logic/queries/Query';
 
-type Props = {
-  type: string,
-  keyword?: string,
-  range?: number,
-  from?: string,
-  to?: string,
-};
+import TimeRangeLivePreview from './TimeRangeLivePreview';
 
 describe('TimeRangeLivePreview', () => {
   it('renders relative timerange', () => {
-    const timerange: Props = { type: 'relative', range: 300 };
+    const timerange: RelativeTimeRange = { type: 'relative', range: 300 };
     render(<TimeRangeLivePreview timerange={timerange} />);
 
     expect(screen.getByText(/5 minutes ago/i)).not.toBeNull();
   });
 
   it('renders absolute timerange', () => {
-    const timerange: Props = { type: 'absolute', from: '1955-5-11 06:15:00.000', to: '1985-25-10 08:18:00.000' };
+    const timerange: AbsoluteTimeRange = { type: 'absolute', from: '1955-5-11 06:15:00.000', to: '1985-25-10 08:18:00.000' };
     render(<TimeRangeLivePreview timerange={timerange} />);
 
     expect(screen.getByText(/1955-5-11 06:15:00.000/i)).not.toBeNull();
@@ -29,7 +27,7 @@ describe('TimeRangeLivePreview', () => {
   });
 
   it('renders keyword timerange', () => {
-    const timerange: Props = { type: 'keyword', keyword: 'Last ten minutes', from: '2020-10-27 15:20:56', to: '2020-10-27 15:30:56' };
+    const timerange: KeywordTimeRange = { type: 'keyword', keyword: 'Last ten minutes', from: '2020-10-27 15:20:56', to: '2020-10-27 15:30:56' };
     render(<TimeRangeLivePreview timerange={timerange} />);
 
     expect(screen.getByText(/2020-10-27 15:20:56/i)).not.toBeNull();
