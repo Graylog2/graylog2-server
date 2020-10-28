46b495ae2e709e7c0d375b99499aba4b1542ca97
Marco Pfatschbacher <marco@graylog.com>
2020-10-28 13:06:10 +0100

    Extract ConcurrentUUID into a separate dual licensed class

--- graylog2-server/src/main/java/org/graylog2/shared/rest/RequestIdFilter.java
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/RequestIdFilter.java
@@ -17,15 +17,13 @@
 package org.graylog2.shared.rest;
 
 import com.google.common.base.Strings;
+import org.graylog.util.uuid.ConcurrentUUID;
 
 import javax.annotation.Priority;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import java.io.IOException;
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 
 // Needs to run before ShiroAuthorizationFilter
 @Priority(Priorities.AUTHORIZATION - 20)
@@ -36,32 +34,8 @@ public class RequestIdFilter implements ContainerRequestFilter {
     public void filter(ContainerRequestContext requestContext) throws IOException {
         String id = requestContext.getHeaderString(X_REQUEST_ID);
         if (Strings.isNullOrEmpty(id)) {
-            id = generateRandomUuid().toString();
+            id = ConcurrentUUID.generateRandomUuid().toString();
         }
         requestContext.getHeaders().putSingle(X_REQUEST_ID, id);
-    }
-
-    /**
-     * Generate a random UUID v4 that will perform reasonably when used by
-     * multiple threads under load.
-     *
-     * @see <a href="https://github.com/Netflix/netflix-commons/blob/v0.3.0/netflix-commons-util/src/main/java/com/netflix/util/concurrent/ConcurrentUUIDFactory.java">ConcurrentUUIDFactory</a>
-     * @return random UUID
-     */
-    private static UUID generateRandomUuid() {
-        final Random rnd = ThreadLocalRandom.current();
-        long mostSig  = rnd.nextLong();
-        long leastSig = rnd.nextLong();
-
-        // Identify this as a version 4 UUID, that is one based on a random value.
-        mostSig &= 0xffffffffffff0fffL;
-        mostSig |= 0x0000000000004000L;
-
-        // Set the variant identifier as specified for version 4 UUID values.  The two
-        // high order bits of the lower word are required to be one and zero, respectively.
-        leastSig &= 0x3fffffffffffffffL;
-        leastSig |= 0x8000000000000000L;
-
-        return new UUID(mostSig, leastSig);
     }
 }
