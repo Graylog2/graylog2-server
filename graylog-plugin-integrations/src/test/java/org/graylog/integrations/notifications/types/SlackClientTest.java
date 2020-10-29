3eabc1f238e4356bcf55890ab547b3d24403e009
Priya <varpri@gmail.com>
2020-10-29 15:55:00 -0500

    Using mock okhttpclient  (Graylog2/graylog-plugin-integrations#618)
    * optimize imports
    
    * added more unit test methods
    
    * Update src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
    
    Co-authored-by: Rob Curtis <6466251+waab76@users.noreply.github.com>
    
    * Update src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
    
    Co-authored-by: Rob Curtis <6466251+waab76@users.noreply.github.com>
    
    * Update src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
    
    Co-authored-by: Rob Curtis <6466251+waab76@users.noreply.github.com>
    
    * Update src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
    
    Co-authored-by: Rob Curtis <6466251+waab76@users.noreply.github.com>
    
    * Update SlackClientTest.java
    
    removing mock web server
    
    * Update SlackClientTest.java
    
    * fix junit failure
    
    * clean up imports
    
    * sent content length check
    
    * fix intellij warning
    
    Co-authored-by: Rob Curtis <6466251+waab76@users.noreply.github.com>

--- graylog-plugin-integrations/src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
+++ graylog-plugin-integrations/src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
@@ -1,6 +1,5 @@
 package org.graylog.integrations.notifications.types;
 
-import com.github.joschi.jadconfig.util.Duration;
 import okhttp3.Call;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -8,67 +7,94 @@ import okhttp3.Protocol;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import okhttp3.mockwebserver.MockWebServer;
 import org.graylog.events.notifications.PermanentEventNotificationException;
-import org.graylog2.shared.bindings.providers.OkHttpClientProvider;
+import org.graylog.events.notifications.TemporaryEventNotificationException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 
 import java.io.IOException;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import org.mockito.ArgumentCaptor;
 
 
 public class SlackClientTest {
-
-    private final MockWebServer server = new MockWebServer();
+    
     private OkHttpClient mockHttpClient;
 
 
     @Before
     public void setUp() throws Exception {
-       mockHttpClient = getMockHttpClient("{\"key\": \"val\"}",200);
-       server.start();
+       mockHttpClient = getMockHttpClient(200);
     }
 
     @After
-    public void tearDown() throws IOException {
-        server.shutdown();
+    public void tearDown() {
         mockHttpClient = null;
     }
 
 
-
     @Test
-    public void send_is_successful() throws Exception {
+    public void send_sendsHttpRequestAsExpected_whenInputIsGood() throws Exception {
         SlackClient slackClient = new SlackClient(mockHttpClient);
         SlackMessage message = new SlackMessage("Henry Hühnchen(little chicken)");
-        slackClient.send(message,"http://url.com");
+        slackClient.send(message,"http://url.com/");
+        
+        ArgumentCaptor<Request> requestCaptor = ArgumentCaptor.forClass(Request.class);
+        verify(mockHttpClient, times(1)).newCall(requestCaptor.capture());
+
+        assertThat(requestCaptor.getValue()).isNotNull();
+        Request sent = requestCaptor.getValue();
+        assertThat(sent.url().toString()).isEqualTo("http://url.com/");
+        assertThat(sent.method()).isEqualTo("POST");
+        assertThat(sent.body()).isNotNull();
+        assertThat(sent.body().contentLength()).isEqualTo(76);
+
     }
 
-    private static OkHttpClient getMockHttpClient(final String serializedBody,int httpCode) throws IOException {
+    @Test(expected = TemporaryEventNotificationException.class)
+    public void send_throwsTempNotifException_whenHttpClientThrowsIOException() throws Exception {
+
+        final OkHttpClient okHttpClient = mock(OkHttpClient.class);
+        final Call remoteCall = mock(Call.class);
+        when(remoteCall.execute()).thenThrow(new IOException("Request timeout"));
+        when(okHttpClient.newCall(any())).thenReturn(remoteCall);
+
+
+        SlackClient slackClient = new SlackClient(okHttpClient);
+        SlackMessage message = new SlackMessage("Henry Hühnchen(little chicken)");
+        slackClient.send(message,"http://url.com/");
+    }
+
+    @Test(expected = PermanentEventNotificationException.class)
+    public void send_throwsPermNotifException_whenPostReturnsHttp402() throws Exception {
+
+        final OkHttpClient okHttpClient = getMockHttpClient(402);
+        SlackClient slackClient = new SlackClient(okHttpClient);
+        SlackMessage message = new SlackMessage("Henry Hühnchen(little chicken)");
+        slackClient.send(message,"http://url.com/");
+    }
+
+
+
+    private static OkHttpClient getMockHttpClient(int httpCode) throws IOException {
         final OkHttpClient okHttpClient = mock(OkHttpClient.class);
 
         final Call remoteCall = mock(Call.class);
 
         final Response response = new Response.Builder()
-                .request(new Request.Builder().url("http://url.com").build())
+                .request(new Request.Builder().url("http://url.com/").build())
                 .protocol(Protocol.HTTP_1_1)
                 .code(httpCode).message("").body(
                         ResponseBody.create(
                                 MediaType.parse("application/json"),
-                                serializedBody
+                                "{\"key\": \"val\"}"
                         ))
                 .build();
 
