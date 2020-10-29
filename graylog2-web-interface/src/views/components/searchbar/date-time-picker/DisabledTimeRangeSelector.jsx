5599a9171c01fa946e888d9afca617014252ab7d
Kyle Knight <kyle@graylog.com>
2020-10-29 04:35:30 -0500

    DateTimePicker Relative Time Range (#9162)
    * Migrating original range options to Popover
    
    * Converting existing DateTimePicker to Popover
    
    * Styling TimeRangeDisplay
    
    * Flow Tests
    
    * Some Cleanup
    
    * Linting
    
    * merge/rebase touchups
    
    * Migrating original range options to Popover
    
    * Converting existing DateTimePicker to Popover
    
    * Flow Tests
    
    * Some Cleanup
    
    * Linting
    
    * Remove TimerangeSelector
    
    * Changing From time updates preview & saveable
    
    * Relative From Range working
    
    * Layout touchup and handle All Time uncheck
    
    * RelativeTimeRangeSelector tests
    
    * Clean up configuration of relative times
    
    * Disable All Time check if query limit defined
    
    * Wroking out a couple responsive issues, but needs more drastic code changes to fix other problems
    
    * merge/rebase touchups
    
    * lint
    
    * Updating some tests to get more passing
    
    * lint
    
    * reverting & commenting
    
    * update to newer function style

--- graylog2-web-interface/src/views/components/searchbar/date-time-picker/DisabledTimeRangeSelector.jsx
+++ graylog2-web-interface/src/views/components/searchbar/date-time-picker/DisabledTimeRangeSelector.jsx
@@ -3,19 +3,15 @@ import * as React from 'react';
 
 import Input from 'components/bootstrap/Input';
 
-import TimerangeSelector from './TimerangeSelector';
-
 const DisabledTimeRangeSelector = () => (
-  <TimerangeSelector style={{ marginLeft: 50 }}>
-    <Input id="no-override-timerange-selector"
-           type="select"
-           disabled
-           value="disabled"
-           title="There is no override for the timerange currently selected"
-           name="no-override">
-      <option value="disabled">No Override</option>
-    </Input>
-  </TimerangeSelector>
+  <Input id="no-override-timerange-selector"
+         type="select"
+         disabled
+         value="disabled"
+         title="There is no override for the timerange currently selected"
+         name="no-override">
+    <option value="disabled">No Override</option>
+  </Input>
 );
 
 export default DisabledTimeRangeSelector;
