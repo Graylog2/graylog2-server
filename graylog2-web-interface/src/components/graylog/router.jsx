2f6ea4996a33518a7fa8c6c4ab480f71d1490f70
Dennis Oelkers <dennis@graylog.com>
2020-10-29 11:53:21 +0100

    Cleaning up helpers a bit.

--- graylog2-web-interface/src/components/graylog/router.jsx
+++ graylog2-web-interface/src/components/graylog/router.jsx
@@ -1,6 +1,6 @@
 // @flow strict
 import * as React from 'react';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import type { LocationShape } from 'react-router';
 
@@ -10,27 +10,18 @@ export type HistoryElement = LocationShape;
 
 const _targetPathname = (to) => {
   const target = typeof to?.pathname === 'string' ? to.pathname : to;
-  const targetWithoutQuery = String(target).split(/[?#]/)[0];
 
-  return targetWithoutQuery;
+  return String(target).split(/[?#]/)[0];
 };
 
 const _setActiveClassName = (pathname, to, currentClassName, displayName) => {
-  const props = {};
   const targetPathname = _targetPathname(to);
   const isActive = targetPathname === pathname;
+  const isButton = displayName === 'Button';
 
-  if (displayName === 'Button' && isActive) {
-    let className = 'active';
-
-    if (currentClassName) {
-      className = `${className} ${currentClassName}`;
-    }
-
-    props.className = className;
-  }
-
-  return props;
+  return isButton && isActive
+    ? `active ${currentClassName ?? ''}`
+    : currentClassName;
 };
 
 type Props = {
@@ -42,7 +33,7 @@ type Props = {
 const LinkContainer = ({ children, onClick, to, ...rest }: Props) => {
   const { pathname } = useLocation();
   const { props: { onClick: childrenOnClick, className }, type: { displayName } } = React.Children.only(children);
-  const childrenClassName = _setActiveClassName(pathname, to, className, displayName);
+  const childrenClassName = useMemo(() => _setActiveClassName(pathname, to, className, displayName), [pathname, to, className, displayName]);
   const _onClick = useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
