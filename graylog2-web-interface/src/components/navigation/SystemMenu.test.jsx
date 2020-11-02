c1caa15062c00b1a3051c925f3c609c74174e826
Konrad Merz <konrad@graylog.com>
2020-11-02 09:35:03 +0100

    Fix system menu test

--- graylog2-web-interface/src/components/navigation/SystemMenu.test.jsx
+++ graylog2-web-interface/src/components/navigation/SystemMenu.test.jsx
@@ -63,7 +63,7 @@ describe('SystemMenu', () => {
     permissions                    | count | links
     ${[]}                          | ${2}  | ${['Overview', 'Nodes']}
     ${['clusterconfigentry:read']} | ${3}  | ${['Configurations']}
-    ${['inputs:read']}             | ${3}  | ${['Inputs']}
+    ${['inputs:read']}             | ${4}  | ${['Inputs', 'Grok Patterns']}
     ${['outputs:read']}            | ${3}  | ${['Outputs']}
     ${['indices:read']}            | ${3}  | ${['Indices']}
     ${['loggers:read']}            | ${3}  | ${['Logging']}
@@ -71,7 +71,7 @@ describe('SystemMenu', () => {
     ${['users:list']}              | ${3}  | ${['Users and Teams']}
     ${['roles:read']}              | ${3}  | ${['Roles']}
     ${['dashboards:create', 'inputs:create', 'streams:create']} | ${4}  | ${['Content Packs']}
-    ${['inputs:edit']}             | ${5}  | ${['Grok Patterns', 'Lookup Tables', 'Sidecars']}
+    ${['inputs:edit']}             | ${4}  | ${['Lookup Tables', 'Sidecars']}
     ${['inputs:create']}           | ${3}  | ${['Pipelines']}
   `('shows $links for user with $permissions permissions', verifyPermissions);
   });
