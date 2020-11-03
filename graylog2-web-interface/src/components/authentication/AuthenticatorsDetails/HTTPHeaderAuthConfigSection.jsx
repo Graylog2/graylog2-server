48e01efdd628aa6a50214d64dfcc62a9a9f18488
Linus Pahl <linus@graylog.com>
2020-11-03 10:12:20 +0100

    Create HTTP header auth config details component

--- graylog2-web-interface/src/components/authentication/AuthenticatorsDetails/HTTPHeaderAuthConfigSection.jsx
+++ graylog2-web-interface/src/components/authentication/AuthenticatorsDetails/HTTPHeaderAuthConfigSection.jsx
@@ -1,12 +1,33 @@
 // @flow strict
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
+import HTTPHeaderAuthConfigDomain from 'domainActions/authentication/HTTPHeaderAuthConfigDomain';
+import { Spinner, ReadOnlyFormGroup } from 'components/common';
 import SectionComponent from 'components/common/Section/SectionComponent';
 
-const HTTPHeaderAuthConfig = () => (
-  <SectionComponent title="Single Sign-On">
-    Details
-  </SectionComponent>
-);
+const HTTPHeaderAuthConfig = () => {
+  const [loadedConfig, setLoadedConfig] = useState();
+  const sectionTitle = 'Single Sign-On';
+
+  useEffect(() => {
+    HTTPHeaderAuthConfigDomain.load().then(setLoadedConfig);
+  }, []);
+
+  if (!loadedConfig) {
+    return (
+      <SectionComponent title={sectionTitle}>
+        <Spinner />
+      </SectionComponent>
+    );
+  }
+
+  return (
+    <SectionComponent title={sectionTitle}>
+      <ReadOnlyFormGroup label="Enabled" value={loadedConfig.enabled} />
+      <ReadOnlyFormGroup label="Username header" value={loadedConfig.usernameHeader} />
+    </SectionComponent>
+  );
+};
 
 export default HTTPHeaderAuthConfig;
