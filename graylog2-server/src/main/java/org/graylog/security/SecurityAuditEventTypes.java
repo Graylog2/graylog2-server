234725b8acb818d70f1f71104e474156ec153030
Bernd Ahlers <bernd@graylog.com>
2020-11-03 11:22:08 +0100

    Fix audit event types for auth service endpoints

--- graylog2-server/src/main/java/org/graylog/security/SecurityAuditEventTypes.java
+++ graylog2-server/src/main/java/org/graylog/security/SecurityAuditEventTypes.java
@@ -23,16 +23,17 @@ import java.util.Set;
 
 public class SecurityAuditEventTypes implements PluginAuditEventTypes {
     public static final String NAMESPACE = "security:";
-    public static final String NAMESPACE_AUTH_SERVICE = NAMESPACE + "auth-service:";
+    public static final String NAMESPACE_AUTH_SERVICE_GLOBAL_CONFIG = NAMESPACE + "auth_service_global_config:";
+    public static final String NAMESPACE_AUTH_SERVICE_BACKEND = NAMESPACE + "auth_service_backend:";
 
     public static final String SHARE_CREATE = NAMESPACE + "share:create";
     public static final String SHARE_UPDATE = NAMESPACE + "share:update";
     public static final String SHARE_DELETE = NAMESPACE + "share:delete";
 
-    public static final String AUTH_SERVICE_GLOBAL_CONFIG_UPDATE = NAMESPACE_AUTH_SERVICE + "global-config:update";
-    public static final String AUTH_SERVICE_BACKEND_CREATE = NAMESPACE_AUTH_SERVICE + "backend:create";
-    public static final String AUTH_SERVICE_BACKEND_DELETE = NAMESPACE_AUTH_SERVICE + "backend:delete";
-    public static final String AUTH_SERVICE_BACKEND_UPDATE = NAMESPACE_AUTH_SERVICE + "backend:update";
+    public static final String AUTH_SERVICE_GLOBAL_CONFIG_UPDATE = NAMESPACE_AUTH_SERVICE_GLOBAL_CONFIG + "update";
+    public static final String AUTH_SERVICE_BACKEND_CREATE = NAMESPACE_AUTH_SERVICE_BACKEND + "create";
+    public static final String AUTH_SERVICE_BACKEND_DELETE = NAMESPACE_AUTH_SERVICE_BACKEND + "delete";
+    public static final String AUTH_SERVICE_BACKEND_UPDATE = NAMESPACE_AUTH_SERVICE_BACKEND + "update";
 
     private static final ImmutableSet<String> EVENT_TYPES = ImmutableSet.of(
             SHARE_CREATE,
