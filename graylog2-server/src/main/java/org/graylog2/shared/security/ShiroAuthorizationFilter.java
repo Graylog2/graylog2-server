6fd27ec8640fac7405274e0506fd0df1289e4729
Marco Pfatschbacher <marco@graylog.com>
2020-11-03 11:27:26 +0100

    Missed a few places when converting username to userId (#9334)

--- graylog2-server/src/main/java/org/graylog2/shared/security/ShiroAuthorizationFilter.java
+++ graylog2-server/src/main/java/org/graylog2/shared/security/ShiroAuthorizationFilter.java
@@ -45,15 +45,15 @@ public class ShiroAuthorizationFilter implements ContainerRequestFilter {
         final SecurityContext securityContext = requestContext.getSecurityContext();
         if (securityContext instanceof ShiroSecurityContext) {
             final ShiroSecurityContext context = (ShiroSecurityContext) securityContext;
-            final String userName = RestTools.getUserNameFromRequest(requestContext);
+            final String userId = RestTools.getUserIdFromRequest(requestContext);
             final ContextAwarePermissionAnnotationHandler annotationHandler = new ContextAwarePermissionAnnotationHandler(context);
             final String[] requiredPermissions = annotation.value();
             try {
-                LOG.debug("Checking authorization for user [{}], needs permissions: {}", userName, requiredPermissions);
+                LOG.debug("Checking authorization for user [{}], needs permissions: {}", userId, requiredPermissions);
                 annotationHandler.assertAuthorized(annotation);
             } catch (AuthorizationException e) {
                 LOG.info("Not authorized. User <{}> is missing permissions {} to perform <{} {}>",
-                        userName, Arrays.toString(requiredPermissions), requestContext.getMethod(), requestContext.getUriInfo().getPath());
+                        userId, Arrays.toString(requiredPermissions), requestContext.getMethod(), requestContext.getUriInfo().getPath());
                 throw new ForbiddenException("Not authorized");
             }
         } else {
