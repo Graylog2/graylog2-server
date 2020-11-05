08b4522858044b375fe521560e3c67609f24731d
Kyle Knight <kyle@graylog.com>
2020-11-05 00:29:24 -0600

    Adding "Click to Close" to UserNotification (#9345)
    * Adding "Click to Close" to UserNotification
    
    * @Gaya feedback

--- graylog2-web-interface/src/util/UserNotification.js
+++ graylog2-web-interface/src/util/UserNotification.js
@@ -1,41 +1,34 @@
 import toastr from 'toastr';
 
+import './UserNotification.css';
+
+const defaultSettings = {
+  debug: false,
+  positionClass: 'toast-bottom-full-width',
+  onclick: null,
+  showDuration: 300,
+  hideDuration: 1000,
+  timeOut: 7000,
+  extendedTimeOut: 1000,
+  escapeHtml: true,
+  closeButton: true,
+  closeHtml: '<button>Close</button>',
+  progressBar: true,
+  preventDuplicates: true,
+};
+
 const UserNotification = {
   error(message, title) {
     toastr.error(message, title || 'Error', {
-      debug: false,
-      positionClass: 'toast-bottom-full-width',
-      onclick: null,
-      showDuration: 300,
-      hideDuration: 1000,
+      ...defaultSettings,
       timeOut: 10000,
-      extendedTimeOut: 1000,
-      escapeHtml: true,
     });
   },
   warning(message, title) {
-    toastr.warning(message, title || 'Attention', {
-      debug: false,
-      positionClass: 'toast-bottom-full-width',
-      onclick: null,
-      showDuration: 300,
-      hideDuration: 1000,
-      timeOut: 7000,
-      extendedTimeOut: 1000,
-      escapeHtml: true,
-    });
+    toastr.warning(message, title || 'Attention', defaultSettings);
   },
   success(message, title) {
-    toastr.success(message, title || 'Information', {
-      debug: false,
-      positionClass: 'toast-bottom-full-width',
-      onclick: null,
-      showDuration: 300,
-      hideDuration: 1000,
-      timeOut: 7000,
-      extendedTimeOut: 1000,
-      escapeHtml: true,
-    });
+    toastr.success(message, title || 'Information', defaultSettings);
   },
 };
 
