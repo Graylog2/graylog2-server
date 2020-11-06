a215424ab769c3bbda00a027fe85f684cb0c716b
Kyle Knight <kyle@graylog.com>
2020-11-06 14:55:57 -0600

    Consistently load the proper theme

--- graylog2-web-interface/src/theme/UseCurrentThemeMode.jsx
+++ graylog2-web-interface/src/theme/UseCurrentThemeMode.jsx
@@ -13,23 +13,25 @@ import usePrefersColorScheme from '../hooks/usePrefersColorScheme';
 const { CurrentUserStore } = CombinedProvider.get('CurrentUser');
 const { PreferencesStore } = CombinedProvider.get('Preferences');
 
-const useCurrentThemeMode = (overrideMode: ?string) => {
-  const browserPreference = usePrefersColorScheme();
+const useCurrentThemeMode = () => {
+  const browserThemePreference = usePrefersColorScheme();
+
   const { userIsReadOnly, username } = useStore(CurrentUserStore, (userStore) => ({
     username: userStore?.currentUser?.username,
     // eslint-disable-next-line camelcase
     userIsReadOnly: userStore?.currentUser?.read_only ?? true,
   }));
+
   const userPreferences = useContext(UserPreferencesContext);
-  const initialThemeMode = overrideMode ?? (userIsReadOnly ? Store.get(PREFERENCES_THEME_MODE) : userPreferences[PREFERENCES_THEME_MODE]) ?? browserPreference ?? DEFAULT_THEME_MODE;
+  const userThemePreference = userPreferences[PREFERENCES_THEME_MODE] ?? Store.get(PREFERENCES_THEME_MODE);
+  const initialThemeMode = userThemePreference ?? browserThemePreference ?? DEFAULT_THEME_MODE;
   const [currentThemeMode, setCurrentThemeMode] = useState<string>(initialThemeMode);
 
   const changeCurrentThemeMode = useCallback((newThemeMode: string) => {
     setCurrentThemeMode(newThemeMode);
+    Store.set(PREFERENCES_THEME_MODE, newThemeMode);
 
-    if (userIsReadOnly) {
-      Store.set(PREFERENCES_THEME_MODE, newThemeMode);
-    } else {
+    if (!userIsReadOnly) {
       const nextPreferences = { ...userPreferences, [PREFERENCES_THEME_MODE]: newThemeMode };
 
       PreferencesStore.saveUserPreferences(username, nextPreferences);
