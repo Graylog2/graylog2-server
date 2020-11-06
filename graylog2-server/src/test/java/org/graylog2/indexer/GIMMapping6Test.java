d4c4244dbe559a0e7cafe50b0da8b23e6150689e
Dennis Oelkers <dennis@graylog.com>
2020-11-06 15:27:58 +0100

    Improving test structure.

--- graylog2-server/src/test/java/org/graylog2/indexer/GIMMapping6Test.java
+++ graylog2-server/src/test/java/org/graylog2/indexer/GIMMapping6Test.java
@@ -16,21 +16,16 @@
  */
 package org.graylog2.indexer;
 
-import org.graylog2.indexer.indexset.IndexSetConfig;
-import org.junit.jupiter.api.Test;
-
-import java.util.Map;
-
-import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+import java.io.IOException;
 
 class GIMMapping6Test extends GIMMappingTest {
-    @Test
-    void matchesJsonSource() throws Exception {
-        final IndexMappingTemplate template = new GIMMapping6();
-        final IndexSetConfig indexSetConfig = mockIndexSetConfig();
+    @Override
+    String expectedGimTemplate() throws IOException {
+        return resource("expected_gim_template6.json");
+    }
 
-        final Map<String, Object> result = template.toTemplate(indexSetConfig, "myindex*", -2147483648);
-
-        assertEquals(resource("expected_gim_template6.json"), json(result), true);
+    @Override
+    IndexMappingTemplate createTemplate() {
+        return new GIMMapping6();
     }
 }
