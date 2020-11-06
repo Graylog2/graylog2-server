6bb1e67106a46ca2942b4296e6f09c4f767fc01e
Linus Pahl <linus@graylog.com>
2020-11-06 14:54:49 +0100

    Display explanation for share button why unsaved searches and dashboards can not be shared (#9383)
    * Display explanation for share button why unsaved searches and dashboards can not be shared
    
    * Update tests

--- graylog2-web-interface/src/components/common/ShareButton.jsx
+++ graylog2-web-interface/src/components/common/ShareButton.jsx
@@ -7,18 +7,22 @@ import HasOwnership from 'components/common/HasOwnership';
 import Icon from 'components/common/Icon';
 
 type Props = {
-  disabled?: boolean,
+  /**
+   * When a custom description is provided
+   * the button will be disabled
+   */
+  disabledInfo?: string,
   entityId: string,
   entityType: string,
   onClick: () => void,
   bsStyle?: string,
 };
 
-const ShareButton = ({ bsStyle, entityId, entityType, onClick, disabled }: Props) => (
+const ShareButton = ({ bsStyle, entityId, entityType, onClick, disabledInfo }: Props) => (
   <HasOwnership id={entityId} type={entityType}>
     {({ disabled: hasMissingPermissions }) => (
-      <Button bsStyle={bsStyle} onClick={onClick} disabled={disabled || hasMissingPermissions} title="Share">
-        <Icon name="user-plus" /> Share {(!disabled && hasMissingPermissions) && <SharingDisabledPopover type={entityType} />}
+      <Button bsStyle={bsStyle} onClick={onClick} disabled={!!disabledInfo || hasMissingPermissions} title="Share">
+        <Icon name="user-plus" /> Share {(!!disabledInfo || hasMissingPermissions) && <SharingDisabledPopover type={entityType} description={disabledInfo} />}
       </Button>
     )}
   </HasOwnership>
@@ -26,7 +30,7 @@ const ShareButton = ({ bsStyle, entityId, entityType, onClick, disabled }: Props
 
 ShareButton.defaultProps = {
   bsStyle: 'info',
-  disabled: false,
+  disabledInfo: undefined,
 };
 
 export default ShareButton;
