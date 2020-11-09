9c29a2599ad4133551ce7c793a68059a0e81a66f
Konrad Merz <konrad@graylog.com>
2020-11-09 13:24:52 +0100

    Add roles users to the context of the roles list response

---
+++ graylog2-web-interface/src/components/roles/RolesOverview/UsersCell.jsx
@@ -0,0 +1,14 @@
+// @flow strict
+import * as React from 'react';
+
+import type { UserContext } from 'logic/roles/Role';
+
+type Props: {
+  users: UserContext[],
+};
+
+const UsersCell = ({ users }: Props) => {
+
+};
+
+return UserCell;
