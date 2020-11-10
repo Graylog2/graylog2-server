Graylog2/graylog2-server

commit d5be0bd81e5c9f7fd8c87ba303dfc0b2f983bbdb

Per request authorization cache (#9287)
* Revert "Use ClusterEventBus instead"

This reverts commit 3a8c704cd30222e39d92ed66825fef5d8aeb3175.

* Revert "Clear the Shiro authorization cache if grants change"

This reverts commit ad4c283d4cfed5b6b834255df2b84ca7c6645d8a.

* Only cache authorization results within a request context

 - Introduce a per-request random ID by using a commonly known HTTP
   header: X-Request-Id
   This ID is either set by clients, load balancers, etc.
   If it's not found, we create our own.

 - The request headers were already accessible within Shiro's ThreadContext.
   But this is now refactored into a separate filter: ShiroRequestHeadersBinder

 - Finally add the request ID into the AuthorizationCacheKey

* Don't revert super. removal

* Extract ConcurrentUUID into a separate dual licensed class

graylog2-server/src/main/java/org/graylog2/security/realm/SessionAuthenticator.java
@@ -27,13 +27,13 @@ import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ThreadContext;
 import org.graylog2.plugin.database.users.User;
 import org.graylog2.shared.security.SessionIdToken;
-import org.graylog2.shared.security.ShiroSecurityContextFilter;
+import org.graylog2.shared.security.ShiroRequestHeadersBinder;
 import org.graylog2.shared.users.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.core.MultivaluedMap;
+import java.util.Optional;
 
 public class SessionAuthenticator extends AuthenticatingRealm {
     private static final Logger LOG = LoggerFactory.getLogger(SessionAuthenticator.class);
@@ -72,11 +72,8 @@ public class SessionAuthenticator extends AuthenticatingRealm {
             LOG.debug("Found session {} for userId {}", session.getId(), userId);
         }
 
-        @SuppressWarnings("unchecked")
-        final MultivaluedMap<String, String> requestHeaders = (MultivaluedMap<String, String>) ThreadContext.get(
-                ShiroSecurityContextFilter.REQUEST_HEADERS);
-        // extend session unless the relevant header was passed.
-        if (requestHeaders != null && !"true".equalsIgnoreCase(requestHeaders.getFirst(X_GRAYLOG_NO_SESSION_EXTENSION))) {
+        final Optional<String> noSessionExtension = ShiroRequestHeadersBinder.getHeaderFromThreadContext(X_GRAYLOG_NO_SESSION_EXTENSION);
+        if (noSessionExtension.isPresent() && !"true".equalsIgnoreCase(noSessionExtension.get())) {
             session.touch();
         } else {
             LOG.debug("Not extending session because the request indicated not to.");

graylog2-server/src/main/java/org/graylog/security/DBGrantService.java
@@ -21,18 +21,13 @@ import com.google.common.collect.ImmutableSet;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Updates;
-import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
-import org.bson.conversions.Bson;
-import org.bson.types.ObjectId;
 import org.graylog.grn.GRN;
 import org.graylog.grn.GRNRegistry;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.MongoConnection;
 import org.graylog2.database.PaginatedDbService;
-import org.graylog2.events.ClusterEventBus;
 import org.graylog2.plugin.database.users.User;
 import org.mongojack.DBQuery;
 
@@ -43,11 +38,9 @@ import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
@@ -57,18 +50,13 @@ public class DBGrantService extends PaginatedDbService<GrantDTO> {
     public static final String COLLECTION_NAME = "grants";
 
     private final GRNRegistry grnRegistry;
-    private final ClusterEventBus clusterEventBus;
-    private final MongoCollection<Document> dbCollection;
 
     @Inject
     public DBGrantService(MongoConnection mongoConnection,
                           MongoJackObjectMapperProvider mapper,
-                          GRNRegistry grnRegistry,
-                          ClusterEventBus clusterEventBus) {
+                          GRNRegistry grnRegistry) {
         super(mongoConnection, mapper, GrantDTO.class, COLLECTION_NAME);
         this.grnRegistry = grnRegistry;
-        this.clusterEventBus = clusterEventBus;
-        this.dbCollection = mongoConnection.getMongoDatabase().getCollection(COLLECTION_NAME);
 
         db.createIndex(new BasicDBObject(GrantDTO.FIELD_GRANTEE, 1));
         db.createIndex(new BasicDBObject(GrantDTO.FIELD_TARGET, 1));
@@ -97,20 +85,6 @@ public class DBGrantService extends PaginatedDbService<GrantDTO> {
                 Filters.eq(GrantDTO.FIELD_CAPABILITY, "grn::::capability:54e3deadbeefdeadbeef0002"),
                 Updates.set(GrantDTO.FIELD_CAPABILITY, Capability.OWN.toId())
         );
-    }
-
-    @Override
-    public GrantDTO save(GrantDTO grantDTO) {
-        final GrantDTO savedGrantDTO = super.save(grantDTO);
-        clusterEventBus.post(GrantChangedEvent.create(savedGrantDTO.id()));
-        return savedGrantDTO;
-    }
-
-    @Override
-    public int delete(String id) {
-        final int delete = super.delete(id);
-        clusterEventBus.post(GrantChangedEvent.create(id));
-        return delete;
     }
 
     public ImmutableSet<GrantDTO> getForGranteesOrGlobal(Set<GRN> grantees) {
@@ -211,32 +185,12 @@ public class DBGrantService extends PaginatedDbService<GrantDTO> {
         return db.find(DBQuery.is(GrantDTO.FIELD_TARGET, target.toString())).toArray();
     }
 
-    public long deleteForGrantee(GRN grantee) {
-        final Bson filter = Filters.eq(GrantDTO.FIELD_GRANTEE, grantee.toString());
-        return deleteForFilter(filter);
+    public int deleteForGrantee(GRN grantee) {
+        return db.remove(DBQuery.is(GrantDTO.FIELD_GRANTEE, grantee.toString())).getN();
     }
 
-    public long deleteForTarget(GRN target) {
-        final Bson filter = Filters.eq(GrantDTO.FIELD_TARGET, target.toString());
-        return deleteForFilter(filter);
-    }
-
-    private long deleteForFilter(Bson filter) {
-        final Set<String> deletedIds = StreamSupport.stream(dbCollection.find(filter).projection(Projections.include()).spliterator(), false)
-                .map(doc -> {
-                    final Object o = doc.get("_id");
-                    if (o instanceof ObjectId) {
-                        return ((ObjectId) o).toHexString();
-                    }
-                    return null;
-                }).filter(Objects::nonNull).collect(Collectors.toSet());
-        final DeleteResult deleteResult = dbCollection.deleteMany(filter);
-
-        final long deletedCount = deleteResult.getDeletedCount();
-        if (deletedCount > 0) {
-            clusterEventBus.post(GrantChangedEvent.create(deletedIds));
-        }
-        return deletedCount;
+    public int deleteForTarget(GRN target) {
+        return db.remove(DBQuery.is(GrantDTO.FIELD_TARGET, target.toString())).getN();
     }
 
     public List<GrantDTO> getForTargetExcludingGrantee(GRN target, GRN grantee) {

graylog2-server/src/main/java/org/graylog/security/GrantChangedEvent.java
@@ -1,45 +1,0 @@
-/**
- * This file is part of Graylog.
- *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
-package org.graylog.security;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.auto.value.AutoValue;
-import com.google.common.collect.ImmutableSet;
-import org.graylog.autovalue.WithBeanGetter;
-
-import java.util.Set;
-
-@JsonAutoDetect
-@AutoValue
-@WithBeanGetter
-public abstract class GrantChangedEvent {
-    private final static String FIELD_GRANT_IDS = "grant_ids";
-
-    @JsonProperty(FIELD_GRANT_IDS)
-    public abstract Set<String> grantIds();
-
-    @JsonCreator
-    public static GrantChangedEvent create(@JsonProperty(FIELD_GRANT_IDS) Set<String> grantIds) {
-        return new AutoValue_GrantChangedEvent(grantIds);
-    }
-
-    public static GrantChangedEvent create(String grantId) {
-        return create(ImmutableSet.of(grantId));
-    }
-}

graylog2-server/src/main/java/org/graylog/util/uuid/ConcurrentUUID.java
@@ -1,0 +1,70 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ *
+ * Taken from
+ * https://github.com/Netflix/netflix-commons/blob/v0.3.0/netflix-commons-util/src/main/java/com/netflix/util/concurrent/ConcurrentUUIDFactory.java
+ *
+ * Original License:
+ *
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.graylog.util.uuid;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ConcurrentUUID {
+    /**
+     * Generate a random UUID v4 that will perform reasonably when used by
+     * multiple threads under load.
+     *
+     * @see <a href="https://github.com/Netflix/netflix-commons/blob/v0.3.0/netflix-commons-util/src/main/java/com/netflix/util/concurrent/ConcurrentUUIDFactory.java">ConcurrentUUIDFactory</a>
+     * @return random UUID
+     */
+    public static UUID generateRandomUuid() {
+        final Random rnd = ThreadLocalRandom.current();
+        long mostSig  = rnd.nextLong();
+        long leastSig = rnd.nextLong();
+
+        // Identify this as a version 4 UUID, that is one based on a random value.
+        mostSig &= 0xffffffffffff0fffL;
+        mostSig |= 0x0000000000004000L;
+
+        // Set the variant identifier as specified for version 4 UUID values.  The two
+        // high order bits of the lower word are required to be one and zero, respectively.
+        leastSig &= 0x3fffffffffffffffL;
+        leastSig |= 0x8000000000000000L;
+
+        return new UUID(mostSig, leastSig);
+    }
+}

graylog2-server/src/main/java/org/graylog2/security/realm/MongoDbAuthorizationRealm.java
@@ -31,10 +31,10 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.graylog.grn.GRN;
 import org.graylog.grn.GRNRegistry;
 import org.graylog.grn.GRNTypes;
-import org.graylog.security.GrantChangedEvent;
 import org.graylog.security.PermissionAndRoleResolver;
 import org.graylog2.plugin.database.users.User;
 import org.graylog2.security.MongoDbAuthorizationCacheManager;
+import org.graylog2.shared.security.ShiroRequestHeadersBinder;
 import org.graylog2.shared.users.UserService;
 import org.graylog2.users.events.UserChangedEvent;
 import org.slf4j.Logger;
@@ -45,6 +45,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.graylog2.shared.rest.RequestIdFilter.X_REQUEST_ID;
 
 public class MongoDbAuthorizationRealm extends AuthorizingRealm {
 
@@ -66,6 +67,17 @@ public class MongoDbAuthorizationRealm extends AuthorizingRealm {
         setCachingEnabled(true);
         setCacheManager(mongoDbAuthorizationCacheManager);
         serverEventBus.register(this);
+    }
+
+    @Override
+    protected Object getAuthorizationCacheKey(PrincipalCollection principals) {
+        final Optional<String> requestId = ShiroRequestHeadersBinder.getHeaderFromThreadContext(X_REQUEST_ID);
+
+        if (requestId.isPresent()) {
+            return ImmutableSet.builder().addAll(principals).add(requestId.get()).build();
+        }
+        LOG.warn("Could not find X-Request-Id header. This is not supposed to happen.");
+        return principals.asSet();
     }
 
     @Override
@@ -142,11 +154,6 @@ public class MongoDbAuthorizationRealm extends AuthorizingRealm {
 
     @Subscribe
     public void handleUserSave(UserChangedEvent event) {
-        getAuthorizationCache().clear();
-    }
-
-    @Subscribe
-    public void handleGrantChange(GrantChangedEvent event) {
         getAuthorizationCache().clear();
     }
 }

graylog2-server/src/main/java/org/graylog2/shared/initializers/JerseyService.java
@@ -48,6 +48,7 @@ import org.graylog2.shared.rest.CORSFilter;
 import org.graylog2.shared.rest.NodeIdResponseFilter;
 import org.graylog2.shared.rest.NotAuthorizedResponseFilter;
 import org.graylog2.shared.rest.PrintModelProcessor;
+import org.graylog2.shared.rest.RequestIdFilter;
 import org.graylog2.shared.rest.RestAccessLogFilter;
 import org.graylog2.shared.rest.VerboseCsrfProtectionFilter;
 import org.graylog2.shared.rest.XHRFilter;
@@ -58,6 +59,7 @@ import org.graylog2.shared.rest.exceptionmappers.JsonMappingExceptionMapper;
 import org.graylog2.shared.rest.exceptionmappers.JsonProcessingExceptionMapper;
 import org.graylog2.shared.rest.exceptionmappers.MissingStreamPermissionExceptionMapper;
 import org.graylog2.shared.rest.exceptionmappers.WebApplicationExceptionMapper;
+import org.graylog2.shared.security.ShiroRequestHeadersBinder;
 import org.graylog2.shared.security.ShiroSecurityContextFilter;
 import org.graylog2.shared.security.tls.KeyStoreUtils;
 import org.graylog2.shared.security.tls.PemKeyStore;
@@ -248,6 +250,7 @@ public class JerseyService extends AbstractIdleService {
                 .register(new AuditEventModelProcessor(pluginAuditEventTypes))
                 .registerClasses(
                         ShiroSecurityContextFilter.class,
+                        ShiroRequestHeadersBinder.class,
                         VerboseCsrfProtectionFilter.class,
                         JacksonJaxbJsonProvider.class,
                         JsonProcessingExceptionMapper.class,
@@ -259,6 +262,7 @@ public class JerseyService extends AbstractIdleService {
                         BadRequestExceptionMapper.class,
                         RestAccessLogFilter.class,
                         NodeIdResponseFilter.class,
+                        RequestIdFilter.class,
                         XHRFilter.class,
                         NotAuthorizedResponseFilter.class,
                         WebAppNotFoundResponseFilter.class)

graylog2-server/src/main/java/org/graylog2/shared/rest/RequestIdFilter.java
@@ -1,0 +1,41 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.shared.rest;
+
+import com.google.common.base.Strings;
+import org.graylog.util.uuid.ConcurrentUUID;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import java.io.IOException;
+
+// Needs to run before ShiroAuthorizationFilter
+@Priority(Priorities.AUTHORIZATION - 20)
+public class RequestIdFilter implements ContainerRequestFilter {
+    public final static String X_REQUEST_ID = "X-Request-Id";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String id = requestContext.getHeaderString(X_REQUEST_ID);
+        if (Strings.isNullOrEmpty(id)) {
+            id = ConcurrentUUID.generateRandomUuid().toString();
+        }
+        requestContext.getHeaders().putSingle(X_REQUEST_ID, id);
+    }
+}

graylog2-server/src/main/java/org/graylog2/shared/security/ShiroRequestHeadersBinder.java
@@ -1,0 +1,56 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.shared.security;
+
+import joptsimple.internal.Strings;
+import org.apache.shiro.util.ThreadContext;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * This filter makes the request headers accessible within Shiro's {@link ThreadContext}.
+ */
+// Needs to run after RequestIdFilter
+@Priority(Priorities.AUTHORIZATION - 10)
+public class ShiroRequestHeadersBinder implements ContainerRequestFilter {
+    public static final String REQUEST_HEADERS = "REQUEST_HEADERS";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        final MultivaluedMap<String, String> headers = requestContext.getHeaders();
+        ThreadContext.put(REQUEST_HEADERS, headers);
+    }
+
+    public static Optional<String> getHeaderFromThreadContext(String headerName) {
+        @SuppressWarnings("unchecked")
+        final MultivaluedMap<String, String> requestHeaders =
+                (MultivaluedMap<String, String>) ThreadContext.get(REQUEST_HEADERS);
+        if (requestHeaders != null) {
+            final String header = requestHeaders.getFirst(headerName);
+            if (!Strings.isNullOrEmpty(header)) {
+                return Optional.of(header);
+            }
+        }
+        return Optional.empty();
+    }
+}

commit eff6441d6d746f35e606fe658c0250032cc96f65

Fix condition for session extension
The session wasn't "touched" if the X-Graylog-No-Session-Extension
header was missing.
(And that's what the frontend is doing)

Bug introduced with #9287

graylog2-server/src/main/java/org/graylog2/security/realm/SessionAuthenticator.java
@@ -73,10 +73,10 @@ public class SessionAuthenticator extends AuthenticatingRealm {
         }
 
         final Optional<String> noSessionExtension = ShiroRequestHeadersBinder.getHeaderFromThreadContext(X_GRAYLOG_NO_SESSION_EXTENSION);
-        if (noSessionExtension.isPresent() && !"true".equalsIgnoreCase(noSessionExtension.get())) {
-            session.touch();
-        } else {
+        if (noSessionExtension.isPresent() && "true".equalsIgnoreCase(noSessionExtension.get())) {
             LOG.debug("Not extending session because the request indicated not to.");
+        } else {
+            session.touch();
         }
         ThreadContext.bind(subject);
 