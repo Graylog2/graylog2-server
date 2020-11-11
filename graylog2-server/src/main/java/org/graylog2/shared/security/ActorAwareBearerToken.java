a96f4d5ab9a6aafcd667df71384cb280958a7e26
Othello Maurer <othello@graylog.com>
2020-11-11 13:29:28 +0100

    Add bearer token suitable for auditing

---
+++ graylog2-server/src/main/java/org/graylog2/shared/security/ActorAwareBearerToken.java
@@ -0,0 +1,39 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graylog2.shared.security;
+
+import org.apache.shiro.authc.BearerToken;
+import org.graylog2.audit.AuditActor;
+
+public class ActorAwareBearerToken extends BearerToken implements ActorAwareAuthenticationToken {
+    private final AuditActor actor;
+
+    public ActorAwareBearerToken(String token, AuditActor actor) {
+        super(token);
+        this.actor = actor;
+    }
+
+    public ActorAwareBearerToken(String token, String host, AuditActor actor) {
+        super(token, host);
+        this.actor = actor;
+    }
+
+    @Override
+    public AuditActor getActor() {
+         return actor;
+    }
+}
