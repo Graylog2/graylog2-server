25736469b2bf8e7c7b4d05ee61ef5fc602387f1f
Konrad Merz <konrad@graylog.com>
2020-11-11 15:00:58 +0100

    Add reset startpage button to profile section of user edit
    ## Motivation
    Prior to this change, there was no way to reset a startpage for a user.
    
    ## Description
    This change will add the most minimal solution by adding a Reset
    Startpage button to the header of the profile section on the user edit page.
    
    ## Also
    Fix setting the Set as Startpage menuitem for streams.

--- graylog2-web-interface/src/components/streams/StreamControls.jsx
+++ graylog2-web-interface/src/components/streams/StreamControls.jsx
@@ -60,7 +60,7 @@ const StreamControls = createReactClass({
 
   _setStartpage() {
     const { user, stream } = this.props;
-    StartpageStore.set(user.username, 'stream', stream.id);
+    StartpageStore.set(user.id, 'stream', stream.id);
   },
 
   render() {
--- graylog2-web-interface/src/components/users/UserEdit/ProfileSection.jsx
+++ graylog2-web-interface/src/components/users/UserEdit/ProfileSection.jsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { Formik, Form } from 'formik';
 
+import StoreProvider from 'injection/StoreProvider';
 import { Button, Col, Row } from 'components/graylog';
 import { ReadOnlyFormGroup } from 'components/common';
 import User from 'logic/users/User';
@@ -9,6 +10,8 @@ import SectionComponent from 'components/common/Section/SectionComponent';
 
 import FullNameFormGroup from '../UserCreate/FullNameFormGroup';
 import EmailFormGroup from '../UserCreate/EmailFormGroup';
+
+const StartpageStore = StoreProvider.getStore('Startpage');
 
 type Props = {
   user: User,
@@ -28,8 +31,25 @@ const ProfileSection = ({
     email,
   } = user;
 
+  const _resetStartpage = () => {
+    // eslint-disable-next-line no-alert
+    if (window.confirm(`You are about to reset the startpage from user ${user.fullName}. Please confirm.`)) {
+      StartpageStore.set(user.id);
+    }
+  };
+
+  const isStartpageSet = !!user.startpage?.type;
+  const resetTitle = isStartpageSet ? 'Reset startpage' : 'No startpage set';
+  const resetButton = (
+    <Button title={resetTitle}
+            disabled={!isStartpageSet}
+            onClick={_resetStartpage}>
+      Reset Startpage
+    </Button>
+  );
+
   return (
-    <SectionComponent title="Profile">
+    <SectionComponent title="Profile" headerActions={resetButton}>
       <Formik onSubmit={onSubmit}
               initialValues={{ email, full_name: fullName }}>
         {({ isSubmitting, isValid }) => (
