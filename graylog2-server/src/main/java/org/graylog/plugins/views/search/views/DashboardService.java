266e16ff3c74e1fb97bf349e63986333f7ea3b62
Dennis Oelkers <dennis@graylog.com>
2020-11-11 13:15:28 +0100

    Fix cluster stats endpoints. (4.0) (#9441)
    * Fixing request endpoint for ES7.
    
    * Avoid using `null` sort field when retrieving dashboard count.
    
    * Removing unused dependency.
    
    * Using previous endpoint with mandatory wildcard.
    
    * Marking cluster stats endpoint as deprecated.
    
    Fixes #9435

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/views/DashboardService.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/views/DashboardService.java
@@ -30,7 +30,7 @@ public class DashboardService {
     }
 
     public long count() {
-        final PaginatedList<ViewDTO> result = viewService.searchPaginatedByType(ViewDTO.Type.DASHBOARD, new SearchQuery(""), dashboard -> true, "ASC", null, 1, 0);
+        final PaginatedList<ViewDTO> result = viewService.searchPaginatedByType(ViewDTO.Type.DASHBOARD, new SearchQuery(""), dashboard -> true, "ASC", ViewDTO.FIELD_ID, 1, 0);
         return result.grandTotal().orElseThrow(() -> new IllegalStateException("Missing grand total in response when counting dashboards!"));
     }
 }
