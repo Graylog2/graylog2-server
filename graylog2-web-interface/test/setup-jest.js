cf5db3fda070b9b0be500d1f9072524ba1ea4669
Dennis Oelkers <dennis@graylog.com>
2020-11-12 16:06:07 +0100

    Removing unneeded jquery setup.

--- graylog2-web-interface/test/setup-jest.js
+++ graylog2-web-interface/test/setup-jest.js
@@ -16,14 +16,9 @@
  */
 /// <reference types="jest-enzyme" />
 
-import jQuery from 'jquery';
 import sizeMe from 'react-sizeme';
 
 import registerBuiltinStores from 'injection/registerBuiltinStores';
-
-global.$ = jQuery;
-
-global.jQuery = jQuery;
 
 sizeMe.noPlaceholders = true;
 
