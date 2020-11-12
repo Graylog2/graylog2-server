da5154e0007e0ab3296259030ed60b87397dca9e
Dennis Oelkers <dennis@graylog.com>
2020-11-12 13:16:44 +0100

    Migrating `UseViewLoader`.

--- graylog2-web-interface/src/views/logic/views/UseViewLoader.ts
+++ graylog2-web-interface/src/views/logic/views/UseViewLoader.ts
@@ -15,13 +15,14 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 // @flow strict
+import * as React from 'react';
 import { useEffect, useState } from 'react';
 
-import type { ViewLoaderFn } from 'views/logic/views/ViewLoader';
-import type { ViewHook } from 'views/logic/hooks/ViewHook';
+import { ViewLoaderFn } from 'views/logic/views/ViewLoader';
+import { ViewHook } from 'views/logic/hooks/ViewHook';
 import usePluginEntities from 'views/logic/usePluginEntities';
 
-const useViewLoader = (viewId: string, query: { [string]: any }, viewLoader: ViewLoaderFn) => {
+const useViewLoader = (viewId: string, query: { [key: string]: any }, viewLoader: ViewLoaderFn): [boolean, React.ElementType | undefined] => {
   const loadingViewHooks: Array<ViewHook> = usePluginEntities('views.hooks.loadingView');
   const executingViewHooks: Array<ViewHook> = usePluginEntities('views.hooks.executingView');
 
@@ -32,28 +33,20 @@ const useViewLoader = (viewId: string, query: { [string]: any }, viewLoader: Vie
     setLoaded(false);
     setHookComponent(undefined);
 
-    viewLoader(
-      viewId,
-      loadingViewHooks,
-      executingViewHooks,
-      query,
-      () => {
-        setHookComponent(undefined);
-      },
-      (e) => {
-        if (e instanceof Error) {
-          throw e;
-        }
+    viewLoader(viewId, loadingViewHooks, executingViewHooks, query, () => {
+      setHookComponent(undefined);
+    }, (e) => {
+      if (e instanceof Error) {
+        throw e;
+      }
 
-        setHookComponent(e);
-      },
-    ).then((results) => {
+      setHookComponent(e);
+    }).then((results) => {
       setLoaded(true);
 
       return results;
     }).catch((e) => e);
-  },
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, // eslint-disable-next-line react-hooks/exhaustive-deps
   [executingViewHooks, loadingViewHooks, viewId, viewLoader]);
 
   return [loaded, hookComponent];
