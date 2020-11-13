12cee9475184cdbf938bff97e081ac273b723575
Kyle Knight <kyle@graylog.com>
2020-11-13 07:30:05 -0600

    started rewrite of Relative form fields

--- graylog2-web-interface/src/views/components/searchbar/date-time-picker/RelativeTimeRangeSelector.jsx
+++ graylog2-web-interface/src/views/components/searchbar/date-time-picker/RelativeTimeRangeSelector.jsx
@@ -1,9 +1,8 @@
 // @flow strict
 import * as React from 'react';
-import { useEffect, useMemo, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { useField } from 'formik';
+import { Field } from 'formik';
 import styled, { css, type StyledComponent } from 'styled-components';
 
 import { type ThemeInterface } from 'theme';
@@ -18,23 +17,24 @@ type Props = {
   originalTimeRange: {
     range: string | number,
   },
+  limitDuration: number,
 };
 
-const RANGE_VALUES = [
+const RANGE_TYPES = [
   {
-    value: 'seconds',
+    type: 'seconds',
     label: 'Seconds',
   }, {
-    value: 'minutes',
+    type: 'minutes',
     label: 'Minutes',
   }, {
-    value: 'hours',
+    type: 'hours',
     label: 'Hours',
   }, {
-    value: 'days',
+    type: 'days',
     label: 'Days',
   }, {
-    value: 'weeks',
+    type: 'weeks',
     label: 'Weeks',
   },
 ];
@@ -104,26 +104,6 @@ const RangeCheck: StyledComponent<{}, ThemeInterface, HTMLLabelElement> = styled
   }
 `);
 
-const RangeLimitNotice: StyledComponent<{}, ThemeInterface, HTMLSpanElement> = styled.span(({ theme }) => css`
-  font-style: italic;
-  font-size: ${theme.fonts.size.small};
-  color: ${theme.colors.variant.darker.warning};
-  grid-area: 1 / 4 / 2 / 8;
-  align-self: self-end;
-  margin-bottom: 5px;
-  display: flex;
-  align-items: center;
-  
-  > span {
-    flex: 1;
-    line-height: 1.1em
-  }
-  
-  > svg {
-    margin-right: 9px;
-  }
-`);
-
 const initialRangeType = ({ range, ...restRange }) => {
   if (range === 0) {
     return {
@@ -135,14 +115,14 @@ const initialRangeType = ({ range, ...restRange }) => {
     };
   }
 
-  return RANGE_VALUES.map(({ value }) => {
-    const diff = moment.duration(range, 'seconds').as(value);
+  return RANGE_TYPES.map(({ type }) => {
+    const diff = moment.duration(range, 'seconds').as(type);
 
     if (diff - Math.floor(diff) === 0) {
       return {
         ...restRange,
         rangeValue: diff || 1,
-        rangeType: value || 'seconds',
+        rangeType: type || 'seconds',
         rangeAllTime: false,
         range,
       };
@@ -152,116 +132,98 @@ const initialRangeType = ({ range, ...restRange }) => {
   }).filter(Boolean).pop();
 };
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'rangeValue':
-      return {
-        ...state,
-        rangeValue: action.value,
-        range: moment.duration(action.value, state.rangeType).asSeconds(),
-      };
-    case 'rangeType':
-      return {
-        ...state,
-        rangeType: action.value,
-        range: moment.duration(state.rangeValue, action.value).asSeconds(),
-      };
-    case 'rangeAllTime':
-      return {
-        ...state,
-        rangeAllTime: action.value,
-        range: action.value ? 0 : moment.duration(state.rangeValue, state.rangeType).asSeconds(),
-      };
-    default:
-      throw new Error();
-  }
-};
-
-const buildRangeTypes = (config) => RANGE_VALUES.map(({ label, value }) => {
-  const typeDuration = moment.duration(1, value).asSeconds();
-  const limitDuration = moment.duration(config.query_time_range_limit).asSeconds();
+const buildRangeTypes = (limitDuration) => RANGE_TYPES.map(({ label, type }) => {
+  const typeDuration = moment.duration(1, type).asSeconds();
 
   if (limitDuration === 0 || typeDuration <= limitDuration) {
-    return { label, value };
+    return { label, type };
   }
 
   return null;
 }).filter(Boolean);
 
-const RelativeTimeRangeSelector = ({ config, disabled, originalTimeRange }: Props) => {
-  const [nextRangeProps, , nextRangeHelpers] = useField('tempTimeRange');
+const RelativeTimeRangeSelector = ({ disabled, originalTimeRange, limitDuration }: Props) => {
+  const availableRangeTypes = buildRangeTypes(limitDuration);
 
-  const [state, dispatch] = useReducer(reducer, {
-    range: originalTimeRange.range,
-    rangeAllTime: originalTimeRange.range === 0,
-  }, initialRangeType);
-
-  const typeDurationSeconds = useMemo(() => moment.duration(state.rangeValue, state.rangeType).asSeconds(), [state.rangeValue, state.rangeType]);
-  const limitDuration = useMemo(() => moment.duration(config.query_time_range_limit).asSeconds(), [config.query_time_range_limit]);
-
-  const availableRangeTypes = buildRangeTypes(config);
-
-  useEffect(() => {
-    if (limitDuration === 0 || (typeDurationSeconds <= limitDuration && limitDuration !== 0)) {
-      nextRangeHelpers.setValue({ ...nextRangeProps.value, ...state });
-    } else {
-      // eslint-disable-next-line no-console
-      console.log('Nuh uh uh - you went over the limit!');
-    }
-  }, [limitDuration, state, typeDurationSeconds]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  const handleAllTime = (event) => dispatch({
-    type: 'rangeAllTime',
-    value: event.target.checked,
-    rangeValue: event.target.checked ? state.rangeValue : initialRangeType(originalTimeRange),
-  });
+  const _isValidRange = (value) => !(limitDuration === 0 || (value <= limitDuration && limitDuration !== 0));
 
   return (
     <RelativeWrapper>
-      <RangeWrapper>
-        <RangeTitle>From:</RangeTitle>
-        <RangeCheck htmlFor="relative-all-time" className={limitDuration !== 0 && 'shortened'}>
-          <input type="checkbox"
-                 id="relative-all-time"
-                 value="0"
-                 checked={state.rangeAllTime}
-                 onChange={handleAllTime}
-                 disabled={limitDuration !== 0} />All Time
-        </RangeCheck>
-        {limitDuration !== 0
-          && (
-            <RangeLimitNotice>
-              <Icon name="exclamation-triangle" />
-              <span>Admin has limited searching to {moment.duration(-limitDuration, 'seconds').humanize(true)}</span>
-            </RangeLimitNotice>
-          )}
-        <InputWrap>
-          <Input id="relative-timerange-from-value"
-                 disabled={disabled || state.rangeAllTime}
-                 type="number"
-                 value={state.rangeValue}
-                 min="1"
-                 title="Set the range value"
-                 name="relative-timerange-from-value"
-                 onChange={(event) => dispatch({
-                   type: 'rangeValue',
-                   value: event.target.value,
-                 })} />
-        </InputWrap>
-        <StyledSelect id="relative-timerange-from-length"
-                      disabled={disabled || state.rangeAllTime}
-                      value={state.rangeType}
-                      options={availableRangeTypes}
-                      placeholder="Select a range type"
-                      name="relative-timerange-from-length"
-                      onChange={(event) => dispatch({
-                        type: 'rangeType',
-                        value: event,
-                      })}
-                      clearable={false} />
+      <Field name="tempTimeRange.range" validate={_isValidRange}>
+        {({ field: { value, onChange, name }, meta }) => {
+          const fromValue = RANGE_TYPES.map(({ type }) => {
+            const diff = moment.duration(value, 'seconds').as(type);
 
-        <Ago />
-      </RangeWrapper>
+            if (diff - Math.floor(diff) === 0) {
+              return {
+                ...originalTimeRange,
+                rangeValue: diff || 1,
+                rangeType: type || 'seconds',
+                rangeAllTime: false,
+                range: value,
+              };
+            }
+
+            return null;
+          }).filter(Boolean).pop();
+
+          const _onChangeTime = (event) => {
+            const newTimeValue = moment.duration(event.target.value, fromValue.rangeType).asSeconds();
+
+            onChange({ target: { name, value: newTimeValue } });
+          };
+
+          const _onChangeType = (type) => {
+            const newTimeValue = moment.duration(fromValue.rangeType, type).asSeconds();
+
+            onChange({ target: { name, value: newTimeValue } });
+          };
+
+          const _onCheckAllTime = (event) => {
+            console.log('_onCheckAllTime', {
+              type: 'rangeAllTime',
+              value: event.target.checked,
+              rangeValue: event.target.checked ? fromValue.rangeValue : initialRangeType(originalTimeRange),
+            });
+
+            onChange({ target: { name, value: event.target.checked ? 0 : originalTimeRange.range } });
+          };
+
+          return (
+            <RangeWrapper>
+              <RangeTitle>From:</RangeTitle>
+              <RangeCheck htmlFor="relative-all-time" className={limitDuration !== 0 && 'shortened'}>
+                <input type="checkbox"
+                       id="relative-all-time"
+                       value="0"
+                       checked={fromValue.rangeAllTime}
+                       onChange={_onCheckAllTime}
+                       disabled={limitDuration !== 0} />All Time
+              </RangeCheck>
+              <InputWrap>
+                <Input id="relative-timerange-from-value"
+                       name="relative-timerange-from-value"
+                       disabled={disabled || fromValue.rangeAllTime}
+                       type="number"
+                       value={fromValue.rangeValue}
+                       title="Set the range value"
+                       onChange={_onChangeTime}
+                       bsStyle={meta.error ? 'error' : null} />
+              </InputWrap>
+              <StyledSelect id="relative-timerange-from-length"
+                            name="relative-timerange-from-length"
+                            disabled={disabled || fromValue.rangeAllTime}
+                            value={fromValue.rangeType}
+                            options={availableRangeTypes}
+                            placeholder="Select a range type"
+                            onChange={_onChangeType}
+                            clearable={false} />
+
+              <Ago />
+            </RangeWrapper>
+          );
+        }}
+      </Field>
 
       <StyledIcon name="arrow-right" />
 
@@ -284,7 +246,7 @@ const RelativeTimeRangeSelector = ({ config, disabled, originalTimeRange }: Prop
 
         <StyledSelect id="relative-timerange-until-length"
                       disabled
-                      value={RANGE_VALUES[0].value}
+                      value={RANGE_TYPES[0].type}
                       options={availableRangeTypes}
                       placeholder="Select an offset"
                       name="relative-timerange-until-length"
