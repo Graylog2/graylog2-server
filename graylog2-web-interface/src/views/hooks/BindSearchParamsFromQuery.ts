e06d3446267cf071ad920317ca5297b55c50fa8e
Dennis Oelkers <dennis@graylog.com>
2020-11-16 17:06:50 +0100

    Migrating `BindSearchParamsFromQuery`.

--- graylog2-web-interface/src/views/hooks/BindSearchParamsFromQuery.ts
+++ graylog2-web-interface/src/views/hooks/BindSearchParamsFromQuery.ts
@@ -18,32 +18,64 @@
 import isDeepEqual from 'stores/isDeepEqual';
 import { DEFAULT_RANGE_TYPE } from 'views/Constants';
 import { QueriesActions } from 'views/stores/QueriesStore';
-import type { ViewHook } from 'views/logic/hooks/ViewHook';
+import { ViewHook } from 'views/logic/hooks/ViewHook';
 import View from 'views/logic/views/View';
-import { createElasticsearchQueryString, filtersForQuery } from 'views/logic/queries/Query';
+import {
+  AbsoluteTimeRange,
+  createElasticsearchQueryString,
+  filtersForQuery, KeywordTimeRange,
+  RelativeTimeRange,
+} from 'views/logic/queries/Query';
+
+type RawRelativeRange = {
+  rangetype: 'relative';
+  relative?: string;
+};
+
+type RawAbsoluteRange = {
+  rangetype: 'absolute';
+  from?: string;
+  to?: string;
+};
+
+type RawKeywordRange = {
+  rangetype: 'keyword';
+  keyword?: string;
+};
+
+const _getRange = (query): RawAbsoluteRange | RawRelativeRange | RawKeywordRange => {
+  const rangetype = query.rangetype || DEFAULT_RANGE_TYPE;
+
+  return { ...query, rangetype };
+};
 
 const _getTimerange = (query = {}) => {
-  const type = query.rangetype || DEFAULT_RANGE_TYPE;
+  const range = _getRange(query);
 
-  switch (type) {
+  switch (range.rangetype) {
     case 'relative':
-      return query.relative ? { type, range: parseInt(query.relative, 10) } : undefined;
+      return range.relative ? { type: range.rangetype, range: parseInt(range.relative, 10) } as RelativeTimeRange : undefined;
     case 'absolute':
-      return (query.from || query.to)
+      return (range.from || range.to)
         ? {
-          type: type,
-          from: query.from,
-          to: query.to,
-        }
+          type: range.rangetype,
+          from: range.from,
+          to: range.to,
+        } as AbsoluteTimeRange
         : undefined;
     case 'keyword':
-      return query.keyword ? { type, keyword: query.keyword } : undefined;
+      return range.keyword ? { type: range.rangetype, keyword: range.keyword } as KeywordTimeRange : undefined;
     default:
-      throw new Error(`Unsupported range type ${type}`);
+      // @ts-ignore
+      throw new Error(`Unsupported range type ${range.rangetype}`);
   }
 };
 
-const _getStreams = (query = {}): Array<string> => {
+type StreamsQuery = {
+  streams?: string;
+};
+
+const _getStreams = (query: StreamsQuery = {}): Array<string> => {
   const rawStreams = query.streams;
 
   if (rawStreams === undefined || rawStreams === null) {
@@ -52,8 +84,10 @@ const _getStreams = (query = {}): Array<string> => {
 
   return String(rawStreams).split(',')
     .map((stream) => stream.trim())
-    .filter((stream) => (stream !== ''));
+    .filter((stream) => stream !== '');
 };
+
+type RawQuery = (RawAbsoluteRange | RawRelativeRange | RawKeywordRange) & StreamsQuery;
 
 const bindSearchParamsFromQuery: ViewHook = ({ query, view }) => {
   if (view.type !== View.Type.Search) {
@@ -61,7 +95,7 @@ const bindSearchParamsFromQuery: ViewHook = ({ query, view }) => {
   }
 
   const { q: queryString } = query;
-  const timerange = _getTimerange(query);
+  const timerange = _getTimerange(query as RawQuery);
   const streams = filtersForQuery(_getStreams(query));
 
   if (!queryString && !timerange && !streams) {
