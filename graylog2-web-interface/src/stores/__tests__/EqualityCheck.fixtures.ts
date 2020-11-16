39fca70a64251b7b0fc607112916d64145a53776
Dennis Oelkers <dennis@graylog.com>
2020-11-16 15:45:16 +0100

    Fixing indexer signatures.

--- graylog2-web-interface/src/stores/__tests__/EqualityCheck.fixtures.ts
+++ graylog2-web-interface/src/stores/__tests__/EqualityCheck.fixtures.ts
@@ -17,11 +17,11 @@
 // @flow strict
 import { List, Map } from 'immutable';
 
-type MixedMapsAndObjects = { [string]: Map<string, { [string]: Map<string, number> }> };
+type MixedMapsAndObjects = { [key: string]: Map<string, { [key: string]: Map<string, number> }> };
 
-export const mapWithObject = (): Map<string, { [string]: number }> => Map({ foo: { bar: 42 } });
-export const listWithObject = (): List<{ [string]: { [string]: number } }> => List([{ foo: { bar: 42 } }]);
-export const objectWithMap = (): { [string]: Map<string, number> } => ({ foo: Map({ bar: 42 }) });
+export const mapWithObject = (): Map<string, { [key: string]: number }> => Map({ foo: { bar: 42 } });
+export const listWithObject = (): List<{ [key: string]: { [key: string]: number } }> => List([{ foo: { bar: 42 } }]);
+export const objectWithMap = (): { [key: string]: Map<string, number> } => ({ foo: Map({ bar: 42 }) });
 export const arrayOfMaps = (): Array<Map<string, number>> => [Map({ foo: 23 }), Map({ bar: 42 })];
 export const mixedMapsAndObjects = (): MixedMapsAndObjects => ({ foo: Map({ bar: { baz: Map({ qux: 42 }) } }) });
 
