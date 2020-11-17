c014777148c4a07712195af836c34f29bebd73e6
Dennis Oelkers <dennis@graylog.com>
2020-11-17 13:47:37 +0100

    Migrating `PivotHandler`.

--- graylog2-web-interface/src/views/logic/searchtypes/pivot/PivotHandler.ts
+++ graylog2-web-interface/src/views/logic/searchtypes/pivot/PivotHandler.ts
@@ -16,40 +16,31 @@
  */
 // @flow strict
 
-export type Key = *;
-export type Value = *;
+export type Key = any;
+export type Value = any;
 
-type Keyed = {|
+type Keyed = {
   key: Array<Key>,
-|};
+};
 
-type SingleValue = {|
+type SingleValue = {
   rollup: boolean,
   value: Value,
-|};
+};
 
-type MultiValue = {|
-  // eslint-disable-next-line no-use-before-define
+type MultiValue = {
   values: Array<Row>,
-|};
+};
 
-export type Leaf = {|
-...{| source: 'leaf' |}, ...Keyed, ...MultiValue,
-|};
+export type Leaf = { source: 'leaf' } & Keyed & MultiValue;
 
-export type NonLeaf = {|
-...{| source: 'non-leaf' |}, ...Keyed, ...MultiValue,
-|};
+export type NonLeaf = { source: 'non-leaf' } & Keyed & MultiValue;
 
-export type ColLeaf = {|
-...{| source: 'col-leaf' |}, ...Keyed, ...SingleValue,
-|};
-export type RowLeaf = {|
-...{| source: 'row-leaf' |}, ...Keyed, ...SingleValue,
-|};
-export type RowInner = {|
-...{| source: 'row-inner' |}, ...Keyed, ...SingleValue,
-|};
+export type ColLeaf = { source: 'col-leaf' } & Keyed & SingleValue;
+
+export type RowLeaf = { source: 'row-leaf' } & Keyed & SingleValue;
+
+export type RowInner = { source: 'row-inner' } & Keyed & SingleValue;
 
 export type Row = Leaf | NonLeaf | ColLeaf | RowLeaf | RowInner;
 export type ResultId = string;
