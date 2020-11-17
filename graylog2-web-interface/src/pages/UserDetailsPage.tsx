2086abf06fa1dc13b45a9b08d3138a5aa51f6945
Konrad Merz <konrad@graylog.com>
2020-11-17 13:24:01 +0100

    Convert UserDetailsPage.tsx to tsx

--- graylog2-web-interface/src/pages/UserDetailsPage.tsx
+++ graylog2-web-interface/src/pages/UserDetailsPage.tsx
@@ -33,7 +33,7 @@ type Props = {
   },
 };
 
-const PageTitle = ({ fullName }: {fullName: ?string}) => (
+const PageTitle = ({ fullName }: {fullName: string}) => (
   <>
     User Details {fullName && (
       <>
