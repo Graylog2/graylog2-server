478ad73a5b457a35025452a1a2a87376900678e1
Dennis Oelkers <dennis@graylog.com>
2020-11-17 12:14:10 +0100

    Migrating `UseCreatedSavedSearch`.

--- graylog2-web-interface/src/views/logic/views/UseCreateSavedSearch.ts
+++ graylog2-web-interface/src/views/logic/views/UseCreateSavedSearch.ts
@@ -20,7 +20,7 @@ import { useMemo } from 'react';
 import { ViewActions } from 'views/stores/ViewStore';
 import View from 'views/logic/views/View';
 
-const useCreateSavedSearch = (streamId: ?string) => useMemo(() => ViewActions.create(View.Type.Search, streamId)
+const useCreateSavedSearch = (streamId?: string) => useMemo(() => ViewActions.create(View.Type.Search, streamId)
   .then(({ view: v }) => v), [streamId]);
 
 export default useCreateSavedSearch;
