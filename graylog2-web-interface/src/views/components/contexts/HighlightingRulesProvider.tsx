4cc45502eef85de69c8ab926e71984e6cce0ce33
Dennis Oelkers <dennis@graylog.com>
2020-11-18 10:34:53 +0100

    Migrating `HighlightingRulesProvider`.

--- graylog2-web-interface/src/views/components/contexts/HighlightingRulesProvider.tsx
+++ graylog2-web-interface/src/views/components/contexts/HighlightingRulesProvider.tsx
@@ -23,7 +23,7 @@ import { HighlightingRulesStore } from 'views/stores/HighlightingRulesStore';
 
 import HighlightingRulesContext from './HighlightingRulesContext';
 
-const HighlightingRulesProvider = ({ children }: { children: React.Node }) => {
+const HighlightingRulesProvider = ({ children }: { children: React.ReactNode }) => {
   const highlightingRules = useStore(HighlightingRulesStore);
 
   return highlightingRules
