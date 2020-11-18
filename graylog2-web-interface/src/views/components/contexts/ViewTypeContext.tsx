7e3a6fee7f139b23b909a7a59c24c89a81c1f47d
Dennis Oelkers <dennis@graylog.com>
2020-11-18 10:33:30 +0100

    Migrating `ViewTypeContext`.

--- graylog2-web-interface/src/views/components/contexts/ViewTypeContext.tsx
+++ graylog2-web-interface/src/views/components/contexts/ViewTypeContext.tsx
@@ -19,6 +19,6 @@ import * as React from 'react';
 
 import type { ViewType } from 'views/logic/views/View';
 
-const ViewTypeContext = React.createContext<?ViewType>();
+const ViewTypeContext = React.createContext<ViewType | undefined>(undefined);
 
 export default ViewTypeContext;
