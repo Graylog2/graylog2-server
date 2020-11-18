f7212cc5abb2cb80263806223fa3bd8e396d30c3
Konrad Merz <konrad@graylog.com>
2020-11-18 12:14:25 +0100

    Convert CountBadge.tsx to tsx

--- graylog2-web-interface/src/components/common/CountBadge.tsx
+++ graylog2-web-interface/src/components/common/CountBadge.tsx
@@ -16,16 +16,16 @@
  */
 // @flow strict
 import * as React from 'react';
-import styled, { type StyledComponent } from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 
 import type { ThemeInterface } from 'theme';
 
-const Badge: StyledComponent<{}, ThemeInterface, HTMLSpanElement> = styled.span(({ theme }) => `
+const Badge: StyledComponent<undefined, ThemeInterface, HTMLSpanElement> = styled.span(({ theme }) => `
   background-color: ${theme.colors.variant.light.info};
 `);
 
 type Props = {
-  children: React.Node;
+  children: JSX.Element | string | number;
 };
 
 const CountBadge = ({ children }: Props) => (
