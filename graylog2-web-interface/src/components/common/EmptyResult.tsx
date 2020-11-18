603b7ddf7527865ba4260943a86a465920c62a67
Konrad Merz <konrad@graylog.com>
2020-11-18 12:14:25 +0100

    Convert EmptyResult.tsx to tsx

--- graylog2-web-interface/src/components/common/EmptyResult.tsx
+++ graylog2-web-interface/src/components/common/EmptyResult.tsx
@@ -21,8 +21,8 @@ import PropTypes from 'prop-types';
 import { Alert } from 'components/graylog';
 
 type Props = {
-  children: React.Node,
-  className: ?string,
+  children: JSX.Element | string,
+  className: string | null | undefined,
 };
 
 const EmptyResult = ({ children, className }: Props) => (
