fc0d44496ad746b33efe65ffd945a5edee6f5af9
Dennis Oelkers <dennis@graylog.com>
2020-11-19 10:13:54 +0100

    Migrating `QueryBarElements`.

--- graylog2-web-interface/src/views/components/QueryBarElements.tsx
+++ graylog2-web-interface/src/views/components/QueryBarElements.tsx
@@ -23,12 +23,16 @@ type Props = {
   queryBarElements: Array<React.ComponentType<{}>>,
 };
 
-const QueryBarElements = ({ queryBarElements = [] }) => queryBarElements
-// eslint-disable-next-line react/no-array-index-key
-  .map((Component, idx) => <Component key={idx} />);
+const QueryBarElements = ({ queryBarElements = [] }: Props) => (
+  <>
+    {queryBarElements
+      // eslint-disable-next-line react/no-array-index-key
+      .map((Component, idx) => <Component key={idx} />)}
+  </>
+);
 
 const mapping = {
   queryBarElements: 'views.elements.queryBar',
 };
 
-export default withPluginEntities<Props, typeof mapping>(QueryBarElements, mapping);
+export default withPluginEntities(QueryBarElements, mapping);
