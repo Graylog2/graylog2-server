f963c78e8b7ef467c36a801621da3d9cacb3b990
Konrad Merz <konrad@graylog.com>
2020-11-19 09:43:26 +0100

    Convert OwnersCell.tsx to tsx

--- graylog2-web-interface/src/components/permissions/SharedEntitiesOverview/SharedEntitiesOverviewItem/OwnersCell.tsx
+++ graylog2-web-interface/src/components/permissions/SharedEntitiesOverview/SharedEntitiesOverviewItem/OwnersCell.tsx
@@ -24,6 +24,7 @@ import { isPermitted } from 'util/PermissionsMixin';
 import CurrentUserContext from 'contexts/CurrentUserContext';
 import type { GranteesList } from 'logic/permissions/EntityShareState';
 import { getShowRouteFromGRN } from 'logic/permissions/GRN';
+import {UserJSON} from 'logic/users/User';
 
 type Props = {
   owners: GranteesList,
@@ -49,7 +50,7 @@ const _getOwnerTitle = ({ type, id, title }, userPermissions) => {
 };
 
 const OwnersCell = ({ owners }: Props) => {
-  const currentUser = useContext(CurrentUserContext);
+  const currentUser = useContext<UserJSON | undefined>(CurrentUserContext);
   const sortedOwners = owners.sort((o1, o2) => defaultCompare(o1.type, o2.type) || defaultCompare(o1.title, o2.title));
 
   return (
