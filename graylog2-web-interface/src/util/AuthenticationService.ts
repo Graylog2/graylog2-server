3149abd98e0075fb192c49863c6bfee68b74db98
Konrad Merz <konrad@graylog.com>
2020-11-19 14:15:11 +0100

    Convert AuthenticationService.ts to tsx

--- graylog2-web-interface/src/util/AuthenticationService.ts
+++ graylog2-web-interface/src/util/AuthenticationService.ts
@@ -18,7 +18,7 @@
 import { PluginStore } from 'graylog-web-plugin/plugin';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getAuthServicePlugin = (type: string, throwError?: boolean = false) => {
+export const getAuthServicePlugin = (type: string, throwError: boolean = false) => {
   const authServices = PluginStore.exports('authentication.services') || [];
   const authService = authServices.find((service) => service.name === type);
 
