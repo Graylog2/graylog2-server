c8e8071b0c9bfb0dfb3f7a45b869f676e1218acf
Linus Pahl <linus@graylog.com>
2020-11-19 17:08:17 +0100

    Remove empty line

--- graylog2-web-interface/src/logic/authentication/AuthenticationBackend.ts
+++ graylog2-web-interface/src/logic/authentication/AuthenticationBackend.ts
@@ -14,7 +14,6 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-
 import * as Immutable from 'immutable';
 import { $PropertyType } from 'utility-types';
 
