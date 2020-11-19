2218113d15a4c99d6e3c5cb592bae75b9ebe354b
Dennis Oelkers <dennis@graylog.com>
2020-11-19 09:54:19 +0100

    Include TypeScript files in `lintChanges` script. (#9544)

--- graylog2-web-interface/dev/lintChanges.sh
+++ graylog2-web-interface/dev/lintChanges.sh
@@ -6,6 +6,6 @@
 # It will give you an idea of the ci result, before pushing your changes.
 # You may need to set the correct permissions for this file by running `chmod +x ./dev/lintChanges.sh`
 
-git diff --name-only --diff-filter=ACMR origin/master...|grep -E '^graylog2-web-interface/(.*).js(x)?$'|sed s,graylog2-web-interface/,,g|xargs yarn lint:path
+git diff --name-only --diff-filter=ACMR origin/master...|grep -E '^graylog2-web-interface/(.*).[jt]s(x)?$'|sed s,graylog2-web-interface/,,g|xargs yarn lint:path
 
-git diff --name-only --diff-filter=ACMR origin/master...|grep -E '^graylog2-web-interface/(.*).js(x)?$'|sed s,graylog2-web-interface/,,g|xargs yarn lint:styles:path
+git diff --name-only --diff-filter=ACMR origin/master...|grep -E '^graylog2-web-interface/(.*).[jt]s(x)?$'|sed s,graylog2-web-interface/,,g|xargs yarn lint:styles:path
