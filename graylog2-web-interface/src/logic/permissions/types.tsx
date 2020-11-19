4387d84947755ae2fed0bfbe0c0750ba5b54e372
Konrad Merz <konrad@graylog.com>
2020-11-19 09:19:33 +0100

    Convert types.tsx to tsx

--- graylog2-web-interface/src/logic/permissions/types.tsx
+++ graylog2-web-interface/src/logic/permissions/types.tsx
@@ -21,28 +21,28 @@ import SharedEntity from 'logic/permissions/SharedEntity';
 
 export type GRN = string;
 
-export type CapabilityType = {|
+export type CapabilityType = {
   id: GRN,
   title: 'Viewer' | 'Manager' | 'Owner',
-|};
+};
 
-export type GranteeType = {|
+export type GranteeType = {
   id: GRN,
   title: string,
   type: 'global' | 'team' | 'user' | 'error',
-|};
+};
 
-export type ActiveShareType = {|
+export type ActiveShareType = {
   grant: GRN,
   grantee: GRN,
   capability: GRN,
-|};
+};
 
-export type SharedEntityType = {|
+export type SharedEntityType = {
   id: GRN,
   owners: Array<GranteeType>,
   title: string,
   type: string,
-|};
+};
 
 export type SharedEntities = Immutable.List<SharedEntity>;
