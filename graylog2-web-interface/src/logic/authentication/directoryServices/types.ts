142b807d485653933c892cf28a600b5ff246e0e2
Linus Pahl <linus@graylog.com>
2020-11-19 17:04:57 +0100

    Remove @flow in directoryServices types

--- graylog2-web-interface/src/logic/authentication/directoryServices/types.ts
+++ graylog2-web-interface/src/logic/authentication/directoryServices/types.ts
@@ -14,7 +14,6 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-// @flow strict
 import AuthenticationBackend, { AuthenticationBackendJSON } from 'logic/authentication/AuthenticationBackend';
 import { $PropertyType } from 'utility-types';
 
