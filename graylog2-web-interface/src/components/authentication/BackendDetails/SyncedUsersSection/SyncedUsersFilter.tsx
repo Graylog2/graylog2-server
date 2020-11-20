00246c6432e36ff79c5c6dda0b62c1394e69b9f5
Linus Pahl <linus@graylog.com>
2020-11-20 11:50:06 +0100

    Migrate `SyncedUsersFilter`

--- graylog2-web-interface/src/components/authentication/BackendDetails/SyncedUsersSection/SyncedUsersFilter.tsx
+++ graylog2-web-interface/src/components/authentication/BackendDetails/SyncedUsersSection/SyncedUsersFilter.tsx
@@ -14,9 +14,8 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-// @flow strict
 import * as React from 'react';
-import styled, { type StyledComponent } from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 
 import type { ThemeInterface } from 'theme';
 import { SearchForm } from 'components/common';
