329fc58b25a15c8620dbba6518f36126ca89baa3
Linus Pahl <linus@graylog.com>
2020-11-20 15:44:33 +0100

    Migrate `BackendWizard/index`

--- graylog2-web-interface/src/components/authentication/directoryServices/BackendWizard/index.tsx
+++ graylog2-web-interface/src/components/authentication/directoryServices/BackendWizard/index.tsx
@@ -14,8 +14,6 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-// @flow strict
-
 import BackendWizard from './BackendWizard';
 
 export default BackendWizard;
