9e471348fb3aa27fe5b23577ce404367a3e0b72d
Linus Pahl <linus@graylog.com>
2020-11-20 15:47:14 +0100

    Migrate `StepTitleWarning`

--- graylog2-web-interface/src/components/authentication/directoryServices/BackendWizard/StepTitleWarning.tsx
+++ graylog2-web-interface/src/components/authentication/directoryServices/BackendWizard/StepTitleWarning.tsx
@@ -14,7 +14,6 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-// @flow strict
 import * as React from 'react';
 
 import { Icon } from 'components/common';
@@ -29,7 +28,7 @@ const StepTitleWarning = ({ invalidStepKeys = [], stepKey }: Props) => {
     return <><Icon name="exclamation-triangle" />{' '}</>;
   }
 
-  return '';
+  return null;
 };
 
 export default StepTitleWarning;
