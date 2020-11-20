1c669106db478a345212f0967962e12121887894
Linus Pahl <linus@graylog.com>
2020-11-20 11:30:38 +0100

    Migrate `AuthenticatorsEdit/index`

--- graylog2-web-interface/src/components/authentication/AuthenticatorsEdit/index.tsx
+++ graylog2-web-interface/src/components/authentication/AuthenticatorsEdit/index.tsx
@@ -14,8 +14,6 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-// @flow strict
-
 import AuthenticatorsEdit from './AuthenticatorsEdit';
 
 export default AuthenticatorsEdit;
