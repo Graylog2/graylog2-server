95443551163e7eb5fbd56914a460a77367de646a
Linus Pahl <linus@graylog.com>
2020-11-20 11:08:52 +0100

    Migrate `AuthenticatiorsDetails/index`

--- graylog2-web-interface/src/components/authentication/AuthenticatorsDetails/index.tsx
+++ graylog2-web-interface/src/components/authentication/AuthenticatorsDetails/index.tsx
@@ -14,8 +14,6 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-// @flow strict
-
-import AuthenticatorsDetails from './AuthenticatorsDetails.jsx';
+import AuthenticatorsDetails from './AuthenticatorsDetails';
 
 export default AuthenticatorsDetails;
