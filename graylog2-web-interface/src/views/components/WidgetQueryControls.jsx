5d6bbf0921daf2682232228f24770b1d5ce097c8
Kyle Knight <kyle@graylog.com>
2020-11-23 10:47:34 -0600

    Add updated TimeRangeDisplay to WidgetQueryControls

--- graylog2-web-interface/src/views/components/WidgetQueryControls.jsx
+++ graylog2-web-interface/src/views/components/WidgetQueryControls.jsx
@@ -20,18 +20,13 @@ import TimeRangeTypeSelector from './searchbar/TimeRangeTypeSelector';
 import StreamsFilter from './searchbar/StreamsFilter';
 import SearchButton from './searchbar/SearchButton';
 import QueryInput from './searchbar/AsyncQueryInput';
+import TimeRangeDisplay from './searchbar/TimeRangeDisplay';
 
 type Props = {
   availableStreams: Array<any>,
   config: any,
   globalOverride: ?GlobalOverride,
 };
-
-const StyledTimeRange = styled.input`
-  width: 100%;
-  padding: 3px 9px;
-  margin: 0 12px;
-`;
 
 const FlexCol = styled(Col)`
   display: flex;
@@ -89,9 +84,8 @@ const WidgetQueryControls = ({ availableStreams, config, globalOverride = {} }: 
             <FlexCol md={4}>
               <TimeRangeTypeSelector disabled={isGloballyOverridden}
                                      config={config} />
-              <StyledTimeRange type="text"
-                               value={JSON.stringify(values?.timerange)}
-                               disabled />
+
+              <TimeRangeDisplay timerange={values?.timerange} />
             </FlexCol>
 
             <Col md={8}>
