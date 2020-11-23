bc6328c3a4a5e31aabe2569682ee20841329588a
Dennis Oelkers <dennis@graylog.com>
2020-11-23 14:52:56 +0100

    Migrating `StoreMock`.

--- graylog2-web-interface/test/helpers/mocking/StoreMock.ts
+++ graylog2-web-interface/test/helpers/mocking/StoreMock.ts
@@ -20,15 +20,16 @@ type MockMethod = string | [string, Function];
 export default function (...args: Array<MockMethod>) {
   const store = {
     // eslint-disable-next-line func-call-spacing,no-spaced-func
-    listen: jest.fn<[], () => void>(() => () => {}),
-    getInitialState: jest.fn<[], void>(),
+    listen: jest.fn(() => () => {}),
+    getInitialState: jest.fn(),
   };
+
   Array.from(args).forEach((method) => {
     if (Array.isArray(method)) {
       const [name, fn] = method;
       store[name] = fn;
     } else {
-      store[method] = jest.fn<[], void>();
+      store[method] = jest.fn();
     }
   });
 
