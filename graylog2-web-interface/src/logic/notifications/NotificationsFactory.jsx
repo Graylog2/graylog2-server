51ca3d6aa83ad5e09ee6f99ce877a480693c194a
Gaya <gaya@theclevernode.com>
2020-11-23 07:34:37 +0100

    hide other systems input link in notifications

--- graylog2-web-interface/src/logic/notifications/NotificationsFactory.jsx
+++ graylog2-web-interface/src/logic/notifications/NotificationsFactory.jsx
@@ -172,7 +172,9 @@ class NotificationsFactory {
             <span>
               There is a node without any running inputs. This means that you are not receiving any messages from this
               node at this point in time. This is most probably an indication of an error or misconfiguration.
-              You can click <Link to={Routes.SYSTEM.INPUTS}>here</Link> to solve this.
+              <HideOnCloud>
+                You can click <Link to={Routes.SYSTEM.INPUTS}>here</Link> to solve this.
+              </HideOnCloud>
             </span>
           ),
         };
