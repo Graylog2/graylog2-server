a10f995622b017b2137fa4ccc17374d6c11e0ab4
Konrad Merz <konrad@graylog.com>
2020-11-26 14:34:26 +0100

    Add highlight creation form

---
+++ graylog2-web-interface/src/views/components/sidebar/highlighting/HighlightForm.tsx
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import { useContext } from 'react';
+import * as Immutable from 'immutable';
+import { Formik, Form, Field } from 'formik';
+import styled from 'styled-components';
+
+import { Button } from 'components/graylog';
+import { Input } from 'components/bootstrap';
+import FieldTypesContext from 'views/components/contexts/FieldTypesContext';
+import FieldTypeMapping from 'views/logic/fieldtypes/FieldTypeMapping';
+import Select from 'components/common/Select';
+import { ColorPickerPopover } from 'components/common';
+import { DEFAULT_CUSTOM_HIGHLIGHT_RANGE } from 'views/Constants';
+import ColorPreview from 'views/components/sidebar/highlighting/ColorPreview';
+import { HighlightingRulesActions } from 'views/stores/HighlightingRulesStore';
+import HighlightingRule from 'views/logic/views/formatting/highlighting/HighlightingRule';
+
+const StyledHeader = styled.h4`
+  margin-bottom: 8px;
+`;
+
+type Props = {
+  onClose: () => void,
+};
+
+const HighlightForm = ({ onClose }: Props) => {
+  const fieldTypes = useContext(FieldTypesContext);
+  const fields = fieldTypes?.all
+    ? fieldTypes.all
+    : Immutable.List<FieldTypeMapping>();
+  const fieldOptions = fields.map(({ name }) => ({ value: name, label: name })).toArray();
+
+  const onSubmit = ({ field, value, color }) => HighlightingRulesActions.add(
+    HighlightingRule.create(field, value, undefined, color),
+  ).then(onClose);
+
+  return (
+    <>
+      <hr />
+      <StyledHeader>New Highlight rule</StyledHeader>
+      <Formik onSubmit={onSubmit}
+              initialValues={{ field: undefined, value: undefined, color: '#6fecc2' }}>
+        {() => (
+          <Form className="form">
+            <Field name="field">
+              {({ field: { name, value, onChange } }) => (
+                <Input id="field_type_controls"
+                       label="Field">
+                  <Select inputId="field-select"
+                          onChange={(newValue) => onChange({ target: { name, value: newValue } })}
+                          options={fieldOptions}
+                          value={value}
+                          placeholder="Pick a field" />
+                </Input>
+              )}
+            </Field>
+            <Field name="value">
+              {({ field: { name, value, onChange } }) => (
+                <Input id={name}
+                       type="text"
+                       onChange={onChange}
+                       value={value}
+                       label="Value" />
+              )}
+            </Field>
+            <Field name="color">
+              {({ field: { name, value, onChange } }) => (
+                <Input id={name}
+                       label="Color">
+                  <ColorPickerPopover id="formatting-rule-color"
+                                      placement="right"
+                                      color={value}
+                                      colors={DEFAULT_CUSTOM_HIGHLIGHT_RANGE.map((c) => [c])}
+                                      triggerNode={<ColorPreview color={value} />}
+                                      onChange={(newColor, _, hidePopover) => {
+                                        hidePopover();
+                                        onChange({ target: { name, value: newColor } });
+                                      }} />
+                </Input>
+              )}
+            </Field>
+            <Button type="submit">Create</Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </Form>
+        )}
+      </Formik>
+      <hr />
+    </>
+  );
+};
+
+export default HighlightForm;
--- graylog2-web-interface/src/views/components/sidebar/highlighting/HighlightingRules.tsx
+++ graylog2-web-interface/src/views/components/sidebar/highlighting/HighlightingRules.tsx
@@ -15,18 +15,21 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { DEFAULT_HIGHLIGHT_COLOR } from 'views/Constants';
 import HighlightingRulesContext from 'views/components/contexts/HighlightingRulesContext';
+import IconButton from 'components/common/IconButton';
 
 import HighlightingRule, { HighlightingRuleGrid } from './HighlightingRule';
 import ColorPreview from './ColorPreview';
+import HighlightForm from './HighlightForm';
 
 import SectionInfo from '../SectionInfo';
 import SectionSubheadline from '../SectionSubheadline';
 
 const HighlightingRules = () => {
+  const [showForm, setShowForm] = useState(false);
   const rules = useContext(HighlightingRulesContext) ?? [];
 
   return (
@@ -35,12 +38,12 @@ const HighlightingRules = () => {
         Search terms and field values can be highlighted. Highlighting your search query in the results can be enabled/disabled in the graylog server config.
         Any field value can be highlighted by clicking on the value and selecting &quot;Highlight this value&quot;.
       </SectionInfo>
-      <SectionSubheadline>Active highlights</SectionSubheadline>
+      <SectionSubheadline>Active highlights <IconButton className="pull-right" name="plus" onClick={() => setShowForm(!showForm)} /> </SectionSubheadline>
+      { showForm && <HighlightForm onClose={() => setShowForm(false)} />}
       <HighlightingRuleGrid>
         <ColorPreview color={DEFAULT_HIGHLIGHT_COLOR} />
         <div>Search terms</div>
       </HighlightingRuleGrid>
-
       {rules.map((rule) => <HighlightingRule key={`${rule.field}-${rule.value}-${rule.color}`} rule={rule} />)}
     </>
   );
