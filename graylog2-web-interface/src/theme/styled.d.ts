09a47cd5add497680b2c86f0addb26cc61d0d1a8
Linus Pahl <linus@graylog.com>
2020-11-26 16:30:09 +0100

    Create declarations for styled-components theme

---
+++ graylog2-web-interface/src/theme/styled.d.ts
@@ -0,0 +1,19 @@
+// import original module declarations
+import 'styled-components';
+
+import type { Colors } from './colors';
+import type { Fonts } from './fonts';
+import type { Utils } from './utils';
+import type { Breakpoints } from './breakpoints';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    breakpoints: Breakpoints,
+    colors: Colors,
+    fonts: Fonts,
+    utils: Utils,
+    mode: string,
+    changeMode: (string) => void,
+    components: { [component: string]: any },
+  }
+}
