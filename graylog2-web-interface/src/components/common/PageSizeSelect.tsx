f35167992f1aa0ce1e2b10552ad192b401f1e3a7
Linus Pahl <linus@graylog.com>
2020-12-01 13:25:07 +0100

    Defining correct `onChange` type for `PageSizeSelect`.

--- graylog2-web-interface/src/components/common/PageSizeSelect.tsx
+++ graylog2-web-interface/src/components/common/PageSizeSelect.tsx
@@ -15,7 +15,6 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { SyntheticEvent } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -42,7 +41,7 @@ type Props = {
   className?: string,
   pageSize: number,
   pageSizes: Array<number>,
-  onChange: (event: SyntheticEvent<HTMLOptionElement>) => void,
+  onChange: (event: React.ChangeEvent<HTMLOptionElement>) => void,
 };
 
 const PageSizeSelect = ({ pageSizes, pageSize, onChange, className }: Props) => (
