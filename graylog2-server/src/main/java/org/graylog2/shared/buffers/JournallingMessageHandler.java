b3f7bb25a450736bbf5ba1eaa470d9420602f7d6
Marco Pfatschbacher <marco@graylog.com>
2020-12-07 10:52:00 +0100

    Remove more unused code

--- graylog2-server/src/main/java/org/graylog2/shared/buffers/JournallingMessageHandler.java
+++ graylog2-server/src/main/java/org/graylog2/shared/buffers/JournallingMessageHandler.java
@@ -18,16 +18,9 @@ package org.graylog2.shared.buffers;
 
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
-import com.github.rholder.retry.Retryer;
-import com.github.rholder.retry.RetryerBuilder;
-import com.github.rholder.retry.StopStrategies;
-import com.github.rholder.retry.WaitStrategies;
 import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
 import com.lmax.disruptor.EventHandler;
-import org.graylog2.shared.journal.Journal;
-import org.graylog2.shared.messageq.MessageQueue;
 import org.graylog2.shared.messageq.MessageQueueWriter;
 import org.graylog2.system.processing.ProcessingStatusRecorder;
 import org.joda.time.DateTime;
@@ -37,46 +30,24 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
 import static com.google.common.collect.Lists.transform;
 
 public class JournallingMessageHandler implements EventHandler<RawMessageEvent> {
     private static final Logger log = LoggerFactory.getLogger(JournallingMessageHandler.class);
 
-    private static final Set<Journal.Entry> NULL_SINGLETON = Collections.singleton(null);
-    private static final Set<MessageQueue.Entry> MESSAGE_JOURNAL_NULL_SINGLETON = Collections.singleton(null);
-    private static final Retryer<Void> JOURNAL_WRITE_RETRYER = RetryerBuilder.<Void>newBuilder()
-            .retryIfException(new Predicate<Throwable>() {
-                @Override
-                public boolean apply(@Nullable Throwable input) {
-                    log.error("Unable to write to journal - retrying with exponential back-off", input);
-                    return true;
-                }
-            })
-            .withWaitStrategy(WaitStrategies.exponentialWait(250, 1, TimeUnit.MINUTES))
-            .withStopStrategy(StopStrategies.neverStop())
-            .build();
-
     private final List<RawMessageEvent> batch = Lists.newArrayList();
     private final Counter byteCounter;
     private final MessageQueueWriter messageQueueWriter;
     private final ProcessingStatusRecorder processingStatusRecorder;
-    private final Semaphore journalFilled;
 
     @Inject
     public JournallingMessageHandler(MetricRegistry metrics,
                                      MessageQueueWriter messageQueueWriter,
-                                     ProcessingStatusRecorder processingStatusRecorder,
-                                     @Named("JournalSignal") Semaphore journalFilled) {
+                                     ProcessingStatusRecorder processingStatusRecorder) {
         this.messageQueueWriter = messageQueueWriter;
         this.processingStatusRecorder = processingStatusRecorder;
-        this.journalFilled = journalFilled;
         byteCounter = metrics.counter(MetricRegistry.name(JournallingMessageHandler.class, "written_bytes"));
     }
 
