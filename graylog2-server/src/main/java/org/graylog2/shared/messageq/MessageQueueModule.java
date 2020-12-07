b21e9f69a00a282e70fef4d4c4e032d436881ed6
Marco Pfatschbacher <marco@graylog.com>
2020-12-07 10:11:32 +0100

    Remove MessageQueueReader interface
     - It doesn't fit our current abstraction

--- graylog2-server/src/main/java/org/graylog2/shared/messageq/MessageQueueModule.java
+++ graylog2-server/src/main/java/org/graylog2/shared/messageq/MessageQueueModule.java
@@ -19,7 +19,6 @@ package org.graylog2.shared.messageq;
 import com.google.inject.Scopes;
 import org.graylog2.plugin.PluginModule;
 import org.graylog2.shared.messageq.kafka.KafkaMessageQueueAcknowledger;
-import org.graylog2.shared.messageq.kafka.KafkaMessageQueueReader;
 import org.graylog2.shared.messageq.kafka.KafkaMessageQueueWriter;
 
 public class MessageQueueModule extends PluginModule {
@@ -31,12 +30,10 @@ public class MessageQueueModule extends PluginModule {
         // Journal with Pulsar
 //        serviceBinder().addBinding().to(PulsarMessageQueueWriter.class).in(Scopes.SINGLETON);
 //        serviceBinder().addBinding().to(PulsarMessageQueueReader.class).in(Scopes.SINGLETON);
-//        bind(MessageQueueReader.class).to(PulsarMessageQueueReader.class).in(Scopes.SINGLETON);
 //        bind(MessageQueueWriter.class).to(PulsarMessageQueueWriter.class).in(Scopes.SINGLETON);
 //        bind(MessageQueueAcknowledger.class).to(PulsarMessageQueueAcknowledger.class).in(Scopes.SINGLETON);
 
         // Journal with Kafka
-        bind(MessageQueueReader.class).to(KafkaMessageQueueReader.class).in(Scopes.SINGLETON);
         bind(MessageQueueWriter.class).to(KafkaMessageQueueWriter.class).in(Scopes.SINGLETON);
         bind(MessageQueueAcknowledger.class).to(KafkaMessageQueueAcknowledger.class).in(Scopes.SINGLETON);
 
