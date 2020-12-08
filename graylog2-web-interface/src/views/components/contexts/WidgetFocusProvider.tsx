f8f7009baf43f72e10dbc6f7120c9f1a39853c4b
Konrad Merz <konrad@graylog.com>
2020-12-08 10:15:50 +0100

    Add WidgetFocusContext for Action replacement

---
+++ graylog2-web-interface/src/views/components/contexts/WidgetFocusProvider.tsx
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import WidgetFocusContext from 'views/components/contexts/WidgetFocusContext';
+
+const WidgetFocusProvider = ({ children }: { children: React.ReactNode }): React.ReactElement => {
+  const [focusedWidget, setFocusedWidget] = useState(undefined);
+
+  return (
+    <WidgetFocusContext.Provider value={{ focusedWidget, setFocusedWidget }}>
+      {children}
+    </WidgetFocusContext.Provider>
+  );
+};
+
+WidgetFocusProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default WidgetFocusProvider;
