37f648c00e8e10a27090e4880d475d54779f33e5
Edmundo Alvarez <edmundo@graylog.com>
2020-12-08 09:52:17 +0100

    Support for better integration of Token creation from Forwarder wizard (#9701)
    * Extract create token form component
    
    * Add id to Token type definition
    
    * Refactor TokenList
    
    * Style and usability improvements in CreateTokenForm
    
    * Do not set expanded in Panel with default value
    
    PanelGroups with the `accordion` prop expect this flag to be `null`, and
    setting it to `false` throws errors in the browser console.
    
    * Correct type definition for Token
    
    Token name is in the `name` property, not `token_name`.
    
    * Fix linter errors in Panel
    
    - Replace conditional `useEffect` with `didMount` ref, so we only
      display the deprecation notice once without the need for an effect
    - Update dependencies of `useEffect` calculating if panel should be
      expanded

--- graylog2-web-interface/src/components/users/TokenList.jsx
+++ graylog2-web-interface/src/components/users/TokenList.jsx
@@ -14,131 +14,87 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
+// @flow strict
 import PropTypes from 'prop-types';
-import React from 'react';
-import Immutable from 'immutable';
+import React, { useState } from 'react';
+import { List } from 'immutable';
 
-import ClipboardButton from 'components/common/ClipboardButton';
-import { Button, Row, Col, FormControl, ControlLabel, Checkbox, ButtonGroup } from 'components/graylog';
-import TableList from 'components/common/TableList';
-import Spinner from 'components/common/Spinner';
+import { ClipboardButton, TableList, Spinner } from 'components/common';
+import { Button, Checkbox, ButtonGroup } from 'components/graylog';
+import type { Token } from 'actions/users/UsersActions';
 
-import TokenListStyle from './TokenList.css';
+import CreateTokenForm from './CreateTokenForm';
 
-class TokenList extends React.Component {
-  static propTypes = {
-    tokens: PropTypes.arrayOf(PropTypes.object),
-    onDelete: PropTypes.func,
-    onCreate: PropTypes.func,
-    creatingToken: PropTypes.bool,
-    deletingToken: PropTypes.string,
+type Props = {
+  creatingToken: boolean,
+  deletingToken: ?string,
+  onCreate: (tokenName: string) => void,
+  onDelete: (tokenId: string, tokenName: string) => void,
+  tokens: Token[],
+};
+
+const TokenList = ({ creatingToken, deletingToken, onCreate, onDelete, tokens }: Props) => {
+  const [hideTokens, setHideTokens] = useState(true);
+
+  const onShowTokensChanged = (event) => {
+    setHideTokens(event.target.checked);
   };
 
-  static defaultProps = {
-    tokens: [],
-    onDelete: () => {},
-    onCreate: () => {},
-    creatingToken: false,
-    deletingToken: undefined,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      token_name: '',
-      hide_tokens: true,
-    };
-
-    this._onNewTokeChanged = this._onNewTokeChanged.bind(this);
-    this._onShowTokensChanged = this._onShowTokensChanged.bind(this);
-    this._createToken = this._createToken.bind(this);
-    this.itemActionsFactory = this.itemActionsFactory.bind(this);
-  }
-
-  _onNewTokeChanged(event) {
-    this.setState({ token_name: event.target.value });
-  }
-
-  _onShowTokensChanged(event) {
-    this.setState({ hide_tokens: event.target.checked });
-  }
-
-  _deleteToken(token) {
+  const deleteToken = (token) => {
     return () => {
-      this.props.onDelete(token.id, token.name);
+      onDelete(token.id, token.name);
     };
-  }
+  };
 
-  _createToken(e) {
-    this.props.onCreate(this.state.token_name);
-    this.setState({ token_name: '' });
-    e.preventDefault();
-  }
-
-  itemActionsFactory(token) {
-    const deleteButton = this.props.deletingToken === token.id ? <Spinner text="Deleting..." /> : 'Delete';
+  const itemActionsFactory = (token) => {
+    const deleteButton = deletingToken === token.id ? <Spinner text="Deleting..." /> : 'Delete';
 
     return (
       <ButtonGroup>
         <ClipboardButton title="Copy to clipboard" text={token.token} bsSize="xsmall" />
         <Button bsSize="xsmall"
-                disabled={this.props.deletingToken === token.id}
+                disabled={deletingToken === token.id}
                 bsStyle="primary"
-                onClick={this._deleteToken(token)}>
+                onClick={deleteToken(token)}>
           {deleteButton}
         </Button>
       </ButtonGroup>
     );
-  }
+  };
 
-  render() {
-    const submitButton = (this.props.creatingToken ? <Spinner text="Creating..." /> : 'Create Token');
+  return (
+    <span>
+      <CreateTokenForm onCreate={onCreate} creatingToken={creatingToken} />
+      <hr />
+      <TableList filterKeys={['name', 'token']}
+                 items={List(tokens)}
+                 idKey="token"
+                 titleKey="name"
+                 descriptionKey="token"
+                 hideDescription={hideTokens}
+                 enableBulkActions={false}
+                 itemActionsFactory={itemActionsFactory} />
+      <Checkbox id="hide-tokens" onChange={onShowTokensChanged} checked={hideTokens}>
+        Hide Tokens
+      </Checkbox>
+    </span>
+  );
+};
 
-    const createTokenForm = (
-      <form onSubmit={this._createToken}>
-        <div className="form-group">
-          <Row>
-            <Col sm={2}>
-              <ControlLabel className={TokenListStyle.tokenNewNameLabel}>Token Name</ControlLabel>
-            </Col>
-            <Col sm={4}>
-              <FormControl id="create-token-input"
-                           type="text"
-                           placeholder="e.g ServiceName"
-                           value={this.state.token_name}
-                           onChange={this._onNewTokeChanged} />
-            </Col>
-            <Col sm={2}>
-              <Button id="create-token"
-                      disabled={this.state.token_name === '' || this.props.creatingToken}
-                      type="submit"
-                      bsStyle="primary">{submitButton}
-              </Button>
-            </Col>
-          </Row>
-        </div>
-        <hr />
-      </form>
-    );
+TokenList.propTypes = {
+  tokens: PropTypes.arrayOf(PropTypes.object),
+  onDelete: PropTypes.func,
+  onCreate: PropTypes.func,
+  creatingToken: PropTypes.bool,
+  deletingToken: PropTypes.string,
+};
 
-    return (
-      <span>
-        {createTokenForm}
-        <TableList filterKeys={['name', 'token']}
-                   items={Immutable.List(this.props.tokens)}
-                   idKey="token"
-                   titleKey="name"
-                   descriptionKey="token"
-                   hideDescription={this.state.hide_tokens}
-                   enableBulkActions={false}
-                   itemActionsFactory={this.itemActionsFactory} />
-        <Checkbox id="hide-tokens" onChange={this._onShowTokensChanged} checked={this.state.hide_tokens}>
-          Hide Tokens
-        </Checkbox>
-      </span>
-    );
-  }
-}
+TokenList.defaultProps = {
+  tokens: [],
+  onDelete: () => {},
+  onCreate: () => {},
+  creatingToken: false,
+  deletingToken: undefined,
+};
 
 export default TokenList;
