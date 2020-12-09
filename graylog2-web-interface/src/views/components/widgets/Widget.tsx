5123f547162103f278fd1e5568933f5025f3ec52
Linus Pahl <linus@graylog.com>
2020-12-09 16:30:38 +0100

    Improving `Widget` prop-types and updating `Widget.test`.

--- graylog2-web-interface/src/views/components/widgets/Widget.tsx
+++ graylog2-web-interface/src/views/components/widgets/Widget.tsx
@@ -62,6 +62,7 @@ import MoveWidgetToTabModal from './MoveWidgetToTabModal';
 import WidgetErrorBoundary from './WidgetErrorBoundary';
 import ReplaySearchButton from './ReplaySearchButton';
 
+import CustomPropTypes from '../CustomPropTypes';
 import IfDashboard from '../dashboard/IfDashboard';
 import InteractiveContext from '../contexts/InteractiveContext';
 import IfInteractive from '../dashboard/IfInteractive';
@@ -129,6 +130,8 @@ const _editComponentForType = (type) => {
 class Widget extends React.Component<Props, State> {
   static propTypes = {
     id: PropTypes.string.isRequired,
+    view: CustomPropTypes.CurrentView.isRequired,
+    widget: PropTypes.instanceOf(WidgetModel).isRequired,
     data: PropTypes.any,
     editing: PropTypes.bool,
     errors: WidgetErrorsList,
@@ -138,6 +141,7 @@ class Widget extends React.Component<Props, State> {
     onSizeChange: PropTypes.func.isRequired,
     onPositionsChange: PropTypes.func.isRequired,
     title: PropTypes.string.isRequired,
+    position: PropTypes.instanceOf(WidgetPosition).isRequired,
   };
 
   static defaultProps = {
