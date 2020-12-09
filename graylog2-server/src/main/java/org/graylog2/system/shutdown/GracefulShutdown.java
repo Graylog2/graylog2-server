eadc67c5da8c05f137e14c35da79fa72457fc86d
Othello Maurer <othello@graylog.com>
2020-12-09 16:33:39 +0100

    Lifecycle handling for Pulsar message queue reader (#9735)
    * Add missing @Singleton annotation
    
    To make sure that if the object is injected, it is actually the same
    object as bound via the service binder.
    
    * Remove explicit shutdown for messageQueueReader
    
    Originally, this shutdown didn't have any effect anyways, because the
    object it was called upon was not the running service. That's because of
     the missing @Singleton annotation in the former JournalReader class.
    
    During shutdown, the message queue reader will stop processing because
    it listens to lifecycle events. At least the Kafka version does.
    
    There is no need to change the behaviour now.
    
    * Fix comment
    
    Interrupting the thread won't help, so just document the issue for now
    
    * Extract lifecycle handling and share for Kafka and Pulsar readers
    
    * Simplify lifecycle switch
    
    * Add license header

--- graylog2-server/src/main/java/org/graylog2/system/shutdown/GracefulShutdown.java
+++ graylog2-server/src/main/java/org/graylog2/system/shutdown/GracefulShutdown.java
@@ -25,7 +25,6 @@ import org.graylog2.plugin.ServerStatus;
 import org.graylog2.shared.initializers.InputSetupService;
 import org.graylog2.shared.initializers.JerseyService;
 import org.graylog2.shared.initializers.PeriodicalsService;
-import org.graylog2.shared.messageq.MessageQueueReader;
 import org.graylog2.shared.system.activities.Activity;
 import org.graylog2.shared.system.activities.ActivityWriter;
 import org.slf4j.Logger;
@@ -51,7 +50,6 @@ public class GracefulShutdown implements Runnable {
     private final JerseyService jerseyService;
     private final GracefulShutdownService gracefulShutdownService;
     private final AuditEventSender auditEventSender;
-    private final MessageQueueReader messageQueueReader;
 
     @Inject
     public GracefulShutdown(ServerStatus serverStatus,
@@ -62,8 +60,7 @@ public class GracefulShutdown implements Runnable {
                             InputSetupService inputSetupService,
                             JerseyService jerseyService,
                             GracefulShutdownService gracefulShutdownService,
-                            AuditEventSender auditEventSender,
-                            MessageQueueReader messageQueueReader) {
+                            AuditEventSender auditEventSender) {
         this.serverStatus = serverStatus;
         this.activityWriter = activityWriter;
         this.configuration = configuration;
@@ -73,7 +70,6 @@ public class GracefulShutdown implements Runnable {
         this.jerseyService = jerseyService;
         this.gracefulShutdownService = gracefulShutdownService;
         this.auditEventSender = auditEventSender;
-        this.messageQueueReader = messageQueueReader;
     }
 
     @Override
@@ -111,8 +107,6 @@ public class GracefulShutdown implements Runnable {
 
         jerseyService.awaitTerminated();
         inputSetupService.awaitTerminated();
-
-        messageQueueReader.stopAsync().awaitTerminated();
 
         // Try to flush all remaining messages from the system
         bufferSynchronizerService.stopAsync().awaitTerminated();
