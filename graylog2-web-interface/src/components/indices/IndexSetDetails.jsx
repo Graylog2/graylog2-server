53dc37bba481c20eaa4a78d1ff9ef6d826903978
Ousmane Samba <ousmane@graylog.com>
2020-12-10 14:42:59 +0100

    Move IndexSetDetails to functional components
     - Remove lifecycle methods
     - remove unused import

--- graylog2-web-interface/src/components/indices/IndexSetDetails.jsx
+++ graylog2-web-interface/src/components/indices/IndexSetDetails.jsx
@@ -17,51 +17,39 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { Row, Col } from 'components/graylog';
+import { Col } from 'components/graylog';
 import { IndicesConfiguration } from 'components/indices';
 
 import StyledIndexSetDetailsRow from './StyledIndexSetDetailsRow';
 
-class IndexSetDetails extends React.Component {
-  static propTypes = {
-    indexSet: PropTypes.object.isRequired,
-  };
+const IndexSetDetails = ({ indexSet }) => {
+  return (
+    <StyledIndexSetDetailsRow>
+      <Col md={3}>
+        <dl>
+          <dt>Index prefix:</dt>
+          <dd>{indexSet.index_prefix}</dd>
 
-  componentDidMount() {
-    // style.use();
-  }
+          <dt>Shards:</dt>
+          <dd>{indexSet.shards}</dd>
 
-  componentWillUnmount() {
-    // style.unuse();
-  }
+          <dt>Replicas:</dt>
+          <dd>{indexSet.replicas}</dd>
 
-  render() {
-    const { indexSet } = this.props;
+          <dt>Field type refresh interval:</dt>
+          <dd>{indexSet.field_type_refresh_interval / 1000.0} seconds</dd>
+        </dl>
+      </Col>
 
-    return (
-      <StyledIndexSetDetailsRow>
-        <Col md={3}>
-          <dl>
-            <dt>Index prefix:</dt>
-            <dd>{indexSet.index_prefix}</dd>
+      <Col md={6}>
+        <IndicesConfiguration indexSet={indexSet} />
+      </Col>
+    </StyledIndexSetDetailsRow>
+  );
+};
 
-            <dt>Shards:</dt>
-            <dd>{indexSet.shards}</dd>
-
-            <dt>Replicas:</dt>
-            <dd>{indexSet.replicas}</dd>
-
-            <dt>Field type refresh interval:</dt>
-            <dd>{indexSet.field_type_refresh_interval / 1000.0} seconds</dd>
-          </dl>
-        </Col>
-
-        <Col md={6}>
-          <IndicesConfiguration indexSet={indexSet} />
-        </Col>
-      </StyledIndexSetDetailsRow>
-    );
-  }
-}
+IndexSetDetails.propTypes = {
+  indexSet: PropTypes.object.isRequired,
+};
 
 export default IndexSetDetails;
