215a498eb84b7b66e27e731555c795954c3d4560
Ousmane Samba <ousmane@graylog.com>
2020-12-11 10:49:49 +0100

    Convert MetricDetails to class component

--- graylog2-web-interface/src/components/metrics/MetricDetails.jsx
+++ graylog2-web-interface/src/components/metrics/MetricDetails.jsx
@@ -14,67 +14,77 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
+// @flow strict
 import PropTypes from 'prop-types';
 import React from 'react';
-import createReactClass from 'create-react-class';
-import Reflux from 'reflux';
 import lodash from 'lodash';
 
 import { CounterDetails, GaugeDetails, HistogramDetails, MeterDetails, TimerDetails } from 'components/metrics';
 import CombinedProvider from 'injection/CombinedProvider';
+import connect from 'stores/connect';
 
 const { MetricsStore, MetricsActions } = CombinedProvider.get('Metrics');
 
-const MetricDetails = createReactClass({
-  displayName: 'MetricDetails',
+class MetricDetails extends React.Component {
+ static propTypes = {
+   metrics: PropTypes.object,
+   metric: PropTypes.object.isRequired,
+   nodeId: PropTypes.string.isRequired,
+ };
 
-  propTypes: {
-    metric: PropTypes.object.isRequired,
-  },
+ static defaultProps = {
+   metrics: undefined,
+ }
 
-  mixins: [Reflux.connect(MetricsStore)],
+ componentDidMount() {
+   const { nodeId, metric: { full_name: fullName } } = this.props;
+   MetricsActions.add(nodeId, fullName);
+ }
 
-  componentDidMount() {
-    MetricsActions.add(this.props.nodeId, this.props.metric.full_name);
-  },
+ componentWillUnmount() {
+   const { nodeId, metric: { full_name: fullName } } = this.props;
+   MetricsActions.remove(nodeId, fullName);
+ }
 
-  componentWillUnmount() {
-    MetricsActions.remove(this.props.nodeId, this.props.metric.full_name);
-  },
+ _formatDetailsForType = (type, metric) => {
+   switch (type) {
+     case 'Counter':
+       return <CounterDetails metric={metric} />;
+     case 'Gauge':
+       return <GaugeDetails metric={metric} />;
+     case 'Histogram':
+       return <HistogramDetails metric={metric} />;
+     case 'Meter':
+       return <MeterDetails metric={metric} />;
+     case 'Timer':
+       return <TimerDetails metric={metric} />;
+     default:
+       return <i>Invalid metric type: {type}</i>;
+   }
+ }
 
-  _formatDetailsForType(type, metric) {
-    switch (type) {
-      case 'Counter':
-        return <CounterDetails metric={metric} />;
-      case 'Gauge':
-        return <GaugeDetails metric={metric} />;
-      case 'Histogram':
-        return <HistogramDetails metric={metric} />;
-      case 'Meter':
-        return <MeterDetails metric={metric} />;
-      case 'Timer':
-        return <TimerDetails metric={metric} />;
-      default:
-        return <i>Invalid metric type: {type}</i>;
-    }
-  },
+ render() {
+   const { nodeId, metric, metric: { full_name: metricName }, metrics } = this.props;
+   const currentMetric = metrics && metrics[nodeId] && metrics[nodeId][metricName]
+     ? metrics[nodeId][metricName] : metric;
+   const type = lodash.capitalize(currentMetric.type);
+   const details = this._formatDetailsForType(type, currentMetric);
 
-  render() {
-    const metricName = this.props.metric.full_name;
-    const { nodeId } = this.props;
-    const metric = this.state.metrics && this.state.metrics[nodeId] && this.state.metrics[nodeId][metricName]
-      ? this.state.metrics[nodeId][metricName] : this.props.metric;
-    const type = lodash.capitalize(metric.type);
-    const details = this._formatDetailsForType(type, metric);
+   return (
+     <div className="metric">
+       <h3>{type}</h3>
 
-    return (
-      <div className="metric">
-        <h3>{type}</h3>
+       {details}
+     </div>
+   );
+ }
+}
 
-        {details}
-      </div>
-    );
-  },
-});
-
-export default MetricDetails;
+export default connect(
+  MetricDetails,
+  { metricsStore: MetricsStore },
+  ({ metricsStore, ...otherProps }) => ({
+    ...otherProps,
+    metrics: metricsStore.metrics,
+  }),
+);
