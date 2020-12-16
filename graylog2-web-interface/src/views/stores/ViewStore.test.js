4aae0df447adcaf890c5094ebb7facd9edd55439
Konrad Merz <konrad@graylog.com>
2020-12-16 14:33:28 +0100

    Fix order of tabs in dashboard after install (#9812)
    ## Motivation
    Prior to this change, a dashboard installed by a content pack had a
    tabs in a random order. Since we cannot change the order of the tabs
    this is quite inconvinient for the user.
    
    ## Description
    This change will do mutiple steps to fix that:
    - Use a LinkedHashMap to ensure the order stays fix during content pack
      creation.
    - Instead of using the serach QueryIds we use the views QueryIds which
      we previously fixed in the order.
    - When loading a view and selecting the activeQuery we also use the
      view.state queryids instead of the search ones.
    
    Fixes #8264

--- graylog2-web-interface/src/views/stores/ViewStore.test.js
+++ graylog2-web-interface/src/views/stores/ViewStore.test.js
@@ -41,14 +41,11 @@ jest.mock('views/logic/Widgets', () => ({
 jest.mock('logic/rest/FetchProvider', () => jest.fn(() => Promise.resolve()));
 
 describe('ViewStore', () => {
-  const dummySearch = Search.create()
-    .toBuilder()
-    .queries([
-      Query.builder().id('firstQueryId').build(),
-      Query.builder().id('secondQueryId').build(),
-    ])
-    .build();
-  const dummyView = View.builder().search(dummySearch).build();
+  const dummyState = Immutable.OrderedMap({
+    firstQueryId: ViewState.builder().build(),
+    secondQueryId: ViewState.builder().build(),
+  });
+  const dummyView = View.builder().state(dummyState).build();
 
   it('.load should select first query if activeQuery is not set', () => ViewActions.load(dummyView)
     .then((state) => expect(state.activeQuery).toBe('firstQueryId')));
@@ -78,6 +75,7 @@ describe('ViewStore', () => {
       .newId()
       .title('Initial title')
       .description('Initail description')
+      .state(dummyState)
       .summary('Initial summary')
       .build();
 
@@ -106,7 +104,10 @@ describe('ViewStore', () => {
 
     it('resets dirty flag when an existing view is updated', () => {
       const search = Search.create();
-      const newView = View.builder().newId().search(search).build();
+      const newView = View.builder().newId()
+        .search(search)
+        .state(dummyState)
+        .build();
 
       return ViewActions.load(newView)
         .then(() => ViewActions.search(search.toBuilder().newId().build()))
