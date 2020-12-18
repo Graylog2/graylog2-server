cc8127b5129197b792687cc68e8409d6c7822ee9
Konrad Merz <konrad@graylog.com>
2020-12-18 15:26:44 +0100

    Validation to color scheme

--- graylog2-web-interface/src/views/components/aggregationbuilder/HeatmapVisualizationConfiguration.tsx
+++ graylog2-web-interface/src/views/components/aggregationbuilder/HeatmapVisualizationConfiguration.tsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 
 import { Checkbox } from 'components/graylog';
@@ -37,18 +37,60 @@ type Props = {
 const _makeOption = (value) => ({ label: value, value });
 const colorScalesOptions = COLORSCALES.map(_makeOption);
 
-const _validateConfig = (config: HeatmapVisualizationConfig, setErrors) => {
-
+type Error = {
+  zmin?: string,
+  zmax?: string,
+  defaultValue?: string,
 }
 
-const HeatmapVisualizationConfiguration = ({ config = HeatmapVisualizationConfig.empty(), onChange }: Props) => {
+const _validateConfig = (config: HeatmapVisualizationConfig, setErrors): boolean => {
+  const { zMax, zMin, defaultValue } = config;
+  const errors = {} as Error;
+
+  if (zMin || zMax) {
+    if (zMin >= zMax) {
+      errors.zmin = 'Min is bigger than Max';
+    }
+
+    if (zMax <= zMin) {
+      errors.zmin = 'Max is smaller than Min';
+    }
+
+    if (defaultValue) {
+      if (defaultValue > zMax || defaultValue < zMin) {
+        errors.defaultValue = 'Default Value is out of range from Min and Max';
+      }
+    }
+  }
+
+  setErrors(errors);
+
+  return Object.keys(errors).length <= 0;
+};
+
+const _parseEvent = (event) => {
+  const { value } = event.target;
+
+  if (value === undefined) {
+    return undefined;
+  }
+
+  return parseFloat(value);
+};
+
+const HeatmapVisualizationConfiguration = ({ config = HeatmapVisualizationConfig.empty(), onChange: onChangeFunc }: Props) => {
+  const [errors, setErrors] = useState<Error>({});
+  const onChange = useCallback((newConfig) => {
+    if (_validateConfig(newConfig, setErrors)) {
+      onChangeFunc(newConfig);
+    }
+  }, [onChangeFunc]);
   const _onColorScaleChange = useCallback(({ value }) => onChange(config.toBuilder().colorScale(value).build()), [config, onChange]);
   const _onReverseScaleChange = useCallback((e) => onChange(config.toBuilder().reverseScale(e.target.checked).build()), [config, onChange]);
-  const _onDefaultValueChange = useCallback((e) => onChange(config.toBuilder().defaultValue(e.target.value).build()), [config, onChange]);
   const _onAutoScaleChange = useCallback((e) => onChange(config.toBuilder().autoScale(e.target.checked).build()), [config, onChange]);
-  const _onZminChange = useCallback((e) => onChange(config.toBuilder().zMin(e.target.value).build()), [config, onChange]);
-  const _onZmidChange = useCallback((e) => onChange(config.toBuilder().zMid(e.target.value).build()), [config, onChange]);
-  const _onZmaxChange = useCallback((e) => onChange(config.toBuilder().zMax(e.target.value).build()), [config, onChange]);
+  const _onZminChange = useCallback((e) => onChange(config.toBuilder().zMin(_parseEvent(e)).build()), [config, onChange]);
+  const _onZmaxChange = useCallback((e) => onChange(config.toBuilder().zMax(_parseEvent(e)).build()), [config, onChange]);
+  const _onDefaultValueChange = useCallback((e) => onChange(config.toBuilder().defaultValue(_parseEvent(e)).build()), [config, onChange]);
 
   return (
     <>
@@ -57,11 +99,6 @@ const HeatmapVisualizationConfiguration = ({ config = HeatmapVisualizationConfig
               onChange={_onColorScaleChange}
               options={colorScalesOptions}
               value={_makeOption(config.colorScale)} />
-      <span>Default</span>
-      <StyledInput type="number"
-                   onChange={_onDefaultValueChange}
-                   label="Default Value" />
-      <span>Scale settings</span>
       <Checkbox onChange={_onReverseScaleChange}
                 checked={config.reverseScale}>
         Reverse Scale
@@ -72,14 +109,23 @@ const HeatmapVisualizationConfiguration = ({ config = HeatmapVisualizationConfig
       </Checkbox>
       <StyledInput type="number"
                    disabled={config.autoScale}
+                   id="zmin"
                    onChange={_onZminChange}
                    value={config.zMin}
+                   error={errors.zmin}
                    label="Min" />
       <StyledInput type="number"
                    disabled={config.autoScale}
+                   id="zmax"
                    value={config.zMax}
+                   error={errors.zmax}
                    onChange={_onZmaxChange}
                    label="Max" />
+      <StyledInput type="number"
+                   id="default_value"
+                   error={errors.defaultValue}
+                   onChange={_onDefaultValueChange}
+                   label="Default Value" />
     </>
   );
 };
