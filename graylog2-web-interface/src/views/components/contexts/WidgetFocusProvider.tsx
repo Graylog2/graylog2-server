d7270e56ce76b4e034984b5f080748602e7f6bff
Konrad Merz <konrad@graylog.com>
2020-12-21 15:33:12 +0100

    Save focused widget id in url
    ## Motivation
    Prior to this change, we did not save the state
    of the focused widget in the url.
    
    ## Description
    This change will sync the focused widget id with the
    url so a user can copy and past the state to another
    user.

--- graylog2-web-interface/src/views/components/contexts/WidgetFocusProvider.tsx
+++ graylog2-web-interface/src/views/components/contexts/WidgetFocusProvider.tsx
@@ -15,16 +15,47 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import URI from 'urijs';
 
 import { useStore } from 'stores/connect';
 import { WidgetStore } from 'views/stores/WidgetStore';
 import WidgetFocusContext from 'views/components/contexts/WidgetFocusContext';
 
+const _syncWithQuery = (query: string, focusedWidget: string) => {
+  const baseUri = new URI(query)
+    .removeSearch('focused');
+
+  if (focusedWidget) {
+    return baseUri.setSearch('focused', focusedWidget).toString();
+  }
+
+  return baseUri.toString();
+};
+
+const getFocusedWidgetFromSearch = (search: string) => {
+  const uri = new URI(search);
+  const { focused } = uri.search(true);
+
+  return focused;
+};
+
 const WidgetFocusProvider = ({ children }: { children: React.ReactNode }): React.ReactElement => {
-  const [focusedWidget, setFocusedWidget] = useState(undefined);
+  const { search, pathname } = useLocation();
+  const query = pathname + search;
+  const history = useHistory();
+  const [focusedWidget, setFocusedWidget] = useState<string | undefined>();
   const widgets = useStore(WidgetStore);
+
+  useEffect(() => {
+    const paramFocusedWidget = getFocusedWidgetFromSearch(search);
+
+    if (paramFocusedWidget && focusedWidget !== paramFocusedWidget) {
+      setFocusedWidget(paramFocusedWidget);
+    }
+  }, [focusedWidget, search]);
 
   useEffect(() => {
     if (focusedWidget && !widgets.has(focusedWidget)) {
@@ -32,10 +63,15 @@ const WidgetFocusProvider = ({ children }: { children: React.ReactNode }): React
     }
   }, [focusedWidget, widgets]);
 
-  const updateFocus = (widgetId: string | undefined | null) => (
-    widgetId === focusedWidget
-      ? setFocusedWidget(undefined)
-      : setFocusedWidget(widgetId));
+  const updateFocus = useCallback((widgetId: string | undefined | null) => {
+    const newFocus = widgetId === focusedWidget
+      ? undefined
+      : widgetId;
+    const newURI = _syncWithQuery(query, newFocus);
+    history.replace(newURI);
+
+    setFocusedWidget(newFocus);
+  }, [focusedWidget, history, query]);
 
   return (
     <WidgetFocusContext.Provider value={{ focusedWidget, setFocusedWidget: updateFocus }}>
