3f2570588cf100c823689fd47829cbc88e989b7b
Marc Ruiz <radykal@radykal.com>
2021-01-05 17:12:11 +0100

    Add HTML emails compatibility for alerts notifications (#9790)
    * Add HTML emails compatibility for alerts notifications
    
    * Default html body as empty string
    
    * Send only HtmlEmail when needed
    
    * Restore default html body for new notifications

--- graylog2-server/src/main/java/org/graylog/events/notifications/types/EmailEventNotificationConfig.java
+++ graylog2-server/src/main/java/org/graylog/events/notifications/types/EmailEventNotificationConfig.java
@@ -74,6 +74,7 @@ public abstract class EmailEventNotificationConfig implements EventNotificationC
     private static final String FIELD_SENDER = "sender";
     private static final String FIELD_SUBJECT = "subject";
     private static final String FIELD_BODY_TEMPLATE = "body_template";
+    private static final String FIELD_HTML_BODY_TEMPLATE = "html_body_template";
     private static final String FIELD_EMAIL_RECIPIENTS = "email_recipients";
     private static final String FIELD_USER_RECIPIENTS = "user_recipients";
 
@@ -86,8 +87,10 @@ public abstract class EmailEventNotificationConfig implements EventNotificationC
     public abstract String subject();
 
     @JsonProperty(FIELD_BODY_TEMPLATE)
-    @NotBlank
     public abstract String bodyTemplate();
+
+    @JsonProperty(FIELD_HTML_BODY_TEMPLATE)
+    public abstract String htmlBodyTemplate();
 
     @JsonProperty(FIELD_EMAIL_RECIPIENTS)
     public abstract Set<String> emailRecipients();
@@ -114,8 +117,8 @@ public abstract class EmailEventNotificationConfig implements EventNotificationC
         if (subject().isEmpty()) {
             validation.addError(FIELD_SUBJECT, "Email Notification subject cannot be empty.");
         }
-        if (bodyTemplate().isEmpty()) {
-            validation.addError(FIELD_BODY_TEMPLATE, "Email Notification body template cannot be empty.");
+        if (bodyTemplate().isEmpty() && htmlBodyTemplate().isEmpty()) {
+            validation.addError("body", "One of Email Notification body template or Email Notification HTML body must not be empty.");
         }
         if (emailRecipients().isEmpty() && userRecipients().isEmpty()) {
             validation.addError("recipients", "Email Notification must have email recipients or user recipients.");
@@ -134,7 +137,8 @@ public abstract class EmailEventNotificationConfig implements EventNotificationC
                     .subject(DEFAULT_SUBJECT)
                     .emailRecipients(ImmutableSet.of())
                     .userRecipients(ImmutableSet.of())
-                    .bodyTemplate(DEFAULT_BODY_TEMPLATE);
+                    .bodyTemplate(DEFAULT_BODY_TEMPLATE)
+                    .htmlBodyTemplate("");
         }
 
         @JsonProperty(FIELD_SENDER)
@@ -145,6 +149,9 @@ public abstract class EmailEventNotificationConfig implements EventNotificationC
 
         @JsonProperty(FIELD_BODY_TEMPLATE)
         public abstract Builder bodyTemplate(String bodyTemplate);
+
+        @JsonProperty(FIELD_HTML_BODY_TEMPLATE)
+        public abstract Builder htmlBodyTemplate(String htmlBodyTemplate);
 
         @JsonProperty(FIELD_EMAIL_RECIPIENTS)
         public abstract Builder emailRecipients(Set<String> emailRecipients);
@@ -161,6 +168,7 @@ public abstract class EmailEventNotificationConfig implements EventNotificationC
             .sender(ValueReference.of(sender()))
             .subject(ValueReference.of(subject()))
             .bodyTemplate(ValueReference.of(bodyTemplate()))
+            .htmlBodyTemplate(ValueReference.of(htmlBodyTemplate()))
             .emailRecipients(emailRecipients())
             .userRecipients(userRecipients())
             .build();
