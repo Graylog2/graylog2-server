0b638ba413b01c155ee26fc0ff9371f2fc035ede
Konrad Merz <konrad@graylog.com>
2021-01-05 15:16:33 +0100

    Styling of the legend component

--- graylog2-web-interface/src/views/components/visualizations/PlotLegend.tsx
+++ graylog2-web-interface/src/views/components/visualizations/PlotLegend.tsx
@@ -15,8 +15,8 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import styled from 'styled-components';
 import { useContext } from 'react';
+import styled from 'styled-components';
 
 import Value from 'views/components/Value';
 import { useStore } from 'stores/connect';
@@ -39,23 +39,30 @@ const Container = styled.div`
   height: 100%;
 `;
 
-const Legend = styled.div`
+const LegendContainer = styled.div`
   padding: 5px;
   max-height: 80px;
   overflow: auto;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  align-items: stretch;
-  align-content: stretch;
+`;
+
+const Legend = styled.div`
+  display: table;
+  width: 100%;
+`;
+
+const LegendRow = styled.div`
+  display: table-row;
+`;
+
+const LegendCell = styled.div`
+  padding: 4px;
+  display: table-cell;
 `;
 
 const LegendEntry = styled.div`
-  margin: 4px;
   display: flex;
-  margin-right: 22px;
 `;
+
 const ValueContainer = styled.div`
   margin-left: 8px; 
   line-height: 12px;
@@ -70,30 +77,71 @@ type Props = {
 const PlotLegend = ({ children, config, chartData }: Props) => {
   const { columnPivots } = config;
   const fields = columnPivots.map(({ field }) => field);
-  const values = chartData.map(({ name }) => name);
+  const values: Array<string> = chartData.map(({ name }) => name);
   const { activeQuery } = useStore(CurrentViewStateStore);
   const { colors } = useContext(ChartColorContext);
 
-  const result = values.map((value) => {
-    let val = value;
+  const chunkCells = (cells, columnCount) => {
+    const { length } = cells;
+    const rowCount = Math.round(length / columnCount) + 1;
+    const result = new Array(rowCount);
+
+    for (let row = 0; row < rowCount; row += 1) {
+      result[row] = [];
+
+      for (let column = 0; column < columnCount; column += 1) {
+        if (cells[(rowCount * column) + row]) {
+          result[row][column] = cells[(rowCount * column) + row];
+        }
+      }
+    }
+
+    return result;
+  };
+
+  const stringLenSort = (s1: string, s2: string) => {
+    if (s1.length < s2.length) {
+      return -1;
+    }
+
+    if (s1.length === s2.length) {
+      return 0;
+    }
+
+    return 1;
+  };
+
+  const tableCells = values.sort(stringLenSort).map((value) => {
+    let val: React.ReactNode = value;
+
     if (fields.length === 1) {
       val = (<Value type={FieldType.Unknown} value={value} field={fields[0]} queryId={activeQuery}>{value}</Value>);
     }
 
     return (
-      <LegendEntry>
-        <ColorHint color={colors.get(value)} />
-        <ValueContainer>
-          {val}
-        </ValueContainer>
-      </LegendEntry>
+      <LegendCell key={value}>
+        <LegendEntry>
+          <ColorHint color={colors.get(value)} />
+          <ValueContainer>
+            {val}
+          </ValueContainer>
+        </LegendEntry>
+      </LegendCell>
     );
   });
+
+  const result = chunkCells(tableCells, 5).map((cells) => (
+    <LegendRow>
+      {cells}
+    </LegendRow>
+  ));
 
   return (
     <Container>
       {children}
-      <Legend>{result}</Legend>
+      <LegendContainer>
+        <Legend>{result}</Legend>
+      </LegendContainer>
     </Container>
   );
 };
