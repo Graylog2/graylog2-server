97a06674948b7ccca5be626a17293775a2d8c11c
Dennis Oelkers <dennis@graylog.com>
2021-01-08 16:06:22 +0100

    Adjusting regex for query string parser.

--- graylog2-server/src/test/java/org/graylog/plugins/views/search/elasticsearch/QueryStringParserTest.java
+++ graylog2-server/src/test/java/org/graylog/plugins/views/search/elasticsearch/QueryStringParserTest.java
@@ -30,10 +30,35 @@ public class QueryStringParserTest {
         assertThat(parse("foo:bar AND some:value")).isEmpty();
         assertThat(parse("foo:$bar$ AND some:value")).containsExactly("bar");
         assertThat(parse("foo:$bar$ AND some:$value$")).containsExactlyInAnyOrder("value", "bar");
+    }
+
+    @Test
+    void testStringsContainingDollars() {
         assertThat(parse("foo:bar$")).isEmpty();
         assertThat(parse("foo:bar$ OR foo:$baz")).isEmpty();
         assertThat(parse("foo:bar$ OR foo:$baz$")).containsExactly("baz");
+        assertThat(parse("foo:$bar$ OR foo:$baz")).containsExactly("bar");
         assertThat(parse("foo:bar$ AND baz$:$baz$")).containsExactly("baz");
+        assertThat(parse("foo:$$")).isEmpty();
+        assertThat(parse("foo:$foo$ AND bar:$$")).containsExactly("foo");
+    }
+
+    @Test
+    void testCharacterSpaceOfParameterNames() {
+        assertThat(parse("foo:$some parameter$")).isEmpty();
+        assertThat(parse("foo:$some-parameter$")).isEmpty();
+        assertThat(parse("foo:$some/parameter$")).isEmpty();
+        assertThat(parse("foo:$some42parameter$")).containsExactly("some42parameter");
+        assertThat(parse("foo:$42parameter$")).isEmpty();
+        assertThat(parse("foo:$parameter42$")).containsExactly("parameter42");
+        assertThat(parse("foo:$someparameter$")).containsExactly("someparameter");
+        assertThat(parse("foo:$some_parameter$")).containsExactly("some_parameter");
+        assertThat(parse("foo:$_someparameter$")).containsExactly("_someparameter");
+        assertThat(parse("foo:$_someparameter_$")).containsExactly("_someparameter_");
+        assertThat(parse("foo:$_someparameter_$")).containsExactly("_someparameter_");
+        assertThat(parse("foo:$_$")).containsExactly("_");
+        assertThat(parse("foo:$s$")).containsExactly("s");
+        assertThat(parse("foo:$9$")).isEmpty();
     }
 
     private Set<String> parse(String query) {
