98c163a535a6589d32da006df767fec065d85051
Dennis Oelkers <dennis@graylog.com>
2021-01-11 15:49:20 +0100

    Generate API files.

--- graylog2-server/src/main/java/org/graylog/api/GenerateApiDefinition.java
+++ graylog2-server/src/main/java/org/graylog/api/GenerateApiDefinition.java
@@ -16,12 +16,18 @@
  */
 package org.graylog.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.graylog2.shared.rest.documentation.generator.Generator;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -37,15 +43,49 @@ public class GenerateApiDefinition {
         System.exit(-1);
     }
 
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) throws IOException {
         if (args.length < 2) {
            bail("Syntax: " + GenerateApiDefinition.class.getSimpleName() + " <outfile> <package1> ... <packageN>");
         }
-        log("Generating Swagger definition for API ...");
         final String targetName = args[0];
+        Files.createDirectories(Paths.get(targetName));
+        log("Generating Swagger definition for API ...");
         final Set<String> packageNames = Arrays.stream(args).skip(1).collect(Collectors.toSet());
-        final Generator generator = new Generator(packageNames, Collections.emptyMap(), "", new ObjectMapper());
+        final Generator generator = new Generator(packageNames, Collections.emptyMap(), "/plugins", new ObjectMapper());
 
-        log(objectMapper.writeValueAsString(generator.generateOverview()));
+        final Map<String, Object> overview = generator.generateOverview();
+
+        final List<Map<String, Object>> apis = retrieveApis(overview);
+
+        apis.forEach(api -> {
+            final String path = pathFromApi(api);
+            try {
+                log("Writing " + path);
+                final Map<String, Object> apiResponse = generator.generateForRoute(path, "/");
+                writeJsonToFile(targetName + path + ".json", apiResponse);
+            } catch (IOException e) {
+                log("Unable to write " + targetName + path + ":" + e);
+            }
+        });
+        log("Done.");
+    }
+
+    private static String pathFromApi(Map<String, Object> api) {
+        return (String)api.get("path");
+    }
+    private static List<Map<String, Object>> retrieveApis(Map<String, Object> overview) {
+        return overview.containsKey("apis")
+                ? (List<Map<String, Object>>)overview.get("apis")
+                : Collections.emptyList();
+    }
+
+    private static void writeJsonToFile(String filename, Object content) throws IOException {
+        final File file = new File(filename);
+        file.getParentFile().mkdirs();
+        file.createNewFile();
+
+        final FileOutputStream outputStream = new FileOutputStream(file);
+
+        objectMapper.writeValue(outputStream, content);
     }
 }
