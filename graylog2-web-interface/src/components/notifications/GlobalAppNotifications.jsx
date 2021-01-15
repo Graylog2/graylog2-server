02ac30827d59faad70d57759b2bffbb5af6f375e
Linus Pahl <linus@graylog.com>
2021-01-15 09:55:09 +0100

    Converting `GlobalAppNotifications` to a functional component.

--- graylog2-web-interface/src/components/notifications/GlobalAppNotifications.jsx
+++ graylog2-web-interface/src/components/notifications/GlobalAppNotifications.jsx
@@ -17,34 +17,32 @@
 import React from 'react';
 import { PluginStore } from 'graylog-web-plugin/plugin';
 
-class GlobalAppNotifications extends React.Component {
-  render() {
-    const globalNotifications = PluginStore.exports('globalNotifications')
-      .map((notification) => {
-        if (!notification.component) {
-          // eslint-disable-next-line no-console
-          console.error('Missing "component" for globalNotification plugin:', notification);
+const GlobalAppNotifications = () => {
+  const globalNotifications = PluginStore.exports('globalNotifications')
+    .map((notification) => {
+      if (!notification.component) {
+        // eslint-disable-next-line no-console
+        console.error('Missing "component" for globalNotification plugin:', notification);
 
-          return null;
-        }
+        return null;
+      }
 
-        const Component = notification.component;
+      const Component = notification.component;
 
-        if (!notification.key) {
-          // eslint-disable-next-line no-console
-          console.warn('Missing "key" for globalNotification plugin:', notification);
-        }
+      if (!notification.key) {
+        // eslint-disable-next-line no-console
+        console.warn('Missing "key" for globalNotification plugin:', notification);
+      }
 
-        return <Component key={notification.key} />;
-      })
-      .filter((component) => !!component);
+      return <Component key={notification.key} />;
+    })
+    .filter((component) => !!component);
 
-    return (
-      <div id="global-notifications">
-        {globalNotifications}
-      </div>
-    );
-  }
-}
+  return (
+    <div id="global-notifications">
+      {globalNotifications}
+    </div>
+  );
+};
 
 export default GlobalAppNotifications;
