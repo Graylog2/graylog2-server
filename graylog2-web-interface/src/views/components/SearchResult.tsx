443dc1f5216f4feae8966b6a9a804bf29196db1b
Linus Pahl <linus@graylog.com>
2021-01-18 10:30:32 +0100

    Preventing styled component prop `hasFocusedWidget` forwarding in `SearchResult`.

--- graylog2-web-interface/src/views/components/SearchResult.tsx
+++ graylog2-web-interface/src/views/components/SearchResult.tsx
@@ -40,9 +40,9 @@ type IndicatorProps = {
   };
 };
 
-const StyledRow = styled(Row)(({ hasFocusedWidget }: { hasFocusedWidget: boolean }) => css`
-  height: ${hasFocusedWidget ? '100%' : 'auto'};
-  overflow: ${hasFocusedWidget ? 'auto' : 'visible'};
+const StyledRow = styled(Row)(({ $hasFocusedWidget }: { $hasFocusedWidget: boolean }) => css`
+  height: ${$hasFocusedWidget ? '100%' : 'auto'};
+  overflow: ${$hasFocusedWidget ? 'auto' : 'visible'};
   margin-bottom: 10px;
 `);
 
