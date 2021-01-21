85e3d3f95071a6e01bf203c19015025c7dfd45c9
Dennis Oelkers <dennis@graylog.com>
2021-01-21 16:57:13 +0100

    Removing duplicate license header.

--- graylog-storage-elasticsearch6/src/test/java/org/graylog/storage/elasticsearch6/jest/GraylogJestRetryHandlerTest.java
+++ graylog-storage-elasticsearch6/src/test/java/org/graylog/storage/elasticsearch6/jest/GraylogJestRetryHandlerTest.java
@@ -14,22 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-/**
- * This file is part of Graylog.
- *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 package org.graylog.storage.elasticsearch6.jest;
 
 import io.searchbox.client.JestRetryHandler;
