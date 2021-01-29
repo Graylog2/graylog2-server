0723a7237b45a9502c3afdf58bd0daf8372a0ce0
Konrad Merz <konrad@graylog.com>
2021-01-29 10:58:38 +0100

    Fix export to dashboard when graylog is behind a proxy
    ## Motivation
    Prior to this change, when loading the new dashboard containing the
    previous search we used a not qualified route, which did not take the
    prefix in account.
    
    ## Description
    This change will use `Routes` to set the new pathname for history.push.

--- graylog2-web-interface/src/views/logic/views/Actions.js
+++ graylog2-web-interface/src/views/logic/views/Actions.js
@@ -17,7 +17,6 @@
 // @flow strict
 import history from 'util/History';
 import Routes from 'routing/Routes';
-import { newDashboardsPath } from 'views/Constants';
 import View from 'views/logic/views/View';
 
 export const loadNewView = () => history.push(`${Routes.SEARCH}/new`);
@@ -31,7 +30,7 @@ export const loadView = (viewId: string) => history.push(`${Routes.SEARCH}/${vie
 export const loadDashboard = (dashboardId: string) => history.push(Routes.pluginRoute('DASHBOARDS_VIEWID')(dashboardId));
 
 export const loadAsDashboard = (view: View) => history.push({
-  pathname: newDashboardsPath,
+  pathname: Routes.pluginRoute('DASHBOARDS_NEW'),
   state: {
     view,
   },
