68372eb0665eb7d08b743be23924bd896954e753
Dennis Oelkers <dennis@graylog.com>
2021-02-05 09:58:26 +0100

    Fixing default import of `FormsUtils`.

--- graylog2-web-interface/src/components/lookup-tables/LookupTableForm.jsx
+++ graylog2-web-interface/src/components/lookup-tables/LookupTableForm.jsx
@@ -21,7 +21,7 @@ import _ from 'lodash';
 import { Col, Row, Button } from 'components/graylog';
 import { Input } from 'components/bootstrap';
 import ObjectUtils from 'util/ObjectUtils';
-import FormsUtils from 'util/FormsUtils';
+import * as FormsUtils from 'util/FormsUtils';
 import { JSONValueInput } from 'components/common';
 import { CachesContainer, CachePicker, DataAdaptersContainer, DataAdapterPicker } from 'components/lookup-tables';
 import CombinedProvider from 'injection/CombinedProvider';
