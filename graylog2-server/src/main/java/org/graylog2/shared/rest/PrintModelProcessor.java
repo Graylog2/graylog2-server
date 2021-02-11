a768237676e85c3486960a711a9bd6bc4563a078
Bernd Ahlers <bernd@graylog.com>
2021-02-11 14:29:07 +0100

    Remove accidental changes

--- graylog2-server/src/main/java/org/graylog2/shared/rest/PrintModelProcessor.java
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/PrintModelProcessor.java
@@ -28,8 +28,6 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.core.Configuration;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
