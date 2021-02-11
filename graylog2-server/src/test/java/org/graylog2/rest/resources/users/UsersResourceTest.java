8eab80e7c0fdcf6094b3417c73bedcdb0bc576d1
Dan Torrey <dan@graylog.com>
2021-02-11 15:21:37 +0100

    Simplify test

--- graylog2-server/src/test/java/org/graylog2/rest/resources/users/UsersResourceTest.java
+++ graylog2-server/src/test/java/org/graylog2/rest/resources/users/UsersResourceTest.java
@@ -1,23 +1,16 @@
 package org.graylog2.rest.resources.users;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.eventbus.EventBus;
-import com.mongodb.client.MongoCollection;
 import org.apache.shiro.subject.Subject;
-import org.bson.Document;
 import org.bson.types.ObjectId;
-import org.graylog.grn.GRNRegistry;
-import org.graylog.security.PermissionAndRoleResolver;
 import org.graylog.testing.mongodb.MongoDBInstance;
 import org.graylog2.Configuration;
 import org.graylog2.configuration.HttpConfiguration;
-import org.graylog2.database.MongoConnection;
 import org.graylog2.plugin.database.ValidationException;
 import org.graylog2.rest.models.users.requests.CreateUserRequest;
 import org.graylog2.rest.models.users.requests.Startpage;
 import org.graylog2.rest.models.users.requests.UpdateUserPreferences;
 import org.graylog2.security.AccessTokenService;
-import org.graylog2.security.InMemoryRolePermissionResolver;
 import org.graylog2.security.MongoDBSessionService;
 import org.graylog2.security.PasswordAlgorithmFactory;
 import org.graylog2.security.hashing.SHA1HashPasswordAlgorithm;
@@ -27,8 +20,6 @@ import org.graylog2.shared.users.UserManagementService;
 import org.graylog2.users.PaginatedUserService;
 import org.graylog2.users.RoleService;
 import org.graylog2.users.UserImpl;
-import org.graylog2.users.UserManagementServiceImpl;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -42,10 +33,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -76,44 +66,21 @@ public class UsersResourceTest {
     @Mock
     private MongoDBSessionService sessionService;
     @Mock
-    private InMemoryRolePermissionResolver permissionResolver;
-    @Mock
-    private EventBus eventBus;
-    @Mock
-    private GRNRegistry grnRegistry;
-    @Mock
-    private PermissionAndRoleResolver roleResolver;
-    @Mock
     private Startpage startPage;
     @Mock
     private Subject subject;
+    @Mock
+    private UserManagementService userManagementService;
 
     UserImplFactory userImplFactory;
-
-    UserManagementService userManagementService;
-
-    MongoCollection<Document> collection;
-
-    MongoConnection mongoConnection;
 
     @Before
     public void setUp() throws Exception {
         final Configuration configuration = new Configuration();
         userImplFactory = new UserImplFactory(configuration,
                                               new Permissions(ImmutableSet.of(new RestPermissions())));
-        mongoConnection = mongodb.mongoConnection();
-        collection = mongodb.mongoConnection().getMongoDatabase().getCollection("inputs");
-        userManagementService = spy(new UserManagementServiceImpl(mongoConnection, configuration,
-                                                                  roleService, accessTokenService, userImplFactory,
-                                                                  permissionResolver, eventBus, grnRegistry,
-                                                                  roleResolver));
         usersResource = new TestUsersResource(userManagementService, paginatedUserService, accessTokenService,
                                               roleService, sessionService, new HttpConfiguration(), subject);
-    }
-
-    @After
-    public void teardown() {
-        mongoConnection.getMongoDatabase().drop();
     }
 
     /**
@@ -122,16 +89,16 @@ public class UsersResourceTest {
      */
     @Test
     public void createSuccess() throws ValidationException {
+        when(userManagementService.create()).thenReturn(userImplFactory.create(new HashMap<>()));
         final Response response = usersResource.create(buildCreateUserRequest());
         Assert.assertEquals(201, response.getStatus());
-        // For some reason, verifying `save` here does not work.
-        verify(userManagementService, times(1)).validate(isA(UserImpl.class), anyMap());
+        verify(userManagementService, times(1)).create(isA(UserImpl.class));
     }
 
     @Test
     public void savePreferencesSuccess() throws ValidationException {
         when(subject.isPermitted(anyString())).thenReturn(true);
-        usersResource.create(buildCreateUserRequest());
+        when(userManagementService.load(eq(USERNAME))).thenReturn(userImplFactory.create(new HashMap<>()));
         usersResource.savePreferences(USERNAME, UpdateUserPreferences.create(new HashMap<>()));
         verify(userManagementService, times(1)).save(isA(UserImpl.class));
     }
@@ -191,6 +158,5 @@ public class UsersResourceTest {
         public UserImpl.LocalAdminUser createLocalAdminUser(String adminRoleObjectId) {
             return null;
         }
-
     }
 }
