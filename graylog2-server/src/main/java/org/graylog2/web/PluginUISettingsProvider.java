b2d801de3441d8a7eb983ad14dbd0c26e9084ff5
Rob Curtis <rob.curtis@graylog.com>
2021-02-17 10:48:59 -0500

    Update documentation on pluggable interface

--- graylog2-server/src/main/java/org/graylog2/web/PluginUISettingsProvider.java
+++ graylog2-server/src/main/java/org/graylog2/web/PluginUISettingsProvider.java
@@ -21,13 +21,20 @@ import java.util.Map;
 /**
  * Some plugins may have dynamic data that needs to be made available to the UI at server startup.  This interface
  * provides a mechanism for providing that data via the generated config.js file's pluginUISettings map.  When an
- * instance of this interface is registered via the Graylog2Module.addPluginUISettingsProvider() method, an entry will
+ * instance of this interface is registered via the PluginModule.addPluginUISettingsProvider() method, an entry will
  * be added to the pluginUISettings map using the key and JSON data the instance provides.
+ *
+ * When registering a new PluginUISettingsProvider via addPluginUISettingsProvider(), you must register with a unique
+ * providerKey value.  To help guarantee uniqueness, we recommend following Java package naming standards.  As an
+ * example, a new Graylog plugin might use the key: "org.graylog.newuiplugin".
  */
 public interface PluginUISettingsProvider {
 
     /**
-     * @return The JSON data to be stored in the pluginUISettings map in config.js
+     * This method returns the UI settings data for your plugin as a Map which will be converted to JSON and
+     * surfaced via the generated config.js file at server startup.
+     *
+     * @return The settings data to be included in config.js
      */
     Map<String,Object> pluginSettings();
 }
