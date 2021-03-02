9b708cdb7bde2479dbe4c98e9e3e60f635b6b63b
Konrad Merz <konrad@graylog.com>
2021-03-02 16:24:13 +0100

    Fix rendering of trend color in report generation (#10157)
    * Fix rendering of trend color in report generation
    
    ## Motivation
    The report generation is using the chrome print to pdf functionallity
    which will turn all backgrounds in css to white.
    
    ## Description
    We add an !important to the css rule (and explain why) and add the
    magical webkit rule which tells chrome to keep the background as it is.
    
    * Fix trend forground color
    
    Before the forground color of the rendered pdf was black.
    
    Now its WHITE :D
    
    * Fix style-lint
    
    * Fix type annotation
    
    * Fix merge of trend background
    
    * Adjust to 3.3 theme interface

--- graylog2-web-interface/src/views/components/visualizations/number/Trend.jsx
+++ graylog2-web-interface/src/views/components/visualizations/number/Trend.jsx
@@ -25,15 +25,36 @@ const Background: StyledComponent<{trend: ?string}, ThemeInterface, HTMLDivEleme
   return css`
     text-align: right;
     ${trend && css`
-      background-color: ${bgColor};
-      color: ${util.contrastingColor(bgColor)};
+      background-color: ${bgColor} !important; /* Needed for report generation */
+      color: ${util.contrastingColor(bgColor)} !important /* Needed for report generation */;
+
+      /* stylelint-disable-next-line property-no-vendor-prefix */
+      -webkit-print-color-adjust: exact !important; /* Needed for report generation */
     `}
   `;
 });
 
-const TextContainer = styled.span`
-  margin: 5px;
-`;
+const TextContainer: StyledComponent<{trend: ?string}, ThemeInterface, HTMLDivElement> = styled.div(({ theme, trend }) => {
+  const { variant } = theme.color;
+  const bgColor = trend && trend === TREND_GOOD ? variant.success : variant.primary;
+
+  return css`
+      margin: 5px;
+      color: ${util.contrastingColor(bgColor)} !important /* Needed for report generation */;
+
+      /* stylelint-disable-next-line property-no-vendor-prefix */
+      -webkit-print-color-adjust: exact !important; /* Needed for report generation */`;
+});
+
+const StyledIcon: StyledComponent<{trend: ?string}, ThemeInterface, typeof Icon> = styled(Icon)(({ theme, trend }) => {
+  const { variant } = theme.color;
+  const bgColor = trend && trend === TREND_GOOD ? variant.success : variant.primary;
+
+  return css`
+    path {
+      fill: ${util.contrastingColor(bgColor)};
+    }`;
+});
 
 const _background = (delta, trendPreference: TrendPreference) => {
   switch (trendPreference) {
@@ -47,12 +68,12 @@ const _background = (delta, trendPreference: TrendPreference) => {
   }
 };
 
-const _trendIcon = (delta) => {
+const _trendIcon = (delta, trend) => {
   if (delta === 0) {
-    return <Icon name="arrow-circle-right" />;
+    return <StyledIcon trend={trend} name="arrow-circle-right" />;
   }
 
-  return <Icon name={delta > 0 ? 'arrow-circle-up' : 'arrow-circle-down'} />;
+  return <StyledIcon trend={trend} name={delta > 0 ? 'arrow-circle-up' : 'arrow-circle-down'} />;
 };
 
 const Trend = React.forwardRef<Props, any>(({ current, previous, trendPreference }: Props, ref) => {
@@ -60,11 +81,11 @@ const Trend = React.forwardRef<Props, any>(({ current, previous, trendPreference
   const differencePercent = previous ? difference / previous : NaN;
 
   const backgroundTrend = _background(difference, trendPreference);
-  const trendIcon = _trendIcon(difference);
+  const trendIcon = _trendIcon(difference, backgroundTrend);
 
   return (
     <Background trend={backgroundTrend} data-test-id="trend-background">
-      <TextContainer ref={ref}>
+      <TextContainer trend={backgroundTrend} ref={ref}>
         {trendIcon} {numeral(difference).format('+0,0[.]0[000]')} / {numeral(differencePercent).format('+0[.]0[0]%')}
       </TextContainer>
     </Background>
