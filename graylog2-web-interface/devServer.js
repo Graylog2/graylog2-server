a78a86cce35b3b8ceddb7106d4977c28a0084363
Bernd Ahlers <bernd@graylog.com>
2021-03-05 14:08:36 +0100

    Fix linter warning

--- graylog2-web-interface/devServer.js
+++ graylog2-web-interface/devServer.js
@@ -44,6 +44,7 @@ const port = argv.port || DEFAULT_PORT;
 const apiUrl = argv.apiUrl || process.env.GRAYLOG_API_URL || DEFAULT_API_URL;
 
 // Proxy all "/api" requests to the server backend API.
+// eslint-disable-next-line no-console
 console.log(`Graylog web interface forwarding /api requests to ${apiUrl}`);
 app.use('/api', proxy(apiUrl, {
   proxyReqPathResolver(req) {
