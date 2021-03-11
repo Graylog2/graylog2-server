b69e5754865e63d9b5777aba0cfa5f715eb69c59
Linus Pahl <linus@graylog.com>
2021-03-11 16:18:57 +0100

    Fixing linter warnings.

--- graylog2-web-interface/src/views/components/aggregationwizard/actionConfiguration/GroupByConfiguration.tsx
+++ graylog2-web-interface/src/views/components/aggregationwizard/actionConfiguration/GroupByConfiguration.tsx
@@ -23,6 +23,7 @@ type Props = {
   onConfigChange: (newConfig: AggregationWidgetConfig) => void
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const GroupByConfiguration = ({ config, onConfigChange }: Props) => {
   return (
     <>
