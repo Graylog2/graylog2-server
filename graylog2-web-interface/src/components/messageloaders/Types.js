a9dfd7655a1d28d5b8f0e2921a74b288ed110d1c
Ousmane Samba <ousmane@graylog.com>
2021-03-17 15:21:27 +0100

    add types file

---
+++ graylog2-web-interface/src/components/messageloaders/Types.js
@@ -0,0 +1,28 @@
+// @flow strict
+export type Input = {
+    id: string,
+    title: string,
+    name: string,
+    type: string,
+    configuration: {
+      [type: string]: any,
+    },
+    input_profile_id: string,
+    version: number,
+    created_at: string,
+    content_pack?: boolean;
+  };
+
+export type Codec ={
+    type: string,
+    name: string,
+    requested_configuration: {
+        [key: string]: {
+            [key: string]: any,
+        },
+    },
+};
+
+export type CodecTypes = {
+  [key: string]: Codec,
+};
