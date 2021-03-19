eec1ecc75e2975dd60d66577d3420ace5807f265
Dennis Oelkers <dennis@graylog.com>
2021-03-19 14:44:13 +0100

    Using correct prop.

--- graylog2-web-interface/src/views/components/aggregationwizard/elementConfiguration/configurationFields/BooleanField.tsx
+++ graylog2-web-interface/src/views/components/aggregationwizard/elementConfiguration/configurationFields/BooleanField.tsx
@@ -4,18 +4,15 @@ import { Input } from 'components/bootstrap';
 import { Checkbox } from 'components/graylog';
 import { FieldComponentProps } from 'views/components/aggregationwizard/elementConfiguration/VisualizationConfigurationOptions';
 
-const BooleanField = ({ field, name, onChange, error, title, value }: FieldComponentProps) => {
-  return (
-    <Input id={`${name}-input`}
-           label={title}
-           error={error}
-           help={field.description}
-           labelClassName="col-sm-11"
-           wrapperClassName="col-sm-1">
-      <Checkbox id={`${name}-input`} name={name} onChange={onChange} checked={value} />
-    </Input>
-
-  );
-};
+const BooleanField = ({ field, name, onChange, error, title, value }: FieldComponentProps) => (
+  <Input id={`${name}-input`}
+         label={title}
+         error={error}
+         help={field.description}
+         labelClassName="col-sm-11"
+         wrapperClassName="col-sm-1">
+    <Checkbox id={`${name}-input`} name={name} onChange={onChange} defaultValue={value} />
+  </Input>
+);
 
 export default BooleanField;
