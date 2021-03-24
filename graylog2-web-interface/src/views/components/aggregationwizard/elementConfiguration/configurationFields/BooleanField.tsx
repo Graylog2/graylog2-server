a5f598db5f40c4a533b971d5c09e929ce6253253
Dennis Oelkers <dennis@graylog.com>
2021-03-24 15:48:11 +0100

    Improve positioning of help block for boolean fields.

--- graylog2-web-interface/src/views/components/aggregationwizard/elementConfiguration/configurationFields/BooleanField.tsx
+++ graylog2-web-interface/src/views/components/aggregationwizard/elementConfiguration/configurationFields/BooleanField.tsx
@@ -17,18 +17,20 @@
 import * as React from 'react';
 
 import { Input } from 'components/bootstrap';
-import { Checkbox } from 'components/graylog';
+import { Checkbox, HelpBlock } from 'components/graylog';
 import { FieldComponentProps } from 'views/components/aggregationwizard/elementConfiguration/VisualizationConfigurationOptions';
 
 const BooleanField = ({ field, name, onChange, error, title, value }: FieldComponentProps) => (
-  <Input id={`${name}-input`}
-         label={title}
-         error={error}
-         help={field.description}
-         labelClassName="col-sm-11"
-         wrapperClassName="col-sm-1">
-    <Checkbox id={`${name}-input`} name={name} onChange={onChange} defaultValue={value} />
-  </Input>
+  <>
+    <Input id={`${name}-input`}
+           label={title}
+           error={error}
+           labelClassName="col-sm-11"
+           wrapperClassName="col-sm-1">
+      <Checkbox id={`${name}-input`} name={name} onChange={onChange} defaultValue={value} />
+    </Input>
+    <HelpBlock>{field.description}</HelpBlock>
+  </>
 );
 
 export default BooleanField;
