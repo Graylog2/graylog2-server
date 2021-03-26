a84cf4c463c0223981a296dfea58ee98d3ded929
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-03-26 12:41:21 -0500

    Adding PublicNotifications modal form w/ example

--- graylog2-web-interface/src/components/graylog/Alert.tsx
+++ graylog2-web-interface/src/components/graylog/Alert.tsx
@@ -43,6 +43,12 @@ const Alert = styled(BootstrapAlert)(({ bsStyle = 'default', theme }: { bsStyle:
         text-decoration: none;
       }
     }
+
+    &.alert-dismissible {
+      .close {
+        top: -9px;
+      }
+    }
   `;
 });
 
