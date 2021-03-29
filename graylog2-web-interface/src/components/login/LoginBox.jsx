c0c8a5e5d3fce9ce0e53481e7fc6834346486743
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-03-29 13:52:58 -0500

    output notification on Login

--- graylog2-web-interface/src/components/login/LoginBox.jsx
+++ graylog2-web-interface/src/components/login/LoginBox.jsx
@@ -20,13 +20,31 @@ import styled, { css } from 'styled-components';
 
 import { Col, Row } from 'components/graylog';
 
+import NotificationExample from '../../../../../graylog-plugin-enterprise/enterprise/src/web/customization/public-notifications/NotificationExample.tsx';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  
+  .row {
+    width: 100%;
+  }
+  
+  &::before,
+  &::after {
+    content: none;
+  }
+`;
+
 const LoginCol = styled(Col)(({ theme }) => css`
   padding: 15px;
   background-color: ${theme.colors.global.contentBackground};
   border: 1px solid ${theme.colors.variant.light.default};
   border-radius: 4px;
   box-shadow: 0 0 21px ${theme.colors.global.navigationBoxShadow};
-  margin-top: 120px;
   
   legend {
     color: ${theme.colors.variant.darker.default};
@@ -36,13 +54,33 @@ const LoginCol = styled(Col)(({ theme }) => css`
 
 const LoginBox = ({ children }) => {
   return (
-    <div className="container">
+    <Wrapper className="container">
+      <Row>
+        <Col md={8} mdOffset={2}>
+          <NotificationExample bsStyle="info"
+                               shortMessage="By doing this stuff you agree to things"
+                               longMessage="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                             A aperiam at atque consequatur eaque eum exercitationem modi officia quaerat quasi?
+
+                             Aliquid aspernatur beatae excepturi itaque necessitatibus porro quia repudiandae sunt.
+                             Beatae dignissimos doloribus, earum fuga illum iusto nemo nobis qui sapiente tempora, ullam vero.
+
+                             A cum deleniti, dicta dignissimos illum incidunt inventore labore nam necessitatibus odio pariatur quisquam voluptas voluptate.
+
+                             Accusamus adipisci aperiam at cumque deleniti distinctio dolorem est eum eveniet fugit ipsa, iure labore laboriosam nihil numquam obcaecati praesentium quaerat quia quisquam reprehenderit rerum sapiente sint tempora temporibus ut?
+
+                             Aliquam asperiores atque blanditiis ducimus, eveniet facere in iste laudantium libero magni non obcaecati officiis praesentium similique sit sunt tenetur totam voluptate voluptatem voluptatibus? Dolores illum natus quae qui? Cumque.
+
+                             Deserunt eum eveniet modi sapiente vel. Amet consectetur corporis delectus dolores et eveniet id molestiae, mollitia nam nisi nobis obcaecati placeat repudiandae sit, ut velit voluptatibus? Expedita, quam qui. Quos."
+                               title="IS User Agreement" />
+        </Col>
+      </Row>
       <Row>
         <LoginCol md={4} mdOffset={4} xs={6} xsOffset={3}>
           {children}
         </LoginCol>
       </Row>
-    </div>
+    </Wrapper>
   );
 };
 
