bcc7e9f8dfed4fb333a85b49b8610ff44697df3b
Dennis Oelkers <dennis@graylog.com>
2021-03-30 10:30:39 +0200

    Removing html from license header.

--- graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/searchtypes/pivot/ESPivot.java
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/searchtypes/pivot/ESPivot.java
@@ -1,15 +1,15 @@
 /*
  * Copyright (C) 2020 Graylog, Inc.
- * <p>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the Server Side Public License, version 1,
  * as published by MongoDB, Inc.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * Server Side Public License for more details.
- * <p>
+ *
  * You should have received a copy of the Server Side Public License
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
