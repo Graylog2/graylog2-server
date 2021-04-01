b6844857ced1975e3aa0caffc17f675dbdef68dc
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-04-01 15:11:07 -0500

    Make Dashboard Tabs responsive

--- graylog2-web-interface/src/views/components/QueryTabs.tsx
+++ graylog2-web-interface/src/views/components/QueryTabs.tsx
@@ -19,39 +19,37 @@ import * as Immutable from 'immutable';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
-import { Tab, Tabs, Col, Row } from 'components/graylog';
+import { Col, Row, Nav, NavItem, NavDropdown, MenuItem } from 'components/graylog';
+import { Icon } from 'components/common';
 import QueryTitle from 'views/components/queries/QueryTitle';
 import QueryTitleEditModal from 'views/components/queries/QueryTitleEditModal';
 import Query, { QueryId } from 'views/logic/queries/Query';
 import type { TitlesMap } from 'views/stores/TitleTypes';
 import ViewState from 'views/logic/views/ViewState';
 
-const StyledQueryTabs = styled(Tabs)(({ theme }) => css`
-  .tab-pane {
-    display: none;
+const StyledQueryNav = styled(Nav)(({ theme }) => css`
+  border-bottom: 0;
+  display: flex;
+  white-space: nowrap;
+  z-index: 2; // without it renders under widget management icons
+  position: relative;
+
+  > li {
+    > a {
+      border-color: ${theme.colors.gray[80]};
+    }
   }
 
-  > .nav-tabs {
-    border-bottom: 0;
+  > li.active {
+    z-index: 1;
 
-    > li {
-      > a {
-        border-color: ${theme.colors.gray[80]};
-      }
-    }
-
-    > li.active {
-      z-index: 1;
-
-      > a {
-        border-bottom-color: transparent;
-      }
+    > a {
+      border-bottom-color: transparent;
     }
   }
 `);
 
 type Props = {
-  children: React.ReactNode,
   onRemove: (queryId: string) => Promise<void> | Promise<ViewState>,
   onSelect: (queryId: string) => Promise<Query> | Promise<string>,
   onTitleChange: (queryId: string, newTitle: string) => Promise<TitlesMap>,
@@ -60,11 +58,37 @@ type Props = {
   titles: Immutable.Map<string, string>,
 };
 
+const adjustTabs = () => {
+  const dashboardTabs = document.querySelector('#dashboard-tabs') as HTMLElement;
+  const tabItems = dashboardTabs.querySelectorAll('li:not(.dropdown):not(.query-tabs-new):not(.dropdown-menu li)');
+  const moreItems = dashboardTabs.querySelectorAll('li.dropdown .dropdown-menu li');
+  const moreBtn = dashboardTabs.querySelector('li.query-tabs-more') as HTMLElement;
+  const newBtn = dashboardTabs.querySelector('li.query-tabs-new') as HTMLElement;
+
+  let maxWidth = moreBtn.offsetWidth + newBtn.offsetWidth;
+  const hiddenItems = [];
+  const primaryWidth = dashboardTabs.offsetWidth;
+
+  tabItems.forEach((tabItem: HTMLElement, idx) => {
+    if (primaryWidth >= maxWidth + tabItem.offsetWidth) {
+      maxWidth += tabItem.offsetWidth;
+    } else {
+      tabItem.classList.add('hidden');
+      hiddenItems.push(idx);
+    }
+  });
+
+  moreItems.forEach((moreItem: HTMLElement, idx) => {
+    if (hiddenItems.includes(idx)) {
+      moreItem.classList.remove('hidden');
+    }
+  });
+};
+
 class QueryTabs extends React.Component<Props> {
   private queryTitleEditModal: QueryTitleEditModal | undefined | null;
 
   static propTypes = {
-    children: PropTypes.node,
     onRemove: PropTypes.func.isRequired,
     onSelect: PropTypes.func.isRequired,
     onTitleChange: PropTypes.func.isRequired,
@@ -73,8 +97,9 @@ class QueryTabs extends React.Component<Props> {
     titles: PropTypes.object.isRequired,
   }
 
-  static defaultProps = {
-    children: null,
+  componentDidMount() {
+    adjustTabs();
+    window.addEventListener('resize', adjustTabs);
   }
 
   openTitleEditModal = (activeQueryTitle: string) => {
@@ -85,7 +110,6 @@ class QueryTabs extends React.Component<Props> {
 
   render() {
     const {
-      children,
       onRemove,
       onSelect,
       onTitleChange,
@@ -94,7 +118,7 @@ class QueryTabs extends React.Component<Props> {
       titles,
     } = this.props;
     const queryTitles = titles;
-    const queryTabs = queries.map((id, index) => {
+    const queryTabs = (itemType: 'navItem' | 'menuItem') => queries.map((id, index) => {
       const title = queryTitles.get(id, `Page#${index + 1}`);
       const tabTitle = (
         <QueryTitle active={id === selectedQueryId}
@@ -104,28 +128,27 @@ class QueryTabs extends React.Component<Props> {
                     title={title} />
       );
 
-      return (
-        <Tab eventKey={id}
-             key={id}
-             mountOnEnter
-             title={tabTitle}
-             unmountOnExit>
-          {children}
-        </Tab>
-      );
+      const output = {
+        navItem: <NavItem eventKey={id} key={id} onClick={() => onSelect(id)}>{tabTitle}</NavItem>,
+        menuItem: <MenuItem eventKey={id} key={id} onClick={() => onSelect(id)} className="hidden">{tabTitle}</MenuItem>,
+      };
+
+      return output[itemType];
     });
-    const newTab = <Tab key="new" eventKey="new" title="+" />;
-    const tabs = [queryTabs, newTab];
+    const newTab = <NavItem key="new" eventKey="new" onClick={() => onSelect('new')} className="query-tabs-new"><Icon name="plus" /></NavItem>;
+    const dropDownTabs = queryTabs('menuItem');
 
     return (
       <Row style={{ marginBottom: 0 }}>
         <Col>
-          <StyledQueryTabs activeKey={selectedQueryId}
-                           animation={false}
-                           id="dashboard-pages"
-                           onSelect={onSelect}>
-            {tabs}
-          </StyledQueryTabs>
+          <StyledQueryNav bsStyle="tabs" activeKey={selectedQueryId} id="dashboard-tabs">
+            {queryTabs('navItem')}
+            <NavDropdown eventKey="more" title={<Icon name="ellipsis-h" />} className="query-tabs-more" noCaret pullRight>
+              {dropDownTabs}
+            </NavDropdown>
+            {newTab}
+          </StyledQueryNav>
+
           {/*
           The title edit modal can't be part of the QueryTitle component,
           due to the react bootstrap tabs keybindings.
