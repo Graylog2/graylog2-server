5be84a1b5bb4a64c062f7c3fb58c6f112d6b41b2
Linus Pahl <linus@graylog.com>
2021-04-09 11:53:12 +0200

    Renaming `ElementConfigurationSection` to `ElementConfigurationContainer`.

--- graylog2-web-interface/src/views/components/aggregationwizard/elementConfigurationSections/ElementConfigurationContainer.test.tsx
+++ graylog2-web-interface/src/views/components/aggregationwizard/elementConfigurationSections/ElementConfigurationContainer.test.tsx
@@ -17,7 +17,7 @@
 import React from 'react';
 import { fireEvent, render, screen } from 'wrappedTestingLibrary';
 
-import ElementConfigurationSection from './ElementConfigurationSection';
+import ElementConfigurationContainer from './ElementConfigurationContainer';
 
 describe('ElementConfigurationSection', () => {
   it('should render elements passed as children', () => {
