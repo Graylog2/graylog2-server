9edc68e8d1b7574b75aa949aabc10b50c79f0151
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-04-12 16:24:41 -0500

    note about "failing" lint

--- graylog2-web-interface/src/components/graylog/Nav.jsx
+++ graylog2-web-interface/src/components/graylog/Nav.jsx
@@ -64,7 +64,7 @@ const Nav = styled(BootstrapNav)(({ theme }) => css`
       }
     }
 
-    &${navTabsStyles}
+    &${navTabsStyles} /* This is a known non-issue that stylelint won't ignore but will hopefully be patched soon https://github.com/stylelint/stylelint/issues/4574 */
   }
 `);
 
