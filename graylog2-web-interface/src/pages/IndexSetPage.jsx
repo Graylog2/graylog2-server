b722eb452cb17c53178b32562f2486e9320f42cf
Ousmane Samba <ousmane@graylog.com>
2021-04-22 12:24:57 +0200

    refactore indexSetPage

--- graylog2-web-interface/src/pages/IndexSetPage.jsx
+++ graylog2-web-interface/src/pages/IndexSetPage.jsx
@@ -14,10 +14,8 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import PropTypes from 'prop-types';
 import React from 'react';
-import createReactClass from 'create-react-class';
-import Reflux from 'reflux';
+import PropTypes from 'prop-types';
 import numeral from 'numeral';
 
 import HideOnCloud from 'util/conditional/HideOnCloud';
@@ -31,53 +29,57 @@ import DocsHelper from 'util/DocsHelper';
 import CombinedProvider from 'injection/CombinedProvider';
 import Routes from 'routing/Routes';
 import withParams from 'routing/withParams';
+import connect from 'stores/connect';
 
 const { IndexSetsStore, IndexSetsActions } = CombinedProvider.get('IndexSets');
 const { IndicesStore, IndicesActions } = CombinedProvider.get('Indices');
 const { IndexerOverviewStore, IndexerOverviewActions } = CombinedProvider.get('IndexerOverview');
 
-const IndexSetPage = createReactClass({
-  displayName: 'IndexSetPage',
+const REFRESH_INTERVAL = 2000;
 
-  propTypes: {
+class IndexSetPage extends React.Component {
+  static propTypes = {
     params: PropTypes.object.isRequired,
-  },
+    indexSet: PropTypes.object,
+    indexerOverview: PropTypes.object,
+    indexerOverviewError: PropTypes.object,
+  };
 
-  mixins: [
-    Reflux.connect(IndexSetsStore),
-    Reflux.connect(IndicesStore, 'indexDetails'),
-    Reflux.connect(IndexerOverviewStore),
-  ],
+  static defaultProps = {
+    indexerOverview: undefined,
+    indexerOverviewError: undefined,
+    indexSet: undefined,
+  }
 
-  getInitialState() {
-    return {
-      indexSet: undefined,
-    };
-  },
+  constructor(props) {
+    super(props);
+    this.indexSet = undefined;
+  }
 
   componentDidMount() {
-    IndexSetsActions.get(this.props.params.indexSetId);
-    IndicesActions.list(this.props.params.indexSetId);
+    const { params: { indexSetId } } = this.props;
+    IndexSetsActions.get(indexSetId);
+    IndicesActions.list(indexSetId);
 
     this.timerId = setInterval(() => {
       IndicesActions.multiple();
-      IndexerOverviewActions.list(this.props.params.indexSetId);
-    }, this.REFRESH_INTERVAL);
-  },
+      IndexerOverviewActions.list(indexSetId);
+    }, REFRESH_INTERVAL);
+  }
 
   componentWillUnmount() {
     if (this.timerId) {
       clearInterval(this.timerId);
     }
-  },
+  }
 
-  REFRESH_INTERVAL: 2000,
+  _totalIndexCount = () => {
+    const { indexerOverview: indices } = this.props;
 
-  _totalIndexCount() {
-    return Object.keys(this.state.indexerOverview.indices).length;
-  },
+    return Object.keys(indices).length;
+  };
 
-  _renderElasticsearchUnavailableInformation() {
+  _renderElasticsearchUnavailableInformation = () => {
     return (
       <Row className="content">
         <Col md={8} mdOffset={2}>
@@ -99,21 +101,23 @@ const IndexSetPage = createReactClass({
         </Col>
       </Row>
     );
-  },
+  };
 
-  _isLoading() {
-    return !this.state.indexSet;
-  },
+  _isLoading = () => {
+    const { indexSet } = this.props;
+
+    return !indexSet;
+  };
 
   render() {
     if (this._isLoading()) {
       return <Spinner />;
     }
 
-    const { indexSet } = this.state;
+    const { indexSet, indexerOverview, indexerOverviewError, params: { indexSetId }, indexDetails: { indices, closedIndices } } = this.props;
 
     const pageHeader = (
-      <PageHeader title={`Index Set: ${indexSet.title}`}>
+      <PageHeader title={`Index Set: ${indexSet?.title}`}>
         <span>
           This is an overview of all indices (message stores) in this index set Graylog is currently taking in account
           for searches and analysis.
@@ -129,16 +133,16 @@ const IndexSetPage = createReactClass({
             <Button bsStyle="info">Index sets overview</Button>
           </LinkContainer>
           &nbsp;
-          <LinkContainer to={Routes.SYSTEM.INDEX_SETS.CONFIGURATION(indexSet.id, 'details')}>
+          <LinkContainer to={Routes.SYSTEM.INDEX_SETS.CONFIGURATION(indexSet?.id, 'details')}>
             <Button bsStyle="info">Edit Index Set</Button>
           </LinkContainer>
           &nbsp;
-          <IndicesMaintenanceDropdown indexSetId={this.props.params.indexSetId} indexSet={this.state.indexSet} />
+          <IndicesMaintenanceDropdown indexSetId={indexSetId} indexSet={indexSet} />
         </span>
       </PageHeader>
     );
 
-    if (this.state.indexerOverviewError) {
+    if (indexerOverviewError) {
       return (
         <span>
           {pageHeader}
@@ -150,28 +154,28 @@ const IndexSetPage = createReactClass({
     let indicesInfo;
     let indicesOverview;
 
-    if (this.state.indexerOverview && this.state.indexDetails.closedIndices) {
-      const deflectorInfo = this.state.indexerOverview.deflector;
+    if (indexerOverview && closedIndices) {
+      const deflectorInfo = indexerOverview.deflector;
 
       indicesInfo = (
         <span>
           <Alert bsStyle="success" style={{ marginTop: '10' }}>
             <Icon name="th" /> &nbsp;{this._totalIndexCount()} indices with a total of{' '}
-            {numeral(this.state.indexerOverview.counts.events).format('0,0')} messages under management,
+            {numeral(indexerOverview.counts.events).format('0,0')} messages under management,
             current write-active index is <i>{deflectorInfo.current_target}</i>.
           </Alert>
           <HideOnCloud>
-            <IndexerClusterHealthSummary health={this.state.indexerOverview.indexer_cluster.health} />
+            <IndexerClusterHealthSummary health={indexerOverview.indexer_cluster.health} />
           </HideOnCloud>
         </span>
       );
 
       indicesOverview = (
-        <IndicesOverview indices={this.state.indexerOverview.indices}
-                         indexDetails={this.state.indexDetails.indices}
-                         indexSetId={this.props.params.indexSetId}
-                         closedIndices={this.state.indexDetails.closedIndices}
-                         deflector={this.state.indexerOverview.deflector} />
+        <IndicesOverview indices={indexerOverview.indices}
+                         indexDetails={indices}
+                         indexSetId={indexSetId}
+                         closedIndices={closedIndices}
+                         deflector={indexerOverview.deflector} />
       );
     } else {
       indicesInfo = <Spinner />;
@@ -199,7 +203,20 @@ const IndexSetPage = createReactClass({
         </div>
       </DocumentTitle>
     );
-  },
-});
+  }
+}
 
-export default withParams(IndexSetPage);
+export default connect(
+  withParams(IndexSetPage),
+  {
+    indexSets: IndexSetsStore,
+    indexerOverview: IndexerOverviewStore,
+    indices: IndicesStore,
+  },
+  ({ indexSets, indexerOverview, indices }) => ({
+    indexSet: indexSets ? indexSets.indexSet : undefined,
+    indexerOverview: indexerOverview && indexerOverview.indexerOverview,
+    indexerOverviewError: indexerOverview && indexerOverview.indexerOverviewError,
+    indexDetails: indices,
+  }),
+);
