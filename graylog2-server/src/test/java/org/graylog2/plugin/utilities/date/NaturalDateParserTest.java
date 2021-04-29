337f1dc347abe264a74024d727944ad27fa62c1f
Jan Heise <jan.heise@graylog.com>
2021-04-29 12:18:02 +0200

    Changed TZ for tests to something that's probably not used in PROD removed tests that are not relevant in this context (User/VM)

--- graylog2-server/src/test/java/org/graylog2/plugin/utilities/date/NaturalDateParserTest.java
+++ graylog2-server/src/test/java/org/graylog2/plugin/utilities/date/NaturalDateParserTest.java
@@ -23,11 +23,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.jodatime.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -35,14 +30,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NaturalDateParserTest {
     private NaturalDateParser naturalDateParser;
-    private NaturalDateParser naturalDateParserBerlin;
+    private NaturalDateParser naturalDateParserAntarctica;
     private NaturalDateParser naturalDateParserUtc;
 
     @BeforeEach
     public void setUp() {
         naturalDateParser = new NaturalDateParser();
         naturalDateParserUtc = new NaturalDateParser("Etc/UTC");
-        naturalDateParserBerlin = new NaturalDateParser("Europe/Berlin");
+        naturalDateParserAntarctica = new NaturalDateParser("Antarctica/Palmer");
     }
 
     @Test
@@ -86,11 +81,11 @@ public class NaturalDateParserTest {
     }
 
     @Test
-    public void testEuropeBerlinTZ() throws Exception {
-        NaturalDateParser.Result today = naturalDateParserBerlin.parse("today");
+    public void testAntarcticaTZ() throws Exception {
+        NaturalDateParser.Result today = naturalDateParserAntarctica.parse("today");
         assertThat(today.getFrom()).as("From should not be null").isNotNull();
         assertThat(today.getTo()).as("To should not be null").isNotNull();
-        assertThat(today.getDateTimeZone().getID()).as("should have the Europe/Berlin as Timezone").isEqualTo("Europe/Berlin");
+        assertThat(today.getDateTimeZone().getID()).as("should have the Antarctica/Palmer as Timezone").isEqualTo("Antarctica/Palmer");
     }
 
     @Test
@@ -216,9 +211,9 @@ https://github.com/Graylog2/graylog2-server/issues/6857
     }
 
     @Test
-    public void testLast4hoursTZBerlin() throws Exception {
+    public void testLast4hoursTZAntarctica() throws Exception {
         DateTime reference = DateTime.now(DateTimeZone.UTC);
-        NaturalDateParser.Result last4 = naturalDateParserBerlin.parse("last 4 hours", reference.toDate());
+        NaturalDateParser.Result last4 = naturalDateParserAntarctica.parse("last 4 hours", reference.toDate());
         assertThat(last4.getFrom()).as("from should be exactly 4 hours in the past").isEqualTo(reference.minusHours(4));
         assertThat(last4.getTo()).as("to should be the reference date").isEqualTo(reference);
     }
@@ -226,25 +221,25 @@ https://github.com/Graylog2/graylog2-server/issues/6857
     @Test
     public void testLast4hoursArtificialReference() throws Exception {
         DateTime reference = DateTime.now(DateTimeZone.UTC).minusHours(7);
-        NaturalDateParser.Result last4 = naturalDateParserBerlin.parse("last 4 hours", reference.toDate());
+        NaturalDateParser.Result last4 = naturalDateParserAntarctica.parse("last 4 hours", reference.toDate());
         assertThat(last4.getFrom()).as("from should be exactly 4 hours in the past").isEqualTo(reference.minusHours(4));
         assertThat(last4.getTo()).as("to should be the reference date").isEqualTo(reference);
 
         reference = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss").withZoneUTC().parseDateTime("12.06.2021 09:45:23");
-        last4 = naturalDateParserBerlin.parse("last 4 hours", reference.toDate());
+        last4 = naturalDateParserAntarctica.parse("last 4 hours", reference.toDate());
         assertThat(last4.getFrom()).as("from should be exactly 4 hours in the past").isEqualTo(reference.minusHours(4));
         assertThat(last4.getTo()).as("to should be the reference date").isEqualTo(reference);
     }
 
     @Test
     public void testLast4hoursArtificialReferenceDSTChange() throws Exception {
-        DateTime reference = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss").withZone(DateTimeZone.forID("Europe/Berlin")).parseDateTime("28.03.2021 03:45:23");
-        NaturalDateParser.Result last4 = naturalDateParserBerlin.parse("last 4 hours", reference.toDate());
+        DateTime reference = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss").withZone(DateTimeZone.forID("Antarctica/Palmer")).parseDateTime("28.03.2021 03:45:23");
+        NaturalDateParser.Result last4 = naturalDateParserAntarctica.parse("last 4 hours", reference.toDate());
         assertThat(last4.getFrom()).as("from should be exactly 4 hours in the past").isEqualTo(reference.minusHours(4));
         assertThat(last4.getTo()).as("to should be the reference date").isEqualTo(reference);
 
-        reference = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss").withZone(DateTimeZone.forID("Europe/Berlin")).parseDateTime("31.10.2021 03:45:23");
-        last4 = naturalDateParserBerlin.parse("last 4 hours", reference.toDate());
+        reference = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss").withZone(DateTimeZone.forID("Antarctica/Palmer")).parseDateTime("31.10.2021 03:45:23");
+        last4 = naturalDateParserAntarctica.parse("last 4 hours", reference.toDate());
         assertThat(last4.getFrom()).as("from should be exactly 4 hours in the past").isEqualTo(reference.minusHours(4));
         assertThat(last4.getTo()).as("to should be the reference date").isEqualTo(reference);
     }
@@ -303,85 +298,4 @@ https://github.com/Graylog2/graylog2-server/issues/6857
     }
 
     // TODO: end of: these tests will have to change in the near future when the alignment to start/end of day will be included
-
-    /**
-     * Test to validate, that Natty's reference date/timezone (usually taken from the VM's env) and
-     * the user's requested timezone does not accidentially roll over during parsing of e.g. "today" (full day parsing).
-     * The day of the point of time where the parsing occurs differs in relation to the timezone.
-     *
-     * Here: VM is almost midnight at Apr, 9 UTC, User requests "today" at April, 10 in "Europe/Berlin"
-     */
-    @Test
-    public void testNattyReferenceDateDiffersFromUsersTZButDoesNotRollToADifferentDayEndOfDayVMRunsUTC() throws Exception {
-        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
-        isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        Date reference = isoFormat.parse("2021-04-09T23:59:00");
-
-        NaturalDateParser.Result today = naturalDateParserBerlin.parse("today", reference);
-        assertThat(today.getFrom()).as("From should not be null").isNotNull();
-        assertThat(today.getTo()).as("To should not be null").isNotNull();
-        assertThat(today.getDateTimeZone().getID()).as("should have the Europe/Berlin as Timezone").isEqualTo("Europe/Berlin");
-        assertThat(today.getFrom().dayOfMonth().get()).as("should be April, 10").isEqualTo(10);
-    }
-
-    /**
-     * Test to validate, that Natty's reference date/timezone (usually taken from the VM's env) and
-     * the user's requested timezone does not accidentially roll over during parsing of e.g. "today" (full day parsing).
-     * The day of the point of time where the parsing occurs differs in relation to the timezone.
-     *
-     * Here: VM is as bit after midnight at Apr, 9 UTC, User requests "today" at April, 9 in "Europe/Berlin"
-     */
-    @Test
-    public void testNattyReferenceDateDiffersFromUsersTZButDoesNotRollToADifferentDayStartOfDayVMRunsUTC() throws Exception {
-        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
-        isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        Date reference = isoFormat.parse("2021-04-09T00:01:00");
-
-
-        NaturalDateParser.Result today = naturalDateParserBerlin.parse("today", reference);
-        assertThat(today.getFrom()).as("From should not be null").isNotNull();
-        assertThat(today.getTo()).as("To should not be null").isNotNull();
-        assertThat(today.getDateTimeZone().getID()).as("should have the Europe/Berlin as Timezone").isEqualTo("Europe/Berlin");
-        assertThat(today.getFrom().dayOfMonth().get()).as("should be April, 9").isEqualTo(9);
-    }
-
-    /**
-     * Test to validate, that Natty's reference date/timezone (usually taken from the VM's env) and
-     * the user's requested timezone does not accidentially roll over during parsing of e.g. "today" (full day parsing).
-     * The day of the point of time where the parsing occurs differs in relation to the timezone.
-     *
-     * Here: VM is almost midnight at Apr, 9 Europe/Berlin, User requests "today" at April, 9 in "UTC"
-     */
-    @Test
-    public void testNattyReferenceDateDiffersFromUsersTZButDoesNotRollToADifferentDayEndOfDayUserIsUTC() throws Exception {
-        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
-        isoFormat.setTimeZone(TimeZone.getTimeZone("Europe/Berlin"));
-        Date reference = isoFormat.parse("2021-04-09T23:59:00");
-
-        NaturalDateParser.Result today = naturalDateParserBerlin.parse("today", reference);
-        assertThat(today.getFrom()).as("From should not be null").isNotNull();
-        assertThat(today.getTo()).as("To should not be null").isNotNull();
-        assertThat(today.getDateTimeZone().getID()).as("should have the Europe/Berlin as Timezone").isEqualTo("Europe/Berlin");
-        assertThat(today.getFrom().dayOfMonth().get()).as("should be April, 9").isEqualTo(9);
-    }
-
-    /**
-     * Test to validate, that Natty's reference date/timezone (usually taken from the VM's env) and
-     * the user's requested timezone does not accidentially roll over during parsing of e.g. "today" (full day parsing).
-     * The day of the point of time where the parsing occurs differs in relation to the timezone.
-     *
-     * Here: VM is almost midnight at Apr, 9 Europe/Berlin, User requests "today" at April, 9 in "UTC"
-     */
-    @Test
-    public void testNattyReferenceDateDiffersFromUsersTZButDoesNotRollToADifferentDayStartOfDayUserIsUTC() throws Exception {
-        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
-        isoFormat.setTimeZone(TimeZone.getTimeZone("Europe/Berlin"));
-        Date reference = isoFormat.parse("2021-04-09T00:01:00");
-
-        NaturalDateParser.Result today = naturalDateParserBerlin.parse("today", reference);
-        assertThat(today.getFrom()).as("From should not be null").isNotNull();
-        assertThat(today.getTo()).as("To should not be null").isNotNull();
-        assertThat(today.getDateTimeZone().getID()).as("should have the Europe/Berlin as Timezone").isEqualTo("Europe/Berlin");
-        assertThat(today.getFrom().dayOfMonth().get()).as("should be April, 9").isEqualTo(9);
-    }
 }
