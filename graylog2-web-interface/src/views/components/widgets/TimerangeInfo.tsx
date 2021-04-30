b6b99c509e70dd3b8b33b0460a658879b3c2eb70
Linus Pahl <linus@graylog.com>
2021-04-30 12:14:43 +0200

    Do not throw error on dashboards with undeclared query parameters.
    We've recently implemented a time range info for dashboard widgets. This
    PR is fixing a runtime error which occurred when no search types exist.
    This is the case when a filter with an undeclared parameter   is being
    applied.

--- graylog2-web-interface/src/views/components/widgets/TimerangeInfo.tsx
+++ graylog2-web-interface/src/views/components/widgets/TimerangeInfo.tsx
@@ -40,6 +40,9 @@ const Wrapper = styled.div(({ theme }) => css`
   width: max-content;
 `);
 
+const getEffectiveWidgetTimerange = (result, activeQuery, searchTypeId) => result?.results[activeQuery]
+  .searchTypes[searchTypeId]?.effective_timerange;
+
 const TimerangeInfo = ({ className, widget, activeQuery, widgetId }: Props) => {
   const { localizeTime } = useContext(TimeLocalizeContext);
   const { result, widgetMapping } = useStore(SearchStore);
@@ -51,9 +54,9 @@ const TimerangeInfo = ({ className, widget, activeQuery, widgetId }: Props) => {
   const configuredTimerange = timerangeToString(widget.timerange || DEFAULT_TIMERANGE, localizeTime);
 
   const searchTypeId = widgetId ? widgetMapping.get(widgetId).first() : undefined;
-  const effectiveTimerange = activeQuery && searchTypeId ? result?.results[activeQuery]
-    .searchTypes[searchTypeId].effective_timerange : {};
-  const effectiveTimerangeString = timerangeToString(effectiveTimerange, localizeTime);
+
+  const effectiveTimerange = (activeQuery && searchTypeId) ? getEffectiveWidgetTimerange(result, activeQuery, searchTypeId) : undefined;
+  const effectiveTimerangeString = effectiveTimerange ? timerangeToString(effectiveTimerange, localizeTime) : 'Effective widget time range is currently not available.';
 
   return (
     <Wrapper className={className}>
