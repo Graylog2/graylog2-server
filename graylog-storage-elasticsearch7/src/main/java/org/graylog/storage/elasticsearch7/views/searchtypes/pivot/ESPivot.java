07e2072a473343153c042b670eebd13e2b95f7dd
Jan Heise <jan.heise@graylog.com>
2021-05-03 13:38:24 +0200

    Added handling for the case when only columns are defined

--- graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/searchtypes/pivot/ESPivot.java
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/searchtypes/pivot/ESPivot.java
@@ -16,7 +16,6 @@
  */
 package org.graylog.storage.elasticsearch7.views.searchtypes.pivot;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import one.util.streamex.EntryStream;
 import org.graylog.plugins.views.search.Query;
@@ -366,105 +365,6 @@ public class ESPivot implements ESSearchTypeHandler<Pivot> {
         return queryResult.getHits().getTotalHits().value;
     }
 
-    /*
-        results from elasticsearch are nested so we need to recurse into the aggregation tree, but our result is a table, thus we need
-        to keep track of the current row keys manually
-         */
-    private void processRows(PivotResult.Builder resultBuilder,
-                             SearchResponse searchResult,
-                             ESGeneratedQueryContext queryContext,
-                             Pivot pivot,
-                             List<BucketSpec> remainingRows,
-                             ArrayDeque<String> rowKeys,
-                             HasAggregations aggregation) {
-        if (remainingRows.isEmpty()) {
-            // this is the last row group, so we need to fork into the columns if they exist.
-            // being here also means that `rowKeys` contains the maximum number of parts, one for each combination of row bucket keys
-            // we will always add the series for this bucket, because that's the entire point of row groups
-
-            final PivotResult.Row.Builder rowBuilder = PivotResult.Row.builder().key(ImmutableList.copyOf(rowKeys));
-            // do the same for columns as we did for the rows
-            processColumns(rowBuilder, searchResult, queryContext, pivot, pivot.columnGroups(), new ArrayDeque<>(), aggregation);
-
-            // also add the series for the entire row
-            // columnKeys is empty, because this is a rollup per row bucket, thus for all columns in that bucket (IOW it's not a leaf!)
-            if (pivot.rollup()) {
-                processSeries(rowBuilder, searchResult, queryContext, pivot, new ArrayDeque<>(), aggregation, true, "row-leaf");
-            }
-            resultBuilder.addRow(rowBuilder.source("leaf").build());
-        } else {
-            // this is not a leaf for the rows, so we add its key to the rowKeys and descend into the aggregation tree
-            // afterwards we'll check if we need to add rollup for intermediate buckets. not all clients need them so they can request
-            // to not calculate them
-            final BucketSpec currentBucket = remainingRows.get(0);
-
-            // this handler should never be missing, because we used it above to generate the query
-            // if it is missing for some weird reason, it's ok to fail hard here
-            final ESPivotBucketSpecHandler<? extends PivotSpec, ? extends Aggregation> handler = bucketHandlers.get(currentBucket.type());
-            final Aggregation aggregationResult = handler.extractAggregationFromResult(pivot, currentBucket, aggregation, queryContext);
-            final Stream<ESPivotBucketSpecHandler.Bucket> bucketStream = handler.handleResult(pivot, currentBucket, searchResult, aggregationResult, this, queryContext);
-            // for each bucket, recurse and eventually collect all the row keys. once we reach a leaf, we'll end up in the other if branch above
-            bucketStream.forEach(bucket -> {
-                // push the bucket's key and use its aggregation as the new source for sub-aggregations
-                rowKeys.addLast(bucket.key());
-                processRows(resultBuilder, searchResult, queryContext, pivot, tail(remainingRows), rowKeys, bucket.aggregation());
-                rowKeys.removeLast();
-            });
-            // also add the series for this row key if the client wants rollups
-            if (pivot.rollup()) {
-                final PivotResult.Row.Builder rowBuilder = PivotResult.Row.builder().key(ImmutableList.copyOf(rowKeys));
-                // columnKeys is empty, because this is a rollup per row bucket, thus for all columns in that bucket (IOW it's not a leaf!)
-                processSeries(rowBuilder, searchResult, queryContext, pivot, new ArrayDeque<>(), aggregation, true, "row-inner");
-                resultBuilder.addRow(rowBuilder.source("non-leaf").build());
-            }
-
-        }
-    }
-
-    private void processColumns(PivotResult.Row.Builder rowBuilder,
-                                SearchResponse searchResult,
-                                ESGeneratedQueryContext queryContext,
-                                Pivot pivot,
-                                List<BucketSpec> remainingColumns,
-                                ArrayDeque<String> columnKeys,
-                                HasAggregations aggregation) {
-        if (remainingColumns.isEmpty()) {
-            // this is the leaf cell of the pivot table, simply add all the series for the complete column key array
-            // in the rollup: false case, this is the only set of series that is going to be added to the result
-            // if we simply don't have any column groups, then don't bother adding the series, this is the special case that
-            // only row grouping was requested, and the rollup for rows is automatically added anyway. otherwise we'll end up
-            // with duplicate data entries
-            if (!columnKeys.isEmpty()) {
-                processSeries(rowBuilder, searchResult, queryContext, pivot, columnKeys, aggregation, false, "col-leaf");
-            }
-        } else {
-            // for a non-leaf column group, we need to recurse further into the aggregation tree
-            // and if rollup was requested we'll add intermediate series according to the column keys
-            final BucketSpec currentBucket = remainingColumns.get(0);
-
-            // this handler should never be missing, because we used it above to generate the query
-            // if it is missing for some weird reason, it's ok to fail hard here
-            final ESPivotBucketSpecHandler<? extends PivotSpec, ? extends Aggregation> handler = bucketHandlers.get(currentBucket.type());
-            final Aggregation aggregationResult = handler.extractAggregationFromResult(pivot, currentBucket, aggregation, queryContext);
-            final Stream<ESPivotBucketSpecHandler.Bucket> bucketStream = handler.handleResult(pivot, currentBucket, searchResult, aggregationResult, this, queryContext);
-
-            // for each bucket, recurse and eventually collect all the column keys. once we reach a leaf, we'll end up in the other if branch above
-            bucketStream.forEach(bucket -> {
-                // push the bucket's key and use its aggregation as the new source for sub-aggregations
-                columnKeys.addLast(bucket.key());
-                processColumns(rowBuilder, searchResult, queryContext, pivot, tail(remainingColumns), columnKeys, bucket.aggregation());
-                columnKeys.removeLast();
-            });
-            // also add the series for the base column key if the client wants rollups, the complete column key is processed in the leaf branch
-            // don't add the empty column key rollup, because that's not the correct bucket here, it's being done in the row-leaf code
-            if (pivot.rollup() && !columnKeys.isEmpty()) {
-                // columnKeys is not empty, because this is a rollup per column in a row
-                processSeries(rowBuilder, searchResult, queryContext, pivot, columnKeys, aggregation, true, "col-inner");
-            }
-
-        }
-    }
-
     private void processSeries(PivotResult.Row.Builder rowBuilder,
                                SearchResponse searchResult,
                                ESGeneratedQueryContext queryContext,
@@ -485,11 +385,6 @@ public class ESPivot implements ESSearchTypeHandler<Pivot> {
                     })
                     .forEach(rowBuilder::addValue);
         });
-    }
-
-    private static <T> List<T> tail(List<T> list) {
-        Preconditions.checkArgument(!list.isEmpty(), "List must not be empty!");
-        return list.subList(1, list.size());
     }
 
     /**
