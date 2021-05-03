41df4e3093a0fb3652175ad5895b5ec05fc5646a
Konrad Merz <konrad@graylog.com>
2021-05-03 15:39:52 +0200

    Fix unused import

--- graylog2-web-interface/src/views/components/aggregationwizard/aggregationElements/SortElement.test.ts
+++ graylog2-web-interface/src/views/components/aggregationwizard/aggregationElements/SortElement.test.ts
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import { SortFormValues, WidgetConfigFormValues } from 'views/components/aggregationwizard/WidgetConfigForm';
+import { WidgetConfigFormValues } from 'views/components/aggregationwizard/WidgetConfigForm';
 
 import SortElement from './SortElement';
 
