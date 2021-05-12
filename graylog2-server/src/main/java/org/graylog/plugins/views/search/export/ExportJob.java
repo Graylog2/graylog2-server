8877c0bedc8dda9ae5a468940794db9d6ee0be74
Dennis Oelkers <dennis@graylog.com>
2021-05-12 13:08:07 +0200

    Serialize `createdAt` field.

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/export/ExportJob.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/export/ExportJob.java
@@ -16,6 +16,7 @@
  */
 package org.graylog.plugins.views.search.export;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.joda.time.DateTime;
@@ -31,5 +32,7 @@ public interface ExportJob {
     String FIELD_ID = "_id";
     String FIELD_CREATED_AT = "created_at";
     String id();
+
+    @JsonProperty
     DateTime createdAt();
 }
