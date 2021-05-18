85983fb984b415c6ed5ccae60d6d11c2e8c93784
Gaya Kessler <gaya@theclevernode.com>
2021-05-18 09:45:26 +0200

    Add Okta backend config type and update active backend endpoint (#10662)
    * add okta backend config shape
    
    * user correct active backend endpoint

--- graylog2-web-interface/src/logic/authentication/okta/types.ts
+++ graylog2-web-interface/src/logic/authentication/okta/types.ts
@@ -1,9 +1,13 @@
+import { $PropertyType } from 'utility-types/dist/utility-types';
+
+import AuthenticationBackend from 'logic/authentication/AuthenticationBackend';
+
 export interface OktaBackendConfig {
   type: 'okta';
   oktaBaseUrl: string;
   clientId: string;
   clientSecret: string;
-  tokenVerifierConnectTimeout: number;
+  tokenVerifierConnectTimeout: string;
   callbackUrl: string;
 }
 
@@ -12,6 +16,16 @@ export interface OktaBackendConfigJson {
   okta_base_url: string;
   client_id: string;
   client_secret: string;
-  token_verifier_connect_timeout: number;
+  token_verifier_connect_timeout: string;
   callback_url: string;
+}
+
+export interface OktaBackend {
+  id: $PropertyType<AuthenticationBackend, 'id'>;
+  defaultRoles: $PropertyType<AuthenticationBackend, 'defaultRoles'>;
+  title: $PropertyType<AuthenticationBackend, 'title'>;
+  description: $PropertyType<AuthenticationBackend, 'description'>;
+  config: Omit<OktaBackendConfig, 'clientSecret'> & {
+    clientSecret: { is_set: boolean };
+  };
 }
