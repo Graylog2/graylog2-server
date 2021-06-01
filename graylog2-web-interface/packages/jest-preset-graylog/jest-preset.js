012dbf97bcc6cf617259719df4bac349856313a1
Dennis Oelkers <dennis@graylog.com>
2021-06-01 10:30:53 +0200

    Updating Jest packages to 27.

--- graylog2-web-interface/packages/jest-preset-graylog/jest-preset.js
+++ graylog2-web-interface/packages/jest-preset-graylog/jest-preset.js
@@ -51,6 +51,7 @@ module.exports = {
     '\\.(css|less)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve('./lib/mocking/fileMock.js'),
   },
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: [
     '.fixtures.[jt]s$',
   ],
