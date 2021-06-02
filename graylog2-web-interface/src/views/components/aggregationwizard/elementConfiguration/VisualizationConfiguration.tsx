c7112d1d85f11e7fe29cf0bf70ad5aac68fd5599
Dennis Oelkers <dennis@graylog.com>
2021-06-02 16:54:03 +0200

    Setting checkbox value to boolean by using custom `onChange`.

--- graylog2-web-interface/src/views/components/aggregationwizard/elementConfiguration/VisualizationConfiguration.tsx
+++ graylog2-web-interface/src/views/components/aggregationwizard/elementConfiguration/VisualizationConfiguration.tsx
@@ -99,7 +99,7 @@ const VisualizationConfiguration = () => {
       </Field>
       {isTimelineChart && supportsEventAnnotations && (
         <Field name="visualization.eventAnnotation">
-          {({ field: { name, value, onChange }, meta: { error } }) => (
+          {({ field: { name, value = false }, meta: { error } }) => (
             <Input id={`${name}-input`}
                    label="Show Event annotations"
                    error={error}
@@ -107,8 +107,8 @@ const VisualizationConfiguration = () => {
                    wrapperClassName="col-sm-1">
               <EventAnnotationCheckbox id={`${name}-input`}
                                        name={name}
-                                       onChange={onChange}
-                                       checked={value === true}
+                                       onChange={() => setFieldValue(name, !value)}
+                                       checked={value}
                                        className="pull-right" />
             </Input>
           )}
