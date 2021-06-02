3a5be0b88f961a1ee129d6e8faab9f4d39cf3757
Linus Pahl <linus@graylog.com>
2021-06-02 11:48:32 +0200

    Implementing async/await version of `suppressConsole` which is easier to understand.

--- graylog2-web-interface/test/helpers/suppressConsole.ts
+++ graylog2-web-interface/test/helpers/suppressConsole.ts
@@ -23,15 +23,16 @@
  *
  * @param {function} fn - the function which should be called after disabling `console.error` and before restoring it.
  */
-const suppressConsole = <T>(fn: () => T): Promise<T> => {
+const suppressConsole = async <R>(fn: () => R): Promise<R> => {
   /* eslint-disable no-console */
   const originalConsoleError = console.error;
   console.error = () => {};
 
-  return Promise.resolve(fn()).then((result) => result).finally(() => {
+  try {
+    return await fn();
+  } finally {
     console.error = originalConsoleError;
-  });
-
+  }
   /* eslint-enable no-console */
 };
 
