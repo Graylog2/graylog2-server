f936970ace8398c2099f247881b538880acda372
Linus Pahl <linus@graylog.com>
2021-06-25 14:37:13 +0200

    Migrating `MultiSelect` from class to functional component.

--- graylog2-web-interface/src/components/common/MultiSelect.jsx
+++ graylog2-web-interface/src/components/common/MultiSelect.jsx
@@ -22,18 +22,10 @@ import Select from 'components/common/Select';
  * Component that wraps and render a `Select` where multiple options can be selected. It passes all
  * props to the underlying `Select` component, so please look there to find more information about them.
  */
-class MultiSelect extends React.Component {
-  static propTypes = Select.propTypes;
+const MultiSelect = ({ onChange, ...rest }) => {
+  return <Select multi {...rest} />;
+};
 
-  _select = undefined;
+MultiSelect.propTypes = Select.propTypes;
 
-  getValue = () => {
-    return this._select.getValue();
-  };
-
-  render() {
-    return <Select ref={(c) => { this._select = c; }} multi {...this.props} />;
-  }
-}
-
-export default MultiSelect;
+return MultiSelect;
