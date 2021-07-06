df25c6c4bb68fd3c1dd647f64627018a7cbf920a
Konrad Merz <konrad@graylog.com>
2021-07-06 16:34:34 +0200

    Add to store

--- graylog2-web-interface/src/actions/pipelines/PipelinesActions.jsx
+++ graylog2-web-interface/src/actions/pipelines/PipelinesActions.jsx
@@ -19,6 +19,7 @@ import Reflux from 'reflux';
 const RulesActions = Reflux.createActions({
   delete: { asyncResult: true },
   list: { asyncResult: true },
+  listPaginated: { asyncResult: true },
   get: { asyncResult: true },
   save: { asyncResult: true },
   update: { asyncResult: true },
