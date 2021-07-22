ac45df7ebd94bc8c1dab05438d699b96d87af514
Marco Pfatschbacher <marco@graylog.com>
2021-07-22 13:53:46 +0200

    Special index sets should not be assigned as default
    This change introduces a new flag `can_be_default` which
    can be used in the UI to disable the "Set as default" action
    on these index sets.
    
    We also disallow this on the API call.

--- graylog2-server/src/main/java/org/graylog2/indexer/indexset/IndexSetConfig.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/indexset/IndexSetConfig.java
@@ -22,6 +22,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.ImmutableSet;
 import org.graylog.autovalue.WithBeanGetter;
 import org.graylog2.plugin.indexer.retention.RetentionStrategyConfig;
 import org.graylog2.plugin.indexer.rotation.RotationStrategyConfig;
@@ -53,6 +54,14 @@ public abstract class IndexSetConfig implements Comparable<IndexSetConfig> {
 
     private static final Duration DEFAULT_FIELD_TYPE_REFRESH_INTERVAL = Duration.standardSeconds(5L);
     public static final TemplateType DEFAULT_INDEX_TEMPLATE_TYPE = TemplateType.MESSAGES;
+    public static final ImmutableSet<TemplateType> ELEGIBLE_FOR_DEFAULT_INDEX = ImmutableSet.of(TemplateType.MESSAGES);
+
+    public static boolean indexCanBeDefault(@Nullable TemplateType templateType, boolean isWritable) {
+        TemplateType type = templateType == null ? DEFAULT_INDEX_TEMPLATE_TYPE : templateType;
+
+        // "isWritable == false" marks the restored-archive index set, which can also not be used as a default
+        return isWritable && ELEGIBLE_FOR_DEFAULT_INDEX.contains(type);
+    }
 
     public enum TemplateType {
         @JsonProperty("messages")
