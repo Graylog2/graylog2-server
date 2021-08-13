8e218278b3fbff74dcb4f72bd36ace258a7f2142
Dennis Oelkers <dennis@graylog.com>
2021-08-13 15:58:57 +0200

    Fixing linter hints.

--- graylog2-web-interface/src/views/components/common/WindowLeaveMessage.tsx
+++ graylog2-web-interface/src/views/components/common/WindowLeaveMessage.tsx
@@ -15,21 +15,17 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { useStore } from 'stores/connect';
 import ConfirmLeaveDialog from 'components/common/ConfirmLeaveDialog';
 import { ViewStore } from 'views/stores/ViewStore';
 
 const WindowLeaveMessage = () => {
-  const dirty = useStore(ViewStore, ({ dirty }) => dirty);
+  const dirty = useStore(ViewStore, (state) => state.dirty);
+
   return dirty
-    ? <ConfirmLeaveDialog question="Are you sure you want to leave the page? Any unsaved changes will be lost."/>
+    ? <ConfirmLeaveDialog question="Are you sure you want to leave the page? Any unsaved changes will be lost." />
     : null;
 };
 
-WindowLeaveMessage.propTypes = {
-  dirty: PropTypes.bool.isRequired,
-};
-
-export default WindowLeaveMessage
+export default WindowLeaveMessage;
