27407f02c8a74ad2c43cd112272457003d1f5b38
Dennis Oelkers <dennis@graylog.com>
2021-08-13 12:21:53 +0200

    Separating handler-based and external link actions.

--- graylog2-web-interface/src/views/components/actions/ActionMenuItem.tsx
+++ graylog2-web-interface/src/views/components/actions/ActionMenuItem.tsx
@@ -17,11 +17,19 @@
 import * as React from 'react';
 import { useContext } from 'react';
 import styled from 'styled-components';
+import { ActionContexts } from 'views/types';
 
 import { Icon } from 'components/common';
 import { MenuItem } from 'components/graylog';
 import WidgetFocusContext from 'views/components/contexts/WidgetFocusContext';
-import { ActionDefinition, createHandlerFor, ActionHandlerArguments } from 'views/components/actions/ActionHandler';
+import {
+  ActionDefinition,
+  createHandlerFor,
+  ActionHandlerArguments,
+  isExternalLinkAction,
+  ExternalLinkAction,
+  HandlerAction,
+} from 'views/components/actions/ActionHandler';
 
 const StyledMenuItem = styled(MenuItem)`
   && > a {
@@ -44,20 +52,20 @@ type Props = {
   type: 'field' | 'value',
 }
 
-const ActionMenuItem = ({ action, handlerArgs, setOverflowingComponents, overflowingComponents, type, onMenuToggle }: Props) => {
+type ExternalLinkItemProps = Pick<Props, 'handlerArgs' | 'onMenuToggle' | 'type'> & {
+  action: ExternalLinkAction<ActionContexts>,
+  disabled: boolean,
+  field: string,
+}
+
+const ExternalLinkItem = ({ action, disabled, field, handlerArgs, onMenuToggle, type }: ExternalLinkItemProps) => {
   const { unsetWidgetFocusing } = useContext(WidgetFocusContext);
 
-  const setActionComponents = (fn) => {
-    setOverflowingComponents(fn(overflowingComponents));
-  };
-
-  const handler = createHandlerFor(action, setActionComponents);
-  const hasLinkTarget = !!action.linkTarget;
-  const linkProps = hasLinkTarget ? {
+  const linkProps = {
     href: action.linkTarget(handlerArgs),
     target: '_blank',
     rel: 'noopener noreferrer',
-  } : {};
+  };
 
   const onSelect = () => {
     const { resetFocus = false } = action;
@@ -66,25 +74,76 @@ const ActionMenuItem = ({ action, handlerArgs, setOverflowingComponents, overflo
       unsetWidgetFocusing();
     }
 
-    if (!hasLinkTarget) {
-      onMenuToggle();
-    }
-
-    handler(handlerArgs);
+    onMenuToggle();
   };
 
-  const { isEnabled = () => true } = action;
-  const actionDisabled = !isEnabled(handlerArgs);
-  const { field } = handlerArgs;
-
   return (
-    <StyledMenuItem disabled={actionDisabled}
+    <StyledMenuItem disabled={disabled}
                     eventKey={{ action: type, field }}
                     onSelect={onSelect}
                     {...linkProps}>
       {action.title}
-      {hasLinkTarget && <ExternalLinkIcon name="external-link" />}
+      <ExternalLinkIcon name="external-link" />
     </StyledMenuItem>
+  );
+};
+
+type ActionHandlerItemProps = Pick<Props, 'handlerArgs' | 'onMenuToggle' | 'overflowingComponents' | 'setOverflowingComponents' | 'type'> & {
+  action: HandlerAction<ActionContexts>,
+  disabled: boolean,
+  field: string,
+};
+
+const ActionHandlerItem = ({ disabled, action, handlerArgs, setOverflowingComponents, overflowingComponents, type, onMenuToggle }: ActionHandlerItemProps) => {
+  const { unsetWidgetFocusing } = useContext(WidgetFocusContext);
+
+  const setActionComponents = (fn) => {
+    setOverflowingComponents(fn(overflowingComponents));
+  };
+
+  const handler = createHandlerFor(action, setActionComponents);
+
+  const onSelect = () => {
+    const { resetFocus = false } = action;
+
+    if (resetFocus) {
+      unsetWidgetFocusing();
+    }
+
+    onMenuToggle();
+
+    handler(handlerArgs);
+  };
+
+  const { field } = handlerArgs;
+
+  return (
+    <StyledMenuItem disabled={disabled}
+                    eventKey={{ action: type, field }}
+                    onSelect={onSelect}>
+      {action.title}
+    </StyledMenuItem>
+  );
+};
+
+const ActionMenuItem = ({ action, handlerArgs, setOverflowingComponents, overflowingComponents, type, onMenuToggle }: Props) => {
+  const { isEnabled = () => true } = action;
+  const actionDisabled = !isEnabled(handlerArgs);
+  const { field } = handlerArgs;
+
+  if (isExternalLinkAction(action)) {
+    return <ExternalLinkItem action={action} disabled={actionDisabled} field={field} handlerArgs={handlerArgs} onMenuToggle={onMenuToggle} type={type} />;
+  }
+
+  return (
+    <ActionHandlerItem action={action}
+                       disabled={actionDisabled}
+                       field={field}
+                       handlerArgs={handlerArgs}
+                       onMenuToggle={onMenuToggle}
+                       overflowingComponents={overflowingComponents}
+                       setOverflowingComponents={setOverflowingComponents}
+                       type={type} />
   );
 };
 
