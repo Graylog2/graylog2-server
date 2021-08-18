c6cb79a162e702c17f2178519d8a90b310719dba
Linus Pahl <linus@graylog.com>
2021-08-18 14:05:45 +0200

    Removing not needed `MessageTableEnrry` test.

--- graylog2-web-interface/src/views/components/messagelist/MessageTableEntry.test.tsx
+++ graylog2-web-interface/src/views/components/messagelist/MessageTableEntry.test.tsx
@@ -18,21 +18,12 @@ import * as React from 'react';
 import { render, screen } from 'wrappedTestingLibrary';
 import * as Immutable from 'immutable';
 import MockStore from 'helpers/mocking/StoreMock';
-import { createSimpleMessageEventType } from 'fixtures/messageEventTypes';
-import asMock from 'helpers/mocking/AsMock';
-
-import usePluginEntities from 'views/logic/usePluginEntities';
 
 import MessageTableEntry from './MessageTableEntry';
 
 jest.mock('stores/configurations/ConfigurationsStore', () => ({
   ConfigurationsStore: MockStore(),
 }));
-
-const simpleEventType = createSimpleMessageEventType(1, { summaryTemplate: '{field1} - {field2}', gl2EventTypeCode: 'event-type-code' });
-const mockMessageEventTypes = [simpleEventType];
-
-jest.mock('views/logic/usePluginEntities', () => jest.fn(() => mockMessageEventTypes));
 
 describe('MessageTableEntry', () => {
   it('renders message for unknown selected fields', () => {
@@ -56,36 +47,5 @@ describe('MessageTableEntry', () => {
     );
 
     expect(screen.getByText('Something happened!')).toBeInTheDocument();
-  });
-
-  it('displays message summary', () => {
-    const exampleEventType = createSimpleMessageEventType(1, { summaryTemplate: '{field1} - {field2}', gl2EventTypeCode: 'event-type-code' });
-    asMock(usePluginEntities).mockReturnValue([exampleEventType]);
-
-    const message = {
-      id: 'deadbeef',
-      index: 'test_0',
-      fields: {
-        gl2_event_type_code: 'event-type-code',
-        message: 'Something happened!',
-        field1: 'Value for field 1',
-        field2: 'Value for field 2',
-      },
-    };
-
-    render(
-      <table>
-        <MessageTableEntry expandAllRenderAsync
-                           toggleDetail={() => {}}
-                           fields={Immutable.List()}
-                           message={message}
-                           showSummary
-                           selectedFields={Immutable.OrderedSet(['message'])}
-                           expanded={false} />
-      </table>,
-    );
-
-    expect(screen.getByText('Value for field 1 - Value for field 2')).toBeInTheDocument();
-    expect(screen.getByText('Value for field 1 - Value for field 2')).toHaveStyle('color: #00752c');
   });
 });
