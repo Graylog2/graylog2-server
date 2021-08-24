0cb60503a21e3111f17fcb88609914ddb299e455
Dan Torrey <dan@graylog.com>
2021-08-24 11:37:34 -0500

    Add metric test

---
+++ graylog2-server/src/test/java/org/graylog/plugins/pipelineprocessor/processors/PipelineInterpreterStateTest.java
@@ -0,0 +1,18 @@
+package org.graylog.plugins.pipelineprocessor.processors;
+
+import org.graylog2.plugin.LocalMetricRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PipelineInterpreterStateTest {
+
+    @Test
+    public void testMetricName() {
+
+        final PipelineInterpreterState state = new PipelineInterpreterState(null, null, null,
+                new LocalMetricRegistry(), 1, false);
+        assertEquals("org.graylog.plugins.pipelineprocessor.processors.PipelineInterpreter.stage-cache",
+                state.getStageCacheMetricName());
+    }
+}
