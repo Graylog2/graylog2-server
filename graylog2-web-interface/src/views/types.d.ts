49a5c1f9d5227e9016cd7d5aca6a99bbe4554554
Linus Pahl <linus@graylog.com>
2021-08-26 18:42:22 +0200

    Create `MessageDetailContextProviders` which renders plugable context provider.

--- graylog2-web-interface/src/views/types.d.ts
+++ graylog2-web-interface/src/views/types.d.ts
@@ -237,6 +237,7 @@ declare module 'graylog-web-plugin/plugin' {
     'views.hooks.copyWidgetToDashboard'?: Array<CopyWidgetToDashboardHook>;
     'views.overrides.widgetEdit'?: Array<React.ComponentType<OverrideProps>>;
     'views.widgets.actions'?: Array<WidgetActionType>;
+    'views.widgets.messageDetails.contextProvider'?: Array<React.ComponentType<MessageDetailContextProviderProps>>,
     'views.requires.provided'?: Array<string>;
     visualizationConfigTypes?: Array<VisualizationConfigType>;
     visualizationTypes?: Array<VisualizationType>;
