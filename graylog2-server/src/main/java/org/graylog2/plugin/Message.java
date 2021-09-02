f7777055ddc80109fc926cc8022a7a7ee8ca1ba9
Tomas Dvorak <tomas.dvorak@graylog.com>
2021-09-02 13:49:57 +0200

    Exclude Reserved Fields from Volume Check #11231

--- graylog2-server/src/main/java/org/graylog2/plugin/Message.java
+++ graylog2-server/src/main/java/org/graylog2/plugin/Message.java
@@ -69,11 +69,7 @@ import java.util.stream.Collectors;
 
 import static com.google.common.base.Predicates.equalTo;
 import static com.google.common.base.Predicates.not;
-import static org.graylog.schema.GraylogSchemaFields.FIELD_ILLUMINATE_EVENT_CATEGORY;
-import static org.graylog.schema.GraylogSchemaFields.FIELD_ILLUMINATE_EVENT_SUBCATEGORY;
-import static org.graylog.schema.GraylogSchemaFields.FIELD_ILLUMINATE_EVENT_TYPE;
-import static org.graylog.schema.GraylogSchemaFields.FIELD_ILLUMINATE_EVENT_TYPE_CODE;
-import static org.graylog.schema.GraylogSchemaFields.FIELD_ILLUMINATE_TAGS;
+import static org.graylog.schema.GraylogSchemaFields.*;
 import static org.graylog2.plugin.Tools.ES_DATE_FORMAT_FORMATTER;
 import static org.graylog2.plugin.Tools.buildElasticSearchTimeFormat;
 import static org.joda.time.DateTimeZone.UTC;
@@ -222,7 +218,13 @@ public class Message implements Messages, Indexable {
             FIELD_ILLUMINATE_EVENT_SUBCATEGORY,
             FIELD_ILLUMINATE_EVENT_TYPE,
             FIELD_ILLUMINATE_EVENT_TYPE_CODE,
-            FIELD_ILLUMINATE_TAGS
+            FIELD_ILLUMINATE_TAGS,
+            FIELD_ILLUMINATE_GIM_EVENT_CLASS,
+            FIELD_ILLUMINATE_GIM_EVENT_CATEGORY,
+            FIELD_ILLUMINATE_GIM_EVENT_TYPE,
+            FIELD_ILLUMINATE_GIM_EVENT_TYPE_CODE,
+            FIELD_ILLUMINATE_GIM_TAGS,
+            FIELD_ILLUMINATE_GIM_VERSION
     );
 
     private static final ImmutableSet<String> CORE_MESSAGE_FIELDS = ImmutableSet.of(
