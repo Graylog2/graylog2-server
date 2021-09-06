cb3e3cb6ab8a5c6457ac8fa144d21c0705abe1e3
Dennis Oelkers <dennis@graylog.com>
2021-09-06 14:27:57 +0200

    Turning `WidgetHorizontalStretch` into functional component.

--- graylog2-web-interface/src/views/components/widgets/WidgetHorizontalStretch.tsx
+++ graylog2-web-interface/src/views/components/widgets/WidgetHorizontalStretch.tsx
@@ -14,7 +14,8 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React from 'react';
+import * as React from 'react';
+import { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import Spinner from 'components/common/Spinner';
@@ -36,40 +37,35 @@ type Props = {
   widgetType: string,
 }
 
-class WidgetHorizontalStretch extends React.Component<Props> {
-  static propTypes = {
-    widgetId: PropTypes.string.isRequired,
-    widgetType: PropTypes.string.isRequired,
-    position: PropTypes.shape(Position).isRequired,
-    onStretch: PropTypes.func.isRequired,
-  };
-
-  _onClick = () => {
-    const { onStretch, position, widgetId, widgetType } = this.props;
+const WidgetHorizontalStretch = ({ onStretch, position, widgetId, widgetType }: Props) => {
+  const onClick = useCallback(() => {
     const { col, row, height, width } = position;
     const { defaultWidth } = widgetDefinition(widgetType);
 
     onStretch({
-      id: widgetId, col: col, row: row, height: height, width: width === Infinity ? defaultWidth : Infinity,
+      id: widgetId, col, row, height, width: width === Infinity ? defaultWidth : Infinity,
     });
-  };
+  }, [onStretch, position, widgetId, widgetType]);
 
-  render() {
-    const { position } = this.props;
-
-    if (!position) {
-      return <Spinner />;
-    }
-
-    const { width } = position;
-    const stretched = width === Infinity;
-    const icon = stretched ? 'compress' : 'arrows-alt-h';
-    const title = stretched ? 'Compress width' : 'Stretch width';
-
-    return (
-      <IconButton onClick={this._onClick} name={icon} title={title} />
-    );
+  if (!position) {
+    return <Spinner />;
   }
-}
+
+  const { width } = position;
+  const stretched = width === Infinity;
+  const icon = stretched ? 'compress' : 'arrows-alt-h';
+  const title = stretched ? 'Compress width' : 'Stretch width';
+
+  return (
+    <IconButton onClick={onClick} name={icon} title={title} />
+  );
+};
+
+WidgetHorizontalStretch.propTypes = {
+  widgetId: PropTypes.string.isRequired,
+  widgetType: PropTypes.string.isRequired,
+  position: PropTypes.shape(Position).isRequired,
+  onStretch: PropTypes.func.isRequired,
+};
 
 export default WidgetHorizontalStretch;
