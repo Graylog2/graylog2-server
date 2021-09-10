e1af8b8f09402cf310b0c7034e19810df6edab04
Ousmane Samba <ousmane@graylog.com>
2021-09-10 13:04:02 +0200

    Add documentation for login form state

--- graylog2-web-interface/src/logic/authentication/constants.ts
+++ graylog2-web-interface/src/logic/authentication/constants.ts
@@ -14,9 +14,10 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-const LOGIN_INITIALIZING_STATE = 'INITIALIZING';
-const LOGIN_INITIALIZED_STATE = 'INITIALIZED';
-const LOGIN_TRANSITIONING_STATE = 'TRANSITIONING';
+
+const LOGIN_INITIALIZING_STATE = 'INITIALIZING'; // Initial State for the plugin login form set on the login page in created.
+const LOGIN_INITIALIZED_STATE = 'INITIALIZED'; // Should be set once the plugin based login form is initialized
+const LOGIN_TRANSITIONING_STATE = 'TRANSITIONING'; // value when the plugin based form is in transition state for example handling login callback
 
 export {
   LOGIN_INITIALIZING_STATE,
