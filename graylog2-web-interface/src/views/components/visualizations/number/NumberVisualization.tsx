710bebd47b43d9a243cd931509c9ad0ed267e4ff
Dennis Oelkers <dennis@graylog.com>
2021-09-29 15:20:55 +0200

    Using a simpler size definition.

--- graylog2-web-interface/src/views/components/visualizations/number/NumberVisualization.tsx
+++ graylog2-web-interface/src/views/components/visualizations/number/NumberVisualization.tsx
@@ -38,7 +38,7 @@ import type { CurrentViewType } from '../../CustomPropTypes';
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 4fr auto;
+  grid-template-rows: 1fr auto;
   grid-column-gap: 0;
   grid-row-gap: 0;
   height: 100%;
