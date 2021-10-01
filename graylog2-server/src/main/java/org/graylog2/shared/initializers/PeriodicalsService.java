332937c22312c2d16bc9b6af63120351c15322e6
Othello Maurer <othello@graylog.com>
2021-10-01 09:34:22 +0200

    Start master-only periodicals when node becomes leader

--- graylog2-server/src/main/java/org/graylog2/shared/initializers/PeriodicalsService.java
+++ graylog2-server/src/main/java/org/graylog2/shared/initializers/PeriodicalsService.java
@@ -17,7 +17,13 @@
 package org.graylog2.shared.initializers;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import com.google.common.util.concurrent.AbstractIdleService;
+import org.graylog2.cluster.leader.LeaderChangedEvent;
+import org.graylog2.cluster.leader.LeaderElectionService;
 import org.graylog2.periodical.Periodicals;
 import org.graylog2.plugin.ServerStatus;
 import org.graylog2.plugin.periodical.Periodical;
@@ -26,6 +32,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ScheduledFuture;
@@ -38,26 +45,70 @@ public class PeriodicalsService extends AbstractIdleService {
     private final Periodicals periodicals;
     private final ServerStatus serverStatus;
     private final Set<Periodical> periodicalSet;
+    private final EventBus eventBus;
+    private final LeaderElectionService leaderElectionService;
+
+    private final Set<Periodical> leaderOnlyPeriodicals = new HashSet<>();
+    private final Set<Periodical> allNodePeriodicals = new HashSet<>();
 
     @Inject
     public PeriodicalsService(Periodicals periodicals,
                               ServerStatus serverStatus,
-                              Set<Periodical> periodicalSet) {
+                              Set<Periodical> periodicalSet, EventBus eventBus, LeaderElectionService leaderElectionService) {
         this.periodicals = periodicals;
         this.serverStatus = serverStatus;
         this.periodicalSet = periodicalSet;
+        this.eventBus = eventBus;
+        this.leaderElectionService = leaderElectionService;
+
+        periodicalSet.forEach(p -> {
+            if (p.masterOnly()) {
+                leaderOnlyPeriodicals.add(p);
+            } else {
+                allNodePeriodicals.add(p);
+            }
+        });
     }
 
     @Override
     protected void startUp() throws Exception {
-        LOG.info("Starting {} periodicals ...", periodicalSet.size());
+        eventBus.register(this);
 
-        for (Periodical periodical : periodicalSet) {
+        if (leaderElectionService.isLeader()) {
+            LOG.info("Starting {} periodicals ...", periodicalSet.size());
+            startPeriodicals(periodicalSet);
+        } else {
+            LOG.info("Starting {} periodicals ...", allNodePeriodicals.size());
+            LOG.info("Delaying start of {} periodicals until this node becomes leader ...", leaderOnlyPeriodicals.size());
+            startPeriodicals(allNodePeriodicals);
+        }
+    }
+
+    @Subscribe
+    public void leaderChanged(LeaderChangedEvent leaderChangedEvent) {
+        if (!leaderElectionService.isLeader()) {
+            return;
+        }
+        LOG.info("Starting {} periodicals ...", leaderOnlyPeriodicals.size());
+        startPeriodicals(leaderOnlyPeriodicals);
+    }
+
+    private synchronized void startPeriodicals(Set<Periodical> periodicalsToStart) {
+        final Sets.SetView<Periodical> notYetRunningPeriodicals =
+                Sets.difference(periodicalsToStart, ImmutableSet.copyOf(periodicals.getAll()));
+
+        int numOfPeriodicalsToSkip = periodicalsToStart.size() - notYetRunningPeriodicals.size();
+
+        if (numOfPeriodicalsToSkip > 0) {
+            LOG.warn("Skipping start of {} periodicals which have already been started.", numOfPeriodicalsToSkip);
+        }
+
+        for (Periodical periodical : notYetRunningPeriodicals) {
             try {
                 periodical.initialize();
 
-                if (periodical.masterOnly() && !serverStatus.hasCapability(ServerStatus.Capability.MASTER)) {
-                    LOG.info("Not starting [{}] periodical. Only started on Graylog master nodes.", periodical.getClass().getCanonicalName());
+                if (periodical.masterOnly() && !leaderElectionService.isLeader()) {
+                    LOG.error("Not starting [{}] periodical. Periodical requires node to be leader.", periodical.getClass().getCanonicalName());
                     continue;
                 }
 
@@ -76,12 +127,14 @@ public class PeriodicalsService extends AbstractIdleService {
 
     @Override
     protected void shutDown() throws Exception {
+        eventBus.unregister(this);
+
         for (Periodical periodical : periodicals.getAllStoppedOnGracefulShutdown()) {
             LOG.info("Shutting down periodical [{}].", periodical.getClass().getCanonicalName());
             Stopwatch s = Stopwatch.createStarted();
 
             // Cancel future executions.
-            Map<Periodical,ScheduledFuture> futures = periodicals.getFutures();
+            Map<Periodical, ScheduledFuture> futures = periodicals.getFutures();
             if (futures.containsKey(periodical)) {
                 futures.get(periodical).cancel(false);
 
