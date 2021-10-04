d9649b6b66c17cd09c4202552954273e20f96a0e
Othello Maurer <othello@graylog.com>
2021-10-04 16:54:14 +0200

    Make RestrictToMasterFilter dynamic
    The leader might change during runtime, so we can't decide on startup
    if access to a resource needs to be restricted.

--- graylog2-server/src/main/java/org/graylog2/shared/security/RestrictToMasterFilter.java
+++ graylog2-server/src/main/java/org/graylog2/shared/security/RestrictToMasterFilter.java
@@ -16,23 +16,52 @@
  */
 package org.graylog2.shared.security;
 
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.graylog2.cluster.leader.LeaderChangedEvent;
+import org.graylog2.cluster.leader.LeaderElectionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Priority;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+@Singleton
 @Priority(Priorities.AUTHORIZATION)
 public class RestrictToMasterFilter implements ContainerRequestFilter {
     private static final Logger LOG = LoggerFactory.getLogger(RestrictToMasterFilter.class);
 
+    private final LeaderElectionService leaderElectionService;
+    private final EventBus eventBus;
+
+    private final AtomicBoolean isMaster;
+
+    @Inject
+    public RestrictToMasterFilter(LeaderElectionService leaderElectionService, EventBus eventBus) {
+        this.leaderElectionService = leaderElectionService;
+        this.eventBus = eventBus;
+
+        this.eventBus.register(this);
+        isMaster = new AtomicBoolean(leaderElectionService.isLeader());
+    }
+
+    @Subscribe
+    public void leaderChanged(LeaderChangedEvent event) {
+        isMaster.set(leaderElectionService.isLeader());
+    }
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        LOG.warn("Rejected request to <{}> which is only allowed against master nodes.", requestContext.getUriInfo().getPath());
-        throw new ForbiddenException("Request is only allowed against master nodes.");
+        if (!isMaster.get()) {
+            LOG.warn("Rejected request to <{}> which is only allowed against master nodes.", requestContext.getUriInfo().getPath());
+            throw new ForbiddenException("Request is only allowed against master nodes.");
+        }
     }
 }
