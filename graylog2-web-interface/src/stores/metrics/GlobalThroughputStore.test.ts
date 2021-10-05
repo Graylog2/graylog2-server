16dbeca3c85b340d041607c3215dce1f82a26330
Dennis Oelkers <dennis@graylog.com>
2021-10-05 11:50:23 +0200

    Adjusting `GlobalThroughputStore` test.

--- graylog2-web-interface/src/stores/metrics/GlobalThroughputStore.test.ts
+++ graylog2-web-interface/src/stores/metrics/GlobalThroughputStore.test.ts
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import GlobalThroughputStore from './GlobalThroughputStore';
+import { GlobalThroughputStore } from './GlobalThroughputStore';
 
 const expectedThroughput = (input, output) => ({ throughput: { input, output, loading: false } });
 const nodeThroughput = (input, output) => ({
