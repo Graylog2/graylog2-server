fc61e0b86740c77ed69c78da98695199c23982ef
Jan Heise <jan.heise@graylog.com>
2021-10-07 10:11:24 +0200

    Testclasses - remove at the end

---
+++ full-backend-tests/src/test/java/org/graylog/plugins/views/containermatrixtests/TestMavenProjectDirProvider.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.plugins.views.containermatrixtests;
+
+import org.graylog.testing.PropertyLoader;
+import org.graylog.testing.completebackend.MavenProjectDirProvider;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class TestMavenProjectDirProvider implements MavenProjectDirProvider {
+    @Override
+    public Path getProjectDir() {
+        return getProjectReposDir().resolve("graylog2-server");
+    }
+
+    @Override
+    public String getUniqueId() {
+        return "test";
+    }
+
+    protected Path getProjectReposDir() {
+        return new File(PropertyLoader.get("api-it-tests.properties", "project_repos_dir")).toPath().normalize();
+    }
+}
