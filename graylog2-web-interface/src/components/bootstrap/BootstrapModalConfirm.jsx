d12c96bcaac6dc902dac5cebdef622a89eb3b008
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-10-20 10:01:35 -0500

    revert BootstrapModalConfirm.jsx

--- graylog2-web-interface/src/components/bootstrap/BootstrapModalConfirm.jsx
+++ graylog2-web-interface/src/components/bootstrap/BootstrapModalConfirm.jsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Modal from './Modal';
@@ -25,104 +25,112 @@ import Button from './Button';
  * Component that displays a confirmation dialog box that the user can
  * cancel or confirm.
  */
-const BootstrapModalConfirm = ({
-  showModal,
-  onModalOpen,
-  onModalClose,
-  onCancel,
-  onConfirm,
-  title,
-  children,
-  cancelButtonDisabled,
-  confirmButtonDisabled,
-  cancelButtonText,
-  confirmButtonText,
-}) => {
-  const modalRef = useRef();
-
-  const close = () => {
-    modalRef.current.close();
+class BootstrapModalConfirm extends React.Component {
+  static propTypes = {
+    /** Indicates whether the modal should be shown by default or not. */
+    showModal: PropTypes.bool,
+    /** Title to use in the modal. */
+    title: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.element,
+    ]).isRequired,
+    /** Text to use in the cancel button. */
+    cancelButtonText: PropTypes.string,
+    /** Text to use in the confirmation button. */
+    confirmButtonText: PropTypes.string,
+    /** Indicates whether the cancel button should be disabled or not. */
+    cancelButtonDisabled: PropTypes.bool,
+    /** Indicates whether the confirm button should be disabled or not. */
+    confirmButtonDisabled: PropTypes.bool,
+    /** Function to call when the modal is opened. The function does not receive any arguments. */
+    onModalOpen: PropTypes.func,
+    /** Function to call when the modal is closed. The function does not receive any arguments. */
+    onModalClose: PropTypes.func,
+    /** Function to call when the action is not confirmed. The function does not receive any arguments. */
+    onCancel: PropTypes.func,
+    /**
+     * Function to call when the action is confirmed. The function receives a callback function to close the modal
+     * dialog box as first argument.
+     */
+    onConfirm: PropTypes.func.isRequired,
+    /**
+     * React elements to display in the modal body. This should be the information the user has
+     * to confirm in order to proceed with the operation.
+     */
+    children: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.element,
+    ]).isRequired,
   };
 
-  const handleCancel = () => {
+  static defaultProps = {
+    showModal: false,
+    cancelButtonText: 'Cancel',
+    confirmButtonText: 'Confirm',
+    cancelButtonDisabled: false,
+    confirmButtonDisabled: false,
+    onCancel: () => {},
+    onModalOpen: () => {},
+    onModalClose: () => {},
+  };
+
+  onCancel = () => {
+    const { onCancel } = this.props;
+
     onCancel();
 
-    close();
+    this.close();
   };
 
-  const handleConfirm = () => {
-    onConfirm(close);
+  onConfirm = () => {
+    const { onConfirm } = this.props;
+
+    onConfirm(this.close);
   };
 
-  return (
-    <BootstrapModalWrapper ref={modalRef}
-                           showModal={showModal}
-                           onOpen={onModalOpen}
-                           onClose={onModalClose}
-                           onHide={handleCancel}
-                           role="alertdialog">
-      <Modal.Header closeButton>
-        <Modal.Title>{title}</Modal.Title>
-      </Modal.Header>
+  open = () => {
+    this.modal.open();
+  };
 
-      <Modal.Body>
-        {children}
-      </Modal.Body>
+  close = () => {
+    this.modal.close();
+  };
 
-      <Modal.Footer>
-        <Button type="button" onClick={onCancel} disabled={cancelButtonDisabled}>{cancelButtonText}</Button>
-        <Button type="button" onClick={handleConfirm} bsStyle="primary" disabled={confirmButtonDisabled}>{confirmButtonText}</Button>
-      </Modal.Footer>
-    </BootstrapModalWrapper>
-  );
-};
+  render() {
+    const {
+      showModal,
+      onModalOpen,
+      onModalClose,
+      title,
+      children,
+      cancelButtonDisabled,
+      confirmButtonDisabled,
+      cancelButtonText,
+      confirmButtonText,
+    } = this.props;
 
-BootstrapModalConfirm.propTypes = {
-  /** Indicates whether the modal should be shown by default or not. */
-  showModal: PropTypes.bool,
-  /** Title to use in the modal. */
-  title: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-  ]).isRequired,
-  /** Text to use in the cancel button. */
-  cancelButtonText: PropTypes.string,
-  /** Text to use in the confirmation button. */
-  confirmButtonText: PropTypes.string,
-  /** Indicates whether the cancel button should be disabled or not. */
-  cancelButtonDisabled: PropTypes.bool,
-  /** Indicates whether the confirm button should be disabled or not. */
-  confirmButtonDisabled: PropTypes.bool,
-  /** Function to call when the modal is opened. The function does not receive any arguments. */
-  onModalOpen: PropTypes.func,
-  /** Function to call when the modal is closed. The function does not receive any arguments. */
-  onModalClose: PropTypes.func,
-  /** Function to call when the action is not confirmed. The function does not receive any arguments. */
-  onCancel: PropTypes.func,
-  /**
-   * Function to call when the action is confirmed. The function receives a callback function to close the modal
-   * dialog box as first argument.
-   */
-  onConfirm: PropTypes.func.isRequired,
-  /**
-   * React elements to display in the modal body. This should be the information the user has
-   * to confirm in order to proceed with the operation.
-   */
-  children: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-  ]).isRequired,
-};
+    return (
+      <BootstrapModalWrapper ref={(c) => { this.modal = c; }}
+                             showModal={showModal}
+                             onOpen={onModalOpen}
+                             onClose={onModalClose}
+                             onHide={this.onCancel}
+                             role="alertdialog">
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
 
-BootstrapModalConfirm.defaultProps = {
-  showModal: false,
-  cancelButtonText: 'Cancel',
-  confirmButtonText: 'Confirm',
-  cancelButtonDisabled: false,
-  confirmButtonDisabled: false,
-  onCancel: () => {},
-  onModalOpen: () => {},
-  onModalClose: () => {},
-};
+        <Modal.Body>
+          {children}
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button type="button" onClick={this.onCancel} disabled={cancelButtonDisabled}>{cancelButtonText}</Button>
+          <Button type="button" onClick={this.onConfirm} bsStyle="primary" disabled={confirmButtonDisabled}>{confirmButtonText}</Button>
+        </Modal.Footer>
+      </BootstrapModalWrapper>
+    );
+  }
+}
 
 export default BootstrapModalConfirm;
