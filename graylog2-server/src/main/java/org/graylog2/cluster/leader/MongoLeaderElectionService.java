a4c93ce308fa5e24e28cce77e33b54f2289ebb29
Marco Pfatschbacher <marco@graylog.com>
2021-10-21 12:07:22 +0200

    Fix nodes table is_master info
     - Make MongoLeaderElectionService a Singleton
     - Ensure up-to-date nodes table before publishing LeaderChangedEvent

--- graylog2-server/src/main/java/org/graylog2/cluster/leader/MongoLeaderElectionService.java
+++ graylog2-server/src/main/java/org/graylog2/cluster/leader/MongoLeaderElectionService.java
@@ -20,26 +20,31 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.util.concurrent.AbstractExecutionThreadService;
 import com.google.common.util.concurrent.Uninterruptibles;
 import org.graylog2.cluster.lock.LockService;
+import org.graylog2.periodical.NodePingThread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+@Singleton
 public class MongoLeaderElectionService extends AbstractExecutionThreadService implements LeaderElectionService {
     private static final String RESOURCE_NAME = "cluster-leader";
     private static final Logger log = LoggerFactory.getLogger(MongoLeaderElectionService.class);
 
     private final LockService lockService;
     private final EventBus eventBus;
+    private final NodePingThread nodePingThread;
 
     private volatile boolean isLeader = false;
 
     @Inject
-    public MongoLeaderElectionService(LockService lockService, EventBus eventBus) {
+    public MongoLeaderElectionService(LockService lockService, EventBus eventBus, NodePingThread nodePingThread) {
         this.lockService = lockService;
         this.eventBus = eventBus;
+        this.nodePingThread = nodePingThread;
     }
 
     @Override
@@ -58,6 +63,8 @@ public class MongoLeaderElectionService extends AbstractExecutionThreadService i
 
             if (wasLeader != isLeader) {
                 log.info("Leader changed. This node is {}.", isLeader ? "now the leader" : "not the leader anymore");
+                // Ensure the nodes collection is up to date before we publish the event
+                nodePingThread.doRun();
                 eventBus.post(new LeaderChangedEvent());
             }
 
