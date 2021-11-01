7d9a352cfad25158cf45f402b447dc0198511d40
Dennis Oelkers <dennis@graylog.com>
2021-11-01 10:41:13 +0100

    Moving generator test to better place.

--- graylog2-web-interface/src/generator/__tests__/parse.test.ts
+++ graylog2-web-interface/src/generator/__tests__/parse.test.ts
@@ -5,7 +5,7 @@ import { parseApi } from 'generator/parse';
 
 describe('parse', () => {
   it('parses an api properly', () => {
-    const rawApi = JSON.parse(fs.readFileSync(path.resolve(__dirname, './fixtures/sample-api.json')).toString());
+    const rawApi = JSON.parse(fs.readFileSync(path.resolve(__dirname, './sample-api.json')).toString());
 
     const result = parseApi('sample', rawApi);
 
