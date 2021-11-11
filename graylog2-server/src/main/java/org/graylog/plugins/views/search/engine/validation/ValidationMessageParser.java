d7fb7600f0269bb0df0026af078905793d95e4b1
Tomas Dvorak <tomas.dvorak@graylog.com>
2021-11-11 14:45:46 +0100

    ValidationMessageParser enhancements

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/engine/validation/ValidationMessageParser.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/engine/validation/ValidationMessageParser.java
@@ -21,9 +21,10 @@ import java.util.regex.Pattern;
 
 public class ValidationMessageParser {
 
-    private static final Pattern regexFull = Pattern.compile("(?:\\[\\w+/\\w+]\\s+)*(\\w+)\\[(.*?)];\\s+nested:.*",  Pattern.MULTILINE | Pattern.DOTALL);
+    private static final Pattern regexForNested = Pattern.compile("(\\w+)\\[(.*)]",  Pattern.MULTILINE | Pattern.DOTALL);
     private static final Pattern regexSimple = Pattern.compile("([\\w.]+):\\s+(.*)",  Pattern.MULTILINE | Pattern.DOTALL);
     private static final Pattern regexPosition = Pattern.compile(".*at line (\\d+), column (\\d+).",  Pattern.MULTILINE | Pattern.DOTALL);
+    private static final Pattern regexForExplanation = Pattern.compile("(\\w+)\\(\"(.+)\"\\)",  Pattern.MULTILINE | Pattern.DOTALL);
 
 
     public static ValidationMessage getHumanReadableMessage(final String input) {
@@ -34,17 +35,25 @@ public class ValidationMessageParser {
 
         final ValidationMessage.Builder errorBuilder = ValidationMessage.builder();
 
-        final Matcher matcher = regexFull.matcher(input);
-        if (matcher.find()) {
-            errorBuilder.errorType(matcher.group(1));
-            errorBuilder.errorMessage(matcher.group(2));
+        if(input.contains("; nested:")) {
+            final String[] stack = input.split("; nested:");
+            final String actualProblem = stack[1];
+            final Matcher matcher = regexForNested.matcher(actualProblem);
+            if(matcher.find()) {
+                errorBuilder.errorType(matcher.group(1));
+                errorBuilder.errorMessage(matcher.group(2));
+            }
         } else {
             final Matcher simpleMatcher = regexSimple.matcher(input);
-            if (simpleMatcher.find()) {
+            if(simpleMatcher.find()) {
                 errorBuilder.errorType(simpleMatcher.group(1));
                 errorBuilder.errorMessage(simpleMatcher.group(2));
-            } else {
-                errorBuilder.errorMessage(input);
+            }
+
+            final Matcher explanationMatcher = regexForExplanation.matcher(input);
+            if(explanationMatcher.find()) {
+                errorBuilder.errorType(explanationMatcher.group(1));
+                errorBuilder.errorMessage(explanationMatcher.group(2));
             }
         }
 
--- graylog2-server/src/test/java/org/graylog/plugins/views/search/engine/validation/ValidationMessageParserTest.java
+++ graylog2-server/src/test/java/org/graylog/plugins/views/search/engine/validation/ValidationMessageParserTest.java
@@ -23,24 +23,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class ValidationMessageParserTest {
     @Test
     void testParseException() {
-        final ValidationMessage message = ValidationMessageParser.getHumanReadableMessage("[graylog_0/7o7KffecQkuTEM7L_VGazA] QueryShardException[Failed to parse query [(lorem:ipsum OR dolor:sit AND _exists_:http_method]]; nested: ParseException[Cannot parse '(lorem:ipsum OR dolor:sit AND _exists_:http_method': Encountered \\\"<EOF>\\\" at line 1, column 50.\\nWas expecting one of:\\n    <AND> ...\\n    <OR> ...\\n    <NOT> ...\\n    \\\"+\\\" ...\\n    \\\"-\\\" ...\\n    <BAREOPER> ...\\n    \\\"(\\\" ...\\n    \\\")\\\" ...\\n    \\\"*\\\" ...\\n    \\\"^\\\" ...\\n    <QUOTED> ...\\n    <TERM> ...\\n    <FUZZY_SLOP> ...\\n    <PREFIXTERM> ...\\n    <WILDTERM> ...\\n    <REGEXPTERM> ...\\n    \\\"[\\\" ...\\n    \\\"{\\\" ...\\n    <NUMBER> ...\\n    ]; nested: ParseException[Encountered \\\"<EOF>\\\" at line 1, column 50.\\nWas expecting one of:\\n    <AND> ...\\n    <OR> ...\\n    <NOT> ...\\n    \\\"+\\\" ...\\n    \\\"-\\\" ...\\n    <BAREOPER> ...\\n    \\\"(\\\" ...\\n    \\\")\\\" ...\\n    \\\"*\\\" ...\\n    \\\"^\\\" ...\\n    <QUOTED> ...\\n    <TERM> ...\\n    <FUZZY_SLOP> ...\\n    <PREFIXTERM> ...\\n    <WILDTERM> ...\\n    <REGEXPTERM> ...\\n    \\\"[\\\" ...\\n    \\\"{\\\" ...\\n    <NUMBER> ...\\n    ];; org.apache.lucene.queryparser.classic.ParseException: Cannot parse '(lorem:ipsum OR dolor:sit AND _exists_:http_method': Encountered \\\"<EOF>\\\" at line 1, column 50.\\nWas expecting one of:\\n    <AND> ...\\n    <OR> ...\\n    <NOT> ...\\n    \\\"+\\\" ...\\n    \\\"-\\\" ...\\n    <BAREOPER> ...\\n    \\\"(\\\" ...\\n    \\\")\\\" ...\\n    \\\"*\\\" ...\\n    \\\"^\\\" ...\\n    <QUOTED> ...\\n    <TERM> ...\\n    <FUZZY_SLOP> ...\\n    <PREFIXTERM> ...\\n    <WILDTERM> ...\\n    <REGEXPTERM> ...\\n    \\\"[\\\" ...\\n    \\\"{\\\" ...\\n    <NUMBER> ...\\n   ");
-        assertThat(message.errorType()).isEqualTo("QueryShardException");
-        assertThat(message.errorMessage()).isEqualTo("Failed to parse query [(lorem:ipsum OR dolor:sit AND _exists_:http_method]");
+        final ValidationMessage message = ValidationMessageParser.getHumanReadableMessage("[graylog_0/7o7KffecQkuTEM7L_VGazA] QueryShardException[Failed to parse query [(lorem:ipsum OR dolor:sit AND _exists_:http_method]]; nested: ParseException[Cannot parse '(lorem:ipsum OR dolor:sit AND _exists_:http_method': Encountered \\\"<EOF>\\\" at line 1, column 50.\\nWas expecting one of:\\n    <AND> ...\\n    <OR> ...\\n    <NOT> ...\\n    \\\"+\\\" ...\\n    \\\"-\\\" ...\\n    <BAREOPER> ...\\n    \\\"(\\\" ...\\n    \\\")\\\" ...\\n    \\\"*\\\" ...\\n    \\\"^\\\" ...\\n    <QUOTED> ...\\n    <TERM> ...\\n    <FUZZY_SLOP> ...\\n    <PREFIXTERM> ...\\n    <WILDTERM> ...\\n    <REGEXPTERM> ...\\n    \\\"[\\\" ...\\n    \\\"{\\\" ...\\n    <NUMBER> ...\\n    ]; nested: ParseException[Encountered \\\"<EOF>\\\" at line 1, column 50.\\nWas expecting one of:\\n    <AND> ...\\n    <OR> ...\\n    <NOT> ...\\n    \\\"+\\\" ...\\n    \\\"-\\\" ...\\n    <BAREOPER> ...\\n    \\\"(\\\" ...\\n    \\\")\\\" ...\\n    \\\"*\\\" ...\\n    \\\"^\\\" ...\\n    <QUOTED> ...\\n    <TERM> ...\\n    <FUZZY_SLOP> ...\\n    <PREFIXTERM> ...\\n    <WILDTERM> ...\\n    <REGEXPTERM> ...\\n    \\\"[\\\" ...\\n    \\\"{\\\" ...\\n    <NUMBER> ...\\n    ];; org.apache.lucene.queryparser.classic.ParseException: Cannot parse '(lorem:ipsum OR dolor:sit AND _exists_:http_method': Encountered \\\"<EOF>\\\" at line 1, column 50.\\nWas expecting one of:\\n    <AND> ...\\n    <OR> ...\\n    <NOT> ...\\n    \\\"+\\\" ...\\n    \\\"-\\\" ...\\n    <BAREOPER> ...\\n    \\\"(\\\" ...\\n    \\\")\\\" ...\\n    \\\"*\\\" ...\\n    \\\"^\\\" ...\\n    <QUOTED> ...\\n    <TERM> ...\\n    <FUZZY_SLOP> ...\\n    <PREFIXTERM> ...\\n    <WILDTERM> ...\\n    <REGEXPTERM> ...\\n    \\\"[\\\" ...\\n    \\\"{\\\" ...\\n    <NUMBER> ...\\n ");
+        assertThat(message.errorType()).isEqualTo("ParseException");
+        assertThat(message.errorMessage()).isEqualTo("Cannot parse '(lorem:ipsum OR dolor:sit AND _exists_:http_method': Encountered \\\"<EOF>\\\" at line 1, column 50.\\nWas expecting one of:\\n    <AND> ...\\n    <OR> ...\\n    <NOT> ...\\n    \\\"+\\\" ...\\n    \\\"-\\\" ...\\n    <BAREOPER> ...\\n    \\\"(\\\" ...\\n    \\\")\\\" ...\\n    \\\"*\\\" ...\\n    \\\"^\\\" ...\\n    <QUOTED> ...\\n    <TERM> ...\\n    <FUZZY_SLOP> ...\\n    <PREFIXTERM> ...\\n    <WILDTERM> ...\\n    <REGEXPTERM> ...\\n    \\\"[\\\" ...\\n    \\\"{\\\" ...\\n    <NUMBER> ...\\n    ");
     }
 
     @Test
     void testNFE() {
         final ValidationMessage message = ValidationMessageParser.getHumanReadableMessage("[graylog_0/Otu1lLawRTS9shE5LUzQiQ] QueryShardException[failed to create query: For input string: \\\"1,2\\\"]; nested: NumberFormatException[For input string: \\\"1,2\\\"];; java.lang.NumberFormatException: For input string: \\\"1,2\\");
-        assertThat(message.errorType()).isEqualTo("QueryShardException");
-        assertThat(message.errorMessage()).isEqualTo("failed to create query: For input string: \\\"1,2\\\"");
+        assertThat(message.errorType()).isEqualTo("NumberFormatException");
+        assertThat(message.errorMessage()).isEqualTo("For input string: \\\"1,2\\\"");
 
     }
 
     @Test
     void testWithoutIndexShard() {
         final ValidationMessage message = ValidationMessageParser.getHumanReadableMessage("QueryShardException[Failed to parse query [path:test/test]]; nested: ParseException[Cannot parse 'path:test/test': Lexical error at line 1, column 22. Encountered: <EOF> after : \\\"/test\\\"]; nested: TokenMgrError[Lexical error at line 1, column 22. Encountered: <EOF> after : \\\"/test\\\"];; org.apache.lucene.queryparser.classic.ParseException: Cannot parse 'path:test/test': Lexical error at line 1, column 22. Encountered: <EOF> after : \\\"/test\\\"\"");
-        assertThat(message.errorType()).isEqualTo("QueryShardException");
-        assertThat(message.errorMessage()).isEqualTo("Failed to parse query [path:test/test]");
+        assertThat(message.errorType()).isEqualTo("ParseException");
+        assertThat(message.errorMessage()).isEqualTo("Cannot parse 'path:test/test': Lexical error at line 1, column 22. Encountered: <EOF> after : \\\"/test\\\"");
         assertThat(message.line()).isEqualTo(1);
         assertThat(message.column()).isEqualTo(22);
     }
@@ -51,5 +51,20 @@ class ValidationMessageParserTest {
         final ValidationMessage error = ValidationMessageParser.getHumanReadableMessage("org.elasticsearch.index.query.QueryShardException: Can only use prefix queries on keyword, text and wildcard fields - not on [winlogbeat_event_code] which is of type [long]");
         assertThat(error.errorType()).isEqualTo("org.elasticsearch.index.query.QueryShardException");
         assertThat(error.errorMessage()).isEqualTo("Can only use prefix queries on keyword, text and wildcard fields - not on [winlogbeat_event_code] which is of type [long]");
+    }
+
+    @Test
+    void testPath() {
+        final ValidationMessage parsed = ValidationMessageParser.getHumanReadableMessage("[graylog_0/7o7KffecQkuTEM7L_VGazA] QueryShardException[Failed to parse query [path:test/test]]; nested: ParseException[Cannot parse 'path:test/test': Lexical error at line 1, column 15.  Encountered: <EOF> after : \"/test\"]; nested: TokenMgrError[Lexical error at line 1, column 15.  Encountered: <EOF> after : \"/test\"];; org.apache.lucene.queryparser.classic.ParseException: Cannot parse 'path:test/test': Lexical error at line 1, column 15.  Encountered: <EOF> after : \"/test\"");
+        assertThat(parsed.errorType()).isEqualTo("ParseException");
+        assertThat(parsed.errorMessage()).isEqualTo("Cannot parse 'path:test/test': Lexical error at line 1, column 15.  Encountered: <EOF> after : \"/test\"");
+    }
+
+    @Test
+    void testMatchNoDocs() {
+        final ValidationMessage parsed = ValidationMessageParser.getHumanReadableMessage("MatchNoDocsQuery(\"unmapped fields [not_existing_field]\")");
+        assertThat(parsed.errorType()).isEqualTo("MatchNoDocsQuery");
+        assertThat(parsed.errorMessage()).isEqualTo("unmapped fields [not_existing_field]");
+
     }
 }
