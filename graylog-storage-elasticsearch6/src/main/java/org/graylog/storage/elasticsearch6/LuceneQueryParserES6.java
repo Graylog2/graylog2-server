c033d63fe0fea25d58a782db0dd1399d92e3fabe
Tomas Dvorak <tomas.dvorak@graylog.com>
2021-11-12 08:56:17 +0100

    Fixed imports

--- graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/LuceneQueryParserES6.java
+++ graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/LuceneQueryParserES6.java
@@ -18,16 +18,12 @@ package org.graylog.storage.elasticsearch6;
 
 import org.graylog.plugins.views.search.engine.LuceneQueryParser;
 import org.graylog.plugins.views.search.engine.LuceneQueryParsingException;
-import org.graylog.shaded.elasticsearch5.org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.graylog.shaded.elasticsearch5.org.apache.lucene.index.Term;
-import org.graylog.shaded.elasticsearch5.org.apache.lucene.queryparser.classic.ParseException;
-import org.graylog.shaded.elasticsearch5.org.apache.lucene.queryparser.classic.QueryParser;
-import org.graylog.shaded.elasticsearch5.org.apache.lucene.search.AutomatonQuery;
-import org.graylog.shaded.elasticsearch5.org.apache.lucene.search.BooleanClause;
-import org.graylog.shaded.elasticsearch5.org.apache.lucene.search.Query;
+import org.graylog.shaded.elasticsearch6.org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.graylog.shaded.elasticsearch6.org.apache.lucene.queryparser.classic.ParseException;
+import org.graylog.shaded.elasticsearch6.org.apache.lucene.queryparser.classic.QueryParser;
+import org.graylog.shaded.elasticsearch6.org.apache.lucene.search.Query;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 public class LuceneQueryParserES6 implements LuceneQueryParser {
