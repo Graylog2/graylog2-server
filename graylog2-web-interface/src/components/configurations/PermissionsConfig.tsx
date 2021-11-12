7ca8ed9c88c8a7562aff84f749f69e7e28e66357
Ousmane Samba <ousmane@graylog.com>
2021-11-12 09:44:35 +0100

    fix

--- graylog2-web-interface/src/components/configurations/PermissionsConfig.tsx
+++ graylog2-web-interface/src/components/configurations/PermissionsConfig.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 import * as React from 'react';
 import { useState } from 'react';
 import styled, { css, DefaultTheme } from 'styled-components';
@@ -120,4 +136,3 @@ const PermissionsConfig = ({ config, updateConfig }: Props) => {
 };
 
 export default PermissionsConfig;
-PERMISSIONS_CONFIGPERMISSIONS_CONFIGPERMISSIONS_CONFIG;
