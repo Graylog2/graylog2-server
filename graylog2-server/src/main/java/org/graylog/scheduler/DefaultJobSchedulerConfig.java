d91cf485fb8b6d72bcea5aa94f65a418b72faf49
Othello Maurer <othello@graylog.com>
2021-11-25 15:04:21 +0100

    Remove leader-status caching from callees

--- graylog2-server/src/main/java/org/graylog/scheduler/DefaultJobSchedulerConfig.java
+++ graylog2-server/src/main/java/org/graylog/scheduler/DefaultJobSchedulerConfig.java
@@ -16,48 +16,30 @@
  */
 package org.graylog.scheduler;
 
-import com.google.common.eventbus.EventBus;
-import com.google.common.eventbus.Subscribe;
-import org.graylog2.cluster.leader.LeaderChangedEvent;
 import org.graylog2.cluster.leader.LeaderElectionService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * This is the default {@link JobSchedulerConfig}.
  */
 @Singleton
 public class DefaultJobSchedulerConfig implements JobSchedulerConfig {
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultJobSchedulerConfig.class);
-
-    private final AtomicBoolean isLeader;
-
     private final LeaderElectionService leaderElectionService;
 
     @Inject
-    public DefaultJobSchedulerConfig(LeaderElectionService leaderElectionService, EventBus eventBus) {
+    public DefaultJobSchedulerConfig(LeaderElectionService leaderElectionService) {
         this.leaderElectionService = leaderElectionService;
-
-        eventBus.register(this);
-        isLeader = new AtomicBoolean(leaderElectionService.isLeader());
     }
 
     @Override
     public boolean canExecute() {
-        return isLeader.get();
+        return leaderElectionService.isLeader();
     }
 
     @Override
     public int numberOfWorkerThreads() {
         return 5;
-    }
-
-    @Subscribe
-    public void leaderChanged(LeaderChangedEvent event) {
-        isLeader.set(leaderElectionService.isLeader());
     }
 }
--- graylog2-server/src/main/java/org/graylog2/shared/security/RestrictToMasterFilter.java
+++ graylog2-server/src/main/java/org/graylog2/shared/security/RestrictToMasterFilter.java
@@ -16,9 +16,6 @@
  */
 package org.graylog2.shared.security;
 
-import com.google.common.eventbus.EventBus;
-import com.google.common.eventbus.Subscribe;
-import org.graylog2.cluster.leader.LeaderChangedEvent;
 import org.graylog2.cluster.leader.LeaderElectionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +28,6 @@ import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Singleton
 @Priority(Priorities.AUTHORIZATION)
@@ -39,27 +35,15 @@ public class RestrictToMasterFilter implements ContainerRequestFilter {
     private static final Logger LOG = LoggerFactory.getLogger(RestrictToMasterFilter.class);
 
     private final LeaderElectionService leaderElectionService;
-    private final EventBus eventBus;
-
-    private final AtomicBoolean isMaster;
 
     @Inject
-    public RestrictToMasterFilter(LeaderElectionService leaderElectionService, EventBus eventBus) {
+    public RestrictToMasterFilter(LeaderElectionService leaderElectionService) {
         this.leaderElectionService = leaderElectionService;
-        this.eventBus = eventBus;
-
-        this.eventBus.register(this);
-        isMaster = new AtomicBoolean(leaderElectionService.isLeader());
-    }
-
-    @Subscribe
-    public void leaderChanged(LeaderChangedEvent event) {
-        isMaster.set(leaderElectionService.isLeader());
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        if (!isMaster.get()) {
+        if (!leaderElectionService.isLeader()) {
             LOG.warn("Rejected request to <{}> which is only allowed against master nodes.", requestContext.getUriInfo().getPath());
             throw new ForbiddenException("Request is only allowed against master nodes.");
         }
