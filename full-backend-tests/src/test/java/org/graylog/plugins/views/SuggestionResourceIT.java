fe02c8c6bf2f37ab50cf97e3e4fa950f8d9825e8
Tomas Dvorak <tomas.dvorak@graylog.com>
2021-12-02 07:24:59 +0100

    Added error-checking IT for suggestions

--- full-backend-tests/src/test/java/org/graylog/plugins/views/SuggestionResourceIT.java
+++ full-backend-tests/src/test/java/org/graylog/plugins/views/SuggestionResourceIT.java
@@ -19,19 +19,19 @@ package org.graylog.plugins.views;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.graylog.testing.completebackend.GraylogBackend;
+import org.graylog.testing.containermatrix.ContainerVersions;
 import org.graylog.testing.containermatrix.annotations.ContainerMatrixTest;
 import org.graylog.testing.containermatrix.annotations.ContainerMatrixTestsConfiguration;
 import org.graylog.testing.utils.GelfInputUtils;
 import org.graylog.testing.utils.SearchUtils;
 
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.graylog.testing.completebackend.Lifecycle.CLASS;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-@ContainerMatrixTestsConfiguration(serverLifecycle = CLASS, extraPorts = SuggestionResourceIT.GELF_HTTP_PORT)
+@ContainerMatrixTestsConfiguration(serverLifecycle = CLASS, extraPorts = SuggestionResourceIT.GELF_HTTP_PORT, esVersions = {ContainerVersions.ES6, ContainerVersions.ES7})
 public class SuggestionResourceIT {
 
     static final int GELF_HTTP_PORT = 12201;
@@ -45,16 +45,16 @@ public class SuggestionResourceIT {
     }
 
     @ContainerMatrixTest
-    void testMinimalisticRequest() {
+    void testMinimalRequest() {
         int mappedPort = sut.mappedPortFor(GELF_HTTP_PORT);
         GelfInputUtils.createGelfHttpInput(mappedPort, GELF_HTTP_PORT, requestSpec);
         GelfInputUtils.postMessage(mappedPort,
-                "{\"short_message\":\"Hello there\", \"host\":\"example.org\", \"facility\":\"test\"}",
+                "{\"short_message\":\"SuggestionResourceIT#testMinimalisticRequest\", \"host\":\"example.org\", \"facility\":\"test\"}",
                 requestSpec);
 
         // mainly because of the waiting logic
-        final List<String> strings = SearchUtils.searchForAllMessages(requestSpec);
-        assertThat(strings.size()).isEqualTo(1);
+        final boolean messagePresent = SearchUtils.searchForMessage(requestSpec, "SuggestionResourceIT#testMinimalisticRequest");
+        assertThat(messagePresent).isTrue();
 
         final ValidatableResponse validatableResponse = given()
                 .spec(requestSpec)
@@ -65,5 +65,29 @@ public class SuggestionResourceIT {
                 .statusCode(200);
         validatableResponse.assertThat().body("suggestions.value[0]", equalTo("test"));
         validatableResponse.assertThat().body("suggestions.occurrence[0]", equalTo(1));
+    }
+
+    @ContainerMatrixTest
+    void testInvalidField() {
+        int mappedPort = sut.mappedPortFor(GELF_HTTP_PORT);
+        GelfInputUtils.createGelfHttpInput(mappedPort, GELF_HTTP_PORT, requestSpec);
+        GelfInputUtils.postMessage(mappedPort,
+                "{\"short_message\":\"SuggestionResourceIT#testInvalidField\", \"host\":\"example.org\", \"facility\":\"test\"}",
+                requestSpec);
+
+        // mainly because of the waiting logic
+        final boolean messagePresent = SearchUtils.searchForMessage(requestSpec, "SuggestionResourceIT#testInvalidField");
+        assertThat(messagePresent).isTrue();
+
+        final ValidatableResponse validatableResponse = given()
+                .spec(requestSpec)
+                .when()
+                .body("{\"field\":\"message\", \"input\":\"foo\"}")
+                .post("/search/suggest")
+                .then()
+                .statusCode(200);
+        // error types and messages are different for each ES version, so let's just check that there is an error in the response
+        validatableResponse.assertThat().body("error", notNullValue());
+
     }
 }
