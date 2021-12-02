6a7d283fd786a861514196389a042f31edb691e6
Dennis Oelkers <dennis@graylog.com>
2021-12-02 15:01:21 +0100

    Retrieve dependencies for factory in `provide` instead of construction. (#11730)

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/rest/contexts/SearchUserFactory.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/rest/contexts/SearchUserFactory.java
@@ -18,32 +18,35 @@ package org.graylog.plugins.views.search.rest.contexts;
 
 import org.apache.shiro.subject.Subject;
 import org.glassfish.hk2.api.Factory;
+import org.glassfish.hk2.api.ServiceLocator;
 import org.graylog.plugins.views.search.permissions.SearchUser;
 import org.graylog.security.UserContext;
-import org.graylog2.shared.rest.resources.RestResource;
 import org.graylog2.shared.security.ShiroPrincipal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.core.Context;
+import javax.inject.Inject;
 import javax.ws.rs.core.SecurityContext;
 import java.security.Principal;
 
 public class SearchUserFactory implements Factory<SearchUser> {
     private static final Logger LOG = LoggerFactory.getLogger(SearchUserFactory.class);
 
-    @Context
-    private UserContext userContext;
+    private final ServiceLocator serviceLocator;
 
-    @Context
-    private SecurityContext securityContext;
+    @Inject
+    public SearchUserFactory(ServiceLocator serviceLocator) {
+        this.serviceLocator = serviceLocator;
+    }
 
     @Override
     public SearchUser provide() {
-        return new SearchUser(userContext.getUser(), this::isPermitted, this::isPermitted);
+        final UserContext userContext = serviceLocator.getService(UserContext.class);
+        final SecurityContext securityContext = serviceLocator.getService(SecurityContext.class);
+        return new SearchUser(userContext.getUser(), (permission) -> this.isPermitted(securityContext, permission), (permission, instanceId) -> this.isPermitted(securityContext, permission, instanceId));
     }
 
-    protected Subject getSubject() {
+    protected Subject getSubject(SecurityContext securityContext) {
         if (securityContext == null) {
             LOG.error("Cannot retrieve current subject, SecurityContext isn't set.");
             return null;
@@ -60,16 +63,15 @@ public class SearchUserFactory implements Factory<SearchUser> {
         return principal.getSubject();
     }
 
-    protected boolean isPermitted(String permission, String instanceId) {
-        return getSubject().isPermitted(permission + ":" + instanceId);
+    protected boolean isPermitted(SecurityContext securityContext, String permission, String instanceId) {
+        return getSubject(securityContext).isPermitted(permission + ":" + instanceId);
     }
 
-    protected boolean isPermitted(String permission) {
-        return getSubject().isPermitted(permission);
+    protected boolean isPermitted(SecurityContext securityContext, String permission) {
+        return getSubject(securityContext).isPermitted(permission);
     }
 
     @Override
     public void dispose(SearchUser searchUser) {
-
     }
 }
