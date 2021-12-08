444dc4903fe59f95f24b9f9cf9e4b8443644c44c
Dennis Oelkers <dennis@graylog.com>
2021-12-08 10:39:47 +0100

    Renaming variable.

--- graylog2-web-interface/src/views/logic/debounceWithPromise.ts
+++ graylog2-web-interface/src/views/logic/debounceWithPromise.ts
@@ -18,7 +18,7 @@ import { debounce } from 'lodash';
 
 type PromiseReturnType<T> = T extends (...args: any[]) => Promise<infer R> ? R : never;
 
-const debouncedValidateQuery = <T extends (...args: any[]) => Promise<any>>(fn: T, delay: number) => {
+const debounceWithPromise = <T extends (...args: any[]) => Promise<any>>(fn: T, delay: number) => {
   const debouncedFn = debounce((resolve: PromiseReturnType<T>, ...args: Parameters<T>) => fn(...args).then(resolve), delay);
 
   return (...args: Parameters<T>) => new Promise<PromiseReturnType<T>>((resolve: PromiseReturnType<T>) => {
@@ -26,4 +26,4 @@ const debouncedValidateQuery = <T extends (...args: any[]) => Promise<any>>(fn: 
   });
 };
 
-export default debouncedValidateQuery;
+export default debounceWithPromise;
