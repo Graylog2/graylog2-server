01a62cb3d84d22bfb1485d1822bd7886f10f6aa3
Linus Pahl <linus@graylog.com>
2021-12-10 16:52:08 +0100

    Normalize time range when validating search query.
    With this change we are using ISO 8601 for times in an absolute
    time range. The API expects this format and will throw an
    error when using a different format.

--- graylog2-web-interface/src/views/components/searchbar/queryvalidation/validateQuery.test.ts
+++ graylog2-web-interface/src/views/components/searchbar/queryvalidation/validateQuery.test.ts
@@ -18,30 +18,50 @@ import * as Immutable from 'immutable';
 import { waitFor } from 'wrappedTestingLibrary';
 
 import fetch from 'logic/rest/FetchProvider';
+import { StoreMock as MockStore } from 'helpers/mocking';
 
-import validateQuery from './validateQuery';
+import validateQuery, { ValidationQuery } from './validateQuery';
 
 jest.mock('logic/rest/FetchProvider', () => jest.fn(() => Promise.resolve()));
+jest.mock('stores/users/CurrentUserStore', () => ({ CurrentUserStore: MockStore('get') }));
 
 describe('validateQuery', () => {
+  const validationInput: ValidationQuery = {
+    queryString: 'source:',
+    timeRange: { type: 'relative', from: 300 } as const,
+    streams: ['stream-id'],
+    parameters: Immutable.Set(),
+    parameterBindings: Immutable.Map(),
+  };
+
+  const requestPayload = {
+    query: 'source:',
+    filter: undefined,
+    timerange: { type: 'relative', from: 300 },
+    streams: ['stream-id'],
+    parameters: Immutable.Set(),
+    parameter_bindings: Immutable.Map(),
+  };
+
   it('calls validate API', async () => {
+    await validateQuery(validationInput);
+
+    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+
+    expect(fetch).toHaveBeenCalledWith('POST', expect.any(String), requestPayload);
+  });
+
+  it('normalizes absolute time ranges', async () => {
     await validateQuery({
-      queryString: 'source:',
-      timeRange: { type: 'relative', from: 300 },
-      streams: ['stream-id'],
-      parameters: Immutable.Set(),
-      parameterBindings: Immutable.Map(),
+      ...validationInput,
+      timeRange: { type: 'absolute', from: '2021-01-01 16:00:00.000', to: '2021-01-01 17:00:00.000' },
     });
 
     await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
 
     const expectedPayload = {
-      query: 'source:',
-      filter: undefined,
-      timerange: { type: 'relative', from: 300 },
-      streams: ['stream-id'],
-      parameters: Immutable.Set(),
-      parameter_bindings: Immutable.Map(),
+      ...requestPayload,
+      timerange: { type: 'absolute', from: '2021-01-01T22:00:00.000Z', to: '2021-01-01T23:00:00.000Z' },
     };
 
     expect(fetch).toHaveBeenCalledWith('POST', expect.any(String), expectedPayload);
