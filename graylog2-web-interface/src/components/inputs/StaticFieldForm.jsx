421e947f4177dde7d3300d90fb1c4ffa7efe8db1
Dennis Oelkers <dennis@graylog.com>
2021-12-14 17:45:07 +0100

    Replacing custom validation with pattern.

--- graylog2-web-interface/src/components/inputs/StaticFieldForm.jsx
+++ graylog2-web-interface/src/components/inputs/StaticFieldForm.jsx
@@ -38,7 +38,9 @@ class StaticFieldForm extends React.Component {
 
   render() {
     return (
-      <BootstrapModalForm ref={(modal) => { this.modal = modal; }}
+      <BootstrapModalForm ref={(modal) => {
+        this.modal = modal;
+      }}
                           title="Add static field"
                           submitButtonText="Add field"
                           onSubmitForm={this._addStaticField}>
@@ -46,15 +48,23 @@ class StaticFieldForm extends React.Component {
           overwritten If the message already has that key. Key must only contain alphanumeric characters or
           underscores and not be a reserved field.
         </p>
-        <Input ref={(fieldName) => { this.fieldName = fieldName; }}
+        <Input ref={(fieldName) => {
+          this.fieldName = fieldName;
+        }}
                type="text"
                id="field-name"
                label="Field name"
-               className="validatable"
-               data-validate="alphanum_underscore"
                required
+               pattern="[A-Za-z0-9_]*"
+               title="Should consist only of alphanumeric characters and underscores."
                autoFocus />
-        <Input ref={(fieldValue) => { this.fieldValue = fieldValue; }} type="text" id="field-value" label="Field value" required />
+        <Input ref={(fieldValue) => {
+          this.fieldValue = fieldValue;
+        }}
+               type="text"
+               id="field-value"
+               label="Field value"
+               required />
       </BootstrapModalForm>
     );
   }
