e0fb42939e0409605d23cb93c6d36ff7b5144517
Dennis Oelkers <dennis@graylog.com>
2022-02-11 13:28:10 +0100

    Moving `FieldTypesMappingList` type to separate module.

---
+++ graylog2-web-interface/src/views/logic/fieldtypes/types.ts
@@ -0,0 +1,5 @@
+import type * as Immutable from 'immutable';
+
+import type FieldTypeMapping from 'views/logic/fieldtypes/FieldTypeMapping';
+
+export type FieldTypeMappingsList = Immutable.List<FieldTypeMapping>;
