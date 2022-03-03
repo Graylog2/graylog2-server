d48c2ea74c890254da5b5db69eb7c4a825d5aa79
Tomas Dvorak <tomas.dvorak@graylog.com>
2022-03-03 11:16:24 +0100

    added TZ

--- graylog2-server/src/main/java/org/graylog2/rest/models/system/sessions/responses/DefaultSessionResponseFactory.java
+++ graylog2-server/src/main/java/org/graylog2/rest/models/system/sessions/responses/DefaultSessionResponseFactory.java
@@ -50,7 +50,7 @@ public class DefaultSessionResponseFactory implements SessionResponseFactory {
 
     protected Date getValidUntil(Session session) {
         if(session.getTimeout() < 0) { // means "session never expires", which is not possible in cookie-based auth
-            return new DateTime().plus(Years.years(10)).toDate(); // careful, later we convert the date to seconds as int and it may overflow for too big values
+            return new DateTime(DateTimeZone.UTC ).plus(Years.years(10)).toDate(); // careful, later we convert the date to seconds as int and it may overflow for too big values
         } else {
             return new DateTime(session.getLastAccessTime(), DateTimeZone.UTC).plus(session.getTimeout()).toDate();
         }
