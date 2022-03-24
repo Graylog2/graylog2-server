25e2b047b0361681345e5f571ad4e7e4cb8aeef8
Dennis Oelkers <dennis@graylog.com>
2022-03-24 18:54:14 +0100

    Set URL prefix for asynchronously loaded assets. (Graylog2/graylog-plugin-integrations#964)
    Similar to Graylog2/graylog-plugin-collectorGraylog2/graylog-plugin-integrations#195, this PR explicitly
    sets the URL prefix for assets which are loaded asynchronously.

--- graylog-plugin-integrations/src/web/index.jsx
+++ graylog-plugin-integrations/src/web/index.jsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import 'webpack-entry';
+import './webpack-entry';
 
 import { PluginManifest, PluginStore } from 'graylog-web-plugin/plugin';
 
---
+++ graylog-plugin-integrations/src/web/webpack-entry.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import URI from 'urijs';
+
+import AppConfig from 'util/AppConfig';
+
+// The webpack-dev-server serves the assets from "/"
+const assetPrefix = AppConfig.gl2DevMode() ? '/' : '/assets/plugin/org.graylog.integrations.IntegrationsPlugin/';
+
+// If app prefix was not set, we need to tell webpack to load chunks from root instead of the relative URL path
+__webpack_public_path__ = URI.joinPaths(AppConfig.gl2AppPathPrefix(), assetPrefix).path() || assetPrefix;
