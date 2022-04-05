03ca5ccf8f11e33133ee73103fd72457a2819456
Dennis Oelkers <dennis@graylog.com>
2022-04-05 18:36:15 +0200

    Provide injectable flag indicating if server is in development. (#12349)
    * Provide injectable flag indicating if server is in development.
    
    * Adding license headers.
    
    * Properly binding `IndexHtmlGeneratorProvider`.

---
+++ graylog2-server/src/main/java/org/graylog2/web/IndexHtmlGeneratorProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.web;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+
+public class IndexHtmlGeneratorProvider implements Provider<IndexHtmlGenerator> {
+    private final Provider<? extends IndexHtmlGenerator> indexHtmlGeneratorProvider;
+
+    @Inject
+    public IndexHtmlGeneratorProvider(Provider<DevelopmentIndexHtmlGenerator> developmentIndexHtmlGeneratorProvider,
+                                      Provider<ProductionIndexHtmlGenerator> productionIndexHtmlGeneratorProvider,
+                                      @Named("isDevelopmentServer") Boolean isDevelopmentServer) {
+        // In development mode we use an external process to provide the web interface.
+        // To avoid errors because of missing production web assets, we use a different implementation for
+        // generating the "index.html" page.
+        this.indexHtmlGeneratorProvider = isDevelopmentServer
+                ? developmentIndexHtmlGeneratorProvider
+                : productionIndexHtmlGeneratorProvider;
+    }
+
+    @Override
+    public IndexHtmlGenerator get() {
+        return indexHtmlGeneratorProvider.get();
+    }
+}
