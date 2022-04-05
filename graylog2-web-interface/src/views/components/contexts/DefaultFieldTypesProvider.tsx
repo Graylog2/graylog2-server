cafeda32ac6d5f483d347f66e36debc446e84071
Gaya Kessler <gaya@theclevernode.com>
2022-04-05 16:27:29 +0200

    fix: change to relative time range

--- graylog2-web-interface/src/views/components/contexts/DefaultFieldTypesProvider.tsx
+++ graylog2-web-interface/src/views/components/contexts/DefaultFieldTypesProvider.tsx
@@ -21,13 +21,13 @@ import PropTypes from 'prop-types';
 
 import useFieldTypes from 'views/logic/fieldtypes/useFieldTypes';
 import { filtersToStreamSet } from 'views/logic/queries/Query';
-import type { AbsoluteTimeRange } from 'views/logic/queries/Query';
+import type { RelativeTimeRange } from 'views/logic/queries/Query';
 import useCurrentQuery from 'views/logic/queries/useCurrentQuery';
 
 import FieldTypesContext from './FieldTypesContext';
 
 const defaultId = '';
-const defaultTimeRange: AbsoluteTimeRange = { type: 'absolute', from: 'now-15m', to: 'now' };
+const defaultTimeRange: RelativeTimeRange = { type: 'relative', from: 300 };
 
 const DefaultFieldTypesProvider = ({ children }: { children: React.ReactElement }) => {
   const currentQuery = useCurrentQuery();
