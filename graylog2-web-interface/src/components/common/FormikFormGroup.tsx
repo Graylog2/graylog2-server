2e6361c096f78e1bd764210ac36bcd44a069c83b
Ousmane Samba <ousmane@graylog.com>
2022-04-07 11:03:04 +0200

    add checked prop to FormikFormGroup

--- graylog2-web-interface/src/components/common/FormikFormGroup.tsx
+++ graylog2-web-interface/src/components/common/FormikFormGroup.tsx
@@ -38,6 +38,7 @@ type Props = {
   minLength?: number,
   maxLength?: number,
   required?: boolean,
+  checked?: boolean,
   bsSize?: 'large' | 'small' | 'xsmall',
   validate?: (any) => string | undefined,
 };
@@ -69,6 +70,7 @@ FormikFormGroup.defaultProps = {
   minLength: undefined,
   maxLength: undefined,
   required: false,
+  checked: undefined,
   validate: () => undefined,
 };
 
