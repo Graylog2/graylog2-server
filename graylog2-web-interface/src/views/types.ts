1099e655863fdc528382a0acd6a6578222f2fdc3
maxiadlovskii <maksym.yadlovskyi@graylog.com>
2022-04-19 13:34:31 +0200

    Add SearchFilterBar component. Change markup in SearchBar (#12441)
    * Add SearchFilterBar component. Change markup in SearchBar
    
    * Add license
    
    * Making naming for SearchBar plugins more generic
    
    * Make rendering of pluggable search controls more generic
    
    * Change label and button size
    
    * Turnoff feature flag for search filter
    
    * Fix type definition for SearchBarControls
    
    * Refactoring conditions in PluggableSearchControls.tsx
    
    * Remove gap when no items to render. Change SearchBarControl interface to handle non-valid license
    
    * Remove unnecessary styled component
    
    * Rename file to PluggableSearchBarControls. Remove use memo
    
    * Fix condition
    
    * Off feature flag

--- graylog2-web-interface/src/views/types.ts
+++ graylog2-web-interface/src/views/types.ts
@@ -235,6 +235,12 @@ interface MessageRowOverrideProps {
   renderMessageRow: () => React.ReactNode,
 }
 
+export interface SearchBarControl {
+  component: React.ComponentType;
+  id: string;
+  placement: 'left' | 'right';
+}
+
 declare module 'graylog-web-plugin/plugin' {
   export interface PluginExports {
     creators?: Array<Creator>;
@@ -248,7 +254,8 @@ declare module 'graylog-web-plugin/plugin' {
     'views.completers'?: Array<Completer>;
     'views.components.widgets.messageTable.previewOptions'?: Array<MessagePreviewOption>;
     'views.components.widgets.messageTable.messageRowOverride'?: Array<React.ComponentType<MessageRowOverrideProps>>;
-    'views.components.widgets.messageDetails.contextProviders'?: Array<React.ComponentType<MessageDetailContextProviderProps>>,
+    'views.components.widgets.messageDetails.contextProviders'?: Array<React.ComponentType<MessageDetailContextProviderProps>>;
+    'views.components.searchBar'?: Array<() => SearchBarControl | null>;
     'views.elements.header'?: Array<React.ComponentType>;
     'views.elements.queryBar'?: Array<React.ComponentType>;
     'views.elements.validationErrorExplanation'?: Array<React.ComponentType<{ validationState: QueryValidationState }>>;
