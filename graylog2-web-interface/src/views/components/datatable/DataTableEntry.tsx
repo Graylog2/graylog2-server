f1339c08f6fa29fba1e256bc7f2f46557aebb572
Linus Pahl <linus@graylog.com>
2022-04-26 15:44:29 +0200

    Memorize `AdditionalContext` value in `DataTable`.

--- graylog2-web-interface/src/views/components/datatable/DataTableEntry.tsx
+++ graylog2-web-interface/src/views/components/datatable/DataTableEntry.tsx
@@ -55,15 +55,26 @@ type Props = {
 
 const _c = (field, value, path, source) => ({ field, value, path, source });
 
-const _column = (field: string, value: any, selectedQuery: string, idx: number, type: FieldType, valuePath: ValuePath, source: string | undefined | null) => (
-  <StyledTd isNumeric={type.isNumeric()} key={`${selectedQuery}-${field}=${value}-${idx}`}>
-    <AdditionalContext.Provider value={{ valuePath }}>
-      <CustomHighlighting field={source ?? field} value={value}>
-        {value !== null && value !== undefined ? <Value field={source ?? field} type={type} value={value} queryId={selectedQuery} render={DecoratedValue} /> : null}
-      </CustomHighlighting>
-    </AdditionalContext.Provider>
-  </StyledTd>
-);
+const _column = (field: string, value: any, selectedQuery: string, idx: number, type: FieldType, valuePath: ValuePath, source: string | undefined | null) => {
+  const additionalContextValue = useMemo(() => ({ valuePath }), [valuePath]);
+
+  return (
+    <StyledTd isNumeric={type.isNumeric()} key={`${selectedQuery}-${field}=${value}-${idx}`}>
+      <AdditionalContext.Provider value={additionalContextValue}>
+        <CustomHighlighting field={source ?? field} value={value}>
+          {value !== null && value !== undefined
+            ? (
+              <Value field={source ?? field}
+                     type={type}
+                     value={value}
+                     queryId={selectedQuery}
+                     render={DecoratedValue} />
+            ) : null}
+        </CustomHighlighting>
+      </AdditionalContext.Provider>
+    </StyledTd>
+  );
+};
 
 const fullValuePathForField = (fieldName, valuePath) => {
   const currentSeries = parseSeries(fieldName);
