2a2bb98fa34338749073c653d4f221bf588cf40d
Martin Wilhelmi <martin.wilhelmi@graylog.com>
2022-04-26 14:23:47 +0000

    feat: add basic auth and api key/secret attributes to http notifications

--- graylog2-web-interface/src/components/event-notifications/event-notification-types/HttpNotificationForm.jsx
+++ graylog2-web-interface/src/components/event-notifications/event-notification-types/HttpNotificationForm.jsx
@@ -19,6 +19,7 @@ import PropTypes from 'prop-types';
 import lodash from 'lodash';
 
 import { URLWhiteListInput } from 'components/common';
+import { Button, Col, Input, Row } from 'components/bootstrap';
 import * as FormsUtils from 'util/FormsUtils';
 
 class HttpNotificationForm extends React.Component {
@@ -31,7 +32,32 @@ class HttpNotificationForm extends React.Component {
 
   static defaultConfig = {
     url: '',
+    api_key: '',
+    api_secret: { keep_value: true },
+    basic_auth: { keep_value: true },
   };
+
+  constructor() {
+    super();
+
+    this.state = {
+      api_secret: '',
+      basic_auth: '',
+    };
+  }
+
+  componentDidMount() {
+    const { config, onChange } = this.props;
+    const nextConfig = lodash.cloneDeep(config);
+
+    nextConfig.basic_auth = config.basic_auth?.is_set ? { keep_value: true } : null;
+    nextConfig.api_secret = config.api_secret?.is_set ? { keep_value: true } : null;
+
+    onChange(nextConfig);
+
+    this.setState({ basic_auth: config.basic_auth.is_set ? '******' : '' });
+    this.setState({ api_secret: config.api_secret.is_set ? '******' : '' });
+  }
 
   propagateChange = (key, value) => {
     const { config, onChange } = this.props;
@@ -43,8 +69,18 @@ class HttpNotificationForm extends React.Component {
 
   handleChange = (event) => {
     const { name } = event.target;
+    const inputValue = FormsUtils.getValueFromInput(event.target);
 
-    this.propagateChange(name, FormsUtils.getValueFromInput(event.target));
+    this.propagateChange(name, inputValue);
+  };
+
+  handleSecretInputChange = (event) => {
+    const { name } = event.target;
+    const inputValue = FormsUtils.getValueFromInput(event.target);
+    const value = inputValue.length === 0 ? { delete_value: true } : { set_value: inputValue };
+
+    this.setState({ [name]: inputValue });
+    this.propagateChange(name, value);
   };
 
   onValidationChange = (validationState) => {
@@ -53,16 +89,89 @@ class HttpNotificationForm extends React.Component {
     setIsSubmitEnabled(validationState !== 'error');
   };
 
+  resetSecret = (attribute) => {
+    this.propagateChange(attribute, { delete_value: true });
+    this.setState({ [attribute]: '' });
+  };
+
+  undoResetSecret = (attribute) => {
+    this.propagateChange(attribute, { keep_value: true });
+    this.setState({ [attribute]: '******' });
+  };
+
   render() {
     const { config, validation } = this.props;
+    const { api_secret, basic_auth } = config;
+    const basicAuthColumnSize = basic_auth?.keep_value || basic_auth?.delete_value ? 10 : 12;
+    const apiSecretColumnSize = api_secret?.keep_value || api_secret?.delete_value ? 10 : 12;
 
     return (
-      <URLWhiteListInput label="URL"
-                         onChange={this.handleChange}
-                         validationState={validation.errors.url ? 'error' : null}
-                         validationMessage={lodash.get(validation, 'errors.url[0]', 'The URL to POST to when an Event occurs.')}
-                         onValidationChange={this.onValidationChange}
-                         url={config.url} />
+      <>
+        <URLWhiteListInput label="URL"
+                           onChange={this.handleChange}
+                           validationState={validation.errors.url ? 'error' : null}
+                           validationMessage={lodash.get(validation, 'errors.url[0]', 'The URL to POST to when an Event occurs.')}
+                           onValidationChange={this.onValidationChange}
+                           url={config.url} />
+        <Row>
+          <Col md={basicAuthColumnSize}>
+            <Input id="basicAuth"
+                   onChange={this.handleSecretInputChange}
+                   label="Basic Authentication"
+                   type="password"
+                   name="basic_auth"
+                   value={this.state.basic_auth || ''} />
+          </Col>
+          {basic_auth?.keep_value === true && (
+            <Col md={2}>
+              <Button bsStyle="danger" type="button" onClick={() => { this.resetSecret('basic_auth'); }}>
+                Reset Secret
+              </Button>
+            </Col>
+          )}
+          {basic_auth?.delete_value === true && (
+            <Col md={2}>
+              <Button type="button" onClick={() => { this.undoResetSecret('basic_auth'); }}>
+                Undo Reset
+              </Button>
+            </Col>
+          )}
+        </Row>
+        <Input id="api_key"
+               name="api_key"
+               label="API Key"
+               type="text"
+               onChange={this.handleChange}
+               bsStyle={validation.errors.api_key ? 'error' : null}
+               help={lodash.get(validation, 'errors.api_key[0]', 'The API Key.')}
+               value={config.api_key} />
+        <Row>
+          <Col md={apiSecretColumnSize}>
+            <Input id="api_secret"
+                   name="api_secret"
+                   label="API Secret"
+                   type="password"
+                   onChange={this.handleSecretInputChange}
+                   bsStyle={validation.errors.api_secret ? 'error' : null}
+                   help={lodash.get(validation, 'errors.api_secret[0]', 'The API Secret')}
+                   value={this.state.api_secret || ''} />
+          </Col>
+          {api_secret?.keep_value === true && (
+            <Col md={2}>
+              <Button bsStyle="danger" type="button" onClick={() => { this.resetSecret('api_secret'); }}>
+                Reset Secret
+              </Button>
+            </Col>
+          )}
+          {api_secret?.delete_value === true && (
+            <Col md={2}>
+              <Button type="button" onClick={() => { this.undoResetSecret('api_secret'); }}>
+                Undo Reset
+              </Button>
+            </Col>
+          )}
+        </Row>
+      </>
     );
   }
 }
