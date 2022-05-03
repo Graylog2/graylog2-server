0745b98445eccfb579890674add366eef5289da6
Othello Maurer <othello@graylog.com>
2022-05-03 10:06:33 +0200

    Add tests for loading different mapping types

--- graylog2-server/src/test/java/org/graylog/metrics/prometheus/mapping/PrometheusMappingConfigLoaderTest.java
+++ graylog2-server/src/test/java/org/graylog/metrics/prometheus/mapping/PrometheusMappingConfigLoaderTest.java
@@ -22,6 +22,8 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.prometheus.client.dropwizard.samplebuilder.MapperConfig;
 import org.graylog2.plugin.system.NodeId;
+import org.graylog2.shared.inputs.InputDescription;
+import org.graylog2.shared.inputs.MessageInputFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,6 +36,8 @@ import java.util.Collections;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -41,20 +45,26 @@ class PrometheusMappingConfigLoaderTest {
     @Mock
     NodeId nodeId;
 
+    @Mock
+    MessageInputFactory messageInputFactory;
+
     PrometheusMappingConfigLoader configLoader;
     ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
-        when(nodeId.toString()).thenReturn("abc-123");
-
         this.configLoader = new PrometheusMappingConfigLoader(
-                ImmutableMap.of(MetricMatchMapping.TYPE, config -> new MetricMatchMapping(nodeId, config)));
+                ImmutableMap.of(
+                        MetricMatchMapping.TYPE, config -> new MetricMatchMapping(nodeId, config),
+                        InputMetricMapping.TYPE, config -> new InputMetricMapping(messageInputFactory, nodeId, config))
+        );
         this.objectMapper = new ObjectMapper(new YAMLFactory());
     }
 
     @Test
     void loadBytes() throws Exception {
+        when(nodeId.toString()).thenReturn("abc-123");
+
         final Map<String, ImmutableList<Serializable>> config = Collections.singletonMap("metric_mappings", ImmutableList.of(
                 ImmutableMap.of(
                         "metric_name", "test1",
@@ -101,5 +111,78 @@ class PrometheusMappingConfigLoaderTest {
                         )
                 )
         );
+    }
+
+    @Test
+    void defaultType() throws Exception {
+        when(nodeId.toString()).thenReturn("abc-123");
+
+        final Map<String, ImmutableList<Serializable>> config = Collections.singletonMap("metric_mappings",
+                ImmutableList.of(
+                        ImmutableMap.of("metric_name", "test1", "match_pattern", "foo.bar")));
+
+        assertThat(configLoader.load(new ByteArrayInputStream(objectMapper.writeValueAsBytes(config))))
+                .containsExactlyInAnyOrder(new MapperConfig(
+                        "foo.bar",
+                        "gl_test1",
+                        ImmutableMap.of("node", "abc-123")));
+    }
+
+    @Test
+    void metricMatchType() throws Exception {
+        when(nodeId.toString()).thenReturn("abc-123");
+
+        final Map<String, ImmutableList<Serializable>> config = Collections.singletonMap("metric_mappings",
+                ImmutableList.of(
+                        ImmutableMap.of(
+                                "type", "metric_match",
+                                "metric_name", "test1",
+                                "match_pattern", "foo.bar"
+                        )));
+
+        assertThat(configLoader.load(new ByteArrayInputStream(objectMapper.writeValueAsBytes(config))))
+                .containsExactlyInAnyOrder(new MapperConfig(
+                        "foo.bar",
+                        "gl_test1",
+                        ImmutableMap.of("node", "abc-123")));
+    }
+
+    @Test
+    void inputMetricType() throws Exception {
+        when(nodeId.toString()).thenReturn("abc-123");
+        when(messageInputFactory.getAvailableInputs()).thenReturn(
+                ImmutableMap.of("test.input", mock(InputDescription.class)));
+
+        final Map<String, ImmutableList<Serializable>> config = Collections.singletonMap("metric_mappings",
+                ImmutableList.of(
+                        ImmutableMap.of(
+                                "type", "input_metric",
+                                "metric_name", "test1",
+                                "input_metric_name", "foo.bar"
+                        )));
+
+        assertThat(configLoader.load(new ByteArrayInputStream(objectMapper.writeValueAsBytes(config))))
+                .containsExactlyInAnyOrder(new MapperConfig(
+                        "test.input.*.foo.bar",
+                        "gl_test1",
+                        ImmutableMap.of(
+                                "node", "abc-123",
+                                "input_id", "${0}",
+                                "input_type", "test.input"
+                        )));
+    }
+
+    @Test
+    void unknownType() {
+        final Map<String, ImmutableList<Serializable>> config = Collections.singletonMap("metric_mappings",
+                ImmutableList.of(
+                        ImmutableMap.of(
+                                "type", "unknown",
+                                "metric_name", "test1",
+                                "unknown_property", "foo.bar"
+                        )));
+
+        assertThatThrownBy(() -> configLoader.load(new ByteArrayInputStream(objectMapper.writeValueAsBytes(config))))
+                .hasMessageContaining("Could not resolve type id 'unknown'");
     }
 }
