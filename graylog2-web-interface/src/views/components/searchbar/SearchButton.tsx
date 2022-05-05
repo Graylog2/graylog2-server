81d39592d5d7e97d8f599dbce541d1bdad4bf570
Linus Pahl <linus@graylog.com>
2022-05-05 15:39:26 +0200

    Simplify implementation of `SearchButton`.

--- graylog2-web-interface/src/views/components/searchbar/SearchButton.tsx
+++ graylog2-web-interface/src/views/components/searchbar/SearchButton.tsx
@@ -16,6 +16,7 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
+import type { DefaultTheme } from 'styled-components';
 import styled, { css } from 'styled-components';
 
 import { Button } from 'components/bootstrap';
@@ -23,24 +24,23 @@ import { Icon } from 'components/common';
 import QueryValidationActions from 'views/actions/QueryValidationActions';
 import type { IconName } from 'components/common/Icon';
 
-const StyledButton = styled(Button)`
+const StyledButton = styled(Button)(({ theme, $dirty }: { theme: DefaultTheme, $dirty: boolean }) => css`
+  position: relative;
   margin-right: 12px;
   min-width: 61px;
-`;
 
-const DirtyButton = styled(StyledButton)(({ theme }) => css`
-  position: relative;
-
-  &::after {
-    position: absolute;
-    content: '';
-    height: 16px;
-    width: 16px;
-    top: -5px;
-    right: -6px;
-    border-radius: 50%;
-    background-color: ${theme.colors.variant.warning};
-  }
+  ${$dirty ? css` 
+    &::after {
+      position: absolute;
+      content: '';
+      height: 16px;
+      width: 16px;
+      top: -5px;
+      right: -6px;
+      border-radius: 50%;
+      background-color: ${theme.colors.variant.warning};
+    }
+  ` : ''}
 `);
 
 type Props = {
@@ -56,35 +56,20 @@ const onButtonClick = (e: MouseEvent, disabled: Boolean) => {
   }
 };
 
-const COMMON_PROPS = {
-  type: 'submit',
-  bsStyle: 'success',
-};
-
-const DirtySearchButton = ({ glyph, className, disabled }: { disabled: boolean, glyph: IconName, className: string }) => (
-  <DirtyButton onClick={(e) => onButtonClick(e, disabled)}
-               title="Perform search (changes were made after last search execution)"
-               className={className}
-               {...COMMON_PROPS}>
-    <Icon name={glyph} />
-  </DirtyButton>
-);
-
-const CleanSearchButton = ({ disabled, glyph, className }: { disabled: boolean, glyph: IconName, className: string }) => (
-  <StyledButton onClick={(e) => onButtonClick(e, disabled)}
-                title="Perform search"
-                className={className}
-                {...COMMON_PROPS}>
-    <Icon name={glyph} />
-  </StyledButton>
-);
-
-const SearchButton = ({ dirty, disabled, ...rest }: Props) => {
+const SearchButton = ({ dirty, disabled, glyph }: Props) => {
   const className = disabled ? 'disabled' : '';
+  const title = dirty ? 'Perform search (changes were made after last search execution)' : 'Perform Search';
 
-  return dirty
-    ? <DirtySearchButton className={className} disabled={disabled} {...rest} />
-    : <CleanSearchButton className={className} disabled={disabled} {...rest} />;
+  return (
+    <StyledButton onClick={(e) => onButtonClick(e, disabled)}
+                  title={title}
+                  className={className}
+                  type="submit"
+                  bsStyle="success"
+                  $dirty={dirty}>
+      <Icon name={glyph} />
+    </StyledButton>
+  );
 };
 
 SearchButton.defaultProps = {
