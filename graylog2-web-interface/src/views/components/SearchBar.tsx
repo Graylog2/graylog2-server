6a6b68ea0f84506b2cc1b739fe461e611eb3b4a4
Linus Pahl <linus@graylog.com>
2022-05-05 15:13:41 +0200

    Reducing amount of rerenders of search query input.

--- graylog2-web-interface/src/views/components/SearchBar.tsx
+++ graylog2-web-interface/src/views/components/SearchBar.tsx
@@ -223,15 +223,12 @@ const SearchBar = ({
                                   <QueryInput value={value}
                                               timeRange={values.timerange}
                                               streams={values.streams}
-                                              placeholder='Type your search query here and press enter. E.g.: ("not found" AND http) OR http_response_code:[400 TO 404]'
-                                              error={error}
-                                              isValidating={isValidating}
-                                              warning={warnings.queryString}
-                                              onChange={(newQuery) => {
-                                                onChange({ target: { value: newQuery, name } });
-
-                                                return Promise.resolve(newQuery);
-                                              }}
+                                              name={name}
+                                                onChange={onChange}
+                                                placeholder='Type your search query here and press enter. E.g.: ("not found" AND http) OR http_response_code:[400 TO 404]'
+                                                error={error}
+                                                isValidating={isValidating}
+                                                warning={warnings.queryString}
                                               disableExecution={disableSearchSubmit}
                                               validate={validateForm}
                                               onExecute={handleSubmit as () => void} />
