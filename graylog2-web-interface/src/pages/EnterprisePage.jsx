0c8a836b94c456ccfcbf7dc9b7e7bd8e5aaeccae
Ousmane Samba <ousmane@graylog.com>
2022-05-13 16:39:19 +0200

    refactor and get clusterId

--- graylog2-web-interface/src/pages/EnterprisePage.jsx
+++ graylog2-web-interface/src/pages/EnterprisePage.jsx
@@ -15,54 +15,17 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import React from 'react';
-import PropTypes from 'prop-types';
-// eslint-disable-next-line no-restricted-imports
-import createReactClass from 'create-react-class';
 import styled, { css } from 'styled-components';
 
-import { DocumentTitle, PageHeader } from 'components/common';
-import { Button, ButtonToolbar, Col, Row } from 'components/bootstrap';
+import { useStore } from 'stores/connect';
+import { NodesStore } from 'stores/nodes/NodesStore';
+import { DocumentTitle, PageHeader, Spinner } from 'components/common';
+import { Col, Row } from 'components/bootstrap';
 import { GraylogClusterOverview } from 'components/cluster';
 import PluginList from 'components/enterprise/PluginList';
+import EnterpriseProductLink from 'components/enterprise/EnterpriseProductLink';
+import ProductLink from 'components/enterprise/ProductLink';
 import HideOnCloud from 'util/conditional/HideOnCloud';
-
-const EnterpriseProductLink = ({ children }) => {
-  return (
-    <a href="https://www.graylog.org/products/enterprise"
-       rel="noopener noreferrer"
-       target="_blank">
-      {children}
-    </a>
-  );
-};
-
-const ProductLink = ({ href, clusterId, children }) => {
-  let hrefWithParam = href;
-
-  if (clusterId) {
-    hrefWithParam = `${hrefWithParam}?cluster_id=${clusterId}`;
-  }
-
-  return (
-    <ButtonToolbar>
-      <Button type="link"
-              target="_blank"
-              rel="noopener noreferrer"
-              href={hrefWithParam}
-              bsStyle="primary">
-        {children}
-      </Button>
-    </ButtonToolbar>
-  );
-};
-
-EnterpriseProductLink.propTypes = {
-  children: PropTypes.node,
-};
-
-EnterpriseProductLink.defaultProps = {
-  children: null,
-};
 
 const BiggerFontSize = styled.div(({ theme }) => css`
   font-size: ${theme.fonts.size.large};
@@ -72,65 +35,65 @@ const GraylogEnterpriseHeader = styled.h2`
   margin-bottom: 10px;
 `;
 
-const EnterprisePage = createReactClass({
-  displayName: 'EnterprisePage',
+const EnterprisePage = () => {
+  const nodes = useStore(NodesStore);
 
-  render() {
-    // TODO: Actually request and set clusterId in state
-    // const { clusterId } = this.state;
-    const clusterId = 'abc-123';
+  if (!nodes) {
+    return <Spinner />;
+  }
 
-    return (
-      <DocumentTitle title="Try Graylog Enterprise">
-        <div>
-          <PageHeader title="Try Graylog Enterprise">
-            {null}
+  const { clusterId } = nodes;
 
-            <span>
-              Graylog Enterprise adds commercial functionality to the Open Source Graylog core. You can learn more
-              about Graylog Enterprise on the <EnterpriseProductLink>product page</EnterpriseProductLink>.
-            </span>
-          </PageHeader>
+  return (
+    <DocumentTitle title="Try Graylog Enterprise">
+      <div>
+        <PageHeader title="Try Graylog Enterprise">
+          {null}
 
-          <GraylogClusterOverview layout="compact">
-            <PluginList />
-          </GraylogClusterOverview>
-          <HideOnCloud>
-            <Row className="content">
-              <Col md={6}>
-                <GraylogEnterpriseHeader>Graylog Operations</GraylogEnterpriseHeader>
-                <BiggerFontSize>
-                  <p>
-                    Designed to meet the needs of resource-constrained IT Operations and Software Engineering teams,
-                    Graylog Operations provides numerous productivity enhancements that will save you thousands of
-                    hours per year in collecting and analyzing log data to uncover the root cause of performance,
-                    outage, and error issues.
-                  </p>
-                  <ProductLink href="https://go2.graylog.org/request-graylog-operations" clusterId={clusterId}>
-                    Request now
-                  </ProductLink>
-                </BiggerFontSize>
-              </Col>
-              <Col md={6}>
-                <GraylogEnterpriseHeader>Graylog Security</GraylogEnterpriseHeader>
-                <BiggerFontSize>
-                  <p>
-                    Extend Graylog Open’s capabilities for detecting, investigating, and responding to cybersecurity
-                    threats with security-specific dashboards and alerts, anomaly detection AI/ML engine,
-                    integrations with other security tools, SOAR capabilities, and numerous compliance reporting
-                    features.
-                  </p>
-                  <ProductLink href="https://go2.graylog.org/request-graylog-security" clusterId={clusterId}>
-                    Request now
-                  </ProductLink>
-                </BiggerFontSize>
-              </Col>
-            </Row>
-          </HideOnCloud>
-        </div>
-      </DocumentTitle>
-    );
-  },
-});
+          <span>
+            Graylog Enterprise adds commercial functionality to the Open Source Graylog core. You can learn more
+            about Graylog Enterprise on the <EnterpriseProductLink>product page</EnterpriseProductLink>.
+          </span>
+        </PageHeader>
+
+        <GraylogClusterOverview layout="compact">
+          <PluginList />
+        </GraylogClusterOverview>
+        <HideOnCloud>
+          <Row className="content">
+            <Col md={6}>
+              <GraylogEnterpriseHeader>Graylog Operations</GraylogEnterpriseHeader>
+              <BiggerFontSize>
+                <p>
+                  Designed to meet the needs of resource-constrained IT Operations and Software Engineering teams,
+                  Graylog Operations provides numerous productivity enhancements that will save you thousands of
+                  hours per year in collecting and analyzing log data to uncover the root cause of performance,
+                  outage, and error issues.
+                </p>
+                <ProductLink href="https://go2.graylog.org/request-graylog-operations" clusterId={clusterId}>
+                  Request now
+                </ProductLink>
+              </BiggerFontSize>
+            </Col>
+            <Col md={6}>
+              <GraylogEnterpriseHeader>Graylog Security</GraylogEnterpriseHeader>
+              <BiggerFontSize>
+                <p>
+                  Extend Graylog Open’s capabilities for detecting, investigating, and responding to cybersecurity
+                  threats with security-specific dashboards and alerts, anomaly detection AI/ML engine,
+                  integrations with other security tools, SOAR capabilities, and numerous compliance reporting
+                  features.
+                </p>
+                <ProductLink href="https://go2.graylog.org/request-graylog-security" clusterId={clusterId}>
+                  Request now
+                </ProductLink>
+              </BiggerFontSize>
+            </Col>
+          </Row>
+        </HideOnCloud>
+      </div>
+    </DocumentTitle>
+  );
+};
 
 export default EnterprisePage;
