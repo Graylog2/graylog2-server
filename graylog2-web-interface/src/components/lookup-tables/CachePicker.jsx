3495824cd6f8f5d9c5f31266d9dec75068b70acd
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2022-06-09 12:33:24 +0200

    introduced formik to LookupTableForm

--- graylog2-web-interface/src/components/lookup-tables/CachePicker.jsx
+++ graylog2-web-interface/src/components/lookup-tables/CachePicker.jsx
@@ -14,58 +14,45 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import PropTypes from 'prop-types';
 import React from 'react';
+import { useField } from 'formik';
 import naturalSort from 'javascript-natural-sort';
-import { PluginStore } from 'graylog-web-plugin/plugin';
 
 import { Input } from 'components/bootstrap';
 import { Select } from 'components/common';
 
-class CachePicker extends React.Component {
-  static propTypes = {
-    onSelect: PropTypes.func.isRequired,
-    selectedId: PropTypes.string,
-    caches: PropTypes.array,
-    pagination: PropTypes.object,
-  };
+const CachePicker = ({
+  name = 'cache_id',
+  caches = [],
+}) => {
+  const [, { value, touched, error }, { setTouched, setValue }] = useField(name);
+  
+  const sortedCaches = caches.map((cache) => {
+    return { value: cache.id, label: `${cache.title} (${cache.name})` };
+  }).sort((a, b) => naturalSort(a.label.toLowerCase(), b.label.toLowerCase()));
 
-  static defaultProps = {
-    selectedId: null,
-    caches: [],
-    pagination: {},
-  };
+  const errorMsg = touched ? error : '';
 
-  render() {
-    const cachePlugins = {};
-
-    PluginStore.exports('lookupTableCaches').forEach((p) => {
-      cachePlugins[p.type] = p;
-    });
-
-    const sortedCaches = this.props.caches.map((cache) => {
-      return { value: cache.id, label: `${cache.title} (${cache.name})` };
-    }).sort((a, b) => naturalSort(a.label.toLowerCase(), b.label.toLowerCase()));
-
-    return (
-      <fieldset>
-        <Input id="cache-select"
-               label="Cache"
-               required
-               autoFocus
-               help="Select an existing cache"
-               labelClassName="col-sm-3"
-               wrapperClassName="col-sm-9">
-          <Select placeholder="Select a cache"
-                  clearable={false}
-                  options={sortedCaches}
-                  matchProp="label"
-                  onChange={this.props.onSelect}
-                  value={this.props.selectedId} />
-        </Input>
-      </fieldset>
-    );
-  }
-}
+  return (
+    <fieldset>
+      <Input id="cache-select"
+             label="Cache"
+             required
+             autoFocus
+             bsStyle={errorMsg ? 'error' : undefined}
+             help={errorMsg || 'Select an existing cache'}
+             labelClassName="col-sm-3"
+             wrapperClassName="col-sm-9">
+        <Select placeholder="Select a cache"
+                clearable={false}
+                options={sortedCaches}
+                matchProp="label"
+                onBlur={() => setTouched(true)}
+                onChange={setValue}
+                value={value} />
+      </Input>
+    </fieldset>
+  );
+};
 
 export default CachePicker;
