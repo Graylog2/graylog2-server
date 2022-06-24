6fabfa2e0eb508b425f17349428ffbc810f74f52
Dennis Oelkers <dennis@graylog.com>
2022-06-24 15:14:56 +0200

    Typing `SortUtils`.

--- graylog2-web-interface/src/util/SortUtils.ts
+++ graylog2-web-interface/src/util/SortUtils.ts
@@ -18,21 +18,19 @@ import moment from 'moment';
 
 import { defaultCompare as naturalSort } from 'logic/DefaultCompare';
 
-// sortOrder: "asc"|"desc"
-export function sortByDate(d1, d2, sortOrder) {
-  const effectiveSortOrder = sortOrder || 'asc';
+type SortOrder = 'asc' | 'desc';
+
+export function sortByDate(d1: string, d2: string, sortOrder: SortOrder = 'asc') {
   const d1Time = moment(d1);
   const d2Time = moment(d2);
 
-  if (effectiveSortOrder === 'asc') {
+  if (sortOrder === 'asc') {
     return (d1Time.isBefore(d2Time) ? -1 : d2Time.isBefore(d1Time) ? 1 : 0);
   }
 
   return (d2Time.isBefore(d1Time) ? -1 : d1Time.isBefore(d2Time) ? 1 : 0);
 }
 
-export function naturalSortIgnoreCase(s1, s2, sortOrder) {
-  const effectiveSortOrder = sortOrder || 'asc';
-
-  return (effectiveSortOrder === 'asc' ? naturalSort(s1.toLowerCase(), s2.toLowerCase()) : naturalSort(s2.toLowerCase(), s1.toLowerCase()));
+export function naturalSortIgnoreCase(s1: string, s2: string, sortOrder: SortOrder = 'asc') {
+  return (sortOrder === 'asc' ? naturalSort(s1.toLowerCase(), s2.toLowerCase()) : naturalSort(s2.toLowerCase(), s1.toLowerCase()));
 }
