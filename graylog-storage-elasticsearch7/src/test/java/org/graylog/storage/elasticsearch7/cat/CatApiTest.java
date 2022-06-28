ab06fd39fc63cfc5ae1f03cac2a982cbc7302044
luk-kaminski <lukasz.kaminski@graylog.com>
2022-06-28 13:57:26 +0200

    Correct behavior when _cat/nodes API from the search engine returns n… (#12941)
    * Correct behavior when _cat/nodes API from the search engine returns nodes without disk/file statistics present.
    
    * Missing license headers
    
    * Minor change : Catching only specific exception.

---
+++ graylog-storage-elasticsearch7/src/test/java/org/graylog/storage/elasticsearch7/cat/CatApiTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.storage.elasticsearch7.cat;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.graylog.storage.elasticsearch7.ElasticsearchClient;
+import org.graylog2.shared.bindings.providers.ObjectMapperProvider;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CatApiTest {
+
+    private final static String SAMPLE_CAT_NODES_RESPONSE = "[" +
+            "{" +
+            "\"id\":\"nodeWithCorrectInfo\"," +
+            "\"name\":\"nodeWithCorrectInfo\"," +
+            "\"role\":\"dimr\"," +
+            "\"ip\":\"182.88.0.2\"," +
+            "\"fileDescriptorMax\":\"1048576\"," +
+            "\"diskUsed\":\"45gb\"," +
+            "\"diskTotal\":\"411.5gb\"," +
+            "\"diskUsedPercent\":\"10.95\"" +
+            "}," +
+            "{" +
+            "\"id\":\"nodeWithMissingDiskStatistics\"," +
+            "\"name\":\"nodeWithMissingDiskStatistics\"," +
+            "\"role\":\"dimr\"," +
+            "\"ip\":\"182.88.0.1\"" +
+            "}" +
+            "]";
+
+    @Test
+    void testNodesMethodParsesAndReturnsEvenNodesThatMissDiskUsageInfo() throws Exception {
+        final ObjectMapper objectMapper = new ObjectMapperProvider().get();
+        final ElasticsearchClient client = mock(ElasticsearchClient.class);
+        final CatApi toTest = new CatApi(objectMapper, client);
+
+        when(client.execute(any(), anyString()))
+                .thenReturn(objectMapper.readValue(SAMPLE_CAT_NODES_RESPONSE, new TypeReference<List<NodeResponse>>() {}));
+
+        final List<NodeResponse> nodes = toTest.nodes();
+
+        assertThat(nodes)
+                .hasSize(2)
+                .contains(NodeResponse.create("nodeWithCorrectInfo",
+                        "nodeWithCorrectInfo",
+                        "dimr",
+                        null,
+                        "182.88.0.2",
+                        "45gb",
+                        "411.5gb",
+                        10.95d,
+                        1048576L))
+                .contains(NodeResponse.create("nodeWithMissingDiskStatistics",
+                        "nodeWithMissingDiskStatistics",
+                        "dimr",
+                        null,
+                        "182.88.0.1",
+                        null,
+                        null,
+                        null,
+                        null));
+    }
+}
