060b5438532d5744aa2565b8e9cbf01dea48f0ae
Linus Pahl <linus@graylog.com>
2022-07-01 11:13:06 +0200

    Removing no longer needed mocks.

--- graylog2-web-interface/src/views/components/searchbar/date-time-picker/TabKeywordTimeRange.test.tsx
+++ graylog2-web-interface/src/views/components/searchbar/date-time-picker/TabKeywordTimeRange.test.tsx
@@ -38,12 +38,6 @@ const TabKeywordTimeRange = ({ defaultValue, ...props }: { defaultValue: string 
   </Formik>
 );
 
-jest.mock('logic/datetimes/DateTime', () => ({
-  fromUTCDateTime: (date) => date,
-  getUserTimezone: () => 'Europe/Berlin',
-  fromDateTimeAndTZ: (date) => date,
-}));
-
 describe('TabKeywordTimeRange', () => {
   beforeEach(() => {
     asMock(ToolsStore.testNaturalDate).mockImplementation(() => Promise.resolve({
