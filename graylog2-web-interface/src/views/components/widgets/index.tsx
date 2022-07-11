9ec1851bb6291a6d9f71181d6f879fa887d2d54a
Linus Pahl <linus@graylog.com>
2022-07-11 10:01:48 +0200

    Removing no longer used `WidgetConfigModal`.

--- graylog2-web-interface/src/views/components/widgets/index.tsx
+++ graylog2-web-interface/src/views/components/widgets/index.tsx
@@ -16,5 +16,4 @@
  */
 export { default as Histogram } from './Histogram';
 export { default as MessageList } from './MessageList';
-export { default as WidgetConfigModal } from './WidgetConfigModal';
 export { default as WidgetHeader } from './WidgetHeader';
