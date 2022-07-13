658e43256bc530b0cf823d809a7f023abdf4b9d0
Linus Pahl <linus@graylog.com>
2022-07-13 10:35:21 +0200

    Simplify `IndicesPage`.

--- graylog2-web-interface/src/pages/IndicesPage.tsx
+++ graylog2-web-interface/src/pages/IndicesPage.tsx
@@ -26,45 +26,39 @@ import { DocumentationLink } from 'components/support';
 import { IndexSetsComponent } from 'components/indices';
 import { IndexerClusterHealth } from 'components/indexers';
 
-const IndicesPage = () => {
-  const pageHeader = (
-    <PageHeader title="Indices & Index Sets">
-      <span>
-        A Graylog stream write messages to an index set, which is a configuration for retention, sharding, and
-        replication of the stored data.
-        By configuring index sets, you could, for example, have different retention times for certain streams.
-      </span>
+const IndicesPage = () => (
+  <DocumentTitle title="Indices and Index Sets">
+    <span>
+      <PageHeader title="Indices & Index Sets">
+        <span>
+          A Graylog stream write messages to an index set, which is a configuration for retention, sharding, and
+          replication of the stored data.
+          By configuring index sets, you could, for example, have different retention times for certain streams.
+        </span>
 
-      <span>
-        You can learn more about the index model in the{' '}
-        <DocumentationLink page={DocsHelper.PAGES.INDEX_MODEL} text="documentation" />
-      </span>
+        <span>
+          You can learn more about the index model in the{' '}
+          <DocumentationLink page={DocsHelper.PAGES.INDEX_MODEL} text="documentation" />
+        </span>
 
-      <span>
-        <LinkContainer to={Routes.SYSTEM.INDEX_SETS.CREATE}>
-          <Button bsStyle="success" bsSize="lg">Create index set</Button>
-        </LinkContainer>
-      </span>
-    </PageHeader>
-  );
+        <span>
+          <LinkContainer to={Routes.SYSTEM.INDEX_SETS.CREATE}>
+            <Button bsStyle="success" bsSize="lg">Create index set</Button>
+          </LinkContainer>
+        </span>
+      </PageHeader>
 
-  return (
-    <DocumentTitle title="Indices and Index Sets">
-      <span>
-        {pageHeader}
+      <HideOnCloud>
+        <IndexerClusterHealth minimal />
+      </HideOnCloud>
 
-        <HideOnCloud>
-          <IndexerClusterHealth minimal />
-        </HideOnCloud>
-
-        <Row className="content">
-          <Col md={12}>
-            <IndexSetsComponent />
-          </Col>
-        </Row>
-      </span>
-    </DocumentTitle>
-  );
-};
+      <Row className="content">
+        <Col md={12}>
+          <IndexSetsComponent />
+        </Col>
+      </Row>
+    </span>
+  </DocumentTitle>
+);
 
 export default IndicesPage;
