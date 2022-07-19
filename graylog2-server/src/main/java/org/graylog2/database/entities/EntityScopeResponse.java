685b437d4793635d9dd61d0d3fb6ba9fccfbf37c
Roberto Benitez <roberto.benitez@graylog.com>
2022-07-19 13:27:57 -0500

    Added resource to provide a mapping of entity scope definitions.

--- graylog2-server/src/main/java/org/graylog2/database/entities/EntityScopeResponse.java
+++ graylog2-server/src/main/java/org/graylog2/database/entities/EntityScopeResponse.java
@@ -25,25 +25,21 @@ import java.util.Objects;
 @AutoValue
 public abstract class EntityScopeResponse {
 
-    private static final String FIELD_NAME = "name";
     private static final String FIELD_IS_MUTABLE = "is_mutable";
 
-    @JsonProperty(FIELD_NAME)
-    public abstract String name();
 
     @JsonProperty(FIELD_IS_MUTABLE)
     public abstract boolean mutable();
 
     @JsonCreator
-    public static EntityScopeResponse create(@JsonProperty(FIELD_NAME) String name,
-                                             @JsonProperty(FIELD_IS_MUTABLE) boolean mutable) {
-        return new AutoValue_EntityScopeResponse(name, mutable);
+    public static EntityScopeResponse create(@JsonProperty(FIELD_IS_MUTABLE) boolean mutable) {
+        return new AutoValue_EntityScopeResponse(mutable);
     }
 
     public static EntityScopeResponse of(EntityScope scope) {
 
         Objects.requireNonNull(scope, "Entity Scope must not be null");
 
-        return create(scope.getName(), scope.isMutable());
+        return create(scope.isMutable());
     }
 }
