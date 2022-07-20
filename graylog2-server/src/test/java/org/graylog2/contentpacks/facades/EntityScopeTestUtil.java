7aa75127d42394f473e185ebbdf1099f489488bc
ryan-carroll <ryan.carroll@graylog.com>
2022-07-20 07:03:29 -0700

    Adds missisng license header

--- graylog2-server/src/test/java/org/graylog2/contentpacks/facades/EntityScopeTestUtil.java
+++ graylog2-server/src/test/java/org/graylog2/contentpacks/facades/EntityScopeTestUtil.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 package org.graylog2.contentpacks.facades;
 
 import com.google.common.collect.ImmutableSet;
@@ -14,7 +30,7 @@ public class EntityScopeTestUtil {
         return new EntityScopeService(scopes);
     }
 
-    private static final class ImmutableEntityScope implements EntityScope {
+    private static final class ImmutableEntityScope extends EntityScope {
 
         @Override
         public String getName() {
