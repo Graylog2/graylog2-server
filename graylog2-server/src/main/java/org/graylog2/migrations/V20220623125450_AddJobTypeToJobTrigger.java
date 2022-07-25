6197761a33c805da3f8135ab8f00ef3afc8f1a7c
Marco Pfatschbacher <marco@graylog.com>
2022-07-25 16:02:52 +0200

    Job scheduler features for archiving (#12838)
    * Implement JobScheduler constraints
    
    * WIP: support progress and cancellation
    
    * merge
    
    * Add more JobScheduler Features for Archiving
    
     - Support Job cancellation
     - rename JobExecutionContext isRunning() to schedulerIsRunning() to
       avoid confusion
     - Add JobTriggerStatus to SystemJobSummary
    
     - Add api stubs for new system job responses
    
    * Fix forbidden invocation warning
    
    * fix test
    
    * add more stubs
    
    * Introduce JobResourceHandler / JobResourceHandlerService
    
    Each JobResourceHandler implementation is responsible on checking the permissions of the UserContext
    and perform the generic job actions accordingly.
    
    All JobResourceHandlers will be called by the JobResourceHandlerService,
    which can return either JobTriggerDTOs or for compat reasons with the
    existing SystemJobs implementation: SystemJobSummary objects.
    
    Duplicate Job type into the JobTriggerDTO.
    This makes it easier to access that info when converting JobTriggers into
    SystemJobSummary objects.
    
    Add last_owner field to the JobTriggerDTO, so we can display which node
    executed a job.
    
    * Add license headers
    
    * Add systemjobname to JobTriggerDetails
    
    * cleanup
    
    * add TODO
    
    * Add context support to LockService
    
    An existing lock with a context can only be renewed from the for the exact same node and context.
    This can be used to make a lock for a resource non-reentrant.
    
    Refactor LockService a bit so it is more convenient to work with.
    
    * add imports
    
    * Properly handle jobtrigger cancelled status
    
    * remove TODO
    
    * Add test for locking with context
    
    * Rename type to job_definition_type
    
    * early review comments
    
    * Add migration for jobDefinitionType
    
    Duplicate the type field from the JobDefinition config to their JobTriggers
    This way we don't have to make it @Nullable
    
    * add licenses
    
    * Fix tests
    
    * fix more tests
    
    * Fix test
    
    * fix more review comments
    
    * Fix more review comments
    
    * Fix LockService related review comments
    
    * chore: add new prop hideCancelButton in ConfirmDialog
    
    * feat: allow to acknowledge an completed system job
    
    * feat: change acknowledge button type for system jobs
    
    * chore: refactor SystemJob component
    
    * Add generic job acknowledge endpoint
    
    This endpoint only deletes (acknowledges) jobs from the new
    job scheduler.
    Legacy jobs are held in memory and remove themselves after completion.
    
    * feat: render error messages from acknowledge/cancel jobs
    
    * feat: add badges for archive jobs
    
    * feat: remove the finished button for completed archive jobs
    
    * feat: improve button wording for jobs on ArchiveCatalog
    
    * chore: use typescript for systemjobs
    
    * fix: satisfy reviewbot
    
    * Add missing index for job_definition_type
    
    * reduce calls to triggerWasCancelled
    
    * chore: add default for systemjob color state mapping
    
    * fix: consider cancelled as the job is able to get acknowledge
    
    * chore: rename variable in SystemJob component
    
    * Refactor job cancellation
    
    We set the cancelled status through the JobTriggerUpdate now.
    
    * Dont run Migration only 3 times
    
    But log a warning if it took longer than 1 minute
    
    * feat: map runnable to queued job_status and rename the acknowledge button on SystemJob
    
    * fix: consider jobs with status cancelled as done
    
    * feat: update text in SystemJob component
    
    * rename MigrationCompleted to MigrationStatus
    
    * Remove testing stubs and properly merge system and scheduler jobs
    
    * chore: introduce enum for job_status attribute
    
    Co-authored-by: Martin Wilhelmi <martin.wilhelmi@graylog.com>

---
+++ graylog2-server/src/main/java/org/graylog2/migrations/V20220623125450_AddJobTypeToJobTrigger.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.migrations;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Streams;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.UpdateOneModel;
+import com.mongodb.client.model.Updates;
+import org.bson.Document;
+import org.graylog.scheduler.DBJobDefinitionService;
+import org.graylog.scheduler.DBJobTriggerService;
+import org.graylog.scheduler.JobTriggerDto;
+import org.graylog2.database.MongoConnection;
+import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class V20220623125450_AddJobTypeToJobTrigger extends Migration {
+    private static final Logger LOG = LoggerFactory.getLogger(V20220623125450_AddJobTypeToJobTrigger.class);
+
+
+    private final MongoConnection mongoConnection;
+
+    private final ClusterConfigService clusterConfigService;
+
+    @Inject
+    public V20220623125450_AddJobTypeToJobTrigger(MongoConnection mongoConnection, ClusterConfigService clusterConfigService) {
+        this.mongoConnection = mongoConnection;
+        this.clusterConfigService = clusterConfigService;
+    }
+
+    @Override
+    public ZonedDateTime createdAt() {
+        return ZonedDateTime.parse("2022-06-23T12:54:50Z");
+    }
+
+    @Override
+    public void upgrade() {
+        final MigrationStatus migrationStatus = clusterConfigService.getOrDefault(MigrationStatus.class, MigrationStatus.createEmpty());
+
+        final Stopwatch started = Stopwatch.createStarted();
+
+        final MongoCollection<Document> jobTriggers = mongoConnection.getMongoDatabase().getCollection(DBJobTriggerService.COLLECTION_NAME);
+
+        // Duplicate the type field from the JobDefinition config to their JobTriggers
+        // Yes, this is what it takes to perform a join in MongoDB  ¯\_(ツ)_/¯
+        final AggregateIterable<Document> query = jobTriggers.aggregate(
+                ImmutableList.of(
+                        Aggregates.match(Filters.exists(JobTriggerDto.FIELD_JOB_DEFINITION_TYPE, false)),
+                        Aggregates.project(Projections.computed("job_definition_id", new Document("$toObjectId", "$job_definition_id"))),
+                        Aggregates.lookup(DBJobDefinitionService.COLLECTION_NAME, "job_definition_id", "_id", "job_definition"),
+                        Aggregates.project(Projections.computed("job_definition", new Document("$arrayElemAt", Arrays.asList("$job_definition", 0)))),
+                        Aggregates.project(Projections.computed("job_type", "$job_definition.config.type"))
+                )
+        );
+
+        List<UpdateOneModel<Document>> typeUpdate = Streams.stream(query).map(d -> new UpdateOneModel<Document>(
+                Filters.eq("_id", d.getObjectId("_id")),
+                Updates.set(JobTriggerDto.FIELD_JOB_DEFINITION_TYPE, d.getString("job_type")))).collect(Collectors.toList());
+
+        long modifiedCount = 0;
+        if (!typeUpdate.isEmpty()) {
+            modifiedCount = jobTriggers.bulkWrite(typeUpdate).getModifiedCount();
+
+            LOG.info("Added type field to <{}> JobTriggers", modifiedCount);
+        }
+        final long elapsed = started.stop().elapsed(TimeUnit.SECONDS);
+        if (elapsed > 60) {
+            LOG.warn("Migration ran longer than expected! Took <{}> seconds", elapsed);
+        }
+
+        clusterConfigService.write(MigrationStatus.
+                create(migrationStatus.convertedTriggersCount() + modifiedCount,
+                        migrationStatus.executionCount() + 1));
+    }
+
+    @AutoValue
+    public static abstract class MigrationStatus {
+        @JsonProperty("converted_triggers_count")
+        public abstract long convertedTriggersCount();
+
+        @JsonProperty("execution_count")
+        public abstract int executionCount();
+
+        @JsonCreator
+        public static MigrationStatus create(@JsonProperty("converted_triggers_count") long convertedTriggersCount, @JsonProperty("execution_count") int executionCount) {
+            return new AutoValue_V20220623125450_AddJobTypeToJobTrigger_MigrationStatus(convertedTriggersCount, executionCount);
+        }
+
+        public static MigrationStatus createEmpty() {
+            return create(0, 0);
+        }
+    }
+}
