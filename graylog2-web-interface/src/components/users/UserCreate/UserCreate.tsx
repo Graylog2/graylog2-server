887869e9e96d893e2e6535618bef6b11dc7efb79
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2022-07-26 15:35:06 +0200

    added debounced function for _validateUsername

--- graylog2-web-interface/src/components/users/UserCreate/UserCreate.tsx
+++ graylog2-web-interface/src/components/users/UserCreate/UserCreate.tsx
@@ -14,14 +14,15 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
+import history from 'util/History';
+import AppConfig from 'util/AppConfig';
+
 import * as React from 'react';
 import { useState } from 'react';
 import * as Immutable from 'immutable';
 import { Formik, Form } from 'formik';
 import { PluginStore } from 'graylog-web-plugin/plugin';
 
-import history from 'util/History';
-import AppConfig from 'util/AppConfig';
 import type { DescriptiveItem } from 'components/common/PaginatedItemOverview';
 import User from 'logic/users/User';
 import UsersDomain from 'domainActions/users/UsersDomain';
@@ -30,6 +31,7 @@ import RolesSelector from 'components/permissions/RolesSelector';
 import { Alert, Col, Row, Button, ButtonToolbar, Input } from 'components/bootstrap';
 import Routes from 'routing/Routes';
 import { UsersActions } from 'stores/users/UsersStore';
+import debounceWithPromise from 'views/logic/debounceWithPromise';
 
 import TimezoneFormGroup from './TimezoneFormGroup';
 import TimeoutFormGroup from './TimeoutFormGroup';
@@ -76,13 +78,15 @@ const _validateUsername = async (errors: { [name: string]: string }, username: s
   return newErrors;
 };
 
+const debouncedValidateUsername = debounceWithPromise(_validateUsername, 350);
+
 const _validate = async (values) => {
   let errors = {};
 
   const { password, password_repeat: passwordRepeat, username } = values;
 
   if (username) {
-    errors = await _validateUsername(errors, username);
+    errors = await debouncedValidateUsername(errors, username);
   }
 
   if (isCloud && oktaUserForm) {
