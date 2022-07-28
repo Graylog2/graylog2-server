4c3ac3a21534bdaee8ed69507e06d6f0f1070d35
Dan Torrey <dan@graylog.com>
2022-07-28 09:17:23 -0500

    Core System Entity/Immutable Entities Initial commit.
    # Conflicts:
    #	graylog2-server/src/main/java/org/graylog2/database/entities/DefaultEntityScope.java
    #	graylog2-server/src/main/java/org/graylog2/database/entities/EntityScope.java
    #	graylog2-server/src/main/java/org/graylog2/database/entities/EntityScopeResponse.java
    #	graylog2-server/src/main/java/org/graylog2/database/entities/EntityScopeService.java
    #	graylog2-server/src/main/java/org/graylog2/database/entities/ScopedEntitiesModule.java

---
+++ graylog2-server/src/test/java/org/graylog2/myentity/MyEntityDBService.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.myentity;
+
+import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
+import org.graylog2.database.MongoConnection;
+import org.graylog2.database.entities.EntityDbService;
+import org.graylog2.database.entities.EntityScopeService;
+
+import javax.inject.Inject;
+
+/**
+ * Specific MyEntity DB service implementation.
+ */
+public class MyEntityDBService extends EntityDbService<MyEntity> {
+
+    public static final String COLLECTION_NAME = "my_entities";
+
+    @Inject
+    public MyEntityDBService(MongoConnection mongoConnection,
+                             MongoJackObjectMapperProvider mapper,
+                             EntityScopeService entityScopeService) {
+        super(mongoConnection, mapper, MyEntity.class, COLLECTION_NAME, entityScopeService);
+    }
+}
