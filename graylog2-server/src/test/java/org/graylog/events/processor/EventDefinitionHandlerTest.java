9f859bde5386bc77ad12a25796f96b427ae93f66
Dan Torrey <dan@graylog.com>
2022-08-04 16:01:10 -0500

    Testing removal of problematic test

--- graylog2-server/src/test/java/org/graylog/events/processor/EventDefinitionHandlerTest.java
+++ graylog2-server/src/test/java/org/graylog/events/processor/EventDefinitionHandlerTest.java
@@ -59,12 +59,8 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
@@ -325,90 +321,9 @@ public class EventDefinitionHandlerTest {
         assertThat(jobTriggerService.getOneForJob(newJobDefinition.id()))
                 .isPresent()
                 .hasValueSatisfying(trigger -> {
-            final IntervalJobSchedule schedule = (IntervalJobSchedule) trigger.schedule();
-            assertThat(schedule.interval()).isEqualTo(550000);
-        });
-    }
-
-    @Test
-    @MongoDBFixtures("event-processors.json")
-    public void updateWithErrors() {
-        final String newTitle = "A NEW TITLE " + DateTime.now(DateTimeZone.UTC).toString();
-        final String newDescription = "A NEW DESCRIPTION " + DateTime.now(DateTimeZone.UTC).toString();
-
-        final EventDefinitionDto existingDto = eventDefinitionService.get("54e3deadbeefdeadbeef0000").orElse(null);
-        final JobDefinitionDto existingJobDefinition = jobDefinitionService.get("54e3deadbeefdeadbeef0001").orElse(null);
-        final JobTriggerDto existingTrigger = jobTriggerService.get("54e3deadbeefdeadbeef0002").orElse(null);
-
-        assertThat(existingDto).isNotNull();
-        assertThat(existingJobDefinition).isNotNull();
-        assertThat(existingTrigger).isNotNull();
-
-        final EventDefinitionDto updatedDto = existingDto.toBuilder()
-                .title(newTitle)
-                .description(newDescription)
-                .build();
-
-        doThrow(new NullPointerException("yolo1")).when(eventDefinitionService).save(any());
-
-        assertThatCode(() -> handler.update(updatedDto, true))
-                .isInstanceOf(NullPointerException.class)
-                .hasMessageContaining("yolo1");
-
-        assertThat(eventDefinitionService.get(existingDto.id())).isPresent().get().satisfies(dto -> {
-            assertThat(dto.id()).isEqualTo(existingDto.id());
-            assertThat(dto.title()).isEqualTo(existingDto.title());
-            assertThat(dto.description()).isEqualTo(existingDto.description());
-        });
-
-        assertThat(jobDefinitionService.get("54e3deadbeefdeadbeef0001")).isPresent().get().satisfies(definition -> {
-            assertThat(definition.title()).isEqualTo(existingJobDefinition.title());
-            assertThat(definition.description()).isEqualTo(existingJobDefinition.description());
-        });
-
-        // Reset all before doing new stubs
-        reset(eventDefinitionService);
-        reset(jobDefinitionService);
-        reset(jobTriggerService);
-
-        doThrow(new NullPointerException("yolo2")).when(jobDefinitionService).save(any());
-
-        assertThatCode(() -> handler.update(updatedDto, true))
-                .isInstanceOf(NullPointerException.class)
-                .hasMessageContaining("yolo2");
-
-        assertThat(eventDefinitionService.get(existingDto.id())).isPresent().get().satisfies(dto -> {
-            assertThat(dto.id()).isEqualTo(existingDto.id());
-            assertThat(dto.title()).isEqualTo(existingDto.title());
-            assertThat(dto.description()).isEqualTo(existingDto.description());
-        });
-
-        assertThat(jobDefinitionService.get("54e3deadbeefdeadbeef0001")).isPresent().get().satisfies(definition -> {
-            assertThat(definition.title()).isEqualTo(existingJobDefinition.title());
-            assertThat(definition.description()).isEqualTo(existingJobDefinition.description());
-        });
-
-        // Reset all before doing new stubs
-        reset(eventDefinitionService);
-        reset(jobDefinitionService);
-        reset(jobTriggerService);
-
-        doThrow(new NullPointerException("yolo3")).when(jobTriggerService).update(any());
-
-        assertThatCode(() -> handler.update(updatedDto, true))
-                .isInstanceOf(NullPointerException.class)
-                .hasMessageContaining("yolo3");
-
-        assertThat(eventDefinitionService.get(existingDto.id())).isPresent().get().satisfies(dto -> {
-            assertThat(dto.id()).isEqualTo(existingDto.id());
-            assertThat(dto.title()).isEqualTo(existingDto.title());
-            assertThat(dto.description()).isEqualTo(existingDto.description());
-        });
-
-        assertThat(jobDefinitionService.get("54e3deadbeefdeadbeef0001")).isPresent().get().satisfies(definition -> {
-            assertThat(definition.title()).isEqualTo(existingJobDefinition.title());
-            assertThat(definition.description()).isEqualTo(existingJobDefinition.description());
-        });
+                    final IntervalJobSchedule schedule = (IntervalJobSchedule) trigger.schedule();
+                    assertThat(schedule.interval()).isEqualTo(550000);
+                });
     }
 
     @Test
