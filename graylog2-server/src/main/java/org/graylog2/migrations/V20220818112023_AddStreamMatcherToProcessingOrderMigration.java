c23c71067437a5ddc0f1dfc0c6b4fd376eed8a09
Marco Pfatschbacher <marco@graylog.com>
2022-08-24 15:06:40 +0200

    Extract StreamMatcherFilter into separate MessageProcessor (#13256)
    Matching (routing) Messages into streams used to happen within the
    MessageFilterChainProcessor and was run as the last filter.
    
    Making Stream Matching a separate Message Processor gives the users
    more flexibility. They can for instance run pipeline and extractors first,
    and perform stream assignment last.
    
    Fixes #6464
    
    Co-authored-by: Othello Maurer <othello@graylog.com>

--- graylog2-server/src/main/java/org/graylog2/migrations/V20220818112023_AddStreamMatcherToProcessingOrderMigration.java
+++ graylog2-server/src/main/java/org/graylog2/migrations/V20220818112023_AddStreamMatcherToProcessingOrderMigration.java
@@ -19,34 +19,26 @@ package org.graylog2.migrations;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.google.auto.value.AutoValue;
-import org.graylog.plugins.pipelineprocessor.processors.PipelineInterpreter;
 import org.graylog2.messageprocessors.MessageFilterChainProcessor;
 import org.graylog2.messageprocessors.MessageProcessorsConfig;
+import org.graylog2.messageprocessors.StreamMatcherFilterProcessor;
 import org.graylog2.plugin.cluster.ClusterConfigService;
 import org.graylog2.plugin.messageprocessors.MessageProcessor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import java.time.ZonedDateTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class V20220719130704_ImprovedDefaultProcessingOrderMigration extends Migration {
-    private static final Logger LOG = LoggerFactory.getLogger(V20220719130704_ImprovedDefaultProcessingOrderMigration.class);
+public class V20220818112023_AddStreamMatcherToProcessingOrderMigration extends Migration {
 
-    private final boolean isFreshInstallation;
     private final ClusterConfigService clusterConfigService;
     private final Set<String> processorClassNames;
 
     @Inject
-    public V20220719130704_ImprovedDefaultProcessingOrderMigration(@Named("isFreshInstallation") boolean isFreshInstallation,
-                                                                   ClusterConfigService clusterConfigService,
-                                                                   Set<MessageProcessor.Descriptor> processorDescriptors) {
-        this.isFreshInstallation = isFreshInstallation;
+    public V20220818112023_AddStreamMatcherToProcessingOrderMigration(ClusterConfigService clusterConfigService,
+                                                                      Set<MessageProcessor.Descriptor> processorDescriptors) {
         this.clusterConfigService = clusterConfigService;
         this.processorClassNames = processorDescriptors.stream()
                 .map(MessageProcessor.Descriptor::className)
@@ -55,30 +47,30 @@ public class V20220719130704_ImprovedDefaultProcessingOrderMigration extends Mig
 
     @Override
     public ZonedDateTime createdAt() {
-        return ZonedDateTime.parse("2022-07-19T13:07:04Z");
+        return ZonedDateTime.parse("2022-08-18T11:20:23Z");
     }
 
     @Override
     public void upgrade() {
-        // This migration will only be executed on new Graylog installations
-        if (!isFreshInstallation) {
-            return;
-        }
-        // This is a bit redundant, but better safe than sorry
         if (clusterConfigService.get(MigrationCompleted.class) != null) {
             return;
         }
-        LOG.info("Fresh Graylog installation detected. Applying new default Message Processor order.");
 
         final MessageProcessorsConfig config = clusterConfigService.getOrDefault(MessageProcessorsConfig.class,
                 MessageProcessorsConfig.defaultConfig());
-        // The former default order was simply based on sorting by class names
+
         final List<String> order = config.withProcessors(processorClassNames).processorOrder();
 
-        // Keep the former order, only swap the Message Filter with the PipelineProcessor
+        // Put the StreamMatcher directly after the MessageFilter. This ensures backwards compatible processing behavior
+        final boolean remove = order.remove(StreamMatcherFilterProcessor.class.getCanonicalName());
+        if (!remove) {
+            throw new IllegalStateException("StreamMatcherFilterProcessor not in processor list");
+        }
         final int filterChainIndex = order.indexOf(MessageFilterChainProcessor.class.getCanonicalName());
-        final int pipelineIndex = order.indexOf(PipelineInterpreter.class.getCanonicalName());
-        Collections.swap(order, filterChainIndex, pipelineIndex);
+        if (filterChainIndex == -1) {
+            throw new IllegalStateException("MessageFilterChainProcessor not in processor list");
+        }
+        order.add(filterChainIndex + 1, StreamMatcherFilterProcessor.class.getCanonicalName());
 
         final MessageProcessorsConfig newProcessorsConfig = MessageProcessorsConfig.defaultConfig().toBuilder()
                 .processorOrder(order)
@@ -93,7 +85,7 @@ public class V20220719130704_ImprovedDefaultProcessingOrderMigration extends Mig
     public static abstract class MigrationCompleted {
         @JsonCreator
         public static MigrationCompleted create() {
-            return new AutoValue_V20220719130704_ImprovedDefaultProcessingOrderMigration_MigrationCompleted();
+            return new AutoValue_V20220818112023_AddStreamMatcherToProcessingOrderMigration_MigrationCompleted();
         }
     }
 }
