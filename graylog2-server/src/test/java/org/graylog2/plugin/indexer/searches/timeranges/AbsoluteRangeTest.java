e22d4c103dc9c3e021943407de424438e269451e
Tomas Dvorak <tomas.dvorak@graylog.com>
2022-08-30 12:28:04 +0200

    Fixed test

--- graylog2-server/src/test/java/org/graylog2/plugin/indexer/searches/timeranges/AbsoluteRangeTest.java
+++ graylog2-server/src/test/java/org/graylog2/plugin/indexer/searches/timeranges/AbsoluteRangeTest.java
@@ -18,15 +18,17 @@ package org.graylog2.plugin.indexer.searches.timeranges;
 
 import org.assertj.core.api.Assertions;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.jupiter.api.Test;
 
 class AbsoluteRangeTest {
 
     @Test
     void testBuilderWithoutExplicitType() {
+        final DateTime now = DateTime.now(DateTimeZone.UTC);
         final AbsoluteRange range = AbsoluteRange.builder()
-                .from(DateTime.now().minus(1000 * 60 * 50))
-                .to(DateTime.now())
+                .from(now.minus(1000 * 60 * 50))
+                .to(now)
                 .build();
         Assertions.assertThat(range.type()).isEqualTo(AbsoluteRange.ABSOLUTE);
     }
