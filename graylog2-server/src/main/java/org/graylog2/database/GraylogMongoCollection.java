367a68ad733234a39055610a97cb0b74923a3b63
Othello Maurer <othello@graylog.com>
2022-08-30 16:54:01 +0200

    Revert "Add factory to create collections using mongojack 4"
    This reverts commit 60283891ca4d258f9baf7f184e20b58ea86b2ff6.

--- graylog2-server/src/main/java/org/graylog2/database/GraylogMongoCollection.java
+++
@@ -1,7 +0,0 @@
-package org.graylog2.database;
-
-import com.mongodb.client.MongoCollection;
-
-public interface GraylogMongoCollection<TDocument> extends MongoCollection<TDocument> {
-    TDocument save(TDocument dto);
-}
