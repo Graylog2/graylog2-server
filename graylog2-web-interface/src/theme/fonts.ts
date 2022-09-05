e5d786617dbd588c464ceb25ff6e8fc7ccad5cdd
Dennis Oelkers <dennis@graylog.com>
2022-09-05 09:48:50 +0200

    Adjusting fonts & colors.

--- graylog2-web-interface/src/theme/fonts.ts
+++ graylog2-web-interface/src/theme/fonts.ts
@@ -22,6 +22,8 @@ import 'opensans-npm-webfont/open_sans.css';
 import 'opensans-npm-webfont/open_sans_italic.css';
 import 'opensans-npm-webfont/open_sans_bold.css';
 import '@openfonts/roboto-mono_latin/index.css';
+import '@openfonts/source-sans-pro_latin/index.css';
+import '@openfonts/barlow-condensed_latin/index.css';
 
 export type Fonts = {
   family: {
@@ -66,8 +68,9 @@ export const fontsPropTypes = PropTypes.shape({
 });
 
 const family = {
-  body: '"Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  body: '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
   monospace: '"Roboto Mono", Menlo, Monaco, Consolas, "Courier New", monospace',
+  navigation: '"Barlow Condensed"',
 };
 
 /* Scaled 1.125 Major Second - https://type-scale.com/ */
