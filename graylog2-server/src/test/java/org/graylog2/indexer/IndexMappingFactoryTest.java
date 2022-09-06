9c2068c7a915a7040e2c24f0e53eff5c178cb48d
Tomas Dvorak <tomas.dvorak@graylog.com>
2022-09-06 10:24:44 +0200

    Remove Elasticsearch 6.x support (#13321)
    * Remove ES6 support
    
    * Added upgrading note about removed ES6 support
    
    * removed forgotten ES6 template providers
    
    * removed unused classes
    
    * code cleanup in tests for index mapping

--- graylog2-server/src/test/java/org/graylog2/indexer/IndexMappingFactoryTest.java
+++ graylog2-server/src/test/java/org/graylog2/indexer/IndexMappingFactoryTest.java
@@ -16,12 +16,10 @@
  */
 package org.graylog2.indexer;
 
-import com.github.zafarkhaja.semver.Version;
 import com.google.common.collect.ImmutableMap;
 import org.graylog2.indexer.cluster.Node;
 import org.graylog2.indexer.indexset.IndexSetConfig;
 import org.graylog2.storage.SearchVersion;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -37,21 +35,10 @@ import static org.mockito.Mockito.when;
 
 public class IndexMappingFactoryTest {
 
-    private Node node;
-
-    private IndexSetConfig indexSetConfig;
-
-    private IndexMappingFactory sut;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        this.node = mock(Node.class);
-        this.indexSetConfig = mock(IndexSetConfig.class);
-        this.sut = new IndexMappingFactory(node, ImmutableMap.of(
-                MESSAGE_TEMPLATE_TYPE, new MessageIndexTemplateProvider(),
-                EVENT_TEMPLATE_TYPE, new EventIndexTemplateProvider()
-        ));
-    }
+    public static final ImmutableMap<String, IndexTemplateProvider> TEMPLATE_PROVIDERS = ImmutableMap.of(
+            MESSAGE_TEMPLATE_TYPE, new MessageIndexTemplateProvider(),
+            EVENT_TEMPLATE_TYPE, new EventIndexTemplateProvider()
+    );
 
     @ParameterizedTest
     @ValueSource(strings = {
@@ -59,76 +46,56 @@ public class IndexMappingFactoryTest {
             "2.0.0",
             "3.0.0",
             "4.0.0",
+            "5.0.0",
+            "6.0.0",
             "8.0.0",
-            "9.0.0"
+            "9.0.0",
+            "OpenSearch:2.0.3"
     })
-    void messageMappingFailsForUnsupportedElasticsearchVersion(String version) {
-        testForUnsupportedVersion(version, MESSAGE_TEMPLATE_TYPE);
+    void mappingFailsForUnsupportedElasticsearchVersion(final String version) {
+        testForUnsupportedVersion(version);
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "1.7.3",
-            "2.0.0",
-            "3.0.0",
-            "4.0.0",
-            "8.0.0",
-            "9.0.0"
-    })
-    void eventsMappingFailsForUnsupportedElasticsearchVersion(String version) {
-        testForUnsupportedVersion(version, EVENT_TEMPLATE_TYPE);
-    }
+    private void testForUnsupportedVersion(final String version) {
+        final IndexMappingFactory indexMappingFactory = new IndexMappingFactory(createNodeWithVersion(version), TEMPLATE_PROVIDERS);
 
-    private void testForUnsupportedVersion(String version, String templateType) {
-        mockNodeVersion(version);
-
-        assertThatThrownBy(() -> sut.createIndexMapping(indexSetConfig))
+        assertThatThrownBy(() -> indexMappingFactory.createIndexMapping(mock(IndexSetConfig.class)))
                 .isInstanceOf(ElasticsearchException.class)
-                .hasMessageContaining("Unsupported Search version: Elasticsearch:" + version)
+                .hasMessageStartingWith("Unsupported Search version")
+                .hasMessageEndingWith(version)
                 .hasNoCause();
     }
 
     @ParameterizedTest
     @CsvSource({
-            "5.0.0, IndexMapping5",
-            "5.1.0, IndexMapping5",
-            "5.2.0, IndexMapping5",
-            "5.3.0, IndexMapping5",
-            "5.4.0, IndexMapping5",
-            "6.3.1, IndexMapping6",
-            "6.8.1, IndexMapping6",
+            "OpenSearch:1.2.3, IndexMapping7",
             "7.8.0, IndexMapping7"
     })
-    void createsMessageIndexMappings(String version, String expectedMappingClass) throws ClassNotFoundException {
+    void createsMessageIndexMappings(final String version, final String expectedMappingClass) throws ClassNotFoundException {
         testForIndexMappingType(version, expectedMappingClass, MESSAGE_TEMPLATE_TYPE);
     }
 
     @ParameterizedTest
     @CsvSource({
-            "5.0.0, EventsIndexMapping6",
-            "5.1.0, EventsIndexMapping6",
-            "5.2.0, EventsIndexMapping6",
-            "5.3.0, EventsIndexMapping6",
-            "5.4.0, EventsIndexMapping6",
-            "6.3.1, EventsIndexMapping6",
-            "6.8.1, EventsIndexMapping6",
+            "OpenSearch:1.2.3, EventsIndexMapping7",
             "7.8.0, EventsIndexMapping7"
     })
-    void createsEventIndexMappings(String version, String expectedMappingClass) throws ClassNotFoundException {
+    void createsEventIndexMappings(final String version, final String expectedMappingClass) throws ClassNotFoundException {
         testForIndexMappingType(version, expectedMappingClass, EVENT_TEMPLATE_TYPE);
     }
 
-    private void testForIndexMappingType(String version, String mappingClassName, String templateType) throws ClassNotFoundException {
-        mockNodeVersion(version);
+    private void testForIndexMappingType(final String version, final String mappingClassName, final String templateType) throws ClassNotFoundException {
+        final IndexMappingFactory indexMappingFactory = new IndexMappingFactory(createNodeWithVersion(version), TEMPLATE_PROVIDERS);
 
+        final IndexSetConfig indexSetConfig = mock(IndexSetConfig.class);
         when(indexSetConfig.indexTemplateType()).thenReturn(Optional.of(templateType));
 
         final Class<?> expectedMappingClass = Class.forName("org.graylog2.indexer." + mappingClassName);
 
-        assertThat(sut.createIndexMapping(indexSetConfig)).isInstanceOf(expectedMappingClass);
+        assertThat(indexMappingFactory.createIndexMapping(indexSetConfig)).isInstanceOf(expectedMappingClass);
     }
 
-    private void mockNodeVersion(String version) {
-        when(node.getVersion()).thenReturn(Optional.of(SearchVersion.elasticsearch(version)));
+    private Node createNodeWithVersion(final String version) {
+        return new Node(() -> Optional.of(SearchVersion.decode(version)));
     }
 }
