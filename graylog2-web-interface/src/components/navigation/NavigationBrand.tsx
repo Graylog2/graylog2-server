2cdb0fa5e3334b8996b3db3a984be159b60cd73d
Linus Pahl <linus@graylog.com>
2022-09-13 16:17:27 +0200

    Change var name.

--- graylog2-web-interface/src/components/navigation/NavigationBrand.tsx
+++ graylog2-web-interface/src/components/navigation/NavigationBrand.tsx
@@ -39,8 +39,7 @@ type Props = {
 }
 
 // Don't pass active prop, since `a` tag doesn't support it.
-// eslint-disable-next-line no-unused-vars
-const BrandComponent = ({ active, ...props }: Props) => {
+const BrandComponent = ({ active: _active, ...props }: Props) => {
   const theme = useTheme();
 
   return (
