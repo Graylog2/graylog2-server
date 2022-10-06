afb8b9ecade1b65390dc347cc6e968e2dbd6a04c
Dennis Oelkers <dennis@graylog.com>
2022-10-06 13:25:59 +0200

    Using correct sources for root/non-leaf rollup metrics.

--- graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/searchtypes/pivot/ESPivot.java
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/searchtypes/pivot/ESPivot.java
@@ -78,11 +78,17 @@ public class ESPivot implements ESSearchTypeHandler<Pivot> {
         final AggTypes aggTypes = new AggTypes();
         contextMap.put(pivot.id(), aggTypes);
 
+        // add global rollup series if those were requested
+        if (pivot.rollup()) {
+            seriesStream(pivot, queryContext, "global rollup")
+                    .forEach(searchSourceBuilder::aggregation);
+        }
+
         final BucketSpecHandler.CreatedAggregations<AggregationBuilder> createdAggregations = createPivots(query, pivot, pivot.rowGroups(), queryContext);
         final AggregationBuilder rootAggregation = createdAggregations.root();
         final AggregationBuilder leafAggregation = createdAggregations.leaf();
         final List<AggregationBuilder> metrics = createdAggregations.metrics();
-        if (pivot.columnGroups().isEmpty() || pivot.rollup()) {
+        if (!pivot.rowGroups().isEmpty() && (pivot.columnGroups().isEmpty() || pivot.rollup())) {
             seriesStream(pivot, queryContext, "metrics")
                     .forEach(aggregation -> metrics.forEach(metric -> metric.subAggregation(aggregation)));
         }
@@ -215,6 +221,12 @@ public class ESPivot implements ESSearchTypeHandler<Pivot> {
                     }
                     resultBuilder.addRow(rowBuilder.build());
                 });
+
+        if (!pivot.rowGroups().isEmpty() && pivot.rollup()) {
+            final PivotResult.Row.Builder rowBuilder = PivotResult.Row.builder().key(ImmutableList.of());
+            processSeries(rowBuilder, queryResult, queryContext, pivot, new ArrayDeque<>(), initialBucket, true, "row-inner");
+            resultBuilder.addRow(rowBuilder.source("non-leaf").build());
+        }
 
         return resultBuilder.build();
     }
