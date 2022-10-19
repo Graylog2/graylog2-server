700103f460e75e04d2daf6301a70d0b25e4602fa
Maxwell Anipah <maxwell.anipah@graylog.com>
2022-10-19 14:16:41 +0200

    move archive configs to plugin configuration

--- graylog2-server/src/main/java/org/graylog2/Configuration.java
+++ graylog2-server/src/main/java/org/graylog2/Configuration.java
@@ -216,13 +216,6 @@ public class Configuration extends BaseConfiguration {
     @Parameter(value = "lock_service_lock_ttl", converter = JavaDurationConverter.class)
     private java.time.Duration lockServiceLockTTL = MongoLockService.MIN_LOCK_TTL;
 
-
-    @Parameter(value = "default_archive_retention_time", validators = {PositiveIntegerValidator.class})
-    private int defaultArchiveRetentionTime = 0;
-
-    @Parameter(value = "max_archive_retention_time", validators = {PositiveIntegerValidator.class})
-    private int maxArchiveRetentionTime = 0;
-
     public boolean maintainsStreamAwareFieldTypes() {
         return streamAwareFieldTypes;
     }
@@ -457,14 +450,6 @@ public class Configuration extends BaseConfiguration {
     @Deprecated
     public Set<String> getEnabledTlsProtocols() {
         return enabledTlsProtocols;
-    }
-
-    public int getDefaultArchiveRetentionTime() {
-        return defaultArchiveRetentionTime;
-    }
-
-    public int getMaxArchiveRetentionTime() {
-        return maxArchiveRetentionTime;
     }
 
     @ValidatorMethod
