ac39a9f304edbba05fc89321e1637309a02ff0fb
Dan Torrey <dan@graylog.com>
2022-10-24 16:11:53 -0500

    Fix more lint issues

--- graylog2-web-interface/src/components/indices/rotation/SizeBasedRotationStrategyConfiguration.jsx
+++ graylog2-web-interface/src/components/indices/rotation/SizeBasedRotationStrategyConfiguration.jsx
@@ -43,16 +43,16 @@ const SizeBasedRotationStrategyConfiguration = ({ config, updateConfig }) => {
 
   return (
     <div>
-    <Input type="number"
-           id="max-size"
-           label="Max size per index (in bytes)"
-           onChange={_onInputUpdate('max_size')}
-           labelClassName="col-sm-3"
-           wrapperClassName="col-sm-9"
-           value={maxSize}
-           help="Maximum size of an index before it gets rotated"
-           addonAfter={_formatSize()}
-           required />
+      <Input type="number"
+             id="max-size"
+             label="Max size per index (in bytes)"
+             onChange={_onInputUpdate('max_size')}
+             labelClassName="col-sm-3"
+             wrapperClassName="col-sm-9"
+             value={maxSize}
+             help="Maximum size of an index before it gets rotated"
+             addonAfter={_formatSize()}
+             required />
     </div>
   );
 };
