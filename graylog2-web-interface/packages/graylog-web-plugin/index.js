f8b725aae667e349c05ef20d452663e95dcdb34c
Dennis Oelkers <dennis@graylog.com>
2022-11-01 12:45:09 +0100

    Fixing up linter hints.

--- graylog2-web-interface/packages/graylog-web-plugin/index.js
+++ graylog2-web-interface/packages/graylog-web-plugin/index.js
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-
+/* eslint-disable global-require */
 module.exports = {
   PluginWebpackConfig: require('./src/PluginWebpackConfig'),
   loadBuildConfig: require('./src/loadBuildConfig'),
