c12f021d59500a519ab8ad0784e3025c274ed6ef
Linus Pahl <linus@graylog.com>
2022-11-10 18:15:12 +0100

    Create separate component for stream status cell.

--- graylog2-web-interface/src/components/bootstrap/Label.jsx
+++ graylog2-web-interface/src/components/bootstrap/Label.jsx
@@ -19,9 +19,9 @@ import styled, { css } from 'styled-components';
 // eslint-disable-next-line no-restricted-imports
 import { Label as BootstrapLabel } from 'react-bootstrap';
 
-const StyledLabel = styled(BootstrapLabel)(({ bsStyle, theme }) => {
+const getColorStyles = (theme, bsStyle) => {
   if (!bsStyle) {
-    return undefined;
+    return '';
   }
 
   const backgroundColor = theme.colors.variant[bsStyle];
@@ -31,12 +31,15 @@ const StyledLabel = styled(BootstrapLabel)(({ bsStyle, theme }) => {
     background-color: ${backgroundColor};
     color: ${textColor};
   `;
-});
+};
 
-const Label = forwardRef(({ ...props }, ref) => {
-  return (
-    <StyledLabel ref={ref} {...props} />
-  );
-});
+const StyledLabel = styled(BootstrapLabel)(({ bsStyle, theme }) => css`
+  ${getColorStyles(theme, bsStyle)}
+  padding: 0.2em 0.6em;
+`);
+
+const Label = forwardRef(({ ...props }, ref) => (
+  <StyledLabel ref={ref} {...props} />
+));
 
 export default Label;
