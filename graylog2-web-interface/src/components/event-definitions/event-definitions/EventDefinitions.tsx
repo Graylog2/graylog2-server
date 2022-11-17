997d374fed134369256cc0c6e03bfc9fa24f5f8b
Martin Wilhelmi <martin.wilhelmi@graylog.com>
2022-11-17 11:48:13 +0100

    Fix warnings in EventDefinitions.tsx

--- graylog2-web-interface/src/components/event-definitions/event-definitions/EventDefinitions.tsx
+++ graylog2-web-interface/src/components/event-definitions/event-definitions/EventDefinitions.tsx
@@ -30,8 +30,9 @@ import Routes from 'routing/Routes';
 import QueryHelper from 'components/common/QueryHelper';
 
 import styles from './EventDefinitions.css';
-import type { EventDefinition } from '../event-definitions-types';
 import EventDefinitionEntry from './EventDefinitionEntry';
+
+import type { EventDefinition } from '../event-definitions-types';
 
 const EmptyContent = () => (
   <Row>
