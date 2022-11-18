d0d9ad18342b55824394a60a555b7b38f69402b9
patrick <patrick.mann@graylog.com>
2022-11-18 17:08:22 +0100

    refactor API response

--- graylog2-server/src/main/java/org/graylog/events/processor/systemnotification/SystemNotificationRenderService.java
+++ graylog2-server/src/main/java/org/graylog/events/processor/systemnotification/SystemNotificationRenderService.java
@@ -5,56 +5,78 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 import net.htmlparser.jericho.Source;
 import org.graylog2.notifications.Notification;
+import org.graylog2.notifications.NotificationService;
 
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.util.Map;
 
 import static org.apache.commons.lang.CharEncoding.UTF_8;
 
 public class SystemNotificationRenderService {
-    public enum RenderFormat {
-        FORMAT_HTML,
-        FORMAT_PLAINTEXT
-    }
-
+    static final String KEY_TITLE = "_title";
+    static final String KEY_DESCRIPTION = "_description";
+    static final String KEY_CLOUD = "_cloud";
+    private NotificationService notificationService;
+    private org.graylog2.Configuration graylogConfig;
     private static final freemarker.template.Configuration cfg =
             new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_28);
 
     @Inject
-    public SystemNotificationRenderService() {
+    public SystemNotificationRenderService(NotificationService notificationService,
+                                           org.graylog2.Configuration graylogConfig) {
+        this.notificationService = notificationService;
+        this.graylogConfig = graylogConfig;
+
         cfg.setDefaultEncoding(UTF_8);
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
         cfg.setLogTemplateExceptions(false);
         cfg.setClassForTemplateLoading(SystemNotificationRenderService.class, "/org/graylog2/freemarker/templates/");
     }
 
-    public String renderHtml(Notification notification) {
-        return renderHtml(notification.getType().toString().toLowerCase(), notification.getDetails());
+    public TemplateRenderResponse renderHtml(Notification notification) {
+        return renderHtml(notification.getType(), notification.getDetails());
     }
 
-    public String renderHtml(String notificationId, Map<String, Object> values) {
-        try (Writer writer = new StringWriter()) {
-            Template template = cfg.getTemplate(notificationId + ".ftl");
+    public TemplateRenderResponse renderHtml(Notification.Type type, Map<String, Object> values) {
+        Notification notification = notificationService.getByType(type)
+                .orElseThrow(() -> new IllegalArgumentException("Event type is not currently active"));
+        values.putAll(notification.getDetails());
+        values.put(KEY_CLOUD, graylogConfig.isCloud());
+
+        try (StringWriter writer = new StringWriter()) {
+            Template template = cfg.getTemplate(type.toString().toLowerCase() + ".ftl");
+
+            values.put(KEY_TITLE, true);
+            values.put(KEY_DESCRIPTION, false);
             template.process(values, writer);
-            return writer.toString();
+            String title = writer.toString();
+
+            writer.getBuffer().setLength(0);
+            values.put(KEY_TITLE, false);
+            values.put(KEY_DESCRIPTION, true);
+            template.process(values, writer);
+            String description = writer.toString();
+
+            return TemplateRenderResponse.create(title, description);
         } catch (TemplateException e) {
-            throw new BadRequestException("Unable to render template " + notificationId + ": " + e.getMessage());
+            throw new BadRequestException("Unable to render template " + type.toString() + ": " + e.getMessage());
         } catch (IOException e) {
-            throw new BadRequestException("Unable to locate template " + notificationId + ": " + e.getMessage());
+            throw new BadRequestException("Unable to locate template " + type.toString() + ": " + e.getMessage());
         }
     }
 
     public String renderPlainText(Notification notification) {
-        return renderPlainText(notification.getType().toString().toLowerCase(), notification.getDetails());
+        return renderPlainText(notification.getType(), notification.getDetails());
     }
 
-    public String renderPlainText(String notificationId, Map<String, Object> values) {
-        String html = renderHtml(notificationId, values);
-        String plaintext = new Source(html).getRenderer().toString();
-        return plaintext;
+    public String renderPlainText(Notification.Type type, Map<String, Object> values) {
+        TemplateRenderResponse templateRenderResponse = renderHtml(type, values);
+        String plainTitle = new Source(templateRenderResponse.title()).getRenderer().toString();
+        String plainDescription = new Source(templateRenderResponse.description()).getRenderer().toString();
+
+        return plainTitle + "\n" + plainDescription;
     }
 }
--- graylog2-server/src/test/java/org/graylog/events/notifications/SystemNotificationRenderServiceTest.java
+++ graylog2-server/src/test/java/org/graylog/events/notifications/SystemNotificationRenderServiceTest.java
@@ -1,22 +1,35 @@
 package org.graylog.events.notifications;
 
 import org.graylog.events.processor.systemnotification.SystemNotificationRenderService;
+import org.graylog.events.processor.systemnotification.TemplateRenderResponse;
 import org.graylog2.notifications.Notification;
 import org.graylog2.notifications.NotificationImpl;
+import org.graylog2.notifications.NotificationService;
 import org.joda.time.DateTime;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SystemNotificationRenderServiceTest {
-    static Notification notification;
+    static NotificationService notificationService = mock(NotificationService.class);
+    static org.graylog2.Configuration graylogConfig = mock(org.graylog2.Configuration.class);
     static SystemNotificationRenderService renderService;
+    Notification notification;
 
     @BeforeAll
     static void setup() {
-        renderService = new SystemNotificationRenderService();
+        renderService = new SystemNotificationRenderService(notificationService, graylogConfig);
+    }
+
+    @Test
+    void htmlRenderTestWithReplace() {
         notification = new NotificationImpl()
                 .addNode("node")
                 .addSeverity(Notification.Severity.URGENT)
@@ -26,27 +39,19 @@ class SystemNotificationRenderServiceTest {
                 .addDetail("dummy", "dummy")
                 .addDetail("exception", new Exception("My Test Exception"))
                 .addTimestamp(DateTime.now());
-    }
+        when(notificationService.getByType(any())).thenReturn(Optional.of(notification));
 
-    @Test
-    void htmlRenderTestWithReplace() {
-        String msg = renderService.renderHtml(notification);
-        assertThat(msg).containsSequence("java.lang.Exception: My Test Exception");
-        assertThat(msg).containsSequence("<span>");
-    }
-
-    @Test
-    void plainTextRenderTestWithReplace() {
-        String msg = renderService.renderPlainText(notification);
-        assertThat(msg).containsSequence("java.lang.Exception: My Test Exception");
-        assertThat(msg).doesNotContain("<span>");
+        TemplateRenderResponse renderResponse = renderService.renderHtml(notification);
+        assertThat(renderResponse.title()).isEqualToIgnoringWhitespace("Email Transport Configuration is missing or invalid!");
+        assertThat(renderResponse.description()).containsSequence("java.lang.Exception: My Test Exception");
+        assertThat(renderResponse.description()).containsSequence("<span>");
     }
 
     @Disabled
     @Test
     void missingTemplates() {
         for (Notification.Type type : Notification.Type.values()) {
-            if (SystemNotificationRenderService.class.getResource(type.toString()) == null) {
+            if (SystemNotificationRenderService.class.getResource(type.toString().toLowerCase()) == null) {
                 System.out.println("Missing FTL: " + type);
             }
         }
