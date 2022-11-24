2c6ccbeebbbce322553dac1cd15e0b103ce93a2a
Linus Pahl <linus@graylog.com>
2022-11-24 10:16:58 +0100

    Update usage of `PluginWebpackConfig` in webpack config of `graylog-plugin-archetype`. (#14043)

--- graylog-plugin-archetype/src/main/resources/archetype-resources/webpack.config.js
+++ graylog-plugin-archetype/src/main/resources/archetype-resources/webpack.config.js
@@ -1,8 +1,8 @@
-const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig;
-const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
+const { PluginWebpackConfig } = require('graylog-web-plugin');
+const { loadBuildConfig } = require('graylog-web-plugin');
 
 // Remember to use the same name here and in `getUniqueId()` in the java MetaData class
-module.exports = new PluginWebpackConfig('${package}.${pluginClassName}Plugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
+module.exports = new PluginWebpackConfig(__dirname, '${package}.${pluginClassName}Plugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
   // Here goes your additional webpack configuration.
 });
