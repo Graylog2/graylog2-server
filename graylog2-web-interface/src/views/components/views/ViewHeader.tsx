ef1736f925f8ec4b3d20fbf7ecf8858152926bd6
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2022-12-12 13:42:34 +0100

    Remove changes in ViewHeader

--- graylog2-web-interface/src/views/components/views/ViewHeader.tsx
+++ graylog2-web-interface/src/views/components/views/ViewHeader.tsx
@@ -28,7 +28,6 @@ import onSaveView from 'views/logic/views/OnSaveViewAction';
 import View from 'views/logic/views/View';
 import Routes from 'routing/Routes';
 import viewTitle from 'views/logic/views/ViewTitle';
-import FavoriteIcon from 'views/components/FavoriteIcon';
 
 const links = {
   [View.Type.Dashboard]: {
@@ -92,15 +91,12 @@ const ViewHeader = () => {
         <TitleWrapper>
           <span>{title}</span>
           {isSavedView && (
-            <>
-              <EditButton onClick={toggleMetadataEdit}
-                          role="button"
-                          title={`Edit ${typeText} ${view.title} metadata`}
-                          tabIndex={0}>
-                <Icon name="pen-to-square" />
-              </EditButton>
-              <FavoriteIcon isFavorite id={view.id} />
-            </>
+            <EditButton onClick={toggleMetadataEdit}
+                        role="button"
+                        title={`Edit ${typeText} ${view.title} metadata`}
+                        tabIndex={0}>
+              <Icon name="pen-to-square" />
+            </EditButton>
           )}
         </TitleWrapper>
         {showMetadataEdit && (
