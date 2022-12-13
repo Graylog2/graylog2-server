4284bd41fe32ffc88b54001426d2834e33ab9894
Dan Torrey <dan@graylog.com>
2022-12-13 06:20:01 -0600

    Add global Index Sets Defaults system configuration (#13018)
    * Work in progress for Index Defaults configuration
    
    * Minimum working version
    
    * Use defaults in Illuminate setup, fallback to server config
    
    * Add missing license headers
    
    * Add other needed config options to backend
    
    * Fix config definition annotations, frontend WIP
    
    * Error-free TSX migration
    
    * Mostly-complete TSX migration
    
    * Complete TSX migration
    
    * Config form fixes
    
    * Index defaults retrieval in Create Index form
    
    * End-to-end working index defaults
    
    * Add missing license header
    
    * Fix lint errors
    
    * Working rotation config
    
    * Add rotation config summary
    
    * Working retention and rotation
    
    * Fix wrapping of rotation/retention sections
    
    * Fix wrapping of rotation/retention sections
    
    * Working creation default: Retention
    
    * All defaults working on index creation
    
    * Fix lint and frontend build errors
    
    * Fix lint and frontend build errors
    
    * lint
    
    * Fix more lint issues
    
    * Fix more lint issues
    
    * Fix more lint issues
    
    * Allow new properties to be nullable
    
    * Cleanup
    
    * Backwards compatibility support
    
    * Clean up graylog.conf index default config
    
    - Remove 2.0 migration warnings.
    - Provide same order for settings as in UI
    - Adjust wording
    
    * Cleanup and fix failing tests
    
    * Lint fixes
    
    * Lint fixes
    
    * Fix failing test
    
    * Add changelog placeholder
    
    * Add change log content
    
    * Fix incorrect field name
    
    * Add validation for interval and max retention time
    
    * Small cleanup for consistency
    
    * Hide configuration UI on Cloud.
    
    * Address PR feedback
    
    * Improve validation for editing defaults
    
    * Add changelog
    
    * Use appropriate defaults for system indices
    
    - Default index set
    - Graylog Events
    - Graylog System Events
    - Graylog Message Failures
    - Restored Archives
    
    * Address PR feedback
    
    * Fix useIndexDefaults key
    
    Co-authored-by: Zack King <91903901+kingzacko1@users.noreply.github.com>
    
    * Add missing license headers
    
    * Fix typo
    
    * Cleanup test class
    
    * Fix lint error
    
    * Restore original default Create Default Index Set migration
    
    * Update change log with all relevant details
    
    * Update default index size, API example defaults
    
    - Set 30GB index rotation size default
    - Use default values in changelog API example
    
    * Update default index size in change log
    
    * Update change log and UPGRADING.md with upgrade advice
    
    * Rotate UPGRADING.md for Graylog 5.1.x
    
    * Fix typos
    
    * Fix changelog link
    
    * Fix misspelled word
    
    * Move changelog details to UPGRADING.md
    
    * Move changelog details to UPGRADING.md
    
    Co-authored-by: Zack King <91903901+kingzacko1@users.noreply.github.com>

---
+++ graylog2-server/src/test/java/org/graylog2/configuration/IndexSetsDefaultConfigurationTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy;
+import org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig;
+import org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy;
+import org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig;
+import org.graylog2.shared.bindings.providers.ObjectMapperProvider;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+class IndexSetsDefaultConfigurationTest {
+
+    @Test
+    void testConvert() {
+        // Verify that JSON annotation in class are properly defined so that the
+        // ClusterConfigService can perform needed payload conversions on reads/writes.
+        final IndexSetsDefaultConfiguration indexConfig = IndexSetsDefaultConfiguration.builder()
+                .indexAnalyzer("standard")
+                .shards(1)
+                .replicas(0)
+                .indexOptimizationDisabled(false)
+                .indexOptimizationMaxNumSegments(0)
+                .fieldTypeRefreshInterval(1)
+                .fieldTypeRefreshIntervalUnit(TimeUnit.SECONDS)
+                .rotationStrategyClass(MessageCountRotationStrategy.class.getCanonicalName())
+                .rotationStrategyConfig(MessageCountRotationStrategyConfig.create(10))
+                .retentionStrategyClass(DeletionRetentionStrategy.class.getCanonicalName())
+                .retentionStrategyConfig(DeletionRetentionStrategyConfig.create(10)).build();
+        final ObjectMapper objectMapper = new ObjectMapperProvider().get();
+        final Map map = objectMapper.convertValue(indexConfig, HashMap.class);
+        objectMapper.convertValue(map, IndexSetsDefaultConfiguration.class);
+    }
+}
