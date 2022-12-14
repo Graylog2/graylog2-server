ad44742b23f1dd411130327222366a20ad2a7f7f
Lukasz Kaminski <lukasz.kaminski@graylog.com>
2022-12-14 13:18:55 +0100

    String formatting with explicit locale

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/errors/ResultWindowLimitError.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/errors/ResultWindowLimitError.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.graylog.plugins.views.search.Query;
 
 import javax.annotation.Nonnull;
+import java.util.Locale;
 
 public class ResultWindowLimitError extends SearchTypeError {
 
@@ -29,7 +30,7 @@ public class ResultWindowLimitError extends SearchTypeError {
             "Result window is too large, [from + size] must be less than or equal to: [%d]. This limit can be set by changing the [index.max_result_window] index level setting, but it is recommended to use more advanced methods in order to get distant chunks of results (i.e. scroll or search after)";
 
     ResultWindowLimitError(@Nonnull Query query, @Nonnull String searchTypeId, int resultWindowLimit) {
-        super(query, searchTypeId, String.format(ERROR_DESCRIPTION, resultWindowLimit));
+        super(query, searchTypeId, String.format(Locale.US, ERROR_DESCRIPTION, resultWindowLimit));
 
         this.resultWindowLimit = resultWindowLimit;
     }
