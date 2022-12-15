c869f16f30af24f95756ca39e0ceab6c54bb8cc2
Linus Pahl <linus@graylog.com>
2022-12-15 15:03:33 +0100

    Display saved searches overview as data table. (#14136)
    * Combine attributes and attribute permissions.
    
    * Unify naming and cleanup code
    
    * Cleaning up rebase
    
    * Combine attributes and attribute permissions.
    
    * Unify naming and cleanup code
    
    * Calculate optimal columns widths.
    
    * Cleanup
    
    * Refactoring
    
    * Fix naming
    
    * Improve calculation of columns widths.
    
    * Adding test
    
    * Cleanup
    
    * Cleaning up rebase.
    
    * Removing not needed tsconfig change.
    
    * Rename `ConfigurableDataTable` to `EntityDataTable`.
    
    * Combine attributes and attribute permissions.
    
    * Unify naming and cleanup code
    
    * Display throughput for streams overview.
    
    * Emplement `EntityDataTable` for `DashboardList`.
    
    * Createing separate components for `DashboardsOverview`.
    
    * Updating and adding tests
    
    * Move dashboards components into dashboard directory.
    
    * Cleanup
    
    * Use `useQuery` to fetch dashboard list.
    
    * Cleaning up rebase.
    
    * Cleaning up rebase.
    
    * Createing separate components for `DashboardsOverview`.
    
    * Move dashboards components into dashboard directory.
    
    * Implement `EntityDataTable` for `SavedSearchesList`.
    
    * Adding `summary` and `description` filed for overview.
    
    * Extend width of saved searches modal.
    
    * Fixing license header
    
    * Improvements for search deletion.
    
    * Always wrap entity data table row actions with `ButtonToolbar` component.
    
    * Fixing page size changes
    
    * Adding changelog.

---
+++ graylog2-web-interface/src/views/components/searchbar/saved-search/SavedSearchesList.tsx
@@ -0,0 +1,237 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import { useState, useCallback } from 'react';
+import styled, { css } from 'styled-components';
+import { useQuery } from '@tanstack/react-query';
+
+import { Alert, Button } from 'components/bootstrap';
+import { PaginatedList, SearchForm, Spinner } from 'components/common';
+import type View from 'views/logic/views/View';
+import ViewLoaderContext from 'views/logic/ViewLoaderContext';
+import QueryHelper from 'components/common/QueryHelper';
+import type { Sort, ColumnRenderers } from 'components/common/EntityDataTable';
+import EntityDataTable from 'components/common/EntityDataTable';
+import type { PaginatedViews } from 'views/stores/ViewManagementStore';
+import { SavedSearchesActions } from 'views/stores/SavedSearchesStore';
+import type FetchError from 'logic/errors/FetchError';
+import UserNotification from 'util/UserNotification';
+
+type SearchParams = {
+  page: number,
+  pageSize: number,
+  query: string,
+  sort: Sort
+}
+
+const INITIAL_COLUMNS = ['title', 'description', 'summary'];
+const COLUMN_DEFINITIONS = [
+  { id: 'created_at', title: 'Created At', sortable: true },
+  { id: 'title', title: 'Title', sortable: true },
+  { id: 'description', title: 'Description', sortable: true },
+  { id: 'summary', title: 'Summary', sortable: true },
+  { id: 'owner', title: 'Owner', sortable: true },
+];
+
+const DEFAULT_PAGINATION = {
+  query: '',
+  page: 1,
+  pageSize: 10,
+};
+
+const NoSavedSearches = styled(Alert)`
+  clear: right;
+  display: flex;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+const onLoad = (onLoadSavedSearch: () => void, selectedSavedSearchId: string, loadFunc: (searchId: string) => void) => {
+  if (!selectedSavedSearchId || !loadFunc) {
+    return false;
+  }
+
+  loadFunc(selectedSavedSearchId);
+
+  onLoadSavedSearch();
+
+  return false;
+};
+
+const TitleLink = styled(Button)(({ $isActive }: { $isActive: boolean }) => css`
+  font-weight: ${$isActive ? 'bold' : 'normal'};
+  padding: 0;
+`);
+
+const customColumnRenderers = (onLoadSavedSearch: () => void, activeSavedSearchId: string): ColumnRenderers<View> => ({
+  title: {
+    renderCell: (search) => (
+      <ViewLoaderContext.Consumer key={search.id}>
+        {(loaderFunc) => (
+          <TitleLink bsStyle="link"
+                     onClick={() => onLoad(onLoadSavedSearch, search.id, loaderFunc)}
+                     $isActive={search.id === activeSavedSearchId}>
+            {search.title}
+          </TitleLink>
+        )}
+      </ViewLoaderContext.Consumer>
+    ),
+  },
+});
+
+const onDelete = (e, savedSearch: View, deleteSavedSearch: (search: View) => Promise<View>, activeSavedSearchId: string, refetch: () => void) => {
+  e.stopPropagation();
+
+  // eslint-disable-next-line no-alert
+  if (window.confirm(`You are about to delete saved search: "${savedSearch.title}". Are you sure?`)) {
+    deleteSavedSearch(savedSearch).then(() => {
+      if (savedSearch.id !== activeSavedSearchId) {
+        refetch();
+      }
+    });
+  }
+};
+
+const usePaginatedSavedSearches = (searchParams: SearchParams): {
+  data: PaginatedViews | undefined,
+  refetch: () => void,
+  isLoading: boolean
+} => {
+  const { data, refetch, isLoading } = useQuery(
+    ['saved-searches', 'overview', searchParams],
+    () => SavedSearchesActions.search({
+      query: searchParams.query,
+      page: searchParams.page,
+      perPage: searchParams.pageSize,
+      sortBy: searchParams.sort.columnId,
+      order: searchParams.sort.order,
+    }),
+    {
+      onError: (error: FetchError) => {
+        UserNotification.error(`Fetching saved searches failed with status: ${error}`,
+          'Could not retrieve saved searches');
+      },
+      keepPreviousData: true,
+    },
+  );
+
+  return ({
+    data,
+    refetch,
+    isLoading,
+  });
+};
+
+type Props = {
+  activeSavedSearchId: string,
+  deleteSavedSearch: (view: View) => Promise<View>,
+  onLoadSavedSearch: () => void,
+};
+
+const SavedSearchesList = ({
+  activeSavedSearchId,
+  deleteSavedSearch,
+  onLoadSavedSearch,
+}: Props) => {
+  const [visibleColumns, setVisibleColumns] = useState(INITIAL_COLUMNS);
+  const [searchParams, setSearchParams] = useState<SearchParams>({
+    page: DEFAULT_PAGINATION.page,
+    pageSize: DEFAULT_PAGINATION.pageSize,
+    query: DEFAULT_PAGINATION.query,
+    sort: {
+      columnId: 'title',
+      order: 'asc',
+    },
+  });
+
+  const { data, isLoading, refetch } = usePaginatedSavedSearches(searchParams);
+
+  const handleSearch = useCallback(
+    (newQuery: string) => setSearchParams((cur) => ({
+      ...cur,
+      query: newQuery,
+      page: DEFAULT_PAGINATION.page,
+    })),
+    [],
+  );
+  const handlePageSizeChange = useCallback(
+    (newPage: number, newPageSize: number) => setSearchParams((cur) => ({
+      ...cur,
+      page: newPage,
+      pageSize: newPageSize,
+    })),
+    [],
+  );
+  const onSortChange = useCallback((newSort: Sort) => {
+    setSearchParams((cur) => ({ ...cur, sort: newSort, page: 1 }));
+  }, []);
+
+  const onResetSearch = useCallback(() => handleSearch(''), [handleSearch]);
+  const onColumnsChange = useCallback((newVisibleColumns: Array<string>) => {
+    setVisibleColumns(newVisibleColumns);
+  }, []);
+
+  const renderSavedSearchActions = useCallback((search: View) => (
+    <Button onClick={(e) => onDelete(e, search, deleteSavedSearch, activeSavedSearchId, refetch)}
+            role="button"
+            bsSize="xsmall"
+            bsStyle="danger"
+            title={`Delete search ${search.title}`}
+            tabIndex={0}>
+      Delete
+    </Button>
+  ), [activeSavedSearchId, deleteSavedSearch, refetch]);
+
+  const columnRenderers = customColumnRenderers(onLoadSavedSearch, activeSavedSearchId);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const { list: savedSearches, pagination } = data;
+
+  return (
+    <PaginatedList onChange={handlePageSizeChange}
+                   activePage={searchParams.page}
+                   totalItems={pagination?.total}
+                   pageSize={searchParams.pageSize}
+                   useQueryParameter={false}>
+      <SearchForm focusAfterMount
+                  onSearch={handleSearch}
+                  queryHelpComponent={<QueryHelper entityName="search" commonFields={['id', 'title']} />}
+                  topMargin={0}
+                  onReset={onResetSearch} />
+      {pagination?.total === 0 && (
+        <NoSavedSearches>
+          No saved searches found.
+        </NoSavedSearches>
+      )}
+      {!!savedSearches?.length && (
+        <EntityDataTable<View> data={savedSearches}
+                               visibleColumns={visibleColumns}
+                               onColumnsChange={onColumnsChange}
+                               onSortChange={onSortChange}
+                               activeSort={searchParams.sort}
+                               rowActions={renderSavedSearchActions}
+                               columnRenderers={columnRenderers}
+                               columnDefinitions={COLUMN_DEFINITIONS} />
+      )}
+    </PaginatedList>
+  );
+};
+
+export default SavedSearchesList;
