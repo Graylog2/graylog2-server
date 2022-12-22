0e5de8d943a98474537b2cd5336edf66961939db
Jan Heise <jan.heise@graylog.com>
2022-12-22 11:06:45 +0100

    add IT test for startPage

--- full-backend-tests/src/test/java/org/graylog/plugins/views/StartPageIT.java
+++ full-backend-tests/src/test/java/org/graylog/plugins/views/StartPageIT.java
@@ -17,19 +17,30 @@
 package org.graylog.plugins.views;
 
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.graylog.testing.completebackend.apis.GraylogApis;
+import org.graylog.testing.completebackend.apis.Streams;
 import org.graylog.testing.completebackend.apis.Users;
 import org.graylog.testing.containermatrix.annotations.ContainerMatrixTest;
 import org.graylog.testing.containermatrix.annotations.ContainerMatrixTestsConfiguration;
+import org.graylog2.plugin.streams.StreamRuleType;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.util.Collections;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @ContainerMatrixTestsConfiguration
 public class StartPageIT {
     private final RequestSpecification requestSpec;
     private final GraylogApis api;
+
+    private static final String username = "john.doe2";
+    private static final String password = "asdfgh";
 
     public StartPageIT(RequestSpecification requestSpec, GraylogApis apis) {
         this.requestSpec = requestSpec;
@@ -39,26 +50,66 @@ public class StartPageIT {
     @BeforeAll
     public void init() {
         final JsonPath user = api.users().createUser(new Users.User(
-                "john.doe2",
-                "asdfgh",
+                this.username,
+                this.password,
                 "John",
                 "Doe",
                 "john.doe2@example.com",
                 false,
                 30_000,
                 "Europe/Vienna",
-                Collections.emptyList(),
+                List.of("Admin"),
                 Collections.emptyList()
         ));
     }
 
-    @ContainerMatrixTest
-    void testCreateRecentActivity() {
+    private ValidatableResponse post(final String url, final String resource, final int expectedResult) {
+        return given()
+                .spec(requestSpec)
+                .auth().basic(username, password)
+                .when()
+                .body(getClass().getClassLoader().getResourceAsStream(resource))
+                .post(url)
+                .then()
+                .log().ifStatusCodeMatches(not(expectedResult))
+                .statusCode(expectedResult);
+    }
 
+    private ValidatableResponse get(final String url, final String username, final String password, final int expectedResult) {
+        return given()
+                .spec(requestSpec)
+                .auth().basic(username, password)
+                .when()
+                .get(url)
+                .then()
+                .log().ifStatusCodeMatches(not(expectedResult))
+                .statusCode(expectedResult);
+    }
+
+    private ValidatableResponse get(final String url, final int expectedResult) {
+        return get(url, this.username, this.password, expectedResult);
     }
 
     @ContainerMatrixTest
-    void testCreateLastOpened() {
+    void testCreateLastOpenedItem() {
+        post("/views/search", "org/graylog/plugins/views/save-search-request.json", 201);
+        post("/views", "org/graylog/plugins/views/startpage-views-request.json", 200);
 
+        var validatableResponse = get("/views", 200);
+        var id = validatableResponse.extract().jsonPath().get("views[0]._id");
+
+        get("/views/" + id, 200);
+        validatableResponse = get("/startpage/lastOpened", 200);
+        validatableResponse.assertThat().body("lastOpened[0].id", equalTo(id));
+    }
+
+
+    @ContainerMatrixTest
+    void testCreateRecentActivity() {
+        final String defaultIndexSetId = api.indices().defaultIndexSetId();
+        var stream1Id = api.streams().createStream("Stream #1", defaultIndexSetId, new Streams.StreamRule(StreamRuleType.EXACT.toInteger(), "stream1", "target_stream", false));
+
+        var validatableResponse = get("/startpage/recentActivity", "admin", "admin", 200).log().body();
+        validatableResponse.assertThat().body("recentActivity[0].item_id", equalTo(stream1Id));
     }
 }
