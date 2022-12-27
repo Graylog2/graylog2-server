d386d7e4417c37402a7e64aec9f2a02b37f1223d
Marco Pfatschbacher <marco@graylog.com>
2022-12-27 13:38:41 +0100

    Escape Event content in HTML email notifications (#14286) (4.3) (#14305)
    * Escape Event content in HTML email notifications (#14286)
    
    * replace Jmte Engine instance with JmteEngineProvider
    
    plugins can add NamedRenderer to engine
    add HtmlSafeRenderer to engine by default
    
    * Add EmailJmteEngineProvider, always escape values
    
    * Apply html escape to new event notification emails
    
    The legacy email notification was plaintext only
    and does not need to be escaped.
    
    Also simplify escaping a bit and don't escape the email Subject.
    
    * Add EmailSenderTest
    
    * add changelog
    
    * remove unneded multibinder
    
    * revert test notification title change
    
    * keep singleton binding for regular template engine
    
    Co-authored-by: 朱穆穆 <mumu.zhu@feat.com>
    (cherry picked from commit 29a4c31682274c252b7a95a355b2361966611471)
    
    * adopt to java 1.8

---
+++ graylog2-server/src/main/java/org/graylog2/bindings/providers/HtmlSafeJmteEngineProvider.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.bindings.providers;
+
+import com.floreysoft.jmte.Engine;
+import com.floreysoft.jmte.Renderer;
+import com.google.common.html.HtmlEscapers;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+import java.util.Locale;
+import java.util.Map;
+
+
+@Singleton
+public class HtmlSafeJmteEngineProvider implements Provider<Engine> {
+    private final Engine engine;
+
+    @Inject
+    public HtmlSafeJmteEngineProvider() {
+      engine = Engine.createEngine();
+      engine.registerRenderer(String.class, new HtmlSafeRenderer());
+    }
+
+    @Override
+    public Engine get() {
+      return engine;
+    }
+
+    private static class HtmlSafeRenderer implements Renderer<String> {
+      @Override
+      public String render(String value, Locale locale, Map<String,Object> model) {
+          return HtmlEscapers.htmlEscaper().escape(value);
+      }
+    }
+}
