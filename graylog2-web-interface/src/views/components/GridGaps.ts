01fff3f9588fdafbbb48daa1f83feadf7a1058e7
Dennis Oelkers <dennis@graylog.com>
2022-12-27 15:44:33 +0100

    Returning gaps at beginning of row as well.

--- graylog2-web-interface/src/views/components/GridGaps.ts
+++ graylog2-web-interface/src/views/components/GridGaps.ts
@@ -28,10 +28,6 @@ const gapBetween = (item: Item, neighbor: Item): Item => ({
   end: { x: neighbor.start.x, y: Math.min(item.end.y, neighbor.end.y) },
 });
 
-const isFirstItemInRow = (items: Item[], item: Item) => {
-
-};
-
 const normalizeInfinity = ({ start, end }: Item, maxWidth: number): Item => ({
   start,
   end: {
@@ -40,14 +36,49 @@ const normalizeInfinity = ({ start, end }: Item, maxWidth: number): Item => ({
   },
 });
 
-export const findGaps = (_items: Item[], maxWidth: number = 12): Item[] => {
+type RowItems = { [row: string]: Item[] };
+
+const findInitialGaps = (rows: Array<number>, rowItems: RowItems, minWidth: number) => rows
+  .map((row) => [row, Math.min(...rowItems[row].map((item) => item.start.x))])
+  .filter(([, startX]) => startX > minWidth)
+  .reduce((gaps, [row, startX]) => {
+    if (gaps.length === 0) {
+      return [{ start: { x: minWidth, y: row }, end: { x: startX, y: row } }] as Item[];
+    }
+
+    const [gap, ...rest] = gaps.reverse();
+
+    if (gap.end.x !== startX) {
+      return [...rest, gap, { start: { x: minWidth, y: row }, end: { x: startX, y: row } }];
+    }
+
+    return [...rest, { start: gap.start, end: { x: startX, y: row } }];
+  }, [] as Item[]);
+
+const findFinalGaps = (rows: Array<number>, rowItems: RowItems, maxWidth: number) => rows
+  .map((row) => [row, Math.max(...rowItems[row].map((item) => item.end.x))])
+  .filter(([, endX]) => endX < maxWidth)
+  .reduce((gaps, [row, endX]) => {
+    if (gaps.length === 0) {
+      return [{ start: { x: endX, y: row }, end: { x: maxWidth + 1, y: row + 1 } }] as Item[];
+    }
+
+    const [gap, ...rest] = gaps.reverse();
+
+    if (gap.start.x !== endX) {
+      return [...rest, gap, { start: { x: endX, y: row }, end: { x: maxWidth + 1, y: row + 1 } }];
+    }
+
+    return [...rest, { start: gap.start, end: { x: maxWidth + 1, y: row + 1 } }];
+  }, [] as Item[]);
+
+export const findGaps = (_items: Item[], minWidth: number = 1, maxWidth: number = 12): Item[] => {
   if (_items.length === 0) {
     return [];
   }
 
   const items = _items.map((item) => normalizeInfinity(item, maxWidth));
-  const minX = Math.min(...items.map(({ start: { x } }) => x));
-  const maxX = Math.max(...items.map(({ end: { x } }) => x));
+  console.log({ items });
   const minY = Math.min(...items.map(({ start: { y } }) => y));
   const maxY = Math.max(...items.map(({ end: { y } }) => y));
 
@@ -55,7 +86,12 @@ export const findGaps = (_items: Item[], maxWidth: number = 12): Item[] => {
 
   const rowItems = Object.fromEntries(rows.map((row) => [row, itemsInRow(items, row)]));
 
-  const initialGaps = items.filter((item) => isFirstItemInRow(items, item));
+  const initialGaps = findInitialGaps(rows, rowItems, minWidth);
+
+  const finalGaps = findFinalGaps(rows, rowItems, maxWidth);
+
+  console.log({ initialGaps, finalGaps });
+
   const gaps = items.flatMap((item) => {
     const neighbors = neighborsToRight(item, rowItems);
 
@@ -63,5 +99,5 @@ export const findGaps = (_items: Item[], maxWidth: number = 12): Item[] => {
       .map((neighbor) => gapBetween(item, neighbor));
   });
 
-  return uniq(gaps);
+  return uniq([...gaps, ...initialGaps, ...finalGaps]);
 };
