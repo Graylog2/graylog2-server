18bcb4ad7e487307ea3aeb9a53b5684783cd86eb
Dennis Oelkers <dennis@graylog.com>
2023-01-05 10:45:08 +0100

    Triggering new state earlier.

--- graylog2-web-interface/src/views/stores/ViewStore.ts
+++ graylog2-web-interface/src/views/stores/ViewStore.ts
@@ -123,6 +123,7 @@ export const ViewStore: ViewStoreType = singletonStore(
       const queries: QuerySet = view?.search?.queries ?? Immutable.Set();
 
       this.activeQuery = _selectedQuery(queries, this.activeQuery, queryId);
+      this._trigger();
 
       const promise = ViewActions.search(view.search)
         .then(() => {
