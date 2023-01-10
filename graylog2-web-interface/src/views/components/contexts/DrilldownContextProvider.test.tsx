9c9eb56cad5644a1fe9e575c8a62c4a4cafd3137
Dennis Oelkers <dennis@graylog.com>
2023-01-10 13:14:36 +0100

    Adjusting test for `DrilldownContextProvider`.

--- graylog2-web-interface/src/views/components/contexts/DrilldownContextProvider.test.tsx
+++ graylog2-web-interface/src/views/components/contexts/DrilldownContextProvider.test.tsx
@@ -23,10 +23,10 @@ import View from 'views/logic/views/View';
 import MessagesWidget from 'views/logic/widgets/MessagesWidget';
 import type { TimeRange, ElasticsearchQueryString } from 'views/logic/queries/Query';
 import Query, { createElasticsearchQueryString, filtersForQuery } from 'views/logic/queries/Query';
-import { GlobalOverrideStore } from 'views/stores/GlobalOverrideStore';
 import GlobalOverride from 'views/logic/search/GlobalOverride';
 import useViewType from 'views/hooks/useViewType';
 import useCurrentQuery from 'views/logic/queries/useCurrentQuery';
+import useGlobalOverride from 'views/hooks/useGlobalOverride';
 
 import DrilldownContextProvider from './DrilldownContextProvider';
 import DrilldownContext from './DrilldownContext';
@@ -37,6 +37,7 @@ jest.mock('views/stores/GlobalOverrideStore', () => ({
 
 jest.mock('views/hooks/useViewType');
 jest.mock('views/logic/queries/useCurrentQuery');
+jest.mock('views/hooks/useGlobalOverride');
 
 describe('DrilldownContextProvider', () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -83,7 +84,7 @@ describe('DrilldownContextProvider', () => {
     it('passes query & timerange of global override, streams of widget', () => {
       asMock(useViewType).mockReturnValue(View.Type.Dashboard);
 
-      asMock(GlobalOverrideStore.getInitialState)
+      asMock(useGlobalOverride)
         .mockReturnValue(GlobalOverride.create(
           { type: 'absolute', from: '2020-01-10T13:23:42.000Z', to: '2020-01-10T14:23:42.000Z' },
           createElasticsearchQueryString('something:"else"'),
