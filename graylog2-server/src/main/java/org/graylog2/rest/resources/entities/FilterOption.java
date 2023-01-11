41df77f11039c219281597e5e6de7ec2ad958032
luk-kaminski <lukasz.kaminski@graylog.com>
2023-01-11 14:23:23 +0100

    Proper field id for status. Filter option contains MongoDB value. (#14419)

--- graylog2-server/src/main/java/org/graylog2/rest/resources/entities/FilterOption.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/entities/FilterOption.java
@@ -18,8 +18,8 @@ package org.graylog2.rest.resources.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record FilterOption(@JsonProperty("id") String id, @JsonProperty("title") String title) {
-    public static FilterOption create(String id, String title) {
-        return new FilterOption(id, title);
+public record FilterOption(@JsonProperty("value") String value, @JsonProperty("title") String title) {
+    public static FilterOption create(String value, String title) {
+        return new FilterOption(value, title);
     }
 }
