23525d498d36e9c1c484421043f34f462f2b5ca6
Linus Pahl <linus@graylog.com>
2023-01-19 17:16:13 +0100

    Change name of view constant `DEFAULT_LIMIT` to `DEFAULT_PIVOT_LIMIT`.

--- graylog2-web-interface/src/views/logic/aggregationbuilder/Pivot.ts
+++ graylog2-web-interface/src/views/logic/aggregationbuilder/Pivot.ts
@@ -15,7 +15,7 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import type { TimeUnits } from 'views/Constants';
-import { DEFAULT_LIMIT } from 'views/Constants';
+import { DEFAULT_PIVOT_LIMIT } from 'views/Constants';
 
 export type AutoTimeConfig = {
   type: 'auto',
@@ -58,7 +58,7 @@ type InternalState = {
 export default class Pivot {
   _value: InternalState;
 
-  constructor(fields: Array<string>, type: string, config: PivotConfigType = { limit: DEFAULT_LIMIT }) {
+  constructor(fields: Array<string>, type: string, config: PivotConfigType = { limit: DEFAULT_PIVOT_LIMIT }) {
     this._value = { fields, type, config };
   }
 
@@ -74,12 +74,12 @@ export default class Pivot {
     return this._value.config;
   }
 
-  static create(fields: Array<string>, type: string, config: PivotConfigType = { limit: DEFAULT_LIMIT }) {
+  static create(fields: Array<string>, type: string, config: PivotConfigType = { limit: DEFAULT_PIVOT_LIMIT }) {
     return new Pivot(fields, type, config);
   }
 
   static fromJSON(value: PivotJson) {
-    const { fields, type, config = { limit: DEFAULT_LIMIT } } = value;
+    const { fields, type, config = { limit: DEFAULT_PIVOT_LIMIT } } = value;
 
     return new Pivot(fields, type, config);
   }
