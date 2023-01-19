96b7fcb21091e91edad5cfd1ab938a09f9bb4a98
Linus Pahl <linus@graylog.com>
2023-01-19 14:16:56 +0100

    Do not display field value actions for aggregation legend if item relates to more than one field.

--- graylog2-web-interface/src/views/components/visualizations/pie/PieVisualization.tsx
+++ graylog2-web-interface/src/views/components/visualizations/pie/PieVisualization.tsx
@@ -88,12 +88,12 @@ const labelMapper = (data: Array<{ labels: Array<string> }>) => data.reduce((acc
 }, []);
 
 const legendField = (columnPivots: Array<Pivot>, rowPivots: Array<Pivot>, series: Array<Series>, isFunction: boolean) => {
-  if (rowPivots.length === 1 && series.length === 1 && !isFunction) {
-    return rowPivots[0].fields.join(', ');
+  if (rowPivots.length === 1 && series.length === 1 && rowPivots[0].fields?.length === 1 && !isFunction) {
+    return rowPivots[0].fields[0];
   }
 
-  if (columnPivots.length === 1) {
-    return columnPivots[0].fields.join(', ');
+  if (columnPivots.length === 1 && columnPivots[0].fields?.length === 1) {
+    return columnPivots[0].fields[0];
   }
 
   return null;
