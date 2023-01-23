96a54d6f93af231e9b1fbef985b438f9a598e3a0
Ousmane Samba <ousmane@graylog.com>
2023-01-23 16:57:36 +0100

    add Status and Scheduler components

---
+++ graylog2-web-interface/src/components/event-definitions/event-definitions/StatusCell.tsx
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { Label } from 'components/bootstrap';
+
+const StatusLabel = styled(Label)`
+  display: inline-flex;
+  justify-content: center;
+  gap: 4px;
+`;
+
+type Props ={
+  status: boolean,
+}
+
+const StatusCell = ({ status } : Props) => {
+  return (
+    <StatusLabel bsStyle={status ? 'success' : 'warning'}>
+      {status ? 'enabled' : 'disabled'}
+    </StatusLabel>
+  );
+};
+
+export default StatusCell;
