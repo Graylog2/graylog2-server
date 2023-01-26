6f155c1f64ce8e1d75e9422e60917bac6626e385
Ousmane Samba <ousmane@graylog.com>
2023-01-26 13:12:16 +0100

    add bindings

--- graylog2-web-interface/src/components/indices/rotation/index.ts
+++ graylog2-web-interface/src/components/indices/rotation/index.ts
@@ -22,6 +22,8 @@ import SizeBasedRotationStrategyConfiguration from './SizeBasedRotationStrategyC
 import SizeBasedRotationStrategySummary from './SizeBasedRotationStrategySummary';
 import TimeBasedRotationStrategyConfiguration from './TimeBasedRotationStrategyConfiguration';
 import TimeBasedRotationStrategySummary from './TimeBasedRotationStrategySummary';
+import TimeBasedSizeOptimizingStrategy from './TimeBasedSizeOptimizingStrategyConfiguration';
+import TimeBasedSizeOptimizingStrategySummary from './TimeBasedSizeOptimizingStrategySummary';
 
 PluginStore.register(new PluginManifest({}, {
   indexRotationConfig: [
@@ -42,6 +44,12 @@ PluginStore.register(new PluginManifest({}, {
       displayName: 'Index Time',
       configComponent: TimeBasedRotationStrategyConfiguration,
       summaryComponent: TimeBasedRotationStrategySummary,
+    },
+    {
+      type: 'org.graylog2.indexer.rotation.strategies.TimeBasedSizeOptimizingStrategy',
+      displayName: 'Smart Time Based Index',
+      configComponent: TimeBasedSizeOptimizingStrategy,
+      summaryComponent: TimeBasedSizeOptimizingStrategySummary,
     },
   ],
 }));
