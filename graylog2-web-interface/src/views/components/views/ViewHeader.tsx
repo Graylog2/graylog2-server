e41e37a339a6d92aa105c603f5d31422a286e702
Dennis Oelkers <dennis@graylog.com>
2023-02-16 17:17:48 +0100

    Fixup handling of dirty flag for views.

--- graylog2-web-interface/src/views/components/views/ViewHeader.tsx
+++ graylog2-web-interface/src/views/components/views/ViewHeader.tsx
@@ -29,7 +29,7 @@ import useViewTitle from 'views/hooks/useViewTitle';
 import useView from 'views/hooks/useView';
 import useAppDispatch from 'stores/useAppDispatch';
 import FavoriteIcon from 'views/components/FavoriteIcon';
-import { loadView } from 'views/logic/slices/viewSlice';
+import { updateView } from 'views/logic/slices/viewSlice';
 
 const links = {
   [View.Type.Dashboard]: {
@@ -84,7 +84,7 @@ const ViewHeader = () => {
 
   const typeText = view?.type?.toLocaleLowerCase();
   const title = useViewTitle();
-  const onChangeFavorite = useCallback((newValue) => dispatch(loadView(view.toBuilder().favorite(newValue).build())), [dispatch, view]);
+  const onChangeFavorite = useCallback((newValue) => dispatch(updateView(view.toBuilder().favorite(newValue).build())), [dispatch, view]);
 
   return (
     <Row>
