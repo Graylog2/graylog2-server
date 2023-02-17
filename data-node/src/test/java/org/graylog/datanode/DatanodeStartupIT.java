2df51faaff05674fee38ffd2c6927f2783877707
Tomas Dvorak <tomas.dvorak@graylog.com>
2023-02-17 14:41:57 +0100

    simplified docker setup for datanode IT

--- data-node/src/test/java/org/graylog/datanode/DatanodeStartupIT.java
+++ data-node/src/test/java/org/graylog/datanode/DatanodeStartupIT.java
@@ -12,7 +12,9 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import java.net.SocketException;
 import java.nio.file.Path;
@@ -21,18 +23,35 @@ import java.util.concurrent.TimeUnit;
 
 public class DatanodeStartupIT {
 
-    private DockerComposeContainer container;
+    private Network network;
+    private GenericContainer mongo;
+    private GenericContainer datanode;
 
     @BeforeEach
     void setUp() {
-        final Path dockerfilePath = Path.of("docker-compose.yml");
-        container = new DockerComposeContainer(dockerfilePath.toFile());
-        container.start();
+
+        network = Network.newNetwork();
+
+        mongo = new GenericContainer("mongo:5.0")
+                .withNetwork(network)
+                .withNetworkAliases("mongodb")
+                .withExposedPorts(27017);
+
+        datanode = new GenericContainer(new ImageFromDockerfile()
+                .withDockerfile(Path.of("./Dockerfile")))
+                .withExposedPorts(8999, 9200)
+                .withNetwork(network)
+                .dependsOn(mongo);
+
+        mongo.start();
+        datanode.start();
     }
 
     @AfterEach
     void tearDown() {
-        container.stop();
+        datanode.stop();
+        mongo.stop();
+        network.close();
     }
 
     @Test
@@ -46,16 +65,18 @@ public class DatanodeStartupIT {
                 .retryIfResult(input -> !input.extract().body().path("process.info.status").equals("AVAILABLE"))
                 .build();
 
-        retryer.call(this::getStatus)
+        final Integer datanodeRestApiPort = datanode.getMappedPort(8999);
+
+        retryer.call(() -> this.getStatus(datanodeRestApiPort))
                 .assertThat()
                 .body("process.info.node_name", Matchers.equalTo("node1"))
                 .body("process.info.pid", Matchers.notNullValue())
                 .body("process.info.user", Matchers.equalTo("opensearch"));
     }
 
-    private ValidatableResponse getStatus() {
+    private ValidatableResponse getStatus(Integer mappedPort) {
         return RestAssured.given()
-                .get("http://localhost:8999")
+                .get("http://localhost:" + mappedPort)
                 .then();
     }
 }
