c2b429a73296c0acb5fd722e16a05fe8d17a0ced
Linus Pahl <linus@graylog.com>
2023-02-24 15:45:42 +0100

    Fix page size select for streams overview.

--- graylog2-web-interface/src/hooks/usePaginationQueryParameter.ts
+++ graylog2-web-interface/src/hooks/usePaginationQueryParameter.ts
@@ -29,7 +29,7 @@ export type PaginationQueryParameterResult = {
   setPagination: (payload: { page?: number, pageSize?: number }) => void;
 };
 
-const usePaginationQueryParameter = (PAGE_SIZES: number[] = DEFAULT_PAGE_SIZES, defaultPageSize: number = DEFAULT_PAGE_SIZES[0]): PaginationQueryParameterResult => {
+const usePaginationQueryParameter = (PAGE_SIZES: number[] = DEFAULT_PAGE_SIZES, propsPageSize: number = DEFAULT_PAGE_SIZES[0], syncPageSizeFromQuery: boolean = true): PaginationQueryParameterResult => {
   const { page: pageQueryParameter, pageSize: pageSizeQueryParameter } = useQuery();
   const history = useHistory();
   const { search, pathname } = useLocation();
@@ -38,10 +38,25 @@ const usePaginationQueryParameter = (PAGE_SIZES: number[] = DEFAULT_PAGE_SIZES, 
   const page = (Number.isInteger(pageQueryParameterAsNumber) && pageQueryParameterAsNumber > 0) ? pageQueryParameterAsNumber : DEFAULT_PAGE;
 
   const pageSizeQueryParameterAsNumber = Number(pageSizeQueryParameter);
-  const pageSize = (Number.isInteger(pageSizeQueryParameterAsNumber) && PAGE_SIZES?.includes(pageSizeQueryParameterAsNumber)) ? pageSizeQueryParameterAsNumber : defaultPageSize;
+
+  const determinePageSize = () => {
+    if (!syncPageSizeFromQuery) {
+      return propsPageSize;
+    }
+
+    return (Number.isInteger(pageSizeQueryParameterAsNumber) && PAGE_SIZES?.includes(pageSizeQueryParameterAsNumber)) ? pageSizeQueryParameterAsNumber : propsPageSize;
+  };
+
+  const pageSize = determinePageSize();
 
   const setPagination = ({ page: newPage = page, pageSize: newPageSize = pageSize }: { page?: number, pageSize?: number }) => {
-    const uri = new URI(query).setSearch({ page: String(newPage), pageSize: String(newPageSize) });
+    const params: { page: string, pageSize?: string } = { page: String(newPage) };
+
+    if (syncPageSizeFromQuery) {
+      params.pageSize = String(newPageSize);
+    }
+
+    const uri = new URI(query).setSearch(params);
     history.replace(uri.toString());
   };
 
