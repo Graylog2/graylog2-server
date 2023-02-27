c08dded03df645d5112b8387f6ff6ee3b6db61de
Ezequiel Lopez <ezequiel.lopez@graylog.com>
2023-02-27 14:02:52 -0500

    feat: Updates alerts styles

--- graylog2-web-interface/src/util/UserNotification.js
+++ graylog2-web-interface/src/util/UserNotification.js
@@ -14,30 +14,51 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
+import React from 'react';
 import toastr from 'toastr';
 
 import './UserNotification.css';
 
 const defaultSettings = {
   debug: false,
-  positionClass: 'toast-bottom-full-width',
+  positionClass: 'toast-top-center',
+  // positionClass: 'toast-bottom-full-width',
   onclick: null,
   showDuration: 300,
   hideDuration: 1000,
+  // timeOut: 0,
   timeOut: 7000,
+  // extendedTimeOut: 0,
   extendedTimeOut: 1000,
   escapeHtml: true,
   closeButton: true,
-  closeHtml: '<button>Close</button>',
+  closeHtml: '<div><button>Close</button></div>',
   progressBar: true,
   preventDuplicates: true,
 };
 
 const UserNotification = {
-  error(message, title) {
+  errorButtons: (stackTrace) => {
+    const buttons = document.createElement('div');
+    const close = document.createElement('button');
+    close.innerHTML = 'Close';
+    buttons.appendChild(close);
+
+    if (stackTrace) {
+      const moreData = document.createElement('button');
+      moreData.innerHTML = 'More';
+      moreData.addEventListener('click', () => alert(stackTrace));
+      buttons.appendChild(moreData);
+    }
+
+    return buttons;
+  },
+  error(message, title, stackTrace = null) {
     toastr.error(message, title || 'Error', {
       ...defaultSettings,
       timeOut: 10000,
+      // timeOut: 0,
+      closeHtml: this.errorButtons(stackTrace),
     });
   },
   warning(message, title) {
