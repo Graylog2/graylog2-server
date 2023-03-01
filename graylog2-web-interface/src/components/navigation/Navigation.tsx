2ac0db676e51d202767abf8b40037fee960158b7
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2023-03-01 16:27:25 +0100

    Add Icon link to welcome page

--- graylog2-web-interface/src/components/navigation/Navigation.tsx
+++ graylog2-web-interface/src/components/navigation/Navigation.tsx
@@ -29,6 +29,7 @@ import { isPermitted } from 'util/PermissionsMixin';
 import useCurrentUser from 'hooks/useCurrentUser';
 import GlobalThroughput from 'components/throughput/GlobalThroughput';
 import Routes, { ENTERPRISE_ROUTE_DESCRIPTION, SECURITY_ROUTE_DESCRIPTION } from 'routing/Routes';
+import { Icon } from 'components/common';
 
 import UserMenu from './UserMenu';
 import HelpMenu from './HelpMenu';
@@ -144,12 +145,6 @@ const Navigation = React.memo(({ pathname }: Props) => {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav className="navbar-main">
-          {currentUser?.startpage !== null ? (
-            <LinkContainer relativeActive to={Routes.WELCOME}>
-              <NavItem to="welcome">Welcome</NavItem>
-            </LinkContainer>
-          ) : (null)}
-
           <LinkContainer relativeActive to={Routes.SEARCH}>
             <NavItem to="search">Search</NavItem>
           </LinkContainer>
@@ -187,6 +182,11 @@ const Navigation = React.memo(({ pathname }: Props) => {
             {pluginItems.map(({ key, component: Item }) => <Item key={key} />)}
           </InactiveNavItem>
 
+          <LinkContainer relativeActive to={Routes.WELCOME}>
+            <NavItem id="welcome-nav-link" aria-label="Welcome">
+              <Icon size="lg" fixedWidth title="Welcome" name="table-columns" />
+            </NavItem>
+          </LinkContainer>
           <ScratchpadToggle />
 
           <HelpMenu active={_isActive(pathname, Routes.WELCOME)} />
