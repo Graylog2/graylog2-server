96ef835b76a4314dfd27bcf9d2c353cc6927ef58
Lukasz Kaminski <lukasz.kaminski@graylog.com>
2023-03-07 10:17:37 +0100

    Basic permission handling.

--- graylog2-server/src/main/java/org/graylog2/database/suggestions/MongoEntitySuggestionService.java
+++ graylog2-server/src/main/java/org/graylog2/database/suggestions/MongoEntitySuggestionService.java
@@ -21,26 +21,33 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Sorts;
+import org.apache.shiro.authz.permission.AllPermission;
 import org.apache.shiro.subject.Subject;
 import org.bson.BsonDocument;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.graylog2.database.MongoConnection;
 import org.graylog2.database.PaginatedList;
+import org.graylog2.indexer.indexset.MongoIndexSetService;
+import org.graylog2.shared.security.RestPermissions;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-//TODO: permissions
-//TODO: collection name -> permission name mapping
+//TODO: Suggestions won't be provided to users that have permissions to just some of the entities in the collection
 public class MongoEntitySuggestionService implements EntitySuggestionService {
 
     private final MongoConnection mongoConnection;
+    //TODO: It would be great to have a mechanism to automatically map collection to proper permission. For now this mapping will be hardcoded for supported collections.
+    private final Map<String, String> mongoCollectionToPermission = new HashMap<>();
 
     @Inject
     public MongoEntitySuggestionService(final MongoConnection mongoConnection) {
         this.mongoConnection = mongoConnection;
+        this.mongoCollectionToPermission.put(MongoIndexSetService.COLLECTION_NAME, RestPermissions.INDEXSETS_READ);
     }
 
     @Override
@@ -50,6 +57,12 @@ public class MongoEntitySuggestionService implements EntitySuggestionService {
                                             final int page,
                                             final int perPage,
                                             final Subject subject) {
+
+        if (!hasAllPermission(subject) && !hasReadPermissionForWholeCollection(subject, collection)) {
+            return new EntitySuggestionResponse(List.of(),
+                    PaginatedList.PaginationInfo.create(0, 0, page, perPage));
+        }
+
         final MongoCollection<Document> mongoCollection = mongoConnection.getMongoDatabase().getCollection(collection);
 
         Bson bsonFilter = (query != null && !query.isEmpty()) ?
@@ -80,6 +93,19 @@ public class MongoEntitySuggestionService implements EntitySuggestionService {
                         page,
                         perPage));
 
+    }
+
+    public boolean hasAllPermission(final Subject subject) {
+        return subject.isPermitted(new AllPermission());
+    }
+
+    public boolean hasReadPermissionForWholeCollection(final Subject subject,
+                                                       final String collection) {
+        final String permission = mongoCollectionToPermission.get(collection);
+        if (permission == null) {
+            return false;
+        }
+        return subject.isPermitted(permission + ":*");
     }
 
 
