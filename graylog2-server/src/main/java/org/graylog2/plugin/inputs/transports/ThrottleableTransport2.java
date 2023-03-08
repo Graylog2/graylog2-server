098d7394508f8dc2ab4ee0d9a2e6c86791b34f50
Marco Pfatschbacher <marco@graylog.com>
2023-03-08 15:37:35 +0100

    make the one argument doLaunch() method final
    Co-authored-by: Othello Maurer <othello@graylog.com>

--- graylog2-server/src/main/java/org/graylog2/plugin/inputs/transports/ThrottleableTransport2.java
+++ graylog2-server/src/main/java/org/graylog2/plugin/inputs/transports/ThrottleableTransport2.java
@@ -44,7 +44,7 @@ public abstract class ThrottleableTransport2 extends ThrottleableTransport {
     }
 
     @Override
-    protected void doLaunch(MessageInput input) throws MisfireException {
+    protected final void doLaunch(MessageInput input) throws MisfireException {
         throw new MisfireException("Launch without InputFailureTracker");
     }
     protected abstract void doLaunch(MessageInput input, InputFailureRecorder inputFailureRecorder) throws MisfireException;
