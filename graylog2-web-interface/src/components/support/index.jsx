93f76d3ffd69edd584da2f82aa38bc871e6bed82
Linus Pahl <linus@graylog.com>
2023-03-09 13:25:16 +0100

    Improve styling of documentation links on system overview page. (#14772)
    * Improve styling of documentation links on system overview page.
    
    * Fix title of documentation link.

--- graylog2-web-interface/src/components/support/index.jsx
+++ graylog2-web-interface/src/components/support/index.jsx
@@ -16,5 +16,4 @@
  */
 export { default as ContactUs } from './ContactUs';
 export { default as DocumentationLink } from './DocumentationLink';
-export { default as SmallSupportLink } from './SmallSupportLink';
 export { default as SupportLink } from './SupportLink';
