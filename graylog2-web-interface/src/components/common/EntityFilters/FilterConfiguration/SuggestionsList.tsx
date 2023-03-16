c93e810e64ef2c3da7c56762f58cccfe3c4fec48
Linus Pahl <linus@graylog.com>
2023-03-16 13:50:25 +0100

    Allow selecting multiple filters.

--- graylog2-web-interface/src/components/common/EntityFilters/FilterConfiguration/SuggestionsList.tsx
+++ graylog2-web-interface/src/components/common/EntityFilters/FilterConfiguration/SuggestionsList.tsx
@@ -72,8 +72,6 @@ const SuggestionsList = ({ attribute, filterValueRenderer, onSubmit, allActiveFi
 
   const debounceOnSearch = debounce((value: string) => handleSearchChange(value), 1000);
 
-  console.log({ allActiveFilters });
-
   return (
     <Container>
       <SearchInput type="text"
