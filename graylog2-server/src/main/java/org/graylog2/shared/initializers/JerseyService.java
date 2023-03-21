60f93ed23b980f7eebf4eb2124e2334ccdbe272b
Maxwell <98284293+kodjo-anipah@users.noreply.github.com>
2023-03-21 14:50:12 +0100

    add content type option filter (#14987)
    * add content type option filter
    
    * add changelog
    
    * add license decleration

--- graylog2-server/src/main/java/org/graylog2/shared/initializers/JerseyService.java
+++ graylog2-server/src/main/java/org/graylog2/shared/initializers/JerseyService.java
@@ -48,6 +48,7 @@ import org.graylog2.plugin.rest.PluginRestResource;
 import org.graylog2.rest.MoreMediaTypes;
 import org.graylog2.rest.filter.WebAppNotFoundResponseFilter;
 import org.graylog2.shared.rest.CORSFilter;
+import org.graylog2.shared.rest.ContentTypeOptionFilter;
 import org.graylog2.shared.rest.EmbeddingControlFilter;
 import org.graylog2.shared.rest.NodeIdResponseFilter;
 import org.graylog2.shared.rest.NotAuthorizedResponseFilter;
@@ -263,7 +264,8 @@ public class JerseyService extends AbstractIdleService {
                         XHRFilter.class,
                         NotAuthorizedResponseFilter.class,
                         WebAppNotFoundResponseFilter.class,
-                        EmbeddingControlFilter.class)
+                        EmbeddingControlFilter.class,
+                        ContentTypeOptionFilter.class)
                 // Replacing this with a lambda leads to missing subtypes - https://github.com/Graylog2/graylog2-server/pull/10617#discussion_r630236360
                 .register(new ContextResolver<ObjectMapper>() {
                     @Override
