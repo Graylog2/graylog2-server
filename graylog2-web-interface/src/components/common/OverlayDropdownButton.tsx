3601f5e8e88455294b5bf8927ae37b0ae40cf6b4
Linus Pahl <linus@graylog.com>
2023-03-23 10:04:39 +0100

    Fix overflow of interval range dropdown in aggregation builder. (#14934)
    * Fix overflow of interval range dropdown in aggregation builder.
    
    * Implement `OverlayDropdownButton` for `Time` component to fix horizontal scrollbar.
    
    * Adding changelog.
    
    * Adding comment
    
    * Move changelog file to correct directory.

---
+++ graylog2-web-interface/src/components/common/OverlayDropdownButton.tsx
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import { useState } from 'react';
+
+import Button from 'components/bootstrap/Button';
+import OverlayDropdown from 'components/common/OverlayDropdown';
+
+type Props = {
+  bsSize?: string,
+  buttonTitle?: string,
+  children: React.ReactNode | ((payload: { toggleDropdown: () => void }) => React.ReactNode),
+  closeOnSelect?: boolean,
+  disabled?: boolean
+  dropdownMinWidth?: number
+  dropdownZIndex?: number,
+  onToggle?: () => void,
+  title: React.ReactNode,
+};
+
+/**
+ * This component is an alternative to the `DropdownButton` component and displays the dropdown in a portal.
+ */
+const OverlayDropdownButton = ({
+  bsSize,
+  buttonTitle,
+  children,
+  closeOnSelect,
+  disabled,
+  dropdownMinWidth,
+  dropdownZIndex,
+  onToggle: onToggleProp,
+  title,
+}: Props) => {
+  const [show, setShowDropdown] = useState(false);
+
+  const _onToggle = () => {
+    if (typeof onToggleProp === 'function') {
+      onToggleProp();
+    }
+
+    setShowDropdown((cur) => !cur);
+  };
+
+  return (
+    <OverlayDropdown show={show}
+                     closeOnSelect={closeOnSelect}
+                     dropdownZIndex={dropdownZIndex}
+                     dropdownMinWidth={dropdownMinWidth}
+                     renderToggle={({ onToggle, toggleTarget }) => (
+                       <div className={`dropdown btn-group ${show ? 'open' : ''}`}>
+                         <Button bsSize={bsSize}
+                                 className="dropdown-toggle"
+                                 ref={toggleTarget}
+                                 title={buttonTitle}
+                                 disabled={disabled}
+                                 onClick={onToggle}>
+                           {title} <span className="caret" />
+                         </Button>
+                       </div>
+                     )}
+                     placement="bottom"
+                     onToggle={_onToggle}>
+      {typeof children === 'function' ? children({ toggleDropdown: _onToggle }) : children}
+    </OverlayDropdown>
+  );
+};
+
+OverlayDropdownButton.defaultProps = {
+  bsSize: undefined,
+  buttonTitle: undefined,
+  closeOnSelect: true,
+  disabled: false,
+  dropdownMinWidth: undefined,
+  dropdownZIndex: undefined,
+  onToggle: undefined,
+};
+
+export default OverlayDropdownButton;
