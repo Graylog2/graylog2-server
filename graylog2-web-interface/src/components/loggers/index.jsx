b9922bddadb32ba6a23cd60b321fcd4963f3fa02
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2023-03-23 16:14:11 +0100

    fix linter issues

--- graylog2-web-interface/src/components/loggers/index.jsx
+++ graylog2-web-interface/src/components/loggers/index.jsx
@@ -14,6 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
+/* eslint-disable import/no-cycle */
 export { default as LoggerOverview } from './LoggerOverview';
 export { default as LoggingSubsystem } from './LoggingSubsystem';
 export { default as LogLevelDropdown } from './LogLevelDropdown';
--- graylog2-web-interface/src/pages/LoggersPage.jsx
+++ graylog2-web-interface/src/pages/LoggersPage.jsx
@@ -19,24 +19,22 @@ import React from 'react';
 import { DocumentTitle, PageHeader } from 'components/common';
 import { LoggerOverview, ClusterSupportBundleOverview } from 'components/loggers';
 
-class LoggersPage extends React.Component {
-  render() {
-    return (
-      <DocumentTitle title="Logging">
-        <span>
-          <PageHeader title="Logging">
-            <span>
-              This section controls logging of the Graylog architecture and allows you to change log
-              levels on the fly. Note that log levels are reset to their defaults after you restart
-              the affected service.
-            </span>
-          </PageHeader>
-          <LoggerOverview />
-          <ClusterSupportBundleOverview />
-        </span>
-      </DocumentTitle>
-    );
-  }
-}
+const LoggersPage = () => {
+  return (
+    <DocumentTitle title="Logging">
+      <span>
+        <PageHeader title="Logging">
+          <span>
+            This section controls logging of the Graylog architecture and allows you to change log
+            levels on the fly. Note that log levels are reset to their defaults after you restart
+            the affected service.
+          </span>
+        </PageHeader>
+        <LoggerOverview />
+        <ClusterSupportBundleOverview />
+      </span>
+    </DocumentTitle>
+  );
+};
 
 export default LoggersPage;
