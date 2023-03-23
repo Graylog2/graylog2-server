7d54f593f08a878623d11e6a01bc2ed98e595a60
Laura Bergenthal-Grotlüschen <laura.bergenthalgrotlueschen@graylog.com>
2023-03-23 16:53:10 +0100

    Disable eslint warning for error boundary

--- graylog2-web-interface/src/pages/configurations/ConfigletContainer.tsx
+++ graylog2-web-interface/src/pages/configurations/ConfigletContainer.tsx
@@ -40,6 +40,7 @@ type BoundaryProps = {
 }
 
 const Boundary = ({ children, title }: BoundaryProps) => (
+  // eslint-disable-next-line react/no-unstable-nested-components
   <ErrorBoundary FallbackComponent={(props) => <ErrorFallback title={title} {...props} />}>
     {children}
   </ErrorBoundary>
