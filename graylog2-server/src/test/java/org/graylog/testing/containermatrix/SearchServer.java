c162aa88b10d4af6a0c9a18fd9417cf04ed8ccf3
Jan Heise <jan.heise@graylog.com>
2023-03-28 17:29:20 +0200

    run matrix tests against 2.6.0

--- graylog2-server/src/test/java/org/graylog/testing/containermatrix/SearchServer.java
+++ graylog2-server/src/test/java/org/graylog/testing/containermatrix/SearchServer.java
@@ -25,9 +25,9 @@ public enum SearchServer {
     ES7(ELASTICSEARCH, "7.10.2"),
     OS1(OPENSEARCH, "1.3.1"),
     OS2(OPENSEARCH, "2.0.1"),
-    OS2_LATEST(OPENSEARCH, "2.4.0");
+    OS2_LATEST(OPENSEARCH, "2.6.0");
 
-    public static final SearchServer DEFAULT_VERSION = OS1;
+    public static final SearchServer DEFAULT_VERSION = OS2_LATEST;
     public static final SearchServer DEFAULT_OPENSEARCH_VERSION = OS2_LATEST;
 
     private final SearchVersion searchVersion;
