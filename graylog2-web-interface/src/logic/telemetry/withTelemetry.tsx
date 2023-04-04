1b26457bd571b8731f422d61196f3301e3fecff1
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2023-04-04 15:38:41 +0200

    added withTelemetry HOC

---
+++ graylog2-web-interface/src/logic/telemetry/withTelemetry.tsx
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+
+import useSendTelemetry from './useSendTelemetry';
+
+const withTelemetry = <C extends React.ComponentType<React.ComponentProps<C>>>(Component: C) => {
+  return function WrappedComponent(props: any) {
+    const sendTelemetry = useSendTelemetry();
+
+    return <Component {...props} sendTelemetry={sendTelemetry} />;
+  };
+};
+
+export default withTelemetry;
