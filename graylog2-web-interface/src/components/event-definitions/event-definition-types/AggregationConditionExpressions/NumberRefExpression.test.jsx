c8665cd1ddfc1118ec9f9abe6c08ee17282225b2
Linus Pahl <linus@graylog.com>
2023-04-05 10:31:44 +0200

    Fixing tests.

--- graylog2-web-interface/src/components/event-definitions/event-definition-types/AggregationConditionExpressions/NumberRefExpression.test.jsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-types/AggregationConditionExpressions/NumberRefExpression.test.jsx
@@ -16,6 +16,7 @@
  */
 import React from 'react';
 import { mount } from 'wrappedEnzyme';
+import { act } from 'react-dom/test-utils';
 
 import NumberRefExpression from './NumberRefExpression';
 
@@ -113,7 +114,9 @@ describe('NumberRefExpression', () => {
 
     const functionSelect = wrapper.find('Select Select.aggregation-function').at(0);
 
-    functionSelect.prop('onChange')({ value: 'card' });
+    act(() => {
+      functionSelect.prop('onChange')({ value: 'card' });
+    });
 
     expect(handleChange.mock.calls.length).toBe(1);
   });
@@ -146,7 +149,9 @@ describe('NumberRefExpression', () => {
 
     const fieldSelect = wrapper.find('Select Select.aggregation-function-field').at(0);
 
-    fieldSelect.prop('onChange')({ value: 'source' }, { action: 'select-option' });
+    act(() => {
+      fieldSelect.prop('onChange')({ value: 'source' }, { action: 'select-option' });
+    });
 
     expect(handleChange.mock.calls.length).toBe(1);
   });
@@ -179,7 +184,9 @@ describe('NumberRefExpression', () => {
 
     const fieldSelect = wrapper.find('Select Select.aggregation-function-field').at(0);
 
-    fieldSelect.prop('onChange')({ value: 'source' }, { action: 'select-option' });
+    act(() => {
+      fieldSelect.prop('onChange')({ value: 'source' }, { action: 'select-option' });
+    });
 
     expect(handleChange.mock.calls.length).toBe(1);
   });
