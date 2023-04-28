c9ac47751336552b7919757ac7324510fff3fb32
Bernd Ahlers <bernd@graylog.com>
2023-04-28 19:26:36 +0200

    Fix missing import in GelfOutputTest

--- graylog2-server/src/test/java/org/graylog2/outputs/GelfOutputTest.java
+++ graylog2-server/src/test/java/org/graylog2/outputs/GelfOutputTest.java
@@ -32,6 +32,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GelfOutputTest {
     @Test
