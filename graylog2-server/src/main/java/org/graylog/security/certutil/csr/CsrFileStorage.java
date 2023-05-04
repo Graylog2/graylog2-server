ca386fc4b4722ad054c1c27b4dfbfbe6486aa1bd
luk-kaminski <lukasz.kaminski@graylog.com>
2023-05-04 08:30:25 +0200

    CSR command with code generating sample CSR (#15210)
    * CSR command with code generating sample CSR
    
    * Charset in FileWriter

---
+++ graylog2-server/src/main/java/org/graylog/security/certutil/csr/CsrFileStorage.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.security.certutil.csr;
+
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.security.Security;
+
+public record CsrFileStorage(String csrFilename) implements CsrStorage {
+
+    static {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+    }
+
+    @Override
+    public void writeCsr(PKCS10CertificationRequest csr)
+            throws IOException, OperatorCreationException {
+
+        try (JcaPEMWriter jcaPEMWriter = new JcaPEMWriter(new FileWriter(csrFilename, Charset.defaultCharset()))) {
+            jcaPEMWriter.writeObject(csr);
+        }
+    }
+
+    @Override
+    public PKCS10CertificationRequest readCsr()
+            throws IOException, OperatorCreationException {
+
+        Reader pemReader = new BufferedReader(new FileReader(csrFilename, Charset.defaultCharset()));
+        PEMParser pemParser = new PEMParser(pemReader);
+        Object parsedObj = pemParser.readObject();
+        if (parsedObj instanceof PKCS10CertificationRequest) {
+            return (PKCS10CertificationRequest) parsedObj;
+        }
+        return null;
+    }
+}
