62fa69d1d776e8d691c07646b09c47f9f82aa69a
Jan Heise <jan.heise@graylog.com>
2023-05-08 17:08:38 +0200

    removed unnecessary resets

--- graylog2-server/src/test/java/org/graylog/events/search/MoreSearchAdapterIT.java
+++ graylog2-server/src/test/java/org/graylog/events/search/MoreSearchAdapterIT.java
@@ -44,9 +44,6 @@ public abstract class MoreSearchAdapterIT extends ElasticsearchBaseTest {
 
     @Before
     public void setUp() throws Exception {
-        // reset possible block from other tests
-        client().resetClusterBlock();
-
         toTest = createMoreSearchAdapter();
         importFixture("org/graylog/events/search/more_search_adapter.json");
     }
