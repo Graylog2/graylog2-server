6ca2d0132ba26d61f31e5ab13eb01681ebfa2df4
Linus Pahl <linus@graylog.com>
2023-05-11 15:39:06 +0200

    Adding missing license header.

--- graylog2-web-interface/src/preflight/hooks/useConfigurationStep.test.tsx
+++ graylog2-web-interface/src/preflight/hooks/useConfigurationStep.test.tsx
@@ -1,5 +1,21 @@
 import { renderHook } from 'wrappedTestingLibrary/hooks';
 
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 import useDataNodesCA from 'preflight/hooks/useDataNodesCA';
 import { CONFIGURATION_STEPS } from 'preflight/Constants';
 import asMock from 'helpers/mocking/AsMock';
