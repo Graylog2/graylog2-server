150a29ac355f132ab7884dcdbbc14838ebe03ece
Maxwell Anipah <maxwell.anipah@graylog.com>
2023-05-12 13:52:00 +0200

    add changelog and license headers

--- graylog2-server/src/test/java/org/graylog/plugins/pipelineprocessor/simulator/RuleSimulatorTest.java
+++ graylog2-server/src/test/java/org/graylog/plugins/pipelineprocessor/simulator/RuleSimulatorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 package org.graylog.plugins.pipelineprocessor.simulator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -36,11 +52,11 @@ class RuleSimulatorTest {
     @Test
     void createMessageFromJson() {
         String jsonMessage = """
-        {
-            "message": "This is a test message",
-            "additionalField": "this is an additional field passed"
-        }
-        """;
+                {
+                    "message": "This is a test message",
+                    "additionalField": "this is an additional field passed"
+                }
+                """;
         Message result = ruleSimulator.createMessage(jsonMessage);
         Map<String, Object> fields = result.getFields();
         Assertions.assertEquals("This is a test message", fields.get("message"));
