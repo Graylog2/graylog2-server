5feea86fc5b13e474314db0b1f2b067d739d9121
luk-kaminski <lukasz.kaminski@graylog.com>
2023-05-15 13:57:12 +0200

    Alpha version of CSR storage in Mongo DB (#15493)

--- graylog2-server/src/main/java/org/graylog2/cluster/NodePreflightConfigService.java
+++ graylog2-server/src/main/java/org/graylog2/cluster/NodePreflightConfigService.java
@@ -16,29 +16,38 @@
  */
 package org.graylog2.cluster;
 
+import org.graylog.security.certutil.csr.CsrGenerator;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.MongoConnection;
 import org.graylog2.database.PaginatedDbService;
 import org.mongojack.DBQuery;
+import org.mongojack.DBUpdate;
 import org.mongojack.JacksonDBCollection;
+import org.mongojack.WriteResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 
+import static org.graylog2.cluster.NodePreflightConfig.FIELD_CSR;
 import static org.graylog2.cluster.NodePreflightConfig.FIELD_NODEID;
+import static org.graylog2.cluster.NodePreflightConfig.FIELD_STATE;
+import static org.graylog2.cluster.NodePreflightConfig.State.CSR;
 
 public class NodePreflightConfigService extends PaginatedDbService<NodePreflightConfig> {
     public static final String COLLECTION_NAME = "node_preflight_config";
 
     private static final Logger LOG = LoggerFactory.getLogger(NodePreflightConfigService.class);
     private final JacksonDBCollection<NodePreflightConfig, String> dbCollection;
+    private final CsrGenerator csrGenerator;
 
     @Inject
     public NodePreflightConfigService(final MongoJackObjectMapperProvider mongoJackObjectMapperProvider,
-                                      final MongoConnection mongoConnection) {
+                                      final MongoConnection mongoConnection,
+                                      final CsrGenerator csrGenerator) {
         super(mongoConnection, mongoJackObjectMapperProvider, NodePreflightConfig.class, COLLECTION_NAME);
         this.dbCollection = JacksonDBCollection.wrap(mongoConnection.getDatabase().getCollection(COLLECTION_NAME), NodePreflightConfig.class, String.class, mongoJackObjectMapperProvider.get());
+        this.csrGenerator = csrGenerator;
     }
 
     public NodePreflightConfig getPreflightConfigFor(Node node) {
@@ -47,6 +56,44 @@ public class NodePreflightConfigService extends PaginatedDbService<NodePreflight
 
     public NodePreflightConfig getPreflightConfigFor(String nodeId) {
         return dbCollection.findOne(DBQuery.is(FIELD_NODEID, nodeId));
+    }
+
+    //TODO: To be decided by Jan if something is worth merging into his DataNodePreflightGeneratePeriodical
+//    public void updatePreflightConfig(final NodeId nodeId) {
+//        final NodePreflightConfig preflightConfigFor = getPreflightConfigFor(nodeId.getNodeId());
+//        if (preflightConfigFor == null) {
+//            dbCollection.insert(NodePreflightConfig.builder()
+//                    .nodeId(nodeId.getNodeId())
+//                    .state(NodePreflightConfig.State.UNCONFIGURED)
+//                    .build());
+//        } else if (Objects.equals(preflightConfigFor.state(), NodePreflightConfig.State.CONFIGURED) && preflightConfigFor.csr() == null) {
+//            try {
+//                //TODO: most of the parameters are hardcoded, so that we can just check if CSR is generated and stored in Mongo
+//                //TODO: the details will be changed in one of the next PRs
+//                final PKCS10CertificationRequest csr = csrGenerator.generateCSR(
+//                        "changeMe".toCharArray(),
+//                        "localhost",
+//                        List.of("data-node"),
+//                        new PrivateKeyEncryptedFileStorage("tbd.key"));
+//                final CsrStorage csrStorage = new CsrMongoStorage(this, nodeId);
+//                csrStorage.writeCsr(csr);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    public boolean writeCsr(final String nodeId,
+                            final String csr) {
+        final WriteResult<NodePreflightConfig, String> result = dbCollection.update(
+                DBQuery.is(FIELD_NODEID, nodeId),
+                new DBUpdate.Builder()
+                        .set(FIELD_CSR, csr)
+                        .set(FIELD_STATE, CSR),
+                false,
+                false);
+
+        return result.getN() > 0;
     }
 
     public void deleteAll() {
