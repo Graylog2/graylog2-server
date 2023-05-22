6042e740be2e4f52bb160b2c6d7e4daab49813e4
Laura Bergenthal-Grotlüschen <laura.bergenthalgrotlueschen@graylog.com>
2023-05-22 10:44:01 +0200

    Add styles for rule display

--- graylog2-web-interface/src/components/rules/rule-builder/RuleBlockDisplay.tsx
+++ graylog2-web-interface/src/components/rules/rule-builder/RuleBlockDisplay.tsx
@@ -15,8 +15,9 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import React from 'react';
+import styled from 'styled-components';
 
-import { Button, Col, Row } from 'components/bootstrap';
+import { Button, ButtonToolbar, Col, Row } from 'components/bootstrap';
 
 import type { BlockDict, RuleBlock } from './types';
 
@@ -27,39 +28,75 @@ type Props = {
   onEdit: () => void,
 }
 
-const RuleBlockDisplay = ({ block, blockDict, onEdit, onDelete }:Props) => (
-  <Row>
-    <Col md={12}>
-      <Row>
-        <Col md={12}>
-          <h3>{blockDict?.rule_builder_title || blockDict?.name}</h3>
-        </Col>
-      </Row>
-      {Object.keys(block.params).map((paramName, key) => {
-        const paramValue = block.params[paramName];
-        const paramValueExists = paramValue && paramValue !== '' && paramValue !== null;
+const BlockInfo = styled(Row)`
+  margin-bottom: 20px;
+`;
 
-        if (paramValueExists) {
-          return (
-            // eslint-disable-next-line react/no-array-index-key
-            <Row key={key}>
-              <Col md={12}>
-                <p><strong>{paramName}:</strong> {paramValue}</p>
-              </Col>
-            </Row>
-          );
-        }
+const ActionsRow = styled(Row)`
+  margin-top: 20px;
+`;
 
-        return null;
-      })}
-      <Row>
-        <Col md={12}>
-          <Button bsSize="small" onClick={onEdit}>Edit</Button>
-          <Button bsSize="small" onClick={onDelete}>Delete</Button>
-        </Col>
-      </Row>
-    </Col>
-  </Row>
-);
+const ParamsRow = styled(Row)`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 50%;
+`;
+
+const ParamCol = styled(Col)`
+  padding-right: 15px;
+  padding-left: 15px;
+`;
+
+const RuleBlockDisplay = ({ block, blockDict, onEdit, onDelete }:Props) => {
+  const paramNames = Object.keys(block.params);
+
+  const paramValueExists = (paramValue: string | number | boolean) : boolean => (
+    paramValue && paramValue !== '' && paramValue !== null
+  );
+
+  const anyParamsSet = () : boolean => (
+    paramNames.some((paramName) => paramValueExists(block.params[paramName]))
+  );
+
+  return (
+    <Row>
+      <Col md={12}>
+        <BlockInfo>
+          <Col md={12}>
+            <h3>{blockDict?.rule_builder_title || blockDict?.name}</h3>
+          </Col>
+        </BlockInfo>
+        {anyParamsSet
+        && (
+        <ParamsRow>
+          {paramNames.map((paramName, key) => {
+            const paramValue = block.params[paramName];
+
+            if (paramValueExists(paramValue)) {
+              return (
+              // eslint-disable-next-line react/no-array-index-key
+                <ParamCol key={key} sm="auto" md="auto" lg="auto">
+                  <p><strong>{paramName}:</strong> {paramValue}</p>
+                </ParamCol>
+              );
+            }
+
+            return null;
+          })}
+        </ParamsRow>
+        )}
+
+        <ActionsRow>
+          <Col md={12}>
+            <ButtonToolbar>
+              <Button bsStyle="success" bsSize="small" onClick={onEdit}>Edit</Button>
+              <Button bsSize="small" onClick={onDelete}>Delete</Button>
+            </ButtonToolbar>
+          </Col>
+        </ActionsRow>
+      </Col>
+    </Row>
+  );
+};
 
 export default RuleBlockDisplay;
