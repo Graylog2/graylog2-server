df800d82c9f146835ae201b54dd048117beeb1f6
Marco Pfatschbacher <marco@graylog.com>
2023-05-31 15:10:37 +0200

    Add more inject dependencies to the forbidden-apis check

--- graylog2-server/src/main/java/org/graylog2/database/dbcatalog/DbEntitiesScanner.java
+++ graylog2-server/src/main/java/org/graylog2/database/dbcatalog/DbEntitiesScanner.java
@@ -17,7 +17,6 @@
 package org.graylog2.database.dbcatalog;
 
 import com.google.common.base.Stopwatch;
-import com.google.inject.Provider;
 import org.graylog2.database.dbcatalog.impl.ClassGraphDbEntitiesScanningMethod;
 import org.graylog2.database.dbcatalog.impl.DbEntitiesScanningMethod;
 import org.graylog2.shared.plugins.ChainingClassLoader;
@@ -25,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
 public class DbEntitiesScanner implements Provider<DbEntitiesCatalog> {
 
