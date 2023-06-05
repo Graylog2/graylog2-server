be3bd3519ed219ef902d152663711ad3513d3a76
Tomas Dvorak <tomas.dvorak@graylog.com>
2023-06-05 16:15:31 +0200

    code cleanup

--- data-node/src/main/java/org/graylog/datanode/management/OpensearchProcessService.java
+++ data-node/src/main/java/org/graylog/datanode/management/OpensearchProcessService.java
@@ -27,7 +27,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 
