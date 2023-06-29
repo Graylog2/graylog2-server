bc35572ebbd10e2af732a802885fa85eb4ce3058
Lukasz Kaminski <lukasz.kaminski@graylog.com>
2023-06-29 10:27:37 +0200

    Added explanation in comment

--- full-backend-tests/src/test/java/org/graylog/plugins/views/StartPageLastOpenedIT.java
+++ full-backend-tests/src/test/java/org/graylog/plugins/views/StartPageLastOpenedIT.java
@@ -56,10 +56,13 @@ public class StartPageLastOpenedIT {
         api.users().createUser(user);
     }
 
+    //This test has to be the only test in this class.
+    //Because of the memoization in Catalog, we cannot guarantee that after search/dashboard creation it will be immediately visible in the catalog.
+    //Keeping this test independent prevents us from using unnecessary sleep of a few seconds.
     @ContainerMatrixTest
     void testCreateLastOpenedItem() {
-        api.postWithResource("/views/search", user,"org/graylog/plugins/views/startpage-save-search-request.json", 201);
-        api.postWithResource("/views", user,"org/graylog/plugins/views/startpage-views-request.json", 200);
+        api.postWithResource("/views/search", user, "org/graylog/plugins/views/startpage-save-search-request.json", 201);
+        api.postWithResource("/views", user, "org/graylog/plugins/views/startpage-views-request.json", 200);
 
         var validatableResponse = api.get("/views", user, Map.of(), 200);
         var id = validatableResponse.extract().jsonPath().get("views[0].id").toString();
