18519b9fdd1f37afbbbb7970598a5ec28e7a320f
Matthias Oesterheld <matthias.oesterheld@graylog.com>
2023-07-14 09:58:46 +0200

    set rulebuilder descriptor and primary param

--- graylog2-server/src/main/java/org/graylog/plugins/pipelineprocessor/rulebuilder/db/migrations/V20220522125200_AddSetGrokToFieldsExtractorFragments.java
+++ graylog2-server/src/main/java/org/graylog/plugins/pipelineprocessor/rulebuilder/db/migrations/V20220522125200_AddSetGrokToFieldsExtractorFragments.java
@@ -55,7 +55,7 @@ public class V20220522125200_AddSetGrokToFieldsExtractorFragments extends Migrat
         log.debug("Adding set_grok_to_fields extractor fragments via migration");
         if (Objects.nonNull(clusterConfigService.get(MigrationCompleted.class))) {
             log.debug("Migration already completed!");
-            return;
+//            return;
 
         }
 
