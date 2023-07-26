1f6d605d589cfb862f029333ff15a5efc406e547
Linus Pahl <linus@graylog.com>
2023-07-26 16:40:42 +0200

    Migrate `SystemOutputsPage` component to TS.

--- graylog2-web-interface/src/pages/SystemOutputsPage.tsx
+++ graylog2-web-interface/src/pages/SystemOutputsPage.tsx
@@ -15,6 +15,7 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import React from 'react';
+
 import { DocumentTitle, PageHeader } from 'components/common';
 import OutputsComponent from 'components/outputs/OutputsComponent';
 import useCurrentUser from 'hooks/useCurrentUser';
