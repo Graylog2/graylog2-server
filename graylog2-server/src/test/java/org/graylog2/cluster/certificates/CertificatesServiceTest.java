3dc3a9dd7051a998bd0c1836c50c411262243892
Jan Heise <jan.heise@graylog.com>
2023-07-27 16:09:21 +0200

    added Test

--- graylog2-server/src/test/java/org/graylog2/cluster/certificates/CertificatesServiceTest.java
+++ graylog2-server/src/test/java/org/graylog2/cluster/certificates/CertificatesServiceTest.java
@@ -91,5 +91,28 @@ public class CertificatesServiceTest {
         ));
     }
 
+    @Test
+    public void testCertRemoval() {
+        certificatesService.writeCert(new KeystoreMongoLocation("node_id_4", DATA_NODE_KEYSTORE_COLLECTION), "Certificate string representation 1");
+        certificatesService.writeCert(new KeystoreMongoLocation("node_id_5", DATA_NODE_KEYSTORE_COLLECTION), "Certificate string representation 2");
+
+        assertTrue(certificatesService.hasCert(
+                new KeystoreMongoLocation("node_id_4", DATA_NODE_KEYSTORE_COLLECTION)
+        ));
+
+        assertTrue(certificatesService.hasCert(
+                new KeystoreMongoLocation("node_id_5", DATA_NODE_KEYSTORE_COLLECTION)
+        ));
+
+        certificatesService.removeCert(new KeystoreMongoLocation("node_id_4", DATA_NODE_KEYSTORE_COLLECTION));
+
+        assertFalse(certificatesService.hasCert(
+                new KeystoreMongoLocation("node_id_4", DATA_NODE_KEYSTORE_COLLECTION)
+        ));
+
+        assertTrue(certificatesService.hasCert(
+                new KeystoreMongoLocation("node_id_5", DATA_NODE_KEYSTORE_COLLECTION)
+        ));
+    }
 
 }
