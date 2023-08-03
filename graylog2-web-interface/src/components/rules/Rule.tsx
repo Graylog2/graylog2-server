752fdd65974e8ca2ff0081268f1a6c0c35bab65a
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2023-08-03 12:25:20 +0200

    converted Rule.jsx to typescript

--- graylog2-web-interface/src/components/rules/Rule.tsx
+++ graylog2-web-interface/src/components/rules/Rule.tsx
@@ -27,7 +27,13 @@ import RuleHelper from './rule-helper/RuleHelper';
 
 import PipelinesPageNavigation from '../pipelines/PipelinesPageNavigation';
 
-const Rule = ({ create, title, isRuleBuilder }) => {
+type Props = {
+  create: boolean,
+  title: string,
+  isRuleBuilder: boolean,
+};
+
+const Rule = ({ create, title, isRuleBuilder }: Props) => {
   let pageTitle;
 
   if (create) {
