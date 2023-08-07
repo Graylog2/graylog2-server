004befea3a1356ca9ef5fcead390580c64d0bcf2
Jan Heise <jan.heise@graylog.com>
2023-08-07 15:23:06 +0200

    fix license

--- graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/CertificateRenewalResource.java
+++ graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/CertificateRenewalResource.java
@@ -20,24 +20,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import org.graylog.security.certutil.CertRenewalService;
 import org.graylog2.audit.jersey.NoAuditEvent;
-import org.graylog2.bootstrap.preflight.PreflightConstants;
 import org.graylog2.cluster.Node;
-/*
- * Copyright (C) 2020 Graylog, Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
- *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
- */
 import org.graylog2.cluster.NodeService;
 import org.graylog2.plugin.rest.PluginRestResource;
 import org.graylog2.utilities.uri.TransportAddressSanitizer;
