a5efe17316985880a85835adb095b0f17a542e3e
Dan Torrey <dan@graylog.com>
2023-08-11 09:27:57 -0500

    Add new GIM Schema fields

--- graylog2-server/src/main/java/org/graylog/schema/VendorFields.java
+++ graylog2-server/src/main/java/org/graylog/schema/VendorFields.java
@@ -21,6 +21,7 @@ public class VendorFields {
     public static final String VENDOR_ALERT_SEVERITY_LEVEL = "vendor_alert_severity_level";
     public static final String VENDOR_AUTHENTICATION_PROVIDER = "vendor_authentication_provider";
     public static final String VENDOR_CREDENTIAL_TYPE = "vendor_credential_type";
+    public static final String VENDOR_CUSTOMER_ID = "vendor_customer_id";
     public static final String VENDOR_EVENT_ACTION = "vendor_event_action";
     public static final String VENDOR_EVENT_DESCRIPTION = "vendor_event_description";
     public static final String VENDOR_EVENT_OUTCOME = "vendor_event_outcome";
@@ -37,4 +38,5 @@ public class VendorFields {
     public static final String VENDOR_TRANSACTION_ID = "vendor_transaction_id";
     public static final String VENDOR_TRANSACTION_TYPE = "vendor_transaction_type";
     public static final String VENDOR_USER_TYPE = "vendor_user_type";
+    public static final String VENDOR_VERSION = "vendor_version";
 }
