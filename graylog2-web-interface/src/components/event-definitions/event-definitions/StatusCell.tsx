46549ebf3b7f3e7dda00cb50f61a7575025c7255
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2023-08-18 13:01:13 +0200

    Enable/Disable EventDefinition status when clicking on status badge

--- graylog2-web-interface/src/components/event-definitions/event-definitions/StatusCell.tsx
+++ graylog2-web-interface/src/components/event-definitions/event-definitions/StatusCell.tsx
@@ -15,9 +15,14 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
+import { useCallback } from 'react';
 import styled from 'styled-components';
 
+import { EventDefinitionsActions } from 'stores/event-definitions/EventDefinitionsStore';
 import { Label } from 'components/bootstrap';
+import { Icon } from 'components/common';
+
+import type { EventDefinition } from '../event-definitions-types';
 
 const StatusLabel = styled(Label)`
   display: inline-flex;
@@ -25,16 +30,57 @@ const StatusLabel = styled(Label)`
   gap: 4px;
 `;
 
+const Spacer = styled.div`
+  border-left: 1px solid currentColor;
+  height: 1em;
+`;
+
+const _title = (disabled: boolean, disabledChange: boolean, description: string) => {
+  if (disabledChange) {
+    return description;
+  }
+
+  return disabled ? 'Enable' : 'Disable';
+};
+
 type Props ={
-  status: string
+  eventDefinition: EventDefinition,
+  refetchEventDefinitions: () => void,
 }
 
-const StatusCell = ({ status } : Props) => {
-  const isSuccess = status === 'ENABLED';
+const StatusCell = ({ eventDefinition, refetchEventDefinitions } : Props) => {
+  const isEnabled = eventDefinition?.state === 'ENABLED';
+  const disableChange = eventDefinition?.config?.type === 'system-notifications-v1';
+  const description = isEnabled ? 'enabled' : 'disabled';
+  const title = _title(!isEnabled, disableChange, description);
+
+  const toggleEventDefinitionStatus = useCallback(async () => {
+    // eslint-disable-next-line no-alert
+    if (isEnabled && window.confirm(`Do you really want to disable event definition '${eventDefinition.title}'?`)) {
+      await EventDefinitionsActions.disable(eventDefinition);
+      await refetchEventDefinitions();
+    }
+
+    if (!isEnabled) {
+      await EventDefinitionsActions.enable(eventDefinition);
+      await refetchEventDefinitions();
+    }
+  }, [isEnabled, eventDefinition, refetchEventDefinitions]);
 
   return (
-    <StatusLabel bsStyle={isSuccess ? 'success' : 'warning'}>
-      {isSuccess ? 'enabled' : 'disabled'}
+    <StatusLabel bsStyle={isEnabled ? 'success' : 'warning'}
+                 onClick={disableChange ? undefined : toggleEventDefinitionStatus}
+                 title={title}
+                 aria-label={title}
+                 role="button"
+                 $clickable={!disableChange}>
+      {description}
+      {!disableChange && (
+        <>
+          <Spacer />
+          <Icon name={isEnabled ? 'pause' : 'play'} />
+        </>
+      )}
     </StatusLabel>
   );
 };
