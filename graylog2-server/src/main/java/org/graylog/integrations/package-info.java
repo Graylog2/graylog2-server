7f468addf484343f21cb8ce1b8018e4f246d4a5d
Dennis Oelkers <dennis@graylog.com>
2023-08-29 16:51:24 +0200

    Adding comment about migration and legacy package name.

---
+++ graylog2-server/src/main/java/org/graylog/integrations/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the Integrations plugin, formerly located at https://github.com/Graylog2/graylog-plugin-integrations.
+ * <p>
+ * The plugin intentionally resides in the same legacy package that was used when it was in the separate repository and
+ * <em>not</em> underneath the {@code org.graylog.plugin} package. Changing the package would break API routes which are,
+ * based on the old package name.
+ */
+package org.graylog.integrations;
