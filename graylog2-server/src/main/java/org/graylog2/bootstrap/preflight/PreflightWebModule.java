98b3bd203f0227e1ce64b2665af38631bf9e56c1
Jan Heise <jan.heise@graylog.com>
2023-09-20 11:52:58 +0200

    fixing eventbus bindings for preflight

--- graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/PreflightWebModule.java
+++ graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/PreflightWebModule.java
@@ -16,6 +16,7 @@
  */
 package org.graylog2.bootstrap.preflight;
 
+import com.google.common.eventbus.EventBus;
 import com.google.common.util.concurrent.Service;
 import com.google.common.util.concurrent.ServiceManager;
 import com.google.inject.TypeLiteral;
@@ -37,10 +38,13 @@ import org.graylog2.cluster.NodeService;
 import org.graylog2.cluster.NodeServiceImpl;
 import org.graylog2.database.MongoConnection;
 import org.graylog2.events.ClusterEventBus;
+import org.graylog2.events.ClusterEventCleanupPeriodical;
+import org.graylog2.events.ClusterEventPeriodical;
 import org.graylog2.plugin.cluster.ClusterConfigService;
 import org.graylog2.plugin.inject.Graylog2Module;
 import org.graylog2.plugin.inputs.MessageInput;
 import org.graylog2.plugin.periodical.Periodical;
+import org.graylog2.shared.bindings.providers.EventBusProvider;
 import org.graylog2.shared.bindings.providers.ServiceManagerProvider;
 
 public class PreflightWebModule extends Graylog2Module {
@@ -67,12 +71,15 @@ public class PreflightWebModule extends Graylog2Module {
 
         Multibinder<Periodical> periodicalBinder = Multibinder.newSetBinder(binder(), Periodical.class);
         periodicalBinder.addBinding().to(GraylogCertificateProvisioningPeriodical.class);
+        periodicalBinder.addBinding().to(ClusterEventPeriodical.class);
+        periodicalBinder.addBinding().to(ClusterEventCleanupPeriodical.class);
 
         Multibinder<Service> serviceBinder = Multibinder.newSetBinder(binder(), Service.class);
         serviceBinder.addBinding().to(PreflightJerseyService.class);
         serviceBinder.addBinding().to(PreflightPeriodicalsService.class);
 
         bind(ClusterEventBus.class).toProvider(ClusterEventBusProvider.class).asEagerSingleton();
+        bind(EventBus.class).toProvider(EventBusProvider.class).asEagerSingleton();
 
         // needed for the ObjectMapperModule
         MapBinder.newMapBinder(binder(),
