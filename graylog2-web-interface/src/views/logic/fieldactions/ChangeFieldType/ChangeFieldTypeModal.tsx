5d1d41fbc742c3fe25a441477029e1ddaf379112
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2023-09-22 10:36:34 +0200

    Add telemetry to ChangeFieldTypeModal

--- graylog2-web-interface/src/views/logic/fieldactions/ChangeFieldType/ChangeFieldTypeModal.tsx
+++ graylog2-web-interface/src/views/logic/fieldactions/ChangeFieldType/ChangeFieldTypeModal.tsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React, { useMemo, useCallback, useState } from 'react';
+import React, { useMemo, useCallback, useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 
 import { Badge, BootstrapModalForm, Alert, Input } from 'components/bootstrap';
@@ -27,6 +27,7 @@ import useStream from 'components/streams/hooks/useStream';
 import { DocumentationLink } from 'components/support';
 import DocsHelper from 'util/DocsHelper';
 import { defaultCompare } from 'logic/DefaultCompare';
+import useSendTelemetry from 'logic/telemetry/useSendTelemetry';
 
 const StyledSelect = styled(Select)`
   width: 400px;
@@ -48,6 +49,7 @@ type Props = {
 }
 
 const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
+  const sendTelemetry = useSendTelemetry();
   const [rotated, setRotated] = useState(false);
   const [newFieldType, setNewFieldType] = useState(null);
   const { data: { fieldTypes }, isLoading: isOptionsLoading } = useFiledTypes();
@@ -70,18 +72,30 @@ const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
       newFieldType,
       rotated,
       field,
-    }).then(() => onClose());
-  }, [field, indexSetSelection, newFieldType, onClose, putFiledTypeMutation, rotated]);
+    }).then(() => {
+      sendTelemetry('form_submit', { app_pathname: 'search', app_action_value: 'change-field-type' });
+      onClose();
+    });
+  }, [field, indexSetSelection, newFieldType, onClose, putFiledTypeMutation, rotated, sendTelemetry]);
 
   const onChangeFieldType = useCallback((value: string) => {
     setNewFieldType(value);
   }, []);
 
+  useEffect(() => {
+    sendTelemetry('modal_open', { app_pathname: 'search', app_action_value: 'change-field-type' });
+  }, [sendTelemetry]);
+
+  const onCancel = useCallback(() => {
+    sendTelemetry('modal_close', { app_pathname: 'search', app_action_value: 'change-field-type' });
+    onClose();
+  }, [onClose, sendTelemetry]);
+
   return (
     <BootstrapModalForm title={<span>Change {field} field type <BetaBadge /></span>}
                         submitButtonText="Change field type"
                         onSubmitForm={onSubmit}
-                        onCancel={onClose}
+                        onCancel={onCancel}
                         show={show}
                         bsSize="large">
       <div>
