bd69ffecd5109f74fe3c10827858acb4d5b8d051
Dennis Oelkers <dennis@graylog.com>
2023-09-29 18:49:23 +0200

    Visual improvements for Field Type Change modal. (#16772)
    * Adding label, sorting options.
    
    * Adding "Beta Feature" badge.
    
    * Changing column title.
    
    * Adding mandatory id.
    
    * Render streams separated with commas.

--- graylog2-web-interface/src/views/logic/fieldactions/ChangeFieldType/ChangeFieldTypeModal.tsx
+++ graylog2-web-interface/src/views/logic/fieldactions/ChangeFieldType/ChangeFieldTypeModal.tsx
@@ -15,22 +15,29 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import React, { useMemo, useCallback, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+import { Badge, BootstrapModalForm, Alert, Input } from 'components/bootstrap';
 import { Select, Spinner } from 'components/common';
 import StreamLink from 'components/streams/StreamLink';
-import { BootstrapModalForm, Alert, Input } from 'components/bootstrap';
 import useFiledTypes from 'views/logic/fieldactions/ChangeFieldType/hooks/useFieldTypes';
 import IndexSetsTable from 'views/logic/fieldactions/ChangeFieldType/IndexSetsTable';
 import usePutFiledTypeMutation from 'views/logic/fieldactions/ChangeFieldType/hooks/useFieldTypeMutation';
 import useStream from 'components/streams/hooks/useStream';
 import { DocumentationLink } from 'components/support';
 import DocsHelper from 'util/DocsHelper';
+import { defaultCompare } from 'logic/DefaultCompare';
 
 const StyledSelect = styled(Select)`
   width: 400px;
   margin-bottom: 20px;
 `;
+
+const RedBadge = styled(Badge)(({ theme }) => css`
+  background-color: ${theme.colors.variant.light.danger};
+`);
+
+const BetaBadge = () => <RedBadge>Beta Feature</RedBadge>;
 
 const failureStreamId = '000000000000000000000004';
 
@@ -44,10 +51,12 @@ const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
   const [rotated, setRotated] = useState(false);
   const [newFieldType, setNewFieldType] = useState(null);
   const { data: { fieldTypes }, isLoading: isOptionsLoading } = useFiledTypes();
-  const fieldTypeOptions = useMemo(() => Object.entries(fieldTypes).map(([value, label]) => ({
-    value,
-    label,
-  })), [fieldTypes]);
+  const fieldTypeOptions = useMemo(() => Object.entries(fieldTypes)
+    .sort(([, label1], [, label2]) => defaultCompare(label1, label2))
+    .map(([value, label]) => ({
+      value,
+      label,
+    })), [fieldTypes]);
   const { data: failureStream, isFetching: failureStreamLoading } = useStream(failureStreamId);
 
   const [indexSetSelection, setIndexSetSelection] = useState<Array<string>>();
@@ -69,7 +78,7 @@ const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
   }, []);
 
   return (
-    <BootstrapModalForm title={`Change ${field} field type`}
+    <BootstrapModalForm title={<span>Change {field} field type <BetaBadge /></span>}
                         submitButtonText="Change field type"
                         onSubmitForm={onSubmit}
                         onCancel={onClose}
@@ -82,14 +91,16 @@ const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
           ingestion errors. It is recommended to enable <DocumentationLink page={DocsHelper.PAGES.INDEXER_FAILURES} displayIcon text="Failure Processing" /> and watch
           the {failureStreamLoading ? <Spinner /> : <StreamLink stream={failureStream} />} stream closely afterwards.
         </Alert>
-        <StyledSelect inputId="field_type"
-                      options={fieldTypeOptions}
-                      value={newFieldType}
-                      onChange={onChangeFieldType}
-                      placeholder="Select field type"
-                      disabled={isOptionsLoading}
-                      inputProps={{ 'aria-label': 'Select field type' }}
-                      required />
+        <Input label="New Field Type:" id="new-field-type">
+          <StyledSelect inputId="field_type"
+                        options={fieldTypeOptions}
+                        value={newFieldType}
+                        onChange={onChangeFieldType}
+                        placeholder="Select field type"
+                        disabled={isOptionsLoading}
+                        inputProps={{ 'aria-label': 'Select field type' }}
+                        required />
+        </Input>
         <Alert bsStyle="info">
           By default the type will be changed in all index sets of the current message/search. By expanding the next section, you can select for which index sets you would like to make the change.
         </Alert>
