6e71a0b3231a0deda8a5e181ce439d8c924c77de
Dennis Oelkers <dennis@graylog.com>
2023-09-29 18:49:23 +0200

    Visual improvements for Field Type Change modal. (#16772)
    * Adding label, sorting options.
    
    * Adding "Beta Feature" badge.
    
    * Changing column title.
    
    * Adding mandatory id.
    
    * Render streams separated with commas.

--- graylog2-web-interface/src/views/logic/fieldactions/ChangeFieldType/ChangeFieldTypeModal.tsx
+++ graylog2-web-interface/src/views/logic/fieldactions/ChangeFieldType/ChangeFieldTypeModal.tsx
@@ -14,24 +14,30 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React, { useMemo, useCallback, useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useMemo, useCallback, useState } from 'react';
+import styled, { css } from 'styled-components';
 
+import { Badge, BootstrapModalForm, Alert, Input } from 'components/bootstrap';
 import { Select, Spinner } from 'components/common';
 import StreamLink from 'components/streams/StreamLink';
-import { BootstrapModalForm, Alert, Input } from 'components/bootstrap';
 import useFiledTypes from 'views/logic/fieldactions/ChangeFieldType/hooks/useFieldTypes';
 import IndexSetsTable from 'views/logic/fieldactions/ChangeFieldType/IndexSetsTable';
 import usePutFiledTypeMutation from 'views/logic/fieldactions/ChangeFieldType/hooks/useFieldTypeMutation';
 import useStream from 'components/streams/hooks/useStream';
 import { DocumentationLink } from 'components/support';
 import DocsHelper from 'util/DocsHelper';
-import useSendTelemetry from 'logic/telemetry/useSendTelemetry';
+import { defaultCompare } from 'logic/DefaultCompare';
 
 const StyledSelect = styled(Select)`
   width: 400px;
   margin-bottom: 20px;
 `;
+
+const RedBadge = styled(Badge)(({ theme }) => css`
+  background-color: ${theme.colors.variant.light.danger};
+`);
+
+const BetaBadge = () => <RedBadge>Beta Feature</RedBadge>;
 
 const failureStreamId = '000000000000000000000004';
 
@@ -42,14 +48,15 @@ type Props = {
 }
 
 const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
-  const sendTelemetry = useSendTelemetry();
   const [rotated, setRotated] = useState(false);
   const [newFieldType, setNewFieldType] = useState(null);
   const { data: { fieldTypes }, isLoading: isOptionsLoading } = useFiledTypes();
-  const fieldTypeOptions = useMemo(() => Object.entries(fieldTypes).map(([value, label]) => ({
-    value,
-    label,
-  })), [fieldTypes]);
+  const fieldTypeOptions = useMemo(() => Object.entries(fieldTypes)
+    .sort(([, label1], [, label2]) => defaultCompare(label1, label2))
+    .map(([value, label]) => ({
+      value,
+      label,
+    })), [fieldTypes]);
   const { data: failureStream, isFetching: failureStreamLoading } = useStream(failureStreamId);
 
   const [indexSetSelection, setIndexSetSelection] = useState<Array<string>>();
@@ -63,30 +70,18 @@ const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
       newFieldType,
       rotated,
       field,
-    }).then(() => {
-      sendTelemetry('form_submit', { app_pathname: 'search', app_action_value: 'change-field-type' });
-      onClose();
-    });
-  }, [field, indexSetSelection, newFieldType, onClose, putFiledTypeMutation, rotated, sendTelemetry]);
+    }).then(() => onClose());
+  }, [field, indexSetSelection, newFieldType, onClose, putFiledTypeMutation, rotated]);
 
   const onChangeFieldType = useCallback((value: string) => {
     setNewFieldType(value);
   }, []);
 
-  useEffect(() => {
-    sendTelemetry('modal_open', { app_pathname: 'search', app_action_value: 'change-field-type' });
-  }, [sendTelemetry]);
-
-  const onCancel = useCallback(() => {
-    sendTelemetry('modal_close', { app_pathname: 'search', app_action_value: 'change-field-type' });
-    onClose();
-  }, [onClose, sendTelemetry]);
-
   return (
-    <BootstrapModalForm title={`Change ${field} field type`}
+    <BootstrapModalForm title={<span>Change {field} field type <BetaBadge /></span>}
                         submitButtonText="Change field type"
                         onSubmitForm={onSubmit}
-                        onCancel={onCancel}
+                        onCancel={onClose}
                         show={show}
                         bsSize="large">
       <div>
@@ -96,14 +91,16 @@ const ChangeFieldTypeModal = ({ show, onClose, field }: Props) => {
           ingestion errors. It is recommended to enable <DocumentationLink page={DocsHelper.PAGES.INDEXER_FAILURES} displayIcon text="Failure Processing" /> and watch
           the {failureStreamLoading ? <Spinner /> : <StreamLink stream={failureStream} />} stream closely afterwards.
         </Alert>
-        <StyledSelect inputId="field_type"
-                      options={fieldTypeOptions}
-                      value={newFieldType}
-                      onChange={onChangeFieldType}
-                      placeholder="Select field type"
-                      disabled={isOptionsLoading}
-                      inputProps={{ 'aria-label': 'Select field type' }}
-                      required />
+        <Input label="New Field Type:" id="new-field-type">
+          <StyledSelect inputId="field_type"
+                        options={fieldTypeOptions}
+                        value={newFieldType}
+                        onChange={onChangeFieldType}
+                        placeholder="Select field type"
+                        disabled={isOptionsLoading}
+                        inputProps={{ 'aria-label': 'Select field type' }}
+                        required />
+        </Input>
         <Alert bsStyle="info">
           By default the type will be changed in all index sets of the current message/search. By expanding the next section, you can select for which index sets you would like to make the change.
         </Alert>
