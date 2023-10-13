fdb00971bb8d38f8bc222e54b7fa878df42ed937
luk-kaminski <lukasz.kaminski@graylog.com>
2023-10-13 15:26:56 +0200

    Export problem fixed, some code duplication eliminated (#16896) (#16924)
    (backporting to 5.1)
    
    * Export problem fixed, some code duplication eliminated
    
    * Changelog file added
    
    * Comments removed
    
    (cherry picked from commit c99dff9c904565e56dd6f0872b5045313485b293)

--- graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/export/ElasticsearchExportBackend.java
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/views/export/ElasticsearchExportBackend.java
@@ -23,8 +23,11 @@ import org.graylog.plugins.views.search.export.ExportBackend;
 import org.graylog.plugins.views.search.export.ExportMessagesCommand;
 import org.graylog.plugins.views.search.export.SimpleMessage;
 import org.graylog.plugins.views.search.export.SimpleMessageChunk;
+import org.graylog.plugins.views.search.searchfilters.db.UsedSearchFiltersToQueryStringsMapper;
+import org.graylog.plugins.views.search.searchfilters.model.UsedSearchFilter;
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.action.search.SearchRequest;
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.action.support.IndicesOptions;
+import org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.BoolQueryBuilder;
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.QueryBuilder;
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.TermsQueryBuilder;
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.search.SearchHit;
@@ -32,19 +35,16 @@ import org.graylog.shaded.elasticsearch7.org.elasticsearch.search.builder.Search
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.search.sort.SortOrder;
 import org.graylog.storage.elasticsearch7.TimeRangeQueryFactory;
 import org.graylog2.plugin.Message;
-import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.function.Consumer;
 
 import static java.util.Objects.requireNonNull;
@@ -53,7 +53,6 @@ import static org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.Qu
 import static org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 import static org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
 import static org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.QueryBuilders.termsQuery;
-import static org.graylog2.plugin.Tools.ES_DATE_FORMAT_FORMATTER;
 
 @SuppressWarnings("rawtypes")
 public class ElasticsearchExportBackend implements ExportBackend {
@@ -63,11 +62,17 @@ public class ElasticsearchExportBackend implements ExportBackend {
     private final RequestStrategy requestStrategy;
     private final boolean allowLeadingWildcard;
 
+    private final UsedSearchFiltersToQueryStringsMapper usedSearchFiltersToQueryStringsMapper;
+
     @Inject
-    public ElasticsearchExportBackend(IndexLookup indexLookup, RequestStrategy requestStrategy, @Named("allow_leading_wildcard_searches") boolean allowLeadingWildcard) {
+    public ElasticsearchExportBackend(IndexLookup indexLookup,
+                                      RequestStrategy requestStrategy,
+                                      @Named("allow_leading_wildcard_searches") boolean allowLeadingWildcard,
+                                      final UsedSearchFiltersToQueryStringsMapper usedSearchFiltersToQueryStringsMapper) {
         this.indexLookup = indexLookup;
         this.requestStrategy = requestStrategy;
         this.allowLeadingWildcard = allowLeadingWildcard;
+        this.usedSearchFiltersToQueryStringsMapper = usedSearchFiltersToQueryStringsMapper;
     }
 
     @Override
@@ -127,17 +132,28 @@ public class ElasticsearchExportBackend implements ExportBackend {
     }
 
     private QueryBuilder queryFrom(ExportMessagesCommand command) {
-        return boolQuery()
-                .filter(queryStringFilter(command))
+        final BoolQueryBuilder boolQueryBuilder = boolQuery()
+                .filter(queryStringFilter(command.queryString()))
                 .filter(timestampFilter(command))
                 .filter(streamsFilter(command));
+
+        final Collection<UsedSearchFilter> usedSearchFilters = command.usedSearchFilters();
+        if (usedSearchFilters != null) {
+            usedSearchFiltersToQueryStringsMapper.map(usedSearchFilters)
+                    .forEach(filterQueryString -> boolQueryBuilder.filter(queryStringFilter(filterQueryString)));
+        }
+        return boolQueryBuilder;
     }
 
-    private QueryBuilder queryStringFilter(ExportMessagesCommand command) {
-        ElasticsearchQueryString backendQuery = command.queryString();
+    private QueryBuilder queryStringFilter(final ElasticsearchQueryString backendQuery) {
         return backendQuery.isEmpty() ?
                 matchAllQuery() :
                 queryStringQuery(backendQuery.queryString()).allowLeadingWildcard(allowLeadingWildcard);
+    }
+
+    private QueryBuilder queryStringFilter(final String queryString) {
+        ElasticsearchQueryString backendQuery = ElasticsearchQueryString.of(queryString);
+        return queryStringFilter(backendQuery);
     }
 
     private QueryBuilder timestampFilter(ExportMessagesCommand command) {
@@ -180,35 +196,5 @@ public class ElasticsearchExportBackend implements ExportBackend {
                 .collect(toCollection(LinkedHashSet::new));
     }
 
-    private SimpleMessage buildHitWithAllFields(Map source, String index, DateTimeZone timeZone) {
-        LinkedHashMap<String, Object> fields = new LinkedHashMap<>();
 
-        for (Object key : source.keySet()) {
-            String name = (String) key;
-            Object value = valueFrom(source, name, timeZone);
-            fields.put(name, value);
-        }
-
-        // _id is needed, because the old decorators implementation relies on it
-        fields.put("_id", UUID.randomUUID().toString());
-
-        return SimpleMessage.from(index, fields);
-    }
-
-    private Object valueFrom(Map source, String name, DateTimeZone timeZone) {
-        if (name.equals(Message.FIELD_TIMESTAMP)) {
-            return fixTimestampFormat(source.get(Message.FIELD_TIMESTAMP), timeZone);
-        }
-        return source.get(name);
-    }
-
-    private Object fixTimestampFormat(Object rawTimestamp, DateTimeZone timeZone) {
-        try {
-            final DateTime parsed = ES_DATE_FORMAT_FORMATTER.parseDateTime(String.valueOf(rawTimestamp));
-            return parsed.withZone(timeZone).toString();
-        } catch (IllegalArgumentException e) {
-            LOG.warn("Could not parse timestamp {}", rawTimestamp, e);
-            return rawTimestamp;
-        }
-    }
 }
