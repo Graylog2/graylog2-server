4227854619607ed9318c464ffe4d14986e309b6c
Linus Pahl <linus@graylog.com>
2023-10-19 14:54:30 +0200

    Fix toUTCFromTz test.

--- graylog2-web-interface/src/util/DateTime.test.ts
+++ graylog2-web-interface/src/util/DateTime.test.ts
@@ -172,11 +172,11 @@ describe('DateTime utils', () => {
 
   describe('toUTCFromTz', () => {
     it('should transform time to UTC based on defined tz', () => {
-      expect(toUTCFromTz('2020-01-01T10:00:00.000', moscowTZ)).toBe('2020-01-01T07:00:00.000Z');
+      expect(toUTCFromTz('2020-01-01T10:00:00.000', moscowTZ).toISOString()).toEqual('2020-01-01T07:00:00.000Z');
     });
 
     it('should prioritize time zone of date time over provided time zone when calculating UTC time', () => {
-      expect(toUTCFromTz('2020-01-01T12:00:00.000+05:00', 'Europe/Berlin')).toBe('2020-01-01T07:00:00.000Z');
+      expect(toUTCFromTz('2020-01-01T12:00:00.000+05:00', 'Europe/Berlin').toISOString()).toBe('2020-01-01T07:00:00.000Z');
     });
   });
 });
