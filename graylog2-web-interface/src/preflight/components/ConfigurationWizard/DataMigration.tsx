28f5b00c8315f6f77330912b339834503ba82962
Laura Bergenthal-Grotlüschen <laura.bergenthalgrotlueschen@graylog.com>
2023-11-09 10:27:43 +0100

    Add data node migration form

--- graylog2-web-interface/src/preflight/components/ConfigurationWizard/DataMigration.tsx
+++ graylog2-web-interface/src/preflight/components/ConfigurationWizard/DataMigration.tsx
@@ -15,20 +15,67 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
+import { Formik, Form } from 'formik';
 
-import { Title, Space, Button } from 'preflight/components/common';
+import { Title, Space, Button, FormikInput, Group } from 'preflight/components/common';
 
 type Props = {
   setShouldMigrateData: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
-const DataMigration = ({ setShouldMigrateData } : Props) => (
-  <div>
-    <Title order={3}>Migrate data from existing node</Title>
-    <p>Migrate your Data</p>
-    <Space h="md" />
-    <Button size="xs" onClick={() => setShouldMigrateData(false)}>Cancel</Button>
-  </div>
-);
+type FormValues = {
+  host: string,
+  username: string,
+  password: string,
+  whitelist: string,
+}
+
+const DataMigration = ({ setShouldMigrateData } : Props) => {
+  const onSubmit = (formValues: FormValues) => { console.log('===submitting', formValues); };
+  const initialValues = {
+    host: '',
+    username: '',
+    password: '',
+    whitelist: '',
+  };
+
+  return (
+    <div>
+      <Title order={3}>Migrate data from existing node</Title>
+      <p>Migrate your your data from an existing OpenSearch node to a <em>Graylog data node</em>. </p>
+      <Space h="md" />
+
+      <Formik initialValues={initialValues} onSubmit={(formValues: FormValues) => onSubmit(formValues)}>
+        {({ isSubmitting, isValid }) => (
+          <Form>
+            <FormikInput placeholder="Host"
+                         name="host"
+                         label="Node Host"
+                         required />
+            <FormikInput placeholder="Username"
+                         name="username"
+                         label="Username"
+                         required />
+            <FormikInput placeholder="Password"
+                         name="password"
+                         label="Password"
+                         required />
+            <FormikInput placeholder="Hosts to add to the whitelist"
+                         name="whitelist"
+                         label="Reindex from Remote Whitelist"
+                         required />
+            <Space h="md" />
+            <Group>
+              <Button disabled={isSubmitting || !isValid} type="submit">
+                {isSubmitting ? 'Starting Migration' : 'Start Migration'}
+              </Button>
+              <Button onClick={() => setShouldMigrateData(false)} variant="light">Cancel</Button>
+            </Group>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default DataMigration;
