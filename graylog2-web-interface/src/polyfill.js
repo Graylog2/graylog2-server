b4c34624365f272a102ff918f04ea706d1f7b2e2
Dennis Oelkers <dennis@graylog.com>
2023-11-15 09:55:14 +0100

    Removing intersection observer polyfill.

--- graylog2-web-interface/src/polyfill.js
+++ graylog2-web-interface/src/polyfill.js
@@ -24,4 +24,3 @@ import 'regenerator-runtime/runtime';
 // To support IE11 (remove if support is dropped)
 import 'promise-polyfill/src/polyfill';
 import 'whatwg-fetch';
-import 'intersection-observer';
