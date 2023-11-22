Graylog2/graylog2-server

commit 6554a6f4312452a6fb3b214bfcec8b7744728526

Feature/14285 migrate event definitions page to new bulk friendly design (#14502)
* feat: add paginated endpoint with sort parameter returning new PageListResponse

* doc: adjust swagger doc

* feat: add scheduler context to event definition dto

* feat: add PageListResponse for notifications

* fix: remove nested attributes for the time being

* update types

* add Status and Scheduler components

* add eventDefinition datatable action

* add paginated eventDefinition and hook

* move EventDefinitionsContainer to ts and use EntityDataTable

* add bulk delete action

* fix types

* doc: make scheduler property read-only

* doc: add changelog

* fix linter

* fix status size

* fix sorting order

* feat: bulk schedule/unschedule

* implement delete, enable and disable bulk action

* add BulkActions test

* fix error

* add searchPaginated to notifcationStore

* use EntityDataTable in event notification

* remove useMemo to fix renderCell

- this fixes the issue when using param with columnRenderers

* use small buttons for actions

* add license headers

* fix review comments

* fix: change type to SearchFieldQuery.Type

* remove unused DetailsList

* Update graylog2-web-interface/src/components/event-notifications/event-notifications/EventNotificationsContainer.tsx

Co-authored-by: Laura <grotlue@users.noreply.github.com>

* fix: correct sorting behavior

* fix: rename alert to status

* fix: set sortable to false for `status`

* remove status sort

* show SYSTEM_EVENT_DEFINITION_TYPE as enabled

* update changelog

* doc: adjust swagger doc wording

* fix: exception if trying to disable system notification events

---------

Co-authored-by: Ousmane Samba <ousmane@graylog.com>
Co-authored-by: Patrick Mann <patrickmann@users.noreply.github.com>
Co-authored-by: Laura <grotlue@users.noreply.github.com>

graylog2-server/src/main/java/org/graylog/events/processor/EventDefinitionHandler.java
@@ -17,6 +17,7 @@
 package org.graylog.events.processor;
 
 import org.graylog.events.notifications.EventNotificationExecutionJob;
+import org.graylog.events.processor.systemnotification.SystemNotificationEventEntityScope;
 import org.graylog.scheduler.DBJobDefinitionService;
 import org.graylog.scheduler.DBJobTriggerService;
 import org.graylog.scheduler.JobDefinitionDto;
@@ -184,6 +185,10 @@ public class EventDefinitionHandler {
      */
     public void unschedule(String eventDefinitionId) {
         final EventDefinitionDto eventDefinition = getEventDefinitionOrThrowIAE(eventDefinitionId);
+
+        if (SystemNotificationEventEntityScope.NAME.equals(eventDefinition.scope())) {
+            throw new IllegalArgumentException("Cannot disable system notification events");
+        }
 
         getJobDefinition(eventDefinition)
                 .ifPresent(jobDefinition -> deleteJobDefinitionAndTrigger(jobDefinition, eventDefinition));

graylog2-server/src/main/java/org/graylog/events/context/EventDefinitionContextService.java
@@ -46,6 +46,7 @@ import java.util.stream.Collectors;
  * their DTOs.
  */
 public class EventDefinitionContextService {
+    public static final String SCHEDULER_KEY = "scheduler";
     private final DBJobDefinitionService jobDefinitionService;
     private final DBJobTriggerService jobTriggerService;
 
@@ -56,12 +57,12 @@ public class EventDefinitionContextService {
     }
 
     public ImmutableMap<String, Object> contextFor(List<EventDefinitionDto> eventDefinitions) {
-        return ImmutableMap.of("scheduler", schedulerContext(eventDefinitions));
+        return ImmutableMap.of(SCHEDULER_KEY, schedulerContext(eventDefinitions));
     }
 
     public ImmutableMap<String, Object> contextFor(EventDefinitionDto eventDefinition) {
         final ImmutableMap<String, SchedulerCtx> schedulerContext = schedulerContext(Collections.singletonList(eventDefinition));
-        return ImmutableMap.of("scheduler", schedulerContext.get(eventDefinition.id()));
+        return ImmutableMap.of(SCHEDULER_KEY, schedulerContext.get(eventDefinition.id()));
     }
 
     private Map<String, List<JobDefinitionDto>> getJobDefinitions(List<EventDefinitionDto> eventDefinitions) {

graylog2-server/src/main/java/org/graylog/events/notifications/DBNotificationService.java
@@ -41,9 +41,9 @@ public class DBNotificationService extends PaginatedDbService<NotificationDto> {
     }
 
     public PaginatedList<NotificationDto> searchPaginated(SearchQuery query, Predicate<NotificationDto> filter,
-                                                          String sortByField, int page, int perPage) {
+                                                          String sortByField, String sortOrder, int page, int perPage) {
         return findPaginatedWithQueryFilterAndSort(query.toDBQuery(), filter,
-                getSortBuilder("asc", sortByField), page, perPage);
+                getSortBuilder(sortOrder, sortByField), page, perPage);
     }
 
     public NotificationDto saveWithOwnership(NotificationDto notificationDto, User user) {

graylog2-server/src/main/java/org/graylog/events/processor/DBEventDefinitionService.java
@@ -58,9 +58,9 @@ public class DBEventDefinitionService extends ScopedDbService<EventDefinitionDto
     }
 
     public PaginatedList<EventDefinitionDto> searchPaginated(SearchQuery query, Predicate<EventDefinitionDto> filter,
-                                                             String sortByField, int page, int perPage) {
+                                                             String sortByField, String sortOrder, int page, int perPage) {
         return findPaginatedWithQueryFilterAndSort(query.toDBQuery(), filter,
-                getSortBuilder("asc", sortByField), page, perPage);
+                getSortBuilder(sortOrder, sortByField), page, perPage);
     }
 
     public EventDefinitionDto saveWithOwnership(EventDefinitionDto eventDefinitionDto, User user) {

graylog2-server/src/main/java/org/graylog/events/processor/EventDefinition.java
@@ -18,6 +18,7 @@ package org.graylog.events.processor;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import org.graylog.events.context.EventDefinitionContextService;
 import org.graylog.events.fields.EventFieldSpec;
 import org.graylog.events.notifications.EventNotificationHandler;
 import org.graylog.events.notifications.EventNotificationSettings;
@@ -53,6 +54,8 @@ public interface EventDefinition {
     ImmutableList<EventNotificationHandler.Config> notifications();
 
     ImmutableList<EventStorageHandler.Config> storage();
+
+    EventDefinitionContextService.SchedulerCtx schedulerCtx();
 
     default Set<String> requiredPermissions() {
         return config().requiredPermissions();

graylog2-server/src/main/java/org/graylog/events/processor/EventDefinitionDto.java
@@ -29,6 +29,7 @@ import org.graylog.autovalue.WithBeanGetter;
 import org.graylog.events.contentpack.entities.EventDefinitionEntity;
 import org.graylog.events.contentpack.entities.EventNotificationHandlerConfigEntity;
 import org.graylog.events.contentpack.entities.EventProcessorConfigEntity;
+import org.graylog.events.context.EventDefinitionContextService;
 import org.graylog.events.fields.EventFieldSpec;
 import org.graylog.events.notifications.EventNotificationHandler;
 import org.graylog.events.notifications.EventNotificationSettings;
@@ -67,6 +68,7 @@ public abstract class EventDefinitionDto extends ScopedEntity implements EventDe
     private static final String FIELD_KEY_SPEC = "key_spec";
     private static final String FIELD_NOTIFICATION_SETTINGS = "notification_settings";
     private static final String FIELD_STORAGE = "storage";
+    private static final String FIELD_SCHEDULERCTX = "scheduler";
     private static final String UPDATED_AT = "updated_at";
 
     @Override
@@ -120,6 +122,11 @@ public abstract class EventDefinitionDto extends ScopedEntity implements EventDe
     @Override
     @JsonProperty(FIELD_STORAGE)
     public abstract ImmutableList<EventStorageHandler.Config> storage();
+
+    @Override
+    @JsonProperty(value = FIELD_SCHEDULERCTX, access = JsonProperty.Access.READ_ONLY)
+    @Nullable
+    public abstract EventDefinitionContextService.SchedulerCtx schedulerCtx();
 
     public static Builder builder() {
         return Builder.create();
@@ -204,6 +211,9 @@ public abstract class EventDefinitionDto extends ScopedEntity implements EventDe
 
         @JsonProperty(FIELD_STORAGE)
         public abstract Builder storage(ImmutableList<EventStorageHandler.Config> storageHandlers);
+
+        @JsonProperty(value = FIELD_SCHEDULERCTX, access = JsonProperty.Access.READ_ONLY)
+        public abstract Builder schedulerCtx(EventDefinitionContextService.SchedulerCtx schedulerCtx);
 
         abstract EventDefinitionDto autoBuild();
 

graylog2-server/src/main/java/org/graylog/events/rest/EventDefinitionsResource.java
@@ -50,6 +50,10 @@ import org.graylog2.rest.bulk.SequentialBulkExecutor;
 import org.graylog2.rest.bulk.model.BulkOperationRequest;
 import org.graylog2.rest.bulk.model.BulkOperationResponse;
 import org.graylog2.rest.models.PaginatedResponse;
+import org.graylog2.rest.models.tools.responses.PageListResponse;
+import org.graylog2.rest.resources.entities.EntityAttribute;
+import org.graylog2.rest.resources.entities.EntityDefaults;
+import org.graylog2.rest.resources.entities.Sorting;
 import org.graylog2.search.SearchQuery;
 import org.graylog2.search.SearchQueryField;
 import org.graylog2.search.SearchQueryParser;
@@ -79,6 +83,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -99,6 +105,17 @@ public class EventDefinitionsResource extends RestResource implements PluginRest
             .put("title", SearchQueryField.create(EventDefinitionDto.FIELD_TITLE))
             .put("description", SearchQueryField.create(EventDefinitionDto.FIELD_DESCRIPTION))
             .build();
+    private static final String DEFAULT_SORT_FIELD = "title";
+    private static final String DEFAULT_SORT_DIRECTION = "asc";
+    private static final List<EntityAttribute> attributes = List.of(
+            EntityAttribute.builder().id("title").title("Title").build(),
+            EntityAttribute.builder().id("description").title("Description").build(),
+            EntityAttribute.builder().id("priority").title("Priority").type(SearchQueryField.Type.INT).build(),
+            EntityAttribute.builder().id("status").title("Status").type(SearchQueryField.Type.BOOLEAN).sortable(false).build()
+    );
+    private static final EntityDefaults settings = EntityDefaults.builder()
+            .sort(Sorting.create(DEFAULT_SORT_FIELD, Sorting.Direction.valueOf(DEFAULT_SORT_DIRECTION.toUpperCase(Locale.ROOT))))
+            .build();
 
     private final DBEventDefinitionService dbService;
     private final EventDefinitionHandler eventDefinitionHandler;
@@ -106,7 +123,9 @@ public class EventDefinitionsResource extends RestResource implements PluginRest
     private final EventProcessorEngine engine;
     private final SearchQueryParser searchQueryParser;
     private final RecentActivityService recentActivityService;
-    private final BulkExecutor<EventDefinitionDto, UserContext> bulkExecutor;
+    private final BulkExecutor<EventDefinitionDto, UserContext> bulkDeletionExecutor;
+    private final BulkExecutor<EventDefinitionDto, UserContext> bulkScheduleExecutor;
+    private final BulkExecutor<EventDefinitionDto, UserContext> bulkUnscheduleExecutor;
 
     @Inject
     public EventDefinitionsResource(DBEventDefinitionService dbService,
@@ -123,11 +142,61 @@ public class EventDefinitionsResource extends RestResource implements PluginRest
         this.engine = engine;
         this.searchQueryParser = new SearchQueryParser(EventDefinitionDto.FIELD_TITLE, SEARCH_FIELD_MAPPING);
         this.recentActivityService = recentActivityService;
-        this.bulkExecutor = new SequentialBulkExecutor<>(this::delete, auditEventSender, objectMapper);
+        this.bulkDeletionExecutor = new SequentialBulkExecutor<>(this::delete, auditEventSender, objectMapper);
+        this.bulkScheduleExecutor = new SequentialBulkExecutor<>(this::schedule, auditEventSender, objectMapper);
+        this.bulkUnscheduleExecutor = new SequentialBulkExecutor<>(this::unschedule, auditEventSender, objectMapper);
+    }
+
+
+    @GET
+    @Timed
+    @Path("/paginated")
+    @ApiOperation(value = "Get a paginated list of event definitions")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PageListResponse<EventDefinitionDto> getPage(@ApiParam(name = "page") @QueryParam("page") @DefaultValue("1") int page,
+                                                        @ApiParam(name = "per_page") @QueryParam("per_page") @DefaultValue("50") int perPage,
+                                                        @ApiParam(name = "query") @QueryParam("query") @DefaultValue("") String query,
+                                                        @ApiParam(name = "sort",
+                                                                  value = "The field to sort the result on",
+                                                                  required = true,
+                                                                  allowableValues = "title,description,priority,status")
+                                                        @DefaultValue(DEFAULT_SORT_FIELD) @QueryParam("sort") String sort,
+                                                        @ApiParam(name = "order", value = "The sort direction", allowableValues = "asc, desc")
+                                                        @DefaultValue(DEFAULT_SORT_DIRECTION) @QueryParam("order") String order) {
+
+        SearchQuery searchQuery;
+        try {
+            searchQuery = searchQueryParser.parse(query);
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException("Invalid argument in search query: " + e.getMessage());
+        }
+        if ("status".equals(sort)) {
+            sort = "alert";
+        }
+        final PaginatedList<EventDefinitionDto> result = dbService.searchPaginated(searchQuery, event -> {
+            return isPermitted(RestPermissions.EVENT_DEFINITIONS_READ, event.id());
+        }, sort, order, page, perPage);
+        PaginatedList<EventDefinitionDto> definitionDtos = new PaginatedList<>(
+                result.delegate(), result.pagination().total(), result.pagination().page(), result.pagination().perPage()
+        );
+
+        final ImmutableMap<String, Object> context = contextService.contextFor(result.delegate());
+        ImmutableMap<String, EventDefinitionContextService.SchedulerCtx> schedulerCtx =
+                (ImmutableMap<String, EventDefinitionContextService.SchedulerCtx>) context.get(EventDefinitionContextService.SCHEDULER_KEY);
+
+        final List<EventDefinitionDto> eventDefinitionDtos =
+                result.delegate()
+                        .stream()
+                        .map(eventDefinition -> eventDefinition.toBuilder().schedulerCtx(schedulerCtx.get(eventDefinition.id())).build())
+                        .toList();
+
+        return PageListResponse.create(query, definitionDtos.pagination(),
+                result.grandTotal().orElse(0L), sort, order, eventDefinitionDtos, attributes, settings);
     }
 
     @GET
     @ApiOperation("List event definitions")
+    @Deprecated
     public PaginatedResponse<EventDefinitionDto> list(@ApiParam(name = "page") @QueryParam("page") @DefaultValue("1") int page,
                                                       @ApiParam(name = "per_page") @QueryParam("per_page") @DefaultValue("50") int perPage,
                                                       @ApiParam(name = "query") @QueryParam("query") @DefaultValue("") String query) {
@@ -139,7 +208,7 @@ public class EventDefinitionsResource extends RestResource implements PluginRest
         }
         final PaginatedList<EventDefinitionDto> result = dbService.searchPaginated(searchQuery, event -> {
             return isPermitted(RestPermissions.EVENT_DEFINITIONS_READ, event.id());
-        }, "title", page, perPage);
+        }, "title", "asc", page, perPage);
         final ImmutableMap<String, Object> context = contextService.contextFor(result.delegate());
         return PaginatedResponse.create("event_definitions", result, query, context);
     }
@@ -229,12 +298,12 @@ public class EventDefinitionsResource extends RestResource implements PluginRest
     @Path("/bulk_delete")
     @Consumes(MediaType.APPLICATION_JSON)
     @Timed
-    @ApiOperation(value = "Delete a bulk of event definitions", response = BulkOperationResponse.class)
+    @ApiOperation(value = "Delete multiple event definitions", response = BulkOperationResponse.class)
     @NoAuditEvent("Audit events triggered manually")
     public Response bulkDelete(@ApiParam(name = "Entities to remove", required = true) final BulkOperationRequest bulkOperationRequest,
                                @Context UserContext userContext) {
 
-        final BulkOperationResponse response = bulkExecutor.executeBulkOperation(bulkOperationRequest,
+        final BulkOperationResponse response = bulkDeletionExecutor.executeBulkOperation(bulkOperationRequest,
                 userContext,
                 new AuditParams(EventsAuditEventTypes.EVENT_DEFINITION_DELETE, "definitionId", EventDefinitionDto.class));
 
@@ -248,9 +317,30 @@ public class EventDefinitionsResource extends RestResource implements PluginRest
     @Consumes(MediaType.WILDCARD)
     @ApiOperation("Enable event definition")
     @AuditEvent(type = EventsAuditEventTypes.EVENT_DEFINITION_UPDATE)
-    public void schedule(@ApiParam(name = "definitionId") @PathParam("definitionId") @NotBlank String definitionId) {
+    public EventDefinitionDto schedule(@ApiParam(name = "definitionId") @PathParam("definitionId") @NotBlank String definitionId,
+                                       @Context UserContext userContext) {
         checkPermission(RestPermissions.EVENT_DEFINITIONS_EDIT, definitionId);
+        final Optional<EventDefinitionDto> eventDefinitionDto = dbService.get(definitionId);
         eventDefinitionHandler.schedule(definitionId);
+        return eventDefinitionDto.orElse(null);
+    }
+
+    @POST
+    @Path("/bulk_schedule")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Timed
+    @ApiOperation(value = "Enable multiple event definitions", response = BulkOperationResponse.class)
+    @NoAuditEvent("Audit events triggered manually")
+    public Response bulkSchedule(@ApiParam(name = "Event definitions to enable", required = true) final BulkOperationRequest bulkOperationRequest,
+                                 @Context UserContext userContext) {
+
+        final BulkOperationResponse response = bulkScheduleExecutor.executeBulkOperation(bulkOperationRequest,
+                userContext,
+                new AuditParams(EventsAuditEventTypes.EVENT_DEFINITION_UPDATE, "definitionId", EventDefinitionDto.class));
+
+        return Response.status(Response.Status.OK)
+                .entity(response)
+                .build();
     }
 
     @PUT
@@ -258,9 +348,30 @@ public class EventDefinitionsResource extends RestResource implements PluginRest
     @Consumes(MediaType.WILDCARD)
     @ApiOperation("Disable event definition")
     @AuditEvent(type = EventsAuditEventTypes.EVENT_DEFINITION_UPDATE)
-    public void unschedule(@ApiParam(name = "definitionId") @PathParam("definitionId") @NotBlank String definitionId) {
+    public EventDefinitionDto unschedule(@ApiParam(name = "definitionId") @PathParam("definitionId") @NotBlank String definitionId,
+                                         @Context UserContext userContext) {
         checkPermission(RestPermissions.EVENT_DEFINITIONS_EDIT, definitionId);
+        final Optional<EventDefinitionDto> eventDefinitionDto = dbService.get(definitionId);
         eventDefinitionHandler.unschedule(definitionId);
+        return eventDefinitionDto.orElse(null);
+    }
+
+    @POST
+    @Path("/bulk_unschedule")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Timed
+    @ApiOperation(value = "Disable multiple event definitions", response = BulkOperationResponse.class)
+    @NoAuditEvent("Audit events triggered manually")
+    public Response bulkUnschedule(@ApiParam(name = "Event definitions to disable", required = true) final BulkOperationRequest bulkOperationRequest,
+                                   @Context UserContext userContext) {
+
+        final BulkOperationResponse response = bulkUnscheduleExecutor.executeBulkOperation(bulkOperationRequest,
+                userContext,
+                new AuditParams(EventsAuditEventTypes.EVENT_DEFINITION_UPDATE, "definitionId", EventDefinitionDto.class));
+
+        return Response.status(Response.Status.OK)
+                .entity(response)
+                .build();
     }
 
     @PUT

graylog2-server/src/main/java/org/graylog/events/rest/EventNotificationsResource.java
@@ -43,6 +43,10 @@ import org.graylog2.plugin.configuration.ConfigurationRequest;
 import org.graylog2.plugin.rest.PluginRestResource;
 import org.graylog2.plugin.rest.ValidationResult;
 import org.graylog2.rest.models.PaginatedResponse;
+import org.graylog2.rest.models.tools.responses.PageListResponse;
+import org.graylog2.rest.resources.entities.EntityAttribute;
+import org.graylog2.rest.resources.entities.EntityDefaults;
+import org.graylog2.rest.resources.entities.Sorting;
 import org.graylog2.search.SearchQuery;
 import org.graylog2.search.SearchQueryField;
 import org.graylog2.search.SearchQueryParser;
@@ -68,6 +72,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -85,6 +91,17 @@ public class EventNotificationsResource extends RestResource implements PluginRe
             .put("id", SearchQueryField.create("_id", SearchQueryField.Type.OBJECT_ID))
             .put("title", SearchQueryField.create(NotificationDto.FIELD_TITLE))
             .put("description", SearchQueryField.create(NotificationDto.FIELD_DESCRIPTION))
+            .build();
+
+    private static final String DEFAULT_SORT_FIELD = "title";
+    private static final String DEFAULT_SORT_DIRECTION = "asc";
+    private static final List<EntityAttribute> attributes = List.of(
+            EntityAttribute.builder().id("title").title("Title").build(),
+            EntityAttribute.builder().id("description").title("Description").build(),
+            EntityAttribute.builder().id("type").title("Type").build()
+    );
+    private static final EntityDefaults settings = EntityDefaults.builder()
+            .sort(Sorting.create(DEFAULT_SORT_FIELD, Sorting.Direction.valueOf(DEFAULT_SORT_DIRECTION.toUpperCase(Locale.ROOT))))
             .build();
 
     private final DBNotificationService dbNotificationService;
@@ -110,14 +127,43 @@ public class EventNotificationsResource extends RestResource implements PluginRe
     }
 
     @GET
+    @Timed
+    @Path("/paginated")
+    @ApiOperation(value = "Get a paginated list of event notifications")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PageListResponse<NotificationDto> getPage(@ApiParam(name = "page") @QueryParam("page") @DefaultValue("1") int page,
+                                                     @ApiParam(name = "per_page") @QueryParam("per_page") @DefaultValue("50") int perPage,
+                                                     @ApiParam(name = "query") @QueryParam("query") @DefaultValue("") String query,
+                                                     @ApiParam(name = "sort",
+                                                               value = "The field to sort the result on",
+                                                               required = true,
+                                                               allowableValues = "title,description,type")
+                                                     @DefaultValue(DEFAULT_SORT_FIELD) @QueryParam("sort") String sort,
+                                                     @ApiParam(name = "order", value = "The sort direction", allowableValues = "asc, desc")
+                                                     @DefaultValue(DEFAULT_SORT_DIRECTION) @QueryParam("order") String order) {
+        final SearchQuery searchQuery = searchQueryParser.parse(query);
+        if ("type".equals(sort)) {
+            sort = "config.type";
+        }
+        final PaginatedList<NotificationDto> result = dbNotificationService.searchPaginated(searchQuery, notification -> {
+            return isPermitted(RestPermissions.EVENT_NOTIFICATIONS_READ, notification.id());
+        }, sort, order, page, perPage);
+
+
+        return PageListResponse.create(query, result.pagination(),
+                result.grandTotal().orElse(0L), sort, order, result.delegate(), attributes, settings);
+    }
+
+    @GET
     @ApiOperation("List all available notifications")
+    @Deprecated
     public PaginatedResponse<NotificationDto> listNotifications(@ApiParam(name = "page") @QueryParam("page") @DefaultValue("1") int page,
                                                                 @ApiParam(name = "per_page") @QueryParam("per_page") @DefaultValue("50") int perPage,
                                                                 @ApiParam(name = "query") @QueryParam("query") @DefaultValue("") String query) {
         final SearchQuery searchQuery = searchQueryParser.parse(query);
         final PaginatedList<NotificationDto> result = dbNotificationService.searchPaginated(searchQuery, notification -> {
             return isPermitted(RestPermissions.EVENT_NOTIFICATIONS_READ, notification.id());
-        }, "title", page, perPage);
+        }, "title", "asc", page, perPage);
         return PaginatedResponse.create("notifications", result, query);
     }
 

commit 2a7eb8611c8e7c004b68a134425af75fd9f63077

Fix saving of system notification event definition
On save, the UI is passing back the "scheduled=false" param
from the context.

There's several ways we could fix this, but it's probably
easiest to not throw an exception, but just ignore the unschedule
call.

This probably got broken with #14502

graylog2-server/src/main/java/org/graylog/events/processor/EventDefinitionHandler.java
@@ -72,7 +72,7 @@ public class EventDefinitionHandler {
      * Creates a new event definition and a corresponding scheduler job definition and trigger.
      *
      * @param unsavedEventDefinition the event definition to save
-     * @param user the user who created this eventDefinition. If empty, no ownership will be registered.
+     * @param user                   the user who created this eventDefinition. If empty, no ownership will be registered.
      * @return the created event definition
      */
     public EventDefinitionDto create(EventDefinitionDto unsavedEventDefinition, Optional<User> user) {
@@ -95,8 +95,9 @@ public class EventDefinitionHandler {
      * Duplicates an existing event definition.
      * The new copy will be disabled by default and will have the {@link DefaultEntityScope}.
      * Also the title will be prefixed with the string "COPY-".
+     *
      * @param eventDefinition the event definition to copy
-     * @param user the user who copied this eventDefinition. If empty, no ownership will be registered.
+     * @param user            the user who copied this eventDefinition. If empty, no ownership will be registered.
      * @return the newly created event definition
      */
     public EventDefinitionDto duplicate(EventDefinitionDto eventDefinition, Optional<User> user) {
@@ -236,7 +237,8 @@ public class EventDefinitionHandler {
         final EventDefinitionDto eventDefinition = getEventDefinitionOrThrowIAE(eventDefinitionId);
 
         if (SystemNotificationEventEntityScope.NAME.equals(eventDefinition.scope())) {
-            throw new IllegalArgumentException("Cannot disable system notification events");
+            LOG.debug("Ignoring disable for system notification events");
+            return;
         }
 
         getJobDefinition(eventDefinition)