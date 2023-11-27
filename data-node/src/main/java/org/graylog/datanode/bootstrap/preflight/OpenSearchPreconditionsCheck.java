45eb40035afc537bde6cad8ffa41d3f39a0b9a2f
Patrick Mann <patrickmann@users.noreply.github.com>
2023-11-27 17:40:31 +0100

    Check vm.max_map_count in datanode preflight (#17436) (#17478)
    * Check vm.max_map_count in datanode preflight (#17436)
    
    * check vm.max_map_count in preflight
    
    * CL
    
    * fix command path for macOS
    
    * Revert "fix command path for macOS"
    
    This reverts commit caff2a512b39110274046c8156071161a71d49df.
    
    * revert unneeded macOS check; rename
    
    (cherry picked from commit a613989e71e4ae85245bfb8f8bc31e709d5fc856)
    
    * remove reference to missing class
    
    * ignore check on Mac

---
+++ data-node/src/main/java/org/graylog/datanode/bootstrap/preflight/OpenSearchPreconditionsCheck.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.datanode.bootstrap.preflight;
+
+import org.apache.commons.exec.OS;
+import org.graylog2.bootstrap.preflight.PreflightCheck;
+import org.graylog2.bootstrap.preflight.PreflightCheckException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+/**
+ * Check required system parameters
+ */
+public class OpenSearchPreconditionsCheck implements PreflightCheck {
+    private static final Logger LOG = LoggerFactory.getLogger(OpenSearchPreconditionsCheck.class);
+    private static final long MAX_MAP_COUNT_MIN = 262144L;
+
+    @Override
+    public void runCheck() throws PreflightCheckException {
+        if (OS.isFamilyMac()) {
+            return; // don't throw an exception on Mac dev machines
+        }
+        
+        ProcessBuilder builder = new ProcessBuilder().redirectErrorStream(true);
+        builder.command("/sbin/sysctl", "-n", "vm.max_map_count");
+        try {
+            final Process process = builder.start();
+            final BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream(), Charset.defaultCharset()));
+            String line = reader.readLine();
+            reader.close();
+            int exitVal = process.waitFor();
+            if (exitVal != 0) {
+                System.out.println("Sysctl failed with error return " + exitVal);
+                return;
+            }
+            if (line != null) {
+                long count = Long.valueOf(line);
+                if (count < MAX_MAP_COUNT_MIN) {
+                    throw new RuntimeException("vm.max_map_count = " + count + " but should be at least " + MAX_MAP_COUNT_MIN);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            LOG.warn("Failed to run sysctl check: {}", e.getMessage(), e);
+        }
+    }
+}
