493a46d6f58583af4349e1162b7b31f3684925ae
Ousmane Samba <ousmanesamba@gmail.com>
2023-11-28 11:23:10 +0100

    update datanode page

--- graylog2-web-interface/src/pages/DataNodePage.tsx
+++ graylog2-web-interface/src/pages/DataNodePage.tsx
@@ -1,38 +1,80 @@
-import React from 'react'
+import * as React from 'react'
+import styled, { css } from 'styled-components';
+
 import useParams from "routing/useParams";
 import useDataNodes from "components/datanode/hooks/useDataNodes";
 import DataNodesPageNavigation from "components/datanode/DataNodePageNavigation";
 import DocsHelper from "util/DocsHelper";
-import { Row, Col } from 'components/bootstrap';
+import {Row, Col, Label} from 'components/bootstrap';
 import {DocumentTitle, PageHeader, Spinner} from 'components/common';
+import {SearchParams} from "stores/PaginationTypes";
+
+const StyledHorizontalDl = styled.dl(({ theme  }) => css`
+  margin: ${theme.spacings.md} 0;
+  > dt {
+    clear: left;
+    float: left;
+    margin-bottom: ${theme.spacings.md};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 150px;
+  }
+  > *:not(dt) {
+    margin-bottom: ${theme.spacings.md};
+    margin-left: 140px;
+  }
+`);
+const StatusLabel = styled(Label)`
+  display: inline-flex;
+  justify-content: center;
+  gap: 4px;
+`;
 const DataNodePage = () => {
-  const { dataNodeId} = useParams();
+  const { dataNodeId } = useParams();
   const { data: { elements }, isInitialLoading } = useDataNodes({
     query: '',
     page: 1,
     pageSize: 0,
-  });
+  } as SearchParams);
 
   if (isInitialLoading) {
     return <Spinner />
   }
   console.log(elements);
+  const datanode = elements.find((datanode) => datanode.node_id === dataNodeId);
+  const datanodeDisabled = datanode.data_node_status !== 'AVAILABLE';
 
   return (
-    <DocumentTitle title="Data Nodes Migration">
+    <DocumentTitle title={`Data Nodes: ${datanode.hostname}`}>
       <DataNodesPageNavigation />
-      <PageHeader title="Data Nodes Migration"
+      <PageHeader title={`Data Nodes: ${datanode.hostname}`}
                   documentationLink={{
                     title: 'Data Nodes documentation',
                     path: DocsHelper.PAGES.GRAYLOG_DATA_NODE,
                   }}>
-      <span>
-        Graylog data nodes offer a better integration with Graylog and simplify future updates. They allow you to index and search through all the messages in your Graylog message database.
-      </span>
       </PageHeader>
       <Row className="content">
         <Col md={12}>
-
+            <Col md={5}>
+                <h2>Details:</h2>
+                <StyledHorizontalDl>
+                    <dt>Hostname:</dt>
+                    <dd>{datanode.hostname}</dd>
+                    <dt>Transport address:</dt>
+                    <dd>{datanode.transport_address || '-'}</dd>
+                    <dt>Status:</dt>
+                    <dd>
+                      <StatusLabel
+                          bsStyle={datanodeDisabled ? 'warning' : 'success'}
+                          title={datanode.data_node_status}
+                          aria-label={datanode.data_node_status}
+                          role="button">
+                          {datanode.status || 'N/A'}
+                      </StatusLabel>
+                    </dd>
+                </StyledHorizontalDl>
+            </Col>
         </Col>
       </Row>
     </DocumentTitle>
