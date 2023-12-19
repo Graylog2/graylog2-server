b1cdad81f34f6aba889bb3848717a8dc4b7b9883
luk-kaminski <lukasz.kaminski@graylog.com>
2023-12-19 15:38:20 +0100

    Basic CRUD

---
+++ graylog2-server/src/main/java/org/graylog2/indexer/indexset/profile/IndexFieldTypeProfile.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.indexer.indexset.profile;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.graylog2.indexer.indexset.CustomFieldMappings;
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+import javax.annotation.Nullable;
+
+public record IndexFieldTypeProfile(@JsonProperty(ID_FIELD_NAME) @Nullable @Id @ObjectId String id,
+                                    @JsonProperty(NAME_FIELD_NAME) String name,
+                                    @JsonProperty(DESCRIPTION_FIELD_NAME) String description,
+                                    @JsonProperty(CUSTOM_MAPPINGS_FIELD_NAME) CustomFieldMappings customFieldMappings) {
+
+    public static final String ID_FIELD_NAME = "id";
+    public static final String NAME_FIELD_NAME = "name";
+    public static final String DESCRIPTION_FIELD_NAME = "description";
+    public static final String CUSTOM_MAPPINGS_FIELD_NAME = "custom_field_mappings";
+}
