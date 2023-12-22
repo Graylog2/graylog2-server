2707a43c789facf1c13367dca4a2f4d7d532c16a
Jan Heise <jan.heise@graylog.com>
2023-12-22 09:18:17 +0100

    adding license header

--- graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/CAResource.java
+++ graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/CAResource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 package org.graylog2.bootstrap.preflight.web.resources;
 
 import io.swagger.annotations.Api;
@@ -10,7 +26,6 @@ import org.graylog.security.certutil.CaService;
 import org.graylog.security.certutil.ca.exceptions.CACreationException;
 import org.graylog.security.certutil.ca.exceptions.KeyStoreStorageException;
 import org.graylog2.audit.jersey.NoAuditEvent;
-import org.graylog2.bootstrap.preflight.PreflightConstants;
 import org.graylog2.bootstrap.preflight.web.resources.model.CA;
 import org.graylog2.bootstrap.preflight.web.resources.model.CreateCARequest;
 import org.graylog2.plugin.rest.ApiError;
