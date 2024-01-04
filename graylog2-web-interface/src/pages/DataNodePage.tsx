93b94ce2c3f85ce1673643583edca364860e826c
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2024-01-04 19:58:17 +0800

    added actions on datanode datails page

--- graylog2-web-interface/src/pages/DataNodePage.tsx
+++ graylog2-web-interface/src/pages/DataNodePage.tsx
@@ -15,14 +15,15 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
+import { useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import useParams from 'routing/useParams';
-import useDataNodes from 'components/datanode/hooks/useDataNodes';
+import useDataNodes, { rejoinDataNode, removeDataNode, startDataNode, stopDataNode } from 'components/datanode/hooks/useDataNodes';
 import DataNodesPageNavigation from 'components/datanode/DataNodePageNavigation';
 import DocsHelper from 'util/DocsHelper';
-import { Row, Col, Label } from 'components/bootstrap';
-import { DocumentTitle, PageHeader, RelativeTime, Spinner } from 'components/common';
+import { Row, Col, Label, Button } from 'components/bootstrap';
+import { ConfirmDialog, DocumentTitle, PageHeader, RelativeTime, Spinner } from 'components/common';
 import type { SearchParams } from 'stores/PaginationTypes';
 import { CertRenewalButton } from 'components/datanode/DataNodeConfiguration/CertificateRenewal';
 import Icon from 'components/common/Icon';
@@ -57,6 +58,34 @@ const BooleanValue = ({ value }: { value: boolean }) => (
   <><BooleanIcon name={value ? 'check-circle' : 'times-circle'} value={value} /> {value ? 'yes' : 'no'}</>
 );
 
+const ActionsCol = styled(Col)`
+  text-align: right;
+`;
+const ActionButton = styled(Button)`
+  margin-left: 4px;
+`;
+
+const DIALOG_TYPES = {
+  STOP: 'stop',
+  REJOIN: 'rejoin',
+  REMOVE: 'remove',
+};
+
+const DIALOG_TEXT = {
+  [DIALOG_TYPES.REJOIN]: {
+    dialogTitle: 'Rejoin Data Node',
+    dialogBody: (datanode: string) => `Are you sure you want to rejoin Data Node "${datanode}"?`,
+  },
+  [DIALOG_TYPES.REMOVE]: {
+    dialogTitle: 'Remove Data Node',
+    dialogBody: (datanode: string) => `Are you sure you want to remove Data Node "${datanode}"?`,
+  },
+  [DIALOG_TYPES.STOP]: {
+    dialogTitle: 'Stop Data Node',
+    dialogBody: (datanode: string) => `Are you sure you want to stop Data Node "${datanode}"?`,
+  },
+};
+
 const DataNodePage = () => {
   const { dataNodeId } = useParams();
   const { data: { elements }, isInitialLoading } = useDataNodes({
@@ -65,12 +94,66 @@ const DataNodePage = () => {
     pageSize: 0,
   } as SearchParams);
 
+  const [dialogType, setDialogType] = useState<string|null>(null);
+
   if (isInitialLoading) {
     return <Spinner />;
   }
 
   const datanode = elements.find((node) => node.node_id === dataNodeId);
   const datanodeDisabled = datanode.data_node_status !== 'AVAILABLE';
+
+  const handleClearState = () => {
+    setDialogType(null);
+  };
+
+  const handleAction = (action) => {
+    switch (action) {
+      case DIALOG_TYPES.REJOIN:
+        setDialogType(DIALOG_TYPES.REJOIN);
+
+        break;
+      case DIALOG_TYPES.REMOVE:
+        setDialogType(DIALOG_TYPES.REMOVE);
+
+        break;
+      case DIALOG_TYPES.STOP:
+        setDialogType(DIALOG_TYPES.STOP);
+
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleConfirm = () => {
+    switch (dialogType) {
+      case DIALOG_TYPES.REJOIN:
+        rejoinDataNode(datanode.node_id).then(() => {
+          handleClearState();
+        });
+
+        break;
+      case DIALOG_TYPES.REMOVE:
+        removeDataNode(datanode.node_id).then(() => {
+          handleClearState();
+        });
+
+        break;
+      case DIALOG_TYPES.STOP:
+        stopDataNode(datanode.node_id).then(() => {
+          handleClearState();
+        });
+
+        break;
+      default:
+        break;
+    }
+  };
+
+  const isDatanodeRunning = datanode.data_node_status === 'AVAILABLE';
+  const isDatanodeRemoved = datanode.data_node_status === 'REMOVED';
+  const isRemovingDatanode = datanode.data_node_status === 'REMOVING';
 
   return (
     <DocumentTitle title={`Data Nodes: ${datanode.hostname}`}>
@@ -81,8 +164,8 @@ const DataNodePage = () => {
                     path: DocsHelper.PAGES.GRAYLOG_DATA_NODE,
                   }} />
       <Row className="content">
-        <Col md={12}>
-          <Col md={5}>
+        <Col xs={12}>
+          <Col xs={9}>
             <h2>Details:</h2>
             <StyledHorizontalDl>
               <dt>Hostname:</dt>
@@ -104,8 +187,22 @@ const DataNodePage = () => {
               <dd><RelativeTime dateTime={datanode.cert_valid_until} /> <CertRenewalButton nodeId={datanode.node_id} status={datanode.status} /></dd>
             </StyledHorizontalDl>
           </Col>
+          <ActionsCol xs={3}>
+            {!isDatanodeRunning && <ActionButton onClick={() => startDataNode(datanode.node_id)} bsSize="small">Start</ActionButton>}
+            {isDatanodeRunning && <ActionButton onClick={() => handleAction(DIALOG_TYPES.STOP)} bsSize="small">Stop</ActionButton>}
+            {isDatanodeRemoved && <ActionButton onClick={() => handleAction(DIALOG_TYPES.REJOIN)} bsSize="small">Rejoin</ActionButton>}
+            {(!isDatanodeRemoved || isRemovingDatanode) && <ActionButton onClick={() => handleAction(DIALOG_TYPES.REMOVE)} bsSize="small">Remove</ActionButton>}
+          </ActionsCol>
         </Col>
       </Row>
+      {!!dialogType && (
+        <ConfirmDialog title={DIALOG_TEXT[dialogType].dialogTitle}
+                       show
+                       onConfirm={handleConfirm}
+                       onCancel={handleClearState}>
+          {DIALOG_TEXT[dialogType].dialogBody(datanode.hostname)}
+        </ConfirmDialog>
+      )}
     </DocumentTitle>
   );
 };
