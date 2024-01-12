abe971d2e84e487b1283e6d140bc4b98cb1a1b51
Ousmane Samba <ousmanesamba@gmail.com>
2024-01-12 15:50:37 +0100

    add license header

--- graylog2-web-interface/src/components/datanode/DataNodeConfiguration/CACreateForm.tsx
+++ graylog2-web-interface/src/components/datanode/DataNodeConfiguration/CACreateForm.tsx
@@ -1,13 +1,29 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 import React from 'react';
 import { Formik, Form } from 'formik';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import fetch from 'logic/rest/FetchProvider';
 import { qualifyUrl } from 'util/URLUtils';
-import UserNotification from 'preflight/util/UserNotification';
-import { QUERY_KEY as DATA_NODES_CA_QUERY_KEY } from 'preflight/hooks/useDataNodesCA';
+import UserNotification from 'util/UserNotification';
 import { FormikInput } from 'components/common';
 import { Button } from 'components/bootstrap';
+import { QUERY_KEY as DATA_NODES_CA_QUERY_KEY } from 'components/datanode/hooks/useDataNodesCA';
 
 type FormValues = {}
 
