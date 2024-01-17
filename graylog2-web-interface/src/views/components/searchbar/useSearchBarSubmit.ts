db136c1792fbd1cf9727ebee61604c28b4e5cdb9
Dennis Oelkers <dennis@graylog.com>
2024-01-17 15:29:20 +0100

    Simplifying hook.

--- graylog2-web-interface/src/views/components/searchbar/useSearchBarSubmit.ts
+++ graylog2-web-interface/src/views/components/searchbar/useSearchBarSubmit.ts
@@ -7,19 +7,15 @@ import { isNoTimeRangeOverride } from 'views/typeGuards/timeRange';
 import { normalizeFromSearchBarForBackend } from 'views/logic/queries/NormalizeTimeRange';
 import type { TimeRange } from 'views/logic/queries/Query';
 
-const executeWithQueryStringRecording = async <R, >(isDirty: boolean, query: string, callback: () => R) => {
-  const trimmedQuery = query.trim();
-
+const recordQueryString = async (isDirty: boolean, query: string) => {
   try {
-    if (isDirty && !!trimmedQuery) {
-      await SearchQueryStrings.queryStringUsed({ query_string: trimmedQuery });
+    if (isDirty && !!query) {
+      await SearchQueryStrings.queryStringUsed({ query_string: query });
     }
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Unable to record last used query string: ', error);
   }
-
-  return callback();
 };
 
 type FormValues = {
@@ -32,19 +28,22 @@ const _trim = (s: string | undefined) => (s === undefined ? undefined : s.trim()
 const useSearchBarSubmit = (initialValues: FormValues, onSubmit: (v: FormValues) => Promise<unknown>) => {
   const { userTimezone } = useUserDateTime();
   const [enableReinitialize, setEnableReinitialize] = useState(true);
-  const _onSubmit = useCallback((values: SearchBarFormValues) => {
+  const _onSubmit = useCallback(async (values: SearchBarFormValues) => {
     setEnableReinitialize(false);
     const { queryString, timerange, ...rest } = values;
 
-    return executeWithQueryStringRecording(
-      _trim(queryString) !== _trim(initialValues?.queryString),
-      queryString,
-      () => onSubmit({
+    const trimmedQueryString = _trim(queryString);
+    await recordQueryString(trimmedQueryString !== _trim(initialValues?.queryString), trimmedQueryString);
+
+    try {
+      return onSubmit({
         queryString,
         timerange: isNoTimeRangeOverride(timerange) ? undefined : normalizeFromSearchBarForBackend(timerange, userTimezone),
         ...rest,
-      }).finally(() => setEnableReinitialize(true)),
-    );
+      });
+    } finally {
+      setEnableReinitialize(true);
+    }
   }, [initialValues?.queryString, onSubmit, userTimezone]);
 
   return { enableReinitialize, onSubmit: _onSubmit };
