c19456cdf42490e5ff911592e1d91f928f3e80a2
Dennis Oelkers <dennis@graylog.com>
2024-01-17 08:38:53 +0100

    Providing `hooks.logout` interface to register logout hooks.

--- graylog2-web-interface/src/hooks/useLogout.ts
+++ graylog2-web-interface/src/hooks/useLogout.ts
@@ -1,16 +1,30 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { SessionActions } from 'stores/sessions/SessionStore';
 import Routes from 'routing/Routes';
 import useHistory from 'routing/useHistory';
+import usePluginEntities from 'hooks/usePluginEntities';
 
 const logout = () => SessionActions.logout()
   .then(() => SessionActions.validate);
 
+const wrapHooks = (hooks: Array<() => void | Promise<unknown>>) => () => Promise.allSettled(hooks
+  .map(async (hook) => {
+    try {
+      await hook();
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error('Error in logout hook: ', e);
+    }
+  }),
+);
+
 const useLogout = () => {
   const history = useHistory();
+  const logoutHooks = usePluginEntities('hooks.logout');
+  const logoutHook = useMemo(() => wrapHooks(logoutHooks), [logoutHooks]);
 
-  return useCallback(() => logout().then(() => history.push(Routes.STARTPAGE)), [history]);
+  return useCallback(() => logoutHook().then(logout).then(() => history.push(Routes.STARTPAGE)), [history, logoutHook]);
 };
 
 export default useLogout;
