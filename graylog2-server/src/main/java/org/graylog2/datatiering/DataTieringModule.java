f79d54d086e6deeb029bf1987ecdec6305713558
Maxwell Anipah <maxwell.anipah@graylog.com>
2024-01-23 15:13:48 +0100

    remove unused import

--- graylog2-server/src/main/java/org/graylog2/datatiering/DataTieringModule.java
+++ graylog2-server/src/main/java/org/graylog2/datatiering/DataTieringModule.java
@@ -16,7 +16,6 @@
  */
 package org.graylog2.datatiering;
 
-import com.google.inject.Singleton;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.multibindings.OptionalBinder;
 import org.graylog2.datatiering.hotonly.HotOnlyDataTieringConfig;
