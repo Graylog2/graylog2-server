523cf88d2a0ee3e84088a7a7ccde61c69e00c09f
Dennis Oelkers <dennis@graylog.com>
2024-01-24 11:10:08 +0100

    Allowing to specify title for dropdown component.

--- graylog2-web-interface/src/components/common/Popover.tsx
+++ graylog2-web-interface/src/components/common/Popover.tsx
@@ -1,14 +1,53 @@
+import * as React from 'react';
 import { Popover as MantinePopover } from '@mantine/core';
-import styled, { css } from 'styled-components';
+import styled, { css, useTheme } from 'styled-components';
 
-const StyledPopoverDropdown = styled(MantinePopover.Dropdown)(({ theme }) => css`
-  background-color: ${theme.colors.global.contentBackground};
-  border-color: ${theme.colors.variant.light.default};
-  .mantine-Popover-arrow {
-    border-color: ${theme.colors.variant.light.default};
-  }
+const Popover = (props: React.ComponentProps<typeof MantinePopover>) => {
+  const theme = useTheme();
+
+  const styles = () => ({
+    dropdown: {
+      backgroundColor: theme.colors.global.contentBackground,
+      borderColor: theme.colors.variant.light.default,
+      padding: 0,
+    },
+    arrow: {
+      borderColor: theme.colors.variant.light.default,
+      backgroundColor: theme.colors.variant.lightest.default,
+    },
+  });
+
+  return <MantinePopover styles={styles} zIndex={1032} {...props} />;
+};
+
+type DropdownProps = React.ComponentProps<typeof MantinePopover.Dropdown> & {
+  title: string,
+}
+
+const Children = styled.div(({ theme }) => css`
+  padding: ${theme.spacings.sm} ${theme.spacings.md}
 `);
 
-MantinePopover.Dropdown = StyledPopoverDropdown;
+const Title = styled.h4(({ theme }) => css`
+  background-color: ${theme.colors.variant.lightest.default};
+  color: ${theme.colors.variant.darkest.default};
+  padding: ${theme.spacings.sm} ${theme.spacings.md};
+  font-size: ${theme.fonts.size.body};
+`);
 
-export default MantinePopover;
+const Dropdown = ({ title, children }: DropdownProps) => (
+  <MantinePopover.Dropdown>
+    <Title>
+      {title}
+    </Title>
+
+    <Children>
+      {children}
+    </Children>
+  </MantinePopover.Dropdown>
+);
+
+Popover.Target = MantinePopover.Target;
+Popover.Dropdown = Dropdown;
+
+export default Popover;
