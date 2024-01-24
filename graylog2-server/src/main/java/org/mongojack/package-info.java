aef335c7651ec5e472377b5ce263298091fb746b
Othello Maurer <othello@graylog.com>
2024-01-24 16:06:59 +0100

    Add package-info.java

---
+++ graylog2-server/src/main/java/org/mongojack/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains a compatibility layer to support old code using the Mongojack 2.x API. It is destined for
+ * removal as soon as all code has been migrated to use the MongoDB driver API directly.
+ * <p>
+ * Instead of the classes from this package, use {@link org.graylog2.database.MongoCollections} as an entrypoint for
+ * interacting with MongoDB.
+ */
+package org.mongojack;
