7cf03e578e27fef230700037202e9035e76a5423
Dennis Oelkers <dennis@graylog.com>
2024-01-25 08:51:08 +0100

    Migrating `Popover` usage in `QueryValidation`.

--- graylog2-web-interface/src/views/components/searchbar/queryvalidation/QueryValidation.tsx
+++ graylog2-web-interface/src/views/components/searchbar/queryvalidation/QueryValidation.tsx
@@ -17,11 +17,10 @@
 import * as React from 'react';
 import { useContext, useState, useRef, useCallback, useEffect, useMemo } from 'react';
 import styled, { css, keyframes } from 'styled-components';
-import { Overlay, Transition } from 'react-overlays';
 import delay from 'lodash/delay';
 import { useFormikContext } from 'formik';
 
-import { Popover } from 'components/bootstrap';
+import Popover from 'components/common/Popover';
 import Icon from 'components/common/Icon';
 import StringUtils from 'util/StringUtils';
 import DocumentationLink from 'components/support/DocumentationLink';
@@ -98,7 +97,7 @@ const shakeAnimation = keyframes`
 type ExtraProps = {
   $shaking: boolean,
 }
-const StyledPopover: React.ComponentType<React.ComponentProps<typeof Popover> & ExtraProps> = styled(Popover)<ExtraProps>(({ $shaking }) => css`
+const StyledPopoverDropdown: React.ComponentType<React.ComponentProps<typeof Popover.Dropdown> & ExtraProps> = styled(Popover.Dropdown)<ExtraProps>(({ $shaking }) => css`
   animation: ${$shaking ? css`${shakeAnimation} 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both` : 'none'};
 `);
 
@@ -199,32 +198,28 @@ const QueryValidation = () => {
   const hasExplanations = validationState && validationState?.status !== 'OK';
 
   return (
-    <>
-      <Container ref={explanationTriggerRef}>
-        {hasExplanations ? (
-          <ExplanationTrigger title="Toggle validation error explanation"
-                              onClick={toggleShow}
-                              $clickable
-                              tabIndex={0}
-                              type="button">
-            <ErrorIcon $status={status} name="exclamation-circle" />
-          </ExplanationTrigger>
-        ) : (
-          <DocumentationLink page={DocsHelper.PAGES.SEARCH_QUERY_LANGUAGE}
-                             title="Search query syntax documentation"
-                             text={<Icon name="lightbulb" />} />
-        )}
-      </Container>
+    <Popover opened={hasExplanations && showExplanation} position="bottom" width={500} withArrow>
+      <Popover.Target>
+        <Container ref={explanationTriggerRef}>
+          {hasExplanations ? (
+            <ExplanationTrigger title="Toggle validation error explanation"
+                                onClick={toggleShow}
+                                $clickable
+                                tabIndex={0}
+                                type="button">
+              <ErrorIcon $status={status} name="exclamation-circle" />
+            </ExplanationTrigger>
+          ) : (
+            <DocumentationLink page={DocsHelper.PAGES.SEARCH_QUERY_LANGUAGE}
+                               title="Search query syntax documentation"
+                               text={<Icon name="lightbulb" />} />
+          )}
+        </Container>
+      </Popover.Target>
       {hasExplanations && showExplanation && (
-      <Overlay show
-               containerPadding={10}
-               placement="bottom"
-               target={explanationTriggerRef.current}
-               shouldUpdatePosition
-               transition={Transition}>
-        <StyledPopover id="query-validation-error-explanation"
-                       title={<ExplanationTitle title={StringUtils.capitalizeFirstLetter(status.toLocaleLowerCase())} />}
-                       $shaking={shakingPopover}>
+        <StyledPopoverDropdown id="query-validation-error-explanation"
+                               title={<ExplanationTitle title={StringUtils.capitalizeFirstLetter(status.toLocaleLowerCase())} />}
+                               $shaking={shakingPopover}>
           <div role="alert">
             {deduplicatedExplanations.map(({ errorType, errorTitle, errorMessage, id }) => (
               <Explanation key={id}>
@@ -239,10 +234,9 @@ const QueryValidation = () => {
               (<PlugableExplanation validationState={validationState} key={index} />)),
             )}
           </div>
-        </StyledPopover>
-      </Overlay>
+        </StyledPopoverDropdown>
       )}
-    </>
+    </Popover>
   );
 };
 
