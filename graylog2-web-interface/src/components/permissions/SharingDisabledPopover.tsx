cf9043721317f42a54647412c43d08b54220ec0b
Dennis Oelkers <dennis@graylog.com>
2024-01-29 11:51:13 +0100

    Removing unused import.

--- graylog2-web-interface/src/components/permissions/SharingDisabledPopover.tsx
+++ graylog2-web-interface/src/components/permissions/SharingDisabledPopover.tsx
@@ -15,7 +15,6 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import styled from 'styled-components';
 
 import HoverForHelp from 'components/common/HoverForHelp';
 
