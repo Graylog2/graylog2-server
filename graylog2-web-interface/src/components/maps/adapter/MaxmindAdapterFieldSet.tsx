Graylog2/graylog2-server

commit 04dee2f7ec2712caefd463e33c3b624d7999d096

Use different set of database types on cloud (#13911)
* Use different set of database types on cloud

* Add changelog

* Fix typo in MaxMindAdapterFieldSet

* Remove underscore for private functions in MaxmindAdapterFieldSet

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.tsx
@@ -1,0 +1,133 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import AppConfig from 'util/AppConfig';
+import ObjectUtils from 'util/ObjectUtils';
+import { Input } from 'components/bootstrap';
+import { Select, TimeUnitInput } from 'components/common';
+
+type MaxmindAdapterFieldSetProps = {
+  config: {
+    [name: string]: string | number;
+  };
+  updateConfig: (newConfig: object) => void,
+  handleFormEvent: (e: { target: { name: string; value?: string } }) => void;
+  validationState: (key: string) => void,
+  validationMessage: (key: string, message: string) => void,
+};
+
+const MaxmindAdapterFieldSet = ({ config, updateConfig, handleFormEvent, validationState, validationMessage }: MaxmindAdapterFieldSetProps) => {
+  const isCloud = AppConfig.isCloud();
+
+  const pathsForCloud = {
+    IPINFO_STANDARD_LOCATION: '/etc/graylog/server/standard_location.mmdb',
+    IPINFO_ASN: '/etc/graylog/server/asn.mmdb',
+  };
+
+  const ipInfoDatabaseTypes = [
+    { label: 'IPinfo location database', value: 'IPINFO_STANDARD_LOCATION' },
+    { label: 'IPinfo ASN database', value: 'IPINFO_ASN' },
+  ];
+
+  let databaseTypes = [
+    { label: 'ASN database', value: 'MAXMIND_ASN' },
+    { label: 'City database', value: 'MAXMIND_CITY' },
+    { label: 'Country database', value: 'MAXMIND_COUNTRY' },
+  ];
+
+  if (isCloud) {
+    databaseTypes = ipInfoDatabaseTypes;
+  } else {
+    databaseTypes.concat(ipInfoDatabaseTypes);
+  }
+
+  const update = (value: number, unit: string, enabled: boolean, name: string) => {
+    const newConfig = ObjectUtils.clone(config);
+
+    newConfig[name] = enabled ? value : 0;
+    newConfig[`${name}_unit`] = unit;
+    updateConfig(newConfig);
+  };
+
+  const updateCheckInterval = (value: number, unit: string, enabled: boolean) => {
+    update(value, unit, enabled, 'check_interval');
+  };
+
+  const onDbTypeSelect = (id: string) => {
+    const newConfig = ObjectUtils.clone(config);
+
+    if (isCloud) {
+      newConfig.path = pathsForCloud[id];
+    }
+
+    newConfig.database_type = id;
+    updateConfig(newConfig);
+  };
+
+  return (
+    <fieldset>
+      {!isCloud && (
+      <Input type="text"
+             id="path"
+             name="path"
+             label="File path"
+             autoFocus
+             required
+             onChange={handleFormEvent}
+             help={validationMessage('path', 'The path to the database file.')}
+             bsStyle={validationState('path')}
+             value={config.path}
+             labelClassName="col-sm-3"
+             wrapperClassName="col-sm-9" />
+      )}
+      <Input id="database-type-select"
+             label="Database type"
+             required
+             autoFocus
+             help="Select the type of the database file"
+             labelClassName="col-sm-3"
+             wrapperClassName="col-sm-9">
+        <Select placeholder="Select the type of database file"
+                clearable={false}
+                options={databaseTypes}
+                matchProp="label"
+                onChange={onDbTypeSelect}
+                value={config.database_type} />
+      </Input>
+      <TimeUnitInput label="Refresh file"
+                     help="If enabled, the database file is checked for modifications and refreshed when it changed on disk."
+                     update={updateCheckInterval}
+                     value={config.check_interval}
+                     unit={config.check_interval_unit || 'MINUTES'}
+                     defaultEnabled={config.check_interval > 0}
+                     labelClassName="col-sm-3"
+                     wrapperClassName="col-sm-9" />
+    </fieldset>
+  );
+};
+
+MaxmindAdapterFieldSet.propTypes = {
+  config: PropTypes.object.isRequired,
+  updateConfig: PropTypes.func.isRequired,
+  handleFormEvent: PropTypes.func.isRequired,
+  validationState: PropTypes.func.isRequired,
+  validationMessage: PropTypes.func.isRequired,
+};
+
+export default MaxmindAdapterFieldSet;

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.jsx
@@ -1,103 +1,0 @@
-/*
- * Copyright (C) 2020 Graylog, Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
- *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
- */
-import PropTypes from 'prop-types';
-import React from 'react';
-
-import ObjectUtils from 'util/ObjectUtils';
-import { Input } from 'components/bootstrap';
-import { Select, TimeUnitInput } from 'components/common';
-
-class MaxmindAdapterFieldSet extends React.Component {
-  static propTypes = {
-    config: PropTypes.object.isRequired,
-    updateConfig: PropTypes.func.isRequired,
-    handleFormEvent: PropTypes.func.isRequired,
-    validationState: PropTypes.func.isRequired,
-    validationMessage: PropTypes.func.isRequired,
-  };
-
-  _update = (value, unit, enabled, name) => {
-    const config = ObjectUtils.clone(this.props.config);
-
-    config[name] = enabled ? value : 0;
-    config[`${name}_unit`] = unit;
-    this.props.updateConfig(config);
-  };
-
-  updateCheckInterval = (value, unit, enabled) => {
-    this._update(value, unit, enabled, 'check_interval');
-  };
-
-  _onDbTypeSelect = (id) => {
-    const config = ObjectUtils.clone(this.props.config);
-
-    config.database_type = id;
-    this.props.updateConfig(config);
-  };
-
-  render() {
-    const { config } = this.props;
-    const databaseTypes = [
-      { label: 'ASN database', value: 'MAXMIND_ASN' },
-      { label: 'City database', value: 'MAXMIND_CITY' },
-      { label: 'Country database', value: 'MAXMIND_COUNTRY' },
-      { label: 'IPinfo location database', value: 'IPINFO_STANDARD_LOCATION' },
-      { label: 'IPinfo ASN database', value: 'IPINFO_ASN' },
-    ];
-
-    return (
-      <fieldset>
-        <Input type="text"
-               id="path"
-               name="path"
-               label="File path"
-               autoFocus
-               required
-               onChange={this.props.handleFormEvent}
-               help={this.props.validationMessage('path', 'The path to the database file.')}
-               bsStyle={this.props.validationState('path')}
-               value={config.path}
-               labelClassName="col-sm-3"
-               wrapperClassName="col-sm-9" />
-        <Input id="database-type-select"
-               label="Database type"
-               required
-               autoFocus
-               help="Select the type of the database file"
-               labelClassName="col-sm-3"
-               wrapperClassName="col-sm-9">
-          <Select placeholder="Select the type of database file"
-                  clearable={false}
-                  options={databaseTypes}
-                  matchProp="label"
-                  onChange={this._onDbTypeSelect}
-                  value={config.database_type} />
-        </Input>
-        <TimeUnitInput label="Refresh file"
-                       help="If enabled, the database file is checked for modifications and refreshed when it changed on disk."
-                       update={this.updateCheckInterval}
-                       value={config.check_interval}
-                       unit={config.check_interval_unit || 'MINUTES'}
-                       defaultEnabled={config.check_interval > 0}
-                       labelClassName="col-sm-3"
-                       wrapperClassName="col-sm-9" />
-      </fieldset>
-    );
-  }
-}
-
-export default MaxmindAdapterFieldSet;

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.test.tsx
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import React from 'react';
+import { render, screen } from 'wrappedTestingLibrary';
+
+import asMock from 'helpers/mocking/AsMock';
+import AppConfig from 'util/AppConfig';
+
+import MaxmindAdapterFieldSet from './MaxmindAdapterFieldSet';
+
+jest.mock('util/AppConfig', () => ({
+  gl2AppPathPrefix: jest.fn(() => ''),
+  gl2ServerUrl: jest.fn(() => ''),
+  isCloud: jest.fn(() => false),
+}));
+
+describe('MaxmindAdapterFieldSet', () => {
+  describe('in cloud mode', () => {
+    beforeEach(() => {
+      asMock(AppConfig.isCloud).mockImplementation(() => true);
+    });
+
+    it('renders MaxmindAdapterFieldSet without path input', async () => {
+      render(<MaxmindAdapterFieldSet config={{}}
+                                     updateConfig={() => {}}
+                                     handleFormEvent={() => {}}
+                                     validationState={() => {}}
+                                     validationMessage={() => {}} />);
+
+      expect(screen.getByText(/select the type of the database file/i)).toBeInTheDocument();
+
+      expect(screen.queryByRole('textbox', { name: /file path/i })).not.toBeInTheDocument();
+    });
+  });
+
+  describe('not in cloud mode', () => {
+    beforeEach(() => {
+      asMock(AppConfig.isCloud).mockImplementation(() => false);
+    });
+
+    it('renders MaxmindAdapterFieldSet with path input', async () => {
+      render(<MaxmindAdapterFieldSet config={{}}
+                                     updateConfig={() => {}}
+                                     handleFormEvent={() => {}}
+                                     validationState={() => {}}
+                                     validationMessage={() => {}} />);
+
+      expect(screen.getByText(/Select the type of the database file/i)).toBeInTheDocument();
+
+      expect(screen.getByRole('textbox', { name: /file path/i })).toBeInTheDocument();
+    });
+  });
+});

commit 69f5e0a8daeeecc77d29be7710ccb8c14aaa1485

Allow selecting IPinfo DB types in Data Adapters
This probably got broken with PR #13911

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.tsx
@@ -58,7 +58,7 @@ const MaxmindAdapterFieldSet = ({ config, updateConfig, handleFormEvent, validat
   if (isCloud) {
     databaseTypes = ipInfoDatabaseTypes;
   } else {
-    databaseTypes.concat(ipInfoDatabaseTypes);
+    databaseTypes = databaseTypes.concat(ipInfoDatabaseTypes);
   }
 
   const update = (value: number, unit: string, enabled: boolean, name: string) => {