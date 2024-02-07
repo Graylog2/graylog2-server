81e34a5b17d91c9f8428a83bce7612eedd3e2a26
Othello Maurer <othello@graylog.com>
2024-02-07 18:04:50 +0100

    Remove dependency to Mongojack 2 (#18016)
    * Use official mongojack dependency
    * Use unset instead of set(field, null) to prevent serialization errors
    * Use different wrappers for single/multi update results
    * Fix id conversion for ObjectId
    * Remove unused method
    * Always return saved object
    * Handle null id in conversion
    * Remove @Id annotation from constructor
    * Extract saved ID via document codec
    * Fix implementation of getN()
    * Fix implementation of isUpdateOfExisting
    * Add "sparse" index option
    * Don't rely on in-place updates
    * Fix #distinct
    * Prevent ID injection into value classes
    * Use ObjectIDs in filter queries matching "_id" fields
    * Add package-info.java
    * Add support for BsonTimestamp to Joda DateTime conversion
    * Replace deprecated classes with Bson in method signatures
    * Add tests that pass with both old and new implementation
    * Deprecate CommonMongoJackObjectMapperProvider
    * Add changelog
    * Add upgrade note
    * Remove handling of removed data type
    * Refresh underlying cursor when find options are set

--- graylog2-server/src/main/java/org/graylog2/cluster/nodes/NodeDto.java
+++ graylog2-server/src/main/java/org/graylog2/cluster/nodes/NodeDto.java
@@ -17,10 +17,8 @@
 package org.graylog2.cluster.nodes;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import de.undercouch.bson4jackson.types.Timestamp;
 import org.graylog2.cluster.Node;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
@@ -82,10 +80,6 @@ public abstract class NodeDto implements Node {
         public abstract B setTransportAddress(String transportAddress);
 
         @JsonProperty("last_seen")
-        public B setLastSeen(Timestamp timestamp) {
-            return setLastSeen(new DateTime(timestamp.getTime() * 1000L, DateTimeZone.UTC));
-        }
-
         public abstract B setLastSeen(DateTime lastSeen);
 
         @JsonProperty("hostname")
