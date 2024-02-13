d22e59c02547273a6c0c23573d478f24aa9a5364
patrickmann <patrick.mann@graylog.com>
2024-02-13 16:50:06 +0100

    linter

--- graylog2-web-interface/src/logic/alerts/AggregationExpressionValidation.js
+++ graylog2-web-interface/src/logic/alerts/AggregationExpressionValidation.js
@@ -59,8 +59,7 @@ const validateExpressionTree = (expression, series, validationTree = {}) => {
           || selectedSeries?.type === 'stddev'
           || selectedSeries?.type === 'sum'
           || selectedSeries?.type === 'sumofsquares'
-          || selectedSeries?.type === 'variance'))
-      { return { message: 'Field must be set' }; }
+          || selectedSeries?.type === 'variance')) { return { message: 'Field must be set' }; }
 
       return (selectedSeries?.type ? {} : error);
       /* eslint-enable no-case-declarations */
