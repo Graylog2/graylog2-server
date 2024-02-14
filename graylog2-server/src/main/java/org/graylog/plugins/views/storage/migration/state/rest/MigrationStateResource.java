062d8801faaf5a3e77a18ec5971b22043f47750d
Jan Heise <jan.heise@graylog.com>
2024-02-14 19:32:25 +0100

    DataNode: Start/Stop Message Processing (#18126)
    * refactoring of the cluster control convinience methods so we can reuse them for the datanode migrations
    
    * fixed missing license headers
    
    * fixing configuration parameters
    
    * fixing return types
    
    * refactoring
    
    * fixed missing bracket
    
    * fixed missing bracket
    
    * fixed constructor parameters
    
    * use state machine extended context to hold remote processing auth token
    
    * removed obsolete method
    
    * waiting for buffers to drain
    
    * added missing imports
    
    * switch to new NodeService
    
    * fix test and add test for authorization header
    
    ---------
    
    Co-authored-by: Tomas Dvorak <tomas.dvorak@graylog.com>
    Co-authored-by: Matthias Oesterheld <matthias.oesterheld@graylog.com>
    Co-authored-by: Matthias Oesterheld <33032967+moesterheld@users.noreply.github.com>

--- graylog2-server/src/main/java/org/graylog/plugins/views/storage/migration/state/rest/MigrationStateResource.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/storage/migration/state/rest/MigrationStateResource.java
@@ -26,13 +26,18 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.graylog.plugins.views.storage.migration.state.machine.MigrationStateMachine;
 import org.graylog2.audit.jersey.NoAuditEvent;
+import org.graylog2.shared.rest.resources.ProxiedResource;
 import org.graylog2.shared.security.RestPermissions;
+
+import static org.graylog.plugins.views.storage.migration.state.machine.MigrationStateMachineContext.AUTH_TOKEN_KEY;
 
 @Path("/migration")
 @RequiresAuthentication
@@ -44,8 +49,10 @@ public class MigrationStateResource {
     private final MigrationStateMachine stateMachine;
 
     @Inject
-    public MigrationStateResource(MigrationStateMachine stateMachine) {
+    public MigrationStateResource(MigrationStateMachine stateMachine,
+                                  @Context HttpHeaders httpHeaders) {
         this.stateMachine = stateMachine;
+        this.stateMachine.getContext().addExtendedState(AUTH_TOKEN_KEY, ProxiedResource.authenticationToken(httpHeaders));
     }
 
     @POST
@@ -77,7 +84,7 @@ public class MigrationStateResource {
     @Produces(MediaType.TEXT_PLAIN)
     @ApiOperation(value = "Serialize", notes = "Serialize migration graph as graphviz source")
     public String serialize() {
-        // you can use https://dreampuf.github.io/GraphvizOnline/ to vizualize the result
+        // you can use https://dreampuf.github.io/GraphvizOnline/ to visualize the result
         return stateMachine.serialize();
     }
 }
