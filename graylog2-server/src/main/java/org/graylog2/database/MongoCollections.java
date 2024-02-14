d8677277446d23ef1172aa6cee7c949153a56e82
Bernd Ahlers <bernd@graylog.com>
2024-02-14 18:32:27 +0100

    Enable model validator

--- graylog2-server/src/main/java/org/graylog2/database/MongoCollections.java
+++ graylog2-server/src/main/java/org/graylog2/database/MongoCollections.java
@@ -22,6 +22,7 @@ import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.jackson.CustomJacksonCodecRegistry;
+import org.graylog2.jackson.JacksonModelValidator;
 
 @Singleton
 public class MongoCollections {
@@ -39,6 +40,8 @@ public class MongoCollections {
      * Get a MongoCollection configured to use Jackson for serialization/deserialization of objects.
      */
     public <T> MongoCollection<T> get(String collectionName, Class<T> valueType) {
+        JacksonModelValidator.check(collectionName, objectMapper, valueType);
+
         final MongoCollection<T> collection = mongoConnection.getMongoDatabase().getCollection(collectionName, valueType);
         final CustomJacksonCodecRegistry jacksonCodecRegistry = new CustomJacksonCodecRegistry(this.objectMapper,
                 collection.getCodecRegistry());
