8124080f6f5b1980f0b5f501ca00f3a3b447de2b
Anton Ebel <anton.ebel@graylog.com>
2024-02-19 11:48:54 +0100

    add index range results to search validation response

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/rest/QueryValidationResource.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/rest/QueryValidationResource.java
@@ -20,32 +20,31 @@ import com.google.common.collect.ImmutableSet;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.apache.shiro.authz.annotation.RequiresAuthentication;
-import org.graylog.plugins.views.search.Parameter;
-import org.graylog.plugins.views.search.permissions.SearchUser;
-import org.graylog.plugins.views.search.validation.QueryValidationService;
-import org.graylog.plugins.views.search.validation.ValidationMessage;
-import org.graylog.plugins.views.search.validation.ValidationMode;
-import org.graylog.plugins.views.search.validation.ValidationRequest;
-import org.graylog.plugins.views.search.validation.ValidationResponse;
-import org.graylog.plugins.views.search.validation.ValidationStatus;
-import org.graylog.plugins.views.search.validation.ValidationType;
-import org.graylog2.audit.jersey.NoAuditEvent;
-import org.graylog2.plugin.indexer.searches.timeranges.InvalidRangeParametersException;
-import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
-import org.graylog2.plugin.rest.PluginRestResource;
-import org.graylog2.shared.rest.resources.RestResource;
-
 import jakarta.inject.Inject;
-
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.graylog.plugins.views.search.ExplainResults;
+import org.graylog.plugins.views.search.Parameter;
+import org.graylog.plugins.views.search.elasticsearch.IndexLookup;
+import org.graylog.plugins.views.search.permissions.SearchUser;
+import org.graylog.plugins.views.search.validation.QueryValidationService;
+import org.graylog.plugins.views.search.validation.ValidationMessage;
+import org.graylog.plugins.views.search.validation.ValidationRequest;
+import org.graylog.plugins.views.search.validation.ValidationResponse;
+import org.graylog.plugins.views.search.validation.ValidationStatus;
+import org.graylog.plugins.views.search.validation.ValidationType;
+import org.graylog2.audit.jersey.NoAuditEvent;
+import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
+import org.graylog2.plugin.rest.PluginRestResource;
+import org.graylog2.shared.rest.resources.RestResource;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
@@ -60,9 +59,13 @@ public class QueryValidationResource extends RestResource implements PluginRestR
 
     private final QueryValidationService queryValidationService;
 
+    private final IndexLookup indexLookup;
+
     @Inject
-    public QueryValidationResource(final QueryValidationService queryValidationService) {
+    public QueryValidationResource(final QueryValidationService queryValidationService,
+                                   final IndexLookup indexLookup) {
         this.queryValidationService = queryValidationService;
+        this.indexLookup = indexLookup;
     }
 
     @POST
@@ -73,7 +76,12 @@ public class QueryValidationResource extends RestResource implements PluginRestR
             @ApiParam(name = "validationRequest") final ValidationRequestDTO validationRequest,
             @Context final SearchUser searchUser
     ) {
+        ValidationRequest request = prepareRequest(validationRequest, searchUser);
+        final ValidationResponse response = queryValidationService.validate(request);
+        return ValidationResponseDTO.create(toStatus(response.status()), toExplanations(response), indexRanges(request));
+    }
 
+    private ValidationRequest prepareRequest(ValidationRequestDTO validationRequest, SearchUser searchUser) {
         final ValidationRequest.Builder q = ValidationRequest.Builder.builder()
                 .query(validationRequest.query())
                 .timerange(validationRequest.timerange().orElse(defaultTimeRange()))
@@ -82,9 +90,13 @@ public class QueryValidationResource extends RestResource implements PluginRestR
                 .validationMode(validationRequest.validationMode().toInternalRepresentation());
 
         validationRequest.filter().ifPresent(q::filter);
+        return q.build();
+    }
 
-        final ValidationResponse response = queryValidationService.validate(q.build());
-        return ValidationResponseDTO.create(toStatus(response.status()), toExplanations(response));
+    private Set<ExplainResults.IndexRangeResult> indexRanges(ValidationRequest request) {
+        return indexLookup.indexRangesForStreamsInTimeRange(request.streams(), request.timerange()).stream()
+                .map(ExplainResults.IndexRangeResult::fromIndexRange)
+                .collect(Collectors.toSet());
     }
 
     private ImmutableSet<Parameter> resolveParameters(final ValidationRequestDTO validationRequest) {
