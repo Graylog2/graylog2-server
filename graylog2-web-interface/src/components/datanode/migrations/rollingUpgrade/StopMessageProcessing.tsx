7d6b2919d27faf75fea775b6c17e67faf69d2800
Ousmane SAMBA <ousmane@graylog.com>
2024-02-22 11:51:31 +0100

    Add data node migration wizard (#17768)
    * Add data node migration wizard
    
    * update useMigrationStep
    
    - rename file
    
    * add compatibility details
    
    * add CA and cert renewal steps
    
    * add compatibility check test
    
    * add default param to useDatanodes
    
    * update help text
    
    * use migrationStep hook in wizard
    
    * update types
    
    * remove WIP
    
    * add license header
    
    * use mobilitycheck api
    
    * add test
    
    * add datanode list, and skip check to welcome step
    
    * add migration state machine api hooks
    
    * integrate state machine
    
    * refactor wizard to use new statemachine state
    
    * Add shutdown and finish to wizard
    
    * add changelog
    
    * refactor wizard
    
    * update compatibility check
    
    * initial component setup
    
    * initial setup RemoteReindexingMigration component
    
    * added useRemoteReindexMigrationStatus hook
    
    * Remote reindexing migration steps polishing
    
    * refaor wizard to use generic next step buttons
    
    * changes to remote reindexing steps
    
    * fix tests eslint issues
    
    * integrated new wizard changes
    
    * cleanup
    
    * integrate provisioning through statemachine
    
    * extrat journal warning alert
    
    * integrated new changes
    
    * cleanup and refactor
    
    - invalidate MIGRATION_STATE_QUERY_KEY when CA and Renewal change
    - refactor Journal warning
    - delete unsused component
    
    * fix linter
    
    * fix useTriggerMigrationState
    
    * remote reindex new steps and actions integration
    
    * fix eslint issues
    
    * cleanup
    
    * cleanup MigrateExistingData
    
    * integrate new step and fix typo
    
    * add replace cluster component
    
    * ConnectionStringRemovalStep change
    
    * cleanup RemoteReindexRunning step
    
    * added response and error_message to MigrationState type
    
    * fix eslint issues
    
    * fix test
    
    * fix types
    
    * check for provisioning state instead of data node state as data node state never transitions to PREPARED
    
    * fix buttons overflow
    
    * add restart graylog step
    
    * handle errors
    
    * fix data node startup trigger
    
    * finalized RemoteReindexRunning step
    
    * fix eslint issue
    
    * show stop opensearch warning
    
    * added ResetMigrationButton
    
    * fix review comments
    
    * add CA form type
    
    * add reset migration button
    
    * fix review
    
    * fix review Remote reindexing errors on first steps
    
    * fix review wording
    
    * fix typos
    
    * fix review typos
    
    ---------
    
    Co-authored-by: Mohamed OULD HOCINE <106236152+gally47@users.noreply.github.com>
    Co-authored-by: Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
    Co-authored-by: Matthias Oesterheld <matthias.oesterheld@graylog.com>
    Co-authored-by: Jan Heise <jan.heise@graylog.com>

---
+++ graylog2-web-interface/src/components/datanode/migrations/rollingUpgrade/StopMessageProcessing.tsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import styled from 'styled-components';
+
+import MigrationStepTriggerButtonToolbar from 'components/datanode/migrations/common/MigrationStepTriggerButtonToolbar';
+import type { MigrationStepComponentProps } from 'components/datanode/Types';
+import { Panel } from 'components/bootstrap';
+import { Icon } from 'components/common';
+import { StyledPanel } from 'components/datanode/migrations/MigrationWelcomeStep';
+
+const StyledHelpPanel = styled(StyledPanel)`
+  margin-top: 30px;
+`;
+
+const StopMessageProcessing = ({ currentStep, onTriggerStep }: MigrationStepComponentProps) => (
+  <>
+    <p>Graylog processing is stopped.</p>
+    <StyledHelpPanel bsStyle="warning">
+      <Panel.Heading>
+        <Panel.Title componentClass="h3"><Icon name="exclamation-triangle" />Stop OpenSearch</Panel.Title>
+      </Panel.Heading>
+      <Panel.Body>
+        <p>Please stop your OpenSearch cluster before proceeding.</p>
+      </Panel.Body>
+    </StyledHelpPanel>
+    <p />
+    <MigrationStepTriggerButtonToolbar nextSteps={currentStep.next_steps} onTriggerStep={onTriggerStep} />
+
+  </>
+);
+
+export default StopMessageProcessing;
