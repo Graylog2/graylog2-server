c56df4d8716d680ebfea312d29849acaf9201485
Dennis Oelkers <dennis@graylog.com>
2024-02-23 09:40:07 +0100

    Return better messages for JSON errors in API.

--- graylog2-server/src/main/java/org/graylog2/shared/rest/exceptionmappers/JsonMappingExceptionMapper.java
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/exceptionmappers/JsonMappingExceptionMapper.java
@@ -17,12 +17,15 @@
 package org.graylog2.shared.rest.exceptionmappers;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
-import org.graylog2.plugin.rest.ApiError;
-
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.graylog2.plugin.rest.ApiError;
+
+import java.util.stream.Collectors;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static jakarta.ws.rs.core.Response.status;
@@ -31,8 +34,32 @@ import static jakarta.ws.rs.core.Response.status;
 public class JsonMappingExceptionMapper implements ExceptionMapper<JsonMappingException> {
     @Override
     public Response toResponse(JsonMappingException e) {
-        final String message = firstNonNull(e.getMessage(), "Couldn't process JSON input");
-        final ApiError apiError = ApiError.create(message);
+        final String messagePrefix = errorWithJsonPath(e);
+        final String problemMessage = firstNonNull(e.getCause().getMessage(), e.getMessage());
+        final String location = "[" + e.getLocation().getLineNr() + ", " + e.getLocation().getColumnNr() + "]";
+        final String message = firstNonNull(problemMessage, "Couldn't process JSON input");
+        final ApiError apiError = ApiError.create("Error at " + location + ": " + messagePrefix + ": " + message);
         return status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(apiError).build();
+    }
+
+    private String errorWithJsonPath(final JsonMappingException e) {
+        final String pathToErrorField = e.getPath().stream()
+                .map(path -> {
+                    final var fieldName = path.getFieldName();
+                    if (fieldName == null && path.getIndex() != -1) {
+                        return "[" + path.getIndex() + "]";
+                    }
+                    return fieldName;
+                })
+                .collect(Collectors.joining("."));
+        final var quotedPath = "\"" + pathToErrorField + "\"";
+        if (e instanceof MismatchedInputException mismatchedInputException) {
+            return quotedPath + " must be of type " + mismatchedInputException.getTargetType().getSimpleName();
+        }
+        if (e instanceof ValueInstantiationException) {
+            return "Unable to construct " + quotedPath;
+        } else {
+            return "Error for " + quotedPath;
+        }
     }
 }
