Graylog2/graylog2-server

commit 04dee2f7ec2712caefd463e33c3b624d7999d096

Use different set of database types on cloud (#13911)
* Use different set of database types on cloud

* Add changelog

* Fix typo in MaxMindAdapterFieldSet

* Remove underscore for private functions in MaxmindAdapterFieldSet

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.tsx
@@ -1,0 +1,133 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import AppConfig from 'util/AppConfig';
+import ObjectUtils from 'util/ObjectUtils';
+import { Input } from 'components/bootstrap';
+import { Select, TimeUnitInput } from 'components/common';
+
+type MaxmindAdapterFieldSetProps = {
+  config: {
+    [name: string]: string | number;
+  };
+  updateConfig: (newConfig: object) => void,
+  handleFormEvent: (e: { target: { name: string; value?: string } }) => void;
+  validationState: (key: string) => void,
+  validationMessage: (key: string, message: string) => void,
+};
+
+const MaxmindAdapterFieldSet = ({ config, updateConfig, handleFormEvent, validationState, validationMessage }: MaxmindAdapterFieldSetProps) => {
+  const isCloud = AppConfig.isCloud();
+
+  const pathsForCloud = {
+    IPINFO_STANDARD_LOCATION: '/etc/graylog/server/standard_location.mmdb',
+    IPINFO_ASN: '/etc/graylog/server/asn.mmdb',
+  };
+
+  const ipInfoDatabaseTypes = [
+    { label: 'IPinfo location database', value: 'IPINFO_STANDARD_LOCATION' },
+    { label: 'IPinfo ASN database', value: 'IPINFO_ASN' },
+  ];
+
+  let databaseTypes = [
+    { label: 'ASN database', value: 'MAXMIND_ASN' },
+    { label: 'City database', value: 'MAXMIND_CITY' },
+    { label: 'Country database', value: 'MAXMIND_COUNTRY' },
+  ];
+
+  if (isCloud) {
+    databaseTypes = ipInfoDatabaseTypes;
+  } else {
+    databaseTypes.concat(ipInfoDatabaseTypes);
+  }
+
+  const update = (value: number, unit: string, enabled: boolean, name: string) => {
+    const newConfig = ObjectUtils.clone(config);
+
+    newConfig[name] = enabled ? value : 0;
+    newConfig[`${name}_unit`] = unit;
+    updateConfig(newConfig);
+  };
+
+  const updateCheckInterval = (value: number, unit: string, enabled: boolean) => {
+    update(value, unit, enabled, 'check_interval');
+  };
+
+  const onDbTypeSelect = (id: string) => {
+    const newConfig = ObjectUtils.clone(config);
+
+    if (isCloud) {
+      newConfig.path = pathsForCloud[id];
+    }
+
+    newConfig.database_type = id;
+    updateConfig(newConfig);
+  };
+
+  return (
+    <fieldset>
+      {!isCloud && (
+      <Input type="text"
+             id="path"
+             name="path"
+             label="File path"
+             autoFocus
+             required
+             onChange={handleFormEvent}
+             help={validationMessage('path', 'The path to the database file.')}
+             bsStyle={validationState('path')}
+             value={config.path}
+             labelClassName="col-sm-3"
+             wrapperClassName="col-sm-9" />
+      )}
+      <Input id="database-type-select"
+             label="Database type"
+             required
+             autoFocus
+             help="Select the type of the database file"
+             labelClassName="col-sm-3"
+             wrapperClassName="col-sm-9">
+        <Select placeholder="Select the type of database file"
+                clearable={false}
+                options={databaseTypes}
+                matchProp="label"
+                onChange={onDbTypeSelect}
+                value={config.database_type} />
+      </Input>
+      <TimeUnitInput label="Refresh file"
+                     help="If enabled, the database file is checked for modifications and refreshed when it changed on disk."
+                     update={updateCheckInterval}
+                     value={config.check_interval}
+                     unit={config.check_interval_unit || 'MINUTES'}
+                     defaultEnabled={config.check_interval > 0}
+                     labelClassName="col-sm-3"
+                     wrapperClassName="col-sm-9" />
+    </fieldset>
+  );
+};
+
+MaxmindAdapterFieldSet.propTypes = {
+  config: PropTypes.object.isRequired,
+  updateConfig: PropTypes.func.isRequired,
+  handleFormEvent: PropTypes.func.isRequired,
+  validationState: PropTypes.func.isRequired,
+  validationMessage: PropTypes.func.isRequired,
+};
+
+export default MaxmindAdapterFieldSet;

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.jsx
@@ -1,103 +1,0 @@
-/*
- * Copyright (C) 2020 Graylog, Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
- *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
- */
-import PropTypes from 'prop-types';
-import React from 'react';
-
-import ObjectUtils from 'util/ObjectUtils';
-import { Input } from 'components/bootstrap';
-import { Select, TimeUnitInput } from 'components/common';
-
-class MaxmindAdapterFieldSet extends React.Component {
-  static propTypes = {
-    config: PropTypes.object.isRequired,
-    updateConfig: PropTypes.func.isRequired,
-    handleFormEvent: PropTypes.func.isRequired,
-    validationState: PropTypes.func.isRequired,
-    validationMessage: PropTypes.func.isRequired,
-  };
-
-  _update = (value, unit, enabled, name) => {
-    const config = ObjectUtils.clone(this.props.config);
-
-    config[name] = enabled ? value : 0;
-    config[`${name}_unit`] = unit;
-    this.props.updateConfig(config);
-  };
-
-  updateCheckInterval = (value, unit, enabled) => {
-    this._update(value, unit, enabled, 'check_interval');
-  };
-
-  _onDbTypeSelect = (id) => {
-    const config = ObjectUtils.clone(this.props.config);
-
-    config.database_type = id;
-    this.props.updateConfig(config);
-  };
-
-  render() {
-    const { config } = this.props;
-    const databaseTypes = [
-      { label: 'ASN database', value: 'MAXMIND_ASN' },
-      { label: 'City database', value: 'MAXMIND_CITY' },
-      { label: 'Country database', value: 'MAXMIND_COUNTRY' },
-      { label: 'IPinfo location database', value: 'IPINFO_STANDARD_LOCATION' },
-      { label: 'IPinfo ASN database', value: 'IPINFO_ASN' },
-    ];
-
-    return (
-      <fieldset>
-        <Input type="text"
-               id="path"
-               name="path"
-               label="File path"
-               autoFocus
-               required
-               onChange={this.props.handleFormEvent}
-               help={this.props.validationMessage('path', 'The path to the database file.')}
-               bsStyle={this.props.validationState('path')}
-               value={config.path}
-               labelClassName="col-sm-3"
-               wrapperClassName="col-sm-9" />
-        <Input id="database-type-select"
-               label="Database type"
-               required
-               autoFocus
-               help="Select the type of the database file"
-               labelClassName="col-sm-3"
-               wrapperClassName="col-sm-9">
-          <Select placeholder="Select the type of database file"
-                  clearable={false}
-                  options={databaseTypes}
-                  matchProp="label"
-                  onChange={this._onDbTypeSelect}
-                  value={config.database_type} />
-        </Input>
-        <TimeUnitInput label="Refresh file"
-                       help="If enabled, the database file is checked for modifications and refreshed when it changed on disk."
-                       update={this.updateCheckInterval}
-                       value={config.check_interval}
-                       unit={config.check_interval_unit || 'MINUTES'}
-                       defaultEnabled={config.check_interval > 0}
-                       labelClassName="col-sm-3"
-                       wrapperClassName="col-sm-9" />
-      </fieldset>
-    );
-  }
-}
-
-export default MaxmindAdapterFieldSet;

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.test.tsx
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import React from 'react';
+import { render, screen } from 'wrappedTestingLibrary';
+
+import asMock from 'helpers/mocking/AsMock';
+import AppConfig from 'util/AppConfig';
+
+import MaxmindAdapterFieldSet from './MaxmindAdapterFieldSet';
+
+jest.mock('util/AppConfig', () => ({
+  gl2AppPathPrefix: jest.fn(() => ''),
+  gl2ServerUrl: jest.fn(() => ''),
+  isCloud: jest.fn(() => false),
+}));
+
+describe('MaxmindAdapterFieldSet', () => {
+  describe('in cloud mode', () => {
+    beforeEach(() => {
+      asMock(AppConfig.isCloud).mockImplementation(() => true);
+    });
+
+    it('renders MaxmindAdapterFieldSet without path input', async () => {
+      render(<MaxmindAdapterFieldSet config={{}}
+                                     updateConfig={() => {}}
+                                     handleFormEvent={() => {}}
+                                     validationState={() => {}}
+                                     validationMessage={() => {}} />);
+
+      expect(screen.getByText(/select the type of the database file/i)).toBeInTheDocument();
+
+      expect(screen.queryByRole('textbox', { name: /file path/i })).not.toBeInTheDocument();
+    });
+  });
+
+  describe('not in cloud mode', () => {
+    beforeEach(() => {
+      asMock(AppConfig.isCloud).mockImplementation(() => false);
+    });
+
+    it('renders MaxmindAdapterFieldSet with path input', async () => {
+      render(<MaxmindAdapterFieldSet config={{}}
+                                     updateConfig={() => {}}
+                                     handleFormEvent={() => {}}
+                                     validationState={() => {}}
+                                     validationMessage={() => {}} />);
+
+      expect(screen.getByText(/Select the type of the database file/i)).toBeInTheDocument();
+
+      expect(screen.getByRole('textbox', { name: /file path/i })).toBeInTheDocument();
+    });
+  });
+});

commit 4a46e2b0d6b04ef652e2e8f673a17ef1b5ccdbc1

Allow more db types for IPinfo mmdb files / reenable IPinfo in data adapter UI (#18124)
* Allow more db types for IPinfo mmdb files

IPinfo recently changed the info in their metadata.

old
```
mmdbctl metadata 24/asn.mmdb
- Binary Format 2.0
- Database Type ipinfo asn.mmdb
```

new
```
mmdbctl metadata newest/asn.mmdb
- Binary Format 2.0
- Database Type ipinfo generic_asn_mmdb_v4.mmdb
```

This is likely because they changed something
in their release process, because the db type is just their
import filename:

 https://github.com/ipinfo/mmdbctl/blob/master/lib/cmd_import.go#L269

Relax our db type match to support this.

Fixes https://github.com/Graylog2/graylog-plugin-enterprise/issues/6436

* Changelog

* Allow selecting IPinfo DB types in Data Adapters

This probably got broken with PR #13911

* Add config to disable IPinfo database type check

The database type check is only a convenience feature to
give a meaningful error if the wrong type of mmdb is used.

In case the upstream metadata format changes again,
allow the check to be disabled temporarly.

The check can be disabled using:
`geo_ip_processor_disable_ipinfo_db_type_check=true`

graylog2-server/src/main/java/org/graylog/plugins/map/config/GeoIpProcessorConfig.java
@@ -18,10 +18,9 @@ package org.graylog.plugins.map.config;
 
 import com.github.joschi.jadconfig.Parameter;
 import com.github.joschi.jadconfig.validators.PathReadableValidator;
+import jakarta.inject.Singleton;
 import org.graylog2.bindings.NamedBindingOverride;
 import org.graylog2.configuration.PathConfiguration;
-
-import jakarta.inject.Singleton;
 
 import java.nio.file.Path;
 import java.util.Optional;
@@ -30,6 +29,7 @@ import java.util.Optional;
 public class GeoIpProcessorConfig extends PathConfiguration {
     private static final String PREFIX = "geo_ip_processor";
     public static final String S3_DOWNLOAD_LOCATION = PREFIX + "_s3_download_location";
+    public static final String DISABLE_IPINFO_DB_TYPE_CHECK = PREFIX + "_disable_ipinfo_db_type_check";
 
     @Parameter(value = S3_DOWNLOAD_LOCATION, validators = PathReadableValidator.class)
     private Path s3DownloadLocation;
@@ -39,4 +39,6 @@ public class GeoIpProcessorConfig extends PathConfiguration {
         return Optional.ofNullable(s3DownloadLocation).orElse(getDataDir().resolve("geolocation"));
     }
 
+    @Parameter(value = DISABLE_IPINFO_DB_TYPE_CHECK)
+    private final boolean disableIpInfoDBTypeCheck = false;
 }

graylog2-server/src/main/java/org/graylog/plugins/map/geoip/IPinfoIPLocationDatabaseAdapter.java
@@ -34,14 +34,16 @@ import java.net.InetAddress;
  */
 public class IPinfoIPLocationDatabaseAdapter implements IPLocationDatabaseAdapter {
     private final Reader reader;
+    private final boolean disableIpInfoDbTypeCheck;
 
-    public IPinfoIPLocationDatabaseAdapter(File databaseFile) throws IOException {
+    public IPinfoIPLocationDatabaseAdapter(File databaseFile, boolean disableIpInfoDbTypeCheck) throws IOException {
         this.reader = new Reader(databaseFile, Reader.FileMode.MEMORY_MAPPED, NoCache.getInstance());
+        this.disableIpInfoDbTypeCheck = disableIpInfoDbTypeCheck;
     }
 
     private <T> T get(InetAddress ipAddress, String type, Class<T> valueClass) throws IOException, AddressNotFoundException {
         final String databaseType = reader.getMetadata().getDatabaseType();
-        if (!databaseType.contains(type)) {
+        if (!disableIpInfoDbTypeCheck && (!databaseType.contains("ipinfo") || !databaseType.contains(type))) {
             final String caller = Thread.currentThread().getStackTrace()[2].getMethodName();
             throw new UnsupportedOperationException("Invalid attempt to open a \"" + databaseType + "\" database using the " + caller + " method");
         }
@@ -56,12 +58,12 @@ public class IPinfoIPLocationDatabaseAdapter implements IPLocationDatabaseAdapte
 
     @Override
     public IPinfoStandardLocation ipInfoStandardLocation(InetAddress ipAddress) throws IOException, AddressNotFoundException {
-        return get(ipAddress, "ipinfo standard_location", IPinfoStandardLocation.class);
+        return get(ipAddress, "standard_location", IPinfoStandardLocation.class);
     }
 
     @Override
     public IPinfoASN ipInfoASN(InetAddress ipAddress) throws IOException, AddressNotFoundException {
-        return get(ipAddress, "ipinfo asn", IPinfoASN.class);
+        return get(ipAddress, "asn", IPinfoASN.class);
     }
 
     @Override

graylog2-server/src/main/java/org/graylog/plugins/map/geoip/IpInfoIpAsnResolver.java
@@ -20,13 +20,15 @@ package org.graylog.plugins.map.geoip;
 import com.codahale.metrics.Timer;
 import com.google.inject.assistedinject.Assisted;
 import com.maxmind.geoip2.exception.AddressNotFoundException;
-
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Locale;
 import java.util.Optional;
+
+import static org.graylog.plugins.map.config.GeoIpProcessorConfig.DISABLE_IPINFO_DB_TYPE_CHECK;
 
 /**
  * A {@link GeoIpResolver} to load IP ASN data from {@link org.graylog.plugins.map.config.DatabaseVendorType#IPINFO}.
@@ -36,8 +38,9 @@ public class IpInfoIpAsnResolver extends IpInfoIpResolver<GeoAsnInformation> {
     @Inject
     public IpInfoIpAsnResolver(@Assisted Timer timer,
                                @Assisted String configPath,
-                               @Assisted boolean enabled) {
-        super(timer, configPath, enabled);
+                               @Assisted boolean enabled,
+                               @Named(DISABLE_IPINFO_DB_TYPE_CHECK) boolean disableIpInfoDbTypeCheck) {
+        super(timer, configPath, enabled, disableIpInfoDbTypeCheck);
     }
 
     @Override

graylog2-server/src/main/java/org/graylog/plugins/map/geoip/IpInfoIpResolver.java
@@ -26,17 +26,19 @@ import java.io.IOException;
 
 abstract class IpInfoIpResolver<T> extends GeoIpResolver<T> {
     protected static final Logger LOG = LoggerFactory.getLogger(IpInfoIpResolver.class);
+    private final boolean disableIpInfoDbTypeCheck;
     protected IPinfoIPLocationDatabaseAdapter adapter;
 
-    IpInfoIpResolver(Timer resolveTime, String configPath, boolean enabled) {
+    IpInfoIpResolver(Timer resolveTime, String configPath, boolean enabled, boolean disableIpInfoDbTypeCheck) {
         super(resolveTime, configPath, enabled);
+        this.disableIpInfoDbTypeCheck = disableIpInfoDbTypeCheck;
     }
 
     @Override
     boolean createDataProvider(File configFile) {
 
         try {
-            adapter = new IPinfoIPLocationDatabaseAdapter(configFile);
+            adapter = new IPinfoIPLocationDatabaseAdapter(configFile, disableIpInfoDbTypeCheck);
         } catch (IOException e) {
             LOG.warn("Error creating IPinfoIPLocationDatabaseAdapter for '{}' from file '{}'", getClass().getSimpleName(), configFile);
             adapter = null;

graylog2-server/src/main/java/org/graylog/plugins/map/geoip/IpInfoLocationResolver.java
@@ -20,13 +20,15 @@ package org.graylog.plugins.map.geoip;
 import com.codahale.metrics.Timer;
 import com.google.inject.assistedinject.Assisted;
 import com.maxmind.geoip2.exception.AddressNotFoundException;
-
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Locale;
 import java.util.Optional;
+
+import static org.graylog.plugins.map.config.GeoIpProcessorConfig.DISABLE_IPINFO_DB_TYPE_CHECK;
 
 /**
  * A {@link GeoIpResolver} to load IP Location data from {@link org.graylog.plugins.map.config.DatabaseVendorType#IPINFO}.
@@ -36,8 +38,9 @@ public class IpInfoLocationResolver extends IpInfoIpResolver<GeoLocationInformat
     @Inject
     public IpInfoLocationResolver(@Assisted Timer resolveTime,
                                   @Assisted String configPath,
-                                  @Assisted boolean enabled) {
-        super(resolveTime, configPath, enabled);
+                                  @Assisted boolean enabled,
+                                  @Named(DISABLE_IPINFO_DB_TYPE_CHECK) boolean disableIpInfoDbTypeCheck) {
+        super(resolveTime, configPath, enabled, disableIpInfoDbTypeCheck);
     }
 
     @Override

graylog2-server/src/main/java/org/graylog/plugins/map/geoip/MaxmindDataAdapter.java
@@ -34,6 +34,11 @@ import com.maxmind.geoip2.model.CityResponse;
 import com.maxmind.geoip2.model.CountryResponse;
 import com.maxmind.geoip2.record.Country;
 import com.maxmind.geoip2.record.Location;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.graylog.autovalue.WithBeanGetter;
 import org.graylog.plugins.map.config.DatabaseType;
 import org.graylog2.plugin.lookup.LookupCachePurge;
@@ -45,13 +50,6 @@ import org.joda.time.Duration;
 import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
-
-import jakarta.inject.Inject;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -65,6 +63,7 @@ import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static org.graylog.plugins.map.config.GeoIpProcessorConfig.DISABLE_IPINFO_DB_TYPE_CHECK;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MaxmindDataAdapter extends LookupDataAdapter {
@@ -73,6 +72,7 @@ public class MaxmindDataAdapter extends LookupDataAdapter {
 
     public static final String NAME = "maxmind_geoip";
     private final Config config;
+    private final boolean disableIpInfoDbTypeCheck;
     private final AtomicReference<IPLocationDatabaseAdapter> databaseAdapter = new AtomicReference<>();
     private FileInfo fileInfo = FileInfo.empty();
 
@@ -80,9 +80,11 @@ public class MaxmindDataAdapter extends LookupDataAdapter {
     protected MaxmindDataAdapter(@Assisted("id") String id,
                                  @Assisted("name") String name,
                                  @Assisted LookupDataAdapterConfiguration config,
+                                 @Named(DISABLE_IPINFO_DB_TYPE_CHECK) boolean disableIpInfoDbTypeCheck,
                                  MetricRegistry metricRegistry) {
         super(id, name, config, metricRegistry);
         this.config = (Config) config;
+        this.disableIpInfoDbTypeCheck = disableIpInfoDbTypeCheck;
     }
 
     @Override
@@ -157,7 +159,7 @@ public class MaxmindDataAdapter extends LookupDataAdapter {
                 return new MaxMindIPLocationDatabaseAdapter(file);
             case IPINFO_ASN:
             case IPINFO_STANDARD_LOCATION:
-                return new IPinfoIPLocationDatabaseAdapter(file);
+                return new IPinfoIPLocationDatabaseAdapter(file, disableIpInfoDbTypeCheck);
             default:
                 throw new IllegalStateException("Unexpected value: " + config.dbType());
         }

graylog2-web-interface/src/components/maps/adapter/MaxmindAdapterFieldSet.tsx
@@ -58,7 +58,7 @@ const MaxmindAdapterFieldSet = ({ config, updateConfig, handleFormEvent, validat
   if (isCloud) {
     databaseTypes = ipInfoDatabaseTypes;
   } else {
-    databaseTypes.concat(ipInfoDatabaseTypes);
+    databaseTypes = databaseTypes.concat(ipInfoDatabaseTypes);
   }
 
   const update = (value: number, unit: string, enabled: boolean, name: string) => {

graylog2-server/src/test/java/org/graylog/plugins/map/geoip/MaxmindDataAdapterTest.java
@@ -40,12 +40,12 @@ import static org.mockito.Mockito.when;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-                            MaxmindDataAdapterTest.CityDatabaseTest.class,
-                            MaxmindDataAdapterTest.CountryDatabaseTest.class,
-                            MaxmindDataAdapterTest.AsnDatabaseTest.class,
-                            MaxmindDataAdapterTest.IPinfoStandardLocationDatabaseTest.class,
-                            MaxmindDataAdapterTest.IPinfoASNDatabaseTest.class,
-                    })
+        MaxmindDataAdapterTest.CityDatabaseTest.class,
+        MaxmindDataAdapterTest.CountryDatabaseTest.class,
+        MaxmindDataAdapterTest.AsnDatabaseTest.class,
+        MaxmindDataAdapterTest.IPinfoStandardLocationDatabaseTest.class,
+        MaxmindDataAdapterTest.IPinfoASNDatabaseTest.class,
+})
 public class MaxmindDataAdapterTest {
     private static final String GEO_LITE2_CITY_MMDB = "/GeoLite2-City.mmdb";
     private static final String GEO_LITE2_COUNTRY_MMDB = "/GeoLite2-Country.mmdb";
@@ -78,7 +78,7 @@ public class MaxmindDataAdapterTest {
                     .path(getTestDatabasePath(DB_PATH.get(databaseType)))
                     .type("test")
                     .build();
-            adapter = new MaxmindDataAdapter("test", "test", config, new MetricRegistry());
+            adapter = new MaxmindDataAdapter("test", "test", config, false, new MetricRegistry());
 
             adapter.doStart();
         }