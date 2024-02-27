527da6b041ccdcfb7cebe2074ae7092e57aa21fc
Linus Pahl <linus@graylog.com>
2024-02-27 10:23:42 +0100

    Further improvements for icon usage. (#18392)
    * Rename `iconClass` to `iconName`.
    
    * Use more meaningful name for keyboard arrow icons.
    
    * Fixing typo
    
    * Improve search page icons.
    
    * Change documentation link icon.

--- graylog2-web-interface/src/components/content-stream/news/ContentStreamNewsContentActions.tsx
+++ graylog2-web-interface/src/components/content-stream/news/ContentStreamNewsContentActions.tsx
@@ -46,8 +46,8 @@ const ContentStreamNewsContentActions = () => {
 
   return (
     <>
-      <Button onClick={() => handlePrev()}><Icon name="keyboard_arrow_left" /></Button>
-      <Button onClick={() => handleNext()}><Icon name="keyboard_arrow_right" /></Button>
+      <Button onClick={() => handlePrev()}><Icon name="chevron_left" /></Button>
+      <Button onClick={() => handleNext()}><Icon name="chevron_right" /></Button>
     </>
   );
 };
