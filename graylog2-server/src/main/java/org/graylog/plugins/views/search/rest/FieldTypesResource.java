6ec3aba9b74732fa9ccafa99b27b103278c53c44
Dennis Oelkers <dennis@graylog.com>
2024-02-29 10:41:57 +0100

    Adding endpoint to trigger full types refresh.

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/rest/FieldTypesResource.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/rest/FieldTypesResource.java
@@ -20,25 +20,25 @@ import com.google.common.collect.ImmutableSet;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.apache.shiro.authz.annotation.RequiresAuthentication;
-import org.graylog.plugins.views.search.permissions.SearchUser;
-import org.graylog2.audit.jersey.NoAuditEvent;
-import org.graylog2.indexer.fieldtypes.MappedFieldTypesService;
-import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
-import org.graylog2.plugin.rest.PluginRestResource;
-import org.graylog2.shared.rest.resources.RestResource;
-
 import jakarta.inject.Inject;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.graylog.plugins.views.search.permissions.SearchUser;
+import org.graylog2.audit.jersey.NoAuditEvent;
+import org.graylog2.indexer.fieldtypes.IndexFieldTypePollerPeriodical;
+import org.graylog2.indexer.fieldtypes.MappedFieldTypesService;
+import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
+import org.graylog2.plugin.rest.PluginRestResource;
+import org.graylog2.shared.rest.resources.RestResource;
 
 import java.util.Set;
 
@@ -50,10 +50,12 @@ import static org.graylog2.shared.rest.documentation.generator.Generator.CLOUD_V
 @RequiresAuthentication
 public class FieldTypesResource extends RestResource implements PluginRestResource {
     private final MappedFieldTypesService mappedFieldTypesService;
+    private final IndexFieldTypePollerPeriodical fieldTypePoller;
 
     @Inject
-    public FieldTypesResource(MappedFieldTypesService mappedFieldTypesService) {
+    public FieldTypesResource(MappedFieldTypesService mappedFieldTypesService, IndexFieldTypePollerPeriodical fieldTypePoller) {
         this.mappedFieldTypesService = mappedFieldTypesService;
+        this.fieldTypePoller = fieldTypePoller;
     }
 
     @GET
@@ -71,5 +73,15 @@ public class FieldTypesResource extends RestResource implements PluginRestResour
                                              @Context SearchUser searchUser) {
         final ImmutableSet<String> streams = searchUser.streams().readableOrAllIfEmpty(request.streams());
         return mappedFieldTypesService.fieldTypesByStreamIds(streams, request.timerange().orElse(RelativeRange.allTime()));
+    }
+
+    @POST
+    @ApiOperation(value = "Retrieve the field list of a given set of streams")
+    @Path("/poll")
+    @RequiresPermissions("*")
+    public Response triggerFieldTypePolling() {
+        fieldTypePoller.triggerFullRefresh();
+
+        return Response.noContent().build();
     }
 }
