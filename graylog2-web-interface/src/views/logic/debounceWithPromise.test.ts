d91681067422921a398ed9ddd11dccdd431b68a9
Dennis Oelkers <dennis@graylog.com>
2024-03-04 10:08:47 +0100

    Fix debouncing async functions. (#18450)
    * Fix debouncing async functions.
    
    * Adding license header.

--- graylog2-web-interface/src/views/logic/debounceWithPromise.test.ts
+++ graylog2-web-interface/src/views/logic/debounceWithPromise.test.ts
@@ -14,8 +14,26 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import debounce from 'p-debounce';
+import debounceWithPromise from './debounceWithPromise';
 
-const debounceWithPromise = debounce;
+describe('debounceWithPromise', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
 
-export default debounceWithPromise;
+  it('never returns a pending promise', async () => {
+    const fn = jest.fn(async (attempt: number) => attempt);
+
+    const debouncedFn = debounceWithPromise(fn, 300);
+
+    const result1 = debouncedFn(1);
+    const result2 = debouncedFn(2);
+    const result3 = debouncedFn(3);
+
+    jest.advanceTimersByTime(400);
+
+    await expect(result1).resolves.toBe(3);
+    await expect(result2).resolves.toBe(3);
+    await expect(result3).resolves.toBe(3);
+  });
+});
