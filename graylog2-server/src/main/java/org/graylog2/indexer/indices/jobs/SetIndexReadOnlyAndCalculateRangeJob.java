210eb5b629d0ca12aaa7fee91edddae88a85a86b
Anton Ebel <anton.ebel@graylog.com>
2024-03-12 14:06:46 +0100

    refactor: set index readonly

--- graylog2-server/src/main/java/org/graylog2/indexer/indices/jobs/SetIndexReadOnlyAndCalculateRangeJob.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/indices/jobs/SetIndexReadOnlyAndCalculateRangeJob.java
@@ -18,40 +18,53 @@ package org.graylog2.indexer.indices.jobs;
 
 import com.google.inject.assistedinject.Assisted;
 import jakarta.inject.Inject;
+import org.graylog2.indexer.IndexSet;
 import org.graylog2.indexer.IndexSetRegistry;
-import org.graylog2.indexer.SetIndexReadOnlyJob;
 import org.graylog2.indexer.fieldtypes.IndexFieldTypePoller;
 import org.graylog2.indexer.fieldtypes.IndexFieldTypesService;
 import org.graylog2.indexer.indices.Indices;
 import org.graylog2.indexer.ranges.CreateNewSingleIndexRangeJob;
+import org.graylog2.plugin.Tools;
+import org.graylog2.shared.system.activities.Activity;
+import org.graylog2.shared.system.activities.ActivityWriter;
 import org.graylog2.system.jobs.SystemJob;
+import org.graylog2.system.jobs.SystemJobConcurrencyException;
+import org.graylog2.system.jobs.SystemJobManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 public class SetIndexReadOnlyAndCalculateRangeJob extends SystemJob {
     private static final Logger LOG = LoggerFactory.getLogger(SetIndexReadOnlyAndCalculateRangeJob.class);
-    private final SetIndexReadOnlyJob.Factory setIndexReadOnlyJobFactory;
+    private final OptimizeIndexJob.Factory optimizeIndexJobFactory;
     private final CreateNewSingleIndexRangeJob.Factory createNewSingleIndexRangeJobFactory;
     private final IndexSetRegistry indexSetRegistry;
     private final Indices indices;
     private final IndexFieldTypesService indexFieldTypesService;
     private final IndexFieldTypePoller indexFieldTypePoller;
+    private final ActivityWriter activityWriter;
+    private final SystemJobManager systemJobManager;
     private final String indexName;
 
     @Inject
-    public SetIndexReadOnlyAndCalculateRangeJob(SetIndexReadOnlyJob.Factory setIndexReadOnlyJobFactory,
+    public SetIndexReadOnlyAndCalculateRangeJob(OptimizeIndexJob.Factory optimizeIndexJobFactory,
                                                 CreateNewSingleIndexRangeJob.Factory createNewSingleIndexRangeJobFactory,
                                                 IndexSetRegistry indexSetRegistry,
                                                 Indices indices,
                                                 IndexFieldTypesService indexFieldTypesService,
                                                 IndexFieldTypePoller indexFieldTypePoller,
+                                                ActivityWriter activityWriter,
+                                                SystemJobManager systemJobManager,
                                                 @Assisted String indexName) {
-        this.setIndexReadOnlyJobFactory = setIndexReadOnlyJobFactory;
+        this.optimizeIndexJobFactory = optimizeIndexJobFactory;
         this.createNewSingleIndexRangeJobFactory = createNewSingleIndexRangeJobFactory;
         this.indexSetRegistry = indexSetRegistry;
         this.indices = indices;
         this.indexFieldTypesService = indexFieldTypesService;
         this.indexFieldTypePoller = indexFieldTypePoller;
+        this.activityWriter = activityWriter;
+        this.systemJobManager = systemJobManager;
         this.indexName = indexName;
     }
 
@@ -65,8 +78,7 @@ public class SetIndexReadOnlyAndCalculateRangeJob extends SystemJob {
             LOG.debug("Not running job for closed index <{}>", indexName);
             return;
         }
-        final SetIndexReadOnlyJob setIndexReadOnlyJob = setIndexReadOnlyJobFactory.create(indexName);
-        setIndexReadOnlyJob.execute();
+        setReadonly();
         final SystemJob createNewSingleIndexRangeJob = createNewSingleIndexRangeJobFactory.create(indexSetRegistry.getAll(), indexName);
         createNewSingleIndexRangeJob.execute();
 
@@ -76,6 +88,40 @@ public class SetIndexReadOnlyAndCalculateRangeJob extends SystemJob {
                     indexFieldTypePoller.pollIndex(indexName, indexSet.getConfig().id())
                             .ifPresent(indexFieldTypesService::upsert);
                 });
+    }
+
+    public String getIndex() {
+        return indexName;
+    }
+
+    public void setReadonly() {
+        final Optional<IndexSet> indexSet = indexSetRegistry.getForIndex(indexName);
+
+        if (indexSet.isEmpty()) {
+            LOG.error("Couldn't find index set for index <{}>", indexName);
+            return;
+        }
+
+        LOG.info("Flushing old index <{}>.", indexName);
+        indices.flush(indexName);
+
+        // Record the time an index was set read-only.
+        // We call this the "closing date" because it denotes when we stopped writing to it.
+        indices.setClosingDate(indexName, Tools.nowUTC());
+
+        LOG.info("Setting old index <{}> to read-only.", indexName);
+        indices.setReadOnly(indexName);
+
+        activityWriter.write(new Activity("Flushed and set <" + indexName + "> to read-only.", SetIndexReadOnlyAndCalculateRangeJob.class));
+
+        if (!indexSet.get().getConfig().indexOptimizationDisabled()) {
+            try {
+                systemJobManager.submit(optimizeIndexJobFactory.create(indexName, indexSet.get().getConfig().indexOptimizationMaxNumSegments()));
+            } catch (SystemJobConcurrencyException e) {
+                // The concurrency limit is very high. This should never happen.
+                LOG.error("Cannot optimize index <" + indexName + ">.", e);
+            }
+        }
     }
 
     @Override
