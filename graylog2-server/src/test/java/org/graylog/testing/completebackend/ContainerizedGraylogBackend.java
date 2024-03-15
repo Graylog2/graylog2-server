33a9c9c0dd1e99cafe7f6a684002dc65023b3778
Jan Heise <jan.heise@graylog.com>
2024-03-15 14:38:00 +0100

    adding skip flag

--- graylog2-server/src/test/java/org/graylog/testing/completebackend/ContainerizedGraylogBackend.java
+++ graylog2-server/src/test/java/org/graylog/testing/completebackend/ContainerizedGraylogBackend.java
@@ -38,6 +38,8 @@ import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.stream.Collectors;
 
+import static org.graylog.testing.graylognode.NodeContainerConfig.flagFromEnvVar;
+
 public class ContainerizedGraylogBackend implements GraylogBackend, AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(ContainerizedGraylogBackend.class);
     public static final String PASSWORD_SECRET = "M4lteserKreuzHerrStrack?-warZuKurzDeshalbMussdaNochWasdranHasToBeAtLeastSixtyFourCharactersInLength";
@@ -81,7 +83,10 @@ public class ContainerizedGraylogBackend implements GraylogBackend, AutoCloseabl
         var mongoDB = services.getMongoDBInstance();
         mongoDB.importFixtures(mongoDBFixtures);
 
-        MavenPackager.packageJarIfNecessary(mavenProjectDirProvider);
+        var skipPackaging = flagFromEnvVar("GRAYLOG_IT_SKIP_PACKAGING");
+        if(!skipPackaging) {
+            MavenPackager.packageJarIfNecessary(mavenProjectDirProvider);
+        }
 
         if (preImportLicense) {
             createLicenses(mongoDB, "GRAYLOG_LICENSE_STRING", "GRAYLOG_SECURITY_LICENSE_STRING");
