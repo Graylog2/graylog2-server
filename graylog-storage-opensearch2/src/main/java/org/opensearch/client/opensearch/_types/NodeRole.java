2042f929e459f2c38ec3bf6f803986f22c87999d
Dennis Oelkers <dennis@graylog.com>
2024-03-15 09:35:43 +0100

    Monkey-patching `NodeRole` class to avoid role parsing bug.

---
+++ graylog-storage-opensearch2/src/main/java/org/opensearch/client/opensearch/_types/NodeRole.java
@@ -0,0 +1,86 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.client.opensearch._types;
+
+import org.opensearch.client.json.JsonEnum;
+import org.opensearch.client.json.JsonpDeserializable;
+
+@JsonpDeserializable
+public enum NodeRole implements JsonEnum {
+    @Deprecated
+    Master("master"),
+
+    ClusterManager("cluster_manager"),
+
+    Data("data"),
+
+    DataCold("data_cold"),
+
+    DataContent("data_content"),
+
+    DataFrozen("data_frozen"),
+
+    DataHot("data_hot"),
+
+    DataWarm("data_warm"),
+
+    Client("client"),
+
+    Ingest("ingest"),
+
+    Ml("ml"),
+
+    VotingOnly("voting_only"),
+
+    Transform("transform"),
+
+    RemoteClusterClient("remote_cluster_client"),
+
+    CoordinatingOnly("coordinating_only"),
+
+    Search("search"),
+
+    ;
+
+    private final String jsonValue;
+
+    NodeRole(String jsonValue) {
+        this.jsonValue = jsonValue;
+    }
+
+    public String jsonValue() {
+        return this.jsonValue;
+    }
+
+    public static final JsonEnum.Deserializer<NodeRole> _DESERIALIZER = new JsonEnum.Deserializer<>(NodeRole.values());
+}
