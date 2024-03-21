a81ef980907bd9844565929ee62dedcf7539d62b
Kay Roepke <kroepke@googlemail.com>
2024-03-21 18:20:31 +0100

    check for correct use of permission annotations on resource methods

---
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/NoPermissionCheckRequired.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.shared.rest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks resource methods that should be accessible without authentication and thus have no {@link org.apache.shiro.authz.annotation.RequiresPermissions @RequiresPermissions} annotation.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface NoPermissionCheckRequired {
+}
