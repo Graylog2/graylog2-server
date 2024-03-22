9c6058347a0cd854498e0c66c1db576f1252e2ed
Tomas Dvorak <tomas.dvorak@graylog.com>
2024-03-22 10:13:38 +0100

    make node roles configurable (#18743)

--- data-node/src/main/java/org/graylog/datanode/Configuration.java
+++ data-node/src/main/java/org/graylog/datanode/Configuration.java
@@ -256,6 +256,9 @@ public class Configuration extends BaseConfiguration {
     @Parameter(value = "opensearch_indices_query_bool_max_clause_count")
     private Integer indicesQueryBoolMaxClauseCount = 32768;
 
+    @Parameter(value = "node_roles", converter = StringListConverter.class)
+    private List<String> nodeRoles = List.of("cluster_manager", "data", "ingest", "remote_cluster_client", "search");
+
     public Integer getIndicesQueryBoolMaxClauseCount() {
         return indicesQueryBoolMaxClauseCount;
     }
@@ -680,5 +683,9 @@ public class Configuration extends BaseConfiguration {
 
     public List<String> getPathRepo() {
         return pathRepo;
+    }
+
+    public List<String> getNodeRoles() {
+        return nodeRoles;
     }
 }
