506d0595b0d17a5752c22cf18f3ea2c80038cd75
Marco Pfatschbacher <marco@graylog.com>
2024-03-22 09:18:30 +0000

    Fix several search export client issues
     - The export client did not report any errors if only individual shards fail.
    
     - The default sort order was wrong.
       We did sort by "timestamp.ASC, timestamp.DESC, gl2_second_sort_field.DESC"
       Change this to: "timestamp.ASC, gl2_second_sort_field.ASC"
    
     - The ExportBackendIT tests were producing errors because the templates
       were applied for '*' which included the ism indices and their
       compound templates. Maybe we can use `graylog_*` for all tests, but
       for now make the wildcard configurable for each test.
    
     - Add proper cleanup between each ExportBackendIT test
    
     - Add missing mocking. some tests were running against all indices
       which is something that never happens in the real world.
    
     - Add test for the missing sort field (gl2_second_sort_field) fix
    
     - Add test for reporting individual shard failures

--- graylog2-server/src/test/java/org/graylog/testing/elasticsearch/ElasticsearchBaseTest.java
+++ graylog2-server/src/test/java/org/graylog/testing/elasticsearch/ElasticsearchBaseTest.java
@@ -47,13 +47,17 @@ public abstract class ElasticsearchBaseTest {
         }
     }
 
+    public String messageTemplateIndexPattern() {
+        return "*";
+    }
+
     private void addGraylogDefaultIndexTemplate() {
         addIndexTemplates(getGraylogDefaultMessageTemplates(searchServer().version()));
     }
 
-    private static Map<String, Template> getGraylogDefaultMessageTemplates(SearchVersion version) {
+    private Map<String, Template> getGraylogDefaultMessageTemplates(SearchVersion version) {
         var template = new MessageIndexTemplateProvider().create(version, null)
-                .messageTemplate("*", "standard", 100L, null);
+                .messageTemplate(messageTemplateIndexPattern(), "standard", 100L, null);
         return Collections.singletonMap("graylog-test-internal", template);
     }
 
