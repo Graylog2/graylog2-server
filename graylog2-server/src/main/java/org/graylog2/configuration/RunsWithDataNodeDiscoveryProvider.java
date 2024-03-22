98f59a895c8dff1e79bca78adb8dbb13adadab06
Tomas Dvorak <tomas.dvorak@graylog.com>
2024-03-22 11:13:17 +0100

    Fix RunsWithDatanode injection for insecure datanodes instances

--- graylog2-server/src/main/java/org/graylog2/configuration/RunsWithDataNodeDiscoveryProvider.java
+++ graylog2-server/src/main/java/org/graylog2/configuration/RunsWithDataNodeDiscoveryProvider.java
@@ -17,36 +17,23 @@
 package org.graylog2.configuration;
 
 import com.google.common.base.Suppliers;
-import org.graylog2.bootstrap.preflight.PreflightConfigResult;
-import org.graylog2.bootstrap.preflight.PreflightConfigService;
-import org.graylog2.cluster.Node;
-import org.graylog2.cluster.nodes.DataNodeDto;
-import org.graylog2.cluster.nodes.NodeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import jakarta.inject.Inject;
 import jakarta.inject.Provider;
+import org.graylog2.cluster.nodes.DataNodeDto;
+import org.graylog2.cluster.nodes.NodeService;
 
-import java.net.URI;
-import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 
 public class RunsWithDataNodeDiscoveryProvider implements Provider<Boolean> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(RunsWithDataNodeDiscoveryProvider.class);
-    private final PreflightConfigService preflightConfigService;
     private final NodeService<DataNodeDto> nodeService;
 
     private final Supplier<Boolean> resultsCachingSupplier;
 
     @Inject
     public RunsWithDataNodeDiscoveryProvider(
-            PreflightConfigService preflightConfigService,
             NodeService<DataNodeDto> nodeService) {
-        this.preflightConfigService = preflightConfigService;
         this.nodeService = nodeService;
         this.resultsCachingSupplier = Suppliers.memoize(this::doGet);
     }
@@ -57,24 +44,6 @@ public class RunsWithDataNodeDiscoveryProvider implements Provider<Boolean> {
     }
 
     private Boolean doGet() {
-        final PreflightConfigResult preflightResult = preflightConfigService.getPreflightConfigResult();
-
-        // if preflight is finished, we assume that there will be some datanode registered via node-service.
-        if (preflightResult == PreflightConfigResult.FINISHED) {
-            final List<URI> discovered = discover();
-            if (!discovered.isEmpty()) {
-                LOG.debug("Running with DataNode(s).");
-                return Boolean.TRUE;
-            }
-        }
-        LOG.debug("Not running with DataNode(s).");
-        return Boolean.FALSE;
-    }
-
-    private List<URI> discover() {
-        return nodeService.allActive().values().stream()
-                .map(Node::getTransportAddress)
-                .map(URI::create)
-                .collect(Collectors.toList());
+        return !nodeService.allActive().isEmpty();
     }
 }
