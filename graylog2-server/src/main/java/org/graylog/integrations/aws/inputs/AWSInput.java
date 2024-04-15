58d92ac8f500d6f77157e478e6650d230d6811e2
Patrick Mann <patrickmann@users.noreply.github.com>
2024-04-15 12:19:37 +0200

    Cloud compatibility annotation (#19054)
    * refactor static cloud compatibility check
    
    * cloud compat annotation
    
    (cherry picked from commit fd427cd33f30d36bfd49a72edbdf3f747f682a6b)

--- graylog2-server/src/main/java/org/graylog/integrations/aws/inputs/AWSInput.java
+++ graylog2-server/src/main/java/org/graylog/integrations/aws/inputs/AWSInput.java
@@ -31,6 +31,7 @@ import org.graylog2.plugin.configuration.fields.ConfigurationField;
 import org.graylog2.plugin.configuration.fields.DropdownField;
 import org.graylog2.plugin.configuration.fields.NumberField;
 import org.graylog2.plugin.configuration.fields.TextField;
+import org.graylog2.plugin.inputs.CloudCompatible;
 import org.graylog2.plugin.inputs.MessageInput;
 import org.graylog2.plugin.inputs.annotations.ConfigClass;
 import org.graylog2.plugin.inputs.annotations.FactoryClass;
@@ -39,6 +40,7 @@ import software.amazon.awssdk.regions.Region;
 /**
  * General AWS input for all types of supported AWS logs.
  */
+@CloudCompatible
 public class AWSInput extends MessageInput {
 
     public static final String NAME = "AWS Kinesis/CloudWatch";
@@ -89,7 +91,7 @@ public class AWSInput extends MessageInput {
             super(NAME, false, "");
         }
 
-        public static boolean isCloudCompatible() {
+        public boolean isCloudCompatible() {
             return true;
         }
     }
