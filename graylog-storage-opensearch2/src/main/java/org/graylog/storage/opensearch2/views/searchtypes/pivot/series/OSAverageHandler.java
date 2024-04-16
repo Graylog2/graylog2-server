f8fbceea6b0edf0a7a3131585c2797c3dfa6d414
Zack King <zack.king@graylog.com>
2024-04-16 13:40:43 -0500

    Support whole numbers on OpenSearch Average

--- graylog-storage-opensearch2/src/main/java/org/graylog/storage/opensearch2/views/searchtypes/pivot/series/OSAverageHandler.java
+++ graylog-storage-opensearch2/src/main/java/org/graylog/storage/opensearch2/views/searchtypes/pivot/series/OSAverageHandler.java
@@ -46,6 +46,14 @@ public class OSAverageHandler extends OSPivotSeriesSpecHandler<Average, Avg> {
                                                                  Avg avgAggregation,
                                                                  OSSearchTypeHandler<Pivot> searchTypeHandler,
                                                                  OSGeneratedQueryContext OSGeneratedQueryContext) {
-        return Stream.of(OSPivotSeriesSpecHandler.Value.create(pivotSpec.id(), Average.NAME, avgAggregation.getValue()));
+        double value = avgAggregation.getValue();
+        if (pivotSpec.wholeNumber()) {
+            if (value == Double.POSITIVE_INFINITY) {
+                value = 0;
+            } else {
+                value = Math.round(value);
+            }
+        }
+        return Stream.of(OSPivotSeriesSpecHandler.Value.create(pivotSpec.id(), Average.NAME, value));
     }
 }
