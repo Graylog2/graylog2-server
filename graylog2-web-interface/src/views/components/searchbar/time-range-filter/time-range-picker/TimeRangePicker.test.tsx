2e93782040d1636381a68eb3bf9d09156d75f766
Linus Pahl <lpahl@protonmail.com>
2024-04-24 10:24:35 +0200

    Decouple keyword time range validation from keyword time range preview in time range picker. (#18879)
    * Consider user time zone for initial keyword time range.
    
    * Make sure to not disable submit button when chanign tabs.
    
    * Decouple keyword time range validation from keyword time range preview in time range picker.
    
    * Adding changelog.
    
    * Fixing test

--- graylog2-web-interface/src/views/components/searchbar/time-range-filter/time-range-picker/TimeRangePicker.test.tsx
+++ graylog2-web-interface/src/views/components/searchbar/time-range-filter/time-range-picker/TimeRangePicker.test.tsx
@@ -88,6 +88,9 @@ describe('TimeRangePicker', () => {
     render(<TimeRangePicker {...defaultProps} />);
 
     const applyButton = screen.getByRole('button', { name: /update time range/i });
+
+    await waitFor(() => expect(applyButton).not.toBeDisabled());
+
     fireEvent.click(applyButton);
 
     await waitFor(() => expect(defaultProps.setCurrentTimeRange).toHaveBeenCalled());
