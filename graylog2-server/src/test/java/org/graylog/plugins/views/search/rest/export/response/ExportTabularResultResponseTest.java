d53a9ca02c1a19a0e971b27dcd4dcd6850fe04b9
Jan Heise <jan.heise@graylog.com>
2024-05-02 10:31:33 +0200

    fixing import

--- graylog2-server/src/test/java/org/graylog/plugins/views/search/rest/export/response/ExportTabularResultResponseTest.java
+++ graylog2-server/src/test/java/org/graylog/plugins/views/search/rest/export/response/ExportTabularResultResponseTest.java
@@ -26,6 +26,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.graylog.plugins.views.search.searchtypes.export.ExportTabularResultResponse.DataRow;
 
 class ExportTabularResultResponseTest {
 
