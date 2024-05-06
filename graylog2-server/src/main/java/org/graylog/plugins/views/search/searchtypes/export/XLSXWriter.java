3b79d566ef9e4b05b10bb9f2e3d90938f73c984c
Jan Heise <jan.heise@graylog.com>
2024-05-06 21:41:14 +0200

    moved XLSXWriter to server package so it can be also used in FE widget exports

---
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/searchtypes/export/XLSXWriter.java
@@ -0,0 +1,64 @@
+package org.graylog.plugins.views.search.searchtypes.export;
+
+import org.apache.poi.ss.usermodel.RichTextString;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+public class XLSXWriter {
+    public static void writeXlsx(final ExportTabularResultResponse widgetExportResponse,
+                                final OutputStream outputStream) throws IOException {
+        try (var wb = new XSSFWorkbook()) {
+            createWorksheetFor(wb, "export", widgetExportResponse);
+            wb.write(outputStream);
+        }
+    }
+
+    public static void createWorksheetFor(final XSSFWorkbook wb, final String sheetName, final ExportTabularResultResponse data) {
+        final var sheet = wb.createSheet(sheetName);
+
+        // write header
+        var row = sheet.createRow(0);
+        for(int c = 0; c < data.header().size(); c++) {
+            var cell = row.createCell(c);
+            cell.setCellValue(data.header().get(c));
+        }
+
+        // write data
+        for(int r = 0; r < data.dataRows().size(); r++) {
+            row = sheet.createRow(r + 1);
+            final var rowData = data.dataRows().get(r).row();
+            // TODO: change to switch statement with Java 21
+            for(int c = 0; c < rowData.size(); c++) {
+                final var cell = row.createCell(c);
+                final var rawData = rowData.get(c);
+                if(rawData instanceof Integer i) {
+                    cell.setCellValue(i);
+                } else if(rawData instanceof Double d) {
+                    cell.setCellValue(d);
+                } else if(rawData instanceof Boolean b) {
+                    cell.setCellValue(b);
+                } if(rawData instanceof Date d) {
+                    cell.setCellValue(d);
+                } if(rawData instanceof LocalDateTime ldt) {
+                    cell.setCellValue(ldt);
+                } if(rawData instanceof Calendar cal) {
+                    cell.setCellValue(cal);
+                } if(rawData instanceof String s) {
+                    cell.setCellValue(s);
+                } if(rawData instanceof RichTextString rts) {
+                    cell.setCellValue(rts);
+                } if(rawData instanceof LocalDate ld) {
+                    cell.setCellValue(ld);
+                } else {
+                    cell.setCellValue(rawData.toString());
+                }
+            }
+        }
+    }
+}
