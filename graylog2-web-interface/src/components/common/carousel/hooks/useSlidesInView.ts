dfad282ad3df00ca4496298c843e0b04cfce74d7
Linus Pahl <linus.pahl@graylog.com>
2024-05-13 11:34:09 +0200

    Implement `useSlidesInView` hook.

---
+++ graylog2-web-interface/src/components/common/carousel/hooks/useSlidesInView.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+
+import type { EmblaCarouselType } from 'embla-carousel';
+import { useCallback, useEffect, useState } from 'react';
+
+import useCarouselApi from './useCarouselApi';
+
+const useSlidesInView = () => {
+  const carouselApi = useCarouselApi('.carousel');
+  const [slidesInView, setSlidesInView] = useState<number[]>([]);
+
+  const updateSlidesInView = useCallback((emblaApi: EmblaCarouselType) => {
+    setSlidesInView((cur) => {
+      if (cur.length === emblaApi.slideNodes().length) {
+        emblaApi.off('slidesInView', updateSlidesInView);
+      }
+
+      const inView = emblaApi
+        .slidesInView()
+        .filter((index) => !cur.includes(index));
+
+      return cur.concat(inView);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!carouselApi) return;
+
+    updateSlidesInView(carouselApi);
+    carouselApi.on('slidesInView', updateSlidesInView);
+    carouselApi.on('reInit', updateSlidesInView);
+  }, [carouselApi, updateSlidesInView]);
+
+  return slidesInView;
+};
+
+export default useSlidesInView;
