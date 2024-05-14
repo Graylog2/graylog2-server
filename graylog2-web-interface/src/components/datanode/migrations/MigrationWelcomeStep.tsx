0ab4a861be2be2ba9d35bc3951a9540def412ce4
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2024-05-14 10:02:32 +0200

    cleanup comments

--- graylog2-web-interface/src/components/datanode/migrations/MigrationWelcomeStep.tsx
+++ graylog2-web-interface/src/components/datanode/migrations/MigrationWelcomeStep.tsx
@@ -91,30 +91,6 @@ const MigrationWelcomeStep = ({ currentStep, onTriggerStep }: MigrationStepCompo
           </p>
         </Panel.Body>
       </StyledHelpPanel>
-      {/* <StyledHelpPanel bsStyle="info">
-        <Panel.Heading>
-          <Panel.Title componentClass="h3"><Icon name="info" /> Migrating Elasticsearch 7.10</Panel.Title>
-        </Panel.Heading>
-        <Panel.Body>
-          <p>Migration from <code>Elasticsearch 7.10</code> needs an additional step. ES 7.10 does not understand JWT
-            authentication.
-            So you will need to first migrate to OpenSearch before running the update of the security information. Please find more information in our <DocumentationLink page="graylog-data-node" text="documentation" />.
-          </p>
-          <h5>Migrating an OpenSearch Cluster</h5>
-          <p>
-            Depending on how you secured your existing cluster, some preliminary changes are needed to the security configuration. We use JWT authentication between Graylog and OpenSearch. If you want to perform an In-Place migration of your existing cluster into the Data Node, you have to manually include JWT authentication to your existing OpenSearch cluster prior to running the migration wizard.
-          </p>
-          <p>
-            Enable JWT authentication in <code>opensearch-security/config.yml</code> (section <code>jwt_auth_domain</code>, <code>http_enabled: true</code>, <code>transport_enabled: true</code>)
-            Add the signing key by converting the <code>GRAYLOG_PASSWORD_SECRET</code> to <code>base64</code> e.g. by doing <code>echo `&quot;`The password secret you chose`&quot;` | base64</code> and put it into the <code>signing_key</code> line
-          </p>
-          <h5>Usage of certificates</h5>
-          <p>
-            If your existing cluster uses certificates, by default these will get replaced with the Graylog CA and automatically generated certificates during provisioning of the Data Nodes. If you have to reuse your own certificates, please read the <DocumentationLink page="graylog-data-node" text="documentation" /> on how to include your own CA/certificates in Graylog/DataNode
-          </p>
-        </Panel.Body>
-      </StyledHelpPanel>
-      <InPlaceMigrationInfo /> */}
     </Col>
   </Row>
 );
