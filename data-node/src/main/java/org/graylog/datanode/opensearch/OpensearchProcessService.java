95a6fa1465bf21b440bdac2d0d5ba9450b186a4f
Tomas Dvorak <tomas.dvorak@graylog.com>
2024-05-29 10:09:37 +0200

    code cleanup, migration actions use datanode command service

--- data-node/src/main/java/org/graylog/datanode/opensearch/OpensearchProcessService.java
+++ data-node/src/main/java/org/graylog/datanode/opensearch/OpensearchProcessService.java
@@ -16,44 +16,28 @@
  */
 package org.graylog.datanode.opensearch;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.common.util.concurrent.AbstractIdleService;
 import jakarta.inject.Inject;
 import jakarta.inject.Provider;
 import jakarta.inject.Singleton;
-import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.graylog.datanode.Configuration;
 import org.graylog.datanode.bootstrap.preflight.DatanodeDirectoriesLockfileCheck;
-import org.graylog.datanode.configuration.DatanodeKeystoreException;
 import org.graylog.datanode.configuration.OpensearchConfigurationProvider;
 import org.graylog.datanode.opensearch.configuration.OpensearchConfiguration;
 import org.graylog.datanode.opensearch.statemachine.OpensearchEvent;
 import org.graylog.datanode.opensearch.statemachine.OpensearchStateMachine;
-import org.graylog.security.certutil.csr.exceptions.CSRGenerationException;
 import org.graylog2.bootstrap.preflight.PreflightConfigResult;
 import org.graylog2.bootstrap.preflight.PreflightConfigService;
-import org.graylog2.cluster.NodeNotFoundException;
-import org.graylog2.cluster.preflight.DataNodeProvisioningConfig;
-import org.graylog2.cluster.preflight.DataNodeProvisioningService;
 import org.graylog2.cluster.preflight.DataNodeProvisioningStateChangeEvent;
 import org.graylog2.datanode.DataNodeLifecycleEvent;
 import org.graylog2.datanode.RemoteReindexAllowlistEvent;
-import org.graylog2.events.ClusterEventBus;
-import org.graylog2.indexer.fieldtypes.IndexFieldTypesService;
 import org.graylog2.plugin.system.NodeId;
-import org.graylog2.shared.SuppressForbidden;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.util.Collections;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Singleton
 public class OpensearchProcessService extends AbstractIdleService implements Provider<OpensearchProcess> {
@@ -63,9 +47,7 @@ public class OpensearchProcessService extends AbstractIdleService implements Pro
     private static final int WATCHDOG_RESTART_ATTEMPTS = 3;
     private final OpensearchProcess process;
     private final OpensearchConfigurationProvider configurationProvider;
-    private final EventBus eventBus;
     private final NodeId nodeId;
-    private final DataNodeProvisioningService dataNodeProvisioningService;
     private final DatanodeDirectoriesLockfileCheck lockfileCheck;
     private final PreflightConfigService preflightConfigService;
     private final Configuration configuration;
@@ -80,18 +62,13 @@ public class OpensearchProcessService extends AbstractIdleService implements Pro
             final OpensearchConfigurationProvider configurationProvider,
             final EventBus eventBus,
             final Configuration configuration,
-            final DataNodeProvisioningService dataNodeProvisioningService,
             final NodeId nodeId,
-            final IndexFieldTypesService indexFieldTypesService,
-            final ClusterEventBus clusterEventBus,
             final DatanodeDirectoriesLockfileCheck lockfileCheck,
             final PreflightConfigService preflightConfigService,
             final OpensearchProcess process, OpensearchStateMachine stateMachine) {
         this.configurationProvider = configurationProvider;
         this.configuration = configuration;
-        this.eventBus = eventBus;
         this.nodeId = nodeId;
-        this.dataNodeProvisioningService = dataNodeProvisioningService;
         this.lockfileCheck = lockfileCheck;
         this.preflightConfigService = preflightConfigService;
         this.process = process;
