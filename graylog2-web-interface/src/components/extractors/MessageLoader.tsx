ec0253d5d926712a590783d9e3818942714f21db
Dennis Oelkers <dennis@graylog.com>
2024-05-31 10:01:54 +0200

    Removing `MessagesStore` and its usages. (#19501)
    * Migrating to `fetchMessage` from `MessagesStore`.
    
    * Migrating `CreateExtractorsPage`.

--- graylog2-web-interface/src/components/extractors/MessageLoader.tsx
+++ graylog2-web-interface/src/components/extractors/MessageLoader.tsx
@@ -19,9 +19,9 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { MessagesActions } from 'stores/messages/MessagesStore';
 import { FormSubmit } from 'components/common';
 import useHistory from 'routing/useHistory';
+import { fetchMessage } from 'views/hooks/useMessage';
 
 import { Button } from '../bootstrap';
 
@@ -61,48 +61,23 @@ type Props = {
   index?: string,
 };
 
-const useMessageLoader = (defaultMessageId: string, defaultIndex: string, onMessageLoaded: (data: any) => void): [boolean, (messageId: string, index: string) => void] => {
-  const [loading, setLoading] = useState(false);
-  const loadMessage = useCallback((messageId: string, index: string) => {
-    if (messageId === '' || index === '') {
-      return;
-    }
-
-    setLoading(true);
-    const promise = MessagesActions.loadMessage(index, messageId);
-
-    promise.then((data) => onMessageLoaded(data));
-    promise.finally(() => setLoading(false));
-  }, [onMessageLoaded]);
-
-  useEffect(() => {
-    if (defaultMessageId && defaultIndex) {
-      loadMessage(defaultMessageId, defaultIndex);
-    }
-  }, [defaultMessageId, defaultIndex, loadMessage]);
-
-  return [loading, loadMessage];
-};
-
 const MessageLoader = ({ hidden, hideText, onMessageLoaded, messageId: defaultMessageId, index: defaultIndex }: Props) => {
+  const [loading, setLoading] = useState(false);
   const [isHidden, setIsHidden] = useState(hidden);
 
   const [messageId, setMessageId] = useState<string>(defaultMessageId);
-  const onChangeMessageId = useCallback((e) => setMessageId(e.target.value), []);
+  const onChangeMessageId = useCallback((e: React.ChangeEvent<HTMLInputElement>) => setMessageId(e.target.value), []);
   const messageIdRef = useRef<HTMLInputElement>();
 
   const [index, setIndex] = useState<string>(defaultIndex);
-  const onChangeIndex = useCallback((e) => setIndex(e.target.value), []);
-
-  const [loading, loadMessage] = useMessageLoader(defaultMessageId, defaultIndex, onMessageLoaded);
+  const onChangeIndex = useCallback((e: React.ChangeEvent<HTMLInputElement>) => setIndex(e.target.value), []);
 
   const _loadMessage = useCallback((e?: React.FormEvent) => {
-    if (e?.preventDefault) {
-      e.preventDefault();
-    }
+    e?.preventDefault?.();
 
-    loadMessage(messageId, index);
-  }, [index, loadMessage, messageId]);
+    setLoading(true);
+    fetchMessage(index, messageId).then(onMessageLoaded).finally(() => setLoading(false));
+  }, [index, messageId, onMessageLoaded]);
 
   const toggleMessageForm = useCallback(() => {
     setIsHidden(!isHidden);
