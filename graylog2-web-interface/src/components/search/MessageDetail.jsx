3e459d1cd19175e3f6749dc5dfdb58b87920da3a
Linus Pahl <linus.pahl@graylog.com>
2024-06-10 15:24:11 +0200

    Migrate `MessageDetail` to functional component.

--- graylog2-web-interface/src/components/search/MessageDetail.jsx
+++ graylog2-web-interface/src/components/search/MessageDetail.jsx
@@ -14,7 +14,6 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import PropTypes from 'prop-types';
 import React from 'react';
 import Immutable from 'immutable';
 
@@ -28,178 +27,146 @@ import Routes from 'routing/Routes';
 import AppConfig from 'util/AppConfig';
 import MessagePermalinkButton from 'views/components/common/MessagePermalinkButton';
 
-class MessageDetail extends React.Component {
-  static propTypes = {
-    renderForDisplay: PropTypes.func.isRequired,
-    inputs: PropTypes.object,
-    nodes: PropTypes.object,
-    message: PropTypes.object.isRequired,
-    streams: PropTypes.object,
-    customFieldActions: PropTypes.node,
-  };
+const MessageActions = ({ messageIndex, messageId }) => (
+  <ButtonGroup className="pull-right">
+    <MessagePermalinkButton messageIndex={messageIndex} messageId={messageId} />
 
-  static defaultProps = {
-    inputs: undefined,
-    nodes: undefined,
-    streams: undefined,
-    customFieldActions: undefined,
-  };
+    <ClipboardButton title="Copy ID" bsSize="small" text={message.id} />
+  </ButtonGroup>
+);
 
-  _inputName = (inputId) => {
-    const { inputs } = this.props;
-    const input = inputs.get(inputId);
+const InputName = ({ inputs, inputId }) => {
+  const input = inputs.get(inputId);
 
-    return input ? <span style={{ wordBreak: 'break-word' }}>{input.title}</span> : 'deleted input';
-  };
+  return input ? <span style={{ wordBreak: 'break-word' }}>{input.title}</span> : 'deleted input';
+};
 
-  _nodeName = (nodeId) => {
-    const { nodes } = this.props;
-    const node = nodes.get(nodeId);
-    let nodeInformation;
+const NodeName = ({ nodes, nodeId }) => {
+  const node = nodes.get(nodeId);
+  let nodeInformation;
 
-    if (node) {
-      const nodeURL = Routes.node(nodeId);
+  if (node) {
+    const nodeURL = Routes.node(nodeId);
 
-      const nodeContent = (
-        <>
-          <Icon name="fork_right" />
-          &nbsp;
-          <span style={{ wordBreak: 'break-word' }}>{node.short_node_id}</span>&nbsp;/&nbsp;
-          <span style={{ wordBreak: 'break-word' }}>{node.hostname}</span>
-        </>
-      );
-
-      nodeInformation = AppConfig.isCloud()
-        ? nodeContent
-        : <a href={nodeURL}>{nodeContent}</a>;
-    } else {
-      nodeInformation = <span style={{ wordBreak: 'break-word' }}>stopped node</span>;
-    }
-
-    return nodeInformation;
-  };
-
-  _formatMessageActions = () => {
-    const { message, customFieldActions } = this.props;
-
-    if (!customFieldActions) {
-      return <ButtonGroup className="pull-right" />;
-    }
-
-    return (
-      <ButtonGroup className="pull-right">
-        <MessagePermalinkButton messageIndex={message.index} messageId={message.id} />
-        <ClipboardButton title="Copy ID" bsSize="small" text={message.id} />
-      </ButtonGroup>
+    const nodeContent = (
+      <>
+        <Icon name="fork_right" />
+        &nbsp;
+        <span style={{ wordBreak: 'break-word' }}>{node.short_node_id}</span>&nbsp;/&nbsp;
+        <span style={{ wordBreak: 'break-word' }}>{node.hostname}</span>
+      </>
     );
-  };
 
-  _getStreamLinks = (streamIds) => {
-    const { streams, message } = this.props;
-
-    if (message.streams) {
-      return message.streams.map((stream) => (<li key={stream.id}><StreamLink stream={stream} /></li>));
-    }
-
-    const _streams = streamIds.map((id) => {
-      const stream = streams.get(id);
-
-      if (stream !== undefined) {
-        return <li key={stream.id}><StreamLink stream={stream} /></li>;
-      }
-
-      return null;
-    });
-
-    return _streams;
-  };
-
-  render() {
-    const { renderForDisplay, nodes, message, customFieldActions } = this.props;
-    const streamIds = Immutable.Set(message.stream_ids);
-    const streamLinks = this._getStreamLinks(streamIds);
-
-    // Legacy
-    const viaRadio = message.source_radio_id
-      ? (
-        <span>
-          via <em>{this._inputName(message.source_radio_input_id)}</em> on
-          radio {this._nodeName(message.source_radio_id)}
-        </span>
-      )
-      : null;
-
-    const rawTimestamp = message.fields.timestamp;
-    const timestamp = [
-      <dt key={`dt-${rawTimestamp}`}>Timestamp</dt>,
-      <dd key={`dd-${rawTimestamp}`}><Timestamp dateTime={rawTimestamp} /></dd>,
-    ];
-
-    const receivedBy = message.source_input_id && message.source_node_id && nodes
-      ? (
-        <div>
-          <dt>Received by</dt>
-          <dd>
-            <em>{this._inputName(message.source_input_id)}</em>{' '}
-            on {this._nodeName(message.source_node_id)}
-            {viaRadio && <br />}
-            {viaRadio}
-          </dd>
-        </div>
-      )
-      : null;
-
-    const messageTitle = message.index
-      ? (
-        <Link to={Routes.message_show(message.index, message.id)}>
-          {message.id}
-        </Link>
-      )
-      : <span>{message.id} <Label bsStyle="warning">Not stored</Label></span>;
-
-    return (
-      <div>
-        <Row className="row-sm">
-          <Col md={12}>
-            {this._formatMessageActions()}
-            <MessageDetailsTitle>
-              <Icon name="mail" />
-              &nbsp;
-              {messageTitle}
-            </MessageDetailsTitle>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={3}>
-            <MessageDetailsDefinitionList>
-              {timestamp}
-              {receivedBy}
-
-              <dt>Stored in index</dt>
-              <dd>{message.index ? message.index : 'Message is not stored'}</dd>
-
-              {streamIds.size > 0 && <dt>Routed into streams</dt>}
-              {streamIds.size > 0
-            && (
-            <dd className="stream-list">
-              <ul>
-                {streamLinks}
-              </ul>
-            </dd>
-            )}
-            </MessageDetailsDefinitionList>
-          </Col>
-          <Col md={9}>
-            <div>
-              <MessageFields message={message}
-                             renderForDisplay={renderForDisplay}
-                             customFieldActions={customFieldActions} />
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
+    nodeInformation = AppConfig.isCloud()
+      ? nodeContent
+      : <a href={nodeURL}>{nodeContent}</a>;
+  } else {
+    nodeInformation = <span style={{ wordBreak: 'break-word' }}>stopped node</span>;
   }
-}
+
+  return nodeInformation;
+};
+
+const StreamLinks = ({ messageStreams, streamIds, streams }) => {
+  if (messageStreams) {
+    return messageStreams.map((stream) => (<li key={stream.id}><StreamLink stream={stream} /></li>));
+  }
+
+  return streamIds.map((id) => {
+    const stream = streams.get(id);
+
+    if (stream !== undefined) {
+      return <li key={stream.id}><StreamLink stream={stream} /></li>;
+    }
+
+    return null;
+  });
+};
+
+const MessageDetail = ({ renderForDisplay, inputs, nodes, streams, message, customFieldActions }) => {
+  const streamIds = Immutable.Set(message.stream_ids);
+
+  // Legacy
+  const viaRadio = message.source_radio_id
+    ? (
+      <span>
+        via <em><InputName inputs={inputs} inputId={message.source_radio_input_id} /></em> on
+        radio <NodeName nodes={nodes} nodeId={message.source_radio_id} />
+      </span>
+    )
+    : null;
+
+  const rawTimestamp = message.fields.timestamp;
+  const timestamp = [
+    <dt key={`dt-${rawTimestamp}`}>Timestamp</dt>,
+    <dd key={`dd-${rawTimestamp}`}><Timestamp dateTime={rawTimestamp} /></dd>,
+  ];
+
+  return (
+    <div>
+      <Row className="row-sm">
+        <Col md={12}>
+          {customFieldActions
+            ? <MessageActions messageIndex={message.index} messageId={message.id} />
+            : <ButtonGroup className="pull-right" />}
+          <MessageDetailsTitle>
+            <Icon name="mail" />
+            &nbsp;
+            {message.index ? (
+              <Link to={Routes.message_show(message.index, message.id)}>
+                {message.id}
+              </Link>
+            ) : <span>{message.id} <Label bsStyle="warning">Not stored</Label></span>}
+          </MessageDetailsTitle>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={3}>
+          <MessageDetailsDefinitionList>
+            {timestamp}
+            {(message.source_input_id && message.source_node_id && nodes) && (
+              <div>
+                <dt>Received by</dt>
+                <dd>
+                  <em><InputName inputs={inputs} inputId={message.source_input_id} /></em>{' '}
+                  on <NodeName nodes={nodes} nodeId={message.source_node_id} />
+                  {viaRadio && <br />}
+                  {viaRadio}
+                </dd>
+              </div>
+            )}
+
+            <dt>Stored in index</dt>
+            <dd>{message.index ? message.index : 'Message is not stored'}</dd>
+
+            {streamIds.size > 0 && <dt>Routed into streams</dt>}
+            {streamIds.size > 0
+              && (
+                <dd className="stream-list">
+                  <ul>
+                    <StreamLinks messageStreams={message.streams} streamIds={streamIds} streams={streams} />
+                  </ul>
+                </dd>
+              )}
+          </MessageDetailsDefinitionList>
+        </Col>
+        <Col md={9}>
+          <div>
+            <MessageFields message={message}
+                           renderForDisplay={renderForDisplay}
+                           customFieldActions={customFieldActions} />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+MessageDetail.defaultProps = {
+  inputs: undefined,
+  nodes: undefined,
+  streams: undefined,
+  customFieldActions: undefined,
+};
 
 export default MessageDetail;
