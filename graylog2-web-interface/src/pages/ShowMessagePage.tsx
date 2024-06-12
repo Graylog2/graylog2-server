c89da05f802afd720b76535f0ce8175b75fedbb1
Linus Pahl <linus.pahl@graylog.com>
2024-06-12 13:40:25 +0200

    Remove custom useStreams hook.

--- graylog2-web-interface/src/pages/ShowMessagePage.tsx
+++ graylog2-web-interface/src/pages/ShowMessagePage.tsx
@@ -15,7 +15,7 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import PropTypes from 'prop-types';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState, useContext } from 'react';
 import * as Immutable from 'immutable';
 
 import useMessage from 'views/hooks/useMessage';
@@ -25,10 +25,8 @@ import { Col, Row } from 'components/bootstrap';
 import InteractiveContext from 'views/components/contexts/InteractiveContext';
 import MessageDetail from 'views/components/messagelist/MessageDetail';
 import withParams from 'routing/withParams';
-import type { Stream } from 'views/stores/StreamsStore';
 import type { Input } from 'components/messageloaders/Types';
 import WindowDimensionsContextProvider from 'contexts/WindowDimensionsContextProvider';
-import StreamsStore from 'stores/streams/StreamsStore';
 import { InputsActions } from 'stores/inputs/InputsStore';
 import { NodesActions } from 'stores/nodes/NodesStore';
 import { isLocalNode } from 'views/hooks/useIsLocalNode';
@@ -36,30 +34,13 @@ import PluggableStoreProvider from 'components/PluggableStoreProvider';
 import View from 'views/logic/views/View';
 import SearchExecutionState from 'views/logic/search/SearchExecutionState';
 import SingleMessageFieldTypesProvider from 'views/components/fieldtypes/SingleMessageFieldTypesProvider';
+import StreamsContext from 'contexts/StreamsContext';
 
 type Props = {
   params: {
     index: string | undefined | null,
     messageId: string | undefined | null,
   },
-};
-
-const useStreams = () => {
-  const [streams, setStreams] = useState<Immutable.Map<string, Stream>>();
-  const [allStreams, setAllStreams] = useState<Immutable.List<Stream>>();
-
-  useEffect(() => {
-    StreamsStore.listStreams().then((newStreams) => {
-      if (newStreams) {
-        const streamsMap = Object.fromEntries(newStreams.map((stream) => [stream.id, stream]));
-
-        setStreams(Immutable.Map(streamsMap));
-        setAllStreams(Immutable.List(newStreams));
-      }
-    });
-  }, [setStreams, setAllStreams]);
-
-  return { streams, allStreams };
 };
 
 const useInputs = (sourceInputId: string | undefined, gl2SourceNode: string | undefined) => {
@@ -94,23 +75,23 @@ const ShowMessagePage = ({ params: { index, messageId } }: Props) => {
     throw new Error('index and messageId need to be specified!');
   }
 
-  const { streams, allStreams } = useStreams();
+  const streams = useContext(StreamsContext);
+  const streamsMap = Immutable.Map(Object.fromEntries(streams.map((stream) => [stream.id, stream])));
+  const streamsList = Immutable.List(streams);
   const { data: message } = useMessage(index, messageId);
   const inputs = useInputs(message?.source_input_id, message?.fields.gl2_source_node);
 
   useEffect(() => { NodesActions.list(); }, []);
 
   const isLoaded = useMemo(() => (message !== undefined
-    && streams !== undefined
-    && inputs !== undefined
-    && allStreams !== undefined), [message, streams, inputs, allStreams]);
+    && inputs !== undefined), [message, inputs]);
 
   const view = useMemo(() => View.create(), []);
   const executionState = useMemo(() => SearchExecutionState.empty(), []);
 
   if (isLoaded) {
     const { streams: messageStreams, timestamp } = message.fields as MessageFields;
-    const fieldTypesStreams = messageStreams.filter((streamId) => streams.has(streamId));
+    const fieldTypesStreams = messageStreams.filter((streamId) => streamsMap.has(streamId));
 
     return (
       <PluggableStoreProvider view={view} initialQuery="none" isNew={false} executionState={executionState}>
@@ -121,8 +102,8 @@ const ShowMessagePage = ({ params: { index, messageId } }: Props) => {
                 <SingleMessageFieldTypesProvider streams={fieldTypesStreams} timestamp={timestamp}>
                   <InteractiveContext.Provider value={false}>
                     <MessageDetail fields={Immutable.List()}
-                                   streams={streams}
-                                   allStreams={allStreams}
+                                   streams={streamsMap}
+                                   allStreams={streamsList}
                                    disableSurroundingSearch
                                    inputs={inputs}
                                    message={message} />
