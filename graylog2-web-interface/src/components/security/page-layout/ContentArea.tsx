8bcc495ddb976d41970529517e84f76f0bf14b5c
Dr. Lint-a-lot <garybot2@graylog.com>
2024-06-18 00:17:50 +0000

    Running lint --fix

--- graylog2-web-interface/src/components/security/page-layout/ContentArea.tsx
+++ graylog2-web-interface/src/components/security/page-layout/ContentArea.tsx
@@ -19,7 +19,7 @@ import styled, { css } from 'styled-components';
 const ContentArea = styled.div<{ $isSecurityPerspective?: boolean; }>(({ $isSecurityPerspective, theme }) => css`
   display: flex;
   flex-direction: column;
-  padding-left: 0px;
+  padding-left: 0;
   padding-top: ${$isSecurityPerspective ? '0px' : '15px'};
   transition: all 0.33s ease-in-out;
   position: relative;
