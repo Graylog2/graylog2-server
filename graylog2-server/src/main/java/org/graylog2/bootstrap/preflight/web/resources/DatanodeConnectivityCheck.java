4ed30f72d655bad25370fd06a6130c81471c69d8
Tomas Dvorak <tomas.dvorak@graylog.com>
2024-06-20 15:44:03 +0200

    Error handling reintroduced for preflight connection checks (#19664)
    * Error handling reintroduced for preflight connection checks
    
    * logging delegate, code cleanup, test
    
    * fixed annotation
    
    * code cleanup

--- graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/DatanodeConnectivityCheck.java
+++ graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/DatanodeConnectivityCheck.java
@@ -26,13 +26,19 @@ import org.graylog2.cluster.nodes.DataNodeDto;
 import org.graylog2.security.IndexerJwtAuthTokenProvider;
 import org.graylog2.storage.SearchVersion;
 import org.graylog2.storage.versionprobe.VersionProbe;
+import org.graylog2.storage.versionprobe.VersionProbeLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.Collections;
-import java.util.Optional;
+import java.util.List;
 
 @Singleton
 public class DatanodeConnectivityCheck {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DatanodeConnectivityCheck.class);
+
     private final VersionProbe versionProbe;
 
     @Inject
@@ -40,8 +46,11 @@ public class DatanodeConnectivityCheck {
         this.versionProbe = new VersionProbe(objectMapper, okHttpClient, 1, Duration.seconds(1), true, opensearchUseJwtAuthentication, indexerJwtAuthTokenProvider);
     }
 
-
-    public Optional<SearchVersion> probe(DataNodeDto node) {
-        return versionProbe.probe(Collections.singletonList(URI.create(node.getTransportAddress())));
+    public ConnectionCheckResult probe(DataNodeDto node) {
+        final List<URI> hosts = Collections.singletonList(URI.create(node.getTransportAddress()));
+        final VersionProbeMessageCollector messageCollector = new VersionProbeMessageCollector(new VersionProbeLogger(LOG));
+        return versionProbe.probe(hosts, messageCollector)
+                .map(ConnectionCheckResult::success)
+                .orElse(ConnectionCheckResult.failure(messageCollector.joinedMessages()));
     }
 }
