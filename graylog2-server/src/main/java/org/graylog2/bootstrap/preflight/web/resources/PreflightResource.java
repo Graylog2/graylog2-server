07a9c24e1cb4db56466b57107e145622a5c40607
Tomas Dvorak <tomas.dvorak@graylog.com>
2024-06-21 12:23:18 +0200

    Graylog CA abstractions refactoring (#19632)
    * Graylog CA abstractions refactoring
    
    * CaTruststore abstraction, bindings
    
    * Add BC providers in key and truststores, tests
    
    * code cleanup
    
    * Fixed forbidden annotation
    
    * fixed bindings
    
    ---------
    
    Co-authored-by: Matthias Oesterheld <33032967+moesterheld@users.noreply.github.com>

--- graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/PreflightResource.java
+++ graylog2-server/src/main/java/org/graylog2/bootstrap/preflight/web/resources/PreflightResource.java
@@ -17,7 +17,6 @@
 package org.graylog2.bootstrap.preflight.web.resources;
 
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
@@ -30,18 +29,17 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataParam;
-import org.graylog.security.certutil.CaService;
-import org.graylog.security.certutil.CertConstants;
+import org.graylog.security.certutil.CaKeystore;
+import org.graylog.security.certutil.CaKeystoreException;
 import org.graylog.security.certutil.ca.exceptions.CACreationException;
 import org.graylog.security.certutil.ca.exceptions.KeyStoreStorageException;
 import org.graylog2.audit.jersey.NoAuditEvent;
 import org.graylog2.bootstrap.preflight.PreflightConstants;
 import org.graylog2.bootstrap.preflight.PreflightWebModule;
-import org.graylog2.bootstrap.preflight.web.resources.model.CA;
 import org.graylog2.bootstrap.preflight.web.resources.model.CertParameters;
+import org.graylog2.bootstrap.preflight.web.resources.model.CertificateAuthorityInformation;
 import org.graylog2.bootstrap.preflight.web.resources.model.CreateCARequest;
 import org.graylog2.cluster.NodeNotFoundException;
 import org.graylog2.cluster.nodes.DataNodeDto;
@@ -54,12 +52,9 @@ import org.graylog2.plugin.certificates.RenewalPolicy;
 import org.graylog2.plugin.cluster.ClusterConfigService;
 import org.graylog2.plugin.rest.ApiError;
 
-import java.io.IOException;
-import java.io.StringWriter;
 import java.net.URI;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.Certificate;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -69,29 +64,27 @@ import java.util.stream.Collectors;
 public class PreflightResource {
 
     private final NodeService<DataNodeDto> nodeService;
-    private final CaService caService;
+    private final CaKeystore caKeystore;
     private final ClusterConfigService clusterConfigService;
-    private final String passwordSecret;
-
     private final DataNodeCommandService dataNodeCommandService;
 
     private final DatanodeConnectivityCheck datanodeConnectivityCheck;
 
     @Inject
     public PreflightResource(final NodeService<DataNodeDto> nodeService,
-                             final CaService caService,
+                             CaKeystore caKeystore,
                              final ClusterConfigService clusterConfigService,
-                             final @Named("password_secret") String passwordSecret, DataNodeCommandService dataNodeCommandService, DatanodeConnectivityCheck datanodeConnectivityCheck) {
+                             DataNodeCommandService dataNodeCommandService, DatanodeConnectivityCheck datanodeConnectivityCheck) {
         this.nodeService = nodeService;
-        this.caService = caService;
+        this.caKeystore = caKeystore;
         this.clusterConfigService = clusterConfigService;
-        this.passwordSecret = passwordSecret;
         this.dataNodeCommandService = dataNodeCommandService;
         this.datanodeConnectivityCheck = datanodeConnectivityCheck;
     }
 
-    public record DataNode(String nodeId, String transportAddress, DataNodeProvisioningConfig.State status, String errorMsg,
-                    String hostname, String shortNodeId, DataNodeStatus dataNodeStatus) {}
+    public record DataNode(String nodeId, String transportAddress, DataNodeProvisioningConfig.State status,
+                           String errorMsg,
+                           String hostname, String shortNodeId, DataNodeStatus dataNodeStatus) {}
 
     @GET
     @Path("/data_nodes")
@@ -129,8 +122,8 @@ public class PreflightResource {
     @GET
     @Path("/ca")
     @RequiresPermissions(PreflightWebModule.PERMISSION_PREFLIGHT_ONLY)
-    public CA get() throws KeyStoreStorageException {
-        return caService.get();
+    public CertificateAuthorityInformation get() throws KeyStoreStorageException {
+        return caKeystore.getInformation().orElse(null);
     }
 
     @GET
@@ -138,36 +131,16 @@ public class PreflightResource {
     @Produces(MediaType.TEXT_PLAIN)
     @RequiresPermissions(PreflightWebModule.PERMISSION_PREFLIGHT_ONLY)
     public String getCaCertificate() {
-        try {
-            return caService.loadKeyStore().map(ks -> {
-                final Certificate caPublicKey;
-                try {
-                    caPublicKey = ks.getCertificate(CertConstants.CA_KEY_ALIAS);
-                    return encode(caPublicKey);
-                } catch (KeyStoreException | IOException e) {
-                    throw new RuntimeException("Failed to obtain CA public key", e);
-                }
-            }).orElseThrow(() -> new IllegalStateException("CA keystore not available"));
-        } catch (KeyStoreException | KeyStoreStorageException | NoSuchAlgorithmException e) {
-            throw new RuntimeException("Failed to obtain CA public key", e);
-        }
+        return caKeystore.getEncodedCertificate().orElseThrow(() -> new IllegalStateException("CA keystore not available"));
     }
 
-    private static String encode(final Object o) throws IOException {
-        var writer = new StringWriter();
-        try (JcaPEMWriter jcaPEMWriter = new JcaPEMWriter(writer)) {
-            jcaPEMWriter.writeObject(o);
-        }
-        return writer.toString();
-    }
 
     @POST
     @Path("/ca/create")
     @RequiresPermissions(PreflightWebModule.PERMISSION_PREFLIGHT_ONLY)
     @NoAuditEvent("No Auditing during preflight")
     public Response createCA(@NotNull @Valid CreateCARequest request) throws CACreationException, KeyStoreStorageException, KeyStoreException, NoSuchAlgorithmException {
-        // TODO: get validity from preflight UI
-        final CA ca = caService.create(request.organization(), CaService.DEFAULT_VALIDITY, passwordSecret.toCharArray());
+        final CertificateAuthorityInformation ca = caKeystore.createSelfSigned(request.organization());
         return Response.created(URI.create("/api/ca")).entity(ca).build();
     }
 
@@ -178,9 +151,9 @@ public class PreflightResource {
     @NoAuditEvent("No Auditing during preflight")
     public Response uploadCA(@FormDataParam("password") String password, @FormDataParam("files") List<FormDataBodyPart> bodyParts) {
         try {
-            caService.upload(password, bodyParts);
+            caKeystore.createFromUpload(password, bodyParts);
             return Response.ok().build();
-        } catch (CACreationException e) {
+        } catch (CaKeystoreException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(ApiError.create(e.getMessage())).build();
         }
     }
@@ -190,7 +163,7 @@ public class PreflightResource {
     @RequiresPermissions(PreflightWebModule.PERMISSION_PREFLIGHT_ONLY)
     @NoAuditEvent("No Auditing during preflight")
     public void startOver() {
-        caService.startOver();
+        caKeystore.reset();
         clusterConfigService.remove(RenewalPolicy.class);
         nodeService.allActive().values().stream()
                 .filter(n -> n.getDataNodeStatus() == DataNodeStatus.AVAILABLE)
