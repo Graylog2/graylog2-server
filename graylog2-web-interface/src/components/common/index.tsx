843a0830b61c0f304f8fe017fa0b491d3955360b
Linus Pahl <46300478+linuspahl@users.noreply.github.com>
2024-07-02 08:19:27 +0200

    Add ring progress component. (#19793)

--- graylog2-web-interface/src/components/common/index.tsx
+++ graylog2-web-interface/src/components/common/index.tsx
@@ -99,6 +99,7 @@ export { default as QueryHelper } from './QueryHelper';
 export { default as ReactGridContainer } from './ReactGridContainer';
 export { default as ReadOnlyFormGroup } from './ReadOnlyFormGroup';
 export { default as RelativeTime } from './RelativeTime';
+export { default as RingProgress } from './RingProgress';
 export { default as ScrollButton } from './ScrollButton';
 export { default as SearchForm } from './SearchForm';
 export { default as Section } from './Section';
