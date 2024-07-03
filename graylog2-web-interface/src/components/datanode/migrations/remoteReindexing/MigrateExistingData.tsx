35c9c74f120c509775a5527080fd53f18155700a
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2024-07-03 18:00:03 +0200

    added optional hideActions prop

--- graylog2-web-interface/src/components/datanode/migrations/remoteReindexing/MigrateExistingData.tsx
+++ graylog2-web-interface/src/components/datanode/migrations/remoteReindexing/MigrateExistingData.tsx
@@ -43,7 +43,7 @@ export type RemoteReindexCheckConnection = {
   error: any,
 }
 
-const MigrateExistingData = ({ currentStep, onTriggerStep }: MigrationStepComponentProps) => {
+const MigrateExistingData = ({ currentStep, onTriggerStep, hideActions }: MigrationStepComponentProps) => {
   const [nextSteps, setNextSteps] = useState<MigrationActions[]>(['CHECK_REMOTE_INDEXER_CONNECTION']);
   const [errorMessage, setErrrorMessage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -206,7 +206,8 @@ const MigrateExistingData = ({ currentStep, onTriggerStep }: MigrationStepCompon
           {isLoading ? (
             <Spinner />
           ) : (
-            <MigrationStepTriggerButtonToolbar nextSteps={nextSteps || currentStep.next_steps}
+            <MigrationStepTriggerButtonToolbar hidden={hideActions}
+                                               nextSteps={nextSteps || currentStep.next_steps}
                                                onTriggerStep={handleTriggerNextStep}
                                                args={{ ...values, indices: filteredSelectedIndices } as RemoteReindexRequest} />
           )}
