7bdd8a32671380aca3b126581d9818b479372631
Dennis Oelkers <dennis@graylog.com>
2024-07-03 08:58:22 +0200

    Extend test, run on single search server only.

--- full-backend-tests/src/test/java/org/graylog2/suggestions/EntitySuggestionsIT.java
+++ full-backend-tests/src/test/java/org/graylog2/suggestions/EntitySuggestionsIT.java
@@ -16,14 +16,18 @@
  */
 package org.graylog2.suggestions;
 
+import io.restassured.response.ValidatableResponse;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.graylog.testing.completebackend.apis.GraylogApis;
+import org.graylog.testing.containermatrix.SearchServer;
 import org.graylog.testing.containermatrix.annotations.ContainerMatrixTest;
 import org.graylog.testing.containermatrix.annotations.ContainerMatrixTestsConfiguration;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
 
-@ContainerMatrixTestsConfiguration
+@ContainerMatrixTestsConfiguration(searchVersions = SearchServer.OS2_LATEST)
 public class EntitySuggestionsIT {
     private final GraylogApis api;
 
@@ -33,21 +37,49 @@ public class EntitySuggestionsIT {
 
     @ContainerMatrixTest
     void returnsTitlesForDashboards() {
-        final var dashboard1 = api.dashboards().createDashboard("Test");
-        final var dashboard2 = api.dashboards().createDashboard("Test 2");
-        given()
+        final var randomIdentifier = RandomStringUtils.randomAlphanumeric(8);
+        final var dashboard1 = api.dashboards().createDashboard("First " + randomIdentifier);
+        final var dashboard2 = api.dashboards().createDashboard("Second " + randomIdentifier);
+        final var dashboard3 = api.dashboards().createDashboard("Third " + randomIdentifier);
+        retrieveSuggestions(1, 100, randomIdentifier)
+                .body("pagination.total", equalTo(3))
+                .body("pagination.count", equalTo(3))
+                .body("suggestions[0].value", equalTo("First " + randomIdentifier))
+                .body("suggestions[0].id", equalTo(dashboard1))
+                .body("suggestions[1].value", equalTo("Second " + randomIdentifier))
+                .body("suggestions[1].id", equalTo(dashboard2))
+                .body("suggestions[2].value", equalTo("Third " + randomIdentifier))
+                .body("suggestions[2].id", equalTo(dashboard3));
+        retrieveSuggestions(1, 1, randomIdentifier)
+                .body("pagination.total", equalTo(3))
+                .body("pagination.count", equalTo(1))
+                .body("suggestions[0].value", equalTo("First " + randomIdentifier))
+                .body("suggestions[0].id", equalTo(dashboard1));
+        retrieveSuggestions(2, 1, randomIdentifier)
+                .body("pagination.total", equalTo(3))
+                .body("pagination.count", equalTo(1))
+                .body("suggestions[0].value", equalTo("Second " + randomIdentifier))
+                .body("suggestions[0].id", equalTo(dashboard2));
+        retrieveSuggestions(3, 1, randomIdentifier)
+                .body("pagination.total", equalTo(3))
+                .body("pagination.count", equalTo(1))
+                .body("suggestions[0].value", equalTo("Third " + randomIdentifier))
+                .body("suggestions[0].id", equalTo(dashboard3));
+        retrieveSuggestions(4, 1, randomIdentifier)
+                .body("pagination.total", equalTo(3))
+                .body("pagination.count", equalTo(0))
+                .body("suggestions[0]", nullValue());
+    }
+
+    private ValidatableResponse retrieveSuggestions(int page, int perPage, String query) {
+        return given()
                 .spec(api.requestSpecification())
                 .log().ifValidationFails()
                 .when()
-                .get("/entity_suggestions?page=1&per_page=100&collection=dashboards&column=title&query=Test")
+                .get("/entity_suggestions?page=" + page + "&per_page=" + perPage + "&collection=dashboards&column=title&query=" + query)
                 .then()
                 .log().ifValidationFails()
                 .assertThat()
-                .statusCode(200)
-                .body("pagination.total", equalTo(2))
-                .body("suggestions[0].value", equalTo("Test"))
-                .body("suggestions[0].id", equalTo(dashboard1))
-                .body("suggestions[1].value", equalTo("Test 2"))
-                .body("suggestions[1].id", equalTo(dashboard2));
+                .statusCode(200);
     }
 }
