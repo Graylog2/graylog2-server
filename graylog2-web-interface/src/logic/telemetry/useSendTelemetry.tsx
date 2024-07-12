5d120402976de585ddc5e03d3edc7c087217bcce
Dennis Oelkers <dennis@graylog.com>
2024-07-12 11:17:07 +0200

    Avoid excessive reexecution of `useSendTelemetry`.

--- graylog2-web-interface/src/logic/telemetry/useSendTelemetry.tsx
+++ graylog2-web-interface/src/logic/telemetry/useSendTelemetry.tsx
@@ -14,21 +14,34 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 import type { Optional } from 'utility-types';
+import { UNSAFE_DataRouterContext as DataRouterContext, matchRoutes } from 'react-router-dom';
 
 import type { TelemetryEventType, TelemetryEvent } from 'logic/telemetry/TelemetryContext';
 import TelemetryContext from 'logic/telemetry/TelemetryContext';
-import useRoutePattern from 'routing/useRoutePattern';
+import AppConfig from 'util/AppConfig';
 
 const useSendTelemetry = () => {
   const { sendTelemetry } = useContext(TelemetryContext);
-  const route = useRoutePattern();
+  const dataRouterContext = useContext(DataRouterContext);
+  const pathPrefixLength = useMemo(() => {
+    const pathPrefix = AppConfig.gl2AppPathPrefix();
 
-  return useCallback((eventType: TelemetryEventType, event: Optional<TelemetryEvent, 'app_path_pattern'>) => sendTelemetry(
-    eventType,
-    { app_path_pattern: route, ...event },
-  ), [sendTelemetry, route]);
+    return (!pathPrefix || pathPrefix === '' || pathPrefix === '/') ? 0 : pathPrefix.length;
+  }, []);
+
+  return useCallback((eventType: TelemetryEventType, event: Optional<TelemetryEvent, 'app_path_pattern'>) => {
+    const { router: { routes } } = dataRouterContext;
+    const pathname = window.location.pathname.slice(pathPrefixLength);
+    const matches = matchRoutes(routes, pathname);
+    const route = matches.at(-1).route.path;
+
+    return sendTelemetry(
+      eventType,
+      { app_path_pattern: route, ...event },
+    );
+  }, [sendTelemetry, dataRouterContext]);
 };
 
 export default useSendTelemetry;
