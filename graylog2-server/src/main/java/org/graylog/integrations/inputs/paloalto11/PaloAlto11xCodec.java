3c918378d082072cef37775e57f13325018213c6
Nithin-Kasam <nkasam@loginsoft.com>
2024-08-17 12:32:36 +0530

    palo alto 11x initial implementation

---
+++ graylog2-server/src/main/java/org/graylog/integrations/inputs/paloalto11/PaloAlto11xCodec.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.integrations.inputs.paloalto11;
+
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+import org.graylog.integrations.inputs.paloalto.PaloAltoMessageBase;
+import org.graylog.integrations.inputs.paloalto.PaloAltoParser;
+import org.graylog.schema.EventFields;
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.MessageFactory;
+import org.graylog2.plugin.configuration.Configuration;
+import org.graylog2.plugin.configuration.ConfigurationRequest;
+import org.graylog2.plugin.configuration.fields.BooleanField;
+import org.graylog2.plugin.configuration.fields.ConfigurationField;
+import org.graylog2.plugin.configuration.fields.DropdownField;
+import org.graylog2.plugin.inputs.annotations.ConfigClass;
+import org.graylog2.plugin.inputs.annotations.FactoryClass;
+import org.graylog2.plugin.inputs.codecs.Codec;
+import org.graylog2.plugin.inputs.codecs.CodecAggregator;
+import org.graylog2.plugin.journal.RawMessage;
+import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.nio.charset.StandardCharsets;
+
+public class PaloAlto11xCodec implements Codec {
+    private static final Logger LOG = LoggerFactory.getLogger(PaloAlto11xCodec.class);
+    static final String CK_STORE_FULL_MESSAGE = "store_full_message";
+    static final String CK_TIMEZONE = "timezone";
+
+    public static final String NAME = "PaloAlto11x";
+    private final Configuration configuration;
+    private final MessageFactory messageFactory;
+    private final PaloAltoParser rawMessageParser;
+    private final DateTimeZone timezone;
+
+    @AssistedInject
+    public PaloAlto11xCodec(@Assisted Configuration configuration,PaloAltoParser rawMessageParser,
+                           MessageFactory messageFactory) {
+        this.configuration = configuration;
+        this.messageFactory = messageFactory;
+        String timezoneID = configuration.getString(CK_TIMEZONE);
+        // previously existing PA inputs after updating will not have a Time Zone configured, default to UTC
+        this.timezone = timezoneID != null ? DateTimeZone.forID(timezoneID) : DateTimeZone.UTC;
+        LOG.trace("Configured with time zone: {}", timezone);
+        this.rawMessageParser = rawMessageParser;
+    }
+
+    @Nullable
+    @Override
+    public Message decode(@Nonnull RawMessage rawMessage) {
+        String s = new String(rawMessage.getPayload(), StandardCharsets.UTF_8);
+        LOG.trace("Received raw message: {}", s);
+        PaloAltoMessageBase p = rawMessageParser.parse(s, timezone);
+        // Return when error occurs parsing syslog header.
+        if (p == null) {
+            return null;
+        }
+        Message message = messageFactory.createMessage(p.payload(), p.source(), p.timestamp());
+        message.addField(EventFields.EVENT_SOURCE_PRODUCT, "PAN");
+        message.addField(EventFields.EVENT_LOG_NAME,p.panType());
+        // Store full message if configured.
+        if (configuration.getBoolean(CK_STORE_FULL_MESSAGE)) {
+            message.addField(Message.FIELD_FULL_MESSAGE, new String(rawMessage.getPayload(), StandardCharsets.UTF_8));
+        }
+        LOG.trace("Successfully processed [{}] message with [{}] fields.", p.panType(), message.getFieldCount());
+        return message;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Nonnull
+    @Override
+    public Configuration getConfiguration() {
+        return this.configuration;
+    }
+
+    @FactoryClass
+    public interface Factory extends Codec.Factory<PaloAlto11xCodec> {
+        @Override
+        PaloAlto11xCodec create(Configuration configuration);
+
+        @Override
+        PaloAlto11xCodec.Config getConfig();
+    }
+
+    @ConfigClass
+    public static class Config implements Codec.Config {
+        @Override
+        public ConfigurationRequest getRequestedConfiguration() {
+            final ConfigurationRequest r = new ConfigurationRequest();
+
+            r.addField(new DropdownField(
+                    CK_TIMEZONE,
+                    "Time Zone",
+                    DateTimeZone.UTC.getID(),
+                    DropdownField.ValueTemplates.timeZones(),
+                    "Time zone of the Palo Alto device",
+                    ConfigurationField.Optional.OPTIONAL));
+
+            r.addField(
+                    new BooleanField(
+                            CK_STORE_FULL_MESSAGE,
+                            "Store full message?",
+                            false,
+                            "Store the full original Palo Alto message as full_message?"
+                    )
+            );
+
+            return r;
+        }
+
+        @Override
+        public void overrideDefaultValues(@Nonnull ConfigurationRequest cr) {
+        }
+    }
+
+    @Nullable
+    @Override
+    public CodecAggregator getAggregator() {
+        return null;
+    }
+}
+
---
+++ graylog2-server/src/test/java/org/graylog/integrations/inputs/paloalto11/PaloAlto11xCodecTest.java
@@ -0,0 +1,98 @@
+package org.graylog.integrations.inputs.paloalto11;
+
+import org.graylog.integrations.inputs.paloalto.PaloAltoParser;
+import org.graylog.schema.EventFields;
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.MessageFactory;
+import org.graylog2.plugin.TestMessageFactory;
+import org.graylog2.plugin.configuration.Configuration;
+import org.graylog2.plugin.journal.RawMessage;
+import org.joda.time.DateTimeZone;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PaloAlto11xCodecTest {
+
+    private static final String TEST_RAW_MESSAGE = "Foo,Bar,Baz,This,That,GLOBALPROTECT";
+    private final MessageFactory messageFactory = new TestMessageFactory();
+
+    // Code Under Test
+    PaloAlto11xCodec cut;
+
+    // Mock Objects
+    @Mock
+    Configuration mockConfig;
+    @Mock
+    PaloAltoParser mockRawParser;
+    // Test Objects
+    RawMessage in;
+    Message out;
+
+    @Before
+    public void setUp() throws Exception {
+        this.cut = new PaloAlto11xCodec(mockConfig, mockRawParser, messageFactory);
+    }
+
+    @Test
+    public void decode_runsSuccessfully_whenGoodInput() {
+        givenGoodInputRawMessage();
+        givenStoreFullMessage(false);
+        whenDecodeIsCalled();
+        thenOutputMessageContainsExpectedFields(false);
+    }
+
+    @Test
+    public void decode_returnsNull_whenRawPaloParseFails() {
+        givenGoodInputRawMessage();
+        givenRawParserReturnsNull();
+        whenDecodeIsCalled();
+        thenOutputMessageIsNull();
+    }
+
+    // GIVENs
+    private void givenGoodInputRawMessage() {
+        in = new RawMessage(TEST_RAW_MESSAGE.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private void givenRawParserReturnsNull() {
+        given(mockRawParser.parse(anyString(), any(DateTimeZone.class))).willReturn(null);
+    }
+
+    private void givenStoreFullMessage(boolean storeFullMessage) {
+        given(mockConfig.getBoolean(PaloAlto11xCodec.CK_STORE_FULL_MESSAGE)).willReturn(storeFullMessage);
+    }
+
+    // WHENs
+    private void whenDecodeIsCalled() {
+        out = cut.decode(in);
+    }
+
+    private void thenOutputMessageContainsExpectedFields(boolean shouldContainFullMessage) {
+        assertThat(out, notNullValue());
+        assertThat(out.getField(EventFields.EVENT_SOURCE_PRODUCT),is("PAN"));
+        if (shouldContainFullMessage) {
+            assertThat(out.getField(Message.FIELD_FULL_MESSAGE), is(TEST_RAW_MESSAGE));
+        } else {
+            assertThat(out.getField(Message.FIELD_FULL_MESSAGE), nullValue());
+        }
+    }
+
+    private void thenOutputMessageIsNull() {
+        assertThat(out, nullValue());
+    }
+
+}
