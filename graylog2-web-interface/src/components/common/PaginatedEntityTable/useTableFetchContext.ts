8351ae291a706273681a5fc68ace9e52a0bcca6b
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2024-10-02 17:55:54 +0200

    fix useTableFetchContext refetch error

--- graylog2-web-interface/src/components/common/PaginatedEntityTable/useTableFetchContext.ts
+++ graylog2-web-interface/src/components/common/PaginatedEntityTable/useTableFetchContext.ts
@@ -16,10 +16,12 @@
  */
 import { useContext } from 'react';
 
-import TableFetchContext from './TableFetchContext';
+import TableFetchContext, { ContextValue } from './TableFetchContext';
 
-const useTableFetchContext = () => {
+const useTableFetchContext = (skip: boolean = false) => {
   const tableFetchContext = useContext(TableFetchContext);
+
+  if (skip) return {} as ContextValue;
 
   if (!tableFetchContext) {
     throw new Error('useTableFetchContext hook needs to be used inside TableFetchContext.Provider');
