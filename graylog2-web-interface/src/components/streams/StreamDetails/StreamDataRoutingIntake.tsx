83705b85809d4c0b98b19c78f96b9ac71ae7a273
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2024-10-11 12:16:40 +0200

    update MatchingTypeSwitcher component

--- graylog2-web-interface/src/components/streams/StreamDetails/StreamDataRoutingIntake.tsx
+++ graylog2-web-interface/src/components/streams/StreamDetails/StreamDataRoutingIntake.tsx
@@ -16,6 +16,7 @@
  */
 import * as React from 'react';
 import styled, { css } from 'styled-components';
+import { useQueryClient } from '@tanstack/react-query';
 
 import { type Stream } from 'stores/streams/StreamsStore';
 import { Alert, Table } from 'components/bootstrap';
@@ -34,9 +35,15 @@ export const Headline = styled.h2(({ theme }) => css`
 `);
 
 const StreamDataRoutingInstake = ({ stream }: Props) => {
+  const queryClient = useQueryClient();
+
   const hasStreamRules = !!stream.rules?.length;
   const isDefaultStream = stream.is_default;
   const isNotEditable = !stream.is_editable;
+
+  const handleMatchingTypeSwitched = () => {
+    queryClient.invalidateQueries(['stream', stream.id]);
+  }
 
   return (
     <>
@@ -53,7 +60,7 @@ const StreamDataRoutingInstake = ({ stream }: Props) => {
                                            streamId={stream.id} />
                  </IfPermitted>
              )}>
-        <MatchingTypeSwitcher stream={stream} onChange={() => {}} />
+        <MatchingTypeSwitcher stream={stream} onChange={handleMatchingTypeSwitched} />
         <Table condensed striped hover>
           <thead>
             <tr>
