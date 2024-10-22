374bb3daef010f4c4aec81ac0cbf0ade9be21865
Linus Pahl <linus.pahl@graylog.com>
2024-10-22 18:52:11 +0200

    Improve reusability of `PaginatedSelect`.

--- graylog2-web-interface/src/components/common/Select/PaginatedSelect.tsx
+++ graylog2-web-interface/src/components/common/Select/PaginatedSelect.tsx
@@ -15,17 +15,82 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import debounce from 'lodash/debounce';
 
 import Select from 'components/common/Select';
+import { Spinner } from 'components/common';
 
-type Props = React.ComponentProps<typeof Select>;
+const DEFAULT_PAGINATION = { page: 1, perPage: 50, query: '' };
 
-const PaginatedSelect = (props: Props) => {
+type Pagination = {
+  page: number,
+  perPage: number,
+  query: string,
+}
+
+type PaginatedOptions = {
+  total: number,
+  pagination: Pagination,
+  list: Array<{ label: string, value: unknown }>,
+}
+
+type Props = Omit<React.ComponentProps<typeof Select>, 'options'> & {
+  onLoadOptions: (pagination: Pagination) => Promise<PaginatedOptions>,
+}
+
+const PaginatedSelect = ({ onLoadOptions, ...rest }: Props) => {
   const selectRef = useRef();
+  const [paginatedOptions, setPaginatedOptions] = useState<PaginatedOptions | undefined>();
+  const [isSearching, setIsSearching] = useState(false);
+
+  const handleSearch = debounce((newValue, actionMeta) => {
+    if (actionMeta.action === 'input-change') {
+      setIsSearching(true);
+
+      onLoadOptions({ ...DEFAULT_PAGINATION, query: newValue }).then((results) => {
+        setIsSearching(false);
+        setPaginatedOptions(results);
+      });
+    } else if (actionMeta.action === 'menu-close') {
+      onLoadOptions(DEFAULT_PAGINATION).then(setPaginatedOptions);
+    }
+  }, 400);
+
+  const handleLoadMore = debounce(() => {
+    const { pagination, total, list } = paginatedOptions;
+
+    if (isSearching) {
+      return;
+    }
+
+    if (total > list.length) {
+      onLoadOptions({ ...pagination, page: pagination.page + 1, query: '' }).then((res) => setPaginatedOptions((cur) => ({
+        ...res,
+        list: [...cur.list, ...res.list],
+      })));
+    }
+  }, 400);
+
+  useEffect(() => {
+    if (!paginatedOptions) {
+      onLoadOptions(DEFAULT_PAGINATION).then(setPaginatedOptions);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!paginatedOptions) {
+    return <Spinner text="Loading options..." />;
+  }
 
   return (
-    <Select ref={selectRef} async {...props} />
+    <Select {...rest}
+            ref={selectRef}
+            options={paginatedOptions.list}
+            onInputChange={handleSearch}
+            loadOptions={handleLoadMore}
+            async
+            total={paginatedOptions.total} />
   );
 };
 
