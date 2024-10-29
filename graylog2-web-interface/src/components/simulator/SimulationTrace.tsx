67bffaf22fb64110d0bc4f2673b4273e3925404e
Dennis Oelkers <dennis@graylog.com>
2024-10-29 14:32:35 +0100

    Migrating `SimulationTrace`.

---
+++ graylog2-web-interface/src/components/simulator/SimulationTrace.tsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import { useEffect } from 'react';
+
+import NumberUtils from 'util/NumberUtils';
+
+import style from './SimulationTrace.lazy.css';
+
+type Props = {
+  simulationResults: { simulation_trace: Array<{ time: string, message: string }> },
+}
+
+const SimulationTrace = ({ simulationResults }: Props) => {
+  useEffect(() => {
+    style.use();
+
+    return () => style.unuse();
+  }, []);
+
+  const simulationTrace = simulationResults.simulation_trace;
+
+  const traceEntries = [];
+
+  simulationTrace.forEach((trace, idx) => {
+    // eslint-disable-next-line react/no-array-index-key
+    traceEntries.push(<dt key={`${trace.time}-${idx}-title`}>{NumberUtils.formatNumber(trace.time)} &#956;s</dt>);
+    // eslint-disable-next-line react/no-array-index-key
+    traceEntries.push(<dd key={`${trace}-${idx}-description`}><span>{trace.message}</span></dd>);
+  });
+
+  return (
+    <dl className="dl-horizontal dl-simulation-trace">
+      {traceEntries}
+    </dl>
+  );
+};
+
+export default SimulationTrace;
