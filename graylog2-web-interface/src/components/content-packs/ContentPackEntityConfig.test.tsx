ca8fce0bbc6b7b693660abce9d13d8ab9fdc3cf2
Dennis Oelkers <dennis@graylog.com>
2024-11-06 11:51:20 +0100

    Removing usage of `ReactDOM.findDOMNode`. (#20845)
    * Removing usage of `ReactDOM.findDOMNode`.
    
    * Removing unused imports.
    
    * Addressing linter hints.
    
    * Addressing linter hints.
    
    * Restoring props.

--- graylog2-web-interface/src/components/content-packs/ContentPackEntityConfig.test.tsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackEntityConfig.test.tsx
@@ -16,7 +16,6 @@
  */
 import React from 'react';
 import { render, screen } from 'wrappedTestingLibrary';
-import 'helpers/mocking/react-dom_mock';
 
 import ContentPackEntityConfig from 'components/content-packs/ContentPackEntityConfig';
 
