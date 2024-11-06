3ed0d53c72107210df18ce300b951d5ab0e6be4e
Dennis Oelkers <dennis@graylog.com>
2024-11-06 11:51:20 +0100

    Removing usage of `ReactDOM.findDOMNode`. (#20845)
    * Removing usage of `ReactDOM.findDOMNode`.
    
    * Removing unused imports.
    
    * Addressing linter hints.
    
    * Addressing linter hints.
    
    * Restoring props.

--- graylog2-web-interface/src/components/grok-patterns/GrokPatternInput.test.tsx
+++ graylog2-web-interface/src/components/grok-patterns/GrokPatternInput.test.tsx
@@ -16,7 +16,6 @@
  */
 import React from 'react';
 import { render, screen, waitFor } from 'wrappedTestingLibrary';
-import 'helpers/mocking/react-dom_mock';
 import userEvent from '@testing-library/user-event';
 
 import GrokPatternInput from 'components/grok-patterns/GrokPatternInput';
