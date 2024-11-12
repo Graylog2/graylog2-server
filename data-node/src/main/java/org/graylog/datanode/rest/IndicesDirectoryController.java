f60a97065848975c5de7d76f1708ca2103b28b34
Tomas Dvorak <tomas.dvorak@graylog.com>
2024-11-12 09:00:49 +0100

    compatibility warning for inplace migration in case of empty data dir

--- data-node/src/main/java/org/graylog/datanode/rest/IndicesDirectoryController.java
+++ data-node/src/main/java/org/graylog/datanode/rest/IndicesDirectoryController.java
@@ -29,6 +29,7 @@ import org.graylog.datanode.filesystem.index.dto.IndexerDirectoryInformation;
 import org.graylog.datanode.filesystem.index.dto.NodeInformation;
 import org.graylog.shaded.opensearch2.org.opensearch.Version;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -60,17 +61,20 @@ public class IndicesDirectoryController {
             final IndexerDirectoryInformation info = indicesDirectoryParser.parse(dataTargetDir);
             final Version currentVersion = Version.fromString(opensearchVersion);
 
+            final List<String> compatibilityWarnings = new ArrayList<>();
+
             if (info.nodes().isEmpty() || info.nodes().stream().allMatch(n -> n.indices().isEmpty())) {
-                throw new IllegalArgumentException("Your configured opensearch_data_location directory " + dataTargetDir.toAbsolutePath() + " doesn't contain any indices! Migration can't continue!");
+                compatibilityWarnings.add("Your configured opensearch_data_location directory " + dataTargetDir.toAbsolutePath() + " doesn't contain any indices! Migration can't continue!");
             }
 
             final List<String> compatibilityErrors = info.nodes().stream()
                     .filter(node -> !isNodeCompatible(node, currentVersion))
                     .map(node -> String.format(Locale.ROOT, "Current version %s of Opensearch is not compatible with index version %s", currentVersion, node.nodeVersion()))
                     .toList();
-            return new CompatibilityResult(hostname, opensearchVersion, info, compatibilityErrors);
+
+            return new CompatibilityResult(hostname, opensearchVersion, info, compatibilityErrors, compatibilityWarnings);
         } catch (Exception e) {
-            return new CompatibilityResult(hostname, opensearchVersion, new IndexerDirectoryInformation(dataTargetDir, Collections.emptyList()), Collections.singletonList(e.getMessage()));
+            return new CompatibilityResult(hostname, opensearchVersion, new IndexerDirectoryInformation(dataTargetDir, Collections.emptyList()), Collections.singletonList(e.getMessage()), Collections.emptyList());
         }
     }
 
