16ecaa0ac8eb758bba68f8cbf10451aec88de757
patrickmann <patrick.mann@graylog.com>
2024-11-14 12:43:16 +0100

    adjust tests

--- graylog2-server/src/test/java/org/graylog2/rest/resources/system/inputs/InputsResourceMaskingPasswordsTest.java
+++ graylog2-server/src/test/java/org/graylog2/rest/resources/system/inputs/InputsResourceMaskingPasswordsTest.java
@@ -22,6 +22,7 @@ import org.apache.shiro.subject.Subject;
 import org.graylog2.Configuration;
 import org.graylog2.database.NotFoundException;
 import org.graylog2.inputs.Input;
+import org.graylog2.inputs.InputDiagnosticService;
 import org.graylog2.inputs.InputService;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
 import org.graylog2.plugin.configuration.fields.ConfigurationField;
@@ -67,7 +68,7 @@ public class InputsResourceMaskingPasswordsTest {
 
     class InputsTestResource extends InputsResource {
         public InputsTestResource(InputService inputService, MessageInputFactory messageInputFactory) {
-            super(inputService, messageInputFactory, new Configuration());
+            super(inputService, mock(InputDiagnosticService.class), messageInputFactory, new Configuration());
         }
 
         @Override
