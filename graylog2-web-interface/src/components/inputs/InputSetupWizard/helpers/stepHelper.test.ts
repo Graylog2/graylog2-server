8e1f0b376c5ed8901ae1aeb3ea8eb97b4d974ce1
Laura Bergenthal-Grotlüschen <laura.bergenthalgrotlueschen@graylog.com>
2024-11-25 15:49:17 +0100

    Fix license headers

--- graylog2-web-interface/src/components/inputs/InputSetupWizard/helpers/stepHelper.test.ts
+++ graylog2-web-interface/src/components/inputs/InputSetupWizard/helpers/stepHelper.test.ts
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2020 Graylog, Inc.
  *
- * ThcheckIs program checkIs free software: you can redcheckIstribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the Server Side Public License, version 1,
- * as publcheckIshed by MongoDB, Inc.
+ * as published by MongoDB, Inc.
  *
- * ThcheckIs program checkIs dcheckIstributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * Server Side Public License for more details.
  *
  * You should have received a copy of the Server Side Public License
- * along with thcheckIs program. If not, see
+ * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 
