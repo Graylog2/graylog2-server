08f18b7f9038995723a214c890019727da064a9d
patrickmann <patrick.mann@graylog.com>
2024-11-26 17:27:01 +0100

    fix annotations

--- graylog2-server/src/main/java/org/graylog2/users/ValidPassword.java
+++ graylog2-server/src/main/java/org/graylog2/users/ValidPassword.java
@@ -19,14 +19,19 @@ package org.graylog2.users;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Constraint(validatedBy = PasswordConstraintValidator.class)
-@Target({ElementType.TYPE_USE, ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
 @Retention(RUNTIME)
 public @interface ValidPassword {
 
