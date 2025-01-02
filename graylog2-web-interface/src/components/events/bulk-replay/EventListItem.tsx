6f66561bc1f3305abe29d81a5e95e827aec38a9e
Dennis Oelkers <dennis@graylog.com>
2025-01-02 16:38:36 +0100

    Improving behavior on hover.

--- graylog2-web-interface/src/components/events/bulk-replay/EventListItem.tsx
+++ graylog2-web-interface/src/components/events/bulk-replay/EventListItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useCallback } from 'react';
 import styled, { css } from 'styled-components';
 
 import IconButton from 'components/common/IconButton';
@@ -24,6 +25,11 @@ const StyledItem = styled.li<StyledItemProps>(({ theme, $selected }) => css`
   justify-content: space-between;
   height: 30px;
   background-color: ${$selected ? theme.colors.background.secondaryNav : 'transparent'};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${theme.colors.background.body};
+  }
 `);
 
 type SummaryProps = {
@@ -32,7 +38,6 @@ type SummaryProps = {
 
 const Summary = styled.span<SummaryProps>(({ theme, $done }) => css`
   margin: 10px;
-  cursor: pointer;
   color: ${$done ? theme.colors.global.textSecondary : theme.colors.global.textDefault};
   white-space: nowrap;
   overflow: hidden;
@@ -43,15 +48,28 @@ const Summary = styled.span<SummaryProps>(({ theme, $done }) => css`
   }
 `);
 
-const EventListItem = ({ done, event, onClick, selected, removeItem, markItemAsDone }: EventListItemProps) => (
-  <StyledItem key={`event-replay-list-${event?.id}`} $selected={selected}>
-    <Summary $done={done} onClick={onClick}>{event?.message ?? <i>Unknown</i>}</Summary>
+const EventListItem = ({ done, event, onClick, selected, removeItem, markItemAsDone }: EventListItemProps) => {
+  const _removeItem = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
 
-    <ButtonGroup>
-      <IconButton onClick={() => removeItem(event.id)} title="Remove event from list" name="delete" />
-      <IconButton onClick={() => markItemAsDone(event.id)} title="Mark event as investigated" name="check" />
-    </ButtonGroup>
-  </StyledItem>
-);
+    return removeItem(event.id);
+  }, [event?.id, removeItem]);
+  const _markItemAsDone = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    return markItemAsDone(event.id);
+  }, [event?.id, markItemAsDone]);
+
+  return (
+    <StyledItem key={`event-replay-list-${event?.id}`} $selected={selected} onClick={onClick}>
+      <Summary $done={done}>{event?.message ?? <i>Unknown</i>}</Summary>
+
+      <ButtonGroup>
+        <IconButton onClick={_removeItem} title="Remove event from list" name="delete" />
+        <IconButton onClick={_markItemAsDone} title="Mark event as investigated" name="check" />
+      </ButtonGroup>
+    </StyledItem>
+  );
+};
 
 export default EventListItem;
