cb3ce4231dd9a9f9a6a116b220c72a9e2a65b95f
Dennis Oelkers <dennis@graylog.com>
2025-01-06 14:10:38 +0100

    Simplifying highlighting rule retrieval.

--- graylog2-web-interface/src/components/event-definitions/replay-search/AggregationConditions.tsx
+++ graylog2-web-interface/src/components/event-definitions/replay-search/AggregationConditions.tsx
@@ -21,7 +21,6 @@ import { useMemo, useCallback } from 'react';
 import { StaticColor } from 'views/logic/views/formatting/highlighting/HighlightingColor';
 import { ColorPickerPopover, Icon } from 'components/common';
 import { DEFAULT_CUSTOM_HIGHLIGHT_RANGE } from 'views/Constants';
-import type HighlightingRule from 'views/logic/views/formatting/highlighting/HighlightingRule';
 import { conditionToExprMapper, exprToConditionMapper } from 'views/logic/ExpressionConditionMappers';
 import useAppSelector from 'stores/useAppSelector';
 import { selectHighlightingRules } from 'views/logic/slices/highlightSelectors';
@@ -59,7 +58,7 @@ const AggregationConditions = () => {
   const { aggregations } = useAlertAndEventDefinitionData(alertId, definitionId);
   const highlightingRules = useHighlightingRules();
 
-  const aggregationsMap = useMemo(() => new Map(aggregations.map((agg) => [
+  const aggregationsMap = useMemo(() => Object.fromEntries(aggregations.map((agg) => [
     `${agg.fnSeries}${agg.expr}${agg.value}`, agg,
   ])), [aggregations]);
 
@@ -67,7 +66,7 @@ const AggregationConditions = () => {
     if (rule) {
       dispatch(updateHighlightingRule(rule, { color: StaticColor.create(newColor) }));
     } else {
-      const { value, fnSeries, expr } = aggregationsMap.get(condition);
+      const { value, fnSeries, expr } = aggregationsMap[condition];
 
       dispatch(createHighlightingRules([
         {
@@ -80,33 +79,27 @@ const AggregationConditions = () => {
     }
   }, [aggregationsMap, dispatch]);
 
-  const highlightedAggregations = useMemo<Map<string, HighlightingRule>>(() => {
-    const initial = new Map<string, HighlightingRule>(aggregations.map(
-      ({ fnSeries, value, expr }) => [
-        `${fnSeries}${expr}${value}`, undefined,
-      ],
-    ));
+  const validAggregations = aggregations.map(({ fnSeries, value, expr }) => `${fnSeries}${expr}${value}`);
 
-    return highlightingRules.reduce((acc, rule) => {
+  const highlightedAggregations = useMemo(() => Object.fromEntries(highlightingRules
+    .map((rule) => {
       const { field, value, condition } = rule;
-      const expr = conditionToExprMapper?.[condition];
-      let result = acc;
+      const expr = conditionToExprMapper[condition];
 
       if (expr) {
         const key = `${field}${expr}${value}`;
 
-        if (acc.has(key)) {
-          result = result.set(key, rule);
-        }
+        return [key, rule] as const;
       }
 
-      return result;
-    }, initial);
-  }, [aggregations, highlightingRules]);
+      return undefined;
+    })
+    .filter((rule) => rule !== undefined && validAggregations.includes(rule[0]))),
+  [highlightingRules, validAggregations]);
 
-  return highlightedAggregations?.size ? (
+  return Object.keys(highlightedAggregations).length ? (
     <List>
-      {Array.from(highlightedAggregations).map(([condition, rule]) => {
+      {Object.entries(highlightedAggregations).map(([condition, rule]) => {
         const color = rule?.color as StaticColor;
         const hexColor = color?.color;
 
