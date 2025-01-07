c44654dfa268d0a2221032bac226dced5f757b9e
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2025-01-07 18:34:24 +0100

    InputDiagnosisStep using the same structure like in other steps

--- graylog2-web-interface/src/components/inputs/InputSetupWizard/steps/InputDiagnosisStep.tsx
+++ graylog2-web-interface/src/components/inputs/InputSetupWizard/steps/InputDiagnosisStep.tsx
@@ -15,24 +15,58 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
+import styled, { css } from 'styled-components';
 
-import { Button } from 'components/bootstrap';
+import { Button, Row, Col } from 'components/bootstrap';
 import Routes from 'routing/Routes';
 import useInputSetupWizard from 'components/inputs/InputSetupWizard/hooks/useInputSetupWizard';
 
-const InputDiagnosisStep = () => {
-  const { wizardData: { input } } = useInputSetupWizard();
+import { checkHasPreviousStep } from '../helpers/stepHelper';
 
+
+const StepCol = styled(Col)(({ theme }) => css`
+  padding-left: ${theme.spacings.lg};
+  padding-right: ${theme.spacings.lg};
+  padding-top: ${theme.spacings.sm};
+`);
+
+const DescriptionCol = styled(Col)(({ theme }) => css`
+  margin-bottom: ${theme.spacings.md};
+`);
+
+const ButtonCol = styled(Col)(({ theme }) => css`
+  display: flex;
+  justify-content: flex-end;
+  gap: ${theme.spacings.xs};
+  margin-top: ${theme.spacings.lg};
+`);
+
+const InputDiagnosisStep = () => {
+  const { wizardData: { input }, goToPreviousStep, orderedSteps, activeStep } = useInputSetupWizard();
+  const hasPreviousStep = checkHasPreviousStep(orderedSteps, activeStep);
+  
   return (
-    <div>
-      <div>Test inputs and parsing without writing any data to the search cluster.</div>
-      <br />
-      {input?.id && (
-        <Button bsSize="xs" onClick={() => window.open(Routes.SYSTEM.INPUT_DIAGNOSIS(input?.id), '_blank')}>
-          Go to Input Diagnosis
-        </Button>
-      )}
-    </div>
+    <Row>
+      <StepCol md={12}>
+        <Row>
+          <DescriptionCol md={12}>
+            <p>Test inputs and parsing without writing any data to the search cluster.</p>
+          </DescriptionCol>
+        </Row>
+        {input?.id && (
+          <Button bsSize="xs" bsStyle="primary" onClick={() => window.open(Routes.SYSTEM.INPUT_DIAGNOSIS(input?.id), '_blank')}>
+            Go to Input Diagnosis
+          </Button>
+        )}
+        {hasPreviousStep && (
+          <Row>
+            <ButtonCol md={12}>
+              <Button onClick={goToPreviousStep}>Back</Button>
+            </ButtonCol>
+          </Row>
+        )}
+      </StepCol>
+    </Row>
   );
 };
 
