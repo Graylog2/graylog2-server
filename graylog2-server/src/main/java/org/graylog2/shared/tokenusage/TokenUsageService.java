b56a3417eda31ccfd0db7d1ce526e4a79dc4774c
Florian Petersen <188503754+fpetersen-gl@users.noreply.github.com>
2025-01-09 16:31:34 +0100

    apidoc

--- graylog2-server/src/main/java/org/graylog2/shared/tokenusage/TokenUsageService.java
+++ graylog2-server/src/main/java/org/graylog2/shared/tokenusage/TokenUsageService.java
@@ -6,6 +6,20 @@ import org.graylog2.rest.models.tokenusage.TokenUsage;
 import org.graylog2.search.SearchQuery;
 
 public interface TokenUsageService {
+
+    /**
+     * Loads entries of {@link TokenUsage} for the given parameters.
+     * <p>
+     * The idea is that all tokens are listed, enriched with information about its owner. This provides an overview of
+     * possibly expired tokens or those whose owner doesn't exist on the system anymore.
+     *
+     * @param page        The page to load, starting at 1.
+     * @param perPage     Number of items per page.
+     * @param searchQuery The search to perform - only items matching this query are returned.
+     * @param sort        Sort by this given field.
+     * @param order       The order of sorting ({@code asc} or {@code desc}.
+     * @return A page of matching {@link TokenUsage}, sorted by the specified field and order.
+     */
     PaginatedList<TokenUsage> loadTokenUsage(int page,
                                              int perPage,
                                              SearchQuery searchQuery,
