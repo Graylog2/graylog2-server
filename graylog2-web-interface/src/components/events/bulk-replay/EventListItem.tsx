8e389582d41460dda3f02c6edff25f1784feddfd
Dennis Oelkers <dennis@graylog.com>
2025-01-09 09:49:24 +0100

    Fixing linter hint.

--- graylog2-web-interface/src/components/events/bulk-replay/EventListItem.tsx
+++ graylog2-web-interface/src/components/events/bulk-replay/EventListItem.tsx
@@ -87,7 +87,8 @@ const EventListItem = ({ done, event, onClick, selected, removeItem, markItemAsD
         <IconButton onClick={_removeItem} title={`Remove event "${event?.id}" from list`} name="delete" />
         <CompletedButton onClick={_markItemAsDone}
                          title={`Mark event "${event?.id}" as ${done ? 'not' : ''} investigated`}
-                         name="verified" iconType={done ? 'solid' : 'regular'}
+                         name="verified"
+                         iconType={done ? 'solid' : 'regular'}
                          $done={done} />
       </ButtonGroup>
     </StyledItem>
