7fd2eba275d2710536b19ad455a45e20c24bd79f
Florian Petersen <188503754+fpetersen-gl@users.noreply.github.com>
2025-01-10 14:50:49 +0100

    Made TokenUsage a record-class

--- graylog2-server/src/main/java/org/graylog2/rest/models/tokenusage/TokenUsage.java
+++ graylog2-server/src/main/java/org/graylog2/rest/models/tokenusage/TokenUsage.java
@@ -16,42 +16,22 @@
  */
 package org.graylog2.rest.models.tokenusage;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.auto.value.AutoValue;
 import org.graylog.autovalue.WithBeanGetter;
 import org.joda.time.DateTime;
 
 import javax.annotation.Nullable;
 
-@JsonAutoDetect
-@AutoValue
 @WithBeanGetter
-public abstract class TokenUsage {
-    @JsonProperty("token_id")
-    public abstract String tokenId();
-
-    @JsonProperty
-    public abstract String username();
-
-    @JsonProperty("user_id")
-    public abstract String userId();
-
-    @JsonProperty("token_name")
-    public abstract String tokenName();
-
-    @Nullable
-    @JsonProperty("created_at")
-    public abstract DateTime createdAt();
-
-    @JsonProperty("last_access")
-    public abstract DateTime lastAccess();
-
-    @JsonProperty("user_is_external")
-    public abstract boolean userIsExternal();
-
-    @JsonProperty("auth_backend")
-    public abstract String authBackend();
+public record TokenUsage(
+        @JsonProperty("token_id") String tokenId,
+        @JsonProperty("username") String username,
+        @JsonProperty("user_id") String userId,
+        @JsonProperty("token_name") String tokenName,
+        @Nullable @JsonProperty("created_at") DateTime createdAt,
+        @JsonProperty("last_access") DateTime lastAccess,
+        @JsonProperty("user_is_external") boolean userIsExternal,
+        @JsonProperty("auth_backend") String authBackend) {
 
     public static TokenUsage create(@JsonProperty("token_id") String tokenId,
                                     @JsonProperty("username") String username,
@@ -61,7 +41,7 @@ public abstract class TokenUsage {
                                     @JsonProperty("last_access") DateTime lastAccess,
                                     @JsonProperty("user_is_external") boolean userIsExternal,
                                     @Nullable @JsonProperty("auth_backend") String authBackend) {
-        return new AutoValue_TokenUsage(tokenId, username, userId,
+        return new TokenUsage(tokenId, username, userId,
                 tokenName,
                 createdAt,
                 lastAccess,
