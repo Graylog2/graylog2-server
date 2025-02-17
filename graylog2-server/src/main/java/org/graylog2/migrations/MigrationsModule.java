578abe4f2faef08ef42bd860c597b288b3928f9f
Florian Petersen <188503754+fpetersen-gl@users.noreply.github.com>
2025-02-17 15:37:04 +0100

    Backend work for "restrict token creation to admin users" (#21619)
    * Added a configuration for allowing only admins to create new access tokens.
    
    * Add license header
    
    * Add changelog
    
    ---------
    
    Co-authored-by: Patrick Mann <patrickmann@users.noreply.github.com>

--- graylog2-server/src/main/java/org/graylog2/migrations/MigrationsModule.java
+++ graylog2-server/src/main/java/org/graylog2/migrations/MigrationsModule.java
@@ -71,5 +71,6 @@ public class MigrationsModule extends PluginModule {
         addMigration(V20240312140000_RemoveFieldTypeMappingsManagerRole.class);
         addMigration(V202404170856_UpdateIndexSetTemplates.class);
         addMigration(V20240927120300_DataNodeMigrationIndexSet.class);
+        addMigration(V20250206105400_TokenManagementConfiguration.class);
     }
 }
