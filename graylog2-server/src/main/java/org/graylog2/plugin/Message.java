b1239794d91b0e863cdd5708dba0cb69a4072301
patrickmann <patrickmann@hotmail.com>
2025-02-18 12:04:19 +0100

    add unit test

--- graylog2-server/src/main/java/org/graylog2/plugin/Message.java
+++ graylog2-server/src/main/java/org/graylog2/plugin/Message.java
@@ -519,7 +519,7 @@ public class Message implements Messages, Indexable, Acknowledgeable {
         }
     }
 
-    private void updateTimeStamp(DateTime oldTimeStamp, DateTime newTimeStamp) {
+    public void updateTimeStamp(DateTime oldTimeStamp, DateTime newTimeStamp) {
         addTimestampField(FIELD_TIMESTAMP, newTimeStamp);
         addTimestampField(FIELD_GL2_ORIGINAL_TIMESTAMP, oldTimeStamp);
     }
