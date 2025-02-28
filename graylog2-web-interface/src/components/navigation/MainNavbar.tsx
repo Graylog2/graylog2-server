158f50adcfc0ef35cec36447b3db80343edc2cbb
Linus Pahl <linus.pahl@graylog.com>
2025-02-28 15:58:26 +0100

    Simplify code for navigation items.

--- graylog2-web-interface/src/components/navigation/MainNavbar.tsx
+++ graylog2-web-interface/src/components/navigation/MainNavbar.tsx
@@ -16,97 +16,20 @@
  */
 
 import * as React from 'react';
-import type { PluginNavigation } from 'graylog-web-plugin';
 import { useMemo } from 'react';
+import type { PluginNavigation } from 'graylog-web-plugin';
 
-import { Nav, NavDropdown } from 'components/bootstrap';
+import { Nav } from 'components/bootstrap';
 import { isPermitted } from 'util/PermissionsMixin';
 import Routes, { ENTERPRISE_ROUTE_DESCRIPTION, SECURITY_ROUTE_DESCRIPTION } from 'routing/Routes';
 import filterByPerspective from 'components/perspectives/util/filterByPerspective';
 import useCurrentUser from 'hooks/useCurrentUser';
 import useActivePerspective from 'components/perspectives/hooks/useActivePerspective';
 import usePluginEntities from 'hooks/usePluginEntities';
-import AppConfig from 'util/AppConfig';
-import isActiveRoute from 'components/navigation/util/isActiveRoute';
 import { navigation as securityNavigation } from 'components/security/bindings';
-
-import NavigationLink from './NavigationLink';
+import NavigationItem from 'components/navigation/NavigationItem';
 
 const LAST_POSITION = 'last';
-const requiredFeatureFlagIsEnabled = (requiredFeatureFlag: undefined | string) =>
-  requiredFeatureFlag ? AppConfig.isFeatureEnabled(requiredFeatureFlag) : true;
-
-type PluginRouteProps = {
-  navigationItem: {
-    path: string;
-    description: string;
-    requiredFeatureFlag?: string;
-    BadgeComponent?: React.ComponentType<{ text: string }>;
-    permissions?: string | Array<string>;
-  };
-  topLevel?: boolean;
-};
-
-const PluginRoute = ({
-  navigationItem: { description, path, permissions, requiredFeatureFlag, BadgeComponent },
-  topLevel = false,
-}: PluginRouteProps) => {
-  const currentUser = useCurrentUser();
-  if (permissions && !isPermitted(currentUser.permissions, permissions)) return null;
-
-  if (!requiredFeatureFlagIsEnabled(requiredFeatureFlag)) {
-    return null;
-  }
-
-  return (
-    <NavigationLink
-      key={description}
-      description={BadgeComponent ? <BadgeComponent text={description} /> : description}
-      path={path}
-      topLevel={topLevel}
-    />
-  );
-};
-
-type PluginNavDropdownProps = {
-  navigationItem: PluginNavigation;
-  pathname: string;
-};
-
-const PluginNavDropdown = ({
-  navigationItem: { children, description, BadgeComponent, requiredFeatureFlag },
-  pathname,
-}: PluginNavDropdownProps) => {
-  const currentUser = useCurrentUser();
-
-  if (!requiredFeatureFlagIsEnabled(requiredFeatureFlag)) {
-    return null;
-  }
-
-  const activeChild = children.filter(({ path, end }) => path && isActiveRoute(pathname, path, end));
-  const title = activeChild.length > 0 ? `${description} / ${activeChild[0].description}` : description;
-  const isEmpty = !children.some(
-    (child) =>
-      isPermitted(currentUser.permissions, child.permissions) &&
-      requiredFeatureFlagIsEnabled(child.requiredFeatureFlag),
-  );
-
-  if (isEmpty) {
-    return null;
-  }
-
-  const renderBadge = children.some(
-    (child) => isPermitted(currentUser.permissions, child.permissions) && child?.BadgeComponent,
-  );
-
-  return (
-    <NavDropdown key={title} title={title} badge={renderBadge ? BadgeComponent : null} inactiveTitle={description}>
-      {children.map((childNavigationItem) => (
-        <PluginRoute navigationItem={childNavigationItem} key={childNavigationItem.description} />
-      ))}
-    </NavDropdown>
-  );
-};
 
 const _existingDropdownItemIndex = (
   existingNavigationItems: Array<PluginNavigation>,
@@ -214,15 +137,9 @@ const MainNavbar = ({ pathname }: { pathname: string }) => {
 
   return (
     <Nav className="navbar-main">
-      {navigationItems.map((navigationItem) => {
-        if (navigationItem.children) {
-          return (
-            <PluginNavDropdown navigationItem={navigationItem} pathname={pathname} key={navigationItem.description} />
-          );
-        }
-
-        return <PluginRoute navigationItem={navigationItem} key={navigationItem.description} topLevel />;
-      })}
+      {navigationItems.map((navigationItem) => (
+        <NavigationItem navigationItem={navigationItem} pathname={pathname} key={navigationItem.description} />
+      ))}
     </Nav>
   );
 };
---
+++ graylog2-web-interface/src/components/navigation/NavigationItem.tsx
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+
+import * as React from 'react';
+import type { PluginNavigation } from 'graylog-web-plugin';
+import type * as Immutable from 'immutable';
+
+import useCurrentUser from 'hooks/useCurrentUser';
+import isActiveRoute from 'components/navigation/util/isActiveRoute';
+import { isPermitted } from 'util/PermissionsMixin';
+import { NavDropdown } from 'components/bootstrap';
+import NavigationLink from 'components/navigation/NavigationLink';
+import AppConfig from 'util/AppConfig';
+
+const shouldRender = (
+  requiredFeatureFlag: string | undefined,
+  requiredPermissions: string | Array<string> | undefined,
+  userPermissions: Immutable.List<string>,
+) => {
+  if (requiredFeatureFlag) {
+    return AppConfig.isFeatureEnabled(requiredFeatureFlag);
+  }
+
+  if (requiredPermissions) {
+    return isPermitted(userPermissions, requiredPermissions);
+  }
+
+  return true;
+};
+
+type PluginNavLinkProps = {
+  path: string;
+  description: string;
+  BadgeComponent?: React.ComponentType<{ text: string }>;
+  topLevel?: boolean;
+};
+
+const PluginNavLink = ({ path, description, BadgeComponent = undefined, topLevel = false }: PluginNavLinkProps) => (
+  <NavigationLink
+    key={description}
+    description={BadgeComponent ? <BadgeComponent text={description} /> : description}
+    path={path}
+    topLevel={topLevel}
+  />
+);
+
+type PluginNavDropdownProps = {
+  description: PluginNavigation['description'];
+  BadgeComponent: PluginNavigation['BadgeComponent'];
+  menuItems: PluginNavigation['children'];
+  pathname: string;
+};
+
+const PluginNavDropdown = ({ menuItems, description, BadgeComponent, pathname }: PluginNavDropdownProps) => {
+  const currentUser = useCurrentUser();
+  const activeMenuItem = menuItems.filter(({ path, end }) => path && isActiveRoute(pathname, path, end));
+  const title = activeMenuItem.length > 0 ? `${description} / ${activeMenuItem[0].description}` : description;
+  const accessibleMenuItems = menuItems.filter((menuItem) =>
+    shouldRender(menuItem.requiredFeatureFlag, menuItem.permissions, currentUser.permissions),
+  );
+
+  if (!accessibleMenuItems.length) {
+    return null;
+  }
+
+  const renderBadge = menuItems.some((menuItem) => menuItem?.BadgeComponent);
+
+  return (
+    <NavDropdown key={title} title={title} badge={renderBadge ? BadgeComponent : null} inactiveTitle={description}>
+      {accessibleMenuItems.map((menuItem) => (
+        <PluginNavLink description={menuItem.description} path={menuItem.path} key={menuItem.description} />
+      ))}
+    </NavDropdown>
+  );
+};
+
+type Props = {
+  pathname: string;
+  navigationItem: PluginNavigation;
+};
+
+const NavigationItem = ({ navigationItem, pathname }: Props) => {
+  const currentUser = useCurrentUser();
+
+  if (!shouldRender(navigationItem.requiredFeatureFlag, navigationItem.permissions, currentUser.permissions)) {
+    return null;
+  }
+
+  if (navigationItem.children) {
+    return (
+      <PluginNavDropdown
+        menuItems={navigationItem.children}
+        description={navigationItem.description}
+        BadgeComponent={navigationItem.BadgeComponent}
+        pathname={pathname}
+        key={navigationItem.description}
+      />
+    );
+  }
+
+  return (
+    <PluginNavLink
+      path={navigationItem.path}
+      description={navigationItem.description}
+      key={navigationItem.description}
+      topLevel
+    />
+  );
+};
+
+export default NavigationItem;
