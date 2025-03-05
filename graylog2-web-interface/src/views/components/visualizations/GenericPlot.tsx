6787564521244bfff721a58e9e80d6f9d734c9c5
Dennis Oelkers <dennis@graylog.com>
2025-03-05 08:20:27 +0100

    Fixing type error.

--- graylog2-web-interface/src/views/components/visualizations/GenericPlot.tsx
+++ graylog2-web-interface/src/views/components/visualizations/GenericPlot.tsx
@@ -114,7 +114,7 @@ const nonInteractiveLayout = {
 
 const style = { height: '100%', width: '100%' };
 
-const config = { displayModeBar: false, doubleClick: false, responsive: true, showTips: false };
+const config = { displayModeBar: false, doubleClick: false, responsive: true, showTips: false } as const;
 
 const usePlotLayout = (layout: Partial<Layout>) => {
   const theme = useTheme();
