1152cb5a69ea2c8629f29ba1dba2d1c7f2b85dca
Linus Pahl <linus.pahl@graylog.com>
2025-03-06 12:14:25 +0100

    Fix test

--- graylog2-web-interface/src/views/components/TypeSpecificValue.tsx
+++ graylog2-web-interface/src/views/components/TypeSpecificValue.tsx
@@ -37,7 +37,7 @@ const usePluggableValueRenderer = () => {
   const pluggableValueRenderer = usePluginEntities('fieldTypeValueRenderer');
 
   return useMemo(
-    () => Object.fromEntries(pluggableValueRenderer.map(({ type, render }) => [type, render])),
+    () => Object.fromEntries((pluggableValueRenderer ?? []).map(({ type, render }) => [type, render])),
     [pluggableValueRenderer],
   );
 };
