104a48b6225f26872ed127014a1384a97ef6351d
Dennis Oelkers <dennis@graylog.com>
2025-03-06 09:54:27 +0100

    Migrating test to testing-library.

--- graylog2-web-interface/src/views/components/visualizations/__tests__/XYPlot.test.tsx
+++ graylog2-web-interface/src/views/components/visualizations/__tests__/XYPlot.test.tsx
@@ -15,7 +15,7 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { mount } from 'wrappedEnzyme';
+import { render, waitFor } from 'wrappedTestingLibrary';
 
 import mockComponent from 'helpers/mocking/MockComponent';
 import asMock from 'helpers/mocking/AsMock';
@@ -33,8 +33,9 @@ import TestStoreProvider from 'views/test/TestStoreProvider';
 import useViewsPlugin from 'views/test/testViewsPlugin';
 import { createSearch } from 'fixtures/searches';
 import { setTimerange } from 'views/logic/slices/viewSlice';
+import GenericPlot from 'views/components/visualizations/GenericPlot';
 
-jest.mock('../GenericPlot', () => mockComponent('GenericPlot'));
+jest.mock('../GenericPlot', () => jest.fn(mockComponent('GenericPlot')));
 jest.mock('views/logic/queries/useCurrentQuery');
 jest.mock('views/logic/queries/useCurrentQueryId');
 jest.mock('views/hooks/useViewType');
@@ -88,96 +89,109 @@ describe('XYPlot', () => {
     asMock(useCurrentQuery).mockReturnValue(defaultCurrentQuery);
     asMock(useCurrentQueryId).mockReturnValue(defaultCurrentQuery.id);
     asMock(useViewType).mockReturnValue(View.Type.Search);
+    jest.clearAllMocks();
   });
 
   it('renders generic X/Y-Plot when no timeline config is passed', () => {
     const emptyConfig = AggregationWidgetConfig.builder().build();
     const timerange = { from: 'foo', to: 'bar', type: 'absolute' };
-    const wrapper = mount(<SimpleXYPlot effectiveTimerange={timerange} config={emptyConfig} />);
-    const genericPlot = wrapper.find('GenericPlot');
+    render(<SimpleXYPlot effectiveTimerange={timerange} config={emptyConfig} />);
 
-    expect(genericPlot).toHaveProp('layout', {
-      yaxis: { fixedrange: true, rangemode: 'tozero', tickformat: ',~r', type: 'linear' },
-      xaxis: { fixedrange: true },
-      hovermode: 'x',
-      legend: { y: -0.14 },
-    });
+    expect(GenericPlot).toHaveBeenCalledWith(
+      expect.objectContaining({
+        layout: {
+          yaxis: { fixedrange: true, rangemode: 'tozero', tickformat: ',~r', type: 'linear' },
+          xaxis: { fixedrange: true },
+          hovermode: 'x',
+          legend: { y: -0.14 },
+        },
+        chartData: expect.anything(),
+      }),
+      expect.anything(),
+    );
 
-    expect(genericPlot).toHaveProp('chartData', chartData);
-
-    genericPlot.get(0).props.onZoom('from', 'to');
+    const { onZoom } = asMock(GenericPlot).mock.calls[0][0];
+    onZoom('from', 'to');
 
     expect(setTimerange).not.toHaveBeenCalled();
   });
 
-  it('adds zoom handler for timeline plot', () => {
+  it('adds zoom handler for timeline plot', async () => {
     const timerange = { from: '2018-10-12T02:04:21.723Z', to: '2018-10-12T10:04:21.723Z', type: 'absolute' };
-    const wrapper = mount(<SimpleXYPlot effectiveTimerange={timerange} />);
-    const genericPlot = wrapper.find('GenericPlot');
+    render(<SimpleXYPlot effectiveTimerange={timerange} />);
 
-    expect(genericPlot).toHaveProp(
-      'layout',
+    expect(GenericPlot).toHaveBeenCalledWith(
       expect.objectContaining({
-        xaxis: { range: ['2018-10-12T04:04:21.723+02:00', '2018-10-12T12:04:21.723+02:00'], type: 'date' },
+        'layout': expect.objectContaining({
+          xaxis: { range: ['2018-10-12T04:04:21.723+02:00', '2018-10-12T12:04:21.723+02:00'], type: 'date' },
+        }),
       }),
+      {},
     );
 
-    genericPlot.get(0).props.onZoom('2018-10-12T04:04:21.723Z', '2018-10-12T08:04:21.723Z');
+    const { onZoom } = asMock(GenericPlot).mock.calls[0][0];
+    onZoom('2018-10-12T04:04:21.723Z', '2018-10-12T08:04:21.723Z');
 
-    expect(setTimerange).toHaveBeenCalledWith('dummyquery', {
-      type: 'absolute',
-      from: '2018-10-12T04:04:21.723+00:00',
-      to: '2018-10-12T08:04:21.723+00:00',
+    await waitFor(() => {
+      expect(setTimerange).toHaveBeenCalledWith('dummyquery', {
+        type: 'absolute',
+        from: '2018-10-12T04:04:21.723+00:00',
+        to: '2018-10-12T08:04:21.723+00:00',
+      });
     });
   });
 
   it('uses effective time range from pivot result if all messages are selected', () => {
     const timerange = { from: '2018-10-12T02:04:21.723Z', to: '2018-10-12T10:04:21.723Z', type: 'absolute' };
     const currentQueryForAllMessages = defaultCurrentQuery.toBuilder().timerange(ALL_MESSAGES_TIMERANGE).build();
-    const wrapper = mount(<SimpleXYPlot effectiveTimerange={timerange} currentQuery={currentQueryForAllMessages} />);
-    const genericPlot = wrapper.find('GenericPlot');
+    render(<SimpleXYPlot effectiveTimerange={timerange} currentQuery={currentQueryForAllMessages} />);
 
-    expect(genericPlot).toHaveProp(
-      'layout',
+    expect(GenericPlot).toHaveBeenCalledWith(
       expect.objectContaining({
-        xaxis: { range: ['2018-10-12T04:04:21.723+02:00', '2018-10-12T12:04:21.723+02:00'], type: 'date' },
+        'layout': expect.objectContaining({
+          xaxis: { range: ['2018-10-12T04:04:21.723+02:00', '2018-10-12T12:04:21.723+02:00'], type: 'date' },
+        }),
       }),
+      {},
     );
   });
 
   it('sets correct plot legend position for small containers', () => {
-    const wrapper = mount(<SimpleXYPlot height={140} />);
-    const genericPlot = wrapper.find('GenericPlot');
+    render(<SimpleXYPlot height={140} />);
 
-    expect(genericPlot).toHaveProp(
-      'layout',
+    expect(GenericPlot).toHaveBeenCalledWith(
       expect.objectContaining({
-        legend: { y: -0.6 },
+        'layout': expect.objectContaining({
+          legend: { y: -0.6 },
+        }),
       }),
+      {},
     );
   });
 
   it('sets correct plot legend position for containers with medium height', () => {
-    const wrapper = mount(<SimpleXYPlot height={350} />);
-    const genericPlot = wrapper.find('GenericPlot');
+    render(<SimpleXYPlot height={350} />);
 
-    expect(genericPlot).toHaveProp(
-      'layout',
+    expect(GenericPlot).toHaveBeenCalledWith(
       expect.objectContaining({
-        legend: { y: -0.2 },
+        'layout': expect.objectContaining({
+          legend: { y: -0.2 },
+        }),
       }),
+      {},
     );
   });
 
   it('sets correct plot legend position for containers with huge height', () => {
-    const wrapper = mount(<SimpleXYPlot height={700} />);
-    const genericPlot = wrapper.find('GenericPlot');
+    render(<SimpleXYPlot height={700} />);
 
-    expect(genericPlot).toHaveProp(
-      'layout',
+    expect(GenericPlot).toHaveBeenCalledWith(
       expect.objectContaining({
-        legend: { y: -0.14 },
+        'layout': expect.objectContaining({
+          legend: { y: -0.14 },
+        }),
       }),
+      {},
     );
   });
 });
