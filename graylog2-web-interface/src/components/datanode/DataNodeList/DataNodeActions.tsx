ae668a53ccb66fbd0cb7d331d4bcaa1b1bd12223
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2025-03-07 16:11:36 +0100

    fix eslint issue

--- graylog2-web-interface/src/components/datanode/DataNodeList/DataNodeActions.tsx
+++ graylog2-web-interface/src/components/datanode/DataNodeList/DataNodeActions.tsx
@@ -67,7 +67,7 @@ const DIALOG_TEXT = {
   },
 };
 
-const DataNodeActions = ({ dataNode, refetch, displayAs = 'dropdown' }: Props) => {
+const DataNodeActions = ({ dataNode, refetch = undefined, displayAs = 'dropdown' }: Props) => {
   const [showLogsDialog, setShowLogsDialog] = useState(false);
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
   const [dialogType, setDialogType] = useState(null);
