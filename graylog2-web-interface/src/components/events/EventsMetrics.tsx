5e5832e9ecd5c8a02121f56cb9f1ae4be00d88fd
Dennis Oelkers <dennis@graylog.com>
2025-03-10 15:52:44 +0100

    Persisting state of collapsed metrics in user preferences.

--- graylog2-web-interface/src/components/events/EventsMetrics.tsx
+++ graylog2-web-interface/src/components/events/EventsMetrics.tsx
@@ -3,6 +3,10 @@ import { useCallback, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import IconButton from 'components/common/IconButton';
+import useUpdateUserLayoutPreferences from 'components/common/EntityDataTable/hooks/useUpdateUserLayoutPreferences';
+import useTableFetchContext from 'components/common/PaginatedEntityTable/useTableFetchContext';
+import useUserLayoutPreferences from 'components/common/EntityDataTable/hooks/useUserLayoutPreferences';
+import Spinner from 'components/common/Spinner';
 
 const Container = styled.div(
   ({ theme }) => css`
@@ -16,12 +20,24 @@ const HeadlineContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
 `;
-type Props = React.PropsWithChildren<{}>;
-const EventsMetrics = ({ children = undefined }: Props) => {
-  const [expanded, setExpanded] = useState(true);
+type EventsMetricsProps = React.PropsWithChildren<{
+  expanded: boolean;
+  onExpandToggle: (expanded: boolean) => void;
+}>;
+const EventsMetrics = ({ children = undefined, expanded: initialExpanded, onExpandToggle }: EventsMetricsProps) => {
+  const [expanded, setExpanded] = useState<boolean>(initialExpanded);
   const expandTitle = `${expanded ? 'Collapse' : 'Expand'} Metrics`;
   const expandIcon = expanded ? 'unfold_less' : 'unfold_more';
-  const onClick = useCallback(() => setExpanded((_expanded) => !_expanded), []);
+  const onClick = useCallback(
+    () =>
+      setExpanded((_expanded) => {
+        const newValue = !_expanded;
+        onExpandToggle(newValue);
+
+        return newValue;
+      }),
+    [onExpandToggle],
+  );
 
   return (
     <Container>
@@ -33,4 +49,26 @@ const EventsMetrics = ({ children = undefined }: Props) => {
     </Container>
   );
 };
-export default EventsMetrics;
+
+type Props = React.PropsWithChildren<{}>;
+const EventsMetricsBarrier = ({ children = undefined }: Props) => {
+  const { entityTableId } = useTableFetchContext();
+  const { mutate: updatePreferences } = useUpdateUserLayoutPreferences(entityTableId);
+  const { data, isInitialLoading: isLoadingPreferences } = useUserLayoutPreferences<{ showMetrics: boolean }>(
+    entityTableId,
+  );
+  const onExpandToggle = useCallback(
+    (expanded: boolean) => updatePreferences({ customPreferences: { showMetrics: expanded } }),
+    [updatePreferences],
+  );
+
+  return isLoadingPreferences ? (
+    <Spinner />
+  ) : (
+    <EventsMetrics expanded={data.customPreferences?.showMetrics ?? true} onExpandToggle={onExpandToggle}>
+      {children}
+    </EventsMetrics>
+  );
+};
+
+export default EventsMetricsBarrier;
