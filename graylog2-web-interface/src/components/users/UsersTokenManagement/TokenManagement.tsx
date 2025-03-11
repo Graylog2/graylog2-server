c5ee75a4e8913c2d774d2a1209d3e7749ee9d17f
kolinnewby <kolin.newby@gmail.com>
2025-03-11 09:59:19 -0600

    change permissions to admins for token management page and add propper formatting

--- graylog2-web-interface/src/components/users/UsersTokenManagement/TokenManagement.tsx
+++ graylog2-web-interface/src/components/users/UsersTokenManagement/TokenManagement.tsx
@@ -15,19 +15,32 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import React, {useMemo, useCallback} from 'react';
+import styled from 'styled-components';
 
 import {
   DEFAULT_LAYOUT, ADDITIONAL_ATTRIBUTES, COLUMNS_ORDER,
 } from 'components/users/UsersTokenManagement/constants';
+import { Row, Col } from 'components/bootstrap'
 import { fetchTokens, keyFn } from 'components/users/UsersTokenManagement/hooks/useTokens';
 import type { Token } from 'components/users/UsersTokenManagement/hooks/useTokens';
-import { PaginatedEntityTable } from 'components/common';
+import {PaginatedEntityTable} from 'components/common';
 import TokenActions from 'components/users/UsersTokenManagement/TokenManagementActions';
 
 import CustomColumnRenderers from './ColumnRenderers';
 
 
 const TokenManagement = () => {
+  const Container = styled.div`
+  .data-table {
+    overflow-x: visible;
+  }
+`;
+
+  const Header = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
   const tokenAction = useCallback(
     ({ user_id, id:tokenId, NAME:tokenName}: Token) => (
       <TokenActions userId={user_id} tokenId={tokenId} tokenName={tokenName} />
@@ -37,16 +50,25 @@ const TokenManagement = () => {
   const columnRenderers = useMemo(() => CustomColumnRenderers(), []);
 
   return (
-    <PaginatedEntityTable<Token> humanName="token management"
-                                    columnsOrder={COLUMNS_ORDER}
-                                    additionalAttributes={ADDITIONAL_ATTRIBUTES}
-                                    actionsCellWidth={320}
-                                    entityActions={tokenAction}
-                                    tableLayout={DEFAULT_LAYOUT}
-                                    fetchEntities={fetchTokens}
-                                    keyFn={keyFn}
-                                    entityAttributesAreCamelCase={false}
-                                    columnRenderers={columnRenderers} />
+    <Container>
+      <Row className="content">
+        <Col xs={12}>
+          <Header>
+            <h2>Tokens</h2>
+          </Header>
+          <PaginatedEntityTable<Token> humanName="tokens"
+                                      columnsOrder={COLUMNS_ORDER}
+                                      additionalAttributes={ADDITIONAL_ATTRIBUTES}
+                                      actionsCellWidth={320}
+                                      entityActions={tokenAction}
+                                      tableLayout={DEFAULT_LAYOUT}
+                                      fetchEntities={fetchTokens}
+                                      keyFn={keyFn}
+                                      entityAttributesAreCamelCase={false}
+                                      columnRenderers={columnRenderers} />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
