6e0aceaec641b0fa78b1becd67c220517fdeaefb
Ousmane Samba <ousmanesamba@gmail.com>
2025-03-12 14:33:54 +0100

    move tooltip to title

--- graylog2-web-interface/src/components/inputs/InputDiagnosis/DiagnosisMessageErrors.tsx
+++ graylog2-web-interface/src/components/inputs/InputDiagnosis/DiagnosisMessageErrors.tsx
@@ -71,17 +71,17 @@ const DiagnosisMessageErrors = ({ messageErrors, inputId }: Props) => {
       </StyledP>
       <StyledListGroup>
         <StyledListGroupItem>
-          Message Error at Input <DiagnosisHelp helpText={DIAGNOSIS_HELP.MESSAGE_ERROR_AT_INPUT} />:{' '}
+         <DiagnosisHelp helpText={DIAGNOSIS_HELP.MESSAGE_ERROR_AT_INPUT}><strong>Message Error at Input</strong></DiagnosisHelp>:{' '}
           {messageErrors.failures_inputs_codecs}
         </StyledListGroupItem>
         <StyledListGroupItem>
-          Message failed to process <DiagnosisHelp helpText={DIAGNOSIS_HELP.MESSAGE_FAILED_TO_PROCESS} />:{' '}
+          <DiagnosisHelp helpText={DIAGNOSIS_HELP.MESSAGE_FAILED_TO_PROCESS}><strong>Message failed to process</strong></DiagnosisHelp>:{' '}
           <LinkCompoment failureType="processing" inputId={inputId}>
             {messageErrors.failures_processing}
           </LinkCompoment>
         </StyledListGroupItem>
         <StyledListGroupItem>
-          Message failed to index <DiagnosisHelp helpText={DIAGNOSIS_HELP.MESSAGE_FAILED_TO_INDEX} />:{' '}
+         <DiagnosisHelp helpText={DIAGNOSIS_HELP.MESSAGE_FAILED_TO_INDEX}><strong>Message failed to index</strong></DiagnosisHelp>:{' '}
           <LinkCompoment failureType="indexing" inputId={inputId}>
             {messageErrors.failures_indexing}
           </LinkCompoment>
