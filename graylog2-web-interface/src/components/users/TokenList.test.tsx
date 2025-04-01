7274c7ab4638ccb9718ae4749b85f9c386dba48b
Ousmane Samba <ousmanesamba@gmail.com>
2025-04-01 18:32:26 +0200

    update test

--- graylog2-web-interface/src/components/users/TokenList.test.tsx
+++ graylog2-web-interface/src/components/users/TokenList.test.tsx
@@ -77,6 +77,7 @@ describe('<TokenList />', () => {
         token: 'beef2003',
         id: 'abc3',
         last_access: '1970-01-01T00:00:00.000Z',
+        expires_at: '2020-01-01T00:00:00.000Z',
         tokenTtl: 'PT72H',
       });
     });
