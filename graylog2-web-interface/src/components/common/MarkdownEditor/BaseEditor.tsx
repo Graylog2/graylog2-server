a9941df6aab60f39902aa4808ec029419659f95e
Dan Torrey <dan.torrey@graylog.com>
2025-04-02 07:46:54 -0500

    Merge commit from fork
    * Sanitize HTML from the Remediation Steps field
    
    * Add change log
    
    * Change log wording
    
    * Add license headers for test file.
    
    * Fix/9778 markdown editor sanitize string value (#21786)
    
    * fix(9778): Updated MarkdownEditor to match the previous implementation
    
    * fix(9778): Added changelog entry
    
    * fix(9778): Added enterprise PR reference to changelog
    # Conflicts:
    #	graylog2-web-interface/src/components/common/MarkdownEditor/Editor.tsx
    #	graylog2-web-interface/src/components/common/MarkdownEditor/EditorModal.tsx
    
    * Remove all HTML with DOMPurify config
    
    # Conflicts:
    #	graylog2-web-interface/src/components/common/Markdown.tsx
    #	graylog2-web-interface/src/components/common/MarkdownEditor/Editor.tsx
    #	graylog2-web-interface/src/components/common/MarkdownEditor/EditorModal.tsx
    
    # Conflicts:
    #	graylog2-web-interface/src/components/common/MarkdownEditor/Editor.tsx
    
    ---------
    
    Co-authored-by: Ezequiel Lopez <105670363+zeeklop@users.noreply.github.com>
    Co-authored-by: Ezequiel Lopez <ezequiel.lopez@graylog.com>

---
+++ graylog2-web-interface/src/components/common/MarkdownEditor/BaseEditor.tsx
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+import styled from 'styled-components';
+import DOMPurify from 'dompurify';
+
+import { SourceCodeEditor } from 'components/common';
+
+const EditorStyles = styled.div<{ $width: string }>`
+  & .ace_editor {
+    border: 1px solid ${({ theme }) => theme.colors.input.border} !important;
+    width: ${({ $width }) => $width};
+  }
+
+  & .ace_cursor {
+    border-color: ${({ theme }) => theme.colors.global.textDefault};
+  }
+`;
+
+type Props = {
+  value: string,
+  onChange: (mdValue: string) => void,
+  width?: string,
+  id?: string,
+  readOnly?: boolean,
+  height?: number,
+  onBlur?: (mdValue: string) => void,
+};
+
+function MDBaseEditor({
+  value,
+  onChange,
+  id,
+  readOnly,
+  height,
+  width,
+  onBlur,
+}: Props) {
+  const handleOnBlur = React.useCallback(() => {
+    const sanitizedValue = DOMPurify.sanitize(value, { USE_PROFILES: { html: false } });
+    if (onBlur) onBlur(sanitizedValue);
+    else onChange(sanitizedValue);
+  }, [onBlur, onChange, value]);
+
+  return (
+    <EditorStyles $width={width}>
+      <SourceCodeEditor id={id}
+                        mode="markdown"
+                        theme="light"
+                        toolbar={false}
+                        resizable={false}
+                        readOnly={readOnly}
+                        height={height}
+                        value={value}
+                        onChange={onChange}
+                        onBlur={handleOnBlur}
+                        onPaste={handleOnBlur} />
+    </EditorStyles>
+  );
+}
+
+MDBaseEditor.defaultProps = {
+  id: 'md-editor',
+  readOnly: false,
+  height: 200,
+  width: '100%',
+  onBlur: undefined,
+};
+
+export default MDBaseEditor;
