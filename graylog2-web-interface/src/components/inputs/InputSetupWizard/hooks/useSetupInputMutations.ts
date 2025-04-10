a267a74026c13c0ecdd01e2da6041deab2b07f19
Ousmane Samba <ousmanesamba@gmail.com>
2025-04-10 14:55:26 +0200

    fix types

--- graylog2-web-interface/src/components/inputs/InputSetupWizard/hooks/useSetupInputMutations.ts
+++ graylog2-web-interface/src/components/inputs/InputSetupWizard/hooks/useSetupInputMutations.ts
@@ -40,6 +40,7 @@ const createStream = async (stream: StreamConfiguration): Promise<{ stream_id: s
     rules: undefined,
     content_pack: undefined,
     description: undefined,
+    entity_share_request: undefined,
     ...stream,
   });
 
