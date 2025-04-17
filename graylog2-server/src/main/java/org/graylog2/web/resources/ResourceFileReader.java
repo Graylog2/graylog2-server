c27e192f4c38d8507157fa304fe362ff0675e3dd
Dennis Oelkers <dennis@graylog.com>
2025-04-17 11:01:27 +0200

    Extracting `ResourceFileReader` class, trimming resource.

---
+++ graylog2-server/src/main/java/org/graylog2/web/resources/ResourceFileReader.java
@@ -0,0 +1,105 @@
+package org.graylog2.web.resources;
+
+import com.google.common.base.Suppliers;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Resources;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.core.EntityTag;
+import org.graylog2.web.PluginAssets;
+
+import javax.annotation.Nonnull;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.function.Supplier;
+
+@Singleton
+public class ResourceFileReader {
+    private final LoadingCache<URI, FileSystem> fileSystemCache;
+
+    @Inject
+    public ResourceFileReader() {
+        this.fileSystemCache = CacheBuilder.newBuilder()
+                .maximumSize(1024)
+                .build(new CacheLoader<>() {
+                    @Override
+                    public FileSystem load(@Nonnull URI key) throws Exception {
+                        try {
+                            return FileSystems.getFileSystem(key);
+                        } catch (FileSystemNotFoundException e) {
+                            try {
+                                return FileSystems.newFileSystem(key, Collections.emptyMap());
+                            } catch (FileSystemAlreadyExistsException f) {
+                                return FileSystems.getFileSystem(key);
+                            }
+                        }
+                    }
+                });
+    }
+
+    public record ResourceFile(java.nio.file.Path path,
+                               Supplier<byte[]> contents,
+                               Supplier<EntityTag> entityTag) {
+        public static ResourceFile create(java.nio.file.Path path, byte[] fileContents) {
+            return new ResourceFile(path, () -> fileContents, Suppliers.memoize(() -> {
+                final var hashCode = Hashing.sha256().hashBytes(fileContents);
+                return new EntityTag(hashCode.toString());
+            }));
+        }
+    }
+
+    public ResourceFile readFile(String filename, Class<?> aClass) throws URISyntaxException, IOException {
+        return readFile(false, filename, aClass);
+    }
+
+    public ResourceFile readFileFromPlugin(String filename, Class<?> aClass) throws URISyntaxException, IOException {
+        return readFile(true, filename, aClass);
+    }
+
+    private ResourceFile readFile(boolean fromPlugin, String filename, Class<?> aClass) throws URISyntaxException, IOException {
+        final URL resourceUrl = aClass.getResource(pluginPrefixFilename(fromPlugin, filename));
+        if (resourceUrl == null) {
+            throw new FileNotFoundException("Resource file " + filename + " not found.");
+        }
+        final URI uri = resourceUrl.toURI();
+
+        switch (resourceUrl.getProtocol()) {
+            case "file": {
+                final var path = Paths.get(uri);
+                final var contents = Files.readAllBytes(path);
+                return ResourceFile.create(path, contents);
+            }
+            case "jar": {
+                final FileSystem fileSystem = fileSystemCache.getUnchecked(uri);
+                final java.nio.file.Path path = fileSystem.getPath(pluginPrefixFilename(fromPlugin, filename));
+                final var contents = Resources.toByteArray(resourceUrl);
+                return ResourceFile.create(path, contents);
+            }
+            default:
+                throw new IllegalArgumentException("Not a JAR or local file: " + resourceUrl);
+        }
+    }
+
+
+    @Nonnull
+    private String pluginPrefixFilename(boolean fromPlugin, String filename) {
+        if (fromPlugin) {
+            return "/" + filename;
+        } else {
+            return "/" + PluginAssets.pathPrefix + "/" + filename;
+        }
+    }
+}
--- graylog2-server/src/main/java/org/graylog2/web/resources/WebInterfaceAssetsResource.java
+++ graylog2-server/src/main/java/org/graylog2/web/resources/WebInterfaceAssetsResource.java
@@ -16,12 +16,6 @@
  */
 package org.graylog2.web.resources;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.hash.HashCode;
-import com.google.common.hash.Hashing;
-import com.google.common.io.Resources;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
@@ -30,7 +24,6 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.CacheControl;
 import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.EntityTag;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Request;
@@ -42,25 +35,14 @@ import org.graylog2.rest.RestTools;
 import org.graylog2.shared.rest.resources.csp.CSP;
 import org.graylog2.shared.rest.resources.csp.CSPDynamicFeature;
 import org.graylog2.web.IndexHtmlGenerator;
-import org.graylog2.web.PluginAssets;
 import org.graylog2.web.customization.CustomizationConfig;
-import org.jooq.lambda.tuple.Tuple2;
 
 import javax.activation.MimetypesFileTypeMap;
-import javax.annotation.Nonnull;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystemAlreadyExistsException;
-import java.nio.file.FileSystemNotFoundException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
-import java.util.Collections;
 import java.util.Date;
 import java.util.Optional;
 import java.util.Set;
@@ -78,55 +60,41 @@ public class WebInterfaceAssetsResource {
     private final MimetypesFileTypeMap mimeTypes;
     private final HttpConfiguration httpConfiguration;
     private final CustomizationConfig customizationConfig;
+    private final ResourceFileReader resourceFileReader;
     private final IndexHtmlGenerator indexHtmlGenerator;
     private final Set<Plugin> plugins;
-    private final LoadingCache<URI, FileSystem> fileSystemCache;
 
     @Inject
     public WebInterfaceAssetsResource(IndexHtmlGenerator indexHtmlGenerator,
                                       Set<Plugin> plugins,
                                       MimetypesFileTypeMap mimeTypes,
                                       HttpConfiguration httpConfiguration,
-                                      CustomizationConfig customizationConfig) {
+                                      CustomizationConfig customizationConfig,
+                                      ResourceFileReader resourceFileReader) {
         this.indexHtmlGenerator = indexHtmlGenerator;
         this.plugins = plugins;
         this.mimeTypes = requireNonNull(mimeTypes);
         this.httpConfiguration = httpConfiguration;
         this.customizationConfig = customizationConfig;
-        this.fileSystemCache = CacheBuilder.newBuilder()
-                .maximumSize(1024)
-                .build(new CacheLoader<>() {
-                    @Override
-                    public FileSystem load(@Nonnull URI key) throws Exception {
-                        try {
-                            return FileSystems.getFileSystem(key);
-                        } catch (FileSystemNotFoundException e) {
-                            try {
-                                return FileSystems.newFileSystem(key, Collections.emptyMap());
-                            } catch (FileSystemAlreadyExistsException f) {
-                                return FileSystems.getFileSystem(key);
-                            }
-                        }
-                    }
-                });
+        this.resourceFileReader = resourceFileReader;
     }
 
     @Path(ASSETS_PREFIX + "/" + FAVICON)
     @GET
     public Response getFavicon(@Context ContainerRequest request) {
-        final var fileContents = customizationConfig.favicon()
+        final var resourceFile = customizationConfig.favicon()
+                .map(favicon -> ResourceFileReader.ResourceFile.create(null, favicon))
                 .or(() -> {
                     try {
-                        return Optional.of(readFile(false, FAVICON, this.getClass()).v2());
+                        return Optional.of(resourceFileReader.readFile(FAVICON, this.getClass()));
                     } catch (URISyntaxException | IOException e) {
                         return Optional.empty();
                     }
                 })
                 .orElseThrow(NotFoundException::new);
+        final var fileContents = resourceFile.contents().get();
 
-        final HashCode hashCode = Hashing.sha256().hashBytes(fileContents);
-        final EntityTag entityTag = new EntityTag(hashCode.toString());
-
+        final var entityTag = resourceFile.entityTag().get();
         final Response.ResponseBuilder response = request.evaluatePreconditions(entityTag);
         if (response != null) {
             return response.build();
@@ -151,7 +119,7 @@ public class WebInterfaceAssetsResource {
         final var filenameWithoutSuffix = trimBasePath(filename, headers);
 
         try {
-            final var resource = readFile(true, filenameWithoutSuffix, plugin.metadata().getClass());
+            final var resource = resourceFileReader.readFileFromPlugin(filenameWithoutSuffix, plugin.metadata().getClass());
             return getResponse(request, filenameWithoutSuffix, resource);
         } catch (URISyntaxException | IOException e) {
             throw new NotFoundException("Couldn't find " + filenameWithoutSuffix + " in plugin " + pluginName, e);
@@ -169,7 +137,7 @@ public class WebInterfaceAssetsResource {
                         @PathParam("filename") String filename) {
         final var filenameWithoutSuffix = trimBasePath(filename, headers);
         try {
-            final var resource = readFile(false, filenameWithoutSuffix, this.getClass());
+            final var resource = resourceFileReader.readFile(filenameWithoutSuffix, this.getClass());
             return getResponse(request, filenameWithoutSuffix, resource);
         } catch (IOException | URISyntaxException e) {
             return generateIndexHtml(headers, (String) request.getProperty(CSPDynamicFeature.CSP_NONCE_PROPERTY));
@@ -198,13 +166,13 @@ public class WebInterfaceAssetsResource {
         return get(request, headers, originalLocation.getPath());
     }
 
-    private Response getResponse(Request request, String filename, Tuple2<java.nio.file.Path, byte[]> resource) throws IOException, URISyntaxException {
-        final byte[] fileContents = resource.v2();
 
-        final FileTime lastModifiedTime = Files.getLastModifiedTime(resource.v1());
+    private Response getResponse(Request request, String filename, ResourceFileReader.ResourceFile resource) throws IOException, URISyntaxException {
+        final byte[] fileContents = resource.contents().get();
+
+        final FileTime lastModifiedTime = Files.getLastModifiedTime(resource.path());
         final Date lastModified = Date.from(lastModifiedTime.toInstant());
-        final HashCode hashCode = Hashing.sha256().hashBytes(fileContents);
-        final EntityTag entityTag = new EntityTag(hashCode.toString());
+        final var entityTag = resource.entityTag().get();
 
         final Response.ResponseBuilder response = request.evaluatePreconditions(lastModified, entityTag);
         if (response != null) {
@@ -223,37 +191,6 @@ public class WebInterfaceAssetsResource {
                 .cacheControl(cacheControl)
                 .lastModified(lastModified)
                 .build();
-    }
-
-    private Tuple2<java.nio.file.Path, byte[]> readFile(boolean fromPlugin, String filename, Class<?> aClass) throws URISyntaxException, IOException {
-        final URL resourceUrl = aClass.getResource(pluginPrefixFilename(fromPlugin, filename));
-        if (resourceUrl == null) {
-            throw new FileNotFoundException("Resource file " + filename + " not found.");
-        }
-        final URI uri = resourceUrl.toURI();
-
-        switch (resourceUrl.getProtocol()) {
-            case "file": {
-                final var path = Paths.get(uri);
-                return new Tuple2<>(path, Files.readAllBytes(path));
-            }
-            case "jar": {
-                final FileSystem fileSystem = fileSystemCache.getUnchecked(uri);
-                final java.nio.file.Path path = fileSystem.getPath(pluginPrefixFilename(fromPlugin, filename));
-                return new Tuple2<>(path, Resources.toByteArray(resourceUrl));
-            }
-            default:
-                throw new IllegalArgumentException("Not a JAR or local file: " + resourceUrl);
-        }
-    }
-
-    @Nonnull
-    private String pluginPrefixFilename(boolean fromPlugin, String filename) {
-        if (fromPlugin) {
-            return "/" + filename;
-        } else {
-            return "/" + PluginAssets.pathPrefix + "/" + filename;
-        }
     }
 
     private Response generateIndexHtml(HttpHeaders headers, String nonce) {
