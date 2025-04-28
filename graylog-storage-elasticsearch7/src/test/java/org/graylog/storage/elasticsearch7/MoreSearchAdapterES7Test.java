5efc3fec173f9829fb7736930136ffb3b5af6de6
Dan Torrey <dan.torrey@graylog.com>
2025-04-28 17:40:10 -0500

    Add support for range queries in MoreSearchAdapter (#22413)
    * Fix invalid event range filter queries
    
    * Add license headers
    
    * Use more specific `startsWith` check
    
    (cherry picked from commit 28cb82ad5b13ca4d3101d631b8107e9b9c01ee73)

---
+++ graylog-storage-elasticsearch7/src/test/java/org/graylog/storage/elasticsearch7/MoreSearchAdapterES7Test.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.storage.elasticsearch7;
+
+import org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.QueryBuilder;
+import org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.QueryBuilders;
+import org.graylog.shaded.elasticsearch7.org.elasticsearch.index.query.RangeQueryBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+class MoreSearchAdapterES7Test {
+    private static final String FIELD = "field";
+    private static final String VALUE = "100";
+
+    @Test
+    void testBuildExtraFilter() {
+        verifyFilter("<=100", QueryBuilders.rangeQuery(FIELD).lte(VALUE), RangeQueryBuilder.class);
+        verifyFilter(">=100", QueryBuilders.rangeQuery(FIELD).gte(VALUE), RangeQueryBuilder.class);
+        verifyFilter("<100", QueryBuilders.rangeQuery(FIELD).lt(VALUE), RangeQueryBuilder.class);
+        verifyFilter(">100", QueryBuilders.rangeQuery(FIELD).gt(VALUE), RangeQueryBuilder.class);
+        verifyFilter(VALUE, QueryBuilders.multiMatchQuery(VALUE, FIELD), MultiMatchQueryBuilder.class);
+    }
+
+    private static void verifyFilter(String value, QueryBuilder expectedFilter, Class<? extends QueryBuilder> expectedFilterClass) {
+        QueryBuilder lessThanOrEqualFilter = MoreSearchAdapterES7.buildExtraFilter(FIELD, value);
+        assertInstanceOf(expectedFilterClass, lessThanOrEqualFilter);
+        assertEquals(expectedFilter, lessThanOrEqualFilter);
+    }
+}
