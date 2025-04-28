740c74cf2de8158e2cf1eb85ded4e68e477d66c2
Linus Pahl <linus.pahl@graylog.com>
2025-04-28 14:52:46 +0200

    Generalize descriptions for components in  dir.

--- graylog2-web-interface/src/components/authentication/directoryServices/ldap/WizardPageHeader.tsx
+++ graylog2-web-interface/src/components/authentication/directoryServices/ldap/WizardPageHeader.tsx
@@ -58,7 +58,7 @@ const WizardPageHeader = ({ authenticationBackend: authBackend, title }: Props) 
           title: 'Authentication documentation',
           path: DocsHelper.PAGES.USERS_ROLES,
         }}>
-        <span>Configure Graylog&apos;s authentication services of this Graylog cluster.</span>
+        <span>Configure authentication services of this cluster.</span>
       </PageHeader>
     </>
   );
