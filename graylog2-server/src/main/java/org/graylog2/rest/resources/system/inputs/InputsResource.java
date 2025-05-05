b58011a735ce95b4fb0f0a6470e3dfa1eb753c34
Matthias Oesterheld <matthias.oesterheld@graylog.com>
2025-05-05 16:48:51 +0200

    add missing check

--- graylog2-server/src/main/java/org/graylog2/rest/resources/system/inputs/InputsResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/system/inputs/InputsResource.java
@@ -1,18 +1,20 @@
 /*
- * Copyright (C) 2020 Graylog, Inc.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
+ *  * Copyright (C) 2020 Graylog, Inc.
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the Server Side Public License, version 1,
+ *  * as published by MongoDB, Inc.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * Server Side Public License for more details.
+ *  *
+ *  * You should have received a copy of the Server Side Public License
+ *  * along with this program. If not, see
+ *  * <http://www.mongodb.com/licensing/server-side-public-license>.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
- *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.graylog2.rest.resources.system.inputs;
 
@@ -133,6 +135,7 @@ public class InputsResource extends AbstractInputsResource {
         checkPermission(RestPermissions.INPUTS_READ, inputId);
 
         final Input input = inputService.find(inputId);
+        checkPermission(RestPermissions.INPUT_TYPES_READ, input.getType());
 
         return getInputSummary(input);
     }
