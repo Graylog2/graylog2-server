b03ab258148fa340ebbdce94e5590b70cd94b344
Matthias Oesterheld <matthias.oesterheld@graylog.com>
2025-05-05 17:19:02 +0200

    add input type permission check

--- graylog2-server/src/main/java/org/graylog2/rest/resources/system/inputs/AbstractInputsResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/system/inputs/AbstractInputsResource.java
@@ -47,7 +47,7 @@ public class AbstractInputsResource extends RestResource {
     protected InputSummary getInputSummary(Input input) {
         final InputDescription inputDescription = this.availableInputs.get(input.getType());
         final ConfigurationRequest configurationRequest = inputDescription != null ? inputDescription.getConfigurationRequest() : null;
-        final Map<String, Object> configuration = isPermitted(RestPermissions.INPUTS_EDIT, input.getId()) ?
+        final Map<String, Object> configuration = isPermitted(RestPermissions.INPUTS_EDIT, input.getId()) && isPermitted(RestPermissions.INPUT_TYPES_EDIT, input.getType()) ?
                 input.getConfiguration() : maskPasswordsInConfiguration(input.getConfiguration(), configurationRequest);
         return InputSummary.create(input.getTitle(),
                 input.isGlobal(),
