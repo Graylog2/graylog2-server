bc929d19d7bc8a7cd4a613bbf0354d1fc86a1322
Simon Huang <simon.huang@graylog.com>
2025-05-13 13:05:24 -0400

    Remove use of the show_event_procedures feature flag and replace with security license check

--- graylog2-web-interface/src/views/types.ts
+++ graylog2-web-interface/src/views/types.ts
@@ -522,6 +522,32 @@ export type SearchDataSource = {
   useCondition: () => boolean;
 };
 
+const LICENSE_SUBJECTS = {
+  enterprise: '/license/enterprise',
+  archive: '/license/enterprise/archive',
+  auditlog: '/license/enterprise/auditlog',
+  illuminate: '/license/enterprise/illuminate',
+  searchFilter: '/license/enterprise/search-filter',
+  customization: '/license/enterprise/customization',
+  views: '/license/enterprise/views',
+  forwarder: '/license/enterprise/forwarder',
+  report: '/license/enterprise/report',
+  security: '/license/security',
+  anomaly: '/license/anomaly',
+} as const;
+
+type LicenseSubject = (typeof LICENSE_SUBJECTS)[keyof typeof LICENSE_SUBJECTS];
+
+export type LicenseCheck = (subject: LicenseSubject) => {
+  data: {
+    valid: boolean;
+    expired: boolean;
+    violated: boolean;
+  };
+  isInitialLoading: boolean;
+  refetch: () => void;
+}
+
 declare module 'graylog-web-plugin/plugin' {
   export interface PluginExports {
     creators?: Array<Creator>;
@@ -588,5 +614,6 @@ declare module 'graylog-web-plugin/plugin' {
     'views.queryInput.commandContextProviders'?: Array<CustomCommandContextProvider<any>>;
     visualizationTypes?: Array<VisualizationType<any>>;
     widgetCreators?: Array<WidgetCreator>;
+    'licenseCheck'?: LicenseCheck;
   }
 }
