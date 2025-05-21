05765901fbbb62903177ede64de74feb8b3dbd1f
Dennis Oelkers <dennis@graylog.com>
2025-05-21 09:31:41 +0200

    Trigger error reporting action instead of rendering `RuntimeErrorPage` itself.

--- graylog2-web-interface/src/routing/loadAsync.tsx
+++ graylog2-web-interface/src/routing/loadAsync.tsx
@@ -17,8 +17,9 @@
 import * as React from 'react';
 import { useEffect } from 'react';
 
+import ErrorsActions from 'actions/errors/ErrorsActions';
 import ErrorBoundary from 'components/errors/ErrorBoundary';
-import RuntimeErrorPage from 'pages/RuntimeErrorPage';
+import { createReactError } from 'logic/errors/ReportedErrors';
 
 type Props = {
   error: Error;
@@ -28,9 +29,10 @@ const ErrorComponent = ({ error }: Props) => {
   useEffect(() => {
     // eslint-disable-next-line no-console
     console.error(error);
+    ErrorsActions.report(createReactError(error, { componentStack: error.stack }));
   }, [error]);
 
-  return <RuntimeErrorPage error={error} componentStack={error.stack} />;
+  return null;
 };
 
 type ComponentSupplier<TProps> = () => Promise<{ default: React.ComponentType<TProps> }>;
