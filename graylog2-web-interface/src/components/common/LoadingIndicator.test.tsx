033491cbc0aa0e65913dbe734f57461ea1e53e83
Linus Pahl <linus.pahl@graylog.com>
2025-05-28 14:57:13 +0200

    Fixing ESLint errors for multiple imports of react.

--- graylog2-web-interface/src/components/common/LoadingIndicator.test.tsx
+++ graylog2-web-interface/src/components/common/LoadingIndicator.test.tsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React from 'react';
+import * as React from 'react';
 import { render, screen } from 'wrappedTestingLibrary';
 import { act } from 'react';
 
--- graylog2-web-interface/src/components/event-notifications/event-notification-types/LegacyNotificationSummaryContainer.tsx
+++ graylog2-web-interface/src/components/event-notifications/event-notification-types/LegacyNotificationSummaryContainer.tsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React from 'react';
+import * as React from 'react';
 import { useEffect } from 'react';
 
 import { Spinner } from 'components/common';
