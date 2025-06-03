406f7d478d6af70c996e0e960659c90595c65726
Dennis Oelkers <dennis@graylog.com>
2025-06-03 09:11:36 +0200

    Adding tests.

---
+++ graylog2-web-interface/src/components/event-definitions/event-definition-types/FilterPreviewContainer.test.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { render, screen } from 'wrappedTestingLibrary';
+
+import type { EventDefinition } from 'components/event-definitions/event-definitions-types';
+import type { Stream } from 'logic/streams/types';
+import StreamsContext from 'contexts/StreamsContext';
+
+import FilterPreviewContainer from './FilterPreviewContainer';
+
+const eventDefinition: EventDefinition = {
+  id: 'deadbeef',
+  title: 'Sample Event Definition',
+  description: 'Sample Event Definition',
+  _scope: 'test',
+  priority: 1,
+  alert: true,
+  config: {
+    type: 'foo',
+    query: '*',
+    query_parameters: [],
+    filters: [],
+    streams: [],
+    group_by: [],
+    _is_scheduled: true,
+    series: [],
+    conditions: {
+      expression: {},
+    },
+    search_within_ms: 300,
+    execute_every_ms: 300,
+    event_limit: 10,
+  },
+  field_spec: {},
+  key_spec: [],
+  notification_settings: {
+    grace_period_ms: 300,
+    backlog_size: 10,
+  },
+  notifications: [],
+  storage: [],
+  updated_at: null,
+};
+
+const SUT = ({ streams = [] }: { streams?: Array<Stream> }) => (
+  <StreamsContext.Provider value={streams}>
+    <FilterPreviewContainer eventDefinition={eventDefinition} />
+  </StreamsContext.Provider>
+);
+
+jest.mock('./FilterPreview', () => () => <span>Filter results</span>);
+
+describe('FilterPreviewContainer', () => {
+  it('does not execute search but shows message if user does not has access to any stream', async () => {
+    render(<SUT />);
+    await screen.findByText(/Unable to preview filter, user does not have access to any streams./i);
+  });
+
+  it('executes search if user has access to at least one stream', async () => {
+    const streams: Array<Stream> = [
+      {
+        id: 'stream1',
+        creator_user_id: 'admin',
+        outputs: [],
+        matching_type: 'AND',
+        title: 'Test Stream',
+        description: 'Test Stream',
+        created_at: 'now',
+        disabled: false,
+        rules: [],
+        content_pack: null,
+        remove_matches_from_default_stream: false,
+        index_set_id: 'index-set-1',
+        is_default: false,
+        is_editable: true,
+        categories: [],
+      },
+    ];
+    render(<SUT streams={streams} />);
+    await screen.findByText(/Filter results/i);
+  });
+});
