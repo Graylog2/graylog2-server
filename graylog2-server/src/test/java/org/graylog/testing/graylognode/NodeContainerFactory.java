55b16b4bd802658e4f1aaa12ed2199325e094e90
Tomas Dvorak <tomas.dvorak@graylog.com>
2025-06-27 07:18:14 +0200

    Define illuminate_config_directory in NodeContainerFactory

--- graylog2-server/src/test/java/org/graylog/testing/graylognode/NodeContainerFactory.java
+++ graylog2-server/src/test/java/org/graylog/testing/graylognode/NodeContainerFactory.java
@@ -145,6 +145,7 @@ public class NodeContainerFactory {
                 .withEnv("GRAYLOG_TRANSPORT_EMAIL_FROM_EMAIL", "developers@graylog.com")
 
                 .withEnv("GRAYLOG_INTEGRATIONS_SCRIPTS_DIR", "/tmp")
+                .withEnv("GRAYLOG_ILLUMINATE_CONFIG_DIRECTORY", "/tmp")
 
                 .withEnv("GRAYLOG_ENABLE_DEBUG_RESOURCES", "true") // see RestResourcesModule#addDebugResources
                 .withEnv(config.configParams)
