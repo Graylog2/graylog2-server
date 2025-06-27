0af1042ab890394915531e6d837959b1299e8bc9
Bernd Ahlers <bernd@graylog.com>
2025-06-27 16:45:32 +0200

    Add Javadoc to GRNType

--- graylog2-server/src/main/java/org/graylog/grn/GRNType.java
+++ graylog2-server/src/main/java/org/graylog/grn/GRNType.java
@@ -21,18 +21,42 @@ import com.google.auto.value.AutoValue;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
 
+/**
+ * Represents a GRN type, which is a part of the {@link GRN}.
+ */
 @AutoValue
 public abstract class GRNType {
+    /**
+     * Returns the type of the GRN, e.g. "stream", "dashboard", etc.
+     *
+     * @return the type of the GRN
+     */
     public abstract String type();
 
+    /**
+     * Returns the GRN for the given entity ID
+     *
+     * @return the type of the GRN
+     */
     public GRN toGRN(String entity) {
         return newGRNBuilder().entity(entity).build();
     }
 
+    /**
+     * Returns a new {@link GRN.Builder} for this GRN type.
+     *
+     * @return the builder for the GRN
+     */
     public GRN.Builder newGRNBuilder() {
         return GRN.builder().type(type()).grnType(this);
     }
 
+    /**
+     * Creates a new GRNType instance with the specified type.
+     *
+     * @param type the type of the GRN, must not be null or empty
+     * @return a new GRNType instance
+     */
     public static GRNType create(String type) {
         checkArgument(!isNullOrEmpty(type), "type cannot be null or empty");
 
