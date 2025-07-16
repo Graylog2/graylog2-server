09aeed84d8d3a68cf67e6b11625e08b3cf8ce239
Linus Pahl <linus.pahl@graylog.com>
2025-07-16 15:14:04 +0200

    Improve `StreamCountBadge` colors by using default variants instead of defining custom colors.

--- graylog2-web-interface/src/components/streams/StreamCountBadge.tsx
+++ graylog2-web-interface/src/components/streams/StreamCountBadge.tsx
@@ -15,14 +15,20 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import styled, { css } from 'styled-components';
+import * as React from 'react';
 
-import { CountBadge } from 'components/common';
+import { Badge } from 'components/bootstrap';
 
-const StreamCountBadge = styled(CountBadge)<{ $disabled: boolean }>(
-  ({ $disabled, theme }) => css`
-    cursor: pointer;
-    background-color: ${$disabled ? theme.colors.variant.default : theme.colors.variant.light.info};
+const StyledBadge = styled(Badge)<{ onClick: () => void }>(
+  ({ onClick }) => css`
+    cursor: ${onClick ? 'pointer' : 'default'};
   `,
+);
+
+const StreamCountBadge = ({ disabled, children, onClick }) => (
+  <StyledBadge bsStyle={disabled ? 'gray' : 'info'} onClick={onClick}>
+    {children}
+  </StyledBadge>
 );
 
 export default StreamCountBadge;
