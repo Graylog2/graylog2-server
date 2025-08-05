f663d7ec4b5590c474eb34b989028144d37435da
Linus Pahl <46300478+linuspahl@users.noreply.github.com>
2025-08-05 11:29:54 +0200

    Improve updates of message table pagination. (#23013) (#23331)
    * Improve updates of message table pagination.
    
    * Adding changelog
    
    * Simplify state management by getting errors from store instead of response.
    
    * Remove not needed type.
    
    * Only show errors for related search type.
    
    ---------
    
    Co-authored-by: Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>

--- graylog2-web-interface/src/views/components/widgets/MessageList.test.tsx
+++ graylog2-web-interface/src/views/components/widgets/MessageList.test.tsx
@@ -19,6 +19,7 @@ import { render, screen, waitFor, within } from 'wrappedTestingLibrary';
 import * as Immutable from 'immutable';
 import userEvent from '@testing-library/user-event';
 
+import useSearchResult from 'views/hooks/useSearchResult';
 import { StoreMock as MockStore } from 'helpers/mocking';
 import asMock from 'helpers/mocking/AsMock';
 import { TIMESTAMP_FIELD, Messages } from 'views/Constants';
@@ -33,7 +34,6 @@ import { finishedLoading } from 'views/logic/slices/searchExecutionSlice';
 import type { AbsoluteTimeRange } from 'views/logic/queries/Query';
 import SearchResult from 'views/logic/SearchResult';
 import reexecuteSearchTypes from 'views/components/widgets/reexecuteSearchTypes';
-import type { SearchErrorResponse } from 'views/logic/SearchError';
 import TestStoreProvider from 'views/test/TestStoreProvider';
 import useViewsPlugin from 'views/test/testViewsPlugin';
 import useAutoRefresh from 'views/hooks/useAutoRefresh';
@@ -55,6 +55,7 @@ jest.mock('stores/inputs/InputsStore', () => ({
 }));
 
 jest.mock('views/hooks/useAutoRefresh');
+jest.mock('views/hooks/useSearchResult');
 
 const searchTypeResults = {
   'search-type-id': {
@@ -70,7 +71,13 @@ const dummySearchJobResults = {
   id: 'foo',
   owner: 'me',
   search_id: 'bar',
-  results: {},
+  results: {
+    deadbeef: {
+      query: { search_types: [{ id: 'search-type-id', limit: 10000 }] },
+      execution_stats: {},
+      errors: [],
+    },
+  },
 };
 jest.mock('views/hooks/useActiveQueryId');
 jest.mock('views/components/widgets/useCurrentSearchTypesResults');
@@ -234,25 +241,29 @@ describe('MessageList', () => {
     await waitFor(() => expect(stopAutoRefresh).toHaveBeenCalledTimes(1));
   });
 
-  it('displays error description, when using pagination throws an error', async () => {
-    const dispatch = jest.fn().mockResolvedValue(finishedLoading({
+  it('displays search result limit errors', async () => {
+    asMock(useSearchResult).mockReturnValue({
       result: new SearchResult({
         ...dummySearchJobResults,
-        errors: [{
-          description: 'Error description',
-        } as SearchErrorResponse],
+        errors: [
+          {
+            query_id: 'deadbeef',
+            search_type_id: 'search-type-id',
+            description: 'Error description',
+            backtrace: undefined,
+            type: 'result_window_limit',
+            result_window_limit: 10000,
+          },
+        ],
       }),
       widgetMapping: Immutable.Map(),
-    }));
-    asMock(useAppDispatch).mockReturnValue(dispatch);
+    });
 
-    const secondPageSize = 10;
+    render(<SimpleMessageList />);
 
-    render(<SimpleMessageList data={{ ...data, total: Messages.DEFAULT_LIMIT + secondPageSize }} />);
-
-    clickNextPageButton();
-
-    await screen.findByText('Error description');
+    await screen.findByText(
+      'Elasticsearch limits the search result to 10000 messages. With a page size of 10000 messages, you can use the first 1 pages. Error description',
+    );
   });
 
   it('calls render completion callback after first render', async () => {
