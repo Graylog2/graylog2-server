a483bd6df2d9578bc0b22fa68507af2817b26989
Linus Pahl <46300478+linuspahl@users.noreply.github.com>
2025-08-06 08:39:25 +0200

    Implement `StatusIcon` component and add color variants for `Icon` component. (#23335) (#23344)

--- graylog2-web-interface/src/components/streams/StreamDetails/routing-destination/IndexSetArchivingCell.tsx
+++ graylog2-web-interface/src/components/streams/StreamDetails/routing-destination/IndexSetArchivingCell.tsx
@@ -15,18 +15,16 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { PluginStore } from 'graylog-web-plugin/plugin';
 
-import { Icon, Tooltip } from 'components/common';
+import { Tooltip, StatusIcon } from 'components/common';
 
 type Props = {
-  isArchivingEnabled: boolean,
-  streamId: string,
-}
-const Wrapper = styled.div<{ $enabled: boolean }>(({ theme, $enabled }) => css`
-  color: ${$enabled ? theme.colors.variant.success : theme.colors.variant.darker.default};
-`);
+  isArchivingEnabled: boolean;
+  streamId: string;
+};
+
 const StyledDiv = styled.div`
   display: flex;
 `;
@@ -37,9 +35,7 @@ const IndexSetArchivingCell = ({ isArchivingEnabled, streamId }: Props) => {
   return (
     <StyledDiv>
       <Tooltip withArrow position="right" label={`Archiving is ${isArchivingEnabled ? 'enabled' : 'disabled'}`}>
-        <Wrapper $enabled={isArchivingEnabled}>
-          <Icon name={isArchivingEnabled ? 'check_circle' : 'cancel'} />
-        </Wrapper>
+        <StatusIcon active={isArchivingEnabled} />
       </Tooltip>
       {StreamIndexSetDataWarehouseWarning && (<StreamIndexSetDataWarehouseWarning streamId={streamId} isArchivingEnabled={isArchivingEnabled} />)}
     </StyledDiv>
