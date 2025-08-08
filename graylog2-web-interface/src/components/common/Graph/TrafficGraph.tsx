76d98946281200e44ab1b7efdccb9b96d99b5db7
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2025-08-08 15:55:47 +0200

    Fix issues with traffic chart on Cluster overview page

--- graylog2-web-interface/src/components/common/Graph/TrafficGraph.tsx
+++ graylog2-web-interface/src/components/common/Graph/TrafficGraph.tsx
@@ -14,13 +14,17 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled, { css, useTheme } from 'styled-components';
 
-import { Spinner } from 'components/common';
 import type { PlotLayout } from 'views/components/visualizations/GenericPlot';
 import GenericPlot from 'views/components/visualizations/GenericPlot';
 import AppConfig from 'util/AppConfig';
+import {
+  getHoverTemplateSettings,
+  getFormatSettingsByData,
+} from 'views/components/visualizations/utils/chartLayoutGenerators';
+import FieldUnit from 'views/logic/aggregationbuilder/FieldUnit';
 
 type Props = {
   traffic: { [key: string]: number };
@@ -37,116 +41,172 @@ const GraphWrapper = styled.div<{
   `,
 );
 
+type GeneratedLayout = {
+  range: [number, number];
+  tickvals: Array<number>;
+  ticktext: Array<string>;
+};
 const TrafficGraph = ({ width, traffic, trafficLimit = undefined }: Props) => {
   const theme = useTheme();
   const isCloud = AppConfig.isCloud();
 
-  const getMaxDailyValue = (arr) => arr.reduce((a, b) => Math.max(a, b));
+  const getMaxDailyValue = (arr: Array<number>) => arr.reduce((a, b) => Math.max(a, b));
 
-  const range = getMaxDailyValue(Object.values(traffic));
+  const yValues = useMemo(() => Object.values(traffic), [traffic]);
 
-  if (!traffic) {
-    return <Spinner />;
-  }
-
-  const trafficLimitAnnotation: Partial<PlotLayout> = {
-    annotations: [
+  const chartData = useMemo(
+    () => [
       {
-        showarrow: false,
-        text: '<b>Licensed traffic limit</b>',
-        align: 'right',
-        x: 1,
-        xref: 'paper',
-        xanchor: 'right',
-        y: trafficLimit,
-        yanchor: 'bottom',
-        font: {
-          color: theme.colors.variant.danger,
-        },
+        type: 'bar',
+        x: Object.keys(traffic),
+        y: yValues,
+        ...getHoverTemplateSettings({
+          convertedValues: yValues,
+          unit: FieldUnit.fromJSON({ abbrev: 'b', unit_type: 'size' }),
+        }),
       },
     ],
-  };
+    [traffic, yValues],
+  );
 
-  const trafficLimitAnnotationShape: Partial<PlotLayout> = {
-    shapes: [
-      {
-        type: 'line',
-        x0: 0,
-        x1: 1,
-        y0: trafficLimit,
-        y1: trafficLimit,
-        name: 'Traffic Limit',
-        xref: 'paper',
-        yref: 'y',
-        line: {
-          color: theme.colors.variant.danger,
-        },
-      },
-    ],
-  };
+  const maxDailyValue = useMemo(() => getMaxDailyValue(yValues), [yValues]);
 
-  const chartData = [
-    {
-      type: 'bar',
-      x: Object.keys(traffic),
-      y: Object.values(traffic),
-    },
-  ];
-
-  const layout: Partial<PlotLayout> = {
-    showlegend: false,
-    margin: {
-      l: 60,
-    },
-    xaxis: {
-      type: 'date',
-      title: {
-        text: 'Time',
-      },
-    },
-    hovermode: 'x',
-    hoverlabel: {
-      namelength: -1,
-    },
-    yaxis: {
-      range: isCloud ? [0, range] : null,
-      title: {
-        text: 'Bytes',
-      },
-      rangemode: 'tozero',
-      hoverformat: '.4s',
-      tickformat: 's',
-    },
-    updatemenus: [
-      {
-        buttons: [
-          {
-            args: ['yaxis.range', [0, isCloud ? trafficLimit : range]],
-            args2: [{ 'yaxis.autorange': 'True' }, { 'yaxis.range': null }],
-            label: 'Zoom/Reset',
-            method: 'relayout',
+  const trafficLimitAnnotation: Partial<PlotLayout> = useMemo(
+    () => ({
+      annotations: [
+        {
+          showarrow: false,
+          text: '<b>Licensed traffic limit</b>',
+          align: 'right',
+          x: 1,
+          xref: 'paper',
+          xanchor: 'right',
+          y: trafficLimit,
+          yanchor: 'bottom',
+          font: {
+            color: theme.colors.variant.danger,
           },
-        ],
-        direction: 'right',
-        showactive: false,
-        bordercolor: theme.colors.global.contentBackground,
-        font: {
-          color: theme.colors.global.link,
         },
-        active: 1,
-        type: 'buttons',
-        visible: trafficLimit && range < trafficLimit,
-        xanchor: 'right',
-        yanchor: 'top',
-        x: 1,
-        y: 1.3,
+      ],
+    }),
+    [theme.colors.variant.danger, trafficLimit],
+  );
+
+  const trafficLimitAnnotationShape: Partial<PlotLayout> = useMemo(
+    () => ({
+      shapes: [
+        {
+          type: 'line',
+          x0: 0,
+          x1: 1,
+          y0: trafficLimit,
+          y1: trafficLimit,
+          name: 'Traffic Limit',
+          xref: 'paper',
+          yref: 'y',
+          line: {
+            color: theme.colors.variant.danger,
+          },
+        },
+      ],
+    }),
+    [theme.colors.variant.danger, trafficLimit],
+  );
+
+  const valuesToGetFormatSettings = useMemo(
+    () => (trafficLimit ? [...yValues, trafficLimit] : yValues),
+    [trafficLimit, yValues],
+  );
+
+  const notZoomedLayout = useMemo<GeneratedLayout>(
+    () => ({
+      rangemode: 'tozero',
+      ...(getFormatSettingsByData('size', valuesToGetFormatSettings) as GeneratedLayout),
+    }),
+    [valuesToGetFormatSettings],
+  );
+  const zoomedLayout = useMemo(
+    () => ({
+      rangemode: 'tozero',
+      ...(getFormatSettingsByData('size', yValues) as GeneratedLayout),
+    }),
+    [yValues],
+  );
+
+  const layout: Partial<PlotLayout> = useMemo(
+    () => ({
+      showlegend: false,
+      margin: {
+        l: 60,
       },
+      xaxis: {
+        type: 'date',
+        title: {
+          text: 'Time',
+        },
+      },
+      hovermode: 'x',
+      hoverlabel: {
+        namelength: -1,
+      },
+      yaxis: notZoomedLayout,
+      updatemenus: [
+        {
+          buttons: [
+            {
+              // args: ['yaxis.range', [0, isCloud ? trafficLimit : range]],
+              args: [
+                {
+                  'yaxis.range': zoomedLayout.range,
+                  'yaxis.tickvals': zoomedLayout.tickvals,
+                  'yaxis.ticktext': zoomedLayout.ticktext,
+                },
+              ],
+              args2: [
+                {
+                  'yaxis.range': notZoomedLayout.range,
+                  'yaxis.tickvals': notZoomedLayout.tickvals,
+                  'yaxis.ticktext': notZoomedLayout.ticktext,
+                },
+              ],
+              label: 'Zoom/Reset',
+              method: 'relayout',
+            },
+          ],
+          direction: 'right',
+          showactive: false,
+          bordercolor: theme.colors.global.contentBackground,
+          font: {
+            color: theme.colors.global.link,
+          },
+          active: 1,
+          type: 'buttons',
+          visible: trafficLimit && maxDailyValue < trafficLimit && !isCloud,
+          xanchor: 'right',
+          yanchor: 'top',
+          x: 1,
+          y: 1.3,
+        },
+      ],
+    }),
+    [
+      isCloud,
+      notZoomedLayout,
+      maxDailyValue,
+      theme.colors.global.contentBackground,
+      theme.colors.global.link,
+      trafficLimit,
+      zoomedLayout.range,
+      zoomedLayout.ticktext,
+      zoomedLayout.tickvals,
     ],
-  };
+  );
 
-  const layoutWithTrafficLimit = { ...layout, ...trafficLimitAnnotation, ...trafficLimitAnnotationShape };
+  const trafficLayout = useMemo(() => {
+    const layoutWithTrafficLimit = { ...layout, ...trafficLimitAnnotation, ...trafficLimitAnnotationShape };
 
-  const trafficLayout = trafficLimit ? layoutWithTrafficLimit : layout;
+    return trafficLimit ? layoutWithTrafficLimit : layout;
+  }, [layout, trafficLimit, trafficLimitAnnotation, trafficLimitAnnotationShape]);
 
   return (
     <GraphWrapper $width={width}>
