01476715b0c2f9cbfa35be07bb81fd0010d7a99c
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2025-08-26 13:24:32 +0200

    rollback unrelated changes

--- graylog2-web-interface/src/components/event-definitions/hooks/useEventDefinitionMutations.ts
+++ graylog2-web-interface/src/components/event-definitions/hooks/useEventDefinitionMutations.ts
@@ -42,7 +42,6 @@ const createEventDefinition = async (newEventDefinition: EventDefinition & Entit
   const { eventDefinition, isScheduled } = extractSchedulerInfo(rest);
 
   return EventsDefinitions.create(isScheduled, {
-    // @ts-ignore
     entity: eventDefinition,
     share_request: {
       selected_collections: share_request?.selected_collections,
@@ -53,7 +52,6 @@ const createEventDefinition = async (newEventDefinition: EventDefinition & Entit
 
 const useEventDefinitionMutations = () => {
   const createMutation = useMutation({
-    // @ts-ignore
     mutationFn: createEventDefinition,
     onError: (errorThrown) => {
       UserNotification.error(
