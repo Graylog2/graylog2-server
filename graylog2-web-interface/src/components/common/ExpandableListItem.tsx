4e4b054f44bc4edae332d86e734d1e625983912a
Linus Pahl <linus.pahl@graylog.com>
2025-09-04 14:39:34 +0200

    Create ExpandableCheckboxListItem to sperate the concerns

--- graylog2-web-interface/src/components/common/ExpandableListItem.tsx
+++ graylog2-web-interface/src/components/common/ExpandableListItem.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import styled, { css } from 'styled-components';
 import { Accordion } from '@mantine/core';
 
-import { Checkbox } from 'components/bootstrap';
+const ContentContainer = styled.div(
+  ({ theme }) => css`
+    border-left: 1px ${theme.colors.gray[90]} solid;
+    margin-left: 7px;
+    margin-top: 0;
+    padding-left: 25px;
+  `,
+);
 
 const Subheader = styled.span(
   ({ theme }) => css`
@@ -12,85 +19,23 @@ const Subheader = styled.span(
   `,
 );
 
-const ContentContainer = styled.div(
-  ({ theme }) => css`
-    border-left: 1px ${theme.colors.gray[90]} solid;
-    margin-left: 7px;
-    margin-top: 0px;
-    padding-left: 25px;
-  `,
-);
-
-const _filterInputProps = (props) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { expanded, indetermined, stayExpanded, padded, ...inputProps } = props;
-
-  return inputProps;
-};
-
-type Props = React.ComponentProps<typeof Checkbox> & {
+type Props = {
   header: React.ReactNode;
-  checked?: boolean;
-  indetermined?: boolean;
-  selectable?: boolean;
-  expandable?: boolean;
-  expanded?: boolean;
-  stayExpanded?: boolean;
+  value: string;
+  children: React.ReactNode;
   subheader?: React.ReactNode;
-  children?: React.ReactNode;
-  padded?: boolean;
-  readOnly?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const ExpandableListItem = ({
-  header,
-  checked = false,
-  indetermined = false,
-  selectable = true,
-  expandable = true,
-  expanded = false,
-  stayExpanded = false,
-  subheader,
-  children = [],
-  padded = true,
-  value,
-  readOnly = false,
-  onChange = () => undefined,
-  ...otherProps
-}: Props) => {
-  const inputProps = _filterInputProps(otherProps);
-
-  const _onChange = (e) => {
-    e.stopPropagation();
-    onChange(e);
-  };
-
-  return (
-    <Accordion.Item value={value ?? String(header)}>
-      <Accordion.Control>
-        {selectable ? (
-          <Checkbox
-            title="Select item"
-            checked={checked}
-            readOnly={readOnly}
-            onClick={(e) => e.stopPropagation()}
-            onChange={_onChange}
-            inline
-            {...inputProps}>
-            <span onClick={(e) => e.stopPropagation()}>{header}</span>
-          </Checkbox>
-        ) : (
-          header
-        )}
-
-        {subheader && <Subheader>{subheader}</Subheader>}
-      </Accordion.Control>
-      <Accordion.Panel>
-        <ContentContainer>{children}</ContentContainer>
-      </Accordion.Panel>
-    </Accordion.Item>
-  );
-};
+const ExpandableListItem = ({ header, children, value, subheader = undefined }: Props) => (
+  <Accordion.Item value={value}>
+    <Accordion.Control>
+      {header}
+      {subheader && <Subheader>{subheader}</Subheader>}
+    </Accordion.Control>
+    <Accordion.Panel>
+      <ContentContainer>{children}</ContentContainer>
+    </Accordion.Panel>
+  </Accordion.Item>
+);
 
 export default ExpandableListItem;
