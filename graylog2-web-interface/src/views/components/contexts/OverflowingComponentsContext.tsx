0feea0a3859b1d923bc0b72789fa85a19e02fcb2
Dennis Oelkers <dennis@graylog.com>
2025-09-05 16:29:34 +0200

    Using simpler types.

--- graylog2-web-interface/src/views/components/contexts/OverflowingComponentsContext.tsx
+++ graylog2-web-interface/src/views/components/contexts/OverflowingComponentsContext.tsx
@@ -21,7 +21,7 @@ import type { ActionComponents } from 'views/components/actions/ActionHandler';
 
 export type OverflowingComponentsContextType = {
   overflowingComponents: ActionComponents;
-  setOverflowingComponents: React.Dispatch<React.SetStateAction<ActionComponents>>;
+  setOverflowingComponents: (newComponents: ActionComponents) => void;
 };
 
 const OverflowingComponentsContext = React.createContext<OverflowingComponentsContextType | null>(null);
