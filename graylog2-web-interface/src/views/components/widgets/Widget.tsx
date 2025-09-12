a7245b3c2bd88fcdb6fac6f45ff14cfaf09ec447
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2025-09-12 09:56:26 +0200

    remove unnecessary condition

--- graylog2-web-interface/src/views/components/widgets/Widget.tsx
+++ graylog2-web-interface/src/views/components/widgets/Widget.tsx
@@ -241,7 +241,7 @@ const Widget = ({ id, editing = false, widget, title, position, onPositionsChang
   const { stopAutoRefresh } = useAutoRefresh();
   const [loading, setLoading] = useState(false);
   const [oldWidget, setOldWidget] = useState(editing ? widget : undefined);
-  const [oldGlobalOverride, setOldGlobalOverride] = useState(editing ? globalOverride : undefined);
+  const [oldGlobalOverride, setOldGlobalOverride] = useState(null);
   const { focusedWidget, setWidgetEditing, unsetWidgetEditing } = useContext(WidgetFocusContext);
   const dispatch = useViewsDispatch();
   const sendWidgetEditTelemetry = useSendWidgetEditTelemetry();
@@ -255,7 +255,7 @@ const Widget = ({ id, editing = false, widget, title, position, onPositionsChang
     if (editing) {
       unsetWidgetEditing();
       setOldWidget(undefined);
-      setOldGlobalOverride(undefined);
+      setOldGlobalOverride(null);
     } else {
       sendWidgetEditTelemetry();
       stopAutoRefresh();
