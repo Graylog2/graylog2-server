22404ffdfacf3491de370bf3da904d5b1b1d7842
Dennis Oelkers <dennis@graylog.com>
2025-09-22 20:28:35 +0200

    Waiting for deflector when creating index sets in tests. (#23716)

--- graylog2-server/src/test/java/org/graylog/testing/completebackend/apis/Indices.java
+++ graylog2-server/src/test/java/org/graylog/testing/completebackend/apis/Indices.java
@@ -68,6 +68,7 @@ public class Indices implements GraylogRestApi {
                 .assertThat().body("id", notNullValue())
                 .extract().body().jsonPath().getString("id");
         waitForIndexNames(id);
+        waitForDeflector(id);
         return id;
     }
 
@@ -124,6 +125,28 @@ public class Indices implements GraylogRestApi {
                 .retryIfResult(List::isEmpty)
                 .build()
                 .call(() -> listOpenIndices(indexSetId).properJSONPath().read("indices.*.index_name"));
+    }
+
+    private boolean isDeflectorUp(String indexSetId) {
+        final var response = given()
+                .spec(api.requestSpecification())
+                .log().ifValidationFails()
+                .when()
+                .get("/system/indexer/overview/" + indexSetId);
+        if (response.statusCode() == 200) {
+            return new GraylogApiResponse(response.then()).properJSONPath().read("deflector.is_up", Boolean.class);
+        } else {
+            return false;
+        }
+    }
+
+    private void waitForDeflector(String indexSetId) throws ExecutionException, RetryException {
+        RetryerBuilder.<Boolean>newBuilder()
+                .withWaitStrategy(WaitStrategies.fixedWait(1, TimeUnit.SECONDS))
+                .withStopStrategy(StopStrategies.stopAfterAttempt(30))
+                .retryIfResult(result -> result == null || result.equals(false))
+                .build()
+                .call(() -> isDeflectorUp(indexSetId));
     }
 
     public void rotateIndexSet(String indexSetId) {
