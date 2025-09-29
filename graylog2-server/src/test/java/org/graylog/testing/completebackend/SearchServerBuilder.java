4d052cdcc73dc7648bf046b463892ccebf25f82b
Bernd Ahlers <bernd@graylog.com>
2025-09-29 16:45:34 +0200

    Bump default heap in SearchServerBuilder to 1 GB

--- graylog2-server/src/test/java/org/graylog/testing/completebackend/SearchServerBuilder.java
+++ graylog2-server/src/test/java/org/graylog/testing/completebackend/SearchServerBuilder.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class SearchServerBuilder<T extends SearchServerInstance> {
-    public static final String DEFAULT_HEAP_SIZE = "512m";
+    public static final String DEFAULT_HEAP_SIZE = "1g";
     private final SearchVersion version;
     private String hostname = "indexer";
     private Network network;
