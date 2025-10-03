f194629ec59956dd59a19f71747c8bb620c98d48
robfromboulder <robfromboulder@gmail.com>
2025-10-03 11:52:03 -0600

    Add MCP configuration panel and storage

---
+++ graylog2-server/src/main/java/org/graylog/mcp/config/McpConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.mcp.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+
+@JsonAutoDetect
+@AutoValue
+public abstract class McpConfiguration {
+    public static final McpConfiguration DEFAULT_VALUES = create(true);
+
+    @JsonProperty("enable_remote_access")
+    public abstract boolean enableRemoteAccess();
+
+    @JsonCreator
+    public static McpConfiguration create(@JsonProperty("enable_remote_access") boolean enableRemoteAccess) {
+        return new AutoValue_McpConfiguration(enableRemoteAccess);
+    }
+}
