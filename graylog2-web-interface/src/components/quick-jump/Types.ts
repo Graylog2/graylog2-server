ab2ee824d22bbc529fc2091ee70b810429e958da
Linus Pahl <linus.pahl@graylog.com>
2025-10-07 15:26:03 +0200

    Fix typo.

--- graylog2-web-interface/src/components/quick-jump/Types.ts
+++ graylog2-web-interface/src/components/quick-jump/Types.ts
@@ -19,7 +19,7 @@ import type { QualifiedUrl } from 'routing/Routes';
 
 export type SearchResultItem = {
   type: typeof PAGE_TYPE | typeof ENTITY_TYPE;
-  link: QualifiedUrl<string;
+  link: QualifiedUrl<string>;
   title: string;
   backendScore?: number;
 };
