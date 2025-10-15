a99314a5d4b3a00f937e3c0a5095d137e79b075b
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2025-10-15 11:00:13 +0200

    Add back button to chart click popover

---
+++ graylog2-web-interface/src/views/components/visualizations/OnClickPopover/PopoverTitle.tsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+import { IconButton } from 'components/common';
+
+const Title = styled.div(
+  ({ theme }) => css`
+    display: flex;
+    gap: ${theme.spacings.xs};
+    color: ${theme.colors.gray[60]};
+    align-items: center;
+    font-size: ${theme.fonts.size.small};
+    min-height: 25px; // as icon button
+  `,
+);
+
+const StyledIconButton = styled(IconButton)`
+  position: absolute;
+  left: 0;
+`;
+
+const StyledLabel = styled.b(
+  ({ theme }) => css`
+    margin-left: ${theme.spacings.sm};
+  `,
+);
+
+type Props = React.PropsWithChildren<{ onBackClick?: () => void | false }>;
+
+const PopoverTitle = ({ children = null, onBackClick = false }: Props) => (
+  <Title>
+    {onBackClick && <StyledIconButton size="xs" name="arrow_back" title="Back" onClick={onBackClick} />}
+    <StyledLabel>{children}</StyledLabel>
+  </Title>
+);
+
+export default PopoverTitle;
