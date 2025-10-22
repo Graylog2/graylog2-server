cec72da6dec8c809d8218d4d1d4d50d7cdd7e785
Tomas Dvorak <tomas.dvorak@graylog.com>
2025-10-22 11:15:05 +0200

    Copy-paste OS2 client to OS3 module

---
+++ graylog-storage-opensearch3/src/main/java/org/graylog/storage/opensearch3/OpenSearch2Module.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.storage.opensearch3;
+
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.binder.LinkedBindingBuilder;
+import com.google.inject.multibindings.Multibinder;
+import org.graylog.events.search.MoreSearchAdapter;
+import org.graylog.plugins.datanode.DatanodeUpgradeServiceAdapter;
+import org.graylog.plugins.views.migrations.V20200730000000_AddGl2MessageIdFieldAliasForEvents;
+import org.graylog.plugins.views.search.engine.QuerySuggestionsService;
+import org.graylog.shaded.opensearch2.org.apache.http.client.CredentialsProvider;
+import org.graylog.shaded.opensearch2.org.opensearch.client.RestHighLevelClient;
+import org.graylog.storage.opensearch3.client.IndexerHostsAdapterOS2;
+import org.graylog.storage.opensearch3.client.OSCredentialsProvider;
+import org.graylog.storage.opensearch3.fieldtypes.streams.StreamsForFieldRetrieverOS2;
+import org.graylog.storage.opensearch3.migrations.V20170607164210_MigrateReopenedIndicesToAliasesClusterStateOS2;
+import org.graylog.storage.opensearch3.sniffer.SnifferBuilder;
+import org.graylog.storage.opensearch3.sniffer.SnifferFilter;
+import org.graylog.storage.opensearch3.sniffer.impl.DatanodesSniffer;
+import org.graylog.storage.opensearch3.sniffer.impl.NodeAttributesFilter;
+import org.graylog.storage.opensearch3.sniffer.impl.NodeLoggingFilter;
+import org.graylog.storage.opensearch3.sniffer.impl.OpensearchClusterSniffer;
+import org.graylog.storage.opensearch3.views.migrations.V20200730000000_AddGl2MessageIdFieldAliasForEventsOS2;
+import org.graylog2.indexer.IndexToolsAdapter;
+import org.graylog2.indexer.client.IndexerHostsAdapter;
+import org.graylog2.indexer.cluster.ClusterAdapter;
+import org.graylog2.indexer.cluster.NodeAdapter;
+import org.graylog2.indexer.counts.CountsAdapter;
+import org.graylog2.indexer.datanode.ProxyRequestAdapter;
+import org.graylog2.indexer.datanode.RemoteReindexingMigrationAdapter;
+import org.graylog2.indexer.datastream.DataStreamAdapter;
+import org.graylog2.indexer.fieldtypes.IndexFieldTypePollerAdapter;
+import org.graylog2.indexer.fieldtypes.streamfiltered.esadapters.StreamsForFieldRetriever;
+import org.graylog2.indexer.indices.IndicesAdapter;
+import org.graylog2.indexer.messages.MessagesAdapter;
+import org.graylog2.indexer.results.MultiChunkResultRetriever;
+import org.graylog2.indexer.searches.SearchesAdapter;
+import org.graylog2.indexer.security.SecurityAdapter;
+import org.graylog2.migrations.V20170607164210_MigrateReopenedIndicesToAliases;
+import org.graylog2.plugin.VersionAwareModule;
+import org.graylog2.storage.SearchVersion;
+
+public class OpenSearch2Module extends VersionAwareModule {
+    private final SearchVersion supportedVersion;
+    private final boolean useComposableIndexTemplates;
+
+    public OpenSearch2Module(final SearchVersion supportedVersion, boolean useComposableIndexTemplates) {
+        this.supportedVersion = supportedVersion;
+        this.useComposableIndexTemplates = useComposableIndexTemplates;
+    }
+
+    @Override
+    protected void configure() {
+        bindForSupportedVersion(StreamsForFieldRetriever.class).to(StreamsForFieldRetrieverOS2.class);
+        bindForSupportedVersion(CountsAdapter.class).to(CountsAdapterOS2.class);
+        bindForSupportedVersion(ClusterAdapter.class).to(ClusterAdapterOS2.class);
+        bindForSupportedVersion(IndicesAdapter.class).to(IndicesAdapterOS2.class);
+        bindForSupportedVersion(DataStreamAdapter.class).to(DataStreamAdapterOS2.class);
+        bindForSupportedVersion(SecurityAdapter.class).to(SecurityAdapterOS.class);
+        if (useComposableIndexTemplates) {
+            bind(IndexTemplateAdapter.class).to(ComposableIndexTemplateAdapter.class);
+        } else {
+            bind(IndexTemplateAdapter.class).to(LegacyIndexTemplateAdapter.class);
+        }
+        bindForSupportedVersion(IndexFieldTypePollerAdapter.class).to(IndexFieldTypePollerAdapterOS2.class);
+        bindForSupportedVersion(IndexToolsAdapter.class).to(IndexToolsAdapterOS2.class);
+        bindForSupportedVersion(MessagesAdapter.class).to(MessagesAdapterOS2.class);
+        bindForSupportedVersion(MultiChunkResultRetriever.class).to(PaginationOS2.class);
+        bindForSupportedVersion(MoreSearchAdapter.class).to(MoreSearchAdapterOS2.class);
+        bindForSupportedVersion(NodeAdapter.class).to(NodeAdapterOS2.class);
+        bindForSupportedVersion(SearchesAdapter.class).to(SearchesAdapterOS2.class);
+        bindForSupportedVersion(V20170607164210_MigrateReopenedIndicesToAliases.ClusterState.class)
+                .to(V20170607164210_MigrateReopenedIndicesToAliasesClusterStateOS2.class);
+        bindForSupportedVersion(V20200730000000_AddGl2MessageIdFieldAliasForEvents.ElasticsearchAdapter.class)
+                .to(V20200730000000_AddGl2MessageIdFieldAliasForEventsOS2.class);
+
+        bindForSupportedVersion(QuerySuggestionsService.class).to(QuerySuggestionsOS2.class);
+
+        bindForSupportedVersion(ProxyRequestAdapter.class).to(ProxyRequestAdapterOS2.class);
+        bindForSupportedVersion(RemoteReindexingMigrationAdapter.class).to(RemoteReindexingMigrationAdapterOS2.class);
+
+        install(new FactoryModuleBuilder().build(ScrollResultOS2.Factory.class));
+
+        bind(RestHighLevelClient.class).toProvider(RestClientProvider.class);
+        bind(CredentialsProvider.class).toProvider(OSCredentialsProvider.class);
+        bindForSupportedVersion(DatanodeUpgradeServiceAdapter.class).to(DatanodeUpgradeServiceAdapterOS2.class);
+
+        Multibinder<SnifferBuilder> snifferBuilders = Multibinder.newSetBinder(binder(), SnifferBuilder.class);
+        snifferBuilders.addBinding().to(OpensearchClusterSniffer.class);
+        snifferBuilders.addBinding().to(DatanodesSniffer.class);
+
+        Multibinder<SnifferFilter> snifferFilters = Multibinder.newSetBinder(binder(), SnifferFilter.class);
+        snifferFilters.addBinding().to(NodeAttributesFilter.class);
+        snifferFilters.addBinding().to(NodeLoggingFilter.class);
+
+        bindForSupportedVersion(IndexerHostsAdapter.class).to(IndexerHostsAdapterOS2.class);
+    }
+
+    private <T> LinkedBindingBuilder<T> bindForSupportedVersion(Class<T> interfaceClass) {
+        return bindForVersion(supportedVersion, interfaceClass);
+    }
+}
---
+++ graylog-storage-opensearch3/src/test/java/org/graylog/storage/opensearch3/testing/DatanodeInstanceBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.storage.opensearch3.testing;
+
+import org.graylog.storage.opensearch3.testing.DatanodeInstance;
+import org.graylog.testing.completebackend.SearchServerBuilder;
+import org.graylog2.storage.SearchVersion;
+
+public class DatanodeInstanceBuilder extends SearchServerBuilder<DatanodeInstance> {
+    public DatanodeInstanceBuilder(SearchVersion version) {
+        super(version);
+    }
+
+    @Override
+    protected DatanodeInstance instantiate() {
+        return new DatanodeInstance(
+                getCachedInstance(),
+                getVersion(),
+                getHostname(),
+                getNetwork(),
+                getMongoDbUri(),
+                getPasswordSecret(),
+                getHeapSize(),
+                getFeatureFlags(),
+                getEnv(),
+                getDatanodePluginJarsProvider()
+        ).init();
+    }
+}
