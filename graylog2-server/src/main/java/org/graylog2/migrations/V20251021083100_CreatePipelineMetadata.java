25962249341ae0d4f4d82c9ac0625485973261a8
patrickmann <patrick.mann@graylog.com>
2025-10-23 12:22:19 +0200

    resolve rules

--- graylog2-server/src/main/java/org/graylog2/migrations/V20251021083100_CreatePipelineMetadata.java
+++ graylog2-server/src/main/java/org/graylog2/migrations/V20251021083100_CreatePipelineMetadata.java
@@ -19,23 +19,31 @@ package org.graylog2.migrations;
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.ImmutableMap;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.InsertOneModel;
 import jakarta.inject.Inject;
 import org.graylog.plugins.pipelineprocessor.ast.Pipeline;
 import org.graylog.plugins.pipelineprocessor.ast.Rule;
-import org.graylog.plugins.pipelineprocessor.db.PipelineDao;
+import org.graylog.plugins.pipelineprocessor.ast.Stage;
+import org.graylog.plugins.pipelineprocessor.db.PipelineMetadataDao;
 import org.graylog.plugins.pipelineprocessor.db.PipelineService;
 import org.graylog.plugins.pipelineprocessor.db.PipelineStreamConnectionsService;
 import org.graylog.plugins.pipelineprocessor.db.RuleService;
-import org.graylog.plugins.pipelineprocessor.db.mongodb.MongoDbPipelineMetadataService;
 import org.graylog.plugins.pipelineprocessor.parser.PipelineRuleParser;
 import org.graylog.plugins.pipelineprocessor.processors.PipelineMetricRegistry;
 import org.graylog.plugins.pipelineprocessor.processors.PipelineResolver;
 import org.graylog.plugins.pipelineprocessor.processors.PipelineResolverConfig;
+import org.graylog2.database.MongoCollection;
+import org.graylog2.database.MongoCollections;
 import org.graylog2.database.MongoConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.graylog.plugins.pipelineprocessor.db.mongodb.MongoDbPipelineMetadataService.COLLECTION_NAME;
 
 /**
  * Migration creating the pipeline metadata collection if it doesn't exist.
@@ -46,9 +54,12 @@ public class V20251021083100_CreatePipelineMetadata extends Migration {
     private final MongoDatabase db;
     private final PipelineResolver pipelineResolver;
     private final PipelineMetricRegistry pipelineMetricRegistry;
+    private final MongoCollection<PipelineMetadataDao> collection;
+
 
     @Inject
     public V20251021083100_CreatePipelineMetadata(MongoConnection mongoConnection,
+                                                  MongoCollections mongoCollections,
                                                   RuleService ruleService,
                                                   PipelineService pipelineService,
                                                   PipelineStreamConnectionsService pipelineStreamConnectionsService,
@@ -57,6 +68,7 @@ public class V20251021083100_CreatePipelineMetadata extends Migration {
                                                   PipelineResolver.Factory pipelineResolverFactory) {
         this.pipelineService = pipelineService;
         this.db = mongoConnection.getMongoDatabase();
+        this.collection = mongoCollections.collection(COLLECTION_NAME, PipelineMetadataDao.class);
         this.pipelineMetricRegistry = PipelineMetricRegistry.create(metricRegistry, Pipeline.class.getName(), Rule.class.getName());
         this.pipelineResolver = pipelineResolverFactory.create(
                 PipelineResolverConfig.of(
@@ -82,7 +94,7 @@ public class V20251021083100_CreatePipelineMetadata extends Migration {
 
     private boolean collectionExists() {
         for (String name : db.listCollectionNames()) {
-            if (name.equals(MongoDbPipelineMetadataService.COLLECTION_NAME)) {
+            if (name.equals(COLLECTION_NAME)) {
                 return true;
             }
         }
@@ -91,11 +103,35 @@ public class V20251021083100_CreatePipelineMetadata extends Migration {
 
     private void createMetadata() {
         LOG.info("Creating pipeline metadata collection.");
-        pipelineService.loadAll().forEach(this::analyzePipeline);
-    }
+        final List<InsertOneModel<PipelineMetadataDao>> records = new ArrayList<>();
 
-    private void analyzePipeline(PipelineDao pipeline) {
         final ImmutableMap<String, Pipeline> pipelines = pipelineResolver.resolvePipelines(pipelineMetricRegistry);
         final ImmutableMap<String, Pipeline> functions = pipelineResolver.resolveFunctions(pipelines.values(), pipelineMetricRegistry);
+
+        pipelines.values().forEach(pipeline -> {
+            final PipelineMetadataDao.Builder builder = PipelineMetadataDao.builder().pipelineId(pipeline.id());
+            Set<Stage> stages = functions.get(pipeline.id()).stages();
+
+            List<String> ruleList = new ArrayList<>();
+            if (stages != null) {
+                stages.forEach(stage -> {
+                    final List<Rule> rules = stage.getRules();
+                    if (rules != null) {
+                        rules.forEach(rule -> {
+                            if (rule != null) {
+                                ruleList.add(rule.id());
+                            }
+                        });
+                    }
+                });
+            }
+            builder.rules(ruleList);
+
+
+            records.add(new InsertOneModel<>(builder.build()));
+        });
+        if (!records.isEmpty()) {
+            collection.bulkWrite(records);
+        }
     }
 }
