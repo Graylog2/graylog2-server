10fb7a0262645eb3370f3418b79986b8fb98b97a
Ismail Belkacim <ismail.belkacim@graylog.com>
2025-10-27 17:22:38 +0100

    Update test class name

--- graylog2-server/src/test/java/org/graylog2/telemetry/suppliers/ShardsMetricsSupplierTest.java
+++ graylog2-server/src/test/java/org/graylog2/telemetry/suppliers/ShardsMetricsSupplierTest.java
@@ -34,7 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ShardMetricsSupplierTest {
+public class ShardsMetricsSupplierTest {
     @Mock
     private ElasticsearchConfiguration elasticsearchConfiguration;
 
