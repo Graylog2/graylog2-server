8af68bdb3641d062f1433c47731e806dc2f61ed6
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2025-10-27 16:00:40 +0100

    show data nodes as DataNodesExpandable

--- graylog2-web-interface/src/components/cluster-configuration/DataNodesExpandable.tsx
+++ graylog2-web-interface/src/components/cluster-configuration/DataNodesExpandable.tsx
@@ -20,19 +20,11 @@ import styled from 'styled-components';
 import { Table, Label } from 'components/bootstrap';
 import { Section, Spinner } from 'components/common';
 import { Link } from 'components/common/router';
+import DataNodeStatusCell from 'components/datanode/DataNodeList/DataNodeStatusCell';
 import Routes from 'routing/Routes';
-import JournalState from 'components/nodes/JournalState';
+import DataNodeActions from 'components/datanode/DataNodeList/DataNodeActions';
 
 import type { ClusterNodes } from './useClusterNodes';
-import ClusterStatusLabel from './ClusterStatusLabel';
-import ClusterActions from './ClusterActions';
-import JvmHeapUsageText from './JvmHeapUsageText';
-
-const SecondaryText = styled.div`
-  span {
-    font-size: small;
-  }
-`;
 
 const NodeInfoTH = styled.th`
   width: 51%;
@@ -83,26 +75,18 @@ const DataNodesExpandable = ({ clusterNodes }: Props) => (
         </tr>
       </thead>
       <tbody>
-        {clusterNodes.graylogNodes.map((graylogNode) => (
-          <tr key={graylogNode.nodeName}>
+        {clusterNodes.dataNodes.map((dataNode) => (
+          <tr key={dataNode.nodeName}>
             <td>
-              <div>
-                <Link to={Routes.SYSTEM.CLUSTER.NODE_SHOW(graylogNode.nodeInfo.node_id)}>{graylogNode.nodeName}</Link>
-              </div>
-              <SecondaryText>
-                <JournalState nodeId={graylogNode.nodeInfo.node_id} />
-              </SecondaryText>
-              <SecondaryText>
-                <JvmHeapUsageText nodeId={graylogNode.nodeInfo.node_id} />
-              </SecondaryText>
+              <Link to={Routes.SYSTEM.CLUSTER.DATANODE_SHOW(dataNode.nodeInfo.node_id)}>{dataNode.nodeName}</Link>
             </td>
-            <td>{graylogNode.type}</td>
-            <td>{getRoleLabels(graylogNode.role)}</td>
+            <td>{dataNode.type}</td>
+            <td>{getRoleLabels(dataNode.role)}</td>
             <td>
-              <ClusterStatusLabel node={graylogNode.nodeInfo} />
+              <DataNodeStatusCell dataNode={dataNode.nodeInfo} />
             </td>
             <td align="right">
-              <ClusterActions node={graylogNode.nodeInfo} />
+              <DataNodeActions dataNode={dataNode.nodeInfo} />
             </td>
           </tr>
         ))}
