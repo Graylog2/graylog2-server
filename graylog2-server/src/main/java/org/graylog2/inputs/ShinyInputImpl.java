575c706ada952b7c075105f904d7ef79bd411e2c
Maxwell Anipah <maxwell.anipah@graylog.com>
2025-10-30 12:06:06 +0100

    fix import and add license header

--- graylog2-server/src/main/java/org/graylog2/inputs/ShinyInputImpl.java
+++ graylog2-server/src/main/java/org/graylog2/inputs/ShinyInputImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 package org.graylog2.inputs;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -5,11 +21,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.auto.value.AutoValue;
+import jakarta.annotation.Nullable;
 import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.graylog2.database.BuildableMongoEntity;
 import org.graylog2.plugin.IOState;
-import org.jetbrains.annotations.Nullable;
 import org.joda.time.DateTime;
 
 import java.util.Map;
