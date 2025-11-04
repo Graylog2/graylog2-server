bd3cd251379a127bc84ac687b2f08bfe8fff8abe
Laura Bergenthal-Grotlüschen <laura.bergenthalgrotlueschen@graylog.com>
2025-11-04 10:22:53 +0100

    Only override index_hot_lifetime_min when data tiering is restricted

--- graylog2-web-interface/src/components/indices/IndexSetConfigurationForm/IndexSetConfigurationForm.tsx
+++ graylog2-web-interface/src/components/indices/IndexSetConfigurationForm/IndexSetConfigurationForm.tsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useMemo, useState, useCallback } from 'react';
 import moment from 'moment';
 import { Formik, Form, Field } from 'formik';
 import styled, { css } from 'styled-components';
@@ -143,6 +143,8 @@ const IndexSetConfigurationForm = ({
     setHiddenFields(tmpHidden);
   }, [indexSet]);
 
+  const isDataTieringImmutable = useMemo(() => immutableFields.includes('data_tiering'), [immutableFields]);
+
   const prepareRetentionConfigBeforeSubmit = useCallback(
     (values: IndexSetFormValues): IndexSet => {
       const indexSetValues = values;
@@ -171,7 +173,7 @@ const IndexSetConfigurationForm = ({
 
       const configWithDataTiering = {
         ...indexSetValues,
-        data_tiering: prepareDataTieringConfig(values.data_tiering, PluginStore),
+        data_tiering: prepareDataTieringConfig(values.data_tiering, PluginStore, isDataTieringImmutable),
       };
 
       if (loadingIndexSetTemplateDefaults || !indexSetTemplateDefaults)
@@ -193,6 +195,7 @@ const IndexSetConfigurationForm = ({
       selectedRetentionSegment,
       enableDataTieringCloud,
       isCloud,
+      isDataTieringImmutable,
     ],
   );
 
@@ -232,7 +235,10 @@ const IndexSetConfigurationForm = ({
 
   const prepareInitialValues = () => {
     if (indexSet.data_tiering) {
-      return { ...indexSet, data_tiering: prepareDataTieringInitialValues(indexSet.data_tiering, PluginStore) };
+      return {
+        ...indexSet,
+        data_tiering: prepareDataTieringInitialValues(indexSet.data_tiering, PluginStore, isDataTieringImmutable),
+      };
     }
 
     return indexSet as unknown as IndexSetFormValues;
