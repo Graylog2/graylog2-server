04598264dfc4baa78d840eb1c87b0413a0edc2c8
Dan Torrey <dan.torrey@graylog.com>
2025-11-04 16:33:38 -0600

    Use default email templates on Customization page, add restore default option

--- graylog2-web-interface/src/components/event-notifications/event-notification-types/EmailNotificationForm.tsx
+++ graylog2-web-interface/src/components/event-notifications/event-notification-types/EmailNotificationForm.tsx
@@ -25,65 +25,10 @@ import { getValueFromInput } from 'util/FormsUtils';
 import HideOnCloud from 'util/conditional/HideOnCloud';
 import usePluggableLicenseCheck from 'hooks/usePluggableLicenseCheck';
 import usePluginEntities from 'hooks/usePluginEntities';
-
-// TODO: Default body template should come from the server
-const DEFAULT_BODY_TEMPLATE = `--- [Event Definition] ---------------------------
-Title:       \${event_definition_title}
-Description: \${event_definition_description}
-Type:        \${event_definition_type}
---- [Event] --------------------------------------
-Alert Replay:         \${http_external_uri}alerts/\${event.id}/replay-search
-Timestamp:            \${event.timestamp}
-Message:              \${event.message}
-Source:               \${event.source}
-Key:                  \${event.key}
-Priority:             \${event.priority}
-Alert:                \${event.alert}
-Timestamp Processing: \${event.timestamp}
-Timerange Start:      \${event.timerange_start}
-Timerange End:        \${event.timerange_end}
-Fields:
-\${foreach event.fields field}  \${field.key}: \${field.value}
-\${end}
-\${if backlog}
---- [Backlog] ------------------------------------
-Last messages accounting for this alert:
-\${foreach backlog message}
-\${message}
-\${end}
-\${end}
-`;
-
-const DEFAULT_HTML_BODY_TEMPLATE = `<table width="100%" border="0" cellpadding="10" cellspacing="0" style="background-color:#f9f9f9;border:none;line-height:1.2"><tbody>
-<tr style="line-height:1.5"><th colspan="2" style="background-color:#e6e6e6">Event Definition</th></tr>
-<tr><td width="200px">Title</td><td>\${event_definition_title}</td></tr>
-<tr><td>Description</td><td>\${event_definition_description}</td></tr>
-<tr><td>Type</td><td>\${event_definition_type}</td></tr>
-</tbody></table>
-<br /><table width="100%" border="0" cellpadding="10" cellspacing="0" style="background-color:#f9f9f9;border:none;line-height:1.2"><tbody>
-<tr><th colspan="2" style="background-color:#e6e6e6;line-height:1.5">Event</th></tr>
-<tr><td>Alert Replay</td><td>\${http_external_uri}alerts/\${event.id}/replay-search</td></tr>
-<tr><td width="200px">Timestamp</td><td>\${event.timestamp}</td></tr>
-<tr><td>Message</td><td>\${event.message}</td></tr>
-<tr><td>Source</td><td>\${event.source}</td></tr>
-<tr><td>Key</td><td>\${event.key}</td></tr>
-<tr><td>Priority</td><td>\${event.priority}</td></tr>
-<tr><td>Alert</td><td>\${event.alert}</td></tr>
-<tr><td>Timestamp Processing</td><td>\${event.timestamp}</td></tr>
-<tr><td>Timerange Start</td><td>\${event.timerange_start}</td></tr>
-<tr><td>Timerange End</td><td>\${event.timerange_end}</td></tr>
-<tr><td>Source Streams</td><td>\${event.source_streams}</td></tr>
-<tr><td>Fields</td><td><ul style="list-style-type:square;">\${foreach event.fields field}<li>\${field.key}:\${field.value}</li>\${end}<ul></td></tr>
-</tbody></table>
-\${if backlog}
-<br /><table width="100%" border="0" cellpadding="10" cellspacing="0" style="background-color:#f9f9f9;border:none;line-height:1.2"><tbody>
-<tr><th style="background-color:#e6e6e6;line-height:1.5">Backlog (Last messages accounting for this alert)</th></tr>
-\${foreach backlog message}
-<tr><td>\${message}</td></tr>
-\${end}
-</tbody></table>
-\${end}
-`;
+import {
+  DEFAULT_BODY_TEMPLATE,
+  DEFAULT_HTML_BODY_TEMPLATE,
+} from 'components/event-notifications/event-notification-types/emailNotificationTemplates';
 
 // eslint-disable-next-line no-template-curly-in-string
 const LOOKUP_KEY_PLACEHOLDER_TEXT = '${event.group_by_fields.group_by_field}';
