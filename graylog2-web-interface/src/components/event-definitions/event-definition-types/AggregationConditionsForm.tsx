fc7e7cb4b53ea2bcb324a0dd162849833be59921
Jan Heise <jan.heise@graylog.com>
2025-11-12 10:04:48 +0100

    fixing linter hint

--- graylog2-web-interface/src/components/event-definitions/event-definition-types/AggregationConditionsForm.tsx
+++ graylog2-web-interface/src/components/event-definitions/event-definition-types/AggregationConditionsForm.tsx
@@ -16,7 +16,6 @@
  */
 import React from 'react';
 import styled from 'styled-components';
-import get from 'lodash/get';
 
 import { Alert, Row } from 'components/bootstrap';
 import { emptyComparisonExpressionConfig } from 'logic/alerts/AggregationExpressionConfig';
@@ -119,7 +118,7 @@ class AggregationConditionsForm extends React.Component<
         <h3 className={commonStyles.title}>Create Events for Definition</h3>
         {validation.errors.conditions && (
           <StyledAlert bsStyle="danger" title="Errors found">
-            <p>{get(validation, 'errors.conditions[0]')}</p>
+            <p>{validation?.errors?.conditions[0]}</p>
           </StyledAlert>
         )}
 
