b9d4cfa6f43dbd54a9613a3352b91b8b90c10c23
Maxwell Anipah <maxwell.anipah@graylog.com>
2025-11-17 16:40:27 +0100

    add missing headers

--- graylog2-web-interface/src/components/inputs/InputsOveriew/Constants.ts
+++ graylog2-web-interface/src/components/inputs/InputsOveriew/Constants.ts
@@ -9,11 +9,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * Server Side Public License for more details.
+ *
  * You should have received a copy of the Server Side Public License
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-
 import type { Sort } from 'stores/PaginationTypes';
 
 const useInputsTableElements = () => {
