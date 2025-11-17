f2b260032cc81cef60d1fa3a1df06d4d2ded0160
Kay Roepke <kroepke@googlemail.com>
2025-11-17 16:57:43 +0100

    remove jukito + vintage engine and adapt usage of legacy junit4 apis

--- graylog-storage-opensearch3/src/test/java/org/graylog/storage/opensearch3/blocks/BlockSettingsParserTest.java
+++ graylog-storage-opensearch3/src/test/java/org/graylog/storage/opensearch3/blocks/BlockSettingsParserTest.java
@@ -26,10 +26,10 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BlockSettingsParserTest {
 
