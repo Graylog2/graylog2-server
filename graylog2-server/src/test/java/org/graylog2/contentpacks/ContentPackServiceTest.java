ad860e48b01fe0a569760a65e04062f63c7fadc0
Jan Heise <jan.heise@graylog.com>
2025-11-27 11:15:40 +0100

    Fix: keep Highlighting rules in order (#24347) - backport to 7.0 (#24351)
    * Fix: keep Highlighting rules in order (#24347)
    
    * using a list instead of a set to keep order intact
    
    * add changelog
    
    * fixing tests
    
    * fixed import

--- graylog2-server/src/test/java/org/graylog2/contentpacks/ContentPackServiceTest.java
+++ graylog2-server/src/test/java/org/graylog2/contentpacks/ContentPackServiceTest.java
@@ -136,6 +136,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -544,7 +545,7 @@ public class ContentPackServiceTest {
                 .widgets(ImmutableSet.of(widgetEntity))
                 .widgetMapping(ImmutableMap.of())
                 .widgetPositions(ImmutableMap.of())
-                .formatting(FormattingSettings.builder().highlighting(ImmutableSet.of()).build())
+                .formatting(FormattingSettings.builder().highlighting(List.of()).build())
                 .displayModeSettings(DisplayModeSettings.empty())
                 .build();
         final ViewEntity entity = ViewEntity.builder()
