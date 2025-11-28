e13117fdefb6f1995a2e7bd0d93222f5ef4b1535
Nithin Kasam <nkasam@loginsoft.com>
2025-11-28 13:40:54 +0530

    format license header

--- graylog2-server/src/test/java/org/graylog/aws/AWSProxyUtilsTest.java
+++ graylog2-server/src/test/java/org/graylog/aws/AWSProxyUtilsTest.java
@@ -12,7 +12,7 @@
  *
  * You should have received a copy of the Server Side Public License
  * along with this program. If not, see
- * <http://www.mongodb.org/licensing/server-side-public-license>.
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.graylog.aws;
 
