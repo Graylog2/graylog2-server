aaebb354f606703b83e8f18dc1499139da510a8c
Ismail Belkacim <xd4rker@users.noreply.github.com>
2025-12-08 16:21:34 +0100

    Create users metrics supplier (#24090) (#24472)
    * Implement users metrics supplier
    
    * Add changelog
    
    ---------
    
    
    (cherry picked from commit a90e9953991acf9f9ca1bc9663e0c7c3096bccb1)
    
    Co-authored-by: Florian Petersen <188503754+fpetersen-gl@users.noreply.github.com>

--- graylog2-server/src/main/java/org/graylog2/telemetry/TelemetryModule.java
+++ graylog2-server/src/main/java/org/graylog2/telemetry/TelemetryModule.java
@@ -19,6 +19,7 @@ package org.graylog2.telemetry;
 import com.google.inject.multibindings.Multibinder;
 import org.graylog2.plugin.PluginModule;
 import org.graylog2.telemetry.scheduler.TelemetrySubmissionPeriodical;
+import org.graylog2.telemetry.suppliers.UsersMetricsSupplier;
 import org.graylog2.telemetry.suppliers.InputsMetricsSupplier;
 import org.graylog2.telemetry.suppliers.OutputsMetricsSupplier;
 import org.graylog2.telemetry.suppliers.MongoDBMetricsSupplier;
@@ -34,6 +35,7 @@ public class TelemetryModule extends PluginModule {
         addPeriodical(TelemetrySubmissionPeriodical.class);
         Multibinder.newSetBinder(binder(), TelemetryDataProvider.class);
 
+        addTelemetryMetricProvider("Users Metrics", UsersMetricsSupplier.class);
         addTelemetryMetricProvider("Inputs Metrics", InputsMetricsSupplier.class);
         addTelemetryMetricProvider("Outputs Metrics", OutputsMetricsSupplier.class);
         addTelemetryMetricProvider("MongoDB Metrics", MongoDBMetricsSupplier.class);
