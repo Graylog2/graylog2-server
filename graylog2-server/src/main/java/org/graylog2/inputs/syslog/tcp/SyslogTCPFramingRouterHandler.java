9553fe86f003ff9d5bd111cd429b800586ef27ee
Florian Petersen <188503754+fpetersen-gl@users.noreply.github.com>
2025-12-11 14:37:35 +0100

    Keeping track of retained buffers, always releasing them after processing

--- graylog2-server/src/main/java/org/graylog2/inputs/syslog/tcp/SyslogTCPFramingRouterHandler.java
+++ graylog2-server/src/main/java/org/graylog2/inputs/syslog/tcp/SyslogTCPFramingRouterHandler.java
@@ -21,11 +21,17 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.util.ReferenceCountUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class SyslogTCPFramingRouterHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    private static final Logger LOG = LoggerFactory.getLogger(SyslogTCPFramingRouterHandler.class);
     private final int maxFrameLength;
     private final ByteBuf[] delimiter;
+    private final Collection<ByteBuf> retainedBuffers = new LinkedList<>();
     private ChannelInboundHandler handler = null;
 
     public SyslogTCPFramingRouterHandler(int maxFrameLength, ByteBuf[] delimiter) {
@@ -35,27 +41,57 @@ public class SyslogTCPFramingRouterHandler extends SimpleChannelInboundHandler<B
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        LOG.warn("RefCnt of msg entering channelRead0: {}", msg.refCnt());
         if (msg.isReadable()) {
+            LOG.info("Is readable.");
             // "Dynamically manipulating a pipeline is relatively an expensive operation."
             // https://stackoverflow.com/a/28846565
             if (handler == null) {
                 if (usesOctetCountFraming(msg)) {
+                    LOG.debug("Uses Octet Count Framing");
                     handler = new SyslogOctetCountFrameDecoder();
                 } else {
+                    LOG.debug("Uses delimiter based Framing with max length {} and delimiter {}", maxFrameLength, delimiter);
                     handler = new DelimiterBasedFrameDecoder(maxFrameLength, delimiter);
                 }
             }
-            try {
-                handler.channelRead(ctx, ReferenceCountUtil.retain(msg));
-            } catch (Exception e) {
-                // Because we've retained the buffer, we must release it in case of an exception to avoid memory leaks
-                ReferenceCountUtil.release(msg);
-                throw e;
-            }
-
+            LOG.info("Passing on to handler (refCnt {}), ctx {}", msg.refCnt(), ctx);
+            // Retain message for possible next frame.
+            retainedBuffers.add(msg.retain());
+            handler.channelRead(ctx, msg);
+            LOG.info("After handler.channelRead(): refCnt {}.", msg.refCnt());
         } else {
+            LOG.info("msg not readable (refCnt {}).", msg.refCnt());
             ctx.fireChannelRead(msg);
         }
+        LOG.warn("RefCnt of msg leaving channelRead0: {}", msg.refCnt());
+    }
+
+    private void cleanup(final String from) {
+        LOG.info("{}: Releasing {} retained buffers.", from, retainedBuffers.size());
+        for (ByteBuf buf : retainedBuffers) {
+            if (buf.refCnt() > 0) {
+                LOG.info("Releasing buffer with refCnt {}.", buf.refCnt());
+                buf.release();
+            } else {
+                LOG.info("buf has a refCnt of 0 already.");
+            }
+
+        }
+        retainedBuffers.clear();
+        LOG.info("Done cleaning up (after {}).", from);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        cleanup("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cleanup("exception");
+        super.exceptionCaught(ctx, cause);
     }
 
     private boolean usesOctetCountFraming(ByteBuf message) {
