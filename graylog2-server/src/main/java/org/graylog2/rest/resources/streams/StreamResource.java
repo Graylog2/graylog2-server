3ff69ca2af0b961f62f57c51eef8af1fba2134c8
Tomas Dvorak <tomas.dvorak@graylog.com>
2025-12-23 08:41:55 +0100

    Code simplification

--- graylog2-server/src/main/java/org/graylog2/rest/resources/streams/StreamResource.java
+++ graylog2-server/src/main/java/org/graylog2/rest/resources/streams/StreamResource.java
@@ -119,7 +119,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
