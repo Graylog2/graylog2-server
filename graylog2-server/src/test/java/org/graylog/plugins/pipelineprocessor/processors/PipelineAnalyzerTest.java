0451021c82283e8b0bc9d05d96fe7d9167436ed2
patrickmann <patrick.mann@graylog.com>
2025-12-23 12:39:28 +0100

    unit test

--- graylog2-server/src/test/java/org/graylog/plugins/pipelineprocessor/processors/PipelineAnalyzerTest.java
+++ graylog2-server/src/test/java/org/graylog/plugins/pipelineprocessor/processors/PipelineAnalyzerTest.java
@@ -22,6 +22,7 @@ import org.graylog.plugins.pipelineprocessor.ast.Pipeline;
 import org.graylog.plugins.pipelineprocessor.db.PipelineInputsMetadataDao;
 import org.graylog.plugins.pipelineprocessor.db.PipelineRulesMetadataDao;
 import org.graylog.plugins.pipelineprocessor.db.PipelineStreamConnectionsService;
+import org.graylog2.database.NotFoundException;
 import org.graylog2.inputs.InputService;
 import org.graylog2.streams.StreamService;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,7 +39,12 @@ import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.
 import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.GL2_SOURCE_INPUT_ID;
 import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.INPUT_ID;
 import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.REMOVE_FIELD_ID;
-import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.STREAM_ID;
+import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.ROUTING_ID;
+import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.STREAM1_ID;
+import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.STREAM2_ID;
+import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.STREAM2_TITLE;
+import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.STREAM3_ID;
+import static org.graylog.plugins.pipelineprocessor.processors.PipelineTestUtil.STREAM3_TITLE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -115,7 +121,7 @@ class PipelineAnalyzerTest {
         Set<PipelineInputsMetadataDao.MentionedInEntry> mentions = result.get(INPUT_ID);
         assertTrue(mentions.stream().anyMatch(entry ->
                 entry.pipelineId().equals(pipeline1.id())
-                        && entry.connectedStreams().contains(STREAM_ID)
+                        && entry.connectedStreams().contains(STREAM1_ID)
                         && entry.ruleId().equals(FROM_INPUT_ID)));
 
         assertTrue(ruleRecords.stream().anyMatch(dao ->
@@ -135,7 +141,7 @@ class PipelineAnalyzerTest {
         Set<PipelineInputsMetadataDao.MentionedInEntry> mentions = result.get(INPUT_ID);
         assertTrue(mentions.stream().anyMatch(entry ->
                 entry.pipelineId().equals(pipeline1.id())
-                        && entry.connectedStreams().contains(STREAM_ID)
+                        && entry.connectedStreams().contains(STREAM1_ID)
                         && entry.ruleId().equals(GL2_SOURCE_INPUT_ID)));
 
         assertTrue(ruleRecords.stream().anyMatch(dao ->
@@ -143,6 +149,32 @@ class PipelineAnalyzerTest {
                         && dao.rules().contains(GL2_SOURCE_INPUT_ID)
                         && dao.functions().contains("has_field")
                         && dao.functions().contains("to_string")
+        ));
+    }
+
+    @Test
+    void routingRule() throws NotFoundException {
+        Pipeline pipeline1 = testUtil.createPipelineWithRules("pipeline1", List.of(testUtil.ROUTING));
+        org.graylog2.plugin.streams.Stream stream2 = mock(org.graylog2.plugin.streams.Stream.class);
+        when(stream2.getTitle()).thenReturn(STREAM2_TITLE);
+        when(streamService.load(STREAM2_ID)).thenReturn(stream2);
+
+        org.graylog2.plugin.streams.Stream stream3 = mock(org.graylog2.plugin.streams.Stream.class);
+        when(stream3.getId()).thenReturn(STREAM3_ID);
+        when(stream3.getTitle()).thenReturn(STREAM3_TITLE);
+        when(streamService.loadAllByTitle(STREAM3_TITLE)).thenReturn(List.of(stream3));
+
+        pipelineAnalyzer.analyzePipelines(
+                ImmutableMap.of(pipeline1.id(), pipeline1), ImmutableMap.of(pipeline1.id(), pipeline1), ruleRecords);
+
+        assertTrue(ruleRecords.stream().anyMatch(dao ->
+                dao.pipelineId().equals(pipeline1.id())
+                        && dao.rules().contains(ROUTING_ID)
+                        && dao.functions().contains("route_to_stream")
+                        && dao.routedStreams().get(STREAM2_ID).equals(STREAM2_TITLE)
+                        && dao.routingRules().get(ROUTING_ID).contains(STREAM2_ID)
+                        && dao.routedStreams().get(STREAM3_ID).equals(STREAM3_TITLE)
+                        && dao.routingRules().get(ROUTING_ID).contains(STREAM3_ID)
         ));
     }
 }
