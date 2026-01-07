ac2d20e20c53d82d34cbed734cb34330c08391b7
Ryan Carroll <ryan.carroll@graylog.com>
2026-01-07 11:12:22 -0600

    add tests

---
+++ graylog2-web-interface/src/components/event-notifications/event-notification-types/EmailNotificationForm.test.tsx
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import React from 'react';
+import { render, waitFor } from 'wrappedTestingLibrary';
+
+import asMock from 'helpers/mocking/AsMock';
+import usePluggableLicenseCheck from 'hooks/usePluggableLicenseCheck';
+import usePluginEntities from 'hooks/usePluginEntities';
+
+import EmailNotificationForm from './EmailNotificationForm';
+import { DEFAULT_BODY_TEMPLATE, DEFAULT_HTML_BODY_TEMPLATE } from './emailNotificationTemplates';
+
+jest.mock('components/common', () => ({
+  IfPermitted: ({ children }) => <>{children}</>,
+  MultiSelect: () => null,
+  SourceCodeEditor: () => null,
+  TimezoneSelect: () => null,
+}));
+
+jest.mock('components/lookup-tables', () => ({
+  LookupTableFields: () => null,
+}));
+
+jest.mock('components/users/UsersSelectField', () => () => null);
+
+jest.mock('components/bootstrap', () => {
+  const Actual = jest.requireActual('components/bootstrap');
+
+  return {
+    ...Actual,
+    Input: (props) => <input {...props} />,
+  };
+});
+
+jest.mock('util/conditional/HideOnCloud', () => ({ children }) => <>{children}</>);
+
+jest.mock('hooks/usePluggableLicenseCheck');
+jest.mock('hooks/usePluginEntities');
+
+const templateHook = () => ({
+  templateConfig: {
+    override_defaults: false,
+    text_body: 'PLUGIN_TEXT',
+    html_body: 'PLUGIN_HTML',
+  },
+});
+
+const defaultValidation = { errors: {} };
+
+const buildConfig = (overrides = {}) => ({
+  ...EmailNotificationForm.defaultConfig,
+  ...overrides,
+});
+
+describe('EmailNotificationForm', () => {
+  beforeEach(() => {
+    asMock(usePluggableLicenseCheck).mockReturnValue({ data: { valid: true } });
+    asMock(usePluginEntities).mockReturnValue([
+      {
+        hooks: {
+          useEmailTemplate: templateHook,
+        },
+      },
+    ]);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('does not override templates when editing existing notification', async () => {
+    const onChange = jest.fn();
+    const config = buildConfig({
+      body_template: 'EXISTING_BODY',
+      html_body_template: 'EXISTING_HTML',
+    });
+
+    render(
+      <EmailNotificationForm
+        config={config}
+        validation={defaultValidation}
+        onChange={onChange}
+        notificationId="123"
+      />,
+    );
+
+    await waitFor(() => {
+      expect(onChange).not.toHaveBeenCalled();
+    });
+  });
+
+  it('applies template defaults for new notifications with empty templates', async () => {
+    const onChange = jest.fn();
+    const config = buildConfig({
+      body_template: '',
+      html_body_template: '',
+    });
+
+    render(<EmailNotificationForm config={config} validation={defaultValidation} onChange={onChange} />);
+
+    await waitFor(() => {
+      expect(onChange).toHaveBeenCalledWith(
+        expect.objectContaining({
+          body_template: DEFAULT_BODY_TEMPLATE,
+          html_body_template: DEFAULT_HTML_BODY_TEMPLATE,
+        }),
+      );
+    });
+  });
+});
