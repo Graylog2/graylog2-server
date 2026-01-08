4cd7d933e5155ab38a3f1058436e4190d9faa268
Dennis Oelkers <dennis@graylog.com>
2026-01-08 12:12:09 +0100

    Migrating parts of webpack config to TS.

--- graylog2-web-interface/webpack/core.ts
+++ graylog2-web-interface/webpack/core.ts
@@ -14,16 +14,16 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-const path = require('path');
+import * as path from 'path';
 
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const { EsbuildPlugin } = require('esbuild-loader');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin');
-const { defineReactCompilerLoaderOption, reactCompilerLoader } = require('react-compiler-webpack');
+import * as webpack from 'webpack';
+import { merge } from 'webpack-merge';
+import { EsbuildPlugin } from 'esbuild-loader';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { CycloneDxWebpackPlugin } from '@cyclonedx/webpack-plugin';
+import { defineReactCompilerLoaderOption, reactCompilerLoader } from 'react-compiler-webpack';
 
-const UniqueChunkIdPlugin = require('./UniqueChunkIdPlugin');
+import UniqueChunkIdPlugin from './UniqueChunkIdPlugin';
 
 const disableTsc = process.env.disable_tsc === 'true';
 
@@ -126,7 +126,7 @@ const config = (target, appPath, rootPath, webInterfaceRoot, supportedBrowsers) 
   const VENDOR_MANIFEST_PATH = path.resolve(MANIFESTS_PATH, 'vendor-manifest.json');
   const BUILD_PATH = path.resolve(rootPath, 'target/web/build');
 
-  const baseConfig = {
+  const baseConfig: webpack.Configuration = {
     output: {
       path: BUILD_PATH,
       filename: '[name].[chunkhash].js',
@@ -224,8 +224,10 @@ const config = (target, appPath, rootPath, webInterfaceRoot, supportedBrowsers) 
         }),
         // Create SBOM files for graylog-server frontend dependencies.
         new CycloneDxWebpackPlugin({
+          // @ts-expect-error
           specVersion: '1.5',
           rootComponentAutodetect: false,
+          // @ts-expect-error
           rootComponentType: 'application',
           rootComponentName: 'graylog-server',
           outputLocation: '../cyclonedx-core',
@@ -238,4 +240,4 @@ const config = (target, appPath, rootPath, webInterfaceRoot, supportedBrowsers) 
   return baseConfig;
 };
 
-module.exports = { rules, config, sortChunks };
+export { rules, config, sortChunks };
