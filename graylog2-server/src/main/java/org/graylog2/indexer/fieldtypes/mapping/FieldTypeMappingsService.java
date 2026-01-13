57d0676dba556d7b710d92e773a23d58498accf8
Jan Heise <jan.heise@graylog.com>
2026-01-13 09:41:10 +0100

    remove unnecessary import

--- graylog2-server/src/main/java/org/graylog2/indexer/fieldtypes/mapping/FieldTypeMappingsService.java
+++ graylog2-server/src/main/java/org/graylog2/indexer/fieldtypes/mapping/FieldTypeMappingsService.java
@@ -42,7 +42,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import static org.graylog2.plugin.Message.FIELDS_UNCHANGEABLE_BY_CUSTOM_MAPPINGS;
