2d365ab180e52e3dbab67e435175c5f0bd7e541a
Zack King <zack.king@graylog.com>
2026-01-18 12:52:29 -0600

    Refactor event summary template logic to EventModifier

---
+++ graylog2-server/src/main/java/org/graylog/events/processor/modifier/EventSummaryModifier.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.events.processor.modifier;
+
+import com.floreysoft.jmte.Engine;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import jakarta.inject.Inject;
+import org.graylog.events.event.Event;
+import org.graylog.events.event.EventWithContext;
+import org.graylog.events.processor.EventDefinition;
+
+public class EventSummaryModifier implements EventModifier {
+    private final Engine templateEngine;
+
+    @Inject
+    public EventSummaryModifier(Engine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public void accept(EventWithContext eventWithContext, EventDefinition eventDefinition) throws EventModifierException {
+        if (!Strings.isNullOrEmpty(eventDefinition.eventSummaryTemplate())) {
+            final ImmutableMap.Builder<String, Object> dataModelBuilder = ImmutableMap.builder();
+
+            if (eventWithContext.messageContext().isPresent()) {
+                dataModelBuilder.put("source", eventWithContext.messageContext().get().getFields());
+            } else if (eventWithContext.eventContext().isPresent()) {
+                dataModelBuilder.put("source", eventWithContext.eventContext().get().toDto().fields());
+            }
+
+            final ImmutableMap<String, Object> dataModel = dataModelBuilder.build();
+
+            final Event event = eventWithContext.event();
+            event.setMessage(templateEngine.transform(eventDefinition.eventSummaryTemplate(), dataModel));
+        }
+    }
+}
