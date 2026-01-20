72fa9b9123216dfcf61dcd517fbab6a0ab5b0dca
Dennis Oelkers <dennis@graylog.com>
2026-01-20 10:32:21 +0100

    Fixing linter hint.

--- graylog2-web-interface/src/components/loggers/LogLevelDropdown.tsx
+++ graylog2-web-interface/src/components/loggers/LogLevelDropdown.tsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React from 'react';
+import * as React from 'react';
 import { useCallback } from 'react';
 import capitalize from 'lodash/capitalize';
 
