e439625acf460d9dc0cc61d8e1a2ffb717d2be1c
Ismail Belkacim <ismail.belkacim@graylog.com>
2026-01-23 14:33:27 +0100

    Expose BLOB_HOST

--- graylog2-server/src/test/java/org/graylog/testing/completebackend/AzuriteContainer.java
+++ graylog2-server/src/test/java/org/graylog/testing/completebackend/AzuriteContainer.java
@@ -30,6 +30,7 @@ public class AzuriteContainer extends GenericContainer<AzuriteContainer> {
 
     public static final String ACCOUNT_NAME = "devstoreaccount1";
     public static final String ACCOUNT_KEY = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
+    public static final String BLOB_HOST = ACCOUNT_NAME + ".blob.localhost";
 
     public AzuriteContainer() {
         super(IMAGE_NAME);
@@ -44,7 +45,7 @@ public class AzuriteContainer extends GenericContainer<AzuriteContainer> {
         return "DefaultEndpointsProtocol=http;"
                 + "AccountName=" + ACCOUNT_NAME + ";"
                 + "AccountKey=" + ACCOUNT_KEY + ";"
-                + "BlobEndpoint=http://%s:%d/%s;".formatted(getHost(), getMappedPort(PORT), ACCOUNT_NAME);
+                + "BlobEndpoint=http://" + BLOB_HOST + ":" + getMappedPort(PORT) + ";";
     }
 
     public BlobServiceClient createBlobServiceClient() {
