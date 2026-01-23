1cbfaa30ee525b36c0612086c6342198cfa54296
Kay Roepke <kroepke@googlemail.com>
2026-01-23 15:48:18 +0100

    feat(collectors): Export all collector components from index
    Update the collectors index.ts to export all components, hooks,
    and types for easier imports from other parts of the application.

--- graylog2-web-interface/src/components/collectors/index.ts
+++ graylog2-web-interface/src/components/collectors/index.ts
@@ -16,4 +16,26 @@
  */
 
 // Collectors v2 UI Components
+
+// Types
 export * from './types';
+
+// Hooks
+export * from './hooks';
+
+// Common components
+export { default as StatCard } from './common/StatCard';
+
+// Overview components
+export { default as CollectorsOverview } from './overview/CollectorsOverview';
+export { default as SourcesTable } from './overview/SourcesTable';
+
+// Fleets components
+export { default as FleetList } from './fleets/FleetList';
+export { default as FleetDetail } from './fleets/FleetDetail';
+
+// Instances components
+export { default as InstanceList } from './instances/InstanceList';
+
+// Deployment components
+export { default as DeploymentForm } from './deployment/DeploymentForm';
