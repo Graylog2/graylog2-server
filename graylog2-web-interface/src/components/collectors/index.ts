c9a1634b38454d0be2026f9996c431f344ee4265
Kay Roepke <kroepke@googlemail.com>
2026-01-26 13:06:45 +0100

    feat(collectors): update main index exports for phase 2 components
    Add exports for new components created in Phase 2:
    - CollectorsPageNavigation and FilterBar from common
    - FleetFormModal and FleetSettings from fleets
    - InstanceDetailDrawer from instances
    - SourceFormModal from sources

--- graylog2-web-interface/src/components/collectors/index.ts
+++ graylog2-web-interface/src/components/collectors/index.ts
@@ -25,6 +25,8 @@ export * from './hooks';
 
 // Common components
 export { default as StatCard } from './common/StatCard';
+export { default as CollectorsPageNavigation } from './common/CollectorsPageNavigation';
+export { default as FilterBar } from './common/FilterBar';
 
 // Overview components
 export { default as CollectorsOverview } from './overview/CollectorsOverview';
@@ -33,9 +35,15 @@ export { default as SourcesTable } from './overview/SourcesTable';
 // Fleets components
 export { default as FleetList } from './fleets/FleetList';
 export { default as FleetDetail } from './fleets/FleetDetail';
+export { default as FleetFormModal } from './fleets/FleetFormModal';
+export { default as FleetSettings } from './fleets/FleetSettings';
 
 // Instances components
 export { default as InstanceList } from './instances/InstanceList';
+export { default as InstanceDetailDrawer } from './instances/InstanceDetailDrawer';
+
+// Sources components
+export { default as SourceFormModal } from './sources/SourceFormModal';
 
 // Deployment components
 export { default as DeploymentForm } from './deployment/DeploymentForm';
