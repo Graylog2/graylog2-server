c56c860ff8110abfe8942178d449dd2de65bf6ba
Ousmane Samba <ousmane.samba@graylog.com>
2026-01-26 12:13:55 +0100

    add automatic credential to azure geoIp

--- graylog2-web-interface/src/components/maps/configurations/ABSGeoIpFormGroup.tsx
+++ graylog2-web-interface/src/components/maps/configurations/ABSGeoIpFormGroup.tsx
@@ -16,30 +16,56 @@
  */
 import * as React from 'react';
 import { useState, useEffect, useCallback } from 'react';
-import { useFormikContext } from 'formik';
+import { useFormikContext, Field } from 'formik';
+import styled from 'styled-components';
 
-import { FormikFormGroup } from 'components/common';
-import type { GeoIpConfigType } from 'components/maps/configurations/types';
-import { Button, Input } from 'components/bootstrap';
+import { FormikFormGroup, Select, Icon } from 'components/common';
+import type { GeoIpConfigType, AzureAuthType, EncryptedValue } from 'components/maps/configurations/types';
+import { Button, Input, Table } from 'components/bootstrap';
+
+const AZURE_AUTH_TYPE_OPTIONS = [
+  { value: 'automatic', label: 'Automatic' },
+  { value: 'keysecret', label: 'Key & Secret' },
+];
+
+const SectionTitle = styled.p`
+  font-weight: bold;
+  font-size: 1.2em;
+  margin: 0 0 12px;
+`;
+
+const SectionNote = styled.p`
+  font-style: italic;
+  margin: 3px 0 0;
+`;
+
+const StyledTable = styled(Table)`
+  margin: 0;
+`;
+
+const EnvVarList = styled.div`
+  margin-top: 4px;
+`;
 
 const ABSGeoIpFormGroup = () => {
   const { values, setFieldValue } = useFormikContext<GeoIpConfigType>();
   const isKeySet = values.azure_account_key && 'is_set' in values.azure_account_key;
   const [isCreate] = useState(() => !isKeySet);
   const [showResetPasswordButton, setShowResetPasswordButton] = useState(isKeySet);
+  const [authType, setAuthType] = useState<AzureAuthType>(values.azure_auth_type ?? 'automatic');
 
   const setAccessKey = useCallback(
-    (nextAccessKey) => {
+    (nextAccessKey: EncryptedValue | undefined) => {
       setFieldValue('azure_account_key', nextAccessKey);
     },
     [setFieldValue],
   );
 
   useEffect(() => {
-    if (isKeySet) {
+    if (isKeySet && authType === 'keysecret') {
       setAccessKey({ keep_value: true });
     }
-  }, [isKeySet, setAccessKey]);
+  }, [isKeySet, setAccessKey, authType]);
 
   const toggleAccountKeyReset = useCallback(() => {
     if (showResetPasswordButton) {
@@ -52,6 +78,21 @@ const ABSGeoIpFormGroup = () => {
     setAccessKey({ keep_value: true });
     setShowResetPasswordButton(true);
   }, [setAccessKey, showResetPasswordButton]);
+
+  const handleAuthTypeChange = useCallback(
+    (option: string) => {
+      const newAuthType = option as AzureAuthType;
+      setAuthType(newAuthType);
+      setFieldValue('azure_auth_type', newAuthType);
+
+      if (newAuthType === 'automatic') {
+        // Clear key/secret fields when switching to automatic
+        setFieldValue('azure_account', undefined);
+        setFieldValue('azure_account_key', undefined);
+      }
+    },
+    [setFieldValue],
+  );
 
   return (
     <>
@@ -72,39 +113,122 @@ const ABSGeoIpFormGroup = () => {
         labelClassName=""
         wrapperClassName=""
       />
-      <FormikFormGroup
-        name="azure_account"
-        type="text"
-        label="Azure account name"
-        placeholder="your-account-name"
-        help="The name of your Azure storage account."
-        required
-        labelClassName=""
-        wrapperClassName=""
-      />
-      {showResetPasswordButton ? (
-        <Input id="azure_account_reset" label="Azure Account Key" labelClassName="col-sm-3" wrapperClassName="col-sm-9">
-          <Button onClick={toggleAccountKeyReset}>Reset password</Button>
-        </Input>
-      ) : (
-        <Input
-          name="azure_account_key"
-          id="azure_account_key"
-          data-testid="azure-account-key-input"
-          type="password"
-          label="Azure account key"
-          onChange={({ target: { value } }) => setAccessKey({ set_value: value })}
-          buttonAfter={
-            !isCreate ? (
-              <Button type="button" onClick={toggleAccountKeyReset}>
-                Undo Reset
-              </Button>
-            ) : undefined
-          }
-          placeholder="****************"
-          help="The account key for your Azure storage account."
-          required
-        />
+      <Field>
+        {() => (
+          <Input id="azure-auth-type-select" label="Azure Authentication Type">
+            <Select
+              id="azureAuthenticationType"
+              inputId="azure-auth-type-input"
+              name="azure_auth_type"
+              placeholder="Select Authentication Type"
+              options={AZURE_AUTH_TYPE_OPTIONS}
+              onChange={handleAuthTypeChange}
+              value={authType}
+            />
+          </Input>
+        )}
+      </Field>
+      {authType === 'automatic' && (
+        <StyledTable condensed>
+          <thead>
+            <tr>
+              <td colSpan={2}>
+                <SectionTitle>Automatic authentication will attempt each of the following in the listed order.</SectionTitle>
+              </td>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th>Environment</th>
+              <td>
+                Authenticates via environment variables.
+                <EnvVarList>
+                  The required environment variables for service principal authentication using client secret are as follows:{' '}
+                  <code>AZURE_CLIENT_ID</code>, <code>AZURE_CLIENT_SECRET</code>, <code>AZURE_TENANT_ID</code>
+                </EnvVarList>
+                <EnvVarList>
+                  The required environment variables for service principal authentication using client certificate are as
+                  follows: <code>AZURE_CLIENT_ID</code>, <code>AZURE_CLIENT_CERTIFICATE_PATH</code>,{' '}
+                  <code>AZURE_CLIENT_CERTIFICATE_PASSWORD</code>, <code>AZURE_TENANT_ID</code>
+                </EnvVarList>
+              </td>
+            </tr>
+            <tr>
+              <th>Workload Identity</th>
+              <td>For use on an Azure host with Workload Identity enabled.</td>
+            </tr>
+            <tr>
+              <th>Managed Identity</th>
+              <td>For use on an Azure host with Managed Identity enabled.</td>
+            </tr>
+            <tr>
+              <th>Azure CLI</th>
+              <td>Uses current Azure CLI account.</td>
+            </tr>
+            <tr>
+              <th>Broker</th>
+              <td>
+                Use the default account logged into the OS via a broker. Requires that the{' '}
+                <code>azure-identity-broker</code> package is installed.
+              </td>
+            </tr>
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan={2}>
+                <SectionNote>
+                  For more information, check out the{' '}
+                  <a
+                    href="https://learn.microsoft.com/en-us/java/api/overview/azure/identity-readme?view=azure-java-stable#defaultazurecredential"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Azure DefaultAzureCredential Documentation <Icon name="open_in_new" />
+                  </a>
+                </SectionNote>
+              </td>
+            </tr>
+          </tfoot>
+        </StyledTable>
+      )}
+      {authType === 'keysecret' && (
+        <>
+          <FormikFormGroup
+            name="azure_account"
+            type="text"
+            label="Azure account name"
+            placeholder="your-account-name"
+            help="The name of your Azure storage account."
+            required
+            labelClassName=""
+            wrapperClassName=""
+          />
+          {showResetPasswordButton ? (
+            <Input id="azure_account_reset" label="Azure Account Key" labelClassName="col-sm-3" wrapperClassName="col-sm-9">
+              <Button onClick={toggleAccountKeyReset}>Reset password</Button>
+            </Input>
+          ) : (
+            <Input
+              name="azure_account_key"
+              id="azure_account_key"
+              data-testid="azure-account-key-input"
+              type="password"
+              label="Azure account key"
+              onChange={({ target: { value } }) => setAccessKey({ set_value: value })}
+              buttonAfter={
+                !isCreate ? (
+                  <Button type="button" onClick={toggleAccountKeyReset}>
+                    Undo Reset
+                  </Button>
+                ) : undefined
+              }
+              placeholder="****************"
+              help="The account key for your Azure storage account."
+              required
+            />
+          )}
+        </>
       )}
     </>
   );
