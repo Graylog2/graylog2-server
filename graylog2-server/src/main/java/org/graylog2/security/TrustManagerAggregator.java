fcf3b0bfa03c6dfee9621d30bb9f0c418b20e13b
Tomas Dvorak <tomas.dvorak@graylog.com>
2026-02-12 12:04:00 +0100

    Provide more details if TrustManagerAggregator rejects certificate

--- graylog2-server/src/main/java/org/graylog2/security/TrustManagerAggregator.java
+++ graylog2-server/src/main/java/org/graylog2/security/TrustManagerAggregator.java
@@ -39,26 +39,75 @@ public class TrustManagerAggregator implements X509TrustManager {
 
     @Override
     public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        List<CertificateException> failures = new ArrayList<>();
+
         for (X509TrustManager trustManager : trustManagers) {
             try {
                 trustManager.checkClientTrusted(chain, authType);
                 return;
             } catch (CertificateException e) {
+                failures.add(e);
             }
         }
-        throw new CertificateException("None of the TrustManagers trust this certificate chain.");
+
+        throw createDetailedException("client", chain, authType, failures);
     }
 
     @Override
     public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        List<CertificateException> failures = new ArrayList<>();
+
         for (X509TrustManager trustManager : trustManagers) {
             try {
                 trustManager.checkServerTrusted(chain, authType);
                 return;
             } catch (CertificateException e) {
+                failures.add(e);
             }
         }
-        throw new CertificateException("None of the TrustManagers trust this certificate chain.");
+
+        throw createDetailedException("server", chain, authType, failures);
+    }
+
+    private CertificateException createDetailedException(
+            String trustType,
+            X509Certificate[] chain,
+            String authType,
+            List<CertificateException> failures) {
+
+        StringBuilder message = new StringBuilder();
+        message.append(String.format(
+                "Certificate chain validation failed for %s trust. ", trustType));
+        message.append(String.format(
+                "Tried %d trust manager(s), all rejected the certificate. ", failures.size()));
+
+        if (chain != null && chain.length > 0) {
+            X509Certificate cert = chain[0];
+            message.append(String.format(
+                    "Certificate details - Subject: '%s', Issuer: '%s', Valid: %s to %s. ",
+                    cert.getSubjectX500Principal().getName(),
+                    cert.getIssuerX500Principal().getName(),
+                    cert.getNotBefore(),
+                    cert.getNotAfter()
+            ));
+        }
+
+        message.append(String.format("Auth type: %s. ", authType));
+        message.append(String.format("Chain length: %d. ", chain != null ? chain.length : 0));
+
+        CertificateException exception = new CertificateException(message.toString());
+
+        // Add all individual failures as suppressed exceptions for detailed debugging
+        for (int i = 0; i < failures.size(); i++) {
+            CertificateException failure = failures.get(i);
+            CertificateException wrappedException = new CertificateException(
+                    "Trust manager #" + (i + 1) + " rejection: " + failure.getMessage(),
+                    failure
+            );
+            exception.addSuppressed(wrappedException);
+        }
+
+        return exception;
     }
 
     @Override
