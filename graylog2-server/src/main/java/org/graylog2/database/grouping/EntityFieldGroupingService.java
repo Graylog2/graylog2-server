66f29043e1ad91f666bc6b9e89c221c66265f97a
luk-kaminski <lukasz.kaminski@graylog.com>
2026-02-18 11:59:16 +0100

    Skeleton

---
+++ graylog2-server/src/main/java/org/graylog2/database/grouping/EntityFieldGroupingService.java
@@ -0,0 +1,14 @@
+package org.graylog2.database.grouping;
+
+import org.apache.shiro.subject.Subject;
+
+import java.util.List;
+
+public interface EntityFieldGroupingService {
+
+    List<EntityFieldGroup> groupByField(String collectionName,
+                                        String fieldName,
+                                        String query,
+                                        String groupFilter,
+                                        Subject subject);
+}
