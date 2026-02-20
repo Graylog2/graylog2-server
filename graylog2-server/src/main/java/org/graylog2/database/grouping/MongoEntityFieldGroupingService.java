9784d56ee4805d5d8a0cac3485b3b22715201588
luk-kaminski <lukasz.kaminski@graylog.com>
2026-02-20 14:50:38 +0100

    License headers added

--- graylog2-server/src/main/java/org/graylog2/database/grouping/MongoEntityFieldGroupingService.java
+++ graylog2-server/src/main/java/org/graylog2/database/grouping/MongoEntityFieldGroupingService.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 package org.graylog2.database.grouping;
 
 import com.google.common.base.Strings;
@@ -61,6 +77,7 @@ public class MongoEntityFieldGroupingService implements EntityFieldGroupingServi
             final var bucketsFilterBson = !Strings.isNullOrEmpty(bucketsFilter)
                     ? Filters.regex(ID_FIELD_NAME, bucketsFilter, "i")
                     : Filters.empty();
+            
             final int total = getTotalNumberOfBuckets(fieldName, mongoCollection, queryFilterBson, bucketsFilterBson);
             final List<EntityFieldBucket> paginatedAndSortedResults = getPage(fieldName, page, pageSize, sortOrder, sortField, mongoCollection, queryFilterBson, bucketsFilterBson);
 
