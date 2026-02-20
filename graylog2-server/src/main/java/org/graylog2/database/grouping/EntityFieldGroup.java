035fe3136bb263561501ef448e56140650310a77
luk-kaminski <lukasz.kaminski@graylog.com>
2026-02-20 11:04:14 +0100

    Refactoring, renaming, pagination with total field in response

--- graylog2-server/src/main/java/org/graylog2/database/grouping/EntityFieldGroup.java
+++
@@ -1,9 +0,0 @@
-package org.graylog2.database.grouping;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-//TODO: unify field names and this record name with Slice record from unmerged Jan's PR
-public record EntityFieldGroup(@JsonProperty("id") String fieldValue,
-                               @JsonProperty("value") String fieldTitle,
-                               @JsonProperty("count") long count) {
-}
