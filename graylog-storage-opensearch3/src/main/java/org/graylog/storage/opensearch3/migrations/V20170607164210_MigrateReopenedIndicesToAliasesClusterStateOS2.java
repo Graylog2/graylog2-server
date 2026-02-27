c00c131f5a5d14949b47404d7f1181cc64a7a77d
Matthias Oesterheld <matthias.oesterheld@graylog.com>
2026-02-27 12:51:09 +0100

    remove PlainJsonApi

--- graylog-storage-opensearch3/src/main/java/org/graylog/storage/opensearch3/migrations/V20170607164210_MigrateReopenedIndicesToAliasesClusterStateOS2.java
+++ graylog-storage-opensearch3/src/main/java/org/graylog/storage/opensearch3/migrations/V20170607164210_MigrateReopenedIndicesToAliasesClusterStateOS2.java
@@ -18,7 +18,7 @@ package org.graylog.storage.opensearch3.migrations;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.inject.Inject;
-import org.graylog.storage.opensearch3.PlainJsonApi;
+import org.graylog.storage.opensearch3.OfficialOpensearchClient;
 import org.graylog2.migrations.V20170607164210_MigrateReopenedIndicesToAliases;
 import org.opensearch.client.opensearch.generic.Request;
 import org.opensearch.client.opensearch.generic.Requests;
@@ -26,16 +26,16 @@ import org.opensearch.client.opensearch.generic.Requests;
 import java.util.Collection;
 
 public class V20170607164210_MigrateReopenedIndicesToAliasesClusterStateOS2 implements V20170607164210_MigrateReopenedIndicesToAliases.ClusterState {
-    private final PlainJsonApi plainJsonApi;
+    private final OfficialOpensearchClient client;
 
     @Inject
-    public V20170607164210_MigrateReopenedIndicesToAliasesClusterStateOS2(PlainJsonApi plainJsonApi) {
-        this.plainJsonApi = plainJsonApi;
+    public V20170607164210_MigrateReopenedIndicesToAliasesClusterStateOS2(OfficialOpensearchClient client) {
+        this.client = client;
     }
 
     @Override
     public JsonNode getForIndices(Collection<String> indices) {
-        return plainJsonApi.performRequest(request(indices),
+        return client.performRequest(request(indices),
                 "Couldn't read cluster state for reopened indices " + indices);
     }
 
