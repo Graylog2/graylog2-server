fe873988d8cb73cb8b82c635abb0ecdf4ba8db51
Linus Pahl <linus.pahl@graylog.com>
2026-03-02 09:25:42 +0100

    Migrate `LinkToNode` tofunctional component.

--- graylog2-web-interface/src/components/common/LinkToNode.tsx
+++ graylog2-web-interface/src/components/common/LinkToNode.tsx
@@ -21,7 +21,7 @@ import Routes from 'routing/Routes';
 import AppConfig from 'util/AppConfig';
 import HideOnCloud from 'util/conditional/HideOnCloud';
 import { NodesStore } from 'stores/nodes/NodesStore';
-import connect from 'stores/connect';
+import { useStore } from 'stores/connect';
 
 import Icon from './Icon';
 import Spinner from './Spinner';
@@ -29,7 +29,6 @@ import Spinner from './Spinner';
 type LinkToNodeProps = {
   /** Node ID that will be used to generate the link. */
   nodeId: string;
-  nodes?: any;
 };
 
 /**
@@ -41,49 +40,35 @@ type LinkToNodeProps = {
  * All this information will be obtained from the `NodesStore`.
  */
 
-class LinkToNode extends React.PureComponent<
-  LinkToNodeProps,
-  {
-    [key: string]: any;
+const LinkToNode = ({ nodeId }: LinkToNodeProps) => {
+  const nodes = useStore(NodesStore, (state) => state?.nodes);
+
+  if (!nodes) {
+    return <Spinner />;
   }
-> {
-  static defaultProps = {
-    nodes: undefined,
-  };
 
-  render() {
-    const { nodes } = this.props;
+  const node = nodes[nodeId];
 
-    if (!nodes) {
-      return <Spinner />;
+  if (node) {
+    const iconName = node.is_leader ? 'star' : 'fork_right';
+    const iconClass = node.is_leader ? 'leader-node' : '';
+    const iconTitle = node.is_leader ? 'This is the leader node in the cluster' : '';
+
+    const content = (
+      <>
+        <Icon name={iconName} className={iconClass} title={iconTitle} /> {node.short_node_id}
+        <HideOnCloud> / {node.hostname}</HideOnCloud>
+      </>
+    );
+
+    if (AppConfig.isCloud()) {
+      return content;
     }
 
-    const node = nodes[this.props.nodeId];
-
-    if (node) {
-      const iconName = node.is_leader ? 'star' : 'fork_right';
-      const iconClass = node.is_leader ? 'leader-node' : '';
-      const iconTitle = node.is_leader ? 'This is the leader node in the cluster' : '';
-
-      const content = (
-        <>
-          <Icon name={iconName} className={iconClass} title={iconTitle} /> {node.short_node_id}
-          <HideOnCloud> / {node.hostname}</HideOnCloud>
-        </>
-      );
-
-      if (AppConfig.isCloud()) {
-        return content;
-      }
-
-      return <Link to={Routes.SYSTEM.CLUSTER.NODE_SHOW(this.props.nodeId)}>{content}</Link>;
-    }
-
-    return <i>Unknown Node</i>;
+    return <Link to={Routes.SYSTEM.CLUSTER.NODE_SHOW(nodeId)}>{content}</Link>;
   }
-}
 
-export default connect(LinkToNode, { nodeStore: NodesStore }, ({ nodeStore, ...rest }) => ({
-  ...rest,
-  nodes: nodeStore.nodes,
-}));
+  return <i>Unknown Node</i>;
+};
+
+export default LinkToNode;
