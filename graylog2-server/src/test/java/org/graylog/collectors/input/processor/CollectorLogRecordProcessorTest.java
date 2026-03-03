8ff4470e266f92e25523217b0a8bcec3d59bd0b1
Othello Maurer <othello@graylog.com>
2026-03-03 12:41:57 +0000

    Add minimal CollectorLogRecordProcessor for collector self-logs

---
+++ graylog2-server/src/test/java/org/graylog/collectors/input/processor/CollectorLogRecordProcessorTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.collectors.input.processor;
+
+import io.opentelemetry.proto.common.v1.AnyValue;
+import io.opentelemetry.proto.common.v1.KeyValue;
+import io.opentelemetry.proto.logs.v1.LogRecord;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CollectorLogRecordProcessorTest {
+
+    private final CollectorLogRecordProcessor processor = new CollectorLogRecordProcessor();
+
+    @Test
+    void processesEmptyLogRecord() {
+        final var logRecord = LogRecord.newBuilder().build();
+        final var result = processor.process(logRecord);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void extractsServiceName() {
+        // TODO: Update once we finalize the schema with real collector self-log payloads.
+        final var logRecord = LogRecord.newBuilder()
+                .addAttributes(KeyValue.newBuilder()
+                        .setKey("service.name")
+                        .setValue(AnyValue.newBuilder().setStringValue("otel-collector").build())
+                        .build())
+                .build();
+        final var result = processor.process(logRecord);
+        assertThat(result).containsEntry("collector_service_name", "otel-collector");
+    }
+}
