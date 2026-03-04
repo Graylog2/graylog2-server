d0462b2e23ca7169bb91d830fce46d5e77b6f062
Dennis Oelkers <dennis@graylog.com>
2026-03-04 10:45:27 +0100

    Assigning stable ids to direction radio inputs.

--- graylog2-web-interface/src/views/components/aggregationwizard/grouping/GroupingsConfiguration.tsx
+++ graylog2-web-interface/src/views/components/aggregationwizard/grouping/GroupingsConfiguration.tsx
@@ -116,7 +116,6 @@ const GroupingsConfiguration = () => {
             items={groupBy?.groupings}
             onMoveItem={(newGroupings) => setFieldValue('groupBy.groupings', newGroupings)}
             customListItemRender={GroupingsItem}
-            displayOverlayInPortal
           />
         )}
       />
