69059d1809c46728c46d01fda08467ff4d3cf53a
Ryan Carroll <ryan.carroll@graylog.com>
2026-03-11 19:53:27 -0500

    cleanup

--- graylog2-server/src/main/java/org/graylog/plugins/views/search/searchfilters/model/UsedSearchFilter.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/search/searchfilters/model/UsedSearchFilter.java
@@ -17,7 +17,6 @@
 package org.graylog.plugins.views.search.searchfilters.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.graylog2.contentpacks.ContentPackable;
