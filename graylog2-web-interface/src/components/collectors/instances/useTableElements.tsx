a91a0e36157dab79c2ad127b5f01c6f854e43977
Kay Roepke <kroepke@googlemail.com>
2026-03-16 17:53:55 +0100

    feat(collectors): add MoreActions and bulk actions to instance table

--- graylog2-web-interface/src/components/collectors/instances/useTableElements.tsx
+++ graylog2-web-interface/src/components/collectors/instances/useTableElements.tsx
@@ -15,19 +15,25 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
-import { Button, ButtonToolbar } from 'components/bootstrap';
+import { Button, ButtonToolbar, MenuItem } from 'components/bootstrap';
 import { LinkContainer } from 'components/common';
+import { MoreActions } from 'components/common/EntityDataTable';
 
 import collectorLogsUrl from '../common/collectorLogsUrl';
 import type { CollectorInstanceView } from '../types';
+
+import ReassignFleetModal from './ReassignFleetModal';
+import BulkActions from './BulkActions';
 
 type Props = {
   onInstanceClick: (instance: CollectorInstanceView) => void;
 };
 
 const useTableElements = ({ onInstanceClick }: Props) => {
+  const [reassigningInstance, setReassigningInstance] = useState<CollectorInstanceView | null>(null);
+
   const entityActions = useCallback(
     (instance: CollectorInstanceView) => (
       <ButtonToolbar>
@@ -39,13 +45,30 @@ const useTableElements = ({ onInstanceClick }: Props) => {
         <Button bsSize="xsmall" onClick={() => onInstanceClick(instance)}>
           Details
         </Button>
+        <MoreActions>
+          <MenuItem onSelect={() => setReassigningInstance(instance)}>
+            Reassign to fleet
+          </MenuItem>
+        </MoreActions>
       </ButtonToolbar>
     ),
     [onInstanceClick],
   );
 
+  const bulkActions = <BulkActions />;
+
+  const renderReassignModal = reassigningInstance ? (
+    <ReassignFleetModal
+      instanceUids={[reassigningInstance.instance_uid]}
+      currentFleetId={reassigningInstance.fleet_id}
+      onClose={() => setReassigningInstance(null)}
+    />
+  ) : null;
+
   return {
     entityActions,
+    bulkActions,
+    renderReassignModal,
   };
 };
 
