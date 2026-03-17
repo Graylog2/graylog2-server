3281bbf730a16144c0a9ed94b253510f3cb15a89
Dan Torrey <dan.torrey@graylog.com>
2026-03-17 15:42:24 -0500

    Fix missing useMemo import in AdapterForm.tsx

--- graylog2-web-interface/src/components/lookup-tables/adapter-form/AdapterForm.tsx
+++ graylog2-web-interface/src/components/lookup-tables/adapter-form/AdapterForm.tsx
@@ -14,7 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { Formik, Form } from 'formik';
 
