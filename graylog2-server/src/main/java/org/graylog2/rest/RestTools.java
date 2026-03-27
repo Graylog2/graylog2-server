5be9dff3714eb4d8bde25103f10a91c46276ec3a
Othello Maurer <othello@graylog.com>
2026-03-27 14:35:06 +0100

    Remove unused import

--- graylog2-server/src/main/java/org/graylog2/rest/RestTools.java
+++ graylog2-server/src/main/java/org/graylog2/rest/RestTools.java
@@ -38,7 +38,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
