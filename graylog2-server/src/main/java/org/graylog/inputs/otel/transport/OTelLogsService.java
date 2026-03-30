313d5af94852a510db48e3275af57f7e536a2304
patrickmann <patrick.mann@graylog.com>
2026-03-30 12:14:47 +0200

    resolve conflict

--- graylog2-server/src/main/java/org/graylog/inputs/otel/transport/OTelLogsService.java
+++ graylog2-server/src/main/java/org/graylog/inputs/otel/transport/OTelLogsService.java
@@ -27,7 +27,6 @@ import io.opentelemetry.proto.collector.logs.v1.LogsServiceGrpc;
 import jakarta.inject.Inject;
 import org.graylog.inputs.otel.OTelJournal;
 import org.graylog.inputs.otel.OTelJournalRecordFactory;
-
 import org.graylog2.plugin.inputs.MessageInput;
 import org.graylog2.plugin.inputs.transports.ThrottleableTransport2;
 import org.graylog2.plugin.journal.RawMessage;
@@ -40,13 +39,16 @@ import static org.graylog.inputs.grpc.GrpcUtils.createThrottledStatusRuntimeExce
 import static org.graylog.inputs.grpc.RemoteAddressProviderInterceptor.REMOTE_ADDRESS;
 
 public class OTelLogsService extends LogsServiceGrpc.LogsServiceImplBase {
+    private final OTelJournalRecordFactory journalRecordFactory;
     private final ThrottleableTransport2 transport;
     private final MessageInput input;
 
     @Inject
-    public OTelLogsService(@Assisted ThrottleableTransport2 transport, @Assisted MessageInput input) {
+    public OTelLogsService(@Assisted ThrottleableTransport2 transport, @Assisted MessageInput input,
+                           OTelJournalRecordFactory journalRecordFactory) {
         this.transport = transport;
         this.input = input;
+        this.journalRecordFactory = journalRecordFactory;
     }
 
     public interface Factory {
