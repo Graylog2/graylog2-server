01055fb919cc9a8c95a44607056ea08e37d1e5b4
Bernd Ahlers <bernd@graylog.com>
2026-04-10 11:44:37 +0200

    Use "Collector System Logs" for the self-logs stream and index-set
    The old "Collector Logs" name was ambiguous.

--- graylog2-server/src/main/java/org/graylog/collectors/migrations/V20260303120000_CollectorDEVMigrations.java
+++ graylog2-server/src/main/java/org/graylog/collectors/migrations/V20260303120000_CollectorDEVMigrations.java
@@ -17,8 +17,18 @@
 package org.graylog.collectors.migrations;
 
 import jakarta.inject.Inject;
+import jakarta.inject.Provider;
+import org.graylog.collectors.CollectorLogsDestinationService;
+import org.graylog.security.UserContext;
 import org.graylog2.database.MongoConnection;
+import org.graylog2.database.NotFoundException;
+import org.graylog2.indexer.indexset.IndexSetService;
+import org.graylog2.indexer.indices.jobs.IndexSetCleanupJob;
 import org.graylog2.migrations.Migration;
+import org.graylog2.plugin.streams.Stream;
+import org.graylog2.shared.users.UserService;
+import org.graylog2.streams.StreamGuardException;
+import org.graylog2.streams.StreamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,10 +42,25 @@ public class V20260303120000_CollectorDEVMigrations extends Migration {
     private static final Logger LOG = LoggerFactory.getLogger(V20260303120000_CollectorDEVMigrations.class);
 
     private final MongoConnection mongoConnection;
+    private final Provider<StreamService> streamServiceProvider;
+    private final Provider<IndexSetService> indexSetServiceProvider;
+    private final Provider<IndexSetCleanupJob.Factory> indexSetCleanupJobFactoryProvider;
+    private final Provider<UserService> userServiceProvider;
+    private final Provider<CollectorLogsDestinationService> logsDestinationServiceProvider;
 
     @Inject
-    public V20260303120000_CollectorDEVMigrations(MongoConnection mongoConnection) {
+    public V20260303120000_CollectorDEVMigrations(MongoConnection mongoConnection,
+                                                  Provider<StreamService> streamServiceProvider,
+                                                  Provider<IndexSetService> indexSetServiceProvider,
+                                                  Provider<IndexSetCleanupJob.Factory> indexSetCleanupJobFactoryProvider,
+                                                  Provider<UserService> userServiceProvider,
+                                                  Provider<CollectorLogsDestinationService> logsDestinationServiceProvider) {
         this.mongoConnection = mongoConnection;
+        this.streamServiceProvider = streamServiceProvider;
+        this.indexSetServiceProvider = indexSetServiceProvider;
+        this.indexSetCleanupJobFactoryProvider = indexSetCleanupJobFactoryProvider;
+        this.userServiceProvider = userServiceProvider;
+        this.logsDestinationServiceProvider = logsDestinationServiceProvider;
     }
 
     @Override
@@ -49,5 +74,37 @@ public class V20260303120000_CollectorDEVMigrations extends Migration {
 
         final var db = mongoConnection.getMongoDatabase();
 
+        removeLegacyCollectorLogsStream();
+
+        UserContext.<Void>runAs("admin", userServiceProvider.get(), () -> {
+            logsDestinationServiceProvider.get().ensureExists();
+            return null;
+        });
+    }
+
+    private void removeLegacyCollectorLogsStream() {
+        final var streamService = streamServiceProvider.get();
+
+        final Stream stream;
+        try {
+            stream = streamService.load(Stream.COLLECTOR_SYSTEM_LOGS_STREAM_ID);
+        } catch (NotFoundException e) {
+            LOG.debug("Stream not found, nothing to do");
+            return;
+        }
+
+        LOG.info("Removing legacy Collector Logs stream and index-set");
+
+        final var indexSet = stream.getIndexSet();
+
+        try {
+            streamService.destroy(stream);
+        } catch (NotFoundException | StreamGuardException e) {
+            throw new RuntimeException("Couldn't destroy stream", e);
+        }
+
+        if (indexSetServiceProvider.get().delete(indexSet.getConfig().id()) > 0) {
+            indexSetCleanupJobFactoryProvider.get().create(indexSet).execute();
+        }
     }
 }
