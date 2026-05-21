d6830cd59aed395ece1809b4a974f4bca1b147b2
Ousmane Samba <ousmane.samba@graylog.com>
2026-05-21 12:26:58 +0200

    Add useEntityTitles hook for bulk id -> title resolution

---
+++ graylog2-web-interface/src/hooks/useEntityTitles.ts
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import { useMemo } from 'react';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+
+import { SystemCatalog } from '@graylog/server-api';
+
+import { defaultOnError } from 'util/conditional/onError';
+
+export type EntityRef = { id: string; type: string };
+
+const fetchEntityTitles = (entities: Array<EntityRef>) => SystemCatalog.getTitles({ entities: entities as any });
+
+const useEntityTitles = (
+  entities: Array<EntityRef>,
+): {
+  titlesById: Record<string, string>;
+  notPermittedIds: Set<string>;
+  isInitialLoading: boolean;
+  isError: boolean;
+} => {
+  const stableEntities = useMemo(() => [...entities].sort((a, b) => a.id.localeCompare(b.id)), [entities]);
+  const enabled = stableEntities.length > 0;
+
+  const { data, isInitialLoading, isError } = useQuery({
+    queryKey: ['entity_titles', stableEntities],
+    queryFn: () =>
+      defaultOnError(
+        fetchEntityTitles(stableEntities),
+        'Loading entity titles failed with status',
+        'Could not load entity titles',
+      ),
+    enabled,
+    placeholderData: keepPreviousData,
+  });
+
+  const titlesById = useMemo(() => {
+    const result: Record<string, string> = {};
+    (data?.entities ?? []).forEach((entry) => {
+      result[entry.id] = entry.title;
+    });
+
+    return result;
+  }, [data]);
+
+  const notPermittedIds = useMemo(() => new Set(data?.not_permitted_to_view ?? []), [data]);
+
+  return {
+    titlesById,
+    notPermittedIds,
+    isInitialLoading,
+    isError,
+  };
+};
+
+export default useEntityTitles;
