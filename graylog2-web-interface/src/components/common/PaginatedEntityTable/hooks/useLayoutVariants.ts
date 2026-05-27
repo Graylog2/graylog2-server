0ccf296c73eaf41b82663983d3654fb09e690b96
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2026-05-27 16:31:03 +0200

    use server-api

--- graylog2-web-interface/src/components/common/PaginatedEntityTable/hooks/useLayoutVariants.ts
+++ graylog2-web-interface/src/components/common/PaginatedEntityTable/hooks/useLayoutVariants.ts
@@ -17,9 +17,9 @@
 
 import { useQuery } from '@tanstack/react-query';
 
-import fetch from 'logic/rest/FetchProvider';
-import { qualifyUrl } from 'util/URLUtils';
-import type {TimeRange} from 'views/logic/queries/Query';
+import { EntityLists } from '@graylog/server-api';
+
+import type { TimeRange } from 'views/logic/queries/Query';
 
 type Props = {
   entityListId: string;
@@ -48,23 +48,24 @@ type LayoutVariant = {
   }>;
 };
 
-const url = ({ entityListId }: Props): string => qualifyUrl(`/entitylists/preferences/list_predefined/${entityListId}`);
-
-const fetchLayoutVariants = (props: Props): Promise<Array<LayoutVariant>> =>
-  fetch('POST', url(props), props.timerange).then((response: Array<LayoutVariantJSON>) =>
-    response.map(({ display_name, layout_variant, entity_list_id, metrics }) => ({
-      displayName: display_name,
-      entityListId: entity_list_id,
-      layoutVariant: layout_variant,
-      metrics: metrics?.map(({ value, metric_name, meaning }) => ({
-        metricName: metric_name,
-        meaning,
-        value,
+const fetchLayoutVariants = ({ entityListId }: Props): Promise<Array<LayoutVariant>> =>
+  EntityLists.listPredefined(entityListId, { type: 'relative', range: 2042592000 }).then(
+    (response: Array<LayoutVariantJSON>) =>
+      response.map(({ display_name, layout_variant, entity_list_id, metrics }) => ({
+        displayName: display_name,
+        entityListId: entity_list_id,
+        layoutVariant: layout_variant,
+        metrics: metrics?.map(({ value, metric_name, meaning }) => ({
+          metricName: metric_name,
+          meaning,
+          value,
+        })),
       })),
-    })),
   );
 
 const useLayoutVariants = (props: Props) => {
+  console.log({ props })
+
   const { data = [], isFetching } = useQuery({
     queryKey: ['layout-variants', props],
     queryFn: () => fetchLayoutVariants(props),
