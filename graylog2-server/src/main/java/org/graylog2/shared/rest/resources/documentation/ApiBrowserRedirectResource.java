1b921057f36d3bcdeb712e404f273c5f444e563c
Othello Maurer <othello@graylog.com>
2026-05-28 08:26:52 +0200

    Fix missing redirect to new API browser (7.1) (#26138)
    * Fix missing redirect to new API browser (#26093)
    
    * Fix missing redirect to new API browser
    
    * add changelog
    
    (cherry picked from commit efc94f5319efa7656de74a59ae31f4f02bde3fb1)
    
    * Hide API browser redirect in OpenAPI description (#26123)
    
    Prevents a validation error like the following:
    - paths.'/api-browser{route}'. Declared path
      parameter route needs to be defined as a path
      parameter in path or operation level
    
    (cherry picked from commit 896e60b73247a77fed676991ba17d204fd636cdb)

---
+++ graylog2-server/src/main/java/org/graylog2/shared/rest/resources/documentation/ApiBrowserRedirectResource.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog2.shared.rest.resources.documentation;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.Response;
+import org.graylog2.configuration.HttpConfiguration;
+import org.graylog2.rest.RestTools;
+
+import java.net.URI;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Redirects the old API browser URLs (served under the REST API prefix) to the
+ * new frontend route at {@code /api-browser}, so that existing bookmarks keep
+ * working after the server-side Swagger UI was removed.
+ */
+@Hidden
+@Path("/api-browser{route: .*}")
+public class ApiBrowserRedirectResource {
+    private final HttpConfiguration httpConfiguration;
+
+    @Inject
+    public ApiBrowserRedirectResource(HttpConfiguration httpConfiguration) {
+        this.httpConfiguration = requireNonNull(httpConfiguration, "httpConfiguration");
+    }
+
+    @GET
+    public Response redirect(@Context HttpHeaders httpHeaders) {
+        final URI base = RestTools.buildExternalUri(httpHeaders.getRequestHeaders(),
+                httpConfiguration.getHttpExternalUri());
+        return Response.status(Response.Status.MOVED_PERMANENTLY)
+                .location(base.resolve("api-browser"))
+                .build();
+    }
+}
