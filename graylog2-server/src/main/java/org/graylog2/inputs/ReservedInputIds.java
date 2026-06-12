1413381e405970f343b9471fed0941e757ebb1c8
Othello Maurer <othello@graylog.com>
2026-06-12 11:10:08 +0200

    Make EPHEMERAL_COLLECTOR_INGEST constant final

--- graylog2-server/src/main/java/org/graylog2/inputs/ReservedInputIds.java
+++ graylog2-server/src/main/java/org/graylog2/inputs/ReservedInputIds.java
@@ -19,7 +19,7 @@ package org.graylog2.inputs;
 public class ReservedInputIds {
 
     // ID of the ephemeral collector ingest input that, on cloud setups, is system-managed and not visible to users.
-    public static String EPHEMERAL_COLLECTOR_INGEST = "000000000000000000000001";
+    public final static String EPHEMERAL_COLLECTOR_INGEST = "000000000000000000000001";
 
     private ReservedInputIds() {
     }
