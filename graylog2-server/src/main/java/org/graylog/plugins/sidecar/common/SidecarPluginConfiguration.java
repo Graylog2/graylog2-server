25875880385d98c4b58531587373bec289b54bfc
Ismail Belkacim <xd4rker@users.noreply.github.com>
2026-06-17 11:21:45 +0200

    Make Sidecar default collector paths and host variable configurable (#26199)
    * Make Sidecar default collector paths and host variable configurable
    
    * Add changelog
    
    * Add doc for sidecar_cache_time and sidecar_cache_max_size
    
    * Update misc/graylog.conf
    
    * Update docs
    
    (cherry picked from commit 05e1a79db1cd18d931755c100e4a5494300a2fb5)

--- graylog2-server/src/main/java/org/graylog/plugins/sidecar/common/SidecarPluginConfiguration.java
+++ graylog2-server/src/main/java/org/graylog/plugins/sidecar/common/SidecarPluginConfiguration.java
@@ -17,17 +17,36 @@
 package org.graylog.plugins.sidecar.common;
 
 import com.github.joschi.jadconfig.Parameter;
+import com.github.joschi.jadconfig.ValidationException;
+import com.github.joschi.jadconfig.Validator;
 import com.github.joschi.jadconfig.util.Duration;
 import com.github.joschi.jadconfig.validators.PositiveDurationValidator;
 import com.github.joschi.jadconfig.validators.PositiveIntegerValidator;
 import com.github.joschi.jadconfig.validators.StringNotEmptyValidator;
 import org.graylog2.plugin.PluginConfigBean;
 
+import java.util.regex.Pattern;
+
 public class SidecarPluginConfiguration implements PluginConfigBean {
     private static final String PREFIX = "sidecar_";
 
     @Parameter(value = PREFIX + "user", validator = StringNotEmptyValidator.class)
     private String user = "graylog-sidecar";
+
+    @Parameter(value = PREFIX + "collector_binary_dir", validator = StringNotEmptyValidator.class)
+    private String collectorBinaryDir = "/usr/lib/graylog-sidecar";
+
+    @Parameter(value = PREFIX + "spool_dir", validator = StringNotEmptyValidator.class)
+    private String spoolDir = "/var/lib/graylog-sidecar";
+
+    @Parameter(value = PREFIX + "windows_install_dir", validator = StringNotEmptyValidator.class)
+    private String windowsInstallDir = "C:\\Program Files\\Graylog\\sidecar";
+
+    @Parameter(value = PREFIX + "server_config_dir", validator = StringNotEmptyValidator.class)
+    private String serverConfigDir = "/etc/graylog/server";
+
+    @Parameter(value = PREFIX + "host_variable", validator = ConfigurationVariableNameValidator.class)
+    private String hostVariable = "graylog_host";
 
     @Parameter(value = PREFIX + "cache_time", validator = PositiveDurationValidator.class)
     private Duration cacheTime = Duration.hours(1L);
@@ -45,5 +64,41 @@ public class SidecarPluginConfiguration implements PluginConfigBean {
 
     public int getCacheMaxSize() {
         return cacheMaxSize;
+    }
+
+    public String getCollectorBinaryDir() {
+        return collectorBinaryDir;
+    }
+
+    public String getSpoolDir() {
+        return spoolDir;
+    }
+
+    public String getWindowsInstallDir() {
+        return windowsInstallDir;
+    }
+
+    public String getServerConfigDir() {
+        return serverConfigDir;
+    }
+
+    public String getHostVariable() {
+        return hostVariable;
+    }
+
+    /**
+     * Rejects values that aren't valid configuration variable names (same rule as {@code ConfigurationVariableResource}),
+     * since the value is embedded into collector templates as {@code ${user.<name>}}.
+     */
+    public static class ConfigurationVariableNameValidator implements Validator<String> {
+        private static final Pattern VALID_NAME = Pattern.compile("^[A-Za-z_][A-Za-z0-9_]*$");
+
+        @Override
+        public void validate(String name, String value) throws ValidationException {
+            if (value == null || !VALID_NAME.matcher(value).matches()) {
+                throw new ValidationException("Parameter " + name + " (\"" + value + "\") may only contain the " +
+                        "characters A-Z, a-z, 0-9, _ and must not start with a digit.");
+            }
+        }
     }
 }
