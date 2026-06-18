dbfd6ab893edbb99f879b5489fecc5eb988839ec
Dennis Oelkers <dennis@graylog.com>
2026-06-18 11:39:42 +0200

    Suppressing linter hint.

--- graylog2-web-interface/vendor.modules.js
+++ graylog2-web-interface/vendor.modules.js
@@ -14,6 +14,7 @@
  * along with this program. If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
+// eslint-disable-next-line no-undef
 module.exports = [
   '@emotion/react',
   '@mantine/core',
