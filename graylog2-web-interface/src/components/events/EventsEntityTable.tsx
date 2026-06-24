e412f734b5c42e5acdfb828471e9b8c546134b56
Jan Heise <jan.heise@graylog.com>
2026-06-24 13:46:39 +0200

    removing unnecessary escaping according to ESLint

--- graylog2-web-interface/src/components/events/EventsEntityTable.tsx
+++ graylog2-web-interface/src/components/events/EventsEntityTable.tsx
@@ -83,7 +83,7 @@ const EventsEntityTable = () => {
         bulkSelection={bulkSelection}
         topRightCol={<EventsRefreshControls />}
         middleSection={EventsWidgets}
-        simpleSearchValidationRegexp={/^(?:(?:[+-]?(?:\w+:\s*)?(?:"[^"]*"|\([^)]*\)|\[[^\]]*\]|{[^}]*}|\w+\*?))\s*(?:AND|OR|NOT)?\s*)+$/i}
+        simpleSearchValidationRegexp={/^(?:[+-]?(?:\w+:\s*)?(?:"[^"]*"|\([^)]*\)|\[[^\]]*\]|{[^}]*}|\w+\*?)\s*(?:AND|OR|NOT)?\s*)+$/i}
       />
     </EventsRefreshProvider>
   );
