1f271b3dbee8327099daa0556e2571c6e1af8497
Kay Roepke <kroepke@googlemail.com>
2026-07-22 17:44:39 +0200

    need to restate defaults for onboarding

--- graylog2-web-interface/src/components/collectors/overview/onboarding/defaultSources.ts
+++ graylog2-web-interface/src/components/collectors/overview/onboarding/defaultSources.ts
@@ -45,6 +45,22 @@ const DEFAULT_SOURCES: NewSource[] = [
     enabled: true,
     type: 'macos_unified_logging',
     config: {
+      predicate: 'subsystem IN {\n' +
+        '\'com.apple.opendirectoryd\',\n' +
+        '\'com.apple.authorization\',\n' +
+        '\'com.apple.loginwindow\',\n' +
+        '\'com.apple.securityd\',\n' +
+        '\'com.apple.TCC\',\n' +
+        '\'com.apple.alf\',\n' +
+        '\'com.apple.networkextension\',\n' +
+        '\'com.apple.DiskManagement\',\n' +
+        '\'com.apple.CoreStorage\',\n' +
+        '\'com.apple.endpointsecurity\',\n' +
+        '\'com.apple.syspolicyd\',\n' +
+        '\'com.apple.launchd\'\n' +
+        '} AND messageType >= error',
+      max_log_age: 'PT24H',
+      max_poll_interval: 'PT30S',
     },
   },
   {
