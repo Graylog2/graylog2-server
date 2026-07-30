4cc903db63306f6bf31b895ea8dea71890483a69
Linus Pahl <linus.pahl@graylog.com>
2026-07-30 10:49:21 +0200

    Update test.

--- graylog2-web-interface/src/components/welcome/FirstUseWelcome.test.tsx
+++ graylog2-web-interface/src/components/welcome/FirstUseWelcome.test.tsx
@@ -71,7 +71,7 @@ describe('FirstUseWelcome', () => {
 
     const links = screen.getAllByRole('link', { name: /continue/i });
 
-    expect(links[0]).toHaveAttribute('href', 'https://www.graylog.org');
+    expect(links[0]).toHaveAttribute('href', 'https://community.graylog.org/');
     expect(links[0]).toHaveAttribute('target', '_blank');
   });
 
@@ -93,16 +93,16 @@ describe('FirstUseWelcome', () => {
     expect(mockDismiss).toHaveBeenCalledTimes(1);
   });
 
-  it('shows the "Set up Other Sources" section when not on cloud', () => {
+  it('shows the "Set up Input" section when not on cloud', () => {
     render(<FirstUseWelcome />);
 
-    expect(screen.getByRole('heading', { name: /set up other sources/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /set up input/i })).toBeInTheDocument();
   });
 
-  it('hides the "Set up Other Sources" section on cloud', () => {
+  it('hides the "Set up Input" section on cloud', () => {
     asMock(AppConfig.isCloud).mockReturnValue(true);
     render(<FirstUseWelcome />);
 
-    expect(screen.queryByRole('heading', { name: /set up other sources/i })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: /set up input/i })).not.toBeInTheDocument();
   });
 });
