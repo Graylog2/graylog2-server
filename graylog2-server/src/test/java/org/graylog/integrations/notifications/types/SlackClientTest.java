45a1cad21a2c251acd903c30f4f0e3b14dd56ffa
Ryan Carroll <ryan.carroll@graylog.com>
2026-08-06 11:38:59 -0500

    Add config option to enforce allowlist for Slack/Teams notifications (#26814)
    * make allowlist enforcement configurable
    
    * move config option to allowlist section
    
    * cleanup
    
    * Update cl
    
    * swap temp exception for permanent for dissallowed urls
    
    * clarify comments
    
    * add tests
    
    * use correct state of enforcement flag for modal instead of default to false
    
    (cherry picked from commit cbcc671643b34520dfb1c3265199bfc2e0d1b916)

--- graylog2-server/src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
+++ graylog2-server/src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
@@ -36,7 +36,6 @@ import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SlackClientTest {
     @Rule
@@ -85,17 +84,6 @@ public class SlackClientTest {
 
         SlackClient slackClient = new SlackClient(httpClient, objectMapper);
         slackClient.send(getMessage(), server.url("/").toString());
-    }
-
-    @Test
-    public void doesNotFollowRedirects() {
-        server.enqueue(new MockResponse(302, Headers.of("Location", server.url("/redirected").toString()), ""));
-        server.enqueue(new MockResponse(200, Headers.of(), ""));
-
-        SlackClient slackClient = new SlackClient(httpClient, objectMapper);
-        assertThatThrownBy(() -> slackClient.send(getMessage(), server.url("/").toString()))
-                .isInstanceOf(PermanentEventNotificationException.class)
-                .hasMessageContaining("[2xx] but got [302]");
     }
 
     private SlackMessage getMessage() {
