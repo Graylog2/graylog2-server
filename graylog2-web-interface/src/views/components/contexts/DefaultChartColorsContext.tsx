6edc11ebedcba15755fb1f695e5243a4254508bd
Linus Pahl <linus.pahl@graylog.com>
2026-08-13 09:43:59 +0200

    Implement option to provide default chart colors.

---
+++ graylog2-web-interface/src/views/components/contexts/DefaultChartColorsContext.tsx
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+import * as React from 'react';
+
+// Overrides the app-wide default chart color palette (`defaultChartColors`) for a subtree,
+// used as the fallback for series without an explicit named color. Leave unset to keep the default palette.
+const DefaultChartColorsContext = React.createContext<Array<string> | undefined>(undefined);
+
+export default DefaultChartColorsContext;
