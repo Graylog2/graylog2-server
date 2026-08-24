824356a982cc842ca2fbcc76caf661e2db3b43e2
Dennis Oelkers <dennis@graylog.com>
2026-08-24 08:44:11 +0200

    refactor(opensearch2): carry derived values on PivotBucket, add OtherBucket

---
+++ graylog-storage-opensearch2/src/main/java/org/graylog/storage/opensearch2/views/searchtypes/pivot/OtherBucket.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package org.graylog.storage.opensearch2.views.searchtypes.pivot;
+
+import org.graylog.shaded.opensearch2.org.opensearch.core.xcontent.XContentBuilder;
+import org.graylog.shaded.opensearch2.org.opensearch.search.aggregations.Aggregations;
+import org.graylog.shaded.opensearch2.org.opensearch.search.aggregations.bucket.MultiBucketsAggregation;
+
+import java.io.IOException;
+
+import static org.graylog.plugins.views.search.aggregations.OtherBucketConstants.OTHER_BUCKET_NAME;
+
+/**
+ * Stands in for the terms that fell outside a values grouping's limit. There is no such bucket in the search
+ * response, so its series values are derived and carried on {@link PivotBucket#derivedValues()} instead. Nothing
+ * should ever walk into this bucket's aggregations: it is always a leaf.
+ */
+public class OtherBucket implements MultiBucketsAggregation.Bucket {
+    private final long docCount;
+
+    private OtherBucket(long docCount) {
+        this.docCount = docCount;
+    }
+
+    public static OtherBucket create(long docCount) {
+        return new OtherBucket(docCount);
+    }
+
+    @Override
+    public Object getKey() {
+        return OTHER_BUCKET_NAME;
+    }
+
+    @Override
+    public String getKeyAsString() {
+        return OTHER_BUCKET_NAME;
+    }
+
+    @Override
+    public long getDocCount() {
+        return this.docCount;
+    }
+
+    @Override
+    public Aggregations getAggregations() {
+        throw new IllegalStateException("The (Other) bucket is a leaf and carries no aggregations.");
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder xContentBuilder, Params params) throws IOException {
+        throw new IllegalStateException("Not implemented!");
+    }
+}
--- graylog-storage-opensearch2/src/main/java/org/graylog/storage/opensearch2/views/searchtypes/pivot/PivotBucket.java
+++ graylog-storage-opensearch2/src/main/java/org/graylog/storage/opensearch2/views/searchtypes/pivot/PivotBucket.java
@@ -19,8 +19,30 @@ package org.graylog.storage.opensearch2.views.searchtypes.pivot;
 import com.google.common.collect.ImmutableList;
 import org.graylog.shaded.opensearch2.org.opensearch.search.aggregations.bucket.MultiBucketsAggregation;
 
-public record PivotBucket(ImmutableList<String> keys, MultiBucketsAggregation.Bucket bucket) {
+import java.util.Map;
+
+/**
+ * A bucket extracted from a search response, together with the grouping keys leading to it.
+ *
+ * @param derivedValues for the synthetic {@code (Other)} bucket, the series values computed by
+ *                      {@code OtherBucketDerivation}, keyed the way {@code PivotResultProcessor} assembles its
+ *                      column keys. Empty for every ordinary bucket, whose values come from the series handlers.
+ */
+public record PivotBucket(ImmutableList<String> keys,
+                          MultiBucketsAggregation.Bucket bucket,
+                          Map<String, Object> derivedValues) {
+
     public static PivotBucket create(ImmutableList<String> keys, MultiBucketsAggregation.Bucket bucket) {
-        return new PivotBucket(keys, bucket);
+        return new PivotBucket(keys, bucket, Map.of());
+    }
+
+    public static PivotBucket createOther(ImmutableList<String> keys,
+                                          MultiBucketsAggregation.Bucket bucket,
+                                          Map<String, Object> derivedValues) {
+        return new PivotBucket(keys, bucket, derivedValues);
+    }
+
+    public boolean isOtherBucket() {
+        return !derivedValues.isEmpty();
     }
 }
