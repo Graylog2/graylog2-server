6ebcda1ffe00900f06607fa7ca582e87cdd41271
Jan Heise <jan.heise@graylog.com>
2026-09-01 15:26:18 +0200

    adding correct imports

--- full-backend-tests/src/test/java/org/graylog/pipelines/SimulatorPermissionTestsIT.java
+++ full-backend-tests/src/test/java/org/graylog/pipelines/SimulatorPermissionTestsIT.java
@@ -19,8 +19,8 @@ package org.graylog.pipelines;
 import net.bytebuddy.utility.RandomString;
 import org.apache.http.HttpStatus;
 import org.graylog.plugins.pipelineprocessor.rest.PipelineRestPermissions;
-import org.graylog.testing.completebackend.FullBackendTest;
-import org.graylog.testing.completebackend.GraylogBackendConfiguration;
+import org.graylog.testing.containermatrix.annotations.ContainerMatrixTest;
+import org.graylog.testing.containermatrix.annotations.ContainerMatrixTestsConfiguration;
 import org.graylog.testing.completebackend.apis.GraylogApiResponse;
 import org.graylog.testing.completebackend.apis.GraylogApis;
 import org.graylog.testing.completebackend.apis.Users;
@@ -33,7 +33,7 @@ import java.util.Set;
 
 import static org.graylog2.plugin.streams.Stream.DEFAULT_STREAM_ID;
 
-@GraylogBackendConfiguration
+@ContainerMatrixTestsConfiguration
 public class SimulatorPermissionTestsIT {
     private static final Map<String, Object> TEST_MESSAGE = Map.of(
                 "_id", "1",
@@ -63,17 +63,17 @@ public class SimulatorPermissionTestsIT {
         api.roles().delete(simulatorRole.properJSONPath().read("name", String.class));
     }
 
-    @FullBackendTest
+    @ContainerMatrixTest
     void testSimulateNotPermittedForReader() {
         api.forUser(Users.JOHN_DOE).simulator().simulate(DEFAULT_STREAM_ID, TEST_MESSAGE, HttpStatus.SC_UNAUTHORIZED);
     }
 
-    @FullBackendTest
+    @ContainerMatrixTest
     void testSimulatePermittedForAdmin() {
         api.forUser(Users.LOCAL_ADMIN).simulator().simulate(DEFAULT_STREAM_ID, TEST_MESSAGE, HttpStatus.SC_OK);
     }
 
-    @FullBackendTest
+    @ContainerMatrixTest
     void testSimulatePermittedForUser() {
         api.forUser(simulatorUser).simulator().simulate(DEFAULT_STREAM_ID, TEST_MESSAGE, HttpStatus.SC_OK);
     }
