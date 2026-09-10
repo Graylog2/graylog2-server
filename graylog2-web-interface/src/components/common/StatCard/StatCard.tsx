425f964ce8efda1004b4420da50b20215a305d19
Ousmane Samba <ousmane.samba@graylog.com>
2026-09-10 09:48:11 +0200

    Drop native title on hover-triggered OverlayTrigger popovers

--- graylog2-web-interface/src/components/common/StatCard/StatCard.tsx
+++ graylog2-web-interface/src/components/common/StatCard/StatCard.tsx
@@ -97,7 +97,7 @@ const StatCard = ({
   <Card $variant={variant} onClick={onClick} className={className}>
     {helpText && (
       <HelpCorner>
-        <HoverForHelp title="More info" placement="right" pullRight={false}>
+        <HoverForHelp title={label} triggerTitle="More info" placement="right" pullRight={false}>
           {helpText}
         </HoverForHelp>
       </HelpCorner>
