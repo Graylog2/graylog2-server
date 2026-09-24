37c711c344581945c51f35ba02c21071e54a2cf0
Zack King <91903901+kingzacko1@users.noreply.github.com>
2026-09-24 15:01:41 -0500

    Fix content pack parameters (#27499)
    (cherry picked from commit b2a038293c230c0a3031cd811422cf676fc95780)

--- graylog2-web-interface/src/components/content-packs/ContentPackEntitiesList.test.tsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackEntitiesList.test.tsx
@@ -15,7 +15,7 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { act, render, screen, waitFor } from 'wrappedTestingLibrary';
+import { act, render, screen, waitFor, within } from 'wrappedTestingLibrary';
 import userEvent from '@testing-library/user-event';
 
 import ContentPack from 'logic/content-packs/ContentPack';
@@ -102,5 +102,25 @@ describe('<ContentPackEntitiesList />', () => {
     await userEvent.click(await screen.findByRole('button', { name: 'Reset search' }));
 
     await screen.findByText('test');
+  });
+
+  it('should only open the edit modal of the clicked entity and apply the parameter to it', async () => {
+    const onParameterApply = jest.fn();
+    const user = setupUser();
+
+    render(<ContentPackEntitiesList contentPack={contentPack} onParameterApply={onParameterApply} />);
+
+    await user.click((await screen.findAllByRole('button', { name: 'Edit' }))[0]);
+
+    const dialog = await screen.findByRole('dialog');
+
+    expect(screen.getAllByRole('dialog')).toHaveLength(1);
+
+    const [configKeySelect, parameterSelect] = within(dialog).getAllByRole('combobox');
+    await user.selectOptions(configKeySelect, within(configKeySelect).getAllByRole('option')[1]);
+    await user.selectOptions(parameterSelect, 'TITLE');
+    await user.click(within(dialog).getByRole('button', { name: 'Apply' }));
+
+    expect(onParameterApply).toHaveBeenCalledWith(entity1.id, expect.any(String), 'TITLE');
   });
 });
